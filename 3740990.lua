4a8a6f41522b9b14cefc6b987c2a17a5f9d3736f
qiao-lima <qiao-lima@github.com>
2026-03-29 03:09:51 +0800

    Manual Update 3740990

---
+++ 3740990.lua
@@ -0,0 +1,12 @@
+-- 3740990's Lua and Manifest Created by Morrenus
+-- 烧尾宴
+-- Created: January 16, 2026 at 06:06:57 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3740990) -- 烧尾宴
+-- MAIN APP DEPOTS
+addappid(3740991, 1, "4b161b0e131cf6875c0d051b65bd89a3ab1d4694b7ea57c3b7c61f93154e523d") -- Depot 3740991
+setManifestid(3740991, "9113750523960892724", 45476268847)
