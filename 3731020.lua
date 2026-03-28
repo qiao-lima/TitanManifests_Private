d14387185dc6c4a3e372d73d5271b0ce627a94fc
qiao-lima <qiao-lima@github.com>
2026-03-29 03:09:45 +0800

    Manual Update 3731020

---
+++ 3731020.lua
@@ -0,0 +1,17 @@
+-- 3731020's Lua and Manifest Created by Morrenus
+-- Five Hearts Under One Roof season 2
+-- Created: February 06, 2026 at 15:11:01 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3731020) -- Five Hearts Under One Roof season 2
+-- MAIN APP DEPOTS
+addappid(3731021, 1, "6c8a7201c4ca604359838797d37767cef71d5a105fa1c69bd3049c2320646947") -- Depot 3731021
+setManifestid(3731021, "8885232732015123741", 26070274447)
+-- DLCS WITH DEDICATED DEPOTS
+-- Five Hearts Under One Roof season2 Photobook (AppID: 4054090)
+addappid(4054090)
+addappid(4054090, 1, "a8181a87732e029df7a081f3c88557a7ff70410aa33046867eb281dc34604c48") -- Five Hearts Under One Roof season2 Photobook - Depot 4054090
+setManifestid(4054090, "859826515876099767", 657742722)
