f6002ee994bc74cba468f38b65b0a82509271036
qiao-lima <qiao-lima@github.com>
2026-03-29 03:09:39 +0800

    Manual Update 3706400

---
+++ 3706400.lua
@@ -0,0 +1,18 @@
+-- 3706400's Lua and Manifest Created by Morrenus
+-- Little Witch Inuko
+-- Created: March 26, 2026 at 05:00:12 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3706400) -- Little Witch Inuko
+-- MAIN APP DEPOTS
+addappid(3706401, 1, "81bc3fc7a30a774b086675627b2d9593dfcec4bbf498190278ab8c20b9574a9e") -- Depot 3706401
+setManifestid(3706401, "3466400195511491283", 198425620)
+addappid(3706402, 1, "781138df4efa56957eb84f991be565725b784e7f8105d967a7e976a97fe62a09") -- Depot 3706402
+setManifestid(3706402, "7597195814832497510", 198515245)
+addappid(3706403, 1, "20b674000bea5414490ae67f3199e4e7d9b708dc7fc2c235eb6685e1449ea8b9") -- Depot 3706403
+setManifestid(3706403, "3234114958268719500", 209556715)
+addappid(3706404, 1, "4cd79330e093b26f821305da99d2f33e48f2afd1e557a5281e1481bc6b3d9cc1") -- Depot 3706404
+setManifestid(3706404, "6184061335845439891", 209505555)
