1e4dacb9bffecd52f233448c2a31f51fa75e542b
qiao-lima <qiao-lima@github.com>
2026-03-29 03:10:02 +0800

    Manual Update 3755320

---
+++ 3755320.lua
@@ -0,0 +1,14 @@
+-- 3755320's Lua and Manifest Created by Morrenus
+-- BakeryDiary
+-- Created: March 25, 2026 at 12:24:08 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3755320, 1, "4234dedaa6ba89d393ae00e771ff251a7eebe432cfc8c2ef0f610b7c3d6b1e7d") -- BakeryDiary
+-- MAIN APP DEPOTS
+addappid(3755328, 1, "35ea6296aaea8d125c82702bb9da38c6dbb6ffeffbb28c85bdd9d7b2860439ca") -- Depot 3755328
+setManifestid(3755328, "2400226015538279370", 216973338)
+addappid(3755329, 1, "7869ab30fea9ae95947be156c91fb23ac06b73e1261ff46acdd00192b91a88f1") -- Depot 3755329
+setManifestid(3755329, "6797229178052127064", 67890614)
