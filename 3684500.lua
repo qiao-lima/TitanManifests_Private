a0303c2f43b4b16576d073d8b016ca9482cec16e
qiao-lima <qiao-lima@github.com>
2026-03-29 03:09:34 +0800

    Manual Update 3684500

---
+++ 3684500.lua
@@ -0,0 +1,14 @@
+-- 3684500's Lua and Manifest Created by Morrenus
+-- 《十三州行动》三国搜打撤
+-- Created: March 15, 2026 at 15:26:20 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3684500) -- 《十三州行动》三国搜打撤
+-- MAIN APP DEPOTS
+addappid(3684501, 1, "6c47f5ad274c6091e5398b7dd4ae78751473e1049d9979f90613e2e4bca0379c") -- Depot 3684501
+setManifestid(3684501, "4741959931623678623", 1001046970)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4399030) -- 
