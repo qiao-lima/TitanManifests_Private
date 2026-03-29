e1ad7e63a9881db05334bcf60fa9b37a2466d13c
qiao-lima <qiao-lima@github.com>
2026-03-29 16:44:11 +0800

    Manual Update 3732490

---
+++ 3732490.lua
@@ -0,0 +1,12 @@
+-- 3732490's Lua and Manifest Created by Morrenus
+-- Mizuki and Me Make a Porno
+-- Created: March 28, 2026 at 17:51:29 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3732490) -- Mizuki and Me Make a Porno
+-- MAIN APP DEPOTS
+addappid(3732491, 1, "d9b5ae9ba07ec1011255b64c1e59f3fef28429888f0097aa77bb41cb2dced31c") -- Depot 3732491
+setManifestid(3732491, "8692390445075641538", 4474890501)
