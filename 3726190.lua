c54fe595049fb79c385bf212f5b0ef9c588100e5
qiao-lima <qiao-lima@github.com>
2026-04-02 20:30:49 +0800

    Manual Update 3726190

---
+++ 3726190.lua
@@ -0,0 +1,12 @@
+-- 3726190's Lua and Manifest Created by Morrenus
+-- DiceyHacker1999
+-- Created: March 31, 2026 at 20:20:38 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3726190) -- DiceyHacker1999
+-- MAIN APP DEPOTS
+addappid(3726191, 1, "dca97dc309927b3a8e09ecda8c091eaad4b2dbc82a5945da44d6ffdf48d033d8") -- Depot 3726191
+setManifestid(3726191, "6810404809026287337", 355166578)
