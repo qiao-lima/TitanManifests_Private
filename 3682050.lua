550adfe8b110b738732e6fc6de91a64c00aa76a8
qiao-lima <qiao-lima@github.com>
2026-04-05 10:55:51 +0800

    Manual Update 3682050

---
+++ 3682050.lua
@@ -0,0 +1,12 @@
+-- 3682050's Lua and Manifest Created by Morrenus
+-- Sickly Days and Summer Traces
+-- Created: December 01, 2025 at 04:28:23 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3682050) -- Sickly Days and Summer Traces
+-- MAIN APP DEPOTS
+addappid(3682051, 1, "09652578b46fb04323a0df42c60d7d41225df719e923c430f528530ae6605d61") -- Depot 3682051
+setManifestid(3682051, "6239054150390143388", 1226513932)
