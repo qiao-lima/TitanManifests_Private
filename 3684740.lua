60d15ff4e17a570813d2ad95e8800ee8e2d46941
qiao-lima <qiao-lima@github.com>
2026-04-06 21:26:29 +0800

    Manual Update 3684740

---
+++ 3684740.lua
@@ -0,0 +1,16 @@
+-- 3684740's Lua and Manifest Created by Morrenus
+-- Crown of Lust [18+] 👑
+-- Created: April 05, 2026 at 06:34:08 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(3684740) -- Crown of Lust [18+] 👑
+-- MAIN APP DEPOTS
+addappid(3684741, 1, "01d09e70d3b1711d8670933f70eadf319635272672f1df5e3c5feb663b16c7d6") -- Depot 3684741
+setManifestid(3684741, "6394363417774603358", 7092396555)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Crown of Lust   Wallpapers [18] (AppID: 4561790) - missing depot keys
+-- addappid(4561790)
