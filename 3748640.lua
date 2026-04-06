018a61814447b53dc757ab0c68ae1da5c44315c1
qiao-lima <qiao-lima@github.com>
2026-04-06 21:26:29 +0800

    Manual Update 3748640

---
+++ 3748640.lua
@@ -0,0 +1,16 @@
+-- 3748640's Lua and Manifest Created by Morrenus
+-- Wasteland Bites
+-- Created: April 05, 2026 at 12:17:22 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3748640) -- Wasteland Bites
+-- MAIN APP DEPOTS
+addappid(3748641, 1, "5b17fc5eac473f7fb9508b6274714c4a49de9887bed04dc8151f0b22f5fa6e48") -- Depot 3748641
+setManifestid(3748641, "496798876254714847", 588645908)
+addappid(3748642, 1, "421ef8a5508d474bf7c1d6ea98d13a13a02c3d93b1c702c435fd2aaadaec7dfe") -- Depot 3748642
+setManifestid(3748642, "5906741144522168761", 527594880)
+addappid(3748643, 1, "8c39bf1f2888c08d2d9471ff04ff3f75b9149f4f5f88c1a7463409e43a8b886a") -- Depot 3748643
+setManifestid(3748643, "3101752134870001204", 642688000)
