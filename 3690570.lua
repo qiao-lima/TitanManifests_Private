fbfc7e031d0b4e924410953a31851dca0b92338d
qiao-lima <qiao-lima@github.com>
2026-04-07 12:00:04 +0800

    Manual Update 3690570

---
+++ 3690570.lua
@@ -0,0 +1,17 @@
+-- 3690570's Lua and Manifest Created by Morrenus
+-- 吸血鬼大小姐想和我恋爱
+-- Created: March 17, 2026 at 18:59:22 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3690570) -- 吸血鬼大小姐想和我恋爱
+-- MAIN APP DEPOTS
+addappid(3690571, 1, "1464677d56bee9f0ae4fb2b9f669f0c87ee724daa1901794f6dafdfd44a23713") -- Depot 3690571
+setManifestid(3690571, "747062337696218696", 3657282253)
+-- DLCS WITH DEDICATED DEPOTS
+--  (AppID: 3957450)
+addappid(3957450)
+addappid(3957450, 1, "80d6608aaaec28bc72582ef09c0382be29bb28fd110dd09c78692d780ea8a519") -- Depot 3957450
+setManifestid(3957450, "4538245811215430418", 2024611)
