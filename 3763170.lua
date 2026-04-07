1ba9f206b94d3b450740755b4c8c8a49ecb19636
qiao-lima <qiao-lima@github.com>
2026-04-07 12:00:36 +0800

    Manual Update 3763170

---
+++ 3763170.lua
@@ -0,0 +1,12 @@
+-- 3763170's Lua and Manifest Created by Morrenus
+-- CreateDun
+-- Created: March 17, 2026 at 18:59:54 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3763170) -- CreateDun
+-- MAIN APP DEPOTS
+addappid(3763171, 1, "616716cc5885a83ac556507358f373d37310bd3e2dc7de67ac8a38794e9c9f30") -- Depot 3763171
+setManifestid(3763171, "5442148564828337694", 835170863)
