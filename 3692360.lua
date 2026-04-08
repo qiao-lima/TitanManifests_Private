5dc5c40398ba2c3a66d4b570daed91a678b3abf1
qiao-lima <qiao-lima@github.com>
2026-04-08 18:24:50 +0800

    Manual Update 3692360

---
+++ 3692360.lua
@@ -0,0 +1,16 @@
+-- 3692360's Lua and Manifest Created by Morrenus
+-- Akuma's Bloodrain
+-- Created: April 07, 2026 at 22:18:16 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3692360) -- Akuma's Bloodrain
+-- MAIN APP DEPOTS
+addappid(3692361, 1, "b20edc33e1ab1f4083de26aa7547f40833f719c5758e218923bd00fd910af3a4") -- Depot 3692361
+setManifestid(3692361, "3128318189939741951", 620545856)
+addappid(3692362, 1, "d91543ff984a7d8e4261c2faea0a6cbf3fe81e38c929adbdc7384c5fe60a41e9") -- Depot 3692362
+setManifestid(3692362, "510074380777140049", 581002936)
+addappid(3692363, 1, "c66573080b4fe0fc2c67f023539d5e6124349ec20ccb83127576a7f80141341a") -- Depot 3692363
+setManifestid(3692363, "6399992404960423222", 590935101)
