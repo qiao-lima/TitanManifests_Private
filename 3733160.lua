f8c29a946674c2aa5983e1840ab4166a408808e1
qiao-lima <qiao-lima@github.com>
2026-04-08 18:24:51 +0800

    Manual Update 3733160

---
+++ 3733160.lua
@@ -0,0 +1,12 @@
+-- 3733160's Lua and Manifest Created by Morrenus
+-- Unwanted Dungeon
+-- Created: April 07, 2026 at 22:17:11 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3733160) -- Unwanted Dungeon
+-- MAIN APP DEPOTS
+addappid(3733161, 1, "899422d105b5ba039e09c0d56b6e4495fafc6f69a63dbea89490807c5b96afb9") -- Depot 3733161
+setManifestid(3733161, "3151944004496526268", 363754392)
