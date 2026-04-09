83b5c73991f3ab09f581fb3e7f72e4b2a8384575
qiao-lima <qiao-lima@github.com>
2026-04-10 02:29:18 +0800

    Add 3100360

---
+++ ManifestHub-3100360/3100360.lua
@@ -0,0 +1,5 @@
+addappid(3100360)
+addappid(228989)
+setManifestid(228989,"550968249685141759")
+addappid(3100361,0,"fa4a1aac59c94142d76824ab0a8d5f943e6d8efb4adda1fb461d4fb1cd4186c4")
+addappid(3100362)
