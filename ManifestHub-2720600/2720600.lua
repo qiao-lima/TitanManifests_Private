2bcbff60eb89b102dec136238af47cfc59fff649
qiao-lima <qiao-lima@github.com>
2026-04-10 02:54:14 +0800

    Add 2720600

---
+++ ManifestHub-2720600/2720600.lua
@@ -0,0 +1,7 @@
+addappid(2720600)
+addappid(228989)
+setManifestid(228989,"550968249685141759")
+addappid(228990)
+setManifestid(228990,"1829726630299308803")
+addappid(2720601,0,"36ff4eeed2aa7b36d4e07ee0b16ea977b8153ccfe1d5ccaf889dcadbdf3370d8")
+addappid(2720602)
