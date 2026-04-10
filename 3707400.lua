c91a513198f412a088f8c25d2e39bc957b4f89da
qiao-lima <qiao-lima@github.com>
2026-04-10 12:58:17 +0800

    Manual Update 3707400

---
+++ 3707400.lua
@@ -0,0 +1,16 @@
+-- 3707400's Lua and Manifest Created by Morrenus
+-- On-Together: Virtual Co-Working
+-- Created: April 10, 2026 at 00:53:13 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3707400, 1, "7616cac0205a75bcae2d80cec61d1a31a19e841e9dd4773e747451717005aec8") -- On-Together: Virtual Co-Working
+-- MAIN APP DEPOTS
+addappid(3707401, 1, "4ebfb73a3e1787480adfc99854126d1c652e98c1f88bdc6539ec09c57067176a") -- Depot 3707401
+setManifestid(3707401, "2992605821759970820", 724977065)
+addappid(3707402, 1, "3026b3137905c00d5ab7570d745134290bad1b9303d96a329172bea9390ef25d") -- Depot 3707402
+setManifestid(3707402, "7378498547991969637", 745328806)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4376610) -- On-Together Early Bird DLC
