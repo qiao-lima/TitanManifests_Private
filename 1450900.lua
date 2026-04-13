d64767656d189d8639919f781949069c140d8cb3
qiao-lima <qiao-lima@github.com>
2026-04-13 23:48:17 +0800

    Manual Update 1450900

---
+++ 1450900.lua
@@ -0,0 +1,18 @@
+-- 1450900's Lua and Manifest Created by Morrenus
+-- Desynced
+-- Created: March 13, 2026 at 10:16:24 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1450900, 1, "08b05055bcd40b9ce8acb4cb95fc7cd00aad31f51e45ad7dce5723f07ba91a6e") -- Desynced
+-- MAIN APP DEPOTS
+addappid(1450901, 1, "d9a1fcf1a5247619a996fda01bbd2cf2143fa24e234d4cbf0973f8eebf062d03") -- Depot 1450901
+setManifestid(1450901, "3730417950993502423", 2776843254)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
