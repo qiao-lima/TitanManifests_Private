fe6a2bed27c3ab3772c787f1adc9ed4088b53eba
qiao-lima <qiao-lima@github.com>
2026-04-14 00:41:15 +0800

    Manual Update 3668370

---
+++ 3668370.lua
@@ -0,0 +1,12 @@
+-- 3668370's Lua and Manifest Created by Morrenus
+-- Night Swarm
+-- Created: March 07, 2026 at 12:21:54 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3668370) -- Night Swarm
+-- MAIN APP DEPOTS
+addappid(3668371, 1, "980df52f8f9145bce6710c1de03dd3e91fce5c30ba47bf36ef798b1217b54718") -- Depot 3668371
+setManifestid(3668371, "8247640519868048315", 1922679566)
