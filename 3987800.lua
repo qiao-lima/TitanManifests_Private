3bc68ea0e9536367ea1281dba088b566ee9c9ed7
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:06 +0800

    Manual Update 3987800

---
+++ 3987800.lua
@@ -0,0 +1,12 @@
+-- 3987800's Lua and Manifest Created by Morrenus
+-- The Ghost’s Intimate Cure
+-- Created: April 03, 2026 at 11:08:35 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3987800, 1, "b7db728bf6fc65dfad76432b6767416822805ea216c74371442e32cce5a14b12") -- The Ghost’s Intimate Cure
+-- MAIN APP DEPOTS
+addappid(3987801, 1, "50f3abf95f14dfef7d3e681f717a5b803f021fd711dd3df762e372fbf8806a9a") -- Depot 3987801
+setManifestid(3987801, "2841383487035233911", 7024522435)
