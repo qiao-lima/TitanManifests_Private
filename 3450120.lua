aaf91051465ad3c099d47ec90ed56214a994566b
qiao-lima <qiao-lima@github.com>
2026-04-14 00:36:53 +0800

    Manual Update 3450120

---
+++ 3450120.lua
@@ -0,0 +1,14 @@
+-- 3450120's Lua and Manifest Created by Morrenus
+-- Gears of Glory
+-- Created: April 09, 2026 at 14:47:03 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3450120) -- Gears of Glory
+-- MAIN APP DEPOTS
+addappid(3450121, 1, "98de0ec9ac52c1a3f9e7ada6639764ddb789a010f49f30c0e2b2c0a6dd7bf2cc") -- Depot 3450121
+setManifestid(3450121, "3348380592522981366", 746717251)
+addappid(3450122, 1, "412d88574f839634e203a973daeb9a9326a84473d87f6054e9ce3685fa1aeda6") -- Depot 3450122
+setManifestid(3450122, "8055225891677709914", 747890501)
