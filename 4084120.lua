803770b1638bb4fbf75b423cd87b612e40ca46ca
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:50 +0800

    Manual Update 4084120

---
+++ 4084120.lua
@@ -0,0 +1,14 @@
+-- 4084120's Lua and Manifest Created by Morrenus
+-- CODEX MORTIS
+-- Created: April 01, 2026 at 22:37:04 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4084120) -- CODEX MORTIS
+-- MAIN APP DEPOTS
+addappid(4084121, 1, "238ee733322b4482b56b1d72b2bdf53ee93a0ff2c6a8f826f46b19a849e75497") -- Depot 4084121
+setManifestid(4084121, "7732920380548706879", 820310652)
+addappid(4084122, 1, "a887c00c97e2076deae2eef8129d7368cec180d1f34f82c604c9654a745da84b") -- Depot 4084122
+setManifestid(4084122, "596045806979928450", 775557237)
