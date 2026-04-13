32358be6fb417665fec502c5989098e6a5482a3d
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:26 +0800

    Manual Update 4039810

---
+++ 4039810.lua
@@ -0,0 +1,25 @@
+-- 4039810's Lua and Manifest Created by Morrenus
+-- Sexorcism: Lust Confession
+-- Created: March 27, 2026 at 15:49:52 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 3
+
+-- MAIN APPLICATION
+addappid(4039810) -- Sexorcism: Lust Confession
+-- MAIN APP DEPOTS
+addappid(4039811, 1, "3098223c32b7d24fee9193e12c6cbfa8931ad41d09c088f6d0ee048cd510d781") -- Depot 4039811
+setManifestid(4039811, "7318028651527849729", 3215648717)
+-- DLCS WITH DEDICATED DEPOTS
+-- Sexorcism Lust Confession - Animation Pack (AppID: 4367150)
+addappid(4367150)
+addappid(4367150, 1, "43c8ae2f3fd2556418f9be8db2d293fc07bb283f3e2741e36897c6ecaffb079b") -- Sexorcism Lust Confession - Animation Pack - Depot 4367150
+setManifestid(4367150, "3827692859207533912", 1608957895)
+-- Sexorcism Lust Confession - Digital Artbook (AppID: 4367160)
+addappid(4367160)
+addappid(4367160, 1, "a695a0fc78b35bdce9be3054a556214e1372782b99ad228b5aa8590b77b1d494") -- Sexorcism Lust Confession - Digital Artbook - Depot 4367160
+setManifestid(4367160, "7096268541005893111", 824828664)
+-- Sexorcism Lust Confession - Wallpapers Pack (AppID: 4367170)
+addappid(4367170)
+addappid(4367170, 1, "00b56cd23ceb3abbd3be54a4b505494e0f6384cf023010bf1a0a378173f31fd5") -- Sexorcism Lust Confession - Wallpapers Pack - Depot 4367170
+setManifestid(4367170, "3579863401802762217", 390043949)
