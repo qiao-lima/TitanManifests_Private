360b70a90a0fe1d309c049a3ad7e82965230b457
qiao-lima <qiao-lima@github.com>
2026-04-13 23:52:42 +0800

    Manual Update 1820000

---
+++ 1820000.lua
@@ -0,0 +1,12 @@
+-- 1820000's Lua and Manifest Created by Morrenus
+-- Homura Hime
+-- Created: March 07, 2026 at 14:23:56 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1820000) -- Homura Hime
+-- MAIN APP DEPOTS
+addappid(1820001, 1, "d2cd36f5c31810a42e9f6042ad4218698a686e24251cb65c3a3547b4dece81df") -- Depot 1820001
+setManifestid(1820001, "7038807815375639532", 37653928523)
