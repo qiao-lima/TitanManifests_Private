61a78bdb9458bb27d76ce1b1a3ae4d57f8c57123
qiao-lima <qiao-lima@github.com>
2026-04-13 23:42:55 +0800

    Manual Update 1340140

---
+++ 1340140.lua
@@ -0,0 +1,12 @@
+-- 1340140's Lua and Manifest Created by Morrenus
+-- DRACU-RIOT!
+-- Created: March 05, 2026 at 00:49:38 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1340140) -- DRACU-RIOT!
+-- MAIN APP DEPOTS
+addappid(1340141, 1, "acb4de520f422632f5cecc9fdf4e471dd6ae4beeb4c14f64db1a4e7985bb65fc") -- Depot 1340141
+setManifestid(1340141, "3756056324633129020", 5712353570)
