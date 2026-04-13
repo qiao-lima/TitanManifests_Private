00a267f45680e86f2f7a6a6d246429d8688732bd
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:05 +0800

    Manual Update 3780110

---
+++ 3780110.lua
@@ -0,0 +1,14 @@
+-- 3780110's Lua and Manifest Created by Morrenus
+-- 梅染倾情琉璃塔
+-- Created: March 12, 2026 at 12:35:21 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3780110, 1, "beeb730de44a64153fb2ceddff98c41c7b201032f0f23082ca2df65dc0bf4ec1") -- 梅染倾情琉璃塔
+-- MAIN APP DEPOTS
+addappid(3780111, 1, "319f6b053580fc8d619d4e3368aa047ef25db597337d4fe8b805cc2fbb1d4073") -- Depot 3780111
+setManifestid(3780111, "1843197741351831762", 229916654)
+addappid(3780112, 1, "4bef6eee77172c606769fd4cbec7b188db6d151f36db4a878013181b72e89610") -- Depot 3780112
+setManifestid(3780112, "5216826125196044043", 222725977)
