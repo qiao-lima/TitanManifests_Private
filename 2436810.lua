bf35a1dbcb663b9600a080e52a2682a82f57a5a5
qiao-lima <qiao-lima@github.com>
2026-04-14 00:07:52 +0800

    Manual Update 2436810

---
+++ 2436810.lua
@@ -0,0 +1,12 @@
+-- 2436810's Lua and Manifest Created by Morrenus
+-- Go! Go! Mister Chickums
+-- Created: April 02, 2026 at 10:02:36 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2436810, 1, "b96c565a4e0a4765166bd25a93fc3c66d8bbc919f9067adcaf9ef130386d928a") -- Go! Go! Mister Chickums
+-- MAIN APP DEPOTS
+addappid(2436811, 1, "359c9b5441069aa0943a294ad0cc8fd3e71fc5b1a931836250c4d508da8893aa") -- Depot 2436811
+setManifestid(2436811, "4125282601222701728", 390353794)
