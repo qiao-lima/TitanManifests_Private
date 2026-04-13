d5d0939b243bbfd6cf4ccc740af0e05e32460504
qiao-lima <qiao-lima@github.com>
2026-04-14 00:25:35 +0800

    Manual Update 3021100

---
+++ 3021100.lua
@@ -0,0 +1,16 @@
+-- 3021100's Lua and Manifest Created by Morrenus
+-- Five Hearts Under One Roof
+-- Created: December 26, 2025 at 03:35:57 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3021100) -- Five Hearts Under One Roof
+-- MAIN APP DEPOTS
+addappid(3021101, 1, "7e7c0976f19ebf717f8bdf5b661b1bec4163b9972058f6b40f37f41ea75a5ad5") -- Depot 3021101
+setManifestid(3021101, "7793127310082659738", 29389149948)
+addappid(3021102, 1, "b823062bd11419706eb109653048db1167267957791dbd3cd9de278b1df22fd4") -- Depot 3021102
+setManifestid(3021102, "935125386564644069", 31103974256)
+addappid(3021103, 1, "c2efb6191fffcccf257712a9903c4ec9c3b6dbd1785540d1a747756791f3eb29") -- Depot 3021103
+setManifestid(3021103, "8114336949438709065", 31079990952)
