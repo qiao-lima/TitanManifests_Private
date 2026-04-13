54522e4fc1f36ed91a1cc03800566b148c0ee943
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:03 +0800

    Manual Update 3944270

---
+++ 3944270.lua
@@ -0,0 +1,18 @@
+-- 3944270's Lua and Manifest Created by Morrenus
+-- Nude Star: Kawaii Strip Show — Backstage Secrets Revealed
+-- Created: March 21, 2026 at 13:35:36 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3944270) -- Nude Star: Kawaii Strip Show — Backstage Secrets Revealed
+-- MAIN APP DEPOTS
+addappid(3944271, 1, "b15185371fb46cb52064de20dd13137316aa7a0360ef307f34d307e7d037b7e2") -- Depot 3944271
+setManifestid(3944271, "3671949629416617779", 17212774304)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
