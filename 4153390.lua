62e28271a79d45ae74c4cff6c517809c4d7f8a29
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:10 +0800

    Manual Update 4153390

---
+++ 4153390.lua
@@ -0,0 +1,12 @@
+-- 4153390's Lua and Manifest Created by Morrenus
+-- 王朝：将军觐见
+-- Created: April 05, 2026 at 19:05:55 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4153390) -- 王朝：将军觐见
+-- MAIN APP DEPOTS
+addappid(4153391, 1, "0e5724a219f6b14e9c92ade649d35c6697d903953313767125711acbc8c91001") -- Depot 4153391
+setManifestid(4153391, "3401289124127302082", 2024252909)
