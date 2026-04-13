f45f1ec7cb826964858a424158c6ed9fee6c9673
qiao-lima <qiao-lima@github.com>
2026-04-13 19:32:53 +0800

    Manual Update 3714370

---
+++ 3714370.lua
@@ -0,0 +1,14 @@
+-- 3714370's Lua and Manifest Created by Morrenus
+-- NoSlack Pets: Lo-Fi Paws
+-- Created: April 12, 2026 at 04:38:21 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3714370) -- NoSlack Pets: Lo-Fi Paws
+-- MAIN APP DEPOTS
+addappid(3714371, 1, "dfe5989b1d0c9328f99d56104af21065359a11bf8f8cf994b242bd66b345b481") -- Depot 3714371
+setManifestid(3714371, "5839233015724659241", 890048507)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4611940) -- Depot 4611940 (empty depot)
