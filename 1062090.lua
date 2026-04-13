4d930f96cee7b5e8a1ec7605091c04aae4922732
qiao-lima <qiao-lima@github.com>
2026-04-13 23:36:14 +0800

    Manual Update 1062090

---
+++ 1062090.lua
@@ -0,0 +1,20 @@
+-- 1062090's Lua and Manifest Created by Morrenus
+-- Timberborn
+-- Created: March 13, 2026 at 07:55:53 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1062090, 1, "ae26e4878b346db42adc3555b618ae1bc88ccd1cca9758d27b4e7c6e41ea64df") -- Timberborn
+-- MAIN APP DEPOTS
+addappid(1062091, 1, "e6291a6d488f1366425014f5742483a513e8771437644a531111ffc9e9674be4") -- Base Content win64
+setManifestid(1062091, "5783572110234625734", 2103443429)
+addappid(1062092, 1, "07decf196b134aeb2362a7dcdf1ebce51004f759e44961e838189f59f9943267") -- Base Content macos
+setManifestid(1062092, "7812172437232744485", 2124196123)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
