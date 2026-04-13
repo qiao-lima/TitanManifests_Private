48f70edcd10be08b6cc49fb8103fa835753acb81
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:00 +0800

    Manual Update 4258390

---
+++ 4258390.lua
@@ -0,0 +1,12 @@
+-- 4258390's Lua and Manifest Created by Morrenus
+-- 厦日恋旅
+-- Created: April 06, 2026 at 00:21:34 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4258390) -- 厦日恋旅
+-- MAIN APP DEPOTS
+addappid(4258391, 1, "061515b86b287d5c422b2152b6f395a118b3043c6e974ebac1c7eb002d2786e7") -- Depot 4258391
+setManifestid(4258391, "5912768600459952663", 26366790082)
