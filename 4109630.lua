5b96d430dc8f33bda221f0f166c9cdbbea99cdfa
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:53 +0800

    Manual Update 4109630

---
+++ 4109630.lua
@@ -0,0 +1,14 @@
+-- 4109630's Lua and Manifest Created by Morrenus
+-- Obey the Insect God
+-- Created: April 11, 2026 at 05:18:49 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4109630, 1, "21bdd37d1508a14a5cdfbb1c38d706aebdef9d4ad0bcccb88b82f7c7e76c8e61") -- Obey the Insect God
+-- MAIN APP DEPOTS
+addappid(4109631, 1, "87d180bbda4819caed8f65e6aac6cd88e77d490084f66c57d3ff08630f3fcf3b") -- Depot 4109631
+setManifestid(4109631, "3289535155846693229", 16919485784)
+addappid(4109632, 1, "7ec0697425cfd369e5611db08472b8c72850ec7a363728e1767aa709e643a6bb") -- Depot 4109632
+setManifestid(4109632, "3918464916570317036", 16927789304)
