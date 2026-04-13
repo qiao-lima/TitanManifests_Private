51d44727a94329cf0631c974fad25220f2789b09
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:23 +0800

    Manual Update 4004850

---
+++ 4004850.lua
@@ -0,0 +1,12 @@
+-- 4004850's Lua and Manifest Created by Morrenus
+-- Another Day Another Dollar
+-- Created: March 17, 2026 at 19:00:30 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4004850) -- Another Day Another Dollar
+-- MAIN APP DEPOTS
+addappid(4004851, 1, "8880f693d68474bcf23ffb15490f6ae54eedeac13f3998f2c435e3dc8b46dfa8") -- Depot 4004851
+setManifestid(4004851, "1116801528159258884", 2689225700)
