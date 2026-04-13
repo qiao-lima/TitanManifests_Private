188807d378e1801e605557526ab5266e0d1f2167
qiao-lima <qiao-lima@github.com>
2026-04-14 00:30:07 +0800

    Manual Update 3224670

---
+++ 3224670.lua
@@ -0,0 +1,14 @@
+-- 3224670's Lua and Manifest Created by Morrenus
+-- Double Date Dilemma
+-- Created: March 05, 2026 at 05:12:46 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3224670) -- Double Date Dilemma
+-- MAIN APP DEPOTS
+addappid(3224671, 1, "946888a6338f68aaf81f09e0f8f9a440453e6d328c4eabfc6473580a9c8bc0b7") -- Depot 3224671
+setManifestid(3224671, "6101722399818879054", 477685464)
+addappid(3224672, 1, "19f3b471fc90f1958f000654baa0c4f597608a5372877f333abf398514716210") -- Depot 3224672
+setManifestid(3224672, "7295384716812737189", 477344645)
