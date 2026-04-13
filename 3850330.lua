e186afd38059917223e080263b0691c54db8d50e
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:33 +0800

    Manual Update 3850330

---
+++ 3850330.lua
@@ -0,0 +1,12 @@
+-- 3850330's Lua and Manifest Created by Morrenus
+-- 书恋与君：仙子线
+-- Created: March 24, 2026 at 03:23:25 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3850330) -- 书恋与君：仙子线
+-- MAIN APP DEPOTS
+addappid(3850331, 1, "c95dcbc0883aaa2179d297080689b0318b670a42921c4fa2d46ca214681e558c") -- Depot 3850331
+setManifestid(3850331, "31245680024852256", 2968325274)
