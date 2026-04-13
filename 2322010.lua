179bc41763796a74222ca479a7141575c155ef29
qiao-lima <qiao-lima@github.com>
2026-04-14 00:06:19 +0800

    Manual Update 2322010

---
+++ 2322010.lua
@@ -0,0 +1,25 @@
+-- 2322010's Lua and Manifest Created by Morrenus
+-- God of War Ragnarök
+-- Created: September 29, 2025 at 14:54:49 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 3
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2322010) -- God of War Ragnarök
+-- MAIN APP DEPOTS
+addappid(2322011, 1, "78758c85a2a8b04770fb4fe430ac7a215a959b72e496d94f6a0ec5ac38066f49") -- Depot 2322011
+setManifestid(2322011, "151255971557114067", 188864554983)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITH DEDICATED DEPOTS
+-- God of War Ragnark - Digital Artbook (AppID: 2974340)
+addappid(2974340)
+addtoken(2974340, "14621696841799701455")
+addappid(2974340, 1, "b54ec4dbd1528067329c6ce102d3dc0501401e41b08a6cabc0c947d0c9541baf") -- God of War Ragnark - Digital Artbook - Depot 2974340
+setManifestid(2974340, "192176833100020722", 11945377)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2974320) -- God of War Ragnark - Pre-purchase Entitlement
+addappid(2974330) -- God of War Ragnark - Digital Deluxe Edition Upgrade
