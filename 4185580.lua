c94d808133017e0157b8854c911bd24bb4b94f98
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:35 +0800

    Manual Update 4185580

---
+++ 4185580.lua
@@ -0,0 +1,12 @@
+-- 4185580's Lua and Manifest Created by Morrenus
+-- Signal Zone
+-- Created: April 07, 2026 at 17:21:01 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4185580) -- Signal Zone
+-- MAIN APP DEPOTS
+addappid(4185581, 1, "24204b7179d9b62d24db9656349e13e323ca5fb6dccfa7cb7900f166b6259574") -- Depot 4185581
+setManifestid(4185581, "5639804016773361862", 245640814)
