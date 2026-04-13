75cf7a4b17716bf54f6ea8626a6a8b7733affeb1
qiao-lima <qiao-lima@github.com>
2026-04-14 00:25:33 +0800

    Manual Update 2997840

---
+++ 2997840.lua
@@ -0,0 +1,16 @@
+-- 2997840's Lua and Manifest Created by Morrenus
+-- Animalkind
+-- Created: April 02, 2026 at 08:16:55 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2997840, 1, "05125a0704ed00834979cca7f385934e4871c1a36965c5b0ac1ac130bdbd8987") -- Animalkind
+-- MAIN APP DEPOTS
+addappid(2997841, 1, "198f7d031aa9da56cf24ac737ba8b659774b0e8796114549c71aaa88ca278c6f") -- Depot 2997841
+setManifestid(2997841, "2360473996185517961", 2275859462)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
