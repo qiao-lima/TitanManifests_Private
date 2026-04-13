5cf249201eff7a5d8045f792144a5b42e994b951
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:40 +0800

    Manual Update 4210540

---
+++ 4210540.lua
@@ -0,0 +1,16 @@
+-- 4210540's Lua and Manifest Created by Morrenus
+-- Apoikia
+-- Created: March 17, 2026 at 16:36:26 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4210540) -- Apoikia
+-- MAIN APP DEPOTS
+addappid(4210541, 1, "8137df337e5610ebcffb13d31cb0cc161c5e839aaba05cb7c5a19afcd007a26f") -- Depot 4210541
+setManifestid(4210541, "7867113832248736263", 2093713917)
+addappid(4210542, 1, "fc46f5eace88bb123f7fd6a2f116e587bd9f877e70be0e28a6c5229f6a7e9a98") -- Depot 4210542
+setManifestid(4210542, "6966708131681535250", 987453935)
+addappid(4210543, 1, "3d4223e392a5d59e565cdbd260ed173b2ab5547b88854c47da29456ab55f8111") -- Depot 4210543
+setManifestid(4210543, "4534461453405413284", 1051509378)
