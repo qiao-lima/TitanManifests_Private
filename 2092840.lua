62bcf9499460ec88f1c64b06f320dbca75f5f774
qiao-lima <qiao-lima@github.com>
2026-04-14 00:02:20 +0800

    Manual Update 2092840

---
+++ 2092840.lua
@@ -0,0 +1,23 @@
+-- 2092840's Lua and Manifest Created by Morrenus
+-- The Occultist
+-- Created: April 09, 2026 at 08:33:32 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2092840) -- The Occultist
+-- MAIN APP DEPOTS
+addappid(2092841, 1, "5e4d0f245900061c32fbb61025c8bd3b4aa38919205b6fd83ba4ecadcdedcc83") -- Depot 2092841
+setManifestid(2092841, "1622735794144342122", 35360111841)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- The Occultist - Deluxe Upgrade Pack (AppID: 4012990)
+addappid(4012990)
+addappid(4012990, 1, "048df12b5011da9da9abe27ac49ce45dc2545810c56653a3a066c1281fda6c76") -- The Occultist - Deluxe Upgrade Pack - Depot 4012990
+setManifestid(4012990, "92848684427552922", 111623904)
