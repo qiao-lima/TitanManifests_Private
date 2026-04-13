4de0c2575a5b298ff44cb09fd519becd96b592c2
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:21 +0800

    Manual Update 3992010

---
+++ 3992010.lua
@@ -0,0 +1,12 @@
+-- 3992010's Lua and Manifest Created by Morrenus
+-- Spee
+-- Created: April 06, 2026 at 02:00:14 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3992010) -- Spee
+-- MAIN APP DEPOTS
+addappid(3992011, 1, "89c1809f18fce0aca69fb69b0fbd5c91b220f69176ad58d08851ae3885f515f6") -- Depot 3992011
+setManifestid(3992011, "8425150259488629083", 1785804239)
