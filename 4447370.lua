9ab560eddf28b188448e3955156e70680726a9a0
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:05 +0800

    Manual Update 4447370

---
+++ 4447370.lua
@@ -0,0 +1,12 @@
+-- 4447370's Lua and Manifest Created by Morrenus
+-- Mistress VS Wife
+-- Created: March 27, 2026 at 15:50:13 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4447370) -- Mistress VS Wife
+-- MAIN APP DEPOTS
+addappid(4447371, 1, "8f168718e2a378fdd096b82c3c77ab68f308c8bda786dbfb46e82f79e849768f") -- Depot 4447371
+setManifestid(4447371, "1726577296652060648", 484673374)
