9d5c7aff3440228ff69f4b0973f47e1e9a10390a
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:34 +0800

    Manual Update 3864890

---
+++ 3864890.lua
@@ -0,0 +1,18 @@
+-- 3864890's Lua and Manifest Created by Morrenus
+-- 星火燎原
+-- Created: April 01, 2026 at 18:15:59 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3864890) -- 星火燎原
+-- MAIN APP DEPOTS
+addappid(3864891, 1, "3610f3146bbe9cb3ff58de8feb4705bcab952a4a067634bc25d5520bcda14bc4") -- Depot 3864891
+setManifestid(3864891, "1974770645145623169", 13445578764)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
