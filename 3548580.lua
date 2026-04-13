b2b51ba38721295a0865e69f33b1f22d4be316a5
qiao-lima <qiao-lima@github.com>
2026-04-14 00:40:35 +0800

    Manual Update 3548580

---
+++ 3548580.lua
@@ -0,0 +1,14 @@
+-- 3548580's Lua and Manifest Created by Morrenus
+-- Chill with You Lo-Fi Story
+-- Created: April 03, 2026 at 06:55:22 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3548580, 1, "8625b461a430eb0ae6b7502b6ae9e753153f4a4506b62990e9d5f40d41acf6e2") -- Chill with You Lo-Fi Story
+-- MAIN APP DEPOTS
+addappid(3548581, 1, "4bd410e2ecc28dd07ee6d887a275a6b32edaeca1e6d401ccab204fe35bd9b99d") -- Depot 3548581
+setManifestid(3548581, "3909659430900873360", 1194676376)
+addappid(3548582, 1, "7f9420fcff9ed1d597a17183b7253b3cb1be15c4247e4f1894fa52b455c035fe") -- Depot 3548582
+setManifestid(3548582, "7264670868702376321", 1400317427)
