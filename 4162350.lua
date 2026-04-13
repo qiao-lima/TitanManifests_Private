8309f4bf1f27e9f67b25e703c42009846f7e61ae
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:11 +0800

    Manual Update 4162350

---
+++ 4162350.lua
@@ -0,0 +1,25 @@
+-- 4162350's Lua and Manifest Created by Morrenus
+-- Lust Manor: Leona’s Secret
+-- Created: March 14, 2026 at 11:40:50 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 3
+
+-- MAIN APPLICATION
+addappid(4162350) -- Lust Manor: Leona’s Secret
+-- MAIN APP DEPOTS
+addappid(4162351, 1, "e654fe24231e150601b18d6167b7e2b509e536d2477aeccd248473a76211c866") -- Depot 4162351
+setManifestid(4162351, "5967145975342018637", 1188036516)
+-- DLCS WITH DEDICATED DEPOTS
+-- Lust Manor Leonas Secret - Animation pack (AppID: 4355370)
+addappid(4355370)
+addappid(4355370, 1, "5a0d39c0e55a7b1507a30f1f324b020112f5e2ca2235e28f518bf4211efd4939") -- Lust Manor Leonas Secret - Animation pack - Depot 4355370
+setManifestid(4355370, "398876405214207813", 1093011081)
+-- Lust Manor Leonas Secret - Digital artbook (AppID: 4355380)
+addappid(4355380)
+addappid(4355380, 1, "2d1e67b924706492188fe0820eaa4c1355ede316901cba326306e67bc6656e4b") -- Lust Manor Leonas Secret - Digital artbook - Depot 4355380
+setManifestid(4355380, "4914319460709086440", 441735469)
+-- Lust Manor Leonas Secret - Wallpapers pack (AppID: 4355390)
+addappid(4355390)
+addappid(4355390, 1, "401d666c58d38072c0c263caa1a05b55e60cf52911803452d45da834e038f3e8") -- Lust Manor Leonas Secret - Wallpapers pack - Depot 4355390
+setManifestid(4355390, "3914484548955697784", 327874600)
