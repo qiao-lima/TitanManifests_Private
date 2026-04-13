e7008bbee11a2a740dd1c8d90077b74f7056dbb3
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:29 +0800

    Manual Update 686060

---
+++ 686060.lua
@@ -0,0 +1,12 @@
+-- 686060's Lua and Manifest Created by Morrenus
+-- Mewgenics
+-- Created: March 08, 2026 at 08:37:00 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(686060, 1, "47a5496d8d545c7aaa2a190f2d3d1b55ec5686b8a83b5ae643415dd58bdd151b") -- Mewgenics
+-- MAIN APP DEPOTS
+addappid(686061, 1, "632bb9334c4d72a84090a9998ec0bf0c8eac465df723928f022b40b6ff04119a") -- Depot 686061
+setManifestid(686061, "7665221748829835537", 4914497242)
