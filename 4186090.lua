1abb011cc4f8855d80b761651ba3d8c51f087fb6
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:35 +0800

    Manual Update 4186090

---
+++ 4186090.lua
@@ -0,0 +1,12 @@
+-- 4186090's Lua and Manifest Created by Morrenus
+-- Rock-Hard Rehab: How to Survive a Hospital Harem
+-- Created: April 06, 2026 at 09:22:32 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4186090) -- Rock-Hard Rehab: How to Survive a Hospital Harem
+-- MAIN APP DEPOTS
+addappid(4186091, 1, "02bf382181f1adcb3d020d6b028dd658a6f6b7c596627bb3fcf123ff68368f49") -- Depot 4186091
+setManifestid(4186091, "2876233733361604285", 841283301)
