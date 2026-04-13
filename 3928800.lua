e00418d6c3dfc89270128508b7933bb32d2186f2
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:01 +0800

    Manual Update 3928800

---
+++ 3928800.lua
@@ -0,0 +1,12 @@
+-- 3928800's Lua and Manifest Created by Morrenus
+-- Elena's Lewd Service
+-- Created: March 14, 2026 at 09:49:28 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3928800) -- Elena's Lewd Service
+-- MAIN APP DEPOTS
+addappid(3928803, 1, "05f5bb25b8df0bdd3a3267c09e25795239fc21983a8442b90bc3f7eb14e0ae19") -- Depot 3928803
+setManifestid(3928803, "4124106802325588555", 1037385273)
