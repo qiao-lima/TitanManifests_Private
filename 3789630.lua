4b6a278d2ea93217116368c856e2f598baddd789
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:06 +0800

    Manual Update 3789630

---
+++ 3789630.lua
@@ -0,0 +1,17 @@
+-- 3789630's Lua and Manifest Created by Morrenus
+-- 100 Hong Kong Cats
+-- Created: April 08, 2026 at 06:29:08 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3789630) -- 100 Hong Kong Cats
+-- MAIN APP DEPOTS
+addappid(3789631, 1, "4900b84ed50fc84e673cfec74370a2bc49561c785534c4b81c46dd4774b53fd8") -- Depot 3789631
+setManifestid(3789631, "1852710432249913331", 432477788)
+-- DLCS WITH DEDICATED DEPOTS
+-- 100 Hong Kong Cats  - Artbook (AppID: 3935080)
+addappid(3935080)
+addappid(3935080, 1, "0dee646cf9f6bcc28096eb740ee61278a6301bc21720603557b25d688360592c") -- 100 Hong Kong Cats  - Artbook - Depot 3935080
+setManifestid(3935080, "7008180759396391825", 64706306)
