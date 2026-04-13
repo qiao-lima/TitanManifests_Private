8109a1c2b2e6a5ed19427367ef871050bd97085f
qiao-lima <qiao-lima@github.com>
2026-04-14 00:27:12 +0800

    Manual Update 3164960

---
+++ 3164960.lua
@@ -0,0 +1,12 @@
+-- 3164960's Lua and Manifest Created by Morrenus
+-- Hold The Mine
+-- Created: March 10, 2026 at 04:25:51 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3164960) -- Hold The Mine
+-- MAIN APP DEPOTS
+addappid(3164961, 1, "d0216f5177e673a12167ca99a7069413a7accb45c5938ec328a5a9903063dcc8") -- Depot 3164961
+setManifestid(3164961, "5846874679564650201", 2140992997)
