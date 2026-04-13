4afa3e997c05dfc165b3323f58c160862ea6535b
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:36 +0800

    Manual Update 4190180

---
+++ 4190180.lua
@@ -0,0 +1,12 @@
+-- 4190180's Lua and Manifest Created by Morrenus
+-- mask of soul
+-- Created: March 28, 2026 at 02:14:47 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4190180) -- mask of soul
+-- MAIN APP DEPOTS
+addappid(4190181, 1, "793c4c1393d30cb229b5a804ea0cd48f9a1cd4d3e42238210d94f3ec75e2e60b") -- Depot 4190181
+setManifestid(4190181, "8109659058756487316", 6868238579)
