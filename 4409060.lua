e3c96e5e67261f198fd37094dfd637b0298f3dd8
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:50 +0800

    Manual Update 4409060

---
+++ 4409060.lua
@@ -0,0 +1,12 @@
+-- 4409060's Lua and Manifest Created by Morrenus
+-- SPY - Adult Only
+-- Created: March 29, 2026 at 12:35:23 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4409060) -- SPY - Adult Only
+-- MAIN APP DEPOTS
+addappid(4409061, 1, "fc6814ea758c982bdca55ce654759629e1fb75b436184a7690a491176a5ec083") -- Depot 4409061
+setManifestid(4409061, "3904912398497104116", 2107350144)
