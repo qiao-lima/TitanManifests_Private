333da55cedd367a6608ccb519cf8c41487888fe2
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:07 +0800

    Manual Update 3802800

---
+++ 3802800.lua
@@ -0,0 +1,23 @@
+-- 3802800's Lua and Manifest Created by Morrenus
+-- MADO MONOGATARI: Fia and the Wondrous Academy
+-- Created: March 25, 2026 at 21:58:52 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 2
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3802800) -- MADO MONOGATARI: Fia and the Wondrous Academy
+-- MAIN APP DEPOTS
+addappid(3802801, 1, "79072cceec2718b15b1f45ea1cc229dc627a407af077de65fc896b65df43bb74") -- Depot 3802801
+setManifestid(3802801, "6821727186403146988", 2288555121)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITH DEDICATED DEPOTS
+-- MADO MONOGATARI Fia and the Wondrous Academy - Deluxe Pack (AppID: 4248830)
+addappid(4248830)
+addappid(4248830, 1, "6a0a59d8b5d2870273e6035f40dd26ac483bb715d572c8c1651b062cafbc30a2") -- MADO MONOGATARI Fia and the Wondrous Academy - Deluxe Pack - Depot 4248830
+setManifestid(4248830, "7694251889361555657", 1135183141)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3818030) -- MADO MONOGATARI Fia and the Wondrous Academy - Apprentice Mage Enrollment Set
