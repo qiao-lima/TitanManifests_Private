88a8f2b68d9f4f8035c62d87faa6d36f595d5e8b
qiao-lima <qiao-lima@github.com>
2026-04-14 00:21:50 +0800

    Manual Update 2927700

---
+++ 2927700.lua
@@ -0,0 +1,12 @@
+-- 2927700's Lua and Manifest Created by Morrenus
+-- Fight Life: Vanguard
+-- Created: April 12, 2026 at 17:09:40 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2927700) -- Fight Life: Vanguard
+-- MAIN APP DEPOTS
+addappid(2927701, 1, "197b73936a631cfa67c320def9c65ad11aa5c0c848015ece4fe14359b4fa2fbb") -- Depot 2927701
+setManifestid(2927701, "6011424791896034", 560274304)
