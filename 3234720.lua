fd13d6134fbeffee739471532a8c213596e38aa0
qiao-lima <qiao-lima@github.com>
2026-04-14 00:30:07 +0800

    Manual Update 3234720

---
+++ 3234720.lua
@@ -0,0 +1,14 @@
+-- 3234720's Lua and Manifest Created by Morrenus
+-- Lugh World
+-- Created: April 09, 2026 at 15:21:01 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3234720) -- Lugh World
+-- MAIN APP DEPOTS
+addappid(3234721, 1, "d6d64a4d94c4e43843b4fdb58c79b0985adf2bdc0460ef18084c1de4cef4834c") -- Depot 3234721
+setManifestid(3234721, "8447234356162460783", 5779730079)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4519860) -- Lugh World - SEASON PASS  012026 WELCOME TO LUGH WORLD
