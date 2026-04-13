5f4b6ba963e9d8a4a10009d92aab932a6e2b4f5e
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:25 +0800

    Manual Update 4021870

---
+++ 4021870.lua
@@ -0,0 +1,12 @@
+-- 4021870's Lua and Manifest Created by Morrenus
+-- 聊斋：画皮鬼
+-- Created: March 10, 2026 at 04:19:08 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4021870) -- 聊斋：画皮鬼
+-- MAIN APP DEPOTS
+addappid(4021871, 1, "57dd2ea3fda164255650f106ac60a02d61f07b9ee01eb47ed44cc2f5c9f08efb") -- Depot 4021871
+setManifestid(4021871, "8586307898888690591", 4401598784)
