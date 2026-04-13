eab64b09d73d59501ae0d8e58d78f698214796ce
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:40 +0800

    Manual Update 4213990

---
+++ 4213990.lua
@@ -0,0 +1,14 @@
+-- 4213990's Lua and Manifest Created by Morrenus
+-- Panic Sell 2
+-- Created: April 08, 2026 at 07:00:45 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4213990) -- Panic Sell 2
+-- MAIN APP DEPOTS
+addappid(4213991, 1, "ee65a1605baa7a273640592126a5277d8f38aff58025309b7523bd17de60f106") -- Depot 4213991
+setManifestid(4213991, "4910246817588229384", 35602871)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4370140) -- Panic Sell 2 - GOLD
