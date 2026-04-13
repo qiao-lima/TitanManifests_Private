18ce31bfaa9164d596737d360590b3ab34d043c7
qiao-lima <qiao-lima@github.com>
2026-04-14 00:40:36 +0800

    Manual Update 3564670

---
+++ 3564670.lua
@@ -0,0 +1,16 @@
+-- 3564670's Lua and Manifest Created by Morrenus
+-- Cult Vacui
+-- Created: April 09, 2026 at 15:56:42 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3564670, 1, "9fba7f5e8d3cc9d41de6ef97ecc63bffb9cad2e785e4c269b7d0155d7970c0dc") -- Cult Vacui
+-- MAIN APP DEPOTS
+addappid(3564671, 1, "2f9c4308ef8ef2aba9c7d6f0bb084947ca34329b1014fd87b31ed0c0c4538015") -- Depot 3564671
+setManifestid(3564671, "5030796562315929163", 78522611)
+addappid(3564672, 1, "7a6053dd8a3f6e0d94c164bd6aa1655a7112bdfbbefd21db286cb154883df17e") -- Depot 3564672
+setManifestid(3564672, "789231039421845429", 107776431)
+addappid(3564673, 1, "ac56adea911b7738add375b312905a0139ffc28eb4c3e9c514af9f67759a9ee4") -- Depot 3564673
+setManifestid(3564673, "6654018480764653173", 105395772)
