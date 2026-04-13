f1cdf255b4ea1904f2fcaa001e05676f9dcfe09d
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:26 +0800

    Manual Update 4026480

---
+++ 4026480.lua
@@ -0,0 +1,12 @@
+-- 4026480's Lua and Manifest Created by Morrenus
+-- Cafe Business Manager
+-- Created: April 12, 2026 at 16:59:18 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4026480) -- Cafe Business Manager
+-- MAIN APP DEPOTS
+addappid(4026481, 1, "88a5cd641810cebf1b7d440abb9fbcbb677a99aeb9a018cecd520109c3d01d3a") -- Depot 4026481
+setManifestid(4026481, "834818382715709886", 7036296178)
