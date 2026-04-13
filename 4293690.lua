3e0cd2424de408a1067ff9840d25504702046cd3
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:23 +0800

    Manual Update 4293690

---
+++ 4293690.lua
@@ -0,0 +1,12 @@
+-- 4293690's Lua and Manifest Created by Morrenus
+-- The Late Guest
+-- Created: March 31, 2026 at 20:28:27 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4293690) -- The Late Guest
+-- MAIN APP DEPOTS
+addappid(4293691, 1, "4af64c7a3b484912a8ce2538145acb7b49d7fbbfa8042f21af3d912526741e67") -- Depot 4293691
+setManifestid(4293691, "4991521794894003825", 861794098)
