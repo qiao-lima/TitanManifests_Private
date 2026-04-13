a766b8c5cd07d8121d9ea6748313abdb3b72b2bd
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:54 +0800

    Manual Update 4119950

---
+++ 4119950.lua
@@ -0,0 +1,12 @@
+-- 4119950's Lua and Manifest Created by Morrenus
+-- The Long Winter: I Am Not an Animal
+-- Created: March 20, 2026 at 07:52:56 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4119950) -- The Long Winter: I Am Not an Animal
+-- MAIN APP DEPOTS
+addappid(4119951, 1, "71dd2764a3e34c27fbb55481b7e72b47e10fb9b8cc3567263cd633a844e1a3db") -- Depot 4119951
+setManifestid(4119951, "3080728029844226042", 2043753651)
