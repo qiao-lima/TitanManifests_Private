460d411daad3d9f5712c5d8daac48166020e76d8
qiao-lima <qiao-lima@github.com>
2026-04-14 00:21:47 +0800

    Manual Update 2890830

---
+++ 2890830.lua
@@ -0,0 +1,12 @@
+-- 2890830's Lua and Manifest Created by Morrenus
+-- Streamer Life Simulator 2
+-- Created: February 05, 2026 at 10:18:08 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2890830) -- Streamer Life Simulator 2
+-- MAIN APP DEPOTS
+addappid(2890831, 1, "f266168cbca0fdbad69d36ca8ed9187480ed8adf90c68090ebea91b92757a77e") -- Depot 2890831
+setManifestid(2890831, "3320170599888792573", 5914857185)
