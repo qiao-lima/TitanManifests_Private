10d49d9e1263d7484b6abf12ad7fbe1ad796016a
qiao-lima <qiao-lima@github.com>
2026-04-14 00:36:55 +0800

    Manual Update 3470610

---
+++ 3470610.lua
@@ -0,0 +1,18 @@
+-- 3470610's Lua and Manifest Created by Morrenus
+-- Dawn Break
+-- Created: April 10, 2026 at 13:26:20 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3470610, 1, "16839233a4666084165098e140a42c41087d94b10f2eea05b65a2c0a5dfbc1a2") -- Dawn Break
+-- MAIN APP DEPOTS
+addappid(3470611, 1, "ef35a45bc08c765df84f3d1e4f82274a19f5399c43464a4914f3fdbd9898b8e2") -- Depot 3470611
+setManifestid(3470611, "6458695526663224715", 16718583553)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
