317cbf5d624eb8a13d83e25ae907ac986b800400
qiao-lima <qiao-lima@github.com>
2026-04-14 00:21:53 +0800

    Manual Update 2975950

---
+++ 2975950.lua
@@ -0,0 +1,20 @@
+-- 2975950's Lua and Manifest Created by Morrenus
+-- Solasta II
+-- Created: March 12, 2026 at 17:23:26 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2975950) -- Solasta II
+-- MAIN APP DEPOTS
+addappid(2975951, 1, "c8f970ccdd87f395644cdde56d98dedf5ecd0a8ccb0ed86a7ddc2746c7c50888") -- Depot 2975951
+setManifestid(2975951, "1123416827650846713", 35440341132)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4045820) -- Solasta II - Supporter Pack
