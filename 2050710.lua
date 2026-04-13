edb656f741450efcf9240dbc452e33a1747e62a2
qiao-lima <qiao-lima@github.com>
2026-04-13 23:58:18 +0800

    Manual Update 2050710

---
+++ 2050710.lua
@@ -0,0 +1,12 @@
+-- 2050710's Lua and Manifest Created by Morrenus
+-- Narin: The Orange Room
+-- Created: April 07, 2026 at 11:49:05 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2050710) -- Narin: The Orange Room
+-- MAIN APP DEPOTS
+addappid(2050711, 1, "ef46f2075a4f8abe47941de848ae375b08e1f10c73e457e09213136d36923f85") -- Depot 2050711
+setManifestid(2050711, "7473496987727066420", 6197723426)
