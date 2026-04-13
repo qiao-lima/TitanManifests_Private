57bd683a51596710cd9f4d67bc280d8b928a6bc1
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:05 +0800

    Manual Update 3976500

---
+++ 3976500.lua
@@ -0,0 +1,20 @@
+-- 3976500's Lua and Manifest Created by Morrenus
+-- The Spotter: Dig or Die
+-- Created: April 07, 2026 at 14:06:28 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(3976500) -- The Spotter: Dig or Die
+-- MAIN APP DEPOTS
+addappid(3976501, 1, "c92f6b2efb79642d76c8bddedf4a55e6c3c5c2b69f4d6509b9640d2239d94ad9") -- Depot 3976501
+setManifestid(3976501, "5923375658448113564", 2451681959)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
