4173241c8e8ad97ab12fea0abbe32fe5a0370d7f
qiao-lima <qiao-lima@github.com>
2026-04-14 00:32:43 +0800

    Manual Update 3376250

---
+++ 3376250.lua
@@ -0,0 +1,12 @@
+-- 3376250's Lua and Manifest Created by Morrenus
+-- Nine-Ball Roulette
+-- Created: February 15, 2026 at 22:27:34 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3376250, 1, "71821e04c9c54f5bedce9e922da94a16d63e79a1e9a73d2695e786b121522052") -- Nine-Ball Roulette
+-- MAIN APP DEPOTS
+addappid(3376251, 1, "b8e4a7ce69ae5aef69562f43522ae837dd6bb3b36efb3a3d923d874afa21397d") -- Depot 3376251
+setManifestid(3376251, "6910792321485371631", 992594632)
