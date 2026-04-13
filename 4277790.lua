e776ec63a3c7c94b93c66b6da8830826990b11c1
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:02 +0800

    Manual Update 4277790

---
+++ 4277790.lua
@@ -0,0 +1,12 @@
+-- 4277790's Lua and Manifest Created by Morrenus
+-- Blackjack Descent
+-- Created: March 28, 2026 at 05:52:15 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4277790) -- Blackjack Descent
+-- MAIN APP DEPOTS
+addappid(4277791, 1, "484af3e24d933a4caae63d6d5d448cc31d46c6e0f0ed6cfaee2289d8f37d49bb") -- Depot 4277791
+setManifestid(4277791, "875564897438040353", 968291336)
