7ae0fde6b9010a5e1a41ee2ba8c5e4e80966cec6
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:35 +0800

    Manual Update 3891620

---
+++ 3891620.lua
@@ -0,0 +1,13 @@
+-- 3891620's Lua and Manifest Created by Morrenus
+-- 久美子之死
+-- Created: March 22, 2026 at 08:11:02 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3891620) -- 久美子之死
+addtoken(3891620, "9977985564863925903")
+-- MAIN APP DEPOTS
+addappid(3891621, 1, "1f9091dd0587da4f83df430e8099c78ffdaa2d1bb4b7b575ce3e5ac831c8248f") -- Depot 3891621
+setManifestid(3891621, "7603623320423448070", 3414431927)
