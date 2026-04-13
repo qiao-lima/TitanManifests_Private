02d682f1aabe395c8865337dd410048903f79c12
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:07 +0800

    Manual Update 2456740

---
+++ 2456740.lua
@@ -0,0 +1,27 @@
+-- 2456740's Lua and Manifest Created by Morrenus
+-- inZOI
+-- Created: February 24, 2026 at 02:07:24 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 6
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2456740, 1, "3539dc3e61ab7546b1030d8a8a44145f1ebcc241b65eb601af0d015c3b7a12a4") -- inZOI
+-- MAIN APP DEPOTS
+addappid(2456741, 1, "e91cba2d1d471bdfb115bee76ac997858f84e6ab0a82af5f17d0054f8a3c9686") -- Depot 2456741
+setManifestid(2456741, "4020009361799501483", 34248182061)
+addappid(2456742, 1, "7c9a756f88682f7ae0913792f29b1c2ad7c702cb41859ecb9965e8c8760aff0d") -- Depot 2456742
+setManifestid(2456742, "3562814636447422629", 31443556993)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- inZOI  Island Getaway (AppID: 3887680)
+addappid(3887680)
+addappid(3887680, 1, "25ed1bd4d03ac4fd1a739171c82248fe6db511beea87dccfe33721091039e9c8") -- inZOI  Island Getaway - Depot 3887680
+setManifestid(3887680, "7439076533127651681", 1237346427)
+addappid(3887681, 1, "443b555d0c838bd99b995ed172ed1d49f028e6347a1f27e3d74e648cd5718bc2") -- inZOI  Island Getaway - Depot 3887681
+setManifestid(3887681, "9145347197546821320", 1299777118)
