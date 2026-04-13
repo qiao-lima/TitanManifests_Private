236ea62101984cf3a680e13068f839886592019b
qiao-lima <qiao-lima@github.com>
2026-04-14 00:21:48 +0800

    Manual Update 2903630

---
+++ 2903630.lua
@@ -0,0 +1,12 @@
+-- 2903630's Lua and Manifest Created by Morrenus
+-- 千年一弈
+-- Created: April 06, 2026 at 01:29:22 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2903630) -- 千年一弈
+-- MAIN APP DEPOTS
+addappid(2903631, 1, "45ce6b0c4ae8cea92e8040427931374165c290b369d521bb5b1124cc0a90b44c") -- Depot 2903631
+setManifestid(2903631, "3586205724435469107", 7754888657)
