69f30ee763b310cf1db7dc509a21b788559799b3
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:39 +0800

    Manual Update 4210070

---
+++ 4210070.lua
@@ -0,0 +1,12 @@
+-- 4210070's Lua and Manifest Created by Morrenus
+-- Shadows Show
+-- Created: April 04, 2026 at 04:52:25 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4210070) -- Shadows Show
+-- MAIN APP DEPOTS
+addappid(4210071, 1, "50908d2b9e737ab02f5d1c3d592c67532179a038c86a3dd20238459b3f2c4b3f") -- Depot 4210071
+setManifestid(4210071, "8067321251967069271", 5089101858)
