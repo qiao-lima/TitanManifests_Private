523dda615192a42608045a1a9ff9eb84275b5b2b
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:47 +0800

    Manual Update 4360980

---
+++ 4360980.lua
@@ -0,0 +1,12 @@
+-- 4360980's Lua and Manifest Created by Morrenus
+-- Ocean
+-- Created: April 04, 2026 at 14:23:24 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4360980) -- Ocean
+-- MAIN APP DEPOTS
+addappid(4360981, 1, "9d953d74646b83a0efb93813f1adcaf561e7c5d0f6e79beb771a8e2aca0570d3") -- Depot 4360981
+setManifestid(4360981, "7351198501497190262", 804889845)
