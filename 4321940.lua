0ca9390c6bb72498e4e700d0e6959db95df94126
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:45 +0800

    Manual Update 4321940

---
+++ 4321940.lua
@@ -0,0 +1,12 @@
+-- 4321940's Lua and Manifest Created by Morrenus
+-- Isekai Mage Girl Gacha Pull Simulator: Elemental Card Collection
+-- Created: March 27, 2026 at 06:31:21 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4321940) -- Isekai Mage Girl Gacha Pull Simulator: Elemental Card Collection
+-- MAIN APP DEPOTS
+addappid(4321941, 1, "1633b4dad8e814233737d86c569a44bb46917003719e078d4cf2c4bfeb912148") -- Depot 4321941
+setManifestid(4321941, "5962255077657742029", 895785155)
