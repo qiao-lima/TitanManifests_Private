281678b8fbefb6c8c04cd371d958b97e72fd7075
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:13 +0800

    Manual Update 4179020

---
+++ 4179020.lua
@@ -0,0 +1,17 @@
+-- 4179020's Lua and Manifest Created by Morrenus
+-- Summer’s Heartbeat
+-- Created: March 24, 2026 at 08:21:30 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4179020, 1, "4f3a0b809e5173f93a445b68aaf60c4c14e977b33548a129b63b3242ab0691f5") -- Summer’s Heartbeat
+-- MAIN APP DEPOTS
+addappid(4179021, 1, "35ee1078d5d6394d65a30a04aa000fb8763a59b0864b0f6175ab238cc27a36ac") -- Depot 4179021
+setManifestid(4179021, "6232654673906900887", 40948549816)
+-- DLCS WITH DEDICATED DEPOTS
+-- Summers Heartbeat Photobook (AppID: 4416280)
+addappid(4416280)
+addappid(4416280, 1, "6a7b6c8d7eadcd7d13f54289e61eaddb732c658197c8e0321caae34bc347f9d8") -- Summers Heartbeat Photobook - Depot 4416280
+setManifestid(4416280, "4977967125058520664", 629457958)
