a85daea9912a643cacad9e4c4ebbfb1c5e1456d6
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:46 +0800

    Manual Update 4346710

---
+++ 4346710.lua
@@ -0,0 +1,12 @@
+-- 4346710's Lua and Manifest Created by Morrenus
+-- Bullet Head
+-- Created: March 05, 2026 at 01:52:42 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4346710) -- Bullet Head
+-- MAIN APP DEPOTS
+addappid(4346711, 1, "c95c2d58d3585ee38317b6b7b48c69e97002b3298963332f8d3b361319d0cf46") -- Depot 4346711
+setManifestid(4346711, "3008846724808121178", 6019914778)
