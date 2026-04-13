14831ef7155de4cf71df06cd8193226980d9b9d7
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:10 +0800

    Manual Update 4509020

---
+++ 4509020.lua
@@ -0,0 +1,12 @@
+-- 4509020's Lua and Manifest Created by Morrenus
+-- Country Inn, Sweet Lust
+-- Created: April 02, 2026 at 13:36:43 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4509020) -- Country Inn, Sweet Lust
+-- MAIN APP DEPOTS
+addappid(4509021, 1, "2a6dfacffd89967ff86d4252c709efeed057ee7cd1346f1cbcbeee01710c3483") -- Depot 4509021
+setManifestid(4509021, "6446431434031617980", 704244822)
