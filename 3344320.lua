386c5e124acd03d7e13a56d5c1a5287451870368
qiao-lima <qiao-lima@github.com>
2026-04-14 00:32:41 +0800

    Manual Update 3344320

---
+++ 3344320.lua
@@ -0,0 +1,12 @@
+-- 3344320's Lua and Manifest Created by Morrenus
+-- Lethal Dungeon
+-- Created: April 10, 2026 at 06:36:34 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3344320, 1, "86d8513d0451869464e3f5541fbbe291015bdf6622997a4da9e0ddd4ab70b641") -- Lethal Dungeon
+-- MAIN APP DEPOTS
+addappid(3344321, 1, "db682b1dbdc346b9273e33e3cbbe6987a3c900527abf7cfc42e0730b9f2f39e8") -- Depot 3344321
+setManifestid(3344321, "7744837249104494848", 681245881)
