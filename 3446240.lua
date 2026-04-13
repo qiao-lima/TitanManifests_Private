cf97b66ba7824fd9d8158b7e25a899d9b48fde9c
qiao-lima <qiao-lima@github.com>
2026-04-14 00:36:52 +0800

    Manual Update 3446240

---
+++ 3446240.lua
@@ -0,0 +1,21 @@
+-- 3446240's Lua and Manifest Created by Morrenus
+-- 宗门起源
+-- Created: March 28, 2026 at 10:59:08 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(3446240) -- 宗门起源
+addtoken(3446240, "12366859424522752049")
+-- MAIN APP DEPOTS
+addappid(3446241, 1, "ddbae286524f900e39fb2c0e6d35401e7a94444b87a43825104ae7a128279543") -- Depot 3446241
+setManifestid(3446241, "6694117596101214498", 8515366204)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 117381405)
