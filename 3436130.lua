c2349a28ce94ada25927c824537ab3a97cc3b2a5
qiao-lima <qiao-lima@github.com>
2026-04-14 00:36:51 +0800

    Manual Update 3436130

---
+++ 3436130.lua
@@ -0,0 +1,12 @@
+-- 3436130's Lua and Manifest Created by Morrenus
+-- Elton Manor: Requiem of the Cursed Roses
+-- Created: April 07, 2026 at 11:09:13 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3436130) -- Elton Manor: Requiem of the Cursed Roses
+-- MAIN APP DEPOTS
+addappid(3436131, 1, "2d415e8ebba7a0bd44ab710dcd995210b658ebcc36422364d4b8029d74031d81") -- Depot 3436131
+setManifestid(3436131, "4061399352754891464", 2293709787)
