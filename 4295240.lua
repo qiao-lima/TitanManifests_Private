181cba726410b2f0d0661c1c34b968dc674b0c74
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:23 +0800

    Manual Update 4295240

---
+++ 4295240.lua
@@ -0,0 +1,12 @@
+-- 4295240's Lua and Manifest Created by Morrenus
+-- Neon Vice Club Simulator
+-- Created: April 11, 2026 at 23:30:46 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4295240) -- Neon Vice Club Simulator
+-- MAIN APP DEPOTS
+addappid(4295241, 1, "1106c3edd620ebee1b3d3902b158e17f4cd8a5cd292b27eba521a61a4e7eac21") -- Depot 4295241
+setManifestid(4295241, "4228470175028088239", 7125802376)
