eff8bd7ab6fbb0c4ae5de62de9a85bb71ed48699
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:24 +0800

    Manual Update 4298880

---
+++ 4298880.lua
@@ -0,0 +1,16 @@
+-- 4298880's Lua and Manifest Created by Morrenus
+-- Timber Rush
+-- Created: March 22, 2026 at 04:33:16 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4298880) -- Timber Rush
+-- MAIN APP DEPOTS
+addappid(4298882, 1, "7cb93d1e10746be5ee9c675b88b46d893e6f0f42c2cb0c539697282704f7d757") -- Depot 4298882
+setManifestid(4298882, "7548225341908978577", 173437521)
+addappid(4298883, 1, "f7275b174229f7c306174020ce12e32a6d6a9065acfc97bb82b4b607a1e464a1") -- Depot 4298883
+setManifestid(4298883, "3871833861785908390", 190263255)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4298881) -- Depot 4298881 (empty depot)
