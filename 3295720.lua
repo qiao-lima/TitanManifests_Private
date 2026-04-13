79b8fd1fe3cc5a8f10b2e26c74bcf383e58f5d6e
qiao-lima <qiao-lima@github.com>
2026-04-14 00:32:39 +0800

    Manual Update 3295720

---
+++ 3295720.lua
@@ -0,0 +1,12 @@
+-- 3295720's Lua and Manifest Created by Morrenus
+-- 逃家少女〜蜜梨與巧克力〜
+-- Created: April 04, 2026 at 10:32:37 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3295720) -- 逃家少女〜蜜梨與巧克力〜
+-- MAIN APP DEPOTS
+addappid(3295721, 1, "f0b51f6bfae21a4e3e1eb39410869e86e9c04ead6bed5b252e27363845268166") -- Depot 3295721
+setManifestid(3295721, "7316021457044657394", 999082950)
