524fac8b7e1857edddc57703a984dc1757580730
qiao-lima <qiao-lima@github.com>
2026-04-14 00:36:50 +0800

    Manual Update 3419520

---
+++ 3419520.lua
@@ -0,0 +1,25 @@
+-- 3419520's Lua and Manifest Created by Morrenus
+-- Quarantine Zone: The Last Check
+-- Created: March 17, 2026 at 13:48:35 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 6
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3419520) -- Quarantine Zone: The Last Check
+-- MAIN APP DEPOTS
+addappid(3419521, 1, "7763e89ed36372394d459530e45e7dab3aca85588166e1c59d3bd1f7993bfac6") -- Depot 3419521
+setManifestid(3419521, "5261874958499089629", 12617996465)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3960620) -- Quarantine Zone Twitch Drop - Rat Skin - Raccoon
+addappid(3960630) -- Quarantine Zone Twitch Drop - Military Skin
+addappid(3960640) -- Quarantine Zone Twitch Drop - Scanner Skin
+addappid(3960650) -- Quarantine Zone Twitch Drop - Hammer Skin
+addappid(3960660) -- Quarantine Zone Twitch Drop - Thermopulse
+addappid(4312380) -- Quarantine Zone The Last Check Supporter Pack
