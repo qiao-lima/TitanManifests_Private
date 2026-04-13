d22ab1e285900dfba630858ae0648f13f2ad2e20
qiao-lima <qiao-lima@github.com>
2026-04-14 00:40:35 +0800

    Manual Update 3548040

---
+++ 3548040.lua
@@ -0,0 +1,12 @@
+-- 3548040's Lua and Manifest Created by Morrenus
+-- Machine Child
+-- Created: April 07, 2026 at 16:45:10 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3548040) -- Machine Child
+-- MAIN APP DEPOTS
+addappid(3548041, 1, "7e641c43dfc0803f6ae4a451a1debd5643f16117aed2aebf2ebb99da70e4ee07") -- Depot 3548041
+setManifestid(3548041, "8689199426885128121", 2811931829)
