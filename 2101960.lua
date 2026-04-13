1e9ce02349b2eb7387db59e802f16f6703e0daed
qiao-lima <qiao-lima@github.com>
2026-04-14 00:02:20 +0800

    Manual Update 2101960

---
+++ 2101960.lua
@@ -0,0 +1,39 @@
+-- 2101960's Lua and Manifest Created by Morrenus
+-- Cronos: The New Dawn
+-- Created: February 25, 2026 at 03:16:32 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 7
+-- Total DLCs: 3
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(2101960) -- Cronos: The New Dawn
+addtoken(2101960, "2704160378812211529")
+-- MAIN APP DEPOTS
+addappid(2101961, 1, "7f2c42a3171b51a0ea7463a8acbf143d1341f27c8e7babad3fa9eeef66f133eb") -- Depot 2101961
+setManifestid(2101961, "7413665617495231317", 23751580336)
+addappid(2101962, 1, "d3f3ea72d940025825a5859f4747ec70d37c6b3df38f8ec038c1c70911d4e0ac") -- Depot 2101962
+setManifestid(2101962, "8662209173710427597", 21929645641)
+addappid(2101965, 1, "30689a8a7fd4b8afac63815d91719c1a0b9bf47b1487b2ab8c6397a823ba0c27") -- Depot 2101965
+setManifestid(2101965, "8069054457755726990", 12)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") -- .NET 3.5 Redist (Shared from App 228980)
+setManifestid(229000, "4622705914179893434", 242743889)
+-- DLCS WITH DEDICATED DEPOTS
+-- Cronos The New Dawn - Artbook (AppID: 3748550)
+addappid(3748550)
+addtoken(3748550, "13073764574007953713")
+addappid(3748551, 1, "a00d5fc8b73f7250ff62ca6a67441ad191eb2a2415db2d0db193285a7bb92780") -- Cronos The New Dawn - Artbook - Depot 3748551
+setManifestid(3748551, "2053467992832422812", 78261945)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3748530) -- Cronos The New Dawn - Preorder Bonus
+addtoken(3748530, "2558675309268600323")
+addappid(3748540) -- Cronos The New Dawn - Deluxe Edition Upgrade
+addtoken(3748540, "7354826723082080627")
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2101963) -- Depot 2101963 (empty depot)
+-- addappid(2101964) -- Depot 2101964 (empty depot)
