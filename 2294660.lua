c5fd66bee1bd9020ec8e30f0933a78be68f80405
qiao-lima <qiao-lima@github.com>
2026-04-14 00:06:18 +0800

    Manual Update 2294660

---
+++ 2294660.lua
@@ -0,0 +1,12 @@
+-- 2294660's Lua and Manifest Created by Morrenus
+-- The Quinfall
+-- Created: March 31, 2026 at 01:51:52 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2294660, 1, "48e5461667575914c3b6a10a154aec26afe54dba6e3e2d9ef78cbaec25ade50c") -- The Quinfall
+-- MAIN APP DEPOTS
+addappid(2294661, 1, "39cbd76774d7cd7e3a7638344169ac77056c44361a66db94cb7e59fac501c576") -- Depot 2294661
+setManifestid(2294661, "7043579331752937489", 83386673678)
