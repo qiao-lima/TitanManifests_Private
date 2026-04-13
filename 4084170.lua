704bcdc0b787d49232d8be1a4b355c13627caaea
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:51 +0800

    Manual Update 4084170

---
+++ 4084170.lua
@@ -0,0 +1,12 @@
+-- 4084170's Lua and Manifest Created by Morrenus
+-- ZEROAD
+-- Created: April 10, 2026 at 22:08:01 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4084170, 1, "815004e39da4fdb73fb5bee982ca2b27a60edd221ddb22c6d259fa036761c5d4") -- ZEROAD
+-- MAIN APP DEPOTS
+addappid(4084171, 1, "ca0ffb46d33d1159935dd81ea73d4cb5a2be144dde29ee8bf151555626448874") -- Depot 4084171
+setManifestid(4084171, "2259242865630115564", 184310915)
