57b88478e01da8f565679cf189f015d7eb52d3ff
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:36 +0800

    Manual Update 389840

---
+++ 389840.lua
@@ -0,0 +1,14 @@
+-- 389840's Lua and Manifest Created by Morrenus
+-- Deserter Simulator
+-- Created: March 17, 2026 at 18:59:03 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(389840) -- Deserter Simulator
+-- MAIN APP DEPOTS
+addappid(389841, 1, "0e9f6f0fd2167bfff9eab448ab2ced3b27d292e87009d8e0a1871a20763467f6") -- Deserter Simulator Content
+setManifestid(389841, "3285968870189098887", 312457216)
+addappid(389842, 1, "bb6dde3ef3ec99a22f13e196f47ac25ece62cd7775ed13725d7667160e9b56d8") -- Deserter Simulator Depot　OSX
+setManifestid(389842, "8936027183419103409", 320266578)
