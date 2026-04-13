61716f7426c0525a995b1c6b4e4b410c9d2750bb
qiao-lima <qiao-lima@github.com>
2026-04-14 00:36:49 +0800

    Manual Update 3418720

---
+++ 3418720.lua
@@ -0,0 +1,12 @@
+-- 3418720's Lua and Manifest Created by Morrenus
+-- 禁地直播
+-- Created: March 31, 2026 at 20:17:16 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3418720) -- 禁地直播
+-- MAIN APP DEPOTS
+addappid(3418721, 1, "88ee7a1ca5637388ae2e94cd1b4e0aa26a750efb1cd56f986bde6d8f1617c0cf") -- Depot 3418721
+setManifestid(3418721, "7073903175331921596", 5651177831)
