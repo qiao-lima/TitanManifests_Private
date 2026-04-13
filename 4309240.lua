7059435835407b9d40149786d0ad95fe4a2de2ff
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:25 +0800

    Manual Update 4309240

---
+++ 4309240.lua
@@ -0,0 +1,14 @@
+-- 4309240's Lua and Manifest Created by Morrenus
+-- DisGo
+-- Created: April 07, 2026 at 12:00:21 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4309240) -- DisGo
+-- MAIN APP DEPOTS
+addappid(4309241, 1, "38ef260c9a52c8a7b8032d0fa65cb99220631e6d0b7465288cce04bfec9aff06") -- Depot 4309241
+setManifestid(4309241, "8286075200518767950", 4044033810)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4309242) -- Depot 4309242 (empty depot)
