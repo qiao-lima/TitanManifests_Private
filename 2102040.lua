77447eb6e9acc83aad3080095624ecdf11fe10ec
qiao-lima <qiao-lima@github.com>
2026-04-14 00:02:21 +0800

    Manual Update 2102040

---
+++ 2102040.lua
@@ -0,0 +1,12 @@
+-- 2102040's Lua and Manifest Created by Morrenus
+-- My Little Puppy
+-- Created: December 16, 2025 at 05:54:52 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2102040) -- My Little Puppy
+-- MAIN APP DEPOTS
+addappid(2102041, 1, "681b6ba43154004b17ec5874c59cb5ca74047ca28cb83a1ef2cd2c41b9b036bf") -- Depot 2102041
+setManifestid(2102041, "4614457378547937086", 7427686167)
