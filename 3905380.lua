6a653b3e16106c015381afc9a85e4911c2fa81fb
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:37 +0800

    Manual Update 3905380

---
+++ 3905380.lua
@@ -0,0 +1,12 @@
+-- 3905380's Lua and Manifest Created by Morrenus
+-- EVERDAWN
+-- Created: April 04, 2026 at 14:16:03 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3905380) -- EVERDAWN
+-- MAIN APP DEPOTS
+addappid(3905381, 1, "4cc6ecc31a22be61c4dd3f4de946d33aa0152655d371ded499003abb066abe07") -- Depot 3905381
+setManifestid(3905381, "2980165094786585201", 1011069775)
