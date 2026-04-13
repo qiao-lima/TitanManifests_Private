c78040caa37be1c55ecc3cf38302323246b99c9e
qiao-lima <qiao-lima@github.com>
2026-04-14 00:06:15 +0800

    Manual Update 2235200

---
+++ 2235200.lua
@@ -0,0 +1,15 @@
+-- 2235200's Lua and Manifest Created by Morrenus
+-- Neon Abyss 2
+-- Created: April 01, 2026 at 05:48:17 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(2235200, 1, "8649ef76e1d2ecec1ac588ba6a2f342134b33e3f79d204cf4e9e86a0a6576fff") -- Neon Abyss 2
+-- MAIN APP DEPOTS
+addappid(2235201, 1, "64b6a7741dff52341709bb548d844f5cbfb4d551a165eba90db90d49ca88586c") -- Depot 2235201
+setManifestid(2235201, "540871712835889642", 1555842235)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3821980) -- Neon Abyss 2 - Supporter Pack
+addappid(4060090) -- Neon Abyss 2 - Archenemy Returns
