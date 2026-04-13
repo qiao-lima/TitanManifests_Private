89035c8cec6328cfc4c5bd708b7d935db95d83c0
qiao-lima <qiao-lima@github.com>
2026-04-14 00:32:39 +0800

    Manual Update 3304320

---
+++ 3304320.lua
@@ -0,0 +1,14 @@
+-- 3304320's Lua and Manifest Created by Morrenus
+-- 中国足球模拟器
+-- Created: March 18, 2026 at 03:43:42 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3304320, 1, "489df7bd84d502fd2966bb836aa94d7dd8c542ea80fcfc3709c1c4b879585d9d") -- 中国足球模拟器
+-- MAIN APP DEPOTS
+addappid(3304321, 1, "27cbbb192ac92d805e680435d0ad958abd9735478520a56476b3d855d9456c5d") -- Depot 3304321
+setManifestid(3304321, "7423461946205433618", 16223140271)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4012700) -- Chinese Football Simulator - Dream Start DLC
