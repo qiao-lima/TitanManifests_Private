5bbe7b29e08f3d0ee6449de416a050e7b23f34c5
qiao-lima <qiao-lima@github.com>
2026-04-13 23:48:22 +0800

    Manual Update 1593970

---
+++ 1593970.lua
@@ -0,0 +1,14 @@
+-- 1593970's Lua and Manifest Created by Morrenus
+-- The Haunted House : Dark Island
+-- Created: April 09, 2026 at 17:47:47 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1593970) -- The Haunted House : Dark Island
+-- MAIN APP DEPOTS
+addappid(1593972, 1, "56276d9c1794784c25be9fac21a24e4ab6ca2a87b5698dd93d684621d7203421") -- Depot 1593972
+setManifestid(1593972, "3386376590888532094", 177309564)
+addappid(1593971, 1, "6137759e5d7cb5181d40f728f1445d430b692eebe1d598b9fa897a842b70ca53") -- Depot 1593971
+setManifestid(1593971, "5181941470093838283", 366807728)
