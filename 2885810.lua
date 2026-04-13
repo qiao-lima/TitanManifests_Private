29b9fe4ad3bf3ed3822b9c5c5b581763d53e4edf
qiao-lima <qiao-lima@github.com>
2026-04-14 00:21:47 +0800

    Manual Update 2885810

---
+++ 2885810.lua
@@ -0,0 +1,16 @@
+-- 2885810's Lua and Manifest Created by Morrenus
+-- Pink Noise
+-- Created: March 11, 2026 at 17:36:46 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(2885810) -- Pink Noise
+-- MAIN APP DEPOTS
+addappid(2885811, 1, "7f379a74c1bf4cddc0b64664071224a6b392d623bfac657b857ea91ce9b90a68") -- Depot 2885811
+setManifestid(2885811, "4698539217682796628", 4034367216)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Pink Noise - Supporter Pack (AppID: 4139670) - missing depot keys
+-- addappid(4139670)
