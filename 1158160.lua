6d6804b650983b5e9d12be89bafa1551a887e99f
qiao-lima <qiao-lima@github.com>
2026-04-13 23:36:21 +0800

    Manual Update 1158160

---
+++ 1158160.lua
@@ -0,0 +1,39 @@
+-- 1158160's Lua and Manifest Created by Morrenus
+-- Coral Island
+-- Created: March 16, 2026 at 18:03:09 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 7
+-- Total DLCs: 4 (1 excluded)
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1158160, 1, "52bf499bc7b8b8e9ac5da6b84aa5e4447bd57fd287e6a634ea6177a3a3439701") -- Coral Island
+-- MAIN APP DEPOTS
+addappid(1158161, 1, "e9303b2124c42a0c9cfc882e92469d8ceb392056ec283045ac876c31df4f5e65") -- Depot 1158161
+setManifestid(1158161, "7062333683288552696", 15881450359)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Kickstarter Exclusive Farmer Outfit Set (AppID: 2273380)
+addappid(2273380)
+addappid(2273380, 1, "f2946bf1df6964993d9597e6f2ac42e9aca3ef22d2f7c7145c3823b241966bf2") -- Kickstarter Exclusive Farmer Outfit Set - Depot 2273380
+setManifestid(2273380, "8577500605315709614", 9)
+-- Coral Island - Ocean Guardian Charity DLC Pack (AppID: 2471180)
+addappid(2471180)
+addappid(2471180, 1, "61aa13efb38e3f9ffaf22c018bde687a97b95b559d21406a6bbc47da860be1ab") -- Coral Island - Ocean Guardian Charity DLC Pack - Depot 2471180
+setManifestid(2471180, "1643256457515370317", 8)
+-- Coral Island - Ocean Explorer Charity DLC Pack (AppID: 2537800)
+addappid(2537800)
+addappid(2537800, 1, "2c18bb7df0d4f51d0d81f95eefaa22e3966f628e7f4defbc9d7d1978476a8b0b") -- Coral Island - Ocean Explorer Charity DLC Pack - Depot 2537800
+setManifestid(2537800, "9022386239993204092", 8)
+-- Kickstarter Exclusive Mythical Pet (AppID: 2626420)
+addappid(2626420)
+addappid(2626420, 1, "8ace641f5b3a2f94eb13ef67916c370cf42e520b76a567c72413c3788fd02a3f") -- Kickstarter Exclusive Mythical Pet - Depot 2626420
+setManifestid(2626420, "6493947519583149268", 10)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Kickstarter Exclusive Backer Outfit Set (AppID: 2561700) - missing depot keys
+-- addappid(2561700)
