295908bcc5391f2521fd724bce583b1eff23138a
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:08 +0800

    Manual Update 3809910

---
+++ 3809910.lua
@@ -0,0 +1,12 @@
+-- 3809910's Lua and Manifest Created by Morrenus
+-- Midnight Shifts with Femboy
+-- Created: April 01, 2026 at 17:39:30 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3809910) -- Midnight Shifts with Femboy
+-- MAIN APP DEPOTS
+addappid(3809914, 1, "652f85fa26c1cd3633c4c2619b93469d04ede595d422097d6ed6058df7c8562f") -- Depot 3809914
+setManifestid(3809914, "3226646054094124797", 630810355)
