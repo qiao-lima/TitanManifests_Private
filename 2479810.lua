4a2588a97d412a4c6666c5a41ba5c25f6db30e22
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:07 +0800

    Manual Update 2479810

---
+++ 2479810.lua
@@ -0,0 +1,22 @@
+-- 2479810's Lua and Manifest Created by Morrenus
+-- Gray Zone Warfare
+-- Created: April 02, 2026 at 10:58:12 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 3
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2479810, 1, "49ada9d51358b95a5416b3f17c6c2ead1b8171c2fe362c9266ed1489ce6a0576") -- Gray Zone Warfare
+-- MAIN APP DEPOTS
+addappid(2479811, 1, "c89db92b0e186d8c1e9db781270fc739b23f152b3e11ed7733f93bba19ce6716") -- Depot 2479811
+setManifestid(2479811, "7485232091571878305", 24921677469)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2633470) -- Gray Zone Warfare - Tactical Edition
+addappid(2914230) -- Gray Zone Warfare - Elite Edition
+addappid(2955580) -- Gray Zone Warfare - Supporter Edition
