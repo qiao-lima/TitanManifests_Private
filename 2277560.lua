3a121e3a5c599e49d76eb17eb726976f14e61a92
qiao-lima <qiao-lima@github.com>
2026-04-14 00:06:16 +0800

    Manual Update 2277560

---
+++ 2277560.lua
@@ -0,0 +1,24 @@
+-- 2277560's Lua and Manifest Created by Morrenus
+-- WUCHANG: Fallen Feathers
+-- Created: November 20, 2025 at 16:17:42 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 5
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2277560, 1, "5a45ebe9e985f6ea27f4be004ba5af9d317bd2242c20008cea7060fb5651844d") -- WUCHANG: Fallen Feathers
+-- MAIN APP DEPOTS
+addappid(2277561, 1, "6db3af8b5be3def24c758a005feb7fa0f65750716392328e2f14321340adb273") -- Depot 2277561
+setManifestid(2277561, "2668839162049698821", 64479245653)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3653760) -- WUCHANG Fallen Feathers - Full-Grown Red Mercury
+addappid(3653770) -- WUCHANG Fallen Feathers - Glistening Red Mercury
+addappid(3653780) -- WUCHANG Fallen Feathers Night  White Cosmetics and Weapon Pack
+addappid(3653790) -- WUCHANG Fallen Feathers Deluxe Upgrade Pack
+addappid(3986030) -- WUCHANG Fallen Feathers Deluxe Expansion Pack 1
