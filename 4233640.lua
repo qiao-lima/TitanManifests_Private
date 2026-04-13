068681bfffcdbe448dd363a19472b1a5de975acb
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:58 +0800

    Manual Update 4233640

---
+++ 4233640.lua
@@ -0,0 +1,14 @@
+-- 4233640's Lua and Manifest Created by Morrenus
+-- I was Reborn in a Fantasy World with a Cock as Long as the Name of this Game!
+-- Created: March 07, 2026 at 11:11:09 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4233640, 1, "3994261a6741180fb733f505d119d2f01dc631d2614192fba38b710097f9ad33") -- I was Reborn in a Fantasy World with a Cock as Long as the Name of this Game!
+-- MAIN APP DEPOTS
+addappid(4233641, 1, "5ef5c0e92fe503617db7239fee34b45e37c3364d49dd2998c09122dee4672d50") -- Depot 4233641
+setManifestid(4233641, "691925898512966989", 1985982403)
+addappid(4233642, 1, "52652a2c1a0841eb4518720d9e4fc6b53f177e2a9760c7c45fe4124164bd9279") -- Depot 4233642
+setManifestid(4233642, "6990735871364410622", 1981586586)
