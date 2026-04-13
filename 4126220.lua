92960b9e67b86f61be224afee77104b17c4066eb
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:54 +0800

    Manual Update 4126220

---
+++ 4126220.lua
@@ -0,0 +1,14 @@
+-- 4126220's Lua and Manifest Created by Morrenus
+-- Crusaders Quest : Hero Town
+-- Created: April 02, 2026 at 07:59:54 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4126220, 1, "bd9a4bfea9ab415fed55f8b93ed4fd25f41bbc88fb11540ef5d45e753e5cc421") -- Crusaders Quest : Hero Town
+-- MAIN APP DEPOTS
+addappid(4126221, 1, "bbee22e7cd268244d481f200c8fd2ae401eaa3deb6b323bb5afca5eea1827635") -- Depot 4126221
+setManifestid(4126221, "5387463895398598245", 307808330)
+addappid(4126222, 1, "29b13602d2bcb788ec71b4871c281d82ca3989b5950cb8722bb72611be030725") -- Depot 4126222
+setManifestid(4126222, "165279809833658386", 284979104)
