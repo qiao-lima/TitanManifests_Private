863812dfc293cee0a2ab0c3abf716b8bc86e1074
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:14 +0800

    Manual Update 4179190

---
+++ 4179190.lua
@@ -0,0 +1,12 @@
+-- 4179190's Lua and Manifest Created by Morrenus
+-- 噬丹
+-- Created: April 04, 2026 at 14:20:36 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4179190) -- 噬丹
+-- MAIN APP DEPOTS
+addappid(4179191, 1, "9542eb37c7b98f3bb0d5db375121bfb91175f166d6d96fbf2dbf1e3f9c96c362") -- Depot 4179191
+setManifestid(4179191, "5951455570658126332", 7586127577)
