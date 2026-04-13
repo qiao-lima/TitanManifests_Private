cc882008474baea443fb5962ac3ad2204955dce6
qiao-lima <qiao-lima@github.com>
2026-04-14 00:32:44 +0800

    Manual Update 3378960

---
+++ 3378960.lua
@@ -0,0 +1,28 @@
+-- 3378960's Lua and Manifest Created by Morrenus
+-- Lost Soul Aside™
+-- Created: December 16, 2025 at 01:38:36 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 5
+-- Total DLCs: 3
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(3378960) -- Lost Soul Aside™
+-- MAIN APP DEPOTS
+addappid(3378961, 1, "02157efe6285cacaa20730d8f0bd83c4b020ec25cd0c75e88ad7ac83465a570a") -- Depot 3378961
+setManifestid(3378961, "6864203328749040206", 91738091467)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") -- .NET 4.7 Redist (Shared from App 228980)
+setManifestid(229006, "1784011429307107530", 83944258)
+-- DLCS WITH DEDICATED DEPOTS
+-- Lost Soul Aside- Digital Artbook (AppID: 3484240)
+addappid(3484240)
+addappid(3484241, 1, "850583f98b56a2b21b65c877cf536c83830fa54c21f30ce3c6397935f065b2bc") -- Lost Soul Aside- Digital Artbook - Depot 3484241
+setManifestid(3484241, "7238494630064099886", 64460450)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3484230) -- Lost Soul Aside- Upgrade to Digital Deluxe
+addappid(3596880) -- Lost Soul Aside- Pre-purchase Entitlement
