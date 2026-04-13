66d1115de13a974da7a7c591f66c08a35bd92882
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:09 +0800

    Manual Update 4137770

---
+++ 4137770.lua
@@ -0,0 +1,12 @@
+-- 4137770's Lua and Manifest Created by Morrenus
+-- 咒影
+-- Created: March 24, 2026 at 08:20:22 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4137770, 1, "2c30c73716ba06e951b250c28a6e4aa1a743ef9b63776a8563f8c5dcec153812") -- 咒影
+-- MAIN APP DEPOTS
+addappid(4137771, 1, "a0b38d45f9740c514181e7937857fd77847a1fb625f80ca9b3ec803153b4a060") -- Depot 4137771
+setManifestid(4137771, "293842430508888677", 504819174)
