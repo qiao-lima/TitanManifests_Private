ca661b4725f7d0c60fd26b21aadc0dc40ea31767
qiao-lima <qiao-lima@github.com>
2026-04-13 23:48:17 +0800

    Manual Update 1455910

---
+++ 1455910.lua
@@ -0,0 +1,12 @@
+-- 1455910's Lua and Manifest Created by Morrenus
+-- Territory: Farming and Warfare
+-- Created: March 24, 2026 at 11:07:52 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1455910, 1, "ae3c77ad65650cc6bc2c7c5383494f4250cde17e7c8ae95e791ab2a5537565c1") -- Territory: Farming and Warfare
+-- MAIN APP DEPOTS
+addappid(1455911, 1, "1853a5d82beb2ff63b27d9d52a4aadfc134d817c5d2e37aa12626bd7e75d4687") -- Depot 1455911
+setManifestid(1455911, "4814766339366057529", 2102960563)
