95a043725392a7d58c07e0ac6e50e1711e5e8fd5
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:23 +0800

    Manual Update 4552250

---
+++ 4552250.lua
@@ -0,0 +1,12 @@
+-- 4552250's Lua and Manifest Created by Morrenus
+-- Cage of Y
+-- Created: April 11, 2026 at 20:01:16 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4552250) -- Cage of Y
+-- MAIN APP DEPOTS
+addappid(4552251, 1, "ba06ccc1d909a14facef425f38a972830c871531d1becc038d5eae9b4d3aee29") -- Depot 4552251
+setManifestid(4552251, "4448546285945404321", 332864241)
