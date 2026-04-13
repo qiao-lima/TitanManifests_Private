352605d5a4918535e81b75b78eac9136879674e7
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:07 +0800

    Manual Update 3800850

---
+++ 3800850.lua
@@ -0,0 +1,16 @@
+-- 3800850's Lua and Manifest Created by Morrenus
+-- Sakura Gozen
+-- Created: February 14, 2026 at 04:54:15 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3800850, 1, "251929abd01900d53547e9e6ee3c1bbf301b31682e6573cadffc370989e71b65") -- Sakura Gozen
+-- MAIN APP DEPOTS
+addappid(3800851, 1, "1831bc85813b463211ceffc957931af0a7c02e034b71123f20011e0bdf872681") -- Depot 3800851
+setManifestid(3800851, "2186193497557148146", 422847836)
+addappid(3800852, 1, "2d9cbb2d9be9c75368f79070ff9cc915bebc24e43b1d493a2c0d5cf39def6945") -- Depot 3800852
+setManifestid(3800852, "1182274634782908446", 458973587)
+addappid(3800853, 1, "ccb874891586c2bc7934e4dd17f85be8cac4364e0c33ebae9e3bd965c17344f7") -- Depot 3800853
+setManifestid(3800853, "15758272879001716", 423055910)
