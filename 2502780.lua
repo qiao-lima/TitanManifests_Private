e7fc50f7675fa721dd35245da0eb501b45b157c6
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:09 +0800

    Manual Update 2502780

---
+++ 2502780.lua
@@ -0,0 +1,18 @@
+-- 2502780's Lua and Manifest Created by Morrenus
+-- Ranger’s Path: National Park Simulator
+-- Created: March 18, 2026 at 12:22:56 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2502780, 1, "e2f68595e770f955a37bf99ca16fea43aa8b54d3d2f5d449ce6d1c1095e1c68d") -- Ranger’s Path: National Park Simulator
+-- MAIN APP DEPOTS
+addappid(2502781, 1, "5282c32747e5a2cf09349ccd9658ef78312cb83f5d83fe39b3cd8f69c5228ca3") -- Depot 2502781
+setManifestid(2502781, "125001364634186437", 23192742235)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
