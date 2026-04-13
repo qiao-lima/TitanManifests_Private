c5cf10954c8b5d98ed12a4246393617fe608225d
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:27 +0800

    Manual Update 4053340

---
+++ 4053340.lua
@@ -0,0 +1,12 @@
+-- 4053340's Lua and Manifest Created by Morrenus
+-- Cucked by My Wife
+-- Created: April 11, 2026 at 00:33:03 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4053340) -- Cucked by My Wife
+-- MAIN APP DEPOTS
+addappid(4053341, 1, "ae155091d93c268ca9b1077410d38e83c8ff213844c6595047dec950c8d0f751") -- Depot 4053341
+setManifestid(4053341, "1885712682172444404", 396542414)
