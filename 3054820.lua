c9f4776aa89b18c85cc42651dbc6f51bbba29870
qiao-lima <qiao-lima@github.com>
2026-04-14 00:25:37 +0800

    Manual Update 3054820

---
+++ 3054820.lua
@@ -0,0 +1,14 @@
+-- 3054820's Lua and Manifest Created by Morrenus
+-- でびるコネクショん
+-- Created: April 05, 2026 at 11:39:20 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3054820, 1, "cc225abe62b2695423e21f0ce10ef8eae9ed35e038c50e1d7978d750f18cafd7") -- でびるコネクショん
+-- MAIN APP DEPOTS
+addappid(3054822, 1, "cd2f7048e21da5b026ab9c1a9e0840cec21c76b3211d7aeec89c59adf12072d0") -- Depot 3054822
+setManifestid(3054822, "4595741298018934418", 7090656971)
+addappid(3054823, 1, "a262c98f915c166f5e9cf4f3cff48311eaad3cc5330d64ee99cb69bec350b8f7") -- Depot 3054823
+setManifestid(3054823, "4243641838564437124", 7238837846)
