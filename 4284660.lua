8daaa6ad02172b51da17276f1b1ffba0ac779f7b
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:21 +0800

    Manual Update 4284660

---
+++ 4284660.lua
@@ -0,0 +1,12 @@
+-- 4284660's Lua and Manifest Created by Morrenus
+-- Miracle Brave
+-- Created: April 07, 2026 at 10:55:51 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4284660) -- Miracle Brave
+-- MAIN APP DEPOTS
+addappid(4284661, 1, "cdd7a99854a9ea1de8fd791450290214f37b3384f21f873401f86b2ffaaf2586") -- Depot 4284661
+setManifestid(4284661, "5180116303455530583", 227383068)
