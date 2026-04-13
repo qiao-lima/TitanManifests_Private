647d17d74a07a71bd7c1543870803758d4bf4422
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:54 +0800

    Manual Update 4123590

---
+++ 4123590.lua
@@ -0,0 +1,17 @@
+-- 4123590's Lua and Manifest Created by Morrenus
+-- 是iKun就下一百层
+-- Created: March 17, 2026 at 18:58:56 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4123590) -- 是iKun就下一百层
+-- MAIN APP DEPOTS
+addappid(4123591, 1, "7fd451e6277b1a0b2577deca87bf01b655ef330de30f76fb20454d029153ef94") -- Depot 4123591
+setManifestid(4123591, "7379036451136509642", 144396472)
+-- DLCS WITH DEDICATED DEPOTS
+-- iKun  (AppID: 4189600)
+addappid(4189600)
+addappid(4189600, 1, "67f324939f023b0dca503da91cf0727f7f52dd9a537459517e72affc3526d299") -- iKun  - Depot 4189600
+setManifestid(4189600, "6635382351136125981", 144349368)
