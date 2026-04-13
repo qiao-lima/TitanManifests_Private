b496b42eea9ae4b8fa9635e53d7747e632302fe0
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:48 +0800

    Manual Update 4380150

---
+++ 4380150.lua
@@ -0,0 +1,12 @@
+-- 4380150's Lua and Manifest Created by Morrenus
+-- Sex Machina
+-- Created: March 06, 2026 at 04:30:56 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4380150) -- Sex Machina
+-- MAIN APP DEPOTS
+addappid(4380151, 1, "953d675a06b0bad974ea3b2e99b3dab2ed159b6c77223c2606826a32c3612df6") -- Depot 4380151
+setManifestid(4380151, "6399644214857664692", 884015819)
