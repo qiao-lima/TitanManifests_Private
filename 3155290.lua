2e2c538410b1a0242ce37d2512408e7e91f8f927
qiao-lima <qiao-lima@github.com>
2026-04-14 00:27:11 +0800

    Manual Update 3155290

---
+++ 3155290.lua
@@ -0,0 +1,14 @@
+-- 3155290's Lua and Manifest Created by Morrenus
+-- Hold Me Tight All Night, Onii-chan!
+-- Created: October 03, 2025 at 03:35:05 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3155290) -- Hold Me Tight All Night, Onii-chan!
+-- MAIN APP DEPOTS
+addappid(3155291, 1, "d506ef49aadb0e02a1268b31785a47909532921ae24c1cfbea8cef3987d07458") -- Depot 3155291
+setManifestid(3155291, "906339483688971536", 841871872)
+addappid(3155292, 1, "1a73c89df41ed44356534e71a28aa7c1fbaa92e62e6ed3479bb606012829cae5") -- Depot 3155292
+setManifestid(3155292, "3807489052830092959", 831419065)
