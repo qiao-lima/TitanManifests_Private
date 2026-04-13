71af7085cb06ceb9692e40df98f9e2266b45b9bf
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:08 +0800

    Manual Update 4480040

---
+++ 4480040.lua
@@ -0,0 +1,12 @@
+-- 4480040's Lua and Manifest Created by Morrenus
+-- Glitchy: Cute Loop
+-- Created: April 09, 2026 at 14:48:23 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4480040) -- Glitchy: Cute Loop
+-- MAIN APP DEPOTS
+addappid(4480041, 1, "f891e9ad90fd7d372594b84490c324241a109dd18fabefc49a291e549280aa17") -- Depot 4480041
+setManifestid(4480041, "6656510524513051329", 348131011)
