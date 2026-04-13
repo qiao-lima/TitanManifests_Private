9d647e5ea7a9a42476af3485617ee470eaf31e0e
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:51 +0800

    Manual Update 2640950

---
+++ 2640950.lua
@@ -0,0 +1,18 @@
+-- 2640950's Lua and Manifest Created by Morrenus
+-- Scott Pilgrim EX
+-- Created: March 05, 2026 at 10:28:27 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2640950) -- Scott Pilgrim EX
+-- MAIN APP DEPOTS
+addappid(2640951, 1, "b281abe36eee7ffe62839dde1861b3f7e780ded4ed7eb77efa44aae4fb495518") -- Depot 2640951
+setManifestid(2640951, "8013293943548205460", 820130843)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 117381405)
