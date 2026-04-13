4553864d46997e2155ba3d33028f1b49df003b57
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:06 +0800

    Manual Update 4466470

---
+++ 4466470.lua
@@ -0,0 +1,12 @@
+-- 4466470's Lua and Manifest Created by Morrenus
+-- Love at Last Light
+-- Created: March 29, 2026 at 12:35:33 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4466470) -- Love at Last Light
+-- MAIN APP DEPOTS
+addappid(4466471, 1, "b29179f08677439110729e1dc295aa491a096d26f62789289f145a287e28d453") -- Depot 4466471
+setManifestid(4466471, "2076209860436763975", 1280106788)
