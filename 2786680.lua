88943f15f1ece7fb995af7d54e097a0668b526ff
qiao-lima <qiao-lima@github.com>
2026-04-14 00:17:50 +0800

    Manual Update 2786680

---
+++ 2786680.lua
@@ -0,0 +1,13 @@
+-- 2786680's Lua and Manifest Created by Morrenus
+-- 美女，请别影响我学习
+-- Created: October 01, 2025 at 12:41:24 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2786680) -- 美女，请别影响我学习
+addtoken(2786680, "16612582771884404058")
+-- MAIN APP DEPOTS
+addappid(2786681, 1, "ba93faedc0bc173e603c50c2a54859361aef98782e177bcbaf501dbfc4671f79") -- Depot 2786681
+setManifestid(2786681, "6432693176925973402", 21821086272)
