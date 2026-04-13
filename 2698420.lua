d5bac2d8a5aae920c6cf7365ca5ed53bebb01831
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:56 +0800

    Manual Update 2698420

---
+++ 2698420.lua
@@ -0,0 +1,18 @@
+-- 2698420's Lua and Manifest Created by Morrenus
+-- Restore Your Island
+-- Created: April 12, 2026 at 20:19:29 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2698420) -- Restore Your Island
+-- MAIN APP DEPOTS
+addappid(2698421, 1, "508e88ce1a7d990f47ad0938e40db7036544f06a8fd753c1561e553edf0c6714") -- Depot 2698421
+setManifestid(2698421, "2870088173950430443", 5960028697)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
