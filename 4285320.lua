6696861c75ab39af20d8944f0b3b8c07eb56b2b2
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:21 +0800

    Manual Update 4285320

---
+++ 4285320.lua
@@ -0,0 +1,14 @@
+-- 4285320's Lua and Manifest Created by Morrenus
+-- Aurora Hills: Chapter 2
+-- Created: March 25, 2026 at 17:29:12 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4285320) -- Aurora Hills: Chapter 2
+-- MAIN APP DEPOTS
+addappid(4285323, 1, "b10c824f27c2112a63cf6b638b4876972d7528d7eea0f601518dbc0e0aeea92f") -- Depot 4285323
+setManifestid(4285323, "8727834878621181660", 1908595572)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4285321) -- Depot 4285321 (empty depot)
