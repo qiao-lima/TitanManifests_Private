3a84c14de3b17229f05fa2a95b5133c51d4d82ca
qiao-lima <qiao-lima@github.com>
2026-04-14 00:27:09 +0800

    Manual Update 3123410

---
+++ 3123410.lua
@@ -0,0 +1,105 @@
+-- 3123410's Lua and Manifest Created by Morrenus
+-- Atelier Yumia: The Alchemist of Memories & the Envisioned Land
+-- Created: October 07, 2025 at 20:11:30 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 18
+-- Total DLCs: 39
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3123410) -- Atelier Yumia: The Alchemist of Memories & the Envisioned Land
+-- MAIN APP DEPOTS
+addappid(3123411, 1, "427bebee696a57c77abdb7f7c291fe1f99efc82922c0ba9b154fd7cb9a2b4e78") -- Depot 3123411
+setManifestid(3123411, "3132765922319834047", 18693922571)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+-- DLCS WITH DEDICATED DEPOTS
+-- Atelier Yumia - Additional Map Falbra Thicket (AppID: 3569670)
+addappid(3569670)
+addappid(3569670, 1, "d982851a814b23e1f5e88bb3c673b79d9d4c97a52d6fcbcfa78c1bf942ddd6e9") -- Atelier Yumia - Additional Map Falbra Thicket - Depot 3569670
+setManifestid(3569670, "4471618427263809992", 316365682)
+-- Atelier Yumia - Gust Extra BGM Pack (AppID: 3569680)
+addappid(3569680)
+addappid(3569680, 1, "55ce8c9a94c2ee0711125b50bc78f8cddd61e9445737985eec0a48b43c72b40c") -- Atelier Yumia - Gust Extra BGM Pack - Depot 3569680
+setManifestid(3569680, "3148517261573723227", 1393627987)
+-- Atelier Yumia - Atelier Series Legacy BGM Pack (AppID: 3569690)
+addappid(3569690)
+addappid(3569690, 1, "21660334e36fa33bce7d5cda073e6ee94fb82b142956b0d330a6e294df0be65b") -- Atelier Yumia - Atelier Series Legacy BGM Pack - Depot 3569690
+setManifestid(3569690, "9175429166301806112", 3546793882)
+-- Atelier Yumia - Special costume for Yumia in collaboration with Asbel from Tales of Graces f Remastered (AppID: 3668860)
+addappid(3668860)
+addappid(3668860, 1, "73a3fce2ec3e466e6b6b8655984a3575ae4f0e10060967ba7a1652440a652007") -- Atelier Yumia - Special costume for Yumia in collaboration with Asbel from Tales of Graces f Remastered - Depot 3668860
+setManifestid(3668860, "5026462911134211774", 13376749)
+-- Atelier Yumia - High-Class Dream Style Costume for Yumia (AppID: 3742540)
+addappid(3742540)
+addappid(3742540, 1, "295336ca4111ed89a772313a004ad5013d180807409660547eda31691e498456") -- Atelier Yumia - High-Class Dream Style Costume for Yumia - Depot 3742540
+setManifestid(3742540, "70435821315850265", 12082980)
+-- Atelier Yumia - Boba Tea Costume for Flammi (AppID: 3742550)
+addappid(3742550)
+addappid(3742550, 1, "fe2c61f8ad7b7b923eac440aa29de36362579438e4683dda4c0351cb404fcb4b") -- Atelier Yumia - Boba Tea Costume for Flammi - Depot 3742550
+setManifestid(3742550, "2135984288032362791", 363905)
+-- Atelier Yumia - Newbie Maid Costume for Isla (AppID: 3742560)
+addappid(3742560)
+addappid(3742560, 1, "45c8a5bede1b8762b28cda906eceeee7af30a25c314af41e15aced3e2b845fb7") -- Atelier Yumia - Newbie Maid Costume for Isla - Depot 3742560
+setManifestid(3742560, "6518471412453131233", 14159665)
+-- Atelier Yumia - Perfect Butler Costume for Viktor (AppID: 3742570)
+addappid(3742570)
+addappid(3742570, 1, "3329194ea955b33111621ca9a795583ff0f42a7b4e752285ea7b51a65e5e7a74") -- Atelier Yumia - Perfect Butler Costume for Viktor - Depot 3742570
+setManifestid(3742570, "7622984624953005941", 12435764)
+-- Atelier Yumia - Wolf of the Streets Costume for Rutger (AppID: 3742580)
+addappid(3742580)
+addappid(3742580, 1, "c2a4b52cc9cd9fa792fcc6ad6977ddff8a449dfe4b21a918a8f2a0f8b21b9381") -- Atelier Yumia - Wolf of the Streets Costume for Rutger - Depot 3742580
+setManifestid(3742580, "931237697530080031", 13826337)
+-- Atelier Yumia - Ultimate Teacher Costume for Nina (AppID: 3742590)
+addappid(3742590)
+addappid(3742590, 1, "f1073d098d84e91cbf83ad2802a4df6099935bcf1226ef800a567a5f8beb9c70") -- Atelier Yumia - Ultimate Teacher Costume for Nina - Depot 3742590
+setManifestid(3742590, "6328905465461595319", 14424994)
+-- Atelier Yumia - Twilight Memories Costume for Lenja (AppID: 3742600)
+addappid(3742600)
+addappid(3742600, 1, "35ece2a582e26accd4bd2553789230099afb8e56a30c00dcc892c52286e3c084") -- Atelier Yumia - Twilight Memories Costume for Lenja - Depot 3742600
+setManifestid(3742600, "4772726277484272009", 10930867)
+-- Atelier Yumia - Recipe and Blueprint Expansion Pack The Art of Aladiss (AppID: 3742630)
+addappid(3742630)
+addappid(3742630, 1, "ec4a0d9dce9958c57d7f7f533243e61e1dd6f53180c4dfd6ac292951e0445386") -- Atelier Yumia - Recipe and Blueprint Expansion Pack The Art of Aladiss - Depot 3742630
+setManifestid(3742630, "906993859335434319", 546740)
+-- Atelier Yumia and TEKKEN Series Collaboration Pack (AppID: 3742640)
+addappid(3742640)
+addappid(3742640, 1, "d38d3feda696cf727d0c475dc1b483babd0b2cb8f7a9c475318f1f6c0b410038") -- Atelier Yumia and TEKKEN Series Collaboration Pack - Depot 3742640
+setManifestid(3742640, "6121532080801575748", 25425128)
+-- Atelier Yumia - Additional Map Idus Training Grounds (AppID: 3855580)
+addappid(3855580)
+addappid(3855580, 1, "cb2264214ff8f31af097095479e149c4a3cb3ac408b2e9a5fa145b184817e4a9") -- Atelier Yumia - Additional Map Idus Training Grounds - Depot 3855580
+setManifestid(3855580, "1424016718044364058", 532447815)
+-- Atelier Yumia - Additional Map Secret Research Area Aladissia (AppID: 3855590)
+addappid(3855590)
+addappid(3855590, 1, "e348d168fe525e6be1135df80bfa03b348bec4df4ae11e77969e7d4586184b67") -- Atelier Yumia - Additional Map Secret Research Area Aladissia - Depot 3855590
+setManifestid(3855590, "1794586099745724022", 453242577)
+-- Atelier Yumia - The Red Alchemist  the White Guardian Costume Set (AppID: 3931880)
+addappid(3931880)
+addappid(3931880, 1, "47ce2691a4488677d31f7d254f9843b5823f6a3209de25a70a5f5e13cec8dd47") -- Atelier Yumia - The Red Alchemist  the White Guardian Costume Set - Depot 3931880
+setManifestid(3931880, "7344842678105258909", 26930131)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3284970) -- Atelier Yumia - T-Shirt Costume for Yumia (Set of Three)
+addappid(3284980) -- Atelier Yumia - Alchemist on Her Day Off Costume for Yumia
+addappid(3284990) -- Atelier Yumia - Backpack Set
+addappid(3285000) -- Atelier Yumia - Colorful Collection
+addappid(3285010) -- Atelier Yumia - Black Rose Magician Costume for Yumia
+addappid(3285020) -- Atelier Yumia - Limited Photo Poses and Frames
+addappid(3285030) -- Atelier Yumia - Athlete of Memories Costume for Yumia
+addappid(3285040) -- Atelier Yumia - Floating Object of Memories Costume for Flammi
+addappid(3285050) -- Atelier Yumia - Exclusive Equipment Training Wrist Weight
+addappid(3285060) -- Atelier Yumia Season Pass
+addappid(3285070) -- Atelier Yumia - Additional Hair Color Set
+addappid(3285080) -- Atelier Yumia - Swimsuit Set
+addappid(3285090) -- Atelier Yumia - Waterside Alchemist Costume for Yumia
+addappid(3285100) -- Atelier Yumia - Flying Water Jug Costume for Flammi
+addappid(3285110) -- Atelier Yumia - Super Cute Beach Memories Costume for Isla
+addappid(3285120) -- Atelier Yumia - Knights Long-Distance Swimwear Costume for Viktor
+addappid(3285130) -- Atelier Yumia - Ebony Outlaw Costume for Rutger
+addappid(3285140) -- Atelier Yumia - Blooming Heart Costume for Nina
+addappid(3285150) -- Atelier Yumia - Cute Kitty Swimsuit Costume for Lenja
+addappid(3401940) -- Atelier Yumia - Aladiss Investigation Preparation Set
+addappid(3401950) -- Atelier Yumia - Yumia and Flammi Swimsuit Set
+addappid(3742610) -- Atelier Yumia - Fantasy Academy Costume Set for Yumia and Flammi
+addappid(3742620) -- Atelier Yumia - Fantasy Academy Costume Set
