419f8fecf9752b8802596c9b361270a792de9e28
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:03 +0800

    Manual Update 3954580

---
+++ 3954580.lua
@@ -0,0 +1,12 @@
+-- 3954580's Lua and Manifest Created by Morrenus
+-- Star Trek: Infection
+-- Created: March 31, 2026 at 20:23:35 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3954580) -- Star Trek: Infection
+-- MAIN APP DEPOTS
+addappid(3954581, 1, "e69eb87be7135103230cadeae24b647b55fa4348325c3fcdd8ba9dc7d075ff25") -- Depot 3954581
+setManifestid(3954581, "3758600248409495953", 11208835300)
