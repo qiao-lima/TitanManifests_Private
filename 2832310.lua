4359a921edd83f58464c8a1179e6ea78e07f907a
qiao-lima <qiao-lima@github.com>
2026-04-14 00:17:52 +0800

    Manual Update 2832310

---
+++ 2832310.lua
@@ -0,0 +1,12 @@
+-- 2832310's Lua and Manifest Created by Morrenus
+-- ITER-8
+-- Created: March 12, 2026 at 23:32:48 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2832310) -- ITER-8
+-- MAIN APP DEPOTS
+addappid(2832311, 1, "37177201a7ff08fab2aca16c7bd748e8decea54ab88b57ae592c23cfb037be1c") -- Depot 2832311
+setManifestid(2832311, "1461651890581803694", 833352285)
