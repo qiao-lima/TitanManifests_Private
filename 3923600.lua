bd607a17b41d4b9856e7b12ad474798e2a0a77ea
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:39 +0800

    Manual Update 3923600

---
+++ 3923600.lua
@@ -0,0 +1,18 @@
+-- 3923600's Lua and Manifest Created by Morrenus
+-- Photography Agency
+-- Created: April 04, 2026 at 06:23:50 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3923600) -- Photography Agency
+-- MAIN APP DEPOTS
+addappid(3923602, 1, "06f6c572179cc0a551d3cf24d31d320882638a123df49d25054eda8d91cd0406") -- Depot 3923602
+setManifestid(3923602, "9059816153608513553", 2870854363)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
