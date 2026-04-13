7688dbf2d8aa78e7418e49565601e74ddc5bee3d
qiao-lima <qiao-lima@github.com>
2026-04-14 00:41:15 +0800

    Manual Update 3667390

---
+++ 3667390.lua
@@ -0,0 +1,12 @@
+-- 3667390's Lua and Manifest Created by Morrenus
+-- Black Hole Fishing
+-- Created: April 08, 2026 at 01:22:40 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3667390) -- Black Hole Fishing
+-- MAIN APP DEPOTS
+addappid(3667391, 1, "de2bcd10a6f1e71570a2dc91eb39fd8087b4f17b4affe4a53b8e6bfd53b9d661") -- Depot 3667391
+setManifestid(3667391, "2026904712196658671", 254703328)
