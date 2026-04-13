d280b36e29d105959827d9f0222eaf5c658a3165
qiao-lima <qiao-lima@github.com>
2026-04-14 00:06:13 +0800

    Manual Update 2192640

---
+++ 2192640.lua
@@ -0,0 +1,12 @@
+-- 2192640's Lua and Manifest Created by Morrenus
+-- The Iron Union
+-- Created: March 17, 2026 at 19:00:10 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2192640) -- The Iron Union
+-- MAIN APP DEPOTS
+addappid(2192641, 1, "364ba5a3cc4b693f40a65adae243363163f22119fd54a84100e7d0bce848948f") -- Depot 2192641
+setManifestid(2192641, "834168877872515940", 723916122)
