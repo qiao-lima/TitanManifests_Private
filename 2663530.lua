8dbeef9b70e98c86a8343324c0fe44e1527c754e
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:53 +0800

    Manual Update 2663530

---
+++ 2663530.lua
@@ -0,0 +1,16 @@
+-- 2663530's Lua and Manifest Created by Morrenus
+-- POOLS
+-- Created: December 20, 2025 at 12:47:58 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2663530) -- POOLS
+-- MAIN APP DEPOTS
+addappid(2663531, 1, "c4286bf964a6ae3ee6a54f2619f4222b3ec80a55a458d8281062b27d35afa8d0") -- Depot 2663531
+setManifestid(2663531, "2309474925044897259", 12324372722)
+addappid(2663533, 1, "bc9994e3ea2a87aa8fac3693a4e3066a9e96a601b8e58f0586ea35a5b135819f") -- Depot 2663533
+setManifestid(2663533, "5423299709825971467", 11912316349)
+addappid(2663532, 1, "8ef4df38b64b5a5ffdb2b07e72c53ed51d3c21efc2ee13b523235da63974179d") -- Depot 2663532
+setManifestid(2663532, "555875490606854523", 11949065452)
