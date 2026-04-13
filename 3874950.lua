f901650724bf50022e38b0b21dbc015b583fe072
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:35 +0800

    Manual Update 3874950

---
+++ 3874950.lua
@@ -0,0 +1,17 @@
+-- 3874950's Lua and Manifest Created by Morrenus
+-- Bit Buddy
+-- Created: March 11, 2026 at 03:01:00 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3874950) -- Bit Buddy
+-- MAIN APP DEPOTS
+addappid(3874951, 1, "4a0ab020c367d0b6c780a374cb44cb92e591e30bb24da08897dc7503e0a3eb2e") -- Depot 3874951
+setManifestid(3874951, "555873440048913715", 156464016)
+-- DLCS WITH DEDICATED DEPOTS
+-- Bit Buddy - Developer Support Pack (AppID: 4056720)
+addappid(4056720)
+addappid(4056720, 1, "c0b1dccc6328009dc58674cf7ddf3c3f2b3382d572bade4aa6653500641d0d3c") -- Bit Buddy - Developer Support Pack - Depot 4056720
+setManifestid(4056720, "1130160974018664065", 13046844)
