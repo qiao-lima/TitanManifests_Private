bb844b29be1798b7773671c1f576ef2b12d9e7f0
qiao-lima <qiao-lima@github.com>
2026-04-14 00:40:31 +0800

    Manual Update 349040

---
+++ 349040.lua
@@ -0,0 +1,73 @@
+-- 349040's Lua and Manifest Created by Morrenus
+-- NARUTO SHIPPUDEN: Ultimate Ninja STORM 4
+-- Created: September 30, 2025 at 02:52:02 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 22
+-- Total DLCs: 9
+-- Shared Depots: 4
+
+-- MAIN APPLICATION
+addappid(349040) -- NARUTO SHIPPUDEN: Ultimate Ninja STORM 4
+-- MAIN APP DEPOTS
+addappid(349041, 1, "a2bc6391431ba0c509c7c64483a6a287069355845f082ddabd65946f8b5f5a1e") -- NARUTO SHIPPUDEN: Ultimate Ninja STORM 4 Content
+setManifestid(349041, "466904989965013797", 41249246381)
+addappid(349042, 1, "745657a624eb759e5dbe7074c730eaec65fa515762c2860d581898c7f323aad4") -- NARUTO SHIPPUDEN: Ultimate Ninja STORM 4 French
+setManifestid(349042, "4781962691495625946", 16310054)
+addappid(349043, 1, "897638cae5c16d5184f5f81979b5f7200d8274b38a4533e05df2d0678d39e53f") -- NARUTO SHIPPUDEN: Ultimate Ninja STORM 4 Italian
+setManifestid(349043, "8701540704013016369", 16479041)
+addappid(349044, 1, "41018cfa7b42cbc12b3ce7eb3561c790c4ed19001c22875196274e2d214d8183") -- NARUTO SHIPPUDEN: Ultimate Ninja STORM 4 German
+setManifestid(349044, "7598657215920648700", 16336775)
+addappid(349045, 1, "3daeb76211a4e33a99603bd45fc2098bed2a3340b4369e27faa8c675c9b6ff15") -- NARUTO SHIPPUDEN: Ultimate Ninja STORM 4 Spanish
+setManifestid(349045, "1672862195389831466", 33187419)
+addappid(349046, 1, "447389274e3143c4cda419ed7e55e7b1853e43c9ed59e0533526efcd975747e8") -- NARUTO SHIPPUDEN: Ultimate Ninja STORM 4 Russian
+setManifestid(349046, "7875414149252292342", 16603455)
+addappid(349047, 1, "22762b0789511571aacdf6ddbcae35c29d4393e15d0b9d687fd3b85ee6c4e6a6") -- NARUTO SHIPPUDEN: Ultimate Ninja STORM 4 Polish
+setManifestid(349047, "2820124058878706807", 16705031)
+addappid(349048, 1, "7e7923b06305d6c360be11963a3feeba945242f6f8a154934f300fded8594622") -- NARUTO SHIPPUDEN: Ultimate Ninja STORM 4 Traditional Chinese
+setManifestid(349048, "2915359124936233149", 23457275)
+addappid(349049, 1, "882b465e23a5ae80855b5740889441804da8d10859264eb81766549284f66c2c") -- NARUTO SHIPPUDEN: Ultimate Ninja STORM 4 Portuguese
+setManifestid(349049, "7135184964776273959", 16539022)
+addappid(352410, 1, "f9f781653d91cfa36539469fbe4e61f45d338f51237cfe837581d96990558418") -- NARUTO SHIPPUDEN: Ultimate Ninja STORM 4 Arabic
+setManifestid(352410, "6484480131915952183", 11629768)
+addappid(352411, 1, "d4a469b6907246533187cca78283280825cfa3b0cc7afcc2b26edeb9b16b1933") -- NARUTO SHIPPUDEN: Ultimate Ninja STORM 4 Korean
+setManifestid(352411, "3002284486267246739", 17011054)
+addappid(352412, 1, "7d85c049fd91857b2585541deffec38bc2687268233052d72e38a0eb072b6e37") -- NARUTO SHIPPUDEN: Ultimate Ninja STORM 4 Thai
+setManifestid(352412, "1644355053840635530", 143123)
+-- SHARED DEPOTS (from other apps)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (Shared from App 228980)
+setManifestid(228983, "8124929965194586177", 19265607)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- VC 2013 Redist (Shared from App 228980)
+setManifestid(228985, "3966345552745568756", 13699237)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229003, 1, "8e0ded6e1d9481eb286217322054ce7b0b8243daae770f31a30667752b1a3c63") -- .NET 4.0 Client Profile Redist (Shared from App 228980)
+setManifestid(229003, "8740933542064151477", 43001447)
+-- DLCS WITH DEDICATED DEPOTS
+-- NARUTO SHIPPUDEN Ultimate Ninja STORM 4 -Shikamarus Tale Extra Scenario Pack (AppID: 416420)
+addappid(416420)
+addappid(416420, 1, "6da23438c5d60737c065d092cc5554380a92cdc3af0f1845f9afde3b92c8ec94") -- NARUTO SHIPPUDEN Ultimate Ninja STORM 4 -Shikamarus Tale Extra Scenario Pack - Naruto Shippuden Ultimate Ninja Storm 4 -Shikamaru's Tale Extra Scenario Pack (416420) Depot
+setManifestid(416420, "40672943479631225", 56617862)
+-- NARUTO SHIPPUDEN Ultimate Ninja STORM 4 -Gaaras Tale Extra Scenario Pack (AppID: 416421)
+addappid(416421)
+addappid(416421, 1, "4e05609d087895d65deb6c9f2abd47057d4ca152e3348f7de32f8aa92fee868a") -- NARUTO SHIPPUDEN Ultimate Ninja STORM 4 -Gaaras Tale Extra Scenario Pack - Naruto Shippuden Ultimate Ninja Storm 4 -Gaara's Tale Extra Scenario Pack (416421) Depot
+setManifestid(416421, "2732989314762671464", 50583874)
+-- NARUTO SHIPPUDEN Ultimate Ninja STORM 4 - The Sound Four Characters Pack (AppID: 416422)
+addappid(416422)
+addappid(416422, 1, "ea4da58e74eac5a31e2438897667d70e0279c0a76460216932a0f7a18fd74a7e") -- NARUTO SHIPPUDEN Ultimate Ninja STORM 4 - The Sound Four Characters Pack - Naruto Shippuden Ultimate Ninja Storm 4 - The Sound Four Characters Pack (416422) Depot
+setManifestid(416422, "82372446095607410", 6488)
+-- NARUTO STORM 4 - Season Pass Bonus (AppID: 416423)
+addappid(416423)
+addappid(416423, 1, "772f5ae3581182804685e3cba0307be6b363d6b95b7f4e822fc39d0fb38547f3") -- NARUTO STORM 4 - Season Pass Bonus - Naruto Shippuden Ultimate Ninja Storm 4 - Akatsuki Secret Technique (416423) Depot
+setManifestid(416423, "51543784152758255", 6468)
+-- NARUTO STORM 4  Road to Boruto Expansion (AppID: 495160)
+addappid(495160)
+addappid(495160, 1, "51a6160181a36342b12a5a20f4cf646c371d8fece6b92d3077608001a6e94694") -- NARUTO STORM 4  Road to Boruto Expansion - NARUTO SHIPPUDEN: Ultimate Ninja STORM 4 - PYLAT (495160) Depot
+setManifestid(495160, "4329254603532854209", 257585204)
+-- NARUTO SHIPPUDEN UNS 4 ROAD TO BORUTO NEXT GENERATIONS Pack (AppID: 1142330)
+addappid(1142330)
+addappid(1142330, 1, "5d15e98459fd64db5e6c648eca639326767a8a2bbee7c3c64244c0b3cd57348e") -- NARUTO SHIPPUDEN UNS 4 ROAD TO BORUTO NEXT GENERATIONS Pack - NARUTO SHIPPUDEN: Ultimate Ninja STORM 4 - DLC1 (1142330) デポ
+setManifestid(1142330, "6981791585030199060", 6468)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(416410) -- Naruto Shippuden Ultimate Ninja Storm 4 - Pre Order DLC
+addappid(416424) -- NARUTO SHIPPUDEN Ultimate Ninja Storm 4 - Season Pass
+addappid(435160) -- NARUTO SHIPPUDEN Ultimate Ninja STORM 4 - Traditional Festival Costume
