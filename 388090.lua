885f21cd6ac423cc4d3fdc989bda5c6e19d61b35
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:35 +0800

    Manual Update 388090

---
+++ 388090.lua
@@ -0,0 +1,12 @@
+-- 388090's Lua and Manifest Created by Morrenus
+-- Five Nights at Freddy's 4
+-- Created: September 29, 2025 at 12:34:30 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(388090) -- Five Nights at Freddy's 4
+-- MAIN APP DEPOTS
+addappid(388091, 1, "76cfb5c74bbd068d0d29e42c39f08a9ee51592ab7b83920028404234ca2637cc") -- Five Nights at Freddy's 4 Content
+setManifestid(388091, "3840504613812117733", 1041535691)
