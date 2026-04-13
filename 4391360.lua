0abd9076bf23c8125012ac46cb7e899b545605d6
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:49 +0800

    Manual Update 4391360

---
+++ 4391360.lua
@@ -0,0 +1,18 @@
+-- 4391360's Lua and Manifest Created by Morrenus
+-- IROONI - Color Tag -
+-- Created: April 05, 2026 at 19:07:01 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4391360) -- IROONI - Color Tag -
+-- MAIN APP DEPOTS
+addappid(4391361, 1, "279b1c095c19dfc15c095da066afc3122d3d121a67534b505e5c56ebcc589743") -- Depot 4391361
+setManifestid(4391361, "1302275232849999715", 1104754763)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
