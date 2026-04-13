516c00d2ef015e66a0fbd91fd42ac96b4cf85632
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:22 +0800

    Manual Update 3995480

---
+++ 3995480.lua
@@ -0,0 +1,16 @@
+-- 3995480's Lua and Manifest Created by Morrenus
+-- Hidden around the World
+-- Created: April 07, 2026 at 22:16:02 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3995480) -- Hidden around the World
+-- MAIN APP DEPOTS
+addappid(3995481, 1, "cc3f61f5fbbe1ef019fdc3985544984b0d8fed19c401b6828934524b0c1eba32") -- Depot 3995481
+setManifestid(3995481, "5361766778715085886", 1415432098)
+addappid(3995482, 1, "1d98dd6bbae59913cf48e8e4d9f636f64b5ed01a4bf1bb2a33bc723cb8dffbf9") -- Depot 3995482
+setManifestid(3995482, "7426210217187208673", 1409843571)
+addappid(3995483, 1, "834643ac5c1649046bac35425aca39c0fd70b3837bee3e1922e8a64b4ff66b7b") -- Depot 3995483
+setManifestid(3995483, "500588632612722818", 1400190750)
