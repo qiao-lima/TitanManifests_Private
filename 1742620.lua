9910ff69ec3f014cc30ad0bcf00b589c780fe19b
qiao-lima <qiao-lima@github.com>
2026-04-13 23:52:41 +0800

    Manual Update 1742620

---
+++ 1742620.lua
@@ -0,0 +1,12 @@
+-- 1742620's Lua and Manifest Created by Morrenus
+-- Sudoku Minimalist Infinite
+-- Created: October 02, 2025 at 00:11:57 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1742620) -- Sudoku Minimalist Infinite
+-- MAIN APP DEPOTS
+addappid(1742621, 1, "7cef2c3e307b16b4e57977b097117b101c0e028287d91fb095c25db9a1840f43") -- Depot 1742621
+setManifestid(1742621, "4166393743763693480", 40525992)
