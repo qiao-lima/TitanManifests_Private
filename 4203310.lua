f6b4ced9aaf2cf9c08132cee55eb12f2f3f25684
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:38 +0800

    Manual Update 4203310

---
+++ 4203310.lua
@@ -0,0 +1,12 @@
+-- 4203310's Lua and Manifest Created by Morrenus
+-- Silent Edge
+-- Created: April 04, 2026 at 13:55:29 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4203310) -- Silent Edge
+-- MAIN APP DEPOTS
+addappid(4203311, 1, "bd5241d6ce7af62f233a1a51e6a109480e56d45fb735979848c450ab660a9cfb") -- Depot 4203311
+setManifestid(4203311, "1065817750701168046", 5707958249)
