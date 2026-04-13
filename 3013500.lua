8c5da4a838f139e20d1f70274b10f59fb9a21add
qiao-lima <qiao-lima@github.com>
2026-04-14 00:25:35 +0800

    Manual Update 3013500

---
+++ 3013500.lua
@@ -0,0 +1,19 @@
+-- 3013500's Lua and Manifest Created by Morrenus
+-- Bunny Space Odyssey: FlopHop
+-- Created: March 17, 2026 at 18:52:11 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3013500) -- Bunny Space Odyssey: FlopHop
+-- MAIN APP DEPOTS
+addappid(3013501, 1, "1049384e370d7ca753aa508e443341adb00b9346be4fb4fe4e100a3810f1d826") -- Depot 3013501
+setManifestid(3013501, "7960941180085507439", 303754250)
+addappid(3013502, 1, "53cbca378a504f304c841eb18a5b6e3782f663b29d81a2c7ee7033c16ce93e3e") -- Depot 3013502
+setManifestid(3013502, "4041198198304363311", 298338565)
+-- DLCS WITH DEDICATED DEPOTS
+-- Bunny Space Odyssey FlopHop Artbook (AppID: 3983460)
+addappid(3983460)
+addappid(3983460, 1, "22cfd27b343b55379cdee4dc5647c8abf356f6ac9a752882b185be34d1c073db") -- Bunny Space Odyssey FlopHop Artbook - Depot 3983460
+setManifestid(3983460, "5301691215105567691", 31581753)
