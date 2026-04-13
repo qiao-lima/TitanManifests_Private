7761c1ea36d568bdcef3ebf78e6b16bec9d5d200
qiao-lima <qiao-lima@github.com>
2026-04-13 23:58:14 +0800

    Manual Update 1926610

---
+++ 1926610.lua
@@ -0,0 +1,20 @@
+-- 1926610's Lua and Manifest Created by Morrenus
+-- Nature
+-- Created: April 06, 2026 at 23:58:14 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 3
+
+-- MAIN APPLICATION
+addappid(1926610) -- Nature
+-- MAIN APP DEPOTS
+addappid(1926611, 1, "56150f1cddb30283fba55ae1ff11f6706de1f763281c7b0e3bee811996722e89") -- Depot 1926611
+setManifestid(1926611, "3726829964762874179", 782503804)
+addappid(1926612, 1, "fd207ee3f26a41851d3cad37cdf5207cef9e0c05a1b8a5e837d03f94b52c6ed9") -- Depot 1926612
+setManifestid(1926612, "66542844348996553", 771598097)
+addappid(1926613, 1, "bc843e693d602032d3be8db8781d697e5d31291becdb5f7a28e79bd6f1f33c6e") -- Depot 1926613
+setManifestid(1926613, "4332403509462019272", 806932598)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3903070) -- Nature Flight Module
+addappid(3903080) -- Nature Jurassic Module
+addappid(4311600) -- Nature Arctic Tundra Module
