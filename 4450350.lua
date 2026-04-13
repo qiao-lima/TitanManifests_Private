0f141812f05bf9319b41378b1bc7457c60b71d2c
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:06 +0800

    Manual Update 4450350

---
+++ 4450350.lua
@@ -0,0 +1,12 @@
+-- 4450350's Lua and Manifest Created by Morrenus
+-- Xvirus
+-- Created: April 02, 2026 at 07:48:42 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4450350) -- Xvirus
+-- MAIN APP DEPOTS
+addappid(4450351, 1, "fa572485596bebf7f3a03e835d4c6f13c4e4b07cb0fe9dc4637ab66ea362f5fa") -- Depot 4450351
+setManifestid(4450351, "8924148294107243577", 165593761)
