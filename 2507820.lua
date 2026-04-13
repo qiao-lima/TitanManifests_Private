00b0266b393987154320bfc14d253cf8f99ee848
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:09 +0800

    Manual Update 2507820

---
+++ 2507820.lua
@@ -0,0 +1,17 @@
+-- 2507820's Lua and Manifest Created by Morrenus
+-- Bloodgrounds
+-- Created: March 13, 2026 at 14:59:33 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(2507820) -- Bloodgrounds
+-- MAIN APP DEPOTS
+addappid(2507821, 1, "c29f337eb3d7efa66fd37b1df6e18a592ecc37256e9c446a30761f851b8c69a2") -- Depot 2507821
+setManifestid(2507821, "8500474281319747026", 439665281)
+-- DLCS WITH DEDICATED DEPOTS
+-- Bloodgrounds - Supporter Pack (AppID: 3921440)
+addappid(3921440)
+addappid(3921440, 1, "a2aba7fc104cac96dc45ecbc53a889a243ae2be8131f546bc6c91703668fae15") -- Bloodgrounds - Supporter Pack - Depot 3921440
+setManifestid(3921440, "3256002527673354673", 41074012)
