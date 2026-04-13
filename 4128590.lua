f866cf6d565bc6262e23ef56e036560fe0e30040
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:55 +0800

    Manual Update 4128590

---
+++ 4128590.lua
@@ -0,0 +1,12 @@
+-- 4128590's Lua and Manifest Created by Morrenus
+-- Call of Girls
+-- Created: April 03, 2026 at 05:14:24 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4128590) -- Call of Girls
+-- MAIN APP DEPOTS
+addappid(4128591, 1, "829e03b3ec760337be90036732769f7b95e098008130a14444bb4b28325a6b28") -- Depot 4128591
+setManifestid(4128591, "7434695660275581396", 1381810658)
