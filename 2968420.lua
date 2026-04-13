2b1b122c3033de8f0095c7fd754f18fcad90a697
qiao-lima <qiao-lima@github.com>
2026-04-14 00:21:53 +0800

    Manual Update 2968420

---
+++ 2968420.lua
@@ -0,0 +1,20 @@
+-- 2968420's Lua and Manifest Created by Morrenus
+-- PowerWash Simulator 2
+-- Created: April 09, 2026 at 10:23:55 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(2968420, 1, "d67e90e69712a46a9aeb2fc1e684d7432a88cd41dad7fbce89e0005ceb3b1322") -- PowerWash Simulator 2
+-- MAIN APP DEPOTS
+addappid(2968422, 1, "2c13dcc1b498282fe021b2cd49229d7309ace370d9e990cd3ad843397cf247c9") -- Depot 2968422
+setManifestid(2968422, "5909065605077659426", 9253798319)
+-- DLCS WITH DEDICATED DEPOTS
+-- PowerWash Simulator 2 - Adventure Time Pack (AppID: 4173830)
+addappid(4173830)
+addappid(4173830, 1, "09117944c3966ed77061eb5f134fb9985cc1b2de5c9d48ac629139c66c1659b9") -- PowerWash Simulator 2 - Adventure Time Pack - Depot 4173830
+setManifestid(4173830, "6742564778197930674", 447253259)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4229450) -- Depot 4229450 (empty depot)
+-- addappid(4556380) -- Depot 4556380 (empty depot)
