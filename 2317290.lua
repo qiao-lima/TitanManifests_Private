4180f73077dac55cf77326de253961049b11eaec
qiao-lima <qiao-lima@github.com>
2026-04-14 00:06:18 +0800

    Manual Update 2317290

---
+++ 2317290.lua
@@ -0,0 +1,12 @@
+-- 2317290's Lua and Manifest Created by Morrenus
+-- EvoCreo 2
+-- Created: April 07, 2026 at 07:27:35 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2317290) -- EvoCreo 2
+-- MAIN APP DEPOTS
+addappid(2317291, 1, "d84a36e45b71c1d29c40addc5ebc64aa2f769de596b1b30d90c5ae21988c62e4") -- Depot 2317291
+setManifestid(2317291, "6860272158243083873", 488015393)
