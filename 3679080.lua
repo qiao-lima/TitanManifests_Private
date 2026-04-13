36c0492f77c22cc12aa471eb2aa959efb2186324
qiao-lima <qiao-lima@github.com>
2026-04-14 00:41:16 +0800

    Manual Update 3679080

---
+++ 3679080.lua
@@ -0,0 +1,18 @@
+-- 3679080's Lua and Manifest Created by Morrenus
+-- The Seven Deadly Sins: Origin
+-- Created: March 20, 2026 at 07:20:17 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3679080, 1, "9ff051463e937779a18fd6a2194fbd773f9ca821fbdc20023701e5968a0b9665") -- The Seven Deadly Sins: Origin
+-- MAIN APP DEPOTS
+addappid(3679081, 1, "9c4b635704762f0245f0f3a01460bddf32d55d49865332dbf5e53b10dfd2a3fc") -- Depot 3679081
+setManifestid(3679081, "6886509677249340973", 23871108022)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
