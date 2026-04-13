21c6cc5b12609987a97152e0ae829060f8c344f5
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:00 +0800

    Manual Update 4258870

---
+++ 4258870.lua
@@ -0,0 +1,12 @@
+-- 4258870's Lua and Manifest Created by Morrenus
+-- Melody Friends
+-- Created: March 24, 2026 at 07:43:43 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4258870, 1, "910c3e366e24231cfbfa32368264366306295fb83a3a7f517f77ef2e9e12f108") -- Melody Friends
+-- MAIN APP DEPOTS
+addappid(4258871, 1, "ab2a8969aa3e956d981f6482621f244f504b140b37898770e8f52d4678f04277") -- Depot 4258871
+setManifestid(4258871, "4556717730239883575", 379029265)
