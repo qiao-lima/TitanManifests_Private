4425b8b45aa9ad5a3298dd16d0bdabd673223ae0
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:10 +0800

    Manual Update 2511210

---
+++ 2511210.lua
@@ -0,0 +1,12 @@
+-- 2511210's Lua and Manifest Created by Morrenus
+-- Gunboat God
+-- Created: April 13, 2026 at 08:08:38 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2511210) -- Gunboat God
+-- MAIN APP DEPOTS
+addappid(2511211, 1, "10b770bd177efcdefbf272ad0218fdd7b360e4281e17378e6b03d784332dec7c") -- Depot 2511211
+setManifestid(2511211, "7014199330397213070", 987581734)
