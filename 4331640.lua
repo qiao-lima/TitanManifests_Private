84556125d5bc5bd21385cee19ea8b13edb2a29f3
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:46 +0800

    Manual Update 4331640

---
+++ 4331640.lua
@@ -0,0 +1,14 @@
+-- 4331640's Lua and Manifest Created by Morrenus
+-- Enjoyable Family Trip
+-- Created: March 06, 2026 at 04:38:03 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4331640) -- Enjoyable Family Trip
+-- MAIN APP DEPOTS
+addappid(4331641, 1, "26bb8611732b650d460148450fa5b1c2b96cea044b5ea29532ff3cd88730b304") -- Depot 4331641
+setManifestid(4331641, "4392968476855800908", 2342508580)
+addappid(4331642, 1, "c94dcfe059df93240deeb3e7d05a5682b3314f07c36b69edd840a651a8cf0ee5") -- Depot 4331642
+setManifestid(4331642, "6735574609068404526", 364336)
