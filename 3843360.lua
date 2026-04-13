0423e464db99104cbc5329845acf87db95206c91
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:10 +0800

    Manual Update 3843360

---
+++ 3843360.lua
@@ -0,0 +1,12 @@
+-- 3843360's Lua and Manifest Created by Morrenus
+-- Heroes of Magic & Cards
+-- Created: March 24, 2026 at 08:15:27 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3843360, 1, "8359deb84a162b75c40358d5e1427dc38b49837f6f2fdf536a7d832441ab0ab9") -- Heroes of Magic & Cards
+-- MAIN APP DEPOTS
+addappid(3843361, 1, "9741fc2f50d3c06ffad7e497184ae9268cb589b9ac2eda7f15959314fe50badf") -- Depot 3843361
+setManifestid(3843361, "8133561663479869005", 1912638401)
