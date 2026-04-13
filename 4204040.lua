b2772633c64f3deebd24c33ee6d0844903e4a1e8
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:38 +0800

    Manual Update 4204040

---
+++ 4204040.lua
@@ -0,0 +1,12 @@
+-- 4204040's Lua and Manifest Created by Morrenus
+-- Hentai Stepsis
+-- Created: March 23, 2026 at 12:30:30 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4204040) -- Hentai Stepsis
+-- MAIN APP DEPOTS
+addappid(4204041, 1, "52aae96f2ed95054172eaad3bc279324ea3dba6699df6e0c779b1e7ed5009261") -- Depot 4204041
+setManifestid(4204041, "8137793676960900232", 514079401)
