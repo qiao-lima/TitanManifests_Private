7f5db34bdd2438686b89dde24a9704679dc45b2d
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:22 +0800

    Manual Update 4511370

---
+++ 4511370.lua
@@ -0,0 +1,12 @@
+-- 4511370's Lua and Manifest Created by Morrenus
+-- 非凡仙途
+-- Created: April 03, 2026 at 08:07:00 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4511370) -- 非凡仙途
+-- MAIN APP DEPOTS
+addappid(4511371, 1, "029404330308e8fbc98036b67a5d66531525e936431d8c119dfb60a98e91c842") -- Depot 4511371
+setManifestid(4511371, "2733912854369153403", 194194322)
