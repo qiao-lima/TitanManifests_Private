12d6a9bd7ad82152e789c80056148aa8755d016f
qiao-lima <qiao-lima@github.com>
2026-04-14 00:32:42 +0800

    Manual Update 3365040

---
+++ 3365040.lua
@@ -0,0 +1,21 @@
+-- 3365040's Lua and Manifest Created by Morrenus
+-- Atelier Ryza 2: Lost Legends & the Secret Fairy DX
+-- Branch: PUBLIC
+-- Created: January 05, 2026 at 13:24:16 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3365040) -- Atelier Ryza 2: Lost Legends & the Secret Fairy DX
+-- MAIN APP DEPOTS
+addappid(3365041, 1, "d598945b8eadbb0ec9bad54928aeaa1549a79faa8df45f1d1d6d83c0d5127cbd") -- Depot 3365041
+setManifestid(3365041, "7779109048099748799", 36088775793)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3782460) -- Atelier Ryza 2 Lost Legends  the Secret Fairy DX - DX Version Pre-order Bonus
