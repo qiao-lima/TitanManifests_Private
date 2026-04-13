8635e4fbee82c13cbadff01f8a9e99783faf96a0
qiao-lima <qiao-lima@github.com>
2026-04-14 00:02:23 +0800

    Manual Update 2151960

---
+++ 2151960.lua
@@ -0,0 +1,12 @@
+-- 2151960's Lua and Manifest Created by Morrenus
+-- Golf 5 eClub
+-- Created: April 06, 2026 at 15:28:21 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2151960) -- Golf 5 eClub
+-- MAIN APP DEPOTS
+addappid(2151961, 1, "4a0313fdfffe2b664ad6170724d4cdd0b202c67a236c27a207ca12c80d3ef968") -- Depot 2151961
+setManifestid(2151961, "5891649065988958394", 23651086056)
