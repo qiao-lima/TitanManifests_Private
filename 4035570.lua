3e81abfedbc1d6fc36b323fff56d353439984a10
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:26 +0800

    Manual Update 4035570

---
+++ 4035570.lua
@@ -0,0 +1,12 @@
+-- 4035570's Lua and Manifest Created by Morrenus
+-- My Ghost Roommate
+-- Created: April 04, 2026 at 10:07:40 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4035570) -- My Ghost Roommate
+-- MAIN APP DEPOTS
+addappid(4035571, 1, "19bf09c67f639a0ed9f0439e936384ed5615ad67f3470ca25fef8d8ab1b515ac") -- Depot 4035571
+setManifestid(4035571, "1856491015620789566", 437325475)
