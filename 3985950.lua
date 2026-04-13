c42eb3594e4ef8d753a1b1f1e47547365eae5bd0
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:05 +0800

    Manual Update 3985950

---
+++ 3985950.lua
@@ -0,0 +1,18 @@
+-- 3985950's Lua and Manifest Created by Morrenus
+-- R.I.P. - Reincarnation Insurance Program
+-- Created: March 24, 2026 at 11:01:05 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3985950, 1, "3cae8a31a2f5ca92726cc7fd7a4ed4de214a8e4804584a0664edb26e1642e388") -- R.I.P. - Reincarnation Insurance Program
+-- MAIN APP DEPOTS
+addappid(3985951, 1, "7bfe7abe6093a6a1a9ba424c4b1139c0df5a57eda478a74ac79c775455fcaad2") -- Depot 3985951
+setManifestid(3985951, "2670000570723791492", 15110928737)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
