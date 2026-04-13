160280ade63d1637b0ffe86e49824772cc833f58
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:28 +0800

    Manual Update 611790

---
+++ 611790.lua
@@ -0,0 +1,51 @@
+-- 611790's Lua and Manifest Created by Morrenus
+-- House Party
+-- Created: March 21, 2026 at 03:15:35 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 11
+-- Total DLCs: 9
+
+-- MAIN APPLICATION
+addappid(611790, 1, "b0943e1310e994a8404e520359646b3d24d4b54b40982f0196bdaf2f6042d413") -- House Party
+-- MAIN APP DEPOTS
+addappid(611791, 1, "83bbf3421af9c12068fd0d7d85cbaf9d368db80bf05f3233c3887561783af1c5") -- House Party x64
+setManifestid(611791, "862787087280995284", 6189886115)
+addappid(611799, 1, "05fa356e7c4a3992252ed97b9252a3160401a04fa42abf9d07b8d794631425e4") -- Depot 611799
+setManifestid(611799, "4684724725315970887", 240398876)
+-- DLCS WITH DEDICATED DEPOTS
+-- House Party - Explicit Content Add-On (AppID: 944540)
+addappid(944540)
+addappid(944540, 1, "df6c415c034e6590623fc12d69f7c5cd0701e5291eb9862d065a1869e642af53") -- House Party - Explicit Content Add-On - House Party - Explicit Content Add-On (944540) Depot
+setManifestid(944540, "4017515925753232693", 1248948508)
+-- House Party - Detective Liz Katz in a Gritty Kitty Murder Mystery Expansion Pack (AppID: 1524910)
+addappid(1524910)
+addappid(1524910, 1, "dd346023a92ee8c9bbe4ec64f2d883f89077667088448f5637656284c4dcfbc8") -- House Party - Detective Liz Katz in a Gritty Kitty Murder Mystery Expansion Pack - Depot 1524910
+setManifestid(1524910, "1123564864331673967", 554800425)
+-- House Party - Doja Cat Expansion Pack (AppID: 1648810)
+addappid(1648810)
+addappid(1648810, 1, "d213a830208db1771092e5c6738647e1a5eb7e9bc54425427b1a9d9dc101b3bf") -- House Party - Doja Cat Expansion Pack - Depot 1648810
+setManifestid(1648810, "2974236380856363371", 293226259)
+-- House Party - Halloween Holiday Pack (AppID: 2229630)
+addappid(2229630)
+addappid(2229630, 1, "c545f2a975f50daee5d68d1a765049dcc1358a95ec2dac81fa934e5268e962d3") -- House Party - Halloween Holiday Pack - Depot 2229630
+setManifestid(2229630, "7662011343527561377", 36135034)
+-- House Party - Winter Holiday Pack (AppID: 2229631)
+addappid(2229631)
+addappid(2229631, 1, "5f0ece6ad4425255cfdc0c892e2f0c1249138a9b27f6a66bcf3c5cc17e5f97e6") -- House Party - Winter Holiday Pack - Depot 2229631
+setManifestid(2229631, "7283481005707280378", 67461419)
+-- House Party - Business and Pleasure Style Pack (AppID: 2363800)
+addappid(2363800)
+addappid(2363800, 1, "97ffa85ee5d7abd365a8dd1a437d69df1443fd3607a9a6f6e9a17e70d02a35c3") -- House Party - Business and Pleasure Style Pack - Depot 2363800
+setManifestid(2363800, "3216822256405404658", 418443176)
+-- House Party - Valentines Day Holiday Pack (AppID: 2783130)
+addappid(2783130)
+addappid(2783130, 1, "de20813392537a16f351523a19c09a06e3a5678e34c3f5ca3ba074f6430d2131") -- House Party - Valentines Day Holiday Pack - Depot 2783130
+setManifestid(2783130, "6783660858676239860", 36201296)
+-- House Party - Supporter Pack (AppID: 3487780)
+addappid(3487780)
+addappid(3487780, 1, "9a33d639ab8987b993e7cbe0900368481213498a6234eccf801e5336f5f9f57e") -- House Party - Supporter Pack - Depot 3487780
+setManifestid(3487780, "500438301241029325", 495141188)
+-- House Party - Nocturnal Temptations Expansion Pack (AppID: 4055790)
+addappid(4055790)
+addappid(4055790, 1, "118745756c98d9b3dea0ee09b93fb79b50f40c548eb96bba14a411eb4090817a") -- House Party - Nocturnal Temptations Expansion Pack - Depot 4055790
+setManifestid(4055790, "3221291543301620828", 584769711)
