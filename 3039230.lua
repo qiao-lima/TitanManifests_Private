2cd2e04f4201f4e4360892efd1a34e26802ae4c5
qiao-lima <qiao-lima@github.com>
2026-04-14 00:25:36 +0800

    Manual Update 3039230

---
+++ 3039230.lua
@@ -0,0 +1,12 @@
+-- 3039230's Lua and Manifest Created by Morrenus
+-- Excoverse
+-- Created: March 22, 2026 at 13:26:07 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3039230) -- Excoverse
+-- MAIN APP DEPOTS
+addappid(3039231, 1, "c0f5409b49dcb78122fba4b957e2c6711b3d3d97963a69b02dbd5a76b7c5eae9") -- Depot 3039231
+setManifestid(3039231, "5767298382482884363", 12356952349)
