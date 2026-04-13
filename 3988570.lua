67dd7ae86f8dfdab12ae4eb1218288b15ce31e8e
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:06 +0800

    Manual Update 3988570

---
+++ 3988570.lua
@@ -0,0 +1,12 @@
+-- 3988570's Lua and Manifest Created by Morrenus
+-- MUSYNX:RETURN
+-- Created: April 07, 2026 at 13:12:03 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3988570) -- MUSYNX:RETURN
+-- MAIN APP DEPOTS
+addappid(3988571, 1, "1c3a7fdbe1eba78b1562f1794f38dad31fde91a7cea8c6a1e9ae9fb817701e5d") -- Depot 3988571
+setManifestid(3988571, "5691985614676106424", 7640589366)
