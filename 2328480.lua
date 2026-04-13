a2673b30f72ed13c9f4a2df2506b0b0fb6d173d6
qiao-lima <qiao-lima@github.com>
2026-04-14 00:06:20 +0800

    Manual Update 2328480

---
+++ 2328480.lua
@@ -0,0 +1,14 @@
+-- 2328480's Lua and Manifest Created by Morrenus
+-- Ascension
+-- Created: December 24, 2025 at 10:25:04 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2328480) -- Ascension
+-- MAIN APP DEPOTS
+addappid(2328481, 1, "09c5b59ae7b6e031e179e66a22db7336d6be2108dbd016fa23b5052619ba8f15") -- Depot 2328481
+setManifestid(2328481, "1731661745705585833", 5094109954)
+addappid(2328482, 1, "013f7da7c5fa0909c02485f8e2be56d004dff4ef0cb153e8304de20f7c0109d8") -- Depot 2328482
+setManifestid(2328482, "8629111367049051029", 5094109954)
