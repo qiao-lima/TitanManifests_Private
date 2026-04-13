54eda9c9d636ddf93dbf7290647b1d21cb890f4e
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:01 +0800

    Manual Update 3923820

---
+++ 3923820.lua
@@ -0,0 +1,12 @@
+-- 3923820's Lua and Manifest Created by Morrenus
+-- Chihuahua Controller
+-- Created: April 04, 2026 at 14:16:19 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3923820) -- Chihuahua Controller
+-- MAIN APP DEPOTS
+addappid(3923821, 1, "56da4c53b05352a5c035b567bb9681c0e8d8da4734c6a87f24ebf1c1d13eae9d") -- Depot 3923821
+setManifestid(3923821, "248696640835654259", 69972300)
