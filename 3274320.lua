78256911a5b521c28d2f3c06eb7500a5f38cc989
qiao-lima <qiao-lima@github.com>
2026-04-14 00:30:09 +0800

    Manual Update 3274320

---
+++ 3274320.lua
@@ -0,0 +1,16 @@
+-- 3274320's Lua and Manifest Created by Morrenus
+-- Heart of the Forest
+-- Created: March 21, 2026 at 04:44:22 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3274320) -- Heart of the Forest
+-- MAIN APP DEPOTS
+addappid(3274321, 1, "51ca034e0e3b4866a4dc4592ee9e26e984fd5a21e7d9cd4eb2a4907058d462aa") -- Depot 3274321
+setManifestid(3274321, "8283944893070776954", 15003201099)
+addappid(3274322, 1, "44a1d0bc6a1f1a4b393a73b5d0c6e1dd8793e330145707c69af053cef9367a7d") -- Depot 3274322
+setManifestid(3274322, "1331773966066037334", 290266583)
+addappid(3274323, 1, "9306621619cfd6db586fd0b6a6fb9cb4fc9bab02680148c8ce914cc04ecb52d3") -- Depot 3274323
+setManifestid(3274323, "6283663331097639553", 256819832)
