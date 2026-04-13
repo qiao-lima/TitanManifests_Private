390268762b8b9484ca6cdef03293dfd1928d4408
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:45 +0800

    Manual Update 4322000

---
+++ 4322000.lua
@@ -0,0 +1,12 @@
+-- 4322000's Lua and Manifest Created by Morrenus
+-- DesktopDancer
+-- Created: April 09, 2026 at 14:52:06 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4322000, 1, "2ec3cf67b99fd62c224e106021e9338ca96c59aa6952e7f4960ab413fd68fa41") -- DesktopDancer
+-- MAIN APP DEPOTS
+addappid(4322001, 1, "25c3b2bafd065ae5601fff90f270c61bad02eaeaff83514a3427e0b4e6fc16da") -- Depot 4322001
+setManifestid(4322001, "4569962870613409937", 227966615)
