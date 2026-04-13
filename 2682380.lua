2db8cd64948dcda442e4aa12437126061ec96285
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:55 +0800

    Manual Update 2682380

---
+++ 2682380.lua
@@ -0,0 +1,12 @@
+-- 2682380's Lua and Manifest Created by Morrenus
+-- Extinction Day
+-- Created: April 11, 2026 at 04:24:04 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2682380) -- Extinction Day
+-- MAIN APP DEPOTS
+addappid(2682381, 1, "60506581f93e172482cf28b0cbbbcce9eeb0ad428b0da9ed42ff1c31dd412143") -- Depot 2682381
+setManifestid(2682381, "7972692570457699728", 2181844691)
