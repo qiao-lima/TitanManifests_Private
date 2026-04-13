c87f1fb45c98aab7acb6f5f3b9bf3d6fad481aa9
qiao-lima <qiao-lima@github.com>
2026-04-13 23:42:52 +0800

    Manual Update 1263240

---
+++ 1263240.lua
@@ -0,0 +1,14 @@
+-- 1263240's Lua and Manifest Created by Morrenus
+-- Skate Story
+-- Created: January 24, 2026 at 15:46:28 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1263240) -- Skate Story
+-- MAIN APP DEPOTS
+addappid(1263241, 1, "72af65204654f278d7a7c9951ca861d535f49b3a65248bdafb2918820e93e1d6") -- Depot 1263241
+setManifestid(1263241, "3844925166736370078", 11819991095)
+addappid(1263242, 1, "36b34df272a59e83258af05bf2f88c2e868bfb7074bff1b3e0aa412170c1e12e") -- Depot 1263242
+setManifestid(1263242, "3323456540390707289", 12109421837)
