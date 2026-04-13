cd6f8930a9a9887f13ae77a6c7df6597ea1a69a9
qiao-lima <qiao-lima@github.com>
2026-04-14 00:07:49 +0800

    Manual Update 2395770

---
+++ 2395770.lua
@@ -0,0 +1,17 @@
+-- 2395770's Lua and Manifest Created by Morrenus
+-- Never Grave
+-- Created: March 13, 2026 at 02:49:24 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(2395770) -- Never Grave
+-- MAIN APP DEPOTS
+addappid(2395771, 1, "dd43f698f3354836535bbfc749faec735ac34c9367cbb5f0de40de1645e000aa") -- Depot 2395771
+setManifestid(2395771, "4308803969371721771", 1970622955)
+-- DLCS WITH DEDICATED DEPOTS
+-- Never Grave Art Book (AppID: 4384400)
+addappid(4384400)
+addappid(4384400, 1, "3c96a9dd869952e819e5cdd47de80979dc7efe1c5bf48bf620fbc47df546a3f1") -- Never Grave Art Book - Depot 4384400
+setManifestid(4384400, "8026249830648057305", 110684113)
