220ee9a87ccd672827664f79d27754490bd5c80a
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:49 +0800

    Manual Update 4057050

---
+++ 4057050.lua
@@ -0,0 +1,12 @@
+-- 4057050's Lua and Manifest Created by Morrenus
+-- Heroines Through My Lens
+-- Created: February 05, 2026 at 06:07:58 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4057050) -- Heroines Through My Lens
+-- MAIN APP DEPOTS
+addappid(4057051, 1, "08db9368d6ef975eb6ff3f8b211c46f28d9dde5dc845f795c8d90efddd994976") -- Depot 4057051
+setManifestid(4057051, "7910778671836234286", 33218523500)
