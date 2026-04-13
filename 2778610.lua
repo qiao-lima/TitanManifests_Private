acb2eb72d89dd3ce77944d4e292915d4d576c6c4
qiao-lima <qiao-lima@github.com>
2026-04-14 00:17:49 +0800

    Manual Update 2778610

---
+++ 2778610.lua
@@ -0,0 +1,14 @@
+-- 2778610's Lua and Manifest Created by Morrenus
+-- Over The Top: WWI
+-- Created: March 17, 2026 at 21:53:51 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(2778610, 1, "6e71039f7b4e48ec743d0e1d678ab832c7814d3024573b98dee49b5d2eaf7927") -- Over The Top: WWI
+-- MAIN APP DEPOTS
+addappid(2778611, 1, "ffa4f7ec012fc2a39e3c7b3e800d80beba4be3ed6efb03015e5223379930c503") -- Depot 2778611
+setManifestid(2778611, "7237467230473833609", 8501201850)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4309530) -- Over the Top WWI - Supporter Pack
