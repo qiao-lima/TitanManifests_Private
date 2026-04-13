2114499c153375cb987f7c1c49a4bd6f8e4893ac
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:50 +0800

    Manual Update 4396570

---
+++ 4396570.lua
@@ -0,0 +1,12 @@
+-- 4396570's Lua and Manifest Created by Morrenus
+-- My Horny Roomies
+-- Created: April 03, 2026 at 12:28:29 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4396570) -- My Horny Roomies
+-- MAIN APP DEPOTS
+addappid(4396571, 1, "ad31fa6457ce460ad50987a5d906c95c043ecd6dd28e84d28e72c3f81d3aa67e") -- Depot 4396571
+setManifestid(4396571, "4865656806322721517", 1165601982)
