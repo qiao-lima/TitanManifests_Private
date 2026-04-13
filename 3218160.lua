d8fb9dc7b7d70c6da63bc7a6fe335d6a4aaa54b5
qiao-lima <qiao-lima@github.com>
2026-04-14 00:30:05 +0800

    Manual Update 3218160

---
+++ 3218160.lua
@@ -0,0 +1,12 @@
+-- 3218160's Lua and Manifest Created by Morrenus
+-- Ride The Dragon: The Awakening of the Dragon Tamer
+-- Created: April 04, 2026 at 13:42:54 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3218160) -- Ride The Dragon: The Awakening of the Dragon Tamer
+-- MAIN APP DEPOTS
+addappid(3218161, 1, "17511cfe974ebc3150d4d2caf7cfccd3c497bfdc04b824160b3df97bd6ff931e") -- Depot 3218161
+setManifestid(3218161, "3382800165642227674", 5251301493)
