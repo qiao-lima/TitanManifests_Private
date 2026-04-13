932c2a872af52bb85f371a2bcb327a0d9e130b53
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:06 +0800

    Manual Update 3987810

---
+++ 3987810.lua
@@ -0,0 +1,17 @@
+-- 3987810's Lua and Manifest Created by Morrenus
+-- 欲望之轮
+-- Created: February 01, 2026 at 14:27:22 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3987810) -- 欲望之轮
+-- MAIN APP DEPOTS
+addappid(3987811, 1, "a592e62a04b5bda67cc83501968d0813dc0c3a95fc888be755504470b70de452") -- Depot 3987811
+setManifestid(3987811, "1653104926795289400", 972203524)
+-- DLCS WITH DEDICATED DEPOTS
+-- Pleasure Cruise - Adult DLC (AppID: 4155780)
+addappid(4155780)
+addappid(4155780, 1, "7b46a973940609d0ae3a67576c455d229a6a6248cd76eda386cda8ca55813e35") -- Pleasure Cruise - Adult DLC - Depot 4155780
+setManifestid(4155780, "8028359355591949307", 972203553)
