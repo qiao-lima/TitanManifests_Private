327e6478a8f6041eb5ca469f10042e1b4c743490
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:12 +0800

    Manual Update 2584990

---
+++ 2584990.lua
@@ -0,0 +1,20 @@
+-- 2584990's Lua and Manifest Created by Morrenus
+-- Shadowverse: Worlds Beyond
+-- Created: March 30, 2026 at 18:04:07 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 3
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2584990) -- Shadowverse: Worlds Beyond
+-- MAIN APP DEPOTS
+addappid(2584991, 1, "65b325cf8e7c64301305a7b0e356f6a23d7871d10053b5e9125ef93393ec8fca") -- Depot 2584991
+setManifestid(2584991, "6231614868456241580", 1095867578)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3244010) -- Shadowverse Worlds Beyond-Deckboost Bundle
+addappid(3483340) -- Shadowverse Worlds Beyond-First Farer Box
+addappid(3483350) -- Shadowverse Worlds Beyond-Starter Bundle
