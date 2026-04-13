f313f77e9dc74db49f20f33e55db5f60d0fb936d
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:33 +0800

    Manual Update 3850560

---
+++ 3850560.lua
@@ -0,0 +1,12 @@
+-- 3850560's Lua and Manifest Created by Morrenus
+-- 血薪记：罪恶园区
+-- Created: March 08, 2026 at 17:05:37 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3850560) -- 血薪记：罪恶园区
+-- MAIN APP DEPOTS
+addappid(3850561, 1, "61903c2633e62e94d33f313bfefafd29b2e47475366438e3d01d2b7832001567") -- Depot 3850561
+setManifestid(3850561, "6149675670665911453", 39365563226)
