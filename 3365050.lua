95cd294a03a7404c1c9374a487320a5d858a3c89
qiao-lima <qiao-lima@github.com>
2026-04-14 00:32:43 +0800

    Manual Update 3365050

---
+++ 3365050.lua
@@ -0,0 +1,20 @@
+-- 3365050's Lua and Manifest Created by Morrenus
+-- Atelier Ryza 3: Alchemist of the End & the Secret Key DX
+-- Created: January 15, 2026 at 13:24:05 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3365050) -- Atelier Ryza 3: Alchemist of the End & the Secret Key DX
+-- MAIN APP DEPOTS
+addappid(3365051, 1, "5f0ac27fa68bdbbb717f526d76c9a8c440ae3cfd217b65f183fa281d0b1cdf85") -- Depot 3365051
+setManifestid(3365051, "3923075188305520948", 48862731000)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3782470) -- Atelier Ryza 3 Alchemist of the End  the Secret Key DX - DX Version Pre-order Bonus
