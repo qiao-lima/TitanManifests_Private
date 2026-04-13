7ab4904eeb14259d9fc22ba5d4cfacf56e810743
qiao-lima <qiao-lima@github.com>
2026-04-14 00:25:31 +0800

    Manual Update 2976260

---
+++ 2976260.lua
@@ -0,0 +1,18 @@
+-- 2976260's Lua and Manifest Created by Morrenus
+-- ChainStaff
+-- Created: April 08, 2026 at 14:12:12 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(2976260) -- ChainStaff
+-- MAIN APP DEPOTS
+addappid(2976261, 1, "cbc89c0db3123cac136ebf549c543f15679e971b6e4c079760072abd4ae03baf") -- Depot 2976261
+setManifestid(2976261, "986439117442217065", 2699265120)
+addappid(2976262, 1, "381bd992b46307b5a3e7414ed2f92a0c3be25ecb460e292f3807f25808a9346c") -- Depot 2976262
+setManifestid(2976262, "2737160674368839008", 0)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- ChainStaff - Artbook (AppID: 4577550) - missing depot keys
+-- addappid(4577550)
