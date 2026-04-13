22a8bc59d715d5710e140c11292a1a7751713866
qiao-lima <qiao-lima@github.com>
2026-04-14 00:06:18 +0800

    Manual Update 2300840

---
+++ 2300840.lua
@@ -0,0 +1,18 @@
+-- 2300840's Lua and Manifest Created by Morrenus
+-- Subliminal
+-- Created: March 31, 2026 at 03:05:06 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2300840) -- Subliminal
+-- MAIN APP DEPOTS
+addappid(2300841, 1, "4c8307a27765753d31cce03154f0d13df969d0339cf42d2df75852fe03211baa") -- Depot 2300841
+setManifestid(2300841, "4391475316998793829", 18909496426)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
