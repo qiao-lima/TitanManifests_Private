a13ab278300f901483249a6bec7e6a5805d97416
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:06 +0800

    Manual Update 2452130

---
+++ 2452130.lua
@@ -0,0 +1,12 @@
+-- 2452130's Lua and Manifest Created by Morrenus
+-- ANLIFE: Motion-Learning Life Evolution
+-- Created: March 11, 2026 at 04:41:51 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2452130) -- ANLIFE: Motion-Learning Life Evolution
+-- MAIN APP DEPOTS
+addappid(2452131, 1, "d48560e41c8edce7c84d1695c8cd470a0550f62e2274e8bcc02ff433d7a77077") -- Depot 2452131
+setManifestid(2452131, "3641453762959592153", 665051141)
