28e3bc175df5d22add49014233e72127325543ab
qiao-lima <qiao-lima@github.com>
2026-04-14 00:32:42 +0800

    Manual Update 3363680

---
+++ 3363680.lua
@@ -0,0 +1,20 @@
+-- 3363680's Lua and Manifest Created by Morrenus
+-- EverSiege: Untold Ages
+-- Created: April 03, 2026 at 21:48:36 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3363680, 1, "a80def00b7205999202fcc05aef33f4e50e0bcb655e383597b4878c2e0de9e33") -- EverSiege: Untold Ages
+-- MAIN APP DEPOTS
+addappid(3363681, 1, "b4de38471e88321e1a1b90ce7a94b8ceee8048b258919344e149d1fce3157274") -- Depot 3363681
+setManifestid(3363681, "5514723149184711832", 11790239332)
+addappid(3363682, 1, "d6eba18d470ba1bc052c8ac71cb9885cd8827e582a8f602dc58edc080c3d4e48") -- Depot 3363682
+setManifestid(3363682, "2045781927303549615", 152741960)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
