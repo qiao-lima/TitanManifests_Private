46078c951e68a3e689f1e52de42a985280ed4bee
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:24 +0800

    Manual Update 4016670

---
+++ 4016670.lua
@@ -0,0 +1,12 @@
+-- 4016670's Lua and Manifest Created by Morrenus
+-- That Summer - Hannah's Summer Vacation
+-- Created: April 10, 2026 at 04:16:33 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4016670) -- That Summer - Hannah's Summer Vacation
+-- MAIN APP DEPOTS
+addappid(4016671, 1, "0525a1b7a998434da92794325faab1ad78ab122881c6cffa4cb68bb9534b51c3") -- Depot 4016671
+setManifestid(4016671, "3468395696468920987", 264579060)
