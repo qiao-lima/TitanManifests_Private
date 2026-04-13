3c8887ef2721c603d6c4843a287433259fc5578e
qiao-lima <qiao-lima@github.com>
2026-04-13 23:36:20 +0800

    Manual Update 1097410

---
+++ 1097410.lua
@@ -0,0 +1,14 @@
+-- 1097410's Lua and Manifest Created by Morrenus
+-- Kittens Game
+-- Created: April 09, 2026 at 05:06:19 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1097410, 1, "82ee27e72fc3ad3a30d212918a998248b8a8d1a39b15911047f91a3cb3323afd") -- Kittens Game
+-- MAIN APP DEPOTS
+addappid(1097412, 1, "0eb42ff7a71b336c2db741a77b9700923f7f678e47bce3be227f28d59ddbadca") -- Depot 1097412
+setManifestid(1097412, "967436493757117220", 306604626)
+addappid(1097414, 1, "962a76d07336e1c35ba09fdd9c8f13624f4dacfbd520198bbf15c35a1d5ae3ae") -- Depot 1097414
+setManifestid(1097414, "8001292276279660282", 306262521)
