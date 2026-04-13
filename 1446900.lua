b0e68f9633b922fe83f0f5831c1861850b2b8f08
qiao-lima <qiao-lima@github.com>
2026-04-13 23:48:16 +0800

    Manual Update 1446900

---
+++ 1446900.lua
@@ -0,0 +1,12 @@
+-- 1446900's Lua and Manifest Created by Morrenus
+-- Fallen Tear: The Ascension
+-- Created: March 21, 2026 at 00:13:24 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1446900) -- Fallen Tear: The Ascension
+-- MAIN APP DEPOTS
+addappid(1446901, 1, "b05889536fd66d9f79de3ab1f457a00b6759ef200d54afc807fb1699263d69ba") -- Depot 1446901
+setManifestid(1446901, "7964202839774064628", 11607792769)
