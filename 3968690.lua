b078f4d6be882692e3bc2f1056c10d3fc473f835
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:04 +0800

    Manual Update 3968690

---
+++ 3968690.lua
@@ -0,0 +1,18 @@
+-- 3968690's Lua and Manifest Created by Morrenus
+-- 除邪2(CHUXIE2)
+-- Created: April 02, 2026 at 10:07:48 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3968690, 1, "5b0e3c99a0c803186dede3801e34f91048f11294920272cbdb48878e0a3a101a") -- 除邪2(CHUXIE2)
+-- MAIN APP DEPOTS
+addappid(3968691, 1, "c6438974151527fbfaab6d48db73d20b620eb3e42771f01bfe816fc5c4b1077e") -- Depot 3968691
+setManifestid(3968691, "5413245694345744578", 19252056887)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
