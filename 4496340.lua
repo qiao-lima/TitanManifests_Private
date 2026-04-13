c2fff3ff6c0d658e1842984645bf8f670bbbf18d
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:09 +0800

    Manual Update 4496340

---
+++ 4496340.lua
@@ -0,0 +1,12 @@
+-- 4496340's Lua and Manifest Created by Morrenus
+-- 封魔剑魂
+-- Created: April 09, 2026 at 22:31:10 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4496340) -- 封魔剑魂
+-- MAIN APP DEPOTS
+addappid(4496341, 1, "c2e11526d8bbce497a9bbf7933b17f770680759bf0f0c14c8a646a7414eb4b90") -- Depot 4496341
+setManifestid(4496341, "4111835533639783505", 57853483)
