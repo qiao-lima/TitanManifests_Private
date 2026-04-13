b6f5a71d1bb25145d38547a82b6dd1d1b834df43
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:59 +0800

    Manual Update 4249980

---
+++ 4249980.lua
@@ -0,0 +1,12 @@
+-- 4249980's Lua and Manifest Created by Morrenus
+-- Memory Maze
+-- Created: April 05, 2026 at 04:30:44 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4249980) -- Memory Maze
+-- MAIN APP DEPOTS
+addappid(4249981, 1, "40c145807736bc6f89852d54bbf1506f415ee509a6963bbcbf734ca684e924d4") -- Depot 4249981
+setManifestid(4249981, "4825786053402025924", 127067077)
