2fef8a2afb3535def6e31a9947cfbf82dc33c031
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:55 +0800

    Manual Update 2687280

---
+++ 2687280.lua
@@ -0,0 +1,12 @@
+-- 2687280's Lua and Manifest Created by Morrenus
+-- Red's Ascent
+-- Created: March 17, 2026 at 18:55:17 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2687280) -- Red's Ascent
+-- MAIN APP DEPOTS
+addappid(2687281, 1, "5656a06bc778bb2f07d73b5dac2fb95d611205f2464fced93510702b09a98438") -- Depot 2687281
+setManifestid(2687281, "4723018717794837112", 362367734)
