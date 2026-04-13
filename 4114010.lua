53f99f621933e14357f33d8f31590cf536051e12
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:53 +0800

    Manual Update 4114010

---
+++ 4114010.lua
@@ -0,0 +1,12 @@
+-- 4114010's Lua and Manifest Created by Morrenus
+-- The Old Man’s Will
+-- Created: April 13, 2026 at 06:48:19 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4114010) -- The Old Man’s Will
+-- MAIN APP DEPOTS
+addappid(4114011, 1, "7f277e1bf005bb7e8ac305bfe6a110121b3ad7c89b93fe5342eda512f5375ddf") -- Depot 4114011
+setManifestid(4114011, "7579845562594862553", 2200540573)
