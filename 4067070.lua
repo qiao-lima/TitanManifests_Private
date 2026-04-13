5ccc1629192682ec0764ba038e3ba9cb10d0c105
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:50 +0800

    Manual Update 4067070

---
+++ 4067070.lua
@@ -0,0 +1,14 @@
+-- 4067070's Lua and Manifest Created by Morrenus
+-- アイドルと〇〇しちゃダメですか？
+-- Created: March 05, 2026 at 23:49:13 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4067070, 1, "16b58dbecf0d56391d4bf0a15bc2f2794d89d3d4b1badbbfe5adf06f26f01730") -- アイドルと〇〇しちゃダメですか？
+-- MAIN APP DEPOTS
+addappid(4067071, 1, "5488884e6c385f8e988816f975f7c7f3b154bb9c2dbf7c294856f736d4eb81ad") -- Depot 4067071
+setManifestid(4067071, "9057623940555910714", 313530784)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4086600) -- Depot 4086600 (empty depot)
