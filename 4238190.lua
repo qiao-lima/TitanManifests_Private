622375188a3bd1dbc93305a9f728a01e886ea9c7
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:58 +0800

    Manual Update 4238190

---
+++ 4238190.lua
@@ -0,0 +1,14 @@
+-- 4238190's Lua and Manifest Created by Morrenus
+-- SUKIMA: The Creeping Gap
+-- Created: March 31, 2026 at 20:25:51 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4238190) -- SUKIMA: The Creeping Gap
+-- MAIN APP DEPOTS
+addappid(4238191, 1, "d39ee1a25e2dc55e6c00c338fb6dd57d884523b47ca73e18bfbe82e4dc1cf6ff") -- Depot 4238191
+setManifestid(4238191, "414161931869582153", 1506954062)
+addappid(4238192, 1, "cfa90b2badfad2c6ecef6baa2a44379f122050151c4a612006ae36665139ca14") -- Depot 4238192
+setManifestid(4238192, "6172246030869268526", 1531370476)
