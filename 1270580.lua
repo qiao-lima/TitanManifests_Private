91102a7e56a2b6ee0d0a5757661219862f03e087
qiao-lima <qiao-lima@github.com>
2026-04-13 23:42:53 +0800

    Manual Update 1270580

---
+++ 1270580.lua
@@ -0,0 +1,12 @@
+-- 1270580's Lua and Manifest Created by Morrenus
+-- Mind Over Magic
+-- Created: September 30, 2025 at 00:51:27 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1270580, 1, "06e83ff6c8c024242b6fa419fcb3c3394cabb2293bec098f53bee6916ec2fa95") -- Mind Over Magic
+-- MAIN APP DEPOTS
+addappid(1270581, 1, "67890182f6c150a904481b542ff399526709e363f448cb001c6cb02fbf8cfe70") -- Depot 1270581
+setManifestid(1270581, "6301682724829815537", 5295898632)
