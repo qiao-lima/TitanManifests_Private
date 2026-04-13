5e2756815871587cdc45dd0b0b5e879a44d3535e
qiao-lima <qiao-lima@github.com>
2026-04-14 00:41:12 +0800

    Manual Update 3628360

---
+++ 3628360.lua
@@ -0,0 +1,18 @@
+-- 3628360's Lua and Manifest Created by Morrenus
+-- Backrooms: No Way Out
+-- Created: March 31, 2026 at 20:19:36 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3628360) -- Backrooms: No Way Out
+-- MAIN APP DEPOTS
+addappid(3628361, 1, "7fa152663e40fcaa1dd741a614c898078719a0df4b2830b3730721a4bd44b8f5") -- Depot 3628361
+setManifestid(3628361, "7415092988933142112", 6305049380)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
