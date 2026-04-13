8e52a5f0c0eb610b8bafd21d0e4832cdfd11bb72
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:46 +0800

    Manual Update 4329260

---
+++ 4329260.lua
@@ -0,0 +1,16 @@
+-- 4329260's Lua and Manifest Created by Morrenus
+-- Substance 3D Painter 2026
+-- Created: April 07, 2026 at 07:39:35 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(4329260) -- Substance 3D Painter 2026
+-- MAIN APP DEPOTS
+addappid(4329262, 1, "cc202c619a64946a9e0c429e354df3ed9584bfd49b434099f84bd614d11fb0bb") -- Depot 4329262
+setManifestid(4329262, "8206191824885273015", 5118983159)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
