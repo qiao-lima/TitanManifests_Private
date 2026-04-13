6f3ac2e80539d70f4485e3fb43d354b93fa98f73
qiao-lima <qiao-lima@github.com>
2026-04-14 00:17:49 +0800

    Manual Update 2758000

---
+++ 2758000.lua
@@ -0,0 +1,13 @@
+-- 2758000's Lua and Manifest Created by Morrenus
+-- 名利游戏
+-- Created: October 01, 2025 at 10:33:47 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2758000) -- 名利游戏
+addtoken(2758000, "13361305384217043753")
+-- MAIN APP DEPOTS
+addappid(2758001, 1, "70d82cd1e81fe339eb087ae9438c39ee6e0540ab7f6efeaab136e4ca57f85199") -- Depot 2758001
+setManifestid(2758001, "8568721491847982468", 23217937382)
