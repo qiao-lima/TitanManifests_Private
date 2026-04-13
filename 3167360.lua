5f8541bd36abfc98c8c167c0326c31b5a9bd76b0
qiao-lima <qiao-lima@github.com>
2026-04-14 00:27:13 +0800

    Manual Update 3167360

---
+++ 3167360.lua
@@ -0,0 +1,18 @@
+-- 3167360's Lua and Manifest Created by Morrenus
+-- DOLL is BACK
+-- Created: April 10, 2026 at 04:03:17 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3167360) -- DOLL is BACK
+-- MAIN APP DEPOTS
+addappid(3167361, 1, "300ed59c9ee5aad3e2d47c8006a2837c1d5ae3ed4f4551cbacff404051f6e5b2") -- Depot 3167361
+setManifestid(3167361, "2167615675279791947", 4157883339)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
