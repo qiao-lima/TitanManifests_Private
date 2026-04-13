a72a5fcf68a5e73ad15d96bbe67075ca9c1f1c59
qiao-lima <qiao-lima@github.com>
2026-04-14 00:25:34 +0800

    Manual Update 3011570

---
+++ 3011570.lua
@@ -0,0 +1,20 @@
+-- 3011570's Lua and Manifest Created by Morrenus
+-- Aztlan Codex
+-- Created: April 11, 2026 at 23:37:02 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(3011570) -- Aztlan Codex
+-- MAIN APP DEPOTS
+addappid(3011571, 1, "3ff316f3283f71ae8d7f2d6b142f4121d4be866a66d425c93881c8e1ccf67824") -- Depot 3011571
+setManifestid(3011571, "9084970825622035614", 5881743750)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
