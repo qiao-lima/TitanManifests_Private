f961958f9f99eb1cee7ce0bd2ca25cb52e1acc7c
qiao-lima <qiao-lima@github.com>
2026-04-14 00:07:50 +0800

    Manual Update 2408820

---
+++ 2408820.lua
@@ -0,0 +1,12 @@
+-- 2408820's Lua and Manifest Created by Morrenus
+-- Luma Island
+-- Created: February 06, 2026 at 13:39:27 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2408820) -- Luma Island
+-- MAIN APP DEPOTS
+addappid(2408821, 1, "49df48241b28b06fdb2844200bd8a711c1e734ae6fa87a67e97a55a3a22e686c") -- Depot 2408821
+setManifestid(2408821, "3723641698785410852", 6739570528)
