4aec9d7ca01c72faac61737193070a97aef7493b
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:22 +0800

    Manual Update 4288810

---
+++ 4288810.lua
@@ -0,0 +1,18 @@
+-- 4288810's Lua and Manifest Created by Morrenus
+-- Unreal Hospital 8
+-- Created: March 31, 2026 at 20:27:56 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4288810) -- Unreal Hospital 8
+-- MAIN APP DEPOTS
+addappid(4288811, 1, "4a850d76fc0a6244047d4163796020dc6211ba0ff36f3afaab109915243c4d84") -- Depot 4288811
+setManifestid(4288811, "164469041370143100", 3926538627)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
