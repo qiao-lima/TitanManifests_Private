7372444d113b3e3cff0ab53e3a957fad8d9b22dd
qiao-lima <qiao-lima@github.com>
2026-04-14 00:27:09 +0800

    Manual Update 3125560

---
+++ 3125560.lua
@@ -0,0 +1,22 @@
+-- 3125560's Lua and Manifest Created by Morrenus
+-- CAT JOURNEY
+-- Created: April 04, 2026 at 13:39:45 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 5
+-- Total DLCs: 0
+-- Shared Depots: 4
+
+-- MAIN APPLICATION
+addappid(3125560) -- CAT JOURNEY
+-- MAIN APP DEPOTS
+addappid(3125561, 1, "150ed994b25d599f57b233e6acb0fe45dd704879123137da846f7b75f9245872") -- Depot 3125561
+setManifestid(3125561, "7750913205121333175", 14783378692)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") -- .NET 3.5 Redist (Shared from App 228980)
+setManifestid(229000, "4622705914179893434", 242743889)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") -- .NET 4.6 Redist (Shared from App 228980)
+setManifestid(229005, "7992454656023763365", 62009092)
