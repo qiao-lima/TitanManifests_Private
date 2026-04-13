50044d32bd0397b990ea3753daec79ea290ab3e9
qiao-lima <qiao-lima@github.com>
2026-04-13 23:52:39 +0800

    Manual Update 1681970

---
+++ 1681970.lua
@@ -0,0 +1,21 @@
+-- 1681970's Lua and Manifest Created by Morrenus
+-- 神都不良探
+-- Created: February 01, 2026 at 12:29:28 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(1681970, 1, "7a0ce0d97c2750c116b54b382033faec3470d9778f706a26689d3e829d57ce84") -- 神都不良探
+-- MAIN APP DEPOTS
+addappid(1681971, 1, "2b1c1ed24f1505af8e7e51e8ebee2247edb3e81ee1b529ffffe29bb3a2d3c2d4") -- Depot 1681971
+setManifestid(1681971, "4788034506938019600", 3134870240)
+addappid(1681972, 1, "123acb660b73f6af4d72bdbbdbd9bb9c8e02f811b726e63fd083a22f8e957557") -- Depot 1681972
+setManifestid(1681972, "3617991158381009872", 3111525691)
+-- DLCS WITH DEDICATED DEPOTS
+--  2-4 (AppID: 1937520)
+addappid(1937520)
+addappid(1937520, 1, "4b7c62d2cca5506136c3d558abf78eec0e5375399c1ebbe6505023b10fb59812") --  2-4 - Depot 1937520
+setManifestid(1937520, "5655409999074162409", 5340993747)
+addappid(1937521, 1, "97d33f3c426699df0d2d3eb0eb8ba02fa2a07f6cdfe8079459d39095dfaf60d2") --  2-4 - Depot 1937521
+setManifestid(1937521, "5658321608942378138", 5340993747)
