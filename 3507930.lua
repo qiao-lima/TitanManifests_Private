e1282d6d069100d2f2475b63eb8b145953302879
qiao-lima <qiao-lima@github.com>
2026-04-14 00:40:31 +0800

    Manual Update 3507930

---
+++ 3507930.lua
@@ -0,0 +1,14 @@
+-- 3507930's Lua and Manifest Created by Morrenus
+-- HellBorn
+-- Created: March 30, 2026 at 17:55:53 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3507930) -- HellBorn
+-- MAIN APP DEPOTS
+addappid(3507931, 1, "0973bc500bc82661d0db38debdd766bf3f1388aee4c9df6cd239a9714de0814e") -- Depot 3507931
+setManifestid(3507931, "6379603476247569254", 9638046589)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3807290) -- Depot 3807290 (empty depot)
