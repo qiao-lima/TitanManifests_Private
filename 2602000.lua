bc37c73e150213c5255136187068e49c0bec0357
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:13 +0800

    Manual Update 2602000

---
+++ 2602000.lua
@@ -0,0 +1,21 @@
+-- 2602000's Lua and Manifest Created by Morrenus
+-- Erogoddess: Olympus
+-- Created: March 17, 2026 at 15:59:06 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(2602000) -- Erogoddess: Olympus
+-- MAIN APP DEPOTS
+addappid(2602001, 1, "c7587a99c973c09500e2b83b2252e83211484288a9ba0a39196dd8fa1166acc2") -- Depot 2602001
+setManifestid(2602001, "3881115420194835681", 832928714)
+-- DLCS WITH DEDICATED DEPOTS
+-- Erogoddess Olympus - Wallpapers Pack (AppID: 3024270)
+addappid(3024270)
+addappid(3024270, 1, "37db060f19e0bdddce3d0c64b7caa6f681d9348c8143f89d2130cfbff94ac4d3") -- Erogoddess Olympus - Wallpapers Pack - Depot 3024270
+setManifestid(3024270, "8553896916424463589", 80145224)
+-- Erogoddess Olympus - Digital Artbook (AppID: 3032050)
+addappid(3032050)
+addappid(3032050, 1, "65242595a8971c61d3e2d42010a54434675a7bba9b0a2c6dca37e3511e3e45be") -- Erogoddess Olympus - Digital Artbook - Depot 3032050
+setManifestid(3032050, "224066664801753837", 4446440)
