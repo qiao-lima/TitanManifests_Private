febd06168f9a349ab6cba35ac33a3c238e36ed30
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:50 +0800

    Manual Update 2624670

---
+++ 2624670.lua
@@ -0,0 +1,12 @@
+-- 2624670's Lua and Manifest Created by Morrenus
+-- Find Matt's Cats
+-- Created: March 17, 2026 at 08:08:51 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2624670) -- Find Matt's Cats
+-- MAIN APP DEPOTS
+addappid(2624671, 1, "9f0be5d3a3a0f5b26f6c364b610a826ea81fdb9d7ebac63b9a2d93df13b630ea") -- Depot 2624671
+setManifestid(2624671, "2229190857761736087", 247279155)
