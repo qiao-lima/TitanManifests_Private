8ea025adece8d4cef06f226901850b9c7b841221
qiao-lima <qiao-lima@github.com>
2026-04-14 00:41:12 +0800

    Manual Update 3618390

---
+++ 3618390.lua
@@ -0,0 +1,18 @@
+-- 3618390's Lua and Manifest Created by Morrenus
+-- Gamblers Table
+-- Created: February 16, 2026 at 15:37:46 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3618390, 1, "a4ed71e9cece916bc3a74c744d76a557a2d3b0e967f770bc7dad12b181b3ae28") -- Gamblers Table
+-- MAIN APP DEPOTS
+addappid(3618391, 1, "af524ae28cb634f861e43a8dc0f9dbc6002bb303ec8e8167e02a8c92fa57d1e9") -- Depot 3618391
+setManifestid(3618391, "1044031281051545008", 116104332)
+addappid(3618392, 1, "0f1983ef9df338918e261c22b47c0bfdb8940a902f198be8f0e872fa7d0083ce") -- Depot 3618392
+setManifestid(3618392, "1386353908247157141", 102886148)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4218840) -- Gamblers Table - Supporter Pack
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4152010) -- Depot 4152010 (empty depot)
