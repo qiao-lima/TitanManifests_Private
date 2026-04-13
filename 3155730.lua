f8534cfd0c604afbc422ae6e439b2aebad765e86
qiao-lima <qiao-lima@github.com>
2026-04-14 00:27:11 +0800

    Manual Update 3155730

---
+++ 3155730.lua
@@ -0,0 +1,20 @@
+-- 3155730's Lua and Manifest Created by Morrenus
+-- Venus Vacation PRISM - DEAD OR ALIVE Xtreme -
+-- Created: October 01, 2025 at 05:24:27 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 3
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3155730) -- Venus Vacation PRISM - DEAD OR ALIVE Xtreme -
+-- MAIN APP DEPOTS
+addappid(3155731, 1, "f5f3f46814cdec1b4afab690622f6dffb042619e839eb1b08310252f9099e0fc") -- Depot 3155731
+setManifestid(3155731, "5453751672566898454", 13114772904)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3199230) -- Venus Vacation PRISM - DEAD OR ALIVE Xtreme - Digital Deluxe Upgrade Pack
+addappid(3204050) -- Venus Vacation PRISM - DEAD OR ALIVE Xtreme - Plumeria Hair Ornament Hairstyles (Set of 6)
+addappid(3204060) -- Venus Vacation PRISM - DEAD OR ALIVE Xtreme - Cheongsam Outfits (Set of 6)
