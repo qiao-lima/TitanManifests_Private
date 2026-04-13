88808fc18b1c4c0cdc7478669a47d49f6e5fb185
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:36 +0800

    Manual Update 3898860

---
+++ 3898860.lua
@@ -0,0 +1,41 @@
+-- 3898860's Lua and Manifest Created by Morrenus
+-- 敏感！恋のアンテナ ビンビン別嬪荘
+-- Created: March 04, 2026 at 00:12:02 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 8
+-- Total DLCs: 7
+
+-- MAIN APPLICATION
+addappid(3898860) -- 敏感！恋のアンテナ ビンビン別嬪荘
+-- MAIN APP DEPOTS
+addappid(3898861, 1, "71ad346d898697f9c9b91f93d981fce8aa2981cf1e9b3ac3bb93f28565967c9a") -- Depot 3898861
+setManifestid(3898861, "3146754115028098371", 36448260014)
+-- DLCS WITH DEDICATED DEPOTS
+--   -- (AppID: 4234230)
+addappid(4234230)
+addappid(4234230, 1, "88aaa6e98c508dce9c073ea49833ceb5df500c742dd5b8a7b0c390dda3ab07ea") --   -- - Depot 4234230
+setManifestid(4234230, "1196947010588116500", 764915370)
+--   -- (AppID: 4251950)
+addappid(4251950)
+addappid(4251950, 1, "c3f17e92579871e1da88a97077d11bf12a92a3aa080ba5bcc7a8bd4de56dd06c") --   -- - Depot 4251950
+setManifestid(4251950, "2596808944728895757", 2509492277)
+--   -- (AppID: 4251960)
+addappid(4251960)
+addappid(4251960, 1, "4aa5e8a2a869296c21b3bb7ccc4559e837115bf74093d343577864048bcb6866") --   -- - Depot 4251960
+setManifestid(4251960, "4691845500177458322", 2618666967)
+--   -- (AppID: 4251970)
+addappid(4251970)
+addappid(4251970, 1, "9f6fad2694d7fb91d1ad05a55a83983900416f537eb2ab81b2c4377aa3024aa6") --   -- - Depot 4251970
+setManifestid(4251970, "4436839249096781403", 2492671497)
+--   -- (AppID: 4251980)
+addappid(4251980)
+addappid(4251980, 1, "a696a54c284e7b9eed57bd224943dd1ae606002006af99a9449ff99af572b32b") --   -- - Depot 4251980
+setManifestid(4251980, "8263200274277094161", 2379585507)
+--   -- (AppID: 4251990)
+addappid(4251990)
+addappid(4251990, 1, "68aa4fa58b2c1a08f3c8bcb99b398714dbcf00f9200ab2d6b7166b4e9197a60e") --   -- - Depot 4251990
+setManifestid(4251990, "4672701165998144383", 2369432957)
+--   -- (AppID: 4252000)
+addappid(4252000)
+addappid(4252000, 1, "915fcff956e5b1b962e6653c3c256d85f883d1fc9103a7603a219ec677560038") --   -- - Depot 4252000
+setManifestid(4252000, "1574397285638964568", 2391142144)
