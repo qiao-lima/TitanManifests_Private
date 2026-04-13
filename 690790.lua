08b331bec03937c5c6ffd8254f6ee319038b4905
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:30 +0800

    Manual Update 690790

---
+++ 690790.lua
@@ -0,0 +1,157 @@
+-- 690790's Lua and Manifest Created by Morrenus
+-- DiRT Rally 2.0
+-- Created: September 29, 2025 at 07:52:32 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 32
+-- Total DLCs: 62
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(690790) -- DiRT Rally 2.0
+-- MAIN APP DEPOTS
+addappid(690791, 1, "c57d5edb5848d6850e206f1b08713190e59990a37f0538a4c3d11660a8435f63") -- RP17 Content
+setManifestid(690791, "8686885309488368555", 16667774233)
+addappid(690792, 1, "8a2922c78ce1d35bed61e1d47bf626b7e05c9f652a0fe8589f2c21888075cf6e") -- RP17 Exe
+setManifestid(690792, "4408182593951867177", 24668160)
+addappid(690794, 1, "20af2a2fed0ab731f0bbf059b6f8de8e18d50dd9badcb94448b5071f592806bb") -- DiRT Rally 2.0 Cars
+setManifestid(690794, "140664630075338429", 11707150674)
+addappid(690795, 1, "94caa31ef05114ab5bebcb8cc1b338313a73e4d43d7fc20827a6209d9250e9c3") -- DiRT Rally 2.0 Location 0 (New Zealand Rally)
+setManifestid(690795, "6152701538376316402", 4028863146)
+addappid(690796, 1, "bef666ee0946d42f1e0eee5d8464d532c750676cb19c7f4e278f959a62d9d5f6") -- DiRT Rally 2.0 Location 1 (Poland Rally)
+setManifestid(690796, "5177698062175283153", 4937525412)
+addappid(690797, 1, "0f39efac3987feb3d232dd26d5c33b3130f10e2d7aa403585fd242c5a5cfe490") -- DiRT Rally 2.0 Location 2 (Spain Rally)
+setManifestid(690797, "5191693106685464446", 4605202050)
+addappid(690798, 1, "4d35d2f1a8e44ad0e81cb0f5e6eeb9fd90b8e129fbc1bc0f22a8365a8ba7356a") -- DiRT Rally 2.0 Location 3 (Argentina Rally)
+setManifestid(690798, "4668670909653897918", 5946013989)
+addappid(690799, 1, "e1e4059e7a0b8576e6164f3adb1431e8a8b19046a112fdd2f8402493600f250d") -- DiRT Rally 2.0 Location 4 (Australia Rally)
+setManifestid(690799, "4438738795134699694", 4164482418)
+addappid(939883, 1, "4eb3983dcd73652e5d5ff1d22bf64e829cbac77720d483044d6ab303139d2c21") -- DiRT Rally 2.0 Location 5 (USA Rally)
+setManifestid(939883, "6775270195344734644", 6426085203)
+addappid(939884, 1, "6b47ff079992b17fac92d5e36b82d0c042d4a053a5ea868f6aa9b7b4df16e47b") -- DiRT Rally 2.0 Location 6 (Belgium RallyX)
+setManifestid(939884, "591077615613640105", 1108436277)
+addappid(939885, 1, "b162cb8f55916165e712dbf1d7eb02d86abd0b6c4ec84c57f01efb75d2934fe9") -- DiRT Rally 2.0 Location 7 (Canada RallyX)
+setManifestid(939885, "9128435499164207835", 2213990069)
+addappid(939886, 1, "69ebc5b31cb130b2782c1373d419fadb400f3d4759f9df34d493130b04c6f3f5") -- DiRT Rally 2.0 Location 8 (France RallyX)
+setManifestid(939886, "6687593820984025421", 926492440)
+addappid(939887, 1, "b2435a10ce6e8cbfece71188e479861752077578f6a2af7b0cde28e2a856a042") -- DiRT Rally 2.0 Location 9 (UK RallyX)
+setManifestid(939887, "7536545564649662169", 1005597419)
+addappid(939888, 1, "fa1ca74fbcce922dcdcd06d1af8abe1a768ee1e56e2d3f1deb49a8aa8268f603") -- DiRT Rally 2.0 Location 10 (Norway RallyX)
+setManifestid(939888, "3729427381119955317", 898473018)
+addappid(939889, 1, "cc43b40dd1a469b3006f775c9f652b2f0bee6ba9298d4be96d9fc3b51491436d") -- DiRT Rally 2.0 Location 11 (Portugal RallyX)
+setManifestid(939889, "2201105350167418927", 883551966)
+addappid(977661, 1, "412b41b6ff2740ebf81a6229edf72cf7ab717fb164b860454b45444d57f90a9d") -- DiRT Rally 2.0 Location 12 (Spain RallyX)
+setManifestid(977661, "3806616963286021545", 981010677)
+addappid(977662, 1, "85e6cd6d88afe4d8c4be76d92d8c96d8486005288be3275fb6b5bdcaf5dd858e") -- DiRT Rally 2.0 Location 13 (Sweden RallyX)
+setManifestid(977662, "2913589459752426828", 1030135258)
+addappid(977663, 1, "8aed11369f499cb6074f7ac2a1e3a323181f36c5c07cf3c97fe7877b39023acb") -- DiRT Rally 2.0 Location 14 (France Rally DLC)
+setManifestid(977663, "7933427068550952239", 7719242850)
+addappid(977664, 1, "ad0f22ed3121942cc2655822e19ad0355d499cf16d7b414687d9cb8b8e1b6c39") -- DiRT Rally 2.0 Location 15 (Sweden Rally DLC)
+setManifestid(977664, "4346122280047190183", 4717109378)
+addappid(977665, 1, "9c7821d5831e41f75dc59b2e1e37b36d59799d4d0e39db2b348e5aa2705e6b0f") -- DiRT Rally 2.0 Location 16 (Germany Rally DLC)
+setManifestid(977665, "5805022767122943285", 4823775346)
+addappid(977666, 1, "4ee3704728ac0aca941d34a43bb744d24a8cc94cbf5ac4a947714de2cd16df8d") -- DiRT Rally 2.0 Location 17 (Latvia RallyX DLC)
+setManifestid(977666, "7421727578757735809", 1222398417)
+addappid(977667, 1, "cf39fe811236b7728901733e3074175af2d2c82d850bbc8e930f95e319551391") -- DiRT Rally 2.0 Location 18 (UK Rally DLC)
+setManifestid(977667, "8094159985011189473", 6578713666)
+addappid(977668, 1, "37d9997bb91cd273c7d7abd066315664d6f21d701a4812cbbe42681c23c9cba5") -- DiRT Rally 2.0 Location 19 (Germany RallyX DLC)
+setManifestid(977668, "208238115072787668", 1256169745)
+addappid(977669, 1, "46733415436b369d63e81b4736773de51f3c8dc678e6c838ff86d21034e78ef2") -- DiRT Rally 2.0 Location 20 (USA Test)
+setManifestid(977669, "1305813921932269257", 2994703473)
+addappid(988663, 1, "0a12b86d79202fa36aa7fc3265dc4f0a34b7a3b72b0102de9e5936ef5617e5b7") -- DiRT Rally 2.0 Location 21 (Greece Rally DLC)
+setManifestid(988663, "4680467482414372261", 5116028898)
+addappid(988664, 1, "685c13edc152083b16daaed4d2525fa457e391c77082d2c6a44fff16844c5984") -- DiRT Rally 2.0 Location 22 (Abu Dhabi RallyX DLC)
+setManifestid(988664, "7280761127559040451", 1051261921)
+addappid(988665, 1, "ecfc9427c810ea24ef1a16a624f407661f5ccb31d94de06da133600483553700") -- DiRT Rally 2.0 Location 23 (Finland Rally DLC)
+setManifestid(988665, "3137744002291191619", 6512598610)
+addappid(988666, 1, "fb31aebf8a3841dce94ca2bfd0a031ef33943663b8a16b68a4ad45bb666ba49b") -- DiRT Rally 2.0 Location 24 (South Africa RallyX DLC)
+setManifestid(988666, "4666520335455950521", 1268820689)
+addappid(988667, 1, "e450f5c73a51080135447068f8a0b20617cb2efe76643000ecaf19403f175f56") -- DiRT Rally 2.0 Location 25 (UK RallyX DLC)
+setManifestid(988667, "3632654337120754339", 1045917761)
+addappid(988668, 1, "6573be0bf0ceca83d993d529b72b95d04332bc3c0f7d2a0b076895ea7243ed83") -- DiRT Rally 2.0 Location 26 (UK Rally DLC)
+setManifestid(988668, "4060341024140888923", 5731840834)
+-- SHARED DEPOTS (from other apps)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (Shared from App 228980)
+setManifestid(228983, "8124929965194586177", 19265607)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- VC 2015 Redist (Shared from App 228980)
+setManifestid(228986, "8782296191957114623", 29759921)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(977660) -- Dirt Rally 2.0 - Porsche 911 RGT Rally Spec
+addtoken(977660, "705840143344201297")
+addappid(988660) -- Dirt Rally 2.0 - Starter Upgrade Pack
+addtoken(988660, "8228544960885563098")
+addappid(988661) -- Dirt Rally 2.0 - Early 5 Cars Gift Pack
+addtoken(988661, "13429364020561745567")
+addappid(988662) -- DiRT Rally 2.0 - H2 RWD Double Pack
+addtoken(988662, "2081404521158865747")
+addappid(988670) -- DiRT Rally 2.0 - Opel Kadett  C GTE
+addappid(988671) -- Dirt Rally 2.0 - Season 1
+addtoken(988671, "5958771460974501722")
+addappid(988680) -- Dirt Rally 2.0 - Season 2
+addtoken(988680, "4422248355464455986")
+addappid(988820) -- DiRT Rally 2.0 - Season 1 - Stage 1 Liveries
+addtoken(988820, "2500185785858272203")
+addappid(1004840) -- DiRT Rally 2.0 - Season 1 - Stage 2 Liveries
+addtoken(1004840, "2312282661249048234")
+addappid(1004870) -- DiRT Rally 2.0 - Season 1 - Stage 3 Liveries
+addtoken(1004870, "5038490864494247588")
+addappid(1004871) -- Dirt 2.0 - Livery 4
+addtoken(1004871, "15958289654231612838")
+addappid(1004880) -- Dirt 2.0 - Livery 5
+addtoken(1004880, "11220422430154154708")
+addappid(1004881) -- Dirt 2.0 - Season Two, Stage 3 Liveries
+addtoken(1004881, "16084724018177602589")
+addappid(1004890) -- DiRT Rally 2.0 - Monte Carlo Rally
+addappid(1004891) -- DiRT Rally 2.0 - Sweden (Rally Location)
+addappid(1004892) -- DiRT Rally 2.0 - Germany Rally
+addappid(1004893) -- DiRT Rally 2.0 - Latvia RX Track
+addappid(1004894) -- DiRT Rally 2.0 - Wales (Rally Location)
+addappid(1004900) -- DiRT Rally 2.0 - Estering, Germany (Rallycross Track)
+addappid(1004920) -- DiRT Rally 2.0 - Skoda Fabia Rally
+addappid(1004921) -- DiRT Rally 2.0 - Citroen C4 Rally
+addappid(1004930) -- DiRT Rally 2.0 - Ford Focus RS Rally 2007
+addappid(1004940) -- DiRT Rally 2.0 - Subaru Impreza
+addappid(1004950) -- DiRT Rally 2.0 - BMW M1 Procar
+addappid(1004960) -- DiRT Rally 2.0 - Opel Manta 400
+addappid(1004970) -- DiRT Rally 2.0 - Peugeot 205 RX
+addappid(1004980) -- DiRT Rally 2.0 - Ford RS200 RX
+addappid(1004990) -- DiRT Rally 2.0 - Porsche 911 SC RS
+addappid(1005010) -- DiRT Rally 2.0 - Lancia 037 Evo 2
+addappid(1005020) -- DiRT Rally 2.0 - Lancia Delta S4 RX
+addappid(1005030) -- DiRT Rally 2.0 - MG Metro 6R4 Rallycross
+addappid(1036470) -- DiRT Rally 2.0 - Deluxe Upgrade Store Package
+addappid(1118250) -- Dirt Rally 2.0 - Season 3
+addtoken(1118250, "11643689171447387428")
+addappid(1118251) -- Dirt Rally 2.0 - Season 4
+addtoken(1118251, "13208033543528230915")
+addappid(1118260) -- Dirt Rally 2.0 - Season 3 Stage 1 Liveries
+addtoken(1118260, "12078956242986787873")
+addappid(1118261) -- DiRT Rally 2.0 - Season 3 - Stage 2 Liveries
+addtoken(1118261, "13926321817946870748")
+addappid(1118262) -- DiRT Rally 2.0 - Season 3 - Stage 3 Liveries
+addtoken(1118262, "9308980897365783561")
+addappid(1118263) -- DiRT Rally 2.0 - Season 4 Stage 1 Liveries
+addappid(1118264) -- DiRT Rally 2.0 - Season 4 - Stage 2 Liveries
+addtoken(1118264, "16820738802027818976")
+addappid(1118265) -- DiRT Rally 2.0 - Season 4 - Stage 3 Liveries
+addappid(1118270) -- Dirt Rally 2.0 - SUBARU Impreza (2001)
+addappid(1118271) -- Dirt Rally 2.0 - Ford Focus RS Rally 2001
+addappid(1118272) -- DiRT Rally 2.0 - Peugeot 306 Maxi
+addappid(1118273) -- DiRT Rally 2.0 - Seat Ibiza Kit Car
+addappid(1118274) -- DiRT Rally 2.0 - Peugeot 206 Rally
+addappid(1118275) -- DiRT Rally 2.0 - Volkswagen Golf Kitcar
+addappid(1118280) -- DiRT Rally 2.0 - Renault Clio R.S. RX
+addappid(1118281) -- DiRT Rally 2.0 - Renault Megane R.S. RX
+addappid(1118282) -- DiRT Rally 2.0 - Ford Fiesta Rallycross (MK8)
+addappid(1118283) -- DiRT Rally 2.0 - Audi S1 EKS RX quattro
+addappid(1118284) -- DiRT Rally 2.0 - Ford Fiesta RXS Evo 5
+addappid(1118285) -- DiRT Rally 2.0 - Mini Cooper SX1
+addappid(1118286) -- DiRT Rally 2.0 - Peugeot 208 WRX
+addappid(1118287) -- DiRT Rally 2.0 - Seat Ibiza RX
+addappid(1118288) -- DiRT Rally 2.0 - Ford Fiesta Rallycross (STARD)
+addappid(1118290) -- DiRT Rally 2.0 - Greece (Rally Location)
+addappid(1118291) -- DiRT Rally 2.0 - Yas Marina Circuit, Abu Dhabi (Rallycross Track)
+addappid(1118292) -- DiRT Rally 2.0 - Finland (Rally Location)
+addappid(1118293) -- DiRT Rally 2.0 - Killarney International Raceway, South Africa (Rallycross Track)
+addappid(1118294) -- DiRT Rally 2.0 - Lydden Hill, UK (Rallycross Track)
+addappid(1142800) -- DiRT Rally 2.0 Deluxe 2.0 (Season34)
+addappid(1211480) -- DiRT Rally 2.0 - Colin McRae FLAT OUT Pack
