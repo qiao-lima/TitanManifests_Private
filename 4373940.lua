21181abe567dfd1e7cf70710220f491898cd37db
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:47 +0800

    Manual Update 4373940

---
+++ 4373940.lua
@@ -0,0 +1,12 @@
+-- 4373940's Lua and Manifest Created by Morrenus
+-- Vamps Lust
+-- Created: March 20, 2026 at 13:42:25 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4373940) -- Vamps Lust
+-- MAIN APP DEPOTS
+addappid(4373941, 1, "33b7fe38231cf96f044f5eb47fae3859a0899e5558d53efc4460d0c45ee4c08f") -- Depot 4373941
+setManifestid(4373941, "2104254799098309150", 1329321687)
