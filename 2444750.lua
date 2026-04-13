98866d74a61b1c461026706358aef88830e47691
qiao-lima <qiao-lima@github.com>
2026-04-14 00:07:52 +0800

    Manual Update 2444750

---
+++ 2444750.lua
@@ -0,0 +1,18 @@
+-- 2444750's Lua and Manifest Created by Morrenus
+-- Shape of Dreams
+-- Created: March 16, 2026 at 23:58:26 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2444750, 1, "7dc7c8c175eae2f6798f4e892251914d68ba702f8066c09c71e3e9342d95943c") -- Shape of Dreams
+-- MAIN APP DEPOTS
+addappid(2444751, 1, "ef362189306209d0c11c3b509c34fd001aed76b0fc0ea258001430f6793519a5") -- Depot 2444751
+setManifestid(2444751, "7034323373329079762", 11573275381)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
