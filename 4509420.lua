5800297971bcb20a32dba55299b8bd7c85975337
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:21 +0800

    Manual Update 4509420

---
+++ 4509420.lua
@@ -0,0 +1,12 @@
+-- 4509420's Lua and Manifest Created by Morrenus
+-- Glass and Ember
+-- Created: April 11, 2026 at 23:27:33 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4509420) -- Glass and Ember
+-- MAIN APP DEPOTS
+addappid(4509421, 1, "61971d84054c4f05cff13bb98419f47bba5372c2f15d7ef7a8e90f0d018c7945") -- Depot 4509421
+setManifestid(4509421, "8255545738853304235", 1113652331)
