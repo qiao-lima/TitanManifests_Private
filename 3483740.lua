f5a04b5e287cf6a8f74889d47e3eaee3a0b7d24f
qiao-lima <qiao-lima@github.com>
2026-04-14 00:40:30 +0800

    Manual Update 3483740

---
+++ 3483740.lua
@@ -0,0 +1,19 @@
+-- 3483740's Lua and Manifest Created by Morrenus
+-- Cast n Chill
+-- Created: March 09, 2026 at 09:13:26 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(3483740) -- Cast n Chill
+-- MAIN APP DEPOTS
+addappid(3483741, 1, "0085e2ab6e91d273c72748b41db876429a43a28e359be3bd5519ddfa4d8ac501") -- Depot 3483741
+setManifestid(3483741, "6694133993138658300", 340771395)
+addappid(3483742, 1, "3413029df000861faa325cd3547914567f865ffb3c71247b47a8b9d7d57c7f6f") -- Depot 3483742
+setManifestid(3483742, "1630882284885344967", 439918606)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3817870) -- Cast n Chill - Supporter Pack
+-- EXCLUDED DLCS:
+-- UNRELEASED DLCS (COMMENTED OUT)
+-- addappid(4001650) -- Cast n Chill - Eastern Winds (unreleased)
