6971596f7d473c0e82c17c92d1181e0db86e9347
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:08 +0800

    Manual Update 3806030

---
+++ 3806030.lua
@@ -0,0 +1,12 @@
+-- 3806030's Lua and Manifest Created by Morrenus
+-- The Seven
+-- Created: February 22, 2026 at 00:46:44 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3806030) -- The Seven
+-- MAIN APP DEPOTS
+addappid(3806031, 1, "904123dc66f5cf23c9e97e0564a76d305862cfcd0d68ef0ae49859ce7d449866") -- Depot 3806031
+setManifestid(3806031, "1550428287593975542", 509890136)
