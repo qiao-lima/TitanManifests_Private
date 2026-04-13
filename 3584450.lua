eaabdeeb666beec4f4a254548d16dd75b3e3d751
qiao-lima <qiao-lima@github.com>
2026-04-14 00:41:10 +0800

    Manual Update 3584450

---
+++ 3584450.lua
@@ -0,0 +1,14 @@
+-- 3584450's Lua and Manifest Created by Morrenus
+-- Ovidius (Blood for Love)
+-- Created: April 05, 2026 at 22:12:13 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3584450) -- Ovidius (Blood for Love)
+-- MAIN APP DEPOTS
+addappid(3584454, 1, "89174595d4d6a68d98035b9a027cee4fa588b6f6dd68d9901c43957968de76b6") -- Depot 3584454
+setManifestid(3584454, "242428072509715382", 1688322295)
+addappid(3584451, 1, "e14d64f4c35ab84c3de44ffd07e4e53ff5f2575073b1683c2ef8c86cb681eade") -- Depot 3584451
+setManifestid(3584451, "849655779662473772", 1670849560)
