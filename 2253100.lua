66cb84772823230fa3124c12eef51cec846108c1
qiao-lima <qiao-lima@github.com>
2026-04-14 00:06:15 +0800

    Manual Update 2253100

---
+++ 2253100.lua
@@ -0,0 +1,26 @@
+-- 2253100's Lua and Manifest Created by Morrenus
+-- Everwind
+-- Created: March 20, 2026 at 16:46:04 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 3
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2253100, 1, "bafa86ed69bf723901710b88a0148adce598bb3962c68b5c69b8a949a77c9b2f") -- Everwind
+-- MAIN APP DEPOTS
+addappid(2253101, 1, "2549c73b78694079c57a8417dd02f5d6e4c6c6286a0483187a49bdbdf9857241") -- Depot 2253101
+setManifestid(2253101, "4077796950488561858", 2029087979)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Everwind - Supporter Pack (AppID: 4321300)
+addappid(4321300)
+addappid(4321300, 1, "63429df6fefe15855a53aa1c67b10e3058181615b7a28a498670309c125c0553") -- Everwind - Supporter Pack - Depot 4321300
+setManifestid(4321300, "4236541669611483898", 525907946)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4351900) -- Capybara Edition Upgrade
+addappid(4434900) -- Everwind - Early Access Thank-You Gift
