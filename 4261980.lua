8c9f02787611f729a6795c2154e3a4be638f4205
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:01 +0800

    Manual Update 4261980

---
+++ 4261980.lua
@@ -0,0 +1,14 @@
+-- 4261980's Lua and Manifest Created by Morrenus
+-- 女神的贴身男仆
+-- Created: February 08, 2026 at 08:53:07 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4261980) -- 女神的贴身男仆
+-- MAIN APP DEPOTS
+addappid(4261982, 1, "e0effba89bed9fd9e71b66eaa80e87a9a23850fdc47d36df355d72a023d4453e") -- Depot 4261982
+setManifestid(4261982, "7892197331351934149", 42065650379)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4261981) -- Depot 4261981 (empty depot)
