24d93f4813be6faa1e5d706cbb78d14e2080c80d
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:22 +0800

    Manual Update 4287900

---
+++ 4287900.lua
@@ -0,0 +1,12 @@
+-- 4287900's Lua and Manifest Created by Morrenus
+-- Alchemist Watch
+-- Created: April 11, 2026 at 20:00:38 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4287900) -- Alchemist Watch
+-- MAIN APP DEPOTS
+addappid(4287901, 1, "987ab20cab285ad7a534dce3a19151f0c0d740a9868c03a986f2f1b6fe8fedfe") -- Depot 4287901
+setManifestid(4287901, "3103231130319738873", 1958262327)
