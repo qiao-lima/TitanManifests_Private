c11744e3c1ff6a9309ebd0ac634ac22b3b78021b
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:14 +0800

    Manual Update 4179270

---
+++ 4179270.lua
@@ -0,0 +1,12 @@
+-- 4179270's Lua and Manifest Created by Morrenus
+-- Don't Crash
+-- Created: March 30, 2026 at 18:17:17 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4179270) -- Don't Crash
+-- MAIN APP DEPOTS
+addappid(4179272, 1, "562ca2b232695622ac6cd7bf9cc7c2ac7ffc580ac79f7cc7cc5794b4885364d9") -- Depot 4179272
+setManifestid(4179272, "7166161573575159668", 1542262419)
