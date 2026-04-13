04a8aa17857edf36225146e96a7609a0b314591b
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:41 +0800

    Manual Update 4216960

---
+++ 4216960.lua
@@ -0,0 +1,12 @@
+-- 4216960's Lua and Manifest Created by Morrenus
+-- Maya, Valentina, Naomi
+-- Created: March 25, 2026 at 09:09:04 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4216960) -- Maya, Valentina, Naomi
+-- MAIN APP DEPOTS
+addappid(4216961, 1, "a7dddc37108933288394a59825c414a602025be877c7a832c6fa04279dede50e") -- Depot 4216961
+setManifestid(4216961, "6786344782220886828", 424552881)
