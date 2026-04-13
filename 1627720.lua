993c0836dd96de4eb83e4d2e24ffa0759254969d
qiao-lima <qiao-lima@github.com>
2026-04-13 23:48:24 +0800

    Manual Update 1627720

---
+++ 1627720.lua
@@ -0,0 +1,25 @@
+-- 1627720's Lua and Manifest Created by Morrenus
+-- Lies of P
+-- Created: September 29, 2025 at 22:03:15 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 3
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1627720, 1, "3b74c91ab7657b50731863962dcf8d62ba04f0c9c5990cdaaeb7e7ae45c91dcd") -- Lies of P
+addtoken(1627720, "16027423782916783116")
+-- MAIN APP DEPOTS
+addappid(1627721, 1, "d847ff3494ad5b72d69b7b38c52474fdb22d437421b569013dd1508d8879f7c1") -- Depot 1627721
+setManifestid(1627721, "8016983253808484576", 63615838723)
+addappid(1627722, 1, "b2550c254da0b229ff5f5bb765b809d2fac5bd74a98ff3611b8cb8870e02aa6a") -- Depot 1627722
+setManifestid(1627722, "1523068324930851272", 64633420055)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2325280) -- Lies of P  Pre-Order Bonus
+addappid(2325281) -- Lies of P Prince of Krat Cosmetics Pack
+addappid(2848330) -- Lies of P Overture
