5087fbb75674fd658fb27e17b75bd4c009798988
qiao-lima <qiao-lima@github.com>
2026-04-14 00:21:52 +0800

    Manual Update 2949120

---
+++ 2949120.lua
@@ -0,0 +1,12 @@
+-- 2949120's Lua and Manifest Created by Morrenus
+-- No Vacation for an Executioner
+-- Created: March 24, 2026 at 03:22:35 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2949120) -- No Vacation for an Executioner
+-- MAIN APP DEPOTS
+addappid(2949121, 1, "84da56c94f2ed8b07d98296c90097e6f5ba22efb5083c78ed8a8e99f29c9b7a9") -- Depot 2949121
+setManifestid(2949121, "5620702015135797654", 1880389827)
