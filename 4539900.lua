bb7d2d64d286701241c1630425f718112a206e11
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:23 +0800

    Manual Update 4539900

---
+++ 4539900.lua
@@ -0,0 +1,14 @@
+-- 4539900's Lua and Manifest Created by Morrenus
+-- Boobie Impact
+-- Created: April 08, 2026 at 06:38:39 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4539900) -- Boobie Impact
+-- MAIN APP DEPOTS
+addappid(4539902, 1, "5b843be5e6025998cd81dc92c0d7c2456c16c9e698f5ee30bedc57c91cf5dc25") -- Depot 4539902
+setManifestid(4539902, "832542408740672805", 973827423)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4539901) -- Depot 4539901 (empty depot)
