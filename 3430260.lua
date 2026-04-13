8ff9abbc59b0b071430750abe039d5be42bb5f66
qiao-lima <qiao-lima@github.com>
2026-04-14 00:36:50 +0800

    Manual Update 3430260

---
+++ 3430260.lua
@@ -0,0 +1,12 @@
+-- 3430260's Lua and Manifest Created by Morrenus
+-- WRATH: Aeon of Ruin VR - Brutal Edition
+-- Created: April 09, 2026 at 14:27:50 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3430260) -- WRATH: Aeon of Ruin VR - Brutal Edition
+-- MAIN APP DEPOTS
+addappid(3430261, 1, "c9590ef2a5b9579242b8763d4bff5ef0f8e5fd1be2d45a3cae91b10705ed0491") -- Depot 3430261
+setManifestid(3430261, "5458598900943899393", 4328847817)
