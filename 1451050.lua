73dbfae87567efa490465520f8a24c62d0a4395d
qiao-lima <qiao-lima@github.com>
2026-04-13 23:48:17 +0800

    Manual Update 1451050

---
+++ 1451050.lua
@@ -0,0 +1,21 @@
+-- 1451050's Lua and Manifest Created by Morrenus
+-- Sherman Commander
+-- Created: March 15, 2026 at 12:15:32 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 1
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1451050) -- Sherman Commander
+-- MAIN APP DEPOTS
+addappid(1451051, 1, "f091fac73b66814bd58a9b68c680cdd081f686c5d2fb89d0791a99b6cb6c7ba0") -- Depot 1451051
+setManifestid(1451051, "6560044451626891539", 23974415771)
+-- SHARED DEPOTS (from other apps)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Sherman Commander - Supporter Pack (AppID: 4464830)
+addappid(4464830)
+addappid(4464830, 1, "d3151b629c1128a050e2f366535352605d59996300bd1806ab3a764386238782") -- Sherman Commander - Supporter Pack - Depot 4464830
+setManifestid(4464830, "1004216776395956237", 5528503)
