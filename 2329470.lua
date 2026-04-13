8edceaf9e606ad825549de7ce3cdeb82a4c80f97
qiao-lima <qiao-lima@github.com>
2026-04-14 00:07:46 +0800

    Manual Update 2329470

---
+++ 2329470.lua
@@ -0,0 +1,14 @@
+-- 2329470's Lua and Manifest Created by Morrenus
+-- 8
+-- Created: March 17, 2026 at 16:35:35 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2329470) -- 8
+-- MAIN APP DEPOTS
+addappid(2329472, 1, "826016c847d582fdaa3e248e4681ae3e2f1743f1fe8ba4b33f7f7aa1297b9b0f") -- Depot 2329472
+setManifestid(2329472, "1617463298674357415", 820762003)
+addappid(2329471, 1, "45db5aba48c5e8653f962d3812944478d57b96d6e964a0a3658b849a9a381f16") -- Depot 2329471
+setManifestid(2329471, "6511578006593534295", 1164655363)
