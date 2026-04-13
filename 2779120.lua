7d1c99ec5da3bcbec0ec5ddb83612984f136de1b
qiao-lima <qiao-lima@github.com>
2026-04-14 00:17:50 +0800

    Manual Update 2779120

---
+++ 2779120.lua
@@ -0,0 +1,19 @@
+-- 2779120's Lua and Manifest Created by Morrenus
+-- Modulus: Factory Automation
+-- Created: April 07, 2026 at 07:24:52 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(2779120, 1, "b370e477fb5f48154bcef1e5a781f1fe72b50960704e9bdbbba283fa0f6dca00") -- Modulus: Factory Automation
+-- MAIN APP DEPOTS
+addappid(2779121, 1, "a31f71a108c3146f0f6b394879d19557153bea224f7db12818aa0dbd5a5c00c1") -- Depot 2779121
+setManifestid(2779121, "3924947560226485639", 3456055149)
+-- DLCS WITH DEDICATED DEPOTS
+-- Modulus Digital Artbook (AppID: 4428400)
+addappid(4428400)
+addappid(4428400, 1, "4ba54950c9c4b0b9a8ea652b6d1c4950fca97f7ea0d224d811f7bf9080866b93") -- Modulus Digital Artbook - Depot 4428400
+setManifestid(4428400, "840869382268008426", 42272632)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4013900) -- Modulus Factory Automation Deluxe Upgrade Pack
