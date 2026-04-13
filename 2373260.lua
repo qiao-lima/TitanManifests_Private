b0500840f7de1048ab15e82ddcd2dba9d4d5c830
qiao-lima <qiao-lima@github.com>
2026-04-14 00:07:47 +0800

    Manual Update 2373260

---
+++ 2373260.lua
@@ -0,0 +1,12 @@
+-- 2373260's Lua and Manifest Created by Morrenus
+-- 钟塔 Belfry
+-- Created: April 02, 2026 at 10:02:22 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2373260) -- 钟塔 Belfry
+-- MAIN APP DEPOTS
+addappid(2373261, 1, "9dc0779c539b510b513c58a950dd3662887c59034c1a31c89d05225b9526a991") -- Depot 2373261
+setManifestid(2373261, "5246539152029558312", 3073868988)
