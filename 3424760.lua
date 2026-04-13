e5475f144e59f3af11144c94846cebd5c76e64df
qiao-lima <qiao-lima@github.com>
2026-04-14 00:36:50 +0800

    Manual Update 3424760

---
+++ 3424760.lua
@@ -0,0 +1,12 @@
+-- 3424760's Lua and Manifest Created by Morrenus
+-- Gun X
+-- Created: March 30, 2026 at 08:37:07 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3424760) -- Gun X
+-- MAIN APP DEPOTS
+addappid(3424761, 1, "a9343642504435b4c78733574512503e9fa9b1e170259c00f6d099d5985f5f51") -- Depot 3424761
+setManifestid(3424761, "8594624356307453397", 2326677045)
