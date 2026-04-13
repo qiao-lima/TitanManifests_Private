4caf6e336d6e18298b9fdc6cbb4b0a55caaaab2b
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:57 +0800

    Manual Update 4225940

---
+++ 4225940.lua
@@ -0,0 +1,12 @@
+-- 4225940's Lua and Manifest Created by Morrenus
+-- 不好啦！师父被女妖精抓走了！
+-- Created: March 08, 2026 at 03:08:56 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4225940) -- 不好啦！师父被女妖精抓走了！
+-- MAIN APP DEPOTS
+addappid(4225941, 1, "d895f760564551ea206a8fddc5d80ec3cf5048493fe6ffaeb31e03cb611ea959") -- Depot 4225941
+setManifestid(4225941, "5382348047410017885", 55855882017)
