57da10d3867504eadf89927cbbda9e94e6779fbd
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:39 +0800

    Manual Update 3923750

---
+++ 3923750.lua
@@ -0,0 +1,12 @@
+-- 3923750's Lua and Manifest Created by Morrenus
+-- 天选花园
+-- Created: March 15, 2026 at 11:52:36 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3923750) -- 天选花园
+-- MAIN APP DEPOTS
+addappid(3923751, 1, "5cd30f6a970a116c68da3cc8a140c3dc551ae24ba02790a11038810c742ebd8d") -- Depot 3923751
+setManifestid(3923751, "1662873728610830446", 499584616)
