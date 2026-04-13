f61a780b8d304b0c42ad83563d5f57e94b0ea3cb
qiao-lima <qiao-lima@github.com>
2026-04-14 00:36:50 +0800

    Manual Update 3421410

---
+++ 3421410.lua
@@ -0,0 +1,18 @@
+-- 3421410's Lua and Manifest Created by Morrenus
+-- Spiritbound: The Goddess's Quest
+-- Created: March 15, 2026 at 13:21:28 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3421410, 1, "1149e8559f3033709ca101d74a990b248ac8a429e8eacfe48689acd282fb3a4a") -- Spiritbound: The Goddess's Quest
+-- MAIN APP DEPOTS
+addappid(3421411, 1, "b280647f8479d33fba895e7de028e65d2522b57c4609b9bbb3a1e227299a8e9e") -- Depot 3421411
+setManifestid(3421411, "3536033035598701978", 668096744)
+addappid(3421412, 1, "97650cbe9c0805ccf8854d79c4a52c5d16f6d33478a2abb51feaa66dddf2782c") -- Depot 3421412
+setManifestid(3421412, "5141046027336934547", 668030537)
+addappid(3421413, 1, "b90e84d4186fc6e61952907a623488611e5e3e0b7a1a0af0a96731c2e63d94aa") -- Depot 3421413
+setManifestid(3421413, "8048673333642536920", 668079069)
+addappid(3421414, 1, "04ac26a981168326b45966f42e9551e8c67445ee562db1a7c4fbfda70c031e07") -- Depot 3421414
+setManifestid(3421414, "830999757304568443", 668150611)
