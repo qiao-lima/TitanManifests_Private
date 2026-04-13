7e2682886ccb20c05e5001ab20c84ad5fd082702
qiao-lima <qiao-lima@github.com>
2026-04-14 00:40:36 +0800

    Manual Update 3564400

---
+++ 3564400.lua
@@ -0,0 +1,12 @@
+-- 3564400's Lua and Manifest Created by Morrenus
+-- ウマ娘 プリティーダービー
+-- Created: March 30, 2026 at 18:19:39 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3564400) -- ウマ娘 プリティーダービー
+-- MAIN APP DEPOTS
+addappid(3564401, 1, "a59796e965d22b2331f2140b8f5af88f823d93c5fd56fab5f5ecd551650903ae") -- Depot 3564401
+setManifestid(3564401, "4224181121502589299", 645392745)
