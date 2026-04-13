77e3908e553a0b506769db154a7bf5c998587f52
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:05 +0800

    Manual Update 3976390

---
+++ 3976390.lua
@@ -0,0 +1,12 @@
+-- 3976390's Lua and Manifest Created by Morrenus
+-- Quintillion Quest
+-- Created: April 02, 2026 at 08:17:38 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3976390) -- Quintillion Quest
+-- MAIN APP DEPOTS
+addappid(3976391, 1, "5c1945bbc9dd473f1465b9f5d55dbf6cc066ac352c8e7b5a2eff388f00ef461d") -- Depot 3976391
+setManifestid(3976391, "5666893026529428941", 276279335)
