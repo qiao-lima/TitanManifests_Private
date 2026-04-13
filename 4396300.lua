2b0522028f8561d7637609775d637661eccb1073
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:49 +0800

    Manual Update 4396300

---
+++ 4396300.lua
@@ -0,0 +1,12 @@
+-- 4396300's Lua and Manifest Created by Morrenus
+-- SEVENS
+-- Created: April 04, 2026 at 14:24:49 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4396300) -- SEVENS
+-- MAIN APP DEPOTS
+addappid(4396301, 1, "42a4b495bfd80ba960ec0a9091881eb92d52b65a4138171438a58c3817580464") -- Depot 4396301
+setManifestid(4396301, "5420618637260791018", 457048581)
