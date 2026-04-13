fdf31be7faf8fe5d858dc3d58e04f3bfb1a89798
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:13 +0800

    Manual Update 2589500

---
+++ 2589500.lua
@@ -0,0 +1,14 @@
+-- 2589500's Lua and Manifest Created by Morrenus
+-- Whisper of the House
+-- Created: March 19, 2026 at 13:45:37 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2589500, 1, "5163fe41419ecfe3e90105d7df21b63bea613635c470bc40684a61343139d02a") -- Whisper of the House
+-- MAIN APP DEPOTS
+addappid(2589501, 1, "6f51226f61dd888511279cdc32b68f2eafddb79f8d92998ffcac30e6dae73021") -- Depot 2589501
+setManifestid(2589501, "6219536483426424097", 2730876976)
+addappid(2589502, 1, "4b0d0eb017d111efc9280dba6335288de7d0c691d506d8ab30559a254745f0c2") -- Depot 2589502
+setManifestid(2589502, "3139584370204310334", 1372179780)
