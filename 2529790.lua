281023c9e9f0b26fabdd81098d0d9c5ef4bdefd6
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:11 +0800

    Manual Update 2529790

---
+++ 2529790.lua
@@ -0,0 +1,12 @@
+-- 2529790's Lua and Manifest Created by Morrenus
+-- GRIME II
+-- Created: March 31, 2026 at 11:10:07 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2529790) -- GRIME II
+-- MAIN APP DEPOTS
+addappid(2529791, 1, "03c60dc289ec3cdddbc924a04d5f9cf10a1f248a2fa65ccc021fdeb6670499ca") -- Depot 2529791
+setManifestid(2529791, "5962550267628872622", 22553258908)
