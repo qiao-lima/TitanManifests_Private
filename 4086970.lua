1a6a62d9e5d1d1f06c64cb70bc9d85489a861a27
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:51 +0800

    Manual Update 4086970

---
+++ 4086970.lua
@@ -0,0 +1,12 @@
+-- 4086970's Lua and Manifest Created by Morrenus
+-- Crossdresser Killer
+-- Created: March 31, 2026 at 09:14:38 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4086970, 1, "8b9cc4518346b851c2fc700b4784c655216b4959e9f02d46e3342d8018cfde0d") -- Crossdresser Killer
+-- MAIN APP DEPOTS
+addappid(4086971, 1, "a10e54028943c0c1f12a7f0a91c508781c0ff5c16e6dcc5d79f4e1ccc902860d") -- Depot 4086971
+setManifestid(4086971, "1079121825854512620", 1341251564)
