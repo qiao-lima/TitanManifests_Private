a4024d1202385b6e340097fe703a6fd0f47eeeba
qiao-lima <qiao-lima@github.com>
2026-04-14 00:41:13 +0800

    Manual Update 3652650

---
+++ 3652650.lua
@@ -0,0 +1,12 @@
+-- 3652650's Lua and Manifest Created by Morrenus
+-- 恋爱从离别开始
+-- Created: October 02, 2025 at 10:43:25 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3652650) -- 恋爱从离别开始
+-- MAIN APP DEPOTS
+addappid(3652651, 1, "67563758787fa992a2cc0e5b21efb6f754ad4a8e54f7d2d5cbaf8cef7862a987") -- Depot 3652651
+setManifestid(3652651, "3893282125511756283", 54611272282)
