e155215d522235392db4ea9dfa93c85a04197e5e
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:40 +0800

    Manual Update 4215780

---
+++ 4215780.lua
@@ -0,0 +1,12 @@
+-- 4215780's Lua and Manifest Created by Morrenus
+-- Hidden Things Beach Elves
+-- Created: April 03, 2026 at 22:47:00 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4215780) -- Hidden Things Beach Elves
+-- MAIN APP DEPOTS
+addappid(4215781, 1, "03a150e10bf5e567dcae1cc1f4bd1fac2dbc3847e8f380b5c0394c1228f546db") -- Depot 4215781
+setManifestid(4215781, "6926913712373230357", 183226117)
