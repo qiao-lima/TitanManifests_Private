adbb3a2b6a9db485035bae4193e7b1cc43bfa683
qiao-lima <qiao-lima@github.com>
2026-04-14 00:21:47 +0800

    Manual Update 2893570

---
+++ 2893570.lua
@@ -0,0 +1,25 @@
+-- 2893570's Lua and Manifest Created by Morrenus
+-- DRAGON QUEST I & II HD-2D Remake
+-- Created: October 30, 2025 at 14:14:07 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 6
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2893570) -- DRAGON QUEST I & II HD-2D Remake
+-- MAIN APP DEPOTS
+addappid(2893571, 1, "511f158856c1819c10a9d2916437455351f4fde9144e27c85cdbd64769037309") -- Depot 2893571
+setManifestid(2893571, "2485611735242171593", 17042305432)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3707290) -- DRAGON QUEST I  II HD-2D Remake Trouble-Free Travel Kit
+addappid(3707300) -- DRAGON QUEST I  II HD-2D Remake Adventurous Accessory Kit
+addappid(3707310) -- DRAGON QUEST I  II HD-2D Remake Reasonably Priced Regalia Kit
+addappid(3904130) -- DRAGON QUEST I  II HD-2D Remake Beginner Battler Kit
+addappid(3904140) -- DRAGON QUEST I  II HD-2D Remake Bona Fide Battle Kit
+addappid(3904280) -- DRAGON QUEST I  II HD-2D Remake Essential Items Kit
