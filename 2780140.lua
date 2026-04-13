3ae0d8a33cb508f3fc307051b30a1d0d5c33433d
qiao-lima <qiao-lima@github.com>
2026-04-14 00:17:50 +0800

    Manual Update 2780140

---
+++ 2780140.lua
@@ -0,0 +1,14 @@
+-- 2780140's Lua and Manifest Created by Morrenus
+-- Sauna of the DEAD
+-- Created: April 02, 2026 at 06:00:38 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2780140, 1, "d242b133a9544c303e85c5dd76e99bfd7826a08ad8065c4ad73b036a40537a14") -- Sauna of the DEAD
+-- MAIN APP DEPOTS
+addappid(2780141, 1, "7092920f9afed8985b9f23b7e7a4107e6aef51a78f5a4c7d50b2687841d4e8c7") -- Depot 2780141
+setManifestid(2780141, "5329787654913849365", 223380883)
+addappid(2780142, 1, "41e1a11ea78a90aa4d6852cb3a468e781fe12074706ba6b81ec14e4b0820551b") -- Depot 2780142
+setManifestid(2780142, "1934608522049942623", 359526452)
