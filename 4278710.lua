782ce3a03dd310b52eb833ed98b42d4770216a96
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:20 +0800

    Manual Update 4278710

---
+++ 4278710.lua
@@ -0,0 +1,12 @@
+-- 4278710's Lua and Manifest Created by Morrenus
+-- Medieval Life Simulator
+-- Created: March 19, 2026 at 23:14:38 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4278710) -- Medieval Life Simulator
+-- MAIN APP DEPOTS
+addappid(4278711, 1, "d7806c9ac69c5f925ec24bfa528ab19ddeb3c5e06c223693098c9ff52226ec34") -- Depot 4278711
+setManifestid(4278711, "1230661837504340993", 2959908047)
