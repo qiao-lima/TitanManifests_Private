179efda3a3515942f298ab86628bf258ce54e536
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:13 +0800

    Manual Update 4173750

---
+++ 4173750.lua
@@ -0,0 +1,12 @@
+-- 4173750's Lua and Manifest Created by Morrenus
+-- Angel Engine
+-- Created: March 17, 2026 at 15:42:03 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4173750) -- Angel Engine
+-- MAIN APP DEPOTS
+addappid(4173751, 1, "e7c319379685910f14c5a8f00d18107b5e6c009aabca995467dbaab55bae3faa") -- Depot 4173751
+setManifestid(4173751, "2355581038833336470", 1067119961)
