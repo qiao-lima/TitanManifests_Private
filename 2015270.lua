3d13f84c63ff1ec69284dc8d8788251e6a927575
qiao-lima <qiao-lima@github.com>
2026-04-13 23:58:17 +0800

    Manual Update 2015270

---
+++ 2015270.lua
@@ -0,0 +1,16 @@
+-- 2015270's Lua and Manifest Created by Morrenus
+-- Rotwood
+-- Created: March 13, 2026 at 23:29:52 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2015270, 1, "256d70eace742a20b4ee30690205b3c12b8b1aa7555febefff2c21904dc316ac") -- Rotwood
+-- MAIN APP DEPOTS
+addappid(2015271, 1, "52e97957aab04bacd50fec8149cc4c447b6cfdf455482b37ab728cb7d14779fe") -- Depot 2015271
+setManifestid(2015271, "3116949840295791505", 1875818525)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
