a22893e60414d57bbf240b8207462a3eccc68c61
qiao-lima <qiao-lima@github.com>
2026-04-14 00:27:14 +0800

    Manual Update 3192340

---
+++ 3192340.lua
@@ -0,0 +1,14 @@
+-- 3192340's Lua and Manifest Created by Morrenus
+-- Steamy Livestreams with My Girlfriend
+-- Created: March 27, 2026 at 06:30:17 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3192340) -- Steamy Livestreams with My Girlfriend
+-- MAIN APP DEPOTS
+addappid(3192341, 1, "09f43f1aa3973ed13bce58f80a4742d9da74999d1f294bfe062c3dc608d98166") -- Depot 3192341
+setManifestid(3192341, "8857530530437600011", 3771172248)
+addappid(3192344, 1, "4b98b30d1a00d7dd5ddbab928a25e31fa44a35b1c241a4880c8878bbd9e7246d") -- Depot 3192344
+setManifestid(3192344, "1943193921938970974", 3771261589)
