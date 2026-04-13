c8e19daa13240dedae25703666db54aa290e3392
qiao-lima <qiao-lima@github.com>
2026-04-14 00:40:34 +0800

    Manual Update 3531160

---
+++ 3531160.lua
@@ -0,0 +1,12 @@
+-- 3531160's Lua and Manifest Created by Morrenus
+-- Island Survival
+-- Created: March 11, 2026 at 21:11:10 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3531160) -- Island Survival
+-- MAIN APP DEPOTS
+addappid(3531161, 1, "dfe7bdcdc7bd94813f7cbe67c4116bed483d6a8a66fb901f3a6200d363d717d9") -- Depot 3531161
+setManifestid(3531161, "4332121231797894170", 68192921870)
