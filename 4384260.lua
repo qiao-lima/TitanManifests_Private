7feb33bc4b0ed2d35f0bc69b32a79eaf0d119bb3
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:48 +0800

    Manual Update 4384260

---
+++ 4384260.lua
@@ -0,0 +1,16 @@
+-- 4384260's Lua and Manifest Created by Morrenus
+-- My Waifu's Stream is Going Viral ‼️😳
+-- Created: April 12, 2026 at 15:12:44 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(4384260) -- My Waifu's Stream is Going Viral ‼️😳
+-- MAIN APP DEPOTS
+addappid(4384261, 1, "f383c9bfdcd6c28004ae945ad3a8f7b104a7bc6d9636f999d0c0e9afeddc589a") -- Depot 4384261
+setManifestid(4384261, "5611405484583392867", 1621788792)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- My Waifus Stream is Going Viral  - Wallpapers Pack  (AppID: 4468000) - missing depot keys
+-- addappid(4468000)
