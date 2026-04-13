11ee84085a0da0930e7173a97499ed0cda3b6b1f
qiao-lima <qiao-lima@github.com>
2026-04-14 00:27:11 +0800

    Manual Update 3150970

---
+++ 3150970.lua
@@ -0,0 +1,12 @@
+-- 3150970's Lua and Manifest Created by Morrenus
+-- Party Madness
+-- Created: April 05, 2026 at 02:30:04 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3150970) -- Party Madness
+-- MAIN APP DEPOTS
+addappid(3150971, 1, "ecbadca87574936a53c312e4d356f1f50ad410dc995a074377e7e0c036047430") -- Depot 3150971
+setManifestid(3150971, "8917035137926289850", 1729922399)
