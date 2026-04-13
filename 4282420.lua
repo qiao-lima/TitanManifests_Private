7b470aede242a043dcfe3db0f77ec56fa89b51ef
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:20 +0800

    Manual Update 4282420

---
+++ 4282420.lua
@@ -0,0 +1,12 @@
+-- 4282420's Lua and Manifest Created by Morrenus
+-- Anomaly 404
+-- Created: April 05, 2026 at 19:06:24 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4282420) -- Anomaly 404
+-- MAIN APP DEPOTS
+addappid(4282421, 1, "e9f2711645bcf01d16dec4504032fc1f09a35d66eb39531daba4805f572f1258") -- Depot 4282421
+setManifestid(4282421, "8805768435435365973", 952913221)
