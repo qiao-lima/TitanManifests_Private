1978c25793865503c0142fb55c93366e1b702605
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:22 +0800

    Manual Update 4513730

---
+++ 4513730.lua
@@ -0,0 +1,12 @@
+-- 4513730's Lua and Manifest Created by Morrenus
+-- Unsettled : NIGHT SHIFT
+-- Created: April 04, 2026 at 04:52:16 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4513730) -- Unsettled : NIGHT SHIFT
+-- MAIN APP DEPOTS
+addappid(4513731, 1, "ce0573e188c7661d8d4433e6aad52ba4c23f78ab6526fcf4e3ddfe16f3d2aa85") -- Depot 4513731
+setManifestid(4513731, "1571969480759154809", 2007411184)
