e90c69fbc7d94937db7d53f8c3720e5f6d4719ed
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:22 +0800

    Manual Update 4529260

---
+++ 4529260.lua
@@ -0,0 +1,12 @@
+-- 4529260's Lua and Manifest Created by Morrenus
+-- Infinite Dungeon: Idle Adventure
+-- Created: April 09, 2026 at 21:12:53 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4529260) -- Infinite Dungeon: Idle Adventure
+-- MAIN APP DEPOTS
+addappid(4529261, 1, "91854ed4de15f9917a4744659fda72bd43b49f8325ce3fe6f7372f5ba0519263") -- Depot 4529261
+setManifestid(4529261, "1866388869563133409", 207600785)
