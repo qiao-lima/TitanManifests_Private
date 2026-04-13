5ba106bfd353c28343ad39f23abcfa696aacdec8
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:03 +0800

    Manual Update 3948120

---
+++ 3948120.lua
@@ -0,0 +1,16 @@
+-- 3948120's Lua and Manifest Created by Morrenus
+-- Scritchy Scratchy
+-- Created: March 18, 2026 at 09:28:28 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3948120) -- Scritchy Scratchy
+-- MAIN APP DEPOTS
+addappid(3948121, 1, "4d5ba58100eba881f6f9a66ee5982857f3db6a375f5319bdfddd6d9b71defc07") -- Depot 3948121
+setManifestid(3948121, "4123795260442269537", 438126944)
+addappid(3948122, 1, "328927d058e2934b073e34b22bcbf5103daa2dea1879877f2ef930f2cf7ef046") -- Depot 3948122
+setManifestid(3948122, "5148960014277779244", 545008520)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4428640) -- Scritchy Scratchy - Supporter Pack
