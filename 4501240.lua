45b53dd0283f2246eb704518b46b2fd173a384a9
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:09 +0800

    Manual Update 4501240

---
+++ 4501240.lua
@@ -0,0 +1,12 @@
+-- 4501240's Lua and Manifest Created by Morrenus
+-- 帝王霸业
+-- Created: April 03, 2026 at 02:02:48 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4501240) -- 帝王霸业
+-- MAIN APP DEPOTS
+addappid(4501241, 1, "98b38faa279aaa3d07f39b784e99db5c5f2434bf8255c72335e169034a8b10c3") -- Depot 4501241
+setManifestid(4501241, "8496971237841177499", 57897025)
