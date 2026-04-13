df7f505f89be38589efb48c95f107e3269184b62
qiao-lima <qiao-lima@github.com>
2026-04-14 00:41:15 +0800

    Manual Update 367520

---
+++ 367520.lua
@@ -0,0 +1,16 @@
+-- 367520's Lua and Manifest Created by Morrenus
+-- Hollow Knight
+-- Created: March 27, 2026 at 03:55:31 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(367520, 1, "94a0a4b4c50ce3065ce9d4d21eecfdb8bae52f99b16c4eea1649ebcce9ac2e15") -- Hollow Knight
+-- MAIN APP DEPOTS
+addappid(367521, 1, "de801b9b61e3385b333259cc0f04d2e5d976c4cb8f933cb859d75a4d0125a2a8") -- Hollow Knight Windows
+setManifestid(367521, "257781644874438846", 5231995691)
+addappid(367522, 1, "c7e67c6bf87d7128302920e88b17cb6ee322e69585472cbcd2a3d913c7d3579a") -- Hollow Knight Mac
+setManifestid(367522, "6809616861563627670", 5284750173)
+addappid(367523, 1, "61dd4c0fd624c81feb31fc22cb61a228c4d8077cf3927e9058f736026d9dd838") -- Hollow Knight Linux
+setManifestid(367523, "708613018541602983", 5241907316)
