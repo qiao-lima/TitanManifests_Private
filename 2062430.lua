cd2d7412c2374c1d2dcc6086f54b28f0a9e28d4b
qiao-lima <qiao-lima@github.com>
2026-04-13 23:58:18 +0800

    Manual Update 2062430

---
+++ 2062430.lua
@@ -0,0 +1,16 @@
+-- 2062430's Lua and Manifest Created by Morrenus
+-- BALL x PIT
+-- Created: January 28, 2026 at 12:21:17 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2062430) -- BALL x PIT
+-- MAIN APP DEPOTS
+addappid(2062431, 1, "5d28ee4226eb2240f32250a615d7cdf1e5beebec25073151597d734fc48d547c") -- Depot 2062431
+setManifestid(2062431, "980001640042031979", 506663860)
+addappid(2062432, 1, "daff4f0a6a959fa32208ec2678d460e5942bc4545b26230176a869b8139706d0") -- Depot 2062432
+setManifestid(2062432, "7550950024373520625", 479900652)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4008250) -- Depot 4008250 (empty depot)
