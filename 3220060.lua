ab1e1e6c27315d09abb3a20142490808a8ac3e06
qiao-lima <qiao-lima@github.com>
2026-04-14 00:30:05 +0800

    Manual Update 3220060

---
+++ 3220060.lua
@@ -0,0 +1,12 @@
+-- 3220060's Lua and Manifest Created by Morrenus
+-- The Weeping Swan: Ten Days of the City's Fall
+-- Created: April 04, 2026 at 03:54:29 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3220060) -- The Weeping Swan: Ten Days of the City's Fall
+-- MAIN APP DEPOTS
+addappid(3220061, 1, "63c6e0eb080e22155cdf3dfa2aef1c9f0f54c9ce06058a349237067e1f6756c4") -- Depot 3220061
+setManifestid(3220061, "1106855610188370963", 5696600566)
