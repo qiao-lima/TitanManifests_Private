8516e2d458b235831cc970c9ad333e9f9d35121f
qiao-lima <qiao-lima@github.com>
2026-04-14 02:53:06 +0800

    Manual Update 960990

---
+++ 960990.lua
@@ -0,0 +1,40 @@
+-- 960990's Lua and Manifest Created by Morrenus
+-- Beyond: Two Souls
+-- Created: September 29, 2025 at 01:03:11 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 14
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(960990) -- Beyond: Two Souls
+-- MAIN APP DEPOTS
+addappid(960991, 1, "120e868e509a7eb6f5fcf3924ff7726c5db98bceb8340ca8e16aa373acc0fe5c") -- BEYOND: Two Souls Depot
+setManifestid(960991, "1913757105463724521", 46332929339)
+addappid(960992, 1, "dede946a447b25c049863aa0ced1b09195c52e5377a8022e12e4ca4509080ee9") -- BEYOND: Two Souls Depot Brazilian
+setManifestid(960992, "4537214821798897964", 2976905881)
+addappid(960993, 1, "7c073d979171b26c99ee683e9918c558408b6144231cea9341ed09789459bc7c") -- BEYOND: Two Souls Depot Dutch
+setManifestid(960993, "2769911930321439716", 2994503139)
+addappid(960994, 1, "ee093b5e12f97397950fb7af50394e1d9c256868d2d1edd40a135205dd714167") -- BEYOND: Two Souls Depot French
+setManifestid(960994, "8747410009632164257", 2972473247)
+addappid(960995, 1, "f22b1bc4870e917078a956d1efd7dee80d92c9616e7dadc8b60e61a09464e4a9") -- BEYOND: Two Souls Depot German
+setManifestid(960995, "8436326980925125749", 2992492061)
+addappid(960996, 1, "d7e856a761b4c499a11e8a8fd695e938d4fd8be141360bfe5d0e6b2d1e6a2ce0") -- BEYOND: Two Souls Depot Italian
+setManifestid(960996, "8305227949044770238", 2967451621)
+addappid(960997, 1, "4e53411c0ef8214d273205bf92c70ef42bf786b70ccc1e1acfbf08ec2630b077") -- BEYOND: Two Souls Depot Japanese
+setManifestid(960997, "790324341833987075", 3009126871)
+addappid(960998, 1, "0366b83db473e8e4c0f960a151fe9391143ab7d224de1a894c7202e63be0c7d7") -- BEYOND: Two Souls Depot LatinAmerical
+setManifestid(960998, "2000188158773816775", 2964764191)
+addappid(960999, 1, "444f4fb2ad630406d92c7056bd7baa3fd5f5f882091f5778fdd64edd24108846") -- BEYOND: Two Souls Depot Polish
+setManifestid(960999, "2295622895764314494", 2995111283)
+addappid(1296460, 1, "fee8e4615e3d5423cc0c4fc5c12f32a79f6628f33a8ce09c2ac30cdd17a42bee") -- BEYOND: Two Souls Depot Portuguese
+setManifestid(1296460, "85138733293325991", 2988967403)
+addappid(1296461, 1, "8c77cedf0520e7617c3f00e3afbfa9c70a8afad20bb9c930539284bbf293b792") -- BEYOND: Two Souls Depot Russian
+setManifestid(1296461, "6720056381366113333", 3090677055)
+addappid(1296462, 1, "61bbb2de2378b6c37fe505c51e5d7a578023b169cdc341961e8c166aebd92345") -- BEYOND: Two Souls Depot Spanish
+setManifestid(1296462, "3012758780453563052", 2986014533)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
