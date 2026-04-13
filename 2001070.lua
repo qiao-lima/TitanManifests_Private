8e3bbc492c8e20f410b053e316309c5baed3c703
qiao-lima <qiao-lima@github.com>
2026-04-13 23:58:16 +0800

    Manual Update 2001070

---
+++ 2001070.lua
@@ -0,0 +1,18 @@
+-- 2001070's Lua and Manifest Created by Morrenus
+-- Heart of the Machine
+-- Created: March 06, 2026 at 18:15:10 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2001070) -- Heart of the Machine
+-- MAIN APP DEPOTS
+addappid(2001071, 1, "1a1bb30e75dac9319e59c05cd2cf671bd58d69622641a320a63a2bd9665ce7a4") -- Depot 2001071
+setManifestid(2001071, "5534434333250899789", 215564956)
+addappid(2001072, 1, "d99c074ccbebc30d9fd3c7e65ee1025c781f28985577c017a291b232cd65f927") -- Depot 2001072
+setManifestid(2001072, "3343554659145372996", 49500)
+addappid(2001073, 1, "dfdd7b00971120c31672747d712438f1195956496423bc46bf525178e8924d34") -- Depot 2001073
+setManifestid(2001073, "2938021080148139687", 3843833459)
+addappid(2001074, 1, "2db2619ffb8909de8d65a6ec36840288680f7d199371e175fa7c6e4b4a582070") -- Depot 2001074
+setManifestid(2001074, "4309287497549223556", 4039904795)
