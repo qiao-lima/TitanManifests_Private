b8d9fc2eed2dfe19472747b822b08c0acf12a93c
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:06 +0800

    Manual Update 4461640

---
+++ 4461640.lua
@@ -0,0 +1,14 @@
+-- 4461640's Lua and Manifest Created by Morrenus
+-- Profit Sea Incremental
+-- Created: April 01, 2026 at 22:34:55 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4461640) -- Profit Sea Incremental
+-- MAIN APP DEPOTS
+addappid(4461641, 1, "d32e5f78a8068a5c03f4be3c1e31cd5b85193d004cd0cebc8e1cc18a24a179fe") -- Depot 4461641
+setManifestid(4461641, "9084560547840299355", 95147717)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4552260) -- Profit Sea Incremental - Supporter Pack
