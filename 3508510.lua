3363d3f9e5e01f7f3a3d51105396b8215850b747
qiao-lima <qiao-lima@github.com>
2026-04-14 00:40:32 +0800

    Manual Update 3508510

---
+++ 3508510.lua
@@ -0,0 +1,12 @@
+-- 3508510's Lua and Manifest Created by Morrenus
+-- Guardians of Dao
+-- Created: March 25, 2026 at 12:27:15 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3508510) -- Guardians of Dao
+-- MAIN APP DEPOTS
+addappid(3508512, 1, "11b67d172b1d3b722b96843bf2af0a0997a1d6d6753607609314a7f497368994") -- Depot 3508512
+setManifestid(3508512, "5620973690702286142", 533564623)
