294bac8019ecc99e7fbee64f7241917b8275637d
qiao-lima <qiao-lima@github.com>
2026-04-14 00:21:49 +0800

    Manual Update 2918500

---
+++ 2918500.lua
@@ -0,0 +1,12 @@
+-- 2918500's Lua and Manifest Created by Morrenus
+-- Cozy Island
+-- Created: January 19, 2026 at 07:58:43 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2918500) -- Cozy Island
+-- MAIN APP DEPOTS
+addappid(2918501, 1, "444ce90f8665148c49ad7c254f7729d6e85e823d967a915c0e2350f887d1245f") -- Depot 2918501
+setManifestid(2918501, "4611973521006905814", 897407533)
