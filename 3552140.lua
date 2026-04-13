490331cefaf53c3941eae0ee651f25a8068a2c52
qiao-lima <qiao-lima@github.com>
2026-04-14 00:40:35 +0800

    Manual Update 3552140

---
+++ 3552140.lua
@@ -0,0 +1,18 @@
+-- 3552140's Lua and Manifest Created by Morrenus
+-- Retro Rewind - Video Store Simulator
+-- Created: March 20, 2026 at 06:57:40 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3552140) -- Retro Rewind - Video Store Simulator
+-- MAIN APP DEPOTS
+addappid(3552141, 1, "8527bf46fe5454e6c8956a042fb1aee9a9792d7f4c73ebe85ab6924dfcc1c0fe") -- Depot 3552141
+setManifestid(3552141, "6991286191459431775", 1866360653)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
