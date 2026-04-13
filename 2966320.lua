156f323ef019492654eb65771c50fbbc865f4f67
qiao-lima <qiao-lima@github.com>
2026-04-14 00:21:52 +0800

    Manual Update 2966320

---
+++ 2966320.lua
@@ -0,0 +1,18 @@
+-- 2966320's Lua and Manifest Created by Morrenus
+-- Starsand Island
+-- Created: March 13, 2026 at 11:13:27 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 4
+
+-- MAIN APPLICATION
+addappid(2966320, 1, "208ea6f9923a37a7b011f392e50f7f7ab2952497337e184dbd00448c63bd1a50") -- Starsand Island
+addtoken(2966320, "1152859757700171993")
+-- MAIN APP DEPOTS
+addappid(2966321, 1, "ce86c628c5c525587d2aea36d5a745c980584e22eca6389d8bfd7a63c0db1c4c") -- Depot 2966321
+setManifestid(2966321, "4771796159869922542", 10757689814)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4391920) --  DLC
+addappid(4395490) --  DLC
+addappid(4424260) --  DLC
+addappid(4460980) --  DLC
