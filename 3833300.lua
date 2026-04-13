d50a99dde6aa09d387263285220485b15765ae6b
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:10 +0800

    Manual Update 3833300

---
+++ 3833300.lua
@@ -0,0 +1,18 @@
+-- 3833300's Lua and Manifest Created by Morrenus
+-- Infinite Cards
+-- Created: March 29, 2026 at 03:18:23 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3833300) -- Infinite Cards
+-- MAIN APP DEPOTS
+addappid(3833301, 1, "dfebe7a95a639fd93e520cb5827bbd1358ed8fc68bd550abe9011bb5a9862079") -- Depot 3833301
+setManifestid(3833301, "8440461818917387337", 2136471436)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
