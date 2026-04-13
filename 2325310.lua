3baecd753ac2b1e11160ecf1904665f8f2861a47
qiao-lima <qiao-lima@github.com>
2026-04-14 00:06:20 +0800

    Manual Update 2325310

---
+++ 2325310.lua
@@ -0,0 +1,13 @@
+-- 2325310's Lua and Manifest Created by Morrenus
+-- 幻想乡：有罪推定~夜明前的三色堇
+-- Created: March 15, 2026 at 15:52:44 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2325310) -- 幻想乡：有罪推定~夜明前的三色堇
+addtoken(2325310, "2863926897060389178")
+-- MAIN APP DEPOTS
+addappid(2325311, 1, "893b2ebfd37db2518e5b1be2c1560fc8c0d4607e8cbd1d0dcaaa811c272c8956") -- Depot 2325311
+setManifestid(2325311, "6414426521508543227", 5177978245)
