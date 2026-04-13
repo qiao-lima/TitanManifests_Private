3074120408a3dc8b3d427e0adb31647067978158
qiao-lima <qiao-lima@github.com>
2026-04-14 00:17:49 +0800

    Manual Update 2762360

---
+++ 2762360.lua
@@ -0,0 +1,18 @@
+-- 2762360's Lua and Manifest Created by Morrenus
+-- Harvest Cafe
+-- Created: April 05, 2026 at 03:15:11 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2762360) -- Harvest Cafe
+-- MAIN APP DEPOTS
+addappid(2762361, 1, "730ea8afd99b28592a591534248e0f96969f2cd530946989a7b0091afb866bc6") -- Depot 2762361
+setManifestid(2762361, "3934069101528793915", 2732715104)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
