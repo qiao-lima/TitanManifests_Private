255d00a41d335555ac68bd14e42f1bbcbc6e123b
qiao-lima <qiao-lima@github.com>
2026-04-13 23:36:13 +0800

    Manual Update 1030300

---
+++ 1030300.lua
@@ -0,0 +1,16 @@
+-- 1030300's Lua and Manifest Created by Morrenus
+-- Hollow Knight: Silksong
+-- Created: November 12, 2025 at 09:52:17 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1030300) -- Hollow Knight: Silksong
+-- MAIN APP DEPOTS
+addappid(1030301, 1, "f4a253f96969f93b8a9392cddf6f1b779886ea10a160e7ca148b92bae09abd28") -- Depot 1030301
+setManifestid(1030301, "3545882420322545098", 8123807415)
+addappid(1030302, 1, "bc736fb6e74601f627b4890559822b4b8e786e36e949b881dcd25419e8a15e9e") -- Depot 1030302
+setManifestid(1030302, "7345001466169537628", 8177053643)
+addappid(1030303, 1, "ea761f41bd28ae29fd22375e81702eadc64796099fd9dcf01ea2a190536cc7c3") -- Depot 1030303
+setManifestid(1030303, "4349246050376532986", 8142182908)
