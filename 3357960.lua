6408dfcafdbaf0e4fbeebbdab9ee6aea77b367d2
qiao-lima <qiao-lima@github.com>
2026-04-14 00:32:41 +0800

    Manual Update 3357960

---
+++ 3357960.lua
@@ -0,0 +1,20 @@
+-- 3357960's Lua and Manifest Created by Morrenus
+-- KuloNiku
+-- Created: April 07, 2026 at 08:56:15 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0 (1 excluded)
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3357960) -- KuloNiku
+-- MAIN APP DEPOTS
+addappid(3357961, 1, "41063b5ba12ea1c0eccad05185cf132f3fcd41b3644350c5befbb95fb84831e7") -- Depot 3357961
+setManifestid(3357961, "5852494441694412480", 1353514106)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- KuloNiku Bowl Up Digital Artbook (AppID: 4338290) - missing depot keys
+-- addappid(4338290)
