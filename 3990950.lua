8eddb93f44e878745e13fed75d0234f50e2f5c28
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:21 +0800

    Manual Update 3990950

---
+++ 3990950.lua
@@ -0,0 +1,12 @@
+-- 3990950's Lua and Manifest Created by Morrenus
+-- Miss World Love Growth Diary
+-- Created: March 17, 2026 at 19:01:33 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3990950) -- Miss World Love Growth Diary
+-- MAIN APP DEPOTS
+addappid(3990951, 1, "e1933a9cd05b7d0f328f83fdacf2f793ce55426f0fdc95556fc613d19161248e") -- Depot 3990951
+setManifestid(3990951, "8927145252839470154", 16909572742)
