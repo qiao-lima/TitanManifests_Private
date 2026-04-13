fe4d09064d2d95051b549b584d830110382b7b2f
qiao-lima <qiao-lima@github.com>
2026-04-13 23:52:43 +0800

    Manual Update 1836560

---
+++ 1836560.lua
@@ -0,0 +1,16 @@
+-- 1836560's Lua and Manifest Created by Morrenus
+-- Aether & Iron
+-- Created: March 30, 2026 at 18:51:32 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(1836560) -- Aether & Iron
+-- MAIN APP DEPOTS
+addappid(1836561, 1, "6e24c23bc4a2516ff1edc94ca6f99ca2b8a2d5cc35606ec7cbf6ec1fbb2e4956") -- Depot 1836561
+setManifestid(1836561, "8074505754214827626", 12894974245)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Aether  Iron - Digital Art Pack (AppID: 4175420) - missing depot keys
+-- addappid(4175420)
