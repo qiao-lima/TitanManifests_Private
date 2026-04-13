b04601f04147dd661c50d750a093d3cd8a93b445
qiao-lima <qiao-lima@github.com>
2026-04-14 00:41:12 +0800

    Manual Update 3629290

---
+++ 3629290.lua
@@ -0,0 +1,12 @@
+-- 3629290's Lua and Manifest Created by Morrenus
+-- Paragnosia: Museum
+-- Created: March 30, 2026 at 14:23:56 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3629290) -- Paragnosia: Museum
+-- MAIN APP DEPOTS
+addappid(3629291, 1, "15e78439b82e81fa3e0cc1fe038e44ebc0c9b8fa6bbf60f44d79a660f4460c5f") -- Depot 3629291
+setManifestid(3629291, "573181076661254943", 6177355999)
