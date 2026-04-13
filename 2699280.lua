367ed129a4a3b1b1b2cf08fee7f9fc573c210cd3
qiao-lima <qiao-lima@github.com>
2026-04-14 00:17:47 +0800

    Manual Update 2699280

---
+++ 2699280.lua
@@ -0,0 +1,14 @@
+-- 2699280's Lua and Manifest Created by Morrenus
+-- Valor of Man
+-- Created: March 22, 2026 at 08:02:43 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2699280) -- Valor of Man
+-- MAIN APP DEPOTS
+addappid(2699281, 1, "5aea9d8d8acfbc2d40001ea6dc5eb9ee92f56ef73cebca35c8ad23b96f407073") -- Depot 2699281
+setManifestid(2699281, "8295561939656557144", 3618334794)
+addappid(2699282, 1, "013c7477d96b0a243f9203d9abe912f2d82ed714bab65400abb2f1344101bac8") -- Depot 2699282
+setManifestid(2699282, "6778959620225322207", 0)
