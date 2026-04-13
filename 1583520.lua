7ead8706600a838b287b8a3f3d5908df5f331505
qiao-lima <qiao-lima@github.com>
2026-04-13 23:48:21 +0800

    Manual Update 1583520

---
+++ 1583520.lua
@@ -0,0 +1,12 @@
+-- 1583520's Lua and Manifest Created by Morrenus
+-- Under The Island
+-- Created: March 11, 2026 at 04:49:23 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1583520) -- Under The Island
+-- MAIN APP DEPOTS
+addappid(1583521, 1, "933f8d4241e3c605f0ccbfd390df7d6d4dd779c93db37a8f8b65e4e19e02fddc") -- Depot 1583521
+setManifestid(1583521, "169803463242427894", 562748925)
