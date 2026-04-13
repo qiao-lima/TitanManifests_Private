a387a0dd9df026b6d367ceabe642c6678bd8110d
qiao-lima <qiao-lima@github.com>
2026-04-14 00:30:10 +0800

    Manual Update 3282390

---
+++ 3282390.lua
@@ -0,0 +1,23 @@
+-- 3282390's Lua and Manifest Created by Morrenus
+--  完蛋！我被美女包围了！ 2
+-- Created: December 08, 2025 at 12:29:35 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 1
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3282390) --  完蛋！我被美女包围了！ 2
+-- MAIN APP DEPOTS
+addappid(3282391, 1, "07a83387135b433b3c5183af9d897c8be8cca42628bfcc25fd8beed93c7c3966") -- Depot 3282391
+setManifestid(3282391, "7428450465038491324", 31771852357)
+addappid(3282392, 1, "8faceae292b941e688bebce259ef4d83ab39c7c02a46223b84b4be13a09aa052") -- Depot 3282392
+setManifestid(3282392, "2805433192131247374", 31912066094)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITH DEDICATED DEPOTS
+-- 2-2K (AppID: 3814100)
+addappid(3814100)
+addappid(3814101, 1, "fd5536c8b4eb8790c06f649a8b20385b482d8fc2f1663c88e6d0b2c058a36d24") -- 2-2K - Depot 3814101
+setManifestid(3814101, "111395973738879839", 43436385440)
