9b5eea910e5ac4f20b999fafe4749995dba039c9
qiao-lima <qiao-lima@github.com>
2026-04-14 00:17:51 +0800

    Manual Update 2807150

---
+++ 2807150.lua
@@ -0,0 +1,21 @@
+-- 2807150's Lua and Manifest Created by Morrenus
+-- Worshippers of Cthulhu
+-- Created: November 11, 2025 at 06:11:44 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(2807150) -- Worshippers of Cthulhu
+-- MAIN APP DEPOTS
+addappid(2807151, 1, "1969ccb9a84fc59a5a2e4c3ea8f3ef0d00ce746e5fe05f2dec27cc4583a45de3") -- Depot 2807151
+setManifestid(2807151, "1965739123291587433", 10498304777)
+-- DLCS WITH DEDICATED DEPOTS
+-- Worshippers of Cthulhu - Artbook (AppID: 3912590)
+addappid(3912590)
+addappid(3912591, 1, "4629dc7d387a903958a204ee7adc286a09491d32cec9a2d953672ca7748aeed3") -- Worshippers of Cthulhu - Artbook - Depot 3912591
+setManifestid(3912591, "8969526720109211949", 39050599)
+-- Worshippers of Cthulhu - Event Pack (AppID: 3964220)
+addappid(3964220)
+addappid(3964220, 1, "acecd028a0536aa248d94522b14928da55e7fc75961f456b121c21f0d828470a") -- Worshippers of Cthulhu - Event Pack - Depot 3964220
+setManifestid(3964220, "6791124115384923238", 16111930)
