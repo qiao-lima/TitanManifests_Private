b5bbadab001cf2d280578fafd9100f3412c907c5
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:38 +0800

    Manual Update 3914500

---
+++ 3914500.lua
@@ -0,0 +1,12 @@
+-- 3914500's Lua and Manifest Created by Morrenus
+-- I Can Only Speak Doner
+-- Created: April 07, 2026 at 22:12:45 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3914500) -- I Can Only Speak Doner
+-- MAIN APP DEPOTS
+addappid(3914501, 1, "84e4f9816868640d9bb0dbe58cfb483069bfa1ccb6b91cafc377aa0f47764241") -- Depot 3914501
+setManifestid(3914501, "3010269195939019566", 339278456)
