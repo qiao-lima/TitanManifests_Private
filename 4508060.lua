57746bafe2cb0ea08127baa9c6c10957fdfd6b50
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:10 +0800

    Manual Update 4508060

---
+++ 4508060.lua
@@ -0,0 +1,12 @@
+-- 4508060's Lua and Manifest Created by Morrenus
+-- Undead Tide: Light's Defense
+-- Created: March 30, 2026 at 07:59:52 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4508060) -- Undead Tide: Light's Defense
+-- MAIN APP DEPOTS
+addappid(4508061, 1, "ab0a1e2bd0ed040b88ac4a60f7d33ea2b524c808bb6fe51081a1909baf34b789") -- Depot 4508061
+setManifestid(4508061, "5798246217516919858", 207073057)
