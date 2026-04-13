aa6f4fb038f7f5b941c9ecbc70ed42f447e08751
qiao-lima <qiao-lima@github.com>
2026-04-14 00:27:14 +0800

    Manual Update 3196180

---
+++ 3196180.lua
@@ -0,0 +1,12 @@
+-- 3196180's Lua and Manifest Created by Morrenus
+-- Trivia About You
+-- Created: April 04, 2026 at 13:15:57 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3196180) -- Trivia About You
+-- MAIN APP DEPOTS
+addappid(3196181, 1, "a0b3316d3e438dd6daf34d92b9f5658b3be233d7a55ee6cfdd703c8717384597") -- Depot 3196181
+setManifestid(3196181, "2808458885036217890", 2722923307)
