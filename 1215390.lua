905951804238ef6f6234fd4ba80b5bb6a16f1fc9
qiao-lima <qiao-lima@github.com>
2026-04-13 23:42:50 +0800

    Manual Update 1215390

---
+++ 1215390.lua
@@ -0,0 +1,12 @@
+-- 1215390's Lua and Manifest Created by Morrenus
+-- Animal Revolt Battle Simulator
+-- Created: September 28, 2025 at 22:32:26 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1215390, 1, "de010137d3e99ec6ced0635aa0c28feab365034f9a3d433d5d6862be663b55a8") -- Animal Revolt Battle Simulator
+-- MAIN APP DEPOTS
+addappid(1215391, 1, "94d913cb61b5cd8d9f0e75d1b2003b0c25004d0633654e4f2ad8bad4b02c331f") -- Animal Revolt Battle Simulator Content
+setManifestid(1215391, "8520929547852091835", 2546394150)
