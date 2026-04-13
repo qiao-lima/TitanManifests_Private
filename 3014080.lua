c42b7a9fd6db12afbaa0e191d5a7ae84da45bc9d
qiao-lima <qiao-lima@github.com>
2026-04-14 00:25:35 +0800

    Manual Update 3014080

---
+++ 3014080.lua
@@ -0,0 +1,19 @@
+-- 3014080's Lua and Manifest Created by Morrenus
+-- The Hundred Line -Last Defense Academy-
+-- Created: September 30, 2025 at 23:44:21 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(3014080) -- The Hundred Line -Last Defense Academy-
+-- MAIN APP DEPOTS
+addappid(3014081, 1, "9c8b0e729ef1fce35224045a12393cf5a7069e60692bc9b3bf2d64b0bb37b6a3") -- Depot 3014081
+setManifestid(3014081, "7379430715548697808", 32640936114)
+-- DLCS WITH DEDICATED DEPOTS
+-- HUNDRED LINE -- Artbook (AppID: 3319760)
+addappid(3319760)
+addappid(3319760, 1, "fe92de42a7232fcb703844d0f9644a24611561c408aa574d4dba47f3ee44b769") -- HUNDRED LINE -- Artbook - Depot 3319760
+setManifestid(3319760, "2992787271555217105", 1169525883)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3422790) -- HUNDRED LINE -- 
