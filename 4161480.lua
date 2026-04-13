beb584df9c0700177f664b96e6b74c49f3328da3
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:11 +0800

    Manual Update 4161480

---
+++ 4161480.lua
@@ -0,0 +1,13 @@
+-- 4161480's Lua and Manifest Created by Morrenus
+-- 你好！我们还有场恋爱没谈-番外篇
+-- Branch: PUBLIC
+-- Created: January 04, 2026 at 06:03:55 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4161480) -- 你好！我们还有场恋爱没谈-番外篇
+-- MAIN APP DEPOTS
+addappid(4161481, 1, "fba6dbf32706fc4083f18981be1156941884005baae73db59900d20c4510c551") -- Depot 4161481
+setManifestid(4161481, "1042287189217066013", 16400026387)
