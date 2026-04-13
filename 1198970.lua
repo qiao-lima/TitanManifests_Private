420815077246ea318b7205c616aa6716dbae8471
qiao-lima <qiao-lima@github.com>
2026-04-13 23:36:23 +0800

    Manual Update 1198970

---
+++ 1198970.lua
@@ -0,0 +1,18 @@
+-- 1198970's Lua and Manifest Created by Morrenus
+-- I Am Jesus Christ
+-- Created: April 06, 2026 at 19:13:28 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1198970, 1, "e68de2d293c0244022594bddc74a170531e0545b5d47cbb77ce61feb9ebe4b81") -- I Am Jesus Christ
+-- MAIN APP DEPOTS
+addappid(1198971, 1, "c8c266d3056534b49d668eb4339adb88c84e23a451b543accadac4591ce4d7b2") -- Depot 1198971
+setManifestid(1198971, "5531625920902576987", 13103699482)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
