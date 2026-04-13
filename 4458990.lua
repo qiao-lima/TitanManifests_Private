8e9b263916d3aa065507c36213cde3c7b66dbe82
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:06 +0800

    Manual Update 4458990

---
+++ 4458990.lua
@@ -0,0 +1,12 @@
+-- 4458990's Lua and Manifest Created by Morrenus
+-- IF YOU SEE A CAT
+-- Created: April 03, 2026 at 07:17:12 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4458990) -- IF YOU SEE A CAT
+-- MAIN APP DEPOTS
+addappid(4458991, 1, "f60c9c179937d0e5bf3e2106223a4530431851a806584aead4352080e58c4d3e") -- Depot 4458991
+setManifestid(4458991, "963269410572701615", 5002812485)
