ff3a6811d8f7e15a3cc23b29709cde4916b348b6
qiao-lima <qiao-lima@github.com>
2026-04-14 00:02:25 +0800

    Manual Update 218620

---
+++ 218620.lua
@@ -0,0 +1,151 @@
+-- 218620's Lua and Manifest Created by Morrenus
+-- PAYDAY 2
+-- Created: March 05, 2026 at 20:05:33 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 24
+-- Total DLCs: 80
+-- Shared Depots: 6
+
+-- MAIN APPLICATION
+addappid(218620, 1, "b005cdff158b60e65c0c26498b91d145f8e68cfc09c793ba62716d87c1a4369e") -- PAYDAY 2
+-- MAIN APP DEPOTS
+addappid(218621, 1, "2d0de203756909d9b2b65506594648ce773a0428741fcae5208616bd9c79d3ea") -- Base
+setManifestid(218621, "5311288259906065299", 90155018631)
+addappid(218623, 1, "125afa058982488c55e77fc1ed4e1a4f160786f8383db3db5ed5510d760611b6") -- Russian
+setManifestid(218623, "7700458186423454139", 0)
+addappid(218624, 1, "deab327ada094059df67910f69c19cfffeb79aff089df65433f1760ad23bf639") -- Dutch
+setManifestid(218624, "6736605256120450039", 0)
+addappid(218625, 1, "af7ebe5bfa557f52f7d9993481d10385f5eb32adf77936bc8d9a4f66a9d80fdc") -- French
+setManifestid(218625, "3325538660884987003", 0)
+addappid(218626, 1, "90e5e5d05e46661b2c26e7194d6531796a4017bca0f5e5900cd79f01dd45537b") -- German
+setManifestid(218626, "8111024477834407195", 0)
+addappid(218627, 1, "5baf56fb99687983be6e6c22a7be8889468866a4eda552bbe3dc5ffc212081b7") -- Italian
+setManifestid(218627, "5146760485964231618", 0)
+addappid(218628, 1, "bf4eac2b5b55bdc1264f00e3f24193e6c82c01284b02f3af2137ab8915211e86") -- Spanish
+setManifestid(218628, "2507768204192281825", 0)
+addappid(218629, 1, "26cbf329bd73db8f9e7beefe23857b1e0c76b3aef5d3e9e4e6b5b3f31be0ddc5") -- Japanese
+setManifestid(218629, "6307434253818453593", 1352)
+addappid(218631, 1, "1c394d148b286577ab96453164cb03ded6619ccfad3da0f01aac23d71302d1e2") -- Simplified Chinese
+setManifestid(218631, "2916203722723946867", 1352)
+addappid(218630, 1, "80aa89abaa061beb1ccf5cb5f1ab79eb7cf11f79c2767f5477e2ae92675f9e55") -- PAYDAY 2: Career Criminal Content Depot
+setManifestid(218630, "5536271309787494781", 0)
+addappid(218632, 1, "2851e3544784ade4486f7cd973ac593c8b7232e951adb1ab11037c750e05c560") -- Base (Linux)
+setManifestid(218632, "7553784073178780379", 88322044800)
+addappid(974460, 1, "876285ae8e877eddef056b17241e282dc5b3e09c99fa91986425309be9782add") -- PAYDAY 2: Offshore Payday (974460) Depot
+setManifestid(974460, "7712954533225333834", 1038200271)
+-- SHARED DEPOTS (from other apps)
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") -- VC 2008 Redist (Shared from App 228980)
+setManifestid(228982, "6413394087650432851", 9688647)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- VC 2012 Redist (Shared from App 228980)
+setManifestid(228984, "2547553897526095397", 13742505)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- VC 2013 Redist (Shared from App 228980)
+setManifestid(228985, "3966345552745568756", 13699237)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- VC 2015 Redist (Shared from App 228980)
+setManifestid(228986, "8782296191957114623", 29759921)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229031, 1, "98d01f1eaa90ae08f978994e043bcbbf1f23fc72a325528ca7d722c827c0eb54") -- PhysX System Software 9.12.1031 (Shared from App 228980)
+setManifestid(229031, "7746630274301172884", 26729083)
+-- DLCS WITH DEDICATED DEPOTS
+-- PAYDAY 2 The Official Soundtrack (AppID: 254260)
+addappid(254260)
+addappid(254260, 1, "685f9ea20bba151bc1afd38d564fbeb79714127d8860e6e3709b42597942586e") -- PAYDAY 2 The Official Soundtrack - PAYDAY 2 Soundtrack (254260) Depot
+setManifestid(254260, "7885621803433237728", 2405537411)
+-- PAYDAY 2 A Merry Payday Christmas Soundtrack (AppID: 267381)
+addappid(267381)
+addappid(267381, 1, "6eb9e5fd6c12890fb9bc3130a6e968b9f8e369df554af53e073e8dae99f40c7f") -- PAYDAY 2 A Merry Payday Christmas Soundtrack - PAYDAY 2: A Merry Payday Christmas Soundtrack (267381) Depot
+setManifestid(267381, "8983085739249833412", 412155423)
+-- PAYDAY 2 The B-Sides Soundtrack (AppID: 368870)
+addappid(368870)
+addappid(368870, 1, "88e548381947ae497f4412b47a2fb962ef3a6c85ac20bff4e3ebcce1b608e5f8") -- PAYDAY 2 The B-Sides Soundtrack - PAYDAY 2: The B-Sides Soundtrack (368870) Depot
+setManifestid(368870, "7190612300832133696", 305404939)
+-- PAYDAY 2 Tijuana Music Pack (AppID: 2076560)
+addappid(2076560)
+addappid(2076560, 1, "107c80c6f6e8200462fb697af71577ad703e4a640b1f64850e60e7fae6226f9b") -- PAYDAY 2 Tijuana Music Pack - Depot 2076560
+setManifestid(2076560, "5811614798887510953", 525363046)
+-- PAYDAY 2 Chinatown Music Pack (AppID: 2353511)
+addappid(2353511)
+addappid(2418070, 1, "9a46ff4127b070013ea0189940cec49551f33a7e20b2c6b612565be0502a5a78") -- PAYDAY 2 Chinatown Music Pack - Depot 2418070
+setManifestid(2418070, "3463837528970200612", 136009839)
+addappid(2418071, 1, "294731a01d179f2db60818570da445503017dd973ac474291fbc855e20502be3") -- PAYDAY 2 Chinatown Music Pack - Depot 2418071
+setManifestid(2418071, "3577794892998287564", 428387443)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(259380) -- PAYDAY 2 Gage Sniper Pack
+addappid(259381) -- PAYDAY 2 Gage Mod Courier
+addappid(264610) -- PAYDAY 2 Armored Transport
+addappid(267380) -- PAYDAY 2 Gage Weapon Pack 01
+addappid(275590) -- PAYDAY 2 Gage Weapon Pack 02
+addappid(306690) -- PAYDAY 2 The Big Bank Heist
+addappid(311050) -- PAYDAY 2 Gage Shotgun Pack
+addappid(320030) -- PAYDAY 2 Gage Assault Pack
+addappid(323500) -- PAYDAY 2 Hotline Miami
+addappid(331900) -- PAYDAY 2 Gage Historical Pack
+addappid(337660) -- PAYDAY 2 The Diamond Heist
+addappid(337661) -- PAYDAY 2 Clover Character Pack
+addappid(339480) -- PAYDAY 2 The Bomb Heists
+addappid(344140) -- PAYDAY 2 Dragan Character Pack
+addappid(348090) -- PAYDAY 2 The OVERKILL Pack
+addappid(349830) -- PAYDAY 2 The Butchers Western Pack
+addappid(351890) -- PAYDAY 2 The Butchers AKCAR Mod Pack
+addappid(358150) -- PAYDAY 2 The Butchers BBQ Pack
+addappid(366660) -- PAYDAY 2 The Alesso Heist
+addappid(374300) -- PAYDAY 2 The Golden Grin Casino Heist
+addappid(374301) -- PAYDAY 2 Sokol Character Pack
+addappid(384020) -- PAYDAY 2 Yakuza Character Pack
+addappid(384021) -- PAYDAY 2 Gage Ninja Pack
+addappid(401650) -- PAYDAY 2 Gage Chivalry Pack
+addappid(433730) -- PAYDAY 2 The Goat Simulator Heist
+addappid(441600) -- PAYDAY 2 The Wolf Pack
+addappid(450660) -- PAYDAY 2 Biker Character Pack
+addappid(468410) -- PAYDAY 2 Sydney Character Pack
+addappid(487210) -- PAYDAY 2 The Biker Heist
+addappid(548420) -- PAYDAY 2 Gage Spec Ops Pack
+addappid(548421) -- PAYDAY 2 Scarface Heist
+addappid(612900) -- PAYDAY 2 Gage Russian Weapon Pack
+addappid(758420) -- PAYDAY 2 h3h3 Character Pack
+addappid(826090) -- PAYDAY 2 VR
+addappid(1184410) -- PAYDAY 2 Tailor Pack 1
+addappid(1184411) -- PAYDAY 2 Border Crossing Heist
+addappid(1184412) -- PAYDAY 2 Cartel Optics Mod Pack
+addappid(1252200) -- PAYDAY 2 San Martn Bank Heist
+addappid(1255150) -- PAYDAY 2 Tailor Pack 2
+addappid(1255151) -- PAYDAY 2 Federales Weapon Pack
+addappid(1255152) -- PAYDAY 2 Weapon Color Pack 1
+addappid(1347750) -- PAYDAY 2 Breakfast in Tijuana Heist
+addappid(1347751) -- PAYDAY 2 Weapon Color Pack 2
+addappid(1351060) -- PAYDAY 2 Fugitive Weapon Pack
+addappid(1449440) -- PAYDAY 2 Tailor Pack 3
+addappid(1449441) -- PAYDAY 2 Gunslinger Weapon Pack
+addappid(1449442) -- PAYDAY 2 Weapon Color Pack 3
+addappid(1449450) -- PAYDAY 2 Bulucs Mansion Heist
+addappid(1555040) -- PAYDAY 2 Dragon Pack
+addappid(1555050) -- PAYDAY 2 Jiu Feng Smuggler Pack
+addappid(1619070) -- PAYDAY 2 Jiu Feng Smuggler Pack 2
+addappid(1654480) -- PAYDAY 2 The Ukrainian Prisoner Heist
+addappid(1654481) -- PAYDAY 2 Guardians Tailor Pack
+addappid(1746580) -- PAYDAY 2 Jiu Feng Smuggler Pack 3
+addappid(1778790) -- PAYDAY 2 Black Cat Heist
+addappid(1778791) -- PAYDAY 2 Mega City Tailor Pack
+addappid(1824880) -- PAYDAY 2 Winter Ghosts Tailor Pack
+addappid(1901420) -- PAYDAY 2 Jiu Feng Smuggler Pack 4
+addappid(1906240) -- PAYDAY 2 Mountain Master Heist
+addappid(1907080) -- PAYDAY 2 Golden Dagger Tailor Pack
+addappid(1945680) -- PAYDAY 2 Southbound Tailor Pack
+addappid(1945681) -- PAYDAY 2 Midland Ranch Heist
+addappid(1945682) -- PAYDAY 2 McShay Weapon Pack
+addappid(2074220) -- PAYDAY 2 High Octane Tailor Pack
+addappid(2074230) -- PAYDAY 2 McShay Weapon Pack 2
+addappid(2074240) -- PAYDAY 2 Lost in Transit Heist
+addappid(2200970) -- PAYDAY 2 McShay Weapon Pack 3
+addappid(2200971) -- PAYDAY 2 Street Smart Tailor Pack
+addappid(2215010) -- PAYDAY 2 Hostile Takeover Heist
+addappid(2218650) -- PAYDAY 2 McShay Mod Pack
+addappid(2352300) -- PAYDAY 2 Lawless Tailor Pack
+addappid(2353510) -- PAYDAY 2 McShay Weapon Pack 4
+addappid(2353512) -- PAYDAY 2 Crude Awakening Heist
+addappid(3188980) -- PAYDAY 2 Jacket Character Pack
+addappid(3847540) -- PAYDAY 2 Subscription
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(218622) -- Depot 218622 (empty depot)
+-- addappid(218633) -- Depot 218633 (empty depot)
+-- addappid(218634) -- Depot 218634 (empty depot)
