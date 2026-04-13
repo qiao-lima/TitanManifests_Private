3a8884a43f83ef4f11e140c02c0160cdb915ae9d
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:36 +0800

    Manual Update 3902520

---
+++ 3902520.lua
@@ -0,0 +1,18 @@
+-- 3902520's Lua and Manifest Created by Morrenus
+-- Ukrainian Warfare: Gostomel Heroes
+-- Created: March 31, 2026 at 07:36:15 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3902520) -- Ukrainian Warfare: Gostomel Heroes
+-- MAIN APP DEPOTS
+addappid(3902521, 1, "10d9ea888c6a80c9ce8b1602c94e3a11cc028045141b9c1c5a10db8f2c93bcf4") -- Depot 3902521
+setManifestid(3902521, "5222601489114406511", 20626993553)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") -- .NET 4.7 Redist (Shared from App 228980)
+setManifestid(229006, "1784011429307107530", 83944258)
