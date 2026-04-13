e89d01eab2eabb4f38e29c3256e3324cae6e7b5d
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:45 +0800

    Manual Update 4328210

---
+++ 4328210.lua
@@ -0,0 +1,12 @@
+-- 4328210's Lua and Manifest Created by Morrenus
+-- Slime City: Trials
+-- Created: April 04, 2026 at 14:22:50 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4328210) -- Slime City: Trials
+-- MAIN APP DEPOTS
+addappid(4328211, 1, "26dd671f52904f9362aafdceb1b77496818c2838a9afdfcd314b320876d3e79f") -- Depot 4328211
+setManifestid(4328211, "3103582594208445453", 905849350)
