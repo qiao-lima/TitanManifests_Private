3e56f8aaa17c5d74375be375fea24d0c2b492619
qiao-lima <qiao-lima@github.com>
2026-04-14 00:02:19 +0800

    Manual Update 2091150

---
+++ 2091150.lua
@@ -0,0 +1,16 @@
+-- 2091150's Lua and Manifest Created by Morrenus
+-- Apocalypse Factory
+-- Created: April 02, 2026 at 20:16:27 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2091150, 1, "4d46925cf564cee90a15fda2085a5a01f447906ff6378bf456ad04257daedde1") -- Apocalypse Factory
+-- MAIN APP DEPOTS
+addappid(2091151, 1, "612341a516f5fcab9366ec18800240d518601f161c4673a56943059d04658686") -- Depot 2091151
+setManifestid(2091151, "6825731298436159457", 1228777806)
+addappid(2091152, 1, "30f4a79a01bb896f7590572c0c67aac9b579cef409160beb5a0764f5291f19e1") -- Depot 2091152
+setManifestid(2091152, "5292523025060139118", 1425358946)
+addappid(2091153, 1, "a78cfa8c372ed7dfa1151c8d173d474ef525c22750ac2e7ae79f433d0541f1d0") -- Depot 2091153
+setManifestid(2091153, "8924974472149647250", 1356580949)
