f5e2f955370872288715a2439ec1bcbabec69903
qiao-lima <qiao-lima@github.com>
2026-04-13 23:48:21 +0800

    Manual Update 1585730

---
+++ 1585730.lua
@@ -0,0 +1,20 @@
+-- 1585730's Lua and Manifest Created by Morrenus
+-- Backbeat
+-- Created: March 31, 2026 at 20:14:20 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1585730, 1, "787a6454dd67c5b8f6884a9b507a3a188f8ed7bd0e98e96d989bd8e5331b8d91") -- Backbeat
+-- MAIN APP DEPOTS
+addappid(1585731, 1, "ae9c9162a78de45c6ad57d40515b532ba44d197366d5df6fb6bf95f6095f4fda") -- Depot 1585731
+setManifestid(1585731, "1734852111853557327", 1430184586)
+addappid(1585732, 1, "ba6f22a5190ed1062e32fdea6987df254f67010a59a9680540c3a47dd69068eb") -- Depot 1585732
+setManifestid(1585732, "2207852918336416786", 1526073425)
+addappid(1585733, 1, "6af4b82b12942cf8dbc593cc901c17602799387be89384416b18443488d156a5") -- Depot 1585733
+setManifestid(1585733, "936839846284935945", 1441829329)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
