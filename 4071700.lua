696d641f815cdcacf24d73a20ae3fc0a373cdd68
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:50 +0800

    Manual Update 4071700

---
+++ 4071700.lua
@@ -0,0 +1,18 @@
+-- 4071700's Lua and Manifest Created by Morrenus
+-- Conductor: Eternal Service
+-- Created: March 22, 2026 at 21:16:10 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4071700) -- Conductor: Eternal Service
+-- MAIN APP DEPOTS
+addappid(4071701, 1, "fb9e751faf522d1f8e24148790094c1ac7a4611e86c121e19be07e73ffd02b4f") -- Depot 4071701
+setManifestid(4071701, "2851667613268002582", 7293824413)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
