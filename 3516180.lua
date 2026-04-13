f011ac50f5be1f8e1a0ccdd85ec3d18cdc32213a
qiao-lima <qiao-lima@github.com>
2026-04-14 00:40:32 +0800

    Manual Update 3516180

---
+++ 3516180.lua
@@ -0,0 +1,12 @@
+-- 3516180's Lua and Manifest Created by Morrenus
+-- Don't Fool Me, Beauties!
+-- Created: October 01, 2025 at 18:12:03 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3516180) -- Don't Fool Me, Beauties!
+-- MAIN APP DEPOTS
+addappid(3516181, 1, "dc747063c492b3c7c598538d95e4a8f61cb724864be1a6eb46e07103bce91942") -- Depot 3516181
+setManifestid(3516181, "4445228082601084974", 34869086558)
