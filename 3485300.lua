c9584576999033e3eddc47356997cc96d6e192f6
qiao-lima <qiao-lima@github.com>
2026-04-14 00:40:30 +0800

    Manual Update 3485300

---
+++ 3485300.lua
@@ -0,0 +1,20 @@
+-- 3485300's Lua and Manifest Created by Morrenus
+-- A Little Perspective
+-- Created: April 04, 2026 at 13:48:30 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3485300) -- A Little Perspective
+-- MAIN APP DEPOTS
+addappid(3485302, 1, "67fb28d73b80edce0f21c5e7a4080d602c319d04da5030993aab2269c750ea7c") -- Depot 3485302
+setManifestid(3485302, "9139030598193473214", 104892117)
+addappid(3485303, 1, "523a2c98705cf22705d7852f4f389b9ec9ac48f31daae2a887af41735dbc481a") -- Depot 3485303
+setManifestid(3485303, "2834598886658804041", 104137243)
+addappid(3485304, 1, "d6dd70c10c57b4d1763a6a6ee0f4bdb9783a589771056fd580cfc93ea1a48ca0") -- Depot 3485304
+setManifestid(3485304, "3415296554421776637", 109129522)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
