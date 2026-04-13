ddbcfb6f179a09108b3e196bd0d75a4e38da591c
qiao-lima <qiao-lima@github.com>
2026-04-14 00:21:46 +0800

    Manual Update 2868840

---
+++ 2868840.lua
@@ -0,0 +1,16 @@
+-- 2868840's Lua and Manifest Created by Morrenus
+-- Slay the Spire 2
+-- Created: March 10, 2026 at 14:43:04 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2868840, 1, "efe405b7cd4ee75b6961f28ad8ba9aa57a9e57057857dbd921e56ad3b347cd3b") -- Slay the Spire 2
+-- MAIN APP DEPOTS
+addappid(2868843, 1, "6e4518ed758f09a3fb1c07ed1ef14a8ccfb4bac6e1fc452adc1e1d8a1c024e42") -- Depot 2868843
+setManifestid(2868843, "5804850638368848497", 1836445558)
+addappid(2868842, 1, "7036cfd5ff85a70ca002aeddf573311c3906b0f7d8d83d099592e759ddca5b31") -- Depot 2868842
+setManifestid(2868842, "3447588968304758517", 2055281854)
+addappid(2868841, 1, "ed043fd4ac0899cd0b5fc59d5407115ff4f87793b162ba16fe49c7e2893938ab") -- Depot 2868841
+setManifestid(2868841, "1025979627422358251", 2649392961)
