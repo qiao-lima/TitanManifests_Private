a92ab84587cd98352286a65e5c9e2a75be8e11c0
qiao-lima <qiao-lima@github.com>
2026-04-14 00:36:51 +0800

    Manual Update 3436170

---
+++ 3436170.lua
@@ -0,0 +1,12 @@
+-- 3436170's Lua and Manifest Created by Morrenus
+-- Let's Start Managing the Sexual Relief Department!
+-- Created: March 10, 2026 at 09:24:38 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3436170) -- Let's Start Managing the Sexual Relief Department!
+-- MAIN APP DEPOTS
+addappid(3436171, 1, "7166758ca45cb83fbc3548258be92b977e635ef7257bc82abd4dcf5e81e76d2d") -- Depot 3436171
+setManifestid(3436171, "7904551095181542256", 1273804687)
