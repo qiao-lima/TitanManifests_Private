2fe0d3bf073c7a1495ac560c69bfc05077ce354c
qiao-lima <qiao-lima@github.com>
2026-04-14 00:36:53 +0800

    Manual Update 3448430

---
+++ 3448430.lua
@@ -0,0 +1,18 @@
+-- 3448430's Lua and Manifest Created by Morrenus
+-- Origament: A Paper Adventure
+-- Created: April 07, 2026 at 20:25:21 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3448430, 1, "210c680d8c73bfd5e4326161f52b5b9a10fb1cd078aabdb15d372577d9baf85b") -- Origament: A Paper Adventure
+-- MAIN APP DEPOTS
+addappid(3448431, 1, "7fde477b747ebc27b2c9369a19f170560850302c582181d77cbd7f3dfe35caf9") -- Depot 3448431
+setManifestid(3448431, "5309000238637201738", 10387241351)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
