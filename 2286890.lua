320c42294b13f307ed00c6b5a84901c7888e8ecc
qiao-lima <qiao-lima@github.com>
2026-04-14 00:06:17 +0800

    Manual Update 2286890

---
+++ 2286890.lua
@@ -0,0 +1,14 @@
+-- 2286890's Lua and Manifest Created by Morrenus
+-- 面仙
+-- Created: March 20, 2026 at 19:21:19 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2286890) -- 面仙
+-- MAIN APP DEPOTS
+addappid(2286891, 1, "f0afd99da6abae9260ac037559fd2122f56501ec7138848acbe86c083c6ffd20") -- Depot 2286891
+setManifestid(2286891, "1689223592492427040", 8510467257)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2286892) -- Depot 2286892 (empty depot)
