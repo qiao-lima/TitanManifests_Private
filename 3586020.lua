cce271a4438405dc397850b14f85d2364beaa94b
qiao-lima <qiao-lima@github.com>
2026-04-14 00:41:11 +0800

    Manual Update 3586020

---
+++ 3586020.lua
@@ -0,0 +1,12 @@
+-- 3586020's Lua and Manifest Created by Morrenus
+-- 浮岛时光
+-- Created: March 31, 2026 at 20:18:30 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3586020) -- 浮岛时光
+-- MAIN APP DEPOTS
+addappid(3586021, 1, "fb3bd319d37efb5146aad4dab4f955f691f19b892e553fafe60c56be137e8a62") -- Depot 3586021
+setManifestid(3586021, "3458218769929213388", 172366576)
