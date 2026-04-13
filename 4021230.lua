c59c2cc22de11a16cc70931cba778046b2d3e0cb
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:24 +0800

    Manual Update 4021230

---
+++ 4021230.lua
@@ -0,0 +1,18 @@
+-- 4021230's Lua and Manifest Created by Morrenus
+-- 动物职场
+-- Created: February 13, 2026 at 23:39:57 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4021230) -- 动物职场
+-- MAIN APP DEPOTS
+addappid(4021231, 1, "74ca1527ce35dca92b6e84fc6f1d5da6f31ed024bcee09fafde37a4b404f544f") -- Depot 4021231
+setManifestid(4021231, "8455130271804857487", 427173081)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
