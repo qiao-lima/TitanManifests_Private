3c13703bf86ba2fe84a508842ebdcff85d49f515
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:23 +0800

    Manual Update 4298400

---
+++ 4298400.lua
@@ -0,0 +1,12 @@
+-- 4298400's Lua and Manifest Created by Morrenus
+-- Dystopian Pet Simulator
+-- Created: April 10, 2026 at 06:33:52 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4298400) -- Dystopian Pet Simulator
+-- MAIN APP DEPOTS
+addappid(4298401, 1, "feac01711e177f15591e0ce65ced04f549f236a96a78e2b3f1d64bb6ff33abec") -- Depot 4298401
+setManifestid(4298401, "5945978481145119320", 1660838544)
