1683a1fa833d00723e881c6339a336b5709ee618
qiao-lima <qiao-lima@github.com>
2026-04-14 00:02:18 +0800

    Manual Update 2070270

---
+++ 2070270.lua
@@ -0,0 +1,20 @@
+-- 2070270's Lua and Manifest Created by Morrenus
+-- Cloudheim
+-- Created: March 05, 2026 at 15:09:46 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2070270) -- Cloudheim
+-- MAIN APP DEPOTS
+addappid(2070272, 1, "edb03c349c8b8875e63425d9789b68b246de360a06f350e8b230357032ebab9b") -- Depot 2070272
+setManifestid(2070272, "6436010937947781187", 17507524527)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4164290) -- Cloudheim - Supporter Pack
