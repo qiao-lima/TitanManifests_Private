43b5e8013aac1b60890e67d483965ddbea7684bb
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:05 +0800

    Manual Update 3780390

---
+++ 3780390.lua
@@ -0,0 +1,12 @@
+-- 3780390's Lua and Manifest Created by Morrenus
+-- Solo Knight
+-- Created: April 02, 2026 at 23:17:32 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3780390) -- Solo Knight
+-- MAIN APP DEPOTS
+addappid(3780391, 1, "09ac056a1a53df0e7c484718e70be4d6ffcbf5aa11cb501ea510a0bd80a2ef26") -- Depot 3780391
+setManifestid(3780391, "9041236657183092620", 3993731182)
