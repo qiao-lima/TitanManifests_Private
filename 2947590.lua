bb8e3c6c8bc4e39efb9995cc6b094f9a31618021
qiao-lima <qiao-lima@github.com>
2026-04-14 00:21:51 +0800

    Manual Update 2947590

---
+++ 2947590.lua
@@ -0,0 +1,16 @@
+-- 2947590's Lua and Manifest Created by Morrenus
+-- Deep Desktop
+-- Created: January 18, 2026 at 11:12:10 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2947590) -- Deep Desktop
+-- MAIN APP DEPOTS
+addappid(2947591, 1, "4cc8708a9b2e69c89afe0002913a8057aab15f719417a4e9b48b423eb77cb873") -- Depot 2947591
+setManifestid(2947591, "3197776524735972473", 379300643)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
