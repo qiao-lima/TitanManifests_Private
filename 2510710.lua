22ad9f6cf2ca86de533a8e1c58c7fd735fa14d8a
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:09 +0800

    Manual Update 2510710

---
+++ 2510710.lua
@@ -0,0 +1,31 @@
+-- 2510710's Lua and Manifest Created by Morrenus
+-- Kunitsu-Gami: Path of the Goddess
+-- Created: September 29, 2025 at 21:15:26 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 6
+-- Total DLCs: 3
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2510710) -- Kunitsu-Gami: Path of the Goddess
+-- MAIN APP DEPOTS
+addappid(2510711, 1, "0442261b453491b8ff8a733d5e971bcff7a9ac403c434f7ff65852c56a63701a") -- Depot 2510711
+setManifestid(2510711, "7638697768564054606", 23708821973)
+addappid(2510712, 1, "dff8a57d0e0c043f126fab74bfc97bf9b19a598fee37b6e831cfd76c607aba74") -- Depot 2510712
+setManifestid(2510712, "6647577562051882113", 370039725)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+-- DLCS WITH DEDICATED DEPOTS
+-- Kunitsu-Gami Path of the Goddess - Mazo Talisman Walking Basket (AppID: 2831770)
+addappid(2831770)
+addappid(2831770, 1, "6eecfc662590d9a66247a70614b99864499bb2656c5b7ca7d17443b5d86aa06c") -- Kunitsu-Gami Path of the Goddess - Mazo Talisman Walking Basket - Depot 2831770
+setManifestid(2831770, "7818367273895087280", 2331179)
+-- Kunitsu-Gami Path of the Goddess - Mazo Talisman Astral Projection (AppID: 2831780)
+addappid(2831780)
+addappid(2831780, 1, "a9497030c311c9e6d7fac9080132b08471d895e55938fa5c60eaf06682bf4b17") -- Kunitsu-Gami Path of the Goddess - Mazo Talisman Astral Projection - Depot 2831780
+setManifestid(2831780, "8822019250960722529", 8633441)
+-- Kunitsu-Gami Path of the Goddess - Artbook of Kunitsu-Gami (AppID: 2831860)
+addappid(2831860)
+addappid(2831860, 1, "67d8cc7a1a5c2bd4d55006bc14817f180545b0d4af84b52a09cbdbd8b4eddaee") -- Kunitsu-Gami Path of the Goddess - Artbook of Kunitsu-Gami - Depot 2831860
+setManifestid(2831860, "7045928971036468308", 96708950)
