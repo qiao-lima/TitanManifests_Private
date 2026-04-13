7a4c635a81fe4120074dbae821aa7a8dd01c1f1d
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:10 +0800

    Manual Update 2523890

---
+++ 2523890.lua
@@ -0,0 +1,25 @@
+-- 2523890's Lua and Manifest Created by Morrenus
+-- Whispers of the Eyeless
+-- Created: March 20, 2026 at 17:05:38 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 1
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2523890) -- Whispers of the Eyeless
+-- MAIN APP DEPOTS
+addappid(2523891, 1, "83437f3f5e700f8dd152ae2dfc17434c2db28d04783993d736d5e4baed4a33e4") -- Depot 2523891
+setManifestid(2523891, "8980303241368054133", 3676679866)
+addappid(2523892, 1, "e8032ef7b46d7da80207121cd1ab1f116afc4cff5d90d5ff7a2e19703785a0b4") -- Depot 2523892
+setManifestid(2523892, "6047734737195072133", 2723220608)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITH DEDICATED DEPOTS
+-- Whispers of the Eyeless Artbook (AppID: 4332100)
+addappid(4332100)
+addappid(4332100, 1, "13c645c8b04ff5876192a3161ae965b432b5f7cb119ff04702d7821aefbc2453") -- Whispers of the Eyeless Artbook - Depot 4332100
+setManifestid(4332100, "639469819766487571", 191241700)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2523893) -- Depot 2523893 (empty depot)
