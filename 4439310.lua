fb5ae89f34a0731def0718c6b4f73453b6abcd8b
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:05 +0800

    Manual Update 4439310

---
+++ 4439310.lua
@@ -0,0 +1,16 @@
+-- 4439310's Lua and Manifest Created by Morrenus
+-- Throbbin Hood 🏹🔞
+-- Created: March 28, 2026 at 22:32:46 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(4439310) -- Throbbin Hood 🏹🔞
+-- MAIN APP DEPOTS
+addappid(4439311, 1, "5edc7f1e4d1ebf7099a9cb697cf40a375d443771aeb3e4013f7d6cd2b00189e7") -- Depot 4439311
+setManifestid(4439311, "6460379166255050718", 2010754415)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Throbbin Hood  Wallpapers (AppID: 4533110) - missing depot keys
+-- addappid(4533110)
