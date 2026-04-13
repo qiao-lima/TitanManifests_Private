11b0e7f935a4c555ef27b55936cdd3e81f1d34c7
qiao-lima <qiao-lima@github.com>
2026-04-14 00:40:35 +0800

    Manual Update 3545990

---
+++ 3545990.lua
@@ -0,0 +1,19 @@
+-- 3545990's Lua and Manifest Created by Morrenus
+-- 美女，请别影响我成仙
+-- Created: March 05, 2026 at 17:03:10 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3545990, 1, "1fd1a551af5be4a8ce26c43c69426b448e26de7894352604b294f726459963d5") -- 美女，请别影响我成仙
+-- MAIN APP DEPOTS
+addappid(3545991, 1, "f66742d81c9b07984f28fee2a81e687feb9fa85c380fb7e3f03e311701d4125c") -- Depot 3545991
+setManifestid(3545991, "7416224555041202245", 27002431527)
+-- DLCS WITH DEDICATED DEPOTS
+-- -2K (AppID: 3701000)
+addappid(3701000)
+addappid(3701000, 1, "fcd04125bc328fc24cc68ec1ae1bbe71f8caabd9217e36e78bef79b93e7f785b") -- -2K - Depot 3701000
+setManifestid(3701000, "1166433337993615636", 79536382240)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3545992) -- Depot 3545992 (empty depot)
