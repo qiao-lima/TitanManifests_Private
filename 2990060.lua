01017135d170ae7ae279d494e41fa1d744edea03
qiao-lima <qiao-lima@github.com>
2026-04-14 00:25:32 +0800

    Manual Update 2990060

---
+++ 2990060.lua
@@ -0,0 +1,12 @@
+-- 2990060's Lua and Manifest Created by Morrenus
+-- Moto Rush Reborn
+-- Created: March 24, 2026 at 08:11:34 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2990060, 1, "7a31d2b7b877a0968ade6b802398d30f242ab3a9cbe715662aa988abb9fac4c8") -- Moto Rush Reborn
+-- MAIN APP DEPOTS
+addappid(2990061, 1, "b4cf5ebe0ca0622ef99f5d5d2ce73deecc03c3f3d8be7d7fac89e200e4d0fd6d") -- Depot 2990061
+setManifestid(2990061, "1242265601612409910", 16816302421)
