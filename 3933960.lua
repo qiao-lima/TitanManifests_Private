7c8f268c51127a032961541d447b1c4ff23b8167
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:01 +0800

    Manual Update 3933960

---
+++ 3933960.lua
@@ -0,0 +1,12 @@
+-- 3933960's Lua and Manifest Created by Morrenus
+-- How Many Secrets Under Ceiling
+-- Created: April 02, 2026 at 03:42:45 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3933960) -- How Many Secrets Under Ceiling
+-- MAIN APP DEPOTS
+addappid(3933961, 1, "4692bb1ddde7e1db0cad446d6a7f4f888d059b1010b8a034723966af71bee303") -- Depot 3933961
+setManifestid(3933961, "2662770903471464331", 649253647)
