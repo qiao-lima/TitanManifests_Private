7c19e449ce0d18685af33676fa6dada1a024284b
qiao-lima <qiao-lima@github.com>
2026-04-14 00:30:04 +0800

    Manual Update 3202030

---
+++ 3202030.lua
@@ -0,0 +1,22 @@
+-- 3202030's Lua and Manifest Created by Morrenus
+-- 龙胤立志传
+-- Created: April 02, 2026 at 06:17:19 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3202030) -- 龙胤立志传
+-- MAIN APP DEPOTS
+addappid(3202031, 1, "7f64c950b5a6bd56e9f3fb9272a80f75a04f0a36b99f540e529f467d1f3584da") -- Depot 3202031
+setManifestid(3202031, "6880466413852473653", 3188563813)
+addappid(3202032, 1, "5e3ef45e8310fefde2bcbe47277fae96fd5a6301115acc02db78f0570273588e") -- Depot 3202032
+setManifestid(3202032, "6335382307680204537", 3188563813)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4383960) --  - 
