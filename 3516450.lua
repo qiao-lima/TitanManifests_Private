d8441099576b5299a6b7eacf2beab5dc488fd58f
qiao-lima <qiao-lima@github.com>
2026-04-14 00:40:32 +0800

    Manual Update 3516450

---
+++ 3516450.lua
@@ -0,0 +1,12 @@
+-- 3516450's Lua and Manifest Created by Morrenus
+-- 回家吧喵
+-- Created: March 31, 2026 at 20:17:54 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3516450) -- 回家吧喵
+-- MAIN APP DEPOTS
+addappid(3516451, 1, "6f14f500317bebf70909d085b97f44c15c9a609669b442109c400f051a1499d2") -- Depot 3516451
+setManifestid(3516451, "2548072843696515834", 533462343)
