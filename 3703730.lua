04c8702466b4e51e90dca81aedd43a11d3a039b2
qiao-lima <qiao-lima@github.com>
2026-04-13 19:32:53 +0800

    Manual Update 3703730

---
+++ 3703730.lua
@@ -0,0 +1,16 @@
+-- 3703730's Lua and Manifest Created by Morrenus
+-- Tezzel: The Tilemaker's Tale
+-- Created: April 11, 2026 at 19:59:40 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3703730) -- Tezzel: The Tilemaker's Tale
+-- MAIN APP DEPOTS
+addappid(3703731, 1, "4bf96cfd7fb9698a8f6db407f237b785f2f4000b813136e0ac1d22286be452d7") -- Depot 3703731
+setManifestid(3703731, "7874345944411424401", 390260277)
+addappid(3703732, 1, "e3cd21a5a5b4f2ef6894dd6a252245db35962c37d9da4ae6fb882022eac97324") -- Depot 3703732
+setManifestid(3703732, "5930526153957672231", 414323112)
+addappid(3703733, 1, "07ff57dcac99cc1b0d5736248d106fa9fe8551c7479d89a77b532ab8d1f9fd6b") -- Depot 3703733
+setManifestid(3703733, "69640129361834017", 388445846)
