9dc79b1be7e1df646504c24198d4d952b9654d4d
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:36 +0800

    Manual Update 4186860

---
+++ 4186860.lua
@@ -0,0 +1,14 @@
+-- 4186860's Lua and Manifest Created by Morrenus
+-- 天使过家家
+-- Created: March 31, 2026 at 20:25:35 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4186860) -- 天使过家家
+-- MAIN APP DEPOTS
+addappid(4186861, 1, "911797ab6097af9e66a579aaa07de8c616ad5fa18b906ac39df4afd9c86dde3d") -- Depot 4186861
+setManifestid(4186861, "7852772122781119763", 3784027770)
+addappid(4186862, 1, "ce88bec72850ff0b91b3d5663eef3f67c20c4a7cc02a3dcfc9b59cfb816c6c32") -- Depot 4186862
+setManifestid(4186862, "2708023649607398070", 3765990255)
