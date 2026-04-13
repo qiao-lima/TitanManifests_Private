75804ba8308c76e8da3466605229d0971235ec3b
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:09 +0800

    Manual Update 3816930

---
+++ 3816930.lua
@@ -0,0 +1,25 @@
+-- 3816930's Lua and Manifest Created by Morrenus
+-- Grind Survivors
+-- Created: March 16, 2026 at 08:55:42 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 2
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3816930) -- Grind Survivors
+-- MAIN APP DEPOTS
+addappid(3816931, 1, "4c48190b96fa1496187f24f2b468e425e8a6532f2b455e623c42f4d1575a48a9") -- Depot 3816931
+setManifestid(3816931, "5029552574874771550", 1834167276)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Grind Survivors Supporter Pack (AppID: 4332160)
+addappid(4332160)
+addappid(4332161, 1, "59168ce0fd1bc22c7c2c683a5140cfcf4d0bc2f0700c8353fe4849e9f9404019") -- Grind Survivors Supporter Pack - Depot 4332161
+setManifestid(4332161, "6425515966201637128", 17236586)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4332170) -- Grind Survivors Ukraine Supporter Pack
