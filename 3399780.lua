7498b39812795710adea6c02fc33df3588495751
qiao-lima <qiao-lima@github.com>
2026-04-14 00:36:49 +0800

    Manual Update 3399780

---
+++ 3399780.lua
@@ -0,0 +1,12 @@
+-- 3399780's Lua and Manifest Created by Morrenus
+-- 镇邪Ⅱ
+-- Created: March 14, 2026 at 22:23:01 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3399780) -- 镇邪Ⅱ
+-- MAIN APP DEPOTS
+addappid(3399781, 1, "392f688f96a0afc206c6e69bbe203680b5daf7ffa3b6b18ed94fa517aaf9ca0e") -- Depot 3399781
+setManifestid(3399781, "6017915915676371953", 20326866214)
