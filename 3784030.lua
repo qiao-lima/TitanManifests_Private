f235d1dc0170331b44636a7642f5e6f4d6189878
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:05 +0800

    Manual Update 3784030

---
+++ 3784030.lua
@@ -0,0 +1,12 @@
+-- 3784030's Lua and Manifest Created by Morrenus
+-- RACCOIN: Coin Pusher Roguelike
+-- Created: March 31, 2026 at 05:59:55 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3784030) -- RACCOIN: Coin Pusher Roguelike
+-- MAIN APP DEPOTS
+addappid(3784031, 1, "b7bfd29859fb8492a8b2cd4c476184e3ade362bbc50eaec4a7be6b45a02e0d4d") -- Depot 3784031
+setManifestid(3784031, "6921530982378794026", 541268693)
