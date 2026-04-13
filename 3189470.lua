f03cb9992e85b6364e959a2daecf179eae2f4b6e
qiao-lima <qiao-lima@github.com>
2026-04-14 00:27:13 +0800

    Manual Update 3189470

---
+++ 3189470.lua
@@ -0,0 +1,12 @@
+-- 3189470's Lua and Manifest Created by Morrenus
+-- OG Streamer Beats
+-- Created: February 23, 2026 at 04:48:54 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3189470) -- OG Streamer Beats
+-- MAIN APP DEPOTS
+addappid(3189471, 1, "db3ec4fef72559370be5aeee757b92ac5c34fad5e9cf874c78211b6c21c580e7") -- Depot 3189471
+setManifestid(3189471, "4820939733309983265", 2254554297)
