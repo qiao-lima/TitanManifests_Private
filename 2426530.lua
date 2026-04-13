3a8130a78f8544cdcb33ce8a3a2835b29068455a
qiao-lima <qiao-lima@github.com>
2026-04-14 00:07:52 +0800

    Manual Update 2426530

---
+++ 2426530.lua
@@ -0,0 +1,18 @@
+-- 2426530's Lua and Manifest Created by Morrenus
+-- Nova Roma
+-- Created: March 27, 2026 at 02:51:47 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2426530, 1, "c716304b91291bd4069b1528c278220a06931ec2d6456708fd05e4ec1469f996") -- Nova Roma
+-- MAIN APP DEPOTS
+addappid(2426533, 1, "11e7b17e52efc229e337671d604d6cda7f84bd02ab665901a7be41d3ec929aba") -- Depot 2426533
+setManifestid(2426533, "3440326936187084420", 945259450)
+addappid(2426534, 1, "19fe1ac6be7f66c75ba7337adfcaf9202925f0609b396ff0d71c82f53fba3f38") -- Depot 2426534
+setManifestid(2426534, "8671447350305718012", 1013420974)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
