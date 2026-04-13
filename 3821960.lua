d45fdaa47ae7cd671b22660d1d5ab2c075bd59b5
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:09 +0800

    Manual Update 3821960

---
+++ 3821960.lua
@@ -0,0 +1,12 @@
+-- 3821960's Lua and Manifest Created by Morrenus
+-- RebreatheR
+-- Created: March 31, 2026 at 20:21:39 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3821960) -- RebreatheR
+-- MAIN APP DEPOTS
+addappid(3821962, 1, "5dcbe188d8a25e77732861ea6d33d0b3a87164e03e710878fe7dee218a46a7ff") -- Depot 3821962
+setManifestid(3821962, "1471703565385555332", 5697456238)
