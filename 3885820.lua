7b1ad87f522be8d58d399d987ee7ad583c7205af
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:35 +0800

    Manual Update 3885820

---
+++ 3885820.lua
@@ -0,0 +1,17 @@
+-- 3885820's Lua and Manifest Created by Morrenus
+-- The Spirit Weaver
+-- Created: April 10, 2026 at 21:52:49 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3885820) -- The Spirit Weaver
+-- MAIN APP DEPOTS
+addappid(3885821, 1, "63132ec69a2f1e65ae23b0c8b7e9d8923a556c8bed0713d31ec609204f7c177e") -- Depot 3885821
+setManifestid(3885821, "5361692041161410203", 1676371310)
+-- DLCS WITH DEDICATED DEPOTS
+-- The Spirit Weaver - The Artbook (AppID: 4310530)
+addappid(4310530)
+addappid(4310531, 1, "2542e63882def3c97f0380c7841306cd7fa78a799a7d72d7a2d79ee92ecef623") -- The Spirit Weaver - The Artbook - Depot 4310531
+setManifestid(4310531, "7943772823829639824", 57565120)
