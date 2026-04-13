a697903afcf6114eab72e0f88b9889d153ddedfe
qiao-lima <qiao-lima@github.com>
2026-04-14 00:02:19 +0800

    Manual Update 2073850

---
+++ 2073850.lua
@@ -0,0 +1,75 @@
+-- 2073850's Lua and Manifest Created by Morrenus
+-- THE FINALS
+-- Created: March 26, 2026 at 16:48:15 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 56
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2073850, 1, "476468a3f5dd1f8f6ac2355d3622c96086c5927eb59ba761a02801591a2adb8f") -- THE FINALS
+-- MAIN APP DEPOTS
+addappid(2073851, 1, "0ac69706a0ad749c7111eeb755df5595603cd8ba035b220276b7f32bca7a9e99") -- Depot 2073851
+setManifestid(2073851, "3934894715887886861", 83954842538)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2897810) -- THE FINALS - Bank Rabbit Set
+addappid(3025990) -- THE FINALS - Season 3 Starter Pack
+addappid(3080260) -- THE FINALS - Metro Drifter Set
+addappid(3202140) -- THE FINALS - Season 4 Starter Pack
+addappid(3211950) -- THE FINALS - rf Tactical Pack
+addappid(3225070) -- THE FINALS - Hedge Hunter Set
+addappid(3288070) -- THE FINALS - Seas The Day Set
+addappid(3348070) -- THE FINALS - Season 5 Starter Pack
+addappid(3348080) -- THE FINALS - Permafang Prowler Set
+addappid(3348090) -- THE FINALS - 1st Anniversary Bundle
+addappid(3411130) -- THE FINALS - IVADA Cataclysm Set
+addappid(3519910) -- THE FINALS - Wavereaver Set
+addappid(3519920) -- THE FINALS - Season 6 Starter Pack
+addappid(3620110) -- THE FINALS - Zero-G Menace Set
+addappid(3668920) -- THE FINALS - Eyecaster Pack
+addappid(3735580) -- THE FINALS - Season 7 Ultimate Battle Pass Bundle
+addappid(3741370) -- THE FINALS - Season 7 Starter Pack
+addappid(3741380) -- THE FINALS - Ops Override Set
+addappid(3841760) -- THE FINALS - Lotus Reaper Set
+addappid(3964970) -- THE FINALS - Season 8 Ultimate Battle Pass Bundle
+addappid(3964980) -- THE FINALS - Season 8 Starter Pack
+addappid(3964990) -- THE FINALS - Depth Charger Set
+addappid(4013200) -- THE FINALS - ALLIANCE TGM25
+addappid(4013210) -- THE FINALS - ENVY TGM25
+addappid(4013220) -- THE FINALS - KCP TGM25
+addappid(4013230) -- THE FINALS - NTMR TGM25
+addappid(4013240) -- THE FINALS - PULSAR TGM25
+addappid(4013250) -- THE FINALS - SSG TGM25
+addappid(4062430) -- THE FINALS - The Apothecarion Set
+addappid(4077560) -- THE FINALS - The Chirurgeon Set
+addappid(4093790) -- THE FINALS - TEAM SECRET TGM25
+addappid(4093800) -- THE FINALS - FNATIC TGM25
+addappid(4093810) -- THE FINALS - GEN.G TGM25
+addappid(4093820) -- THE FINALS - KINGZERO TGM25
+addappid(4093830) -- THE FINALS - TSM TGM25
+addappid(4093840) -- THE FINALS - VANGUARD TGM25
+addappid(4124650) -- THE FINALS - DRG TGM25
+addappid(4124660) -- THE FINALS - APE SQUAD TGM25
+addappid(4124670) -- THE FINALS - FIVE FEARS TGM25
+addappid(4124680) -- THE FINALS - FN ESPORTS TGM25
+addappid(4124770) -- THE FINALS - New Contestant Pack
+addappid(4162390) -- THE FINALS - Season 9 Ultimate Battle Pass Bundle
+addappid(4162400) -- THE FINALS - Season 9 Starter Pack
+addappid(4162410) -- THE FINALS - 2nd Anniversary Bundle
+addappid(4167860) -- THE FINALS - Project H Set
+addappid(4167870) -- THE FINALS - NTMR TGM25 CHAMPIONS SET
+addappid(4191140) -- THE FINALS - Legacy Battle Pass Season 1
+addappid(4191150) -- THE FINALS - Legacy Battle Pass Season 2
+addappid(4191160) -- THE FINALS - Legacy Battle Pass Season 3
+addappid(4191170) -- THE FINALS - Legacy Battle Pass Season 4
+addappid(4191180) -- THE FINALS - Year 1 Deluxe Exclusives
+addappid(4197310) -- THE FINALS - Year 1 Deluxe Edition
+addappid(4271490) -- THE FINALS - Glitch Prowler Set
+addappid(4403950) -- THE FINALS - Season 10 Ultimate Battle Pass Bundle
+addappid(4403960) -- THE FINALS - Season 10 Starter Pack
+addappid(4406660) -- THE FINALS - Morrgans Call Set
