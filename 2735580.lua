35643c51a7f20c642ddb3948c8c3c9def69b9e36
qiao-lima <qiao-lima@github.com>
2026-04-14 00:17:48 +0800

    Manual Update 2735580

---
+++ 2735580.lua
@@ -0,0 +1,14 @@
+-- 2735580's Lua and Manifest Created by Morrenus
+-- Netherworld Covenant
+-- Created: March 19, 2026 at 15:21:26 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2735580, 1, "2330ce0de1bdf719af6a326474ae4dc5674723fbaeaf00ab7d706d491091183c") -- Netherworld Covenant
+-- MAIN APP DEPOTS
+addappid(2735583, 1, "c55904da7d44d2a56d195136d0a47a4c722bd166cd278e69055417c1972b39be") -- Depot 2735583
+setManifestid(2735583, "7844273384017158928", 4766070280)
+addappid(2735582, 1, "a8082a9e953e30d867a5024427ee2f63cc686720b49ddef2361e570d8ab5a381") -- Depot 2735582
+setManifestid(2735582, "1938712536417697838", 4894661419)
