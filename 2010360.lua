053f33778d97a6804f07ab20a2d002331a168515
qiao-lima <qiao-lima@github.com>
2026-04-13 23:58:16 +0800

    Manual Update 2010360

---
+++ 2010360.lua
@@ -0,0 +1,16 @@
+-- 2010360's Lua and Manifest Created by Morrenus
+-- Uncharted Space - project 3062
+-- Created: April 04, 2026 at 13:27:06 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2010360) -- Uncharted Space - project 3062
+-- MAIN APP DEPOTS
+addappid(2010361, 1, "611572bed2012a3090c7471c7ae2600b69777bceae74d12a769ecc1c1cd98f06") -- Depot 2010361
+setManifestid(2010361, "3209629002302203904", 3633193885)
+-- SHARED DEPOTS (from other apps)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
