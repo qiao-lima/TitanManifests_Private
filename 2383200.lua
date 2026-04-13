e04b5db50aaa365f050f33eb038701403db51396
qiao-lima <qiao-lima@github.com>
2026-04-14 00:07:48 +0800

    Manual Update 2383200

---
+++ 2383200.lua
@@ -0,0 +1,12 @@
+-- 2383200's Lua and Manifest Created by Morrenus
+-- PATAPON 1+2 REPLAY
+-- Created: December 22, 2025 at 09:37:33 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2383200) -- PATAPON 1+2 REPLAY
+-- MAIN APP DEPOTS
+addappid(2383201, 1, "e6ab327b2ce7b1d4a38492ab781adbe69841ef9b0a99d0d939c6c9d65eed4950") -- Depot 2383201
+setManifestid(2383201, "6990912997922452095", 2591345502)
