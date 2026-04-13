a1c520be26e01b6ea0113c415737235f81d8db0e
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:55 +0800

    Manual Update 4130040

---
+++ 4130040.lua
@@ -0,0 +1,12 @@
+-- 4130040's Lua and Manifest Created by Morrenus
+-- Succubus Contract I
+-- Created: April 09, 2026 at 15:56:48 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4130040, 1, "b34ac300fade1b1b109a5ba021786be61d45bb91088fa6970a6c67616e8d451d") -- Succubus Contract I
+-- MAIN APP DEPOTS
+addappid(4130041, 1, "77f0f5aa83a196788e7278a7c498e2bffe48061fe0ca206fb31314e0fe208f40") -- Depot 4130041
+setManifestid(4130041, "5517798197775895883", 10722989445)
