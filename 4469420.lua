21f7766c172bf05dadeac5030cd4caa61ade6942
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:07 +0800

    Manual Update 4469420

---
+++ 4469420.lua
@@ -0,0 +1,12 @@
+-- 4469420's Lua and Manifest Created by Morrenus
+-- Pixel Factory
+-- Created: April 06, 2026 at 09:14:09 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4469420) -- Pixel Factory
+-- MAIN APP DEPOTS
+addappid(4469421, 1, "42a1c727c28729d73d00075310a6a99961069906190da699ec9db1d183165519") -- Depot 4469421
+setManifestid(4469421, "2895278322606281612", 245590449)
