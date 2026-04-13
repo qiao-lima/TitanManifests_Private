0ab3f4dab7eca8a0c71df3823edca0cae13712ea
qiao-lima <qiao-lima@github.com>
2026-04-14 00:17:52 +0800

    Manual Update 2837790

---
+++ 2837790.lua
@@ -0,0 +1,14 @@
+-- 2837790's Lua and Manifest Created by Morrenus
+-- Spinny Dungeon
+-- Created: April 06, 2026 at 09:50:17 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2837790) -- Spinny Dungeon
+-- MAIN APP DEPOTS
+addappid(2837791, 1, "0f7aa7be7af752d9274236c2e5c027b5a6a7c7e03b3c3d0bfb96e5272c3fa631") -- Depot 2837791
+setManifestid(2837791, "519461582956180253", 180697041)
+addappid(2837792, 1, "f6516ba71637e4e1fa3fa14cef3eb2b561c9575a8ac68fdbc4781d3c1d8126f6") -- Depot 2837792
+setManifestid(2837792, "6813746845601237311", 175040869)
