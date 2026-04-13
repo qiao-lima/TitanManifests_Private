80ed10d83e69feb0825f360d342913cff5d1f112
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:12 +0800

    Manual Update 2569670

---
+++ 2569670.lua
@@ -0,0 +1,27 @@
+-- 2569670's Lua and Manifest Created by Morrenus
+-- Farmer's Dynasty 2
+-- Created: April 03, 2026 at 12:10:09 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 5
+-- Total DLCs: 2
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2569670, 1, "a6bcee9fd0169c0021953ce1e7d2b986d5cd7401f24636bdfc8c4b1ae3fcc16c") -- Farmer's Dynasty 2
+-- MAIN APP DEPOTS
+addappid(2569671, 1, "ec134e28be1330625eaa14ca31746901115f91b2350216524873b0e45bf97f3d") -- Depot 2569671
+setManifestid(2569671, "3839267823481654159", 22633615571)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Farmers Dynasty 2 - Official Guide (AppID: 3287820)
+addappid(3287820)
+addappid(3287820, 1, "5c797e49fc1bea3832192814383d54552b71932f13e6f6dc5056ef02ea8e925a") -- Farmers Dynasty 2 - Official Guide - Depot 3287820
+setManifestid(3287820, "7687712714039141840", 61875982)
+-- Farmers Dynasty 2 - Official Cookbook (AppID: 4553090)
+addappid(4553090)
+addappid(4553090, 1, "c2c7b36cfb4935e7754f6d269d48e1ca809db2daa30fbc923c274a95c09f0d20") -- Farmers Dynasty 2 - Official Cookbook - Depot 4553090
+setManifestid(4553090, "4309897858944987277", 119267624)
