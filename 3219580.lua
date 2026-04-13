bd1723c2fe7e69e1dc39584e9e7ebdf66eec452c
qiao-lima <qiao-lima@github.com>
2026-04-14 00:30:05 +0800

    Manual Update 3219580

---
+++ 3219580.lua
@@ -0,0 +1,12 @@
+-- 3219580's Lua and Manifest Created by Morrenus
+-- Theta and Paralldoxs on Worldlines
+-- Created: April 03, 2026 at 04:50:21 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3219580) -- Theta and Paralldoxs on Worldlines
+-- MAIN APP DEPOTS
+addappid(3219581, 1, "f31a80dd133a36f57ea1b860951fb73f0d489156a190b81d36a3f5b650fca51d") -- Depot 3219581
+setManifestid(3219581, "7122487976825026772", 456821486)
