ac6bd3ced33b06a6d2d49ab09195076c958abe44
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:07 +0800

    Manual Update 3990760

---
+++ 3990760.lua
@@ -0,0 +1,12 @@
+-- 3990760's Lua and Manifest Created by Morrenus
+-- Too Well Hidden
+-- Created: March 31, 2026 at 20:24:11 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3990760) -- Too Well Hidden
+-- MAIN APP DEPOTS
+addappid(3990761, 1, "a0abc1198f5b448bc23b373b172ffe871512353c20cbfdf6dbb9b91e40e6f55e") -- Depot 3990761
+setManifestid(3990761, "5540770780991981016", 6558225872)
