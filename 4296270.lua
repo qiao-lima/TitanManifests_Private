f4b104f37a6c5ee4ea991a671eda574834357f77
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:23 +0800

    Manual Update 4296270

---
+++ 4296270.lua
@@ -0,0 +1,12 @@
+-- 4296270's Lua and Manifest Created by Morrenus
+-- 逃出三国塔 Escape the Dynasty
+-- Created: April 03, 2026 at 10:38:48 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4296270) -- 逃出三国塔 Escape the Dynasty
+-- MAIN APP DEPOTS
+addappid(4296271, 1, "ebacfe9886efaf41fa29bd2e1dc6e62e8b9140789c80095d02034fabcf1f9bb3") -- Depot 4296271
+setManifestid(4296271, "2654069431704629993", 1253295874)
