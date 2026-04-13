fb5e43386280c468e1fde9b4b72b095791b0145e
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:05 +0800

    Manual Update 4432350

---
+++ 4432350.lua
@@ -0,0 +1,47 @@
+-- 4432350's Lua and Manifest Created by Morrenus
+-- Multiplier Goblin
+-- Created: April 11, 2026 at 13:48:58 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 12
+-- Total DLCs: 5
+
+-- MAIN APPLICATION
+addappid(4432350) -- Multiplier Goblin
+-- MAIN APP DEPOTS
+addappid(4432351, 1, "b302dd4e487e1ad1712d7208e1f8a3e236c06a22057085ee271230a59c18bc1a") -- Depot 4432351
+setManifestid(4432351, "2553824857916526759", 594034354)
+addappid(4432352, 1, "ec09ee20800260a510f388e17d84c9728c8b1f8a59cae4d8fffc1118d994f6fa") -- Depot 4432352
+setManifestid(4432352, "7844545659993344885", 597328564)
+-- DLCS WITH DEDICATED DEPOTS
+-- Multiplier Goblin - Succubus Character Pack (AppID: 4501580)
+addappid(4501580)
+addappid(4501580, 1, "e69427cabedefe20de104de2da01ee998c5068c2039033d4dffed09c4a77b3ee") -- Multiplier Goblin - Succubus Character Pack - Depot 4501580
+setManifestid(4501580, "7983393827162363269", 64151036)
+addappid(4501581, 1, "0f8bc018de17d347e819a9dc178685e077edff3c5945b28af4657af2fe137457") -- Multiplier Goblin - Succubus Character Pack - Depot 4501581
+setManifestid(4501581, "204282767984880715", 63850896)
+-- Multiplier Goblin - Slime Character Pack (AppID: 4501590)
+addappid(4501590)
+addappid(4501590, 1, "7f7e647227aacddfb45a433b4c455a3c23f54d9b4560cc67a0c8dc255714f304") -- Multiplier Goblin - Slime Character Pack - Depot 4501590
+setManifestid(4501590, "6385934391135314416", 69003115)
+addappid(4501591, 1, "376417b7be92cd256c574c200b08227a9e96aee48bbe3b71c866bb4e397b2189") -- Multiplier Goblin - Slime Character Pack - Depot 4501591
+setManifestid(4501591, "3177191899327564023", 68927469)
+-- Multiplier Goblin - Tentacle Character Pack (AppID: 4501600)
+addappid(4501600)
+addappid(4501600, 1, "d7fc624d6b263a735de95b58f702f003875f4ee75fd654dea03d2ae6a72d52c4") -- Multiplier Goblin - Tentacle Character Pack - Depot 4501600
+setManifestid(4501600, "7577994253607955045", 81639707)
+addappid(4501601, 1, "782638dad284dfbf266742db7567fc7c0b2e37d63c8ea129af8f83a73ba226fc") -- Multiplier Goblin - Tentacle Character Pack - Depot 4501601
+setManifestid(4501601, "8573910383889629676", 80941052)
+-- Multiplier Goblin - Orc Character Pack (AppID: 4501610)
+addappid(4501610)
+addappid(4501610, 1, "1553fed9faee63cb1c14799d6c2f18f778a473197406fdcfc25dcae4b4488cc8") -- Multiplier Goblin - Orc Character Pack - Depot 4501610
+setManifestid(4501610, "3246790321078278154", 53590023)
+addappid(4501611, 1, "907221d04031b6564fa685b2a82790f41407a8fb4e7a064f4e9c575d0c786323") -- Multiplier Goblin - Orc Character Pack - Depot 4501611
+setManifestid(4501611, "4902347654227857283", 51732187)
+-- Multiplier Goblin - Evil Eye Character Pack (AppID: 4501620)
+addappid(4501620)
+addappid(4501620, 1, "618d0b9341d2fe06961c28352df2c8da93bf7f0516eec42252c13f2686cff9d5") -- Multiplier Goblin - Evil Eye Character Pack - Depot 4501620
+setManifestid(4501620, "8134643217266908282", 64187626)
+addappid(4501621, 1, "b4ee7cca261ca0db20032fa64225847eb1081b8beb4d6bb0b635648d00e80106") -- Multiplier Goblin - Evil Eye Character Pack - Depot 4501621
+setManifestid(4501621, "5761040560417832255", 63936441)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4432353) -- Depot 4432353 (empty depot)
