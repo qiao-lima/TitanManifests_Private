ca32576f86fa6de7dfbd9cf15e276f5a88d45eea
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:58 +0800

    Manual Update 4228880

---
+++ 4228880.lua
@@ -0,0 +1,12 @@
+-- 4228880's Lua and Manifest Created by Morrenus
+-- Tiny Utopia
+-- Created: April 09, 2026 at 12:21:44 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4228880) -- Tiny Utopia
+-- MAIN APP DEPOTS
+addappid(4228881, 1, "ec199d674be7532085f2550e0e33520517468ba7b734a86b8fdc512f7a6b0be4") -- Depot 4228881
+setManifestid(4228881, "247501709896277504", 8920980920)
