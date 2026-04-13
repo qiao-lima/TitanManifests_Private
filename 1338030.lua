7f34dac00c630199f88f833b83407c677a689d5a
qiao-lima <qiao-lima@github.com>
2026-04-13 23:42:55 +0800

    Manual Update 1338030

---
+++ 1338030.lua
@@ -0,0 +1,13 @@
+-- 1338030's Lua and Manifest Created by Morrenus
+-- 电视台模拟国
+-- Created: March 20, 2026 at 15:16:14 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1338030) -- 电视台模拟国
+addtoken(1338030, "13310321601085722540")
+-- MAIN APP DEPOTS
+addappid(1338031, 1, "3683c6aa34c6d5f71b5f86ff1d5306bc19d9ea041016bcac947fe6b6d4f5540c") -- Depot 1338031
+setManifestid(1338031, "8634208570271964549", 3769774356)
