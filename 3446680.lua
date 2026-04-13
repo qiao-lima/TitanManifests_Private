ad16b67a734ee64d724923393dfbceb1bf627fda
qiao-lima <qiao-lima@github.com>
2026-04-14 00:36:52 +0800

    Manual Update 3446680

---
+++ 3446680.lua
@@ -0,0 +1,21 @@
+-- 3446680's Lua and Manifest Created by Morrenus
+-- 心跳大冒险：泰遇
+-- Created: January 15, 2026 at 12:21:25 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(3446680) -- 心跳大冒险：泰遇
+-- MAIN APP DEPOTS
+addappid(3446681, 1, "1baa52ded4f657a337210ea04dd4cce31d150ce76e78cc0dc32ac8b395de8668") -- Depot 3446681
+setManifestid(3446681, "4024959806575523828", 46220446256)
+-- DLCS WITH DEDICATED DEPOTS
+--  - 4K  (AppID: 4048000)
+addappid(4048000)
+addappid(4048000, 1, "0be5540a4d5f134e97d1c2b280784b13f2921017eb487057db4a06bc67d15e8b") --  - 4K  - Depot 4048000
+setManifestid(4048000, "4419548848859026580", 68020448230)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+--  - 2K  (AppID: 4048010) - missing depot keys
+-- addappid(4048010)
