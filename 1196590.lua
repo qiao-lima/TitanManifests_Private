39154de3343bdc7508c0ea06f7a769fa42db72ad
qiao-lima <qiao-lima@github.com>
2026-04-13 23:36:23 +0800

    Manual Update 1196590

---
+++ 1196590.lua
@@ -0,0 +1,47 @@
+-- 1196590's Lua and Manifest Created by Morrenus
+-- Resident Evil Village
+-- Created: March 30, 2026 at 21:15:00 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 10
+-- Total DLCs: 6
+-- Shared Depots: 2
+-- Blacklisted Depots: 3
+--   Depot 1196592, 1196594, 1196595: Blacklisted
+
+-- MAIN APPLICATION
+addappid(1196590, 1, "5540e7492fe9a6b06aa86fcac6251b512b4bbb781df477b6b0a3db1ff677dae7") -- Resident Evil Village
+-- MAIN APP DEPOTS
+addappid(1196591, 1, "7ccf761ec4f6be4f36416cd1304044deb8b419204f823cae95c9d9f64f94297c") -- VILLAGE WW1
+setManifestid(1196591, "3932505801699900266", 32798697456)
+addappid(1196593, 1, "95a6023be95ec2186ae5340ee6b7e84f093106e5944e1c8ca09b884622dc41e6") -- VILLAGE WW2
+setManifestid(1196593, "5460835064978089028", 215580031)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Resident Evil Village - Trauma Pack (AppID: 1456360)
+addappid(1456360)
+addappid(1456360, 1, "ba65fa909516f46db9fd95fb6a0e266c4d244b386d260346272290642726b63e") -- Resident Evil Village - Trauma Pack - Resident Evil Village - Trauma Pack (1456360) デポ
+setManifestid(1456360, "3932673717585863523", 102217264)
+-- Resident Evil Village - Extra Content Shop All Access Voucher (AppID: 1456361)
+addappid(1456361)
+addappid(1456361, 1, "c847c83484f69cc3195464a7153560fafdd9ec985afb7bf13e2e48ea552dee6d") -- Resident Evil Village - Extra Content Shop All Access Voucher - Village_DLC02 (1456361) デポ
+setManifestid(1456361, "5314093679455010857", 69036400)
+-- Resident Evil Village - Mr. Raccoon Weapon Charm (AppID: 1456362)
+addappid(1456362)
+addappid(1456362, 1, "46bff99e18ca51b8748333d1dafa1f759b59ca126d648bba42ec8ad386d83827") -- Resident Evil Village - Mr. Raccoon Weapon Charm - Resident Evil Village - Mr. Raccoon Weapon Charm (1456362) デポ
+setManifestid(1456362, "7205803887098070804", 804371)
+-- Resident Evil Village - Survival Resources Pack (AppID: 1456363)
+addappid(1456363)
+addappid(1456363, 1, "39be524780f16cc175cb4e9813d4d4bb50152d4f72be6a4ea0cb23cf7f4669d3") -- Resident Evil Village - Survival Resources Pack - Resident Evil Village - Survival Resources Pack (1456363) デポ
+setManifestid(1456363, "9200672067094795390", 4344)
+-- Resident Evil Village - Winters Expansion (AppID: 1731080)
+addappid(1731080)
+addappid(1731080, 1, "caa8356d4e747e982b77b28cfcb1df9095bea681dfae768c0a727a3cd96a9279") -- Resident Evil Village - Winters Expansion - Depot 1731080
+setManifestid(1731080, "767965597380462013", 7314185482)
+-- Resident Evil Village - Street Wolf Outfit (AppID: 1731081)
+addappid(1731081)
+addappid(1731081, 1, "69638321e164cb5fd216b2ab217bffa7073fce678124665c60934e972bcf0c2e") -- Resident Evil Village - Street Wolf Outfit - Depot 1731081
+setManifestid(1731081, "3897428114068735554", 77053219)
