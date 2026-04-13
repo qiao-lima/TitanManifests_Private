8a2bb4db907f812c962ba65616496cddcf1ad7db
qiao-lima <qiao-lima@github.com>
2026-04-14 00:25:37 +0800

    Manual Update 3071380

---
+++ 3071380.lua
@@ -0,0 +1,12 @@
+-- 3071380's Lua and Manifest Created by Morrenus
+-- The Villainess Quits
+-- Created: March 17, 2026 at 16:51:18 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3071380, 1, "cd3a21b935ac8527d150b0f1c3ed6f6a1bb15d60181b9e9c30026c9733546107") -- The Villainess Quits
+-- MAIN APP DEPOTS
+addappid(3071381, 1, "fc33489b2b15f9dec6faf7e6d2144335d148bba822b286d6de48eaa98daf23f3") -- Depot 3071381
+setManifestid(3071381, "1485409768186942280", 13143560853)
