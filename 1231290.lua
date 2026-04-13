c4ee850175e16cadbff8650025fe88ad3f608269
qiao-lima <qiao-lima@github.com>
2026-04-13 23:42:51 +0800

    Manual Update 1231290

---
+++ 1231290.lua
@@ -0,0 +1,19 @@
+-- 1231290's Lua and Manifest Created by Morrenus
+-- 恋野迷踪 ~ Koishi's LUMO
+-- Created: March 17, 2026 at 15:50:23 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(1231290) -- 恋野迷踪 ~ Koishi's LUMO
+addtoken(1231290, "16480238593931094157")
+-- MAIN APP DEPOTS
+addappid(1231291, 1, "a148471f622eefe7ca7a034c319cdbdb20553d102fa183eff7c9597542664ac4") -- 恋野迷踪 ~ LUMO Content
+setManifestid(1231291, "6639748188949081150", 843150671)
+-- DLCS WITH DEDICATED DEPOTS
+--   Koishis LUMO CG (AppID: 1413080)
+addappid(1413080)
+addtoken(1413080, "4335354307384224141")
+addappid(1413081, 1, "121f4d75d1b1825a1f8b24943d159cfacea265449b86983dc680d907a777e1ed") --   Koishis LUMO CG - Depot 1413081
+setManifestid(1413081, "3556236258256234810", 1214092724)
