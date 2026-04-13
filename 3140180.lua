6d694ce29aef02930a06a707418a917f9502e9c9
qiao-lima <qiao-lima@github.com>
2026-04-14 00:27:10 +0800

    Manual Update 3140180

---
+++ 3140180.lua
@@ -0,0 +1,12 @@
+-- 3140180's Lua and Manifest Created by Morrenus
+-- THYSIASTERY
+-- Created: March 15, 2026 at 12:34:31 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3140180) -- THYSIASTERY
+-- MAIN APP DEPOTS
+addappid(3140181, 1, "c4f0f31a104f9640955c36d212344db60ac5f3277ae59c48cd47e3387047d8be") -- Depot 3140181
+setManifestid(3140181, "6774538088787422569", 423158285)
