e5e4cfb1b8a683f815b2bc15273c562e452160b6
qiao-lima <qiao-lima@github.com>
2026-04-13 23:58:11 +0800

    Manual Update 1889890

---
+++ 1889890.lua
@@ -0,0 +1,12 @@
+-- 1889890's Lua and Manifest Created by Morrenus
+-- Shinehill
+-- Created: March 18, 2026 at 13:43:15 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1889890, 1, "cc3f595f3ff56d38e6269c2efa1cb86014755cbc925722f976e3059ba7dbd334") -- Shinehill
+-- MAIN APP DEPOTS
+addappid(1889891, 1, "394f614167c2148a460030a32651bb7767018fd312d0faf108ccd8fc3ee988a2") -- Depot 1889891
+setManifestid(1889891, "254947458659955296", 1012935617)
