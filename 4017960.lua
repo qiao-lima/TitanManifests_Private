5b571217739316bd0feef451b6e23db491e4326f
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:24 +0800

    Manual Update 4017960

---
+++ 4017960.lua
@@ -0,0 +1,14 @@
+-- 4017960's Lua and Manifest Created by Morrenus
+-- Rose Guns Days -Season 1-
+-- Created: April 09, 2026 at 20:21:12 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4017960) -- Rose Guns Days -Season 1-
+-- MAIN APP DEPOTS
+addappid(4017961, 1, "09b94b27e734be64a7043448b5b4745846c312d95e1d8cf9a93daadf151bc94f") -- Depot 4017961
+setManifestid(4017961, "4334388109902355968", 228706286)
+addappid(4017964, 1, "32aa7527e1da0ffb29b24529624bfcdb30c5a404727a85f9dcd85b95e1063681") -- Depot 4017964
+setManifestid(4017964, "268696206004181625", 1801851570)
