d9480d0f9e7fa3162a6603acdfba8e8263d80e20
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:51 +0800

    Manual Update 4102950

---
+++ 4102950.lua
@@ -0,0 +1,16 @@
+-- 4102950's Lua and Manifest Created by Morrenus
+-- Hyperborea: Hornysolation
+-- Created: April 09, 2026 at 17:03:24 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(4102950) -- Hyperborea: Hornysolation
+-- MAIN APP DEPOTS
+addappid(4102951, 1, "32e298f0daad14231d9fc898a5d3de199a0c4583ce8d95df7d59ec8ca68075d3") -- Depot 4102951
+setManifestid(4102951, "3189676736661767062", 412471814)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Hyperborea Hornysolation - Supporter Pack (AppID: 4403500) - missing depot keys
+-- addappid(4403500)
