e5b2f25a9692163a5bd8eb3f6e8b569b12cf6d93
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:54 +0800

    Manual Update 2681240

---
+++ 2681240.lua
@@ -0,0 +1,18 @@
+-- 2681240's Lua and Manifest Created by Morrenus
+-- In Her Head
+-- Created: April 06, 2026 at 10:27:17 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2681240) -- In Her Head
+-- MAIN APP DEPOTS
+addappid(2681241, 1, "d3dc392e2be0d95479e6f668b23036761d443804df0090f923188475bf505a86") -- Depot 2681241
+setManifestid(2681241, "7576686534050397093", 321990909)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
