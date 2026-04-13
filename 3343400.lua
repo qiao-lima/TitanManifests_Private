46cfb82a5bd09eecf6ab04121a6a0ae7d38b40cd
qiao-lima <qiao-lima@github.com>
2026-04-14 00:32:40 +0800

    Manual Update 3343400

---
+++ 3343400.lua
@@ -0,0 +1,12 @@
+-- 3343400's Lua and Manifest Created by Morrenus
+-- The Little World
+-- Created: March 17, 2026 at 18:53:39 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3343400) -- The Little World
+-- MAIN APP DEPOTS
+addappid(3343401, 1, "6ebaa6f87249c42d3d4fa780bb8d0fabaf3c7b21c4a7b01914f603fad6da6b27") -- Depot 3343401
+setManifestid(3343401, "6057862615732271315", 406031150)
