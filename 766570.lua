a758bc7bd97df0e5de4a81a797e788901ef08742
qiao-lima <qiao-lima@github.com>
2026-04-14 02:53:05 +0800

    Manual Update 766570

---
+++ 766570.lua
@@ -0,0 +1,18 @@
+-- 766570's Lua and Manifest Created by Morrenus
+-- Russian Fishing 4
+-- Created: March 31, 2026 at 01:50:43 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 3
+
+-- MAIN APPLICATION
+addappid(766570, 1, "75a52072c143f83e52784fab136727b9b26b060cb4ff3a4796c8b93446801444") -- Russian Fishing 4
+-- MAIN APP DEPOTS
+addappid(766571, 1, "d7dba035e1a1f07015025cf2749727599df4262383e44f58bb210d3c7f33655e") -- rf4_win64_release
+setManifestid(766571, "5480480909704625236", 42907405110)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(986060) -- Russian Fishing 4 - Amber Lake
+addappid(1366920) -- Russian Fishing 4 - Lower Tunguska River
+addappid(2338590) -- Russian Fishing 4 - Norwegian Sea
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(766572) -- Depot 766572 (empty depot)
