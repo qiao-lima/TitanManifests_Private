4be5332a6152082b5f1bb71770aea1f2c38552f6
qiao-lima <qiao-lima@github.com>
2026-04-14 00:30:10 +0800

    Manual Update 3288210

---
+++ 3288210.lua
@@ -0,0 +1,20 @@
+-- 3288210's Lua and Manifest Created by Morrenus
+-- Super Meat Boy 3D
+-- Created: March 31, 2026 at 20:16:33 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3288210, 1, "c7180d2190d8abf5a1d10be0d13fe3f7254d90faebcf3c4bad0433c5876822ba") -- Super Meat Boy 3D
+-- MAIN APP DEPOTS
+addappid(3288211, 1, "3e57dcc6c265afb1765f400ee54fbeee89d7f6a69e2a2aa626c7d8e52981946e") -- Depot 3288211
+setManifestid(3288211, "3354199473127656688", 3582571802)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3288214) -- Depot 3288214 (empty depot)
