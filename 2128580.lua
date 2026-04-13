baac6d015bc1db29e92b66ed4bbd8c8f5a2670fe
qiao-lima <qiao-lima@github.com>
2026-04-14 00:02:22 +0800

    Manual Update 2128580

---
+++ 2128580.lua
@@ -0,0 +1,12 @@
+-- 2128580's Lua and Manifest Created by Morrenus
+-- Demiurges
+-- Created: February 22, 2026 at 00:18:06 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2128580, 1, "3974365b023e1e4d33a7f8e49167db8d152f8c19d82f30a8d6aa61a4101036f0") -- Demiurges
+-- MAIN APP DEPOTS
+addappid(2128581, 1, "2efbcce313569b18a83a947888416fb1ebd709e58bf2bc9196b2ca9241fecf65") -- Depot 2128581
+setManifestid(2128581, "60616459125074519", 463851383)
