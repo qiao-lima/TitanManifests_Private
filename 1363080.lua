24b11077dd096173847c8a05dcbaee431305deea
qiao-lima <qiao-lima@github.com>
2026-04-13 23:42:57 +0800

    Manual Update 1363080

---
+++ 1363080.lua
@@ -0,0 +1,27 @@
+-- 1363080's Lua and Manifest Created by Morrenus
+-- Manor Lords
+-- Created: December 19, 2025 at 10:10:23 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 6
+-- Total DLCs: 1
+-- Shared Depots: 4
+
+-- MAIN APPLICATION
+addappid(1363080, 1, "e5bf8cd0faa28d9d63f560b028dca5bc8045f07979973e57536826644f2f4f43") -- Manor Lords
+-- MAIN APP DEPOTS
+addappid(1363081, 1, "28763f0001125b10d1d269c0655d8d3f9e6a7678dd9fed0af3f4a37701443e70") -- Depot 1363081
+setManifestid(1363081, "8643641473205241416", 14181412121)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 117381405)
+addappid(229033, 1, "9a789440bbc16131547ac6c062e5f168f7684da533a2899a71fdfaaeec21efc3") -- PhysX System Software 9.14.0702 (Shared from App 228980)
+setManifestid(229033, "2059065101492814639", 60039803)
+-- DLCS WITH DEDICATED DEPOTS
+-- Manor Lords - Artbook (AppID: 2932660)
+addappid(2932660)
+addappid(2932660, 1, "1adba25f4fa5ce03145e033f1f027e5d411e8db2042fbad6317352153d507bfe") -- Manor Lords - Artbook - Depot 2932660
+setManifestid(2932660, "7430712793949389372", 440862129)
