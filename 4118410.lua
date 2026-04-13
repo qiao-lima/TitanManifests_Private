4b637ceccdde44c3abdacfc6f90f9c3a84e0df0c
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:53 +0800

    Manual Update 4118410

---
+++ 4118410.lua
@@ -0,0 +1,16 @@
+-- 4118410's Lua and Manifest Created by Morrenus
+-- 100 StarPaws Cats
+-- Created: April 07, 2026 at 17:48:40 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(4118410) -- 100 StarPaws Cats
+-- MAIN APP DEPOTS
+addappid(4118411, 1, "a13b5b2ab56f680186d3e28f7e581bdc18ebd329102bce2383e4128182609761") -- Depot 4118411
+setManifestid(4118411, "482549803104605346", 331733265)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- 100 StarPaws Cats - Artbook (AppID: 4159020) - missing depot keys
+-- addappid(4159020)
