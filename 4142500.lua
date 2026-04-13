1dbe38f394fbaf07c8fa4512112f848d112c2616
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:09 +0800

    Manual Update 4142500

---
+++ 4142500.lua
@@ -0,0 +1,25 @@
+-- 4142500's Lua and Manifest Created by Morrenus
+-- AV Sex Manager
+-- Created: March 20, 2026 at 20:40:34 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 3
+
+-- MAIN APPLICATION
+addappid(4142500) -- AV Sex Manager
+-- MAIN APP DEPOTS
+addappid(4142501, 1, "94e361bc4af9724f03532eeb1bc2bc81f49711e6959f0b732a57ca638407badd") -- Depot 4142501
+setManifestid(4142501, "3341930502427647757", 1846822890)
+-- DLCS WITH DEDICATED DEPOTS
+-- AV Sex Manager - Digital Artbook (AppID: 4178700)
+addappid(4178700)
+addappid(4178700, 1, "597f7b8ed2a4feaf798d5d62a0b65e7a71b5752d170e3c4285129611766e095e") -- AV Sex Manager - Digital Artbook - Depot 4178700
+setManifestid(4178700, "6616795368436523857", 795933350)
+-- AV Sex Manager - Animation Pack (AppID: 4178710)
+addappid(4178710)
+addappid(4178710, 1, "97bd0e342e82d34a48318ce5a63d428c35167a86ccb90f65b72b658d04b3e4e1") -- AV Sex Manager - Animation Pack - Depot 4178710
+setManifestid(4178710, "1065785404620154263", 1607375558)
+-- AV Sex Manager - Wallpapers Pack (AppID: 4178730)
+addappid(4178730)
+addappid(4178730, 1, "7e004b93d5e3d6e46be8370a9155da46a484e8e631b4a76158e2464277b16c08") -- AV Sex Manager - Wallpapers Pack - Depot 4178730
+setManifestid(4178730, "6334372502811218717", 727114370)
