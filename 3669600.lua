6d1fe6ef57fead15efa7470a922d3ee97d05989f
qiao-lima <qiao-lima@github.com>
2026-04-14 00:41:15 +0800

    Manual Update 3669600

---
+++ 3669600.lua
@@ -0,0 +1,18 @@
+-- 3669600's Lua and Manifest Created by Morrenus
+-- Service Please
+-- Created: March 18, 2026 at 13:42:35 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3669600) -- Service Please
+-- MAIN APP DEPOTS
+addappid(3669601, 1, "30c8cbded77d8cc920c97626857cd68ced77a2020bc59ec6fddf3d83a9877f22") -- Depot 3669601
+setManifestid(3669601, "907314361660601102", 4460208870)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
