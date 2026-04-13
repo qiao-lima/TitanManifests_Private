d7d50e84908444b08ca5737cce9226c008b984d6
qiao-lima <qiao-lima@github.com>
2026-04-14 00:41:13 +0800

    Manual Update 3655990

---
+++ 3655990.lua
@@ -0,0 +1,12 @@
+-- 3655990's Lua and Manifest Created by Morrenus
+-- 宁静之海
+-- Created: April 02, 2026 at 21:03:56 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3655990, 1, "bf977c8e30bb0478e3d1a1e739643c898a3fd7af4c966a2194b7ca2aebf15a18") -- 宁静之海
+-- MAIN APP DEPOTS
+addappid(3655991, 1, "5b0edf73003450bb1d2a8e7d1a5cf7dc4d7c306cfced093aaa83c61ff7dc97be") -- Depot 3655991
+setManifestid(3655991, "4091383556318167269", 2993198492)
