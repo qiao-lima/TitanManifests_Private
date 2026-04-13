9beeb9981ce63887931e9f42563ba270dda99870
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:52 +0800

    Manual Update 4108490

---
+++ 4108490.lua
@@ -0,0 +1,20 @@
+-- 4108490's Lua and Manifest Created by Morrenus
+-- Morkull Ascend to the Gods
+-- Created: April 04, 2026 at 14:19:04 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(4108490) -- Morkull Ascend to the Gods
+-- MAIN APP DEPOTS
+addappid(4108491, 1, "8f6b8c5828a696e6da340768d8fab149da5185db90eb0c26e83990373c956ef0") -- Depot 4108491
+setManifestid(4108491, "5180423749404533091", 1813231438)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
