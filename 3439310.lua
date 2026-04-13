3e757b3c08fc5a1b72ba11ec9a65492bbbc3a5fc
qiao-lima <qiao-lima@github.com>
2026-04-14 00:36:51 +0800

    Manual Update 3439310

---
+++ 3439310.lua
@@ -0,0 +1,12 @@
+-- 3439310's Lua and Manifest Created by Morrenus
+-- Storytime Cats - A Twilight Tale
+-- Created: April 02, 2026 at 21:03:37 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3439310) -- Storytime Cats - A Twilight Tale
+-- MAIN APP DEPOTS
+addappid(3439311, 1, "455945e4b67d8ef2ae3cb9b007d4ec5f30625d505ac3b99386c9736d081fed3d") -- Depot 3439311
+setManifestid(3439311, "3878166772474133688", 1007212167)
