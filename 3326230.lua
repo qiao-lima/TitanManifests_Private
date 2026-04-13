e0965895c04fb2de5a87c9b090bb9f778d56c983
qiao-lima <qiao-lima@github.com>
2026-04-14 00:32:40 +0800

    Manual Update 3326230

---
+++ 3326230.lua
@@ -0,0 +1,18 @@
+-- 3326230's Lua and Manifest Created by Morrenus
+-- Hozy
+-- Created: March 30, 2026 at 12:09:12 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3326230) -- Hozy
+-- MAIN APP DEPOTS
+addappid(3326231, 1, "f18e6e61bbba466e4f42d2c89e1eeacdd7b8b7431b642e32db491ebe514cc98c") -- Depot 3326231
+setManifestid(3326231, "6556632591208776307", 5334597588)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
