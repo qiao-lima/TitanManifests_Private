7531ad493a5c44dc69359857e23d06262b0a2f8b
qiao-lima <qiao-lima@github.com>
2026-04-14 00:40:36 +0800

    Manual Update 3557460

---
+++ 3557460.lua
@@ -0,0 +1,23 @@
+-- 3557460's Lua and Manifest Created by Morrenus
+-- DAMON and BABY
+-- Created: March 26, 2026 at 06:16:11 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3557460) -- DAMON and BABY
+-- MAIN APP DEPOTS
+addappid(3557461, 1, "619812592e6754cce59f92a7d12d9bd53b6b8ece6c073c41b223c2dfbdc148e3") -- Depot 3557461
+setManifestid(3557461, "9081231390350116820", 5000451196)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- DAMON and BABY Artwoks and Soundtrack (AppID: 4346800)
+addappid(4346800)
+addappid(4346800, 1, "69b376de9287728049b96b653ccd767dfdd2f5f6f7bd47d412fceddf84928a1b") -- DAMON and BABY Artwoks and Soundtrack - Depot 4346800
+setManifestid(4346800, "5669512532956907274", 95692608)
