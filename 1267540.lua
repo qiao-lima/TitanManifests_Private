91daedd52afbece11c4807edb1197944899cabb7
qiao-lima <qiao-lima@github.com>
2026-04-13 23:42:52 +0800

    Manual Update 1267540

---
+++ 1267540.lua
@@ -0,0 +1,26 @@
+-- 1267540's Lua and Manifest Created by Morrenus
+-- WRC 9 FIA World Rally Championship
+-- Created: October 01, 2025 at 08:14:06 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 5
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(1267540) -- WRC 9 FIA World Rally Championship
+-- MAIN APP DEPOTS
+addappid(1267541, 1, "af197905780602fd49b05bccf6e8f59a872a943619b0aa7c563b2bc9e48e3ee7") -- WRC 9 FIA World Rally Championship Content
+setManifestid(1267541, "5783580886174022879", 23557189985)
+-- SHARED DEPOTS (from other apps)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (Shared from App 228980)
+setManifestid(228983, "8124929965194586177", 19265607)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- VC 2012 Redist (Shared from App 228980)
+setManifestid(228984, "2547553897526095397", 13742505)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1530500) -- WRC 9 Audi Quattro A2 1984
+addappid(1530501) -- WRC 9 Toyota Corolla 1999
+addappid(1530502) -- WRC 9 Barcelona SSS
+addappid(1530503) -- WRC 9 Career Starter Upgrades
+addappid(1530504) -- WRC 9 FIA Rally Star
