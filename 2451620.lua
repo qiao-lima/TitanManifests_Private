08536cfd3a54144ac693b2ff0591c3c30b0fe8fc
qiao-lima <qiao-lima@github.com>
2026-04-14 00:07:53 +0800

    Manual Update 2451620

---
+++ 2451620.lua
@@ -0,0 +1,12 @@
+-- 2451620's Lua and Manifest Created by Morrenus
+-- Saddle Up and Drive
+-- Created: April 02, 2026 at 05:41:56 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2451620) -- Saddle Up and Drive
+-- MAIN APP DEPOTS
+addappid(2451622, 1, "94b00b8d895f2852e984a37176e7ccf99f6899a8b8a54ef2b323136a2ca26886") -- Depot 2451622
+setManifestid(2451622, "8267851887939823298", 330245204)
