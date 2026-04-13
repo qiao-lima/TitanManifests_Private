ac59d7ff4414ed69a639d576464dd7788b303e3d
qiao-lima <qiao-lima@github.com>
2026-04-14 00:32:42 +0800

    Manual Update 3365030

---
+++ 3365030.lua
@@ -0,0 +1,20 @@
+-- 3365030's Lua and Manifest Created by Morrenus
+-- Atelier Ryza: Ever Darkness & the Secret Hideout DX
+-- Created: December 22, 2025 at 04:52:55 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3365030) -- Atelier Ryza: Ever Darkness & the Secret Hideout DX
+-- MAIN APP DEPOTS
+addappid(3365031, 1, "ed0362b8bfac94a627decf3ed0f4413bbfa39ccf6bcb02819e0ee86ce2b3df22") -- Depot 3365031
+setManifestid(3365031, "7299452652668494523", 29033675231)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3782450) -- Atelier Ryza Ever Darkness  the Secret Hideout DX - DX Version Pre-order Bonus
