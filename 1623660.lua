19e786886e526de81edfaf80e7ba2d6d7a575cf0
qiao-lima <qiao-lima@github.com>
2026-04-13 23:48:23 +0800

    Manual Update 1623660

---
+++ 1623660.lua
@@ -0,0 +1,18 @@
+-- 1623660's Lua and Manifest Created by Morrenus
+-- MIR4
+-- Created: March 31, 2026 at 01:47:10 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1623660, 1, "fe7d206203a96e49944584429689917974e27f70ff9f9916eeb3acadba4228c1") -- MIR4
+-- MAIN APP DEPOTS
+addappid(1623661, 1, "1041c3b158f316ee0590805796ae98ddb308f7468e1ce6de0ed1df161c879141") -- Depot 1623661
+setManifestid(1623661, "8346026815598355324", 595602516)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
