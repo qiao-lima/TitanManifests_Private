b77a0dbf7e4c3df6a4d5fa2ed0a9b003947b15b7
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:11 +0800

    Manual Update 4164580

---
+++ 4164580.lua
@@ -0,0 +1,18 @@
+-- 4164580's Lua and Manifest Created by Morrenus
+-- 逆袭重启
+-- Created: March 08, 2026 at 03:10:01 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4164580) -- 逆袭重启
+-- MAIN APP DEPOTS
+addappid(4164581, 1, "c99142aa95911c464ad5c0fb82181203e5f8f174f4ce5ab62a85b98b76359795") -- Depot 4164581
+setManifestid(4164581, "7398325038742686808", 369527645)
+addappid(4164582, 1, "6450375231b39beec00a593d1438f4b97947ce280e005bc45b188dc0f5ade8cf") -- Depot 4164582
+setManifestid(4164582, "7493824658306093717", 4599015108)
+addappid(4164583, 1, "b9a827dfcbf0589eec59c833ac75663fa3d3f0ef1ce1738a0cea37c54e4b32c0") -- Depot 4164583
+setManifestid(4164583, "6272567561716867107", 14153334)
+addappid(4164584, 1, "efddcbafe03bb62866d956a802bbf525eff3f3d4674b6657e3fc978b48b31626") -- Depot 4164584
+setManifestid(4164584, "3343384034862704736", 71086448567)
