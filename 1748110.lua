e9cdfd0f5ffadea13d5108d9619270024cdc1639
qiao-lima <qiao-lima@github.com>
2026-04-13 23:52:41 +0800

    Manual Update 1748110

---
+++ 1748110.lua
@@ -0,0 +1,18 @@
+-- 1748110's Lua and Manifest Created by Morrenus
+-- Town of Zoz
+-- Created: April 09, 2026 at 18:39:02 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1748110) -- Town of Zoz
+-- MAIN APP DEPOTS
+addappid(1748111, 1, "8b37aec374a04cac134ac28a6a99d8b3ffcb618cffd9638a3fbf961f1d305a13") -- Depot 1748111
+setManifestid(1748111, "2732972486663077103", 5243100819)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
