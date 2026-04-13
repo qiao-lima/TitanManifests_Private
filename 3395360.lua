dc2d45d2d205258018b2180b93ee8de7f1097b38
qiao-lima <qiao-lima@github.com>
2026-04-14 00:32:45 +0800

    Manual Update 3395360

---
+++ 3395360.lua
@@ -0,0 +1,16 @@
+-- 3395360's Lua and Manifest Created by Morrenus
+-- Reflections of the void
+-- Created: March 14, 2026 at 11:40:11 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3395360, 1, "40550157366d8393c3a95bfc9d4edc5920fc1e436f38757fef89ce21494b232a") -- Reflections of the void
+-- MAIN APP DEPOTS
+addappid(3395362, 1, "f36ec8369f12727a8ecc9a86547ef02a77b7f298d9b86c794d42e3725fdd9d8a") -- Depot 3395362
+setManifestid(3395362, "1863659183645455319", 1877147176)
+addappid(3395363, 1, "bfcf5a646446408bb0b92a5e66e019704a1e8b932b8a40238c7c81de9cd384d0") -- Depot 3395363
+setManifestid(3395363, "3810860356713204031", 1864862357)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3395361) -- Depot 3395361 (empty depot)
