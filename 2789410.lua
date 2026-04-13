0bb45441bdf3d9de81c70222246cfa085b209def
qiao-lima <qiao-lima@github.com>
2026-04-14 00:17:51 +0800

    Manual Update 2789410

---
+++ 2789410.lua
@@ -0,0 +1,12 @@
+-- 2789410's Lua and Manifest Created by Morrenus
+-- Komadori Inn
+-- Created: April 01, 2026 at 11:26:02 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2789410) -- Komadori Inn
+-- MAIN APP DEPOTS
+addappid(2789411, 1, "97b924ef34e279d63dc59146e5eaba413da0367b332dd2db539303fc04dd5ef6") -- Depot 2789411
+setManifestid(2789411, "7076643964311409300", 1083460659)
