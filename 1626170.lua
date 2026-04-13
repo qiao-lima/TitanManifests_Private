9d2f8767ea8e8563a9f042d3d9806289035e0af0
qiao-lima <qiao-lima@github.com>
2026-04-13 23:48:23 +0800

    Manual Update 1626170

---
+++ 1626170.lua
@@ -0,0 +1,18 @@
+-- 1626170's Lua and Manifest Created by Morrenus
+-- People of Note
+-- Created: April 07, 2026 at 09:04:55 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1626170) -- People of Note
+-- MAIN APP DEPOTS
+addappid(1626171, 1, "fae921d149575c5467467a880e8bad39a256ca7c01e1edae43f781895e749ab6") -- Depot 1626171
+setManifestid(1626171, "765785342231683049", 26138453497)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
