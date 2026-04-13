d0d9dc5ff57f0aab080bc9967f71fdea8ac497c3
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:07 +0800

    Manual Update 4468350

---
+++ 4468350.lua
@@ -0,0 +1,12 @@
+-- 4468350's Lua and Manifest Created by Morrenus
+-- Voxelnova
+-- Created: April 06, 2026 at 13:49:08 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4468350) -- Voxelnova
+-- MAIN APP DEPOTS
+addappid(4468351, 1, "ea06cb54d32bb915cc04ba1c39b0d36ac915598a14fa92b2f570140575e43288") -- Depot 4468351
+setManifestid(4468351, "1810683613311620547", 131588296)
