dec4be22d7aaa10013f216adbc650bf188d99af5
qiao-lima <qiao-lima@github.com>
2026-04-13 23:52:39 +0800

    Manual Update 1663000

---
+++ 1663000.lua
@@ -0,0 +1,12 @@
+-- 1663000's Lua and Manifest Created by Morrenus
+-- Forlorn: The mysteries of Highshadowland
+-- Created: March 29, 2026 at 12:34:56 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1663000) -- Forlorn: The mysteries of Highshadowland
+-- MAIN APP DEPOTS
+addappid(1663001, 1, "4092f13bfc3661d8b4e5bcfb2df3a7790b8ffa6ffc8c85b73551c6369836ceb8") -- Depot 1663001
+setManifestid(1663001, "4128136394053185192", 10194108796)
