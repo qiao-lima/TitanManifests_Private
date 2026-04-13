feee922d6e453242c757eaf20d279c18d66bc79d
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:10 +0800

    Manual Update 4147820

---
+++ 4147820.lua
@@ -0,0 +1,20 @@
+-- 4147820's Lua and Manifest Created by Morrenus
+-- Your Mom
+-- Created: April 07, 2026 at 12:24:04 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(4147820, 1, "c8d270556cdecd8005f1a549a68df9d8e6eae1c46c8ea059fdf4790543039f1a") -- Your Mom
+-- MAIN APP DEPOTS
+addappid(4147821, 1, "c7402a81531af57513c9124867754dfe6be6b039c3e68fa3c8ba1597870a51f1") -- Depot 4147821
+setManifestid(4147821, "528429301777467423", 252050344)
+-- DLCS WITH DEDICATED DEPOTS
+-- Your Mom - DLC (AppID: 4206120)
+addappid(4206120)
+addappid(4206120, 1, "84c8a7ed06ebda90864c5e66cf33136e50891102fb6796edd7958b922f916cac") -- Your Mom - DLC - Depot 4206120
+setManifestid(4206120, "52280185410003384", 246328738)
+-- EXCLUDED DLCS:
+-- UNRELEASED DLCS (COMMENTED OUT)
+-- addappid(4442970) -- Your Mom's DLC (unreleased)
