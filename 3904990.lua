f997582131504d5e1aeb2fff98d91f1f590ecd49
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:37 +0800

    Manual Update 3904990

---
+++ 3904990.lua
@@ -0,0 +1,12 @@
+-- 3904990's Lua and Manifest Created by Morrenus
+-- Love Love Mystery Club
+-- Created: January 29, 2026 at 08:04:50 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3904990) -- Love Love Mystery Club
+-- MAIN APP DEPOTS
+addappid(3904991, 1, "14ad8caa761cccb2b5fd4e645d49b37d343b3f6b9c08396e3b0fd37a3547088b") -- Depot 3904991
+setManifestid(3904991, "5262813596952459816", 47907826727)
