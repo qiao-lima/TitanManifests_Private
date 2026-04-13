6719589e4485a571250e61c52714dc6a578a94a9
qiao-lima <qiao-lima@github.com>
2026-04-13 23:52:40 +0800

    Manual Update 1697870

---
+++ 1697870.lua
@@ -0,0 +1,19 @@
+-- 1697870's Lua and Manifest Created by Morrenus
+-- Crown of Greed
+-- Created: March 31, 2026 at 12:58:57 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(1697870) -- Crown of Greed
+-- MAIN APP DEPOTS
+addappid(1697871, 1, "fbb7cc5a010587be482a97583b5a31774bc9f2fe7c1adada0c1448f0d80c4578") -- Depot 1697871
+setManifestid(1697871, "2956374936980594125", 8831969747)
+-- DLCS WITH DEDICATED DEPOTS
+-- Crown of Greed - Supporter Pack (AppID: 4527750)
+addappid(4527750)
+addappid(4527750, 1, "c4e5e3a281de3be9d872c1a95de026fd248d187e978b01e3e7206dc3df503457") -- Crown of Greed - Supporter Pack - Depot 4527750
+setManifestid(4527750, "7657130452707524801", 1169701445)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4527751) -- Depot 4527751 (empty depot)
