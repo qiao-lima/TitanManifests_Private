077c06c10bdefab5db8c42375b64bda70b32e6a0
qiao-lima <qiao-lima@github.com>
2026-04-13 23:42:57 +0800

    Manual Update 1371980

---
+++ 1371980.lua
@@ -0,0 +1,24 @@
+-- 1371980's Lua and Manifest Created by Morrenus
+-- No Rest for the Wicked
+-- Created: March 18, 2026 at 17:26:08 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 5
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1371980, 1, "baf4a4496a90c673e65c0458177cfa9ff7edf6480002cbcafa70b2a2f1d944c9") -- No Rest for the Wicked
+-- MAIN APP DEPOTS
+addappid(1371981, 1, "9598cfe06b948f9eca5cbd915b40bec41730f61f6349bbd41d5ee6e86c47a3f6") -- Depot 1371981
+setManifestid(1371981, "7804900631797363954", 48315074353)
+addappid(1371982, 1, "d2a7d91fcd80393c3bf0c9e55165cba9c37c264dbe3eddb717215de20508e607") -- Depot 1371982
+setManifestid(1371982, "2318903721624615448", 566037597)
+addappid(1371985, 1, "9f273c98ebab602f1cf76be43e4834f934f44f098537271c9c04c9fbb449004c") -- Depot 1371985
+setManifestid(1371985, "5366900976129448097", 2613815244)
+addappid(1371986, 1, "f63741bbbae5e090d2c6cb14d8cb10351d16ad159acdeb6aa051da9425702c67") -- Depot 1371986
+setManifestid(1371986, "4857274357134453251", 2622101996)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(1371983) -- Depot 1371983 (empty depot)
