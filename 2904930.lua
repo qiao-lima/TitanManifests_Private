b327206d2c7d01eea7e47e91643b633230895e28
qiao-lima <qiao-lima@github.com>
2026-04-14 00:21:48 +0800

    Manual Update 2904930

---
+++ 2904930.lua
@@ -0,0 +1,12 @@
+-- 2904930's Lua and Manifest Created by Morrenus
+-- The PARADOGX Incident
+-- Created: April 03, 2026 at 11:22:43 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2904930, 1, "b8c8357230fce56711841c717617015f0714a9405e91f7c939627bfc507a1ff6") -- The PARADOGX Incident
+-- MAIN APP DEPOTS
+addappid(2904931, 1, "9fbfedb8f80d35abf2c4e1131021aeb91c8cc048aa368ae6476e68b131e5468c") -- Depot 2904931
+setManifestid(2904931, "2786782878049834905", 3586512866)
