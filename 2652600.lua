c714bb672e14f773006783d0bdcd847a94f6e8ad
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:52 +0800

    Manual Update 2652600

---
+++ 2652600.lua
@@ -0,0 +1,12 @@
+-- 2652600's Lua and Manifest Created by Morrenus
+-- SealChain: Call of Blood
+-- Created: March 22, 2026 at 06:52:40 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2652600, 1, "900f810d9d34394f56b6178be9358649212209a7e83a4d11ced4ffb94af1b2fb") -- SealChain: Call of Blood
+-- MAIN APP DEPOTS
+addappid(2652601, 1, "5b1991827d932651881e7ec625da6ec7a412de9e30f565555114d1663551ea2a") -- Depot 2652601
+setManifestid(2652601, "5276840263973444205", 516617801)
