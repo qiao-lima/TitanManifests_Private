bafef22b70192126ccb8f1931fe9fe39d04faddd
qiao-lima <qiao-lima@github.com>
2026-04-14 00:30:10 +0800

    Manual Update 3289090

---
+++ 3289090.lua
@@ -0,0 +1,18 @@
+-- 3289090's Lua and Manifest Created by Morrenus
+-- Ghost Master: Resurrection
+-- Created: March 21, 2026 at 03:24:47 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3289090, 1, "f165cf03a4fc9bb0a2129ecc8f494f8259aa1e3da57449f6114c7a19f2f94338") -- Ghost Master: Resurrection
+-- MAIN APP DEPOTS
+addappid(3289091, 1, "da1a6bcad881eef9d2c2a1224fa9d28942b4f4ecd313cb24b7d91007c2666f23") -- Depot 3289091
+setManifestid(3289091, "1194595355999728715", 12474000259)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
