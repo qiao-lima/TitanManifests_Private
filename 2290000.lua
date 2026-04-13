85be1f87182d37bab12178e8e015822106cd294e
qiao-lima <qiao-lima@github.com>
2026-04-14 00:06:17 +0800

    Manual Update 2290000

---
+++ 2290000.lua
@@ -0,0 +1,20 @@
+-- 2290000's Lua and Manifest Created by Morrenus
+-- TerraScape
+-- Created: March 20, 2026 at 16:42:34 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(2290000, 1, "3b81dc48bc9babb5d518e3e9dd6775bc94b17d2e1c67c0208004648094dfe0f7") -- TerraScape
+-- MAIN APP DEPOTS
+addappid(2290001, 1, "3cf35d14bf13777748547142d001d9010de60946610bcdb524db81acc663069f") -- Depot 2290001
+setManifestid(2290001, "4657553731486159974", 1766210847)
+addappid(2290002, 1, "61efeff52056556b09f82b288770f199af4e0c0d2ec218f55b017b1060bd72c9") -- Depot 2290002
+setManifestid(2290002, "310700091835295907", 2460096843)
+-- DLCS WITH DEDICATED DEPOTS
+-- TerraScape Ancient Egypt (AppID: 3868750)
+addappid(3868750)
+addappid(3868751, 1, "ea1089935b8eebb6ca645cf00ccfb5669e23f3fde1c955add8024feb19dcd577") -- TerraScape Ancient Egypt - Depot 3868751
+setManifestid(3868751, "8922594128995173054", 1484949757)
+-- addappid(3868752, 1, "MISSING_KEY") -- TerraScape Ancient Egypt - Depot 3868752 (no key available)
