9e753511e7a880427e79eef08acdff91d41c47b6
qiao-lima <qiao-lima@github.com>
2026-04-14 00:40:34 +0800

    Manual Update 3535760

---
+++ 3535760.lua
@@ -0,0 +1,12 @@
+-- 3535760's Lua and Manifest Created by Morrenus
+-- High Above
+-- Created: April 09, 2026 at 12:57:14 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3535760) -- High Above
+-- MAIN APP DEPOTS
+addappid(3535761, 1, "1f5307a3c601d2764c01b01d2b6b270b1e261d9cc3769ec7d907b57ca5277e8c") -- Depot 3535761
+setManifestid(3535761, "8813009374835506937", 3093225772)
