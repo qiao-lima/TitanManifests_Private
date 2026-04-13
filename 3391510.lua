fc83296634ead5e57beaa32b1757785d3b0bb26d
qiao-lima <qiao-lima@github.com>
2026-04-14 00:32:44 +0800

    Manual Update 3391510

---
+++ 3391510.lua
@@ -0,0 +1,12 @@
+-- 3391510's Lua and Manifest Created by Morrenus
+-- Island Market Simulator
+-- Created: March 28, 2026 at 20:04:26 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3391510) -- Island Market Simulator
+-- MAIN APP DEPOTS
+addappid(3391511, 1, "386b71bed1a9faeeda969d8a3c0269c2a2f9877c50dcae805b20ccf9da4d737d") -- Depot 3391511
+setManifestid(3391511, "4365414580337603468", 6399130028)
