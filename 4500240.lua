8a99154513aaa6b4b63e518d5bf8678284dd3bad
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:09 +0800

    Manual Update 4500240

---
+++ 4500240.lua
@@ -0,0 +1,12 @@
+-- 4500240's Lua and Manifest Created by Morrenus
+-- Love in Every Note
+-- Created: April 04, 2026 at 10:08:16 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4500240) -- Love in Every Note
+-- MAIN APP DEPOTS
+addappid(4500241, 1, "20ffed1691f992f07182904f626262891b223b69555dd6906d021498f00efd5a") -- Depot 4500241
+setManifestid(4500241, "8028717866387301085", 1062001722)
