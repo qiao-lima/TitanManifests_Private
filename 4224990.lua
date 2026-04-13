2dfc6606bdaa0eee127f800b685a86ccd694b0cb
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:57 +0800

    Manual Update 4224990

---
+++ 4224990.lua
@@ -0,0 +1,12 @@
+-- 4224990's Lua and Manifest Created by Morrenus
+-- Before Dawn
+-- Created: March 17, 2026 at 17:03:54 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4224990) -- Before Dawn
+-- MAIN APP DEPOTS
+addappid(4224991, 1, "ea279b1b86d4a084dc4bfde8bb5b52e2ee881835eb58a16443473666b3706cdf") -- Depot 4224991
+setManifestid(4224991, "1757916827339847261", 1150602992)
