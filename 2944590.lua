65a3f80ad5e9185d19546561df644619c3dfabe8
qiao-lima <qiao-lima@github.com>
2026-04-14 00:21:51 +0800

    Manual Update 2944590

---
+++ 2944590.lua
@@ -0,0 +1,12 @@
+-- 2944590's Lua and Manifest Created by Morrenus
+-- Chowdown Kitty
+-- Created: March 11, 2026 at 03:04:54 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2944590, 1, "754062db6871fa842ebacaa040331ed7a07a5829089700e1f624a48f9eda0bff") -- Chowdown Kitty
+-- MAIN APP DEPOTS
+addappid(2944591, 1, "ca32e76ee3c0185f41745955c952991b36f1016f1b216295947ae66c45041ce0") -- Depot 2944591
+setManifestid(2944591, "6156373451413084383", 357686018)
