10f0d1b7a1ed985da16940a004cf609a83a11760
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:39 +0800

    Manual Update 4207590

---
+++ 4207590.lua
@@ -0,0 +1,16 @@
+-- 4207590's Lua and Manifest Created by Morrenus
+-- Catjong 4: Secrets of the Purramids
+-- Created: April 03, 2026 at 04:26:06 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4207590) -- Catjong 4: Secrets of the Purramids
+-- MAIN APP DEPOTS
+addappid(4207591, 1, "eac0f5c936b3498f708e068c9dd9ebd5a44fa80bb7d1fd9a7c0c2ade07c02c84") -- Depot 4207591
+setManifestid(4207591, "185971295776595079", 205742022)
+addappid(4207592, 1, "bf764c6d07df0281efdfbcf00e96e92f199f2aafd65bf2de8f85db0d4d64aedc") -- Depot 4207592
+setManifestid(4207592, "7276821865261960228", 232599766)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4350920) -- Catjong 4 Secrets of the Purramids Full Version Unlock
