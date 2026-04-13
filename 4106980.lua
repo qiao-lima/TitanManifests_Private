30baecc2dbfa113463871ae7328458bc2de777cd
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:52 +0800

    Manual Update 4106980

---
+++ 4106980.lua
@@ -0,0 +1,12 @@
+-- 4106980's Lua and Manifest Created by Morrenus
+-- 山河小侠
+-- Created: April 05, 2026 at 06:30:39 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4106980) -- 山河小侠
+-- MAIN APP DEPOTS
+addappid(4106981, 1, "51c5a487525fce88a77ee4e08f0d1b5e6145fb0a3b1375ffd6d60d80d50528f9") -- Depot 4106981
+setManifestid(4106981, "7617818180682816119", 1163763152)
