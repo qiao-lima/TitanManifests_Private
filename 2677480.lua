c8f35302fc16f0e34546e4551177295b52767b18
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:53 +0800

    Manual Update 2677480

---
+++ 2677480.lua
@@ -0,0 +1,12 @@
+-- 2677480's Lua and Manifest Created by Morrenus
+-- Beat the Beats VR
+-- Created: April 04, 2026 at 13:33:41 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2677480) -- Beat the Beats VR
+-- MAIN APP DEPOTS
+addappid(2677481, 1, "56f94a30ac167ac5fa6cbb61122180df3e03d997eeec846a3b5b3626a5fba6d5") -- Depot 2677481
+setManifestid(2677481, "488681021444250333", 1378392145)
