db73bd6967b568b5b68ff963f98ab044b79055df
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:00 +0800

    Manual Update 4259660

---
+++ 4259660.lua
@@ -0,0 +1,12 @@
+-- 4259660's Lua and Manifest Created by Morrenus
+-- Robot Hero
+-- Created: March 31, 2026 at 20:26:25 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4259660) -- Robot Hero
+-- MAIN APP DEPOTS
+addappid(4259661, 1, "aca51ab16d85af3e677b76ca3f16585daf27a3a609df3c8e2a5ef8afb5dbb0d4") -- Depot 4259661
+setManifestid(4259661, "9175329999477037394", 250335052)
