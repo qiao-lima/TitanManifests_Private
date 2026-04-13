c0e9894589563e2d60814f1fac994aaaaa0b7b14
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:29 +0800

    Manual Update 644930

---
+++ 644930.lua
@@ -0,0 +1,23 @@
+-- 644930's Lua and Manifest Created by Morrenus
+-- They Are Billions
+-- Created: January 25, 2026 at 09:40:12 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(644930, 1, "4fd3e48b43b5c2f4a31038f55ce533a9bfe9f59175ffdc3b8da2084d0954a9bc") -- They Are Billions
+-- MAIN APP DEPOTS
+addappid(644931, 1, "44dfb497f7adb842912ea368c2535be6c163e2d25e1a4a9b06f682f60508a673") -- They Are Billions Content
+setManifestid(644931, "8644553349467774858", 5512797915)
+-- SHARED DEPOTS (from other apps)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") -- .NET 4.6 Redist (Shared from App 228980)
+setManifestid(229005, "7992454656023763365", 62009092)
+-- DLCS WITH DEDICATED DEPOTS
+-- They Are Billions - Soundtrack  Art Book (AppID: 1100130)
+addappid(1100130)
+addappid(1100130, 1, "58b125475d5c892070bc9b783a235987f0838d35e87719d52be3a3036b15955e") -- They Are Billions - Soundtrack  Art Book - They Are Billions - Soundtrack & Art Book (1100130) Depot
+setManifestid(1100130, "3000532913243472563", 613415580)
