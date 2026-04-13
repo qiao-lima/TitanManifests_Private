11dd2efdaa0d79e70c3abbe7c5807651cf5eb122
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:10 +0800

    Manual Update 4151830

---
+++ 4151830.lua
@@ -0,0 +1,19 @@
+-- 4151830's Lua and Manifest Created by Morrenus
+-- Nippets
+-- Created: April 07, 2026 at 22:15:38 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4151830) -- Nippets
+-- MAIN APP DEPOTS
+addappid(4151831, 1, "4ab8874dc44ff4460b0ac9ca202b7adbe875fe25b0c25f4487d70d7ded017ea8") -- Depot 4151831
+setManifestid(4151831, "3791340045991046495", 4737292775)
+addappid(4151832, 1, "6ba9d65d3ffa37858db25b3672ae000c035c26939edc5edb658a8faa4522ef18") -- Depot 4151832
+setManifestid(4151832, "3655697649892227359", 4752598062)
+-- DLCS WITH DEDICATED DEPOTS
+-- Nippets - Exclusive Wallpapers (AppID: 4572980)
+addappid(4572980)
+addappid(4572989, 1, "bae42058c344b996d798688b6e4ce01cc55ee42d462f25deef3114beb9ca71da") -- Nippets - Exclusive Wallpapers - Depot 4572989
+setManifestid(4572989, "1912462108358229211", 36282364)
