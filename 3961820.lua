8307502a5bd6188ca3459c29606f4009a9dfce84
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:04 +0800

    Manual Update 3961820

---
+++ 3961820.lua
@@ -0,0 +1,16 @@
+-- 3961820's Lua and Manifest Created by Morrenus
+-- Kindling
+-- Created: April 06, 2026 at 01:59:57 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3961820) -- Kindling
+-- MAIN APP DEPOTS
+addappid(3961822, 1, "df4f45eb78588e10d1fbee4d15372aa487a7e36250d9253ca2b128b97727fb7a") -- Depot 3961822
+setManifestid(3961822, "643361610191291358", 1116876764)
+addappid(3961823, 1, "da74aaf7ed871ee94804956187ecc1c435c1e04d3610782aa4d046d58770e973") -- Depot 3961823
+setManifestid(3961823, "7778916606498374169", 1048254346)
+addappid(3961824, 1, "ee3720dc99e6aded017f75718409aa59c2bd7911ca23d64541cb2d1409cf862f") -- Depot 3961824
+setManifestid(3961824, "487734541503567213", 1013977068)
