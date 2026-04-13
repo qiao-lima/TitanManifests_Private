9fc42b8d62c41cb4ad503d5bc3eac2d6b8025958
qiao-lima <qiao-lima@github.com>
2026-04-13 23:52:43 +0800

    Manual Update 1834850

---
+++ 1834850.lua
@@ -0,0 +1,13 @@
+-- 1834850's Lua and Manifest Created by Morrenus
+-- save the princess
+-- Created: April 08, 2026 at 14:10:05 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1834850) -- save the princess
+addtoken(1834850, "7969727571288084052")
+-- MAIN APP DEPOTS
+addappid(1834851, 1, "c98a3cd4a6ca3aaf7089892639b43915f92fffe47a6f8b0f681a8080ac1945fd") -- save the princess Content
+setManifestid(1834851, "3305998055206485344", 507196454)
