6789cb1beabf6fdce7c0497900fc9f13c1d89fbb
qiao-lima <qiao-lima@github.com>
2026-04-13 21:23:11 +0800

    Manual Update 3758360

---
+++ 3758360.lua
@@ -0,0 +1,12 @@
+-- 3758360's Lua and Manifest Created by Morrenus
+-- Alive 2
+-- Created: April 13, 2026 at 06:36:07 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3758360) -- Alive 2
+-- MAIN APP DEPOTS
+addappid(3758361, 1, "6659102e4409c056bcb5342849a0067bf87da62ebbc91e66dca5969e477065af") -- Depot 3758361
+setManifestid(3758361, "309825748949447992", 6306703857)
