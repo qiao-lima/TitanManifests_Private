a324f7a4e708bca90b1dc3b66f8d9f84c6697083
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:08 +0800

    Manual Update 2490960

---
+++ 2490960.lua
@@ -0,0 +1,25 @@
+-- 2490960's Lua and Manifest Created by Morrenus
+-- 驱灵天师
+-- Created: April 06, 2026 at 06:44:14 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 6
+-- Total DLCs: 0
+-- Shared Depots: 5
+
+-- MAIN APPLICATION
+addappid(2490960) -- 驱灵天师
+addtoken(2490960, "10796824397001052789")
+-- MAIN APP DEPOTS
+addappid(2490961, 1, "b7a145e516a87c9d870e1fcef976e3df5114db7e226e6bd609c02343012c2165") -- Depot 2490961
+setManifestid(2490961, "8211637985102022960", 14384695083)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") -- .NET 4.5.2 Redist (Shared from App 228980)
+setManifestid(229004, "5220958916987797232", 70000464)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") -- .NET 4.6 Redist (Shared from App 228980)
+setManifestid(229005, "7992454656023763365", 62009092)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") -- .NET 4.7 Redist (Shared from App 228980)
+setManifestid(229006, "1784011429307107530", 83944258)
