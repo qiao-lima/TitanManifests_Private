f02863c3390aae3a2edaedb5fe318f95abe4881f
qiao-lima <qiao-lima@github.com>
2026-04-14 00:17:51 +0800

    Manual Update 2795380

---
+++ 2795380.lua
@@ -0,0 +1,12 @@
+-- 2795380's Lua and Manifest Created by Morrenus
+-- Dino Park
+-- Created: April 01, 2026 at 12:24:35 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2795380) -- Dino Park
+-- MAIN APP DEPOTS
+addappid(2795381, 1, "33e10583a32a3cd22ad8cab53391701a238f5bc9802dc83aa9962edbb0a2e33e") -- Depot 2795381
+setManifestid(2795381, "7166694824300958968", 267166548)
