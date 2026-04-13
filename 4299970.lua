dfa5bc7856edb0f17cdb538fbd3bc87b8845eb57
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:24 +0800

    Manual Update 4299970

---
+++ 4299970.lua
@@ -0,0 +1,12 @@
+-- 4299970's Lua and Manifest Created by Morrenus
+-- Immoral office : Hypno Switch
+-- Created: March 22, 2026 at 20:41:17 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4299970) -- Immoral office : Hypno Switch
+-- MAIN APP DEPOTS
+addappid(4299971, 1, "35c364518040f6730117ea4a2f988d3aed383a167d336a886a68242f8feece1b") -- Depot 4299971
+setManifestid(4299971, "7578794882449656963", 1614783903)
