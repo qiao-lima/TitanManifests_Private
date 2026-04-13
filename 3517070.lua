c48ed599512291c3dc001d96bdcc5fc906888226
qiao-lima <qiao-lima@github.com>
2026-04-14 00:40:33 +0800

    Manual Update 3517070

---
+++ 3517070.lua
@@ -0,0 +1,18 @@
+-- 3517070's Lua and Manifest Created by Morrenus
+-- Agent Sirius
+-- Created: April 12, 2026 at 06:20:35 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3517070) -- Agent Sirius
+-- MAIN APP DEPOTS
+addappid(3517071, 1, "c9c5040d64d10edf9c318a213269fb6ba62b067e21986b4194bf186cff9c23be") -- Depot 3517071
+setManifestid(3517071, "1731430467614277214", 613056057)
+addappid(3517072, 1, "80be18e219aea3cb36068035e8e594608167ff3bd839399d269b3c737883545c") -- Depot 3517072
+setManifestid(3517072, "4299298061417483699", 613307871)
+addappid(3517073, 1, "0f4cc008ffbaf62759f843ed6a1c96a966deddb527e958a0d6c1bf38789653cd") -- Depot 3517073
+setManifestid(3517073, "513377659838498534", 613074124)
+addappid(3517074, 1, "29cc7f6ff36e834bf8946aea561562b6d03f733b4a4aa50915da3c6258b4369b") -- Depot 3517074
+setManifestid(3517074, "1221417848880961549", 613074624)
