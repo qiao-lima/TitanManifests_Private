0dbfb1cccaa69ab9c71f87d3237a40eb53b6e714
qiao-lima <qiao-lima@github.com>
2026-04-14 00:27:12 +0800

    Manual Update 3167020

---
+++ 3167020.lua
@@ -0,0 +1,14 @@
+-- 3167020's Lua and Manifest Created by Morrenus
+-- Escape from Duckov
+-- Created: February 13, 2026 at 06:07:56 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3167020, 1, "868bad77591803956d03d9863f4775a7d402330362cbd41c1c764c994dfae9fa") -- Escape from Duckov
+-- MAIN APP DEPOTS
+addappid(3167021, 1, "6a1377569bff7441f985074b6ed8692c0c8f6681b80a75e020dd1a10419b5dea") -- Depot 3167021
+setManifestid(3167021, "8061703654704935155", 2798208533)
+addappid(3167022, 1, "862fd0e5628b2423c67e71daa4e95147265f746e4f4adf57350eb171f822c616") -- Depot 3167022
+setManifestid(3167022, "1411786298266016599", 2832201192)
