a3567c980cf2b049b24d9c4f3896bba9502e9ca9
qiao-lima <qiao-lima@github.com>
2026-04-14 00:30:04 +0800

    Manual Update 3214610

---
+++ 3214610.lua
@@ -0,0 +1,13 @@
+-- 3214610's Lua and Manifest Created by Morrenus
+-- 灰烬之国 Cinderia
+-- Created: March 30, 2026 at 10:45:13 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3214610) -- 灰烬之国 Cinderia
+addtoken(3214610, "16472338112919955715")
+-- MAIN APP DEPOTS
+addappid(3214611, 1, "11606ac2e0d357064a1b121a23ed7409bcc466ceefa174aa3a08b275753d6115") -- Depot 3214611
+setManifestid(3214611, "6734912344503580750", 9630837741)
