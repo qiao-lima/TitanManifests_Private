a24af037d8241ba6b634eb9d90318bd59b42ecfb
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:12 +0800

    Manual Update 4171090

---
+++ 4171090.lua
@@ -0,0 +1,12 @@
+-- 4171090's Lua and Manifest Created by Morrenus
+-- Freaking Hallway
+-- Created: March 17, 2026 at 18:55:57 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4171090) -- Freaking Hallway
+-- MAIN APP DEPOTS
+addappid(4171091, 1, "8e5192fe58844a56d50bbdbd2fa107103cd553b44721624393cdc1c453676ca4") -- Depot 4171091
+setManifestid(4171091, "5945631686806964793", 937290368)
