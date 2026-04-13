f53793be826bf34c2b1dbbb9cdc007b18e02c2dd
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:23 +0800

    Manual Update 4005370

---
+++ 4005370.lua
@@ -0,0 +1,12 @@
+-- 4005370's Lua and Manifest Created by Morrenus
+-- Maid of Sker VR
+-- Created: March 18, 2026 at 09:43:06 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4005370) -- Maid of Sker VR
+-- MAIN APP DEPOTS
+addappid(4005371, 1, "ddcc433bf8c1d5d08883427c36184b0fab7fb0092f6cf7731906814860c6796b") -- Depot 4005371
+setManifestid(4005371, "4336425485193702729", 12295470205)
