00b08b18a19603c0539bab423a78cd38f084b189
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:09 +0800

    Manual Update 4137460

---
+++ 4137460.lua
@@ -0,0 +1,14 @@
+-- 4137460's Lua and Manifest Created by Morrenus
+-- 我的机器人女友
+-- Created: March 26, 2026 at 11:23:40 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4137460) -- 我的机器人女友
+-- MAIN APP DEPOTS
+addappid(4137461, 1, "b65605494b1ea5aa23e1c0fb211124f88141ed1d702a6927d2dd65c583055f06") -- Depot 4137461
+setManifestid(4137461, "3344812251122817627", 34112843457)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4158630) --  - 
