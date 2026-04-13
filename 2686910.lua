bf5ca4290df333aaae37765a0116abe1037b3f03
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:55 +0800

    Manual Update 2686910

---
+++ 2686910.lua
@@ -0,0 +1,18 @@
+-- 2686910's Lua and Manifest Created by Morrenus
+-- Renxia
+-- Created: March 24, 2026 at 04:25:39 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2686910) -- Renxia
+-- MAIN APP DEPOTS
+addappid(2686911, 1, "2b054e3f77878202f7a6f51ee805ac779fbe0d16b8e55520005218d06006835f") -- Depot 2686911
+setManifestid(2686911, "8621766547421898776", 8777458879)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
