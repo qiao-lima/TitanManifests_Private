eb2e97756b27aa742e30c52e18aae599720e32c3
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:21 +0800

    Manual Update 428750

---
+++ 428750.lua
@@ -0,0 +1,31 @@
+-- 428750's Lua and Manifest Created by Morrenus
+-- Neon Chrome
+-- Created: September 30, 2025 at 03:15:49 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 7
+-- Total DLCs: 2
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(428750, 1, "d314e42503ee1204e905786fe21ece5db0edeb5859eeb5b19113baed245dbdcb") -- Neon Chrome
+-- MAIN APP DEPOTS
+addappid(428751, 1, "36a7de627692cd960ef56153846ea243de884e56de4ca6fb0cf5a90a753166fe") -- Neon Chrome Windows
+setManifestid(428751, "8579756546736533724", 157389695)
+addappid(428752, 1, "d02825259cc9d4411260356a1523294175fcf5e07d35ef241dbda3d6457291e3") -- Neon Chrome Mac
+setManifestid(428752, "142298954794648705", 225921513)
+addappid(428753, 1, "7f3395501fcf9ad0b8f550e35ef6e26665f50de9ce6955938bc6d67dfcf91489") -- Neon Chrome Linux
+setManifestid(428753, "6027247917930520055", 141563760)
+-- SHARED DEPOTS (from other apps)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- VC 2013 Redist (Shared from App 228980)
+setManifestid(228985, "3966345552745568756", 13699237)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- VC 2015 Redist (Shared from App 228980)
+setManifestid(228986, "8782296191957114623", 29759921)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Neon Chrome - Original Soundtrack (AppID: 453250)
+addappid(453250)
+addappid(453250, 1, "03f46c5f0ee0256f39f871c59f92c88cbd45d72d76881b45003d85517a85da1c") -- Neon Chrome - Original Soundtrack - Neon Chrome Original Soundtrack (453250) Depot
+setManifestid(453250, "351248175688382270", 247161580)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(568850) -- Neon Chrome - Arena
