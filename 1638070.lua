53cbee80f673f77ee448b82b3d9dcb68535d2b7e
qiao-lima <qiao-lima@github.com>
2026-04-13 23:48:24 +0800

    Manual Update 1638070

---
+++ 1638070.lua
@@ -0,0 +1,14 @@
+-- 1638070's Lua and Manifest Created by Morrenus
+-- Fishbowl
+-- Created: April 05, 2026 at 14:21:57 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1638070, 1, "6b98892fd39857a979512a1e544b9547279f6c64ef7a5b2a88e36e6495e026f3") -- Fishbowl
+-- MAIN APP DEPOTS
+addappid(1638071, 1, "3542495949f9c41eb2c3b49a31552edbcb0088ffdbff844b91bfdfff915a657e") -- Depot 1638071
+setManifestid(1638071, "2469443413411700163", 209291117)
+addappid(1638072, 1, "540d7ce9dc8ceacab544273171cba038f6b72a4de7dcab7b7afbf0bfe084910a") -- Depot 1638072
+setManifestid(1638072, "7561115006622043154", 281915700)
