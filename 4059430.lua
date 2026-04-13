a1496bf87779f9369e593194295827d69e0aefe6
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:49 +0800

    Manual Update 4059430

---
+++ 4059430.lua
@@ -0,0 +1,18 @@
+-- 4059430's Lua and Manifest Created by Morrenus
+-- 束魂
+-- Created: March 31, 2026 at 20:24:46 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4059430) -- 束魂
+-- MAIN APP DEPOTS
+addappid(4059431, 1, "2e2e8fdeaf4402c012c573ca18439fb547ea908aaf6c2fd9d5fe91ccb269da2f") -- Depot 4059431
+setManifestid(4059431, "1637720162312905612", 9995520818)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
