832e1cbb69d99499e71f9685ac9b46dc9f73f270
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:09 +0800

    Manual Update 4489770

---
+++ 4489770.lua
@@ -0,0 +1,12 @@
+-- 4489770's Lua and Manifest Created by Morrenus
+-- Orbitmine
+-- Created: April 07, 2026 at 10:57:02 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4489770, 1, "fd4f56e5bac1683a754f62bfeefefbbab354f2b42c7bffa0e492b6d5f1b631ad") -- Orbitmine
+-- MAIN APP DEPOTS
+addappid(4489771, 1, "a96744aa30d239a045704ce6bb797ca7d946be3c88b54beb8fc2ef25c5adcf7c") -- Depot 4489771
+setManifestid(4489771, "7011058035726957258", 149567576)
