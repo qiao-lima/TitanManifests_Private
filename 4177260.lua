151c041b0ebb2bca492ad3ae85ae4d8e7a6f46b2
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:13 +0800

    Manual Update 4177260

---
+++ 4177260.lua
@@ -0,0 +1,12 @@
+-- 4177260's Lua and Manifest Created by Morrenus
+-- Jumping Robo
+-- Created: April 11, 2026 at 17:28:28 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4177260) -- Jumping Robo
+-- MAIN APP DEPOTS
+addappid(4177261, 1, "ea0f94de0596c016a25e6eb14c24e0c43265e05495ffbf95d238179c1fbaa43f") -- Depot 4177261
+setManifestid(4177261, "4233352995399545313", 142499292)
