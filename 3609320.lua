f5c9587c60ce76a15bc06c83aa3106327e2d5cf1
qiao-lima <qiao-lima@github.com>
2026-04-14 00:41:11 +0800

    Manual Update 3609320

---
+++ 3609320.lua
@@ -0,0 +1,14 @@
+-- 3609320's Lua and Manifest Created by Morrenus
+-- Bombun
+-- Created: March 28, 2026 at 22:25:58 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3609320, 1, "bca76af15ec9c6aa6cdfce0bb065156ec3c9c84e970cd4615e1ad4dd26249bb4") -- Bombun
+-- MAIN APP DEPOTS
+addappid(3609321, 1, "7d468f4505ada1acfd9e3fccddebbc1df14b07093003ea202c35302e2e053261") -- Depot 3609321
+setManifestid(3609321, "9184712554358617127", 893718560)
+addappid(3609322, 1, "4f7c8f731a8bbcb3a90d416b7a4de970aa7dd2f0b86af88c0d5a01df65d89e7a") -- Depot 3609322
+setManifestid(3609322, "285395565849807291", 894028048)
