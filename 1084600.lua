f6eaf8a5b62c58afc9be289a4b16d94a5a3a0cfd
qiao-lima <qiao-lima@github.com>
2026-04-13 23:36:16 +0800

    Manual Update 1084600

---
+++ 1084600.lua
@@ -0,0 +1,40 @@
+-- 1084600's Lua and Manifest Created by Morrenus
+-- My Time at Sandrock
+-- Created: September 30, 2025 at 02:38:33 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 5
+-- Total DLCs: 15
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1084600) -- My Time at Sandrock
+-- MAIN APP DEPOTS
+addappid(1084602, 1, "262b20ffd5fbd41eb4e6993b44dba982695731cf82c7f787236a1bc1b82109aa") -- Depot 1084602
+setManifestid(1084602, "5014485731095115828", 9718865034)
+addappid(1084601, 1, "7af65c0378d727ff872dcf1bfc511faa6bda56ddb31f4d891ba88a15e5540d17") -- Depot 1084601
+setManifestid(1084601, "6033883375651283552", 29929992527)
+addappid(1084603, 1, "5298981b009169e714731cbf957866f073237c264807e27cc5ddc061d41efb5d") -- Depot 1084603
+setManifestid(1084603, "6318924155206180160", 29929992607)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITH DEDICATED DEPOTS
+-- My Time at Sandrock - Digital Artbook (AppID: 2182080)
+addappid(2182080)
+addappid(2182080, 1, "8052cc32bd4cd71ff86ef8c4db68c2dc5ab50884feea0cc90db814a73eb3dcf7") -- My Time at Sandrock - Digital Artbook - Depot 2182080
+setManifestid(2182080, "6000569290448241032", 324143382)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2181522) -- My Time at Sandrock - Builders Beach and Ball Clothing Pack
+addappid(2181528) -- My Time at Sandrock - Summer Heat Collection 1
+addappid(2595600) -- My Time at Sandrock - Summer Heat Collection 2
+addappid(2595610) -- My Time at Sandrock - Summer Heat Collection 3
+addappid(2595620) -- My Time at Sandrock - Starlight Collection 1
+addappid(2595710) -- My Time at Sandrock - Starlight Collection 2
+addappid(2595720) -- My Time at Sandrock - Starlight Collection 3
+addappid(2595730) -- My Time at Sandrock - Pet Pack Lil Gecko
+addappid(2595740) -- My Time at Sandrock - Interior Decorator Pack
+addappid(2595750) -- My Time at Sandrock - Far East Furniture Pack
+addappid(2595760) -- My Time at Sandrock - Cute Critter Furniture Pack
+addappid(2622210) -- My Time at Sandrock - Alienware Gift
+addappid(2808740) -- My Time at Sandrock - Monster Whisperer
+addappid(2969170) -- My Time at Sandrock - Love Chronicles
