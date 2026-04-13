74bebfc61bc054b5966699734c81cf848d86906b
qiao-lima <qiao-lima@github.com>
2026-04-14 02:53:07 +0800

    Manual Update 981890

---
+++ 981890.lua
@@ -0,0 +1,18 @@
+-- 981890's Lua and Manifest Created by Morrenus
+-- Olympic Games Tokyo 2020 – The Official Video Game™
+-- Created: September 30, 2025 at 04:36:57 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(981890) -- Olympic Games Tokyo 2020 – The Official Video Game™
+-- MAIN APP DEPOTS
+addappid(981891, 1, "9da779d82b2a62043e5fef763724f86270c7b14295c5544faa0bb130debd91c4") -- Musashi Content
+setManifestid(981891, "2165330610565540031", 14792164968)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
