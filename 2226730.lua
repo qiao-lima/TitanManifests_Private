d27233ef8fe45141db7f7fea8ccb304c60b59dbd
qiao-lima <qiao-lima@github.com>
2026-04-14 00:06:14 +0800

    Manual Update 2226730

---
+++ 2226730.lua
@@ -0,0 +1,18 @@
+-- 2226730's Lua and Manifest Created by Morrenus
+-- OVIS LOOP
+-- Created: April 06, 2026 at 23:32:05 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2226730, 1, "81141592002d86d4a9edd1603e7c97ce64bbaffffcb3f0fb824ee20709f84960") -- OVIS LOOP
+-- MAIN APP DEPOTS
+addappid(2226731, 1, "e024ff155fb9f3ce8e387c1c4d4fb860d5f5959d17c8d99d6edbd005e25816a4") -- Depot 2226731
+setManifestid(2226731, "5879385133791402696", 543334469)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
