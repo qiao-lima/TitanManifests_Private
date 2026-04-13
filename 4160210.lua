2e385029985755a729697898d0efe73fc425eb4f
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:10 +0800

    Manual Update 4160210

---
+++ 4160210.lua
@@ -0,0 +1,18 @@
+-- 4160210's Lua and Manifest Created by Morrenus
+-- The Artisan of Glimmith
+-- Created: March 21, 2026 at 05:54:12 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4160210) -- The Artisan of Glimmith
+-- MAIN APP DEPOTS
+addappid(4160211, 1, "71ac392c49f1d69a9641226b81a353c6eb51604e03a0e45f96ac4fe8c88317ba") -- Depot 4160211
+setManifestid(4160211, "7007857492841797183", 2051012392)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
