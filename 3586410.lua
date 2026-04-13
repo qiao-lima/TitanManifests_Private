767163f46860ba849f19d8be73b498241ee816fe
qiao-lima <qiao-lima@github.com>
2026-04-14 00:41:11 +0800

    Manual Update 3586410

---
+++ 3586410.lua
@@ -0,0 +1,18 @@
+-- 3586410's Lua and Manifest Created by Morrenus
+-- 怪獸分解公司
+-- Created: March 17, 2026 at 00:53:15 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3586410) -- 怪獸分解公司
+-- MAIN APP DEPOTS
+addappid(3586411, 1, "9d1c081a569f04925b886d4495b9dd05cfe3fb8cb375f1066934adcb84d2ae9e") -- Depot 3586411
+setManifestid(3586411, "8410934995265663773", 3380676266)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
