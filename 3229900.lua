fffb600552c07fc08bb7696878ff20ed9d598dec
qiao-lima <qiao-lima@github.com>
2026-04-14 00:30:07 +0800

    Manual Update 3229900

---
+++ 3229900.lua
@@ -0,0 +1,12 @@
+-- 3229900's Lua and Manifest Created by Morrenus
+-- Asuka Idol Journey
+-- Created: March 17, 2026 at 18:56:54 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3229900) -- Asuka Idol Journey
+-- MAIN APP DEPOTS
+addappid(3229901, 1, "2ba52979c0afbe7225da6c66c88b55fbc4f1f1dc45d19c6653bd2c1b437b96db") -- Depot 3229901
+setManifestid(3229901, "4271490640376193340", 1264910450)
