d00089431cae3a76162641a5e2ad1c62f158e3fa
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:53 +0800

    Manual Update 4118810

---
+++ 4118810.lua
@@ -0,0 +1,12 @@
+-- 4118810's Lua and Manifest Created by Morrenus
+-- VirtualTies ヰ世界情緒夢想曲
+-- Created: March 19, 2026 at 07:39:40 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4118810) -- VirtualTies ヰ世界情緒夢想曲
+-- MAIN APP DEPOTS
+addappid(4118811, 1, "1aa2256b0f1fba560c84c7aef0e28524f021755a33ab3fdb74243f2994fb9ded") -- Depot 4118811
+setManifestid(4118811, "3603633602755281659", 2707120740)
