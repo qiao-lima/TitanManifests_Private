0fabd487eb1c9213538690240b1c1c83aadeb868
qiao-lima <qiao-lima@github.com>
2026-04-14 00:40:32 +0800

    Manual Update 3516860

---
+++ 3516860.lua
@@ -0,0 +1,18 @@
+-- 3516860's Lua and Manifest Created by Morrenus
+-- Lab 47
+-- Created: March 17, 2026 at 18:54:22 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3516860) -- Lab 47
+-- MAIN APP DEPOTS
+addappid(3516861, 1, "dc4a3c0fcdf132c4a9005782d702e4469c710fa5835cc44ae72e123e61949d03") -- Depot 3516861
+setManifestid(3516861, "1004167809633893797", 3630329030)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
