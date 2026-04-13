12ad83eef7beb65a5e64756d456bd411c492f755
qiao-lima <qiao-lima@github.com>
2026-04-14 00:40:33 +0800

    Manual Update 3520420

---
+++ 3520420.lua
@@ -0,0 +1,12 @@
+-- 3520420's Lua and Manifest Created by Morrenus
+-- Nullpoint Protocol
+-- Created: April 06, 2026 at 18:13:20 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3520420) -- Nullpoint Protocol
+-- MAIN APP DEPOTS
+addappid(3520421, 1, "fe2837c8d5a6a3170c6d6935ccec79fe2d6bfb7f4614d1315be4c0448f10bfca") -- Depot 3520421
+setManifestid(3520421, "6923671962839575583", 5466885886)
