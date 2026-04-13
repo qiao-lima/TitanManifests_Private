3457697afec4f47077b567817426526da3b07900
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:01 +0800

    Manual Update 4275090

---
+++ 4275090.lua
@@ -0,0 +1,18 @@
+-- 4275090's Lua and Manifest Created by Morrenus
+-- Wittengrad is no more
+-- Created: April 09, 2026 at 02:53:55 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4275090) -- Wittengrad is no more
+-- MAIN APP DEPOTS
+addappid(4275091, 1, "fa40f104db11768b298b15acbb402342fa2abf4100f615cf134cc3fc3f58f80f") -- Depot 4275091
+setManifestid(4275091, "3868674942198005238", 1266076049)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
