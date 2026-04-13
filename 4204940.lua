2a75cd561be89d11e896154eb8d9e715512842c6
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:38 +0800

    Manual Update 4204940

---
+++ 4204940.lua
@@ -0,0 +1,14 @@
+-- 4204940's Lua and Manifest Created by Morrenus
+-- Karigurashi Ren'ai: Living on Borrowed Love
+-- Created: March 26, 2026 at 08:16:58 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4204940) -- Karigurashi Ren'ai: Living on Borrowed Love
+-- MAIN APP DEPOTS
+addappid(4204941, 1, "74338278122b0118b3169fbb0f0b0ec264392a454da15297ffdb8b41ac3e42fc") -- Depot 4204941
+setManifestid(4204941, "6629414229036872191", 452524675)
+addappid(4204943, 1, "bd3d74c9f1a05a9e653cad5456d3684b19a2eb16879e85f23acc87155dba1bfb") -- Depot 4204943
+setManifestid(4204943, "7019489016860400634", 364544)
