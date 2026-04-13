e963630c291525bcb06c90e345d193681363d20a
qiao-lima <qiao-lima@github.com>
2026-04-14 00:41:12 +0800

    Manual Update 3634520

---
+++ 3634520.lua
@@ -0,0 +1,23 @@
+-- 3634520's Lua and Manifest Created by Morrenus
+-- Samson
+-- Created: April 08, 2026 at 09:03:04 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 0 (1 excluded)
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3634520) -- Samson
+-- MAIN APP DEPOTS
+addappid(3634521, 1, "2b2dd4cc3b87b9bb23562b460165aa44365a3d32cb7eb75081c38048e36382df") -- Depot 3634521
+setManifestid(3634521, "2980966909922762300", 13207977054)
+addappid(3634522, 1, "53d49f2257c96df0765ec675b69b9dddd47da7763f5d708a13bd0cafb21006d7") -- Depot 3634522
+setManifestid(3634522, "558008199131345330", 791087825)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- EXCLUDED DLCS:
+-- UNRELEASED DLCS (COMMENTED OUT)
+-- addappid(4540000) -- DLC 4540000 (unreleased)
