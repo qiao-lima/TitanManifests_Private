a81733db48617666cfd87d2635f21baf26b20765
qiao-lima <qiao-lima@github.com>
2026-04-14 00:17:52 +0800

    Manual Update 2821610

---
+++ 2821610.lua
@@ -0,0 +1,18 @@
+-- 2821610's Lua and Manifest Created by Morrenus
+-- Echoes of the End: Enhanced Edition
+-- Created: March 06, 2026 at 12:58:35 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2821610) -- Echoes of the End: Enhanced Edition
+-- MAIN APP DEPOTS
+addappid(2821611, 1, "9d26f14b9a968c26e106af6e03b8738a5038d34a341d6dd6c68ea51cfbd25985") -- Depot 2821611
+setManifestid(2821611, "4519338590456177265", 50847077817)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
