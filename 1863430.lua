cb8cdf05a44a85e9fbd55df59bee1a4b8f710f8d
qiao-lima <qiao-lima@github.com>
2026-04-13 23:52:43 +0800

    Manual Update 1863430

---
+++ 1863430.lua
@@ -0,0 +1,25 @@
+-- 1863430's Lua and Manifest Created by Morrenus
+-- Dragonkin: The Banished
+-- Created: March 23, 2026 at 08:15:45 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 6
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1863430, 1, "de70eb391abaca321b8c4b5102031f3f9276c7e044f57db1c6f0ac6db5f0ed15") -- Dragonkin: The Banished
+-- MAIN APP DEPOTS
+addappid(1863431, 1, "328f355333eea5102d7e3e43ead832f03f1005e6b29a00b563799c6db5148602") -- Depot 1863431
+setManifestid(1863431, "2728671138200449761", 40459017299)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3462450) -- Dragonkin The Banished - Founder Pack 1
+addappid(3594840) -- Dragonkin The Banished - Founder Pack 2
+addappid(3594850) -- Dragonkin The Banished - Founder Pack 3
+addappid(3594860) -- Dragonkin The Banished - Founder Pack 4
+addappid(4130000) -- Dragonkin The Banished - Lord Pack
+addappid(4508200) -- Dragonkin The Banished - Light of the First Praise
