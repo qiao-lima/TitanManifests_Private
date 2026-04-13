2035c5707a4e82540d4551d7c602020fc4892c1c
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:22 +0800

    Manual Update 4000090

---
+++ 4000090.lua
@@ -0,0 +1,12 @@
+-- 4000090's Lua and Manifest Created by Morrenus
+-- 颈椎病 Stiff Neck
+-- Created: April 02, 2026 at 23:38:01 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4000090) -- 颈椎病 Stiff Neck
+-- MAIN APP DEPOTS
+addappid(4000091, 1, "51546e23e8568339c45f4408bb1ac7582952d4a77a4e84e3b3cfa2b57b9ab51d") -- Depot 4000091
+setManifestid(4000091, "7763521070506506219", 718320216)
