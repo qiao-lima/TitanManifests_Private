c34aeed0d5a40386fddb48bb5728397273d08122
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:48 +0800

    Manual Update 4384320

---
+++ 4384320.lua
@@ -0,0 +1,18 @@
+-- 4384320's Lua and Manifest Created by Morrenus
+-- BioTech Labs
+-- Created: March 31, 2026 at 13:26:09 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4384320) -- BioTech Labs
+-- MAIN APP DEPOTS
+addappid(4384321, 1, "7e0068ceb04fa638d798a973fbf0e4fa3c330aa329a386396a98dd7daaf38451") -- Depot 4384321
+setManifestid(4384321, "1637799703025793438", 2202368073)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
