e9ac9fce14300aff9b86465ebfb0688538631eb6
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:26 +0800

    Manual Update 538030

---
+++ 538030.lua
@@ -0,0 +1,12 @@
+-- 538030's Lua and Manifest Created by Morrenus
+-- Xenonauts 2
+-- Created: April 07, 2026 at 17:35:42 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(538030, 1, "3eb5bb3608bcc8bfb7eba2db2c649b7445cd6b5d0bb82425e59f66c8a50b92cd") -- Xenonauts 2
+-- MAIN APP DEPOTS
+addappid(538031, 1, "abd9295b438de55315009b4ca303c5f54b32b21d98b8cb090ff2434392e583b9") -- Xenonauts 2 Content
+setManifestid(538031, "5390755086601142481", 7738862545)
