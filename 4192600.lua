3f2c95e35bf50643f8bb8f281e322f82233635a8
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:36 +0800

    Manual Update 4192600

---
+++ 4192600.lua
@@ -0,0 +1,12 @@
+-- 4192600's Lua and Manifest Created by Morrenus
+-- 最后幸存者的后宫
+-- Created: April 10, 2026 at 05:40:13 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4192600) -- 最后幸存者的后宫
+-- MAIN APP DEPOTS
+addappid(4192601, 1, "14c6e2f773e7a3bad208e46c1866e7fbc203a08098aa99d62f17796f8370c606") -- Depot 4192601
+setManifestid(4192601, "2483093689069825963", 1960782374)
