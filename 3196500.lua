b61b3176fae99c4e29f4dd6c9a44a710e7643aa7
qiao-lima <qiao-lima@github.com>
2026-04-14 00:27:15 +0800

    Manual Update 3196500

---
+++ 3196500.lua
@@ -0,0 +1,32 @@
+-- 3196500's Lua and Manifest Created by Morrenus
+-- Wayblazer Dämmerung
+-- Created: March 09, 2026 at 06:40:20 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 7
+-- Total DLCs: 1
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3196500, 1, "ec8acec04842dc4e1ba2caecdc0625471646ec41b3dc00f94f9b291506656c9b") -- Wayblazer Dämmerung
+-- MAIN APP DEPOTS
+addappid(3196502, 1, "2f7b8bb0953297908c514e6501621b990cf4684fec5022dde0a2a944396b3370") -- Depot 3196502
+setManifestid(3196502, "7315623531447157312", 7666261417)
+addappid(3196503, 1, "2fc80b321c28b6de452e921a131c29f72045ca04c1d4269458f17302ce7fcfbb") -- Depot 3196503
+setManifestid(3196503, "3546691787242636377", 7649833186)
+addappid(3196504, 1, "ae55db744b7fa7192a8353832ffde8f1ba04a9c37ceb5eef5976d64bfa2b23ce") -- Depot 3196504
+setManifestid(3196504, "2977020620902021900", 7647892208)
+-- SHARED DEPOTS (from other apps)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Wayblazer Dmmerung - What If DLC (AppID: 4229510)
+addappid(4229510)
+addappid(4229511, 1, "5348a57482619df0dee805904a048b77efc025dbd73cda233ff067393663736a") -- Wayblazer Dmmerung - What If DLC - Depot 4229511
+setManifestid(4229511, "5360970689527605695", 185374976)
+addappid(4229512, 1, "fcf86ab8a3d7c3e878dbc080cb3b693802bb8849f0b65cfd68c0e6d9a32ccb86") -- Wayblazer Dmmerung - What If DLC - Depot 4229512
+setManifestid(4229512, "6648749751552032304", 183893859)
+addappid(4229513, 1, "4c15b95b44d45f999156670ebbb4f96cab9fcd6d8bf45439c90ff20fc9c2bbd5") -- Wayblazer Dmmerung - What If DLC - Depot 4229513
+setManifestid(4229513, "2835349540434926170", 183893122)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3196501) -- Depot 3196501 (empty depot)
+-- addappid(3196508) -- Depot 3196508 (empty depot)
