f2e3505c300e4fa253df0d7c3d3f90735a96bf9b
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:23 +0800

    Manual Update 4003290

---
+++ 4003290.lua
@@ -0,0 +1,14 @@
+-- 4003290's Lua and Manifest Created by Morrenus
+-- Ninjora Echoes
+-- Created: April 11, 2026 at 20:01:04 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4003290) -- Ninjora Echoes
+-- MAIN APP DEPOTS
+addappid(4003291, 1, "adccedf1c241fce0591fd748abf28da7dc94fb48dd7a5edc11f06d63e993af84") -- Depot 4003291
+setManifestid(4003291, "6892358549309900890", 361141452)
+addappid(4003292, 1, "a656188eba315d17372da3faf954c4e1b271280580b31caadc3146acf2358513") -- Depot 4003292
+setManifestid(4003292, "2286264774946420782", 446116934)
