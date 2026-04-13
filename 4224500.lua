19691433b8547b5222fa2e9fc2f95a5c378f5431
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:57 +0800

    Manual Update 4224500

---
+++ 4224500.lua
@@ -0,0 +1,12 @@
+-- 4224500's Lua and Manifest Created by Morrenus
+-- Pupple Pop
+-- Created: April 08, 2026 at 06:07:44 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4224500) -- Pupple Pop
+-- MAIN APP DEPOTS
+addappid(4224501, 1, "38da0f3b5f0cb39dd5b77b150d6ab3f816353964f219c7fa4e5ecd04d4fd58a3") -- Depot 4224501
+setManifestid(4224501, "7438592805251362082", 638448486)
