1988f1d5ff8724618dcb5c473f6e25dae19dd8f8
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:54 +0800

    Manual Update 2678830

---
+++ 2678830.lua
@@ -0,0 +1,21 @@
+-- 2678830's Lua and Manifest Created by Morrenus
+-- Halcyon Days at Taoyuan
+-- Created: March 10, 2026 at 11:19:17 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(2678830) -- Halcyon Days at Taoyuan
+-- MAIN APP DEPOTS
+addappid(2678831, 1, "b8914f851e3c92fffc62f0b8e8b4e4f60dd23e298942554f1d64af42dbefcec7") -- Depot 2678831
+setManifestid(2678831, "8051737984888736959", 1447495144)
+-- DLCS WITH DEDICATED DEPOTS
+-- Halcyon Days at Taoyuan - Golden Snake Pack (AppID: 4327360)
+addappid(4327360)
+addappid(4327360, 1, "08560971852a825ee16b3691d58db0915131c0cf007feccfbf63c7a9c3217be0") -- Halcyon Days at Taoyuan - Golden Snake Pack - Depot 4327360
+setManifestid(4327360, "4211766581588235259", 0)
+-- Halcyon Days at Taoyuan - Galloping Horse Pack (AppID: 4391750)
+addappid(4391750)
+addappid(4391750, 1, "d5e5f3bec06e63e28defa0269c1b57337b7618c353b061383444ad48d2da106b") -- Halcyon Days at Taoyuan - Galloping Horse Pack - Depot 4391750
+setManifestid(4391750, "6230238512074826080", 0)
