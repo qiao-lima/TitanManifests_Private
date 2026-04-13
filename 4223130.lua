ea01f0a3d9eb9640892201c738110a5c59068f8b
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:56 +0800

    Manual Update 4223130

---
+++ 4223130.lua
@@ -0,0 +1,25 @@
+-- 4223130's Lua and Manifest Created by Morrenus
+-- Lilac
+-- Created: March 31, 2026 at 06:24:31 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 2
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(4223130) -- Lilac
+-- MAIN APP DEPOTS
+addappid(4223131, 1, "a1ec3a6939520e81120b1c61a4273eae9472f51a30b352513f468b0a0a4fd7fc") -- Depot 4223131
+setManifestid(4223131, "277640994199721975", 432321319)
+-- SHARED DEPOTS (from other apps)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Lilac -side Witch- (AppID: 4223150)
+addappid(4223150)
+addappid(4223150, 1, "bd4c7027e555b310c935e20214f38bc641aca7947b4e197b0bc7663863a89e85") -- Lilac -side Witch- - Depot 4223150
+setManifestid(4223150, "6926368152095247739", 3117706929)
+-- Lilac -side Wizard- (AppID: 4223160)
+addappid(4223160)
+addappid(4223160, 1, "87bbf25bf9c7c481d57feef47199af045c35c40b0012f4b9ef472f6f2d745b50") -- Lilac -side Wizard- - Depot 4223160
+setManifestid(4223160, "7615743146563309537", 3080086711)
