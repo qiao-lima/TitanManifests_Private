e4b19453dac933ee74c0f4254785eb75a88aac08
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:52 +0800

    Manual Update 4105080

---
+++ 4105080.lua
@@ -0,0 +1,12 @@
+-- 4105080's Lua and Manifest Created by Morrenus
+-- 贝如塔按摩店模拟器 Beta Massage Parlor Simulator
+-- Created: April 05, 2026 at 19:05:44 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4105080) -- 贝如塔按摩店模拟器 Beta Massage Parlor Simulator
+-- MAIN APP DEPOTS
+addappid(4105081, 1, "aae3bf920ef3efbfa8477c4ee6a2fca0b14d41da325ae2a732fc3610b712226f") -- Depot 4105081
+setManifestid(4105081, "8015359784863603513", 4351149785)
