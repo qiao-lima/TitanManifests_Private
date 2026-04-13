cf13bb498053fcab1c6c19164f7dc31a3a117ce3
qiao-lima <qiao-lima@github.com>
2026-04-14 00:02:19 +0800

    Manual Update 2075070

---
+++ 2075070.lua
@@ -0,0 +1,16 @@
+-- 2075070's Lua and Manifest Created by Morrenus
+-- KinitoPET
+-- Created: November 05, 2025 at 12:06:00 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2075070, 1, "9860637585ddc50c2b3e7e3850349b891d66c4011ac21de79c64b10d746722d1") -- KinitoPET
+-- MAIN APP DEPOTS
+addappid(2075071, 1, "add3aec9cb7617a6460aaafda50ed1df8a1529fe3b88cd858796ace96aaefce6") -- Depot 2075071
+setManifestid(2075071, "1248367819829409595", 837764504)
+-- SHARED DEPOTS (from other apps)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") -- .NET 4.7 Redist (Shared from App 228980)
+setManifestid(229006, "1784011429307107530", 83944258)
