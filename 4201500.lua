1aac7bc8abc3dff2f6ffe5f891869e73afbc1193
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:37 +0800

    Manual Update 4201500

---
+++ 4201500.lua
@@ -0,0 +1,12 @@
+-- 4201500's Lua and Manifest Created by Morrenus
+-- Ba Bị
+-- Created: March 15, 2026 at 12:15:39 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4201500) -- Ba Bị
+-- MAIN APP DEPOTS
+addappid(4201501, 1, "bf2f3f9f1db60f0db625e4071ec08d59f98c0a72a2aa0e9ab93557af478636cf") -- Depot 4201501
+setManifestid(4201501, "5579010821603709146", 493222160)
