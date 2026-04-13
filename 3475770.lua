6c70d121435130e8ed54ca43e0955db27006292c
qiao-lima <qiao-lima@github.com>
2026-04-14 00:36:55 +0800

    Manual Update 3475770

---
+++ 3475770.lua
@@ -0,0 +1,12 @@
+-- 3475770's Lua and Manifest Created by Morrenus
+-- 元卡牌之背包传说 Meta Card Backpack Legend
+-- Created: March 21, 2026 at 16:01:08 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3475770) -- 元卡牌之背包传说 Meta Card Backpack Legend
+-- MAIN APP DEPOTS
+addappid(3475771, 1, "f0448367275b347bc2e4d5003aebe8066e2fc58e211d56e55c5639afae9f6888") -- Depot 3475771
+setManifestid(3475771, "4451876690118825761", 5911147401)
