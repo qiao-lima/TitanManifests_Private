e5e5e37e21c83eeb1b8f5fa43598ad8b016e6af5
qiao-lima <qiao-lima@github.com>
2026-04-14 00:27:10 +0800

    Manual Update 3143290

---
+++ 3143290.lua
@@ -0,0 +1,18 @@
+-- 3143290's Lua and Manifest Created by Morrenus
+-- Making*Lovers: First Blush
+-- Created: April 11, 2026 at 20:00:51 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3143290) -- Making*Lovers: First Blush
+-- MAIN APP DEPOTS
+addappid(3143291, 1, "c207ae68bbb47adaf03358932e4ea2f89e60a983cc25ea6dcf8314403fe05df9") -- Depot 3143291
+setManifestid(3143291, "1010694278971643089", 1737436325)
+addappid(3143292, 1, "3c7dd04e9c64ee12c853d02c21835510311426ef8ea85591ddb3dc04d411c16b") -- Depot 3143292
+setManifestid(3143292, "5849748291218081077", 1726014537)
+addappid(3143293, 1, "685d1b996792d6a86a29276d79badc2110b2783af970a1c006de719b27a2fea2") -- Depot 3143293
+setManifestid(3143293, "4059238938666936408", 1674534550)
+addappid(3143294, 1, "0e930eec9e36d2778cc294e6c231b97b8364a680395571b018b4d81d5c7eef0c") -- Depot 3143294
+setManifestid(3143294, "1677743060261467643", 11264)
