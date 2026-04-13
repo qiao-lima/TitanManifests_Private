c334d3f0556b66d897500dbe4ced9164853d3d03
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:01 +0800

    Manual Update 4262610

---
+++ 4262610.lua
@@ -0,0 +1,12 @@
+-- 4262610's Lua and Manifest Created by Morrenus
+-- Wannabe Galgame God!!!
+-- Created: April 10, 2026 at 05:27:19 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4262610) -- Wannabe Galgame God!!!
+-- MAIN APP DEPOTS
+addappid(4262611, 1, "9c1f4e9bdc166074d8a27794e99cc11e5eeae79dd4ea22add79e766081c81575") -- Depot 4262611
+setManifestid(4262611, "7808652563487107255", 390843119)
