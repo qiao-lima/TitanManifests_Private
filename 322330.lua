d10dc37dee43eb22b993ea2ae09ec1cab41f590f
qiao-lima <qiao-lima@github.com>
2026-04-14 00:30:06 +0800

    Manual Update 322330

---
+++ 322330.lua
@@ -0,0 +1,75 @@
+-- 322330's Lua and Manifest Created by Morrenus
+-- Don't Starve Together
+-- Created: April 02, 2026 at 00:03:03 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 6
+-- Total DLCs: 49
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(322330, 1, "c947e0b06f2977593e083d4a0b02bac9de29ab99d49478d07dabef37265f2bf8") -- Don't Starve Together
+addtoken(322330, "4367778632350439069")
+-- MAIN APP DEPOTS
+addappid(322331, 1, "3ee94d4c47554fc580de37cdeb4bb41020ece2d5ab195d51d8b87f5e438e9f23") -- Don't Starve Together - Windows
+setManifestid(322331, "6245651240936792068", 4320010930)
+addappid(322332, 1, "8f36286253d1b2e61f2e7bfec57510d6ec34da84a9d126dc4adf19f6cff117be") -- Don't Starve Together - Linux
+setManifestid(322332, "3449297281910006679", 4200431818)
+addappid(322333, 1, "065e9fc3141e45c895786254045560d2af8860c513d0a6077fceaca5fda4c32f") -- Don't Starve Together - OSX
+setManifestid(322333, "5049556345642426694", 4179224344)
+-- SHARED DEPOTS (from other apps)
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") -- VC 2008 Redist (Shared from App 228980)
+setManifestid(228982, "6413394087650432851", 9688647)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(798740) -- Dont Starve Together Beating Heart Chest
+addappid(798750) -- Dont Starve Together Original Verdant Chest
+addappid(840750) -- Dont Starve Together Victorian Belongings Chest
+addappid(840760) -- Dont Starve Together Original Survivors Victorian Chest
+addappid(840770) -- Dont Starve Together Hallowed Nights Survivors Chest
+addappid(962960) -- Dont Starve Together Hallowed Nights Belongings Chest
+addappid(974740) -- Dont Starve Together All Survivors Magmatic Chest
+addappid(981700) -- Dont Starve Together All Survivors Gladiator Chest
+addappid(981701) -- Dont Starve Together Forge Weapons Chest
+addappid(981702) -- Dont Starve Together Forge Armor Chest
+addappid(987340) -- Dont Starve Together Snowfallen Survivors Chest
+addappid(1051700) -- Dont Starve Together Wortox Deluxe Chest
+addappid(1084430) -- Dont Starve Together Wormwood Deluxe Chest
+addappid(1167330) -- Dont Starve Together Wurt Deluxe Chest
+addappid(1167510) -- Dont Starve Together Hallowed Nights Survivors Chest, Part II
+addappid(1172860) -- Dont Starve Together Starter Pack 2019
+addappid(1205500) -- Dont Starve Together Merrymaker Survivors Chest
+addappid(1205501) -- Dont Starve Together Merrymaker Belongings Chest
+addappid(1230830) -- Dont Starve Together Bounteous Bundle
+addappid(1272440) -- Dont Starve Together Starter Pack 2020
+addappid(1289760) -- Dont Starve Together Spring Cleaning Bundle
+addappid(1338540) -- Dont Starve Together Walter Deluxe Wardrobe
+addappid(1447360) -- Dont Starve Together Hallowed Nights Survivors Chest, Part III
+addappid(1485060) -- Dont Starve Together Snowfallen Survivors Chest, Part II
+addappid(1522880) -- Dont Starve Together Victorian Antiques Chest
+addappid(1558300) -- Dont Starve Together Latecomers Victorian Chest
+addappid(1612410) -- Dont Starve Together Cottage Cache Chest
+addappid(1694780) -- Dont Starve Together Seaside Chest
+addappid(1698560) -- Dont Starve Together Fresh Verdant Chest
+addappid(1710240) -- Dont Starve Together Starter Pack 2021
+addappid(1710241) -- Dont Starve Together Wanda Deluxe Chest
+addappid(1836630) -- Dont Starve Together Merrymaker Survivors Chest, Part II
+addappid(1874370) -- Dont Starve Together Fantasmical Chest
+addappid(1899050) -- Dont Starve Together Gothic Belongings Chest
+addappid(2007370) -- Dont Starve Together Midsummer Cawnival Chest
+addappid(2058280) -- Dont Starve Together Inventors Excursion Chest
+addappid(2058281) -- Dont Starve Together Swashbuckler Survivors Chest
+addappid(2132800) -- Dont Starve Together Moonbound Survivors Chest
+addappid(2170670) -- Dont Starve Together Fantasmical Chest, Part II
+addappid(2170671) -- Dont Starve Together Gothic Belongings Chest, Part II
+addappid(2236180) -- Dont Starve Together Snowfallen Survivors Chest, Part III
+addappid(2241030) -- Dont Starve Together Starter Pack 2023
+addappid(2377640) -- Dont Starve Together Blooming Verdant Chest
+addappid(2711510) -- Dont Starve Together Starter Pack 2024
+addappid(2769680) -- Dont Starve Together Merrymaker Survivors Chest, Part III
+addappid(3155070) -- Dont Starve Together Complete Roseate Chest
+addappid(3373360) -- Dont Starve Together Merrymaker Survivors Chest, Part IV
+addappid(3373370) -- Dont Starve Together Starter Pack 2025
+addappid(4211520) -- Dont Starve Together Starter Pack 2026
