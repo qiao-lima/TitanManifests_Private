ed55f27776edbba2229aea687d988de342419401
qiao-lima <qiao-lima@github.com>
2026-04-14 00:02:23 +0800

    Manual Update 2155180

---
+++ 2155180.lua
@@ -0,0 +1,27 @@
+-- 2155180's Lua and Manifest Created by Morrenus
+-- Pioneers of Pagonia
+-- Created: March 11, 2026 at 14:29:11 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 3
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2155180) -- Pioneers of Pagonia
+-- MAIN APP DEPOTS
+addappid(2155181, 1, "d38873bac59e2a12948771b27a94623a3ece822844d75202b42361f61b5b1504") -- Depot 2155181
+setManifestid(2155181, "4790665775963824763", 6157409570)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITH DEDICATED DEPOTS
+-- Pioneers of Pagonia Wallpaper (AppID: 2721120)
+addappid(2721120)
+addappid(2721120, 1, "669357bce75468df0e5d7d4e09d2444ab6dacbc04427ed6fe88482429c099289") -- Pioneers of Pagonia Wallpaper - Depot 2721120
+setManifestid(2721120, "6439517453764568790", 309523080)
+-- Pioneers of Pagonia - Official Guide (AppID: 3322960)
+addappid(3322960)
+addappid(3322960, 1, "346ce58d54bd6101aed396886f8de43ea868f0c24455479ed66d9e321b855ba2") -- Pioneers of Pagonia - Official Guide - Depot 3322960
+setManifestid(3322960, "1244520110520626987", 174772002)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4351430) -- Pioneers of Pagonia - Decorations
