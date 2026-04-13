0a6ccfe3a3154b19e3dbafb3818511bb218bdd1f
qiao-lima <qiao-lima@github.com>
2026-04-14 00:06:17 +0800

    Manual Update 2283330

---
+++ 2283330.lua
@@ -0,0 +1,12 @@
+-- 2283330's Lua and Manifest Created by Morrenus
+-- Military Battlefield
+-- Created: October 03, 2025 at 01:11:36 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2283330) -- Military Battlefield
+-- MAIN APP DEPOTS
+addappid(2283332, 1, "5618a27ee5bc87449fa74d3626bf99e51b7f13b17bbd1c96917bfd37f06d706b") -- Depot 2283332
+setManifestid(2283332, "1844257079261859325", 194734198)
