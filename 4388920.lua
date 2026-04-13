be80cbf89bca578daed45358ff7be366f86e9a5b
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:49 +0800

    Manual Update 4388920

---
+++ 4388920.lua
@@ -0,0 +1,18 @@
+-- 4388920's Lua and Manifest Created by Morrenus
+-- University 33
+-- Created: April 12, 2026 at 17:10:56 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4388920) -- University 33
+-- MAIN APP DEPOTS
+addappid(4388921, 1, "d4ea973120a61c62b39efe8abb1aa4fa443f141a5c8c6caf1a7db69a36e0a395") -- Depot 4388921
+setManifestid(4388921, "5945196852035421382", 2349922034)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
