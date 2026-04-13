49981e77ad6cba984a2b0b9a9e29d3d97e69c9e2
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:08 +0800

    Manual Update 4130990

---
+++ 4130990.lua
@@ -0,0 +1,12 @@
+-- 4130990's Lua and Manifest Created by Morrenus
+-- Tale of Immortal Ring
+-- Created: April 03, 2026 at 18:08:47 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4130990) -- Tale of Immortal Ring
+-- MAIN APP DEPOTS
+addappid(4130991, 1, "7b2da875c1c80cf28d27fd971443b83db4d0df506819ef7dd2a4365508f90ae7") -- Depot 4130991
+setManifestid(4130991, "2108852608029049278", 866775744)
