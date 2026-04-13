08fc448606938b4da5785fb4303d70c4169ca282
qiao-lima <qiao-lima@github.com>
2026-04-13 23:48:19 +0800

    Manual Update 1536090

---
+++ 1536090.lua
@@ -0,0 +1,19 @@
+-- 1536090's Lua and Manifest Created by Morrenus
+-- Echoes of the Plum Grove
+-- Created: March 04, 2026 at 00:20:00 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(1536090) -- Echoes of the Plum Grove
+-- MAIN APP DEPOTS
+addappid(1536091, 1, "3d3857b760b7b5ff52f50f5b402d4413a1e911ba3238c4b9ee9c225792fc32b9") -- Depot 1536091
+setManifestid(1536091, "6975002750020980000", 2222848367)
+addappid(1536093, 1, "69847c5909ab3810e703594a25197aaf86b1650459daa2f33e45c8ce5ba73bd7") -- Depot 1536093
+setManifestid(1536093, "5958677828791820119", 2250687118)
+-- DLCS WITH DEDICATED DEPOTS
+-- Echoes of the Plum Grove - The Art of Echoes of the Plum Grove (AppID: 3373350)
+addappid(3373350)
+addappid(3373351, 1, "eac3a236eb7b7b8a6b5ccc84d8b290c39edfaaf15f7f329b326fc650c6f911d6") -- Echoes of the Plum Grove - The Art of Echoes of the Plum Grove - Depot 3373351
+setManifestid(3373351, "5309438589228101761", 21653318)
