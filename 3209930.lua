2a928d94f3dac7fb7e7c80aa1b57126ab66ad1da
qiao-lima <qiao-lima@github.com>
2026-04-14 00:30:04 +0800

    Manual Update 3209930

---
+++ 3209930.lua
@@ -0,0 +1,12 @@
+-- 3209930's Lua and Manifest Created by Morrenus
+-- 大宋烽烟
+-- Created: April 10, 2026 at 06:34:28 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3209930) -- 大宋烽烟
+-- MAIN APP DEPOTS
+addappid(3209931, 1, "4d4bd263185eb21b814d6e0291eb7701fc0920f0ec4d15c56e7235882b1038c0") -- Depot 3209931
+setManifestid(3209931, "8966465623511077080", 1501467946)
