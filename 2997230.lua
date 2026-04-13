6c01dec4b2e22a32bb018d6c979ebb6a16ccbdb4
qiao-lima <qiao-lima@github.com>
2026-04-14 00:25:33 +0800

    Manual Update 2997230

---
+++ 2997230.lua
@@ -0,0 +1,17 @@
+-- 2997230's Lua and Manifest Created by Morrenus
+-- Planet of Lana II
+-- Created: March 05, 2026 at 13:24:28 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(2997230) -- Planet of Lana II
+-- MAIN APP DEPOTS
+addappid(2997231, 1, "a397e3852d84a423e6087d18c06db8f5a9123a2b20acee6f925c214b021dfd2d") -- Depot 2997231
+setManifestid(2997231, "4871357915864752788", 43354623122)
+-- DLCS WITH DEDICATED DEPOTS
+-- Planet of Lana II - Supporter Pack (AppID: 4349060)
+addappid(4349060)
+addappid(4349060, 1, "3bbe18ac24992eb412d137d3a12eb9405da20f0db5804decada1d8aec939a1fd") -- Planet of Lana II - Supporter Pack - Depot 4349060
+setManifestid(4349060, "5452787470878400762", 117905407)
