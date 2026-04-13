f8faac495a981d0554fed0aeef56c2fd9812fe0f
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:25 +0800

    Manual Update 501620

---
+++ 501620.lua
@@ -0,0 +1,12 @@
+-- 501620's Lua and Manifest Created by Morrenus
+-- CyberLink PowerDirector 15 Ultimate
+-- Created: April 07, 2026 at 22:13:00 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(501620) -- CyberLink PowerDirector 15 Ultimate
+-- MAIN APP DEPOTS
+addappid(501621, 1, "df0ef8cfb54803422d718beb45304989c839c5b052e50dead2d741de2d47e80c") -- CyberLink PowerDirector 15 Ultimate Content
+setManifestid(501621, "598646608365312204", 6333256684)
