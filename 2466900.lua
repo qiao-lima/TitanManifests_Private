9f37dcceccd760eed16f0295056e9d53c20d42a7
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:07 +0800

    Manual Update 2466900

---
+++ 2466900.lua
@@ -0,0 +1,14 @@
+-- 2466900's Lua and Manifest Created by Morrenus
+-- The Posthumous Investigation
+-- Created: April 01, 2026 at 17:00:09 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2466900) -- The Posthumous Investigation
+-- MAIN APP DEPOTS
+addappid(2466902, 1, "b7d607bbba48b7a5f10ab60b4d536c2dcd377a5ae746f7c36c00e19cd4b166eb") -- Depot 2466902
+setManifestid(2466902, "6503084841673632080", 464301464)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2466901) -- Depot 2466901 (empty depot)
