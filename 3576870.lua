3e8a71784550378a85bc300494b08a2671008dd4
qiao-lima <qiao-lima@github.com>
2026-04-14 00:41:10 +0800

    Manual Update 3576870

---
+++ 3576870.lua
@@ -0,0 +1,12 @@
+-- 3576870's Lua and Manifest Created by Morrenus
+-- Blossom: The Seed Of Life
+-- Created: March 15, 2026 at 00:49:49 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3576870) -- Blossom: The Seed Of Life
+-- MAIN APP DEPOTS
+addappid(3576871, 1, "dfd5ee2380a4b33d99aa8350f5d8623e0e9701efa326cdef82ff216fe94e83fb") -- Depot 3576871
+setManifestid(3576871, "2874380951247462375", 1767282715)
