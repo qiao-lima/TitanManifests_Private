6f8431e929e627f85cbc20864cd1e75d5c2b33ed
qiao-lima <qiao-lima@github.com>
2026-04-13 23:58:13 +0800

    Manual Update 1903170

---
+++ 1903170.lua
@@ -0,0 +1,14 @@
+-- 1903170's Lua and Manifest Created by Morrenus
+-- The Quest For Excalibur - Puy Du Fou
+-- Created: April 09, 2026 at 15:56:27 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1903170) -- The Quest For Excalibur - Puy Du Fou
+-- MAIN APP DEPOTS
+addappid(1903171, 1, "8181739502c051ddbd9770ff8410d5fc69a3ef627034c3ba922159f2930a78fa") -- Depot 1903171
+setManifestid(1903171, "734723358552760525", 2674611954)
+addappid(1903172, 1, "5906988c04a093cdde2bc96534a16188ae818f58974d8a5ee94e431672b50c97") -- Depot 1903172
+setManifestid(1903172, "3304584188557950223", 2642927572)
