3b09594885de31648b96611cbb44aca57d1fd0d8
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:26 +0800

    Manual Update 4031520

---
+++ 4031520.lua
@@ -0,0 +1,17 @@
+-- 4031520's Lua and Manifest Created by Morrenus
+-- Sweet Starlight Sisters
+-- Created: March 25, 2026 at 13:03:19 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4031520, 1, "92128cad836dd3d499d03a871532540a5cdfc09c2dbedac16edfff271efdd3ad") -- Sweet Starlight Sisters
+-- MAIN APP DEPOTS
+addappid(4031521, 1, "b02ca9e39ce063cf7d23ab7039f47be486e8ab5ce09da376f38b2bfff42ddb83") -- Depot 4031521
+setManifestid(4031521, "136010417707694374", 1364226471)
+-- DLCS WITH DEDICATED DEPOTS
+-- Sweet Starlight Sisters - Content Expansion Pack (AppID: 4284880)
+addappid(4284880)
+addappid(4284880, 1, "756167bcd872cbd1d9a6e9ca643417366b83f660354abdb906a6c45434285f16") -- Sweet Starlight Sisters - Content Expansion Pack - Depot 4284880
+setManifestid(4284880, "6558204176212309548", 97293535)
