6a4bc3e6b8d880742e1164ae622795e88a37f128
qiao-lima <qiao-lima@github.com>
2026-04-14 00:41:09 +0800

    Manual Update 3564740

---
+++ 3564740.lua
@@ -0,0 +1,12 @@
+-- 3564740's Lua and Manifest Created by Morrenus
+-- Where Winds Meet
+-- Created: March 26, 2026 at 22:19:59 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3564740) -- Where Winds Meet
+-- MAIN APP DEPOTS
+addappid(3564741, 1, "aa3a50f12026e3afcec72d8fc64817a0ed1d8dfe896e16ab4713a04133b5e5b1") -- Depot 3564741
+setManifestid(3564741, "4714159259554593046", 125292329424)
