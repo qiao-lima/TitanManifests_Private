40d55b4975f2fe703c3323ba7f7f12e7dd8e8904
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:22 +0800

    Manual Update 4000980

---
+++ 4000980.lua
@@ -0,0 +1,12 @@
+-- 4000980's Lua and Manifest Created by Morrenus
+-- 第零封情书
+-- Created: February 15, 2026 at 06:36:29 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4000980, 1, "fe7f20f0f93f0707df30c88a625232fa33802b7364209c34a81cc7a939d261a5") -- 第零封情书
+-- MAIN APP DEPOTS
+addappid(4000981, 1, "73099236aaeb8d8e2eb7b42fed1a369d49838ee6c3d9d2f68209800b38eae941") -- Depot 4000981
+setManifestid(4000981, "3590347227989103052", 1126119768)
