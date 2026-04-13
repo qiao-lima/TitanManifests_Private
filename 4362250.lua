16c54c7acacecae0fa1a65ffa0112397bc278722
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:47 +0800

    Manual Update 4362250

---
+++ 4362250.lua
@@ -0,0 +1,18 @@
+-- 4362250's Lua and Manifest Created by Morrenus
+-- DERACE
+-- Created: April 05, 2026 at 18:40:04 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4362250) -- DERACE
+-- MAIN APP DEPOTS
+addappid(4362251, 1, "58279de117b2f6e3c694da3a715d69fe5e582cae97a09972a45c7e79927b3263") -- Depot 4362251
+setManifestid(4362251, "5222555397605407635", 1211655717)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
