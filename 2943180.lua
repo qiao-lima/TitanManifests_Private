b7d9d0bb1ca4891df07264c34085df58b2a4958a
qiao-lima <qiao-lima@github.com>
2026-04-14 00:21:50 +0800

    Manual Update 2943180

---
+++ 2943180.lua
@@ -0,0 +1,12 @@
+-- 2943180's Lua and Manifest Created by Morrenus
+-- Virtual Cottage 2
+-- Created: March 28, 2026 at 08:24:10 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2943180) -- Virtual Cottage 2
+-- MAIN APP DEPOTS
+addappid(2943181, 1, "979c9f42cb86386ffa40de7f30c597023328324420e6b7f16b0b3ac2c3703d0d") -- Depot 2943181
+setManifestid(2943181, "3235445582271834884", 1164784917)
