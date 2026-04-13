29e25d43ed6e61a0cc257708b0aaa83a15616c74
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:46 +0800

    Manual Update 4356500

---
+++ 4356500.lua
@@ -0,0 +1,12 @@
+-- 4356500's Lua and Manifest Created by Morrenus
+-- My Little Someone
+-- Created: April 08, 2026 at 06:30:16 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4356500, 1, "72c52717cb3dd33ff84ce4b7403772e9c88f3071e0dfb5cbc43b366d1e064836") -- My Little Someone
+-- MAIN APP DEPOTS
+addappid(4356501, 1, "b4a25ab3117c003d495aad7bab4b9bfff686ab65daa75ba5ab3b58a4f457ca65") -- Depot 4356501
+setManifestid(4356501, "5141531898119034584", 168430422)
