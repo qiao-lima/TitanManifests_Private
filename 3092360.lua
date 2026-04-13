28c4c39818a6e0b173510300ef51ef53e3a9700d
qiao-lima <qiao-lima@github.com>
2026-04-14 00:27:08 +0800

    Manual Update 3092360

---
+++ 3092360.lua
@@ -0,0 +1,16 @@
+-- 3092360's Lua and Manifest Created by Morrenus
+-- Super Alloy Crush
+-- Created: April 09, 2026 at 11:50:26 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3092360) -- Super Alloy Crush
+-- MAIN APP DEPOTS
+addappid(3092361, 1, "8cb3ddb83369fc877c5baf9c5707f2c23f19611e6b37e3b85ecfc3e7be3d2a74") -- Depot 3092361
+setManifestid(3092361, "3165074004678959786", 3331467122)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4228970) -- Super Alloy Crush - Supporter Pack
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4228970) -- Depot 4228970 (empty depot)
