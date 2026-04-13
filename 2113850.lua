aac5f6f52b71c60d39560f91adf0bc11011d9922
qiao-lima <qiao-lima@github.com>
2026-04-14 00:02:21 +0800

    Manual Update 2113850

---
+++ 2113850.lua
@@ -0,0 +1,28 @@
+-- 2113850's Lua and Manifest Created by Morrenus
+-- Spirit City: Lofi Sessions
+-- Created: March 27, 2026 at 09:10:19 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 5
+-- Total DLCs: 5
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(2113850, 1, "1932694589dd90996efba523baea663403455b1a2ad770252beb90bc752e2b99") -- Spirit City: Lofi Sessions
+-- MAIN APP DEPOTS
+addappid(2113851, 1, "c96341ff6c37bb63b6a46045e820f5c956aeea66a47703fb62c8476f09300ae9") -- Depot 2113851
+setManifestid(2113851, "7779522014318796809", 5157398638)
+addappid(2113852, 1, "554e69a56122534ea8d5ddaab8928c69bcd639fd5ca10425459c1617d2f3f908") -- Depot 2113852
+setManifestid(2113852, "6431481822402220597", 5662481806)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2374850) -- Spirit City Lofi Sessions - Supporter pack
+addappid(3210380) -- Spirit City Lofi Sessions - Cozy Kitchen DLC
+addappid(3349330) -- Spirit City Lofi Sessions - Winter Spirit Cosmetics Pack
+addappid(3815280) -- Spirit City Lofi Sessions - All Aboard DLC
+addappid(4242210) -- Spirit City Lofi Sessions - Enchanted Garden DLC
