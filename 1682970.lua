72286ee8365f7bda2cb3be7b5c36d3952fec6c46
qiao-lima <qiao-lima@github.com>
2026-04-13 23:52:39 +0800

    Manual Update 1682970

---
+++ 1682970.lua
@@ -0,0 +1,20 @@
+-- 1682970's Lua and Manifest Created by Morrenus
+-- Uncrashed : FPV Drone Simulator
+-- Created: March 22, 2026 at 16:16:26 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1682970, 1, "4db6e5901c33c1f06d876d69f5e1e147bb57e75ecfc3e738ec548df55a916c48") -- Uncrashed : FPV Drone Simulator
+-- MAIN APP DEPOTS
+addappid(1682971, 1, "b624e2fc8bd702706ce1a60197150203bca12f0b0b1eb6b19232061dc24b05d2") -- Depot 1682971
+setManifestid(1682971, "7704333438189543043", 19645258810)
+addappid(1682972, 1, "4d25bd53e902ac5aa1bddfcbe2ae13b2f731da7a0115b978803db7786236e649") -- Depot 1682972
+setManifestid(1682972, "3846913878834258285", 21256543561)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
