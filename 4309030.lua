85ec4d740e3820ea480da83de3f21b16dd26eb33
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:25 +0800

    Manual Update 4309030

---
+++ 4309030.lua
@@ -0,0 +1,12 @@
+-- 4309030's Lua and Manifest Created by Morrenus
+-- 错过的暑假可以放第二次吗？
+-- Created: April 06, 2026 at 07:42:05 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4309030, 1, "96200f80c3b85df5db372bfd80b398f68281c8cef36489b32fe2df8d47b6cc2d") -- 错过的暑假可以放第二次吗？
+-- MAIN APP DEPOTS
+addappid(4309031, 1, "5a4321016c2fb3c44d28b772146f53e0733eb362892d805415f3e9573461b6f6") -- Depot 4309031
+setManifestid(4309031, "8993366471510195073", 753162938)
