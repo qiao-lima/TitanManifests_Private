6004a2c03be4bebb593849e52aacb7fe210900e9
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:04 +0800

    Manual Update 3956070

---
+++ 3956070.lua
@@ -0,0 +1,16 @@
+-- 3956070's Lua and Manifest Created by Morrenus
+-- The Succubus Trap Island
+-- Created: April 12, 2026 at 06:20:42 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3956070, 1, "110015b5009ef6322233e9f056c8c75adda2d0c6662bb1bf4e994c250e647616") -- The Succubus Trap Island
+-- MAIN APP DEPOTS
+addappid(3956071, 1, "e43eb2cc9f9982f4106e536b96bfaf8f58f5da26fb71f73b7bbc36ae8a2a1faa") -- Depot 3956071
+setManifestid(3956071, "3636586442064081316", 335131622)
+addappid(3956072, 1, "b7980d5e2995a85203a3168c9b4719a12c278d85e4901527244a51656ea2c588") -- Depot 3956072
+setManifestid(3956072, "116750751056671154", 346488786)
+addappid(3956073, 1, "129794af64f398b2cd08c712ff9b4771cae9bf94f627ceb3ba346eceff51f7df") -- Depot 3956073
+setManifestid(3956073, "3513080271961974575", 335826750)
