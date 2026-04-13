6d803fe6a0df140b3ab8b21e9251e8f176ac3952
qiao-lima <qiao-lima@github.com>
2026-04-14 00:07:53 +0800

    Manual Update 2445590

---
+++ 2445590.lua
@@ -0,0 +1,14 @@
+-- 2445590's Lua and Manifest Created by Morrenus
+-- A Completely Fictional Story About a City Inside a Whale
+-- Created: March 29, 2026 at 13:57:48 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2445590) -- A Completely Fictional Story About a City Inside a Whale
+-- MAIN APP DEPOTS
+addappid(2445591, 1, "3f914eb327c94233accba1e4db2f12beafea4735fd8d3410960ff037ba5b30b3") -- Depot 2445591
+setManifestid(2445591, "3310675704873393936", 1781575585)
+addappid(2445592, 1, "360a4453e47114b5dd285294924016128e9a6865639e825f5b1616b10257344a") -- Depot 2445592
+setManifestid(2445592, "6970215420215224144", 1777321963)
