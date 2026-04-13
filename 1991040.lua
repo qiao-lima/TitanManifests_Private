fd87363053c041f74e25711723e6b58d793762ec
qiao-lima <qiao-lima@github.com>
2026-04-13 23:58:15 +0800

    Manual Update 1991040

---
+++ 1991040.lua
@@ -0,0 +1,21 @@
+-- 1991040's Lua and Manifest Created by Morrenus
+-- 学生时代
+-- Created: February 11, 2026 at 16:38:50 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(1991040, 1, "96c8b9fd0203b756007339afcf9049714caa1a3a9af688f949b066a8655f62e3") -- 学生时代
+-- MAIN APP DEPOTS
+addappid(1991041, 1, "db44c404057a2295a7c0d1dcfcba71f1a9486672998b2012d88d9d1a6edc72b9") -- Depot 1991041
+setManifestid(1991041, "6983690348560690650", 4572225056)
+-- DLCS WITH DEDICATED DEPOTS
+--  -  (AppID: 4110130)
+addappid(4110130)
+addappid(4110130, 1, "4ea0b2fd71c6595e07981f9f4b4f45005795d1c2c91f78ee138012234c519a19") --  -  - Depot 4110130
+setManifestid(4110130, "3691928797666355848", 164528108)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4276880) --  - 
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4110130) -- Depot 4110130 (empty depot)
