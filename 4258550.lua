8b01d72ab463a79862e4a711f51db4e4b39f84c4
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:00 +0800

    Manual Update 4258550

---
+++ 4258550.lua
@@ -0,0 +1,12 @@
+-- 4258550's Lua and Manifest Created by Morrenus
+-- AMEN 11 11
+-- Created: March 15, 2026 at 12:15:54 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4258550) -- AMEN 11 11
+-- MAIN APP DEPOTS
+addappid(4258551, 1, "3bebd0da186c34e33c2263de5835460e343a93e6b71e1cfc86a626fdf8061b82") -- Depot 4258551
+setManifestid(4258551, "2753048752896025698", 1439445773)
