c2b7eacdddd5d66d3ab48ee4ab141b175dff97d0
qiao-lima <qiao-lima@github.com>
2026-04-14 00:32:41 +0800

    Manual Update 3355940

---
+++ 3355940.lua
@@ -0,0 +1,18 @@
+-- 3355940's Lua and Manifest Created by Morrenus
+-- Card Coder
+-- Created: April 06, 2026 at 18:05:44 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3355940) -- Card Coder
+-- MAIN APP DEPOTS
+addappid(3355941, 1, "6d490ecdf91f8b632d9feda6a10f754884cd760182ad9aa1881a8264a3ad4d74") -- Depot 3355941
+setManifestid(3355941, "3489693829134028171", 2521288584)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
