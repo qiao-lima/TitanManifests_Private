6b3b91fbe309ad354b23d54cbaa13c62967fce74
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:33 +0800

    Manual Update 3856120

---
+++ 3856120.lua
@@ -0,0 +1,12 @@
+-- 3856120's Lua and Manifest Created by Morrenus
+-- Sohee
+-- Created: April 02, 2026 at 08:22:31 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3856120) -- Sohee
+-- MAIN APP DEPOTS
+addappid(3856121, 1, "2ff21fa6a4d1d0e8407135ad904c6a874262fadcae894ca88f3d675032221d4a") -- Depot 3856121
+setManifestid(3856121, "6686053600490001826", 2589102415)
