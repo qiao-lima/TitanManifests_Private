8df33d37bd750d726f3de7771f0421eea03e7050
qiao-lima <qiao-lima@github.com>
2026-04-14 00:32:41 +0800

    Manual Update 3362750

---
+++ 3362750.lua
@@ -0,0 +1,16 @@
+-- 3362750's Lua and Manifest Created by Morrenus
+-- Stormbridge
+-- Created: February 20, 2026 at 01:49:21 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3362750) -- Stormbridge
+-- MAIN APP DEPOTS
+addappid(3362751, 1, "ad8f7c810723c11621e18fccd7ecdae177bde913cdc806b421aecd830ab075b1") -- Depot 3362751
+setManifestid(3362751, "2448914854926712445", 704758325)
+addappid(3362752, 1, "36c0a70400b5f10f5cae941aecdf7c60e85cd67ff6ab0bce6878a6591c8f1ed4") -- Depot 3362752
+setManifestid(3362752, "6294292677172751883", 698948310)
+addappid(3362753, 1, "f615fb8ab566272f7c51744d92ca298a872144da9846c4a9305c61cdb08bea09") -- Depot 3362753
+setManifestid(3362753, "419707258605553169", 714439387)
