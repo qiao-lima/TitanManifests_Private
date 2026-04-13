457d660f0eaf07a9443487f045605350fdd7de23
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:27 +0800

    Manual Update 582660

---
+++ 582660.lua
@@ -0,0 +1,38 @@
+-- 582660's Lua and Manifest Created by Morrenus
+-- Black Desert
+-- Created: March 26, 2026 at 10:50:11 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 11
+-- Total DLCs: 3
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(582660, 1, "f2e8854def95b98c4dcafab250efc2fabb51347a9cda47f16ed4a4a57f07a336") -- Black Desert
+-- MAIN APP DEPOTS
+addappid(582661, 1, "cf9f3a203ad68c0c055b195e48a06607901c9c7b0b960bb45ae011d51528a155") -- Black Desert Online Test
+setManifestid(582661, "3297871429683895393", 93338310178)
+addappid(582662, 1, "c1049d5deacc67bb2871b9e237b33ccc8d127bc0b6eb053259a94b0f927c850f") -- Black Desert RU
+setManifestid(582662, "7975167850997671041", 83050621270)
+addappid(582663, 1, "92f1b0eb046931b2d9b3f23273d655d82d3d578307c33f865ea0597b25a49adb") -- Black Desert TH
+setManifestid(582663, "3713599349073397007", 62225602766)
+addappid(582664, 1, "e1a1f69056f20847a921db1840c4aec48bae9df5b03c2caa82afbecfa99d198c") -- Black Desert SEA
+setManifestid(582664, "7105219149815562736", 62189090016)
+addappid(582665, 1, "a9bbd9d29f9cce9c082e50fc2b2ba67a3776755b1d6d008be8f83c28113b3fc1") -- Black Desert TR/MENA
+setManifestid(582665, "3491210605104161738", 83679785135)
+addappid(582666, 1, "fdc5459b7ace070aab239046eaf293dd587c2f92b48d321ae66d2b182a12ec0b") -- Black Desert JP
+setManifestid(582666, "5751486560215706978", 91267342470)
+addappid(582667, 1, "46e6635d93a9ac08a999c41fb7226a837c8b60040ce5cc3ad82a69948a6873a7") -- Black Desert TW
+setManifestid(582667, "5446154394008866098", 95311513119)
+addappid(582668, 1, "e854c123af5b69124d6aef53ff7804f2f026523caac579cfdb3fa9109d2fbc53") -- Black Desert SA
+setManifestid(582668, "3293731478124420027", 84013688033)
+addappid(582669, 1, "8e94093bb1779e7174d0be454b1f6e9b1a3034d9c9c2be90c3101466acf88087") -- Depot 582669
+setManifestid(582669, "4317697721515764735", 94759654158)
+-- SHARED DEPOTS (from other apps)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- VC 2013 Redist (Shared from App 228980)
+setManifestid(228985, "3966345552745568756", 13699237)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- VC 2015 Redist (Shared from App 228980)
+setManifestid(228986, "8782296191957114623", 29759921)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1914120) -- [NAEU] Traveler to Explorer DLC
+addappid(1914121) -- [NAEU] Explorer to Conqueror DLC
+addappid(1914122) -- [NAEU] Traveler to Conqueror DLC
