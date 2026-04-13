10f5464bbc7fe622ac71f695c2df2ffc01f9c23d
qiao-lima <qiao-lima@github.com>
2026-04-14 00:14:11 +0800

    Manual Update 2531310

---
+++ 2531310.lua
@@ -0,0 +1,24 @@
+-- 2531310's Lua and Manifest Created by Morrenus
+-- The Last of Us™ Part II Remastered
+-- Created: January 18, 2026 at 17:18:16 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 5
+-- Total DLCs: 1
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2531310) -- The Last of Us™ Part II Remastered
+-- MAIN APP DEPOTS
+addappid(2531311, 1, "f7506952e736e71547c2d3c2c82959438e836c66d5b8b8f3208ccc082f60df23") -- Depot 2531311
+setManifestid(2531311, "4566212634007989287", 87609292845)
+addappid(2531312, 1, "60bec6c44987ec1a1d91da8bd7b25f4bbb58f6159b7d7ece95e2f107f7d456a4") -- Depot 2531312
+setManifestid(2531312, "666389493307802768", 6894822582)
+addappid(2531313, 1, "a88758211f172f53efb57ad5c819b9d5f34b0796b44bd876639a09029e6a8089") -- Depot 2531313
+setManifestid(2531313, "4994458262959665537", 36274855915)
+addappid(2531314, 1, "3dc1a7d997476f1b4eb3a78f11af1be0d750a14719dc5d5eaeb940bcd44b74e5") -- Depot 2531314
+setManifestid(2531314, "6262322802969244361", 685268757)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3420050) -- The Last of Us Part II Remastered - Pre-purchase Entitlements
