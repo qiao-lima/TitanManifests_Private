041d64ee12b66b9d53b24e75b174f8764212dd89
qiao-lima <qiao-lima@github.com>
2026-04-14 00:17:47 +0800

    Manual Update 2701440

---
+++ 2701440.lua
@@ -0,0 +1,20 @@
+-- 2701440's Lua and Manifest Created by Morrenus
+-- PARANORMASIGHT: The Mermaid's Curse
+-- Created: February 20, 2026 at 09:05:04 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(2701440) -- PARANORMASIGHT: The Mermaid's Curse
+-- MAIN APP DEPOTS
+addappid(2701441, 1, "72344397f0187bfc67a5170dc2e46fa4e1e0f16016d89e43fe8e6572207f1b49") -- Depot 2701441
+setManifestid(2701441, "5081005723971621040", 1018475048)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
