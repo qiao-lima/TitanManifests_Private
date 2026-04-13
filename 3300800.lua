b62d2417e661edcc8d8db8388853acd464956137
qiao-lima <qiao-lima@github.com>
2026-04-14 00:32:39 +0800

    Manual Update 3300800

---
+++ 3300800.lua
@@ -0,0 +1,12 @@
+-- 3300800's Lua and Manifest Created by Morrenus
+-- HER TREES : PUZZLE DREAM
+-- Created: March 07, 2026 at 06:16:46 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3300800) -- HER TREES : PUZZLE DREAM
+-- MAIN APP DEPOTS
+addappid(3300801, 1, "45d71ee75dc90f729bc9ddff90bc98cb8a1819bb8b6bddd03cdf9c0b188d9b73") -- Depot 3300801
+setManifestid(3300801, "577107929580256627", 580656978)
