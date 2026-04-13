d9d178e3b29cdb9b3357dd4cf7096940bd37b32c
qiao-lima <qiao-lima@github.com>
2026-04-14 00:02:21 +0800

    Manual Update 2112740

---
+++ 2112740.lua
@@ -0,0 +1,12 @@
+-- 2112740's Lua and Manifest Created by Morrenus
+-- Humanica
+-- Created: March 09, 2026 at 17:31:36 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2112740) -- Humanica
+-- MAIN APP DEPOTS
+addappid(2112741, 1, "05a239aab771f004b53f5783b779852a87d5647830795cf68b26d61d957dadfa") -- Depot 2112741
+setManifestid(2112741, "1910809580594094762", 349697727)
