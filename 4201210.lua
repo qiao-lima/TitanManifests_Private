b78410e35a203a355fae9bf3812d708dbd5f0af8
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:37 +0800

    Manual Update 4201210

---
+++ 4201210.lua
@@ -0,0 +1,16 @@
+-- 4201210's Lua and Manifest Created by Morrenus
+-- All Me Action Game
+-- Created: April 10, 2026 at 06:36:46 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(4201210) -- All Me Action Game
+-- MAIN APP DEPOTS
+addappid(4201211, 1, "100664ff0faf435924bdfc10b3f94d62ad775222e32f0eac6bc88f02c51ab89a") -- Depot 4201211
+setManifestid(4201211, "348766421003839918", 222898736)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
