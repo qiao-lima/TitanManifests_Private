87ffc5716ad36f18e4848ed42c1d6befbde6117b
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:21 +0800

    Manual Update 4287090

---
+++ 4287090.lua
@@ -0,0 +1,14 @@
+-- 4287090's Lua and Manifest Created by Morrenus
+-- Mahjong Inkflow
+-- Created: April 04, 2026 at 14:22:09 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4287090) -- Mahjong Inkflow
+-- MAIN APP DEPOTS
+addappid(4287091, 1, "bf37117cf9ff9dfe46904d1a3474760fead1a8e3a0c1201979696cee6307e70d") -- Depot 4287091
+setManifestid(4287091, "3262423635211012079", 407759364)
+addappid(4287092, 1, "5492760a56863407f2b9d91229f15a485fb8a9dc12012446204abd49a469c9c2") -- Depot 4287092
+setManifestid(4287092, "8790691201819335473", 375039980)
