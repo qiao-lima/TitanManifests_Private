52992541a28ea51763707f82bb83bd4e7feb9936
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:04 +0800

    Manual Update 3771230

---
+++ 3771230.lua
@@ -0,0 +1,18 @@
+-- 3771230's Lua and Manifest Created by Morrenus
+-- 纸上的魔法使
+-- Created: April 07, 2026 at 20:47:30 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3771230) -- 纸上的魔法使
+-- MAIN APP DEPOTS
+addappid(3771239, 1, "41cdc8f5c8f067a4769c636e01150e22b5c7e13c02714ac7af80fa3d60fd3b77") -- Depot 3771239
+setManifestid(3771239, "2638084087504353748", 3177281470)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
