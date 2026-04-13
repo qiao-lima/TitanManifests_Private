d2139a344d5e7196b5204746c57ce30cfd918865
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:12 +0800

    Manual Update 4172530

---
+++ 4172530.lua
@@ -0,0 +1,22 @@
+-- 4172530's Lua and Manifest Created by Morrenus
+-- Legend of YMIR
+-- Created: April 07, 2026 at 13:35:13 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 3
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4172530) -- Legend of YMIR
+-- MAIN APP DEPOTS
+addappid(4172531, 1, "50f8f7553f9ffaceaebea724c7a40a4b00b76aaec013b3c9fbdd580f2975e07c") -- Depot 4172531
+setManifestid(4172531, "1745946029794252296", 1030595309)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4520020) -- Starter Pack
+addappid(4520030) -- Value Pack
+addappid(4520040) -- Supporter Pack
