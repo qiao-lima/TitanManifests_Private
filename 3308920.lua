a37e7a2a831eece76ba379fcbb3cd8db43e554c1
qiao-lima <qiao-lima@github.com>
2026-04-14 00:32:40 +0800

    Manual Update 3308920

---
+++ 3308920.lua
@@ -0,0 +1,16 @@
+-- 3308920's Lua and Manifest Created by Morrenus
+-- Depths of Endor: Dungeon Crawler
+-- Created: April 03, 2026 at 11:04:31 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3308920) -- Depths of Endor: Dungeon Crawler
+-- MAIN APP DEPOTS
+addappid(3308921, 1, "622a1449d0d5521799c0589500acf67fd947ec42fad1199f9dad8edbbd83df5f") -- Depot 3308921
+setManifestid(3308921, "3883099958368106017", 83071536)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
