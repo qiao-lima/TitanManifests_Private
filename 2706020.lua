a9806250faf5c9c553efcb772848d825d1a64b2f
qiao-lima <qiao-lima@github.com>
2026-04-14 00:17:48 +0800

    Manual Update 2706020

---
+++ 2706020.lua
@@ -0,0 +1,12 @@
+-- 2706020's Lua and Manifest Created by Morrenus
+-- All Will Fall
+-- Created: April 07, 2026 at 14:34:17 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2706020, 1, "1e77dbc1e9853e8b3745dc5f83ea2f91a1a6235c8d318ce3b2d16c5396d90ba5") -- All Will Fall
+-- MAIN APP DEPOTS
+addappid(2706021, 1, "8ca6191f4f09dcb309688901e511599e604be2d09f060774e028dce04bd063e3") -- Depot 2706021
+setManifestid(2706021, "4199666166972114874", 5816769055)
