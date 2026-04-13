9d3564fa56934f992a03f5ff4a2a1f91c2b733f9
qiao-lima <qiao-lima@github.com>
2026-04-13 23:42:56 +0800

    Manual Update 1346840

---
+++ 1346840.lua
@@ -0,0 +1,19 @@
+-- 1346840's Lua and Manifest Created by Morrenus
+-- Dead Reset
+-- Created: February 13, 2026 at 23:28:22 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(1346840) -- Dead Reset
+-- MAIN APP DEPOTS
+addappid(1346841, 1, "97c6131497053c1bb5d67ba18ab299d620ed9102c349d33a39a88f631ffbea41") -- Depot 1346841
+setManifestid(1346841, "4785986818608701145", 14884984455)
+addappid(1346842, 1, "639e6998b8fd3c6504cc09f16dc96c068665e905632e4045daa48aae2f01c32c") -- Depot 1346842
+setManifestid(1346842, "5353980225232955240", 227465735)
+-- DLCS WITH DEDICATED DEPOTS
+-- Dead Reset - 4K Video Pack (AppID: 3496680)
+addappid(3496680)
+addappid(3496680, 1, "19a5899fea50d947201d7200b65cbaf2c7f8bdba26db650310272d1da2be8b44") -- Dead Reset - 4K Video Pack - Depot 3496680
+setManifestid(3496680, "3340273974508078880", 27589721031)
