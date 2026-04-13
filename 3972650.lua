7aedc97c17103a0722c8523e9b28e3a2bb39b7e4
qiao-lima <qiao-lima@github.com>
2026-04-14 02:49:05 +0800

    Manual Update 3972650

---
+++ 3972650.lua
@@ -0,0 +1,12 @@
+-- 3972650's Lua and Manifest Created by Morrenus
+-- 背包闯江湖
+-- Created: April 07, 2026 at 11:29:35 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3972650) -- 背包闯江湖
+-- MAIN APP DEPOTS
+addappid(3972651, 1, "9c9134f309e41446401aba03f4d3e85d8d1d4d1b559779312a4f85a4fc6eeb7b") -- Depot 3972651
+setManifestid(3972651, "8677617551647459602", 1034554825)
