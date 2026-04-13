0426267b09f5f44908729e90e6bc638d68d0ca11
qiao-lima <qiao-lima@github.com>
2026-04-14 00:25:34 +0800

    Manual Update 3005210

---
+++ 3005210.lua
@@ -0,0 +1,12 @@
+-- 3005210's Lua and Manifest Created by Morrenus
+-- The Saint | 聖者 (Seija)
+-- Created: March 31, 2026 at 11:37:25 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3005210) -- The Saint | 聖者 (Seija)
+-- MAIN APP DEPOTS
+addappid(3005211, 1, "ddb549bb19342add97d28c65a57f4564ab80ce036e6adef8837f3136b6762710") -- Depot 3005211
+setManifestid(3005211, "1811386465480463131", 5031929805)
