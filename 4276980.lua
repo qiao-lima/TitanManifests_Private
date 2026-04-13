e738761abeb5a61350e3ff3ad5c240a35da61370
qiao-lima <qiao-lima@github.com>
2026-04-14 02:51:02 +0800

    Manual Update 4276980

---
+++ 4276980.lua
@@ -0,0 +1,14 @@
+-- 4276980's Lua and Manifest Created by Morrenus
+-- Tiny Biomes: Cozy Idle
+-- Created: March 14, 2026 at 21:01:08 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4276980) -- Tiny Biomes: Cozy Idle
+-- MAIN APP DEPOTS
+addappid(4276981, 1, "1363c737173dea2d7345f7e55c8eaeec8f74cb3f869ce4bd5f2b6bb58f5de772") -- Depot 4276981
+setManifestid(4276981, "2287696538345454617", 460236376)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4500180) -- Tiny Biomes Supporter Pack
