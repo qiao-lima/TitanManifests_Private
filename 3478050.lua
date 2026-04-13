53e01ea482a1d070037121a9229bfec7d3fcf649
qiao-lima <qiao-lima@github.com>
2026-04-14 00:36:55 +0800

    Manual Update 3478050

---
+++ 3478050.lua
@@ -0,0 +1,24 @@
+-- 3478050's Lua and Manifest Created by Morrenus
+-- Road to Empress I
+-- Created: February 05, 2026 at 06:10:19 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 1
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3478050) -- Road to Empress I
+-- MAIN APP DEPOTS
+addappid(3478051, 1, "b6dfb81926ab11079fe9a9966f287b28e32479c417877c63847d8d422e12aea9") -- Depot 3478051
+setManifestid(3478051, "1306708397443831371", 5732679084)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITH DEDICATED DEPOTS
+-- Road to Empress Props  Bloopers Expansion Pack (AppID: 3972390)
+addappid(3972390)
+addappid(3972390, 1, "54ce84a9747fb4dadf6076ce0d851004973cc0593493aa0d29e71084f8cd324f") -- Road to Empress Props  Bloopers Expansion Pack - Depot 3972390
+setManifestid(3972390, "4121592045994007525", 2036427)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3478052) -- Depot 3478052 (empty depot)
+-- addappid(3478053) -- Depot 3478053 (empty depot)
