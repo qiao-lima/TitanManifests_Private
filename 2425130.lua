0c6c37a516bdc0307225ac366b91433d56d7976e
qiao-lima <qiao-lima@github.com>
2026-04-14 00:07:52 +0800

    Manual Update 2425130

---
+++ 2425130.lua
@@ -0,0 +1,14 @@
+-- 2425130's Lua and Manifest Created by Morrenus
+-- Hacked: The Streamer
+-- Created: April 06, 2026 at 09:05:43 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2425130) -- Hacked: The Streamer
+-- MAIN APP DEPOTS
+addappid(2425131, 1, "3e164a0f941b3f102eabdb831fc47ce4cdc7b88140b00604f4ab6afd0f03e40a") -- Depot 2425131
+setManifestid(2425131, "3026715713479399839", 19719770533)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2425132) -- Depot 2425132 (empty depot)
