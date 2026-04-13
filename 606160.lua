a0e753ebcce7cf8b4e462f379875fdc78a7a5b3f
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:28 +0800

    Manual Update 606160

---
+++ 606160.lua
@@ -0,0 +1,12 @@
+-- 606160's Lua and Manifest Created by Morrenus
+-- ROUTINE
+-- Created: March 09, 2026 at 19:37:37 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(606160, 1, "368986b48cf7cf15a2876422b0ce68c5f7ec1436badd957b0193db563ca7bb63") -- ROUTINE
+-- MAIN APP DEPOTS
+addappid(606161, 1, "f1cd8fe75a865cf2333d06c14f644b5ef8dd9fc4425aac2a35ae7a75eb4da0b1") -- Depot 606161
+setManifestid(606161, "5082450058729967054", 10883662130)
