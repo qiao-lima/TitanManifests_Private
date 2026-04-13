3d87e96f84a6e47302d310fe0176ae0e50c3c64a
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:37 +0800

    Manual Update 3908930

---
+++ 3908930.lua
@@ -0,0 +1,12 @@
+-- 3908930's Lua and Manifest Created by Morrenus
+-- Eco City Planner
+-- Created: April 06, 2026 at 09:30:10 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3908930) -- Eco City Planner
+-- MAIN APP DEPOTS
+addappid(3908931, 1, "f79137a5aa72fcc9c4f684443b5ab5d66ea132e3ecd40fdfd5d0e40df5e87258") -- Depot 3908931
+setManifestid(3908931, "3157690308200604651", 1541964610)
