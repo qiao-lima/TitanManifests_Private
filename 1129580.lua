8fcd61fbed9a8f8db53fa34dfd26ac8861cdc358
qiao-lima <qiao-lima@github.com>
2026-04-13 23:36:20 +0800

    Manual Update 1129580

---
+++ 1129580.lua
@@ -0,0 +1,38 @@
+-- 1129580's Lua and Manifest Created by Morrenus
+-- Medieval Dynasty
+-- Created: February 19, 2026 at 10:50:00 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 7
+-- Total DLCs: 6
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1129580, 1, "878911f0cb5b2d6d6bf64a1538c3a8445acbf57f498dfd71d1dc045da3a621ab") -- Medieval Dynasty
+-- MAIN APP DEPOTS
+addappid(1129581, 1, "1046c854c37b8fbffc2463ed255b11644eb113572b68ab4d509a5e09a9f47098") -- Medieval Dynasty Content
+setManifestid(1129581, "5507159257848767359", 20787698457)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITH DEDICATED DEPOTS
+-- Medieval Dynasty - Digital Supporter Pack (AppID: 1418980)
+addappid(1418980)
+addappid(1418980, 1, "4725ba16d1f74610293041fa477eedfb35cad844c9c064efb54a7ff031a89e3d") -- Medieval Dynasty - Digital Supporter Pack - Digital Supporter Pack Content
+setManifestid(1418980, "8707615711707866919", 5421331447)
+-- Medieval Dynasty - Official Guide (AppID: 1766020)
+addappid(1766020)
+addappid(1766020, 1, "ca76a7535ac3f0665611290b40894720e2d8ddcfbae6330301f0bf49cef0c64b") -- Medieval Dynasty - Official Guide - Official Guide Content
+setManifestid(1766020, "4528843924608308590", 262446578)
+-- Medieval Dynasty - Official Cookbook (AppID: 1766021)
+addappid(1766021)
+addappid(1766021, 1, "e63723c50d9ecdb7093d2f318c5ce1418817e4fc2148706bba820c94f83de162") -- Medieval Dynasty - Official Cookbook - Official Cook Book Content
+setManifestid(1766021, "7860095987267006881", 52043844)
+-- Medieval Dynasty - Map Pack (AppID: 1766022)
+addappid(1766022)
+addappid(1766022, 1, "42d607c1515fec47b5c19b04778c4c52bbfe10cac2d0acb459d0f61a02eebacf") -- Medieval Dynasty - Map Pack - Map Pack Content
+setManifestid(1766022, "9195790049611468482", 329581592)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3774330) -- Medieval Dynasty - Echoes of Nature
+addappid(4159030) -- Medieval Dynasty - Exquisite Pack
