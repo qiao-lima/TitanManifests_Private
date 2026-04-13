33dce2ed86ac8fcc6cf1cb2bb87e21f5e8e597d5
qiao-lima <qiao-lima@github.com>
2026-04-14 00:36:52 +0800

    Manual Update 3445040

---
+++ 3445040.lua
@@ -0,0 +1,14 @@
+-- 3445040's Lua and Manifest Created by Morrenus
+-- BelleCitique
+-- Created: April 07, 2026 at 22:13:58 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3445040) -- BelleCitique
+-- MAIN APP DEPOTS
+addappid(3445041, 1, "b2d64cdf260a4d852e877ebb5a7d630b53686f08156dfb3ae64e86b837cea0b3") -- Depot 3445041
+setManifestid(3445041, "6347563179229136573", 731664257)
+addappid(3445042, 1, "73b2b7c4d3c5daf33ed1610fd5c8a0d1c77cc3c0cb6ea78c50bb10050c955f88") -- Depot 3445042
+setManifestid(3445042, "808184755118734808", 749114786)
