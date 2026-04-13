bab93b9bd2475c6c7084aa54ebf9c0c77b0a1311
qiao-lima <qiao-lima@github.com>
2026-04-14 00:27:10 +0800

    Manual Update 3145480

---
+++ 3145480.lua
@@ -0,0 +1,12 @@
+-- 3145480's Lua and Manifest Created by Morrenus
+-- 喵与时光的街角
+-- Created: April 04, 2026 at 13:40:17 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3145480) -- 喵与时光的街角
+-- MAIN APP DEPOTS
+addappid(3145481, 1, "ad4ebcda30be47f53d4af511ad445ce53ca4a2a5785c9977dc5c0d56e804bb41") -- Depot 3145481
+setManifestid(3145481, "4661022247942801668", 2693762864)
