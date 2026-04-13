84d77946c3856ad568c5ff9656e97e2422b87929
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:04 +0800

    Manual Update 3776880

---
+++ 3776880.lua
@@ -0,0 +1,12 @@
+-- 3776880's Lua and Manifest Created by Morrenus
+-- ELEMENTAL SOUL
+-- Created: April 06, 2026 at 18:04:43 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3776880) -- ELEMENTAL SOUL
+-- MAIN APP DEPOTS
+addappid(3776881, 1, "799407395a5eb2b9d5f1e304e3b16a7ec2e990151c0983e23444894aabf1c55f") -- Depot 3776881
+setManifestid(3776881, "1139705918770781561", 341536574)
