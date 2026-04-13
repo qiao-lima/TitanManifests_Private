b21f6d5105761430911e0252dba4e8a085b8fb9f
qiao-lima <qiao-lima@github.com>
2026-04-14 00:40:33 +0800

    Manual Update 3519900

---
+++ 3519900.lua
@@ -0,0 +1,16 @@
+-- 3519900's Lua and Manifest Created by Morrenus
+-- Cyberscape
+-- Created: April 13, 2026 at 00:30:17 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3519900) -- Cyberscape
+-- MAIN APP DEPOTS
+addappid(3519901, 1, "b1625d07b7ebf458cf615481f6b85f0ee5d48dffbf94f08e397c105e7dcb765a") -- Depot 3519901
+setManifestid(3519901, "4385965259283136038", 257585831)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
