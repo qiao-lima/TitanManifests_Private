76018df8d588a092b791e5d1604d9be1d96b8b3d
qiao-lima <qiao-lima@github.com>
2026-04-13 23:52:38 +0800

    Manual Update 1658840

---
+++ 1658840.lua
@@ -0,0 +1,13 @@
+-- 1658840's Lua and Manifest Created by Morrenus
+-- Black Hair Girl Is Best Girl
+-- Created: April 07, 2026 at 22:16:36 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1658840) -- Black Hair Girl Is Best Girl
+addtoken(1658840, "8900004769945374437")
+-- MAIN APP DEPOTS
+addappid(1658841, 1, "c24c64d3f10ed153a505aaf06abee0e83f373f19799825903e5f02e58340cacf") -- 黑长直恋爱物语 Content
+setManifestid(1658841, "7427301460389803865", 780181707)
