0e7e19da476b07cd1f1ade0680e9703740ccc39d
qiao-lima <qiao-lima@github.com>
2026-04-14 00:41:10 +0800

    Manual Update 3573460

---
+++ 3573460.lua
@@ -0,0 +1,12 @@
+-- 3573460's Lua and Manifest Created by Morrenus
+-- Beebo & Luna
+-- Created: April 10, 2026 at 06:29:16 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3573460) -- Beebo & Luna
+-- MAIN APP DEPOTS
+addappid(3573461, 1, "61d0ffbe625555db7a556cf1366fea34501a4003e3a5ef734333d0ca191f8bf6") -- Depot 3573461
+setManifestid(3573461, "8888990634885499110", 546197906)
