1bd232f3ede5ee4ac3a7e4f4993f33d8474f6511
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:59 +0800

    Manual Update 4249770

---
+++ 4249770.lua
@@ -0,0 +1,12 @@
+-- 4249770's Lua and Manifest Created by Morrenus
+-- 猫船长与环球渔场
+-- Created: March 11, 2026 at 04:39:30 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4249770) -- 猫船长与环球渔场
+-- MAIN APP DEPOTS
+addappid(4249771, 1, "9169199b60c0f8002b40ec006b86b914bd854c0602c199c57281607e3b1fb845") -- Depot 4249771
+setManifestid(4249771, "5256956690579499312", 787141359)
