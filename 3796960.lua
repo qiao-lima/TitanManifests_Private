77c19d24b76586b5066c85fe5db631298f57ff8a
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:07 +0800

    Manual Update 3796960

---
+++ 3796960.lua
@@ -0,0 +1,20 @@
+-- 3796960's Lua and Manifest Created by Morrenus
+-- Elemental: Reforged
+-- Created: March 17, 2026 at 16:52:24 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3796960) -- Elemental: Reforged
+-- MAIN APP DEPOTS
+addappid(3796961, 1, "c1d0d41e73d3e1ed7a0564bd1d97490fbfe0c7f5b5f9deb1b30335d6ed41d334") -- Depot 3796961
+setManifestid(3796961, "7112120857325041462", 11773487284)
+addappid(3796962, 1, "fecd201d8a945628e196f5a3e49126c8be8fea466bfb2af5e88e85a82edb9196") -- Depot 3796962
+setManifestid(3796962, "7949774489289641042", 94347097)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
