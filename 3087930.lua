b88a21acc41e99b076e2fde1b8ea9ca9f6a13224
qiao-lima <qiao-lima@github.com>
2026-04-14 00:25:38 +0800

    Manual Update 3087930

---
+++ 3087930.lua
@@ -0,0 +1,12 @@
+-- 3087930's Lua and Manifest Created by Morrenus
+-- Pebble Knights
+-- Created: April 13, 2026 at 06:19:32 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3087930) -- Pebble Knights
+-- MAIN APP DEPOTS
+addappid(3087931, 1, "07c33babc4319eb3141fe1d0682586166d725a42071a51f97a389d6dff687202") -- Depot 3087931
+setManifestid(3087931, "2531060623142028786", 4215113393)
