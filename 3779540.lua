a863a8b1a4a4158b40841e9474aea9182b657f8a
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:04 +0800

    Manual Update 3779540

---
+++ 3779540.lua
@@ -0,0 +1,12 @@
+-- 3779540's Lua and Manifest Created by Morrenus
+-- The Dragon and the Sword
+-- Created: April 07, 2026 at 02:56:33 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3779540) -- The Dragon and the Sword
+-- MAIN APP DEPOTS
+addappid(3779541, 1, "2ddc0d2eaa17f0bf8df4dfa619bcd05671d51008c7e61724ed12ce3c14490319") -- Depot 3779541
+setManifestid(3779541, "9022541896655845211", 3820582676)
