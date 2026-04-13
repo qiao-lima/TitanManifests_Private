3a31292c6f23682a0b2d56af28a01eff1d90d2a1
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:38 +0800

    Manual Update 4204880

---
+++ 4204880.lua
@@ -0,0 +1,14 @@
+-- 4204880's Lua and Manifest Created by Morrenus
+-- 你的哈基米
+-- Created: March 17, 2026 at 18:51:21 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4204880) -- 你的哈基米
+-- MAIN APP DEPOTS
+addappid(4204881, 1, "507f89a0427e8172f15e696597c45df57d8ec92ed26db26e294e040ea8f2c3af") -- Depot 4204881
+setManifestid(4204881, "8637515013308591246", 68994672)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4313960) --  - Supporter Pack
