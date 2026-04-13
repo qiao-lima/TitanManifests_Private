dcc623555ee7559256144b945296af64c13e2280
qiao-lima <qiao-lima@github.com>
2026-04-14 02:53:04 +0800

    Manual Update 739630

---
+++ 739630.lua
@@ -0,0 +1,16 @@
+-- 739630's Lua and Manifest Created by Morrenus
+-- Phasmophobia
+-- Created: March 03, 2026 at 10:47:34 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(739630, 1, "6850686ca304da94d7b1a8ebd9203bcf007712610dc92eb9452e22a31b6b823d") -- Phasmophobia
+-- MAIN APP DEPOTS
+addappid(739631, 1, "a0af61717bae449e5db4a1cc6eb68d7604ac3a8e05ff5226faa14b5be67d640c") -- VR_Project Content
+setManifestid(739631, "1979679807443248579", 36659487075)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
