afc714bf892916b9226f5877ce6fea1c99f2bafe
qiao-lima <qiao-lima@github.com>
2026-04-14 02:48:06 +0800

    Manual Update 3794770

---
+++ 3794770.lua
@@ -0,0 +1,12 @@
+-- 3794770's Lua and Manifest Created by Morrenus
+-- Island of Hearts
+-- Created: March 27, 2026 at 09:15:49 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3794770) -- Island of Hearts
+-- MAIN APP DEPOTS
+addappid(3794771, 1, "7b79d0b03a649d3a80e8bf662192be4fd7847007549c55ed51a197c3fee913d6") -- Depot 3794771
+setManifestid(3794771, "1941227305030301784", 45887598785)
