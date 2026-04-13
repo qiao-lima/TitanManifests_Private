307eee1bc70d46aa70c2f7fb5095dfcc2d7af004
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:57 +0800

    Manual Update 4224460

---
+++ 4224460.lua
@@ -0,0 +1,14 @@
+-- 4224460's Lua and Manifest Created by Morrenus
+-- SCP-3008: Infinite Store
+-- Created: March 14, 2026 at 20:43:22 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4224460) -- SCP-3008: Infinite Store
+-- MAIN APP DEPOTS
+addappid(4224461, 1, "70d58d2c93e31ae1464dd5a03d965e1e8c363ab2a2649e299f9106bd7edf07ce") -- Depot 4224461
+setManifestid(4224461, "400592709175304519", 2209807690)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4224463) -- Depot 4224463 (empty depot)
