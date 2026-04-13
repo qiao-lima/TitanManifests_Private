75dab2a552de945785a532fbcf461c9479d39fcc
qiao-lima <qiao-lima@github.com>
2026-04-14 02:50:59 +0800

    Manual Update 4240090

---
+++ 4240090.lua
@@ -0,0 +1,18 @@
+-- 4240090's Lua and Manifest Created by Morrenus
+-- KunKunNest
+-- Created: March 15, 2026 at 14:35:40 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4240090) -- KunKunNest
+-- MAIN APP DEPOTS
+addappid(4240091, 1, "e352e4c265a65542c2b295b7b7d63ad01c8cfa12cee5948546395eaaf270244e") -- Depot 4240091
+setManifestid(4240091, "1614478338739384413", 1615418799)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
