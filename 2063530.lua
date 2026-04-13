20fcfaecc69895af867c959e9986e9c9c62dfc98
qiao-lima <qiao-lima@github.com>
2026-04-13 23:58:19 +0800

    Manual Update 2063530

---
+++ 2063530.lua
@@ -0,0 +1,12 @@
+-- 2063530's Lua and Manifest Created by Morrenus
+-- Methods: The Canada Files
+-- Created: March 17, 2026 at 01:10:12 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2063530, 1, "1880cf706221b3f400b7e2f7f85e074ec286c213233b8520e6d7d9d05b5782c6") -- Methods: The Canada Files
+-- MAIN APP DEPOTS
+addappid(2063531, 1, "9bacc99a7f73fd27446afddf22374e4e8d8da712dcd154e6eef1c003751cd579") -- Depot 2063531
+setManifestid(2063531, "949058175340321229", 1116215149)
