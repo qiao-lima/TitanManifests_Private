ab64f3bcef763ff7c6e0257a1bb623b42d72e5c6
qiao-lima <qiao-lima@github.com>
2026-04-13 23:42:54 +0800

    Manual Update 1315180

---
+++ 1315180.lua
@@ -0,0 +1,12 @@
+-- 1315180's Lua and Manifest Created by Morrenus
+-- Spark in the Dark
+-- Created: April 07, 2026 at 13:49:46 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1315180) -- Spark in the Dark
+-- MAIN APP DEPOTS
+addappid(1315182, 1, "6fe651af63a3040dd06f81a129a304fee0a54d5c9ca3662b950d0fe69e5675ce") -- Depot 1315182
+setManifestid(1315182, "3869396552225933028", 7564492245)
