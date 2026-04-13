6c12041cccfd052f90d9b14da250ba7e5ff44805
qiao-lima <qiao-lima@github.com>
2026-04-14 00:17:48 +0800

    Manual Update 2750780

---
+++ 2750780.lua
@@ -0,0 +1,12 @@
+-- 2750780's Lua and Manifest Created by Morrenus
+-- 月华辉映之刻
+-- Created: February 14, 2026 at 10:21:43 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2750780) -- 月华辉映之刻
+-- MAIN APP DEPOTS
+addappid(2750781, 1, "9cadd04d7b31e6009784c7ac33576344329cd56636774e4a5b77e0b2931502b4") -- Depot 2750781
+setManifestid(2750781, "5409099423085359394", 7836573067)
