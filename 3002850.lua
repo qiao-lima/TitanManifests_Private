a19961e4103a8ef7e31cf98ee25dbf091596e24c
qiao-lima <qiao-lima@github.com>
2026-04-14 00:25:33 +0800

    Manual Update 3002850

---
+++ 3002850.lua
@@ -0,0 +1,32 @@
+-- 3002850's Lua and Manifest Created by Morrenus
+-- FAIRY TAIL 2
+-- Created: September 29, 2025 at 11:23:35 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 15
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3002850) -- FAIRY TAIL 2
+-- MAIN APP DEPOTS
+addappid(3002851, 1, "e948f2d613815929e41d2ea8914218fc1071df961d38a2f8e0336060c00fdb6c") -- Depot 3002851
+setManifestid(3002851, "3514777598807704182", 15650483909)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3193120) -- FAIRY TAIL 2 - Natsu Outfit Natsu of Edolas
+addappid(3193130) -- FAIRY TAIL 2 - Lucy Outfit Miss FAIRY TAIL Contest
+addappid(3193140) -- FAIRY TAIL 2 - Digital Deluxe Upgrade Pack
+addappid(3193160) -- FAIRY TAIL 2 - Season Pass
+addappid(3193170) -- FAIRY TAIL 2 - New Party Member Jellal
+addappid(3193180) -- FAIRY TAIL 2 - New Party Member Mavis
+addappid(3193190) -- FAIRY TAIL 2 - New Party Member Zeref
+addappid(3193200) -- FAIRY TAIL 2 - Bonus Scenario Fairy Tail Hot Springs
+addappid(3193210) -- FAIRY TAIL 2 - Bonus Feature Arena
+addappid(3193220) -- FAIRY TAIL 2 - Special Outfit Complete Set
+addappid(3193230) -- FAIRY TAIL 2 - Special Outfit Set Luxury Restaurant
+addappid(3193240) -- FAIRY TAIL 2 - Special Outfit Set Happy Wedding
+addappid(3193250) -- FAIRY TAIL 2 - Special Outfit Set Fairy School
+addappid(3260320) -- FAIRY TAIL 2 - Erza Outfit Miss FAIRY TAIL Contest
+addappid(3260330) -- FAIRY TAIL 2 - Outfit Set Swimsuit
