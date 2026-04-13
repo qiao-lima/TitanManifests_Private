a7ebfae644b9722df5e09efc6ef67162704c83dc
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:28 +0800

    Manual Update 598980

---
+++ 598980.lua
@@ -0,0 +1,16 @@
+-- 598980's Lua and Manifest Created by Morrenus
+-- The Coin Game
+-- Created: March 22, 2026 at 08:03:19 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(598980, 1, "c21a454ef1de1cabb04e7a9c0b680cd5d91c256adf16dd90683d0851fbd409a6") -- The Coin Game
+-- MAIN APP DEPOTS
+addappid(598981, 1, "5eef890e869bba07fb135158a507ede6b276453d706d5c4e36f2ac5a66e43963") -- The Coin Game Content WINDOWS
+setManifestid(598981, "7423561695571616238", 4784220634)
+addappid(598984, 1, "8cd74454dd76b1413f9e9edce77dc07524709e46e9cca1523699953c636ad7dd") -- The Coin Game Content WINDOWS 64
+setManifestid(598984, "5829605892213927306", 6132809777)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1267250) -- The Coin Game - Supporter Edition
