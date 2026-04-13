20e96e1d44fca44aa917deb250851f57256e0100
qiao-lima <qiao-lima@github.com>
2026-04-14 02:52:08 +0800

    Manual Update 4475600

---
+++ 4475600.lua
@@ -0,0 +1,18 @@
+-- 4475600's Lua and Manifest Created by Morrenus
+-- Yuki Onna: Snowball Fight!
+-- Created: April 07, 2026 at 08:04:24 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4475600) -- Yuki Onna: Snowball Fight!
+-- MAIN APP DEPOTS
+addappid(4475601, 1, "fc986737b92795e9def01559bf1962997201289b747672a3ca5c7cf3af9e0680") -- Depot 4475601
+setManifestid(4475601, "6686250265542604716", 3105954595)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
