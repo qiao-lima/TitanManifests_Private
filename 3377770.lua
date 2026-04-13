a857b453f93e5881926631269d18e75c96dc05bd
qiao-lima <qiao-lima@github.com>
2026-04-14 00:32:43 +0800

    Manual Update 3377770

---
+++ 3377770.lua
@@ -0,0 +1,12 @@
+-- 3377770's Lua and Manifest Created by Morrenus
+-- Haunting Havens
+-- Created: March 28, 2026 at 10:05:34 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3377770) -- Haunting Havens
+-- MAIN APP DEPOTS
+addappid(3377771, 1, "66d9fcae2691745f33f9d4c65e97e53e6b10a4923be5ec52b6930ae92f65f758") -- Depot 3377771
+setManifestid(3377771, "1071051144411713112", 11450707203)
