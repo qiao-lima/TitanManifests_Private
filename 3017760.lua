80126c39bd0dce204d06128bd8f0ce43ed24df2d
qiao-lima <qiao-lima@github.com>
2026-04-14 18:49:15 +0800

    Manual Update 3017760

---
+++ 3017760.lua
@@ -0,0 +1,12 @@
+-- 3017760's Lua and Manifest Created by Morrenus
+-- Relic Abyss
+-- Created: April 13, 2026 at 14:34:46 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3017760) -- Relic Abyss
+-- MAIN APP DEPOTS
+addappid(3017761, 1, "d5f0f2bb913f66ccb5cb05b1d6aac41d0b47e68e0ab28c6898afd6ffed961ed3") -- Depot 3017761
+setManifestid(3017761, "8905292447963841032", 2741076885)
