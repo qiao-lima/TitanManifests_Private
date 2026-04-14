b43b489e83447b8fac0ee4b5a9f43b9df7524f74
qiao-lima <qiao-lima@github.com>
2026-04-14 18:49:16 +0800

    Manual Update 3130170

---
+++ 3130170.lua
@@ -0,0 +1,12 @@
+-- 3130170's Lua and Manifest Created by Morrenus
+-- Full Metal Sergeant 2
+-- Created: April 13, 2026 at 15:07:12 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3130170) -- Full Metal Sergeant 2
+-- MAIN APP DEPOTS
+addappid(3130171, 1, "434cbd48d7fa0687e11e8974524d3f5e023d7748212257ffab092e32cf9adb9a") -- Depot 3130171
+setManifestid(3130171, "8938619426946423085", 176265087)
