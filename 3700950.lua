bd30c03bac84d3c958c326fabcfc887e99873bf2
qiao-lima <qiao-lima@github.com>
2026-04-14 18:49:17 +0800

    Manual Update 3700950

---
+++ 3700950.lua
@@ -0,0 +1,12 @@
+-- 3700950's Lua and Manifest Created by Morrenus
+-- Under the Disco Lights
+-- Created: April 13, 2026 at 10:07:17 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3700950) -- Under the Disco Lights
+-- MAIN APP DEPOTS
+addappid(3700951, 1, "4ad09c3e8982671f287280729d98df0b168657aa5064419c9a009d1dca376be6") -- Depot 3700951
+setManifestid(3700951, "7149570427772001726", 6893213431)
