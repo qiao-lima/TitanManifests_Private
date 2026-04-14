1649f94de8cd64fed1a502a139fa26a97d0bddc4
qiao-lima <qiao-lima@github.com>
2026-04-14 18:49:17 +0800

    Manual Update 3972320

---
+++ 3972320.lua
@@ -0,0 +1,18 @@
+-- 3972320's Lua and Manifest Created by Morrenus
+-- Loot Loop
+-- Created: April 13, 2026 at 12:01:57 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3972320) -- Loot Loop
+-- MAIN APP DEPOTS
+addappid(3972321, 1, "860a0b4eb27c395ed13d5af3854a9a11f60f38f970717870e12ae7bf80eaf090") -- Depot 3972321
+setManifestid(3972321, "1346391937085944044", 591147264)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
