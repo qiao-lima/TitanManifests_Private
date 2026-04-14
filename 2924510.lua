966db84ff99374d293bc3d27f0e18606cb00a02a
qiao-lima <qiao-lima@github.com>
2026-04-14 18:49:15 +0800

    Manual Update 2924510

---
+++ 2924510.lua
@@ -0,0 +1,12 @@
+-- 2924510's Lua and Manifest Created by Morrenus
+-- 刀剑封魔录外传：上古传说
+-- Created: December 07, 2025 at 04:28:09 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2924510) -- 刀剑封魔录外传：上古传说
+-- MAIN APP DEPOTS
+addappid(2924511, 1, "e13fe17cd4bd691c92ad88b7aa50a4a622fe4b448767cb53f7e782c5f8fd74bc") -- Depot 2924511
+setManifestid(2924511, "6258691633397616458", 2409354731)
