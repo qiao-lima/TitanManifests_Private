7f1090ad0af6aaf3492b2c91bb21ed6ee56f5b3c
qiao-lima <qiao-lima@github.com>
2026-04-14 18:49:17 +0800

    Manual Update 4417890

---
+++ 4417890.lua
@@ -0,0 +1,12 @@
+-- 4417890's Lua and Manifest Created by Morrenus
+-- Mercury Elopement Syndrome
+-- Created: April 13, 2026 at 13:39:12 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4417890) -- Mercury Elopement Syndrome
+-- MAIN APP DEPOTS
+addappid(4417891, 1, "fae9e48ac47350260bf63ea50a6382a28fd1e331562a84363e19c5f16e2b0b41") -- Depot 4417891
+setManifestid(4417891, "2232809290201267650", 590303833)
