19a88054c55943fd3057d71b3a793da1066aa010
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:22 +0800

    Manual Update 2972300

---
+++ 2972300.lua
@@ -0,0 +1,16 @@
+-- 2972300's Lua and Manifest Created by Hubcap Manifest
+-- Beauty Jigsaw
+-- Created: April 16, 2026 at 08:33:37 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(2972300) -- Beauty Jigsaw
+-- MAIN APP DEPOTS
+addappid(2972301, 1, "a5771ec6430ee0d156d88a1520eb1dbb0141f3b30fd5838a0218670ebc4ad6b8") -- Depot 2972301
+setManifestid(2972301, "2184289570077113035", 390534573)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Beauty Jigsaw - Image Pack (AppID: 3157060) - missing depot keys
+-- addappid(3157060)
