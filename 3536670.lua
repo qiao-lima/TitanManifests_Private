64ee67651121c8178a2424ef270cc253478c1be4
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:23 +0800

    Manual Update 3536670

---
+++ 3536670.lua
@@ -0,0 +1,12 @@
+-- 3536670's Lua and Manifest Created by Hubcap Manifest
+-- Conveyor Belt Sushi Simulator
+-- Created: April 16, 2026 at 08:07:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3536670) -- Conveyor Belt Sushi Simulator
+-- MAIN APP DEPOTS
+addappid(3536671, 1, "7ff02d3349ca7c5b302e5ddff703c32ab06304e4677d70f3086bb3d40df4a487") -- Depot 3536671
+setManifestid(3536671, "6910749047430293320", 2623834593)
