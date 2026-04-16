875a71a9fc4c6f681be6a4a57993f2ccd42b857a
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:21 +0800

    Manual Update 2133890

---
+++ 2133890.lua
@@ -0,0 +1,12 @@
+-- 2133890's Lua and Manifest Created by Hubcap Manifest
+-- Mixture
+-- Created: April 16, 2026 at 06:34:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2133890) -- Mixture
+-- MAIN APP DEPOTS
+addappid(2133891, 1, "e3901c2c691424c145837d92642942c2e5f6e394b2cb0662dd231b867a2a13a1") -- Depot 2133891
+setManifestid(2133891, "1972250859646727862", 8199538754)
