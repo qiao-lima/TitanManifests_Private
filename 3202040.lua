fc880787fb47eed34bf80f15f7ef5daf6e174a3c
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:23 +0800

    Manual Update 3202040

---
+++ 3202040.lua
@@ -0,0 +1,12 @@
+-- 3202040's Lua and Manifest Created by Hubcap Manifest
+-- なつめ繚乱 -ハイスペ脳筋妖狐の恋愛奇譚-
+-- Created: April 16, 2026 at 05:55:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3202040) -- なつめ繚乱 -ハイスペ脳筋妖狐の恋愛奇譚-
+-- MAIN APP DEPOTS
+addappid(3202041, 1, "b1db3be8c005a4a443dc7bdaf6d24e44371db270e4b05395908d87815f00fbd3") -- Depot 3202041
+setManifestid(3202041, "6034629077583356752", 214750207)
