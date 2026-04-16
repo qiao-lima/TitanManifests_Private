02aa5ae61621e5b49999620603c4b16e31196836
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:38 +0800

    Manual Update 4175140

---
+++ 4175140.lua
@@ -0,0 +1,12 @@
+-- 4175140's Lua and Manifest Created by Hubcap Manifest
+-- 3D Escape Room: Cursed Legacy
+-- Created: April 16, 2026 at 08:31:30 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4175140) -- 3D Escape Room: Cursed Legacy
+-- MAIN APP DEPOTS
+addappid(4175141, 1, "2ca5673e01c74826504d46d3416e7c1971c4c2a37379cd99b2da687028cf92c7") -- Depot 4175141
+setManifestid(4175141, "6472638727019605128", 7408322629)
