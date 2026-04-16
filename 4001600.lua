7b8848db87a0873c0559237a57d08516140eb108
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:37 +0800

    Manual Update 4001600

---
+++ 4001600.lua
@@ -0,0 +1,14 @@
+-- 4001600's Lua and Manifest Created by Hubcap Manifest
+-- Curse upon the Defeated Sow Knight!
+-- Created: April 16, 2026 at 08:39:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4001600) -- Curse upon the Defeated Sow Knight!
+-- MAIN APP DEPOTS
+addappid(4001601, 1, "e1b70de2aa888a0823094c4ecd12e3e1d75f3d1740446e91894cb81eaf731da2") -- Depot 4001601
+setManifestid(4001601, "3895002807891351852", 781678322)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4001602) -- Depot 4001602 (empty depot)
