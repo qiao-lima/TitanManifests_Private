c4d0a15ec867c61f3fe3c02e96094e599e400b82
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:36 +0800

    Manual Update 3937230

---
+++ 3937230.lua
@@ -0,0 +1,12 @@
+-- 3937230's Lua and Manifest Created by Hubcap Manifest
+-- House Cleaner Simulator
+-- Created: April 16, 2026 at 11:06:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3937230) -- House Cleaner Simulator
+-- MAIN APP DEPOTS
+addappid(3937231, 1, "853a6e7c8356a17f49d10884899644ee07bb8f04148856861c90cccdc65aa6cb") -- Depot 3937231
+setManifestid(3937231, "1993214309253289479", 2147377063)
