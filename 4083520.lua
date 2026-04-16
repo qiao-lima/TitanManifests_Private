53f647f3f95bf4d9a5d3aef4915ccadb6515fd2c
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:37 +0800

    Manual Update 4083520

---
+++ 4083520.lua
@@ -0,0 +1,13 @@
+-- 4083520's Lua and Manifest Created by Hubcap Manifest
+-- 橫斬勇者
+-- Created: April 16, 2026 at 08:33:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4083520) -- 橫斬勇者
+addtoken(4083520, "18425946712026748355")
+-- MAIN APP DEPOTS
+addappid(4083521, 1, "98666c143da823fbe69fe3425b64ab53789690074ed8fc573c8bcbc98b9dcc06") -- Depot 4083521
+setManifestid(4083521, "6010459935721714271", 162613090)
