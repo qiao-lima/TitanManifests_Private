0b56e4bb0b24eabb0f5501b55d850e2af9c1fbd7
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:21 +0800

    Manual Update 2340130

---
+++ 2340130.lua
@@ -0,0 +1,18 @@
+-- 2340130's Lua and Manifest Created by Hubcap Manifest
+-- Ground Zero
+-- Created: April 16, 2026 at 10:58:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2340130) -- Ground Zero
+-- MAIN APP DEPOTS
+addappid(2340132, 1, "e0e214f9f7220376644549f356213481b28cd6eb97ddc8d3c0fe04596b9e5833") -- Depot 2340132
+setManifestid(2340132, "5578378785794721658", 29269423662)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
