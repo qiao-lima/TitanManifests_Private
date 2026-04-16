331bb3754f18a70c91c1af7fde7c303d4280406c
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:39 +0800

    Manual Update 4231970

---
+++ 4231970.lua
@@ -0,0 +1,12 @@
+-- 4231970's Lua and Manifest Created by Hubcap Manifest
+-- Kings Do Not Fall
+-- Created: April 16, 2026 at 11:44:18 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4231970) -- Kings Do Not Fall
+-- MAIN APP DEPOTS
+addappid(4231971, 1, "be0dcef26fc86c9974cdea567fbd90d8a80e9109e8a7e2171e852541a2e0f1db") -- Depot 4231971
+setManifestid(4231971, "7646328364565049067", 995757556)
