a94c827c4eac7ccb444ce7f895901f9f20a9dfe9
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:23 +0800

    Manual Update 3326910

---
+++ 3326910.lua
@@ -0,0 +1,12 @@
+-- 3326910's Lua and Manifest Created by Hubcap Manifest
+-- なぃとみ〜ちゅ！ - おばけのめあと癒しのヒトイキ -
+-- Created: April 16, 2026 at 05:29:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3326910) -- なぃとみ〜ちゅ！ - おばけのめあと癒しのヒトイキ -
+-- MAIN APP DEPOTS
+addappid(3326911, 1, "454ccee93210a7215c02991be54180b8379d67fd74091516d2f1fd568d01a75e") -- Depot 3326911
+setManifestid(3326911, "1546259272973444372", 358672903)
