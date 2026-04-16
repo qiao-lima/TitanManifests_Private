e27e564f8edda03157607e060cb452004ec55a16
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:40 +0800

    Manual Update 4442400

---
+++ 4442400.lua
@@ -0,0 +1,16 @@
+-- 4442400's Lua and Manifest Created by Hubcap Manifest
+-- Vanishing In The Island Halphéra
+-- Created: April 16, 2026 at 14:11:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(4442400) -- Vanishing In The Island Halphéra
+-- MAIN APP DEPOTS
+addappid(4442401, 1, "b0840937c1fb462aab75bd8bfd59f0a49add9e2f4d4f299f658cfa1d13712659") -- Depot 4442401
+setManifestid(4442401, "5588007876648447579", 4728871947)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
