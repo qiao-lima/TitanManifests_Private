44f57544b4904567edbd4095be12a82fdd0ada38
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:39 +0800

    Manual Update 4257850

---
+++ 4257850.lua
@@ -0,0 +1,12 @@
+-- 4257850's Lua and Manifest Created by Hubcap Manifest
+-- Mirai's Midnight Training
+-- Created: April 16, 2026 at 05:05:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4257850) -- Mirai's Midnight Training
+-- MAIN APP DEPOTS
+addappid(4257851, 1, "c546d8551ff25ea41d289d2ac5cb1de50887071ca3dece82e245fc92245325f7") -- Depot 4257851
+setManifestid(4257851, "1944713401144100513", 560102224)
