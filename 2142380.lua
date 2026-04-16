b10dd4428c99a795534d3b6def430ce028ab90f0
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:21 +0800

    Manual Update 2142380

---
+++ 2142380.lua
@@ -0,0 +1,17 @@
+-- 2142380's Lua and Manifest Created by Hubcap Manifest
+-- Retro Drive: Revamped
+-- Created: April 16, 2026 at 07:38:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(2142380, 1, "9b6245cd370e7ff7fbd01e85e19f14e5d6b09c15ecb509d0f5035caaadd8f5ba") -- Retro Drive: Revamped
+-- MAIN APP DEPOTS
+addappid(2142381, 1, "7db951f9b6c3476a58c8bce6552925ab30b707370d0ec3af7d954e1e6c7f5dbd") -- Depot 2142381
+setManifestid(2142381, "4657077192260225907", 897612741)
+-- DLCS WITH DEDICATED DEPOTS
+-- Retro Drive Cinematic Pack (AppID: 2638360)
+addappid(2638360)
+addappid(2638360, 1, "237ea47f40e7bc306f9d0164c18453a3bdd123b246b7135dccba608a4ea87c6e") -- Retro Drive Cinematic Pack - Depot 2638360
+setManifestid(2638360, "6319406666750050222", 444102047)
