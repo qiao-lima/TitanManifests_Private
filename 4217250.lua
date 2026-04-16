3624f387a0180484a5453f70f3ad4ae6d8b31350
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:39 +0800

    Manual Update 4217250

---
+++ 4217250.lua
@@ -0,0 +1,12 @@
+-- 4217250's Lua and Manifest Created by Hubcap Manifest
+-- 龙陨交响
+-- Created: April 15, 2026 at 18:04:25 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4217250) -- 龙陨交响
+-- MAIN APP DEPOTS
+addappid(4217251, 1, "e0c9a2600547b04d78adf7786026528208e5e302bea906f1246d7100b3b5d69d") -- Depot 4217251
+setManifestid(4217251, "7454558949553550783", 6238518312)
