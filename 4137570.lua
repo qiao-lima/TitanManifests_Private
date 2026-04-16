d6b233863750fd93c0d9bf1e891068677f2b22b1
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:38 +0800

    Manual Update 4137570

---
+++ 4137570.lua
@@ -0,0 +1,18 @@
+-- 4137570's Lua and Manifest Created by Hubcap Manifest
+-- 分头行动:chibichibi
+-- Created: April 16, 2026 at 01:28:19 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4137570) -- 分头行动:chibichibi
+-- MAIN APP DEPOTS
+addappid(4137571, 1, "fb54375cd6ddd5dfc82d2efdacd530fe87f3b1e833061f16f815142783487fd9") -- Depot 4137571
+setManifestid(4137571, "6289867290682633229", 2315846064)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
