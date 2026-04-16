e86f763c8a1adf885a9ce87064fcabd9e7d158f0
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:55 +0800

    Manual Update 804300

---
+++ 804300.lua
@@ -0,0 +1,13 @@
+-- 804300's Lua and Manifest Created by Hubcap Manifest
+-- Tesla roadster going to mars
+-- Created: April 16, 2026 at 08:32:36 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(804300) -- Tesla roadster going to mars
+addtoken(804300, "11454952490022628770")
+-- MAIN APP DEPOTS
+addappid(804301, 1, "454efb867bad3a61707112a1e8b4958063064107907c5ba9761f41e5b2310d1a") -- Depot 804301
+setManifestid(804301, "2720200936296805257", 104091613)
