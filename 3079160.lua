8914e495612c0330d3b4ddb5ca3919dba1d14874
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:22 +0800

    Manual Update 3079160

---
+++ 3079160.lua
@@ -0,0 +1,12 @@
+-- 3079160's Lua and Manifest Created by Hubcap Manifest
+-- Don't Lose Aggro
+-- Created: April 15, 2026 at 13:49:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3079160) -- Don't Lose Aggro
+-- MAIN APP DEPOTS
+addappid(3079161, 1, "09af0de7327f85a6b7975b409d4226c7ce5a3ef46f6fc5754ef9d2bdfaf77db7") -- Depot 3079161
+setManifestid(3079161, "7162648718969024850", 6515235598)
