160138ea9d7ebe2eee4570a1fe20f90e041ab2ed
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:23 +0800

    Manual Update 3146630

---
+++ 3146630.lua
@@ -0,0 +1,12 @@
+-- 3146630's Lua and Manifest Created by Hubcap Manifest
+-- 電際亂戰ShoPaDen
+-- Created: April 16, 2026 at 08:32:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3146630) -- 電際亂戰ShoPaDen
+-- MAIN APP DEPOTS
+addappid(3146631, 1, "f93a45bf1b00bd82543030cfd238aa60669585f62ecc87c6f984d7480c3bf7e0") -- Depot 3146631
+setManifestid(3146631, "5657975803412943795", 915432253)
