933283bad51df0849fe5b5fbd86a4364d457dd26
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:20 +0800

    Manual Update 1139700

---
+++ 1139700.lua
@@ -0,0 +1,21 @@
+-- 1139700's Lua and Manifest Created by Hubcap Manifest
+-- Hunter's Arena: Legends (Closed Beta)
+-- Created: April 16, 2026 at 08:34:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(1139700) -- Hunter's Arena: Legends (Closed Beta)
+addtoken(1139700, "1258533019463437821")
+-- MAIN APP DEPOTS
+addappid(1139701, 1, "1afed84c1e3664c0d5c35d514ddda116311a05e53862a4e21c3d8e6d6af22d0a") -- Depot 1139701
+setManifestid(1139701, "6203531498275595920", 23038236696)
+-- SHARED DEPOTS (from other apps)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- VC 2015 Redist (Shared from App 228980)
+setManifestid(228986, "8782296191957114623", 29759921)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
