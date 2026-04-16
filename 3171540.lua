cfff4abb1bf793d70af9a62e28f73879c1d03bac
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:23 +0800

    Manual Update 3171540

---
+++ 3171540.lua
@@ -0,0 +1,20 @@
+-- 3171540's Lua and Manifest Created by Hubcap Manifest
+-- Packmates
+-- Created: April 16, 2026 at 08:36:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3171540) -- Packmates
+-- MAIN APP DEPOTS
+addappid(3171541, 1, "e3a7d23611ea543338a537cd23b843767ca7d75ee533376604150a94eddf725e") -- Depot 3171541
+setManifestid(3171541, "41926921489627259", 1211422482)
+addappid(3171542, 1, "7dfcfd4dc8af8fdb52430858cdf98634dc4286e2408449344c3c37e71c124f28") -- Depot 3171542
+setManifestid(3171542, "5961131466250205660", 1120804532)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
