46f9242828b6968b1b741c3de969bafbb2dd6e66
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:39 +0800

    Manual Update 4249800

---
+++ 4249800.lua
@@ -0,0 +1,12 @@
+-- 4249800's Lua and Manifest Created by Hubcap Manifest
+-- 蜃境
+-- Created: April 16, 2026 at 08:37:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4249800) -- 蜃境
+-- MAIN APP DEPOTS
+addappid(4249802, 1, "827b08cafe01b25d3cf26e2258ff4e68fa319e70c30e9aed79e2c8fd8c865241") -- Depot 4249802
+setManifestid(4249802, "4667226965315474540", 2291235061)
