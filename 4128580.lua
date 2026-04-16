750bf71187e5ae1da2a5e25931d5f1ceb3645df4
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:37 +0800

    Manual Update 4128580

---
+++ 4128580.lua
@@ -0,0 +1,14 @@
+-- 4128580's Lua and Manifest Created by Hubcap Manifest
+-- BidKing
+-- Created: April 16, 2026 at 14:12:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4128580) -- BidKing
+-- MAIN APP DEPOTS
+addappid(4128581, 1, "4f74fc9947de95eddfad71e6c6be2abf0b55b2f096917fab98728d2f5f37ec12") -- Depot 4128581
+setManifestid(4128581, "4231345361680621949", 3616855253)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4464600) -- BidKing
