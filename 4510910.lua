96af787d4988b7614950d98daa740095fc6046b7
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:55 +0800

    Manual Update 4510910

---
+++ 4510910.lua
@@ -0,0 +1,12 @@
+-- 4510910's Lua and Manifest Created by Hubcap Manifest
+-- 迴避我的死亡結局
+-- Created: April 16, 2026 at 08:39:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4510910) -- 迴避我的死亡結局
+-- MAIN APP DEPOTS
+addappid(4510911, 1, "7057509e9f642ef00356049022b34ca14cfdd9169261adf1bb4ad6109f0e6694") -- Depot 4510911
+setManifestid(4510911, "4855659685156719038", 855686833)
