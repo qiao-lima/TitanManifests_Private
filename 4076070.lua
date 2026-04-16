476fea94a8b3f4f1d0a4daf3850d700900a582de
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:37 +0800

    Manual Update 4076070

---
+++ 4076070.lua
@@ -0,0 +1,12 @@
+-- 4076070's Lua and Manifest Created by Hubcap Manifest
+-- Frontline: Afrika Korps
+-- Created: April 16, 2026 at 14:06:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4076070) -- Frontline: Afrika Korps
+-- MAIN APP DEPOTS
+addappid(4076071, 1, "40abfc13791577ba1db919d66ba75ad5aae23ddbad5b9495ae921bd752ac47fc") -- Depot 4076071
+setManifestid(4076071, "8557824867806271091", 2584741024)
