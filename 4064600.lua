5210e0cb0189c0c47a4ffc4990186c1402fc0c9d
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:37 +0800

    Manual Update 4064600

---
+++ 4064600.lua
@@ -0,0 +1,18 @@
+-- 4064600's Lua and Manifest Created by Hubcap Manifest
+-- Parkour Assassin: Sprint Run 2
+-- Created: April 16, 2026 at 08:29:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4064600) -- Parkour Assassin: Sprint Run 2
+-- MAIN APP DEPOTS
+addappid(4064601, 1, "dbab2eda8b98267630e9105dfbb7addf5a881b1a1fcd6204c95569013065be34") -- Depot 4064601
+setManifestid(4064601, "7335631771660944305", 1214470518)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
