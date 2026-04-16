808d254a58354baecaaaffbc8edb615136416c8f
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:36 +0800

    Manual Update 3718790

---
+++ 3718790.lua
@@ -0,0 +1,12 @@
+-- 3718790's Lua and Manifest Created by Hubcap Manifest
+-- 100 Cats Lost In USA Find & Color
+-- Created: April 16, 2026 at 08:32:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3718790) -- 100 Cats Lost In USA Find & Color
+-- MAIN APP DEPOTS
+addappid(3718791, 1, "70242e7f7148927a204b89373194253ab75bc2136d79505a45e34ff2f39db29c") -- Depot 3718791
+setManifestid(3718791, "6149145451103037983", 2389412107)
