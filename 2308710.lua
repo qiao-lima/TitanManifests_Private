fda00dd18216985e26cd7fd03fad30099ec8d7f8
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:21 +0800

    Manual Update 2308710

---
+++ 2308710.lua
@@ -0,0 +1,12 @@
+-- 2308710's Lua and Manifest Created by Hubcap Manifest
+-- Green Valley County
+-- Created: April 16, 2026 at 09:48:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2308710) -- Green Valley County
+-- MAIN APP DEPOTS
+addappid(2308711, 1, "de07dc338a15818cffa7787a9489417464ef4857abe81f698710b7e83e1c615e") -- Depot 2308711
+setManifestid(2308711, "7941102843722113840", 10094807168)
