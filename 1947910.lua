270089af79fa1a6ccc4230ac20c647248c76e06d
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:20 +0800

    Manual Update 1947910

---
+++ 1947910.lua
@@ -0,0 +1,25 @@
+-- 1947910's Lua and Manifest Created by Hubcap Manifest
+-- OPUS: Prism Peak
+-- Created: April 16, 2026 at 08:36:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 3
+
+-- MAIN APPLICATION
+addappid(1947910) -- OPUS: Prism Peak
+-- MAIN APP DEPOTS
+addappid(1947911, 1, "2d774dad6615d2bf47d32919e1d943ad1931ce694beed4a34f250788ba410cfe") -- Depot 1947911
+setManifestid(1947911, "112321103377156269", 5730174394)
+addappid(1947912, 1, "bc33249ce1ad8a15d3019cd930b4d0af3ce1f9e25708b5b574efb1f4af96ad50") -- Depot 1947912
+setManifestid(1947912, "3320145864505980816", 8227143689)
+-- DLCS WITH DEDICATED DEPOTS
+-- OPUS Prism Peak - Official Artbook (AppID: 4549960)
+addappid(4549960)
+addappid(4549960, 1, "0d8530b75e0c419767ecde235ce435c59ebf37d69cb65f0b0228bbae7fc9c455") -- OPUS Prism Peak - Official Artbook - Depot 4549960
+setManifestid(4549960, "7245548680511213736", 3627875753)
+-- OPUS Prism Peak - Digital Wallpaper (AppID: 4549970)
+addappid(4549970)
+addappid(4549970, 1, "b491719fbfbd294cd17d1b96a47523882874d69556fb087778e55b9ed222e1b6") -- OPUS Prism Peak - Digital Wallpaper - Depot 4549970
+setManifestid(4549970, "5521796940303035664", 151893552)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4549980) -- OPUS Prism Peak - Character Costume Pack
