055de4fbf970d41c75319c80ccfcf1394ea96451
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:55 +0800

    Manual Update 4546080

---
+++ 4546080.lua
@@ -0,0 +1,12 @@
+-- 4546080's Lua and Manifest Created by Hubcap Manifest
+-- 诡校中学实录
+-- Created: April 16, 2026 at 08:34:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4546080) -- 诡校中学实录
+-- MAIN APP DEPOTS
+addappid(4546081, 1, "5adfad687cbf4208cbfa930502b225d6c3d0c6cebfae48674fda5b65d0d91718") -- Depot 4546081
+setManifestid(4546081, "3369553770914684252", 327655521)
