93dfc70bcd007ae93e77f3e3082e55ab5990e95b
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:22 +0800

    Manual Update 3120030

---
+++ 3120030.lua
@@ -0,0 +1,16 @@
+-- 3120030's Lua and Manifest Created by Hubcap Manifest
+-- The Day I Became a Bird
+-- Created: April 16, 2026 at 08:35:58 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3120030) -- The Day I Became a Bird
+-- MAIN APP DEPOTS
+addappid(3120031, 1, "8e32434c707bb2a73454a8d9b7a719b961d834e23156537241d31169f0ec2d84") -- Depot 3120031
+setManifestid(3120031, "4313500990514682063", 2729774933)
+addappid(3120032, 1, "f2470de90dc65127f32306c3faad0515d3452c3b4e1068a72b9c25bdbcb7b3f5") -- Depot 3120032
+setManifestid(3120032, "4190922792686338451", 1738461620)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4396140) -- The Day I Became a Bird - Artbook
