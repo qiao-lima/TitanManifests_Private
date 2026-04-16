05f737730489408b39a5c66171fb455e77061e22
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:22 +0800

    Manual Update 2665100

---
+++ 2665100.lua
@@ -0,0 +1,14 @@
+-- 2665100's Lua and Manifest Created by Hubcap Manifest
+-- Project Breakout
+-- Created: April 16, 2026 at 08:38:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2665100) -- Project Breakout
+-- MAIN APP DEPOTS
+addappid(2665101, 1, "03b376c264a408fd7554db26d30e81d0230c1ef9ffdafde492daf8abafa4b1a4") -- Depot 2665101
+setManifestid(2665101, "457036206539839960", 1214767584)
+addappid(2665102, 1, "36701ce6f6781d9f65ee6c2d943f48c0a205244dfbd67f87d1c6f15262c1b53a") -- Depot 2665102
+setManifestid(2665102, "3205826195117132252", 1166995175)
