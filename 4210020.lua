045510a17d26597987642621e25a1f65fb2f941b
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:38 +0800

    Manual Update 4210020

---
+++ 4210020.lua
@@ -0,0 +1,12 @@
+-- 4210020's Lua and Manifest Created by Hubcap Manifest
+-- A game about sucking (up ghosts with a Vacuum)
+-- Created: April 16, 2026 at 14:11:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4210020) -- A game about sucking (up ghosts with a Vacuum)
+-- MAIN APP DEPOTS
+addappid(4210021, 1, "367a22bcf4ed58919fde177abcb771147a2eeafbc85bff615170c24035e0df79") -- Depot 4210021
+setManifestid(4210021, "7559046158496980635", 551296505)
