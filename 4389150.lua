13a6c32237f7f558e1e27a00fb70be06f116fb2c
qiao-lima <qiao-lima@github.com>
2026-04-17 02:16:39 +0800

    Manual Update 4389150

---
+++ 4389150.lua
@@ -0,0 +1,12 @@
+-- 4389150's Lua and Manifest Created by Hubcap Manifest
+-- Doner Madness
+-- Created: April 16, 2026 at 08:39:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4389150) -- Doner Madness
+-- MAIN APP DEPOTS
+addappid(4389151, 1, "e33bdf6e1941168c4edc06583de29126624cc5f362e76f545b35a152e3b2327f") -- Depot 4389151
+setManifestid(4389151, "1187049059058400985", 242716655)
