036e92564763b721abc13cbe13010046f5cee50a
qiao-lima <qiao-lima@github.com>
2026-04-17 19:19:25 +0800

    Manual Update 3962020

---
+++ 3962020.lua
@@ -0,0 +1,18 @@
+-- 3962020's Lua and Manifest Created by Hubcap Manifest
+-- EXD - Extra Dimensional
+-- Created: April 16, 2026 at 13:32:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3962020) -- EXD - Extra Dimensional
+-- MAIN APP DEPOTS
+addappid(3962021, 1, "78190f5930c805f8bc5b14a724ff065924c8a9ccac0eb55555604c6f80fbc1c9") -- Depot 3962021
+setManifestid(3962021, "5324270944245560705", 21697573228)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
