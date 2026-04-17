d6cff86606d96521647f3fd1536cd1b00446b518
qiao-lima <qiao-lima@github.com>
2026-04-17 19:18:07 +0800

    Manual Update 2760560

---
+++ 2760560.lua
@@ -0,0 +1,30 @@
+-- 2760560's Lua and Manifest Created by Hubcap Manifest
+-- Cthulhu: The Cosmic Abyss
+-- Created: April 16, 2026 at 20:15:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 5
+-- Total DLCs: 4
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2760560) -- Cthulhu: The Cosmic Abyss
+-- MAIN APP DEPOTS
+addappid(2760561, 1, "ac37c0a1bf3f91a4cb04f16b963e95e76007027ec18d154eeeb5cc6ae164eff0") -- Depot 2760561
+setManifestid(2760561, "448197360605346937", 17544736851)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Cthulhu The Cosmic Abyss  non-Euclidian Artbook (AppID: 4339000)
+addappid(4339000)
+addappid(4339000, 1, "867df084e2c5cef3410de17d23ec1ac39e33aa23c12b8306c8abc9c865ccfc21") -- Cthulhu The Cosmic Abyss  non-Euclidian Artbook - Depot 4339000
+setManifestid(4339000, "8544583366762777513", 131204307)
+-- Cthulhu The Cosmic Abyss  Arkham Horror TTRPG Scenario (AppID: 4339020)
+addappid(4339020)
+addappid(4339020, 1, "f064232607d205cb1a38033c122d2e3e199890b2629f080dd601b12fc831f021") -- Cthulhu The Cosmic Abyss  Arkham Horror TTRPG Scenario - Depot 4339020
+setManifestid(4339020, "4993221923045226295", 3017109)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4277070) -- Cthulhu The Cosmic Abyss - Sanity Skin Pack
+addappid(4277080) -- Cthulhu The Cosmic Abyss - Shining Trapezohedron Quest
