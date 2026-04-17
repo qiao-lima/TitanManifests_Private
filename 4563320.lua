a827466897a5da4dbdc3e7f2aaf03a3ca95d6cf9
qiao-lima <qiao-lima@github.com>
2026-04-17 19:19:27 +0800

    Manual Update 4563320

---
+++ 4563320.lua
@@ -0,0 +1,16 @@
+-- 4563320's Lua and Manifest Created by Hubcap Manifest
+-- 愿有神助
+-- Created: April 17, 2026 at 06:37:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(4563320) -- 愿有神助
+-- MAIN APP DEPOTS
+addappid(4563321, 1, "e4c2a86cb087881c9c5518000f67d6318a9334f916834a956103266b7c68b954") -- Depot 4563321
+setManifestid(4563321, "5134079974878658095", 407487830)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- - (AppID: 4564120) - missing depot keys
+-- addappid(4564120)
