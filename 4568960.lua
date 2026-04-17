d048b8db21f8935509519d1118653aac0047ff73
qiao-lima <qiao-lima@github.com>
2026-04-17 19:19:27 +0800

    Manual Update 4568960

---
+++ 4568960.lua
@@ -0,0 +1,12 @@
+-- 4568960's Lua and Manifest Created by Hubcap Manifest
+-- 某一年四季
+-- Created: April 17, 2026 at 04:58:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4568960) -- 某一年四季
+-- MAIN APP DEPOTS
+addappid(4568961, 1, "4b42415901617b6a9cc9f1758ddadb8bf6c12294a80f435839681096ce4ac09e") -- Depot 4568961
+setManifestid(4568961, "7584123526591344892", 759353347)
