c18252c7695002f09eaecdf34d830c6c3466382e
qiao-lima <qiao-lima@github.com>
2026-04-17 19:19:27 +0800

    Manual Update 4506510

---
+++ 4506510.lua
@@ -0,0 +1,12 @@
+-- 4506510's Lua and Manifest Created by Hubcap Manifest
+-- Again!? Yakuza: Reclaim Power, Interrogate Bodies, Subjugate
+-- Created: April 15, 2026 at 10:34:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4506510, 1, "220a858d95a49862afe49e07d45b6908b3e7fcd9bee04bc63397b17cdc103971") -- Again!? Yakuza: Reclaim Power, Interrogate Bodies, Subjugate
+-- MAIN APP DEPOTS
+addappid(4506511, 1, "65762ed65feca8edb9e059a56e153f250bb7fe55a6aa4790926f02ca399850ab") -- Depot 4506511
+setManifestid(4506511, "1569114610960146589", 1470322485)
