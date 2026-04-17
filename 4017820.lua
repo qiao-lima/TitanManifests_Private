cb05143e438ce8c2ad0b7ce5eacd7bb5d24094a9
qiao-lima <qiao-lima@github.com>
2026-04-17 19:19:26 +0800

    Manual Update 4017820

---
+++ 4017820.lua
@@ -0,0 +1,16 @@
+-- 4017820's Lua and Manifest Created by Hubcap Manifest
+-- 神明也没猜到结局
+-- Created: April 17, 2026 at 07:07:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(4017820, 1, "1c1369fc6b3edd5dd59873a01cd73cb0114d08f5e3032adad9f55ac5391a66d5") -- 神明也没猜到结局
+-- MAIN APP DEPOTS
+addappid(4017821, 1, "057ead8152b6fce442476d745d3d48e420d97971680196258a3369fa4951cafe") -- Depot 4017821
+setManifestid(4017821, "8086444375572637639", 616743585)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+--  -  (AppID: 4192610) - missing depot keys
+-- addappid(4192610)
