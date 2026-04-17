904a84552f904836af737438d86cf0363706587d
qiao-lima <qiao-lima@github.com>
2026-04-17 19:18:10 +0800

    Manual Update 3511520

---
+++ 3511520.lua
@@ -0,0 +1,14 @@
+-- 3511520's Lua and Manifest Created by Hubcap Manifest
+-- The Artifactory
+-- Created: April 17, 2026 at 02:59:08 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3511520, 1, "d4d4c7c62ec0ff43178b657f6853d7a71ffd8b538015efa656740be99975e412") -- The Artifactory
+-- MAIN APP DEPOTS
+addappid(3511521, 1, "158774aee9b541bbd095fc453200be11879ce2e764f7dfa0a1752c3d881e50be") -- Depot 3511521
+setManifestid(3511521, "5256251849432753897", 1058059010)
+addappid(3511522, 1, "081710f10b636d61c306331f793c2a23780ccada896c8f490414a11ef3638ef9") -- Depot 3511522
+setManifestid(3511522, "4807688849615031260", 1172718201)
