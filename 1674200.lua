627a150a2193e0e7d7a4aac895e6f63f5b4cf686
qiao-lima <qiao-lima@github.com>
2026-04-17 19:18:07 +0800

    Manual Update 1674200

---
+++ 1674200.lua
@@ -0,0 +1,12 @@
+-- 1674200's Lua and Manifest Created by Hubcap Manifest
+-- Alaska Gold Fever
+-- Created: April 17, 2026 at 03:56:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1674200) -- Alaska Gold Fever
+-- MAIN APP DEPOTS
+addappid(1674201, 1, "9804e57792e850549d9fbe261b8ef6b2e87cd7823a141a114291be845090df86") -- Depot 1674201
+setManifestid(1674201, "7355488435870260275", 11068432732)
