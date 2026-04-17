8aa2690ca8f437a7e01c84eee9888798152b8d0c
qiao-lima <qiao-lima@github.com>
2026-04-17 19:19:26 +0800

    Manual Update 4337830

---
+++ 4337830.lua
@@ -0,0 +1,13 @@
+-- 4337830's Lua and Manifest Created by Hubcap Manifest
+-- Jobless Hero
+-- Created: April 17, 2026 at 04:04:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4337830) -- Jobless Hero
+addtoken(4337830, "17694697920474225529")
+-- MAIN APP DEPOTS
+addappid(4337831, 1, "7d28dae9f0c6c960f50c9974549b6076a4cf8f825230a054aeed54c651af4f74") -- Depot 4337831
+setManifestid(4337831, "4264085757755050401", 420225135)
