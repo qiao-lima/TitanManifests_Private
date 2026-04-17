464e55cac954f522732ca0f11296487d11f00fee
qiao-lima <qiao-lima@github.com>
2026-04-17 19:18:09 +0800

    Manual Update 3376290

---
+++ 3376290.lua
@@ -0,0 +1,18 @@
+-- 3376290's Lua and Manifest Created by Hubcap Manifest
+-- 海蓝星之梦
+-- Created: April 15, 2026 at 09:07:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3376290, 1, "ccd00d7a58647c866ec75087a7a7293a3df9b5065a183d8fb4995ca06c3b1f97") -- 海蓝星之梦
+-- MAIN APP DEPOTS
+addappid(3376291, 1, "6a6a00130228e1bcca55b3c846a290c21c16e76b36432b0534bdf22e44355789") -- Depot 3376291
+setManifestid(3376291, "6871348002722924230", 1840174103)
+-- DLCS WITH DEDICATED DEPOTS
+--   (AppID: 4106360)
+addappid(4106360)
+addappid(4106360, 1, "62510f55bcb753f6357d6ef64901229e107d2a02be5a4623b3ae7c42a51eb665") --   - Depot 4106360
+setManifestid(4106360, "7154184520018424348", 58863726)
+-- addappid(4106361, 1, "MISSING_KEY") --   - Depot 4106361 (no key available)
