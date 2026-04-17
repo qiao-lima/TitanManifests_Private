813b55c534940da1591c55772ba6a5044f1bd17d
qiao-lima <qiao-lima@github.com>
2026-04-17 19:18:10 +0800

    Manual Update 3867610

---
+++ 3867610.lua
@@ -0,0 +1,12 @@
+-- 3867610's Lua and Manifest Created by Hubcap Manifest
+-- The Amusement
+-- Created: April 17, 2026 at 03:36:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3867610) -- The Amusement
+-- MAIN APP DEPOTS
+addappid(3867611, 1, "fae0592fe2de31a215206d054fdd0c6c1c04cba4086a95094d76f4119f880c91") -- Depot 3867611
+setManifestid(3867611, "6787219223757293187", 8823606726)
