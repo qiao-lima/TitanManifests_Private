54e592d25364a827dff10ccd43c6597dc2e49bc1
qiao-lima <qiao-lima@github.com>
2026-04-17 19:18:08 +0800

    Manual Update 3111030

---
+++ 3111030.lua
@@ -0,0 +1,12 @@
+-- 3111030's Lua and Manifest Created by Hubcap Manifest
+-- Express Car Wash Simulator
+-- Created: April 17, 2026 at 05:53:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3111030) -- Express Car Wash Simulator
+-- MAIN APP DEPOTS
+addappid(3111031, 1, "9f3910e98c95ceae0dc00f2e8efbfbd6c769a64b00cfdf53a09b719d0a87b82b") -- Depot 3111031
+setManifestid(3111031, "7896407064884812157", 4029627240)
