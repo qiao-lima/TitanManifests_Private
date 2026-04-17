5c95ba51c291b64667ece514825b9fcb0e630c28
qiao-lima <qiao-lima@github.com>
2026-04-17 19:18:09 +0800

    Manual Update 3381680

---
+++ 3381680.lua
@@ -0,0 +1,18 @@
+-- 3381680's Lua and Manifest Created by Hubcap Manifest
+-- Age of History II Definitive Edition
+-- Created: April 16, 2026 at 22:23:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3381680, 1, "b78b455f58e6475d3cb8260ac287ad9e09abfd712091d1051ae7ecf15b75e8b9") -- Age of History II Definitive Edition
+-- MAIN APP DEPOTS
+addappid(3381681, 1, "6da20eee236f318943eb6fa58a2bbf06d46002564c5afb77f116558dece50e0e") -- Depot 3381681
+setManifestid(3381681, "3471357464495691598", 659984704)
+addappid(3381682, 1, "f1d9944b4d4ef1cd9ea1807f25c6dd0a77a38b3b24fc8653a186394de684c9f3") -- Depot 3381682
+setManifestid(3381682, "3641174782882490280", 397008263)
+-- SHARED DEPOTS (from other apps)
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") -- OpenAL 2.0.7.0 Redist (Shared from App 228980)
+setManifestid(229020, "5799761707845834510", 810085)
