7215c8dd5e7ea9b85fff7fbacdd779347cc1426b
qiao-lima <qiao-lima@github.com>
2026-04-17 19:19:26 +0800

    Manual Update 4232600

---
+++ 4232600.lua
@@ -0,0 +1,12 @@
+-- 4232600's Lua and Manifest Created by Hubcap Manifest
+-- Slay All Bosses: Idle
+-- Created: April 16, 2026 at 16:39:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4232600) -- Slay All Bosses: Idle
+-- MAIN APP DEPOTS
+addappid(4232601, 1, "cc843d41e25a17c2f37bb3dd5691470de1c75d23f5c37abc5ddcf03e638f3ba6") -- Depot 4232601
+setManifestid(4232601, "2876627387259749327", 1115539059)
