fc3aaf2dde917fbd51b7e5f4736efb3b956269da
qiao-lima <qiao-lima@github.com>
2026-04-17 19:19:26 +0800

    Manual Update 3974290

---
+++ 3974290.lua
@@ -0,0 +1,21 @@
+-- 3974290's Lua and Manifest Created by Hubcap Manifest
+-- Calamity Angels: Special Delivery
+-- Created: April 16, 2026 at 07:31:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 1
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3974290, 1, "28afe48366412e72576a91aee0d9f3340e3212a78b1d5611c5f5cdfc02fe2bad") -- Calamity Angels: Special Delivery
+-- MAIN APP DEPOTS
+addappid(3974291, 1, "f086ce87d93a1e86d8de0e78fcd14d7f1057719a5120ff29f607afdf83540123") -- Depot 3974291
+setManifestid(3974291, "8754508475710446128", 6874126541)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITH DEDICATED DEPOTS
+-- Calamity Angels Art book  Wallpapers (AppID: 4211040)
+addappid(4211040)
+addappid(4211040, 1, "a67aea4f66bc4f3f880d55a39703d666af7b9482aa0dfc3143d226343590170b") -- Calamity Angels Art book  Wallpapers - Depot 4211040
+setManifestid(4211040, "2528496500902056243", 414324094)
