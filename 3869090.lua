96f0ed55bd7b6cec49433839935c9b734ebff53b
qiao-lima <qiao-lima@github.com>
2026-04-17 19:19:25 +0800

    Manual Update 3869090

---
+++ 3869090.lua
@@ -0,0 +1,12 @@
+-- 3869090's Lua and Manifest Created by Hubcap Manifest
+-- Beta Kafe : Interactive Movie
+-- Created: April 17, 2026 at 06:35:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3869090) -- Beta Kafe : Interactive Movie
+-- MAIN APP DEPOTS
+addappid(3869091, 1, "4ac3fcecdc427ea5b5c7328e3eb41fe80b6b867c98c637ea5dab87d1523f1469") -- Depot 3869091
+setManifestid(3869091, "3829700897132828795", 39656856524)
