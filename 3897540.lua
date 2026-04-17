1fa7918d44af27753b711cbf9f2c1b5b8e25abf7
qiao-lima <qiao-lima@github.com>
2026-04-17 19:19:25 +0800

    Manual Update 3897540

---
+++ 3897540.lua
@@ -0,0 +1,12 @@
+-- 3897540's Lua and Manifest Created by Hubcap Manifest
+-- Cult Nation
+-- Created: April 15, 2026 at 11:19:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3897540) -- Cult Nation
+-- MAIN APP DEPOTS
+addappid(3897541, 1, "1c1c04e8dee5f926e65728f37ff0df253980695952705fc5edcf2947f5fc34bc") -- Depot 3897541
+setManifestid(3897541, "8409150169815684506", 127194290)
