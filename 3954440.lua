82922d85fc6d733eea5d4087eccfb03969732b98
qiao-lima <qiao-lima@github.com>
2026-04-17 19:19:25 +0800

    Manual Update 3954440

---
+++ 3954440.lua
@@ -0,0 +1,12 @@
+-- 3954440's Lua and Manifest Created by Hubcap Manifest
+-- RESEED
+-- Created: April 17, 2026 at 06:32:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3954440) -- RESEED
+-- MAIN APP DEPOTS
+addappid(3954441, 1, "020fe73ef98a658f8224e3b5cd027beef438af57ad797d4a5b790e36d93f48ce") -- Depot 3954441
+setManifestid(3954441, "743081275928814389", 223734818)
