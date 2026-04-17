d5947714dca8185542710908dad4049dce4bfe48
qiao-lima <qiao-lima@github.com>
2026-04-17 19:18:07 +0800

    Manual Update 2066230

---
+++ 2066230.lua
@@ -0,0 +1,45 @@
+-- 2066230's Lua and Manifest Created by Hubcap Manifest
+-- Bunny Alice in Wonderland
+-- Created: April 15, 2026 at 08:53:08 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 14
+-- Total DLCs: 3
+
+-- MAIN APPLICATION
+addappid(2066230) -- Bunny Alice in Wonderland
+-- MAIN APP DEPOTS
+addappid(2066232, 1, "67f674a388ea9e61195c70dfca9d9f4aae6a071cb8955999494d5bfc19647073") -- Depot 2066232
+setManifestid(2066232, "6720208449398768460", 405795609)
+addappid(2066231, 1, "382b858c4a680611191a51f9c978607b1ab5436db34b6ce2ec4699255852e838") -- Depot 2066231
+setManifestid(2066231, "4847947443842051704", 384283844)
+addappid(2066233, 1, "1c35098374e225887ac005b4dda9aeb84914fda0c524ff3966dbc558dbf28311") -- Depot 2066233
+setManifestid(2066233, "2167228060025165938", 384283844)
+addappid(2066234, 1, "78aa2b593c32dd29889601142173e65d0d12ef95a74316cecd310be3d9f5a011") -- Depot 2066234
+setManifestid(2066234, "5602158140868214299", 384346893)
+addappid(2066235, 1, "0d18e4d10407cc81e81b9543dbb5de0eed27b36b8de65c2007933b544bb903ab") -- Depot 2066235
+setManifestid(2066235, "3803564976138436806", 404499027)
+addappid(2066236, 1, "a79386defa6e2221f58ae68f2320e3f3d3d6b256052b1941a62268dd50028a93") -- Depot 2066236
+setManifestid(2066236, "2430551648045508284", 402582193)
+addappid(2066237, 1, "8d7ea0c7e8f71ded40774d72d1c62a5cf8cbcf077408aedcd282a7ac2567a3d7") -- Depot 2066237
+setManifestid(2066237, "4724413727964972289", 417085584)
+addappid(2066239, 1, "521604ed235e1f7e812a7fe0c99964c1e10bf477fa6e30a66c4f7283dcf55d56") -- Depot 2066239
+setManifestid(2066239, "8196944266103295602", 383669889)
+-- DLCS WITH DEDICATED DEPOTS
+-- Bunny Alice in Wonderland DLC- Chibi Stickers (AppID: 2991830)
+addappid(2991830)
+addappid(2991831, 1, "438145b3536607fc669bbb328066ec53ecb86b795388bd2b2c760c98ea0d9781") -- Bunny Alice in Wonderland DLC- Chibi Stickers - Depot 2991831
+setManifestid(2991831, "7166702291547769428", 39969585)
+-- Bunny Alice in Wonderland DLC- Artbooks (AppID: 2991840)
+addappid(2991840)
+addappid(2991841, 1, "f90f3a054a4e90166ad114ed1f4b8dc07a02ba9cf1c3fb2f5d7ef8b49a3d5276") -- Bunny Alice in Wonderland DLC- Artbooks - Depot 2991841
+setManifestid(2991841, "4104460297016496339", 21173498)
+addappid(2991842, 1, "fa5d73c8dd51b1b6d64b8b8a5a1b707387d54e99cd13272ecad1aec9aa5a725d") -- Bunny Alice in Wonderland DLC- Artbooks - Depot 2991842
+setManifestid(2991842, "8101343386513629927", 19661173)
+addappid(2991843, 1, "c0c77b6ef48c0473b57e7e0f9ebf0a3fe8ef8e86507bfc983213de371c8556a8") -- Bunny Alice in Wonderland DLC- Artbooks - Depot 2991843
+setManifestid(2991843, "7840564597139673622", 18707471)
+addappid(2991844, 1, "6440052386b0a200e41ab27a3a4912228e3f568a61aef22882cc193e218101fe") -- Bunny Alice in Wonderland DLC- Artbooks - Depot 2991844
+setManifestid(2991844, "6259869279469965118", 22337300)
+-- Bunny Alice in Wonderland-puzzle answers (AppID: 3014980)
+addappid(3014980)
+addappid(3014981, 1, "a387de3b88dbc2757a16f0e1ae51b3ad780179281a6f206cb2224b57c6d57c74") -- Bunny Alice in Wonderland-puzzle answers - Depot 3014981
+setManifestid(3014981, "8824168654829023853", 5949073)
