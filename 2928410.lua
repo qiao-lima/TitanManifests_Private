10a89fa817bd3dbddaafc8d24644e02d897c59a6
qiao-lima <qiao-lima@github.com>
2026-04-17 19:18:08 +0800

    Manual Update 2928410

---
+++ 2928410.lua
@@ -0,0 +1,15 @@
+-- 2928410's Lua and Manifest Created by Hubcap Manifest
+-- Under Par Golf Architect
+-- Created: April 17, 2026 at 05:53:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(2928410) -- Under Par Golf Architect
+-- MAIN APP DEPOTS
+addappid(2928411, 1, "639ddcbeadf4fda3c7c43e9c13dd6885e34c07d9b4bb5379c9c8d41aa853ab02") -- Depot 2928411
+setManifestid(2928411, "7392212580121268358", 1104815560)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4232230) -- Supporter Pack
+addappid(4268900) -- Pacific Islands
