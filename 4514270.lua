06730205eee5db90003069e4aebaa55108b4fdbb
qiao-lima <qiao-lima@github.com>
2026-04-17 19:19:27 +0800

    Manual Update 4514270

---
+++ 4514270.lua
@@ -0,0 +1,12 @@
+-- 4514270's Lua and Manifest Created by Hubcap Manifest
+-- Bust Evolution: Bunnygirl VIP Paizuri！
+-- Created: April 15, 2026 at 08:54:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4514270) -- Bust Evolution: Bunnygirl VIP Paizuri！
+-- MAIN APP DEPOTS
+addappid(4514271, 1, "ce9e01f2ce6bb60e893d667f95b31138923ec0276de45003018e474ff6bba42d") -- Depot 4514271
+setManifestid(4514271, "4097163135911503335", 396119384)
