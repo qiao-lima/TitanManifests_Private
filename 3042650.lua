aa9ad369e185271ba2883ad0c0e4faccb6765613
qiao-lima <qiao-lima@github.com>
2026-04-17 19:18:08 +0800

    Manual Update 3042650

---
+++ 3042650.lua
@@ -0,0 +1,22 @@
+-- 3042650's Lua and Manifest Created by Hubcap Manifest
+-- Race Max Pro
+-- Created: April 17, 2026 at 06:55:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 3
+
+-- MAIN APPLICATION
+addappid(3042650) -- Race Max Pro
+-- MAIN APP DEPOTS
+addappid(3042652, 1, "b1199fbe1502b8bad49ed4320a756cfb10b0d4170edffcba7537240825fcf017") -- Depot 3042652
+setManifestid(3042652, "9217631153318067073", 3561529139)
+addappid(3042651, 1, "bdbf61fd56c396ba405c0813656b13f9f7686066bee9b29b3e3a9c29f712cb55") -- Depot 3042651
+setManifestid(3042651, "1070446528490531500", 3415168371)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4550020) -- Starter Edition
+addappid(4550030) -- Pro Edition
+addappid(4550040) -- Champion Edition
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4550020) -- Depot 4550020 (empty depot)
+-- addappid(4550030) -- Depot 4550030 (empty depot)
+-- addappid(4550040) -- Depot 4550040 (empty depot)
