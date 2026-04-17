a440bac37005146288d8abd767b55010b091d2d0
qiao-lima <qiao-lima@github.com>
2026-04-17 19:19:27 +0800

    Manual Update 4416880

---
+++ 4416880.lua
@@ -0,0 +1,14 @@
+-- 4416880's Lua and Manifest Created by Hubcap Manifest
+-- AICOMI
+-- Created: April 17, 2026 at 06:25:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4416880, 1, "91e46a2aee727cfb078140a1cbdc0ba868b93098414447ffa8f76b225c715836") -- AICOMI
+-- MAIN APP DEPOTS
+addappid(4416881, 1, "072c827512f8a7606854adf00efba6adc7b36832e46fe46edfa31047f7a941fc") -- Depot 4416881
+setManifestid(4416881, "8363894615502212413", 15793309059)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4607890) -- Depot 4607890 (empty depot)
