968caeceb6abfebc3a816d3630d9c97c8263b16a
qiao-lima <qiao-lima@github.com>
2026-04-17 19:18:08 +0800

    Manual Update 3041230

---
+++ 3041230.lua
@@ -0,0 +1,18 @@
+-- 3041230's Lua and Manifest Created by Hubcap Manifest
+-- Windrose
+-- Created: April 17, 2026 at 04:03:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3041230) -- Windrose
+-- MAIN APP DEPOTS
+addappid(3041231, 1, "2be97dc7c9e688c9293eae10d8eac83e45ef0e1d40b83fe62cd19af3f211d774") -- Depot 3041231
+setManifestid(3041231, "236776606111118540", 22067905669)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
