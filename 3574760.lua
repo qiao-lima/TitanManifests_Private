264f7d93f1762057722509367ca8152fe2c0244e
qiao-lima <qiao-lima@github.com>
2026-04-17 19:18:10 +0800

    Manual Update 3574760

---
+++ 3574760.lua
@@ -0,0 +1,12 @@
+-- 3574760's Lua and Manifest Created by Hubcap Manifest
+-- Cozy Sanctuary
+-- Created: April 16, 2026 at 14:37:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3574760) -- Cozy Sanctuary
+-- MAIN APP DEPOTS
+addappid(3574761, 1, "388ac8e295fdba2ab41dec8c76acf47eb520a0e4c17e079ecf486825e4c3a5a8") -- Depot 3574761
+setManifestid(3574761, "6401735522364904654", 564639839)
