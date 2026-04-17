70d4b618c7ffdb761e70795a6f6c380255eac340
qiao-lima <qiao-lima@github.com>
2026-04-17 19:18:06 +0800

    Manual Update 1290760

---
+++ 1290760.lua
@@ -0,0 +1,12 @@
+-- 1290760's Lua and Manifest Created by Hubcap Manifest
+-- Gecko Gods
+-- Created: April 16, 2026 at 20:03:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1290760) -- Gecko Gods
+-- MAIN APP DEPOTS
+addappid(1290761, 1, "09296ac59aa67605bc27389f12111c011e001a409679a0e868d00a4c89016d9c") -- Depot 1290761
+setManifestid(1290761, "610186671027308268", 3752589673)
