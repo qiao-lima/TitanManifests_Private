efdf6876fa3f64f9e0597505351a8a125765ea98
qiao-lima <qiao-lima@github.com>
2026-04-17 19:18:08 +0800

    Manual Update 2962570

---
+++ 2962570.lua
@@ -0,0 +1,12 @@
+-- 2962570's Lua and Manifest Created by Hubcap Manifest
+-- tiny Cats
+-- Created: April 16, 2026 at 06:49:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2962570, 1, "1ca6d702a22b2aba31dca6f5033d4e89ad011bd60826122cb3b0add128c5b038") -- tiny Cats
+-- MAIN APP DEPOTS
+addappid(2962571, 1, "35640b6f2d14a33205fb16eb0df285332f049642fb263bfb49c13045de40f849") -- Depot 2962571
+setManifestid(2962571, "8945065830740323755", 201318761)
