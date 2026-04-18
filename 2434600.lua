943e9de06fb26bd44955c9d21c12cc2335b03338
qiao-lima <qiao-lima@github.com>
2026-04-18 14:50:54 +0800

    Manual Update 2434600

---
+++ 2434600.lua
@@ -0,0 +1,18 @@
+-- 2434600's Lua and Manifest Created by Hubcap Manifest
+-- ShantyTown
+-- Created: April 17, 2026 at 09:01:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2434600) -- ShantyTown
+-- MAIN APP DEPOTS
+addappid(2434601, 1, "1727714ffa403cca9c4e68f63ae469cde651650f7371862b2d0b91bbd877256d") -- Depot 2434601
+setManifestid(2434601, "1697411614641896329", 1383609029)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
