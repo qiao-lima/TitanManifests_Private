541dba73d6d93893f7ff806ff22a500ddd2058ea
qiao-lima <qiao-lima@github.com>
2026-04-18 22:27:28 +0800

    Manual Update 3650740

---
+++ 3650740.lua
@@ -0,0 +1,12 @@
+-- 3650740's Lua and Manifest Created by Hubcap Manifest
+-- Dig & Deal
+-- Created: April 18, 2026 at 10:26:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3650740, 1, "85777815333d869805462ffc8fd85af92d41ba99a2e95cef7be66144737a9e9e") -- Dig & Deal
+-- MAIN APP DEPOTS
+addappid(3650741, 1, "31a4da06050256cbfb22223c16d475dba80b279bfa7d8cce1336cf926a1885a4") -- Depot 3650741
+setManifestid(3650741, "4218620525087612511", 575418041)
