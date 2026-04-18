3ca99be51f1bfb315415f6dccfb9d5a8cd870fef
qiao-lima <qiao-lima@github.com>
2026-04-18 22:27:29 +0800

    Manual Update 4561040

---
+++ 4561040.lua
@@ -0,0 +1,12 @@
+-- 4561040's Lua and Manifest Created by Hubcap Manifest
+-- The Lustful Bride
+-- Created: April 17, 2026 at 23:57:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4561040) -- The Lustful Bride
+-- MAIN APP DEPOTS
+addappid(4561041, 1, "7e716e4fc54f960b76a072837ce2105e127d3ec23071e6a8e9b321d038f5c7f1") -- Depot 4561041
+setManifestid(4561041, "2273924746393569971", 747044413)
