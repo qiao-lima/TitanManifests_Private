3bddb769c2eafb45a9ec7dc946ff97a6be4d4273
qiao-lima <qiao-lima@github.com>
2026-04-18 22:27:28 +0800

    Manual Update 4384120

---
+++ 4384120.lua
@@ -0,0 +1,14 @@
+-- 4384120's Lua and Manifest Created by Hubcap Manifest
+-- 你好，你配吗？
+-- Created: April 18, 2026 at 05:04:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4384120) -- 你好，你配吗？
+-- MAIN APP DEPOTS
+addappid(4384122, 1, "0298eae2d09304526253fb101215da5ba619db7aa3cd7b975c48e8afd87a15d1") -- Depot 4384122
+setManifestid(4384122, "4030110965049893805", 20980818151)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4384121) -- Depot 4384121 (empty depot)
