1970575efdb207be890295f7278eb3cdf5b79aeb
qiao-lima <qiao-lima@github.com>
2026-04-18 22:27:27 +0800

    Manual Update 3558880

---
+++ 3558880.lua
@@ -0,0 +1,12 @@
+-- 3558880's Lua and Manifest Created by Hubcap Manifest
+-- Do As I Say - Obedience to the Priest
+-- Created: April 17, 2026 at 23:56:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3558880) -- Do As I Say - Obedience to the Priest
+-- MAIN APP DEPOTS
+addappid(3558881, 1, "c7a211f6824f057919a9ac5d42e3a5a17a2048557acce72d22962392601b7819") -- Depot 3558881
+setManifestid(3558881, "2437692273554589059", 494748973)
