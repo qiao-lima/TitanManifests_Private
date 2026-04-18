44535d8886b8ac1f1929fbe81076384fad06bdca
qiao-lima <qiao-lima@github.com>
2026-04-18 14:50:55 +0800

    Manual Update 3570060

---
+++ 3570060.lua
@@ -0,0 +1,14 @@
+-- 3570060's Lua and Manifest Created by Hubcap Manifest
+-- The Ember Guardian
+-- Created: April 17, 2026 at 15:53:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3570060) -- The Ember Guardian
+-- MAIN APP DEPOTS
+addappid(3570061, 1, "515cabaa91c90fa4e194cfbff43b0055eee2f728c633897d9ee39de78ad6bfb7") -- Depot 3570061
+setManifestid(3570061, "829265967668480629", 2945684323)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4501320) -- The Ember Guardian - Supporter Pack
