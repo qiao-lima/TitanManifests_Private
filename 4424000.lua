c7b677a80a475b273e85e8ce9b6d103316b55a5d
qiao-lima <qiao-lima@github.com>
2026-04-18 22:27:28 +0800

    Manual Update 4424000

---
+++ 4424000.lua
@@ -0,0 +1,12 @@
+-- 4424000's Lua and Manifest Created by Hubcap Manifest
+-- A Mystic Journey With Umbra
+-- Created: April 17, 2026 at 16:33:53 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4424000) -- A Mystic Journey With Umbra
+-- MAIN APP DEPOTS
+addappid(4424001, 1, "adf67700a49fec34c035140fd736eb753ffc7932a7ece42c7f1d1d717db9fc7d") -- Depot 4424001
+setManifestid(4424001, "7432218639673219669", 230998276)
