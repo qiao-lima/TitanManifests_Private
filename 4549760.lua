df8eb8de7f91f0fcad5c076ae19c2670ca6ed8e8
qiao-lima <qiao-lima@github.com>
2026-04-18 22:27:29 +0800

    Manual Update 4549760

---
+++ 4549760.lua
@@ -0,0 +1,12 @@
+-- 4549760's Lua and Manifest Created by Hubcap Manifest
+-- 山海牧场
+-- Created: April 18, 2026 at 02:50:19 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4549760) -- 山海牧场
+-- MAIN APP DEPOTS
+addappid(4549761, 1, "bd9a6446e9e00a4936e70b7db08f1f8a5a971221a88fb97599cebc6e69ab62cf") -- Depot 4549761
+setManifestid(4549761, "1628932886621442987", 218134509)
