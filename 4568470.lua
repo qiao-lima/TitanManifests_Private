8f7144594c35d5af4233aba875d8466164c92d75
qiao-lima <qiao-lima@github.com>
2026-04-18 14:50:56 +0800

    Manual Update 4568470

---
+++ 4568470.lua
@@ -0,0 +1,16 @@
+-- 4568470's Lua and Manifest Created by Hubcap Manifest
+-- 沐光游记：小镇
+-- Created: April 17, 2026 at 12:00:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(4568470) -- 沐光游记：小镇
+-- MAIN APP DEPOTS
+addappid(4568471, 1, "3014eef4bf700c0f2f77f07b198082e11e7b153c52f627ff19b419ba23d85875") -- Depot 4568471
+setManifestid(4568471, "8693543038375843602", 785129761)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+--  (AppID: 4613710) - missing depot keys
+-- addappid(4613710)
