aeac0d8d382922b4c871f3758eb3209c0f9fd727
qiao-lima <qiao-lima@github.com>
2026-04-18 22:27:27 +0800

    Manual Update 2832650

---
+++ 2832650.lua
@@ -0,0 +1,12 @@
+-- 2832650's Lua and Manifest Created by Hubcap Manifest
+-- Cozy Designer
+-- Created: April 17, 2026 at 23:02:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2832650) -- Cozy Designer
+-- MAIN APP DEPOTS
+addappid(2832651, 1, "e9774c6bf9ec2942d6218dd96bb803b39dc90442e0fd9b49ac61db1aec52f685") -- Depot 2832651
+setManifestid(2832651, "4659451638777033215", 2057948686)
