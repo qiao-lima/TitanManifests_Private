8b0ecc777de32bb39d028d07a36075130b77c2c9
qiao-lima <qiao-lima@github.com>
2026-04-18 22:27:27 +0800

    Manual Update 3097680

---
+++ 3097680.lua
@@ -0,0 +1,12 @@
+-- 3097680's Lua and Manifest Created by Hubcap Manifest
+-- Cat Clean Ocean
+-- Created: April 17, 2026 at 23:02:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3097680) -- Cat Clean Ocean
+-- MAIN APP DEPOTS
+addappid(3097681, 1, "2e75986806a673532f2343c5ec6568c2c00e7166b97f0f14513b1ffd8bfad1b3") -- Depot 3097681
+setManifestid(3097681, "8284504853514380795", 268025877)
