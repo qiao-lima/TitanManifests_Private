6b53371d01f8e75aac4618fe44121dddc8faaad0
qiao-lima <qiao-lima@github.com>
2026-04-18 22:27:27 +0800

    Manual Update 2646460

---
+++ 2646460.lua
@@ -0,0 +1,29 @@
+-- 2646460's Lua and Manifest Created by Hubcap Manifest
+-- Soulmask
+-- Created: April 17, 2026 at 05:33:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 5
+-- Total DLCs: 3
+-- Shared Depots: 4
+
+-- MAIN APPLICATION
+addappid(2646460, 1, "b23ed007512964e7e8555f41e6bb10b5e69ea21eb12bbb01e396db6520b78d59") -- Soulmask
+-- MAIN APP DEPOTS
+addappid(2646461, 1, "b888b7fdf975bfc80d87df6beb2c5d56ada854d2adb6537fce281905a5d74d2a") -- Depot 2646461
+setManifestid(2646461, "6198563301666449840", 24367413174)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") -- .NET 4.6 Redist (Shared from App 228980)
+setManifestid(229005, "7992454656023763365", 62009092)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 117381405)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3336600) -- Soulmask-Cozy Tribe Theme Pack
+addtoken(3336600, "1998959128630753984")
+addappid(3731990) -- Soulmask - The Golden Legend
+addappid(3966020) -- Soulmask Shifting Sands
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3336600) -- Depot 3336600 (empty depot)
