5a3b29150911c929ee5bcb8e2b7cb0c03df2fa32
qiao-lima <qiao-lima@github.com>
2026-04-18 22:27:26 +0800

    Manual Update 2133570

---
+++ 2133570.lua
@@ -0,0 +1,18 @@
+-- 2133570's Lua and Manifest Created by Hubcap Manifest
+-- SpaceSlog
+-- Created: April 17, 2026 at 16:13:53 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(2133570, 1, "2f98b0cf701b0b31925a4692134f055244e3e4cb437319fcc6cf62da99c4afef") -- SpaceSlog
+-- MAIN APP DEPOTS
+addappid(2133572, 1, "315d681a7d60eff4efba36e6670a8d289f7d8f74d2756bc7df8ee4f98da0523d") -- Depot 2133572
+setManifestid(2133572, "1187388523439120202", 760281436)
+addappid(2133573, 1, "7d113ab964af745586d8f0f2d5f5b3793107c9d39ef441c89c1dd1cb0d65895b") -- Depot 2133573
+setManifestid(2133573, "7954683527945870728", 773515788)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2398740) -- SpaceSlog Custom Character in Game Access
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2133571) -- Depot 2133571 (empty depot)
