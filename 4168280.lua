9807c71150d3960aca4922b5e1f8bfd2b0abdd52
qiao-lima <qiao-lima@github.com>
2026-04-18 22:27:28 +0800

    Manual Update 4168280

---
+++ 4168280.lua
@@ -0,0 +1,18 @@
+-- 4168280's Lua and Manifest Created by Hubcap Manifest
+-- The Butler
+-- Created: April 18, 2026 at 01:41:37 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4168280) -- The Butler
+-- MAIN APP DEPOTS
+addappid(4168281, 1, "9ef0874c3646eaac06c8b39b2a1ddccca8644ff1918a7283711db9223951eb78") -- Depot 4168281
+setManifestid(4168281, "4143404515876758019", 7904597398)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
