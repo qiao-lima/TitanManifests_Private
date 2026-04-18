1b84363aeecbdd4ab29e0dab7850fb99d9befd77
qiao-lima <qiao-lima@github.com>
2026-04-18 22:27:26 +0800

    Manual Update 2272590

---
+++ 2272590.lua
@@ -0,0 +1,12 @@
+-- 2272590's Lua and Manifest Created by Hubcap Manifest
+-- Rizline
+-- Created: February 10, 2026 at 12:37:26 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2272590, 1, "89664fb0131d613e03a1014fa0fa2421f0be6171902b671f98deb160345d585e") -- Rizline
+-- MAIN APP DEPOTS
+addappid(2272591, 1, "e56041aeb98949481847cb4180afcbea078290b96d8535e4d4f5849819da3570") -- Depot 2272591
+setManifestid(2272591, "6200288455602654600", 2147198904)
