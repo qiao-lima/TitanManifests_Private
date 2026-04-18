e4c0b84333a4b94f93191ac004ba219bc4ed2017
qiao-lima <qiao-lima@github.com>
2026-04-18 22:27:27 +0800

    Manual Update 3144450

---
+++ 3144450.lua
@@ -0,0 +1,12 @@
+-- 3144450's Lua and Manifest Created by Hubcap Manifest
+-- Laundry Simulator 2026
+-- Created: April 17, 2026 at 14:35:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3144450) -- Laundry Simulator 2026
+-- MAIN APP DEPOTS
+addappid(3144451, 1, "e860aa300df6a0ba2cdf55f66e0e4f3b276192ae62b789916e2f7b4dab57478e") -- Depot 3144451
+setManifestid(3144451, "6255418658751352909", 4277620893)
