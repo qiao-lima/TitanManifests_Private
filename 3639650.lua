e884f0916f7ba86e4e929ea27eaf56759c4e9d2e
qiao-lima <qiao-lima@github.com>
2026-04-18 14:50:55 +0800

    Manual Update 3639650

---
+++ 3639650.lua
@@ -0,0 +1,14 @@
+-- 3639650's Lua and Manifest Created by Hubcap Manifest
+-- Kotama and Academy Citadel
+-- Created: March 27, 2026 at 11:26:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3639650, 1, "b1d4d4b116f5be81820cab3d201749d7605a05ea7aa4c67e80887cbd8173894e") -- Kotama and Academy Citadel
+-- MAIN APP DEPOTS
+addappid(3639651, 1, "1d8e7052572c205bad0554d9198e38b66c9e5c7e9633df56ec3fe95b9636f264") -- Depot 3639651
+setManifestid(3639651, "4146038665620294479", 11348262656)
+addappid(3639652, 1, "3046b907177f645936ef93f5f9b17a6383113f9f9f7a4feeac5806c14fc2aacf") -- Depot 3639652
+setManifestid(3639652, "2648779801559231143", 11124368255)
