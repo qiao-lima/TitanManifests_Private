dfddc70f9a78f07e1236f1e5125b37b36896e363
qiao-lima <qiao-lima@github.com>
2026-04-19 22:10:10 +0800

    Manual Update 3748140

---
+++ 3748140.lua
@@ -0,0 +1,16 @@
+-- 3748140's Lua and Manifest Created by Hubcap Manifest
+-- シルと星無きセカイの再栄録
+-- Created: April 18, 2026 at 09:21:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3748140) -- シルと星無きセカイの再栄録
+-- MAIN APP DEPOTS
+addappid(3748141, 1, "8e78741867ad72903e97969628c047e6fb7287bfb67a63b9fb3b5f11169fa530") -- Depot 3748141
+setManifestid(3748141, "5015857287837630655", 1403396432)
+addappid(3748142, 1, "7b637583ae7488624cd6baaa873ff0417358a63bf02a6546d156c2a59408db96") -- Depot 3748142
+setManifestid(3748142, "6931523117933920232", 1184115386)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4519140) --  - DLC
