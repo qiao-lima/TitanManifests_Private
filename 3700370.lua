6b67ce1c34f986ea86e60453644de5dfcaa4d267
qiao-lima <qiao-lima@github.com>
2026-04-19 22:10:10 +0800

    Manual Update 3700370

---
+++ 3700370.lua
@@ -0,0 +1,15 @@
+-- 3700370's Lua and Manifest Created by Hubcap Manifest
+-- A Tragedy in Elvish
+-- Created: April 18, 2026 at 09:26:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3700370) -- A Tragedy in Elvish
+addtoken(3700370, "9861168376806535901")
+-- MAIN APP DEPOTS
+addappid(3700371, 1, "6bc9bed2dc4706819006aa8ec213e7d41968e35e508cd9dfa9d09ff174bc772b") -- Depot 3700371
+setManifestid(3700371, "3638206642063911987", 507398863)
+addappid(3700373, 1, "4c45f7de0192fb511b922bf2d9a54414394ef02a7d30ba4cebeade09f22d933c") -- Depot 3700373
+setManifestid(3700373, "2223365103143114997", 502877794)
