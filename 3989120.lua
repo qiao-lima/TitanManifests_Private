efe90ba788cd1dabfdfd574be36008ad558351d8
qiao-lima <qiao-lima@github.com>
2026-04-19 22:10:12 +0800

    Manual Update 3989120

---
+++ 3989120.lua
@@ -0,0 +1,12 @@
+-- 3989120's Lua and Manifest Created by Hubcap Manifest
+-- Rift Miner
+-- Created: April 18, 2026 at 09:25:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3989120) -- Rift Miner
+-- MAIN APP DEPOTS
+addappid(3989121, 1, "8f5ba0c7eb7172976404255f159ace62aa5ac1a75e2353c58efa551f2f9ba794") -- Depot 3989121
+setManifestid(3989121, "4836301967182682191", 654977117)
