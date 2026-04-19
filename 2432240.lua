1745dd6bc6d34d8db7da87f28dc38145f07a423a
qiao-lima <qiao-lima@github.com>
2026-04-19 22:10:07 +0800

    Manual Update 2432240

---
+++ 2432240.lua
@@ -0,0 +1,15 @@
+-- 2432240's Lua and Manifest Created by Hubcap Manifest
+-- Smash it Wild
+-- Created: April 18, 2026 at 21:25:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2432240) -- Smash it Wild
+-- MAIN APP DEPOTS
+addappid(2432241, 1, "a9a4bdf84b787d55cfb02672958d2d856ea64cbbc89b6f088473b4743c3dff41") -- Depot 2432241
+setManifestid(2432241, "616800251562509372", 459577873)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2432242) -- Depot 2432242 (empty depot)
+-- addappid(2432243) -- Depot 2432243 (empty depot)
