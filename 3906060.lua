02707b302dd980ce669bdb530c3666226b69e547
qiao-lima <qiao-lima@github.com>
2026-04-19 22:10:11 +0800

    Manual Update 3906060

---
+++ 3906060.lua
@@ -0,0 +1,18 @@
+-- 3906060's Lua and Manifest Created by Hubcap Manifest
+-- Echo 91
+-- Created: April 19, 2026 at 06:40:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3906060) -- Echo 91
+-- MAIN APP DEPOTS
+addappid(3906062, 1, "2b453fb461bca1807f3d7e28ff2f0cb192604d9ca223dcaeaf72392b46ac16ae") -- Depot 3906062
+setManifestid(3906062, "1023964567594051051", 5043759955)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
