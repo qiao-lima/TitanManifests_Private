3705b309ccd5ac7eecce572e7945fdc1f7175b40
qiao-lima <qiao-lima@github.com>
2026-04-19 22:16:04 +0800

    Manual Update 4224620

---
+++ 4224620.lua
@@ -0,0 +1,16 @@
+-- 4224620's Lua and Manifest Created by Hubcap Manifest
+-- Treasure Hunter: Echo of War
+-- Created: April 18, 2026 at 09:20:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4224620) -- Treasure Hunter: Echo of War
+-- MAIN APP DEPOTS
+addappid(4224621, 1, "a94198aa56de7b1b4f384dfd2f9ce15d9cb0f98bb4610144d50540bf0ee53815") -- Depot 4224621
+setManifestid(4224621, "4369242855714227784", 810465944)
+addappid(4224622, 1, "5345be1c81354eb7c26a378f34664b4a34a46ca166debba57b6915df5d961731") -- Depot 4224622
+setManifestid(4224622, "8212925261430050663", 848394145)
+addappid(4224623, 1, "0eead083c183a17dad8b2d38c0fd5bada40c1fbc3238554a052f24afd2436da5") -- Depot 4224623
+setManifestid(4224623, "1887932888897146606", 905202530)
