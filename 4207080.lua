46f1d17313f134eda744b5d01e4a09b2d6bad5ca
qiao-lima <qiao-lima@github.com>
2026-04-19 22:16:03 +0800

    Manual Update 4207080

---
+++ 4207080.lua
@@ -0,0 +1,13 @@
+-- 4207080's Lua and Manifest Created by Hubcap Manifest
+-- 愛のあるマナー指導
+-- Created: April 18, 2026 at 09:35:59 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4207080) -- 愛のあるマナー指導
+addtoken(4207080, "5180403315608619237")
+-- MAIN APP DEPOTS
+addappid(4207084, 1, "7d1105dccb69b6c0fa4df0ba04382a0f8eac956388f94c91c79068b3c399781c") -- Depot 4207084
+setManifestid(4207084, "7150535895862428725", 1294050232)
