7a309bbc08958a4ec16190c0b70640a5d386b466
qiao-lima <qiao-lima@github.com>
2026-04-19 22:16:02 +0800

    Manual Update 4159390

---
+++ 4159390.lua
@@ -0,0 +1,12 @@
+-- 4159390's Lua and Manifest Created by Hubcap Manifest
+-- Sex Beach Club
+-- Created: April 18, 2026 at 09:28:59 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4159390) -- Sex Beach Club
+-- MAIN APP DEPOTS
+addappid(4159391, 1, "f1d2e53702c53987ac0cb8e816234cb63319cc03dbc8670db970c564703f980c") -- Depot 4159391
+setManifestid(4159391, "5387884496264474712", 1268971964)
