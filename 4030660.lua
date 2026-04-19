1fe95ad968a92fa9b0dace70d75121a1292b5417
qiao-lima <qiao-lima@github.com>
2026-04-19 22:10:14 +0800

    Manual Update 4030660

---
+++ 4030660.lua
@@ -0,0 +1,17 @@
+-- 4030660's Lua and Manifest Created by Hubcap Manifest
+-- 花城秘事
+-- Created: April 18, 2026 at 09:23:57 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4030660) -- 花城秘事
+-- MAIN APP DEPOTS
+addappid(4030661, 1, "df6fce0618d0c9e0a21f7534ffc3ef3838fe2c8780a8d17273733f8c549da7a4") -- Depot 4030661
+setManifestid(4030661, "8107294469983350187", 1649132946)
+-- DLCS WITH DEDICATED DEPOTS
+--  (AppID: 4513240)
+addappid(4513240)
+addappid(4513240, 1, "e777a1c3fac3c0d7ac61eceb152bc464a5fec463ed454a6d83d5e31ab5aac088") -- Depot 4513240
+setManifestid(4513240, "3245803920824591525", 233369827)
