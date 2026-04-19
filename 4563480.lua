e066391fd19ce11942a03fcba543c94685060994
qiao-lima <qiao-lima@github.com>
2026-04-19 22:16:06 +0800

    Manual Update 4563480

---
+++ 4563480.lua
@@ -0,0 +1,12 @@
+-- 4563480's Lua and Manifest Created by Hubcap Manifest
+-- 粒子战争 Particle Wars
+-- Created: April 19, 2026 at 00:30:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4563480) -- 粒子战争 Particle Wars
+-- MAIN APP DEPOTS
+addappid(4563481, 1, "dd84466f58f0dd57f63b70eb01b2985f5531777409369439b8aac02d291a4c9b") -- Depot 4563481
+setManifestid(4563481, "8181312902567881091", 93990166)
