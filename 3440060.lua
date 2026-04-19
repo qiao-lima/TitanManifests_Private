02302cc5fb0f3201e3432422b2327054072103a8
qiao-lima <qiao-lima@github.com>
2026-04-19 22:10:09 +0800

    Manual Update 3440060

---
+++ 3440060.lua
@@ -0,0 +1,18 @@
+-- 3440060's Lua and Manifest Created by Hubcap Manifest
+-- Cralon
+-- Created: April 19, 2026 at 00:47:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3440060, 1, "ffca6589d0b3b8f03ddafac7b875ee6dde9b1ab9f22ecda117a931c7410192d3") -- Cralon
+-- MAIN APP DEPOTS
+addappid(3440061, 1, "13b02cbdab019503265bf8693a3419d2654b413730c2338e8a48fa46e6c490f8") -- Depot 3440061
+setManifestid(3440061, "719457942484656980", 8091090762)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
