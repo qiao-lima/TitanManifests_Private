12cbf47610ce4da6debeed74746a8aee7e5d89b2
qiao-lima <qiao-lima@github.com>
2026-04-19 22:16:04 +0800

    Manual Update 4353600

---
+++ 4353600.lua
@@ -0,0 +1,14 @@
+-- 4353600's Lua and Manifest Created by Hubcap Manifest
+-- Radiance of Annihilation -Disorder Eclipse-
+-- Created: April 18, 2026 at 20:39:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4353600) -- Radiance of Annihilation -Disorder Eclipse-
+-- MAIN APP DEPOTS
+addappid(4353601, 1, "1bba49148f843983c899586d6286d850833f984cbf88eebb92534d1d0c3afc79") -- Depot 4353601
+setManifestid(4353601, "7006443141277876113", 460485081)
+addappid(4353602, 1, "4e3fb442abc391a9b7c26a15be051cf53634f85c3725286bd2acd63844e22659") -- Depot 4353602
+setManifestid(4353602, "897803632510164389", 19089673)
