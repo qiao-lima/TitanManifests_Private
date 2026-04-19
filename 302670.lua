f13c8e62a57f01ed3b3eac4b3634875365bba4e6
qiao-lima <qiao-lima@github.com>
2026-04-19 22:10:08 +0800

    Manual Update 302670

---
+++ 302670.lua
@@ -0,0 +1,49 @@
+-- 302670's Lua and Manifest Created by Hubcap Manifest
+-- Call to Arms
+-- Created: September 29, 2025 at 03:06:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 11
+-- Total DLCs: 6
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(302670, 1, "f6b48b0db2c1040cd057e3cb5d6199da70bdadc30b2e4dd74d33263e89a11149") -- Call to Arms
+-- MAIN APP DEPOTS
+addappid(302671, 1, "3e65babcddf3e355150640051939bd668679ca9d6a28dbd7f4202d63f7e6855b") -- Call to Arms Content
+setManifestid(302671, "5874641083769419250", 44154452914)
+addappid(302672, 1, "432ed372fbe5cf4a1d8a170402021d29d199bbbabed929fd1069cf895bc855ac") -- French
+setManifestid(302672, "3878693461403278198", 694747)
+addappid(302673, 1, "8bf7411ec6ce26a0017cf39bf6896dcc9af1a7ba7f6d63e3d856ea2757416c86") -- Italian
+setManifestid(302673, "3297551281436826935", 682541)
+addappid(302674, 1, "531c102af53824a44023438a8e9c3b33bea8b74a2143c9ae2a607c329529e9d5") -- German
+setManifestid(302674, "1951706433659989322", 725084)
+addappid(302675, 1, "83cba74766ae1a90e1c63e1b33d14039bb4355026d0141e98539404bebdccc50") -- Spanish
+setManifestid(302675, "3929365054658762053", 680175)
+addappid(302676, 1, "084e98289252950f851b92de3dea17763ecfa23b09e92ca851a36e436d671cb3") -- Russian
+setManifestid(302676, "2458243870272403304", 847994)
+addappid(302677, 1, "e3f4473df15ada8b23932e8e0c60835de254abd33b5fe06f2153b40703073738") -- Chinese
+setManifestid(302677, "7874021940792233110", 650434)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+-- DLCS WITH DEDICATED DEPOTS
+-- Call to Arms - Allied Army (AppID: 475170)
+addappid(475170)
+addtoken(475170, "16148638344371578379")
+addappid(475170, 1, "83373b30c2525b93bb866726e94bf8272b47b258424bb720dbcd20d7d67b654f") -- Call to Arms - Allied Army - Call to Arms - Deluxe Edition (475170) Depot
+setManifestid(475170, "3527328475409059396", 344324445)
+-- Call to Arms - Basic Edition (AppID: 487170)
+addappid(487170)
+addtoken(487170, "9430053684577479859")
+addappid(487170, 1, "f9ba895f81f7169a8a8cc8798a184f192c18884f17fb63a578d66df3585f2532") -- Call to Arms - Basic Edition - Call to Arms - Basic Edition (487170) Depot
+setManifestid(487170, "928611649125190267", 309561093)
+-- Call to Arms - Season Pass (AppID: 561140)
+addappid(561140)
+addappid(561140, 1, "64e9170f08642db2b67673315d4486d0cb4e78c261fd3e95c1df03b17af30291") -- Call to Arms - Season Pass - Call to Arms - Ultimate Edition (561140) Depot
+setManifestid(561140, "1913783994644488003", 1217289014)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(374390) -- Call to Arms - Deluxe Edition
+addappid(487310) -- Call to Arms - German Army
+addtoken(487310, "16438904020536839253")
+addappid(548180) -- Call to Arms - Russian Army
+addtoken(548180, "13066396460508797281")
