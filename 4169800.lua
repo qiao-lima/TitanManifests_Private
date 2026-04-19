7c53cd1e3a396d6996a6f8bce90f90fab7b1a27e
qiao-lima <qiao-lima@github.com>
2026-04-19 22:16:02 +0800

    Manual Update 4169800

---
+++ 4169800.lua
@@ -0,0 +1,12 @@
+-- 4169800's Lua and Manifest Created by Hubcap Manifest
+-- Rulers of Cospaia
+-- Created: April 19, 2026 at 00:59:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4169800) -- Rulers of Cospaia
+-- MAIN APP DEPOTS
+addappid(4169801, 1, "0815558b1a6c11398f64c7f313f60112cad4e28bbe596afec5ce8bd91e490ac6") -- Depot 4169801
+setManifestid(4169801, "7610787808454988383", 737782114)
