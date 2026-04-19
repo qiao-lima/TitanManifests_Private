f7f59b03766d684d870bb8e6e2ed839f160a96ec
qiao-lima <qiao-lima@github.com>
2026-04-19 22:10:07 +0800

    Manual Update 2216600

---
+++ 2216600.lua
@@ -0,0 +1,12 @@
+-- 2216600's Lua and Manifest Created by Hubcap Manifest
+-- Korea Dynasty (조선메타실록)
+-- Created: April 18, 2026 at 11:48:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2216600) -- Korea Dynasty (조선메타실록)
+-- MAIN APP DEPOTS
+addappid(2216601, 1, "1d6d0e05295dc2a4f6d61ce418462fc335e95ce3e41bf907498567c8c6f60be6") -- Depot 2216601
+setManifestid(2216601, "5030896498273153620", 7955776604)
