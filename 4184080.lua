789803ba51918a2604010257cb4939359d6d5f5e
qiao-lima <qiao-lima@github.com>
2026-04-19 22:16:03 +0800

    Manual Update 4184080

---
+++ 4184080.lua
@@ -0,0 +1,12 @@
+-- 4184080's Lua and Manifest Created by Hubcap Manifest
+-- KING of HOLO
+-- Created: April 18, 2026 at 09:30:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4184080) -- KING of HOLO
+-- MAIN APP DEPOTS
+addappid(4184081, 1, "89b26ec588f9d869ad4fb4ede56a42fd369b2ea4f09e44a7081b661219752a02") -- Depot 4184081
+setManifestid(4184081, "5678772646953557601", 228786064)
