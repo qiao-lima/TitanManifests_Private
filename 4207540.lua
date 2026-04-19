bb234dbfdef3178a7546cfa2e3bc02ea2446bedd
qiao-lima <qiao-lima@github.com>
2026-04-19 22:16:03 +0800

    Manual Update 4207540

---
+++ 4207540.lua
@@ -0,0 +1,12 @@
+-- 4207540's Lua and Manifest Created by Hubcap Manifest
+-- 记忆芯片
+-- Created: April 18, 2026 at 09:29:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4207540) -- 记忆芯片
+-- MAIN APP DEPOTS
+addappid(4207541, 1, "332c28b34a9b517660167f91617653edc1cf78ce3917959f52d62ae4b13eab56") -- Depot 4207541
+setManifestid(4207541, "709526298344469016", 187349741)
