88be555bf3b06a12a145bfe8594cdae6db4202dc
qiao-lima <qiao-lima@github.com>
2026-04-19 22:16:05 +0800

    Manual Update 4500970

---
+++ 4500970.lua
@@ -0,0 +1,12 @@
+-- 4500970's Lua and Manifest Created by Hubcap Manifest
+-- The Presence: Ritual
+-- Created: April 19, 2026 at 06:41:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4500970) -- The Presence: Ritual
+-- MAIN APP DEPOTS
+addappid(4500971, 1, "89213553e09bd105958c782c351ed4921f6005ac8cb9548bbeb073b8727f86a2") -- Depot 4500971
+setManifestid(4500971, "5963997567715866881", 9826039736)
