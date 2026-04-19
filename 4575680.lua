2f8e31104163282c913cab84702d8760dc47b11c
qiao-lima <qiao-lima@github.com>
2026-04-19 22:16:07 +0800

    Manual Update 4575680

---
+++ 4575680.lua
@@ -0,0 +1,12 @@
+-- 4575680's Lua and Manifest Created by Hubcap Manifest
+-- Prison Glow
+-- Created: April 18, 2026 at 23:09:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4575680) -- Prison Glow
+-- MAIN APP DEPOTS
+addappid(4575681, 1, "d04bb517b318cb87b8bb748d2bd6009cc08da2692718c5ca59e9856a672ccf68") -- Depot 4575681
+setManifestid(4575681, "7624851441873691711", 404347871)
