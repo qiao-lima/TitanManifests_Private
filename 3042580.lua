9c062b5681ecda9da7faaa4498b10f222f5a2aab
qiao-lima <qiao-lima@github.com>
2026-04-19 22:10:08 +0800

    Manual Update 3042580

---
+++ 3042580.lua
@@ -0,0 +1,12 @@
+-- 3042580's Lua and Manifest Created by Hubcap Manifest
+-- TheMute
+-- Created: April 19, 2026 at 10:05:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3042580) -- TheMute
+-- MAIN APP DEPOTS
+addappid(3042582, 1, "c373306e1cddccbd9af14b642f6f4543e852e2182c0664dbd5a175d9f7737f91") -- Depot 3042582
+setManifestid(3042582, "8957643972490223113", 4911065645)
