68ca38826e78bf86f9b6359e264b077f7214e0b1
qiao-lima <qiao-lima@github.com>
2026-04-19 22:10:11 +0800

    Manual Update 3779390

---
+++ 3779390.lua
@@ -0,0 +1,12 @@
+-- 3779390's Lua and Manifest Created by Hubcap Manifest
+-- Dungeon of Erotic Master Reboot
+-- Created: April 18, 2026 at 14:23:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3779390, 1, "0c5cd2e00784c8bf0a222b3b9b91582866d55520ad449a892a963af2f05eee52") -- Dungeon of Erotic Master Reboot
+-- MAIN APP DEPOTS
+addappid(3779392, 1, "f758e55423074650d6a9ae051eb97068f6a40757d3f4db8cd08d7a5ed94cb2a3") -- Depot 3779392
+setManifestid(3779392, "3269064817763600145", 555987842)
