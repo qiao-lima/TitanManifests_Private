0c8abbfc6bee03e97a2ea738090086b197b081eb
qiao-lima <qiao-lima@github.com>
2026-04-19 22:10:14 +0800

    Manual Update 4112790

---
+++ 4112790.lua
@@ -0,0 +1,12 @@
+-- 4112790's Lua and Manifest Created by Hubcap Manifest
+-- Sacrifice
+-- Created: April 18, 2026 at 15:57:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4112790) -- Sacrifice
+-- MAIN APP DEPOTS
+addappid(4112792, 1, "c71af7a4b5f60b404f2d1f66abff1334d0c4f44945b445420c796e5c06044dd0") -- Depot 4112792
+setManifestid(4112792, "4406548860466913284", 62546148)
