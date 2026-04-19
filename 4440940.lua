34deaa88a9404f856ce292c3cd07797a6637c8ad
qiao-lima <qiao-lima@github.com>
2026-04-19 22:16:05 +0800

    Manual Update 4440940

---
+++ 4440940.lua
@@ -0,0 +1,12 @@
+-- 4440940's Lua and Manifest Created by Hubcap Manifest
+-- Lab: Balance
+-- Created: April 19, 2026 at 06:41:08 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4440940) -- Lab: Balance
+-- MAIN APP DEPOTS
+addappid(4440941, 1, "4ebb187656a3942c3e159e0f95493b0ed86fa34991bf44a7b5b6a92e4934c3b0") -- Depot 4440941
+setManifestid(4440941, "3460457113063115805", 2402583097)
