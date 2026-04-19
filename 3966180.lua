ada282c632d0f1b5964c523487ef766580a09326
qiao-lima <qiao-lima@github.com>
2026-04-19 22:10:12 +0800

    Manual Update 3966180

---
+++ 3966180.lua
@@ -0,0 +1,12 @@
+-- 3966180's Lua and Manifest Created by Hubcap Manifest
+-- 淀み海の溺れ唄
+-- Created: April 18, 2026 at 09:21:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3966180) -- 淀み海の溺れ唄
+-- MAIN APP DEPOTS
+addappid(3966181, 1, "fcdf71f89a5234918d7b098c93d98144d3b75f42f3f612a369dc7ced682c64a6") -- Depot 3966181
+setManifestid(3966181, "9159467540672003209", 887201793)
