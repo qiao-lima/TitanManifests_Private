9f3ddce12cf3c06f4c93a116614b562c1f24c72e
qiao-lima <qiao-lima@github.com>
2026-04-19 22:16:06 +0800

    Manual Update 4510830

---
+++ 4510830.lua
@@ -0,0 +1,20 @@
+-- 4510830's Lua and Manifest Created by Hubcap Manifest
+-- Myth of Empires: Throne
+-- Created: April 18, 2026 at 09:23:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(4510830) -- Myth of Empires: Throne
+-- MAIN APP DEPOTS
+addappid(4510831, 1, "1dd5fcad34c279ed05eaaecc360ace76ff4c575fe11cc6ee345aad65ab8e4462") -- Depot 4510831
+setManifestid(4510831, "4286424559860977574", 60319138071)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") -- .NET 4.7 Redist (Shared from App 228980)
+setManifestid(229006, "1784011429307107530", 83944258)
