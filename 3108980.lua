db369303ed08dad42a9a76e6a7dcb98133de09da
qiao-lima <qiao-lima@github.com>
2026-04-19 22:10:09 +0800

    Manual Update 3108980

---
+++ 3108980.lua
@@ -0,0 +1,20 @@
+-- 3108980's Lua and Manifest Created by Hubcap Manifest
+-- Part-Time Witch
+-- Created: April 18, 2026 at 05:18:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (3 excluded)
+
+-- MAIN APPLICATION
+addappid(3108980) -- Part-Time Witch
+-- MAIN APP DEPOTS
+addappid(3108981, 1, "c2012af5efe84f809a8cddfa507e9958d7a85eec5e1ccfde422bf4cd05efdc67") -- Depot 3108981
+setManifestid(3108981, "4434812088885757670", 783130618)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Part Time Witch - Artbook (AppID: 4082060) - missing depot keys
+-- addappid(4082060)
+-- Part Time Witch - Animations Pack (AppID: 4082070) - missing depot keys
+-- addappid(4082070)
+-- Part Time Witch - Wallpapers Pack (AppID: 4082080) - missing depot keys
+-- addappid(4082080)
