a6d0d09f741257fa0f9bbc566654a2772ed43415
qiao-lima <qiao-lima@github.com>
2026-04-19 22:16:05 +0800

    Manual Update 4495180

---
+++ 4495180.lua
@@ -0,0 +1,12 @@
+-- 4495180's Lua and Manifest Created by Hubcap Manifest
+-- One Dice Dungeon Delve
+-- Created: April 19, 2026 at 00:57:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4495180, 1, "06f81a4ce7cb5da2d661e3b12b2bbc2022cc8bf4ac61b3fb2c53d82a349f8854") -- One Dice Dungeon Delve
+-- MAIN APP DEPOTS
+addappid(4495181, 1, "9e6794e8b78fecc8530072bce039b37f65b43e6975f011d8875e69b41d529bd8") -- Depot 4495181
+setManifestid(4495181, "2711691645014231072", 272764197)
