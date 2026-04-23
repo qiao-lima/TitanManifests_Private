16104c715d9667340998f7c10a4a6e261cc1b0e0
qiao-lima <qiao-lima@github.com>
2026-04-23 20:37:40 +0800

    Manual Update 2858880

---
+++ 2858880.lua
@@ -0,0 +1,12 @@
+-- 2858880's Lua and Manifest Created by Hubcap Manifest
+-- One King Is Enough
+-- Created: April 23, 2026 at 08:32:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2858880) -- One King Is Enough
+-- MAIN APP DEPOTS
+addappid(2858881, 1, "4a488ae6b1715b065431b917b0db6467f61f4b48b109de0942fd3fc5079b562a") -- Depot 2858881
+setManifestid(2858881, "4866970602546306247", 373463819)
