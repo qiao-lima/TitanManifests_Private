55421135c57d84bf1296baa7653cf923b9137631
qiao-lima <qiao-lima@github.com>
2026-04-23 20:54:18 +0800

    Manual Update 4296930

---
+++ 4296930.lua
@@ -0,0 +1,12 @@
+-- 4296930's Lua and Manifest Created by Hubcap Manifest
+-- European Logistics Tycoon
+-- Created: April 21, 2026 at 03:17:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4296930) -- European Logistics Tycoon
+-- MAIN APP DEPOTS
+addappid(4296931, 1, "d466e31b6d0fe8691d222fb88bbeb6664cb780975d15656949e442f7dee5a84f") -- Depot 4296931
+setManifestid(4296931, "2390835855891364386", 429031448)
