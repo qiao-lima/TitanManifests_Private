2ba5e17eb136757b107129285cdc11a5326db7a1
qiao-lima <qiao-lima@github.com>
2026-04-23 20:43:34 +0800

    Manual Update 3670580

---
+++ 3670580.lua
@@ -0,0 +1,12 @@
+-- 3670580's Lua and Manifest Created by Hubcap Manifest
+-- TownsFolk
+-- Created: April 21, 2026 at 05:46:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3670580) -- TownsFolk
+-- MAIN APP DEPOTS
+addappid(3670582, 1, "21e4594be77728507d4ccd2a17ffd855150ae10614884231e7a7374cff834b79") -- Depot 3670582
+setManifestid(3670582, "82646975336373163", 234519716)
