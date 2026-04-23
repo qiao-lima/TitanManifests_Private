a11a02a1fd367f1280c09be2e296de4eb2a6d05b
qiao-lima <qiao-lima@github.com>
2026-04-23 20:54:20 +0800

    Manual Update 4484000

---
+++ 4484000.lua
@@ -0,0 +1,12 @@
+-- 4484000's Lua and Manifest Created by Hubcap Manifest
+-- A Lucky Hunt With Nerida
+-- Created: April 23, 2026 at 06:08:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4484000) -- A Lucky Hunt With Nerida
+-- MAIN APP DEPOTS
+addappid(4484001, 1, "acb0796d736b7f701aa7d2f0009ed70474642da710312ec48517d928ebb60a44") -- Depot 4484001
+setManifestid(4484001, "3716914635109877551", 233872834)
