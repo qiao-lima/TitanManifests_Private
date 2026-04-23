17a28b1353f710565750704f10ae833550659b18
qiao-lima <qiao-lima@github.com>
2026-04-23 20:54:20 +0800

    Manual Update 4481540

---
+++ 4481540.lua
@@ -0,0 +1,14 @@
+-- 4481540's Lua and Manifest Created by Hubcap Manifest
+-- Core Defense 2099
+-- Created: April 20, 2026 at 08:16:36 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4481540) -- Core Defense 2099
+-- MAIN APP DEPOTS
+addappid(4481542, 1, "e4ab6c750a4e4e4f3633039c29182bfe6beb23d269984fd5467a199e59b177c5") -- Depot 4481542
+setManifestid(4481542, "1971334740968505074", 267796424)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4481541) -- Depot 4481541 (empty depot)
