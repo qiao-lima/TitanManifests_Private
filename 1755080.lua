e59e20d621685ada394b4f08861d35c3e898d5c6
qiao-lima <qiao-lima@github.com>
2026-04-23 20:37:37 +0800

    Manual Update 1755080

---
+++ 1755080.lua
@@ -0,0 +1,20 @@
+-- 1755080's Lua and Manifest Created by Hubcap Manifest
+-- Away From Life
+-- Created: April 23, 2026 at 08:33:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(1755080) -- Away From Life
+-- MAIN APP DEPOTS
+addappid(1755081, 1, "79c84849a1a81fa593c55422ad86e6312f057be50635ccb9fb8ddbe88ac5a300") -- Depot 1755081
+setManifestid(1755081, "7224045128999661865", 9450457682)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 117381405)
