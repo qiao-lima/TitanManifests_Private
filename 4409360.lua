44d59d667068e68f1e7c219902d99c7373ad592a
qiao-lima <qiao-lima@github.com>
2026-04-23 20:54:19 +0800

    Manual Update 4409360

---
+++ 4409360.lua
@@ -0,0 +1,12 @@
+-- 4409360's Lua and Manifest Created by Hubcap Manifest
+-- A Lucky Hunt With Sora
+-- Created: April 23, 2026 at 06:07:53 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4409360) -- A Lucky Hunt With Sora
+-- MAIN APP DEPOTS
+addappid(4409361, 1, "251058b513f19ac7a2a36770c4bc0a4f2ef8b9317886fb282a0cca548da83b91") -- Depot 4409361
+setManifestid(4409361, "7541593475936406427", 233871632)
