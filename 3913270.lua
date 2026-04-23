7dba7890662c736458d0f7a6a478b0b4dab75767
qiao-lima <qiao-lima@github.com>
2026-04-23 20:43:36 +0800

    Manual Update 3913270

---
+++ 3913270.lua
@@ -0,0 +1,12 @@
+-- 3913270's Lua and Manifest Created by Hubcap Manifest
+-- Slimeward
+-- Created: April 23, 2026 at 03:26:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3913270) -- Slimeward
+-- MAIN APP DEPOTS
+addappid(3913271, 1, "f1553e06b7231a3e89d68ff23e5ae4af109864593b2122cff4dd8cb6f9aeafa3") -- Depot 3913271
+setManifestid(3913271, "6910960342889126302", 318267196)
