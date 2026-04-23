5265e83a9a87773abceeabc57752eb9887984d36
qiao-lima <qiao-lima@github.com>
2026-04-23 20:37:40 +0800

    Manual Update 3143550

---
+++ 3143550.lua
@@ -0,0 +1,12 @@
+-- 3143550's Lua and Manifest Created by Hubcap Manifest
+-- Liz and Rose's Alchemy Factory
+-- Created: April 23, 2026 at 04:00:58 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3143550) -- Liz and Rose's Alchemy Factory
+-- MAIN APP DEPOTS
+addappid(3143551, 1, "7a96dd1d967de43b52b6e214ac03b37e9b3accb86e4885d22b85b0f0d316643e") -- Depot 3143551
+setManifestid(3143551, "5258097759020690702", 1631691736)
