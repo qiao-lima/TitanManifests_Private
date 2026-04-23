0854d02407e6719353135ba55c5a5c8fd35def6d
qiao-lima <qiao-lima@github.com>
2026-04-23 20:37:38 +0800

    Manual Update 2423650

---
+++ 2423650.lua
@@ -0,0 +1,12 @@
+-- 2423650's Lua and Manifest Created by Hubcap Manifest
+-- 3D PUZZLE - PORT
+-- Created: April 22, 2026 at 20:23:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2423650) -- 3D PUZZLE - PORT
+-- MAIN APP DEPOTS
+addappid(2423651, 1, "20f945bd8916a005a7485f57dec6141cda6999227fbda6a2cfcdc0b1f33c02ea") -- Depot 2423651
+setManifestid(2423651, "676773814501719915", 4560101718)
