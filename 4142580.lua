f47ebe926ad71459ecca3c6a4eb714876f5c83ce
qiao-lima <qiao-lima@github.com>
2026-04-23 20:48:27 +0800

    Manual Update 4142580

---
+++ 4142580.lua
@@ -0,0 +1,19 @@
+-- 4142580's Lua and Manifest Created by Hubcap Manifest
+-- Live Rise!!: 4K Fever
+-- Created: April 20, 2026 at 03:05:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4142580, 1, "bbd032a4f17902d3e9e4cc90864f62d271177b59dc3c299a5bfd5b368b12f496") -- Live Rise!!: 4K Fever
+-- MAIN APP DEPOTS
+addappid(4142581, 1, "4e746b89d3557c57fa34e6e66bff241a9291211cde4c5adef6c0901a401454f1") -- Depot 4142581
+setManifestid(4142581, "7408058431317116760", 407156808)
+addappid(4142582, 1, "7c114d061424fec50ca6a8563df36cf3f0d763ba03868310777c8249f901112e") -- Depot 4142582
+setManifestid(4142582, "7220716134070502740", 701681953)
+-- DLCS WITH DEDICATED DEPOTS
+-- Live Rise 4K Fever - Official Beatmaps Pack (AppID: 4251400)
+addappid(4251400)
+addappid(4251401, 1, "18a398f07f5f3be0fde3454bec98d5a4546c465b0a8e169998ac35ea69935794") -- Live Rise 4K Fever - Official Beatmaps Pack - Depot 4251401
+setManifestid(4251401, "8492721830396555198", 1281802069)
