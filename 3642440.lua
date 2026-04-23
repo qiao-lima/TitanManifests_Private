48148b0b8e1a207f85dee7dafaccb35bcfdf315f
qiao-lima <qiao-lima@github.com>
2026-04-23 20:43:34 +0800

    Manual Update 3642440

---
+++ 3642440.lua
@@ -0,0 +1,16 @@
+-- 3642440's Lua and Manifest Created by Hubcap Manifest
+-- Luxman Moonlit Market
+-- Created: April 19, 2026 at 12:39:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3642440, 1, "3910d1fb03cb8dca9db2fe20e148af48266f68585501b3c0a8e0accd76334ffa") -- Luxman Moonlit Market
+-- MAIN APP DEPOTS
+addappid(3642441, 1, "a633815a239256620a684f27c713a33c68110db0e1298647afb7693e5e2a740f") -- Depot 3642441
+setManifestid(3642441, "2874599807483418929", 469702944)
+addappid(3642442, 1, "8e80f2d6b7772adcf87b4de8f1c6f3ad48233bc7077d8a072007769a9c908d3c") -- Depot 3642442
+setManifestid(3642442, "9066594754098957345", 566916919)
+addappid(3642443, 1, "c06c26a3feb4a645818099685ba99a832c399256c06224f55a165e3c6c3b249d") -- Depot 3642443
+setManifestid(3642443, "1262255997885082820", 439509870)
