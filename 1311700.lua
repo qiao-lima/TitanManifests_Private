4fc2f4b56484a1c01018278424ede4e16e6e009e
qiao-lima <qiao-lima@github.com>
2026-04-23 20:37:36 +0800

    Manual Update 1311700

---
+++ 1311700.lua
@@ -0,0 +1,147 @@
+-- 1311700's Lua and Manifest Created by Hubcap Manifest
+-- 汉武大帝传
+-- Created: March 11, 2026 at 11:31:59 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 22
+-- Total DLCs: 16 (28 excluded)
+-- Shared Depots: 5
+
+-- MAIN APPLICATION
+addappid(1311700, 1, "d4076fbf50f704241dc1abd87d4911d6cfba470aeb267f7f556dc8a7ac780da3") -- 汉武大帝传
+-- MAIN APP DEPOTS
+addappid(1311701, 1, "26e1b002b8fa347b38c1e63240641b748d755290a8208c14a9b4cc012dd275bc") -- 汉武大帝传 Content
+setManifestid(1311701, "2121391258914559193", 275837575)
+-- SHARED DEPOTS (from other apps)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (Shared from App 228980)
+setManifestid(228983, "8124929965194586177", 19265607)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- VC 2012 Redist (Shared from App 228980)
+setManifestid(228984, "2547553897526095397", 13742505)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- VC 2013 Redist (Shared from App 228980)
+setManifestid(228985, "3966345552745568756", 13699237)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+--  -  (AppID: 1414540)
+addappid(1414540)
+addappid(1414540, 1, "e5e127e2a851a0be5184502cbc6c8193ef1aa37f9de8e60ce895da07c6b15dc4") --  -  - 汉武大帝传 - 内容追加福利大礼包 (1414540) 个 Depot
+setManifestid(1414540, "7382413270517288458", 217964281)
+--  (AppID: 1417810)
+addappid(1417810)
+addappid(1417810, 1, "e62faba73d461c829c8b0567a69ec1b8a60510bb8261c4a12f94746c1427e43a") -- （非必需购买项）神龙天御礼包 (1417810) 个 Depot
+setManifestid(1417810, "5692429604758009249", 27455)
+-- --DLC (AppID: 1418710)
+addappid(1418710)
+addappid(1418710, 1, "76e27cd04c5a10f0e09e3eca66ea690f0c1fcfc1cd8d17adfe41746955f18c96") -- --DLC - 汉武大帝传 - 崛起之路 (1418710) 个 Depot
+setManifestid(1418710, "2863427958183994901", 23490)
+-- - (AppID: 2419400)
+addappid(2419400)
+addappid(2419400, 1, "5f2301211980a01874bc242813bc3a43a4c2f3b079dd8fa480e881b21b23a6a9") -- - - Depot 2419400
+setManifestid(2419400, "4592640076181216830", 8343)
+-- - (AppID: 2509750)
+addappid(2509750)
+addappid(2509750, 1, "159dee3cdeb01eeb8e5e7dc3ebce5fac3640af5aaf869b646e34185136dd7a69") -- - - Depot 2509750
+setManifestid(2509750, "8656812437192293338", 16932)
+-- -() (AppID: 2509760)
+addappid(2509760)
+addappid(2509760, 1, "7cef29d59a4b73a340de8a6e6c2f354df45764ab4eb58aea0e8afd9ca13f294f") -- -() - Depot 2509760
+setManifestid(2509760, "4855984171804785535", 16932)
+-- - (AppID: 2509770)
+addappid(2509770)
+addappid(2509770, 1, "7ae435292928f397b0466646406b5d328e737c6c9925a891aa87753ea6567bd1") -- - - Depot 2509770
+setManifestid(2509770, "5670267496673178614", 16932)
+-- -DLC (AppID: 2524840)
+addappid(2524840)
+addappid(2524840, 1, "f6295857aaf39fe564387b38c511f1d81aab4bf278d5c4963f630129245a0152") -- -DLC - Depot 2524840
+setManifestid(2524840, "8449604875137942949", 16932)
+-- -DLC (AppID: 3095470)
+addappid(3095470)
+addappid(3095470, 1, "d1b1b99c5df4468df638f2e74fcf50828b9b5337837a0fb823c786aa56830985") -- -DLC - Depot 3095470
+setManifestid(3095470, "3872302842777983444", 16932)
+-- - DLC2 (AppID: 3594630)
+addappid(3594630)
+addappid(3594630, 1, "c2a0d1087f283ad5d35a68cb69b55bffa794b829529ae5dd28a5829d971c2ed6") -- - DLC2 - Depot 3594630
+setManifestid(3594630, "6114578488581410897", 20238)
+-- - DLC3 (AppID: 3594640)
+addappid(3594640)
+addappid(3594640, 1, "7878d873a46f3dd6cc6ef3bb28dff171197ae3f226dc43d319a8bc6beadbbc9a") -- - DLC3 - Depot 3594640
+setManifestid(3594640, "3862457746359805261", 17544)
+-- - DLC4 (AppID: 3607760)
+addappid(3607760)
+addappid(3607760, 1, "9ee3fd63156c19fa17a563b7e34f8dcc304cffde7e721bbec54469947c9d56c8") -- - DLC4 - Depot 3607760
+setManifestid(3607760, "310324433665678485", 20446)
+-- -DLC (AppID: 3615570)
+addappid(3615570)
+addappid(3615570, 1, "b676cdc325705829036088bcce255efd28958577890a25053b3aae83596b532c") -- -DLC - Depot 3615570
+setManifestid(3615570, "2879675458631472167", 16932)
+-- - DLC1 (AppID: 3615930)
+addappid(3615930)
+addappid(3615930, 1, "d15496b60ed129484df1d7945826c99f00277b163584b5d8c29d9ea53af644d8") -- - DLC1 - Depot 3615930
+setManifestid(3615930, "3093490433548909446", 20446)
+-- DLC-D (AppID: 3638460)
+addappid(3638460)
+addappid(3638460, 1, "cdcd00117f8002c4439291b9b7170b07d598a53a4ff0bfa983f230bbac4918df") -- DLC-D - Depot 3638460
+setManifestid(3638460, "8885020991214573060", 37686)
+-- DLC-Jack (AppID: 3638470)
+addappid(3638470)
+addappid(3638470, 1, "970438c1ca0c114d328d3fbbf6aebfcb45a4c20b6d3d113ed20533078bb734ba") -- DLC-Jack - Depot 3638470
+setManifestid(3638470, "4993256269102892597", 32558)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- - (AppID: 2056850) - missing depot keys
+-- addappid(2056850)
+-- - (AppID: 2509780) - missing depot keys
+-- addappid(2509780)
+-- -DLC (AppID: 3615580) - missing depot keys
+-- addappid(3615580)
+-- -1 (AppID: 3647990) - missing depot keys
+-- addappid(3647990)
+-- -1 (AppID: 3648000) - missing depot keys
+-- addappid(3648000)
+-- -1 (AppID: 3648010) - missing depot keys
+-- addappid(3648010)
+-- -1 (AppID: 3648020) - missing depot keys
+-- addappid(3648020)
+-- -1-D (AppID: 3648030) - missing depot keys
+-- addappid(3648030)
+-- -2-J (AppID: 3648040) - missing depot keys
+-- addappid(3648040)
+-- -1 (AppID: 3666430) - missing depot keys
+-- addappid(3666430)
+-- - (AppID: 3666440) - missing depot keys
+-- addappid(3666440)
+-- -  60 (AppID: 3667300) - missing depot keys
+-- addappid(3667300)
+-- -  6 (AppID: 3667310) - missing depot keys
+-- addappid(3667310)
+-- -  30 (AppID: 3667320) - missing depot keys
+-- addappid(3667320)
+-- -DLC1 (AppID: 3679640) - missing depot keys
+-- addappid(3679640)
+-- -DLC2 (AppID: 3679650) - missing depot keys
+-- addappid(3679650)
+-- -DLC (AppID: 3762850) - missing depot keys
+-- addappid(3762850)
+-- -DLC (AppID: 3762860) - missing depot keys
+-- addappid(3762860)
+-- -DLC (AppID: 3835310) - missing depot keys
+-- addappid(3835310)
+-- -DLC (AppID: 3835320) - missing depot keys
+-- addappid(3835320)
+-- -DLC (AppID: 3926780) - missing depot keys
+-- addappid(3926780)
+-- -DLC (AppID: 3926790) - missing depot keys
+-- addappid(3926790)
+-- - - DLC (AppID: 4205930) - missing depot keys
+-- addappid(4205930)
+-- - - DLC (AppID: 4205940) - missing depot keys
+-- addappid(4205940)
+-- -DLC (AppID: 4305080) - missing depot keys
+-- addappid(4305080)
+-- -DLC (AppID: 4305090) - missing depot keys
+-- addappid(4305090)
+-- -2026 (AppID: 4388530) - missing depot keys
+-- addappid(4388530)
+-- - (AppID: 4388540) - missing depot keys
+-- addappid(4388540)
