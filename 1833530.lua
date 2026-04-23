5dc0708ffa3a48ae7ac75ee70e8bfd9ab8984cec
qiao-lima <qiao-lima@github.com>
2026-04-23 20:37:38 +0800

    Manual Update 1833530

---
+++ 1833530.lua
@@ -0,0 +1,12 @@
+-- 1833530's Lua and Manifest Created by Hubcap Manifest
+-- Beatus Creation Solitaire
+-- Created: April 23, 2026 at 06:39:58 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1833530) -- Beatus Creation Solitaire
+-- MAIN APP DEPOTS
+addappid(1833531, 1, "f33ab1f1e89a83be1ff97e9131f43362f34f8f533cf71d0146e9d416f87997e1") -- Depot 1833531
+setManifestid(1833531, "607166556064618973", 1759240177)
