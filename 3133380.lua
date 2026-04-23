d73f05e936b7ac1e8fe0e5515f3d8fecd39b2cdb
qiao-lima <qiao-lima@github.com>
2026-04-23 20:37:40 +0800

    Manual Update 3133380

---
+++ 3133380.lua
@@ -0,0 +1,12 @@
+-- 3133380's Lua and Manifest Created by Hubcap Manifest
+-- Profiteer
+-- Created: April 20, 2026 at 06:57:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3133380) -- Profiteer
+-- MAIN APP DEPOTS
+addappid(3133381, 1, "9cca26624412c82ebbffeb8e7a436702bace549fc5ef01988b6f0322cab9c6a4") -- Depot 3133381
+setManifestid(3133381, "6901799373401094993", 279340147)
