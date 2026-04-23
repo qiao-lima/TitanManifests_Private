114bd5780b185c891709a54159abea676bce3b6f
qiao-lima <qiao-lima@github.com>
2026-04-23 20:43:34 +0800

    Manual Update 3565040

---
+++ 3565040.lua
@@ -0,0 +1,18 @@
+-- 3565040's Lua and Manifest Created by Hubcap Manifest
+-- Rumbral
+-- Created: April 23, 2026 at 06:16:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3565040, 1, "449b955077ffff2581d3d068e37dd504894703892963264c0631b4260335a475") -- Rumbral
+-- MAIN APP DEPOTS
+addappid(3565041, 1, "ec83358d6a3580a2779f2f17a81782096e215d89accf8afe53bfc3c5080c588a") -- Depot 3565041
+setManifestid(3565041, "1629386354387062171", 646469912)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
