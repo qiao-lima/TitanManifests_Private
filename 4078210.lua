9c67fcb1b6e3f113cdb2da1417193b74878b4bb5
qiao-lima <qiao-lima@github.com>
2026-04-23 20:48:26 +0800

    Manual Update 4078210

---
+++ 4078210.lua
@@ -0,0 +1,12 @@
+-- 4078210's Lua and Manifest Created by Hubcap Manifest
+-- Save My Scrap
+-- Created: April 22, 2026 at 05:28:03 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4078210) -- Save My Scrap
+-- MAIN APP DEPOTS
+addappid(4078211, 1, "87ee7ce012a04d21a84a37d95623f441b484a62c4290367def8ff15443d5e7b2") -- Depot 4078211
+setManifestid(4078211, "1941034592466185179", 1516609237)
