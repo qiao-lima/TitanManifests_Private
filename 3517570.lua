043e45223efc40a32abd6658a2f81fdf0fcccdbc
qiao-lima <qiao-lima@github.com>
2026-04-23 20:43:33 +0800

    Manual Update 3517570

---
+++ 3517570.lua
@@ -0,0 +1,12 @@
+-- 3517570's Lua and Manifest Created by Hubcap Manifest
+-- Village Flow
+-- Created: April 23, 2026 at 08:33:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3517570) -- Village Flow
+-- MAIN APP DEPOTS
+addappid(3517571, 1, "d97e22e04acb2c728a1bdf9b5c186a420b6fd96d8a553bfff75aa46c2ac26bce") -- Depot 3517571
+setManifestid(3517571, "1228275090451116269", 114762612)
