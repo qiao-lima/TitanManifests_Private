faf94cdfbaf88872f089322e9d8069aa4026fadf
qiao-lima <qiao-lima@github.com>
2026-04-23 20:48:26 +0800

    Manual Update 4077830

---
+++ 4077830.lua
@@ -0,0 +1,12 @@
+-- 4077830's Lua and Manifest Created by Hubcap Manifest
+-- A Lucky Hunt With Elara
+-- Created: April 22, 2026 at 21:35:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4077830) -- A Lucky Hunt With Elara
+-- MAIN APP DEPOTS
+addappid(4077831, 1, "9065ec738f7e16364cbbeb9be36973a00d1bad2258b004b67f7857da84a11770") -- Depot 4077831
+setManifestid(4077831, "5431490184979083023", 232964243)
