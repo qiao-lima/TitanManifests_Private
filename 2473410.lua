4c2e363e98775761445f964a7d5ec73b8128a25a
qiao-lima <qiao-lima@github.com>
2026-04-23 20:37:39 +0800

    Manual Update 2473410

---
+++ 2473410.lua
@@ -0,0 +1,14 @@
+-- 2473410's Lua and Manifest Created by Hubcap Manifest
+-- Deckbane
+-- Created: April 20, 2026 at 08:33:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2473410) -- Deckbane
+-- MAIN APP DEPOTS
+addappid(2473412, 1, "88b36a01fa352ac325f3d50580daf206a65793f8cfcbc977b1d60137825ae763") -- Depot 2473412
+setManifestid(2473412, "4367461537622940286", 393080666)
+addappid(2473413, 1, "baaec9d99817a30fc45db4639f040168c7ff2f6e2c9ff3dcba1da1a47e095605") -- Depot 2473413
+setManifestid(2473413, "5388986639623197044", 418390049)
