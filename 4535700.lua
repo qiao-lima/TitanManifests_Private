c92225459a2e18f5630486f8a6454fbda83d0a84
qiao-lima <qiao-lima@github.com>
2026-04-23 20:54:21 +0800

    Manual Update 4535700

---
+++ 4535700.lua
@@ -0,0 +1,12 @@
+-- 4535700's Lua and Manifest Created by Hubcap Manifest
+-- ESKAPI2 7カイキ
+-- Created: April 20, 2026 at 07:08:59 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4535700) -- ESKAPI2 7カイキ
+-- MAIN APP DEPOTS
+addappid(4535701, 1, "b5ab25e277234005f7e93354ae438a7c4d78079f764e5fe6c07beefbfad92729") -- Depot 4535701
+setManifestid(4535701, "8389314357473072458", 871990066)
