64b272e7f6ba6c68bbe579e525c673eb73ae6660
qiao-lima <qiao-lima@github.com>
2026-04-23 20:48:25 +0800

    Manual Update 3981670

---
+++ 3981670.lua
@@ -0,0 +1,12 @@
+-- 3981670's Lua and Manifest Created by Hubcap Manifest
+-- The Demon King's Daughters
+-- Created: April 22, 2026 at 14:58:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3981670) -- The Demon King's Daughters
+-- MAIN APP DEPOTS
+addappid(3981671, 1, "7850d42699135ee28c810ee6bb0506ae27f20daa79de9362bc12234b3a4b8093") -- Depot 3981671
+setManifestid(3981671, "4888510628969193461", 352073662)
