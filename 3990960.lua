2ff64797899af9f4ff021bdd113c6f9020fff7f1
qiao-lima <qiao-lima@github.com>
2026-04-23 20:48:25 +0800

    Manual Update 3990960

---
+++ 3990960.lua
@@ -0,0 +1,12 @@
+-- 3990960's Lua and Manifest Created by Hubcap Manifest
+-- Puzzle Bobble Everybubble!
+-- Created: April 23, 2026 at 02:01:59 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3990960) -- Puzzle Bobble Everybubble!
+-- MAIN APP DEPOTS
+addappid(3990961, 1, "dfa72cab78ab731f73865eb063e6ff13d28930e7ade3d0d929265ee837938525") -- Depot 3990961
+setManifestid(3990961, "3508847760102271685", 2459886474)
