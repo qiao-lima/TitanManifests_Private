3da9103c1f8b34964367867a7e683613ec6e1a4b
qiao-lima <qiao-lima@github.com>
2026-04-23 20:37:37 +0800

    Manual Update 1449110

---
+++ 1449110.lua
@@ -0,0 +1,35 @@
+-- 1449110's Lua and Manifest Created by Hubcap Manifest
+-- The Outer Worlds 2
+-- Created: March 19, 2026 at 05:38:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 6
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1449110, 1, "96501dedb573e5800c3f202d6b726f4604e982ad55e8d2cba564275bd4b5a581") -- The Outer Worlds 2
+-- MAIN APP DEPOTS
+addappid(1449111, 1, "1691760a5a08bde773f0880b6741283070c2c3931c41a09cab6682e29171c563") -- Depot 1449111
+setManifestid(1449111, "1820823336122851894", 82422722672)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- The Outer Worlds 2  Digital Artbook  Soundtrack (AppID: 3729850)
+addappid(3729850)
+addtoken(3729850, "13872637775158194224")
+addappid(3729850, 1, "5de7b19426b9195dd06c2774cb6432657fb1a818723432602636beb88dce3dd4") -- The Outer Worlds 2  Digital Artbook  Soundtrack - Depot 3729850
+setManifestid(3729850, "7345451025778384948", 7934669657)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3729860) -- The Outer Worlds 2 - Corporate Appreciation Premium Pack
+addtoken(3729860, "16691061149821772499")
+addappid(3729880) -- The Outer Worlds 2 - DLC Pass 
+addtoken(3729880, "4577848091489489557")
+addappid(3729910) -- The Outer Worlds 2 - Commander Zanes Battle Pack
+addtoken(3729910, "7170877342749329240")
+addappid(3938110) -- The Outer Worlds 2 Pet Tie-in
+addappid(4055350) -- The Outer Worlds 2 Pet Tie-in 2
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(1449112) -- Depot 1449112 (empty depot)
