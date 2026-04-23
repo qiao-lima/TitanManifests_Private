97f315ff8f9dd9235337c6600f791c6fd260c92c
qiao-lima <qiao-lima@github.com>
2026-04-23 20:43:35 +0800

    Manual Update 3744430

---
+++ 3744430.lua
@@ -0,0 +1,18 @@
+-- 3744430's Lua and Manifest Created by Hubcap Manifest
+-- Together: Moon Escape
+-- Created: April 23, 2026 at 04:20:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3744430) -- Together: Moon Escape
+-- MAIN APP DEPOTS
+addappid(3744431, 1, "b38f5da2a4b062624f91a54ec7b6bde5de2b23e5218c701c18f12ac21fa09125") -- Depot 3744431
+setManifestid(3744431, "6498484321647963135", 13399205459)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
