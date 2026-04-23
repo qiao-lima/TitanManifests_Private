094fc4ff82b8b7721f131ac16d1e64b5fc704b38
qiao-lima <qiao-lima@github.com>
2026-04-23 20:48:26 +0800

    Manual Update 4062570

---
+++ 4062570.lua
@@ -0,0 +1,12 @@
+-- 4062570's Lua and Manifest Created by Hubcap Manifest
+-- A Spooky Hunt With Malvina
+-- Created: April 23, 2026 at 06:07:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4062570) -- A Spooky Hunt With Malvina
+-- MAIN APP DEPOTS
+addappid(4062571, 1, "bf8a974911ce7e08c841e1a807f99d3cab5300a748ed44d4bd9dd11da94b41be") -- Depot 4062571
+setManifestid(4062571, "6760022190987996381", 231186182)
