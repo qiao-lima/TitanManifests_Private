962ee93582aa2792d62f69cf8e3157246bed8f5d
qiao-lima <qiao-lima@github.com>
2026-04-23 20:43:33 +0800

    Manual Update 3371420

---
+++ 3371420.lua
@@ -0,0 +1,12 @@
+-- 3371420's Lua and Manifest Created by Hubcap Manifest
+-- 吸血鬼杀人事件
+-- Created: April 22, 2026 at 18:42:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3371420) -- 吸血鬼杀人事件
+-- MAIN APP DEPOTS
+addappid(3371421, 1, "c3e82750a44a3c1b86321285c7a7e2a71ad25b5031f9dc52f86f2253d121bdf3") -- Depot 3371421
+setManifestid(3371421, "7169404661892250056", 373577388)
