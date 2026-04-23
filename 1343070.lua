0e2a8a855c2900d45fc84e038e2e59a912f65315
qiao-lima <qiao-lima@github.com>
2026-04-23 20:18:35 +0800

    Manual Update 1343070

---
+++ 1343070.lua
@@ -0,0 +1,22 @@
+-- 1343070's Lua and Manifest Created by Hubcap Manifest
+-- Call to Arms: Panzer Elite
+-- Created: March 17, 2026 at 18:19:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1343070, 1, "a2935f17fec75548150b0912b2c7be8fe4625d2bf3ce14efe20d1571b211da32") -- Call to Arms: Panzer Elite
+-- MAIN APP DEPOTS
+addappid(1343071, 1, "b7016d2b047bbb70ab70d41f3ee1aa698feaeed8ec18f88f59eb81ee7f5f4bab") -- Depot 1343071
+setManifestid(1343071, "7311418281636938020", 84307304456)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3903700) -- Call to Arms Panzer Elite - Deluxe Edition
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3903700) -- Depot 3903700 (empty depot)
