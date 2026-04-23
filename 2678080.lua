00ddb09aff270f4f78ab4f4e4495ae0870c83e75
qiao-lima <qiao-lima@github.com>
2026-04-23 20:37:39 +0800

    Manual Update 2678080

---
+++ 2678080.lua
@@ -0,0 +1,20 @@
+-- 2678080's Lua and Manifest Created by Hubcap Manifest
+-- Tides of Tomorrow
+-- Created: April 23, 2026 at 05:37:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2678080) -- Tides of Tomorrow
+-- MAIN APP DEPOTS
+addappid(2678081, 1, "e3ad26ed33c39f69df7e2b6fe575021a59b655ac1402a64641bb3de639b88e26") -- Depot 2678081
+setManifestid(2678081, "382889893051077633", 11693707445)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4212570) -- Tides of Tomorrow Delta Agent Pack
