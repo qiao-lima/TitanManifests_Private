ef0a6fbe868a2ddfe7adf243dc6cc288382c39bc
qiao-lima <qiao-lima@github.com>
2026-04-23 20:43:35 +0800

    Manual Update 3780950

---
+++ 3780950.lua
@@ -0,0 +1,16 @@
+-- 3780950's Lua and Manifest Created by Hubcap Manifest
+-- Chipmatic
+-- Created: April 20, 2026 at 13:04:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3780950) -- Chipmatic
+-- MAIN APP DEPOTS
+addappid(3780951, 1, "b95dc482fd38c978aedd3ed86094725cdd027e5731d479e1c2d25856c616c900") -- Depot 3780951
+setManifestid(3780951, "8097534265617795632", 60634446)
+addappid(3780952, 1, "80ae64fb996947ef4806d6436a0506437383395eff5091af3cc72dd07bfb68fb") -- Depot 3780952
+setManifestid(3780952, "4198815862285273699", 60674647)
+addappid(3780953, 1, "bbfd318ae88f04eb7266ca1eda920f8557f1015f4ed700efa158ce85ba4a31a7") -- Depot 3780953
+setManifestid(3780953, "6843715410145506906", 343297302)
