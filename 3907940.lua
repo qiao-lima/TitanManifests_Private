bc516c8642d0f4dae796fc7fb5d7351dd83607a3
qiao-lima <qiao-lima@github.com>
2026-04-23 20:43:36 +0800

    Manual Update 3907940

---
+++ 3907940.lua
@@ -0,0 +1,14 @@
+-- 3907940's Lua and Manifest Created by Hubcap Manifest
+-- MMO98
+-- Created: April 20, 2026 at 07:45:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3907940) -- MMO98
+-- MAIN APP DEPOTS
+addappid(3907941, 1, "677845fe225f4f310ac2e0c20e2700eec2b852b071751cbbcbf44b1fad360aad") -- Depot 3907941
+setManifestid(3907941, "3565353750196707112", 288755043)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4510400) -- MMO98 - Supporter Pack
