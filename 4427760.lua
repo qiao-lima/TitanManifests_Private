ade99d38238ccddf415210c55d17303c8413cc48
qiao-lima <qiao-lima@github.com>
2026-04-23 20:54:20 +0800

    Manual Update 4427760

---
+++ 4427760.lua
@@ -0,0 +1,12 @@
+-- 4427760's Lua and Manifest Created by Hubcap Manifest
+-- SoulBite
+-- Created: April 20, 2026 at 08:31:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4427760, 1, "2bb6f909ba770c0d30d2f8c6caa3f130622f5143307dee0ac2a240805ca173ff") -- SoulBite
+-- MAIN APP DEPOTS
+addappid(4427761, 1, "abc4fe66e47e6751bf782f4a1c63f37530584669d7403602598c243a4c53c234") -- Depot 4427761
+setManifestid(4427761, "2713668261552724829", 163521408)
