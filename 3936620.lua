8c99a07b2674c16c073436a00e61f5c0179059fe
qiao-lima <qiao-lima@github.com>
2026-04-23 20:43:36 +0800

    Manual Update 3936620

---
+++ 3936620.lua
@@ -0,0 +1,12 @@
+-- 3936620's Lua and Manifest Created by Hubcap Manifest
+-- 金泉风土记
+-- Created: April 20, 2026 at 06:20:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3936620) -- 金泉风土记
+-- MAIN APP DEPOTS
+addappid(3936621, 1, "1e2e119e1c9d26e93170afe8cec32160d77583885cbfdd3ecd10e89b239bdc20") -- Depot 3936621
+setManifestid(3936621, "5056601697219196522", 980774619)
