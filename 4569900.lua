88a6023a8324d8445137bbbfb18d16ef577ff8b1
qiao-lima <qiao-lima@github.com>
2026-04-23 20:54:21 +0800

    Manual Update 4569900

---
+++ 4569900.lua
@@ -0,0 +1,12 @@
+-- 4569900's Lua and Manifest Created by Hubcap Manifest
+-- Creepytape Rewind - Not From Here
+-- Created: April 19, 2026 at 15:15:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4569900) -- Creepytape Rewind - Not From Here
+-- MAIN APP DEPOTS
+addappid(4569901, 1, "fb64722322f76b78e53531f48df97bab2a5ac15603a0182cba4f8663587edd9a") -- Depot 4569901
+setManifestid(4569901, "88827387810923374", 2524439999)
