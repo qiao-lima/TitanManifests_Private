36fd55d7031413c8ffc653ff17b205b87c0097cc
qiao-lima <qiao-lima@github.com>
2026-04-23 20:48:27 +0800

    Manual Update 4136410

---
+++ 4136410.lua
@@ -0,0 +1,12 @@
+-- 4136410's Lua and Manifest Created by Hubcap Manifest
+-- Idle Rat: Island Digger
+-- Created: April 23, 2026 at 06:33:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4136410) -- Idle Rat: Island Digger
+-- MAIN APP DEPOTS
+addappid(4136411, 1, "055bedbbbc1b42732e57327d81840fc1e4018bb2db0da2fb380fcd7ad5efa0bb") -- Depot 4136411
+setManifestid(4136411, "5695932178424636988", 178723516)
