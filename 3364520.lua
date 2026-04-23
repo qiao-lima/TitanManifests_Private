504617e7de466f03d058be897cb8a8ccb9243e69
qiao-lima <qiao-lima@github.com>
2026-04-23 20:37:41 +0800

    Manual Update 3364520

---
+++ 3364520.lua
@@ -0,0 +1,12 @@
+-- 3364520's Lua and Manifest Created by Hubcap Manifest
+-- Scenes from the Heart - Part 1
+-- Created: April 22, 2026 at 20:22:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3364520) -- Scenes from the Heart - Part 1
+-- MAIN APP DEPOTS
+addappid(3364521, 1, "4b582e9646ea7397e6aad1104e90eadebd5d85998d9a7117d5538ae05e5a13b8") -- Depot 3364521
+setManifestid(3364521, "1065977604173156909", 810570707)
