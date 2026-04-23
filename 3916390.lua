0951c15f652821509acc4fb46fa77a71fae6f0e5
qiao-lima <qiao-lima@github.com>
2026-04-23 20:43:36 +0800

    Manual Update 3916390

---
+++ 3916390.lua
@@ -0,0 +1,12 @@
+-- 3916390's Lua and Manifest Created by Hubcap Manifest
+-- Scream under the house
+-- Created: April 22, 2026 at 18:46:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3916390) -- Scream under the house
+-- MAIN APP DEPOTS
+addappid(3916397, 1, "e03ad1cac21b78b8930a6ad6ad9da966d5050f11b36a93909075104907b639f4") -- Depot 3916397
+setManifestid(3916397, "1742369855462479299", 3060721180)
