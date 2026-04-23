2347ef7f215640c3f5acddeb65f831693215154b
qiao-lima <qiao-lima@github.com>
2026-04-23 20:48:27 +0800

    Manual Update 4165310

---
+++ 4165310.lua
@@ -0,0 +1,14 @@
+-- 4165310's Lua and Manifest Created by Hubcap Manifest
+-- Loot Tycoon
+-- Created: April 20, 2026 at 04:03:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4165310) -- Loot Tycoon
+-- MAIN APP DEPOTS
+addappid(4165311, 1, "9153920d3d36325c625304683a68bf9e8aecff17ada098341cf9622715893799") -- Depot 4165311
+setManifestid(4165311, "7763694814872088170", 18469048587)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4604960) -- Loot Tycoon - Supporter Pack
