7b7a2d025b810924ddda7edd3e67b3cd3275af2f
qiao-lima <qiao-lima@github.com>
2026-04-23 20:37:38 +0800

    Manual Update 2134850

---
+++ 2134850.lua
@@ -0,0 +1,12 @@
+-- 2134850's Lua and Manifest Created by Hubcap Manifest
+-- Space Drilling Station
+-- Created: April 20, 2026 at 12:45:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2134850) -- Space Drilling Station
+-- MAIN APP DEPOTS
+addappid(2134851, 1, "fbcfa9b822ad3aedd5dc35f0241b9945687490ffd25802a85ab4333b6f54b630") -- Depot 2134851
+setManifestid(2134851, "4147583236792475471", 1704220946)
