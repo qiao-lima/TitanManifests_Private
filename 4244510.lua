85d7c702ea6e1c4403ef868b2872844ac9e23df4
qiao-lima <qiao-lima@github.com>
2026-04-23 20:48:28 +0800

    Manual Update 4244510

---
+++ 4244510.lua
@@ -0,0 +1,14 @@
+-- 4244510's Lua and Manifest Created by Hubcap Manifest
+-- Pratfall
+-- Created: April 20, 2026 at 18:09:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4244510) -- Pratfall
+-- MAIN APP DEPOTS
+addappid(4244511, 1, "f51cc41519216d4769c3d179a9ca0908c96de20df61c3a4b5e461f27e8a75dec") -- Depot 4244511
+setManifestid(4244511, "3737878809719748459", 761044084)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4327880) -- Pratfall - Supporter Pack
