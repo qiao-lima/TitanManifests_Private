25143feca3e1d23dd4cc4b4f0463f0464d77531d
qiao-lima <qiao-lima@github.com>
2026-04-23 20:43:34 +0800

    Manual Update 3551620

---
+++ 3551620.lua
@@ -0,0 +1,12 @@
+-- 3551620's Lua and Manifest Created by Hubcap Manifest
+-- Kiss of Sin: Rewinding Flame
+-- Created: April 22, 2026 at 05:28:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3551620) -- Kiss of Sin: Rewinding Flame
+-- MAIN APP DEPOTS
+addappid(3551621, 1, "839ce5f6923feb1be0158bcd4f28dfd3926af5b6b34b36d1daeb912322536407") -- Depot 3551621
+setManifestid(3551621, "8967885681432546798", 1691714205)
