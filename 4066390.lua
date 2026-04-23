4f1969fbeb503c31528b2520c4b431bb40808480
qiao-lima <qiao-lima@github.com>
2026-04-23 20:48:26 +0800

    Manual Update 4066390

---
+++ 4066390.lua
@@ -0,0 +1,12 @@
+-- 4066390's Lua and Manifest Created by Hubcap Manifest
+-- City States: Medieval
+-- Created: April 22, 2026 at 18:47:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4066390) -- City States: Medieval
+-- MAIN APP DEPOTS
+addappid(4066391, 1, "c288cc34c2f25c8446639f41562a8cdcf147bcff6889e1c84e5b55e993cb546d") -- Depot 4066391
+setManifestid(4066391, "8617471205332012295", 5020148004)
