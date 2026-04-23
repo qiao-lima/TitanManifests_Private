54c75b82ccef1c6de1c1933fc868c9c436e1dae7
qiao-lima <qiao-lima@github.com>
2026-04-23 20:54:20 +0800

    Manual Update 4491840

---
+++ 4491840.lua
@@ -0,0 +1,14 @@
+-- 4491840's Lua and Manifest Created by Hubcap Manifest
+-- 我們的蜂狂人生
+-- Created: April 21, 2026 at 02:18:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4491840) -- 我們的蜂狂人生
+-- MAIN APP DEPOTS
+addappid(4491841, 1, "39e7de516b990dc06e897b4318d74eb3827c5ca20172c09882775def2ecb0704") -- Depot 4491841
+setManifestid(4491841, "7363707923391878506", 1381270125)
+addappid(4491842, 1, "2c2a63c6786800e3256146c608adcf6d6de4a9dbe363f9c7e98399a3c3c4a2bf") -- Depot 4491842
+setManifestid(4491842, "2701529860185834126", 1397773945)
