169ff3560c3fead6bf89b278598ac976a803a8f5
qiao-lima <qiao-lima@github.com>
2026-04-23 20:54:21 +0800

    Manual Update 4547250

---
+++ 4547250.lua
@@ -0,0 +1,12 @@
+-- 4547250's Lua and Manifest Created by Hubcap Manifest
+-- ANIMA : Dark Fairy Tale for Adults
+-- Created: April 23, 2026 at 08:33:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4547250) -- ANIMA : Dark Fairy Tale for Adults
+-- MAIN APP DEPOTS
+addappid(4547251, 1, "084496eecf9bee2316c87920158a7d50daf4696eca6c932d9f8b17f27142d5ea") -- Depot 4547251
+setManifestid(4547251, "504210651805367159", 662137517)
