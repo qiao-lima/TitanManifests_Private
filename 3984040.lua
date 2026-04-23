2f922b75f9afb2284cba0ebbf86119c47e6666f0
qiao-lima <qiao-lima@github.com>
2026-04-23 20:48:25 +0800

    Manual Update 3984040

---
+++ 3984040.lua
@@ -0,0 +1,18 @@
+-- 3984040's Lua and Manifest Created by Hubcap Manifest
+-- SPEEDSHOT
+-- Created: April 20, 2026 at 09:37:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3984040) -- SPEEDSHOT
+-- MAIN APP DEPOTS
+addappid(3984041, 1, "dcbbf970f984d8b18f11c5d35c3ae12b6abd44dad7ae59e587f6a645525d6cbb") -- Depot 3984041
+setManifestid(3984041, "6127072576200969713", 1429233475)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
