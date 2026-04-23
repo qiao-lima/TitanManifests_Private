1f6c11978b3964a7f524e7c97083d5d5368f1aab
qiao-lima <qiao-lima@github.com>
2026-04-23 20:43:36 +0800

    Manual Update 3909470

---
+++ 3909470.lua
@@ -0,0 +1,12 @@
+-- 3909470's Lua and Manifest Created by Hubcap Manifest
+-- A Lucky Hunt With Daphne
+-- Created: April 23, 2026 at 06:06:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3909470) -- A Lucky Hunt With Daphne
+-- MAIN APP DEPOTS
+addappid(3909471, 1, "aea047153395b1d49efcbf8c30e30310c68a098406ece6ec442b992d1852df75") -- Depot 3909471
+setManifestid(3909471, "1944760695958862711", 232383460)
