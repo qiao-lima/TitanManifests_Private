4240ae703494697287fd34af73db86d4ad2d2e36
qiao-lima <qiao-lima@github.com>
2026-04-23 20:54:19 +0800

    Manual Update 4414250

---
+++ 4414250.lua
@@ -0,0 +1,17 @@
+-- 4414250's Lua and Manifest Created by Hubcap Manifest
+-- Sisters in Sin 🔞
+-- Created: April 20, 2026 at 02:10:37 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4414250) -- Sisters in Sin 🔞
+-- MAIN APP DEPOTS
+addappid(4414251, 1, "c83f8b37e4b98049fa4416b945ce8eb674b6be6cf16f35a528b38d3c7b0a2b4e") -- Depot 4414251
+setManifestid(4414251, "7732044961251016019", 4232276297)
+-- DLCS WITH DEDICATED DEPOTS
+-- Sisters in Sin-Wallpapers 18 (AppID: 4613200)
+addappid(4613200)
+addappid(4613200, 1, "000948e41fe7357cd12dc809bae2da6c6737260908931277e6b3e4a309dfd3b2") -- Sisters in Sin-Wallpapers 18 - Depot 4613200
+setManifestid(4613200, "8334617565677923042", 312617803)
