c3d62590aca11416c40bdabda4779271f936df12
qiao-lima <qiao-lima@github.com>
2026-04-23 20:54:18 +0800

    Manual Update 4320940

---
+++ 4320940.lua
@@ -0,0 +1,12 @@
+-- 4320940's Lua and Manifest Created by Hubcap Manifest
+-- GAME The Strongest Job Is Apparently Not a Hero or a Sage, but an Appraiser (Provisional)! ~Dungeon and Mystery Girl~
+-- Created: April 22, 2026 at 18:46:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4320940) -- GAME The Strongest Job Is Apparently Not a Hero or a Sage, but an Appraiser (Provisional)! ~Dungeon and Mystery Girl~
+-- MAIN APP DEPOTS
+addappid(4320941, 1, "967aca5b2e361068b4e4c46c352d22d3b5fb80e83002a1beaa1bb4045b7cdcd6") -- Depot 4320941
+setManifestid(4320941, "4107696229075300047", 1300195934)
