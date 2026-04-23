830c57e6a449d3131b48c156633879070ea99120
qiao-lima <qiao-lima@github.com>
2026-04-23 20:54:18 +0800

    Manual Update 4312050

---
+++ 4312050.lua
@@ -0,0 +1,12 @@
+-- 4312050's Lua and Manifest Created by Hubcap Manifest
+-- A Lucky Hunt With Thalia
+-- Created: April 23, 2026 at 06:07:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4312050) -- A Lucky Hunt With Thalia
+-- MAIN APP DEPOTS
+addappid(4312051, 1, "1f31258285dc68a46f62d59941469f8713aebd5ad2411922f7dc964dcd9e3e7c") -- Depot 4312051
+setManifestid(4312051, "5504374489347039130", 233924466)
