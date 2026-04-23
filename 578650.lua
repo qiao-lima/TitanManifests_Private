472828f7dda4cc9ed7b8ce6762338e4eea238374
qiao-lima <qiao-lima@github.com>
2026-04-23 20:59:05 +0800

    Manual Update 578650

---
+++ 578650.lua
@@ -0,0 +1,23 @@
+-- 578650's Lua and Manifest Created by Hubcap Manifest
+-- The Outer Worlds
+-- Created: October 01, 2025 at 00:29:06 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 4
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(578650) -- The Outer Worlds
+-- MAIN APP DEPOTS
+addappid(578651, 1, "a877c80b32c589baa437c2ed817cf77259a5af3619a9159ea1316e9f98f6eda7") -- Chelan - Windows
+setManifestid(578651, "5368676825946976338", 59428957421)
+-- SHARED DEPOTS (from other apps)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") -- VC 2017 Redist (Shared from App 228980)
+setManifestid(228987, "4302102680580581867", 29664201)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1393110) -- The Outer Worlds Peril on Gorgon
+addappid(1393111) -- The Outer Worlds Murder on Eridanos
+addappid(1425450) -- The Outer Worlds Expansion Pass
+addappid(2170820) -- The Outer Worlds Spacers Choice Edition Upgrade
