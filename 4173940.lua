74537589a27a120254939f99b3b67e95c090bded
qiao-lima <qiao-lima@github.com>
2026-04-23 20:48:27 +0800

    Manual Update 4173940

---
+++ 4173940.lua
@@ -0,0 +1,12 @@
+-- 4173940's Lua and Manifest Created by Hubcap Manifest
+-- A Mystic Journey With Terra
+-- Created: April 20, 2026 at 06:59:13 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4173940) -- A Mystic Journey With Terra
+-- MAIN APP DEPOTS
+addappid(4173941, 1, "b54e2cf6b759973b4b9150fe080ffb3c2c7e766990120ee074ea00a788570521") -- Depot 4173941
+setManifestid(4173941, "6084885988424067858", 231067668)
