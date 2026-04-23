d0b4a03e528e400e572230ca52f28926b0245fe6
qiao-lima <qiao-lima@github.com>
2026-04-23 20:54:21 +0800

    Manual Update 4501700

---
+++ 4501700.lua
@@ -0,0 +1,12 @@
+-- 4501700's Lua and Manifest Created by Hubcap Manifest
+-- A Mystic Journey With Marina
+-- Created: April 20, 2026 at 07:00:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4501700) -- A Mystic Journey With Marina
+-- MAIN APP DEPOTS
+addappid(4501701, 1, "28c240da842276cf741155cd1feeb78785d16c100b40a36e4e6d4504bf5979d5") -- Depot 4501701
+setManifestid(4501701, "5747642976220375064", 231058053)
