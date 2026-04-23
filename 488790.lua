2b9470f92668092d83077e4863062bd897829c43
qiao-lima <qiao-lima@github.com>
2026-04-23 20:54:22 +0800

    Manual Update 488790

---
+++ 488790.lua
@@ -0,0 +1,51 @@
+-- 488790's Lua and Manifest Created by Hubcap Manifest
+-- South Park The Fractured But Whole
+-- Created: April 15, 2026 at 09:05:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 7
+-- Total DLCs: 19
+-- Shared Depots: 4
+
+-- MAIN APPLICATION
+addappid(488790, 1, "5907fcd2605979040d2b932557e48a364333d1edeccf9ad0e6a053120271e09e") -- South Park The Fractured But Whole
+-- MAIN APP DEPOTS
+addappid(488791, 1, "91ae41f7e0eba911942b0e6c268e1d22a942fdd9c9521e3e68ecf42563202678") -- South Park The Fractured But Whole Content
+setManifestid(488791, "8142417002848833950", 30400419874)
+addappid(488792, 1, "7b37fd64e50668a85d6c00223deed02337c23e3c6c29023c0f55bb2045c54112") -- South Park The Fractured But Whole - English Audio
+setManifestid(488792, "4674572693373306454", 667)
+addappid(488793, 1, "b61f408c4574381dcf80351f090aa9e445965227032f6987d0dcd464160288cb") -- South Park The Fractured But Whole - FIGS+ Audio
+setManifestid(488793, "5642782365483620567", 4008)
+-- SHARED DEPOTS (from other apps)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (Shared from App 228980)
+setManifestid(228983, "8124929965194586177", 19265607)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- VC 2012 Redist (Shared from App 228980)
+setManifestid(228984, "2547553897526095397", 13742505)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") -- Ubisoft Connect PC Client Content (Shared from App 1716750)
+setManifestid(1716751, "8055867430366823504", 262818680)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(564780) -- South Park The Fractured But Whole - Danger Deck
+addappid(564790) -- South Park The Fractured But Whole - From Dusk Till Casa Bonita
+addappid(564791) -- South Park The Fractured But Whole - Bring the Crunch
+addappid(564792) -- South Park The Fractured But Whole - Towelie Your Gaming Bud
+addappid(564793) -- South Park The Fractured but Whole - Relics of Zaron
+addappid(564794) -- South Park The Fractured but Whole - Season Pass
+addappid(729590) -- South Park The Fractured But Whole - Super Streamer Starter Pack
+addappid(732560) -- South Park The Fractured But Whole - Bring the Crunch - Uplay Activation
+addtoken(732560, "8409833164453668767")
+addappid(732570) -- South Park The Fractured But Whole - From Dusk Till Casa Bonita - Uplay Activation
+addappid(732580) -- South Park The Fractured But Whole - Danger Deck - Uplay Activation
+addtoken(732580, "7970424596322729233")
+addappid(732590) -- South Park The Fractured But Whole - Relics of Zaron - Uplay Activation
+addappid(732600) -- South Park The Fractured But Whole - Towelie - Uplay Activation
+addtoken(732600, "5113368451491934685")
+addappid(732610) -- South Park The Fractured But Whole - Season Pass - Uplay Activation
+addappid(732620) -- South Park The Fractured But Whole - Standard Prepurchase - Uplay Activation
+addappid(732630) -- South Park The Fractured But Whole - Gold Prepurchase - Uplay Activation
+addappid(732640) -- South Park The Fractured But Whole - Standard - Uplay Activation
+addappid(732650) -- South Park The Fractured But Whole - Gold - Uplay Activation
+addappid(797320) -- South Park The Fractured But Whole  Single Bundle Standard - Uplay Activation
+addtoken(797320, "6016193076413967915")
+addappid(797321) -- South Park The Fractured But Whole - Single Bundle Gold - Uplay Activation
+addtoken(797321, "5375530328461915439")
