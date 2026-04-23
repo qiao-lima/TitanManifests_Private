2f271c9e58024554833d62b37c9ef27888ed0684
qiao-lima <qiao-lima@github.com>
2026-04-23 20:48:28 +0800

    Manual Update 4201710

---
+++ 4201710.lua
@@ -0,0 +1,13 @@
+-- 4201710's Lua and Manifest Created by Hubcap Manifest
+-- Meow My Crop!
+-- Created: April 19, 2026 at 18:00:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4201710) -- Meow My Crop!
+addtoken(4201710, "15223853365847870200")
+-- MAIN APP DEPOTS
+addappid(4201711, 1, "5eff90b5fd75ec13b94f23b3ffce049c99870d125c6fa1d35f7e28fb7fa6090e") -- Depot 4201711
+setManifestid(4201711, "1426381795724830466", 401375966)
