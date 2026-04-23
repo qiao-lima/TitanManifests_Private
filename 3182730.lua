5cc7674e03b9be0d6edb7c2f6a0434ffa0c8608d
qiao-lima <qiao-lima@github.com>
2026-04-23 20:37:41 +0800

    Manual Update 3182730

---
+++ 3182730.lua
@@ -0,0 +1,12 @@
+-- 3182730's Lua and Manifest Created by Hubcap Manifest
+-- Blades, Bows & Magic
+-- Created: April 20, 2026 at 13:45:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3182730) -- Blades, Bows & Magic
+-- MAIN APP DEPOTS
+addappid(3182731, 1, "d81a6e9f10331e319503f0bc2d70705ecad5ad9949bb9e12eb05cfc3877cae7f") -- Depot 3182731
+setManifestid(3182731, "606792698387300508", 182761332)
