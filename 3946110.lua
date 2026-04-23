4d539270dcda6cd663740863868a3faea76b62b8
qiao-lima <qiao-lima@github.com>
2026-04-23 20:43:37 +0800

    Manual Update 3946110

---
+++ 3946110.lua
@@ -0,0 +1,12 @@
+-- 3946110's Lua and Manifest Created by Hubcap Manifest
+-- Fracture Field
+-- Created: April 20, 2026 at 03:24:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3946110) -- Fracture Field
+-- MAIN APP DEPOTS
+addappid(3946111, 1, "1289c01d4dab9fc716f2590cec8294a1554f51333aadc8aaf9e03287ad0e69f8") -- Depot 3946111
+setManifestid(3946111, "5847301244927475785", 251224983)
