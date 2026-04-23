f90645fb6f9777f6bec19f94d2587fc87aa49b02
qiao-lima <qiao-lima@github.com>
2026-04-23 20:54:18 +0800

    Manual Update 4278180

---
+++ 4278180.lua
@@ -0,0 +1,12 @@
+-- 4278180's Lua and Manifest Created by Hubcap Manifest
+-- Studio Sim
+-- Created: April 19, 2026 at 12:20:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4278180) -- Studio Sim
+-- MAIN APP DEPOTS
+addappid(4278182, 1, "e068f8475c66e44f8041be3f6bafc82553691ba47bd86ec747f4fe1af0bba46b") -- Depot 4278182
+setManifestid(4278182, "2251639161793582223", 360624600)
