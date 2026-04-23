c003a5bdd952d33e84f8da2c7545a1635a3656dd
qiao-lima <qiao-lima@github.com>
2026-04-23 20:37:37 +0800

    Manual Update 1474390

---
+++ 1474390.lua
@@ -0,0 +1,12 @@
+-- 1474390's Lua and Manifest Created by Hubcap Manifest
+-- Broken : Paranormal investigation
+-- Created: April 22, 2026 at 14:00:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1474390) -- Broken : Paranormal investigation
+-- MAIN APP DEPOTS
+addappid(1474391, 1, "ff2071a73ffe4e279acdfff28de8a52f8ace870c829fe3cb1ffcfa99a7f6d3f8") -- Depot 1474391
+setManifestid(1474391, "7017559693222752229", 19956117108)
