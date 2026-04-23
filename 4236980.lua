45fe4e87b83948d3fe950a7ff3fd8d8a8170038f
qiao-lima <qiao-lima@github.com>
2026-04-23 20:48:28 +0800

    Manual Update 4236980

---
+++ 4236980.lua
@@ -0,0 +1,12 @@
+-- 4236980's Lua and Manifest Created by Hubcap Manifest
+-- A Lucky Hunt With Phaedra
+-- Created: April 23, 2026 at 06:07:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4236980) -- A Lucky Hunt With Phaedra
+-- MAIN APP DEPOTS
+addappid(4236981, 1, "cdaa2b90de2ef8cceb02a39d5a41992e5410ccbf936f3d6089d43f916ea4cddc") -- Depot 4236981
+setManifestid(4236981, "3859242362667770456", 233238275)
