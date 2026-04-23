f8c7e57ae5c458f8a685ab8ab35b9c35f2bc0884
qiao-lima <qiao-lima@github.com>
2026-04-23 20:37:38 +0800

    Manual Update 2461440

---
+++ 2461440.lua
@@ -0,0 +1,22 @@
+-- 2461440's Lua and Manifest Created by Hubcap Manifest
+-- Bylina
+-- Created: April 20, 2026 at 11:36:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0 (1 excluded)
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2461440) -- Bylina
+-- MAIN APP DEPOTS
+addappid(2461441, 1, "68d325a5afd07cabcc37abb8cd429b37a12165c54342c731b8889794773792e6") -- Depot 2461441
+setManifestid(2461441, "5619962699207573290", 16670062478)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Bylina Collectors Pack (AppID: 4591670) - missing depot keys
+-- addappid(4591670)
