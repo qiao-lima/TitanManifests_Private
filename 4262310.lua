9f26f1ffe012e851d3ec7c5c43109baada4355e9
qiao-lima <qiao-lima@github.com>
2026-04-23 20:48:29 +0800

    Manual Update 4262310

---
+++ 4262310.lua
@@ -0,0 +1,12 @@
+-- 4262310's Lua and Manifest Created by Hubcap Manifest
+-- All Hail the Orb
+-- Created: April 20, 2026 at 08:19:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4262310) -- All Hail the Orb
+-- MAIN APP DEPOTS
+addappid(4262311, 1, "2409fb5e25e5270ed099dc37df6e6a85f9e14b52f65f991d781914af6ee88f2d") -- Depot 4262311
+setManifestid(4262311, "3647941341565013665", 306450933)
