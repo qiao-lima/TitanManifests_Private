32540c2e4f0b69c068d60afa0696fec5fd7e73b2
qiao-lima <qiao-lima@github.com>
2026-04-23 20:48:28 +0800

    Manual Update 4244570

---
+++ 4244570.lua
@@ -0,0 +1,12 @@
+-- 4244570's Lua and Manifest Created by Hubcap Manifest
+-- A Mystic Journey With Ignis
+-- Created: April 20, 2026 at 06:59:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4244570) -- A Mystic Journey With Ignis
+-- MAIN APP DEPOTS
+addappid(4244571, 1, "cd14da9b569219fda39e5442e908103b403f0fd8ee24ce22fb9f68c391d20f5d") -- Depot 4244571
+setManifestid(4244571, "6170001205435400684", 230206788)
