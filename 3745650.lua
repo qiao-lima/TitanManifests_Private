6c25c9b183b64b7cc52e7e0fbe19a8ca0d076030
qiao-lima <qiao-lima@github.com>
2026-04-23 20:43:35 +0800

    Manual Update 3745650

---
+++ 3745650.lua
@@ -0,0 +1,12 @@
+-- 3745650's Lua and Manifest Created by Hubcap Manifest
+-- 海岛寻宝
+-- Created: April 23, 2026 at 08:31:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3745650) -- 海岛寻宝
+-- MAIN APP DEPOTS
+addappid(3745651, 1, "e775a7d05bde596577c5b9d9557c77a75b83369b5a4a3193b0361251546b2be3") -- Depot 3745651
+setManifestid(3745651, "7662057736342510398", 413274036)
