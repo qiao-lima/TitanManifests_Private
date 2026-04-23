608a4b2b5b70290cca42132d242d78b1803d8c80
qiao-lima <qiao-lima@github.com>
2026-04-23 20:54:20 +0800

    Manual Update 4488860

---
+++ 4488860.lua
@@ -0,0 +1,12 @@
+-- 4488860's Lua and Manifest Created by Hubcap Manifest
+-- The Red Pearls Of Borneo
+-- Created: April 19, 2026 at 18:01:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4488860) -- The Red Pearls Of Borneo
+-- MAIN APP DEPOTS
+addappid(4488863, 1, "f4d3cb39b2453cf955732538b202d422a30986d991f9c639bddb930190b15aaf") -- Depot 4488863
+setManifestid(4488863, "5735908971789277903", 624572918)
