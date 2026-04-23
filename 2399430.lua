b0ab2be725c15d90244b5366d641c98f93052099
qiao-lima <qiao-lima@github.com>
2026-04-23 20:37:38 +0800

    Manual Update 2399430

---
+++ 2399430.lua
@@ -0,0 +1,18 @@
+-- 2399430's Lua and Manifest Created by Hubcap Manifest
+-- Combat Kart
+-- Created: April 22, 2026 at 22:08:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (2 excluded)
+
+-- MAIN APPLICATION
+addappid(2399430) -- Combat Kart
+-- MAIN APP DEPOTS
+addappid(2399431, 1, "d86d1b9b0561a6404cf7da28d757baf3a7b5ca28b393118f1ff74ac5dac5b91c") -- Depot 2399431
+setManifestid(2399431, "6629363692679869917", 3711559749)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- CombatKart - Rainbow (AppID: 2653800) - missing depot keys
+-- addappid(2653800)
+-- Combat Kart - Super Kart (AppID: 4108340) - missing depot keys
+-- addappid(4108340)
