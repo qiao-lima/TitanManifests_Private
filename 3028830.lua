a16d441a7c77ee42ad128f960e84e0e78350f663
qiao-lima <qiao-lima@github.com>
2026-04-23 20:37:40 +0800

    Manual Update 3028830

---
+++ 3028830.lua
@@ -0,0 +1,12 @@
+-- 3028830's Lua and Manifest Created by Hubcap Manifest
+-- Stranger Maidens Please Protect Me!
+-- Created: April 19, 2026 at 12:32:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3028830) -- Stranger Maidens Please Protect Me!
+-- MAIN APP DEPOTS
+addappid(3028831, 1, "17b6d8fa0a51ffee90b7d6ed8d56b674be74a7453bb8452f85b081a0f8d797cf") -- Depot 3028831
+setManifestid(3028831, "2963015712472826222", 5717904751)
