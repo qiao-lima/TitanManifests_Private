120b2722d0fbb12e9b7754ded449d01565f0a7b4
qiao-lima <qiao-lima@github.com>
2026-04-23 20:43:33 +0800

    Manual Update 3499720

---
+++ 3499720.lua
@@ -0,0 +1,12 @@
+-- 3499720's Lua and Manifest Created by Hubcap Manifest
+-- Bedtime Stories 3
+-- Created: April 23, 2026 at 06:06:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3499720) -- Bedtime Stories 3
+-- MAIN APP DEPOTS
+addappid(3499721, 1, "e60f09c82c836bf4ff59801266983d79a782288df0fc4096387b579ba15ae926") -- Depot 3499721
+setManifestid(3499721, "389235206734113564", 3104673114)
