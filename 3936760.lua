23d096b805e275aa0e18ff451f7231fbb6c512a9
qiao-lima <qiao-lima@github.com>
2026-04-23 20:43:37 +0800

    Manual Update 3936760

---
+++ 3936760.lua
@@ -0,0 +1,12 @@
+-- 3936760's Lua and Manifest Created by Hubcap Manifest
+-- A Lucky Hunt With Lyra
+-- Created: April 23, 2026 at 06:06:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3936760) -- A Lucky Hunt With Lyra
+-- MAIN APP DEPOTS
+addappid(3936761, 1, "edd7b8f5bac77286cdc1ae2b4fc6cde0d00a911f4c14d48f5403e44070ccf78c") -- Depot 3936761
+setManifestid(3936761, "769699843298529905", 232358882)
