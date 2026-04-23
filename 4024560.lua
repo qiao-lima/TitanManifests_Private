d8d79af33d886dc426d02a4bf1d75127aba6f403
qiao-lima <qiao-lima@github.com>
2026-04-23 20:48:26 +0800

    Manual Update 4024560

---
+++ 4024560.lua
@@ -0,0 +1,12 @@
+-- 4024560's Lua and Manifest Created by Hubcap Manifest
+-- A Mystic Journey With Aria
+-- Created: April 19, 2026 at 12:44:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4024560) -- A Mystic Journey With Aria
+-- MAIN APP DEPOTS
+addappid(4024561, 1, "69cb54cd7863fe9dae68532c1ca555be21a68d94df7e50697410a3dca708f82f") -- Depot 4024561
+setManifestid(4024561, "2612365293756778420", 230051669)
