6445a0e6756043168e64eaac70116d9ba97c690b
qiao-lima <qiao-lima@github.com>
2026-04-23 20:54:17 +0800

    Manual Update 4275970

---
+++ 4275970.lua
@@ -0,0 +1,12 @@
+-- 4275970's Lua and Manifest Created by Hubcap Manifest
+-- Dead Rush
+-- Created: April 22, 2026 at 18:39:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4275970) -- Dead Rush
+-- MAIN APP DEPOTS
+addappid(4275971, 1, "dd97b9048de14c8d360ab2318245dd7bed2274261cff22c066c03bbccaa44094") -- Depot 4275971
+setManifestid(4275971, "3774857969325958020", 1476181472)
