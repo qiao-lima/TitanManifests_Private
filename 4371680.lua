c2432ea289e6fb88d5c3d07c557046253708aad8
qiao-lima <qiao-lima@github.com>
2026-04-23 20:54:18 +0800

    Manual Update 4371680

---
+++ 4371680.lua
@@ -0,0 +1,12 @@
+-- 4371680's Lua and Manifest Created by Hubcap Manifest
+-- Shadowbane: Resurgence
+-- Created: April 20, 2026 at 04:22:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4371680) -- Shadowbane: Resurgence
+-- MAIN APP DEPOTS
+addappid(4371681, 1, "f8b4e3ad74fa1f0128b7da26021e3c694cd459a32884088487e4ec41d7a631f2") -- Depot 4371681
+setManifestid(4371681, "5660234314938043887", 2547994180)
