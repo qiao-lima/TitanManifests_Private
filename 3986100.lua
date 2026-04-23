075d663a351ba9b2ef81cb30859abf47a350826b
qiao-lima <qiao-lima@github.com>
2026-04-23 20:48:25 +0800

    Manual Update 3986100

---
+++ 3986100.lua
@@ -0,0 +1,16 @@
+-- 3986100's Lua and Manifest Created by Hubcap Manifest
+-- Game Shop Shayou
+-- Created: April 22, 2026 at 18:48:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3986100) -- Game Shop Shayou
+-- MAIN APP DEPOTS
+addappid(3986102, 1, "4664e95693522e49741b6ce15182fb95b4326604936953d4597ef18dc07b1e7a") -- Depot 3986102
+setManifestid(3986102, "8433088030625643425", 407120064)
+addappid(3986103, 1, "f43c88d70b450a30e305c38b946d8c7e25ee1afa1c13c57c77db5ea2398a6a21") -- Depot 3986103
+setManifestid(3986103, "6379885832843681887", 435523971)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3986101) -- Depot 3986101 (empty depot)
