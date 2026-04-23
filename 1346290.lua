f16a64a6096807a5601798f0e9d215008e59baef
qiao-lima <qiao-lima@github.com>
2026-04-23 20:37:36 +0800

    Manual Update 1346290

---
+++ 1346290.lua
@@ -0,0 +1,12 @@
+-- 1346290's Lua and Manifest Created by Hubcap Manifest
+-- Jay and Silent Bob: Chronic Blunt Punch
+-- Created: April 20, 2026 at 11:17:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1346290) -- Jay and Silent Bob: Chronic Blunt Punch
+-- MAIN APP DEPOTS
+addappid(1346291, 1, "7dd476fe9c02775dfd68d5b60ef33c1f242163b647b80d93a27fe13bb7dbadad") -- Depot 1346291
+setManifestid(1346291, "5959858976634756813", 19485600872)
