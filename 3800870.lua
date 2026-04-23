55d4559f632d79a3d1e1094e61cb68986951ff2f
qiao-lima <qiao-lima@github.com>
2026-04-23 20:43:35 +0800

    Manual Update 3800870

---
+++ 3800870.lua
@@ -0,0 +1,12 @@
+-- 3800870's Lua and Manifest Created by Hubcap Manifest
+-- Divineblaze : March Tactics
+-- Created: April 22, 2026 at 18:38:53 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3800870) -- Divineblaze : March Tactics
+-- MAIN APP DEPOTS
+addappid(3800871, 1, "f4c11fd433dd3763cb26ea45dc93d39f87bf757d8d38ba959f3a4f5c6f6cf967") -- Depot 3800871
+setManifestid(3800871, "4980376267722541972", 2464026890)
