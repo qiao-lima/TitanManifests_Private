98465c582fe7209e1b7ab9ea60774218ea1080a7
qiao-lima <qiao-lima@github.com>
2026-04-23 20:54:19 +0800

    Manual Update 4395900

---
+++ 4395900.lua
@@ -0,0 +1,12 @@
+-- 4395900's Lua and Manifest Created by Hubcap Manifest
+-- Subliminal Echoes
+-- Created: April 22, 2026 at 18:41:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4395900) -- Subliminal Echoes
+-- MAIN APP DEPOTS
+addappid(4395901, 1, "c87de568ab012c1e0fb954f13a3ce92004a6e754dd29c53722d684a159c859e3") -- Depot 4395901
+setManifestid(4395901, "2498330673330918499", 354391872)
