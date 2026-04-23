408cc043d36120203ac13271d79fd28332eac593
qiao-lima <qiao-lima@github.com>
2026-04-23 20:37:40 +0800

    Manual Update 3006090

---
+++ 3006090.lua
@@ -0,0 +1,12 @@
+-- 3006090's Lua and Manifest Created by Hubcap Manifest
+-- Crystant
+-- Created: April 22, 2026 at 18:47:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3006090) -- Crystant
+-- MAIN APP DEPOTS
+addappid(3006091, 1, "01daa2d35f7867d292f102b2319b1e20d64cab759b9fbb9496469836b4ca83e3") -- Depot 3006091
+setManifestid(3006091, "4737895170903893365", 1815462255)
