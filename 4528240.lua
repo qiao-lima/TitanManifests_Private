305a593105603dd308d5ae9b851f4fd9f28316db
qiao-lima <qiao-lima@github.com>
2026-04-24 12:40:28 +0800

    Manual Update 4528240

---
+++ 4528240.lua
@@ -0,0 +1,12 @@
+-- 4528240's Lua and Manifest Created by Hubcap Manifest
+-- 暗黑战神
+-- Created: April 24, 2026 at 00:02:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4528240) -- 暗黑战神
+-- MAIN APP DEPOTS
+addappid(4528241, 1, "a6da18d727583b11dbead08bd840d3992ab02d2c26aa5ae460d011d360ec867f") -- Depot 4528241
+setManifestid(4528241, "5231545292959695263", 57867721)
