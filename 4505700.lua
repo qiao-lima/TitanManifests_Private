25999d14c1ac31d0f3fb0f90e76ff3d593e4c2d8
qiao-lima <qiao-lima@github.com>
2026-04-24 12:40:28 +0800

    Manual Update 4505700

---
+++ 4505700.lua
@@ -0,0 +1,12 @@
+-- 4505700's Lua and Manifest Created by Hubcap Manifest
+-- 16 Personalities Simulator
+-- Created: April 23, 2026 at 22:33:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4505700) -- 16 Personalities Simulator
+-- MAIN APP DEPOTS
+addappid(4505701, 1, "174d6fd53a8eac42834215234bdf0f23e0859d419167489d68fabe6e75f178cf") -- Depot 4505701
+setManifestid(4505701, "1475466001224010305", 123262872)
