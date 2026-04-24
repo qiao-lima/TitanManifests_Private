1dc54ac141650ff58b2b3c294ad7a2c3ed170f10
qiao-lima <qiao-lima@github.com>
2026-04-24 12:40:26 +0800

    Manual Update 2759280

---
+++ 2759280.lua
@@ -0,0 +1,12 @@
+-- 2759280's Lua and Manifest Created by Hubcap Manifest
+-- Kumitantei: Old-School Slaughter
+-- Created: April 23, 2026 at 16:05:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2759280) -- Kumitantei: Old-School Slaughter
+-- MAIN APP DEPOTS
+addappid(2759281, 1, "6e0589bcb337ebe277c7165fad7ccf54e3d04cb2cc8720b720803d98297e71e0") -- Depot 2759281
+setManifestid(2759281, "5901074231559715891", 4930764292)
