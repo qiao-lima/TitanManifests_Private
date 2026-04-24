6565d5a0eee5216a5a8ef4d341716303e4084cae
qiao-lima <qiao-lima@github.com>
2026-04-24 12:40:26 +0800

    Manual Update 2775040

---
+++ 2775040.lua
@@ -0,0 +1,18 @@
+-- 2775040's Lua and Manifest Created by Hubcap Manifest
+-- SokoFarm
+-- Created: April 23, 2026 at 16:57:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2775040) -- SokoFarm
+-- MAIN APP DEPOTS
+addappid(2775041, 1, "5dda3277806b9b00faffad5a64aa11d476dd491138c66901e0eeec26fe722693") -- Depot 2775041
+setManifestid(2775041, "8941069189900745790", 135776491)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") -- OpenAL 2.0.7.0 Redist (Shared from App 228980)
+setManifestid(229020, "5799761707845834510", 810085)
