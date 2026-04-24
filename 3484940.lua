cecf887006b66c17b80731f7a3fb1219d229f708
qiao-lima <qiao-lima@github.com>
2026-04-24 12:40:26 +0800

    Manual Update 3484940

---
+++ 3484940.lua
@@ -0,0 +1,12 @@
+-- 3484940's Lua and Manifest Created by Hubcap Manifest
+-- Curse of Pirates
+-- Created: April 23, 2026 at 12:35:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3484940) -- Curse of Pirates
+-- MAIN APP DEPOTS
+addappid(3484941, 1, "6a46e22525d6b088fabba15f3d258c7c1c9c62907440224e50ab6cdd16e64f6a") -- Depot 3484941
+setManifestid(3484941, "6472288601121634443", 368394844)
