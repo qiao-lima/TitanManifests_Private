21d71e738100c4f112918e45eb8473e905fa7579
qiao-lima <qiao-lima@github.com>
2026-04-24 12:40:26 +0800

    Manual Update 3476880

---
+++ 3476880.lua
@@ -0,0 +1,12 @@
+-- 3476880's Lua and Manifest Created by Hubcap Manifest
+-- Brno Transit
+-- Created: April 23, 2026 at 12:37:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3476880) -- Brno Transit
+-- MAIN APP DEPOTS
+addappid(3476881, 1, "842de32fac41d23dc49bb0399960ec59507f5ba2bc8c7bba1cc465c12738c92d") -- Depot 3476881
+setManifestid(3476881, "2999478781299185842", 167043770)
