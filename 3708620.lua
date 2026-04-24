f630ac1e9ea01cd11d543f2d3da74e060e2f4091
qiao-lima <qiao-lima@github.com>
2026-04-24 12:40:27 +0800

    Manual Update 3708620

---
+++ 3708620.lua
@@ -0,0 +1,26 @@
+-- 3708620's Lua and Manifest Created by Hubcap Manifest
+-- Numeral Strike
+-- Created: April 23, 2026 at 22:56:29 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 7
+-- Total DLCs: 0
+-- Shared Depots: 6
+
+-- MAIN APPLICATION
+addappid(3708620) -- Numeral Strike
+-- MAIN APP DEPOTS
+addappid(3708621, 1, "41d3e7086a542b753d74518f799a2147ad3bb896d94e21bb52d03f1909264adc") -- Depot 3708621
+setManifestid(3708621, "6615089161751042673", 382607406)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 117381405)
+addappid(229012, 1, "421e499f39a617c1a3b6baf11fbc65ffebd541d2c39c38d3bf6ffbd87919ce12") -- XNA 4.0 Redist (Shared from App 228980)
+setManifestid(229012, "4353723233161159493", 7061608)
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") -- OpenAL 2.0.7.0 Redist (Shared from App 228980)
+setManifestid(229020, "5799761707845834510", 810085)
+addappid(229033, 1, "9a789440bbc16131547ac6c062e5f168f7684da533a2899a71fdfaaeec21efc3") -- PhysX System Software 9.14.0702 (Shared from App 228980)
+setManifestid(229033, "2059065101492814639", 60039803)
