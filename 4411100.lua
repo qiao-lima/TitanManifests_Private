0dd053d801df6c3a2893ad11262623a58fa25b88
qiao-lima <qiao-lima@github.com>
2026-04-24 12:40:28 +0800

    Manual Update 4411100

---
+++ 4411100.lua
@@ -0,0 +1,12 @@
+-- 4411100's Lua and Manifest Created by Hubcap Manifest
+-- Tutu Games
+-- Created: April 23, 2026 at 12:36:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4411100) -- Tutu Games
+-- MAIN APP DEPOTS
+addappid(4411101, 1, "82cb59e2356d56e9a8b65ba45a15eb39cfdb450f8d24bc760a847be429e26f68") -- Depot 4411101
+setManifestid(4411101, "83384391584373993", 126037636)
