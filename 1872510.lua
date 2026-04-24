ab49b7d3b8703c1653dab9fcb69dc631820097f1
qiao-lima <qiao-lima@github.com>
2026-04-24 12:40:25 +0800

    Manual Update 1872510

---
+++ 1872510.lua
@@ -0,0 +1,17 @@
+-- 1872510's Lua and Manifest Created by Hubcap Manifest
+-- Working Daily
+-- Created: April 23, 2026 at 23:48:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(1872510) -- Working Daily
+-- MAIN APP DEPOTS
+addappid(1872511, 1, "02d88f300eb36e0c4c6f0d680738370f3b278de3bce831fa48c99adf5190d511") -- Depot 1872511
+setManifestid(1872511, "3978797790760806193", 502489622)
+-- DLCS WITH DEDICATED DEPOTS
+-- Working Daily-Patch (AppID: 1880270)
+addappid(1880270)
+addappid(1880270, 1, "63457f91cfe619747737820be6b989031c5913d816621d03d2f275b566713602") -- Working Daily-Patch - Depot 1880270
+setManifestid(1880270, "6610039006734452876", 622672000)
