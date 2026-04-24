904e279f51b7932d9192bb3ce0bd5a53e5f6faa6
qiao-lima <qiao-lima@github.com>
2026-04-24 12:40:27 +0800

    Manual Update 3963490

---
+++ 3963490.lua
@@ -0,0 +1,20 @@
+-- 3963490's Lua and Manifest Created by Hubcap Manifest
+-- LYSA HORA: The Haunted Hill
+-- Created: April 24, 2026 at 00:38:57 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(3963490) -- LYSA HORA: The Haunted Hill
+-- MAIN APP DEPOTS
+addappid(3963491, 1, "8dec8df4cb1c3373019b2b6abbfea5c1ee4b3aa0f1b30ac8f00a0bbce0d5807a") -- Depot 3963491
+setManifestid(3963491, "1412202332454145590", 7490113616)
+-- SHARED DEPOTS (from other apps)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (Shared from App 228980)
+setManifestid(228983, "8124929965194586177", 19265607)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
