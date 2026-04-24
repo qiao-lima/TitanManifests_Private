70b297d5929a6b34c7f4d640d4a4d2fbc87ea58f
qiao-lima <qiao-lima@github.com>
2026-04-24 12:40:28 +0800

    Manual Update 4466990

---
+++ 4466990.lua
@@ -0,0 +1,18 @@
+-- 4466990's Lua and Manifest Created by Hubcap Manifest
+-- The Guest II
+-- Created: April 23, 2026 at 12:36:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4466990) -- The Guest II
+-- MAIN APP DEPOTS
+addappid(4466991, 1, "1cd3a421cdfd7f37795335d9b441c88e9c02efae4f1f975e30cfa4a00cdf2010") -- Depot 4466991
+setManifestid(4466991, "4601255385740946403", 5064805766)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
