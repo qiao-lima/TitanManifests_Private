61e2bc929a11adef889d30bb911ae4f5dec702bf
qiao-lima <qiao-lima@github.com>
2026-04-24 12:40:28 +0800

    Manual Update 4231560

---
+++ 4231560.lua
@@ -0,0 +1,29 @@
+-- 4231560's Lua and Manifest Created by Hubcap Manifest
+-- Memories Off Sousou Break out of my shell
+-- Created: April 23, 2026 at 12:36:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 5
+-- Total DLCs: 1 (1 excluded)
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(4231560) -- Memories Off Sousou Break out of my shell
+-- MAIN APP DEPOTS
+addappid(4231561, 1, "9f364fada613896382a0b9cbff8cb930c487d340991f54f89d77b3fa2804810a") -- Depot 4231561
+setManifestid(4231561, "3855534312016017907", 3517617207)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") -- OpenAL 2.0.7.0 Redist (Shared from App 228980)
+setManifestid(229020, "5799761707845834510", 810085)
+-- DLCS WITH DEDICATED DEPOTS
+-- Memories Off Sousou Break out of my shell Material Book (AppID: 4558970)
+addappid(4558970)
+addappid(4558970, 1, "aa48fab96f5b621b05740791645a85897b8c02e2da038d04afce8ab956bb4b5c") -- Memories Off Sousou Break out of my shell Material Book - Depot 4558970
+setManifestid(4558970, "449764721973443693", 33280457)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Memories Off KOIKOI Taisen (AppID: 4500900) - missing depot keys
+-- addappid(4500900)
