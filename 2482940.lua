d46642b2c93d90364bea6bb4191f1ecf52666fdb
qiao-lima <qiao-lima@github.com>
2026-04-24 12:40:25 +0800

    Manual Update 2482940

---
+++ 2482940.lua
@@ -0,0 +1,12 @@
+-- 2482940's Lua and Manifest Created by Hubcap Manifest
+-- Little Nightmares VR: Altered Echoes
+-- Created: April 23, 2026 at 18:11:18 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2482940) -- Little Nightmares VR: Altered Echoes
+-- MAIN APP DEPOTS
+addappid(2482941, 1, "96295c7ad4b61b8b8842c50ab5eab91d05f530a2364689fedaae0d0663de121a") -- Depot 2482941
+setManifestid(2482941, "2864149954535957129", 10523375128)
