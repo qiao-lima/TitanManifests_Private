aec841a9689288af597dd1f50a088a6a5e1f2a7e
qiao-lima <qiao-lima@github.com>
2026-04-24 12:40:25 +0800

    Manual Update 2352620

---
+++ 2352620.lua
@@ -0,0 +1,22 @@
+-- 2352620's Lua and Manifest Created by Hubcap Manifest
+-- Fellowship
+-- Created: March 26, 2026 at 12:13:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 3
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2352620, 1, "1c6834466e845d2238ffb3cfba0a032c03b4ae948b1622e41ebc24ceecc20f76") -- Fellowship
+-- MAIN APP DEPOTS
+addappid(2352621, 1, "60938858a99f1cfbe331ae096354a6c7b0bdb5db303d42eb4feb417074140597") -- Depot 2352621
+setManifestid(2352621, "4842742006028555197", 15085411843)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4079610) -- Fellowship - Guardians Supporter Pack
+addappid(4082970) -- Fellowship - Exiles Supporter Pack
+addappid(4082980) -- Fellowship - Wanderers Supporter Pack
