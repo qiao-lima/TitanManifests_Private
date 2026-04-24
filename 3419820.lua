100c1025362e20cae87a3a4d5f7e33f12ac503f5
qiao-lima <qiao-lima@github.com>
2026-04-24 12:40:26 +0800

    Manual Update 3419820

---
+++ 3419820.lua
@@ -0,0 +1,12 @@
+-- 3419820's Lua and Manifest Created by Hubcap Manifest
+-- Welcome to Cafe Lumière! ~A Dragon Girl Concept Cafe~
+-- Created: April 23, 2026 at 21:44:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3419820) -- Welcome to Cafe Lumière! ~A Dragon Girl Concept Cafe~
+-- MAIN APP DEPOTS
+addappid(3419821, 1, "a64a47336311dbf64afe886bf72795e01ed41b5967b67ebf50bc675080234116") -- Depot 3419821
+setManifestid(3419821, "2919384835607901728", 2687709806)
