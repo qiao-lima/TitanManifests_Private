569ee7c4f33836743fcde8d21f4ba1ee5eec8ecc
qiao-lima <qiao-lima@github.com>
2026-04-24 12:40:27 +0800

    Manual Update 4197890

---
+++ 4197890.lua
@@ -0,0 +1,14 @@
+-- 4197890's Lua and Manifest Created by Hubcap Manifest
+-- The Last Cat in the Universe
+-- Created: April 23, 2026 at 12:34:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4197890) -- The Last Cat in the Universe
+-- MAIN APP DEPOTS
+addappid(4197891, 1, "ccfb3e271840f47f3aac00a4de89cf9d22c2d590304886a295d4277a7f9321c7") -- Depot 4197891
+setManifestid(4197891, "1027183634050768364", 221846705)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4530730) -- The Last Cat in the Universe - Supporter Pack
