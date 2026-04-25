de72bd9d1e3733beac82a63a129232ca3dd531e0
qiao-lima <qiao-lima@github.com>
2026-04-25 23:15:52 +0800

    Manual Update 4082900

---
+++ 4082900.lua
@@ -0,0 +1,14 @@
+-- 4082900's Lua and Manifest Created by Hubcap Manifest
+-- castle idler
+-- Created: April 25, 2026 at 08:15:19 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4082900) -- castle idler
+-- MAIN APP DEPOTS
+addappid(4082901, 1, "5d086a9fc834466ed1437be4d075e796339fcbb195cac9b9eda9f2c5f91ef0f1") -- Depot 4082901
+setManifestid(4082901, "1848799389330422799", 156858702)
+addappid(4082902, 1, "31b8d5642af3a76d56a524b9bea68f2d4c0fbd42d1ce2fdad970171969163a85") -- Depot 4082902
+setManifestid(4082902, "2044067716790851001", 171475445)
