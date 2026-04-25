9f351b8d1799bc676498d470f7b819718f046dd7
qiao-lima <qiao-lima@github.com>
2026-04-25 23:08:03 +0800

    Manual Update 4008970

---
+++ 4008970.lua
@@ -0,0 +1,16 @@
+-- 4008970's Lua and Manifest Created by Hubcap Manifest
+-- YourOwnWaifu
+-- Created: April 25, 2026 at 02:30:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(4008970) -- YourOwnWaifu
+-- MAIN APP DEPOTS
+addappid(4008971, 1, "ffc8423a8f05c7c37962bd7c161479c70f24408b696c12c2282509ea7e6a5be9") -- Depot 4008971
+setManifestid(4008971, "7159187220288441074", 815838703)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
