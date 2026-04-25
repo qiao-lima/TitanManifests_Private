830376a37b726d947c7ab1a6bcdc43f11f366bf9
qiao-lima <qiao-lima@github.com>
2026-04-25 23:08:02 +0800

    Manual Update 3365940

---
+++ 3365940.lua
@@ -0,0 +1,12 @@
+-- 3365940's Lua and Manifest Created by Hubcap Manifest
+-- Well Keeper
+-- Created: April 24, 2026 at 10:03:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3365940) -- Well Keeper
+-- MAIN APP DEPOTS
+addappid(3365941, 1, "4c548f8409185358b41518ce56a6077698c01c7414796b4eefec9b00b7e4e407") -- Depot 3365941
+setManifestid(3365941, "1707537238419336019", 708139162)
