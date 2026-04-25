22e1f8481a9425a1682c607cbf3bf89a2bd58c65
qiao-lima <qiao-lima@github.com>
2026-04-25 23:15:54 +0800

    Manual Update 4383370

---
+++ 4383370.lua
@@ -0,0 +1,20 @@
+-- 4383370's Lua and Manifest Created by Hubcap Manifest
+-- Swaddle
+-- Created: April 24, 2026 at 10:00:36 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4383370) -- Swaddle
+-- MAIN APP DEPOTS
+addappid(4383371, 1, "6601333022667e9b4841f92b85e812178fa36f08eeb6fe8d0de7b57cbc78d30e") -- Depot 4383371
+setManifestid(4383371, "9168718931694664596", 2161891041)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4383372) -- Depot 4383372 (empty depot)
