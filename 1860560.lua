a23bc3be75bfb21c60a4fec3b59ba2780d9514a8
qiao-lima <qiao-lima@github.com>
2026-04-25 23:07:59 +0800

    Manual Update 1860560

---
+++ 1860560.lua
@@ -0,0 +1,16 @@
+-- 1860560's Lua and Manifest Created by Hubcap Manifest
+-- OFFERING APP
+-- Created: April 24, 2026 at 10:04:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(1860560) -- OFFERING APP
+-- MAIN APP DEPOTS
+addappid(1860562, 1, "d212e5cfc13afb86cecd52bb81f19d3cf325b8e4b8aaa50fc7b75fcab8da3c71") -- Depot 1860562
+setManifestid(1860562, "5312584205855027170", 31050732307)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- OFFERING APP - Supporter Pack (AppID: 4536630) - missing depot keys
+-- addappid(4536630)
