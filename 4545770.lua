067d670c0281a481da76097949bbf24b60c19d16
qiao-lima <qiao-lima@github.com>
2026-04-25 23:15:55 +0800

    Manual Update 4545770

---
+++ 4545770.lua
@@ -0,0 +1,12 @@
+-- 4545770's Lua and Manifest Created by Hubcap Manifest
+-- Pick Them Up!
+-- Created: April 24, 2026 at 10:03:19 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4545770) -- Pick Them Up!
+-- MAIN APP DEPOTS
+addappid(4545771, 1, "7f700d2e1bde6db240625fa331e0c5c63d214ff879a8d91c5da4c3bebea48502") -- Depot 4545771
+setManifestid(4545771, "5850247320652495429", 390540015)
