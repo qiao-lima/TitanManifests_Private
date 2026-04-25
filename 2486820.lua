58ebd815ecd12c6c75b790a4cbc38b9c9116d4c2
qiao-lima <qiao-lima@github.com>
2026-04-25 22:51:42 +0800

    Manual Update 2486820

---
+++ 2486820.lua
@@ -0,0 +1,27 @@
+-- 2486820's Lua and Manifest Created by Hubcap Manifest
+-- Sonic Racing: CrossWorlds
+-- Created: March 26, 2026 at 15:26:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 8
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2486820, 1, "62be689d4dfb4728b7c5dd3be9339ee4a95afcae29179dd0819cbe39aad03f15") -- Sonic Racing: CrossWorlds
+-- MAIN APP DEPOTS
+addappid(2486821, 1, "08e2539b55bc79fefa9447d3479c57b481c4256c12ebd50529fc3722fcc3727f") -- Depot 2486821
+setManifestid(2486821, "3494414753270212375", 18440061002)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3565190) -- Sonic Racing CrossWorlds - Minecraft Pack
+addappid(3592250) -- Sonic Racing CrossWorlds Season Pass
+addappid(3601430) -- Sonic Racing CrossWorlds - SpongeBob SquarePants Pack
+addappid(3601440) -- Sonic Racing CrossWorlds - PAC-MAN Pack
+addappid(3601480) -- Sonic Racing CrossWorlds - Mega Man Pack
+addappid(3601500) -- Sonic Racing CrossWorlds - Sonic Prime Character Pack
+addappid(3601510) -- Sonic Racing CrossWorlds - Blue Star Extreme Gear
+addappid(3601530) -- Sonic Racing CrossWorlds - Werehog Pack
