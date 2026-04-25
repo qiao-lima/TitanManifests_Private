025b88dd391c2cc2878fa01b24b24e8129abb152
qiao-lima <qiao-lima@github.com>
2026-04-25 23:08:02 +0800

    Manual Update 3458500

---
+++ 3458500.lua
@@ -0,0 +1,12 @@
+-- 3458500's Lua and Manifest Created by Hubcap Manifest
+-- Lichgate: Tower Survivor
+-- Created: April 25, 2026 at 08:15:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3458500) -- Lichgate: Tower Survivor
+-- MAIN APP DEPOTS
+addappid(3458501, 1, "51f8dd63bc74b9f5a5605a5541e506d8d8a7edfccafa2d848c7e32c92003581c") -- Depot 3458501
+setManifestid(3458501, "3587844626914660126", 517187461)
