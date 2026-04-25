955e1db623b94e58a8958ab58323f9bf81087ef0
qiao-lima <qiao-lima@github.com>
2026-04-25 23:15:55 +0800

    Manual Update 4486210

---
+++ 4486210.lua
@@ -0,0 +1,12 @@
+-- 4486210's Lua and Manifest Created by Hubcap Manifest
+-- 狩魔之旅
+-- Created: April 25, 2026 at 11:06:30 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4486210) -- 狩魔之旅
+-- MAIN APP DEPOTS
+addappid(4486211, 1, "e2ad8b7cc6980b6f1cc1194dbfd5183fb34c7dff2f7ecfff51f5f64688146b5e") -- Depot 4486211
+setManifestid(4486211, "8112923258609716206", 2026961294)
