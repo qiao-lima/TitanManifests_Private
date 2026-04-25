afc4ff87a9dde3a9aebd39b7a0a87e794c30290a
qiao-lima <qiao-lima@github.com>
2026-04-25 22:51:43 +0800

    Manual Update 307960

---
+++ 307960.lua
@@ -0,0 +1,80 @@
+-- 307960's Lua and Manifest Created by Hubcap Manifest
+-- IL-2 Sturmovik: Battle of Stalingrad
+-- Created: April 15, 2026 at 20:10:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 57
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(307960, 1, "513c15aa2931c94c126b77cfe3bb505b501f6a246b90c4170d9f105af18dbda2") -- IL-2 Sturmovik: Battle of Stalingrad
+-- MAIN APP DEPOTS
+addappid(307961, 1, "23b7d427a0663be8f3d3540b80b7a0bb9d98b7f4e6d17ddc7a7224ac5d20e947") -- IL-2 Sturmovik: Battle of Stalingrad Content
+setManifestid(307961, "3362147066090258945", 101446882847)
+-- SHARED DEPOTS (from other apps)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") -- VC 2017 Redist (Shared from App 228980)
+setManifestid(228987, "4302102680580581867", 29664201)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") -- .NET 4.0 Redist (Shared from App 228980)
+setManifestid(229002, "7260605429366465749", 50450161)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(307970) -- IL-2 Sturmovik La-5 Series 8 Collector Plane
+addtoken(307970, "17925414906939921191")
+addappid(307971) -- IL-2 Sturmovik Fw 190 A-3 Collector Plane 
+addtoken(307971, "18122828786672535331")
+addappid(502860) -- IL-2 Sturmovik Yak-1b Collector Plane
+addappid(502870) -- IL-2 Sturmovik Battle of Moscow
+addappid(502890) -- IL-2 Sturmovik P-40E-1 Collector Plane
+addappid(502891) -- IL-2 Sturmovik MC.202 Series VIII Collector Plane
+addappid(561400) -- IL-2 Sturmovik Ju 52m Collector Plane
+addappid(629700) -- IL-2 Sturmovik Battle of Kuban
+addappid(630430) -- IL-2 Sturmovik Blazing Steppe Campaign
+addappid(894690) -- IL-2 Sturmovik Hs 129 B-2 Collector Plane
+addappid(894700) -- IL-2 Sturmovik Spitfire Mk.VB Collector Plane
+addappid(894710) -- IL-2 Sturmovik Bf 109 G-6 Collector Plane
+addappid(894720) -- IL-2 Sturmovik La-5FN series 2 Collector Plane
+addappid(1001370) -- IL-2 Sturmovik Fortress on the Volga Campaign
+addappid(1001371) -- IL-2 Sturmovik Polikarpov U-2VS
+addappid(1198460) -- IL-2 Sturmovik Battle of Bodenplatte
+addappid(1198461) -- IL-2 Sturmovik Fw 190 D-9 Collector Plane
+addappid(1198462) -- IL-2 Sturmovik P-38J-25 Collector Plane
+addappid(1198470) -- IL-2 Sturmovik Flying Circus - Volume I
+addappid(1331820) -- IL-2 Sturmovik Yak-9 Series 1 Collector Plane
+addappid(1331821) -- IL-2 Sturmovik Yak-9T Series 1 Collector Plane
+addappid(1467120) -- IL-2 Sturmovik Tank Crew - Clash at Prokhorovka
+addappid(1467121) -- IL-2 Sturmovik Hurricane Mk.II Collector Plane
+addappid(1494300) -- IL-2 Sturmovik Ice Ring Campaign
+addappid(1622750) -- IL-2 Sturmovik GAZ-MM 72-K Anti-Aircraft Gun
+addappid(1728910) -- IL-2 Sturmovik Sd.Kfz. 105 Flak 38 Anti-Aircraft Gun
+addappid(2118560) -- IL-2 Sturmovik Battle of Normandy
+addappid(2118561) -- IL-2 Sturmovik Arado Ar 234 B-2 Collector Plane
+addappid(2118562) -- IL-2 Sturmovik Spitfire Mk.XIV Collector Plane
+addappid(2175470) -- IL-2 Sturmovik Churchill IV Collector Vehicle
+addappid(2220830) -- IL-2 Sturmovik Steel Birds Campaign
+addappid(2220831) -- IL-2 Sturmovik StuG III Ausf.G Collector Vehicle
+addappid(2220832) -- IL-2 Sturmovik C-47A Skytrain Collector Plane
+addappid(2255110) -- IL-2 Sturmovik Sopwith Snipe Collector Plane
+addappid(2366320) -- IL-2 Sturmovik Flying Circus - Volume II
+addappid(2366321) -- IL-2 Sturmovik Spitfire Mk.XIVe with Teardrop Canopy
+addappid(2366322) -- IL-2 Sturmovik SSW D.IV Collector Plane
+addappid(2405220) -- IL-2 Sturmovik Bf 109 G-6AS Collector Plane
+addappid(2405221) -- IL-2 Sturmovik Lisunov Li-2 Collector Plane
+addappid(2405222) -- IL-2 Sturmovik I.A.R. 80-A and I.A.R. 80-B Collector Planes
+addappid(2488330) -- IL-2 Sturmovik Sky Nomads Campaign
+addappid(2601040) -- IL-2 Sturmovik Ten Days of Autumn Campaign
+addappid(2601050) -- IL-2 Sturmovik Hell Hawks Over the Bulge Campaign
+addappid(2659090) -- IL-2 Sturmovik La-5F series 38 Collector Plane
+addappid(2659100) -- IL-2 Sturmovik Overlord Campaign
+addappid(2731420) -- IL-2 Sturmovik Flying Circus - Volume III
+addappid(2731430) -- IL-2 Sturmovik CG-4A Collector Plane
+addappid(2731440) -- IL-2 Sturmovik Achtung Spitfire Campaign
+addappid(3021360) -- IL-2 Sturmovik Ta 152 H-1 Collector Plane
+addappid(3053230) -- IL-2 Sturmovik Havoc Over the Kuban Campaign
+addappid(3212100) -- IL-2 Sturmovik Flying Circus - Volume IV
+addappid(3398310) -- IL-2 Sturmovik Gestapo Hunters Campaign
+addappid(3398320) -- IL-2 Sturmovik Iron Wind Campaign
+addappid(4042760) -- IL-2 Sturmovik Hornets Nest Campaign
+addappid(4271990) -- IL-2 Sturmovik Odessa and Leningrad - Siege and Liberation
+addappid(4272000) -- IL-2 Sturmovik Ram Squadron Campaign
+addappid(4513110) -- IL-2 Sturmovik Siege of Odessa Campaigns
