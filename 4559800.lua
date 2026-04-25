0229d119b206349ce2b89e040ebecdf044357fd5
qiao-lima <qiao-lima@github.com>
2026-04-25 23:15:56 +0800

    Manual Update 4559800

---
+++ 4559800.lua
@@ -0,0 +1,18 @@
+-- 4559800's Lua and Manifest Created by Hubcap Manifest
+-- 地狱空荡荡，恶魔在打牌，在打恶魔牌
+-- Created: April 24, 2026 at 10:03:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4559800) -- 地狱空荡荡，恶魔在打牌，在打恶魔牌
+-- MAIN APP DEPOTS
+addappid(4559801, 1, "9f1080482f1ddd34aed80fe6dcc61ee77bbac1f918a4fb1bb00d3d93c8c4923c") -- Depot 4559801
+setManifestid(4559801, "8458831695304326929", 2077228468)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
