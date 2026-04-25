9d35e0e855da8bde1d632c4c96c58dd675cdb5b1
qiao-lima <qiao-lima@github.com>
2026-04-25 23:15:54 +0800

    Manual Update 4328680

---
+++ 4328680.lua
@@ -0,0 +1,14 @@
+-- 4328680's Lua and Manifest Created by Hubcap Manifest
+-- Certified Mother Fucker
+-- Created: April 25, 2026 at 08:25:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4328680, 1, "67aa4fb3ebca446af87a000ad2cb7e739b70c79eaff51c63cd5a31667308b605") -- Certified Mother Fucker
+-- MAIN APP DEPOTS
+addappid(4328681, 1, "a6a8f83ebc322399e021ac22d800cd773fc998fca5f088619db42c14bcf9967a") -- Depot 4328681
+setManifestid(4328681, "8007386081047648694", 1768701016)
+addappid(4328682, 1, "088ea4bbf385684305e237ac849676f6ce53403a4ea355cfd953c0daecfee574") -- Depot 4328682
+setManifestid(4328682, "3321362984008016234", 1760379122)
