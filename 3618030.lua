bba0e3c5f8abe396ca12a14176012dcdfb24b2a9
qiao-lima <qiao-lima@github.com>
2026-04-25 23:08:03 +0800

    Manual Update 3618030

---
+++ 3618030.lua
@@ -0,0 +1,16 @@
+-- 3618030's Lua and Manifest Created by Hubcap Manifest
+-- Flock Around
+-- Created: April 25, 2026 at 03:34:13 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3618030) -- Flock Around
+-- MAIN APP DEPOTS
+addappid(3618031, 1, "9d63dd57e66e2d55e88bde8007ff230f542748c6da92938b9e551528e45b77d7") -- Depot 3618031
+setManifestid(3618031, "8381840859418372884", 795024104)
+addappid(3618032, 1, "1cbce55497296ae110289d3ae41bde579e09e043e3d171f3105d8798b12029e5") -- Depot 3618032
+setManifestid(3618032, "2510378307431490997", 967370000)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4585870) -- Flock Around Supporter Pack
