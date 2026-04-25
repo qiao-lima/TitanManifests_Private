dd3ea05084d3a477dca5dd68f40fa7c81067b969
qiao-lima <qiao-lima@github.com>
2026-04-25 23:07:59 +0800

    Manual Update 1530800

---
+++ 1530800.lua
@@ -0,0 +1,12 @@
+-- 1530800's Lua and Manifest Created by Hubcap Manifest
+-- Frank and Drake
+-- Created: April 25, 2026 at 09:15:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1530800, 1, "b213812e8226a9c64b041a37058ac05dddf8b38dca87506a1292dc27a17302c4") -- Frank and Drake
+-- MAIN APP DEPOTS
+addappid(1530801, 1, "4f000a6817973dbed0370059717fe02efa5770844ca3324a14170a7ff4d1f1ec") -- Depot 1530801
+setManifestid(1530801, "7648519305467739196", 11914403539)
