85026c0f70a2803afa413dcbc9b291466e4a7185
qiao-lima <qiao-lima@github.com>
2026-04-25 23:08:02 +0800

    Manual Update 3395760

---
+++ 3395760.lua
@@ -0,0 +1,14 @@
+-- 3395760's Lua and Manifest Created by Hubcap Manifest
+-- With Me: Aquatic Time
+-- Created: April 24, 2026 at 18:15:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3395760) -- With Me: Aquatic Time
+-- MAIN APP DEPOTS
+addappid(3395762, 1, "d84793ecd8b3d3b05c27fb8a2aaff698e810c7d46bc5274fec7acede353ac610") -- Depot 3395762
+setManifestid(3395762, "6673554724848653890", 1453856610)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3395765) -- Depot 3395765 (empty depot)
