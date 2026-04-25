84582bb4026c0cc67a3325fdb78d207f437033d8
qiao-lima <qiao-lima@github.com>
2026-04-25 23:15:56 +0800

    Manual Update 4572110

---
+++ 4572110.lua
@@ -0,0 +1,12 @@
+-- 4572110's Lua and Manifest Created by Hubcap Manifest
+-- ESCAPE THE NEST
+-- Created: April 25, 2026 at 08:14:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4572110) -- ESCAPE THE NEST
+-- MAIN APP DEPOTS
+addappid(4572111, 1, "d99f258de60dcd8e0846f81d7eb0b0b0fcb79a399054de1dcee42f26246ef22e") -- Depot 4572111
+setManifestid(4572111, "6276112502228808390", 1940861852)
