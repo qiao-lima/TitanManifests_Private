634f6b4bd28bd01be956e6748b87631b21998dab
qiao-lima <qiao-lima@github.com>
2026-04-25 23:08:03 +0800

    Manual Update 3525390

---
+++ 3525390.lua
@@ -0,0 +1,14 @@
+-- 3525390's Lua and Manifest Created by Hubcap Manifest
+-- Coloring Game: Girls 3
+-- Created: April 25, 2026 at 07:16:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3525390) -- Coloring Game: Girls 3
+-- MAIN APP DEPOTS
+addappid(3525391, 1, "4334b43cc94f866a8fac4c0e23efe93e007097a02386d5cb2b888f54c5b54e17") -- Depot 3525391
+setManifestid(3525391, "6583660977214360264", 25367261)
+addappid(3525393, 1, "9e176ae49a69011cde86a0639f3e523e07f28213c0f1e1bf4f28f158ec00cfdf") -- Depot 3525393
+setManifestid(3525393, "3110570026073504443", 191053401)
