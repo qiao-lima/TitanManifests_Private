edb05678232bda7941cd11ff93dc78946398e814
qiao-lima <qiao-lima@github.com>
2026-04-25 23:15:52 +0800

    Manual Update 4125010

---
+++ 4125010.lua
@@ -0,0 +1,12 @@
+-- 4125010's Lua and Manifest Created by Hubcap Manifest
+-- Micro Overdrive: Home Tour
+-- Created: April 24, 2026 at 19:03:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4125010) -- Micro Overdrive: Home Tour
+-- MAIN APP DEPOTS
+addappid(4125011, 1, "60c39c89bb06e07034ea88e115418d1de71d79535dc34403791901c78c027184") -- Depot 4125011
+setManifestid(4125011, "6998736248149569063", 1002238431)
