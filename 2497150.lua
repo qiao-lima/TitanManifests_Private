03bfac5229e26a5ae3800762b5c3cb822bd66ce2
qiao-lima <qiao-lima@github.com>
2026-04-25 23:08:01 +0800

    Manual Update 2497150

---
+++ 2497150.lua
@@ -0,0 +1,20 @@
+-- 2497150's Lua and Manifest Created by Hubcap Manifest
+-- UP TO THE SKY
+-- Created: April 25, 2026 at 11:06:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(2497150) -- UP TO THE SKY
+-- MAIN APP DEPOTS
+addappid(2497151, 1, "18bfb9ea738edc32849ddfc4e478000573194ab74c3ea9cac7f1f2fa1d9b2e43") -- Depot 2497151
+setManifestid(2497151, "6418858004043076326", 2837344792)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") -- .NET 3.5 Redist (Shared from App 228980)
+setManifestid(229000, "4622705914179893434", 242743889)
