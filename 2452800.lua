d6de7aafdf7b914150831317177428ede49b8a3c
qiao-lima <qiao-lima@github.com>
2026-04-25 23:08:00 +0800

    Manual Update 2452800

---
+++ 2452800.lua
@@ -0,0 +1,12 @@
+-- 2452800's Lua and Manifest Created by Hubcap Manifest
+-- EON Trooper
+-- Created: April 25, 2026 at 08:14:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2452800) -- EON Trooper
+-- MAIN APP DEPOTS
+addappid(2452801, 1, "4f472498631a45262738aa5d8ee02c628a3884097227546a28cd49bb8e95f30f") -- Depot 2452801
+setManifestid(2452801, "3829078592789779110", 232639506)
