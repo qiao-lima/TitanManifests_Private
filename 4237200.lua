2fe2dfc726da1ed71d23ed8d6a3113bff1b58e77
qiao-lima <qiao-lima@github.com>
2026-04-25 23:15:54 +0800

    Manual Update 4237200

---
+++ 4237200.lua
@@ -0,0 +1,16 @@
+-- 4237200's Lua and Manifest Created by Hubcap Manifest
+-- Elise and the Spellbinding Marionette
+-- Created: April 25, 2026 at 08:14:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4237200) -- Elise and the Spellbinding Marionette
+-- MAIN APP DEPOTS
+addappid(4237201, 1, "a534c27216c4168f4ead2a025d16e835733e9dcb1695b9a5cc31c93898bfe5af") -- Depot 4237201
+setManifestid(4237201, "4291315518382228301", 657305503)
+addappid(4237202, 1, "7aac0b8d2bac38eecb526cdf3a1c94e3f48150a4f7f5c6a8051fa3a31ed0c302") -- Depot 4237202
+setManifestid(4237202, "4711930171532648760", 29469564)
+addappid(4237203, 1, "b8cc28ccaa097e0c117ce78c3d2efa2eeb2711af70993eadfcbc8d43e69304c5") -- Depot 4237203
+setManifestid(4237203, "6108028414821353829", 15458105)
