21145ca14186a893d3f5253bccd1cfbddd9bc5e1
qiao-lima <qiao-lima@github.com>
2026-04-25 23:08:00 +0800

    Manual Update 2381810

---
+++ 2381810.lua
@@ -0,0 +1,38 @@
+-- 2381810's Lua and Manifest Created by Hubcap Manifest
+-- The Paper Arcade
+-- Created: April 25, 2026 at 10:07:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 5
+-- Total DLCs: 4 (5 excluded)
+
+-- MAIN APPLICATION
+addappid(2381810) -- The Paper Arcade
+-- MAIN APP DEPOTS
+addappid(2381811, 1, "f30fc58e87de4acde59ab0f995e77f0d48d2d9c152425a644dc48dd6180bf5c0") -- Depot 2381811
+setManifestid(2381811, "5421956572289970779", 66824393)
+-- DLCS WITH DEDICATED DEPOTS
+-- The Paper Arcade - Maze Chase (AppID: 2426480)
+addappid(2426480)
+addappid(2426480, 1, "78a2491337113470c62573e8580b468bb3af76687b291b82aa09046e29888bcc") -- The Paper Arcade - Maze Chase - Depot 2426480
+setManifestid(2426480, "4247078392092509466", 2803676)
+-- The Paper Arcade - Paper Bird (AppID: 2426510)
+addappid(2426510)
+addappid(2426510, 1, "8b2fe27f6f8a2cb99814f7996d83c4ad16db1ceb1dfcbdb9885bf24ffb1c64d3") -- The Paper Arcade - Paper Bird - Depot 2426510
+setManifestid(2426510, "3567392014555497136", 7070414)
+-- The Paper Arcade Singing Sparrow (AppID: 2431420)
+addappid(2431420)
+addappid(2431420, 1, "ebc049ec6c30706547caa63b53b19c5129c88dc83a80d2487ce58df8c1910282") -- The Paper Arcade Singing Sparrow - Depot 2431420
+setManifestid(2431420, "152018835008709576", 7303295)
+-- The Paper Arcade Kingdom Kitty (AppID: 2431421)
+addappid(2431421)
+addappid(2431421, 1, "2fb9c8b1bf0ad63779c520ef90871e18db17b4fad6ba240b52377e929fe6ae51") -- The Paper Arcade Kingdom Kitty - Depot 2431421
+setManifestid(2431421, "2254572621373692938", 10553556)
+-- EXCLUDED DLCS:
+-- UNRELEASED DLCS (COMMENTED OUT)
+-- addappid(2426490) -- DLC 2426490 (unreleased)
+-- addappid(2426491) -- DLC 2426491 (unreleased)
+-- addappid(2426540) -- DLC 2426540 (unreleased)
+-- addappid(2426541) -- DLC 2426541 (unreleased)
+-- addappid(2426542) -- DLC 2426542 (unreleased)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2426480) -- Depot 2426480 (empty depot)
