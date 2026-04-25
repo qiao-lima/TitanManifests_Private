561cf6db9c2de015f6596726ad4d72c4484d476b
qiao-lima <qiao-lima@github.com>
2026-04-25 23:08:01 +0800

    Manual Update 3126200

---
+++ 3126200.lua
@@ -0,0 +1,18 @@
+-- 3126200's Lua and Manifest Created by Hubcap Manifest
+-- Divine Wish
+-- Created: April 25, 2026 at 08:16:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3126200) -- Divine Wish
+-- MAIN APP DEPOTS
+addappid(3126201, 1, "7eabfdf0f35a6c077432744d7459f039c066cc267ad5467a03fb9f92fc941590") -- Depot 3126201
+setManifestid(3126201, "6498852300904067460", 795471132)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
