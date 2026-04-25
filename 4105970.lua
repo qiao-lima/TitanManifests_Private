946e92b736fed560ea83c944925d288474e47dd7
qiao-lima <qiao-lima@github.com>
2026-04-25 23:15:52 +0800

    Manual Update 4105970

---
+++ 4105970.lua
@@ -0,0 +1,12 @@
+-- 4105970's Lua and Manifest Created by Hubcap Manifest
+-- Otaku and Bubbly Girl - 游戏宅与青春女
+-- Created: April 24, 2026 at 22:41:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4105970) -- Otaku and Bubbly Girl - 游戏宅与青春女
+-- MAIN APP DEPOTS
+addappid(4105971, 1, "c2c34e9dfa6975c94b76dd99fe82f827d65d1f2ae997cad3dc85b0383315982f") -- Depot 4105971
+setManifestid(4105971, "1221290281812863224", 947767182)
