6d3f0bbbfb0604bdd83c179266da037a5f542a95
qiao-lima <qiao-lima@github.com>
2026-04-25 23:15:53 +0800

    Manual Update 4182770

---
+++ 4182770.lua
@@ -0,0 +1,18 @@
+-- 4182770's Lua and Manifest Created by Hubcap Manifest
+-- Lost in Voice
+-- Created: April 24, 2026 at 14:43:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4182770) -- Lost in Voice
+-- MAIN APP DEPOTS
+addappid(4182771, 1, "194252b78f5b9881277b4dd67ddc68c03ec24b7814cf11c905f77fee26e2e982") -- Depot 4182771
+setManifestid(4182771, "3383207921119006828", 24934698790)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
