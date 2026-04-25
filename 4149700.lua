0a9d67fc804f6996aa2e09f0f64b04c688de5e59
qiao-lima <qiao-lima@github.com>
2026-04-25 23:15:53 +0800

    Manual Update 4149700

---
+++ 4149700.lua
@@ -0,0 +1,12 @@
+-- 4149700's Lua and Manifest Created by Hubcap Manifest
+-- Dreamcore: Rabbit Hole
+-- Created: April 25, 2026 at 11:04:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4149700) -- Dreamcore: Rabbit Hole
+-- MAIN APP DEPOTS
+addappid(4149701, 1, "a25dab69be88fc2f6bef1faa5de4b440efe2116cd93a9cefcdaa232612fbc0ac") -- Depot 4149701
+setManifestid(4149701, "8926353069961185507", 10117714395)
