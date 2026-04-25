4035ba81e8b783e42449d0fc4fb02d6a0bc345c7
qiao-lima <qiao-lima@github.com>
2026-04-25 23:15:56 +0800

    Manual Update 4572680

---
+++ 4572680.lua
@@ -0,0 +1,12 @@
+-- 4572680's Lua and Manifest Created by Hubcap Manifest
+-- MINDFUNK In 13 Days
+-- Created: April 24, 2026 at 19:04:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4572680) -- MINDFUNK In 13 Days
+-- MAIN APP DEPOTS
+addappid(4572681, 1, "216cadf6ecdf957c0229a852a4c3505f37b0eb4482039edbcc8279d209a35534") -- Depot 4572681
+setManifestid(4572681, "3250887383602154503", 772030646)
