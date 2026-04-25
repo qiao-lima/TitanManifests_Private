0fecbaf43bdd2e70a2b752d1e1c7c2b465c61a93
qiao-lima <qiao-lima@github.com>
2026-04-25 23:15:54 +0800

    Manual Update 4238920

---
+++ 4238920.lua
@@ -0,0 +1,18 @@
+-- 4238920's Lua and Manifest Created by Hubcap Manifest
+-- Paws Coven
+-- Created: April 24, 2026 at 21:18:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4238920) -- Paws Coven
+-- MAIN APP DEPOTS
+addappid(4238921, 1, "d966f5dbd2f94f2cf8bf3417f8c3ac50e63c55be1d89528eb3e0c6de60ce5f47") -- Depot 4238921
+setManifestid(4238921, "8330256573412847464", 3181158952)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
