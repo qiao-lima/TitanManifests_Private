fa6b2486a8b1a40036c2803bf28362a466342f58
qiao-lima <qiao-lima@github.com>
2026-04-25 23:15:55 +0800

    Manual Update 4533810

---
+++ 4533810.lua
@@ -0,0 +1,18 @@
+-- 4533810's Lua and Manifest Created by Hubcap Manifest
+-- Todomeki
+-- Created: April 24, 2026 at 13:24:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4533810) -- Todomeki
+-- MAIN APP DEPOTS
+addappid(4533811, 1, "c3012c5f0ed0cf786be87d259b2755c9e78a6512e595f63ada6472489a455291") -- Depot 4533811
+setManifestid(4533811, "8187894117382956493", 943890967)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
