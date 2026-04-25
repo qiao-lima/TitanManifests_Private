ee4d35d610b691b8d4d1d3336d303455a9d3df17
qiao-lima <qiao-lima@github.com>
2026-04-25 23:08:00 +0800

    Manual Update 2381570

---
+++ 2381570.lua
@@ -0,0 +1,16 @@
+-- 2381570's Lua and Manifest Created by Hubcap Manifest
+-- Circus of the Moon
+-- Created: April 25, 2026 at 08:14:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2381570) -- Circus of the Moon
+-- MAIN APP DEPOTS
+addappid(2381571, 1, "0b940db7b60878894abbb91aba886ffda82363d30012ecad1c5efc92612814c4") -- Depot 2381571
+setManifestid(2381571, "4428595544098018865", 611756070)
+addappid(2381572, 1, "2fbb09a4985ef1c0b23276290104d4809c4d1d8827d615cd5582548fd0d46265") -- Depot 2381572
+setManifestid(2381572, "4718976962757159803", 635733960)
+addappid(2381573, 1, "b06ba2c14acc21df9487d0600e954732e913345a494cd228bf19699ef1b4c516") -- Depot 2381573
+setManifestid(2381573, "7752606628810178008", 601977816)
