1a98e1de8680f81c0638f494d2bc43b99c6346b8
qiao-lima <qiao-lima@github.com>
2026-04-25 23:15:55 +0800

    Manual Update 4481350

---
+++ 4481350.lua
@@ -0,0 +1,12 @@
+-- 4481350's Lua and Manifest Created by Hubcap Manifest
+-- 绝色特工2
+-- Created: April 24, 2026 at 19:03:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4481350) -- 绝色特工2
+-- MAIN APP DEPOTS
+addappid(4481351, 1, "1c5b9621f14264c928be5c01abe4a6ce24e1f9d23ce3b5e6265da6cad2852c8e") -- Depot 4481351
+setManifestid(4481351, "2992886759944614796", 9824427473)
