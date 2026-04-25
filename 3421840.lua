ec2ead94a85c51e6b8cacece393df65c66e52feb
qiao-lima <qiao-lima@github.com>
2026-04-25 23:08:02 +0800

    Manual Update 3421840

---
+++ 3421840.lua
@@ -0,0 +1,14 @@
+-- 3421840's Lua and Manifest Created by Hubcap Manifest
+-- 宝贝别怕，有我在
+-- Created: April 25, 2026 at 07:16:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3421840) -- 宝贝别怕，有我在
+-- MAIN APP DEPOTS
+addappid(3421843, 1, "c50d904d142674b0d76f4e98825c345cfb1e6ba8ef6390faa5ff00b3d08f822d") -- Depot 3421843
+setManifestid(3421843, "2995491172854257039", 49244149967)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3421842) -- Depot 3421842 (empty depot)
