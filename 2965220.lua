54d8ec20337234078026a90ebb164d0da294d8df
qiao-lima <qiao-lima@github.com>
2026-04-25 23:08:01 +0800

    Manual Update 2965220

---
+++ 2965220.lua
@@ -0,0 +1,20 @@
+-- 2965220's Lua and Manifest Created by Hubcap Manifest
+-- Strings of Fate XI: Magic dream
+-- Created: April 25, 2026 at 08:14:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2965220) -- Strings of Fate XI: Magic dream
+-- MAIN APP DEPOTS
+addappid(2965222, 1, "d1df50395e246fcc5241c79ab1b4cb761163d5d1362e32f3b5924c65bb727b73") -- Depot 2965222
+setManifestid(2965222, "7126223892489081157", 16918168363)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2965221) -- Depot 2965221 (empty depot)
