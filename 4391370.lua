549ae251d11b4b95a2405b8467ebb6357d15ce12
qiao-lima <qiao-lima@github.com>
2026-04-25 23:15:55 +0800

    Manual Update 4391370

---
+++ 4391370.lua
@@ -0,0 +1,12 @@
+-- 4391370's Lua and Manifest Created by Hubcap Manifest
+-- Hentai Misaki
+-- Created: April 25, 2026 at 02:30:08 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4391370) -- Hentai Misaki
+-- MAIN APP DEPOTS
+addappid(4391371, 1, "e0adb3609d749e2285821583b770b28c616f4143600bea97ceb4363074522282") -- Depot 4391371
+setManifestid(4391371, "8122840835214754108", 240828217)
