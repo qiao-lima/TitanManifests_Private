54c0609eaa04a2d1cba9740f69985c718508c147
qiao-lima <qiao-lima@github.com>
2026-04-25 23:08:03 +0800

    Manual Update 3612130

---
+++ 3612130.lua
@@ -0,0 +1,16 @@
+-- 3612130's Lua and Manifest Created by Hubcap Manifest
+-- Stack to Ascension
+-- Created: April 25, 2026 at 11:06:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3612130) -- Stack to Ascension
+-- MAIN APP DEPOTS
+addappid(3612131, 1, "1150dc8e31ded80efed61a28bcd49ac84a2661d8a408ab57bc288fd08485f89c") -- Depot 3612131
+setManifestid(3612131, "2460842841819273562", 1741792268)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
