ab4f77a71a25ae325cfd4a128086e8a3a9b38daa
qiao-lima <qiao-lima@github.com>
2026-04-25 23:08:00 +0800

    Manual Update 2010010

---
+++ 2010010.lua
@@ -0,0 +1,12 @@
+-- 2010010's Lua and Manifest Created by Hubcap Manifest
+-- Boomerang Jack
+-- Created: April 25, 2026 at 08:15:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2010010) -- Boomerang Jack
+-- MAIN APP DEPOTS
+addappid(2010011, 1, "374f6f4fbd9c27f232c7caccbed1157a7b5948a8504aad75ca481d136231ac44") -- Depot 2010011
+setManifestid(2010011, "5073318176637771317", 1033946911)
