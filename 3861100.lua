14e26bcc5bdf051a2fb91fe308d846eb03b98300
qiao-lima <qiao-lima@github.com>
2026-04-27 22:54:41 +0800

    Manual Update 3861100

---
+++ 3861100.lua
@@ -0,0 +1,14 @@
+-- 3861100's Lua and Manifest Created by Hubcap Manifest
+-- 1492 - Colonization of the new world
+-- Created: April 25, 2026 at 12:59:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3861100, 1, "6f632423687a9ee4c4d83fbe46eed8354659e1a1fd7d4b884d4965cde8f66c9c") -- 1492 - Colonization of the new world
+-- MAIN APP DEPOTS
+addappid(3861101, 1, "e62ff6cca5a907284a2c81bfcfa6dc1efe83010a23a757fe3cad75e0c150e5c5") -- Depot 3861101
+setManifestid(3861101, "8758420287851438912", 736660249)
+addappid(3861102, 1, "dc9c5299a0e9fea756d9604012b42b97a20b875f97fbe4cc405f9417f75891c3") -- Depot 3861102
+setManifestid(3861102, "5587784767050215835", 736722786)
