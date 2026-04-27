93142e280806b5c35749af0991ad8db4ac4952cb
qiao-lima <qiao-lima@github.com>
2026-04-27 22:54:38 +0800

    Manual Update 2834250

---
+++ 2834250.lua
@@ -0,0 +1,12 @@
+-- 2834250's Lua and Manifest Created by Hubcap Manifest
+-- Frogbound: the Legend of Sir Hopper
+-- Created: April 27, 2026 at 10:22:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2834250) -- Frogbound: the Legend of Sir Hopper
+-- MAIN APP DEPOTS
+addappid(2834251, 1, "9e9ac14c6504cf671e08a97dd602c6791987706271c3a5724b120bd66fccd359") -- Depot 2834251
+setManifestid(2834251, "3677326492793366336", 162488223)
