f42fe8bb05d19317146ae05f18925d37267bc7ce
qiao-lima <qiao-lima@github.com>
2026-04-27 22:54:38 +0800

    Manual Update 2614100

---
+++ 2614100.lua
@@ -0,0 +1,16 @@
+-- 2614100's Lua and Manifest Created by Hubcap Manifest
+-- Demon's GunPlay 2
+-- Created: April 26, 2026 at 09:37:36 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2614100) -- Demon's GunPlay 2
+-- MAIN APP DEPOTS
+addappid(2614101, 1, "c4a1045c71e4b91e01077012201ef0f7cfcba55379632e6dff69f941a33505c5") -- Depot 2614101
+setManifestid(2614101, "1209836456126591660", 1083168093)
+addappid(2614102, 1, "f7ba0cd15f023873063928e9659bb277de991edc067238a1217dbc7eefd025a3") -- Depot 2614102
+setManifestid(2614102, "2467863615780256431", 1108923902)
+addappid(2614103, 1, "dc4a70402d6b76ed912f78594b8be914ec48119722e2d1fa06c347ed91735fe3") -- Depot 2614103
+setManifestid(2614103, "7966340701875973330", 1094465531)
