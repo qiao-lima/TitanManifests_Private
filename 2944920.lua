461f3739290f7ac3bfad47443aedb83d7372ae3f
qiao-lima <qiao-lima@github.com>
2026-04-27 22:54:39 +0800

    Manual Update 2944920

---
+++ 2944920.lua
@@ -0,0 +1,12 @@
+-- 2944920's Lua and Manifest Created by Hubcap Manifest
+-- Octo's Balloon Challenge
+-- Created: April 26, 2026 at 22:56:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2944920) -- Octo's Balloon Challenge
+-- MAIN APP DEPOTS
+addappid(2944921, 1, "93593bb8e899db922cca003148db3dc6ccf169260a3c65913ec28bfe440a3e09") -- Depot 2944921
+setManifestid(2944921, "7819348427247496592", 189847842)
