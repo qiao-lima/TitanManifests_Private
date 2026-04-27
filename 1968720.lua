254ed22397cb477c17356267e617a50231e2ce58
qiao-lima <qiao-lima@github.com>
2026-04-27 22:54:37 +0800

    Manual Update 1968720

---
+++ 1968720.lua
@@ -0,0 +1,12 @@
+-- 1968720's Lua and Manifest Created by Hubcap Manifest
+-- Paracreat
+-- Created: April 26, 2026 at 11:15:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1968720) -- Paracreat
+-- MAIN APP DEPOTS
+addappid(1968721, 1, "ecca78e2bafdd677c8227ce84f6ebd33169e6eab2db228dc18eeb443f0aba52f") -- Depot 1968721
+setManifestid(1968721, "8620178398254808530", 388605735)
