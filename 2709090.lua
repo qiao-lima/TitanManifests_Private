0eb8bffb6651de910c7eea51dec55ceec3989cad
qiao-lima <qiao-lima@github.com>
2026-04-27 22:54:38 +0800

    Manual Update 2709090

---
+++ 2709090.lua
@@ -0,0 +1,16 @@
+-- 2709090's Lua and Manifest Created by Hubcap Manifest
+-- Cycle of Corruption
+-- Created: April 26, 2026 at 08:40:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(2709090) -- Cycle of Corruption
+-- MAIN APP DEPOTS
+addappid(2709091, 1, "15f215ed95c43b217eeecf0ab25da70a62fedb1a8dab9e8e61c0abb9fb3b99ba") -- Depot 2709091
+setManifestid(2709091, "3235612557015031279", 726973906)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Cycle of Corruption - Official Walkthrough (AppID: 4593740) - missing depot keys
+-- addappid(4593740)
