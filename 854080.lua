0c0a883cb68367b2851a1e5f0f1d8cb153889250
qiao-lima <qiao-lima@github.com>
2026-04-27 23:01:14 +0800

    Manual Update 854080

---
+++ 854080.lua
@@ -0,0 +1,12 @@
+-- 854080's Lua and Manifest Created by Hubcap Manifest
+-- Dread Trials
+-- Created: April 26, 2026 at 02:43:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(854080) -- Dread Trials
+-- MAIN APP DEPOTS
+addappid(854081, 1, "66e7da1cd5f7226043867837a0df41a4ed1603a2cac57b35e9a8ce443c4d1370") -- Depot 854081
+setManifestid(854081, "6163057155552815499", 5321776690)
