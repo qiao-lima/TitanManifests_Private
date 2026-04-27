5800a504f5e2be482a90b1a4dd78a9971158ae64
qiao-lima <qiao-lima@github.com>
2026-04-27 22:54:40 +0800

    Manual Update 3511360

---
+++ 3511360.lua
@@ -0,0 +1,18 @@
+-- 3511360's Lua and Manifest Created by Hubcap Manifest
+-- Zombus
+-- Created: April 25, 2026 at 18:34:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3511360) -- Zombus
+-- MAIN APP DEPOTS
+addappid(3511361, 1, "817f596990580fe0102df163cccd02bdebe36ab364cbe547f129be20eae8a1f6") -- Depot 3511361
+setManifestid(3511361, "7828666799568192308", 2522395390)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
