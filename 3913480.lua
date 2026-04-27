b3ad9d348a00f942965b7afa0da8fc71c9f0a557
qiao-lima <qiao-lima@github.com>
2026-04-27 23:01:11 +0800

    Manual Update 3913480

---
+++ 3913480.lua
@@ -0,0 +1,12 @@
+-- 3913480's Lua and Manifest Created by Hubcap Manifest
+-- Apocalypse with Femboy
+-- Created: April 26, 2026 at 09:37:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3913480) -- Apocalypse with Femboy
+-- MAIN APP DEPOTS
+addappid(3913481, 1, "8fbe784a17befc59e9ad3ad4b02f4236f5d083683be33b0b68a8b6e544ec879f") -- Depot 3913481
+setManifestid(3913481, "1630900685956142540", 627708954)
