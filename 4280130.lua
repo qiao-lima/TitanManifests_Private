9fecce85aeae61872aca733609f47a91a3fdde01
qiao-lima <qiao-lima@github.com>
2026-04-27 23:01:12 +0800

    Manual Update 4280130

---
+++ 4280130.lua
@@ -0,0 +1,16 @@
+-- 4280130's Lua and Manifest Created by Hubcap Manifest
+-- SEX, SIN & ROCK'N'ROLL 🤘🎸
+-- Created: April 26, 2026 at 09:36:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(4280130) -- SEX, SIN & ROCK'N'ROLL 🤘🎸
+-- MAIN APP DEPOTS
+addappid(4280131, 1, "1abdcd5f6295ca7ebcb1440e4f5b5cdf0c29047e781d3d178265bdc061b25c41") -- Depot 4280131
+setManifestid(4280131, "5229540149485771092", 17100037559)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- SEX, SIN  ROCKNROLL  - Wallpapers 18 (AppID: 4655730) - missing depot keys
+-- addappid(4655730)
