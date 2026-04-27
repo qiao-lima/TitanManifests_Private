eb96694cc1307e0780f7b8c757538dac4568dee8
qiao-lima <qiao-lima@github.com>
2026-04-27 22:54:40 +0800

    Manual Update 3405160

---
+++ 3405160.lua
@@ -0,0 +1,17 @@
+-- 3405160's Lua and Manifest Created by Hubcap Manifest
+-- Yokai Art 2: Tales of the Nine-Tails
+-- Created: April 26, 2026 at 08:02:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3405160) -- Yokai Art 2: Tales of the Nine-Tails
+-- MAIN APP DEPOTS
+addappid(3405161, 1, "ed48f9ed03da805eb57da01c0feb75d51fc40199852aac4b262805843d49cfea") -- Depot 3405161
+setManifestid(3405161, "4114870949671966515", 3742061297)
+-- DLCS WITH DEDICATED DEPOTS
+-- Yokai Art 2 Adult Content Patch (AppID: 3813400)
+addappid(3813400)
+addappid(3813400, 1, "cff9399cc9cee7006513bd244df20633d6dc996354809f0c6d1178483de228c5") -- Yokai Art 2 Adult Content Patch - Depot 3813400
+setManifestid(3813400, "4471025067997849984", 4231283183)
