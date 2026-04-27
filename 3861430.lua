a1a2b2abe215970424d64ed1e4cb16eca96a1d22
qiao-lima <qiao-lima@github.com>
2026-04-27 23:01:10 +0800

    Manual Update 3861430

---
+++ 3861430.lua
@@ -0,0 +1,14 @@
+-- 3861430's Lua and Manifest Created by Hubcap Manifest
+-- Bongo Waifu
+-- Created: April 27, 2026 at 10:51:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3861430, 1, "7b216a9435e5d459163021a3998047bacfee5c36b8ae2626df7185710d0f4d56") -- Bongo Waifu
+-- MAIN APP DEPOTS
+addappid(3861431, 1, "71f3fff69211a1f6edfa3e4d2249ee9823ece3a5c42d281d65141333fa9cb44b") -- Depot 3861431
+setManifestid(3861431, "1968994619317109436", 1088708031)
+addappid(3861432, 1, "80b1a7ab76ef534237ea0500f5d7fa6625bf3e0db67b71ed527ceacacba73b2d") -- Depot 3861432
+setManifestid(3861432, "9122004310721597816", 907952325)
