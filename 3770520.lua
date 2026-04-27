1b8b6f7eeb987044a3d97460a1b8418c7deb4193
qiao-lima <qiao-lima@github.com>
2026-04-27 22:54:41 +0800

    Manual Update 3770520

---
+++ 3770520.lua
@@ -0,0 +1,18 @@
+-- 3770520's Lua and Manifest Created by Hubcap Manifest
+-- STIGMA: The Salem Legacy
+-- Created: April 26, 2026 at 10:48:25 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3770520) -- STIGMA: The Salem Legacy
+-- MAIN APP DEPOTS
+addappid(3770521, 1, "e594bc4523c59c9f0146b16e5c0eb8b8881f6a3bef50fc443806c2dc68a7351a") -- Depot 3770521
+setManifestid(3770521, "7537331064933591295", 6707983564)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
