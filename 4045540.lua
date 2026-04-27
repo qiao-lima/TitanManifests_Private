43bdd1985ee90c65546b68fc0abf7f712d1cf190
qiao-lima <qiao-lima@github.com>
2026-04-27 23:01:11 +0800

    Manual Update 4045540

---
+++ 4045540.lua
@@ -0,0 +1,12 @@
+-- 4045540's Lua and Manifest Created by Hubcap Manifest
+-- Agrivore
+-- Created: April 27, 2026 at 06:56:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4045540) -- Agrivore
+-- MAIN APP DEPOTS
+addappid(4045541, 1, "2823210c59153a486695114d2646114c2b2301612a9d1509eb10ae6ec5e6823a") -- Depot 4045541
+setManifestid(4045541, "4271074664949488545", 295207139)
