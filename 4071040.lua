872eb37301c1b536eaf445a592cc41007055d1da
qiao-lima <qiao-lima@github.com>
2026-04-27 23:01:12 +0800

    Manual Update 4071040

---
+++ 4071040.lua
@@ -0,0 +1,12 @@
+-- 4071040's Lua and Manifest Created by Hubcap Manifest
+-- 三国：百将牌
+-- Created: April 26, 2026 at 09:38:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4071040) -- 三国：百将牌
+-- MAIN APP DEPOTS
+addappid(4071041, 1, "4f4b2013d7a120b9d6d31d9fee9ee80013f78690821ac9154c19fc79742fe694") -- Depot 4071041
+setManifestid(4071041, "4713215827584118917", 3029143389)
