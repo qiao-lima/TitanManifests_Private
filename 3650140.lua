78d8fea1dd49095a6ed154917a5760fb78f303b8
qiao-lima <qiao-lima@github.com>
2026-04-27 22:54:40 +0800

    Manual Update 3650140

---
+++ 3650140.lua
@@ -0,0 +1,12 @@
+-- 3650140's Lua and Manifest Created by Hubcap Manifest
+-- Internet Entrepreneurship Simulator
+-- Created: April 25, 2026 at 18:34:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3650140) -- Internet Entrepreneurship Simulator
+-- MAIN APP DEPOTS
+addappid(3650141, 1, "af2ac4471a6cb1cb1374ff8f7792074e3d9c0899417f4565e62c4d9b09cb6df0") -- Depot 3650141
+setManifestid(3650141, "8632761448229777520", 648396207)
