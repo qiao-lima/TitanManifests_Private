4dd08af54941dd1772c1555dbc133bceded23136
qiao-lima <qiao-lima@github.com>
2026-04-27 23:01:13 +0800

    Manual Update 4512330

---
+++ 4512330.lua
@@ -0,0 +1,12 @@
+-- 4512330's Lua and Manifest Created by Hubcap Manifest
+-- Pretty Girl Puzzle · Summer Phantom
+-- Created: April 26, 2026 at 09:38:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4512330) -- Pretty Girl Puzzle · Summer Phantom
+-- MAIN APP DEPOTS
+addappid(4512331, 1, "2a0aa4e9a4945b7fa489a2d2bdcf535460fd499b43ad58224a5fd6ce863db2cb") -- Depot 4512331
+setManifestid(4512331, "5111343390159607801", 378428211)
