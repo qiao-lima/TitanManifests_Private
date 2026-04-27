0f7f7af6664313d49e6c2d31dc44031aa27f1347
qiao-lima <qiao-lima@github.com>
2026-04-27 23:01:12 +0800

    Manual Update 4273130

---
+++ 4273130.lua
@@ -0,0 +1,12 @@
+-- 4273130's Lua and Manifest Created by Hubcap Manifest
+-- Frontier Fire
+-- Created: April 26, 2026 at 09:37:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4273130) -- Frontier Fire
+-- MAIN APP DEPOTS
+addappid(4273131, 1, "68a35e4d32d295bc5013acc0a6aab4942ec55807588cffde03621fdb69a64409") -- Depot 4273131
+setManifestid(4273131, "8784169557010499607", 649576105)
