df7f78fff4d59995cf7495801e9223a0dcaeebb6
qiao-lima <qiao-lima@github.com>
2026-04-27 22:54:39 +0800

    Manual Update 2944950

---
+++ 2944950.lua
@@ -0,0 +1,12 @@
+-- 2944950's Lua and Manifest Created by Hubcap Manifest
+-- Toad's Soul Hopper
+-- Created: April 26, 2026 at 23:56:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2944950) -- Toad's Soul Hopper
+-- MAIN APP DEPOTS
+addappid(2944951, 1, "91dcb3871f84366f3e8cf201ece499503d6719ad7353334ce3bc6eb493a0f741") -- Depot 2944951
+setManifestid(2944951, "6943129686752484235", 1201254781)
