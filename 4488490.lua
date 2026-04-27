f89573c84eaf9a49c528afcdc04e7d4f097ef4a0
qiao-lima <qiao-lima@github.com>
2026-04-27 23:01:13 +0800

    Manual Update 4488490

---
+++ 4488490.lua
@@ -0,0 +1,12 @@
+-- 4488490's Lua and Manifest Created by Hubcap Manifest
+-- MY VAMPIRE WIFE
+-- Created: April 25, 2026 at 08:49:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4488490) -- MY VAMPIRE WIFE
+-- MAIN APP DEPOTS
+addappid(4488491, 1, "1c866e2b12eb55c59ddfe09bfe67ec06c7495819d0b3189bbf822845372ae2df") -- Depot 4488491
+setManifestid(4488491, "3927306808714772872", 3659616545)
