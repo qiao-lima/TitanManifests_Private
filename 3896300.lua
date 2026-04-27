dafcab21b3072de8a144f599a527d7158cb08852
qiao-lima <qiao-lima@github.com>
2026-04-27 23:01:11 +0800

    Manual Update 3896300

---
+++ 3896300.lua
@@ -0,0 +1,14 @@
+-- 3896300's Lua and Manifest Created by Hubcap Manifest
+-- Toll Booth Simulator
+-- Created: April 27, 2026 at 08:56:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3896300) -- Toll Booth Simulator
+-- MAIN APP DEPOTS
+addappid(3896301, 1, "2fd5062f0cec3adfec53548aa70d5986bdf372f400f85f7aa862b57caafdf038") -- Depot 3896301
+setManifestid(3896301, "5504098958899312028", 5090297374)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4506650) -- Toll Booth Simulator - Supporter Pack
