24bce2444a0e78d2d80458c38ffabd31bf1576c1
qiao-lima <qiao-lima@github.com>
2026-04-27 22:54:39 +0800

    Manual Update 3029570

---
+++ 3029570.lua
@@ -0,0 +1,16 @@
+-- 3029570's Lua and Manifest Created by Hubcap Manifest
+-- BROTANK
+-- Created: April 25, 2026 at 15:49:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3029570, 1, "5318f2140b434db4654ef2fc7cc985f0b05590c7ab28f75af5000df1c863eff7") -- BROTANK
+-- MAIN APP DEPOTS
+addappid(3029571, 1, "21ea405ae8545044bbd23d8dd9ccc58945490096dd25449b545699257af170e8") -- Depot 3029571
+setManifestid(3029571, "6351834429497798621", 139968816)
+addappid(3029572, 1, "b67a03b7a1d0a77efe6a773a29296bfc9e8076d4e51c25cfb2c0c46496fc20d7") -- Depot 3029572
+setManifestid(3029572, "1931580056228905253", 216284706)
+addappid(3029573, 1, "06510682810f90f2ed975512dee2ce7de382dd96a631b3c615ef6f98de42bc3d") -- Depot 3029573
+setManifestid(3029573, "2955370464979789725", 158646734)
