0c5c9c9ccfeb16406d6b4973fd2a322785528b51
qiao-lima <qiao-lima@github.com>
2026-04-27 23:01:13 +0800

    Manual Update 4530890

---
+++ 4530890.lua
@@ -0,0 +1,12 @@
+-- 4530890's Lua and Manifest Created by Hubcap Manifest
+-- Cyber Bike Duels
+-- Created: April 25, 2026 at 22:53:18 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4530890) -- Cyber Bike Duels
+-- MAIN APP DEPOTS
+addappid(4530891, 1, "1404d9148d2f06e2aff764ed053fc5530bdab1ad6d1681f9039de8f4d971806a") -- Depot 4530891
+setManifestid(4530891, "7568940002700307905", 344099869)
