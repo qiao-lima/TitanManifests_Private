5cf61d41f1bbb156759feb3896518161a9805bdf
qiao-lima <qiao-lima@github.com>
2026-04-27 22:54:38 +0800

    Manual Update 2386620

---
+++ 2386620.lua
@@ -0,0 +1,12 @@
+-- 2386620's Lua and Manifest Created by Hubcap Manifest
+-- Das verrückte Labyrinth
+-- Created: April 26, 2026 at 09:38:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2386620) -- Das verrückte Labyrinth
+-- MAIN APP DEPOTS
+addappid(2386621, 1, "8e4699c383234375e529c2216a3b577e7e9268e5bb03b3173e686f375fc1d999") -- Depot 2386621
+setManifestid(2386621, "2255497582251053148", 646310396)
