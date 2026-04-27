c2dc93b0fa4324cfcda2633e8542d8c3c33e06c2
qiao-lima <qiao-lima@github.com>
2026-04-27 22:54:37 +0800

    Manual Update 1636460

---
+++ 1636460.lua
@@ -0,0 +1,18 @@
+-- 1636460's Lua and Manifest Created by Hubcap Manifest
+-- Magissy
+-- Created: April 26, 2026 at 09:36:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1636460) -- Magissy
+-- MAIN APP DEPOTS
+addappid(1636461, 1, "1a16d9ebfbd5ec0f237897d3d5b6e6b54f182961ae215a3473fc3d20357a6f9c") -- Depot 1636461
+setManifestid(1636461, "6238724748080818049", 11066836426)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
