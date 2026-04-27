2c53ee0b3173293d31e946b5ad7bea43f7965e69
qiao-lima <qiao-lima@github.com>
2026-04-27 22:54:41 +0800

    Manual Update 3853120

---
+++ 3853120.lua
@@ -0,0 +1,12 @@
+-- 3853120's Lua and Manifest Created by Hubcap Manifest
+-- The Window 8:Millennium
+-- Created: April 26, 2026 at 11:00:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3853120, 1, "4fd67f8f611fc2819465930d859a701f81f6174e173d3bb4372fc7469168ab59") -- The Window 8:Millennium
+-- MAIN APP DEPOTS
+addappid(3853121, 1, "9c0effefc35ec999018ad64b71af09f9bf645d13b02e6d3eb6c8094dc3195494") -- Depot 3853121
+setManifestid(3853121, "8282831700973538002", 844135915)
