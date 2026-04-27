f2d9e108abfbf8a677204e32e0543aa6d440ee23
qiao-lima <qiao-lima@github.com>
2026-04-27 22:54:40 +0800

    Manual Update 3570940

---
+++ 3570940.lua
@@ -0,0 +1,17 @@
+-- 3570940's Lua and Manifest Created by Hubcap Manifest
+-- Isekai Bistro
+-- Created: April 26, 2026 at 14:03:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3570940) -- Isekai Bistro
+-- MAIN APP DEPOTS
+addappid(3570941, 1, "fda6e87cead70aa7bd022e1e4fa314b383fe72dc4f6ebf4bbe5c9a367e3055f0") -- Depot 3570941
+setManifestid(3570941, "9045560129893401279", 4531473343)
+-- DLCS WITH DEDICATED DEPOTS
+-- Isekai Bistro Artbook (AppID: 4591780)
+addappid(4591780)
+addappid(4591781, 1, "d279897475d270125126e3d22f62f9773ebc3955ea222f8b414685ab94f046fc") -- Isekai Bistro Artbook - Depot 4591781
+setManifestid(4591781, "9114846704921011404", 194602213)
