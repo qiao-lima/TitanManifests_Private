b70039700e89833036b17731cd869710737f270f
qiao-lima <qiao-lima@github.com>
2026-04-27 23:01:14 +0800

    Manual Update 4587800

---
+++ 4587800.lua
@@ -0,0 +1,12 @@
+-- 4587800's Lua and Manifest Created by Hubcap Manifest
+-- Married Witch: Forbidden Affair
+-- Created: April 26, 2026 at 09:38:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4587800) -- Married Witch: Forbidden Affair
+-- MAIN APP DEPOTS
+addappid(4587801, 1, "766ff00859fb7b7305902b513f9d3142375d5994eb9872b5a73fe3210e767788") -- Depot 4587801
+setManifestid(4587801, "90501626454531203", 562241931)
