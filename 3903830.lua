79f423bcf49e08a845b2969e2fc1803859b05cc0
qiao-lima <qiao-lima@github.com>
2026-04-27 23:01:11 +0800

    Manual Update 3903830

---
+++ 3903830.lua
@@ -0,0 +1,18 @@
+-- 3903830's Lua and Manifest Created by Hubcap Manifest
+-- Arena of Sacrifice
+-- Created: April 25, 2026 at 08:49:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3903830) -- Arena of Sacrifice
+-- MAIN APP DEPOTS
+addappid(3903831, 1, "2fc2f4b6407c9220520f28a8ac0a568a8dd36096eeab1ba3f33d1a65754768b5") -- Depot 3903831
+setManifestid(3903831, "2438606436107297687", 189996588)
+addappid(3903832, 1, "857364ede1af6bf94b6fbbc6ed4c2a41afa4e306ea4ea8fdd85fdb5cad50d175") -- Depot 3903832
+setManifestid(3903832, "2264428907823842431", 189822801)
+addappid(3903833, 1, "722a351eb0abbce6d00cbefbdf0b3e5f9c4f04618bda2a74b6f50ba5ed1640d2") -- Depot 3903833
+setManifestid(3903833, "5320927734858636787", 189660886)
+addappid(3903834, 1, "781714ca33ea9b7c5200a5912960416589848b1e62ed0488d60515a5687a7198") -- Depot 3903834
+setManifestid(3903834, "1086916601688394223", 189673012)
