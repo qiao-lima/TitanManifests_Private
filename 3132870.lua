d0649b9809ce9bb3c92c7816039c8b54fe070624
qiao-lima <qiao-lima@github.com>
2026-04-27 22:54:39 +0800

    Manual Update 3132870

---
+++ 3132870.lua
@@ -0,0 +1,16 @@
+-- 3132870's Lua and Manifest Created by Hubcap Manifest
+-- DarkSwitch
+-- Created: April 22, 2026 at 10:11:30 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3132870) -- DarkSwitch
+-- MAIN APP DEPOTS
+addappid(3132871, 1, "2b3c1e7b2c734a5301f3e11fd1c4823a941979e863e7add919dc10f6cfa8e7e6") -- Depot 3132871
+setManifestid(3132871, "5518465968303199240", 10109861101)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
