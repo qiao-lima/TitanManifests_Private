b301c5c9ec6a13967a3809608b896e3fdfa8ffe4
qiao-lima <qiao-lima@github.com>
2026-04-27 22:54:40 +0800

    Manual Update 3279140

---
+++ 3279140.lua
@@ -0,0 +1,12 @@
+-- 3279140's Lua and Manifest Created by Hubcap Manifest
+-- Swimsuit Girl: Battle to Avoid
+-- Created: April 27, 2026 at 10:21:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3279140) -- Swimsuit Girl: Battle to Avoid
+-- MAIN APP DEPOTS
+addappid(3279141, 1, "0aca1c1555974c06ca7320b0262d3acab7baa58f29ef3cd7204d01fd545a08c5") -- Depot 3279141
+setManifestid(3279141, "7671463460165566828", 390972052)
