22044d4d7e580e7fb246bb67018135fb70b6ce85
qiao-lima <qiao-lima@github.com>
2026-04-27 23:01:14 +0800

    Manual Update 4602100

---
+++ 4602100.lua
@@ -0,0 +1,12 @@
+-- 4602100's Lua and Manifest Created by Hubcap Manifest
+-- Outer Space Bar
+-- Created: April 26, 2026 at 09:38:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4602100) -- Outer Space Bar
+-- MAIN APP DEPOTS
+addappid(4602101, 1, "12955d4eabb34cc0d9c9367f8782dd39c99ccd0dcad83cc4752728f381590a16") -- Depot 4602101
+setManifestid(4602101, "7803050813071822777", 1388057632)
