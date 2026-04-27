a9a5d6457cb02ceb5adf2b1a7d79c90bf7ca4a38
qiao-lima <qiao-lima@github.com>
2026-04-27 23:01:14 +0800

    Manual Update 4556150

---
+++ 4556150.lua
@@ -0,0 +1,20 @@
+-- 4556150's Lua and Manifest Created by Hubcap Manifest
+-- BERSERK TANK
+-- Created: April 25, 2026 at 18:33:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(4556150) -- BERSERK TANK
+-- MAIN APP DEPOTS
+addappid(4556151, 1, "63ea919809e141a46a9812478b9259f3540c667bd4c2cef5d22ece0d4a584e1a") -- Depot 4556151
+setManifestid(4556151, "2994452456357469461", 3044725431)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
