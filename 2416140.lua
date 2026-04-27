32d42ef65a091f003b63a8b41b1b2b99ddb6159c
qiao-lima <qiao-lima@github.com>
2026-04-27 22:54:38 +0800

    Manual Update 2416140

---
+++ 2416140.lua
@@ -0,0 +1,12 @@
+-- 2416140's Lua and Manifest Created by Hubcap Manifest
+-- 萌动魔盒
+-- Created: April 25, 2026 at 18:33:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2416140) -- 萌动魔盒
+-- MAIN APP DEPOTS
+addappid(2416141, 1, "715e4cae0c00c2f46efad6830159038dac8972eb18b8eb68f82d0849b55ef273") -- Depot 2416141
+setManifestid(2416141, "6715654841012612405", 387033827)
