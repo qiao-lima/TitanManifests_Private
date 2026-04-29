403a2ee32ebf27a936ac455916a6f75c055f9459
qiao-lima <qiao-lima@github.com>
2026-04-29 11:56:48 +0800

    Manual Update 2804020

---
+++ 2804020.lua
@@ -0,0 +1,12 @@
+-- 2804020's Lua and Manifest Created by Hubcap Manifest
+-- Rogue Gladius Survivors
+-- Created: April 27, 2026 at 14:06:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2804020) -- Rogue Gladius Survivors
+-- MAIN APP DEPOTS
+addappid(2804021, 1, "5084c0fb05772c643e5569545fcb0cb57b1f374afa5e398fd0cae3534312f0b7") -- Depot 2804021
+setManifestid(2804021, "4396579886816132544", 516568939)
