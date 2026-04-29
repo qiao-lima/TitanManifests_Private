b6e03f0df66bb37fd922b6630637d325c9c6c2a0
qiao-lima <qiao-lima@github.com>
2026-04-29 11:58:04 +0800

    Manual Update 3412900

---
+++ 3412900.lua
@@ -0,0 +1,18 @@
+-- 3412900's Lua and Manifest Created by Hubcap Manifest
+-- 代号三国：龙起
+-- Created: April 28, 2026 at 07:35:36 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3412900) -- 代号三国：龙起
+-- MAIN APP DEPOTS
+addappid(3412901, 1, "1b0053e043a4cdd8f620d8ab2263f9d03add55596f64923327c1633cbe8ade64") -- Depot 3412901
+setManifestid(3412901, "9143195735560008728", 287107388)
+addappid(3412902, 1, "7e0c2a3d2f9eb574e066a6848d94f45a64d95fa86deb5b27c7a5cd5655f665f0") -- Depot 3412902
+setManifestid(3412902, "6079801669962021783", 2658015713)
+addappid(3412903, 1, "1c78b9c94482c21c05ba0936eb1daf7bfb82d2917b2ba176a011567ac6d1897b") -- Depot 3412903
+setManifestid(3412903, "5267239871688238185", 14100479)
+addappid(3412904, 1, "e472012bec404331d0a06079441f61ce3eeaf6cbb409f1af9dffa609d21ef815") -- Depot 3412904
+setManifestid(3412904, "5466482407002457299", 77905233774)
