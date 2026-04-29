cd347d4a801d9be119dc43958ca81fa18810126c
qiao-lima <qiao-lima@github.com>
2026-04-29 19:07:02 +0800

    Manual Update 2389170

---
+++ 2389170.lua
@@ -0,0 +1,19 @@
+-- 2389170's Lua and Manifest Created by Hubcap Manifest
+-- 华夏史诗：战国
+-- Created: April 29, 2026 at 04:34:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(2389170) -- 华夏史诗：战国
+-- MAIN APP DEPOTS
+addappid(2389171, 1, "0d52bd6fc3de22427af2f6912f4b737fa72560c3cebc810b2b5b238ea58eb797") -- Depot 2389171
+setManifestid(2389171, "7101278461037146673", 17131802107)
+-- DLCS WITH DEDICATED DEPOTS
+-- Rebirth from Flame - Supporter Pack (AppID: 4501300)
+addappid(4501300)
+addappid(4501300, 1, "0ccc3bc733b899447a5a2586a9c44183bcf8aeac56ed7b9c67f734086fa73485") -- Rebirth from Flame - Supporter Pack - Depot 4501300
+setManifestid(4501300, "5897515274804285274", 133390894)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2389172) -- Depot 2389172 (empty depot)
