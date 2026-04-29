b9040f66575ef78e1afe54bd71b00018c14aa4d6
qiao-lima <qiao-lima@github.com>
2026-04-29 11:56:47 +0800

    Manual Update 2177020

---
+++ 2177020.lua
@@ -0,0 +1,12 @@
+-- 2177020's Lua and Manifest Created by Hubcap Manifest
+-- Kanji RPG
+-- Created: April 28, 2026 at 09:38:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2177020) -- Kanji RPG
+-- MAIN APP DEPOTS
+addappid(2177021, 1, "ca1fe9d23515e379503b1919a1e932845d09874500a311190ebac2e6b15a7d79") -- Depot 2177021
+setManifestid(2177021, "6521734556901457529", 497918207)
