51b1e2c56e8bfbbdf9fc216a6fd3ca3ee762d448
qiao-lima <qiao-lima@github.com>
2026-04-29 11:58:06 +0800

    Manual Update 3864100

---
+++ 3864100.lua
@@ -0,0 +1,12 @@
+-- 3864100's Lua and Manifest Created by Hubcap Manifest
+-- Tales of Terrarum
+-- Created: April 27, 2026 at 22:02:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3864100) -- Tales of Terrarum
+-- MAIN APP DEPOTS
+addappid(3864101, 1, "4becacb1391b3d5bc237829bb19870c0f8eccf22b863bb8102d537f28bba035f") -- Depot 3864101
+setManifestid(3864101, "7874104820520237086", 3499646417)
