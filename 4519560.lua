0853c33438121035dd931123a6e3415b04fcb25a
qiao-lima <qiao-lima@github.com>
2026-04-29 11:58:09 +0800

    Manual Update 4519560

---
+++ 4519560.lua
@@ -0,0 +1,12 @@
+-- 4519560's Lua and Manifest Created by Hubcap Manifest
+-- Jinmengyo
+-- Created: April 28, 2026 at 09:37:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4519560) -- Jinmengyo
+-- MAIN APP DEPOTS
+addappid(4519561, 1, "a00db9f181f98a870b0829dc5e5af06b6041e61a010277b9a35d94b0b891047f") -- Depot 4519561
+setManifestid(4519561, "7835295076888616619", 729275907)
