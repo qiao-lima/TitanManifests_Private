ed30cb1208f0264ec23b08933d9ff661373e8d15
qiao-lima <qiao-lima@github.com>
2026-04-29 11:58:07 +0800

    Manual Update 3966480

---
+++ 3966480.lua
@@ -0,0 +1,12 @@
+-- 3966480's Lua and Manifest Created by Hubcap Manifest
+-- Lustquake Legion
+-- Created: April 27, 2026 at 08:01:19 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3966480) -- Lustquake Legion
+-- MAIN APP DEPOTS
+addappid(3966481, 1, "7dccb660ccbdf253001b3a86ddc923ae528eebdd6acb3e308d866008593cd77b") -- Depot 3966481
+setManifestid(3966481, "1495206079793093054", 669194821)
