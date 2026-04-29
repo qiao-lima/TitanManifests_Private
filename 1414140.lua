354dcad5a2cc1e12af106968ddb3c60cf19629b3
qiao-lima <qiao-lima@github.com>
2026-04-29 11:56:45 +0800

    Manual Update 1414140

---
+++ 1414140.lua
@@ -0,0 +1,34 @@
+-- 1414140's Lua and Manifest Created by Hubcap Manifest
+-- 魂之刃 Blade of God
+-- Created: April 27, 2026 at 14:02:29 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 4
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1414140) -- 魂之刃 Blade of God
+addtoken(1414140, "13523991797460292140")
+-- MAIN APP DEPOTS
+addappid(1414141, 1, "27dfaaaa23a8b140c7402ced9b9cea4477fe510c5f99044794854cd26e097957") -- Depot 1414141
+setManifestid(1414141, "3318844734042100210", 4806164545)
+-- SHARED DEPOTS (from other apps)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+--  (AppID: 1447530)
+addappid(1447530)
+addtoken(1447530, "1637713886368322908")
+addappid(1447530, 1, "7b970b8ccd3b3c29aef3f842c5ca557d76d2cbdc33bd52721d44e13c36fa9bff") -- Depot 1447530
+setManifestid(1447530, "6087998774623766783", 14)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1447500) -- 
+addtoken(1447500, "1403187673464077830")
+addappid(1447510) -- 
+addtoken(1447510, "7258711102168386579")
+addappid(1447520) -- 
+addtoken(1447520, "270715353593369284")
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(1447500) -- Depot 1447500 (empty depot)
+-- addappid(1447510) -- Depot 1447510 (empty depot)
+-- addappid(1447520) -- Depot 1447520 (empty depot)
