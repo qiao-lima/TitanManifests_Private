dbd2169045cb608c8877e1456e5abba8ec61663d
qiao-lima <qiao-lima@github.com>
2026-04-29 11:56:46 +0800

    Manual Update 2022380

---
+++ 2022380.lua
@@ -0,0 +1,17 @@
+-- 2022380's Lua and Manifest Created by Hubcap Manifest
+-- 房间的秘密
+-- Created: April 27, 2026 at 14:00:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2022380, 1, "25e4d8d74c247c173e570faec4b5a4f2b3a957b1a8e316726f09c5b3c733e53b") -- 房间的秘密
+addtoken(2022380, "2753040714255056285")
+-- MAIN APP DEPOTS
+addappid(2022381, 1, "152181a0a68504264e19e099ed2a62210e712d11f24f2c6d02016c0249b1b1ff") -- Depot 2022381
+setManifestid(2022381, "7347162637368908019", 227560335)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
