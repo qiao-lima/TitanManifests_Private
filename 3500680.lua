38b447b4d6e5fdfce05131fa878c3112423c344c
qiao-lima <qiao-lima@github.com>
2026-04-29 11:58:05 +0800

    Manual Update 3500680

---
+++ 3500680.lua
@@ -0,0 +1,20 @@
+-- 3500680's Lua and Manifest Created by Hubcap Manifest
+-- Cropdeck
+-- Created: April 28, 2026 at 14:08:37 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3500680) -- Cropdeck
+-- MAIN APP DEPOTS
+addappid(3500681, 1, "23f6633875ca138f232d470ec79587fd49a336449734b36b99773549c627795c") -- Depot 3500681
+setManifestid(3500681, "5485146597353449114", 586285010)
+addappid(3500682, 1, "2ecd8beb78264b7ac3060a6f02e5addec8ef3ceba9c68922c89216f77e765049") -- Depot 3500682
+setManifestid(3500682, "165348386766803293", 745465545)
+addappid(3500683, 1, "3f7944cc4ce2fa02eec930c7cf401022cf81fd22cd4c241a573cd3164af125cb") -- Depot 3500683
+setManifestid(3500683, "8549516198387609410", 607786636)
+-- SHARED DEPOTS (from other apps)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
