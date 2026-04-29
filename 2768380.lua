ff7f084eec131cc0f6bf7bd17ef1bddf2962e70d
qiao-lima <qiao-lima@github.com>
2026-04-29 11:56:48 +0800

    Manual Update 2768380

---
+++ 2768380.lua
@@ -0,0 +1,14 @@
+-- 2768380's Lua and Manifest Created by Hubcap Manifest
+-- Repterra
+-- Created: April 28, 2026 at 14:08:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2768380) -- Repterra
+-- MAIN APP DEPOTS
+addappid(2768381, 1, "a11ef14574c7c7c231f5cd190cef4fa1bb611eef644ae385265748b5a3fab152") -- Depot 2768381
+setManifestid(2768381, "4278023401719031514", 1081564069)
+addappid(2768382, 1, "3992c01f129d44dd4757bddd165e08503f2d13c6d6e54c40a0521bf592615473") -- Depot 2768382
+setManifestid(2768382, "6993988765378752880", 1115284877)
