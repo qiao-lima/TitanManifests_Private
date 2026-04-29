1d5366054727354598ba29066aa9e860469780fa
qiao-lima <qiao-lima@github.com>
2026-04-29 11:56:49 +0800

    Manual Update 3256150

---
+++ 3256150.lua
@@ -0,0 +1,38 @@
+-- 3256150's Lua and Manifest Created by Hubcap Manifest
+-- Moomintroll: Winter's Warmth
+-- Created: April 28, 2026 at 09:38:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 8
+-- Total DLCs: 5
+
+-- MAIN APPLICATION
+addappid(3256150) -- Moomintroll: Winter's Warmth
+-- MAIN APP DEPOTS
+addappid(3256151, 1, "2e154b1c0d5b3c72ab4d2610e491a5f262967fc96cb148666c2cf2f2b2d05c0b") -- Depot 3256151
+setManifestid(3256151, "8490949412962437988", 3646504362)
+addappid(3256154, 1, "b6d0822618b4f941bf273d57d1a07e3eac89781704132297db7ba6eef85909bb") -- Depot 3256154
+setManifestid(3256154, "7461702279879374650", 3645127207)
+addappid(3256153, 1, "c6eb1699098152f6a41c6b7307e4fb245d68bff238b14bcca7ba11df3d6e24fc") -- Depot 3256153
+setManifestid(3256153, "1282413307145046996", 1578027809)
+-- DLCS WITH DEDICATED DEPOTS
+-- Moomintroll Winters Warmth - Backgroundwallpapers for PC and mobile (AppID: 4331970)
+addappid(4331970)
+addappid(4331970, 1, "881f17b8528d375433958f5b0ef835fd8c531b95a7902c95b74401a6d042fb08") -- Moomintroll Winters Warmth - Backgroundwallpapers for PC and mobile - Depot 4331970
+setManifestid(4331970, "5090349023517406104", 521987608)
+-- Moomintroll Winters Warmth - Digital Artbook (AppID: 4331980)
+addappid(4331980)
+addappid(4331980, 1, "0ca49997c33c1f9bb18e3391624bef275731356fc5ff5bd823844e7b6f597989") -- Moomintroll Winters Warmth - Digital Artbook - Depot 4331980
+setManifestid(4331980, "6249520768726745988", 79852362)
+-- Moomintroll Winters Warmth - Scarf  Mittens Cosmetic Pack (AppID: 4331990)
+addappid(4331990)
+addappid(4331990, 1, "03bfdf8cdb77bffd6ca72e97bc51ab66b3c430cb1658e670e2e577225f959a3f") -- Moomintroll Winters Warmth - Scarf  Mittens Cosmetic Pack - Depot 4331990
+setManifestid(4331990, "5112103889037684524", 335)
+-- Moomintroll Winters Warmth - Screensaver for PC  Mac (AppID: 4384190)
+addappid(4384190)
+addappid(4384190, 1, "1701f708ecf1149e2fb438c140753887318c3caab5bf84762fb8436eea53776f") -- Moomintroll Winters Warmth - Screensaver for PC  Mac - Depot 4384190
+setManifestid(4384190, "3919185941925370294", 421600670)
+-- addappid(4384191, 1, "MISSING_KEY") -- Moomintroll Winters Warmth - Screensaver for PC  Mac - Depot 4384191 (no key available)
+-- Moomintroll Winters Warmth - Digital coloring book (AppID: 4384200)
+addappid(4384200)
+addappid(4384200, 1, "65b3e88c3d45f5fa51dc974212381e5a8ff7b619ef377314eb67aa1fb665724c") -- Moomintroll Winters Warmth - Digital coloring book - Depot 4384200
+setManifestid(4384200, "8863653252650919736", 51078134)
