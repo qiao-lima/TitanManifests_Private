89c9e2c5a44e58a808f4e9645b3865e74ad6e22b
qiao-lima <qiao-lima@github.com>
2026-04-29 11:56:45 +0800

    Manual Update 1381750

---
+++ 1381750.lua
@@ -0,0 +1,13 @@
+-- 1381750's Lua and Manifest Created by Hubcap Manifest
+-- Virtual Girl:Classroom
+-- Created: April 27, 2026 at 14:02:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1381750) -- Virtual Girl:Classroom
+addtoken(1381750, "67778886219879045")
+-- MAIN APP DEPOTS
+addappid(1381751, 1, "845a229697a0fd5a0fcb32e1df982aa73943ce129971b13ab09434a3a3af85b5") -- Depot 1381751
+setManifestid(1381751, "4726932022810355734", 13802190452)
