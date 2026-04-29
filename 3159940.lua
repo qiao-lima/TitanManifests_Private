6264d6623d1b4623b8843a7e75bb06a3775ed560
qiao-lima <qiao-lima@github.com>
2026-04-29 11:56:49 +0800

    Manual Update 3159940

---
+++ 3159940.lua
@@ -0,0 +1,14 @@
+-- 3159940's Lua and Manifest Created by Hubcap Manifest
+-- ケモノの従者と王子の花嫁
+-- Created: April 28, 2026 at 09:37:57 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3159940) -- ケモノの従者と王子の花嫁
+-- MAIN APP DEPOTS
+addappid(3159941, 1, "0431adc0390c6dca82cd06eab6abac7aac548efd4f9a24b72fd41f8bf7e56934") -- Depot 3159941
+setManifestid(3159941, "1757200643624277916", 1394229365)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3159942) -- Depot 3159942 (empty depot)
