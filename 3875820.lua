93a99db15b00613b5c776d02a26ca19afc36da0e
qiao-lima <qiao-lima@github.com>
2026-04-29 11:58:06 +0800

    Manual Update 3875820

---
+++ 3875820.lua
@@ -0,0 +1,12 @@
+-- 3875820's Lua and Manifest Created by Hubcap Manifest
+-- Where is Mother
+-- Created: April 27, 2026 at 14:01:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3875820) -- Where is Mother
+-- MAIN APP DEPOTS
+addappid(3875821, 1, "b9091901b1cfda066859b5ecd9699f5b8e79ce56f30fe12ef6ade30f36c9f858") -- Depot 3875821
+setManifestid(3875821, "1651970769806115951", 6953984998)
