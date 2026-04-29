5201690562e065819e11ea901f2d9082b540e047
qiao-lima <qiao-lima@github.com>
2026-04-29 11:58:06 +0800

    Manual Update 3795000

---
+++ 3795000.lua
@@ -0,0 +1,18 @@
+-- 3795000's Lua and Manifest Created by Hubcap Manifest
+-- Open Robot
+-- Created: April 28, 2026 at 09:37:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3795000) -- Open Robot
+-- MAIN APP DEPOTS
+addappid(3795001, 1, "24c68a233fc6414f9b6f21d572996845e9ca75f1ff672a39707b1883e40f7d6a") -- Depot 3795001
+setManifestid(3795001, "7703069003762350042", 2080230586)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
