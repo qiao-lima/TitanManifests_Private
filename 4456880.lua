c5d3c1e3809a5ae05859fbb24fdc7844c110274f
qiao-lima <qiao-lima@github.com>
2026-04-29 11:58:09 +0800

    Manual Update 4456880

---
+++ 4456880.lua
@@ -0,0 +1,14 @@
+-- 4456880's Lua and Manifest Created by Hubcap Manifest
+-- Ghostify
+-- Created: April 27, 2026 at 06:49:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4456880) -- Ghostify
+-- MAIN APP DEPOTS
+addappid(4456881, 1, "086dcf86e02f240f3f4794cc0306ead5ff3746fd2afe837d1e9f39817ac21e3f") -- Depot 4456881
+setManifestid(4456881, "6222449510094936543", 386382486)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4628130) -- Ghostify - Supporter Pack
