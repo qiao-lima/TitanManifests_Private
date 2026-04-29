38d1fc04d3b95d9c2e5f4662440aa4d3df2caf5b
qiao-lima <qiao-lima@github.com>
2026-04-29 11:56:46 +0800

    Manual Update 1960880

---
+++ 1960880.lua
@@ -0,0 +1,14 @@
+-- 1960880's Lua and Manifest Created by Hubcap Manifest
+-- 无名者
+-- Created: April 28, 2026 at 08:55:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1960880) -- 无名者
+-- MAIN APP DEPOTS
+addappid(1960881, 1, "36566b6e73d1d9b5d4457d42729de617371de15e7968e1bf6525561c1eb0363f") -- Depot 1960881
+setManifestid(1960881, "8306513852713748501", 2826199467)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4168710) -- Depot 4168710 (empty depot)
