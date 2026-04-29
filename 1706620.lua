370bb9a0f51273780dff91be36a043c903d4bbda
qiao-lima <qiao-lima@github.com>
2026-04-29 11:56:46 +0800

    Manual Update 1706620

---
+++ 1706620.lua
@@ -0,0 +1,13 @@
+-- 1706620's Lua and Manifest Created by Hubcap Manifest
+-- 与姐姐同居别墅的日子
+-- Created: April 27, 2026 at 14:04:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1706620) -- 与姐姐同居别墅的日子
+addtoken(1706620, "294230786621515954")
+-- MAIN APP DEPOTS
+addappid(1706621, 1, "17ad9524f2ef5c1fd323a715a050d8af1e1f309295fee8213b53287e12e28a9f") -- Depot 1706621
+setManifestid(1706621, "7760224072228357555", 3451107889)
