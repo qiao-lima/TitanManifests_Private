b895419e6f5196ea8d0a220b7fd65808c4e75715
qiao-lima <qiao-lima@github.com>
2026-04-29 19:19:45 +0800

    Manual Update 599140

---
+++ 599140.lua
@@ -0,0 +1,48 @@
+-- 599140's Lua and Manifest Created by Hubcap Manifest
+-- Graveyard Keeper
+-- Created: April 09, 2026 at 12:11:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 13
+-- Total DLCs: 4
+
+-- MAIN APPLICATION
+addappid(599140, 1, "877beb842b05f75b5828d0f1fd4e702c933cabecb879e2c65f20dfd97c478d67") -- Graveyard Keeper
+-- MAIN APP DEPOTS
+addappid(599141, 1, "68b3f80530eaafde932cbddddc3b1246f8683fc9afef320669c7a75ad54346ac") -- Graveyard Keeper Content
+setManifestid(599141, "1026336457626388576", 1766921952)
+addappid(599142, 1, "7cb7f8533bf578c1fd50fe061de8d7e6e45a6286de3598bf851c96ce1b3033c9") -- Graveyard Keeper Content - OSX
+setManifestid(599142, "9019423966611775112", 1761747487)
+addappid(599143, 1, "5627fae0ee797b854f790aa49d2adfcdc10d7baf78dc8daa88e0df381a359ede") -- Graveyard Keeper Content - Linux
+setManifestid(599143, "8794685739656643050", 1807149676)
+-- DLCS WITH DEDICATED DEPOTS
+-- Graveyard Keeper Artbook (AppID: 919051)
+addappid(919051)
+addappid(919051, 1, "0996ef7d3f26b5f3026d53c97fc3cfc33ede2ad50a470e3bfcd8e2af65b6d01a") -- Graveyard Keeper Artbook - Хранилище Graveyard Keeper Artbook (919051)
+setManifestid(919051, "4155613206917584963", 86283687)
+-- Graveyard Keeper - Stranger Sins (AppID: 1163770)
+addappid(1163770)
+addappid(1163771, 1, "c7d4c97a14fa0f9634524973a0051d60142ed16fe6086072a3945c9d9bc44840") -- Graveyard Keeper - Stranger Sins - Stranger Sins - Win
+setManifestid(1163771, "2231067810691836648", 128)
+addappid(1163772, 1, "6ed89ac24cd4b2a37b59a2190f9146a1c3643874b20098b16858be6eda10141a") -- Graveyard Keeper - Stranger Sins - Stranger Sins - MacOS
+setManifestid(1163772, "2014532622632730985", 128)
+addappid(1163773, 1, "05d13d3cf6d780173cbedf5719d68a53fcd2b42f7dde4b721940b4ee25effac6") -- Graveyard Keeper - Stranger Sins - Stranger Sins - Linux
+setManifestid(1163773, "7802013534269438121", 128)
+-- Graveyard Keeper - Game of Crone (AppID: 1430990)
+addappid(1430990)
+addtoken(1430990, "6002393830453812636")
+addappid(1430991, 1, "6dd1ee83133b9341b25f39357f3d1d2cfed3b8bfb308bc8c14e2d13c0fdc89e7") -- Graveyard Keeper - Game of Crone - Game of Crone - Win
+setManifestid(1430991, "4645023560976072695", 512)
+addappid(1430992, 1, "14228abd8ed31925b4b315b5ec93a5a298cef2c300c0fb2bd74a0d95368c0b54") -- Graveyard Keeper - Game of Crone - Game of Crone - MacOS
+setManifestid(1430992, "7334053426650407908", 512)
+addappid(1430993, 1, "fa811b193d4dd8cb2323f3c9485920ddc01867a22063c6d5f5afcacf8bb222ee") -- Graveyard Keeper - Game of Crone - Game of Crone - Linux
+setManifestid(1430993, "744873112858588034", 512)
+-- Graveyard Keeper - Better Save Soul (AppID: 1788370)
+addappid(1788370)
+addappid(1788371, 1, "8be97e74b10855b0401aab75a3d857eea24c22872d5eb5337ea455d8d1ee838d") -- Graveyard Keeper - Better Save Soul - Better Save Soul - Win
+setManifestid(1788371, "1819763601295886461", 512)
+addappid(1788372, 1, "84fc9f59b2feabcf9cae522e724da951a23a51396656446126ee6bfce0ee312b") -- Graveyard Keeper - Better Save Soul - Better Save Soul - MacOS
+setManifestid(1788372, "8992971804176766029", 512)
+addappid(1788373, 1, "a97291c760fc6dfc94b1e2129359ce4795fe26c6be755a3dd1bf23c3380edfb6") -- Graveyard Keeper - Better Save Soul - Better Save Soul - Linux
+setManifestid(1788373, "3576409815092577850", 512)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(1163770) -- Depot 1163770 (empty depot)
