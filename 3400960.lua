4748694b37c9b2cf7a168e02942d181d5b1e5162
qiao-lima <qiao-lima@github.com>
2026-04-29 11:56:50 +0800

    Manual Update 3400960

---
+++ 3400960.lua
@@ -0,0 +1,14 @@
+-- 3400960's Lua and Manifest Created by Hubcap Manifest
+-- Oaken Tower
+-- Created: April 28, 2026 at 19:20:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3400960) -- Oaken Tower
+-- MAIN APP DEPOTS
+addappid(3400961, 1, "9975b9c771b76745869399d9f3b7be6e3ceb769e83eb21ce6cb682dc4f614cf6") -- Depot 3400961
+setManifestid(3400961, "6763380220793116776", 434212988)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4596710) -- Oaken Tower Supporter Pack
