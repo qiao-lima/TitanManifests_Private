ee9f11d90b2220cd4108ac62219ca6f11b4997d5
qiao-lima <qiao-lima@github.com>
2026-04-29 11:58:08 +0800

    Manual Update 4187000

---
+++ 4187000.lua
@@ -0,0 +1,14 @@
+-- 4187000's Lua and Manifest Created by Hubcap Manifest
+-- Watt's the Limit?
+-- Created: April 28, 2026 at 19:00:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4187000) -- Watt's the Limit?
+-- MAIN APP DEPOTS
+addappid(4187001, 1, "67de325c194193450c6ea05f3ec6e63e4ffa26e87817f959f2ce8413a450071c") -- Depot 4187001
+setManifestid(4187001, "7501191341674452155", 614125734)
+addappid(4187002, 1, "b87b6be10f10ac82faba786f436bf08a3ff76de16720ed9797e23ee883c13454") -- Depot 4187002
+setManifestid(4187002, "6546144610509116714", 631210080)
