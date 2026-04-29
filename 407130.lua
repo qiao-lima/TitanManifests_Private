14d657fe161d95cff89185d7a30dab782932fe24
qiao-lima <qiao-lima@github.com>
2026-04-29 19:13:54 +0800

    Manual Update 407130

---
+++ 407130.lua
@@ -0,0 +1,35 @@
+-- 407130's Lua and Manifest Created by Hubcap Manifest
+-- Tank Mechanic Simulator
+-- Created: April 10, 2026 at 13:56:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 8
+-- Total DLCs: 3
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(407130, 1, "0335e7605ee0d542e3f7ed2be3585fe90e15a523a18f64a01136aba6ba3bf080") -- Tank Mechanic Simulator
+-- MAIN APP DEPOTS
+addappid(407131, 1, "989efb78a9368874e2d18ac087df37d06459b0dc786fa054628e00bd27a9867c") -- Tank Mechanic Simulator Content
+setManifestid(407131, "5014349148459496033", 14035459251)
+addappid(407132, 1, "0bd11b4aebe64e0fd0d926b63f8fe3f9eb86ce78b954d2a208ebbde79eb0ab09") -- Tank Mechanic Simulator - MacOs Depot
+setManifestid(407132, "4270416266453424738", 14068595690)
+-- SHARED DEPOTS (from other apps)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") -- VC 2017 Redist (Shared from App 228980)
+setManifestid(228987, "4302102680580581867", 29664201)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Tank Mechanic Simulator - Shermans DLC (AppID: 1065400)
+addappid(1065400)
+addappid(1065403, 1, "ddd06be3ac29a3ced9b5bcda682295cd0d7a64b075eebd0ec406c31a20b9c653") -- Tank Mechanic Simulator - Shermans DLC - Depot 1065403
+setManifestid(1065403, "9087659577122393754", 1104491273)
+-- Tank Mechanic Simulator - First Supply DLC (AppID: 1485420)
+addappid(1485420)
+addappid(1485420, 1, "eeed40b3a7fc89c6cc44631639551f329dd573372eb17a2d9155b8545c7643de") -- Tank Mechanic Simulator - First Supply DLC - Tank Mechanic Simulator - DLC #1 (1485420) - magazyn zawartości
+setManifestid(1485420, "4073122479434345244", 992282680)
+addappid(1485421, 1, "41c1b9f4d7fd37022c0e843fed4271e4bbe0cc0c4f562e480f9323b269777a8b") -- Tank Mechanic Simulator - First Supply DLC - Depot 1485421
+setManifestid(1485421, "2925331946354259482", 990067777)
+-- Tank Mechanic Simulator - Cromwell Mk.IV DLC (AppID: 4185020)
+addappid(4185020)
+addappid(4185021, 1, "e766d8c81fa4dec49cf3e787991b6e620ac54c285c54a81ef1b031847ce9d69b") -- Tank Mechanic Simulator - Cromwell Mk.IV DLC - Depot 4185021
+setManifestid(4185021, "8948034188208490587", 476889170)
