4fa00979731fd37c20ffab42525382f260a9b152
qiao-lima <qiao-lima@github.com>
2026-04-29 11:58:07 +0800

    Manual Update 3930190

---
+++ 3930190.lua
@@ -0,0 +1,12 @@
+-- 3930190's Lua and Manifest Created by Hubcap Manifest
+-- Wavekin
+-- Created: April 28, 2026 at 14:42:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3930190) -- Wavekin
+-- MAIN APP DEPOTS
+addappid(3930191, 1, "987e785bd8e7744bde03c6fd1f6e4ec6e0bc21865064d5026363be797b0b1835") -- Depot 3930191
+setManifestid(3930191, "4749717642702370077", 1711094710)
