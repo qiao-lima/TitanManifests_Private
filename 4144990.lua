409dd5610f61aa78e44a5f8672b98e30a48ea88b
qiao-lima <qiao-lima@github.com>
2026-04-29 11:58:08 +0800

    Manual Update 4144990

---
+++ 4144990.lua
@@ -0,0 +1,12 @@
+-- 4144990's Lua and Manifest Created by Hubcap Manifest
+-- Criticality
+-- Created: April 28, 2026 at 21:57:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4144990, 1, "3dcfccfa04726dd16581b13db23f4cac9cd8c1f2fdb35325174b6a257ca52c39") -- Criticality
+-- MAIN APP DEPOTS
+addappid(4144991, 1, "eba317d1f901baf614abf06d42a4279c56fffb865aa172f4ecba083f1bbeaf26") -- Depot 4144991
+setManifestid(4144991, "5358182440967056242", 280830678)
