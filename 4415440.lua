095ba30bb98bac2b6f344c3f39ef9f7d1cbe36d6
qiao-lima <qiao-lima@github.com>
2026-04-29 11:58:08 +0800

    Manual Update 4415440

---
+++ 4415440.lua
@@ -0,0 +1,12 @@
+-- 4415440's Lua and Manifest Created by Hubcap Manifest
+-- MAIDENS OF ETERNAL NIGHT
+-- Created: April 28, 2026 at 14:35:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4415440) -- MAIDENS OF ETERNAL NIGHT
+-- MAIN APP DEPOTS
+addappid(4415441, 1, "01f0d09f3f804b606003b8b7562987ed795eb3149b3437fd80eb400e75f01a2f") -- Depot 4415441
+setManifestid(4415441, "6268426171889158514", 775102192)
