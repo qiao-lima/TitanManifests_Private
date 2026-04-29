1a93437fb204f26396e4c9e6efced5c6678bfbcd
qiao-lima <qiao-lima@github.com>
2026-04-29 11:58:05 +0800

    Manual Update 3690030

---
+++ 3690030.lua
@@ -0,0 +1,14 @@
+-- 3690030's Lua and Manifest Created by Hubcap Manifest
+-- The Last Gas Station
+-- Created: April 28, 2026 at 16:45:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3690030) -- The Last Gas Station
+-- MAIN APP DEPOTS
+addappid(3690031, 1, "6e046d7bf13d154a199fdcd2f9a1bedd5de66dc45496e4db97af27a0f31fc5d9") -- Depot 3690031
+setManifestid(3690031, "3877915842506357979", 760108810)
+addappid(3690032, 1, "41aaa4bb7e847316b65b3df578c82cae1a6789c35a9c906b776aa156d1802a55") -- Depot 3690032
+setManifestid(3690032, "3261260310367948217", 1293300011)
