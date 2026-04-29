a274bb8f0aecdb6ef107b59f486ae4d594ce74aa
qiao-lima <qiao-lima@github.com>
2026-04-29 11:56:45 +0800

    Manual Update 1399470

---
+++ 1399470.lua
@@ -0,0 +1,13 @@
+-- 1399470's Lua and Manifest Created by Hubcap Manifest
+-- ANTI 世界岛·原爆点
+-- Created: April 28, 2026 at 09:41:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1399470, 1, "df5697dc2d59c171a5f766088e66524076c269c24007995b89411910a377b10a") -- ANTI 世界岛·原爆点
+addtoken(1399470, "1503824317241732788")
+-- MAIN APP DEPOTS
+addappid(1399471, 1, "4adaaa049a51fe1bd10bc4543e9857999b53c81d04acb5b210ce5231699221f4") -- Depot 1399471
+setManifestid(1399471, "7406447461022119457", 13775442236)
