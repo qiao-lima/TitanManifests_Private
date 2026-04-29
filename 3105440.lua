af4c91663036a82f88ab6aa2e0606238183da7eb
qiao-lima <qiao-lima@github.com>
2026-04-29 11:56:48 +0800

    Manual Update 3105440

---
+++ 3105440.lua
@@ -0,0 +1,13 @@
+-- 3105440's Lua and Manifest Created by Hubcap Manifest
+-- Heroes of Might and Magic: Olden Era
+-- Created: April 28, 2026 at 14:08:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3105440) -- Heroes of Might and Magic: Olden Era
+addtoken(3105440, "822347138807159306")
+-- MAIN APP DEPOTS
+addappid(3105441, 1, "ed44e10054a9907358f5bd7827b31171c8ba2e2a11b5b8a738ecd73dd62b0424") -- Depot 3105441
+setManifestid(3105441, "4631098798098189150", 0)
