84c39b8647b762a3ebce8b3a30a28d008e0471ab
qiao-lima <qiao-lima@github.com>
2026-04-29 11:58:05 +0800

    Manual Update 3546480

---
+++ 3546480.lua
@@ -0,0 +1,12 @@
+-- 3546480's Lua and Manifest Created by Hubcap Manifest
+-- Midnight Billiards
+-- Created: April 28, 2026 at 10:53:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3546480) -- Midnight Billiards
+-- MAIN APP DEPOTS
+addappid(3546481, 1, "898d53fa64345851df6c8be59a44213a27776b7f4e1c602177c40abee3a837e4") -- Depot 3546481
+setManifestid(3546481, "1865141963204807268", 358002329)
