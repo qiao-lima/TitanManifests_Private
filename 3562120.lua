0bba087c0a70a248b3e2f85f95f34a4af04d8fe9
qiao-lima <qiao-lima@github.com>
2026-04-29 11:58:05 +0800

    Manual Update 3562120

---
+++ 3562120.lua
@@ -0,0 +1,12 @@
+-- 3562120's Lua and Manifest Created by Hubcap Manifest
+-- Magical Princess
+-- Created: April 28, 2026 at 03:47:13 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3562120) -- Magical Princess
+-- MAIN APP DEPOTS
+addappid(3562121, 1, "ee547d3b84ed23ce194cad216a55f4fddebd7d676bb84f7a3bee62cdd0c8907c") -- Depot 3562121
+setManifestid(3562121, "9209057447910507206", 7281351301)
