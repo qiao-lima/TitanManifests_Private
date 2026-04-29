1e021f1f98d228256b6292ecc835187c041cf1f2
qiao-lima <qiao-lima@github.com>
2026-04-29 11:58:23 +0800

    Manual Update 4603270

---
+++ 4603270.lua
@@ -0,0 +1,12 @@
+-- 4603270's Lua and Manifest Created by Hubcap Manifest
+-- Island God Simulator
+-- Created: April 28, 2026 at 09:40:29 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4603270) -- Island God Simulator
+-- MAIN APP DEPOTS
+addappid(4603271, 1, "df02d3e9d8ea0a83138f24b444cc88b0c061ab594c82650b0845eb1a378f6bd6") -- Depot 4603271
+setManifestid(4603271, "3934429138535980055", 332488446)
