bf7d9ecb63d71fdc08592ff3eab1c3440dfa68cb
qiao-lima <qiao-lima@github.com>
2026-04-29 19:19:44 +0800

    Manual Update 1384630

---
+++ 1384630.lua
@@ -0,0 +1,16 @@
+-- 1384630's Lua and Manifest Created by Hubcap Manifest
+-- Sword x Hime
+-- Created: February 01, 2026 at 11:58:23 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1384630, 1, "24c807ff350f7e8bf4433862ae5963a9301e1f438a749ed430d13be7177e3043") -- Sword x Hime
+-- MAIN APP DEPOTS
+addappid(1384631, 1, "ce72ef8ddbf891aadec67833dcd27f6cccda1d2dcc919ab33e2178830533259a") -- Sword x Hime Content
+setManifestid(1384631, "8776754837525575168", 14029173948)
+addappid(1384632, 1, "33c44122fb63024323d6f5f00ee459b9a6e95b41c764f0d21422eab418b38767") -- Depot 1384632
+setManifestid(1384632, "3421227697176537434", 14323020070)
+addappid(1384633, 1, "c9d672e66e927a842b56c3c8d50978f3252a2483e50cc2de24ab5df6664158fb") -- Depot 1384633
+setManifestid(1384633, "4445328619999239663", 14528077588)
