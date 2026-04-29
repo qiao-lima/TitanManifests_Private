cb7167ea6bbb7185d9370de3bb918382de8ccc50
qiao-lima <qiao-lima@github.com>
2026-04-29 11:58:09 +0800

    Manual Update 4463020

---
+++ 4463020.lua
@@ -0,0 +1,12 @@
+-- 4463020's Lua and Manifest Created by Hubcap Manifest
+-- Treasure's Mine!
+-- Created: April 28, 2026 at 09:37:53 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4463020) -- Treasure's Mine!
+-- MAIN APP DEPOTS
+addappid(4463021, 1, "14cbd1ed0203d84099b9c8457d7551b78647a52f1e59c7d9597d09e0f8d3e3ed") -- Depot 4463021
+setManifestid(4463021, "4831840368282643987", 1009528279)
