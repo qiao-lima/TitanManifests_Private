aaf7c15a88a708691e722b4a9d84fa21eb3dabb6
qiao-lima <qiao-lima@github.com>
2026-04-29 11:58:07 +0800

    Manual Update 4104120

---
+++ 4104120.lua
@@ -0,0 +1,21 @@
+-- 4104120's Lua and Manifest Created by Hubcap Manifest
+-- 对不起，我是警察
+-- Created: April 28, 2026 at 06:47:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 1
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(4104120) -- 对不起，我是警察
+-- MAIN APP DEPOTS
+addappid(4104121, 1, "d14a0a51cf4bd2e31531453608bddc58317a016a3251ac51e5f8c8c4fd303a28") -- Depot 4104121
+setManifestid(4104121, "3561202099983631040", 26129872621)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITH DEDICATED DEPOTS
+--  - 4K (AppID: 4378880)
+addappid(4378880)
+addappid(4378881, 1, "f748fe44132344a73daebb474c8eb017654211d3e72a73b502f7604a97b10637") --  - 4K - Depot 4378881
+setManifestid(4378881, "8996416830032616791", 102863876400)
