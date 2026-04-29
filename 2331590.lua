6ed2e2aa5492a2299cca2c203b6b66a02b3b6898
qiao-lima <qiao-lima@github.com>
2026-04-29 11:56:47 +0800

    Manual Update 2331590

---
+++ 2331590.lua
@@ -0,0 +1,13 @@
+-- 2331590's Lua and Manifest Created by Hubcap Manifest
+-- 世界岛·编辑器
+-- Created: April 28, 2026 at 09:40:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2331590) -- 世界岛·编辑器
+addtoken(2331590, "960453436494717335")
+-- MAIN APP DEPOTS
+addappid(2331591, 1, "3549ab7afb8717beb5b66a7ba5cf48db7d9aded7b32c7e6d675139c9cfe0cbc7") -- Depot 2331591
+setManifestid(2331591, "6211054186586576370", 1488091688)
