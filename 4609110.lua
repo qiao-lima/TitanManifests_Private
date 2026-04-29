c6a4df6977e7f1da380284c4c215a64135c2a80f
qiao-lima <qiao-lima@github.com>
2026-04-29 11:58:23 +0800

    Manual Update 4609110

---
+++ 4609110.lua
@@ -0,0 +1,12 @@
+-- 4609110's Lua and Manifest Created by Hubcap Manifest
+-- BULLET GIRL：ZOMBIE DEFENSE
+-- Created: April 28, 2026 at 14:35:18 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4609110) -- BULLET GIRL：ZOMBIE DEFENSE
+-- MAIN APP DEPOTS
+addappid(4609112, 1, "2fb3862597de9fcf756bdbafa9826139afd08e6af61979bf5e38d97301a72294") -- Depot 4609112
+setManifestid(4609112, "4523599504849933691", 218627484)
