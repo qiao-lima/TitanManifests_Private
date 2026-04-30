75c011b34b6fa9e8d0eaad6e0690dfe4d14c5b5d
qiao-lima <qiao-lima@github.com>
2026-05-01 00:37:27 +0800

    Manual Update 4293360

---
+++ 4293360.lua
@@ -0,0 +1,14 @@
+-- 4293360's Lua and Manifest Created by Hubcap Manifest
+-- Milf Solitaire: Lust Collector
+-- Created: April 30, 2026 at 08:02:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4293360) -- Milf Solitaire: Lust Collector
+-- MAIN APP DEPOTS
+addappid(4293361, 1, "40c2fcf192992b6180732f6c45c411ff7e86fe08b4fb9fdc945399f896037aca") -- Depot 4293361
+setManifestid(4293361, "2936133952922378745", 411223461)
+addappid(4293362, 1, "b197c20307fd3602058e158974ce6bd4fa8d2130fdd7f69f44e50c03a330a624") -- Depot 4293362
+setManifestid(4293362, "1934448438904624111", 677719033)
