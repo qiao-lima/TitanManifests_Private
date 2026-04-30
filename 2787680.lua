7a85ee0168cae2c848f4deb98e8bdaaf2a70a08b
qiao-lima <qiao-lima@github.com>
2026-05-01 00:31:38 +0800

    Manual Update 2787680

---
+++ 2787680.lua
@@ -0,0 +1,12 @@
+-- 2787680's Lua and Manifest Created by Hubcap Manifest
+-- Candy Rangers
+-- Created: April 29, 2026 at 14:23:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2787680) -- Candy Rangers
+-- MAIN APP DEPOTS
+addappid(2787681, 1, "7dedda6fffc70b75add9a0c6523098ee5b7e81250663c70861d375f14f6cefe9") -- Depot 2787681
+setManifestid(2787681, "29968666443587863", 463715005)
