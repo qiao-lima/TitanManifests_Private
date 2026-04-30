50428401867e7c0288b661e0f46ca74ccdd4024a
qiao-lima <qiao-lima@github.com>
2026-05-01 01:20:19 +0800

    Manual Update 1260320

---
+++ 1260320.lua
@@ -0,0 +1,20 @@
+-- 1260320's Lua and Manifest Created by Hubcap Manifest
+-- Party Animals
+-- Created: April 23, 2026 at 09:37:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 2
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1260320, 1, "bc1f6dc33cc56a820c67ef2a4e6a6a601077f693cc39530bbed01b96a2acdfcb") -- Party Animals
+addtoken(1260320, "7641392735724118585")
+-- MAIN APP DEPOTS
+addappid(1260321, 1, "9623dafeee383701a837274d2547c7a5cbae7334393d64e82b4d2cdc01ef679e") -- Depot 1260321
+setManifestid(1260321, "1780873685801747038", 11400713442)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2342040) -- Party Animals - Deluxe Pack
+addappid(2545150) -- Party Animals - Preorder Pack
