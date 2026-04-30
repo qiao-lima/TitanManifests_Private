26d4ea078fd857077a5c0f4919ec4304472a7d4f
qiao-lima <qiao-lima@github.com>
2026-05-01 00:31:40 +0800

    Manual Update 3804290

---
+++ 3804290.lua
@@ -0,0 +1,12 @@
+-- 3804290's Lua and Manifest Created by Hubcap Manifest
+-- R-TYPE DX: Music Encore
+-- Created: April 30, 2026 at 06:52:03 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3804290) -- R-TYPE DX: Music Encore
+-- MAIN APP DEPOTS
+addappid(3804291, 1, "93592928037f2bda069dbb48407c14c2c06a38e3b6ca69dafa47e08f61f4947c") -- Depot 3804291
+setManifestid(3804291, "4639839383650207029", 415957269)
