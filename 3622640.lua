26e7cc11c3be158bced8842ee7e77b35e6400ea4
qiao-lima <qiao-lima@github.com>
2026-05-01 02:05:17 +0800

    Manual Update 3622640

---
+++ 3622640.lua
@@ -0,0 +1,18 @@
+-- 3622640's Lua and Manifest Created by Hubcap Manifest
+-- Vending Machine Co.
+-- Created: April 28, 2026 at 14:01:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3622640, 1, "53e048d9a15a05812e5499078e1604e96583c35351d1edb814085377a987137f") -- Vending Machine Co.
+-- MAIN APP DEPOTS
+addappid(3622641, 1, "beec5643a86ba10a04c36be36408f578c8e8301c3124f89d23ebda5560435f82") -- Depot 3622641
+setManifestid(3622641, "443446759556109050", 5029612785)
+addappid(3622642, 1, "2ae50ee58e5bd34c28da3decb3eeffe67a47f15779949c47a3971bf72602bfd4") -- Depot 3622642
+setManifestid(3622642, "7844414936224629305", 5046120928)
+addappid(3622643, 1, "d9364417551ab197e230f2ee66596010e6a29472b04ac80c8bc1baf3fedc6f51") -- Depot 3622643
+setManifestid(3622643, "8812174099389541291", 5025861775)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4585810) -- Vending Machine Co. - Supporter Pack
