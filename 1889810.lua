a629f9119c084a105a27ca94aa83ab1755766800
qiao-lima <qiao-lima@github.com>
2026-05-01 00:31:37 +0800

    Manual Update 1889810

---
+++ 1889810.lua
@@ -0,0 +1,12 @@
+-- 1889810's Lua and Manifest Created by Hubcap Manifest
+-- Bobo Bay
+-- Created: April 29, 2026 at 21:02:57 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1889810) -- Bobo Bay
+-- MAIN APP DEPOTS
+addappid(1889812, 1, "c60721f1a8c15e6cceb4ffd6a49c4f41ece3e832db3c5fb5d5ef4038f2745841") -- Depot 1889812
+setManifestid(1889812, "8793818175297032072", 1838201092)
