fa52325843a8a619bd91ebf8017fae7c9e22c809
qiao-lima <qiao-lima@github.com>
2026-05-01 00:37:28 +0800

    Manual Update 4559110

---
+++ 4559110.lua
@@ -0,0 +1,12 @@
+-- 4559110's Lua and Manifest Created by Hubcap Manifest
+-- 次元战姬ol
+-- Created: April 29, 2026 at 21:47:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4559110) -- 次元战姬ol
+-- MAIN APP DEPOTS
+addappid(4559111, 1, "53c96690c41c3ab04a94a78e6887de3e325c2ece312d12a3d00ad024e52435c5") -- Depot 4559111
+setManifestid(4559111, "808870368723408188", 11267813395)
