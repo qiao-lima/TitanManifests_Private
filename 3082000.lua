c095a30d08016305496fb43bef8bd58b66b37d77
qiao-lima <qiao-lima@github.com>
2026-05-01 00:31:39 +0800

    Manual Update 3082000

---
+++ 3082000.lua
@@ -0,0 +1,16 @@
+-- 3082000's Lua and Manifest Created by Hubcap Manifest
+-- Path of Gear: Blacksmith’s Legend
+-- Created: April 30, 2026 at 06:59:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3082000) -- Path of Gear: Blacksmith’s Legend
+-- MAIN APP DEPOTS
+addappid(3082001, 1, "ff25adea7087d14d7bc9a07924233ef6b301cd0f7339d4c480430331b5c341e1") -- Depot 3082001
+setManifestid(3082001, "7981533409930702233", 1333886556)
+-- SHARED DEPOTS (from other apps)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
