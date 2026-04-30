cba0718f6590d93898b9df62f363db94fc3283d9
qiao-lima <qiao-lima@github.com>
2026-05-01 00:31:37 +0800

    Manual Update 1304280

---
+++ 1304280.lua
@@ -0,0 +1,12 @@
+-- 1304280's Lua and Manifest Created by Hubcap Manifest
+-- Elementallis
+-- Created: April 30, 2026 at 09:19:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1304280) -- Elementallis
+-- MAIN APP DEPOTS
+addappid(1304281, 1, "bbf064e8f7dce6a390f15f34afbc6484581c2b3890e5df54967a3e9900851fa5") -- Depot 1304281
+setManifestid(1304281, "2052823799495531440", 1113377078)
