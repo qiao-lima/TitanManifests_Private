6ff34af3ece19b863f51c65b34a7a4f8ddec28e9
qiao-lima <qiao-lima@github.com>
2026-05-01 00:31:39 +0800

    Manual Update 3481020

---
+++ 3481020.lua
@@ -0,0 +1,22 @@
+-- 3481020's Lua and Manifest Created by Hubcap Manifest
+-- 恶魔牌
+-- Created: April 30, 2026 at 07:03:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(3481020) -- 恶魔牌
+-- MAIN APP DEPOTS
+addappid(3481021, 1, "9052a43814b74337717ba6ef7dd21596f047f8e9f7e6393c07005ef0ae36ebc9") -- Depot 3481021
+setManifestid(3481021, "4140768449740869849", 1719941851)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 117381405)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3481022) -- Depot 3481022 (empty depot)
