19fc5d015d2832c7b6c1b0860cc3fdec628c9558
qiao-lima <qiao-lima@github.com>
2026-05-01 02:05:18 +0800

    Manual Update 4283360

---
+++ 4283360.lua
@@ -0,0 +1,14 @@
+-- 4283360's Lua and Manifest Created by Hubcap Manifest
+-- Legionbound
+-- Created: April 30, 2026 at 13:59:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4283360) -- Legionbound
+-- MAIN APP DEPOTS
+addappid(4283361, 1, "01f200be7d1a3ee646f73e5bda964c9a0cd0649be19338738dda86338578ed52") -- Depot 4283361
+setManifestid(4283361, "5171593554476275804", 261339284)
+addappid(4283362, 1, "4ec1e6b49bdf03cb59d1f6d4d3c9f1a9f3b2540a7e17777a32fc4b150752ece0") -- Depot 4283362
+setManifestid(4283362, "8177007528052303609", 235568292)
