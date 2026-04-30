49e308d61bb48af03ca60612021ed96ed707378e
qiao-lima <qiao-lima@github.com>
2026-05-01 00:31:40 +0800

    Manual Update 3834880

---
+++ 3834880.lua
@@ -0,0 +1,12 @@
+-- 3834880's Lua and Manifest Created by Hubcap Manifest
+-- 明镜江湖行：朔风起
+-- Created: April 30, 2026 at 10:22:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3834880) -- 明镜江湖行：朔风起
+-- MAIN APP DEPOTS
+addappid(3834881, 1, "a3a3fb65346331b5eaa3bdc5a7b290f83b58b4afc12e507d4a8beb40dfcb43bf") -- Depot 3834881
+setManifestid(3834881, "7165697409718702435", 2703010469)
