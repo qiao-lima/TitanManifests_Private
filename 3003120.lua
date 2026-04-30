734c9c3f95a5a6be6e83180821a6902fabdd517d
qiao-lima <qiao-lima@github.com>
2026-05-01 00:31:39 +0800

    Manual Update 3003120

---
+++ 3003120.lua
@@ -0,0 +1,12 @@
+-- 3003120's Lua and Manifest Created by Hubcap Manifest
+-- Zombiehood
+-- Created: April 30, 2026 at 09:08:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3003120) -- Zombiehood
+-- MAIN APP DEPOTS
+addappid(3003121, 1, "b6c5373834474d548442fcd784847036428ecb16724e627d6533479c1886104a") -- Depot 3003121
+setManifestid(3003121, "7487531161432211631", 327920392)
