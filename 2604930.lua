c5e5d57f1fc3b28ffee86ae6590b10847cf06340
qiao-lima <qiao-lima@github.com>
2026-05-01 00:31:38 +0800

    Manual Update 2604930

---
+++ 2604930.lua
@@ -0,0 +1,12 @@
+-- 2604930's Lua and Manifest Created by Hubcap Manifest
+-- Parallel TCG
+-- Created: April 29, 2026 at 14:44:58 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2604930) -- Parallel TCG
+-- MAIN APP DEPOTS
+addappid(2604931, 1, "7788392f6214d6fa5021b9e09f1c67651a1e99d01af6f93388c8c81b70f51032") -- Depot 2604931
+setManifestid(2604931, "3782734083381736236", 5877291065)
