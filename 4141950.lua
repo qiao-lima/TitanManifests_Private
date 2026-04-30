0f8ff15e4e4fb1b942d673172a6f8f0c0f56768c
qiao-lima <qiao-lima@github.com>
2026-05-01 00:37:26 +0800

    Manual Update 4141950

---
+++ 4141950.lua
@@ -0,0 +1,12 @@
+-- 4141950's Lua and Manifest Created by Hubcap Manifest
+-- 久我山栞の死様手帖
+-- Created: April 29, 2026 at 17:11:57 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4141950) -- 久我山栞の死様手帖
+-- MAIN APP DEPOTS
+addappid(4141951, 1, "6178903340e72f28440744bdd3621dcb80b82d3a39f824b994fdfb32b98e6e69") -- Depot 4141951
+setManifestid(4141951, "155364835080329512", 3525655320)
