61bd49ca62255cfc1ca448c5e67ab83c4842e8d5
qiao-lima <qiao-lima@github.com>
2026-05-01 00:37:25 +0800

    Manual Update 3977220

---
+++ 3977220.lua
@@ -0,0 +1,12 @@
+-- 3977220's Lua and Manifest Created by Hubcap Manifest
+-- Tick Hop 2
+-- Created: April 30, 2026 at 12:27:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3977220) -- Tick Hop 2
+-- MAIN APP DEPOTS
+addappid(3977221, 1, "dd72fa263358b2b0955b1988dfa2a87ded6e64d77cf0bd7ffaa85bb9083d2aac") -- Depot 3977221
+setManifestid(3977221, "5046747912877295656", 1037309772)
