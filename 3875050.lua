83483721677ef5ae0e909e7c8da9cd3a3f5b4384
qiao-lima <qiao-lima@github.com>
2026-05-01 00:37:24 +0800

    Manual Update 3875050

---
+++ 3875050.lua
@@ -0,0 +1,21 @@
+-- 3875050's Lua and Manifest Created by Hubcap Manifest
+-- MotoGP™26
+-- Created: April 29, 2026 at 03:32:53 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 2
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3875050) -- MotoGP™26
+-- MAIN APP DEPOTS
+addappid(3875051, 1, "864e2cf2bdbaeaa76e9ab4aa3a41c0b43f2ecdce4494b96756871e2c90541b74") -- Depot 3875051
+setManifestid(3875051, "2241005043971470433", 30903081396)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4332650) -- MotoGP26 - Scorpion Helmet Pack
+addappid(4332660) -- MotoGP26 - HJC Helmet Pack
