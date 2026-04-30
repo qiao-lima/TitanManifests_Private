5c109943a97ec77fc7b44704ab8e8801e398dd77
qiao-lima <qiao-lima@github.com>
2026-05-01 00:37:27 +0800

    Manual Update 4238740

---
+++ 4238740.lua
@@ -0,0 +1,12 @@
+-- 4238740's Lua and Manifest Created by Hubcap Manifest
+-- The Board is Yours
+-- Created: April 30, 2026 at 07:40:06 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4238740, 1, "76675c6393c758a827abba0f5cebf27f9389aed9e0064e0b1f1732e295c873b9") -- The Board is Yours
+-- MAIN APP DEPOTS
+addappid(4238741, 1, "4b3dcc464fe6150d92cd17eb9b33f9a5a33c179a7cd9e947e74cde71e5adff0b") -- Depot 4238741
+setManifestid(4238741, "7032106508915429607", 384216474)
