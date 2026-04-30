1e492c21b84a84e18df617590ba2b695b5890743
qiao-lima <qiao-lima@github.com>
2026-05-01 00:37:28 +0800

    Manual Update 4553440

---
+++ 4553440.lua
@@ -0,0 +1,12 @@
+-- 4553440's Lua and Manifest Created by Hubcap Manifest
+-- 团战不能输
+-- Created: April 30, 2026 at 07:05:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4553440) -- 团战不能输
+-- MAIN APP DEPOTS
+addappid(4553441, 1, "b7c1c99a694a3e25d495c38df3f00e374d942e5d24d7172f02e782d9007d5590") -- Depot 4553441
+setManifestid(4553441, "6545199203557792825", 234639351)
