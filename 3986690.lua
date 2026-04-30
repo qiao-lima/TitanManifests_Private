e815f99e010317e8b9cc16fecb2d60aa03e0dd44
qiao-lima <qiao-lima@github.com>
2026-05-01 00:37:25 +0800

    Manual Update 3986690

---
+++ 3986690.lua
@@ -0,0 +1,25 @@
+-- 3986690's Lua and Manifest Created by Hubcap Manifest
+-- Ariana and the Elder Codex
+-- Created: April 29, 2026 at 12:57:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 2 (4 excluded)
+
+-- MAIN APPLICATION
+addappid(3986690) -- Ariana and the Elder Codex
+-- MAIN APP DEPOTS
+addappid(3986691, 1, "a4d9c62943e8881dd3339f75b865e5da3384c232df2cd3bba96d13309628f767") -- Depot 3986691
+setManifestid(3986691, "5436490476763064730", 1584199981)
+-- DLCS WITH DEDICATED DEPOTS
+-- Ariana and the Elder Codex - PC and Mobile Wallpapers (AppID: 4356790)
+addappid(4356790)
+addappid(4356790, 1, "b9427368674542dd2db2d13119a9b431888f5129aebe760f21bfc3cf70373937") -- Ariana and the Elder Codex - PC and Mobile Wallpapers - Depot 4356790
+setManifestid(4356790, "8308051710605086203", 26240574)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4131000) -- Ariana and the Elder Codex - Ocean Bookmark
+-- EXCLUDED DLCS:
+-- UNRELEASED DLCS (COMMENTED OUT)
+-- addappid(4131010) -- DLC 4131010 (unreleased)
+-- addappid(4131030) -- DLC 4131030 (unreleased)
+-- addappid(4152630) -- DLC 4152630 (unreleased)
+-- addappid(4211250) -- DLC 4211250 (unreleased)
