32340a5c283a0812072f5832960075b318cb8f71
qiao-lima <qiao-lima@github.com>
2026-05-01 00:31:36 +0800

    Manual Update 1128860

---
+++ 1128860.lua
@@ -0,0 +1,33 @@
+-- 1128860's Lua and Manifest Created by Hubcap Manifest
+-- Men of War II
+-- Created: September 30, 2025 at 00:21:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 9
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(1128860, 1, "e384715e6bf11b2b5ddbf7fbfcd9404ed9309515f9935c17a00fe9b5432485e2") -- Men of War II
+-- MAIN APP DEPOTS
+addappid(1128861, 1, "bc219840d5a6f2bc845e7f1cfa80c5229cca19207a39f755b9ed09e0fbd72697") -- Depot 1128861
+setManifestid(1128861, "5111841049222983859", 25925107171)
+addappid(1128862, 1, "8e2e9ac5037d74ae628342f68da83ebd33e500c378dc6aa3a77783d7ae34bcca") -- Depot 1128862
+setManifestid(1128862, "3169747346325658444", 66226625)
+addappid(1128863, 1, "f777f9cca5604faa664c3c32d3948204742b63a9ecda1a05e17edd378e97cfe8") -- Depot 1128863
+setManifestid(1128863, "3275418631828410400", 34928640)
+addappid(1128864, 1, "c6bac469c052dac209d1a910bb88424658d7e75994b426edbfce64b817e28faa") -- Depot 1128864
+setManifestid(1128864, "7600844478778049063", 17891840)
+addappid(1128865, 1, "984762a4a689d7a00cc456f6498c3398a7c0bf6d19bf5e57b250bf3d1c79b9cc") -- Depot 1128865
+setManifestid(1128865, "3218303862362196571", 39751082)
+addappid(1128866, 1, "759319e14931efce28e2c50ccd9f70bac6c64684b87d88adb60c7050a6fa75fe") -- Depot 1128866
+setManifestid(1128866, "8384434365200529661", 32988107)
+addappid(1128867, 1, "b1dd045cedc9a70672bdefd5cf5e7cc63ab868421580bf79a554c60bb2b8e3a4") -- Depot 1128867
+setManifestid(1128867, "1077624452265151930", 17408186)
+-- DLCS WITH DEDICATED DEPOTS
+-- Men of War II - Digital Artbook (AppID: 2530640)
+addappid(2530640)
+addappid(2530640, 1, "d31d820766d92a03c17b6b3df527ed3b7f336b26e7e9cc2fe36b49b2e791e6bb") -- Men of War II - Digital Artbook - Depot 2530640
+setManifestid(2530640, "7757618600484110922", 28398303)
+-- Men of War II - Frontline Hero Pack (AppID: 2530660)
+addappid(2530660)
+addappid(2530660, 1, "db0f5afae33c22a230e8b4516a90f36f856461915fd14f8d2ba61c06f4dc5a7e") -- Men of War II - Frontline Hero Pack - Depot 2530660
+setManifestid(2530660, "7241877389079790517", 39769443)
