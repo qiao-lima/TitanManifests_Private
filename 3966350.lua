62fe69e63b8dec87fd3dd9261e49c7a70fa49144
qiao-lima <qiao-lima@github.com>
2026-05-01 00:37:24 +0800

    Manual Update 3966350

---
+++ 3966350.lua
@@ -0,0 +1,16 @@
+-- 3966350's Lua and Manifest Created by Hubcap Manifest
+-- 崩壊世界のナヴィガトーレ
+-- Created: April 30, 2026 at 05:36:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(3966350) -- 崩壊世界のナヴィガトーレ
+-- MAIN APP DEPOTS
+addappid(3966351, 1, "b3de31b446be6db798a8da78f11d386e73fd378f24f2fffd2896e7bcd6d1eeba") -- Depot 3966351
+setManifestid(3966351, "1688575787881271643", 2260460197)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+--   (AppID: 4536250) - missing depot keys
+-- addappid(4536250)
