d1f1faff9017a1cc20d9f666adc770e9ed61061c
qiao-lima <qiao-lima@github.com>
2026-05-01 00:37:26 +0800

    Manual Update 4063380

---
+++ 4063380.lua
@@ -0,0 +1,12 @@
+-- 4063380's Lua and Manifest Created by Hubcap Manifest
+-- Stalker from Willowfield St.
+-- Created: April 28, 2026 at 20:05:03 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4063380) -- Stalker from Willowfield St.
+-- MAIN APP DEPOTS
+addappid(4063381, 1, "09a5c494d6fff9150c817544effa6ced74e4a4fdad1a23d209af251adbe18087") -- Depot 4063381
+setManifestid(4063381, "8287003614717979407", 1817428758)
