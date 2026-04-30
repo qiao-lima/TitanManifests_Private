1eab4c268191c9babda3bb729a88ff08f179a98a
qiao-lima <qiao-lima@github.com>
2026-05-01 00:31:38 +0800

    Manual Update 2723430

---
+++ 2723430.lua
@@ -0,0 +1,12 @@
+-- 2723430's Lua and Manifest Created by Hubcap Manifest
+-- inKONBINI: One Store. Many Stories
+-- Created: April 30, 2026 at 03:57:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2723430) -- inKONBINI: One Store. Many Stories
+-- MAIN APP DEPOTS
+addappid(2723431, 1, "79ca9ef0b42e6d5c9d71f834631501c3083ee69dc6a8ab1a09248ec247f9efc6") -- Depot 2723431
+setManifestid(2723431, "426871671867915312", 3132487317)
