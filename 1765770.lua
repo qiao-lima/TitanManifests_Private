4827d86602151d83b410483199b1b50f32af5041
qiao-lima <qiao-lima@github.com>
2026-05-01 00:31:37 +0800

    Manual Update 1765770

---
+++ 1765770.lua
@@ -0,0 +1,14 @@
+-- 1765770's Lua and Manifest Created by Hubcap Manifest
+-- Blackfire Crusade
+-- Created: April 29, 2026 at 23:15:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1765770) -- Blackfire Crusade
+-- MAIN APP DEPOTS
+addappid(1765771, 1, "72e74e534a1e09bdd5993cc5c96af5b5e679c6cc3568f489f290d9b9d58b81a0") -- Depot 1765771
+setManifestid(1765771, "4307761526768036672", 470966904)
+addappid(1765772, 1, "33db90d29ce873e338e49cf4512479d58dc7242edcca72ea4edaac99037cb072") -- Depot 1765772
+setManifestid(1765772, "5573829633433235488", 483805416)
