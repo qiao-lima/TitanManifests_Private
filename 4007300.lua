047567228ce3855695eb94cb0a68584312ef4e16
qiao-lima <qiao-lima@github.com>
2026-05-01 00:37:25 +0800

    Manual Update 4007300

---
+++ 4007300.lua
@@ -0,0 +1,12 @@
+-- 4007300's Lua and Manifest Created by Hubcap Manifest
+-- 荒域奇谭(Legends of Wasteland)
+-- Created: April 30, 2026 at 02:51:53 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4007300) -- 荒域奇谭(Legends of Wasteland)
+-- MAIN APP DEPOTS
+addappid(4007301, 1, "fdff838b56c01202af7eefa70e031f44e34cb7ef2ae9ea1707a906bdf84a9f01") -- Depot 4007301
+setManifestid(4007301, "673569245554986664", 1011982506)
