cf1d0c3059ef4582b5b7d91b76c67d003227ea76
qiao-lima <qiao-lima@github.com>
2026-05-01 00:37:26 +0800

    Manual Update 4117860

---
+++ 4117860.lua
@@ -0,0 +1,12 @@
+-- 4117860's Lua and Manifest Created by Hubcap Manifest
+-- Alice Landing：愛莉絲迫降
+-- Created: April 30, 2026 at 07:28:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4117860) -- Alice Landing：愛莉絲迫降
+-- MAIN APP DEPOTS
+addappid(4117861, 1, "c61dd1be04af3e0126d8fe7cfa1c47de9f5d72b1b8c6f294d8f4403fc05fd71e") -- Depot 4117861
+setManifestid(4117861, "9188420366727179900", 793571442)
