c6637c2e536e11573c018f5efb95e9e4fce352ea
qiao-lima <qiao-lima@github.com>
2026-05-01 00:37:25 +0800

    Manual Update 4038550

---
+++ 4038550.lua
@@ -0,0 +1,12 @@
+-- 4038550's Lua and Manifest Created by Hubcap Manifest
+-- HAMAM
+-- Created: April 29, 2026 at 20:23:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4038550) -- HAMAM
+-- MAIN APP DEPOTS
+addappid(4038551, 1, "133f75f345c990de8cedb2c1c217b83fd4471092b1ab4f8b05b8e902df783f27") -- Depot 4038551
+setManifestid(4038551, "641101474912208255", 1811166697)
