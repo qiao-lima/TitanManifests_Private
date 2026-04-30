623401d9e0bee555a267138230d9a760f464d4d2
qiao-lima <qiao-lima@github.com>
2026-05-01 00:31:38 +0800

    Manual Update 2916590

---
+++ 2916590.lua
@@ -0,0 +1,12 @@
+-- 2916590's Lua and Manifest Created by Hubcap Manifest
+-- Fantasy Friends: Dream Worlds
+-- Created: April 30, 2026 at 07:27:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2916590, 1, "69e76f2aef509bcf77c6526f8581c424dc5284bc10966a2c81a38ccf70183c62") -- Fantasy Friends: Dream Worlds
+-- MAIN APP DEPOTS
+addappid(2916591, 1, "75a978b9e84c2af464f194d9ce37b6c0228a8603d7b259486bc07ddd9f7d24cc") -- Depot 2916591
+setManifestid(2916591, "327803365664116656", 1507976396)
