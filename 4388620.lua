2511423f8cc93e5c7603927d1ba3dbbeac14c1f7
qiao-lima <qiao-lima@github.com>
2026-05-01 00:37:27 +0800

    Manual Update 4388620

---
+++ 4388620.lua
@@ -0,0 +1,18 @@
+-- 4388620's Lua and Manifest Created by Hubcap Manifest
+-- Cosmic Wayfinder
+-- Created: April 30, 2026 at 07:46:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4388620) -- Cosmic Wayfinder
+-- MAIN APP DEPOTS
+addappid(4388622, 1, "1a793a0784f1a746fb1d9c0c11bed15ed499d0777a84b5e4302ab2511f5528c0") -- Depot 4388622
+setManifestid(4388622, "1821784565665378005", 225441831)
+addappid(4388623, 1, "711a86db43c27083870a91603a46f02cb7bb5e06eebc5e6aa52e77db66a2c228") -- Depot 4388623
+setManifestid(4388623, "5666139780405338575", 304479650)
+addappid(4388624, 1, "72d93cf4af647f116cdcd79c9244ef5206db8ec5c7f175c530dd262394cb8c3f") -- Depot 4388624
+setManifestid(4388624, "6247055616860660055", 193278496)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4388621) -- Depot 4388621 (empty depot)
