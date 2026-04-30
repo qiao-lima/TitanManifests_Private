5c0e897234b4a520da8bf2ac2b71cdb987b87e56
qiao-lima <qiao-lima@github.com>
2026-05-01 00:37:24 +0800

    Manual Update 3885590

---
+++ 3885590.lua
@@ -0,0 +1,25 @@
+-- 3885590's Lua and Manifest Created by Hubcap Manifest
+-- The Coma 3: Bloodlines
+-- Created: April 30, 2026 at 04:57:19 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 8 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(3885590) -- The Coma 3: Bloodlines
+-- MAIN APP DEPOTS
+addappid(3885591, 1, "7462789e82b8d5bfd1826e778373fec8ee0ce31a257685828ec1cd26143df489") -- Depot 3885591
+setManifestid(3885591, "2810460550208895778", 8497678061)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4279340) -- The Coma 3 Bloodlines - Nightmare Slayer Skin
+addappid(4279350) -- The Coma 3 Bloodlines - Ghost Agent No. 3 Skin
+addappid(4279360) -- The Coma 3 Bloodlines - Overaction Skin
+addappid(4279370) -- The Coma 3 Bloodlines - Autumn Girl Skin
+addappid(4279380) -- The Coma 3 Bloodlines - Sehwa Arts High School Skin
+addappid(4279390) -- The Coma 3 Bloodlines - Gangster Shaman Girl Skin
+addappid(4279400) -- The Coma 3 Bloodlines - Overly Attractive Teacher Skin
+addappid(4279410) -- The Coma 3 Bloodlines - Daughter of a Millionaire Skin
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- The Coma 3 Bloodlines - Artbook (AppID: 4279430) - missing depot keys
+-- addappid(4279430)
