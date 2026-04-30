a5033555baf090c722fe4863305ef284b41b512e
qiao-lima <qiao-lima@github.com>
2026-05-01 00:31:37 +0800

    Manual Update 2154070

---
+++ 2154070.lua
@@ -0,0 +1,18 @@
+-- 2154070's Lua and Manifest Created by Hubcap Manifest
+-- INDUSTRIA 2
+-- Created: April 29, 2026 at 14:09:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2154070, 1, "3a4acd5f8fb2a723e9a593fdf54d689d8b759cb4f067b41add3a322618eecff9") -- INDUSTRIA 2
+-- MAIN APP DEPOTS
+addappid(2154071, 1, "76c42a0b4edfb80951de77cdc5c41b48136e530d8ad1908c4883153887f61042") -- Depot 2154071
+setManifestid(2154071, "8232561928791202603", 18194075104)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
