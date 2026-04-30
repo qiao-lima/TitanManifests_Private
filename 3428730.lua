edc2688862e6bf9c59fc657b7a2c9f1036153dc6
qiao-lima <qiao-lima@github.com>
2026-05-01 00:31:39 +0800

    Manual Update 3428730

---
+++ 3428730.lua
@@ -0,0 +1,14 @@
+-- 3428730's Lua and Manifest Created by Hubcap Manifest
+-- Firefighter Frankie
+-- Created: April 29, 2026 at 00:12:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3428730) -- Firefighter Frankie
+-- MAIN APP DEPOTS
+addappid(3428731, 1, "c71bbc1567147bd9a9752aafcb7d38c6aee5ce03ba6b5feb630f32ccbc590540") -- Depot 3428731
+setManifestid(3428731, "3660966625523846408", 947692467)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3428732) -- Depot 3428732 (empty depot)
