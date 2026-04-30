90142f0c289a9e5b8316083f4ec4cfc9d43d5687
qiao-lima <qiao-lima@github.com>
2026-05-01 00:37:25 +0800

    Manual Update 3994890

---
+++ 3994890.lua
@@ -0,0 +1,12 @@
+-- 3994890's Lua and Manifest Created by Hubcap Manifest
+-- Throat Goat
+-- Created: April 27, 2026 at 14:05:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3994890) -- Throat Goat
+-- MAIN APP DEPOTS
+addappid(3994891, 1, "33985bd19cd30bca7a54082d4e1bf1577d86d5f1595d866b51a9a09cd092ac42") -- Depot 3994891
+setManifestid(3994891, "3461794199222165506", 386899868)
