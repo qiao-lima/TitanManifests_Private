9e619bfe8f2c30707a481420cc3a9cef0ebf9a90
qiao-lima <qiao-lima@github.com>
2026-05-01 00:37:27 +0800

    Manual Update 4488180

---
+++ 4488180.lua
@@ -0,0 +1,12 @@
+-- 4488180's Lua and Manifest Created by Hubcap Manifest
+-- Dark Fantasy Tower Defense
+-- Created: April 30, 2026 at 07:28:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4488180, 1, "a2909b67f827e6dfccf254c7f16d030ea4780e240f5388d5991c78b41f23f86f") -- Dark Fantasy Tower Defense
+-- MAIN APP DEPOTS
+addappid(4488181, 1, "fcd072fbe2d01b3f634ca0862d7088ea33ac3656f5b20c16014f1ca0eabddccf") -- Depot 4488181
+setManifestid(4488181, "4249235094671948809", 271111384)
