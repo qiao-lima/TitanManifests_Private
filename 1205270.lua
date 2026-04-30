180d44236bf94bb9bf9d852d656d254875af7893
qiao-lima <qiao-lima@github.com>
2026-05-01 00:31:36 +0800

    Manual Update 1205270

---
+++ 1205270.lua
@@ -0,0 +1,23 @@
+-- 1205270's Lua and Manifest Created by Hubcap Manifest
+-- Magin: The Rat Project Stories
+-- Created: April 30, 2026 at 07:16:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 1
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1205270, 1, "04f3987d341bd369df14a22e365df473c32ebf62d1199831eaeebbb90b02803c") -- Magin: The Rat Project Stories
+-- MAIN APP DEPOTS
+addappid(1205271, 1, "d2cd72b8f306246fc5bbc6c496f1e4b74bef2727e8eeab7a4bc7bab03ce10332") -- Depot 1205271
+setManifestid(1205271, "8021968964779243765", 4256657987)
+-- SHARED DEPOTS (from other apps)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Magin The Rat Project Stories - Supporter Pack (AppID: 2731250)
+addappid(2731250)
+addappid(2731250, 1, "2f788c4422d875e833c5af65b2fc3341755bc1f2146c3b94b46dae9f1f29efb2") -- Magin The Rat Project Stories - Supporter Pack - Depot 2731250
+setManifestid(2731250, "8564958847932467301", 183565162)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(1205272) -- Depot 1205272 (empty depot)
