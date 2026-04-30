e19593e1b2e21c47b33ddb5b62df02a7dd422a3b
qiao-lima <qiao-lima@github.com>
2026-05-01 00:37:26 +0800

    Manual Update 4090270

---
+++ 4090270.lua
@@ -0,0 +1,14 @@
+-- 4090270's Lua and Manifest Created by Hubcap Manifest
+-- BastionOS
+-- Created: April 30, 2026 at 07:28:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4090270, 1, "8cba3d9cb6636d38ad25d1f4cf54ca0712f218d728237f78c3bebbb0fc654360") -- BastionOS
+-- MAIN APP DEPOTS
+addappid(4090271, 1, "e90d8a99289fc32b298bc6cc4b7ad70878e11d2f4542f2c54fdd38ed198c94b7") -- Depot 4090271
+setManifestid(4090271, "7908080033663880350", 182773425)
+addappid(4090272, 1, "2c84cda2109bbb27ce844189d12baa0d1e8526791908f3568771b4f9c0ea86b4") -- Depot 4090272
+setManifestid(4090272, "7334289684327092883", 255633305)
