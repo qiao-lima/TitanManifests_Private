16f72a1b65f4421e1d656708c6ecde0c57cfa0ed
qiao-lima <qiao-lima@github.com>
2026-05-02 01:00:23 +0800

    Manual Update 3955300

---
+++ 3955300.lua
@@ -0,0 +1,12 @@
+-- 3955300's Lua and Manifest Created by Hubcap Manifest
+-- The Queen's Quest
+-- Created: May 01, 2026 at 07:22:08 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3955300) -- The Queen's Quest
+-- MAIN APP DEPOTS
+addappid(3955301, 1, "c100850828db43f739cb7080e1782c0a472f190d976b10c9df9d82d029db3536") -- Depot 3955301
+setManifestid(3955301, "8050581885290986457", 999226688)
