5f0535dbbd79ab3d9f32b534dfaa976f8d7bc48d
qiao-lima <qiao-lima@github.com>
2026-05-02 01:00:24 +0800

    Manual Update 4157130

---
+++ 4157130.lua
@@ -0,0 +1,18 @@
+-- 4157130's Lua and Manifest Created by Hubcap Manifest
+-- Dark Pals: The 1st Floor
+-- Created: May 01, 2026 at 11:27:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4157130) -- Dark Pals: The 1st Floor
+-- MAIN APP DEPOTS
+addappid(4157131, 1, "f0553aec8a8550b7deee04cf5eec6bc0b3fd13a407b189a4201260b5c9198251") -- Depot 4157131
+setManifestid(4157131, "6513262579257236913", 2506695664)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
