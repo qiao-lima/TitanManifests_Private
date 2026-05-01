784d0fdd176df18ab2d1ab3b095b22398a59803c
qiao-lima <qiao-lima@github.com>
2026-05-01 14:25:58 +0800

    Manual Update 3627790

---
+++ 3627790.lua
@@ -0,0 +1,19 @@
+-- 3627790's Lua and Manifest Created by Hubcap Manifest
+-- 纸嫁衣7卿不负
+-- Created: January 19, 2026 at 13:27:51 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3627790) -- 纸嫁衣7卿不负
+-- MAIN APP DEPOTS
+addappid(3627792, 1, "0788ce3014968b600343007fef05d8da9b9b32e374175d933f0c900866a04c1b") -- Depot 3627792
+setManifestid(3627792, "6161949926552624338", 237688211)
+addappid(3627793, 1, "55974ab4cdd99a71daa7121176a6d78dce576c10ec03897bee52c2ddada58c59") -- Depot 3627793
+setManifestid(3627793, "2927501132684571745", 228730806)
+-- DLCS WITH DEDICATED DEPOTS
+-- 7- (AppID: 3774080)
+addappid(3774080)
+addappid(3774080, 1, "4d1c0ba02f11ee44e87127dc966a34868fdfb013e8c09b26c1262c05dc1f6faa") -- 7- - Depot 3774080
+setManifestid(3774080, "5576845473352011314", 58601940)
