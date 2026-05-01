08f484373ce125e96f2a68d29330cc34837ba537
qiao-lima <qiao-lima@github.com>
2026-05-02 01:00:25 +0800

    Manual Update 4482960

---
+++ 4482960.lua
@@ -0,0 +1,12 @@
+-- 4482960's Lua and Manifest Created by Hubcap Manifest
+-- 驱入虚空
+-- Created: May 01, 2026 at 11:27:37 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4482960) -- 驱入虚空
+-- MAIN APP DEPOTS
+addappid(4482961, 1, "b085d69665bdb243a33fe5548fe7123715ee9482390f0294a7ce47cdd5d83977") -- Depot 4482961
+setManifestid(4482961, "3898409593040293493", 22597881969)
