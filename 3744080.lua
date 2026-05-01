a7281f43fe7a76f3a83b7584aa715bdb1f69b73c
qiao-lima <qiao-lima@github.com>
2026-05-02 01:00:22 +0800

    Manual Update 3744080

---
+++ 3744080.lua
@@ -0,0 +1,14 @@
+-- 3744080's Lua and Manifest Created by Hubcap Manifest
+-- Mining Merchant
+-- Created: May 01, 2026 at 12:36:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3744080) -- Mining Merchant
+-- MAIN APP DEPOTS
+addappid(3744081, 1, "b187b84097a6579d540175e21cd93b2947d205a1524f766d0197ac4ad2c21cdc") -- Depot 3744081
+setManifestid(3744081, "7406315136277181409", 137278574)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4257340) -- Mining Merchant - Supporter Pack
