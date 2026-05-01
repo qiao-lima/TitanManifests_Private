6c4947d11de10e32017873a4c00494fd8ba0d75e
qiao-lima <qiao-lima@github.com>
2026-05-02 01:00:24 +0800

    Manual Update 4205010

---
+++ 4205010.lua
@@ -0,0 +1,12 @@
+-- 4205010's Lua and Manifest Created by Hubcap Manifest
+-- 成为皇帝！我的万亿美女星球帝国
+-- Created: May 01, 2026 at 07:22:03 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4205010) -- 成为皇帝！我的万亿美女星球帝国
+-- MAIN APP DEPOTS
+addappid(4205011, 1, "2a5e23c74aaaae189c475f0b142c22a2f4585b15028b04db3a849eb95c07e35c") -- Depot 4205011
+setManifestid(4205011, "3213150178429283652", 37017504208)
