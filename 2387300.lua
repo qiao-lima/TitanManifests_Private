3964d6733b8fb070545ba8814275464a6eb52d36
qiao-lima <qiao-lima@github.com>
2026-05-02 01:00:21 +0800

    Manual Update 2387300

---
+++ 2387300.lua
@@ -0,0 +1,12 @@
+-- 2387300's Lua and Manifest Created by Hubcap Manifest
+-- 绝尘
+-- Created: May 01, 2026 at 07:23:13 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2387300) -- 绝尘
+-- MAIN APP DEPOTS
+addappid(2387301, 1, "315ea410d27538eb3474eee89bb4389b3d59a6675dbee038468bc781feb5cb4e") -- Depot 2387301
+setManifestid(2387301, "1183321255855972440", 380135119)
