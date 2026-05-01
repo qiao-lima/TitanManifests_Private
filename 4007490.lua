2d1fb20645a347932f753d6c565717fc8d46dacf
qiao-lima <qiao-lima@github.com>
2026-05-02 01:00:23 +0800

    Manual Update 4007490

---
+++ 4007490.lua
@@ -0,0 +1,12 @@
+-- 4007490's Lua and Manifest Created by Hubcap Manifest
+-- Winx Club: The Magic is Back
+-- Created: May 01, 2026 at 11:57:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4007490) -- Winx Club: The Magic is Back
+-- MAIN APP DEPOTS
+addappid(4007491, 1, "fd9d89393d376b62bceac9a49dd53a2ace494e3e947c5907d4ab673ab16105c7") -- Depot 4007491
+setManifestid(4007491, "1275204724015573768", 2335255402)
