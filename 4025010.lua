97a4a5ba1b736368efd04be971e7bf48521b3afa
qiao-lima <qiao-lima@github.com>
2026-05-02 01:00:23 +0800

    Manual Update 4025010

---
+++ 4025010.lua
@@ -0,0 +1,12 @@
+-- 4025010's Lua and Manifest Created by Hubcap Manifest
+-- YAMIGATARI: FOR3ST
+-- Created: May 01, 2026 at 12:46:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4025010) -- YAMIGATARI: FOR3ST
+-- MAIN APP DEPOTS
+addappid(4025011, 1, "8a3f4835b53f1bc849f732357bcd55a649d1fe9e1f2b7e05b881b1ec017cbfe9") -- Depot 4025011
+setManifestid(4025011, "5101490494031291620", 887189822)
