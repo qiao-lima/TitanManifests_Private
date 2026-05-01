ee6ae4bac95fc1c6011ee5196e2a8512e9dc39d1
qiao-lima <qiao-lima@github.com>
2026-05-01 14:25:59 +0800

    Manual Update 4048300

---
+++ 4048300.lua
@@ -0,0 +1,19 @@
+-- 4048300's Lua and Manifest Created by Hubcap Manifest
+-- 緋月仙行錄
+-- Created: April 30, 2026 at 23:56:58 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4048300) -- 緋月仙行錄
+-- MAIN APP DEPOTS
+addappid(4048301, 1, "3cd301e2477af9d8360795892389f0a1ad3b953513271435f6ce27ad62c9ed03") -- Depot 4048301
+setManifestid(4048301, "7014481716838168126", 3153900139)
+addappid(4048303, 1, "3b31ec7bbb5a5ce5bb7e5a52aa162b1ebd6618a90578201b7cc7036a6e15fa15") -- Depot 4048303
+setManifestid(4048303, "1153762638590096827", 3153900139)
+-- DLCS WITH DEDICATED DEPOTS
+--   (AppID: 4579960)
+addappid(4579960)
+addappid(4579961, 1, "0263bac6a0758341a7b546e074566da3e9a74fcb74b1099374ec102971309092") --   - Depot 4579961
+setManifestid(4579961, "2360373448587687651", 478585175)
