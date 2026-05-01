33a7cf0b509a88f24bbfaaa9be7162637fe0663d
qiao-lima <qiao-lima@github.com>
2026-05-02 01:00:24 +0800

    Manual Update 4310900

---
+++ 4310900.lua
@@ -0,0 +1,12 @@
+-- 4310900's Lua and Manifest Created by Hubcap Manifest
+-- GLADIMAKER : Auto Battle RPG
+-- Created: May 01, 2026 at 07:24:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4310900) -- GLADIMAKER : Auto Battle RPG
+-- MAIN APP DEPOTS
+addappid(4310901, 1, "d16331225b09ca77bf37e3f3aa13490c4d8b8dbf6d20ee917ff052ab19efb159") -- Depot 4310901
+setManifestid(4310901, "7958242002557678414", 1278506753)
