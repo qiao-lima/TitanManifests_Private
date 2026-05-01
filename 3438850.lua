25c0946506ba524cc67b36396e55c453561d944f
qiao-lima <qiao-lima@github.com>
2026-05-02 01:00:22 +0800

    Manual Update 3438850

---
+++ 3438850.lua
@@ -0,0 +1,14 @@
+-- 3438850's Lua and Manifest Created by Hubcap Manifest
+-- Sledding Game
+-- Created: May 01, 2026 at 12:10:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3438850, 1, "30e1f7e188e92cc3ddfe88f8da2ddd9201e70d77603ac4bc798e055b2fdb91f1") -- Sledding Game
+-- MAIN APP DEPOTS
+addappid(3438851, 1, "4f2a0854d927f1098bcc4ef65ce87280d5779230d0ce4a79549a990e7a04f2a7") -- Depot 3438851
+setManifestid(3438851, "7311454735255286733", 977776032)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4527730) -- Sledding Game - Supporter Pack
