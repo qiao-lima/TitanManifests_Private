d5708bc5e04ac4cd2e6d7c4ef0425f95140c8172
qiao-lima <qiao-lima@github.com>
2026-05-02 01:00:25 +0800

    Manual Update 4453250

---
+++ 4453250.lua
@@ -0,0 +1,12 @@
+-- 4453250's Lua and Manifest Created by Hubcap Manifest
+-- Tides of Trade: Eastern Horizons
+-- Created: May 01, 2026 at 09:08:18 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4453250) -- Tides of Trade: Eastern Horizons
+-- MAIN APP DEPOTS
+addappid(4453251, 1, "b76dfb9c84c2befe41493bc8cfdce6b54d4716858fd05690986ae94c6ff63a85") -- Depot 4453251
+setManifestid(4453251, "5671952061975365184", 540372825)
