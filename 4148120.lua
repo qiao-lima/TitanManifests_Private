8df542e060302eb3daafa2a4bf3077ad69279262
qiao-lima <qiao-lima@github.com>
2026-05-02 01:00:24 +0800

    Manual Update 4148120

---
+++ 4148120.lua
@@ -0,0 +1,12 @@
+-- 4148120's Lua and Manifest Created by Hubcap Manifest
+-- Cell Racers
+-- Created: May 01, 2026 at 07:23:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4148120) -- Cell Racers
+-- MAIN APP DEPOTS
+addappid(4148121, 1, "240c411297e9a005f58846f69803417d10ea6e77b72edd8e0cabcf32c5aa569d") -- Depot 4148121
+setManifestid(4148121, "1753912212557429298", 3298230816)
