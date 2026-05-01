e2b24e776257e4bbda565b136de2669173c09142
qiao-lima <qiao-lima@github.com>
2026-05-02 01:00:23 +0800

    Manual Update 4116460

---
+++ 4116460.lua
@@ -0,0 +1,12 @@
+-- 4116460's Lua and Manifest Created by Hubcap Manifest
+-- TOYCRAFTER
+-- Created: May 01, 2026 at 07:22:13 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4116460) -- TOYCRAFTER
+-- MAIN APP DEPOTS
+addappid(4116461, 1, "78b4566def7b237ecdee4b72d679cc18989c5f3d64638c21fcbbbea5f3bfeb80") -- Depot 4116461
+setManifestid(4116461, "6185050539249531898", 546194671)
