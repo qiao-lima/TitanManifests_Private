d66da6e6d0ae3c61374b8310e031ad3cc53f141c
qiao-lima <qiao-lima@github.com>
2026-05-03 02:16:29 +0800

    Manual Update 3204620

---
+++ 3204620.lua
@@ -0,0 +1,18 @@
+-- 3204620's Lua and Manifest Created by Hubcap Manifest
+-- Tellus: Echo of the Void
+-- Created: May 01, 2026 at 19:31:29 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3204620) -- Tellus: Echo of the Void
+-- MAIN APP DEPOTS
+addappid(3204621, 1, "e2c6078404f01e915244271f2b5a7e99bef9fee7a025c546dc8c93f8d08b6efa") -- Depot 3204621
+setManifestid(3204621, "7066322964337018184", 9768717374)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
