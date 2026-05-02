87c4ced7d8f49d449b0568559af23417e532db7a
qiao-lima <qiao-lima@github.com>
2026-05-03 02:16:29 +0800

    Manual Update 2779400

---
+++ 2779400.lua
@@ -0,0 +1,16 @@
+-- 2779400's Lua and Manifest Created by Hubcap Manifest
+-- Intown Nightmares
+-- Created: May 02, 2026 at 12:04:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2779400) -- Intown Nightmares
+-- MAIN APP DEPOTS
+addappid(2779401, 1, "fb78baadf119f44211df5b626a9daa8a94816c0610456cfcb46a93c1702dc6ee") -- Depot 2779401
+setManifestid(2779401, "1612583501753413767", 2922222246)
+addappid(2779402, 1, "37046ab4db901dd6b8d4016ed250c6d93703425b9ed50b47f8e3f1b7a676804c") -- Depot 2779402
+setManifestid(2779402, "8263805794369935119", 3083567712)
+addappid(2779403, 1, "83996513b3f75580c0a146e974d07c39b8cc0f36af51677208826ee6e826b18f") -- Depot 2779403
+setManifestid(2779403, "6887348532732692734", 3779092509)
