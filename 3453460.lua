6f7f0462ec32f3f819cc6e6a5c13cfe9b6c4fcbf
qiao-lima <qiao-lima@github.com>
2026-05-03 02:16:30 +0800

    Manual Update 3453460

---
+++ 3453460.lua
@@ -0,0 +1,12 @@
+-- 3453460's Lua and Manifest Created by Hubcap Manifest
+-- 东方联战
+-- Created: May 02, 2026 at 14:14:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3453460) -- 东方联战
+-- MAIN APP DEPOTS
+addappid(3453461, 1, "dec2fe61e78f80d049dfe3af9ff8971f00db06fbe85690497872104e47830555") -- Depot 3453461
+setManifestid(3453461, "641028069074228039", 896591337)
