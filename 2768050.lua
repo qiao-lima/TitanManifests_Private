cfffff9ec69eef7c488b143986a681838cd21c26
qiao-lima <qiao-lima@github.com>
2026-05-03 05:08:11 +0800

    Manual Update 2768050

---
+++ 2768050.lua
@@ -0,0 +1,12 @@
+-- 2768050's Lua and Manifest Created by Hubcap Manifest
+-- Elden Gunfire
+-- Created: May 02, 2026 at 13:57:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2768050) -- Elden Gunfire
+-- MAIN APP DEPOTS
+addappid(2768051, 1, "e2e703ab7fd8427d04b327acdb496878a0c06e251f7f69587e71945544f56861") -- Depot 2768051
+setManifestid(2768051, "6599504740400602356", 336811732)
