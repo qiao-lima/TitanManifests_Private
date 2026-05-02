1f2c1934bf93fc49f70d0c0922075717dc32c5a7
qiao-lima <qiao-lima@github.com>
2026-05-03 05:08:35 +0800

    Manual Update 4575390

---
+++ 4575390.lua
@@ -0,0 +1,12 @@
+-- 4575390's Lua and Manifest Created by Hubcap Manifest
+-- Bow-wow Battle
+-- Created: May 01, 2026 at 09:57:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4575390) -- Bow-wow Battle
+-- MAIN APP DEPOTS
+addappid(4575391, 1, "5db43126ea3762c201898ae2e6b08c448e65d75e9fd759804c40a9b2b6752166") -- Depot 4575391
+setManifestid(4575391, "9120397723617789007", 175313256)
