4196e3e16c2f226db10db57c3826daa7fec99d17
qiao-lima <qiao-lima@github.com>
2026-05-03 05:08:12 +0800

    Manual Update 3307050

---
+++ 3307050.lua
@@ -0,0 +1,18 @@
+-- 3307050's Lua and Manifest Created by Hubcap Manifest
+-- 凛冬将至Winter is coming
+-- Created: May 02, 2026 at 14:24:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3307050) -- 凛冬将至Winter is coming
+-- MAIN APP DEPOTS
+addappid(3307051, 1, "873cfdd5b2fd3571257464f4d8705018eac4d3c05b8ee1966911e608ef2508d5") -- Depot 3307051
+setManifestid(3307051, "3975652277559242324", 2026647918)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
