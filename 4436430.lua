463852d29a3a5ab7424fb6c98d31a0db891d202b
qiao-lima <qiao-lima@github.com>
2026-05-03 02:16:33 +0800

    Manual Update 4436430

---
+++ 4436430.lua
@@ -0,0 +1,12 @@
+-- 4436430's Lua and Manifest Created by Hubcap Manifest
+-- Fragments In Situ: Escape Room
+-- Created: May 02, 2026 at 14:13:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4436430) -- Fragments In Situ: Escape Room
+-- MAIN APP DEPOTS
+addappid(4436431, 1, "0c00e381ebf8ae57ef70638f008e591bb26b0f20133258dad86c270c22d4d6ef") -- Depot 4436431
+setManifestid(4436431, "2674140436425799205", 3450087608)
