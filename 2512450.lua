ed2914eda08d9a9b6081f183a02c0157d9d94a4b
qiao-lima <qiao-lima@github.com>
2026-05-03 05:08:11 +0800

    Manual Update 2512450

---
+++ 2512450.lua
@@ -0,0 +1,12 @@
+-- 2512450's Lua and Manifest Created by Hubcap Manifest
+-- SoulQuest
+-- Created: May 02, 2026 at 14:13:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2512450) -- SoulQuest
+-- MAIN APP DEPOTS
+addappid(2512451, 1, "8c0d4b93f033466e6f02a78300901e6612247a91af79d42b9dd0cf5b94b1835a") -- Depot 2512451
+setManifestid(2512451, "1572020587208013431", 1459080343)
