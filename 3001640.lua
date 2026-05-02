b6d99b3919586fb73b0fa2d0fdfd3fa845e0908a
qiao-lima <qiao-lima@github.com>
2026-05-03 02:16:29 +0800

    Manual Update 3001640

---
+++ 3001640.lua
@@ -0,0 +1,12 @@
+-- 3001640's Lua and Manifest Created by Hubcap Manifest
+-- The Bucketlist Tourist
+-- Created: May 01, 2026 at 19:31:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3001640) -- The Bucketlist Tourist
+-- MAIN APP DEPOTS
+addappid(3001641, 1, "238c99510394fb699303b0b2be39dcbc7521abb6698110ae240f6d141fcf8c1c") -- Depot 3001641
+setManifestid(3001641, "373101179351949231", 249372563)
