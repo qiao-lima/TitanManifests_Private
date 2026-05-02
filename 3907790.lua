ed6bf33bddca6ca0d3e81e135faaaf35d42b0f78
qiao-lima <qiao-lima@github.com>
2026-05-03 05:08:13 +0800

    Manual Update 3907790

---
+++ 3907790.lua
@@ -0,0 +1,12 @@
+-- 3907790's Lua and Manifest Created by Hubcap Manifest
+-- Oxygen Train
+-- Created: May 02, 2026 at 17:06:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3907790) -- Oxygen Train
+-- MAIN APP DEPOTS
+addappid(3907791, 1, "22db0cd15dfca4f2c4a944655338c6007b15260252ee974fb5f0bfdf87db60f2") -- Depot 3907791
+setManifestid(3907791, "1176363684911684758", 4268373516)
