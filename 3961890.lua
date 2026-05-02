933d5a1efa21beaeb9a92485635a588a196bbc1f
qiao-lima <qiao-lima@github.com>
2026-05-03 02:16:30 +0800

    Manual Update 3961890

---
+++ 3961890.lua
@@ -0,0 +1,12 @@
+-- 3961890's Lua and Manifest Created by Hubcap Manifest
+-- Room 713
+-- Created: May 02, 2026 at 14:15:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3961890) -- Room 713
+-- MAIN APP DEPOTS
+addappid(3961891, 1, "cadf4f2d80c20f1b2eb56210cd30ae8f8b52cfc65abf123c17997cb248ccca89") -- Depot 3961891
+setManifestid(3961891, "7490466969047041913", 3629303771)
