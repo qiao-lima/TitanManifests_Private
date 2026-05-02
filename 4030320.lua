73f850a1db9bb96fe90ab65f4f196db9409a2ada
qiao-lima <qiao-lima@github.com>
2026-05-03 02:16:32 +0800

    Manual Update 4030320

---
+++ 4030320.lua
@@ -0,0 +1,12 @@
+-- 4030320's Lua and Manifest Created by Hubcap Manifest
+-- Penance
+-- Created: May 01, 2026 at 19:30:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4030320) -- Penance
+-- MAIN APP DEPOTS
+addappid(4030321, 1, "aa84c7e35f0e24a5b2f9749c70d87c9da61e82ada9564ce77243278d52de2467") -- Depot 4030321
+setManifestid(4030321, "458404659650825511", 431585496)
