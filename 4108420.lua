1b5fe741e42e022a85b7dd5be82e4582faa5ce60
qiao-lima <qiao-lima@github.com>
2026-05-03 05:08:13 +0800

    Manual Update 4108420

---
+++ 4108420.lua
@@ -0,0 +1,12 @@
+-- 4108420's Lua and Manifest Created by Hubcap Manifest
+-- Oceanside Whispers
+-- Created: May 01, 2026 at 19:32:57 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4108420) -- Oceanside Whispers
+-- MAIN APP DEPOTS
+addappid(4108421, 1, "03c503c09bdc9e0b23efacbb4babe1a30a9af0a5ae3775d803e0b38273523f2e") -- Depot 4108421
+setManifestid(4108421, "1110606399231981669", 1475647639)
