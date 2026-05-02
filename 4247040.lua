1df581ef5b78fc76c4b8640feb1aca2263ee69fc
qiao-lima <qiao-lima@github.com>
2026-05-03 05:08:13 +0800

    Manual Update 4247040

---
+++ 4247040.lua
@@ -0,0 +1,18 @@
+-- 4247040's Lua and Manifest Created by Hubcap Manifest
+-- Valenium
+-- Created: May 01, 2026 at 20:30:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4247040) -- Valenium
+-- MAIN APP DEPOTS
+addappid(4247041, 1, "5e0dadab70e414421a42b11e6c4757320150afdd98d4a11e082c68f308933754") -- Depot 4247041
+setManifestid(4247041, "7950409377856966900", 41057906346)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
