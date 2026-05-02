dc6b485b95f26801a07575a6e2fb1e5c903222fd
qiao-lima <qiao-lima@github.com>
2026-05-03 02:16:30 +0800

    Manual Update 3618660

---
+++ 3618660.lua
@@ -0,0 +1,12 @@
+-- 3618660's Lua and Manifest Created by Hubcap Manifest
+-- Antidote to despair
+-- Created: May 01, 2026 at 19:31:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3618660) -- Antidote to despair
+-- MAIN APP DEPOTS
+addappid(3618661, 1, "902a78b6b9dbaa275e71eea365f9876951cb048a7cfb8e5c767de3a95c8fcc39") -- Depot 3618661
+setManifestid(3618661, "5117924625118131690", 337195440)
