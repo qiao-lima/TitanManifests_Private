8236f4c07ca5c80bc76ea155912cf6995cf4e4dc
qiao-lima <qiao-lima@github.com>
2026-05-03 05:08:36 +0800

    Manual Update 4620480

---
+++ 4620480.lua
@@ -0,0 +1,12 @@
+-- 4620480's Lua and Manifest Created by Hubcap Manifest
+-- Defective Detectives 2
+-- Created: May 02, 2026 at 15:15:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4620480) -- Defective Detectives 2
+-- MAIN APP DEPOTS
+addappid(4620481, 1, "00f130d8c8e3e74c35fa8579f017af54f706724dbafd3951ac7b57c81130b55c") -- Depot 4620481
+setManifestid(4620481, "5355053862657193201", 545621953)
