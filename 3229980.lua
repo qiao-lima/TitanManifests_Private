d9e291024e60c1a4f4765c5ea2e09cabdf8a366e
qiao-lima <qiao-lima@github.com>
2026-05-03 05:08:12 +0800

    Manual Update 3229980

---
+++ 3229980.lua
@@ -0,0 +1,12 @@
+-- 3229980's Lua and Manifest Created by Hubcap Manifest
+-- This is Fine: Maximum Cope
+-- Created: May 01, 2026 at 17:31:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3229980) -- This is Fine: Maximum Cope
+-- MAIN APP DEPOTS
+addappid(3229981, 1, "fa3e29abd11ef0f00dc79623c31bfeb1eda3d2aaf369fdfb57d818fce5e4ce01") -- Depot 3229981
+setManifestid(3229981, "2904545793800962561", 1778428688)
