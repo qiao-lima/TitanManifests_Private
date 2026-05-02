995a009d6c082535eb1c1ea22ec755b7b19040df
qiao-lima <qiao-lima@github.com>
2026-05-03 05:08:14 +0800

    Manual Update 4357110

---
+++ 4357110.lua
@@ -0,0 +1,12 @@
+-- 4357110's Lua and Manifest Created by Hubcap Manifest
+-- Japanese Mature: Sex Dreams
+-- Created: May 02, 2026 at 07:18:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4357110) -- Japanese Mature: Sex Dreams
+-- MAIN APP DEPOTS
+addappid(4357111, 1, "79c122b2e6be671b1e13735af7f79780685fbc3da83a7b1a478aa17e30048505") -- Depot 4357111
+setManifestid(4357111, "553541321328821782", 11813791395)
