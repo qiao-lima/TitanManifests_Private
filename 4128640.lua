b9dbc252716aa9008019c871694c0b7728d935e0
qiao-lima <qiao-lima@github.com>
2026-05-03 05:08:13 +0800

    Manual Update 4128640

---
+++ 4128640.lua
@@ -0,0 +1,12 @@
+-- 4128640's Lua and Manifest Created by Hubcap Manifest
+-- The martial world of drinking：Wu Song
+-- Created: May 01, 2026 at 07:48:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4128640) -- The martial world of drinking：Wu Song
+-- MAIN APP DEPOTS
+addappid(4128641, 1, "3bcd04fc3e8ba1a3025602670b9d7ba1cd87d59c73e6aa644a141f06c8e4c1d1") -- Depot 4128641
+setManifestid(4128641, "2062967000247765314", 678520101)
