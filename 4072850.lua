acf16db5f4029615fb7fa37d385332e14be8bb60
qiao-lima <qiao-lima@github.com>
2026-05-03 02:16:32 +0800

    Manual Update 4072850

---
+++ 4072850.lua
@@ -0,0 +1,18 @@
+-- 4072850's Lua and Manifest Created by Hubcap Manifest
+-- Isle of Food
+-- Created: May 01, 2026 at 19:31:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4072850) -- Isle of Food
+-- MAIN APP DEPOTS
+addappid(4072851, 1, "c7cb331bebec0d2c3021de0739ca590c9ff2b5f05a14a8bdd9c476908a794e74") -- Depot 4072851
+setManifestid(4072851, "1855629776158319528", 621288326)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
