ff61236b00438e15e28bc4acade7c64d21b30893
qiao-lima <qiao-lima@github.com>
2026-05-03 05:08:11 +0800

    Manual Update 2890910

---
+++ 2890910.lua
@@ -0,0 +1,12 @@
+-- 2890910's Lua and Manifest Created by Hubcap Manifest
+-- MATRESHKA
+-- Created: May 01, 2026 at 07:24:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2890910) -- MATRESHKA
+-- MAIN APP DEPOTS
+addappid(2890911, 1, "81c632f75fc1e975154245eef33289c686a8d290e35765ca9eb0ea1d130449bc") -- Depot 2890911
+setManifestid(2890911, "5122499887451968924", 3164843185)
