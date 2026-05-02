2f4e346015d96e22752bdbe0ed1a2828be873ae3
qiao-lima <qiao-lima@github.com>
2026-05-03 05:08:12 +0800

    Manual Update 3429890

---
+++ 3429890.lua
@@ -0,0 +1,22 @@
+-- 3429890's Lua and Manifest Created by Hubcap Manifest
+-- Devil of the Plague
+-- Created: May 01, 2026 at 23:09:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3429890) -- Devil of the Plague
+-- MAIN APP DEPOTS
+addappid(3429891, 1, "100357b25d70ddcc40c9c4e78744fe2781e538586e2418a238cff8c0c875d7d6") -- Depot 3429891
+setManifestid(3429891, "78780440629723186", 4575008024)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3429892) -- Depot 3429892 (empty depot)
+-- addappid(3429893) -- Depot 3429893 (empty depot)
+-- addappid(3429894) -- Depot 3429894 (empty depot)
