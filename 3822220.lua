bb8e9ec2d7447e1f7d6ee4fa9294ce2362db5dbc
qiao-lima <qiao-lima@github.com>
2026-05-03 05:08:13 +0800

    Manual Update 3822220

---
+++ 3822220.lua
@@ -0,0 +1,18 @@
+-- 3822220's Lua and Manifest Created by Hubcap Manifest
+-- WORLD HEROES PERFECT
+-- Created: May 02, 2026 at 09:23:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3822220) -- WORLD HEROES PERFECT
+-- MAIN APP DEPOTS
+addappid(3822221, 1, "84a1e222ef164a6dc0ecfaa07545fc8d76104adccefef01c54ea26f39a49f465") -- Depot 3822221
+setManifestid(3822221, "6097586096519290186", 431855056)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
