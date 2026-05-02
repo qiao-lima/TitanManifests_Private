79b64e3a532a48241cec24314d2c541ea5758ad9
qiao-lima <qiao-lima@github.com>
2026-05-03 02:16:33 +0800

    Manual Update 4604410

---
+++ 4604410.lua
@@ -0,0 +1,12 @@
+-- 4604410's Lua and Manifest Created by Hubcap Manifest
+-- Unbox your Workspace
+-- Created: May 01, 2026 at 19:30:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4604410) -- Unbox your Workspace
+-- MAIN APP DEPOTS
+addappid(4604411, 1, "0a6aea58c427ac95695e379fabd68613b68dafb31ac0fd1ae5a78fa0b80179c2") -- Depot 4604411
+setManifestid(4604411, "568829722913888101", 791214807)
