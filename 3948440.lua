daf6231cb44d0960325549074cb4d82a37f81236
qiao-lima <qiao-lima@github.com>
2026-05-03 02:16:30 +0800

    Manual Update 3948440

---
+++ 3948440.lua
@@ -0,0 +1,12 @@
+-- 3948440's Lua and Manifest Created by Hubcap Manifest
+-- MagiClean
+-- Created: May 01, 2026 at 19:30:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3948440) -- MagiClean
+-- MAIN APP DEPOTS
+addappid(3948441, 1, "d7a72eb4385a36895c229cbf98a1222462e2ce03fce3e912638cdee79c8c310f") -- Depot 3948441
+setManifestid(3948441, "2129105196233817173", 1243295185)
