03468af9e8276432f91c810320654f8a47be8468
qiao-lima <qiao-lima@github.com>
2026-05-03 05:08:12 +0800

    Manual Update 3813910

---
+++ 3813910.lua
@@ -0,0 +1,12 @@
+-- 3813910's Lua and Manifest Created by Hubcap Manifest
+-- Koloboke: Sickness Simulator
+-- Created: May 01, 2026 at 11:27:06 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3813910) -- Koloboke: Sickness Simulator
+-- MAIN APP DEPOTS
+addappid(3813911, 1, "c8bb58840e0ef31d5498f49e3fd7000b6ac6ba298afeba0b41a1285e847f8e7f") -- Depot 3813911
+setManifestid(3813911, "8875173984905444926", 1535666736)
