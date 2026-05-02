d2d964cf112388e6b43b4ffee0deca0ceb2e676e
qiao-lima <qiao-lima@github.com>
2026-05-03 05:08:11 +0800

    Manual Update 2377450

---
+++ 2377450.lua
@@ -0,0 +1,14 @@
+-- 2377450's Lua and Manifest Created by Hubcap Manifest
+-- Mayor May Knott
+-- Created: May 02, 2026 at 17:05:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2377450) -- Mayor May Knott
+-- MAIN APP DEPOTS
+addappid(2377451, 1, "0b2fc9d2b310d1fb570ab6cab6dd633b55eaf9aebe9af2631e469a7b18aa7d67") -- Depot 2377451
+setManifestid(2377451, "3628268994042069160", 1691277910)
+addappid(2377453, 1, "d4796d4ad36b44a6f217ae36ba02eba5fe0a68e3a61980b430f87886e2148e4a") -- Depot 2377453
+setManifestid(2377453, "3692328855334609538", 1718708177)
