16943d9222f329a4e8e16dd09977fd10dd3b073f
qiao-lima <qiao-lima@github.com>
2026-05-03 02:16:32 +0800

    Manual Update 4123570

---
+++ 4123570.lua
@@ -0,0 +1,12 @@
+-- 4123570's Lua and Manifest Created by Hubcap Manifest
+-- STRIKERS 1945
+-- Created: May 01, 2026 at 14:55:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4123570) -- STRIKERS 1945
+-- MAIN APP DEPOTS
+addappid(4123571, 1, "4c7f0167a4b732bb049f52952f68ab5c7b354c436eaeff9c7ce6cd9dee60da3c") -- Depot 4123571
+setManifestid(4123571, "1451143535788914135", 91779967)
