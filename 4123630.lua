cfb522549f716505cb8a03d1be0511ad4d20a30b
qiao-lima <qiao-lima@github.com>
2026-05-03 02:16:33 +0800

    Manual Update 4123630

---
+++ 4123630.lua
@@ -0,0 +1,12 @@
+-- 4123630's Lua and Manifest Created by Hubcap Manifest
+-- STRIKERS 1945 III
+-- Created: May 01, 2026 at 14:55:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4123630) -- STRIKERS 1945 III
+-- MAIN APP DEPOTS
+addappid(4123631, 1, "aec7b61c4a373761156fef2f55f0adeeaf06e339d16d2e2cdc7c1ce5b18c8242") -- Depot 4123631
+setManifestid(4123631, "4538807723820107327", 111926645)
