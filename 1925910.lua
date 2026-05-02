e30b6d74c61727f70865eb6275f23f6f2bec665a
qiao-lima <qiao-lima@github.com>
2026-05-03 05:08:10 +0800

    Manual Update 1925910

---
+++ 1925910.lua
@@ -0,0 +1,14 @@
+-- 1925910's Lua and Manifest Created by Hubcap Manifest
+-- Raven
+-- Created: May 01, 2026 at 11:27:18 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1925910) -- Raven
+-- MAIN APP DEPOTS
+addappid(1925911, 1, "723bc4d1fef04680a9ea37ed0e7a265e2fce378d3cc7c416afac05b06c99cd54") -- Depot 1925911
+setManifestid(1925911, "5198499764787671994", 559234322)
+addappid(1925912, 1, "8bb4952afd1ed7ff2c8ecb43d0909913ffdffe6d1147d7c1187dbe5b57e26f21") -- Depot 1925912
+setManifestid(1925912, "1691900712973790971", 587913019)
