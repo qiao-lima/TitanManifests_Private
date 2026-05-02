51cd0cd8e0d5978d84fb6d83ad50b55eda49f148
qiao-lima <qiao-lima@github.com>
2026-05-03 02:16:31 +0800

    Manual Update 4005300

---
+++ 4005300.lua
@@ -0,0 +1,29 @@
+-- 4005300's Lua and Manifest Created by Hubcap Manifest
+-- 失恋玩家
+-- Created: May 02, 2026 at 00:17:03 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 6
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(4005300) -- 失恋玩家
+-- MAIN APP DEPOTS
+addappid(4005301, 1, "dbc55a2ce3db09d32fb7d1c6531fa4ca49a1204963f32e4f58c0ea35c8173323") -- Depot 4005301
+setManifestid(4005301, "7182242497418893278", 74368030625)
+addappid(4005302, 1, "fab0bccf00c820ae9df85df259aafcf571b8da2120990fc86b7525260cf51eda") -- Depot 4005302
+setManifestid(4005302, "4661224661779559243", 74388712801)
+-- DLCS WITH DEDICATED DEPOTS
+--  - 2K  (AppID: 4144880)
+addappid(4144880)
+addappid(4144881, 1, "89db9202fefb4865f5f5e920c1de874c10577e5b67ae17096248492942c00723") --  - 2K  - Depot 4144881
+setManifestid(4144881, "7764593338283270133", 97131607283)
+addappid(4144882, 1, "6f90745a05b975de3701ddd996428d575832a420ebde84b91ac9aec174e625b1") --  - 2K  - Depot 4144882
+setManifestid(4144882, "9098132543374378769", 97131607283)
+--  - 4K  (AppID: 4144890)
+addappid(4144890)
+addappid(4144891, 1, "d92fba743084bf5c2d324606ca445f6b531197458cb9d05771032cacf4436d91") --  - 4K  - Depot 4144891
+setManifestid(4144891, "2587432827469438336", 149374468767)
+addappid(4144892, 1, "8c3a70cea81408e8a0037aa6bde7e7b73e5553abd9e62fa8ce658c95140cea4a") --  - 4K  - Depot 4144892
+setManifestid(4144892, "1518574470108562086", 149374468767)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4005303) -- Depot 4005303 (empty depot)
