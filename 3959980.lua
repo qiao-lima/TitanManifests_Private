9c5497b0a55e93605235c70366bc26fa20791a6e
qiao-lima <qiao-lima@github.com>
2026-05-04 00:12:50 +0800

    Manual Update 3959980

---
+++ 3959980.lua
@@ -0,0 +1,12 @@
+-- 3959980's Lua and Manifest Created by Hubcap Manifest
+-- 【Live2D & フルボイス】幼馴染のヒモ姉とのゆったり性活
+-- Created: May 02, 2026 at 23:27:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3959980) -- 【Live2D & フルボイス】幼馴染のヒモ姉とのゆったり性活
+-- MAIN APP DEPOTS
+addappid(3959981, 1, "110eeeda529a7330572bedf8a9bc3e74125f0d71d26ba05dd0e6d79f5b38c130") -- Depot 3959981
+setManifestid(3959981, "2694689966677340847", 424362541)
