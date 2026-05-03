850159a9072950d85616120188754cc517c06b9a
qiao-lima <qiao-lima@github.com>
2026-05-04 00:36:06 +0800

    Manual Update 1278540

---
+++ 1278540.lua
@@ -0,0 +1,21 @@
+-- 1278540's Lua and Manifest Created by Hubcap Manifest
+-- Esports Godfather
+-- Created: December 24, 2025 at 12:38:10 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(1278540, 1, "e095b05f8a3da986081b8efa8dbacc99f1bddeecc1c100a42beae15a8a712926") -- Esports Godfather
+-- MAIN APP DEPOTS
+addappid(1278541, 1, "d21f5b076b6db9da7c37edfedf5a2ad0796b5dbbb932e64221e350380c0a6aba") -- Depot 1278541
+setManifestid(1278541, "7158560357565892181", 3526625874)
+-- DLCS WITH DEDICATED DEPOTS
+--  (AppID: 3379820)
+addappid(3379820)
+addappid(3379820, 1, "a7c2de730c281be6bfb6fe56721d89855b2de0ffd3b85c92a4ed0ccd3aa38649") -- Depot 3379820
+setManifestid(3379820, "66686574752664721", 163060)
+-- -2 (AppID: 4065190)
+addappid(4065190)
+addappid(4065190, 1, "e1576e305aeb22fcc5d62b2c8781cd5668b92705d8e1c522b09bdba8daf80d1e") -- -2 - Depot 4065190
+setManifestid(4065190, "357901510147596818", 153015)
