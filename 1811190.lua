92e595195635845a9f13b2c539175a6aec158491
qiao-lima <qiao-lima@github.com>
2026-05-04 00:12:46 +0800

    Manual Update 1811190

---
+++ 1811190.lua
@@ -0,0 +1,12 @@
+-- 1811190's Lua and Manifest Created by Hubcap Manifest
+-- The Bunker Notes
+-- Created: May 02, 2026 at 21:43:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1811190) -- The Bunker Notes
+-- MAIN APP DEPOTS
+addappid(1811191, 1, "a3a119052551b71ddb641bd13339fdaf369310346b279d5942503904c027027f") -- Depot 1811191
+setManifestid(1811191, "8226335049354428902", 767411296)
