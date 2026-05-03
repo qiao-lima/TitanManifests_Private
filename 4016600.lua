5d6e7627e0eef6924b66aa659b252b98ddc2af6b
qiao-lima <qiao-lima@github.com>
2026-05-04 00:12:50 +0800

    Manual Update 4016600

---
+++ 4016600.lua
@@ -0,0 +1,12 @@
+-- 4016600's Lua and Manifest Created by Hubcap Manifest
+-- 胞-胞实验室 Cell-Cell Lab
+-- Created: May 02, 2026 at 23:24:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4016600) -- 胞-胞实验室 Cell-Cell Lab
+-- MAIN APP DEPOTS
+addappid(4016601, 1, "f8bac8b88d33d3fd5314df0bec7eff7ffac902fc6ba4aa31e4f659b635f98c4d") -- Depot 4016601
+setManifestid(4016601, "1014207001045307325", 738081078)
