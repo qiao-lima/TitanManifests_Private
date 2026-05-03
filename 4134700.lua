1f711628c62c06b32ff8c7ff3d130cc17e3d0bfa
qiao-lima <qiao-lima@github.com>
2026-05-04 00:12:51 +0800

    Manual Update 4134700

---
+++ 4134700.lua
@@ -0,0 +1,12 @@
+-- 4134700's Lua and Manifest Created by Hubcap Manifest
+-- Memoria Arcana
+-- Created: May 02, 2026 at 23:25:18 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4134700) -- Memoria Arcana
+-- MAIN APP DEPOTS
+addappid(4134701, 1, "30d1f97057f1d82564ac96cf5a849c0e4ec5472669bbcb4c53a62fc2b439b7a7") -- Depot 4134701
+setManifestid(4134701, "8293541348494948481", 540053394)
