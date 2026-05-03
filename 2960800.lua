4cc1aae6e8b4487aea3335c1642a98089d67bacb
qiao-lima <qiao-lima@github.com>
2026-05-04 00:12:46 +0800

    Manual Update 2960800

---
+++ 2960800.lua
@@ -0,0 +1,12 @@
+-- 2960800's Lua and Manifest Created by Hubcap Manifest
+-- summer haze/glory days
+-- Created: May 03, 2026 at 12:09:03 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2960800) -- summer haze/glory days
+-- MAIN APP DEPOTS
+addappid(2960801, 1, "dd1d007da9421a5c6948d342f2a7c59f0528edd3483fc9273d7c6a27060780d1") -- Depot 2960801
+setManifestid(2960801, "2833950768125814563", 670082206)
