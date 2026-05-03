95f5830c807ac06bdef9a5dbe61181163cca9157
qiao-lima <qiao-lima@github.com>
2026-05-04 00:12:49 +0800

    Manual Update 3903740

---
+++ 3903740.lua
@@ -0,0 +1,15 @@
+-- 3903740's Lua and Manifest Created by Hubcap Manifest
+-- Nemunai
+-- Created: May 02, 2026 at 23:26:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(3903740) -- Nemunai
+-- MAIN APP DEPOTS
+addappid(3903741, 1, "d50f469bec952f62e0fcade2526d8668595128451c76999c6aee8d5ce60afa28") -- Depot 3903741
+setManifestid(3903741, "8878855036393988635", 1545332924)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4405140) -- Nemunai Wallpaper Pack
+addappid(4405150) -- Nemunai Digital Artbook
