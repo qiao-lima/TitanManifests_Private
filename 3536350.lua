48503ec54c44841847250a7993dfa5d202afe48a
qiao-lima <qiao-lima@github.com>
2026-05-04 00:12:48 +0800

    Manual Update 3536350

---
+++ 3536350.lua
@@ -0,0 +1,12 @@
+-- 3536350's Lua and Manifest Created by Hubcap Manifest
+-- My wife is a porn star
+-- Created: May 03, 2026 at 00:30:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3536350) -- My wife is a porn star
+-- MAIN APP DEPOTS
+addappid(3536351, 1, "adfee8bffe33299fb84c4a3b2fc0eef214ad787f375c1ab9aa8a54fa8952a555") -- Depot 3536351
+setManifestid(3536351, "4455582781277971023", 589636117)
