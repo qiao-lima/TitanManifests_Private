60c4e4c59b76d837c6888f597eb120d170714914
qiao-lima <qiao-lima@github.com>
2026-05-04 00:12:49 +0800

    Manual Update 3615670

---
+++ 3615670.lua
@@ -0,0 +1,12 @@
+-- 3615670's Lua and Manifest Created by Hubcap Manifest
+-- Geometry Factory
+-- Created: May 02, 2026 at 23:27:13 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3615670) -- Geometry Factory
+-- MAIN APP DEPOTS
+addappid(3615671, 1, "8cdd6fd60904e0db39b9257e6ce0432337232c2497e88abf65f109dc38f1becd") -- Depot 3615671
+setManifestid(3615671, "2525834335417355312", 305091898)
