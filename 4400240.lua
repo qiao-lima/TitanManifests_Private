42901ad0afb71bb8dc04a42e4a26680b657a1509
qiao-lima <qiao-lima@github.com>
2026-05-04 00:16:28 +0800

    Manual Update 4400240

---
+++ 4400240.lua
@@ -0,0 +1,12 @@
+-- 4400240's Lua and Manifest Created by Hubcap Manifest
+-- Munchy Cat
+-- Created: May 02, 2026 at 23:44:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4400240) -- Munchy Cat
+-- MAIN APP DEPOTS
+addappid(4400241, 1, "0e1c21189ba0b18551d4c18dc649fc900cce4930ec862ef8646c136650a1adec") -- Depot 4400241
+setManifestid(4400241, "543637427878835511", 254038072)
