e36f4fe6e56b114e87f3aa422f497817b314d238
qiao-lima <qiao-lima@github.com>
2026-05-04 00:12:50 +0800

    Manual Update 3957140

---
+++ 3957140.lua
@@ -0,0 +1,12 @@
+-- 3957140's Lua and Manifest Created by Hubcap Manifest
+-- 电音弹道
+-- Created: May 02, 2026 at 23:24:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3957140) -- 电音弹道
+-- MAIN APP DEPOTS
+addappid(3957141, 1, "607d155c9a674330983e2fc984fdd6e1e963518198a584190e02f2fd6d24db42") -- Depot 3957141
+setManifestid(3957141, "1527247934699346109", 313272696)
