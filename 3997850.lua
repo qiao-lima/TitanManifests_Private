123a89b0095045bfe82f33ac18bc58110b8ba24c
qiao-lima <qiao-lima@github.com>
2026-05-04 00:12:50 +0800

    Manual Update 3997850

---
+++ 3997850.lua
@@ -0,0 +1,12 @@
+-- 3997850's Lua and Manifest Created by Hubcap Manifest
+-- GODWAY
+-- Created: May 02, 2026 at 23:26:53 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3997850) -- GODWAY
+-- MAIN APP DEPOTS
+addappid(3997851, 1, "3c4ba5df6f47cca2bb21ccf362fb26fcbe8fc59618f2d1f1d2055113e44c9ae6") -- Depot 3997851
+setManifestid(3997851, "989852081355953074", 3329962316)
