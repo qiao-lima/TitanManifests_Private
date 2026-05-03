c0d2c43aea09515d65ad51c3556def6f4c14ec3b
qiao-lima <qiao-lima@github.com>
2026-05-04 00:12:49 +0800

    Manual Update 3755220

---
+++ 3755220.lua
@@ -0,0 +1,12 @@
+-- 3755220's Lua and Manifest Created by Hubcap Manifest
+-- Dead 4 Now: Bio-Weapon Simulator
+-- Created: May 03, 2026 at 09:18:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3755220) -- Dead 4 Now: Bio-Weapon Simulator
+-- MAIN APP DEPOTS
+addappid(3755221, 1, "a953bac740df8cf6c28a66e150233f97a9121b72082fcd2b00e7a945a98abd7a") -- Depot 3755221
+setManifestid(3755221, "4550860673952994465", 1843621611)
