87aaa52a01e5241d3a9785f25bd8b68dd25533c1
qiao-lima <qiao-lima@github.com>
2026-05-04 00:12:50 +0800

    Manual Update 3961740

---
+++ 3961740.lua
@@ -0,0 +1,12 @@
+-- 3961740's Lua and Manifest Created by Hubcap Manifest
+-- PaJinko
+-- Created: May 02, 2026 at 23:26:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3961740) -- PaJinko
+-- MAIN APP DEPOTS
+addappid(3961744, 1, "807e9d38715aa49312a7ed3550dec2feb2c1d027fb2e3476bd4e342f26f5b9c6") -- Depot 3961744
+setManifestid(3961744, "5606826157009957071", 270386862)
