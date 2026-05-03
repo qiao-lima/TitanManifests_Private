88d1d30e45f86fd232b2dfde7ba1ddf22b0369c7
qiao-lima <qiao-lima@github.com>
2026-05-04 00:12:46 +0800

    Manual Update 2821230

---
+++ 2821230.lua
@@ -0,0 +1,12 @@
+-- 2821230's Lua and Manifest Created by Hubcap Manifest
+-- ちょこナナ
+-- Created: May 02, 2026 at 23:46:37 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2821230) -- ちょこナナ
+-- MAIN APP DEPOTS
+addappid(2821231, 1, "a946ae4cffb5f154dabb4fcf2e10d1c772551cf24c9b2efdbe73334a1f5dfc3e") -- Depot 2821231
+setManifestid(2821231, "1220326551123699589", 10916037042)
