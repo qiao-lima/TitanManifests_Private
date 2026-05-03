9a15a3ac628caf67966006ea613fb8c577367ea4
qiao-lima <qiao-lima@github.com>
2026-05-04 00:12:46 +0800

    Manual Update 2141280

---
+++ 2141280.lua
@@ -0,0 +1,12 @@
+-- 2141280's Lua and Manifest Created by Hubcap Manifest
+-- GladiEATers
+-- Created: May 03, 2026 at 08:45:18 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2141280) -- GladiEATers
+-- MAIN APP DEPOTS
+addappid(2141281, 1, "3298549a9e4072332ca8281eea8f7339da48d6cc4735a635485a038c959e750e") -- Depot 2141281
+setManifestid(2141281, "7614874330659973128", 1311092151)
