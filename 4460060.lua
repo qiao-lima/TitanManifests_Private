1450b7d2c3f85dcb1d4faef7fc2874cbf7ccd10e
qiao-lima <qiao-lima@github.com>
2026-05-04 00:16:29 +0800

    Manual Update 4460060

---
+++ 4460060.lua
@@ -0,0 +1,18 @@
+-- 4460060's Lua and Manifest Created by Hubcap Manifest
+-- The Heresy of Gibur
+-- Created: May 02, 2026 at 23:26:18 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4460060) -- The Heresy of Gibur
+-- MAIN APP DEPOTS
+addappid(4460061, 1, "3f7a4c1bc3495f2f5ddaf3eff7330019b35a78cb8ccb6cada99fcbfbcec643a5") -- Depot 4460061
+setManifestid(4460061, "9071463521928276526", 1571097582)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
