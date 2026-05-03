07f12cefdd3485f8f455e4e86f9e7cc815eacb37
qiao-lima <qiao-lima@github.com>
2026-05-04 00:16:29 +0800

    Manual Update 4603130

---
+++ 4603130.lua
@@ -0,0 +1,20 @@
+-- 4603130's Lua and Manifest Created by Hubcap Manifest
+-- The Woman Above
+-- Created: May 03, 2026 at 07:16:29 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(4603130) -- The Woman Above
+-- MAIN APP DEPOTS
+addappid(4603131, 1, "9c0fa8484182a0124053b658afeec614b86dfe9d8f6481672042e66b181246a3") -- Depot 4603131
+setManifestid(4603131, "8667836453963064525", 6956339138)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 117381405)
