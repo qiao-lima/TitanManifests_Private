8ab8c69034e8bda07f7476dc0d1807f4652c90c9
qiao-lima <qiao-lima@github.com>
2026-05-04 00:12:51 +0800

    Manual Update 4320820

---
+++ 4320820.lua
@@ -0,0 +1,12 @@
+-- 4320820's Lua and Manifest Created by Hubcap Manifest
+-- The Curse 404
+-- Created: May 02, 2026 at 23:26:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4320820) -- The Curse 404
+-- MAIN APP DEPOTS
+addappid(4320821, 1, "1b2d0135251e3adc3455a81ac9b5a75e6fa36a159d92c3ae6366ea66f8029c6e") -- Depot 4320821
+setManifestid(4320821, "6825058442852304834", 8755695946)
