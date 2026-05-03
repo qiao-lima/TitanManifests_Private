bc2240a952ebe587bfba1edb260c3d55b280bfcc
qiao-lima <qiao-lima@github.com>
2026-05-04 00:12:49 +0800

    Manual Update 3774210

---
+++ 3774210.lua
@@ -0,0 +1,12 @@
+-- 3774210's Lua and Manifest Created by Hubcap Manifest
+-- Next Quintillionaire
+-- Created: May 02, 2026 at 19:53:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3774210) -- Next Quintillionaire
+-- MAIN APP DEPOTS
+addappid(3774211, 1, "f383bc681ad8ba9d7614f81538057acbfdccd5fd13118b21d3d11133046e3954") -- Depot 3774211
+setManifestid(3774211, "7077587342772916461", 92483960)
