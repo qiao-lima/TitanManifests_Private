0974290ec3e4975673b35ee9ab2f93792e079798
qiao-lima <qiao-lima@github.com>
2026-05-04 00:12:51 +0800

    Manual Update 4227700

---
+++ 4227700.lua
@@ -0,0 +1,12 @@
+-- 4227700's Lua and Manifest Created by Hubcap Manifest
+-- AI Desktop Pet
+-- Created: May 02, 2026 at 23:25:25 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4227700) -- AI Desktop Pet
+-- MAIN APP DEPOTS
+addappid(4227701, 1, "a737609714211441a53b57a5d4ec1388259a2d63fb9f3380e868688514b348a4") -- Depot 4227701
+setManifestid(4227701, "1427779536021163987", 22257237728)
