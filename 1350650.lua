17f41d82f31fa9cbb388c53bd8cce37d038a8e6b
qiao-lima <qiao-lima@github.com>
2026-05-04 10:39:16 +0800

    Manual Update 1350650

---
+++ 1350650.lua
@@ -0,0 +1,15 @@
+-- 1350650's Lua and Manifest Created by Hubcap Manifest
+-- FreshWomen - Season 1
+-- Created: February 26, 2026 at 23:36:11 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(1350650, 1, "43936ce41f7c79d82419d8e8254aabfb329bb4857b691e80463244d4d38c2fe8") -- FreshWomen - Season 1
+-- MAIN APP DEPOTS
+addappid(1350651, 1, "cf1b50f8d8ab2dbf3f7cda0c9962aaf6ecaf1dfa64dd60809e3b3c2dcf94ac31") -- Depot 1350651
+setManifestid(1350651, "267916864789440876", 3759588313)
+-- EXCLUDED DLCS:
+-- UNRELEASED DLCS (COMMENTED OUT)
+-- addappid(2691310) -- FreshWomen - Season 2 (unreleased)
