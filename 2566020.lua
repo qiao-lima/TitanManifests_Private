9b27bf1db7455daf507c73316aa8169d61e05d66
qiao-lima <qiao-lima@github.com>
2026-05-04 21:38:07 +0800

    Manual Update 2566020

---
+++ 2566020.lua
@@ -0,0 +1,12 @@
+-- 2566020's Lua and Manifest Created by Hubcap Manifest
+-- Raindrop Sprinters
+-- Created: May 04, 2026 at 08:34:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2566020, 1, "ccd199c2d4a43f417e6dc3c48fe9c6205989817301e1110e5aefb6aab7860ae6") -- Raindrop Sprinters
+-- MAIN APP DEPOTS
+addappid(2566021, 1, "18ce16edf5a81de03fd4b90f43011fcb2a60c0074c087856571e788d39e228ec") -- Depot 2566021
+setManifestid(2566021, "2605377526233301925", 101699414)
