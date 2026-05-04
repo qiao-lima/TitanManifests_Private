b79ec8b35de97f0ab49c5d00b50158128208c290
qiao-lima <qiao-lima@github.com>
2026-05-04 21:38:07 +0800

    Manual Update 2672770

---
+++ 2672770.lua
@@ -0,0 +1,16 @@
+-- 2672770's Lua and Manifest Created by Hubcap Manifest
+-- Hexle
+-- Created: May 04, 2026 at 05:43:13 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2672770) -- Hexle
+-- MAIN APP DEPOTS
+addappid(2672771, 1, "1f72400da4c1201647f444e44264aefecf0ba75bc4327e973bac23d8c33b4dee") -- Depot 2672771
+setManifestid(2672771, "7605513483378729965", 68661174)
+addappid(2672772, 1, "47c8da87f69ba06d7c78661f1e25ed862d24bf39651edac215bb2ba636443000") -- Depot 2672772
+setManifestid(2672772, "601301653158615508", 67030074)
+addappid(2672773, 1, "a727d9dadf544ea4dcbf87d6a1fa3a7f3aea76ad0e5191755420f316a7963eb5") -- Depot 2672773
+setManifestid(2672773, "8326240777939422804", 70167100)
