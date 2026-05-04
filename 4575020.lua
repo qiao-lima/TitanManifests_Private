86a3ffe133a5f2748516d336dc0c892869bb2ded
qiao-lima <qiao-lima@github.com>
2026-05-04 21:38:10 +0800

    Manual Update 4575020

---
+++ 4575020.lua
@@ -0,0 +1,12 @@
+-- 4575020's Lua and Manifest Created by Hubcap Manifest
+-- Harmful Attachment
+-- Created: May 03, 2026 at 23:29:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4575020) -- Harmful Attachment
+-- MAIN APP DEPOTS
+addappid(4575021, 1, "2ebd9b7d98f918d2750e24dcde2ce4e341952ca74cc8b418f3d9ab3aa29891a5") -- Depot 4575021
+setManifestid(4575021, "826796027340687874", 1097332201)
