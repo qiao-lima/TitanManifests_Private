6d47ba7cc8c17794aca8b5d6c5080848fdc7bd3d
qiao-lima <qiao-lima@github.com>
2026-05-04 21:38:06 +0800

    Manual Update 1743930

---
+++ 1743930.lua
@@ -0,0 +1,12 @@
+-- 1743930's Lua and Manifest Created by Hubcap Manifest
+-- Froggo Swing 'n Grapple
+-- Created: May 04, 2026 at 05:39:36 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1743930) -- Froggo Swing 'n Grapple
+-- MAIN APP DEPOTS
+addappid(1743931, 1, "6d3f1dd03832d75636cae05d377180ccc9da5d9eb86cef9188532be698b7a6b2") -- Depot 1743931
+setManifestid(1743931, "8906384377604336452", 327723366)
