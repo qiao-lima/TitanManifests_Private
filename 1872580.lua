edd486099bc8e58f352fae4b63ae5d08ca4ffd0c
qiao-lima <qiao-lima@github.com>
2026-05-04 21:38:06 +0800

    Manual Update 1872580

---
+++ 1872580.lua
@@ -0,0 +1,17 @@
+-- 1872580's Lua and Manifest Created by Hubcap Manifest
+-- Ghostly Moans
+-- Created: May 03, 2026 at 21:17:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(1872580, 1, "6b1e6483c0077d8d59cd48a57c73c640d8259f784908349e8cfaf5ff2cb0b9b2") -- Ghostly Moans
+-- MAIN APP DEPOTS
+addappid(1872581, 1, "713c89a25f1837652cd0f50d808127725a80c7560ae11f319028b657f916e572") -- Depot 1872581
+setManifestid(1872581, "136258150507053022", 503440220)
+-- DLCS WITH DEDICATED DEPOTS
+-- Ghostly Moans - Guide (AppID: 2276410)
+addappid(2276410)
+addappid(1872582, 1, "88ba3dc29fdfa1f69263afef8bda63bf9f770fc8a1fcfe1f2f6ea6f4f73bed69") -- Ghostly Moans - Guide - Depot 1872582
+setManifestid(1872582, "5094797674241503208", 972878)
