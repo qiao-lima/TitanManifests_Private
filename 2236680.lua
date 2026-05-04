030d2d87f898d54e5ce1882ce6db880c336307f2
qiao-lima <qiao-lima@github.com>
2026-05-04 21:38:07 +0800

    Manual Update 2236680

---
+++ 2236680.lua
@@ -0,0 +1,16 @@
+-- 2236680's Lua and Manifest Created by Hubcap Manifest
+-- Llamasoft: The Jeff Minter Story
+-- Created: May 04, 2026 at 05:40:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2236680) -- Llamasoft: The Jeff Minter Story
+-- MAIN APP DEPOTS
+addappid(2236681, 1, "a8de0641045abd0e1b99d833af1379c2726a65f4fc812555ce173612201c1c48") -- Depot 2236681
+setManifestid(2236681, "4246170371077147939", 4021722388)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
