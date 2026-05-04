a05cf9fbb1dbd3ac88067fac7132b3dbbf1cc4ec
qiao-lima <qiao-lima@github.com>
2026-05-04 21:38:09 +0800

    Manual Update 4488780

---
+++ 4488780.lua
@@ -0,0 +1,16 @@
+-- 4488780's Lua and Manifest Created by Hubcap Manifest
+-- Heave-Ho: Uphill
+-- Created: May 04, 2026 at 05:20:19 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4488780) -- Heave-Ho: Uphill
+-- MAIN APP DEPOTS
+addappid(4488782, 1, "461010eb2d96b1b08ff0dd03d10f7873be3af3508ef4fbdd72814cc34292d596") -- Depot 4488782
+setManifestid(4488782, "3662773379134344458", 936091661)
+addappid(4488783, 1, "4dc213bf38074aa00d68b7f733a6b71df86ea33c79a51c42625a147aa3bc8fad") -- Depot 4488783
+setManifestid(4488783, "571970824720006581", 889211278)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4488781) -- Depot 4488781 (empty depot)
