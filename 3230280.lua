89b1bb6f7873538b8154d475c4dd33a440e9d3da
qiao-lima <qiao-lima@github.com>
2026-05-04 21:38:08 +0800

    Manual Update 3230280

---
+++ 3230280.lua
@@ -0,0 +1,16 @@
+-- 3230280's Lua and Manifest Created by Hubcap Manifest
+-- Horizons: The End Of Words
+-- Created: May 04, 2026 at 05:44:03 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3230280) -- Horizons: The End Of Words
+-- MAIN APP DEPOTS
+addappid(3230281, 1, "ad234bb30eadc14715ff2d9eb0107ee0f74576303290ff146d8521e67d43220b") -- Depot 3230281
+setManifestid(3230281, "2940371881153444427", 124957575)
+addappid(3230282, 1, "23dd7a788e92b03f241a44a62c2c3884d3de134dc1655829f94835091217d8cd") -- Depot 3230282
+setManifestid(3230282, "859922383723807407", 123043360)
+addappid(3230283, 1, "980dad93dcc115bfab1542fe9ef0ea28727c47a2d0641c94a1f3e72c8c42f1b3") -- Depot 3230283
+setManifestid(3230283, "7241018548538539004", 126516366)
