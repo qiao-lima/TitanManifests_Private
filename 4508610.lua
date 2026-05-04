b24fadbf98ddec3cb09abcc5552139baf8ff2b81
qiao-lima <qiao-lima@github.com>
2026-05-04 10:39:16 +0800

    Manual Update 4508610

---
+++ 4508610.lua
@@ -0,0 +1,12 @@
+-- 4508610's Lua and Manifest Created by Hubcap Manifest
+-- 翻翻卡
+-- Created: May 03, 2026 at 20:34:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4508610) -- 翻翻卡
+-- MAIN APP DEPOTS
+addappid(4508611, 1, "131a25330e5e569a0ae3a6052e0f689defdf8cb83ff24d6e1ff461712199bc34") -- Depot 4508611
+setManifestid(4508611, "1724667737550579606", 1084540373)
