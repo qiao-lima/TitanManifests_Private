9f7da07b2e4f056dd359a4557dbd547af6e017da
qiao-lima <qiao-lima@github.com>
2026-05-04 21:38:08 +0800

    Manual Update 2849010

---
+++ 2849010.lua
@@ -0,0 +1,12 @@
+-- 2849010's Lua and Manifest Created by Hubcap Manifest
+-- Prehistoric Gal
+-- Created: May 04, 2026 at 05:43:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2849010) -- Prehistoric Gal
+-- MAIN APP DEPOTS
+addappid(2849011, 1, "028275aa98e6fc3fc7356dc36b2925c2ea25348e11800dbb1dae1a65d9eac0e1") -- Depot 2849011
+setManifestid(2849011, "8496157262733698189", 24409258)
