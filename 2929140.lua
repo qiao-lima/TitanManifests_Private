a5dac5bbbd6ad43db37b830f099afd6438c701d5
qiao-lima <qiao-lima@github.com>
2026-05-04 10:39:16 +0800

    Manual Update 2929140

---
+++ 2929140.lua
@@ -0,0 +1,12 @@
+-- 2929140's Lua and Manifest Created by Hubcap Manifest
+-- Ataegina Act I
+-- Created: May 03, 2026 at 11:44:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2929140) -- Ataegina Act I
+-- MAIN APP DEPOTS
+addappid(2929143, 1, "007d93289ac0cf899cf8c695611aed5b3d9e04720e6c8e56d93bd4a37fded1c7") -- Depot 2929143
+setManifestid(2929143, "5546971129900218360", 8805601920)
