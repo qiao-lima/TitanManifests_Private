6c373f5044fcbb8a92ce83cb4a0e856e74a232c9
qiao-lima <qiao-lima@github.com>
2026-05-04 10:39:16 +0800

    Manual Update 1865100

---
+++ 1865100.lua
@@ -0,0 +1,14 @@
+-- 1865100's Lua and Manifest Created by Hubcap Manifest
+-- Pengu Never Left
+-- Created: May 03, 2026 at 11:36:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1865100) -- Pengu Never Left
+-- MAIN APP DEPOTS
+addappid(1865101, 1, "27a2534d38ad6522a2a80e66f558bdbed139c38025a03357d4bf57cf4c5fed13") -- Depot 1865101
+setManifestid(1865101, "8757984463984651635", 136830840)
+addappid(1865103, 1, "a451f6f890c3667f22ebbf500975ab960f470cb4dd0bf9808f836ccba32ba43a") -- Depot 1865103
+setManifestid(1865103, "8817615032851676681", 138541432)
