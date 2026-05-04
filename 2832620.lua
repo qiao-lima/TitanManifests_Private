7bd6747dc2e771b7fd4e67cf5352fa2e1f06727b
qiao-lima <qiao-lima@github.com>
2026-05-04 21:38:08 +0800

    Manual Update 2832620

---
+++ 2832620.lua
@@ -0,0 +1,16 @@
+-- 2832620's Lua and Manifest Created by Hubcap Manifest
+-- Stray Shot
+-- Created: May 04, 2026 at 05:43:36 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2832620) -- Stray Shot
+-- MAIN APP DEPOTS
+addappid(2832621, 1, "6fc5971441097d64c583d8a30d04b2354758536b67fc7de021c3c3d5c0f28e48") -- Depot 2832621
+setManifestid(2832621, "1042752499118399441", 31524858)
+addappid(2832622, 1, "c8515dc02807054f9ba2036effc9fe2169468fc5bd76ca852929e67518220e36") -- Depot 2832622
+setManifestid(2832622, "4964609795841685755", 30414520)
+addappid(2832623, 1, "99c8d58f9280f19b29772dfe26419fad4e32d466793e828e2c7413e24f301978") -- Depot 2832623
+setManifestid(2832623, "1483254590210973748", 33176479)
