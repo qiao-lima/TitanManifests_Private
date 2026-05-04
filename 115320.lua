b12672b7c6914e180c28993e6c6b08837fc3f53a
qiao-lima <qiao-lima@github.com>
2026-05-04 10:39:15 +0800

    Manual Update 115320

---
+++ 115320.lua
@@ -0,0 +1,32 @@
+-- 115320's Lua and Manifest Created by Hubcap Manifest
+-- PROTOTYPE 2
+-- Created: January 19, 2026 at 05:41:03 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 9
+-- Total DLCs: 1
+-- Blacklisted Depots: 1
+--   Depot 115330: Blacklisted
+
+-- MAIN APPLICATION
+addappid(115320, 1, "786d15c2e12214478dcde6bc073bee2f9f8b0844631217e137d9a9c31f2ed4e2") -- PROTOTYPE 2
+-- MAIN APP DEPOTS
+addappid(115322, 1, "3d7689509f18f07324101c3ec7b69ea574d0e19850b91bc6322bb0f5b8d24212") -- Prototype 2 Content
+setManifestid(115322, "9048180919412686294", 8838171098)
+addappid(115321, 1, "e3194adebf12413cfa342ecde1b0ddc66bd81adfc66ad17f2616d2f0a5f02510") -- Prototype 2 Binaries
+setManifestid(115321, "463566073465694220", 18758656)
+addappid(115323, 1, "c7512012479b7f2eb36c5af3e0721d6c7a1a7b30fbee18e13a27adba94b5550f") -- Prototype 2 english
+setManifestid(115323, "7756741017743835184", 1965277690)
+addappid(115324, 1, "7d8ee96f625a82c8191e46069f244733ac12101c210caa437169a3eb8e41e54d") -- Prototype 2 french
+setManifestid(115324, "5304196080885964704", 1842118514)
+addappid(115325, 1, "67faa76bc924012913caab4abb9604f61318b4bd942dc6485b124217793dd3e3") -- Prototype 2 italian
+setManifestid(115325, "1644767041622555488", 1981779714)
+addappid(115326, 1, "2a0ee2ec61ccce4d4a8647f2f5d394ad54ed4ff72dabdcc426a330bfd0e1afa5") -- Prototype 2 german
+setManifestid(115326, "3833695619289562819", 1965277690)
+addappid(115327, 1, "9de5db50458a6f9a8c01f5562172063707a1242812609bdc73335a3547fc8d2b") -- Prototype 2 spanish
+setManifestid(115327, "2166545564719408452", 1950402954)
+addappid(115328, 1, "1192afe75a30de751af526c446152591cb02640f05bb680448d8399e36bec2b8") -- Prototype 2 Polish
+setManifestid(115328, "3095157929607387828", 1965019642)
+addappid(115329, 1, "706926bc2419db4262e8834ca365a012ea9c1b5abc645192a5f1fc57e6451b30") -- Prototype 2 Russian
+setManifestid(115329, "6916332080060994995", 2104674574)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(115340) -- Prototype 2 RADNET Access Pack
