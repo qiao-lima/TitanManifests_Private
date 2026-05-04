08a114ba35b2d29deebd4e363af7c5b80d3ffd38
qiao-lima <qiao-lima@github.com>
2026-05-04 21:38:09 +0800

    Manual Update 4303240

---
+++ 4303240.lua
@@ -0,0 +1,14 @@
+-- 4303240's Lua and Manifest Created by Hubcap Manifest
+-- SupermarCAT
+-- Created: May 04, 2026 at 05:16:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4303240) -- SupermarCAT
+-- MAIN APP DEPOTS
+addappid(4303241, 1, "fd49f814a2c7c78a34f875a49e64f470bf19003f02ccfc8a6af9d354f2dd9c3e") -- Depot 4303241
+setManifestid(4303241, "318347065934342239", 182691992)
+addappid(4303242, 1, "1eaada18eb54a18b0d23737e9b9d2ae7f10d844835a7388192ba3faae66da2fe") -- Depot 4303242
+setManifestid(4303242, "1381437274987574892", 149197512)
