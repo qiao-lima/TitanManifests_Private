01b7a06fd448653f1918fc6bbc3b7235e0980be8
qiao-lima <qiao-lima@github.com>
2026-05-04 21:38:06 +0800

    Manual Update 1371570

---
+++ 1371570.lua
@@ -0,0 +1,14 @@
+-- 1371570's Lua and Manifest Created by Hubcap Manifest
+-- A Fox Tale
+-- Created: May 04, 2026 at 08:23:53 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1371570, 1, "0fba912d72345c35b941fa8cf55a42ab9fef8c8263613fe1cfc34abe8aa47876") -- A Fox Tale
+-- MAIN APP DEPOTS
+addappid(1371571, 1, "47214c99a110a2d8139e3f783511747e856088f25766d1dca0010ad730f252c7") -- Depot 1371571
+setManifestid(1371571, "81299254466509725", 874173026)
+addappid(1371572, 1, "ff6d1b265c98dc1cd2bf93f73e9d466136ccebbc74623af63417df4827d79935") -- Depot 1371572
+setManifestid(1371572, "5040440247828610482", 867554158)
