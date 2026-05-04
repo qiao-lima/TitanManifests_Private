302ef4f97bc704fe08a105cc67be64f8bfcf6bc7
qiao-lima <qiao-lima@github.com>
2026-05-04 21:38:08 +0800

    Manual Update 3169660

---
+++ 3169660.lua
@@ -0,0 +1,12 @@
+-- 3169660's Lua and Manifest Created by Hubcap Manifest
+-- Looney Landers
+-- Created: May 04, 2026 at 05:43:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3169660) -- Looney Landers
+-- MAIN APP DEPOTS
+addappid(3169661, 1, "7c552d04de6bd61490df02f06752b48a74e8fbd066094f5674ddcb48a98d991b") -- Depot 3169661
+setManifestid(3169661, "2235111211353049318", 32966347)
