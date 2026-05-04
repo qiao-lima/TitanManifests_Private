f490e2c774453dca0f85aec1436d2459f9e9ea9a
qiao-lima <qiao-lima@github.com>
2026-05-04 21:38:09 +0800

    Manual Update 3262600

---
+++ 3262600.lua
@@ -0,0 +1,14 @@
+-- 3262600's Lua and Manifest Created by Hubcap Manifest
+-- Bubble Frog DX
+-- Created: May 04, 2026 at 08:29:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3262600, 1, "70dac67fd4153d375128c1f4658feb260fc52ef6dea43df021b9f4d79dbe4a6d") -- Bubble Frog DX
+-- MAIN APP DEPOTS
+addappid(3262601, 1, "0b5d76c10e77ca20555bc8bb200ed50932332fe1ff8017992de43c4a3df4721e") -- Depot 3262601
+setManifestid(3262601, "5698873409066667582", 340508072)
+addappid(3262602, 1, "c68ad2b6d3f32bf490d358811ae96ec3e9360b53fba0931e46372dd4ff6df7cc") -- Depot 3262602
+setManifestid(3262602, "2245235595855814736", 297385277)
