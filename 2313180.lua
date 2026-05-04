34dcb5c4978935d9187c9b3fe659ebe06e2252c9
qiao-lima <qiao-lima@github.com>
2026-05-04 21:38:07 +0800

    Manual Update 2313180

---
+++ 2313180.lua
@@ -0,0 +1,14 @@
+-- 2313180's Lua and Manifest Created by Hubcap Manifest
+-- El Panadero
+-- Created: May 04, 2026 at 08:30:57 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2313180, 1, "d0c450d1a037f7a95cdb52668803349dcacd66ccd7c56aab71008a6f641c8b92") -- El Panadero
+-- MAIN APP DEPOTS
+addappid(2313181, 1, "3271e1258abf1cb98adbd424084bb7cdc99bc6e44aec392bfc1513dc63bbc73a") -- Depot 2313181
+setManifestid(2313181, "8400947303353306292", 658971000)
+addappid(2313182, 1, "19febb449bde386f93d00b2012ffa6ddcb5da30df6a93ef23436459cd66e046c") -- Depot 2313182
+setManifestid(2313182, "674612844737234551", 662387180)
