3608121443472896fd980f85d798f216b8623230
qiao-lima <qiao-lima@github.com>
2026-05-05 10:40:01 +0800

    Manual Update 2964090

---
+++ 2964090.lua
@@ -0,0 +1,23 @@
+-- 2964090's Lua and Manifest Created by Hubcap Manifest
+-- World of Warships: Legends
+-- Created: May 04, 2026 at 11:40:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 6
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2964090) -- World of Warships: Legends
+-- MAIN APP DEPOTS
+addappid(2964091, 1, "81d9633ceae53d1a55f43a061c5d0ecb40e363c50f9fedf4fc176b93c71e2057") -- Depot 2964091
+setManifestid(2964091, "4039434238228592393", 53203554554)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4596240) -- World of Warships Legends  Prince of Prussia
+addappid(4604950) -- World of Warships Legends  The Ocean Runner
+addappid(4626910) -- World of Warships Legends  Mighty Starter Pack
+addappid(4636090) -- World of Warships Legends  Pilgrim From Devon
+addappid(4640340) -- World of Warships Legends  a Tale of Bravery
+addappid(4652620) -- World of Warships Legends  Dawns Guardian
