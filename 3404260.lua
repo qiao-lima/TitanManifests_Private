74bac86b38bf3b9de3af2f14548e696d7e6325cc
qiao-lima <qiao-lima@github.com>
2026-05-06 07:36:56 +0800

    Manual Update 3404260

---
+++ 3404260.lua
@@ -0,0 +1,18 @@
+-- 3404260's Lua and Manifest Created by Hubcap Manifest
+-- Dead as Disco
+-- Created: May 05, 2026 at 11:07:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3404260) -- Dead as Disco
+-- MAIN APP DEPOTS
+addappid(3404261, 1, "973bb66b7d1dcddebad49a40c9bad583fb090e27d3df4fc57fea8b9732ce6680") -- Depot 3404261
+setManifestid(3404261, "3421426437893529435", 6307447654)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
