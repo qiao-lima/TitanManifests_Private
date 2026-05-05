6c05f675c8d2358523500c1f19d71c12c8b26b7e
qiao-lima <qiao-lima@github.com>
2026-05-05 10:34:49 +0800

    Manual Update 3274880

---
+++ 3274880.lua
@@ -0,0 +1,14 @@
+-- 3274880's Lua and Manifest Created by Hubcap Manifest
+-- Rollick N' Roll
+-- Created: April 24, 2026 at 10:01:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3274880) -- Rollick N' Roll
+-- MAIN APP DEPOTS
+addappid(3274881, 1, "913bf1bea4e8e7e27c2bcb4950e996e260447b3e835726ad408560bae4a4c72a") -- Depot 3274881
+setManifestid(3274881, "8726923779755019896", 507036137)
+addappid(3274882, 1, "0d53f17ed5925d722a340a5fa5dfb52a1c8c77077b8812e56ce428824799a892") -- Depot 3274882
+setManifestid(3274882, "209895369253338740", 598840102)
