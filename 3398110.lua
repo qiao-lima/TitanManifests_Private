efa5adb72ad12bffb6b4584d372b39d035ce5d4a
qiao-lima <qiao-lima@github.com>
2026-05-05 19:21:03 +0800

    Manual Update 3398110

---
+++ 3398110.lua
@@ -0,0 +1,18 @@
+-- 3398110's Lua and Manifest Created by Hubcap Manifest
+-- The Walking Trade
+-- Created: May 03, 2026 at 05:30:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3398110, 1, "2efcc9d4df6bd08ef96f67ee00716b91c2ebb906a9f9001273d8d1f68e1f4ce3") -- The Walking Trade
+-- MAIN APP DEPOTS
+addappid(3398112, 1, "cad843e27a0eace2f72e865fff2696d8e59ebfeffb2fe12eb8833cb1f9cc2d7e") -- Depot 3398112
+setManifestid(3398112, "2188112702377665553", 2727164058)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
