97b15accaaba0ccfa0b91ddb035bed5b1338009d
qiao-lima <qiao-lima@github.com>
2026-05-06 07:36:58 +0800

    Manual Update 4552380

---
+++ 4552380.lua
@@ -0,0 +1,12 @@
+-- 4552380's Lua and Manifest Created by Hubcap Manifest
+-- 說好只是打工，為什麼大小姐都在撩我
+-- Created: May 05, 2026 at 04:38:59 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4552380) -- 說好只是打工，為什麼大小姐都在撩我
+-- MAIN APP DEPOTS
+addappid(4552381, 1, "48fcef3e623a86aa3797c96cae7f2d529944ce489fcca66d3445a501919817f7") -- Depot 4552381
+setManifestid(4552381, "1820952161791869369", 881521567)
