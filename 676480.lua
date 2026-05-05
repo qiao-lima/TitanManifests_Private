f06cfee50843899f65756374b9ceadbcbd82bd1b
qiao-lima <qiao-lima@github.com>
2026-05-06 07:37:19 +0800

    Manual Update 676480

---
+++ 676480.lua
@@ -0,0 +1,16 @@
+-- 676480's Lua and Manifest Created by Hubcap Manifest
+-- Jettomero
+-- Created: May 04, 2026 at 12:25:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(676480, 1, "d0d90a4646b63ab0ce0aff66930c39118522099befc52e077001b02dd52bd78a") -- Jettomero
+-- MAIN APP DEPOTS
+addappid(676481, 1, "02dcbd79b18216fd948c1d2b4f6b0d16ac6fa937dfc8d56b1e198888f645cfb3") -- Depot 676481
+setManifestid(676481, "197907540544061969", 258758184)
+addappid(676482, 1, "19352d92c9b772772acdf5d850ad695e7636b48c515cff7393e10e65a067c0d2") -- Depot 676482
+setManifestid(676482, "7353994129402980651", 238643478)
+addappid(676483, 1, "c9f6ec83979b62da93d7cd6dac94f3a39259f0006ea25526abb38db80b9108dd") -- Depot 676483
+setManifestid(676483, "7049092022463931311", 292437016)
