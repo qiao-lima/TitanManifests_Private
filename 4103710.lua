b9ddb53c6e0e8fc77bb73a116a3616d7751912ac
qiao-lima <qiao-lima@github.com>
2026-05-05 10:40:02 +0800

    Manual Update 4103710

---
+++ 4103710.lua
@@ -0,0 +1,12 @@
+-- 4103710's Lua and Manifest Created by Hubcap Manifest
+-- Amazons vs Zombies
+-- Created: May 04, 2026 at 08:46:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4103710) -- Amazons vs Zombies
+-- MAIN APP DEPOTS
+addappid(4103711, 1, "c055a1274df62da1ea6f8203cf62bafbbb0725c8e2935d47d3b5039c6b2a9483") -- Depot 4103711
+setManifestid(4103711, "6568884324456934053", 2075727189)
