b6e7c08ce8d2e9469e479463d4df60526f4e6c47
qiao-lima <qiao-lima@github.com>
2026-05-05 10:40:01 +0800

    Manual Update 3214480

---
+++ 3214480.lua
@@ -0,0 +1,13 @@
+-- 3214480's Lua and Manifest Created by Hubcap Manifest
+-- Prime Monster
+-- Created: May 04, 2026 at 11:32:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3214480) -- Prime Monster
+addtoken(3214480, "6822190370146535122")
+-- MAIN APP DEPOTS
+addappid(3214481, 1, "7db9e40e95d69dc43d409ff261670d1d2e6468130b65826cda60d09168eb5260") -- Depot 3214481
+setManifestid(3214481, "6608290626917449272", 1721751964)
