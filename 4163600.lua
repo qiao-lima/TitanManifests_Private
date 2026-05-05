47a1a0ff54a42be48cdb78fb52701fbb06b25b2f
qiao-lima <qiao-lima@github.com>
2026-05-06 07:36:58 +0800

    Manual Update 4163600

---
+++ 4163600.lua
@@ -0,0 +1,12 @@
+-- 4163600's Lua and Manifest Created by Hubcap Manifest
+-- Galactic Overthrow
+-- Created: May 05, 2026 at 19:34:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4163600) -- Galactic Overthrow
+-- MAIN APP DEPOTS
+addappid(4163601, 1, "8661756bed3dcb1cbf3883be93ff1830fb48c12cae5e0de43fd6a4c55bb73412") -- Depot 4163601
+setManifestid(4163601, "2226877403718243744", 337320538)
