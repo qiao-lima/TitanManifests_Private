47bc1037897977ab5bf8f2b2a85331306b26805e
qiao-lima <qiao-lima@github.com>
2026-05-05 10:40:02 +0800

    Manual Update 4553070

---
+++ 4553070.lua
@@ -0,0 +1,12 @@
+-- 4553070's Lua and Manifest Created by Hubcap Manifest
+-- Almost Castaways
+-- Created: May 04, 2026 at 06:32:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4553070, 1, "f1f2ca6c62b69220f58f633b70babb4e6480a0698434157ce5e3dda8e79f8eb6") -- Almost Castaways
+-- MAIN APP DEPOTS
+addappid(4553071, 1, "2538f5402cd6b18acf0769adca50371c8b08ec7f8ffdb197881613ac6d188c9b") -- Depot 4553071
+setManifestid(4553071, "9120785510888308428", 2021042792)
