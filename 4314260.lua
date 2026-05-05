0f8e8a5c1b2fb3c9c6b775c8be6f329ecf380678
qiao-lima <qiao-lima@github.com>
2026-05-06 07:36:58 +0800

    Manual Update 4314260

---
+++ 4314260.lua
@@ -0,0 +1,16 @@
+-- 4314260's Lua and Manifest Created by Hubcap Manifest
+-- A Game About Making A Planet
+-- Created: May 05, 2026 at 10:51:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(4314260, 1, "d1096883e6f11768329fa08774da895aa1c6381b6a8201fa6f060c03ad2d7884") -- A Game About Making A Planet
+-- MAIN APP DEPOTS
+addappid(4314261, 1, "99d5fb7312714109c2d6a72a6455d6013178471c174848b1828e84525743516b") -- Depot 4314261
+setManifestid(4314261, "3545169341499115648", 304440502)
+-- SHARED DEPOTS (from other apps)
+addappid(4662060, 1, "0c18191215f4151240c3cbbb78cf317cf68e5956648c1c548a5ac39d4d77b93a") -- Depot 4662060 (Shared from App 4662060)
+setManifestid(4662060, "2154250911378427590", 304440502)
