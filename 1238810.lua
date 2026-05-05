3473332a14dbb41847a7f316b3406dc12183282e
qiao-lima <qiao-lima@github.com>
2026-05-05 10:34:49 +0800

    Manual Update 1238810

---
+++ 1238810.lua
@@ -0,0 +1,59 @@
+-- 1238810's Lua and Manifest Created by Hubcap Manifest
+-- Battlefield™ V
+-- Created: May 01, 2026 at 15:00:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 18
+-- Total DLCs: 7
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(1238810, 1, "073bd77784efe9fd414dede7e20c42cce559ada8951ae00c9f01bc496490de88") -- Battlefield™ V
+-- MAIN APP DEPOTS
+addappid(1238811, 1, "7271dde6918406c6d85c6bb7a4b1445e0c39c9c2a10a6a4fc40cc1c5034c774a") -- Battlefield V - Content
+setManifestid(1238811, "6721650049133320511", 98014484970)
+addappid(1238812, 1, "0523a08b011e472da981321449f4dd0fddf89e2e021c806ee7e32504437c100e") -- Battlefield V - en_US
+setManifestid(1238812, "1300094161914919522", 118568)
+addappid(1238813, 1, "9828e66572dbb34fdb84625660c194a7d805a0665d88d4691aa85740d62ea718") -- Battlefield V - fr_FR
+setManifestid(1238813, "8474848118184594486", 128250)
+addappid(1238814, 1, "a72306efce63de26f5086cb44339d50331891f072fda6c686e919496c41211ea") -- Battlefield V - de_DE
+setManifestid(1238814, "5981482195974506984", 113621)
+addappid(1238815, 1, "bd0096b1350ca6efbb761a6fa00bc13622b14c0b7d9ea0a9442456190ccbcb2f") -- Battlefield V - es_ES
+setManifestid(1238815, "1497103128603889123", 133510)
+addappid(1238816, 1, "81a7d6f82a3e0fef1ccf48dcb4cd52b3c9d1b403f6ca535a913ec763b8bbdc76") -- Battlefield V - it_IT
+setManifestid(1238816, "9077088086266899877", 123917)
+addappid(1238817, 1, "ed0fd93b52120ed0750d84399c54b6c8df2bd092e427b3a25f3681dc3755507a") -- Battlefield V - ru_RU
+setManifestid(1238817, "8039174010806844919", 200393)
+addappid(1238818, 1, "419aedfbebf515490a1a8da77df1fc650cc074ea73f24c9413123942cb8343ff") -- Battlefield V - pl_PL
+setManifestid(1238818, "6061542863892843574", 126522)
+addappid(1238819, 1, "54fc167de029c497af84d15e1267a8783eab472f9cd3951da1d6c5836daf6653") -- Battlefield V - ja_JP
+setManifestid(1238819, "3407266195047573380", 206967)
+addappid(1254583, 1, "8772e7d481fae4c8fc944cb674af3388208d28e061165f14ec233693d517651f") -- Battlefield V - zh_TW
+setManifestid(1254583, "4010359096937541454", 172546)
+addappid(1254584, 1, "cbf97b8608a9ac634d01aeace9b029ba74ee7fa015499a1cb2c4210822fb4983") -- Battlefield V - zh_CN
+setManifestid(1254584, "351324621064682329", 183302)
+addappid(1254585, 1, "b3ce07efc7b1c7ce7fe09e2ef9a563cec7f7972be95e3483f9773a4c5d1da39b") -- Battlefield V - pt_BR
+setManifestid(1254585, "1604421096638849752", 120099)
+addappid(1254586, 1, "2b59a1686c87c60ba2b9028cdd3457a86b1872ceae988a2929cc2b5a47173970") -- Battlefield V - es_MX
+setManifestid(1254586, "4972677978363761993", 128426)
+addappid(1254587, 1, "59a3a36a5add4b37b4f21e266622d7c4a88d7e512cf0bc8bdffed4866727743b") -- Battlefield V - ar_SA
+setManifestid(1254587, "2709673409888802550", 279549)
+addappid(1254588, 1, "cfee412ddf5154c1ddf30ed4cea51d36180406740e7ea8d313d15ba175839b32") -- Battlefield V - ko_KR
+setManifestid(1254588, "8679391186132147187", 399612)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(3340991, 1, "023daedb070e5af8704dc88ee0af829f5c11923d2e6a42cca11ba5713b9f4491") -- Depot 3340991 (Shared from App 3340990)
+setManifestid(3340991, "2169475907772545910", 241860287)
+addappid(3893181, 1, "7f675c2fe8e758d16f3cf8d3b493956afaee78e96d78cdb668a5edb8ac1580f6") -- Depot 3893181 (Shared from App 3893180)
+setManifestid(3893181, "6526419712295796375", 324759261)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1254580) -- Battlefield V Year 2 Edition - Key
+addtoken(1254580, "9102077105603784487")
+addappid(1254581) -- Battlefield V - Key
+addtoken(1254581, "17731499009230969982")
+addappid(1254582) -- Battlefield V - Year 1 Upgrade
+addtoken(1254582, "3493210344438440284")
+addappid(1264170) -- Battlefield V - Premium Starter Pack
+addappid(1264171) -- Battlefield V - Starter Pack
+addappid(1264172) -- Battlefield V - Chapter 6 Premium Booster Pack
+addappid(1438690) -- Battlefield V Definitive Edition - The Pacific - Key
