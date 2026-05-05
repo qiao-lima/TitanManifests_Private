20e10ff288566f0305468ca03118d37ebe8ad83b
qiao-lima <qiao-lima@github.com>
2026-05-06 07:36:58 +0800

    Manual Update 4486090

---
+++ 4486090.lua
@@ -0,0 +1,12 @@
+-- 4486090's Lua and Manifest Created by Hubcap Manifest
+-- Tyrants
+-- Created: May 05, 2026 at 04:56:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4486090) -- Tyrants
+-- MAIN APP DEPOTS
+addappid(4486091, 1, "1474080ee553a9ce03b308ca126e7f417741b8208a4d2ab020f74be03f99a26b") -- Depot 4486091
+setManifestid(4486091, "294941954526608873", 138338359)
