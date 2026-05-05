812a95015e5927ba29291430e018bb8109bd0729
qiao-lima <qiao-lima@github.com>
2026-05-06 07:36:58 +0800

    Manual Update 4548840

---
+++ 4548840.lua
@@ -0,0 +1,14 @@
+-- 4548840's Lua and Manifest Created by Hubcap Manifest
+-- Belonging to Eunoia
+-- Created: May 05, 2026 at 11:05:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4548840) -- Belonging to Eunoia
+-- MAIN APP DEPOTS
+addappid(4548841, 1, "85efce5a5ded923d36af6b87afa719fd69746acfdd5551d8e0a0311e51901a82") -- Depot 4548841
+setManifestid(4548841, "5761734461467975902", 1351927953)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4548842) -- Depot 4548842 (empty depot)
