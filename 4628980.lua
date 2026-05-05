dbd6c7db8ef1cfb06979f92b81fec2ff25e5481c
qiao-lima <qiao-lima@github.com>
2026-05-06 07:36:59 +0800

    Manual Update 4628980

---
+++ 4628980.lua
@@ -0,0 +1,14 @@
+-- 4628980's Lua and Manifest Created by Hubcap Manifest
+-- 石楠小馆：一斗浮生
+-- Created: May 05, 2026 at 04:56:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4628980) -- 石楠小馆：一斗浮生
+-- MAIN APP DEPOTS
+addappid(4628981, 1, "6acad964b758f1473d8f8ad0778f37f02d53267bcf99b6787af4e3e1c6a5e240") -- Depot 4628981
+setManifestid(4628981, "9214514131051604146", 1332480750)
+addappid(4628982, 1, "a1b56d2843558824314cde89b403874471e2a009cebb42ed43d1dfc152f1711e") -- Depot 4628982
+setManifestid(4628982, "7337553008248894502", 1250274184)
