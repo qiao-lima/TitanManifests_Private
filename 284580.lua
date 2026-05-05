94baa163cac7f41791e2135e0a636ec8acd8759b
qiao-lima <qiao-lima@github.com>
2026-05-05 10:40:01 +0800

    Manual Update 284580

---
+++ 284580.lua
@@ -0,0 +1,18 @@
+-- 284580's Lua and Manifest Created by Hubcap Manifest
+-- ZAMB! Biomutant Extermination
+-- Created: May 04, 2026 at 07:17:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(284580) -- ZAMB! Biomutant Extermination
+-- MAIN APP DEPOTS
+addappid(284581, 1, "a4dc825a843a9c8e9fea3dcc5118cd9f8c0505cb7e1496faafdc6e6cb869c2bb") -- ZAMB! Biomutant Extermination Content
+setManifestid(284581, "8947950102374653601", 371438630)
+-- SHARED DEPOTS (from other apps)
+addappid(229001, 1, "ec23e1a1e1da6205722291f91bf72d1989cb347245e5a14d8d514c4ad5ba4e97") -- .NET 3.5 Client Profile Redist (Shared from App 228980)
+setManifestid(229001, "4049573910112143457", 267964564)
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") -- OpenAL 2.0.7.0 Redist (Shared from App 228980)
+setManifestid(229020, "5799761707845834510", 810085)
