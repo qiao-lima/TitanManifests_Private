208f27077aff9c722f4fcf84fae0dd16cbf6ff3e
qiao-lima <qiao-lima@github.com>
2026-05-05 10:40:01 +0800

    Manual Update 2536720

---
+++ 2536720.lua
@@ -0,0 +1,20 @@
+-- 2536720's Lua and Manifest Created by Hubcap Manifest
+-- U.V.S. Nirmana
+-- Created: May 04, 2026 at 14:10:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2536720, 1, "cbe97e167f3b264b9ad533cb330e319303ef0fa199ed60fc815c89c164353c24") -- U.V.S. Nirmana
+-- MAIN APP DEPOTS
+addappid(2536721, 1, "8568ecd9e9dcb76a2fc8b6b1d2e48ab84083a3b5f6194f3f17fd2b5f4dc93c14") -- Depot 2536721
+setManifestid(2536721, "963618426581722372", 178650263)
+addappid(2536722, 1, "cfe9c28e427f8548bfec6f21c7993369d4be3f18c87725ec39a5c71b96dabdb4") -- Depot 2536722
+setManifestid(2536722, "53620124135101640", 207492135)
+addappid(2536723, 1, "9caf850382151702fc1788e428d8832a1d27a255840042cbac63d2e9908ec3be") -- Depot 2536723
+setManifestid(2536723, "7319101400854018811", 235071782)
+-- SHARED DEPOTS (from other apps)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 117381405)
