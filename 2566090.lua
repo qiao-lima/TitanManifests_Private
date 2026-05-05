36c2b1383505bbda9e957faa35a1a5bc799af709
qiao-lima <qiao-lima@github.com>
2026-05-05 10:40:01 +0800

    Manual Update 2566090

---
+++ 2566090.lua
@@ -0,0 +1,12 @@
+-- 2566090's Lua and Manifest Created by Hubcap Manifest
+-- Missile Dancer 2
+-- Created: May 04, 2026 at 07:25:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2566090) -- Missile Dancer 2
+-- MAIN APP DEPOTS
+addappid(2566091, 1, "d5f749e99fd938ec868c739610b5152ea100c07fd428de21072d713f3e54c4ba") -- Depot 2566091
+setManifestid(2566091, "6872634579302149359", 329997030)
