1b0d4184fc6fdd494683ee1ddb6b88f9d83c9ad8
qiao-lima <qiao-lima@github.com>
2026-05-06 07:36:58 +0800

    Manual Update 4457480

---
+++ 4457480.lua
@@ -0,0 +1,12 @@
+-- 4457480's Lua and Manifest Created by Hubcap Manifest
+-- Sinful Vacation
+-- Created: May 05, 2026 at 06:19:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4457480) -- Sinful Vacation
+-- MAIN APP DEPOTS
+addappid(4457481, 1, "2e2abf8ae666939cad2bc3a6564c64afab75a339a7b08a06f49bc157228e53a8") -- Depot 4457481
+setManifestid(4457481, "788774722086656833", 1577482944)
