28a22710dfc00505b0814e88affbc7cf0abbc465
qiao-lima <qiao-lima@github.com>
2026-05-06 07:36:57 +0800

    Manual Update 4089180

---
+++ 4089180.lua
@@ -0,0 +1,12 @@
+-- 4089180's Lua and Manifest Created by Hubcap Manifest
+-- [Vladimere Lhore's] Bone Fire Effigy
+-- Created: May 05, 2026 at 10:01:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4089180) -- [Vladimere Lhore's] Bone Fire Effigy
+-- MAIN APP DEPOTS
+addappid(4089182, 1, "9a30d7ed388061a081303aae4c815be4475af771c96b04dd3c73e0ae689e28ab") -- Depot 4089182
+setManifestid(4089182, "354110479053162540", 168445050)
