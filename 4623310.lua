f7eab376654bc90d62bbcf17d7d4498bb0e82567
qiao-lima <qiao-lima@github.com>
2026-05-06 07:36:59 +0800

    Manual Update 4623310

---
+++ 4623310.lua
@@ -0,0 +1,12 @@
+-- 4623310's Lua and Manifest Created by Hubcap Manifest
+-- Charm And Clue 2 Collector's Edition
+-- Created: May 05, 2026 at 05:41:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4623310) -- Charm And Clue 2 Collector's Edition
+-- MAIN APP DEPOTS
+addappid(4623311, 1, "6d6558e52b02479b0da4937cd0f3bd2850109431368dc7592298a9c7894cebb7") -- Depot 4623311
+setManifestid(4623311, "3362791311918701469", 754280800)
