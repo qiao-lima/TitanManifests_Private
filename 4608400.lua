86bf824b3b6b10199c0f8e4b09e556a58858ed0b
qiao-lima <qiao-lima@github.com>
2026-05-06 07:36:59 +0800

    Manual Update 4608400

---
+++ 4608400.lua
@@ -0,0 +1,18 @@
+-- 4608400's Lua and Manifest Created by Hubcap Manifest
+-- ク・アドラ
+-- Created: May 05, 2026 at 04:56:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4608400) -- ク・アドラ
+-- MAIN APP DEPOTS
+addappid(4608401, 1, "7e74565e435b577c86435a3687c361c278731de6421e4752c8051dc45556594f") -- Depot 4608401
+setManifestid(4608401, "8684635703628441204", 239510229)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
