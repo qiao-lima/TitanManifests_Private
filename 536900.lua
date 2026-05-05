ca022cd1fc7b23a842099bd0693875a3d451e451
qiao-lima <qiao-lima@github.com>
2026-05-06 07:37:18 +0800

    Manual Update 536900

---
+++ 536900.lua
@@ -0,0 +1,14 @@
+-- 536900's Lua and Manifest Created by Hubcap Manifest
+-- The Last Sky
+-- Created: May 05, 2026 at 00:05:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(536900, 1, "506ffb39d43a65360045391c9eb5e452e3d973190ee9c01f2cf6871df3f4e886") -- The Last Sky
+-- MAIN APP DEPOTS
+addappid(536901, 1, "e3a0474089110209c12f59babd1d177e115d1d7abe4504a8a87d894e6d9186ed") -- The Last Sky Win
+setManifestid(536901, "7342674117855592235", 5063928707)
+addappid(536902, 1, "6cc49001262c2c18452643f05800e063502a5485e91b59dc6f071e794104e35b") -- The Last Sky OSX
+setManifestid(536902, "6206021821547139518", 4908422240)
