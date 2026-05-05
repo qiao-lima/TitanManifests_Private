77eb54b5afc6819f9c3c9298cbc1899aee5dd8dd
qiao-lima <qiao-lima@github.com>
2026-05-05 10:40:02 +0800

    Manual Update 3648730

---
+++ 3648730.lua
@@ -0,0 +1,12 @@
+-- 3648730's Lua and Manifest Created by Hubcap Manifest
+-- Blood Vial
+-- Created: May 04, 2026 at 18:33:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3648730) -- Blood Vial
+-- MAIN APP DEPOTS
+addappid(3648731, 1, "5c7ee6509cc9d6912b7177d781e891336a89c2353db00750b26a6192075bf3ed") -- Depot 3648731
+setManifestid(3648731, "3998376944074004279", 209164880)
