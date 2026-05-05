5932d8614d4f9ae023750371e17427b85c70475a
qiao-lima <qiao-lima@github.com>
2026-05-06 07:36:56 +0800

    Manual Update 3602570

---
+++ 3602570.lua
@@ -0,0 +1,12 @@
+-- 3602570's Lua and Manifest Created by Hubcap Manifest
+-- Horsey Game
+-- Created: April 28, 2026 at 18:04:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3602570, 1, "226f242c84171fc801e9b44af3c9f94d00d96100efaffdb0729eb88c3f8d019a") -- Horsey Game
+-- MAIN APP DEPOTS
+addappid(3602571, 1, "981ec53970d5867ed7ed6e4807a2aa04ee04ad58e0116b68e9d1fe5bb4951795") -- Depot 3602571
+setManifestid(3602571, "9114474502895981993", 53768757)
