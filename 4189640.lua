6afaaee497de2951ebe4854c25282a26b140b3f8
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:54 +0800

    Manual Update 4189640

---
+++ 4189640.lua
@@ -0,0 +1,12 @@
+-- 4189640's Lua and Manifest Created by Hubcap Manifest
+-- Feed The Maw
+-- Created: May 06, 2026 at 09:05:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4189640) -- Feed The Maw
+-- MAIN APP DEPOTS
+addappid(4189641, 1, "ce8f1b11a5e19f618e2d0238cddb6fc5cfb2ca7534b8eb3339ade985d8036c9f") -- Depot 4189641
+setManifestid(4189641, "1123093247912860595", 1373094119)
