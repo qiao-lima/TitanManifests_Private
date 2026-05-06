4fc506c8e50428042c651de632113aa0e3e82ddf
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:42 +0800

    Manual Update 4098760

---
+++ 4098760.lua
@@ -0,0 +1,12 @@
+-- 4098760's Lua and Manifest Created by Hubcap Manifest
+-- Intimate Massage with a Snow Leopard Lady
+-- Created: May 06, 2026 at 05:13:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4098760) -- Intimate Massage with a Snow Leopard Lady
+-- MAIN APP DEPOTS
+addappid(4098761, 1, "27cd098ef742a48591c15e61b7327846967644b8d140706e3e5b55e8f11fe0ae") -- Depot 4098761
+setManifestid(4098761, "1181090719994154859", 916036179)
