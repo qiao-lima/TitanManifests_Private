9a3753ee6cbdb6927e76c0913447a15fa30923cd
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:55 +0800

    Manual Update 4331210

---
+++ 4331210.lua
@@ -0,0 +1,12 @@
+-- 4331210's Lua and Manifest Created by Hubcap Manifest
+-- 星珀庄园的花语
+-- Created: May 06, 2026 at 13:06:37 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4331210) -- 星珀庄园的花语
+-- MAIN APP DEPOTS
+addappid(4331211, 1, "03c10720c5f914188a5bfbcf82b98ad2bfd03679b09e321d881d6de25aa75502") -- Depot 4331211
+setManifestid(4331211, "2868757894547863622", 1206244479)
