c126112ee05eeae6c217161786095eb33ac0d082
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:38 +0800

    Manual Update 1682060

---
+++ 1682060.lua
@@ -0,0 +1,12 @@
+-- 1682060's Lua and Manifest Created by Hubcap Manifest
+-- Warriors of the Nile 2
+-- Created: October 01, 2025 at 06:43:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1682060) -- Warriors of the Nile 2
+-- MAIN APP DEPOTS
+addappid(1682061, 1, "ca21c05ae26ef70ea28fc33a4b52cead451561b3bf58513cdd9d2c3f3aa99863") -- Depot 1682061
+setManifestid(1682061, "314668003733138737", 3001134493)
