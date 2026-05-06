7cd5abf562ff0f0665dbc00647a6b497516b6221
qiao-lima <qiao-lima@github.com>
2026-05-06 16:52:09 +0800

    Manual Update 4024130

---
+++ 4024130.lua
@@ -0,0 +1,12 @@
+-- 4024130's Lua and Manifest Created by Hubcap Manifest
+-- Charm&Clue
+-- Created: May 05, 2026 at 06:18:53 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4024130) -- Charm&Clue
+-- MAIN APP DEPOTS
+addappid(4024131, 1, "bba131a1492ab03689299e759593ce4805a511bc594eb989744c37ae1685e1f0") -- Depot 4024131
+setManifestid(4024131, "4883139346603616394", 640509222)
