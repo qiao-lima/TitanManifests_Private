0991015cd17aeb61327c29379ce5cfbccc642220
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:56 +0800

    Manual Update 4382900

---
+++ 4382900.lua
@@ -0,0 +1,18 @@
+-- 4382900's Lua and Manifest Created by Hubcap Manifest
+-- Guns and Exorsister
+-- Created: May 06, 2026 at 05:17:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4382900) -- Guns and Exorsister
+-- MAIN APP DEPOTS
+addappid(4382901, 1, "81bc971dedf534ad3b20b62f2d1bb9cc289dd68f5c5d02d42cf0b1db30734b93") -- Depot 4382901
+setManifestid(4382901, "5709794209564166751", 1048987108)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
