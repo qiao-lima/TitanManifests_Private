147aa0f59fad47366d00326a56f5f15f4785d7f2
qiao-lima <qiao-lima@github.com>
2026-05-06 16:52:07 +0800

    Manual Update 2830030

---
+++ 2830030.lua
@@ -0,0 +1,18 @@
+-- 2830030's Lua and Manifest Created by Hubcap Manifest
+-- MOTORSLICE
+-- Created: May 05, 2026 at 11:54:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2830030) -- MOTORSLICE
+-- MAIN APP DEPOTS
+addappid(2830031, 1, "79dd0da4c8e6d06442724528b5a706b68dfc6beaf2aaec6678caab6b7b268700") -- Depot 2830031
+setManifestid(2830031, "6196997935459972217", 1809346469)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
