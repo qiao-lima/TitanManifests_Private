e81d57dfa73a29201dc2ccd8afadad9fe5e8c640
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:53 +0800

    Manual Update 4113250

---
+++ 4113250.lua
@@ -0,0 +1,23 @@
+-- 4113250's Lua and Manifest Created by Hubcap Manifest
+-- Ultimate Sugar Crush! Brick Breaker Desire
+-- Created: May 06, 2026 at 05:14:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4113250) -- Ultimate Sugar Crush! Brick Breaker Desire
+-- MAIN APP DEPOTS
+addappid(4113251, 1, "5c24a00fc8006855dc5c9fa785076ff176dfb6a0e98a76f731dc994d9a3ad92b") -- Depot 4113251
+setManifestid(4113251, "9085615429743273299", 1516382559)
+addappid(4113252, 1, "6a0b0bb5306b852fa189242cba34b4b5894eb8317c344a53c1b87bf1e2c0ba02") -- Depot 4113252
+setManifestid(4113252, "3130886446546476942", 1637916420)
+-- DLCS WITH DEDICATED DEPOTS
+-- Ultimate Sugar Crush Brick Breaker Desire - Expansion Pack 01 (AppID: 4331600)
+addappid(4331600)
+addappid(4113253, 1, "702300b32e978ef08d8ec8954560363a7009784aab707d5f7177630672eeef7b") -- Ultimate Sugar Crush Brick Breaker Desire - Expansion Pack 01 - Depot 4113253
+setManifestid(4113253, "7901814040625107169", 72856794)
+addappid(4113254, 1, "4e3e78cf2d02c92d721cf5db2fe1baa666bf2e503ad054f7a50824924a0209eb") -- Ultimate Sugar Crush Brick Breaker Desire - Expansion Pack 01 - Depot 4113254
+setManifestid(4113254, "2160669203236500801", 72855416)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4331600) -- Depot 4331600 (empty depot)
