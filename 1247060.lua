28587eeb5b64b4ca0679f5bd25afae1512d1541c
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:38 +0800

    Manual Update 1247060

---
+++ 1247060.lua
@@ -0,0 +1,21 @@
+-- 1247060's Lua and Manifest Created by Hubcap Manifest
+-- Let me leave corona zone
+-- Created: May 06, 2026 at 12:02:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(1247060) -- Let me leave corona zone
+addtoken(1247060, "70049832584472296")
+-- MAIN APP DEPOTS
+addappid(1247061, 1, "6fe9d1f98677a7cae37a4449e1c6c925f500172ccc96c102675fe96f3a277ba0") -- Depot 1247061
+setManifestid(1247061, "3282923314320972404", 3658952850)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229033, 1, "9a789440bbc16131547ac6c062e5f168f7684da533a2899a71fdfaaeec21efc3") -- PhysX System Software 9.14.0702 (Shared from App 228980)
+setManifestid(229033, "2059065101492814639", 60039803)
