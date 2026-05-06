6dda8d066a4faf48ff965e8113c5b1ad12f00c07
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:53 +0800

    Manual Update 4119100

---
+++ 4119100.lua
@@ -0,0 +1,12 @@
+-- 4119100's Lua and Manifest Created by Hubcap Manifest
+-- Until They Burn
+-- Created: May 06, 2026 at 05:14:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4119100) -- Until They Burn
+-- MAIN APP DEPOTS
+addappid(4119101, 1, "00029a52d01dc261ce58deb8dad269a01a3550f2706dd8dff32b73655a6fe971") -- Depot 4119101
+setManifestid(4119101, "31285032373810432", 7953496028)
