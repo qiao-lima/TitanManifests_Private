d1151e97f55296ee296770f18c87cfdee2004a93
qiao-lima <qiao-lima@github.com>
2026-05-06 16:52:08 +0800

    Manual Update 3634010

---
+++ 3634010.lua
@@ -0,0 +1,12 @@
+-- 3634010's Lua and Manifest Created by Hubcap Manifest
+-- Islantiles
+-- Created: May 05, 2026 at 12:29:53 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3634010) -- Islantiles
+-- MAIN APP DEPOTS
+addappid(3634011, 1, "e57204646c74cb12d2b33283d55df359dff711b6b4b072d4b52a785db5369f23") -- Depot 3634011
+setManifestid(3634011, "197234007802321512", 2052602132)
