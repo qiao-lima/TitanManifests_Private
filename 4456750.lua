50955c2fd5c21337f382f97e6fcfaa92d354a333
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:56 +0800

    Manual Update 4456750

---
+++ 4456750.lua
@@ -0,0 +1,17 @@
+-- 4456750's Lua and Manifest Created by Hubcap Manifest
+-- After Hours: Lines of Desire - S1
+-- Created: May 06, 2026 at 13:01:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4456750, 1, "3ca06ad6d0b2b2436d76dc986e3915c047f385cc987398498f5e05b92880b970") -- After Hours: Lines of Desire - S1
+-- MAIN APP DEPOTS
+addappid(4456751, 1, "d8d9cdceb31500ea42f81d6c619b12fb90d256ff957005901d6e565f39fa7e42") -- Depot 4456751
+setManifestid(4456751, "1979877873892062534", 459240912)
+-- DLCS WITH DEDICATED DEPOTS
+-- After Hours Lines of Desire S1 - Supporter Pack (AppID: 4666060)
+addappid(4666060)
+addappid(4666060, 1, "114bb17c7d625b9317b5815542556cb8278b1b62782a6bb8f2300beadf8d3121") -- After Hours Lines of Desire S1 - Supporter Pack - Depot 4666060
+setManifestid(4666060, "753946473536762830", 197454051)
