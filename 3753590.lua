d0e35e2badfc6b918a805f3e7460d90a3c7ab305
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:40 +0800

    Manual Update 3753590

---
+++ 3753590.lua
@@ -0,0 +1,12 @@
+-- 3753590's Lua and Manifest Created by Hubcap Manifest
+-- Find the Differences 3D
+-- Created: May 06, 2026 at 05:12:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3753590) -- Find the Differences 3D
+-- MAIN APP DEPOTS
+addappid(3753591, 1, "1f93955b845a6842e159f76c44a53461247b60e774dc5ece14f99fddb91ab0c1") -- Depot 3753591
+setManifestid(3753591, "8719214124989797969", 729220732)
