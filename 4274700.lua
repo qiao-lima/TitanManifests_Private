085d4bf2f8c60692aa7b39deadf3056b7b6b5b5d
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:55 +0800

    Manual Update 4274700

---
+++ 4274700.lua
@@ -0,0 +1,12 @@
+-- 4274700's Lua and Manifest Created by Hubcap Manifest
+-- Tonight , resist drinking MiLK!!
+-- Created: May 06, 2026 at 05:16:58 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4274700) -- Tonight , resist drinking MiLK!!
+-- MAIN APP DEPOTS
+addappid(4274701, 1, "7c26599f37a8e43ce523e36b3010418e5120b82641bb0398ffa6788fa7d8b16e") -- Depot 4274701
+setManifestid(4274701, "4681848815051660332", 605996734)
