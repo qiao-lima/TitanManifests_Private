c7c6860f8cf1b1ec1ab7aa523a31f0a8156102ed
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:56 +0800

    Manual Update 4503950

---
+++ 4503950.lua
@@ -0,0 +1,12 @@
+-- 4503950's Lua and Manifest Created by Hubcap Manifest
+-- 理想のたわわ
+-- Created: May 06, 2026 at 05:19:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4503950) -- 理想のたわわ
+-- MAIN APP DEPOTS
+addappid(4503951, 1, "938b50450dd82079fbe9e100a061d4c87ee5c41761f78a2c3d0d231f733ba8d0") -- Depot 4503951
+setManifestid(4503951, "4653490716144886839", 179174814)
