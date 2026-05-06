a9c0dd73b6691054f8e3d6bbe91dae059d9b6e9f
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:55 +0800

    Manual Update 4287530

---
+++ 4287530.lua
@@ -0,0 +1,14 @@
+-- 4287530's Lua and Manifest Created by Hubcap Manifest
+-- The subject: Protocol zero
+-- Created: May 06, 2026 at 11:48:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4287530) -- The subject: Protocol zero
+-- MAIN APP DEPOTS
+addappid(4287531, 1, "9ce4ceeaba2b1b16bdd09ebf0823409b14378ef65e45a352aa0e97d3e0b26276") -- Depot 4287531
+setManifestid(4287531, "3782052390930040583", 723011860)
+addappid(4287532, 1, "6b0e2eb1830826031b5ac37d997dffb05d3114ab260c5c2bd4b561cb76831c87") -- Depot 4287532
+setManifestid(4287532, "3176281963716943350", 720213202)
