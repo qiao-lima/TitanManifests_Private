a32ebecf3cfba1af0674d0696dd27e6cc0610744
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:40 +0800

    Manual Update 3575650

---
+++ 3575650.lua
@@ -0,0 +1,12 @@
+-- 3575650's Lua and Manifest Created by Hubcap Manifest
+-- Dark Adelita
+-- Created: May 06, 2026 at 05:15:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3575650) -- Dark Adelita
+-- MAIN APP DEPOTS
+addappid(3575651, 1, "ebd3352a3628f4df1e4a1534a77c3a1f595e0faec78184e77279c8d454c68d43") -- Depot 3575651
+setManifestid(3575651, "8534485467059209979", 111134117)
