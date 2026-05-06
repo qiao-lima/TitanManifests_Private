d0ea88fdfcb5634b2a91142282666614765d8864
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:40 +0800

    Manual Update 3470370

---
+++ 3470370.lua
@@ -0,0 +1,12 @@
+-- 3470370's Lua and Manifest Created by Hubcap Manifest
+-- 色系战记 Rainbow Legends
+-- Created: May 06, 2026 at 13:39:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3470370, 1, "9f9da5bf89d4cd8ab4c29d729ce572fe3dea467c4a9261534cf3f932e2b623fa") -- 色系战记 Rainbow Legends
+-- MAIN APP DEPOTS
+addappid(3470371, 1, "301b398234f4ddd86faab7fdc4438fd6518449d2f988847d96bb0aa26a18d98a") -- Depot 3470371
+setManifestid(3470371, "3740546857927599146", 2290082182)
