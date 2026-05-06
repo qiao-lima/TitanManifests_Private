c8fc58d5e003a53c2e15e7fe4ba9ff1d87e6291d
qiao-lima <qiao-lima@github.com>
2026-05-06 16:52:07 +0800

    Manual Update 2769240

---
+++ 2769240.lua
@@ -0,0 +1,19 @@
+-- 2769240's Lua and Manifest Created by Hubcap Manifest
+-- Wax Heads
+-- Created: May 05, 2026 at 12:30:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(2769240, 1, "6f10e873444ddecb222ef2d913fff4292af04e7481dfef651bed286c8c847405") -- Wax Heads
+-- MAIN APP DEPOTS
+addappid(2769241, 1, "4a9cbeb84870d4e1d37d2473ec003f9f7d35690899dc8c94ab6123b4e21e739b") -- Depot 2769241
+setManifestid(2769241, "1555224235235555286", 1858689244)
+addappid(2769242, 1, "712c1493f39f232ff5c793606fd23529f8e506cc1af9990376cea186bd02079a") -- Depot 2769242
+setManifestid(2769242, "2739644397341436006", 1833328768)
+-- DLCS WITH DEDICATED DEPOTS
+-- Wax Heads - Supporter Pack (AppID: 4665240)
+addappid(4665240)
+addappid(4665240, 1, "874fcab053c1909c4c8474712007711c7d66239b1879902a7410b9b1f21ce4d9") -- Wax Heads - Supporter Pack - Depot 4665240
+setManifestid(4665240, "4872585101533646650", 398233212)
