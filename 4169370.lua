760dd38b5b333544a035cc04484303a7e26072c0
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:54 +0800

    Manual Update 4169370

---
+++ 4169370.lua
@@ -0,0 +1,12 @@
+-- 4169370's Lua and Manifest Created by Hubcap Manifest
+-- Wives Will Do Everything for Elven Hosts
+-- Created: May 06, 2026 at 05:15:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4169370) -- Wives Will Do Everything for Elven Hosts
+-- MAIN APP DEPOTS
+addappid(4169371, 1, "08b87c31153d06af3fe46c011a1f801a4e599cbe6595a5aaf1d672223b42e5ed") -- Depot 4169371
+setManifestid(4169371, "1457401305111787040", 475576549)
