2cbf0a17591f97932141728f7536e6d2ae6b469d
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:56 +0800

    Manual Update 4343280

---
+++ 4343280.lua
@@ -0,0 +1,12 @@
+-- 4343280's Lua and Manifest Created by Hubcap Manifest
+-- Space Lora - Adult Only
+-- Created: May 06, 2026 at 05:19:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4343280) -- Space Lora - Adult Only
+-- MAIN APP DEPOTS
+addappid(4343281, 1, "e27926c9fd75be59f5f6fffa4107daaa41f4f633d4cf7cd14167ffb49e67d476") -- Depot 4343281
+setManifestid(4343281, "8284045362238494653", 912435824)
