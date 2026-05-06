b77586d2ec018c2b54f7eb4f16f9daa2b2abf329
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:39 +0800

    Manual Update 2423290

---
+++ 2423290.lua
@@ -0,0 +1,16 @@
+-- 2423290's Lua and Manifest Created by Hubcap Manifest
+-- Handmancers
+-- Created: May 06, 2026 at 13:50:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2423290, 1, "d5e268ffc7cd65475a4c7f5f83d4925b9912cce2bc62fcc256244fa1c4e04abe") -- Handmancers
+-- MAIN APP DEPOTS
+addappid(2423292, 1, "55da243430f467cecf76791980cdbce8b78cf199b63a7a2c4d28bdeac84004f8") -- Depot 2423292
+setManifestid(2423292, "1012827220175041967", 6471186407)
+addappid(2423295, 1, "7f35a3528da7a9e2f6793d4569355d2e20421af8af8b1dd9ac601d7b6b511113") -- Depot 2423295
+setManifestid(2423295, "5571662298430722757", 6510090506)
+addappid(2423296, 1, "8eff4cc3ff85df1954906590faf051626b2d0485b1d7b34ec0f9c07de9d4a0b3") -- Depot 2423296
+setManifestid(2423296, "6308183220011047139", 6491552320)
