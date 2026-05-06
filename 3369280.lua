961c6ef1a6561f041766c9785bdb6b8edad68391
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:40 +0800

    Manual Update 3369280

---
+++ 3369280.lua
@@ -0,0 +1,12 @@
+-- 3369280's Lua and Manifest Created by Hubcap Manifest
+-- Nightlife
+-- Created: May 06, 2026 at 05:16:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3369280) -- Nightlife
+-- MAIN APP DEPOTS
+addappid(3369281, 1, "ca8c949ca64b8c2892f7dd9d895e6e8dd37f9b6ce43d73f690640999e4dc1fbb") -- Depot 3369281
+setManifestid(3369281, "6796014644126713438", 2455074904)
