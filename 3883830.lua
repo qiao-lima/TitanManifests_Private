7500ec6f3ac16c19a3166ba7b1c1f094db8233f6
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:41 +0800

    Manual Update 3883830

---
+++ 3883830.lua
@@ -0,0 +1,20 @@
+-- 3883830's Lua and Manifest Created by Hubcap Manifest
+-- Sweet Life ~Our happy days,until they're tainted by cuckoldry~
+-- Created: May 06, 2026 at 05:17:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(3883830) -- Sweet Life ~Our happy days,until they're tainted by cuckoldry~
+-- MAIN APP DEPOTS
+addappid(3883831, 1, "7b7d2e9f27a07eb5ac666b9c45827beb9dd92579c6a2d2856eb47141177a155b") -- Depot 3883831
+setManifestid(3883831, "8129181420751118389", 4098832630)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
