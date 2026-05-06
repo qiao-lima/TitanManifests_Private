ee12f869daa6ab00cdc3a713aa3fc0e9999d0c6a
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:41 +0800

    Manual Update 3759330

---
+++ 3759330.lua
@@ -0,0 +1,14 @@
+-- 3759330's Lua and Manifest Created by Hubcap Manifest
+-- 煉金術士的秘藥
+-- Created: May 06, 2026 at 05:17:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3759330) -- 煉金術士的秘藥
+-- MAIN APP DEPOTS
+addappid(3759331, 1, "fa2ab37c28a6aebd67e73624e23468c3a60efaf64319e2fe49c365d1c555b228") -- Depot 3759331
+setManifestid(3759331, "5786403316067889730", 1105395821)
+addappid(3759332, 1, "f01a67d611d32f9e416c6ad9af98e06830e42ca4bf0748aa78e67d1782d5dee7") -- Depot 3759332
+setManifestid(3759332, "1994751476827184440", 1110470514)
