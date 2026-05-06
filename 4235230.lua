41071f75be100156c0314ad54f8e57fe05179aea
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:54 +0800

    Manual Update 4235230

---
+++ 4235230.lua
@@ -0,0 +1,23 @@
+-- 4235230's Lua and Manifest Created by Hubcap Manifest
+-- Werewolf: The Inner Beast
+-- Created: May 06, 2026 at 16:24:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4235230) -- Werewolf: The Inner Beast
+-- MAIN APP DEPOTS
+addappid(4235231, 1, "31ef69542587c9fb8b15d4810b23816e77dd7595178bf40bfbd771552b27aa03") -- Depot 4235231
+setManifestid(4235231, "876025468052831811", 12204743451)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Werewolf The Inner Beast - Unrated (AppID: 4560040)
+addappid(4560040)
+addappid(4560040, 1, "719cce34a60fb713b7b6c02c4d069efd4c3c05f6d50188891e02d0384dd66deb") -- Werewolf The Inner Beast - Unrated - Depot 4560040
+setManifestid(4560040, "6066024537978046367", 54051479)
