a0c929acb5b31984efe7a896fe185a88087301d0
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:41 +0800

    Manual Update 3959960

---
+++ 3959960.lua
@@ -0,0 +1,12 @@
+-- 3959960's Lua and Manifest Created by Hubcap Manifest
+-- 魅魔貓的報恩
+-- Created: May 06, 2026 at 05:19:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3959960) -- 魅魔貓的報恩
+-- MAIN APP DEPOTS
+addappid(3959961, 1, "c7145d886747b6b56eae6eb9f988aaf004146fc65103e0a9492244c6c0a3c83a") -- Depot 3959961
+setManifestid(3959961, "3882356804727188743", 1322849716)
