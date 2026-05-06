285228bb6f075a1cb8577da863018bfe6d98355f
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:56 +0800

    Manual Update 4599650

---
+++ 4599650.lua
@@ -0,0 +1,12 @@
+-- 4599650's Lua and Manifest Created by Hubcap Manifest
+-- Flip My Room: Makeover
+-- Created: May 06, 2026 at 05:19:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4599650) -- Flip My Room: Makeover
+-- MAIN APP DEPOTS
+addappid(4599651, 1, "d77a4af140076253f550a2241f38a99401b3beb8efc28cfff69e70a8ecb88029") -- Depot 4599651
+setManifestid(4599651, "377973353447493138", 1127366445)
