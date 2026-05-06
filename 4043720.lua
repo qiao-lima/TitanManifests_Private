036d912352cb7626bfc51bcfbc27db21e2af652a
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:42 +0800

    Manual Update 4043720

---
+++ 4043720.lua
@@ -0,0 +1,14 @@
+-- 4043720's Lua and Manifest Created by Hubcap Manifest
+-- Without A Hitch
+-- Created: May 06, 2026 at 11:19:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4043720, 1, "81af6ab0ab5b7e0771e870eb2292fc10b7ae26944e71310a7f6c493ae0b61bbe") -- Without A Hitch
+-- MAIN APP DEPOTS
+addappid(4043721, 1, "f6ab1d2ee9c742c8b329e219af3dff761a4e5690de7b5e130e1d6c74c6cc684f") -- Depot 4043721
+setManifestid(4043721, "3239476446699004602", 1090798993)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4515680) -- WITHOUT A HITCH Steam Exclusive - Valve Hat
