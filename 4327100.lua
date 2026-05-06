4df9a081d356091dcb75014d05b4335b60ddc556
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:55 +0800

    Manual Update 4327100

---
+++ 4327100.lua
@@ -0,0 +1,12 @@
+-- 4327100's Lua and Manifest Created by Hubcap Manifest
+-- Hentai Fluffy Wolf Camp!!
+-- Created: May 06, 2026 at 05:18:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4327100) -- Hentai Fluffy Wolf Camp!!
+-- MAIN APP DEPOTS
+addappid(4327101, 1, "b8fa7cf7796a4799b2caa56d449ad384dd73fcd8da199ee6282fb9d46c0d4a5f") -- Depot 4327101
+setManifestid(4327101, "3319019584921021882", 403408382)
