8e4550aace9079bc9e6ed248bb6457cf1fe143d9
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:39 +0800

    Manual Update 2368100

---
+++ 2368100.lua
@@ -0,0 +1,12 @@
+-- 2368100's Lua and Manifest Created by Hubcap Manifest
+-- The Lonely Hacker
+-- Created: May 06, 2026 at 13:38:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2368100) -- The Lonely Hacker
+-- MAIN APP DEPOTS
+addappid(2368101, 1, "1c52eb1a736cf3395f4cafab3c41da15450f98832c2175945f214c965231b344") -- Depot 2368101
+setManifestid(2368101, "7834350518593146340", 934874426)
