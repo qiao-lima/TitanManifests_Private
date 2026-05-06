38e86df796903172229fd9b8e8ed563ecf1179e4
qiao-lima <qiao-lima@github.com>
2026-05-06 16:52:08 +0800

    Manual Update 3756020

---
+++ 3756020.lua
@@ -0,0 +1,12 @@
+-- 3756020's Lua and Manifest Created by Hubcap Manifest
+-- Racer Overdrive
+-- Created: May 05, 2026 at 21:40:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3756020) -- Racer Overdrive
+-- MAIN APP DEPOTS
+addappid(3756021, 1, "37a9e86f76eaa86f5c6c30543e9447370f0fd894771fd799051a66276d5e1cd1") -- Depot 3756021
+setManifestid(3756021, "250131492773401060", 15364667676)
