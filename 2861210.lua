a1e637a9967f1f000fec51bfb0116aac5d1b1933
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:39 +0800

    Manual Update 2861210

---
+++ 2861210.lua
@@ -0,0 +1,18 @@
+-- 2861210's Lua and Manifest Created by Hubcap Manifest
+-- HYPNOS
+-- Created: May 06, 2026 at 14:09:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2861210, 1, "e353d7d40a12029d156b0c1c3f1a836b169ae9b4e600f79997df276083a2b090") -- HYPNOS
+-- MAIN APP DEPOTS
+addappid(2861211, 1, "e91e3a24f8694fc26efa4022122959e98a6f31a6de98c026b8f58e845af7dd20") -- Depot 2861211
+setManifestid(2861211, "507329964056751986", 20542519674)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
