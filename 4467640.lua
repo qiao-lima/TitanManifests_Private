192dc5005f1476a86717bcf1d49cd8d9f256b532
qiao-lima <qiao-lima@github.com>
2026-05-06 16:52:09 +0800

    Manual Update 4467640

---
+++ 4467640.lua
@@ -0,0 +1,14 @@
+-- 4467640's Lua and Manifest Created by Hubcap Manifest
+-- 防线：末日尸潮
+-- Created: May 05, 2026 at 22:50:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4467640) -- 防线：末日尸潮
+-- MAIN APP DEPOTS
+addappid(4467641, 1, "aeae0a6f8a29d8ddba771f47c112dd461f7d3fb6153fbc34e911f014cc4c7843") -- Depot 4467641
+setManifestid(4467641, "7902210730104767663", 122387625)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4467642) -- Depot 4467642 (empty depot)
