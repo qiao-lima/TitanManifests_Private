5ea80082c96e1525b5ba44ee6c432cede6d12db0
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:42 +0800

    Manual Update 4063960

---
+++ 4063960.lua
@@ -0,0 +1,14 @@
+-- 4063960's Lua and Manifest Created by Hubcap Manifest
+-- Oppai Party Creampie Quest
+-- Created: May 06, 2026 at 05:19:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4063960) -- Oppai Party Creampie Quest
+-- MAIN APP DEPOTS
+addappid(4063961, 1, "25ff0b26058d82b89250a0c785c3b548302bd240fdea1251f8de2691713b5eaa") -- Depot 4063961
+setManifestid(4063961, "1195402273594540718", 1984653301)
+addappid(4063962, 1, "a895da36ca9d296f12940215966c2addb13c7060b532520493a827be7af3bc71") -- Depot 4063962
+setManifestid(4063962, "5948365954072552455", 1979633676)
