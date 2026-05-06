1c8092d1cb2f7f5239badc1fcee1fc57602196d4
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:40 +0800

    Manual Update 3386010

---
+++ 3386010.lua
@@ -0,0 +1,12 @@
+-- 3386010's Lua and Manifest Created by Hubcap Manifest
+-- Survivor these Girls
+-- Created: May 06, 2026 at 05:20:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3386010) -- Survivor these Girls
+-- MAIN APP DEPOTS
+addappid(3386012, 1, "756587c0b73306340b69d1928d7084b46105d58937d03993bb4d26a4f2145c69") -- Depot 3386012
+setManifestid(3386012, "5090364975090394650", 437502958)
