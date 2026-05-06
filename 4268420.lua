b54982d750f35c9a214f1e8d7728bd9d7c28d955
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:55 +0800

    Manual Update 4268420

---
+++ 4268420.lua
@@ -0,0 +1,12 @@
+-- 4268420's Lua and Manifest Created by Hubcap Manifest
+-- A Sinister Glitch
+-- Created: May 06, 2026 at 09:04:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4268420) -- A Sinister Glitch
+-- MAIN APP DEPOTS
+addappid(4268421, 1, "309818561341aaf656dcafe1599a21e49ea8bac1dce087f2d38b0ba0f6c1c3e1") -- Depot 4268421
+setManifestid(4268421, "3338671703240297220", 411914172)
