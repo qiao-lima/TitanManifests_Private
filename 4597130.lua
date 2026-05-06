5bcf2dfeb2f976fcece0a2e91ef2883b0bc9ed1e
qiao-lima <qiao-lima@github.com>
2026-05-06 16:52:09 +0800

    Manual Update 4597130

---
+++ 4597130.lua
@@ -0,0 +1,14 @@
+-- 4597130's Lua and Manifest Created by Hubcap Manifest
+-- Occupied Hearts
+-- Created: May 05, 2026 at 06:19:29 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4597130) -- Occupied Hearts
+-- MAIN APP DEPOTS
+addappid(4597131, 1, "00e02de0518d976f84318cfedd805c534083b23540c4afbb62f2da8283f71eb8") -- Depot 4597131
+setManifestid(4597131, "1931412379079344158", 1201570756)
+addappid(4597132, 1, "f27bba8c1e5bd057252e57398a09ffe29827a2b5b4fd959a6371121ad2cb1d91") -- Depot 4597132
+setManifestid(4597132, "5129465009249643799", 1050670280)
