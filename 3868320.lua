1ad5c055df415f4554357c9d8261535e87c4dbdc
qiao-lima <qiao-lima@github.com>
2026-05-06 16:52:09 +0800

    Manual Update 3868320

---
+++ 3868320.lua
@@ -0,0 +1,12 @@
+-- 3868320's Lua and Manifest Created by Hubcap Manifest
+-- Forage Wizard
+-- Created: May 05, 2026 at 13:12:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3868320) -- Forage Wizard
+-- MAIN APP DEPOTS
+addappid(3868323, 1, "9e28b51aa4740d06ca3b37fe73b23b365a51c318798a53a4bd622d73b76e476d") -- Depot 3868323
+setManifestid(3868323, "5258478242752598587", 726375287)
