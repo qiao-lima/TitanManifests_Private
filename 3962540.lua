8cd7203cc634f3bb328cd079f1c6ebcbcef10ac5
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:42 +0800

    Manual Update 3962540

---
+++ 3962540.lua
@@ -0,0 +1,16 @@
+-- 3962540's Lua and Manifest Created by Hubcap Manifest
+-- A Ragdoll Rage Game
+-- Created: May 06, 2026 at 18:20:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3962540, 1, "56559940d465fc42c8c03f87cf26d1f23eedd6f89c6dcea3c80833c73703f698") -- A Ragdoll Rage Game
+-- MAIN APP DEPOTS
+addappid(3962541, 1, "a2d7e7f5927122799ffa177d4eb6917cfa7ad6e56c968def3a496265ef20788c") -- Depot 3962541
+setManifestid(3962541, "3352581571575750972", 500783245)
+addappid(3962542, 1, "34ba0c15a00a38144f167addac3dab8a42713fc9b9934e58f61013941bd02afc") -- Depot 3962542
+setManifestid(3962542, "1423749383919680125", 715481980)
+addappid(3962543, 1, "325f49b9edb88eef344748d132e345df3b2fbe1aba34179a5b5c930eb4d7bbe3") -- Depot 3962543
+setManifestid(3962543, "1459787134388502853", 711299263)
