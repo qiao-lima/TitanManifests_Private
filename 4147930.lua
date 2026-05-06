cdde81dcf33b9a759c236fbd8fe3a89eb7eaa719
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:54 +0800

    Manual Update 4147930

---
+++ 4147930.lua
@@ -0,0 +1,14 @@
+-- 4147930's Lua and Manifest Created by Hubcap Manifest
+-- Flash Knight Nymphelia
+-- Created: May 06, 2026 at 05:18:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4147930) -- Flash Knight Nymphelia
+-- MAIN APP DEPOTS
+addappid(4147931, 1, "c4a4b707ef6bccb91a07d5360de509ff5b2ea5cda790d905f0fd08d5ae67f89c") -- Depot 4147931
+setManifestid(4147931, "8088412679334559746", 944817388)
+addappid(4147932, 1, "60f2da0e622718ec5417301c12f0dc7fc80d7cb78ff6c849299e966c817c0043") -- Depot 4147932
+setManifestid(4147932, "8261286041052205252", 953267716)
