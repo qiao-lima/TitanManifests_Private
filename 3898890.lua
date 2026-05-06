5f792997d1b1b5f900fbe1496a24da537be0da43
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:41 +0800

    Manual Update 3898890

---
+++ 3898890.lua
@@ -0,0 +1,12 @@
+-- 3898890's Lua and Manifest Created by Hubcap Manifest
+-- Princess Elheim in Exile
+-- Created: May 06, 2026 at 05:19:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3898890) -- Princess Elheim in Exile
+-- MAIN APP DEPOTS
+addappid(3898891, 1, "a18966569c6f262c8b8d36a44565fc761d21b97a2a8f27631a67da028c5dd5ff") -- Depot 3898891
+setManifestid(3898891, "827290277761826133", 1444823096)
