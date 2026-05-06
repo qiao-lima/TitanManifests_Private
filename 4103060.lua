af77ef7e9d975776a5126daa47ecc62de05a0321
qiao-lima <qiao-lima@github.com>
2026-05-07 06:26:53 +0800

    Manual Update 4103060

---
+++ 4103060.lua
@@ -0,0 +1,16 @@
+-- 4103060's Lua and Manifest Created by Hubcap Manifest
+-- The Counter Gambit
+-- Created: May 06, 2026 at 05:15:53 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(4103060) -- The Counter Gambit
+-- MAIN APP DEPOTS
+addappid(4103061, 1, "91756d9663d83c7ea6c2e56b8764d42d2a96a628720be90588ce7c8276ad1f9e") -- Depot 4103061
+setManifestid(4103061, "5915422839260871846", 825010452)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- The Counter Gambit 18 Content DLC (AppID: 4222590) - missing depot keys
+-- addappid(4222590)
