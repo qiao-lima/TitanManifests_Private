c3e7571dd4a15ff25c66b0c8d662e472034e558b
qiao-lima <qiao-lima@github.com>
2026-05-06 16:52:08 +0800

    Manual Update 3277170

---
+++ 3277170.lua
@@ -0,0 +1,14 @@
+-- 3277170's Lua and Manifest Created by Hubcap Manifest
+-- Chubby Story
+-- Created: May 05, 2026 at 06:18:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3277170, 1, "db96c951516345f657c95389d54402c170af46c9cd0ac413b558398ca424ddc4") -- Chubby Story
+-- MAIN APP DEPOTS
+addappid(3277171, 1, "cc8696abd7532a825d4fe57acca530ae9edbd12277de44522463f36f40d1ca69") -- Depot 3277171
+setManifestid(3277171, "1244239995669155683", 975416166)
+addappid(3277172, 1, "1ccc443db86356fa0f1f7d59b3e5bc60fd51b05fa89497145c8d8aa5061cbda0") -- Depot 3277172
+setManifestid(3277172, "8410115080164628545", 980775570)
