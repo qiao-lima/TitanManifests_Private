c517b89bccd66a855b6bc2a8e0bf5c9dde09ab2e
qiao-lima <qiao-lima@github.com>
2026-05-08 00:00:22 +0800

    Manual Update 2876150

---
+++ 2876150.lua
@@ -0,0 +1,13 @@
+-- 2876150's Lua and Manifest Created by Hubcap Manifest
+-- Alien Strike: Blasting the Intruders
+-- Created: May 07, 2026 at 09:49:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2876150) -- Alien Strike: Blasting the Intruders
+addtoken(2876150, "3408117766225732174")
+-- MAIN APP DEPOTS
+addappid(2876151, 1, "cfff3c50f19207909e1f9c837d0539be3c3bc89766a5f82cdf76bd72fa052646") -- Depot 2876151
+setManifestid(2876151, "3217183543862187896", 2024432662)
