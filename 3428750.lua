bf94fc20d8561d0fbab06fdd057d5319ece1b14e
qiao-lima <qiao-lima@github.com>
2026-05-08 00:00:23 +0800

    Manual Update 3428750

---
+++ 3428750.lua
@@ -0,0 +1,12 @@
+-- 3428750's Lua and Manifest Created by Hubcap Manifest
+-- Pookie's Lingerie Shop
+-- Created: May 07, 2026 at 07:21:29 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3428750) -- Pookie's Lingerie Shop
+-- MAIN APP DEPOTS
+addappid(3428751, 1, "1534ae978e83b59cb0b1f3be2a095cd79419264b02958c779cce720f4aa8fd67") -- Depot 3428751
+setManifestid(3428751, "9058941246565199767", 632162177)
