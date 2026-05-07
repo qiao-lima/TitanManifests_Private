52469c0750b5d5bce16e0b847d965cc697752d66
qiao-lima <qiao-lima@github.com>
2026-05-08 00:00:23 +0800

    Manual Update 3265700

---
+++ 3265700.lua
@@ -0,0 +1,14 @@
+-- 3265700's Lua and Manifest Created by Hubcap Manifest
+-- Vampire Crawlers
+-- Created: April 30, 2026 at 06:43:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3265700, 1, "20f9ef13db9be4cd2b66262d177c6107c74912a164f5c1e519d194e090bc57e6") -- Vampire Crawlers
+-- MAIN APP DEPOTS
+addappid(3265701, 1, "1e7525ea73ba61b029745bf11a16c0f88090e25fd8a3841cf7c06879636857f8") -- Depot 3265701
+setManifestid(3265701, "7864173163031085045", 1048011858)
+addappid(3265702, 1, "2753b9f6a521270a2c776ff684a32c6bfef84ab95fa241f60c2392c13776032b") -- Depot 3265702
+setManifestid(3265702, "7284603806571514722", 1205979315)
