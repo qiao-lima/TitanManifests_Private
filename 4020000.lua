3dc3c7606fbfcde405ce650b24d88786c621d6fb
qiao-lima <qiao-lima@github.com>
2026-05-08 00:00:24 +0800

    Manual Update 4020000

---
+++ 4020000.lua
@@ -0,0 +1,12 @@
+-- 4020000's Lua and Manifest Created by Hubcap Manifest
+-- Nano Flat Owner
+-- Created: May 07, 2026 at 06:07:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4020000) -- Nano Flat Owner
+-- MAIN APP DEPOTS
+addappid(4020001, 1, "00cb18328284d5a23dd95eefee3d26a9b0e12a93ca208e4f70c4886e5c5a662e") -- Depot 4020001
+setManifestid(4020001, "618588821222517864", 1743232992)
