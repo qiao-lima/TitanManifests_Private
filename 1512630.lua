e29f140f0f99d8dcdf157d6f4dfe80d7d307d91c
qiao-lima <qiao-lima@github.com>
2026-05-08 00:00:21 +0800

    Manual Update 1512630

---
+++ 1512630.lua
@@ -0,0 +1,18 @@
+-- 1512630's Lua and Manifest Created by Hubcap Manifest
+-- Fake Roads
+-- Created: May 07, 2026 at 11:34:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1512630) -- Fake Roads
+-- MAIN APP DEPOTS
+addappid(1512631, 1, "fdc874e786f3d557e1c32841a45a15cf73dc0255ba7867caae45b64f6fd56f24") -- Depot 1512631
+setManifestid(1512631, "5381241936346921528", 1322888685)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
