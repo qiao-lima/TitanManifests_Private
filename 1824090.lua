12a80e0d5258072d54878b680fc4cf9b560ef454
qiao-lima <qiao-lima@github.com>
2026-05-08 00:00:21 +0800

    Manual Update 1824090

---
+++ 1824090.lua
@@ -0,0 +1,22 @@
+-- 1824090's Lua and Manifest Created by Hubcap Manifest
+-- Goose vs Zombies
+-- Created: May 07, 2026 at 09:43:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(1824090) -- Goose vs Zombies
+-- MAIN APP DEPOTS
+addappid(1824091, 1, "c0e5b7c1ec382424b5189e9473be77ef2d3f3dc451b878e97d968f52297a19c5") -- Depot 1824091
+setManifestid(1824091, "2933469983316167889", 7791478205)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 117381405)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(1824092) -- Depot 1824092 (empty depot)
