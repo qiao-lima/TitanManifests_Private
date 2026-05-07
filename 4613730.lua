a1c31fdd8f01a518d5cd5866a47542d76cae6400
qiao-lima <qiao-lima@github.com>
2026-05-08 00:05:20 +0800

    Manual Update 4613730

---
+++ 4613730.lua
@@ -0,0 +1,12 @@
+-- 4613730's Lua and Manifest Created by Hubcap Manifest
+-- Brooks Investment Simulator
+-- Created: May 06, 2026 at 14:34:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4613730) -- Brooks Investment Simulator
+-- MAIN APP DEPOTS
+addappid(4613731, 1, "13b23d2b76e9649c670e3345ce1b05cbde2b8da199b14f0542c45ae7f2b27be9") -- Depot 4613731
+setManifestid(4613731, "6686193416421339177", 288120762)
