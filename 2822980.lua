b6e7742b1b892f5ff6f905825fe4079a108bb1de
qiao-lima <qiao-lima@github.com>
2026-05-08 00:00:22 +0800

    Manual Update 2822980

---
+++ 2822980.lua
@@ -0,0 +1,14 @@
+-- 2822980's Lua and Manifest Created by Hubcap Manifest
+-- ARSONATE
+-- Created: May 07, 2026 at 06:08:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2822980) -- ARSONATE
+-- MAIN APP DEPOTS
+addappid(2822981, 1, "bf5fcc89866cb0dfd74367b785755e40f5b0bd488cdc7148b19a8f5e7d42bce6") -- Depot 2822981
+setManifestid(2822981, "6147340458277081984", 440470912)
+addappid(2822982, 1, "8fa9960549e18fd22e2d0ccae2e8bed623a8b4f6f4b7512d3b7de7b3ada0c353") -- Depot 2822982
+setManifestid(2822982, "524494584731385730", 443094100)
