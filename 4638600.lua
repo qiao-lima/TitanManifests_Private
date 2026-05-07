78e863ee13e66f215de1adb48decc30c0ac8da70
qiao-lima <qiao-lima@github.com>
2026-05-08 00:05:21 +0800

    Manual Update 4638600

---
+++ 4638600.lua
@@ -0,0 +1,12 @@
+-- 4638600's Lua and Manifest Created by Hubcap Manifest
+-- 忍者刺杀行动
+-- Created: May 07, 2026 at 06:09:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4638600) -- 忍者刺杀行动
+-- MAIN APP DEPOTS
+addappid(4638601, 1, "055242c7c80596bc8eaed9caf619ed253fe301e2347a6001c0c06664bf96e373") -- Depot 4638601
+setManifestid(4638601, "4666060286827859642", 343039852)
