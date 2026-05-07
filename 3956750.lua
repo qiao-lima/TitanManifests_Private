7edf8bb606dc8a0a43a576561ec23a9985191a0c
qiao-lima <qiao-lima@github.com>
2026-05-08 00:00:24 +0800

    Manual Update 3956750

---
+++ 3956750.lua
@@ -0,0 +1,12 @@
+-- 3956750's Lua and Manifest Created by Hubcap Manifest
+-- Giantfall
+-- Created: May 07, 2026 at 06:08:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3956750) -- Giantfall
+-- MAIN APP DEPOTS
+addappid(3956751, 1, "48d7dc64bcd0f851f1c006d369691ec095c47a29c8e14fc7bdfc5e3594a63b63") -- Depot 3956751
+setManifestid(3956751, "4040215014276645154", 743081360)
