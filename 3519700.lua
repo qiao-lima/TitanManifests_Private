2225327e3826c86d382a26337ea4d460fa3844c8
qiao-lima <qiao-lima@github.com>
2026-05-08 00:00:23 +0800

    Manual Update 3519700

---
+++ 3519700.lua
@@ -0,0 +1,20 @@
+-- 3519700's Lua and Manifest Created by Hubcap Manifest
+-- Go Forward Survivors
+-- Created: May 07, 2026 at 06:08:06 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3519700) -- Go Forward Survivors
+-- MAIN APP DEPOTS
+addappid(3519701, 1, "6c223e466f1ba2f988f7ff3aef11f5cfd42281ab307cf21397db5273ff6ec127") -- Depot 3519701
+setManifestid(3519701, "347130650275403960", 346372740)
+addappid(3519702, 1, "68aff834ff64c145e60066921ff72520b62c77c7a91de6fbc5685e89dc7483d6") -- Depot 3519702
+setManifestid(3519702, "5889250467815528957", 321665148)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 117381405)
