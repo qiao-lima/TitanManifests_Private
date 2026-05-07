24e301a0647e10ed674fb914b09e5cf3365ec4cf
qiao-lima <qiao-lima@github.com>
2026-05-08 00:00:24 +0800

    Manual Update 4235390

---
+++ 4235390.lua
@@ -0,0 +1,12 @@
+-- 4235390's Lua and Manifest Created by Hubcap Manifest
+-- Midnight Ramen Shop
+-- Created: May 07, 2026 at 11:33:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4235390) -- Midnight Ramen Shop
+-- MAIN APP DEPOTS
+addappid(4235391, 1, "09d1dc75771c0f74c65259b2c3473091767fafbe3cc84da6245298f547f4bdac") -- Depot 4235391
+setManifestid(4235391, "2389810032531680123", 210234741)
