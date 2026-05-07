165a1dbb946934d7e68715617655a078c92c919c
qiao-lima <qiao-lima@github.com>
2026-05-08 00:00:21 +0800

    Manual Update 2473350

---
+++ 2473350.lua
@@ -0,0 +1,12 @@
+-- 2473350's Lua and Manifest Created by Hubcap Manifest
+-- Huntdown: Overtime
+-- Created: May 07, 2026 at 09:49:08 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2473350) -- Huntdown: Overtime
+-- MAIN APP DEPOTS
+addappid(2473355, 1, "aa4a5990815d09efa23ef850e8d1368388e49239cc9be688991389f4e2dd9f66") -- Depot 2473355
+setManifestid(2473355, "5516704904561173269", 4515207166)
