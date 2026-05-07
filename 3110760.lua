a7c07bee49869147b67ffaea71d76d38963c01c9
qiao-lima <qiao-lima@github.com>
2026-05-08 00:00:22 +0800

    Manual Update 3110760

---
+++ 3110760.lua
@@ -0,0 +1,16 @@
+-- 3110760's Lua and Manifest Created by Hubcap Manifest
+-- Alabaster Dawn
+-- Created: May 07, 2026 at 11:43:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3110760) -- Alabaster Dawn
+-- MAIN APP DEPOTS
+addappid(3110761, 1, "03f56edb0d3d06afcd1f625b1898343e100f2e00657b86a4bea0bfbac0f341ee") -- Depot 3110761
+setManifestid(3110761, "6401245567671831929", 321890657)
+addappid(3110762, 1, "f49e304c3273bbf6b8574de212a764f730163a1ff28205c886c4f0d3fa2e35c7") -- Depot 3110762
+setManifestid(3110762, "296416421950771157", 373932871)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3110766) -- Depot 3110766 (empty depot)
