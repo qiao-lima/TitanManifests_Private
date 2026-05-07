67b1f27a8f06e419ed20a82178393617205f6939
qiao-lima <qiao-lima@github.com>
2026-05-08 00:05:20 +0800

    Manual Update 4612930

---
+++ 4612930.lua
@@ -0,0 +1,12 @@
+-- 4612930's Lua and Manifest Created by Hubcap Manifest
+-- 谐了个音 - 这是谐音梗
+-- Created: May 07, 2026 at 06:08:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4612930) -- 谐了个音 - 这是谐音梗
+-- MAIN APP DEPOTS
+addappid(4612931, 1, "78fd16ee73311fa6a8608c6068eb1c9a09f9db14a720a43e5d38d3d2802b4a2a") -- Depot 4612931
+setManifestid(4612931, "6285319489815416616", 502273984)
