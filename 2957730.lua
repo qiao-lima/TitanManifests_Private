933cb26a5ad4f3d0abf762163711465fea087df2
qiao-lima <qiao-lima@github.com>
2026-05-08 00:00:22 +0800

    Manual Update 2957730

---
+++ 2957730.lua
@@ -0,0 +1,12 @@
+-- 2957730's Lua and Manifest Created by Hubcap Manifest
+-- 离谱的炼金术
+-- Created: May 07, 2026 at 06:09:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2957730) -- 离谱的炼金术
+-- MAIN APP DEPOTS
+addappid(2957731, 1, "5af18ddc6b428e42fa6177a5db054ceff47933e26e4151649ecf60400558197b") -- Depot 2957731
+setManifestid(2957731, "6415817782037184325", 324631595)
