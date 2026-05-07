66f0bf0209b257ae418651d029d3d6735c54f159
qiao-lima <qiao-lima@github.com>
2026-05-08 00:00:21 +0800

    Manual Update 2293640

---
+++ 2293640.lua
@@ -0,0 +1,12 @@
+-- 2293640's Lua and Manifest Created by Hubcap Manifest
+-- Alter Evo
+-- Created: May 07, 2026 at 06:07:08 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2293640) -- Alter Evo
+-- MAIN APP DEPOTS
+addappid(2293641, 1, "383c1036cd12bff0f1ea65caee6f6fbf825caffb2e6d89ad4c8e59c8013d1897") -- Depot 2293641
+setManifestid(2293641, "2326464038854217299", 21478811490)
