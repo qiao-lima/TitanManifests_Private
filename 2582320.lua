c4924c563df8e2451e037ec3bac67eed6849aab5
qiao-lima <qiao-lima@github.com>
2026-05-08 00:00:22 +0800

    Manual Update 2582320

---
+++ 2582320.lua
@@ -0,0 +1,22 @@
+-- 2582320's Lua and Manifest Created by Hubcap Manifest
+-- Mixtape
+-- Created: May 07, 2026 at 09:11:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(2582320) -- Mixtape
+-- MAIN APP DEPOTS
+addappid(2582321, 1, "bf956320114c20821c9daa9195bbe47763d47fd8755086843025b3354e081337") -- Depot 2582321
+setManifestid(2582321, "4076322107703909011", 12198909061)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 117381405)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2582322) -- Depot 2582322 (empty depot)
