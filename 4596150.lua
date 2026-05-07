95f83ba7553a5c11c408e86439749fd6836f9cdf
qiao-lima <qiao-lima@github.com>
2026-05-08 00:00:24 +0800

    Manual Update 4596150

---
+++ 4596150.lua
@@ -0,0 +1,12 @@
+-- 4596150's Lua and Manifest Created by Hubcap Manifest
+-- Water Margin: Defying Heaven
+-- Created: May 07, 2026 at 11:20:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4596150) -- Water Margin: Defying Heaven
+-- MAIN APP DEPOTS
+addappid(4596151, 1, "d03565b5bceb937da9ebae833eb51ca3c6a9a329ceb9653520f4318cc0b9b61b") -- Depot 4596151
+setManifestid(4596151, "594595848042521849", 2720407398)
