1f000794e35fc3029913b26c33f359b4f6405725
qiao-lima <qiao-lima@github.com>
2026-05-08 00:00:24 +0800

    Manual Update 4125470

---
+++ 4125470.lua
@@ -0,0 +1,12 @@
+-- 4125470's Lua and Manifest Created by Hubcap Manifest
+-- The Forsaken Tomb
+-- Created: May 07, 2026 at 09:51:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4125470) -- The Forsaken Tomb
+-- MAIN APP DEPOTS
+addappid(4125471, 1, "ad4fd70ac39c97bb0f40e7d78c452072cb09bbcfb1ad19f3f1d2b10c2eba46f1") -- Depot 4125471
+setManifestid(4125471, "4899298330983620815", 384735464)
