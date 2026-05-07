5c038792c14b1a0e54b0942f2ceb25f96898f8ce
qiao-lima <qiao-lima@github.com>
2026-05-08 00:00:23 +0800

    Manual Update 3144860

---
+++ 3144860.lua
@@ -0,0 +1,18 @@
+-- 3144860's Lua and Manifest Created by Hubcap Manifest
+-- WILL: Follow the Light
+-- Created: May 07, 2026 at 09:47:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3144860) -- WILL: Follow the Light
+-- MAIN APP DEPOTS
+addappid(3144861, 1, "f483f5792b835bc140b19cbb4893740fc5f9072439dae77496ca5384c1d0137a") -- Depot 3144861
+setManifestid(3144861, "1662818817548124092", 23137489068)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
