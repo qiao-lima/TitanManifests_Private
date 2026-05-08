dd693612987f53d1d7103aa204b4a040a44a5ab9
qiao-lima <qiao-lima@github.com>
2026-05-08 09:09:06 +0800

    Manual Update 2680670

---
+++ 2680670.lua
@@ -0,0 +1,12 @@
+-- 2680670's Lua and Manifest Created by Hubcap Manifest
+-- Wardrum
+-- Created: May 07, 2026 at 12:34:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2680670) -- Wardrum
+-- MAIN APP DEPOTS
+addappid(2680671, 1, "b6afac8a63cb8c1ca7cc078ef509452f5ef89e35d7ab1a90061e0af73af6b563") -- Depot 2680671
+setManifestid(2680671, "2046844857093804801", 2260649453)
