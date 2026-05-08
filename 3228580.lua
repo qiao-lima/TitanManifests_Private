36aed4b61fadafb381ca8c1102e21009164abbc8
qiao-lima <qiao-lima@github.com>
2026-05-08 09:09:06 +0800

    Manual Update 3228580

---
+++ 3228580.lua
@@ -0,0 +1,14 @@
+-- 3228580's Lua and Manifest Created by Hubcap Manifest
+-- Stolen Realm Survivors
+-- Created: May 07, 2026 at 12:39:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3228580, 1, "ae00755a4c78da7587924a4d97d5b4f4476384eda4f8b30dd98f07c310d3477f") -- Stolen Realm Survivors
+-- MAIN APP DEPOTS
+addappid(3228581, 1, "f9205aa6022e460da618e9088f9b94d66363a4cfea34f36d20537cde7f0798de") -- Depot 3228581
+setManifestid(3228581, "6168938578214161070", 7664347447)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4685040) -- Stolen Realm Survivors  Supporter Pack
