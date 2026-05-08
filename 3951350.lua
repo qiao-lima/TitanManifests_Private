d3267e787942b0b2d5f52b22c3ee95703f6d4ae2
qiao-lima <qiao-lima@github.com>
2026-05-08 22:57:51 +0800

    Manual Update 3951350

---
+++ 3951350.lua
@@ -0,0 +1,14 @@
+-- 3951350's Lua and Manifest Created by Hubcap Manifest
+-- Ashes
+-- Created: May 08, 2026 at 04:02:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3951350) -- Ashes
+-- MAIN APP DEPOTS
+addappid(3951352, 1, "e68925c8ff19c28ad79cb61c0c0e66e943950bf5883bf8838d2d70fd47de6249") -- Depot 3951352
+setManifestid(3951352, "7120181324436996592", 2421662956)
+addappid(3951351, 1, "9bbdc5c0eadf0477b6c096a2683614d2be114701749e771b98439c368cea3859") -- Depot 3951351
+setManifestid(3951351, "2883993068443372352", 2438906076)
