141913ae54de5cee986d121d3f2f55ec51f71144
qiao-lima <qiao-lima@github.com>
2026-05-08 09:09:06 +0800

    Manual Update 3232380

---
+++ 3232380.lua
@@ -0,0 +1,12 @@
+-- 3232380's Lua and Manifest Created by Hubcap Manifest
+-- FROGGY HATES SNOW
+-- Created: May 07, 2026 at 17:20:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3232380) -- FROGGY HATES SNOW
+-- MAIN APP DEPOTS
+addappid(3232381, 1, "69a2aab568f099e226d1009d82828af5b12d8db2a7a78e52142c033c606be7cd") -- Depot 3232381
+setManifestid(3232381, "3331162739904840581", 962940448)
