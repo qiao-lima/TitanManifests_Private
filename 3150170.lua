0f358987953efc932438bf59fda32c9e22dc3910
qiao-lima <qiao-lima@github.com>
2026-05-08 22:57:50 +0800

    Manual Update 3150170

---
+++ 3150170.lua
@@ -0,0 +1,22 @@
+-- 3150170's Lua and Manifest Created by Hubcap Manifest
+-- God of Yoga
+-- Created: December 22, 2025 at 14:05:29 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 5
+-- Total DLCs: 0
+-- Shared Depots: 4
+
+-- MAIN APPLICATION
+addappid(3150170) -- God of Yoga
+-- MAIN APP DEPOTS
+addappid(3150171, 1, "1228afe3f56074410817655808ddcf0e1952613b280d59b689864190efa81bb0") -- Depot 3150171
+setManifestid(3150171, "4916717436090996308", 12918308874)
+-- SHARED DEPOTS (from other apps)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (Shared from App 228980)
+setManifestid(228983, "8124929965194586177", 19265607)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- VC 2013 Redist (Shared from App 228980)
+setManifestid(228985, "3966345552745568756", 13699237)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
