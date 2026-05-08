16bc3b5eadf02afac712492ccfbf4823a5393ed7
qiao-lima <qiao-lima@github.com>
2026-05-08 22:57:51 +0800

    Manual Update 3667730

---
+++ 3667730.lua
@@ -0,0 +1,18 @@
+-- 3667730's Lua and Manifest Created by Hubcap Manifest
+-- Console Store Simulator
+-- Created: May 07, 2026 at 12:45:58 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3667730) -- Console Store Simulator
+-- MAIN APP DEPOTS
+addappid(3667731, 1, "18f06bdd2e91fe2f5408119808bea507129ea34b2c75a10af07f4cbd554fbb4c") -- Depot 3667731
+setManifestid(3667731, "8020430189652008534", 4684642259)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
