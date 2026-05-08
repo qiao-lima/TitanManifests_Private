a257a42ae60f27750ff36a431cc26e13a9765300
qiao-lima <qiao-lima@github.com>
2026-05-08 22:57:53 +0800

    Manual Update 4637960

---
+++ 4637960.lua
@@ -0,0 +1,12 @@
+-- 4637960's Lua and Manifest Created by Hubcap Manifest
+-- Blessed To Die
+-- Created: May 08, 2026 at 04:03:06 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4637960) -- Blessed To Die
+-- MAIN APP DEPOTS
+addappid(4637961, 1, "9a979d60d13e77019d61585a8f6b26002571eb156076c19ffe09487aee683397") -- Depot 4637961
+setManifestid(4637961, "8204012308554144716", 735809214)
