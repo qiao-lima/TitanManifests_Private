247fc687c0a5121fbe63060f597795a83eb8d306
qiao-lima <qiao-lima@github.com>
2026-05-08 22:57:53 +0800

    Manual Update 4643990

---
+++ 4643990.lua
@@ -0,0 +1,12 @@
+-- 4643990's Lua and Manifest Created by Hubcap Manifest
+-- 放置：武器工坊
+-- Created: May 08, 2026 at 04:01:25 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4643990) -- 放置：武器工坊
+-- MAIN APP DEPOTS
+addappid(4643991, 1, "fbe2465f9f1a03fddf510f623530d040f4b9f39d16326ab67768676568641e92") -- Depot 4643991
+setManifestid(4643991, "3393904811405187322", 2333099789)
