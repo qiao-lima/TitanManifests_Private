57a4301db99d3eff412c9c0c0d5e660662b1bb04
qiao-lima <qiao-lima@github.com>
2026-05-08 09:09:07 +0800

    Manual Update 4020250

---
+++ 4020250.lua
@@ -0,0 +1,16 @@
+-- 4020250's Lua and Manifest Created by Hubcap Manifest
+-- Bochi Collection
+-- Created: May 07, 2026 at 21:08:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4020250) -- Bochi Collection
+-- MAIN APP DEPOTS
+addappid(4020251, 1, "64a5b2d7e4e2a0ce536bf1e6af9cf4630009b3332edfe25d1b8f9487acf3e220") -- Depot 4020251
+setManifestid(4020251, "2955958841343697164", 732587002)
+addappid(4020252, 1, "347f5188da57b7b39a9578a63e3d192bad4a2026f73442f1909f722c4a3ff76b") -- Depot 4020252
+setManifestid(4020252, "596272295667461271", 748690232)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4594890) -- Bochi Collection - Supporter Pack
