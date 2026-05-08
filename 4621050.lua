06f09b5ccc8cc802706d051984070fb7b89b9ca1
qiao-lima <qiao-lima@github.com>
2026-05-08 22:57:53 +0800

    Manual Update 4621050

---
+++ 4621050.lua
@@ -0,0 +1,18 @@
+-- 4621050's Lua and Manifest Created by Hubcap Manifest
+-- SAI-1
+-- Created: May 08, 2026 at 04:01:13 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4621050) -- SAI-1
+-- MAIN APP DEPOTS
+addappid(4621051, 1, "f6a9f8078857fefa98893328b080e5e0fec6a5c286cc11fde0ba19806cd898cc") -- Depot 4621051
+setManifestid(4621051, "5396429736021630530", 802191479)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
