776a5ca2d7924982bf485b565510667118e0ac8c
qiao-lima <qiao-lima@github.com>
2026-05-08 09:09:06 +0800

    Manual Update 2654490

---
+++ 2654490.lua
@@ -0,0 +1,14 @@
+-- 2654490's Lua and Manifest Created by Hubcap Manifest
+-- Relic Arena
+-- Created: May 07, 2026 at 13:26:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2654490) -- Relic Arena
+-- MAIN APP DEPOTS
+addappid(2654491, 1, "5072320c3b13027e6caa364fb7cfee5bb59e2d0c808731dfb4f728e65a7beed3") -- Depot 2654491
+setManifestid(2654491, "5813694242125511657", 3633646470)
+addappid(2654492, 1, "6016e2a6b0729717cab50c1a927a4dce4971ffc1dfd9ee0773e354a2ff5cec12") -- Depot 2654492
+setManifestid(2654492, "7354368265332769580", 3582367473)
