ad830d621bad3a344d09795eb78991e04841b37b
qiao-lima <qiao-lima@github.com>
2026-05-08 09:09:06 +0800

    Manual Update 2478750

---
+++ 2478750.lua
@@ -0,0 +1,12 @@
+-- 2478750's Lua and Manifest Created by Hubcap Manifest
+-- Demon Hunter
+-- Created: May 07, 2026 at 14:38:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2478750) -- Demon Hunter
+-- MAIN APP DEPOTS
+addappid(2478751, 1, "9d06b43884e298f9002aa5e87b9bec349c25028b56838cd7b19eea3df370cc58") -- Depot 2478751
+setManifestid(2478751, "1319187935367581440", 5288839395)
