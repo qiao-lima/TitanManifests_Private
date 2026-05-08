9e1963b5cfec2dacc4dfd6ef7791f4d3cc51a19e
qiao-lima <qiao-lima@github.com>
2026-05-08 19:43:34 +0800

    Manual Update 2445690

---
+++ 2445690.lua
@@ -0,0 +1,15 @@
+-- 2445690's Lua and Manifest Created by Hubcap Manifest
+-- Lost Castle 2
+-- Created: May 08, 2026 at 04:02:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2445690, 1, "6e39d0da9d7ac3fbc396e7db8e789a71a59d33412790e89ff97fbf608ee82cae") -- Lost Castle 2
+addtoken(2445690, "12154854772648009173")
+-- MAIN APP DEPOTS
+addappid(2445691, 1, "58aa213ce335109a11490383c49a406f0bb7a5c68f2844e33e7883c4e70dc1b3") -- Depot 2445691
+setManifestid(2445691, "6282477459675613384", 2373004920)
+addappid(2445695, 1, "91b6b4a727098238ce348dc1f01e054f447397b059c1523e79f9ebf765484d74") -- Depot 2445695
+setManifestid(2445695, "7117476379333716026", 2373004920)
