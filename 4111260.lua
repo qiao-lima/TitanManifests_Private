60044b4deade3687f29bbb951a3ed802955ed090
qiao-lima <qiao-lima@github.com>
2026-05-08 09:09:08 +0800

    Manual Update 4111260

---
+++ 4111260.lua
@@ -0,0 +1,18 @@
+-- 4111260's Lua and Manifest Created by Hubcap Manifest
+-- 恶意
+-- Created: May 07, 2026 at 12:06:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4111260) -- 恶意
+-- MAIN APP DEPOTS
+addappid(4111261, 1, "b9a0ef6c02592ab3a2ed2b79b0693ee1c9cc1de1af3fef87598c25595d4fb85d") -- Depot 4111261
+setManifestid(4111261, "998039935520277204", 12241590138)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
