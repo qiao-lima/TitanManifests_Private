531bd71e6d37838de0e1f3ad400204c014608001
qiao-lima <qiao-lima@github.com>
2026-05-08 09:09:07 +0800

    Manual Update 3441280

---
+++ 3441280.lua
@@ -0,0 +1,12 @@
+-- 3441280's Lua and Manifest Created by Hubcap Manifest
+-- The Greenening
+-- Created: May 07, 2026 at 13:43:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3441280) -- The Greenening
+-- MAIN APP DEPOTS
+addappid(3441281, 1, "26373e389247fb1413650a0cd67a85e404c654c371244f1ce08dad378fa4a77f") -- Depot 3441281
+setManifestid(3441281, "4924559181903222934", 373473081)
