b47f83ff120ac95c556fea8490db88ac17ac8307
qiao-lima <qiao-lima@github.com>
2026-05-08 22:57:52 +0800

    Manual Update 4193320

---
+++ 4193320.lua
@@ -0,0 +1,12 @@
+-- 4193320's Lua and Manifest Created by Hubcap Manifest
+-- Olivia
+-- Created: May 08, 2026 at 08:11:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4193320) -- Olivia
+-- MAIN APP DEPOTS
+addappid(4193321, 1, "ebfd2e17a24cfc1c17e86212418a93a496992fd3ef1305059f2fcb910dc6832e") -- Depot 4193321
+setManifestid(4193321, "8677009029019852440", 1446959164)
