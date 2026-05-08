8d36e5cd1794067aed49ab4e04d75d11db23bb18
qiao-lima <qiao-lima@github.com>
2026-05-08 22:57:51 +0800

    Manual Update 3844750

---
+++ 3844750.lua
@@ -0,0 +1,12 @@
+-- 3844750's Lua and Manifest Created by Hubcap Manifest
+-- FlatOut 4: Total Insanity VR
+-- Created: May 07, 2026 at 21:50:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3844750) -- FlatOut 4: Total Insanity VR
+-- MAIN APP DEPOTS
+addappid(3844751, 1, "7bc934bf7622cd1bd236d0164a7e9c9496a3d2f0d6bdac0f880b8d413f1b85d7") -- Depot 3844751
+setManifestid(3844751, "702913832066582012", 37902298478)
