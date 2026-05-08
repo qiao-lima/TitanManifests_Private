d9424e9d249f4a6cb74df32573d690417d878665
qiao-lima <qiao-lima@github.com>
2026-05-08 22:57:50 +0800

    Manual Update 2381060

---
+++ 2381060.lua
@@ -0,0 +1,12 @@
+-- 2381060's Lua and Manifest Created by Hubcap Manifest
+-- Alnico Smithery
+-- Created: May 08, 2026 at 04:02:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2381060) -- Alnico Smithery
+-- MAIN APP DEPOTS
+addappid(2381061, 1, "4c344fe90d8c275833c85c7184f99ab8888bda8d4b5a125e17da83e4ccb57444") -- Depot 2381061
+setManifestid(2381061, "4228155861242345147", 404872378)
