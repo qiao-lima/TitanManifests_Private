6e9bf55e4673fad8db95b4e05551ea173fd74490
qiao-lima <qiao-lima@github.com>
2026-05-08 22:57:53 +0800

    Manual Update 4590920

---
+++ 4590920.lua
@@ -0,0 +1,16 @@
+-- 4590920's Lua and Manifest Created by Hubcap Manifest
+-- CatToySimulator
+-- Created: May 08, 2026 at 04:01:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(4590920) -- CatToySimulator
+-- MAIN APP DEPOTS
+addappid(4590921, 1, "bfce35ae787acf9a9b4297b879e25248352fef44af4dd05e18e99954ee50670b") -- Depot 4590921
+setManifestid(4590921, "8323504814736900258", 206096095)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
