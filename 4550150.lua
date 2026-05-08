11478656e9882c5d1e85acb2eccdc552c6d2047f
qiao-lima <qiao-lima@github.com>
2026-05-08 09:09:08 +0800

    Manual Update 4550150

---
+++ 4550150.lua
@@ -0,0 +1,12 @@
+-- 4550150's Lua and Manifest Created by Hubcap Manifest
+-- Get Rid Of Those Corners
+-- Created: May 07, 2026 at 12:40:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4550150) -- Get Rid Of Those Corners
+-- MAIN APP DEPOTS
+addappid(4550151, 1, "ecd8a31ba6e0bf788dbad500262d9677daed9404fad84ba213cf44ecc2b69f45") -- Depot 4550151
+setManifestid(4550151, "4125309828863504594", 1023142605)
