935b442001db620ac5395885c9ce8f2150db3d2d
qiao-lima <qiao-lima@github.com>
2026-05-08 22:57:50 +0800

    Manual Update 1936020

---
+++ 1936020.lua
@@ -0,0 +1,12 @@
+-- 1936020's Lua and Manifest Created by Hubcap Manifest
+-- Summer Beach
+-- Created: April 30, 2026 at 17:16:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1936020) -- Summer Beach
+-- MAIN APP DEPOTS
+addappid(1936021, 1, "00b7c04293f112b3b3488b23f9df2f30fc3b7c1d0789d33daf2a0de0310c253c") -- Depot 1936021
+setManifestid(1936021, "4524703219700761513", 16111782794)
