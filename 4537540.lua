e4e694eb60e235ec4da02fbcd0ca6c05c47872eb
qiao-lima <qiao-lima@github.com>
2026-05-08 22:57:52 +0800

    Manual Update 4537540

---
+++ 4537540.lua
@@ -0,0 +1,12 @@
+-- 4537540's Lua and Manifest Created by Hubcap Manifest
+-- 海神捕鱼
+-- Created: May 07, 2026 at 21:55:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4537540) -- 海神捕鱼
+-- MAIN APP DEPOTS
+addappid(4537541, 1, "f4fb9374995a3dca4228e2cc409710b473b37a1df4cc41b589b27b8c09126373") -- Depot 4537541
+setManifestid(4537541, "609589984897555405", 137313671)
