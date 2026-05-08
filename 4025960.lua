3e0320ed2fea2543cdf3e7e6a0d549a2cf9d8997
qiao-lima <qiao-lima@github.com>
2026-05-08 09:09:08 +0800

    Manual Update 4025960

---
+++ 4025960.lua
@@ -0,0 +1,12 @@
+-- 4025960's Lua and Manifest Created by Hubcap Manifest
+-- Lost Files: 1978
+-- Created: May 07, 2026 at 12:40:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4025960) -- Lost Files: 1978
+-- MAIN APP DEPOTS
+addappid(4025961, 1, "f072004341ca709109b4d792959a5373dd3e8725b4777dc893eb7cedd5bc9f93") -- Depot 4025961
+setManifestid(4025961, "1467622067388277090", 1951638987)
