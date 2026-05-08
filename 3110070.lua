7502e17a210135a85621bae683bb61e4eb18e746
qiao-lima <qiao-lima@github.com>
2026-05-08 22:57:50 +0800

    Manual Update 3110070

---
+++ 3110070.lua
@@ -0,0 +1,20 @@
+-- 3110070's Lua and Manifest Created by Hubcap Manifest
+-- Fever Meme
+-- Created: May 02, 2026 at 20:10:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3110070) -- Fever Meme
+-- MAIN APP DEPOTS
+addappid(3110071, 1, "aa68431c19f848b6c30f1f201b4579dc95275533fa1b64c2b8f5fb8a74c50a66") -- Depot 3110071
+setManifestid(3110071, "2922425634866942683", 2380962419)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3110072) -- Depot 3110072 (empty depot)
