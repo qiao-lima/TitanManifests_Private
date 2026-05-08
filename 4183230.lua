ddf27b2f72c8af5c97714e89534a22bfad9de247
qiao-lima <qiao-lima@github.com>
2026-05-08 22:57:51 +0800

    Manual Update 4183230

---
+++ 4183230.lua
@@ -0,0 +1,12 @@
+-- 4183230's Lua and Manifest Created by Hubcap Manifest
+-- In Trusted Hands
+-- Created: May 07, 2026 at 13:27:08 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4183230) -- In Trusted Hands
+-- MAIN APP DEPOTS
+addappid(4183231, 1, "a84a2dd90224267698d06db85fc0034f0da8827aa84693a82d5abbe347977f37") -- Depot 4183231
+setManifestid(4183231, "8330964368085102387", 796342226)
