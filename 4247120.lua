2e38167099802b2b587869ee066bdb8601f6f46b
qiao-lima <qiao-lima@github.com>
2026-05-08 22:57:52 +0800

    Manual Update 4247120

---
+++ 4247120.lua
@@ -0,0 +1,18 @@
+-- 4247120's Lua and Manifest Created by Hubcap Manifest
+-- Bumper Blitz TD
+-- Created: May 08, 2026 at 04:04:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4247120) -- Bumper Blitz TD
+-- MAIN APP DEPOTS
+addappid(4247121, 1, "2a6da8633b8c966f9d7f7a38bb7d1ad4c9ee1d6828715774c05d6bd0eef81cf8") -- Depot 4247121
+setManifestid(4247121, "7130134896241412308", 622074135)
+addappid(4247122, 1, "63d5869f1e1b4e65c5071a8ca01a7b718e8fec5e93543b254ce1e3fd5a077ec9") -- Depot 4247122
+setManifestid(4247122, "8799299034908526218", 704121597)
+addappid(4247123, 1, "8f99c9457074bde614567eb9be4880ced23cac566f0e05b985f87f2cf5ad1a28") -- Depot 4247123
+setManifestid(4247123, "3321259813179387246", 653393836)
+addappid(4247124, 1, "e9f3a5f2aba11ac2deba864f0d41d192a3386b4777675c1d518b5017de39ed09") -- Depot 4247124
+setManifestid(4247124, "8168311342621306973", 524211353)
