056e1248d0c1072eacb51b30c1424bf8456d33ac
qiao-lima <qiao-lima@github.com>
2026-05-08 22:57:53 +0800

    Manual Update 4608160

---
+++ 4608160.lua
@@ -0,0 +1,12 @@
+-- 4608160's Lua and Manifest Created by Hubcap Manifest
+-- Gin and Wall
+-- Created: May 08, 2026 at 04:02:36 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4608160) -- Gin and Wall
+-- MAIN APP DEPOTS
+addappid(4608161, 1, "c159ff08f92d30a97bc344a8f352f36c43a22b0b607eedea93c85ce1306fd25c") -- Depot 4608161
+setManifestid(4608161, "3199384209111082366", 67301201)
