86c42f40bf7278a3f04f7061c365efa02e0b5070
qiao-lima <qiao-lima@github.com>
2026-05-08 22:57:52 +0800

    Manual Update 4282200

---
+++ 4282200.lua
@@ -0,0 +1,18 @@
+-- 4282200's Lua and Manifest Created by Hubcap Manifest
+-- Pharma Craft
+-- Created: May 08, 2026 at 04:04:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4282200) -- Pharma Craft
+-- MAIN APP DEPOTS
+addappid(4282201, 1, "6ff2199abb1739f6328d92afca4b304e70d13e3489203b77348bc722397e095f") -- Depot 4282201
+setManifestid(4282201, "6523508320892185395", 3297445171)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
