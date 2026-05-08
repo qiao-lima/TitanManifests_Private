3ccde02edc1e86fa95d7d1d90353c02bcb1295c3
qiao-lima <qiao-lima@github.com>
2026-05-08 22:57:52 +0800

    Manual Update 4391420

---
+++ 4391420.lua
@@ -0,0 +1,18 @@
+-- 4391420's Lua and Manifest Created by Hubcap Manifest
+-- Spymaster
+-- Created: May 08, 2026 at 04:02:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4391420) -- Spymaster
+-- MAIN APP DEPOTS
+addappid(4391421, 1, "57ba2e61c2d97f12fbd5de7ce98f6aabacf98229ad11e0936d1a311b80246cba") -- Depot 4391421
+setManifestid(4391421, "348102794869004145", 2847665828)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
