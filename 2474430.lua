92af353f2d110eaf1cefc01ac6c324cc682a750f
qiao-lima <qiao-lima@github.com>
2026-05-08 22:57:50 +0800

    Manual Update 2474430

---
+++ 2474430.lua
@@ -0,0 +1,18 @@
+-- 2474430's Lua and Manifest Created by Hubcap Manifest
+-- TetherGeist
+-- Created: May 08, 2026 at 00:11:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(2474430) -- TetherGeist
+-- MAIN APP DEPOTS
+addappid(2474431, 1, "572ea146209713c8b7487150b512b652eb5d79b2a47417aba9c89fff9227b170") -- Depot 2474431
+setManifestid(2474431, "3921612521992569466", 432255021)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- TetherGeist Digital Art Book (AppID: 4641470) - missing depot keys
+-- addappid(4641470)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2474432) -- Depot 2474432 (empty depot)
