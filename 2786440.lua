a3626d68676590ae525bd294ee1350fce504c56d
qiao-lima <qiao-lima@github.com>
2026-05-09 08:58:49 +0800

    Manual Update 2786440

---
+++ 2786440.lua
@@ -0,0 +1,12 @@
+-- 2786440's Lua and Manifest Created by Hubcap Manifest
+-- Eko and the Bewitched lands
+-- Created: May 08, 2026 at 14:12:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2786440) -- Eko and the Bewitched lands
+-- MAIN APP DEPOTS
+addappid(2786441, 1, "07f7c5c9bd53fef351c4c1d179e0f09a3facb2cabd499d2b574e51300659ab40") -- Depot 2786441
+setManifestid(2786441, "3192206989981769568", 704622338)
