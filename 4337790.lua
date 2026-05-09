77cd3cb3fa7a62293a41afac2c516d47b43c73fd
qiao-lima <qiao-lima@github.com>
2026-05-09 08:58:51 +0800

    Manual Update 4337790

---
+++ 4337790.lua
@@ -0,0 +1,24 @@
+-- 4337790's Lua and Manifest Created by Hubcap Manifest
+-- Akuma Rise
+-- Created: May 08, 2026 at 14:18:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 5
+
+-- MAIN APPLICATION
+addappid(4337790) -- Akuma Rise
+-- MAIN APP DEPOTS
+addappid(4337791, 1, "1084601da3bb181dcf233f41b062c46263c2850f59274b47ad4288521a66e899") -- Depot 4337791
+setManifestid(4337791, "4886800407869162843", 944096210)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4337910) -- Experience x3 - Akuma Rise
+addappid(4337920) -- Orb EXP x3 - Akuma Rise
+addappid(4337930) -- Experience  Orb EXP x2 - Akuma Rise
+addappid(4337940) -- Damage x2 - Akuma Rise
+addappid(4337960) -- No Skill Cost - Akuma Rise
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4337910) -- Depot 4337910 (empty depot)
+-- addappid(4337920) -- Depot 4337920 (empty depot)
+-- addappid(4337930) -- Depot 4337930 (empty depot)
+-- addappid(4337940) -- Depot 4337940 (empty depot)
+-- addappid(4337960) -- Depot 4337960 (empty depot)
