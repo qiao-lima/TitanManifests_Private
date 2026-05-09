ede33b748ac4456e69021a190fa7a345b79329a6
qiao-lima <qiao-lima@github.com>
2026-05-09 11:54:33 +0800

    Manual Update 3802470

---
+++ 3802470.lua
@@ -0,0 +1,18 @@
+-- 3802470's Lua and Manifest Created by Hubcap Manifest
+-- Turnbound
+-- Created: May 06, 2026 at 14:40:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3802470, 1, "283045d92bf4a156bb5b16f3cc8b21eff20b4dcc2e2ba81cf1b1a41ddc8dff66") -- Turnbound
+-- MAIN APP DEPOTS
+addappid(3802471, 1, "442c7d3ae80e78480b3ee434166c940f7dfad83b2c876b0223c43ac080a6f2b5") -- Depot 3802471
+setManifestid(3802471, "1774002186426229268", 443059216)
+addappid(3802472, 1, "03e37f4836a7705db6f70b572b8901caaa488432f7749178bcd468786443a5a6") -- Depot 3802472
+setManifestid(3802472, "320511792769566705", 425196216)
+addappid(3802473, 1, "1c7ce28c13f9315d9fcd4900154b6094841283a4c5032971e22bf1616794af7f") -- Depot 3802473
+setManifestid(3802473, "3356630415040551563", 540220115)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4314960) -- Founders Edition Content
