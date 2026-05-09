97af90535ee6554abd762362a40df7421801f9e8
qiao-lima <qiao-lima@github.com>
2026-05-09 08:58:50 +0800

    Manual Update 3804180

---
+++ 3804180.lua
@@ -0,0 +1,12 @@
+-- 3804180's Lua and Manifest Created by Hubcap Manifest
+-- A summer in Oneeshota Town
+-- Created: May 08, 2026 at 05:25:06 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3804180) -- A summer in Oneeshota Town
+-- MAIN APP DEPOTS
+addappid(3804184, 1, "1381ea187105943123c06a4335fe799d31ab37b6316edb078e1ed022cabab5a2") -- Depot 3804184
+setManifestid(3804184, "1160224311783552603", 693513509)
