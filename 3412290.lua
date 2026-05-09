ee4b37461129849b4ac06c2653588d7b330b590e
qiao-lima <qiao-lima@github.com>
2026-05-09 08:58:50 +0800

    Manual Update 3412290

---
+++ 3412290.lua
@@ -0,0 +1,22 @@
+-- 3412290's Lua and Manifest Created by Hubcap Manifest
+-- NTRaholic
+-- Created: April 17, 2026 at 19:25:03 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3412290) -- NTRaholic
+-- MAIN APP DEPOTS
+addappid(3412291, 1, "6ffadbf13b82f0bb9451503db899a299c83c0e2289e8bcb117282226421a96cc") -- Depot 3412291
+setManifestid(3412291, "7271651349746946200", 5625069690)
+-- DLCS WITH DEDICATED DEPOTS
+-- NTRaholic - ArtBook (AppID: 4234740)
+addappid(4234740)
+addappid(4234742, 1, "d06d3db6b9ed92524202410c2439c5457f4054ecc47d9f4331bc09737d9b1d11") -- NTRaholic - ArtBook - Depot 4234742
+setManifestid(4234742, "2351154192606060678", 30814406)
+addappid(4234741, 1, "e60f623fd5a69d4c4e53f474549a7e3a3b96b64de85d0fd9f8032d4ed6cf11a3") -- NTRaholic - ArtBook - Depot 4234741
+setManifestid(4234741, "891903234942766292", 30738401)
+-- addappid(4234743, 1, "MISSING_KEY") -- NTRaholic - ArtBook - Depot 4234743 (no key available)
+-- addappid(4234744, 1, "MISSING_KEY") -- NTRaholic - ArtBook - Depot 4234744 (no key available)
+-- addappid(4234745, 1, "MISSING_KEY") -- NTRaholic - ArtBook - Depot 4234745 (no key available)
