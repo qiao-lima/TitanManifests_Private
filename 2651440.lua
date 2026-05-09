8b3013d8d11da4566b6bc2f9ba450f73c3359f62
qiao-lima <qiao-lima@github.com>
2026-05-09 08:58:49 +0800

    Manual Update 2651440

---
+++ 2651440.lua
@@ -0,0 +1,12 @@
+-- 2651440's Lua and Manifest Created by Hubcap Manifest
+-- Fallen Elf Freya
+-- Created: March 26, 2026 at 22:52:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2651440) -- Fallen Elf Freya
+-- MAIN APP DEPOTS
+addappid(2651441, 1, "9f20b1b76e295cdd391c66998004ddd2c8ebaa8777e96a4b5c067ef1a38bacb4") -- Depot 2651441
+setManifestid(2651441, "6776615499298707681", 1443197839)
