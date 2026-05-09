7d70128bf9dfb6503461838d4b62f309d41757ca
qiao-lima <qiao-lima@github.com>
2026-05-09 08:58:51 +0800

    Manual Update 4227680

---
+++ 4227680.lua
@@ -0,0 +1,12 @@
+-- 4227680's Lua and Manifest Created by Hubcap Manifest
+-- SEX Shop Simulator: Forbidden Service
+-- Created: May 08, 2026 at 07:33:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4227680) -- SEX Shop Simulator: Forbidden Service
+-- MAIN APP DEPOTS
+addappid(4227681, 1, "692467b2c2ba86ccfa3c07671d3fd941377fd2e2accd8251587a26db398c7d91") -- Depot 4227681
+setManifestid(4227681, "6199329220026937196", 12693782590)
