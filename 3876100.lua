fe53410413fd0c5e9875df4b04be4e2cf4fffafd
qiao-lima <qiao-lima@github.com>
2026-05-09 08:58:50 +0800

    Manual Update 3876100

---
+++ 3876100.lua
@@ -0,0 +1,14 @@
+-- 3876100's Lua and Manifest Created by Hubcap Manifest
+-- Cosmonade
+-- Created: May 08, 2026 at 20:57:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3876100) -- Cosmonade
+-- MAIN APP DEPOTS
+addappid(3876101, 1, "8d97fa2eda34711d01d669d97d8c2ae2f9e1a8630dd7d0d649ade4ef3c4345a7") -- Depot 3876101
+setManifestid(3876101, "8541078335388225562", 366783410)
+addappid(3876102, 1, "b332afb940f6a5febb152fd7c8b7259cc0667c49c2200eabfd3a2012a1e263c0") -- Depot 3876102
+setManifestid(3876102, "431723197534395854", 403816479)
