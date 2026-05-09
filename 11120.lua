f289347d7c6102a5a75bf0c2d4187e24e26b6d0b
qiao-lima <qiao-lima@github.com>
2026-05-09 08:58:48 +0800

    Manual Update 11120

---
+++ 11120.lua
@@ -0,0 +1,12 @@
+-- 11120's Lua and Manifest Created by Hubcap Manifest
+-- Pro Cycling Manager Season 2009
+-- Created: May 08, 2026 at 12:07:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(11120) -- Pro Cycling Manager Season 2009
+-- MAIN APP DEPOTS
+addappid(11121, 1, "3bef1e279cfcdfb79597522370280f9c1fb5a7a26534ff3e74428b22a0f5d0c7") -- Pro Cycling Manager Season 2009 content
+setManifestid(11121, "3587892518878585296", 2354080764)
