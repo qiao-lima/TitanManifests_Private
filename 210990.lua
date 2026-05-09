39a992d940b5889d668bb4920604143c428934fe
qiao-lima <qiao-lima@github.com>
2026-05-09 08:58:48 +0800

    Manual Update 210990

---
+++ 210990.lua
@@ -0,0 +1,39 @@
+-- 210990's Lua and Manifest Created by Hubcap Manifest
+-- Pro Cycling Manager 2012
+-- Created: May 08, 2026 at 12:08:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 10
+-- Total DLCs: 3
+
+-- MAIN APPLICATION
+addappid(210990) -- Pro Cycling Manager 2012
+-- MAIN APP DEPOTS
+addappid(210991, 1, "2eda21a6440d3b607a24faa701c42ee0d0f9cdb350ee854f9206b86695ab05b8") -- Pro Cycling Manager 2012 Common
+setManifestid(210991, "3416685155049752080", 6647169454)
+addappid(210992, 1, "5f5ac2a99c5fa136f52b73499bae2002df07dcabfc36fd6484f6e67f6625e6bf") -- Pro Cycling Manager 2012 english
+setManifestid(210992, "7843411867704327367", 81)
+addappid(210993, 1, "32c3a6dc26f8b34d49ce7c1538e543872004bd7d88050534c13f6bd3a1525372") -- Pro Cycling Manager 2012 french
+setManifestid(210993, "1937261593945635758", 81)
+addappid(210994, 1, "f6d6ffb69e554f2cba91eddf8ed961beeac4fa3b639239d120d4dd21601623d1") -- Pro Cycling Manager 2012 italian
+setManifestid(210994, "5084890987240530351", 82)
+addappid(210995, 1, "69ac64f4da34055be6fd399860bd16afd3a10250af2bf7fdf23a97249554c661") -- Pro Cycling Manager 2012 german
+setManifestid(210995, "8744306737731065270", 80)
+addappid(210996, 1, "b50c423faea22b8a23d754c3d49ffebe7bee1688ab91b81a0190372bb1cbfdc7") -- Pro Cycling Manager 2012 spanish
+setManifestid(210996, "177108903838359754", 82)
+addappid(210997, 1, "df0fdda3feae5ac1d797544ddae1a1c1bfad3e8323c35f8e26c19f4c6072ee39") -- Pro Cycling Manager 2012 Dutch
+setManifestid(210997, "3625937975971773506", 80)
+addappid(210998, 1, "d4baa465a147e7324643528b453333eccf676d77bdb022846495302d84bf0570") -- Pro Cycling Manager 2012 Portuguese
+setManifestid(210998, "5047135132661181269", 85)
+-- DLCS WITH DEDICATED DEPOTS
+-- pro cycling manager polish (AppID: 210999)
+addappid(210999)
+addappid(210999, 1, "4a60ecbc9d274189bbeac0ff07c9ed4620ab3873fe42e226f1797cfb9402c104") -- pro cycling manager polish - Pro Cycling Manager 2012 Polish
+setManifestid(210999, "8678100738309275185", 81)
+-- Pro Cycling Manager 2012 DLC 1 (AppID: 211001)
+addappid(211001)
+addappid(211001, 1, "5df6748afeccb3f44c2296de8b431f8da1364347914b1f508bb97a2187ef78fb") -- Pro Cycling Manager 2012 DLC 1 - Stage Pack DLC 1
+setManifestid(211001, "5506820309826411768", 796767660)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(211000) -- Pro Cycling Manager 2012 DLC 2
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(211000) -- Depot 211000 (empty depot)
