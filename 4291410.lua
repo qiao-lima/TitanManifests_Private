5badd2963f46c3e43d64902b7dd0204f0ba6b55b
qiao-lima <qiao-lima@github.com>
2026-05-09 08:58:51 +0800

    Manual Update 4291410

---
+++ 4291410.lua
@@ -0,0 +1,20 @@
+-- 4291410's Lua and Manifest Created by Hubcap Manifest
+-- Nurture
+-- Created: May 08, 2026 at 07:41:53 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(4291410) -- Nurture
+-- MAIN APP DEPOTS
+addappid(4291411, 1, "486303eb1f71e31f9baf4473ec52d6b751422a9aee33c719b41ac929230d03a2") -- Depot 4291411
+setManifestid(4291411, "1885858841299262892", 3220211753)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
