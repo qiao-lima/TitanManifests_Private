b907b1cd6235cd29814bb8e58045fe8a715d764c
qiao-lima <qiao-lima@github.com>
2026-05-09 08:58:52 +0800

    Manual Update 4554740

---
+++ 4554740.lua
@@ -0,0 +1,12 @@
+-- 4554740's Lua and Manifest Created by Hubcap Manifest
+-- Ice Cream Simulator - Prologue
+-- Created: May 08, 2026 at 07:46:59 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4554740) -- Ice Cream Simulator - Prologue
+-- MAIN APP DEPOTS
+addappid(4554741, 1, "20b1963b02ea8a455dd8f54253cb2e68ef12b6ff0c5b6890946029c59f79359d") -- Depot 4554741
+setManifestid(4554741, "6578758975300981519", 9100276369)
