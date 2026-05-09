932a02ae089efc635e2a6abd868ecb6fae9b32ae
qiao-lima <qiao-lima@github.com>
2026-05-09 08:58:51 +0800

    Manual Update 4317500

---
+++ 4317500.lua
@@ -0,0 +1,18 @@
+-- 4317500's Lua and Manifest Created by Hubcap Manifest
+-- Cat in Temple
+-- Created: May 08, 2026 at 14:20:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4317500) -- Cat in Temple
+-- MAIN APP DEPOTS
+addappid(4317501, 1, "2bbad212576b0ef5c487b547f21f2aa2c59410ccdabe886c95580bfed849a0e0") -- Depot 4317501
+setManifestid(4317501, "8918896918786337671", 714359345)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
