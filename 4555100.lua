dc2fca109bd4ed86facd525a831a45af9afd77af
qiao-lima <qiao-lima@github.com>
2026-05-09 08:58:52 +0800

    Manual Update 4555100

---
+++ 4555100.lua
@@ -0,0 +1,14 @@
+-- 4555100's Lua and Manifest Created by Hubcap Manifest
+-- War Room Football
+-- Created: May 08, 2026 at 14:14:13 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4555100, 1, "484cf45382a7374c0468514cb386c1464da742feb751bb4eac8969196a7f5009") -- War Room Football
+-- MAIN APP DEPOTS
+addappid(4555101, 1, "66f94ec5027aac32bfd6193f8320fae583a96ce54d462a1dd67c50a8bcb2e1be") -- Depot 4555101
+setManifestid(4555101, "2407738388024051625", 538378381)
+addappid(4555102, 1, "b66853f392b7523d44573442abdba04651453434398c3a437f733d9e684d8d5e") -- Depot 4555102
+setManifestid(4555102, "3023150098367551529", 382220757)
