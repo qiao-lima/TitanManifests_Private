f23dfa883443f7841ab1624fde30c3400ce345b2
qiao-lima <qiao-lima@github.com>
2026-05-09 08:58:49 +0800

    Manual Update 2248330

---
+++ 2248330.lua
@@ -0,0 +1,18 @@
+-- 2248330's Lua and Manifest Created by Hubcap Manifest
+-- Backrooms Break
+-- Created: September 28, 2025 at 23:54:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2248330) -- Backrooms Break
+-- MAIN APP DEPOTS
+addappid(2248332, 1, "7b582b092523bf76efb37236eb87a6753ce32e2553bee033388fdb83a8fdef39") -- Depot 2248332
+setManifestid(2248332, "2094194331924763873", 22919973249)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
