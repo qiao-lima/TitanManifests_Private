1aa349f56dc61de287772abc27f2d01fbbdf3f47
qiao-lima <qiao-lima@github.com>
2026-05-09 08:58:49 +0800

    Manual Update 219800

---
+++ 219800.lua
@@ -0,0 +1,38 @@
+-- 219800's Lua and Manifest Created by Hubcap Manifest
+-- Pro Cycling Manager 2013
+-- Created: May 08, 2026 at 12:08:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 13
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(219800) -- Pro Cycling Manager 2013
+-- MAIN APP DEPOTS
+addappid(219801, 1, "a0fa4e2d3379f66d52b323b74bddf0b2bb43245f03c3bc14f73718c5e364e2ec") -- Main content
+setManifestid(219801, "6399186124343364145", 10436764140)
+addappid(219802, 1, "482121c82f2b9917e5707c7005aad0f043040139a236a7c463ef6526cca26079") -- English language
+setManifestid(219802, "3191658844110662133", 81)
+addappid(219803, 1, "56ca182fc69aa76b5a4e462f536819ac0c0e2efab80262f3eb20c26481597fb1") -- French language
+setManifestid(219803, "1397144728760979455", 81)
+addappid(219804, 1, "b0a56e0e9df0871df90903b53ed1af2440b3b3635ab3b9e9b231155bb02980f7") -- Italian language
+setManifestid(219804, "2216431004722021944", 82)
+addappid(219805, 1, "0fdcbaac76ee2cfef3334e4df8f63f3758739d45e7eae405c4f9506e1a038e88") -- German language
+setManifestid(219805, "2405074360450334729", 80)
+addappid(219806, 1, "ae9805fecce7dec7b456b18bd4cfa9109c179e3483d7555cf6651ee818a4bb6a") -- Spanish language
+setManifestid(219806, "1001546634519123682", 82)
+addappid(219807, 1, "2dd1e6fa9d86a870c3e3c9dcb20957424ca04457e45e87e8371f83db8b2c4dca") -- Dutch language
+setManifestid(219807, "397862133252946332", 80)
+addappid(219808, 1, "8ecb6df26fdeebb2aced3798107bf4fca373fbd41abacfbac03f21496bdb164b") -- Portuguese language
+setManifestid(219808, "3038266147657047892", 85)
+addappid(219809, 1, "6e01bd8c5cde05a1cb0e3a09dc9b0ea1ff86099367e9ce5c9b52ca5d3f81e15f") -- Polish language
+setManifestid(219809, "6463235151359563396", 81)
+addappid(219810, 1, "5fbcfcbb8a4d0186eb90122ce5c1443baaf78388a85fff86d38ae0b638effc85") -- Danish Language
+setManifestid(219810, "7550507995339828360", 80)
+addappid(219811, 1, "4a6cd439e8e2b8f111e1cc9e139486c9fe7996439c08f2cdad8acd29f6fc2644") -- Norwegian Language
+setManifestid(219811, "8784183717893850913", 84)
+-- SHARED DEPOTS (from other apps)
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") -- VC 2008 Redist (Shared from App 228980)
+setManifestid(228982, "6413394087650432851", 9688647)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
