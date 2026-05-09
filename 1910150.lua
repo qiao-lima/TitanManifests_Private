5434fd2f64872d00091e54306c2b27c678eb81a2
qiao-lima <qiao-lima@github.com>
2026-05-09 11:54:33 +0800

    Manual Update 1910150

---
+++ 1910150.lua
@@ -0,0 +1,20 @@
+-- 1910150's Lua and Manifest Created by Hubcap Manifest
+-- Walk of Life
+-- Created: May 05, 2026 at 18:29:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1910150, 1, "07fcdd2dcc7bbf61b5e396b56c83dbfaf3eff1fa7b8e32860fbf6f1a0902cf9f") -- Walk of Life
+-- MAIN APP DEPOTS
+addappid(1910151, 1, "5cbbc95cccd40e60f11b5d0fb36915125437fd13361dfa371a3d98e334b8a79b") -- Depot 1910151
+setManifestid(1910151, "1152252383560173873", 630304316)
+addappid(1910152, 1, "50e35cf8f7bd0eef3ab674d866f80c22f2c5948017df217f5cc7d4ced8b16d6b") -- Depot 1910152
+setManifestid(1910152, "3162887384815352375", 751341336)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
