a2fa078ef38596a6d2d9cee7f316830657dd3a59
qiao-lima <qiao-lima@github.com>
2026-05-09 08:58:50 +0800

    Manual Update 3526710

---
+++ 3526710.lua
@@ -0,0 +1,14 @@
+-- 3526710's Lua and Manifest Created by Hubcap Manifest
+-- Everything is Crab
+-- Created: May 08, 2026 at 05:26:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3526710) -- Everything is Crab
+-- MAIN APP DEPOTS
+addappid(3526711, 1, "ed639283b231e8c9cf3f2809bafb053eca94f9edd0d68f2678b1b9ecbc100e83") -- Depot 3526711
+setManifestid(3526711, "2649499746178748611", 1134941272)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4540950) -- Everything is Crab - Supporter Pack
