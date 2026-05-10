b3962263a374684b9cc8e8d02704d7959ce6f7cd
qiao-lima <qiao-lima@github.com>
2026-05-10 18:33:50 +0800

    Manual Update 1092730

---
+++ 1092730.lua
@@ -0,0 +1,18 @@
+-- 1092730's Lua and Manifest Created by Hubcap Manifest
+-- Hello Neighbor Alpha 3
+-- Created: March 16, 2026 at 15:47:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1092730, 1, "39c8d59ae17be3bcf16dc7b41cf5ece1846a2f990431df18643d2509d099e7a5") -- Hello Neighbor Alpha 3
+-- MAIN APP DEPOTS
+addappid(1092731, 1, "212c79af37a0e4d2b8050d538cd0bea11513b30d71b99689228bfd1bd32b2885") -- Hello Neighbor Alpha 3 Content
+setManifestid(1092731, "8485018536137971250", 2706383759)
+-- SHARED DEPOTS (from other apps)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- VC 2013 Redist (Shared from App 228980)
+setManifestid(228985, "3966345552745568756", 13699237)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
