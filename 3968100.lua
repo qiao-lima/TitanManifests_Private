9f5fb79e54c71ebf6e6b8a13bc8522b78228cb31
qiao-lima <qiao-lima@github.com>
2026-05-10 18:40:07 +0800

    Manual Update 3968100

---
+++ 3968100.lua
@@ -0,0 +1,12 @@
+-- 3968100's Lua and Manifest Created by Hubcap Manifest
+-- CD-ROM
+-- Created: May 10, 2026 at 05:50:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3968100) -- CD-ROM
+-- MAIN APP DEPOTS
+addappid(3968101, 1, "4e5e7d78568eada999dc40b72974e09723edd5ae5c20469cdad0bc250ad70030") -- Depot 3968101
+setManifestid(3968101, "2925026149169332169", 1297080098)
