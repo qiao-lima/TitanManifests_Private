cc61f6c74176ff5d8d8d4817af9886e60bbb4c95
qiao-lima <qiao-lima@github.com>
2026-05-10 18:33:53 +0800

    Manual Update 4457870

---
+++ 4457870.lua
@@ -0,0 +1,12 @@
+-- 4457870's Lua and Manifest Created by Hubcap Manifest
+-- G.I.R.L.S. will be Girls
+-- Created: May 09, 2026 at 09:55:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4457870) -- G.I.R.L.S. will be Girls
+-- MAIN APP DEPOTS
+addappid(4457871, 1, "f6168ce33f893e2d601a35e2c9d21fde7988b37049dc1527bbb453681a2cfe99") -- Depot 4457871
+setManifestid(4457871, "2397171770097843005", 3264265204)
