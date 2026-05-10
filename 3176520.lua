54b7fcbb0715f1834c0fe61f445a31edbc0a9a23
qiao-lima <qiao-lima@github.com>
2026-05-10 18:40:06 +0800

    Manual Update 3176520

---
+++ 3176520.lua
@@ -0,0 +1,14 @@
+-- 3176520's Lua and Manifest Created by Hubcap Manifest
+-- Scorchie Adventures
+-- Created: May 09, 2026 at 23:59:53 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3176520) -- Scorchie Adventures
+-- MAIN APP DEPOTS
+addappid(3176523, 1, "82e34f3c918f56973de6b8ac6d133cb79b4ae45f778e87e2eddc48275e4c6ad0") -- Depot 3176523
+setManifestid(3176523, "4852502176227475154", 558429623)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3176522) -- Depot 3176522 (empty depot)
