4a1d1270055630be2f4b979a824232f8c37014de
qiao-lima <qiao-lima@github.com>
2026-05-10 18:36:23 +0800

    Manual Update 4519430

---
+++ 4519430.lua
@@ -0,0 +1,12 @@
+-- 4519430's Lua and Manifest Created by Hubcap Manifest
+-- Chess of Five Elements
+-- Created: May 09, 2026 at 10:14:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4519430) -- Chess of Five Elements
+-- MAIN APP DEPOTS
+addappid(4519431, 1, "85dbc57a7f16d623b58863682bdd1c57cac899466c57fc4b263fdcec505c7973") -- Depot 4519431
+setManifestid(4519431, "4028320375977298695", 37893674)
