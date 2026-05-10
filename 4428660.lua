4238b93162f900649843199cfcaa2e8d7433a048
qiao-lima <qiao-lima@github.com>
2026-05-10 18:33:53 +0800

    Manual Update 4428660

---
+++ 4428660.lua
@@ -0,0 +1,14 @@
+-- 4428660's Lua and Manifest Created by Hubcap Manifest
+-- Comet Busters
+-- Created: May 09, 2026 at 09:41:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4428660) -- Comet Busters
+-- MAIN APP DEPOTS
+addappid(4428661, 1, "7ea757fb47c49e586a65bb620d4edc66136db89ff234c24ccdf0e5cc9258d07e") -- Depot 4428661
+setManifestid(4428661, "4902454794920459147", 116864616)
+addappid(4428662, 1, "cb78253582f67dd45ef403b25779ddd9121eef7344773effbbbd4393e2813a8d") -- Depot 4428662
+setManifestid(4428662, "8087294713890501738", 61527293)
