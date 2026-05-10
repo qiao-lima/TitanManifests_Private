4feea2854da73a9e2575ab7d140250a9c60f6efe
qiao-lima <qiao-lima@github.com>
2026-05-10 18:33:53 +0800

    Manual Update 4455660

---
+++ 4455660.lua
@@ -0,0 +1,16 @@
+-- 4455660's Lua and Manifest Created by Hubcap Manifest
+-- Ivory Quest
+-- Created: May 09, 2026 at 09:54:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4455660) -- Ivory Quest
+-- MAIN APP DEPOTS
+addappid(4455661, 1, "adca38a3d9cd58b03ce4b87671d8b098e26fab0131f57c8e00e3eeb6a028f2af") -- Depot 4455661
+setManifestid(4455661, "3885018275695612202", 226)
+addappid(4455662, 1, "61a2a5359529119abcd4a909a63bce9567062b08dda2bed98f9934e6b7e7fece") -- Depot 4455662
+setManifestid(4455662, "8819171027340750955", 294573252)
+addappid(4455663, 1, "59420511080965f42da64ba087c47ffa9264b3802f70064985b21366100777e6") -- Depot 4455663
+setManifestid(4455663, "2394977946415403673", 373719033)
