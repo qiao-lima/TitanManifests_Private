b836b0a9cd7453666c7fc395986949c9b2ce4223
qiao-lima <qiao-lima@github.com>
2026-05-10 18:36:22 +0800

    Manual Update 4504030

---
+++ 4504030.lua
@@ -0,0 +1,18 @@
+-- 4504030's Lua and Manifest Created by Hubcap Manifest
+-- 梦境管理公司之渡魂行者
+-- Created: May 09, 2026 at 10:09:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4504030) -- 梦境管理公司之渡魂行者
+-- MAIN APP DEPOTS
+addappid(4504031, 1, "28f478bdd454f5394e459a4146f278566cd78d7ab27062de323fe61795e60271") -- Depot 4504031
+setManifestid(4504031, "3160322160839661400", 16766519106)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
