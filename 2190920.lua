a59fc534ae55bb92b0943f7b202fb68566ed2203
qiao-lima <qiao-lima@github.com>
2026-05-10 18:40:06 +0800

    Manual Update 2190920

---
+++ 2190920.lua
@@ -0,0 +1,12 @@
+-- 2190920's Lua and Manifest Created by Hubcap Manifest
+-- Corebreaker
+-- Created: May 10, 2026 at 01:01:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2190920) -- Corebreaker
+-- MAIN APP DEPOTS
+addappid(2190921, 1, "6c18843356a652e89cc64d96ee9fd06c9a772b17feb02ce0766903fcb00793f0") -- Depot 2190921
+setManifestid(2190921, "4665309953143424238", 465429286)
