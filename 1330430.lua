624482c66acc066b9167d972dcfa76c3334828bf
qiao-lima <qiao-lima@github.com>
2026-05-10 18:33:50 +0800

    Manual Update 1330430

---
+++ 1330430.lua
@@ -0,0 +1,18 @@
+-- 1330430's Lua and Manifest Created by Hubcap Manifest
+-- Celestial Empire
+-- Created: March 11, 2026 at 12:19:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1330430) -- Celestial Empire
+-- MAIN APP DEPOTS
+addappid(1330431, 1, "9c9d8c218847fb1a49d3243f5a04d891620b8993b95b25473aa63ec229cffa52") -- Depot 1330431
+setManifestid(1330431, "3263149289129878035", 35437901073)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
