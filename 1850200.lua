4e23c536a6d7c9d14d13cdda35fba726ed300693
qiao-lima <qiao-lima@github.com>
2026-05-10 18:40:06 +0800

    Manual Update 1850200

---
+++ 1850200.lua
@@ -0,0 +1,12 @@
+-- 1850200's Lua and Manifest Created by Hubcap Manifest
+-- Dodge-imals
+-- Created: May 09, 2026 at 23:52:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1850200) -- Dodge-imals
+-- MAIN APP DEPOTS
+addappid(1850201, 1, "9be526fd5d2856c2024f290568f61107b106d30d335ee633f6dee1ed30039c66") -- Depot 1850201
+setManifestid(1850201, "3713032224337046030", 1164409620)
