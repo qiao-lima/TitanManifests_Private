288cfa3f302e0fad8531df307fb9294800c61245
qiao-lima <qiao-lima@github.com>
2026-05-10 18:33:54 +0800

    Manual Update 4492120

---
+++ 4492120.lua
@@ -0,0 +1,12 @@
+-- 4492120's Lua and Manifest Created by Hubcap Manifest
+-- PAIC: Moon Rider Story
+-- Created: May 09, 2026 at 10:06:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4492120) -- PAIC: Moon Rider Story
+-- MAIN APP DEPOTS
+addappid(4492121, 1, "9bad5894a5e3abca07668b0bd98b93b876b9e70af3f828635d9c3af75afd7375") -- Depot 4492121
+setManifestid(4492121, "8242176200268648077", 504290669)
