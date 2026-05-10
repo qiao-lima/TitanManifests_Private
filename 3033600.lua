bab74bd9797c86caa90e18651e1d44786021ae64
qiao-lima <qiao-lima@github.com>
2026-05-10 18:33:51 +0800

    Manual Update 3033600

---
+++ 3033600.lua
@@ -0,0 +1,12 @@
+-- 3033600's Lua and Manifest Created by Hubcap Manifest
+-- Rogue Rollout
+-- Created: May 08, 2026 at 19:44:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3033600) -- Rogue Rollout
+-- MAIN APP DEPOTS
+addappid(3033601, 1, "b5df572430ed4f483d161a0ed6d3fd4074740d124313a13bb83255d0cc132d7b") -- Depot 3033601
+setManifestid(3033601, "2786843354405647948", 2209553609)
