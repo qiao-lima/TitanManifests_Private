37b6265299849de1c8a0f252784874e937f2a20e
qiao-lima <qiao-lima@github.com>
2026-05-10 18:40:09 +0800

    Manual Update 4563570

---
+++ 4563570.lua
@@ -0,0 +1,21 @@
+-- 4563570's Lua and Manifest Created by Hubcap Manifest
+-- Battle 3D - Strategy game
+-- Created: May 09, 2026 at 10:30:25 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 8
+
+-- MAIN APPLICATION
+addappid(4563570) -- Battle 3D - Strategy game
+-- MAIN APP DEPOTS
+addappid(4563571, 1, "06d9eeea9f85a0ffbb0bdfea2918d06a0a8be1daafec826eabf9fa834dda145b") -- Depot 4563571
+setManifestid(4563571, "6089116865031886086", 947892651)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4588780) -- Battle 3D - Strategy game - Main Campaign
+addappid(4588790) -- Battle 3D - Strategy game - Reinforcement
+addappid(4588800) -- Battle 3D - Strategy game - AtomicPower
+addappid(4588810) -- Battle 3D - Strategy game - Red Side
+addappid(4588820) -- Battle 3D - Strategy game - Special Maps
+addappid(4588830) -- Battle 3D - Strategy game - World Map
+addappid(4588840) -- Battle 3D - Strategy game - Nature Campaign
+addappid(4588850) -- Battle 3D - Strategy game - Cars and Night
