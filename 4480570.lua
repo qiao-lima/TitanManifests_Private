59e3f1f8db86e9af4e442bdc75ebf1c3b807ddbe
qiao-lima <qiao-lima@github.com>
2026-05-10 18:33:54 +0800

    Manual Update 4480570

---
+++ 4480570.lua
@@ -0,0 +1,14 @@
+-- 4480570's Lua and Manifest Created by Hubcap Manifest
+-- Intelligence
+-- Created: May 09, 2026 at 10:02:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4480570) -- Intelligence
+-- MAIN APP DEPOTS
+addappid(4480572, 1, "9bf96fabc73e9f6f16fd35a2122ac5b278297e47e34cebb7ea9c56adc44b8f1e") -- Depot 4480572
+setManifestid(4480572, "6853281744926050533", 502193601)
+addappid(4480573, 1, "b17077a92c2122003b59c415580f1e5fee8d2d3a37539e00994be1ea4f368cc4") -- Depot 4480573
+setManifestid(4480573, "5345221053627712200", 150113280)
