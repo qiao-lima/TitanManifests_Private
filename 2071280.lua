67c78b092daa34d61811ebca2a5f7e20778e1e4c
qiao-lima <qiao-lima@github.com>
2026-05-10 18:33:50 +0800

    Manual Update 2071280

---
+++ 2071280.lua
@@ -0,0 +1,30 @@
+-- 2071280's Lua and Manifest Created by Hubcap Manifest
+-- Ravenswatch
+-- Created: February 17, 2026 at 21:40:49 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 9
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2071280) -- Ravenswatch
+-- MAIN APP DEPOTS
+addappid(2071281, 1, "708c675853989cbd400455e166d30f9dec051473847f4577ef48f7c9331edb97") -- Depot 2071281
+setManifestid(2071281, "2510764546266596512", 7368869769)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+-- DLCS WITH DEDICATED DEPOTS
+-- Ravenswatch - Digital Artbook (AppID: 3113130)
+addappid(3113130)
+addappid(3113130, 1, "d599bff2a5cf7c111a24f0fdceff1571f319bd064f49239e640c15c6356b8fd7") -- Ravenswatch - Digital Artbook - Depot 3113130
+setManifestid(3113130, "8098826225145136318", 122105513)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3113100) -- Ravenswatch - Fairytales Skin Pack
+addappid(3113110) -- Ravenswatch - Ravens Skin Pack
+addappid(3113120) -- Ravenswatch - Nightmares Skin Pack
+addappid(3393430) -- Ravenswatch - Unleashed Skin Pack
+addappid(3607920) -- Ravenswatch - Romeo  Juliet
+addappid(3607930) -- Ravenswatch - Romeo  Juliet Skin Pack
+addappid(3607940) -- Ravenswatch - Timeless Skin Pack
+addappid(3607950) -- Ravenswatch - Merlin
