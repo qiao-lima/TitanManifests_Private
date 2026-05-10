f91a566564d3b3fe96cdc9d9ff46da13c7fb4972
qiao-lima <qiao-lima@github.com>
2026-05-10 18:33:53 +0800

    Manual Update 4442960

---
+++ 4442960.lua
@@ -0,0 +1,12 @@
+-- 4442960's Lua and Manifest Created by Hubcap Manifest
+-- Loong Reversi
+-- Created: May 09, 2026 at 09:48:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4442960) -- Loong Reversi
+-- MAIN APP DEPOTS
+addappid(4442961, 1, "0ef3d53717d3f140aa60218c864a2047164fa3d2ee4e1735d8e9749a41e3bacc") -- Depot 4442961
+setManifestid(4442961, "3832644753257031914", 372197072)
