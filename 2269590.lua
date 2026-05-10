cb1d1472322a0141834f7e55e355e7d40f3d1507
qiao-lima <qiao-lima@github.com>
2026-05-10 18:40:06 +0800

    Manual Update 2269590

---
+++ 2269590.lua
@@ -0,0 +1,12 @@
+-- 2269590's Lua and Manifest Created by Hubcap Manifest
+-- BanHown
+-- Created: May 09, 2026 at 23:59:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2269590) -- BanHown
+-- MAIN APP DEPOTS
+addappid(2269591, 1, "d57d9a6581eda119e583f8e2b1fce7b385488fdb7c2a19479dcf74bcadf3a52c") -- Depot 2269591
+setManifestid(2269591, "3147222720534576327", 541409032)
