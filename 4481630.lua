0e011e658182dfeb75230bcf3061e813d6d9d4ea
qiao-lima <qiao-lima@github.com>
2026-05-10 18:33:54 +0800

    Manual Update 4481630

---
+++ 4481630.lua
@@ -0,0 +1,12 @@
+-- 4481630's Lua and Manifest Created by Hubcap Manifest
+-- Gunja Tteokbokki
+-- Created: May 09, 2026 at 10:03:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4481630) -- Gunja Tteokbokki
+-- MAIN APP DEPOTS
+addappid(4481631, 1, "24e6255a2719a67b97d59267dd98ffa7c4abf828c70513a716ca155fdf2d9fab") -- Depot 4481631
+setManifestid(4481631, "5084478844312337281", 823489716)
