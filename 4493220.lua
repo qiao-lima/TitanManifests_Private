1e658efeb602dae822a4ad8644426844e04fdf47
qiao-lima <qiao-lima@github.com>
2026-05-10 18:33:54 +0800

    Manual Update 4493220

---
+++ 4493220.lua
@@ -0,0 +1,12 @@
+-- 4493220's Lua and Manifest Created by Hubcap Manifest
+-- 28 Leicester Square
+-- Created: May 09, 2026 at 10:07:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4493220) -- 28 Leicester Square
+-- MAIN APP DEPOTS
+addappid(4493221, 1, "6ab9caac0d7ac79a188ccba381d733c681d6db6ead2e867c02644d9075cdd621") -- Depot 4493221
+setManifestid(4493221, "6881972031314532252", 1272401047)
