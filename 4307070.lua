f8ec9859bea3ee73d0898f684969fb94649cf1b1
qiao-lima <qiao-lima@github.com>
2026-05-10 18:40:07 +0800

    Manual Update 4307070

---
+++ 4307070.lua
@@ -0,0 +1,20 @@
+-- 4307070's Lua and Manifest Created by Hubcap Manifest
+-- Sakura Drift
+-- Created: May 10, 2026 at 00:00:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(4307070) -- Sakura Drift
+-- MAIN APP DEPOTS
+addappid(4307071, 1, "30b7c05cd0fd03bfebd858019472f8eb20d72f738c81ea9be24542ccb257af2a") -- Depot 4307071
+setManifestid(4307071, "4496039582179726051", 3873129363)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 117381405)
