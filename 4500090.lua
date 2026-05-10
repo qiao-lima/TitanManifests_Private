aa7a387f5c6a462bffac007dcc49e59c551a758e
qiao-lima <qiao-lima@github.com>
2026-05-10 18:33:54 +0800

    Manual Update 4500090

---
+++ 4500090.lua
@@ -0,0 +1,18 @@
+-- 4500090's Lua and Manifest Created by Hubcap Manifest
+-- TeepyTime
+-- Created: May 09, 2026 at 10:08:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4500090) -- TeepyTime
+-- MAIN APP DEPOTS
+addappid(4500091, 1, "c65f0203b30b718a6d72ea8f473bdd9f2cdfcefa85068694cfb3f995313b1261") -- Depot 4500091
+setManifestid(4500091, "735291390700379565", 496353212)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
