7d2f0e9f4ae9ec7e2de0bb2627b316e8d200ae5f
qiao-lima <qiao-lima@github.com>
2026-05-10 18:40:06 +0800

    Manual Update 2458530

---
+++ 2458530.lua
@@ -0,0 +1,18 @@
+-- 2458530's Lua and Manifest Created by Hubcap Manifest
+-- 魔女的夜宴
+-- Created: October 01, 2025 at 13:18:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2458530) -- 魔女的夜宴
+-- MAIN APP DEPOTS
+addappid(2458531, 1, "a112ff17726a4eadb397f7d89ea34e081ba3891aa001fe4de40b3b3f868bb1a7") -- Depot 2458531
+setManifestid(2458531, "643445481308705647", 3203561766)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
