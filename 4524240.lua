4e44eb721c9261a41d48c4cdd6ea408871807b29
qiao-lima <qiao-lima@github.com>
2026-05-10 18:40:09 +0800

    Manual Update 4524240

---
+++ 4524240.lua
@@ -0,0 +1,12 @@
+-- 4524240's Lua and Manifest Created by Hubcap Manifest
+-- Orpheus: Echo of Hades
+-- Created: May 10, 2026 at 00:00:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4524240) -- Orpheus: Echo of Hades
+-- MAIN APP DEPOTS
+addappid(4524241, 1, "15d89abb660d8e6ba1afb6c14127be32d6e1b5e990e8bcf66ed69b826584c481") -- Depot 4524241
+setManifestid(4524241, "1227857786632202343", 661974930)
