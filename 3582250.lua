1b7fed97a83cb17a25ee6d829d73efdc73a9671d
qiao-lima <qiao-lima@github.com>
2026-05-10 18:33:52 +0800

    Manual Update 3582250

---
+++ 3582250.lua
@@ -0,0 +1,12 @@
+-- 3582250's Lua and Manifest Created by Hubcap Manifest
+-- Farm to Table
+-- Created: May 09, 2026 at 10:20:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3582250) -- Farm to Table
+-- MAIN APP DEPOTS
+addappid(3582251, 1, "282a9be675c4c24f4c51245fcd9cd0042ae1e1f88072efb8288bb8eb425f1fc9") -- Depot 3582251
+setManifestid(3582251, "5720568872068327372", 2395680203)
