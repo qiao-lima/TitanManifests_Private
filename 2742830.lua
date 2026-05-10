56633ed8be2cd9ea40645c7fdf9ea7db89105bb6
qiao-lima <qiao-lima@github.com>
2026-05-10 18:33:51 +0800

    Manual Update 2742830

---
+++ 2742830.lua
@@ -0,0 +1,23 @@
+-- 2742830's Lua and Manifest Created by Hubcap Manifest
+-- Monster Train 2
+-- Created: April 14, 2026 at 17:06:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(2742830, 1, "dc5527cff9a8818233a73bcf7233419d062246971fee04c383497ffdc70213d2") -- Monster Train 2
+-- MAIN APP DEPOTS
+addappid(2742831, 1, "c2a5386e1f6feabad976ffd5e88028153d29a7d432483ee3763229bff9075fcb") -- Depot 2742831
+setManifestid(2742831, "1692318783842382971", 2061498633)
+addappid(2742832, 1, "291e0435b173cddc5d3e54923647f2ce7a6336d61715008c2f89c61520a03444") -- Depot 2742832
+setManifestid(2742832, "3859875271971746386", 2050394041)
+-- DLCS WITH DEDICATED DEPOTS
+-- Monster Train 2 Digital Compendium (AppID: 3580220)
+addappid(3580220)
+addappid(3580220, 1, "6a48b119fa0f4779ec8618000d21337750db9a1e0eac4f89cdf1afb16ea4a7ec") -- Monster Train 2 Digital Compendium - Depot 3580220
+setManifestid(3580220, "5756384931734052985", 1101002946)
+-- Monster Train 2 Destiny of the Railforged (AppID: 3993450)
+addappid(3993450)
+addappid(3993450, 1, "7b7c0154d34e61c411d6cbee7be3ae571e2de654b27d75f2632d0cb1e786fbf0") -- Monster Train 2 Destiny of the Railforged - Depot 3993450
+setManifestid(3993450, "8605106753921413412", 34605229)
