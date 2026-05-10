073f057e637ba25001e155a315a89d820e9e3d59
qiao-lima <qiao-lima@github.com>
2026-05-10 18:40:07 +0800

    Manual Update 4148530

---
+++ 4148530.lua
@@ -0,0 +1,33 @@
+-- 4148530's Lua and Manifest Created by Hubcap Manifest
+-- City Transport Simulator 2026
+-- Created: May 05, 2026 at 16:42:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 12
+-- Shared Depots: 19
+
+-- MAIN APPLICATION
+addappid(4148530) -- City Transport Simulator 2026
+-- MAIN APP DEPOTS
+addappid(4148531, 1, "fa46ca95704770be85f6f457e4fa2da91205e6c1ff0b79653473de4651ce3989") -- Depot 4148531
+setManifestid(4148531, "3135287639497669003", 19176759820)
+-- SHARED DEPOTS (from other apps)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- VC 2013 Redist (Shared from App 228980)
+setManifestid(228985, "3966345552745568756", 13699237)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4399470) -- CTS 2026 - Tram DLC Flexity Vienna
+addappid(4399490) -- CTS 2026 - Tram DLC StadtRegioTram
+addappid(4399500) -- CTS 2026 - Tram DLC E1 Vienna
+addappid(4399510) -- CTS 2026 - Tram DLC GT8N Mannheim
+addappid(4399520) -- CTS 2026 - Tram DLC GT6
+addappid(4399530) -- CTS 2026 - Bus DLC MAN New Lions City
+addappid(4399540) -- CTS 2026 - Bus DLC MAN Lions City A26  A47
+addappid(4399550) -- CTS 2026 - Cosmetic DLC Basel Style
+addappid(4399560) -- CTS 2026 - Bus DLC Citybus C2
+addappid(4399590) -- CTS 2026 - Map DLC Tram Depot  Southern-Route
+addappid(4427800) -- CTS 2026 - Expert Upgrade
+addappid(4428130) -- CTS 2026 - Season Pass
