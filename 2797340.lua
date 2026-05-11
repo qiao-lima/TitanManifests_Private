3ecc8fc5eb4aa81f2361fc588c71e93f88bb3f06
qiao-lima <qiao-lima@github.com>
2026-05-11 08:24:05 +0800

    Manual Update 2797340

---
+++ 2797340.lua
@@ -0,0 +1,32 @@
+-- 2797340's Lua and Manifest Created by Hubcap Manifest
+-- Cat God Ranch
+-- Created: May 10, 2026 at 20:16:29 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 8
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(2797340, 1, "95f09ee91a9f90f02a4766347c83dfce9332604e08ccd0198399e8d5ba8f80de") -- Cat God Ranch
+addtoken(2797340, "2198558448921994873")
+-- MAIN APP DEPOTS
+addappid(2797341, 1, "4adf02cfdd9a75f570ed860197c2c5d7565aa95826e5a3da4afecccc758f7438") -- Depot 2797341
+setManifestid(2797341, "4267264926190418068", 349675253)
+addappid(2797342, 1, "601cac1d72326834702cc24f3d04ef708c2f4b0f31801585d67af777630f5b21") -- Depot 2797342
+setManifestid(2797342, "4262201206077580657", 479996234)
+addappid(2797343, 1, "729f8eb295d3397c5aae9b8e0e0490570ffabdb56898362cd99a157ec9ab92ae") -- Depot 2797343
+setManifestid(2797343, "3445078555252636343", 336984672)
+addappid(2797344, 1, "d26ca53ce4d7f80159254bd178b720fc83cff181524cdd7ef76b7494a9b6e07a") -- Depot 2797344
+setManifestid(2797344, "3943423129938704264", 495412042)
+-- DLCS WITH DEDICATED DEPOTS
+-- Cat God RanchAge of Dinosaurs (AppID: 3795220)
+addappid(3795220)
+addappid(3795221, 1, "2e89963ffcab13efa5665387c67d010a7f27006209b5a2fb3a6428c5273d55b9") -- Cat God RanchAge of Dinosaurs - Depot 3795221
+setManifestid(3795221, "658487063292845365", 19371530)
+addappid(3795222, 1, "4d46978b2b6ce02b20b6adfb8f7f4f72ffe961b5c26d3fa9a7eb5ee8e8482e04") -- Cat God RanchAge of Dinosaurs - Depot 3795222
+setManifestid(3795222, "8041569942312118205", 19396174)
+addappid(3795223, 1, "66e79e58eaa8ce1cbe7dc5d3585f7961401f0453164e7b4e8dc3c710ef051c70") -- Cat God RanchAge of Dinosaurs - Depot 3795223
+setManifestid(3795223, "1797424461234074387", 366553149)
+addappid(3795224, 1, "767e570d2d0995c60368b427b6c066b03a41d58d111c57f3a6934004d0db2bb8") -- Cat God RanchAge of Dinosaurs - Depot 3795224
+setManifestid(3795224, "5416955426217868820", 485011101)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4575460) -- Cat God Ranch  Frozen Beats
