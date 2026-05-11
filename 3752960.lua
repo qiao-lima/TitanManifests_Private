61cc79ef9da8b3cedad8b984ab5b5eba8e59c612
qiao-lima <qiao-lima@github.com>
2026-05-12 00:57:59 +0800

    Manual Update 3752960

---
+++ 3752960.lua
@@ -0,0 +1,17 @@
+-- 3752960's Lua and Manifest Created by Hubcap Manifest
+-- Off the Beaten Track
+-- Created: May 11, 2026 at 09:04:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3752960, 1, "4c95fe407b9e175dc073745d5f9502992e85caa1135d2be82bb3169dc7cdd4da") -- Off the Beaten Track
+addtoken(3752960, "4058643403973371462")
+-- MAIN APP DEPOTS
+addappid(3752961, 1, "87bdc865763756611bd75aea8a45ab7f4643d68b48f872560234d8562596c6e9") -- Depot 3752961
+setManifestid(3752961, "7300865237881209554", 2142749714)
+addappid(3752962, 1, "3d7e7e13b8c348e4635152d385bde5760c56395669a036123ce7cf22f618d253") -- Depot 3752962
+setManifestid(3752962, "3082391616897963458", 2150149516)
+addappid(3752963, 1, "72f5c93390067198a6eb7ca28c2ddf4379839204556bfdb54931c634cfccc19b") -- Depot 3752963
+setManifestid(3752963, "9220469429937100694", 2294876260)
