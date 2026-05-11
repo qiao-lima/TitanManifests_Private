2f50dcb05f506d4f9dd139446779656cc40adac4
qiao-lima <qiao-lima@github.com>
2026-05-11 19:54:29 +0800

    Manual Update 4435720

---
+++ 4435720.lua
@@ -0,0 +1,12 @@
+-- 4435720's Lua and Manifest Created by Hubcap Manifest
+-- HOLY GRAIL for DUMMIES
+-- Created: May 10, 2026 at 15:39:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4435720) -- HOLY GRAIL for DUMMIES
+-- MAIN APP DEPOTS
+addappid(4435721, 1, "84ef0f1dfd8e4c44f1149bf3553b9fb8f1085e91fb5e63ba405e6edd86788f8a") -- Depot 4435721
+setManifestid(4435721, "2128386885756024386", 350778006)
