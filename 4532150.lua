fb58352f7c223d026fb5f143f4f6a89d9a71c05d
qiao-lima <qiao-lima@github.com>
2026-05-11 19:54:30 +0800

    Manual Update 4532150

---
+++ 4532150.lua
@@ -0,0 +1,12 @@
+-- 4532150's Lua and Manifest Created by Hubcap Manifest
+-- G-MODEアーカイブス+ ゼノサーガ パイドパイパー
+-- Created: May 10, 2026 at 15:24:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4532150, 1, "cacc4cdb0f4e6b9e0c949b49988ca10d15a9ece0d5d648b8f5f01a0e53371027") -- G-MODEアーカイブス+ ゼノサーガ パイドパイパー
+-- MAIN APP DEPOTS
+addappid(4532151, 1, "d0d1b88e26a759375a2b8d5c6e57a7bd63aec8292626159220e9f011e190f6e4") -- Depot 4532151
+setManifestid(4532151, "5205753548991993372", 127233763)
