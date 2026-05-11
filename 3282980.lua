cfaa354e392709c841fd19ab968f6ad06a4c92d3
qiao-lima <qiao-lima@github.com>
2026-05-11 19:54:25 +0800

    Manual Update 3282980

---
+++ 3282980.lua
@@ -0,0 +1,18 @@
+-- 3282980's Lua and Manifest Created by Hubcap Manifest
+-- Urbex Murder
+-- Created: May 09, 2026 at 14:44:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3282980, 1, "fa1674833f86b0748d4b5f5d131a4a4e4395d6ee38ad97fbf5bcbdf82bc93dd4") -- Urbex Murder
+-- MAIN APP DEPOTS
+addappid(3282981, 1, "917a514ec698c1e3b07521e8e7ce2cf26baeeb419f32f7037c16efd5fd797182") -- Depot 3282981
+setManifestid(3282981, "7379952920042580120", 5121119846)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
