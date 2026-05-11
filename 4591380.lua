3398089870c6872e2cdc79e92d93e39d001cd514
qiao-lima <qiao-lima@github.com>
2026-05-11 19:58:17 +0800

    Manual Update 4591380

---
+++ 4591380.lua
@@ -0,0 +1,12 @@
+-- 4591380's Lua and Manifest Created by Hubcap Manifest
+-- 嘉德莉亚的挽歌 ～Wind and Dream～
+-- Created: May 10, 2026 at 14:55:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4591380) -- 嘉德莉亚的挽歌 ～Wind and Dream～
+-- MAIN APP DEPOTS
+addappid(4591381, 1, "019a02aff97e0cd629e756aec6b8b8dca8477834c3e7b78eacbb9702284f1ad9") -- Depot 4591381
+setManifestid(4591381, "6155161225693124167", 742333822)
