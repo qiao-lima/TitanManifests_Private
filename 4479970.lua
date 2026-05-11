1d170b90bfb41dd9c3f440e7c0ef32476c4513eb
qiao-lima <qiao-lima@github.com>
2026-05-11 19:54:29 +0800

    Manual Update 4479970

---
+++ 4479970.lua
@@ -0,0 +1,12 @@
+-- 4479970's Lua and Manifest Created by Hubcap Manifest
+-- Scary Game 2: The Mad Shepherd
+-- Created: May 10, 2026 at 16:45:57 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4479970) -- Scary Game 2: The Mad Shepherd
+-- MAIN APP DEPOTS
+addappid(4479971, 1, "f3d497f4618414c1cefac1eb0c8b63b903eb385e4be155203efdbfe849232f8c") -- Depot 4479971
+setManifestid(4479971, "1322452782578452360", 3045578118)
