0de93efce6fe3ab1df19d38f72b2c4e8daf8a22e
qiao-lima <qiao-lima@github.com>
2026-05-11 19:54:24 +0800

    Manual Update 293280

---
+++ 293280.lua
@@ -0,0 +1,18 @@
+-- 293280's Lua and Manifest Created by Hubcap Manifest
+-- Wander
+-- Created: May 11, 2026 at 01:30:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(293280) -- Wander
+-- MAIN APP DEPOTS
+addappid(293281, 1, "bf681f0ac237dd0caabb7f76644ca3c30bc14539346fd9b795189c8fefae771a") -- Depot 293281
+setManifestid(293281, "1112703120056723712", 7138670529)
+-- SHARED DEPOTS (from other apps)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- VC 2012 Redist (Shared from App 228980)
+setManifestid(228984, "2547553897526095397", 13742505)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
