87c4666045d610cadd959b50028d83467de466ad
qiao-lima <qiao-lima@github.com>
2026-05-12 00:58:00 +0800

    Manual Update 4460530

---
+++ 4460530.lua
@@ -0,0 +1,12 @@
+-- 4460530's Lua and Manifest Created by Hubcap Manifest
+-- Towerix91
+-- Created: May 11, 2026 at 08:01:37 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4460530) -- Towerix91
+-- MAIN APP DEPOTS
+addappid(4460531, 1, "9bd5310f5018cb4a7f8d1910dc0887517986aaad9b8db7739b40396699a7f2cc") -- Depot 4460531
+setManifestid(4460531, "3691866053019239112", 269978993)
