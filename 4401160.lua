775325ad95d8cf6d69fad77c826d36dcf05b3f34
qiao-lima <qiao-lima@github.com>
2026-05-11 19:54:28 +0800

    Manual Update 4401160

---
+++ 4401160.lua
@@ -0,0 +1,12 @@
+-- 4401160's Lua and Manifest Created by Hubcap Manifest
+-- Fireboy & Watergirl and Friends
+-- Created: May 10, 2026 at 04:39:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4401160) -- Fireboy & Watergirl and Friends
+-- MAIN APP DEPOTS
+addappid(4401161, 1, "fc6292c5f1389e3b4e8e2fe7f504ea83d381695f57fb6ed6880fa96f4c15ba12") -- Depot 4401161
+setManifestid(4401161, "3361930021214653681", 157781238)
