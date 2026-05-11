c32c052bfdae91462d596fc704e7745cb96005d6
qiao-lima <qiao-lima@github.com>
2026-05-11 19:54:28 +0800

    Manual Update 4386250

---
+++ 4386250.lua
@@ -0,0 +1,12 @@
+-- 4386250's Lua and Manifest Created by Hubcap Manifest
+-- Protocol: Umbra
+-- Created: May 10, 2026 at 04:39:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4386250) -- Protocol: Umbra
+-- MAIN APP DEPOTS
+addappid(4386251, 1, "1d0f7823bbaed63248b91b370ac87f4ecb124bd5eaab7636d4f09a2dc1297a1c") -- Depot 4386251
+setManifestid(4386251, "8386078419486195043", 63059095)
