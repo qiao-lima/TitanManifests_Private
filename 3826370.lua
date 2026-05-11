68202e97539e1d917aa033c02a77d7e630341240
qiao-lima <qiao-lima@github.com>
2026-05-11 19:54:26 +0800

    Manual Update 3826370

---
+++ 3826370.lua
@@ -0,0 +1,22 @@
+-- 3826370's Lua and Manifest Created by Hubcap Manifest
+-- Headquarters: Cold War
+-- Created: May 10, 2026 at 16:15:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 5
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(3826370) -- Headquarters: Cold War
+-- MAIN APP DEPOTS
+addappid(3826371, 1, "df9a47cef0f6f32a8ca8a74451bd638c32fb9710287a341d91f62511560e1361") -- Depot 3826371
+setManifestid(3826371, "1595159466302180302", 28844953481)
+addappid(3826372, 1, "3e8fd0d511ce8eeb4f892bd49199409302234fe0bbe17f05aebb03af5a5d3d29") -- Depot 3826372
+setManifestid(3826372, "8142646523849970699", 4232450)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") -- .NET 4.7 Redist (Shared from App 228980)
+setManifestid(229006, "1784011429307107530", 83944258)
