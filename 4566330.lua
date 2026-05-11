99ea0ae3e01f5c26ae48fa29827833431a28c884
qiao-lima <qiao-lima@github.com>
2026-05-12 00:58:00 +0800

    Manual Update 4566330

---
+++ 4566330.lua
@@ -0,0 +1,18 @@
+-- 4566330's Lua and Manifest Created by Hubcap Manifest
+-- Sussy Cat
+-- Created: May 11, 2026 at 04:28:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4566330) -- Sussy Cat
+-- MAIN APP DEPOTS
+addappid(4566331, 1, "2fda1de72e4c50868d606640558ffd4a01f25ef562e8b413e5f4f16443cd8e34") -- Depot 4566331
+setManifestid(4566331, "6775068338274071370", 590219722)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
