91615fdf652c59e262118a33dfd33d1a653f0560
qiao-lima <qiao-lima@github.com>
2026-05-11 19:58:16 +0800

    Manual Update 4537970

---
+++ 4537970.lua
@@ -0,0 +1,12 @@
+-- 4537970's Lua and Manifest Created by Hubcap Manifest
+-- Region at War: The Shogun's Tale of War
+-- Created: May 10, 2026 at 09:36:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4537970) -- Region at War: The Shogun's Tale of War
+-- MAIN APP DEPOTS
+addappid(4537971, 1, "516bc70d14537457a65a00db50ce9f2a1d5a5926c782b2113452e8ccfb28a520") -- Depot 4537971
+setManifestid(4537971, "357961318557088616", 630998059)
