faa1524c855909f3fb3cbdd806243d548d276286
qiao-lima <qiao-lima@github.com>
2026-05-12 00:57:59 +0800

    Manual Update 4187140

---
+++ 4187140.lua
@@ -0,0 +1,12 @@
+-- 4187140's Lua and Manifest Created by Hubcap Manifest
+-- Don't Panic! It is Just Turbulence
+-- Created: May 11, 2026 at 06:57:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4187140) -- Don't Panic! It is Just Turbulence
+-- MAIN APP DEPOTS
+addappid(4187141, 1, "45bfa6b3b7af407152dac002b68fe8cfc3c1b2a0918bacea0b67e2efeeeb40b1") -- Depot 4187141
+setManifestid(4187141, "6102028527724791848", 3731809679)
