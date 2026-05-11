1a9b46429ca63ecd2f85e6259e2d3a527b0749f2
qiao-lima <qiao-lima@github.com>
2026-05-12 00:58:00 +0800

    Manual Update 4216740

---
+++ 4216740.lua
@@ -0,0 +1,12 @@
+-- 4216740's Lua and Manifest Created by Hubcap Manifest
+-- Toot Odyssey
+-- Created: May 11, 2026 at 04:28:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4216740) -- Toot Odyssey
+-- MAIN APP DEPOTS
+addappid(4216741, 1, "69906e1833f6116723a7f8ad7216ed57874e74ee8488c51e46e4824512b2868f") -- Depot 4216741
+setManifestid(4216741, "5476956339689092314", 367254225)
