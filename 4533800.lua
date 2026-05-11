984e6c6f71c65bdc806d911feef27b5099a2a835
qiao-lima <qiao-lima@github.com>
2026-05-11 19:54:30 +0800

    Manual Update 4533800

---
+++ 4533800.lua
@@ -0,0 +1,12 @@
+-- 4533800's Lua and Manifest Created by Hubcap Manifest
+-- Horny: Zero Moan 🔞🦕
+-- Created: May 10, 2026 at 07:19:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4533800) -- Horny: Zero Moan 🔞🦕
+-- MAIN APP DEPOTS
+addappid(4533801, 1, "c8e1aea4b6a54c665e647238331e6c211a3c3ba7493d29f44a6ec6f780f04057") -- Depot 4533801
+setManifestid(4533801, "7123489745878596841", 3127678634)
