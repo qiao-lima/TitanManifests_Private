3fd49b43ce5ac0d487b7ea28f93e0497538ab8b2
qiao-lima <qiao-lima@github.com>
2026-05-12 00:57:59 +0800

    Manual Update 3119700

---
+++ 3119700.lua
@@ -0,0 +1,12 @@
+-- 3119700's Lua and Manifest Created by Hubcap Manifest
+-- NOOK FALL: West Town
+-- Created: May 11, 2026 at 06:13:08 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3119700) -- NOOK FALL: West Town
+-- MAIN APP DEPOTS
+addappid(3119701, 1, "62b9d6f97e09a2f32b518459b0b31440faa147bf0ffe29bcee6a7501242da471") -- Depot 3119701
+setManifestid(3119701, "9031271053162669430", 3891233274)
