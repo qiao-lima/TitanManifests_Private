eb65f650d7594f8b4444a9282f8cdc0cdd62f843
qiao-lima <qiao-lima@github.com>
2026-05-11 19:58:17 +0800

    Manual Update 4572730

---
+++ 4572730.lua
@@ -0,0 +1,12 @@
+-- 4572730's Lua and Manifest Created by Hubcap Manifest
+-- Box Office Mayhem: Theatre Tycoon
+-- Created: May 10, 2026 at 11:00:25 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4572730) -- Box Office Mayhem: Theatre Tycoon
+-- MAIN APP DEPOTS
+addappid(4572731, 1, "0df331b1f9ed41408d48a167ee3447d23c4a2dff50c532de1ac52ac57475b05e") -- Depot 4572731
+setManifestid(4572731, "8020136831268590555", 366764822)
