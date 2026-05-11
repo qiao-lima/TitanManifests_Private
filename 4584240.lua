d8e993a4f52fcdabdfa49fd698436a8f64135510
qiao-lima <qiao-lima@github.com>
2026-05-12 00:58:01 +0800

    Manual Update 4584240

---
+++ 4584240.lua
@@ -0,0 +1,12 @@
+-- 4584240's Lua and Manifest Created by Hubcap Manifest
+-- Cheap Car Repair: Welcome to Nowhere
+-- Created: May 11, 2026 at 09:59:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4584240) -- Cheap Car Repair: Welcome to Nowhere
+-- MAIN APP DEPOTS
+addappid(4584241, 1, "1c657046a969fe15a40b3ff2ebfcfa0c7624f4670b6918fde1dd200c21b59a87") -- Depot 4584241
+setManifestid(4584241, "1959136955043194668", 4660369678)
