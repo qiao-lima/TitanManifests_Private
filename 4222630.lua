55886c831e683d1c1415ded0f377ce79c65abef6
qiao-lima <qiao-lima@github.com>
2026-05-11 19:54:27 +0800

    Manual Update 4222630

---
+++ 4222630.lua
@@ -0,0 +1,14 @@
+-- 4222630's Lua and Manifest Created by Hubcap Manifest
+-- Kuentame
+-- Created: May 10, 2026 at 04:39:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4222630) -- Kuentame
+-- MAIN APP DEPOTS
+addappid(4222633, 1, "e19ffa18c4230fa69bcb6fde03e5fd5864a04c115948e44a7cd41c91c63f366c") -- Depot 4222633
+setManifestid(4222633, "6067506250953254875", 542172504)
+addappid(4222634, 1, "c7f5cb3bc475820f4f023805ffcb15bb93672ee68454e72ef3518d5bbe21ca39") -- Depot 4222634
+setManifestid(4222634, "3964446226183286749", 515646552)
