5cfcddcf48097222de8f96c482165744baa59a5f
qiao-lima <qiao-lima@github.com>
2026-05-11 19:54:28 +0800

    Manual Update 4400640

---
+++ 4400640.lua
@@ -0,0 +1,18 @@
+-- 4400640's Lua and Manifest Created by Hubcap Manifest
+-- Fight For Peace
+-- Created: May 10, 2026 at 00:00:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4400640) -- Fight For Peace
+-- MAIN APP DEPOTS
+addappid(4400641, 1, "370e971c94675b103c09165feaad65ae6df9070ad86803120e14a2f43b4b7b05") -- Depot 4400641
+setManifestid(4400641, "1932703896117224998", 986623259)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
