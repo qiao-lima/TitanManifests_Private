4bd9c8d21cfdc3d54b20f05d43d410436d62ce6e
qiao-lima <qiao-lima@github.com>
2026-05-12 00:57:57 +0800

    Manual Update 2535950

---
+++ 2535950.lua
@@ -0,0 +1,16 @@
+-- 2535950's Lua and Manifest Created by Hubcap Manifest
+-- Battlestar Galactica: Scattered Hopes
+-- Created: May 11, 2026 at 11:49:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(2535950) -- Battlestar Galactica: Scattered Hopes
+-- MAIN APP DEPOTS
+addappid(2535951, 1, "9522e4772ecea6f6bf60334b81a0cfe043c04559e35dc9525911ef68219219f9") -- Depot 2535951
+setManifestid(2535951, "7768690925371688647", 5425019681)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Battlestar Galactica Scattered Hopes - Digital Artbook (AppID: 4428720) - missing depot keys
+-- addappid(4428720)
