8a798d73faef7956955d3ea8b3e6dc5ee69313f3
qiao-lima <qiao-lima@github.com>
2026-05-12 00:57:58 +0800

    Manual Update 2561190

---
+++ 2561190.lua
@@ -0,0 +1,12 @@
+-- 2561190's Lua and Manifest Created by Hubcap Manifest
+-- Claire's Cruisin' Cafe: Fest Frenzy
+-- Created: May 11, 2026 at 09:13:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2561190) -- Claire's Cruisin' Cafe: Fest Frenzy
+-- MAIN APP DEPOTS
+addappid(2561191, 1, "d82df86732c67eedcd19b8a28bc4277fa23587e8be4adeb2a0c3b00d7a757324") -- Depot 2561191
+setManifestid(2561191, "3744120582483857549", 331685878)
