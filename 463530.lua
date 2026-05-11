ee0cb01f2e613bc36e899711f2636baf172fff68
qiao-lima <qiao-lima@github.com>
2026-05-11 08:24:05 +0800

    Manual Update 463530

---
+++ 463530.lua
@@ -0,0 +1,37 @@
+-- 463530's Lua and Manifest Created by Hubcap Manifest
+-- Empires of the Undergrowth
+-- Created: May 06, 2026 at 06:08:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 10
+-- Total DLCs: 2
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(463530, 1, "57dedb1ce5e4b1cfb32fe82e1c70b82b1701d871586649e1eb5b9cfdf9b08ae6") -- Empires of the Undergrowth
+-- MAIN APP DEPOTS
+addappid(463531, 1, "913b6f7c71e6b2453f127bfaa8581883b1fda1fa980ddbe4b1ef1b3f9b6c9cef") -- Empires of the Undergrowth Content
+setManifestid(463531, "7607265908213533438", 5264522201)
+addappid(463532, 1, "f933bfcc5faca765720f0b113d3ca3048cec73afc259ec932c39cd7980444066") -- Empires of the Undergrowth Depot Win32
+setManifestid(463532, "8740411461507035877", 5930473396)
+addappid(463533, 1, "ee2baffa6fd92a5208e990513c71a8ea15fe00f12c67f74b6a45cc1b993f8191") -- Empires of the Undergrowth Depot Osx
+setManifestid(463533, "5168334823267069661", 10752423846)
+addappid(463534, 1, "d1d941eebf6e7e6d5b934828293a9707500d04c17bc3424e2120d7eb5bd29cea") -- Empires of the Undergrowth Depot Linux
+setManifestid(463534, "4928343751148336280", 3553255409)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Empires of the Undergrowth - Exploding Ants (AppID: 3402420)
+addappid(3402420)
+addappid(3402420, 1, "d38fba59cfa07acbbbde76ba76e404d66924c9af533c727158dfda0295ebe53d") -- Empires of the Undergrowth - Exploding Ants - Depot 3402420
+setManifestid(3402420, "4497732885158900609", 130198349)
+addappid(3402421, 1, "675e99a0c64273da53414a089742e4a957b854368a68ac7f159f7bd5a50cf60e") -- Empires of the Undergrowth - Exploding Ants - Depot 3402421
+setManifestid(3402421, "6774842373039925003", 290346216)
+-- Empires of the Undergrowth - Supporter Pack (AppID: 3402630)
+addappid(3402630)
+addappid(3402630, 1, "7e357e77cf247a88ee09e9b0316e424dcf790c29998d90cdf003e980f37ce804") -- Empires of the Undergrowth - Supporter Pack - Depot 3402630
+setManifestid(3402630, "5167536031889139784", 2326246319)
+addappid(3402631, 1, "983320607be10bce900732aa790408c9c2abdcd205be4f97f081709f9391de86") -- Empires of the Undergrowth - Supporter Pack - Depot 3402631
+setManifestid(3402631, "3738389894317744682", 2326281112)
