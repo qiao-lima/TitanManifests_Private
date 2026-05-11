64a0267fb17729e710c045a66634987be9e83bf0
qiao-lima <qiao-lima@github.com>
2026-05-11 19:54:24 +0800

    Manual Update 256050

---
+++ 256050.lua
@@ -0,0 +1,24 @@
+-- 256050's Lua and Manifest Created by Hubcap Manifest
+-- Adventure Park
+-- Created: May 11, 2026 at 01:30:06 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 7
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(256050) -- Adventure Park
+-- MAIN APP DEPOTS
+addappid(256051, 1, "506747827253f50336caa045f107a6064ba59f3ee95cb3862a8b1676fc954fa2") -- AdventurePark_release
+setManifestid(256051, "728579172639062323", 1465883641)
+addappid(256052, 1, "a8d5d91980c17a66465b244d06488999511b64fc83e82c14e2a596766d46096b") -- AdventurePark_english
+setManifestid(256052, "2645856934938260018", 4474583)
+addappid(256053, 1, "df9cc2c14669e42cdaaad2c8dd961ab78a9ce6e1ac46059179319d12bffd6659") -- AdventurePark_german
+setManifestid(256053, "6344129536746730228", 5446631)
+addappid(256054, 1, "e4c1478ae0e084984bae455bd374413c9a4360294384d52a868aa4d47fc32b86") -- AdventurePark_french
+setManifestid(256054, "3711037234134556718", 4488559)
+addappid(256055, 1, "b5611b49faaefd3fe8d05e392b44fbe931bc4323fad7a4d7ddf1ff6b9ceb9ea6") -- AdventurePark_spanish
+setManifestid(256055, "8338326151694608832", 4487743)
+addappid(256056, 1, "9b19b797bc3e193d4f97df1611d5893a8d96e6ade49e6fe941e5752023931adf") -- AdventurePark_italian
+setManifestid(256056, "2763904906784482424", 4486856)
+addappid(256057, 1, "41e9c832c7716fefe9477101ed26a3a7fdcf1463ced9dfe2c851a523c2715cf6") -- AdventurePark_russian
+setManifestid(256057, "6140370487726974000", 4480390)
