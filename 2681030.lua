60a1f6a4f686b6573c75adf682b0e7f957984a4d
qiao-lima <qiao-lima@github.com>
2026-05-12 00:57:58 +0800

    Manual Update 2681030

---
+++ 2681030.lua
@@ -0,0 +1,14 @@
+-- 2681030's Lua and Manifest Created by Hubcap Manifest
+-- Outbound
+-- Created: May 11, 2026 at 10:34:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(2681030) -- Outbound
+-- MAIN APP DEPOTS
+addappid(2681031, 1, "feac3f781af42ac4e65cdb05e24f482c33e5660d23b377e86d744be6cc53a4bd") -- Depot 2681031
+setManifestid(2681031, "8448597862051614256", 13152889287)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4148860) -- Outbound  School Bus Adventures
