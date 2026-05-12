13d79650ec8cbbc0826d66f9c04e009b0354cd98
qiao-lima <qiao-lima@github.com>
2026-05-12 10:24:30 +0800

    Manual Update 2212790

---
+++ 2212790.lua
@@ -0,0 +1,12 @@
+-- 2212790's Lua and Manifest Created by Hubcap Manifest
+-- Asbury Pines
+-- Created: February 21, 2026 at 19:02:17 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2212790) -- Asbury Pines
+-- MAIN APP DEPOTS
+addappid(2212791, 1, "5cece11efae5950a5321f18fd31ec23dd2ec6ce70346200579cfd08943e1a4f1") -- Depot 2212791
+setManifestid(2212791, "881165531752007489", 910822673)
