c5d204510dd68284b19ee8f08d6fe35d5487c5a2
qiao-lima <qiao-lima@github.com>
2026-05-12 10:24:29 +0800

    Manual Update 1708950

---
+++ 1708950.lua
@@ -0,0 +1,22 @@
+-- 1708950's Lua and Manifest Created by Hubcap Manifest
+-- Battle Train
+-- Created: May 11, 2026 at 18:12:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0 (1 excluded)
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1708950) -- Battle Train
+-- MAIN APP DEPOTS
+addappid(1708951, 1, "c6eb8ad9996d20bfc317d60e24749f1e8d23b4f1773669414cb3c50a304c5423") -- Depot 1708951
+setManifestid(1708951, "8896979231224257763", 1343250403)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Battle Train - Revenge of the Puffin (AppID: 3509750) - missing depot keys
+-- addappid(3509750)
