a301de52a0f331258613838139d3ebc7c5bfcefb
qiao-lima <qiao-lima@github.com>
2026-05-12 10:24:30 +0800

    Manual Update 2144990

---
+++ 2144990.lua
@@ -0,0 +1,16 @@
+-- 2144990's Lua and Manifest Created by Hubcap Manifest
+-- Bryce Tiles
+-- Created: May 11, 2026 at 14:22:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2144990, 1, "de5ff1a352ccc2d54e640a3af90e1243edac8a3a1d16244f2cd1f4467ccf250c") -- Bryce Tiles
+-- MAIN APP DEPOTS
+addappid(2144991, 1, "3e7d2618ff7dae0cf4ec85e7589e1d7d7e299240f6265834e600f0ccf83a80da") -- Depot 2144991
+setManifestid(2144991, "533341442015010625", 468584223)
+addappid(2144992, 1, "c88bd66903274a499a683bcacb534a72af06af8236daf5a92328f108db5a5912") -- Depot 2144992
+setManifestid(2144992, "1727619092210046717", 481330647)
+addappid(2144993, 1, "b764d6c313532971baecb8fbace3c990924077f4e95b37e2dea6c5909064a147") -- Depot 2144993
+setManifestid(2144993, "8699555429012805487", 555501582)
