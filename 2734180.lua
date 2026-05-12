aece6dc76af0f5cadcb8e7c3691e9683a2c9e6f9
qiao-lima <qiao-lima@github.com>
2026-05-12 10:24:31 +0800

    Manual Update 2734180

---
+++ 2734180.lua
@@ -0,0 +1,12 @@
+-- 2734180's Lua and Manifest Created by Hubcap Manifest
+-- Cube-Field+
+-- Created: May 11, 2026 at 18:16:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2734180) -- Cube-Field+
+-- MAIN APP DEPOTS
+addappid(2734182, 1, "91b7783a7ffcaec42d98ded410847fc748a6ca0740592dff3876d1e31c5cdd6d") -- Depot 2734182
+setManifestid(2734182, "1013733680122806869", 212042462)
