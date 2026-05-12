f92d571b02191293ee0515eda9f8b8cee6aa2c90
qiao-lima <qiao-lima@github.com>
2026-05-12 10:24:31 +0800

    Manual Update 3271720

---
+++ 3271720.lua
@@ -0,0 +1,14 @@
+-- 3271720's Lua and Manifest Created by Hubcap Manifest
+-- Obliterator-X
+-- Created: May 11, 2026 at 18:19:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3271720, 1, "78a5959f6cefc7f44c4a8e0230424b89e2c33dcf037ec07f09d5ed1b29b6b444") -- Obliterator-X
+-- MAIN APP DEPOTS
+addappid(3271721, 1, "860cbfaa2da449f7a27ffe9ef6d98b859daf3e2732a0962e1488d407319304d4") -- Depot 3271721
+setManifestid(3271721, "2420499162095740717", 597958378)
+addappid(3271722, 1, "f1bad2fc61e903f575b4f04132c3ba6f4d586d63319861395add6620f0c1190f") -- Depot 3271722
+setManifestid(3271722, "1223884508039127940", 684111306)
