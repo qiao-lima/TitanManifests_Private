a96d1630d02027ccf79a1adc16cd62b4e64b81b4
qiao-lima <qiao-lima@github.com>
2026-05-12 10:24:29 +0800

    Manual Update 1872110

---
+++ 1872110.lua
@@ -0,0 +1,12 @@
+-- 1872110's Lua and Manifest Created by Hubcap Manifest
+-- Super Ninja Miner
+-- Created: May 11, 2026 at 14:22:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1872110) -- Super Ninja Miner
+-- MAIN APP DEPOTS
+addappid(1872111, 1, "2d2096bd91e7e5e2be561096a36395d5ee7572421ee47f8482c81cfd2160a988") -- Depot 1872111
+setManifestid(1872111, "5493505298693693488", 86366463)
