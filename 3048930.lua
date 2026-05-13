addcf53501207e4f78bea9c6fbfccfd33436d012
qiao-lima <qiao-lima@github.com>
2026-05-13 09:41:36 +0800

    Manual Update 3048930

---
+++ 3048930.lua
@@ -0,0 +1,16 @@
+-- 3048930's Lua and Manifest Created by Hubcap Manifest
+-- Duriano
+-- Created: May 12, 2026 at 01:33:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(3048930) -- Duriano
+-- MAIN APP DEPOTS
+addappid(3048931, 1, "d3a86128848a33a4ac7a83312dedec0a9aa1df7292469662afafbb748564ee7f") -- Depot 3048931
+setManifestid(3048931, "2216966398052860319", 2523523965)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Duriano - Digital Artbook (AppID: 3678720) - missing depot keys
+-- addappid(3678720)
