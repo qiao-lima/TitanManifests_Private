9deb47aa6e33517a11a088a26ff99a899fe005aa
qiao-lima <qiao-lima@github.com>
2026-05-13 09:46:25 +0800

    Manual Update 4633540

---
+++ 4633540.lua
@@ -0,0 +1,14 @@
+-- 4633540's Lua and Manifest Created by Hubcap Manifest
+-- Soft Money
+-- Created: May 12, 2026 at 07:43:13 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4633540) -- Soft Money
+-- MAIN APP DEPOTS
+addappid(4633541, 1, "35752ad94282e9a8b0f6cf34d93394b917074006343c6e386167955d11e12aae") -- Depot 4633541
+setManifestid(4633541, "6071356982531412879", 1572259063)
+addappid(4633542, 1, "cb2df2ce0633ed53eb78e3c78a92161a42335fa2f24ecc397deff2f5d716d38a") -- Depot 4633542
+setManifestid(4633542, "384898107308165360", 1409642430)
