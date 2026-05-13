3c9d9bc76e293533bb0105dc6a5714501019afaa
qiao-lima <qiao-lima@github.com>
2026-05-13 09:41:37 +0800

    Manual Update 3459760

---
+++ 3459760.lua
@@ -0,0 +1,20 @@
+-- 3459760's Lua and Manifest Created by Hubcap Manifest
+-- 18Floor
+-- Created: May 12, 2026 at 15:37:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3459760) -- 18Floor
+-- MAIN APP DEPOTS
+addappid(3459761, 1, "00f893269f2868054a01636846742e07c30dfc7c287875eb143ae127ecd08d76") -- Depot 3459761
+setManifestid(3459761, "2208885190295987248", 820715658)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3459762) -- Depot 3459762 (empty depot)
