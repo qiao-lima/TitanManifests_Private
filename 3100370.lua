2147ad176159f72bb123b4ef431042b16316179c
qiao-lima <qiao-lima@github.com>
2026-05-13 09:41:36 +0800

    Manual Update 3100370

---
+++ 3100370.lua
@@ -0,0 +1,14 @@
+-- 3100370's Lua and Manifest Created by Hubcap Manifest
+-- Black Jacket
+-- Created: May 12, 2026 at 15:47:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3100370) -- Black Jacket
+-- MAIN APP DEPOTS
+addappid(3100371, 1, "6f520af8fe511bdc08ff85343a4c6976c94bea0213dd877ab716758d5813cb2b") -- Depot 3100371
+setManifestid(3100371, "4513406843138498793", 3135748759)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4635860) -- Supporter Pack
