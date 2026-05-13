270bf1407de90de3a5135e06c9d2ec33f856b381
qiao-lima <qiao-lima@github.com>
2026-05-13 09:41:37 +0800

    Manual Update 3392300

---
+++ 3392300.lua
@@ -0,0 +1,12 @@
+-- 3392300's Lua and Manifest Created by Hubcap Manifest
+-- Sterling Shroud
+-- Created: May 12, 2026 at 07:42:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3392300, 1, "add147829929e7b46ad47fc60953380275411868029e931e8f6ba235420cff97") -- Sterling Shroud
+-- MAIN APP DEPOTS
+addappid(3392301, 1, "a1575d112c829edf4b55c37af0145f757d7a078722e6751245b3d4dd0b65b8be") -- Depot 3392301
+setManifestid(3392301, "1193567378930831434", 129389653)
