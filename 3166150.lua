f0028e5fdb857fd77ad2a152e376c1461b43c933
qiao-lima <qiao-lima@github.com>
2026-05-13 09:41:37 +0800

    Manual Update 3166150

---
+++ 3166150.lua
@@ -0,0 +1,12 @@
+-- 3166150's Lua and Manifest Created by Hubcap Manifest
+-- Ratcheteer DX
+-- Created: May 12, 2026 at 01:04:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3166150) -- Ratcheteer DX
+-- MAIN APP DEPOTS
+addappid(3166151, 1, "2790cf495381e5043a5d733b5cc40e470fe16acaebb31284b8f3590d0dafc6da") -- Depot 3166151
+setManifestid(3166151, "3275889095211057451", 316742187)
