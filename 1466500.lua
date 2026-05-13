af197dd249a1c449816d627d748264edf2d3a633
qiao-lima <qiao-lima@github.com>
2026-05-13 09:41:34 +0800

    Manual Update 1466500

---
+++ 1466500.lua
@@ -0,0 +1,14 @@
+-- 1466500's Lua and Manifest Created by Hubcap Manifest
+-- AKUMA
+-- Created: May 12, 2026 at 02:05:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1466500, 1, "b8e6fab8f5eee750b4666c060266668fbe3220f10330cf00bb1aca27dd4b8a71") -- AKUMA
+-- MAIN APP DEPOTS
+addappid(1466501, 1, "98a9aefed0bee633bf3eceba41f5f139e72e621b22f247278d42a2d6cad2c272") -- Depot 1466501
+setManifestid(1466501, "934486498058030189", 693391050)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(1466502) -- Depot 1466502 (empty depot)
