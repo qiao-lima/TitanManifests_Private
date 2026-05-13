6f94d719716cb1ab8dc63328d330656094139e1e
qiao-lima <qiao-lima@github.com>
2026-05-13 09:46:24 +0800

    Manual Update 4629680

---
+++ 4629680.lua
@@ -0,0 +1,12 @@
+-- 4629680's Lua and Manifest Created by Hubcap Manifest
+-- The Door in The Skybox
+-- Created: May 12, 2026 at 07:36:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4629680) -- The Door in The Skybox
+-- MAIN APP DEPOTS
+addappid(4629681, 1, "3f234686b1d8f5dfa325d4870f7987b60f32d019ddf3f3eaaf9dbdf544f19b68") -- Depot 4629681
+setManifestid(4629681, "2579072784587498242", 665380574)
