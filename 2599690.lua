7b903073a38316a41f1596af970c0a02a08f5f86
qiao-lima <qiao-lima@github.com>
2026-05-13 09:41:36 +0800

    Manual Update 2599690

---
+++ 2599690.lua
@@ -0,0 +1,18 @@
+-- 2599690's Lua and Manifest Created by Hubcap Manifest
+-- Better than Dead
+-- Created: May 12, 2026 at 11:30:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2599690) -- Better than Dead
+-- MAIN APP DEPOTS
+addappid(2599692, 1, "0a0fefd8dd1858b402ca615385cc45515e507284cac4fce433441209664fd80a") -- Depot 2599692
+setManifestid(2599692, "5240197179450449471", 10869779243)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
