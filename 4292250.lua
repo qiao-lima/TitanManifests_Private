6680d4555647ccd554480982c735ab280716f3c7
qiao-lima <qiao-lima@github.com>
2026-05-13 09:41:38 +0800

    Manual Update 4292250

---
+++ 4292250.lua
@@ -0,0 +1,14 @@
+-- 4292250's Lua and Manifest Created by Hubcap Manifest
+-- Lastlore Saga
+-- Created: May 12, 2026 at 15:37:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4292250, 1, "9cc8b93eee84af61f3e727669e242b94c26ad36f297f9e87366071354d6f39c4") -- Lastlore Saga
+-- MAIN APP DEPOTS
+addappid(4292251, 1, "b0e004d7b15e75208554951cbe18a5e253ac1525de51209ea1e7e6e38db250c6") -- Depot 4292251
+setManifestid(4292251, "5050362864346300505", 1162409046)
+addappid(4292252, 1, "5de22e204c4f2fcb330eaa1f4f9ae2f54022af39140afb34d7c7acc4d4592611") -- Depot 4292252
+setManifestid(4292252, "5109253176588429533", 1154861006)
