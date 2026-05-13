e2109feb646a1089a1fddcdbae23ccaf4dd94776
qiao-lima <qiao-lima@github.com>
2026-05-13 09:46:24 +0800

    Manual Update 4499530

---
+++ 4499530.lua
@@ -0,0 +1,14 @@
+-- 4499530's Lua and Manifest Created by Hubcap Manifest
+-- Don Quijote
+-- Created: May 12, 2026 at 05:45:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4499530) -- Don Quijote
+-- MAIN APP DEPOTS
+addappid(4499531, 1, "282fcbdd30eb52472ff24f88dd0dd823c0c4f95ee699d01383b141de391a6f0c") -- Depot 4499531
+setManifestid(4499531, "7246425610010566564", 507647340)
+addappid(4499532, 1, "3827f5570509d6f9f79483188ed34189039c46f7aafce17bdec75f2810f2797c") -- Depot 4499532
+setManifestid(4499532, "4765351138167804339", 481044068)
