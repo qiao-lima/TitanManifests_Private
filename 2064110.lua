a25efc50692ed8644d37ce43a0768b40dc2d6ce1
qiao-lima <qiao-lima@github.com>
2026-05-13 09:41:35 +0800

    Manual Update 2064110

---
+++ 2064110.lua
@@ -0,0 +1,13 @@
+-- 2064110's Lua and Manifest Created by Hubcap Manifest
+-- U96
+-- Created: May 11, 2026 at 20:11:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2064110) -- U96
+addtoken(2064110, "7256279354153026029")
+-- MAIN APP DEPOTS
+addappid(2064111, 1, "67b6e39ca48d467b40094ce30f1b74a2caf49d207515bba723fcb391154c8681") -- Depot 2064111
+setManifestid(2064111, "1680194350917489638", 7464768)
