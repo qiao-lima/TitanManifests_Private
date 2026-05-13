bde1dbd6f4dfc42bca0c42334555d82f2c76454d
qiao-lima <qiao-lima@github.com>
2026-05-13 09:46:24 +0800

    Manual Update 4402730

---
+++ 4402730.lua
@@ -0,0 +1,16 @@
+-- 4402730's Lua and Manifest Created by Hubcap Manifest
+-- Port Trader
+-- Created: May 12, 2026 at 07:42:59 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4402730) -- Port Trader
+-- MAIN APP DEPOTS
+addappid(4402732, 1, "3ddc6f1df103be24beb6d48740432de394e1dff8a5e9c17d8fe7e8638f8a50b2") -- Depot 4402732
+setManifestid(4402732, "4685568540278869350", 341031105)
+addappid(4402733, 1, "abfec983bb76b92a6c3d9ae873b2e04dc025a5d1ab38138f63dae18888118ea0") -- Depot 4402733
+setManifestid(4402733, "6985238006217619891", 315382901)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4402731) -- Depot 4402731 (empty depot)
