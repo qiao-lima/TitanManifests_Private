6a4a0d20f12765293207e85eb40da34e6fba2693
qiao-lima <qiao-lima@github.com>
2026-05-13 09:46:24 +0800

    Manual Update 4519530

---
+++ 4519530.lua
@@ -0,0 +1,12 @@
+-- 4519530's Lua and Manifest Created by Hubcap Manifest
+-- Mining Goblins Idle
+-- Created: May 12, 2026 at 15:37:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4519530) -- Mining Goblins Idle
+-- MAIN APP DEPOTS
+addappid(4519531, 1, "edf09e9b4d37d90fceb498207ac3d16f965dfa83011335f4d2223600111e7cd4") -- Depot 4519531
+setManifestid(4519531, "7688493439780659539", 174522468)
