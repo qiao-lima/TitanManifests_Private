ce0a285dc49817da75d08c2ae17069e5f7ba43b4
qiao-lima <qiao-lima@github.com>
2026-05-13 09:41:37 +0800

    Manual Update 3432650

---
+++ 3432650.lua
@@ -0,0 +1,12 @@
+-- 3432650's Lua and Manifest Created by Hubcap Manifest
+-- 追曙ZHUISHU
+-- Created: May 12, 2026 at 04:35:13 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3432650) -- 追曙ZHUISHU
+-- MAIN APP DEPOTS
+addappid(3432651, 1, "01779487e165f7b1979f0df937b512266305e71529193ab67ba92988b975cd08") -- Depot 3432651
+setManifestid(3432651, "7808164008573998572", 16824616742)
