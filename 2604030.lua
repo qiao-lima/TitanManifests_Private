434a6c11fbaaa349c4628127ccbe0a9274de244a
qiao-lima <qiao-lima@github.com>
2026-05-13 09:41:36 +0800

    Manual Update 2604030

---
+++ 2604030.lua
@@ -0,0 +1,12 @@
+-- 2604030's Lua and Manifest Created by Hubcap Manifest
+-- Skies Above
+-- Created: May 11, 2026 at 18:21:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2604030) -- Skies Above
+-- MAIN APP DEPOTS
+addappid(2604032, 1, "9be0a4f2812ec41d5aeca36cd28f93a95e0830b8ebd5e0ac8ae8dffd1231c044") -- Depot 2604032
+setManifestid(2604032, "4805028936828249713", 279023176)
