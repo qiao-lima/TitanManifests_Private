b6f1cb50df603732cbee195909ed8f4c473f9254
qiao-lima <qiao-lima@github.com>
2026-05-13 09:41:38 +0800

    Manual Update 3952880

---
+++ 3952880.lua
@@ -0,0 +1,14 @@
+-- 3952880's Lua and Manifest Created by Hubcap Manifest
+-- Chill & Reel
+-- Created: May 12, 2026 at 05:45:13 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3952880) -- Chill & Reel
+-- MAIN APP DEPOTS
+addappid(3952881, 1, "d576aceeb475973bec89bb8a659c4df3e92d18d276971ae80d8db0a838dfdf40") -- Depot 3952881
+setManifestid(3952881, "501222513199965816", 444997556)
+addappid(3952882, 1, "2b2e9ddd6b6f8f1a8ade84f675e28418173b7d9510c909461778ea066ef881c6") -- Depot 3952882
+setManifestid(3952882, "3121098458903951985", 424480600)
