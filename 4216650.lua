a76473a7654cbefcbaea66c5245c688848f08a64
qiao-lima <qiao-lima@github.com>
2026-05-13 09:41:38 +0800

    Manual Update 4216650

---
+++ 4216650.lua
@@ -0,0 +1,18 @@
+-- 4216650's Lua and Manifest Created by Hubcap Manifest
+-- Tennis Manager 26
+-- Created: May 12, 2026 at 07:42:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(4216650, 1, "b37460bcb3739547363d0e99f57a4a6779d81f2e23656cfa0f493a538073f382") -- Tennis Manager 26
+-- MAIN APP DEPOTS
+addappid(4216651, 1, "10173c2f1704f4fa8ad0ea3effc90cfd41ad7fbd63b4a3c51efed2723c846391") -- Depot 4216651
+setManifestid(4216651, "73151809304894521", 4264955491)
+addappid(4216653, 1, "6bb8a9b457bf62a7c464c18eb7c69e6c28e849bd6d7af3e7c5eabc1d43cead9f") -- Depot 4216653
+setManifestid(4216653, "7792380258567544954", 4096287147)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
