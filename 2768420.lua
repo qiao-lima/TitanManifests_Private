d72f749c54fcf104e28cb7292345dc7d9c277b50
qiao-lima <qiao-lima@github.com>
2026-05-13 09:41:36 +0800

    Manual Update 2768420

---
+++ 2768420.lua
@@ -0,0 +1,14 @@
+-- 2768420's Lua and Manifest Created by Hubcap Manifest
+-- The Last Winter Knight
+-- Created: May 12, 2026 at 07:42:08 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2768420, 1, "d94e380224eafc591a9d222ef21c1dd9517447fc220328051852ece5dce03d29") -- The Last Winter Knight
+-- MAIN APP DEPOTS
+addappid(2768429, 1, "45392ad5c18fb1eefa059dbaae6f26f8f4adde9c5007a65cd5252de8b2026cdb") -- Depot 2768429
+setManifestid(2768429, "6192791249042545052", 1892536634)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2768428) -- Depot 2768428 (empty depot)
