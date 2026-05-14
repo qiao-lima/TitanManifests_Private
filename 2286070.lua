9c1126ae0e5b00755d4c496365d92881253303c7
qiao-lima <qiao-lima@github.com>
2026-05-14 13:19:16 +0800

    Manual Update 2286070

---
+++ 2286070.lua
@@ -0,0 +1,12 @@
+-- 2286070's Lua and Manifest Created by Hubcap Manifest
+-- Tale of REN
+-- Created: May 13, 2026 at 07:01:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2286070) -- Tale of REN
+-- MAIN APP DEPOTS
+addappid(2286071, 1, "51b1c6a4037d36d4e0f5417a233b860435fc35b7539a34473102a7920430cda3") -- Depot 2286071
+setManifestid(2286071, "5529668652357233859", 3831577055)
