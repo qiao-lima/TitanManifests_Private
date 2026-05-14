e2d542e24daad3ef5f1d81994d06ce73d0c9a0ce
qiao-lima <qiao-lima@github.com>
2026-05-14 13:19:15 +0800

    Manual Update 1190000

---
+++ 1190000.lua
@@ -0,0 +1,38 @@
+-- 1190000's Lua and Manifest Created by Hubcap Manifest
+-- Car Mechanic Simulator 2021
+-- Created: March 30, 2026 at 12:45:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 21
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1190000, 1, "c0d7730c96f14fba164bf0e6ff5c8c420edd65811dca098c637b67c6317985a1") -- Car Mechanic Simulator 2021
+-- MAIN APP DEPOTS
+addappid(1190001, 1, "b4f04348a15ca7c50e5275db9f0569947a2e3926ad0f05db23b4032050ee2f77") -- Car Mechanic Simulator 2021 Content
+setManifestid(1190001, "5273150823273090139", 26819240726)
+-- SHARED DEPOTS (from other apps)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1685720) -- Car Mechanic Simulator 2021 - Nissan DLC
+addappid(1685721) -- Car Mechanic Simulator 2021 - Electric Car DLC
+addappid(1748990) -- Car Mechanic Simulator 2021 - Jaguar DLC
+addappid(1748991) -- Car Mechanic Simulator 2021 - Land Rover DLC
+addappid(1773800) -- Car Mechanic Simulator 2021 - Pagani Remastered DLC
+addappid(1773801) -- Car Mechanic Simulator 2021 - Porsche Remastered DLC
+addappid(1931780) -- Car Mechanic Simulator 2021 - Hot Rod DLC
+addappid(1981550) -- Car Mechanic Simulator 2021 - Lotus Remastered DLC
+addappid(2085260) -- Car Mechanic Simulator 2021 - Mazda Remastered DLC
+addappid(2112230) -- Car Mechanic Simulator 2021 - Aston Martin DLC
+addappid(2112231) -- Car Mechanic Simulator 2021 - Drag Racing DLC
+addappid(2112232) -- Car Mechanic Simulator 2021 - Mercedes Remastered DLC
+addappid(2282030) -- Car Mechanic Simulator 2021 - Ford Remastered DLC
+addappid(2464230) -- Car Mechanic Simulator 2021 - Dodge  Plymouth  Chrysler DLC
+addappid(2546830) -- Car Mechanic Simulator 2021 - China DLC
+addappid(2601200) -- Car Mechanic Simulator 2021 - BMW DLC
+addappid(2721070) -- Car Mechanic Simulator 2021 - Rims DLC
+addappid(2889700) -- Car Mechanic Simulator 2021 - Jeep  RAM Remastered DLC
+addappid(3335470) -- Car Mechanic Simulator 2021 - Taxi DLC
+addappid(3660210) -- Car Mechanic Simulator 2021 - Police DLC
+addappid(4185010) -- Car Mechanic Simulator 2021 - Rims 2 DLC
