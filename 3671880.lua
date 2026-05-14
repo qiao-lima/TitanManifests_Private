f5378f70047e8e8cb36938c8457690f76ffd1597
qiao-lima <qiao-lima@github.com>
2026-05-14 13:19:19 +0800

    Manual Update 3671880

---
+++ 3671880.lua
@@ -0,0 +1,12 @@
+-- 3671880's Lua and Manifest Created by Hubcap Manifest
+-- BAROQUE SHOOTING: REVERSED
+-- Created: May 13, 2026 at 01:17:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3671880) -- BAROQUE SHOOTING: REVERSED
+-- MAIN APP DEPOTS
+addappid(3671881, 1, "11caf1b65c343b902904754ce5446216458671fcf9d2ca49e49874e4a67af7e5") -- Depot 3671881
+setManifestid(3671881, "6771087169364485852", 267538961)
