85dc1a6f89b09443897ba43840778503e07262e5
qiao-lima <qiao-lima@github.com>
2026-05-14 13:21:54 +0800

    Manual Update 4541460

---
+++ 4541460.lua
@@ -0,0 +1,12 @@
+-- 4541460's Lua and Manifest Created by Hubcap Manifest
+-- A Smithing Game
+-- Created: May 13, 2026 at 13:31:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4541460) -- A Smithing Game
+-- MAIN APP DEPOTS
+addappid(4541461, 1, "fde1089e7043134b8994fe7f18685dd2e2ffc1e698031f28375d9277a0174b8f") -- Depot 4541461
+setManifestid(4541461, "5849215430095623603", 1210639453)
