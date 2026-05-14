307608b3e4644483a2a05dfdaf506dd4d12d5b83
qiao-lima <qiao-lima@github.com>
2026-05-14 13:19:19 +0800

    Manual Update 3784760

---
+++ 3784760.lua
@@ -0,0 +1,14 @@
+-- 3784760's Lua and Manifest Created by Hubcap Manifest
+-- Elfie: A Sand Plan
+-- Created: May 13, 2026 at 06:44:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3784760, 1, "ab5e73a328ac58352817eb695f6029bb20272f3731d85136c9ee2b8677e97a1b") -- Elfie: A Sand Plan
+-- MAIN APP DEPOTS
+addappid(3784761, 1, "14dcd89782cc84820623e19de916dd680c505a24b65078876754c31466192e2f") -- Depot 3784761
+setManifestid(3784761, "590615279147388794", 262707760)
+addappid(3784762, 1, "35cffd51ad53357540360b700fbc4a769ed4e57c928ddff5b3a3ffd58c3d51b5") -- Depot 3784762
+setManifestid(3784762, "4161578973446960275", 363617499)
