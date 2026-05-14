0373345abf8be1f0226245967a03c1b097030f3e
qiao-lima <qiao-lima@github.com>
2026-05-14 13:21:54 +0800

    Manual Update 4581410

---
+++ 4581410.lua
@@ -0,0 +1,12 @@
+-- 4581410's Lua and Manifest Created by Hubcap Manifest
+-- CAMERAMAN NEVER DIES
+-- Created: May 13, 2026 at 11:56:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4581410) -- CAMERAMAN NEVER DIES
+-- MAIN APP DEPOTS
+addappid(4581411, 1, "ccefc1d71f1132def69c9223b6c2b106cf40fbb2183faed2f75c03ae27f9e498") -- Depot 4581411
+setManifestid(4581411, "2007950918078165019", 3054487155)
