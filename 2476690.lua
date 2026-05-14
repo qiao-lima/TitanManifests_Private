2ca17b3d38c06d874ce2d3b8c3a24d753a9f8c6e
qiao-lima <qiao-lima@github.com>
2026-05-14 13:19:17 +0800

    Manual Update 2476690

---
+++ 2476690.lua
@@ -0,0 +1,12 @@
+-- 2476690's Lua and Manifest Created by Hubcap Manifest
+-- ARIE: Moonprayer
+-- Created: May 13, 2026 at 10:14:19 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2476690) -- ARIE: Moonprayer
+-- MAIN APP DEPOTS
+addappid(2476691, 1, "ab07aff75fd1c3442072a305009647507e3ad47c8437ab3583bdbcd7ff1846b1") -- Depot 2476691
+setManifestid(2476691, "2833680888998650803", 3218274016)
