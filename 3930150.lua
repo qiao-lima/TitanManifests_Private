31e54fe2ff5101877fb3e9f36fcb88587f39bab9
qiao-lima <qiao-lima@github.com>
2026-05-14 13:19:19 +0800

    Manual Update 3930150

---
+++ 3930150.lua
@@ -0,0 +1,12 @@
+-- 3930150's Lua and Manifest Created by Hubcap Manifest
+-- Mr Toilet
+-- Created: May 13, 2026 at 15:08:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3930150) -- Mr Toilet
+-- MAIN APP DEPOTS
+addappid(3930151, 1, "1798bd7c3ef1ee4dabd50a52efde0ec501c94b3dc75f1aedd858e037db3ff7a4") -- Depot 3930151
+setManifestid(3930151, "8963992070064792972", 1398435621)
