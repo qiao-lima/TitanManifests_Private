82012c0555df9d2491955861c590f7e884492a55
qiao-lima <qiao-lima@github.com>
2026-05-14 13:21:54 +0800

    Manual Update 4258510

---
+++ 4258510.lua
@@ -0,0 +1,12 @@
+-- 4258510's Lua and Manifest Created by Hubcap Manifest
+-- 廢案少⼥~克隆千⾦的調教⽣活
+-- Created: May 13, 2026 at 15:09:03 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4258510) -- 廢案少⼥~克隆千⾦的調教⽣活
+-- MAIN APP DEPOTS
+addappid(4258511, 1, "ab37226fc84e87937f90bd920a520fa79d90e0c901ac71b62a12cd224508ad83") -- Depot 4258511
+setManifestid(4258511, "2469338639107224999", 3075321190)
