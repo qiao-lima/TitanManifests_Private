bc4c1656495e1607a33f38809e69123ad12ea389
qiao-lima <qiao-lima@github.com>
2026-05-14 13:19:19 +0800

    Manual Update 3520180

---
+++ 3520180.lua
@@ -0,0 +1,12 @@
+-- 3520180's Lua and Manifest Created by Hubcap Manifest
+-- Tasty Bite
+-- Created: May 13, 2026 at 11:06:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3520180) -- Tasty Bite
+-- MAIN APP DEPOTS
+addappid(3520181, 1, "d0cbbe84df028f12d34503f8dd53e82f05299fc8b3a8a06a9e465b2051fe3198") -- Depot 3520181
+setManifestid(3520181, "3555790714842737763", 697917107)
