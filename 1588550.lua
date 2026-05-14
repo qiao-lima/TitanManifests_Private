d2cc78f01b9ed357b18a004b18e2c5a4ae7aaad3
qiao-lima <qiao-lima@github.com>
2026-05-14 13:19:16 +0800

    Manual Update 1588550

---
+++ 1588550.lua
@@ -0,0 +1,21 @@
+-- 1588550's Lua and Manifest Created by Hubcap Manifest
+-- Cairn
+-- Created: March 24, 2026 at 18:14:30 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 1
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1588550, 1, "9fdaabf80001d2f849f78555b34c5634ca0aa6ab5c89c74050d230fd44572ea8") -- Cairn
+-- MAIN APP DEPOTS
+addappid(1588551, 1, "4ce5dce22497dfd82c8fc6fc3a6f0c4f9961a811d460ab7ab6a590b717cae583") -- Depot 1588551
+setManifestid(1588551, "6920036086488603675", 12045354156)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITH DEDICATED DEPOTS
+-- Cairn - Deluxe Upgrade Pack (AppID: 4031100)
+addappid(4031100)
+addappid(4031101, 1, "a7b1bec9765224ce4eb45fd57d5111b7625aa4f21fd5a64f8f5a13851c770abe") -- Cairn - Deluxe Upgrade Pack - Depot 4031101
+setManifestid(4031101, "6645330320897889494", 21054175)
