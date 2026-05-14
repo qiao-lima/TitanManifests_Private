b345e2249aee3fdbf5691619eb9dff737f763b02
qiao-lima <qiao-lima@github.com>
2026-05-14 13:19:16 +0800

    Manual Update 1954200

---
+++ 1954200.lua
@@ -0,0 +1,20 @@
+-- 1954200's Lua and Manifest Created by Hubcap Manifest
+-- Kena: Bridge of Spirits
+-- Created: September 29, 2025 at 20:36:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1954200) -- Kena: Bridge of Spirits
+-- MAIN APP DEPOTS
+addappid(1954201, 1, "dbc042174e03cba3e4b8b08266abefcc5d6b3aab5926133d77969966a45d8b3e") -- Depot 1954201
+setManifestid(1954201, "1974986714015120124", 25120378060)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1964240) -- Kena Bridge of Spirits - Deluxe Pack
