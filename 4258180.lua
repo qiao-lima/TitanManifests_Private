9b07070ab94881df9227f8281e977037a7baf3ea
qiao-lima <qiao-lima@github.com>
2026-05-14 13:21:54 +0800

    Manual Update 4258180

---
+++ 4258180.lua
@@ -0,0 +1,14 @@
+-- 4258180's Lua and Manifest Created by Hubcap Manifest
+-- Inquiry Counse
+-- Created: May 12, 2026 at 21:14:30 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4258180) -- Inquiry Counse
+-- MAIN APP DEPOTS
+addappid(4258181, 1, "da4f657fe7ce49c8e8951c82a30628732cf07ca7772f7bcb47d1c364cf1307fa") -- Depot 4258181
+setManifestid(4258181, "7573353447537689193", 469866284)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4258182) -- Depot 4258182 (empty depot)
