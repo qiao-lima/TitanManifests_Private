0e5cb39bf105b1e68c2125e6201ace32ff085e49
qiao-lima <qiao-lima@github.com>
2026-05-14 13:19:18 +0800

    Manual Update 3296830

---
+++ 3296830.lua
@@ -0,0 +1,12 @@
+-- 3296830's Lua and Manifest Created by Hubcap Manifest
+-- CaseCracker3
+-- Created: May 13, 2026 at 07:11:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3296830) -- CaseCracker3
+-- MAIN APP DEPOTS
+addappid(3296831, 1, "9329c54a867f7dab8f8fe5abaa948c86ad04663dbfdba89c9d3bdd3f562701f4") -- Depot 3296831
+setManifestid(3296831, "6818081629341124099", 1149144352)
