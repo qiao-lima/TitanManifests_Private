97ab1cb868d6cf253f3058befb2326be51c83fdf
qiao-lima <qiao-lima@github.com>
2026-05-14 13:19:16 +0800

    Manual Update 1938100

---
+++ 1938100.lua
@@ -0,0 +1,18 @@
+-- 1938100's Lua and Manifest Created by Hubcap Manifest
+-- Notmads
+-- Created: May 14, 2026 at 01:17:58 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1938100) -- Notmads
+-- MAIN APP DEPOTS
+addappid(1938102, 1, "f059d1afa2f55d6d4bc7739ce06994a456bd5e384e75406d16ef2314b41bbec4") -- Depot 1938102
+setManifestid(1938102, "8312132354224991974", 5740060865)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
