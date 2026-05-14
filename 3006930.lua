afe94da96cac8d201bf1112a1da474656c4a6fce
qiao-lima <qiao-lima@github.com>
2026-05-14 13:19:17 +0800

    Manual Update 3006930

---
+++ 3006930.lua
@@ -0,0 +1,12 @@
+-- 3006930's Lua and Manifest Created by Hubcap Manifest
+-- 10秒ルール
+-- Created: May 13, 2026 at 15:08:25 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3006930) -- 10秒ルール
+-- MAIN APP DEPOTS
+addappid(3006931, 1, "00d8af8154662b2e19a958c667c9af3193cc697dd84f110def61f6ab8278f60e") -- Depot 3006931
+setManifestid(3006931, "3360370509943582793", 651539033)
