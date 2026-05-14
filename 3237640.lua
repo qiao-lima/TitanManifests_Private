382b633c8c6665e36db652b757983d518a428a59
qiao-lima <qiao-lima@github.com>
2026-05-14 13:19:18 +0800

    Manual Update 3237640

---
+++ 3237640.lua
@@ -0,0 +1,18 @@
+-- 3237640's Lua and Manifest Created by Hubcap Manifest
+-- Dark Dominion
+-- Created: May 13, 2026 at 14:14:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3237640) -- Dark Dominion
+-- MAIN APP DEPOTS
+addappid(3237641, 1, "01d1dd5e0582c599ded28e15f65de665c97f68b3a22583ce1783e92e3dd75831") -- Depot 3237641
+setManifestid(3237641, "2034671870623807366", 68809069437)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
