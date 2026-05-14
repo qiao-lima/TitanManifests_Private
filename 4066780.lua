70940c49e8c807317ca3f19124951935c6b4d993
qiao-lima <qiao-lima@github.com>
2026-05-14 13:19:20 +0800

    Manual Update 4066780

---
+++ 4066780.lua
@@ -0,0 +1,12 @@
+-- 4066780's Lua and Manifest Created by Hubcap Manifest
+-- Cool story bro!
+-- Created: May 13, 2026 at 11:15:37 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4066780) -- Cool story bro!
+-- MAIN APP DEPOTS
+addappid(4066781, 1, "79c4b46612b6f33ff8525c9fa483bbaf71c3b9204265bdee323ca636a659286c") -- Depot 4066781
+setManifestid(4066781, "8999269338349862978", 289907181)
