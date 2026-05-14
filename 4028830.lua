9b2a34334a5bc521fcf34219742ea19e1e4d6ca4
qiao-lima <qiao-lima@github.com>
2026-05-14 13:19:19 +0800

    Manual Update 4028830

---
+++ 4028830.lua
@@ -0,0 +1,14 @@
+-- 4028830's Lua and Manifest Created by Hubcap Manifest
+-- WonderLang Mandarin Chinese
+-- Created: May 12, 2026 at 19:38:59 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4028830) -- WonderLang Mandarin Chinese
+-- MAIN APP DEPOTS
+addappid(4028831, 1, "99bbd4a2c7b43fd64185795b56447c78a6b6d2856f21f885ef58efb66ddda9f0") -- Depot 4028831
+setManifestid(4028831, "7091950258029464865", 3075748974)
+addappid(4028832, 1, "3211a066dde8bd05e8fbabe755e5d52a61e620d5393df87320de7b6c7cd31cec") -- Depot 4028832
+setManifestid(4028832, "7365393701885138368", 3222299165)
