85fb0ed79572ab89f9f99f67e39f29ec67a23264
qiao-lima <qiao-lima@github.com>
2026-05-14 13:19:17 +0800

    Manual Update 2397300

---
+++ 2397300.lua
@@ -0,0 +1,23 @@
+-- 2397300's Lua and Manifest Created by Hubcap Manifest
+-- Half Sword
+-- Created: March 14, 2026 at 11:27:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2397300, 1, "524c672a6125d66225973b18d8f1b53631b38000295384082e5feca5ec3071c3") -- Half Sword
+-- MAIN APP DEPOTS
+addappid(2397301, 1, "0d49aa268c5fc78babcfe220c4828bd7bbc422ae09419958a82240f57b82e161") -- Depot 2397301
+setManifestid(2397301, "5855130673420038266", 10435984157)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Half Sword - Supporter Edition (AppID: 4355970)
+addappid(4355970)
+addappid(4355970, 1, "a1a3a30f2d8cabc41059d866af336784462840865d6bcbc5eb9962fcb794dc42") -- Half Sword - Supporter Edition - Depot 4355970
+setManifestid(4355970, "1233827490267229814", 1882614375)
