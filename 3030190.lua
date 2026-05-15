f54f3dd1f71dd8ea4e66337f70e050d23b9760b4
qiao-lima <qiao-lima@github.com>
2026-05-15 16:56:55 +0800

    Manual Update 3030190

---
+++ 3030190.lua
@@ -0,0 +1,12 @@
+-- 3030190's Lua and Manifest Created by Hubcap Manifest
+-- Pandora's Toybox
+-- Created: May 14, 2026 at 23:14:37 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3030190) -- Pandora's Toybox
+-- MAIN APP DEPOTS
+addappid(3030191, 1, "0f219be1ad031423647344a0a0b1246240eb85f2ef42937a5d60ed8df564cb7e") -- Depot 3030191
+setManifestid(3030191, "1122393779862154505", 475095950)
