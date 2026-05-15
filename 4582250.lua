a3ee82f81f0b4e4f539e7508a11bcf6b33ba4ce1
qiao-lima <qiao-lima@github.com>
2026-05-15 16:56:57 +0800

    Manual Update 4582250

---
+++ 4582250.lua
@@ -0,0 +1,12 @@
+-- 4582250's Lua and Manifest Created by Hubcap Manifest
+-- Socratic Smackdown
+-- Created: May 14, 2026 at 17:11:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4582250) -- Socratic Smackdown
+-- MAIN APP DEPOTS
+addappid(4582251, 1, "18930aeeb00b9e2f48de7383a689e4229cab2d6b34264e74a071792d4cecfda5") -- Depot 4582251
+setManifestid(4582251, "9125102518204985169", 1207998491)
