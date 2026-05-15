ddaa64f7513a9a1bb01c5e85cc468c95f76c118e
qiao-lima <qiao-lima@github.com>
2026-05-15 16:56:56 +0800

    Manual Update 3313720

---
+++ 3313720.lua
@@ -0,0 +1,14 @@
+-- 3313720's Lua and Manifest Created by Hubcap Manifest
+-- 今古群侠传
+-- Created: May 15, 2026 at 01:55:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3313720) -- 今古群侠传
+-- MAIN APP DEPOTS
+addappid(3313721, 1, "ee11efd6a3f4b69b34662632aa725443ff59d29d7c5f5b17f2c02414af66623b") -- Depot 3313721
+setManifestid(3313721, "1085728991134617046", 9732860485)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4599150) --  - 
