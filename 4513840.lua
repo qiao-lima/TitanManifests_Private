30dd1217d0a6a4c7867834ba3d0d6122f80c7142
qiao-lima <qiao-lima@github.com>
2026-05-15 16:56:57 +0800

    Manual Update 4513840

---
+++ 4513840.lua
@@ -0,0 +1,12 @@
+-- 4513840's Lua and Manifest Created by Hubcap Manifest
+-- Embers of the Gods
+-- Created: May 14, 2026 at 22:59:36 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4513840) -- Embers of the Gods
+-- MAIN APP DEPOTS
+addappid(4513841, 1, "4691048d1ee03451e6a013700eb7e18f102e28b64036111c016e39dadf1ebf1b") -- Depot 4513841
+setManifestid(4513841, "1727928696351865039", 123764587)
