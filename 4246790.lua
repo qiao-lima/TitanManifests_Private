a2941b69b8f257f98a13aab28845926bb159dfba
qiao-lima <qiao-lima@github.com>
2026-05-15 16:56:56 +0800

    Manual Update 4246790

---
+++ 4246790.lua
@@ -0,0 +1,12 @@
+-- 4246790's Lua and Manifest Created by Hubcap Manifest
+-- Glorgo's Microplastics Mine
+-- Created: May 14, 2026 at 16:16:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4246790) -- Glorgo's Microplastics Mine
+-- MAIN APP DEPOTS
+addappid(4246791, 1, "2890624d78c48082ea4c0810bd68393ff397f183f34be6d975cd9a02cf004e38") -- Depot 4246791
+setManifestid(4246791, "216727099754520602", 1156374639)
