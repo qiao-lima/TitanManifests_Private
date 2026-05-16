9319738f37398591296a2cba240e396d04c861c4
qiao-lima <qiao-lima@github.com>
2026-05-16 14:33:24 +0800

    Manual Update 4624790

---
+++ 4624790.lua
@@ -0,0 +1,12 @@
+-- 4624790's Lua and Manifest Created by Hubcap Manifest
+-- Lost Totem Trajipon!
+-- Created: May 16, 2026 at 01:46:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4624790) -- Lost Totem Trajipon!
+-- MAIN APP DEPOTS
+addappid(4624791, 1, "d9afaa292ee3b852c267de59c40946e923d509f1b8170f9581821990c95ea912") -- Depot 4624791
+setManifestid(4624791, "7594054395221348408", 141010784)
