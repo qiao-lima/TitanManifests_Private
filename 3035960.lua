223d737925825c3cf0c0b472e4346a6477dee0a9
qiao-lima <qiao-lima@github.com>
2026-05-16 14:33:21 +0800

    Manual Update 3035960

---
+++ 3035960.lua
@@ -0,0 +1,12 @@
+-- 3035960's Lua and Manifest Created by Hubcap Manifest
+-- Augment Anthem
+-- Created: May 16, 2026 at 01:46:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3035960) -- Augment Anthem
+-- MAIN APP DEPOTS
+addappid(3035969, 1, "fa7f135eb24d884dbad747d4715cba056b2dab308805b8523b6d5d985e5b8939") -- Depot 3035969
+setManifestid(3035969, "1225323268410681695", 1080678369)
