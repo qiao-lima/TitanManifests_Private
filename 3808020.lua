ec72b3daa5f1dfac5d41b70ebac45314e76d4bb0
qiao-lima <qiao-lima@github.com>
2026-05-16 14:33:21 +0800

    Manual Update 3808020

---
+++ 3808020.lua
@@ -0,0 +1,12 @@
+-- 3808020's Lua and Manifest Created by Hubcap Manifest
+-- Oceanopolis 2000
+-- Created: May 16, 2026 at 01:35:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3808020) -- Oceanopolis 2000
+-- MAIN APP DEPOTS
+addappid(3808021, 1, "4bbeb76262ba8232daf3fb0ddb3d596ed19d530516e832c09f8872af0286c9f4") -- Depot 3808021
+setManifestid(3808021, "7176591810171099554", 178393131)
