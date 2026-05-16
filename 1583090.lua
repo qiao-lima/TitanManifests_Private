6ac4e82e98b60c364d12f92ed9a8824c3fc35c46
qiao-lima <qiao-lima@github.com>
2026-05-16 14:33:20 +0800

    Manual Update 1583090

---
+++ 1583090.lua
@@ -0,0 +1,18 @@
+-- 1583090's Lua and Manifest Created by Hubcap Manifest
+-- GIRL.EXE
+-- Created: May 16, 2026 at 02:16:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1583090, 1, "02ddee936c374482c7a3f44d585f8d14ca82aebd5b52d3cb7598deb5b1b2f69e") -- GIRL.EXE
+-- MAIN APP DEPOTS
+addappid(1583091, 1, "4f4a833a14aaf8b6d82fb7ade99b2e09c227ea13e66ce55a88c3b01d96a46646") -- Depot 1583091
+setManifestid(1583091, "8768788117523885421", 1860610183)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
