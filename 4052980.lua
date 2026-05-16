dc3d0748a3d037e11a20e3012dae41c7c15de094
qiao-lima <qiao-lima@github.com>
2026-05-16 14:33:22 +0800

    Manual Update 4052980

---
+++ 4052980.lua
@@ -0,0 +1,12 @@
+-- 4052980's Lua and Manifest Created by Hubcap Manifest
+-- Aion - Part 1 : Return to Palace
+-- Created: May 16, 2026 at 01:34:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4052980) -- Aion - Part 1 : Return to Palace
+-- MAIN APP DEPOTS
+addappid(4052981, 1, "210c9ffc15c50521a18f4b53c3155b9ae8db01445fadbe2f761f83e175050612") -- Depot 4052981
+setManifestid(4052981, "968957836343801653", 5753865582)
