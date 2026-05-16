b82d70120285fefee829fdc7a5ff2260a1d2cca2
qiao-lima <qiao-lima@github.com>
2026-05-16 14:33:21 +0800

    Manual Update 3418990

---
+++ 3418990.lua
@@ -0,0 +1,12 @@
+-- 3418990's Lua and Manifest Created by Hubcap Manifest
+-- 神陨之地 Shattered Divinities
+-- Created: May 16, 2026 at 00:26:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3418990) -- 神陨之地 Shattered Divinities
+-- MAIN APP DEPOTS
+addappid(3418991, 1, "42df9079dedc1fc0173d9f6326ec3e832908c5f6398d1d4d106f5a8db311c4bb") -- Depot 3418991
+setManifestid(3418991, "1098444456369856896", 2820069069)
