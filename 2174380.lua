3b158c24ad75aa9dc0ef9e29e049d521f9818928
qiao-lima <qiao-lima@github.com>
2026-05-16 14:33:20 +0800

    Manual Update 2174380

---
+++ 2174380.lua
@@ -0,0 +1,23 @@
+-- 2174380's Lua and Manifest Created by Hubcap Manifest
+-- Call of the Elder Gods
+-- Created: May 14, 2026 at 08:19:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2174380) -- Call of the Elder Gods
+-- MAIN APP DEPOTS
+addappid(2174381, 1, "b23891968a4d2f805a9d01cb4c6f8ccd8f498fdd22b105ff97a529c07d159c0a") -- Depot 2174381
+setManifestid(2174381, "3914530727829671346", 22888985042)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- The Art of Call of the Elder Gods (AppID: 4656660)
+addappid(4656660)
+addappid(4656660, 1, "f7f48f32a10051f7745bbe1799249468dd70d66ea6ba5630104133e017528dbd") -- The Art of Call of the Elder Gods - Depot 4656660
+setManifestid(4656660, "4341944777700775357", 203449211)
