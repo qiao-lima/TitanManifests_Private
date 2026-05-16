46a14b6ce7a04257d59678c1bc406c9292437265
qiao-lima <qiao-lima@github.com>
2026-05-16 14:33:20 +0800

    Manual Update 1976540

---
+++ 1976540.lua
@@ -0,0 +1,12 @@
+-- 1976540's Lua and Manifest Created by Hubcap Manifest
+-- 蚀界档案
+-- Created: May 16, 2026 at 02:16:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1976540, 1, "391a66804ce0267351aabb89bb562142e8675aa87c9dd07af3014c2a354e823c") -- 蚀界档案
+-- MAIN APP DEPOTS
+addappid(1976541, 1, "5c1b58ef16b26784ace51da44b978cdb87cad0d76cc89db583547947781f3dbd") -- Depot 1976541
+setManifestid(1976541, "1386905049426533106", 1000075211)
