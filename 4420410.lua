0c3a7a1169cf3734c5845fea0982741bf291d954
qiao-lima <qiao-lima@github.com>
2026-05-16 14:33:23 +0800

    Manual Update 4420410

---
+++ 4420410.lua
@@ -0,0 +1,14 @@
+-- 4420410's Lua and Manifest Created by Hubcap Manifest
+-- Tavern Timer
+-- Created: May 16, 2026 at 01:32:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4420410) -- Tavern Timer
+-- MAIN APP DEPOTS
+addappid(4420411, 1, "605de2ee0e04b8093b1482881d26a9f63ae898619b7682d50f185a4194ccdf23") -- Depot 4420411
+setManifestid(4420411, "3964528038626616350", 199838192)
+addappid(4420412, 1, "2cfd858712354485609b3a1b4382c3d9f77f80583f1dcd13677b89d1c00cc520") -- Depot 4420412
+setManifestid(4420412, "2008806748166931870", 280678815)
