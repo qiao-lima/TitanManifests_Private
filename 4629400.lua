eb48c00e2e623755a84324399cbfef2b42c3f401
qiao-lima <qiao-lima@github.com>
2026-05-16 14:33:24 +0800

    Manual Update 4629400

---
+++ 4629400.lua
@@ -0,0 +1,12 @@
+-- 4629400's Lua and Manifest Created by Hubcap Manifest
+-- Somewhere Café
+-- Created: May 15, 2026 at 11:01:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4629400) -- Somewhere Café
+-- MAIN APP DEPOTS
+addappid(4629401, 1, "842e496aaa0b438c2d81df55d5269814dbb7e65466f7f34a8e0d9c00eb899a08") -- Depot 4629401
+setManifestid(4629401, "3340649904371856826", 1365910851)
