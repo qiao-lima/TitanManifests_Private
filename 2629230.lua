7d4a71ad4878fb4e5b87ad2739cc0fc43e8d07ae
qiao-lima <qiao-lima@github.com>
2026-05-21 16:50:34 +0800

    Manual Update 2629230

---
+++ 2629230.lua
@@ -0,0 +1,14 @@
+-- 2629230's Lua and Manifest Created by Hubcap Manifest
+-- The Adventures of Sir Kicksalot
+-- Created: May 16, 2026 at 16:48:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2629230, 1, "bcad38d41b88872ac31f3c00b622af223e8aab3c99036b18e66360dfd5f38da2") -- The Adventures of Sir Kicksalot
+-- MAIN APP DEPOTS
+addappid(2629231, 1, "531e9fb4aec7de2c570cd5deacfcf0dfbe6c43116d4809c97a1ffe76d9a369b5") -- Depot 2629231
+setManifestid(2629231, "3392800939517855636", 257188520)
+addappid(2629232, 1, "f40df32d47524c0a6f41f79e9d2278d1760a93429edef3ea1044fe30493b8ad1") -- Depot 2629232
+setManifestid(2629232, "1366215864324029914", 290002272)
