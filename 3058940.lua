32eb9b579649f49776dd8298464a9e752b55a093
qiao-lima <qiao-lima@github.com>
2026-05-21 16:50:35 +0800

    Manual Update 3058940

---
+++ 3058940.lua
@@ -0,0 +1,12 @@
+-- 3058940's Lua and Manifest Created by Hubcap Manifest
+-- Backspace Backspace Backspace
+-- Created: May 20, 2026 at 08:31:25 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3058940) -- Backspace Backspace Backspace
+-- MAIN APP DEPOTS
+addappid(3058941, 1, "d3ee1d43b5f4313c562b20cf83edd78103ca1d84940ca7336688d8e29856e4d1") -- Depot 3058941
+setManifestid(3058941, "7466397309656546364", 298677205)
