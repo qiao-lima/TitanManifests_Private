955514ee603b98747fa9c7884eb8f91a6f0d5ecc
qiao-lima <qiao-lima@github.com>
2026-05-21 16:50:36 +0800

    Manual Update 3588140

---
+++ 3588140.lua
@@ -0,0 +1,12 @@
+-- 3588140's Lua and Manifest Created by Hubcap Manifest
+-- Tune in to the show Part 2
+-- Created: May 20, 2026 at 06:52:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3588140, 1, "600405961c898d883d575ed40907bcc3c4db6012f2de1f7454cebfb56af32c24") -- Tune in to the show Part 2
+-- MAIN APP DEPOTS
+addappid(3588141, 1, "20d240da27fc65fe351f011addacbfc863d332c2151f25eefe50888b7261a118") -- Depot 3588141
+setManifestid(3588141, "5753850332178977809", 2560939185)
