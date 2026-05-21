201cbc8f6316b8d828e2b06354a108a723d73de7
qiao-lima <qiao-lima@github.com>
2026-05-21 16:50:36 +0800

    Manual Update 3542380

---
+++ 3542380.lua
@@ -0,0 +1,12 @@
+-- 3542380's Lua and Manifest Created by Hubcap Manifest
+-- Rune Dice
+-- Created: May 19, 2026 at 11:03:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3542380) -- Rune Dice
+-- MAIN APP DEPOTS
+addappid(3542381, 1, "31d465c765aa06e4d5586a3829ca8090d4980c2fb0edfea602d8fc38b2059378") -- Depot 3542381
+setManifestid(3542381, "35290934045735688", 531399340)
