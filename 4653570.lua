31f2bbf9f2a5d0531d46046a5d713cfc0a416462
qiao-lima <qiao-lima@github.com>
2026-05-21 16:52:50 +0800

    Manual Update 4653570

---
+++ 4653570.lua
@@ -0,0 +1,16 @@
+-- 4653570's Lua and Manifest Created by Hubcap Manifest
+-- Lighthouse Duty With Tsundere
+-- Created: May 18, 2026 at 14:13:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4653570, 1, "dcbf8dcb89fe2bf5f6cc090bed45806c120949685b9c586bed9cb7f34ed25e8d") -- Lighthouse Duty With Tsundere
+-- MAIN APP DEPOTS
+addappid(4653571, 1, "c1c14e4f367a6245f9eb1fae3252980c9e5ccebdb1823faae1d59c991242567d") -- Depot 4653571
+setManifestid(4653571, "1577214065377075016", 604691851)
+addappid(4653572, 1, "04a99632274a105f542b19ea68b62fc4d3ac4f986bbfe6aa433e12febd9bf039") -- Depot 4653572
+setManifestid(4653572, "3541063571509996786", 590531440)
+addappid(4653573, 1, "c588d0610765fa02efb27708ff6f0e6f1a41b2c52235114fdffb0c40d7ab383e") -- Depot 4653573
+setManifestid(4653573, "7920473525382636676", 556264661)
