584968dc8b25b20c533344f3f94f6c6944f75605
qiao-lima <qiao-lima@github.com>
2026-05-21 16:50:34 +0800

    Manual Update 2661470

---
+++ 2661470.lua
@@ -0,0 +1,16 @@
+-- 2661470's Lua and Manifest Created by Hubcap Manifest
+-- Banshee Hunt
+-- Created: May 20, 2026 at 09:01:30 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(2661470) -- Banshee Hunt
+-- MAIN APP DEPOTS
+addappid(2661471, 1, "83b9beebd5b12abdcd20a30bfc167bc83a8711fb82f861304f9cea855ba4d579") -- Depot 2661471
+setManifestid(2661471, "3447222494833676664", 6035373721)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Banshee Hunt Artbook (AppID: 2890210) - missing depot keys
+-- addappid(2890210)
