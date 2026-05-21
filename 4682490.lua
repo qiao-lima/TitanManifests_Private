60a1e6510355263114346b9a1376a4c188f0ce25
qiao-lima <qiao-lima@github.com>
2026-05-21 16:52:50 +0800

    Manual Update 4682490

---
+++ 4682490.lua
@@ -0,0 +1,12 @@
+-- 4682490's Lua and Manifest Created by Hubcap Manifest
+-- 三生忆梦(Dream Of Three Lives)
+-- Created: May 20, 2026 at 06:13:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4682490) -- 三生忆梦(Dream Of Three Lives)
+-- MAIN APP DEPOTS
+addappid(4682491, 1, "48d7133eed366e5d791bc68c250ae27a3b476130f1fd1c276b20398fa0b75799") -- Depot 4682491
+setManifestid(4682491, "911211017546094977", 445942708)
