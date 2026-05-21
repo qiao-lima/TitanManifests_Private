a3575903ac1554ca8dbd9677a8d4a9e84dfb3889
qiao-lima <qiao-lima@github.com>
2026-05-21 16:50:37 +0800

    Manual Update 3815890

---
+++ 3815890.lua
@@ -0,0 +1,12 @@
+-- 3815890's Lua and Manifest Created by Hubcap Manifest
+-- Lucky Dog 1
+-- Created: May 20, 2026 at 12:37:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3815890) -- Lucky Dog 1
+-- MAIN APP DEPOTS
+addappid(3815891, 1, "075d9220f77fca47f97cfd3eb12dd7172fdcf59eb6acc1eb1c3f068fdf0163df") -- Depot 3815891
+setManifestid(3815891, "5424794648641837391", 1948895966)
