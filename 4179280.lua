c5d49698719abf3caa663dd10025a1e736f3b8b5
qiao-lima <qiao-lima@github.com>
2026-05-21 16:52:48 +0800

    Manual Update 4179280

---
+++ 4179280.lua
@@ -0,0 +1,12 @@
+-- 4179280's Lua and Manifest Created by Hubcap Manifest
+-- Moonshiner Simulator
+-- Created: May 20, 2026 at 12:17:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4179280) -- Moonshiner Simulator
+-- MAIN APP DEPOTS
+addappid(4179281, 1, "0127d3d8ace2962fd669945db02094f4b790fb3c26b5c0e37345163de2e9e986") -- Depot 4179281
+setManifestid(4179281, "6945596965384348052", 8312147548)
