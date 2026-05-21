9a91f15c29bf07a0321ac522d657b93a37ccf7b1
qiao-lima <qiao-lima@github.com>
2026-05-21 16:50:33 +0800

    Manual Update 2109620

---
+++ 2109620.lua
@@ -0,0 +1,12 @@
+-- 2109620's Lua and Manifest Created by Hubcap Manifest
+-- Aery - Path of Corruption
+-- Created: May 20, 2026 at 12:36:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2109620) -- Aery - Path of Corruption
+-- MAIN APP DEPOTS
+addappid(2109621, 1, "f5b6690de71b55d6d7713930dad633b821f7ba4975846a1d1093b631bc92462a") -- Depot 2109621
+setManifestid(2109621, "7623274102724577787", 3680418003)
