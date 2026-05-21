e358fadf497433fabf5ab9450e957e21cabcba33
qiao-lima <qiao-lima@github.com>
2026-05-21 16:50:35 +0800

    Manual Update 2712480

---
+++ 2712480.lua
@@ -0,0 +1,12 @@
+-- 2712480's Lua and Manifest Created by Hubcap Manifest
+-- Shikhondo: Youkai Rampage
+-- Created: May 19, 2026 at 09:37:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2712480, 1, "9b1b197805554c09da18e58abfc97054d44dd10dbc680314df7a5544481538b4") -- Shikhondo: Youkai Rampage
+-- MAIN APP DEPOTS
+addappid(2712481, 1, "0bb88661bf186ef3d84a9b0cb92194f8a2e95febe5499e0b6349d33abb1c4c86") -- Depot 2712481
+setManifestid(2712481, "2766682844161321634", 831718721)
