a7ef737ef10f79facefade728b133414330c35b9
qiao-lima <qiao-lima@github.com>
2026-05-21 16:50:38 +0800

    Manual Update 4131900

---
+++ 4131900.lua
@@ -0,0 +1,16 @@
+-- 4131900's Lua and Manifest Created by Hubcap Manifest
+-- Heartbeat House: Hong Kong
+-- Created: May 16, 2026 at 14:32:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4131900, 1, "a5297e356a6b48221cef5854c347411b2ebcfea8d1c11e83f5873f6dbb435d65") -- Heartbeat House: Hong Kong
+-- MAIN APP DEPOTS
+addappid(4131901, 1, "7c2ed9aabc4095356b366dbc7000c1aebcf28553b47d3cb206d919bfb9da8a37") -- Depot 4131901
+setManifestid(4131901, "6620569656676633353", 6770546589)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4366820) -- Heartbeat House Hong Kong - Reunion
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4131902) -- Depot 4131902 (empty depot)
