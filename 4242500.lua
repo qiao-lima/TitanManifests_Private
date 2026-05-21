8512f146c216fa233b5cd504d76ca128aa7c5548
qiao-lima <qiao-lima@github.com>
2026-05-21 16:52:49 +0800

    Manual Update 4242500

---
+++ 4242500.lua
@@ -0,0 +1,12 @@
+-- 4242500's Lua and Manifest Created by Hubcap Manifest
+-- カタネガイ
+-- Created: May 18, 2026 at 06:51:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4242500) -- カタネガイ
+-- MAIN APP DEPOTS
+addappid(4242501, 1, "36b39bb6bd0136ae144dd875b9a371965fa7a31161c346659114672e14b7451b") -- Depot 4242501
+setManifestid(4242501, "2170734821549358957", 736887973)
