faf259bcec607c83b3f48e2770acd37e367b01f6
qiao-lima <qiao-lima@github.com>
2026-05-21 16:50:34 +0800

    Manual Update 2513650

---
+++ 2513650.lua
@@ -0,0 +1,13 @@
+-- 2513650's Lua and Manifest Created by Hubcap Manifest
+-- Choppy Heady
+-- Created: May 20, 2026 at 06:13:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2513650) -- Choppy Heady
+addtoken(2513650, "8636076695590724082")
+-- MAIN APP DEPOTS
+addappid(2513651, 1, "8636533c43eb322215853eead5bd946964f3b74a4a6f8808589677b878b5f385") -- Depot 2513651
+setManifestid(2513651, "5032919448459469876", 33216867)
