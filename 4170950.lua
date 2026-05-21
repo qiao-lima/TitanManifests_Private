57f9a709aa3fa8c144fbce3561f3f98cd5d986a6
qiao-lima <qiao-lima@github.com>
2026-05-21 16:52:48 +0800

    Manual Update 4170950

---
+++ 4170950.lua
@@ -0,0 +1,12 @@
+-- 4170950's Lua and Manifest Created by Hubcap Manifest
+-- Hentai Secretary (Desktop Mascot)
+-- Created: May 19, 2026 at 09:14:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4170950) -- Hentai Secretary (Desktop Mascot)
+-- MAIN APP DEPOTS
+addappid(4170951, 1, "6bd698d563c5d57251d7fe660a66610dd6f0300005dad85f9ca72f4970b8aa89") -- Depot 4170951
+setManifestid(4170951, "8080269432454468819", 244773910)
