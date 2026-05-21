6367c8e293ec674e0058b95de9368d7a615f1ae9
qiao-lima <qiao-lima@github.com>
2026-05-21 16:50:33 +0800

    Manual Update 1202580

---
+++ 1202580.lua
@@ -0,0 +1,12 @@
+-- 1202580's Lua and Manifest Created by Hubcap Manifest
+-- Beyond Eden: Dear Edward
+-- Created: May 20, 2026 at 12:36:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1202580) -- Beyond Eden: Dear Edward
+-- MAIN APP DEPOTS
+addappid(1202581, 1, "8fe9a56a4207b2bb5538d30500d3f940d8f22a04efc60da065ddb720d69255c9") -- Beyond Eden: Dear Edward Content
+setManifestid(1202581, "4665554695360424120", 1088212987)
