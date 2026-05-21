9b2683561ec8f041d76cec0bf9c07d37b4725c58
qiao-lima <qiao-lima@github.com>
2026-05-21 16:52:49 +0800

    Manual Update 4268430

---
+++ 4268430.lua
@@ -0,0 +1,12 @@
+-- 4268430's Lua and Manifest Created by Hubcap Manifest
+-- Midnight Kebab
+-- Created: May 17, 2026 at 23:31:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4268430) -- Midnight Kebab
+-- MAIN APP DEPOTS
+addappid(4268431, 1, "fe2214adf843fd1241f21b0fdd28bbf373bf9fa023379549e65396837417e6f9") -- Depot 4268431
+setManifestid(4268431, "9154779741947952916", 586715068)
