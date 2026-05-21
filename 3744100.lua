55888a8e41caa0b9f061675ae8aadb4260b59eaf
qiao-lima <qiao-lima@github.com>
2026-05-21 16:50:37 +0800

    Manual Update 3744100

---
+++ 3744100.lua
@@ -0,0 +1,12 @@
+-- 3744100's Lua and Manifest Created by Hubcap Manifest
+-- It’s Quiz Time: GUINNESS WORLD RECORDS™ 2026 Edition
+-- Created: May 20, 2026 at 13:29:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3744100) -- It’s Quiz Time: GUINNESS WORLD RECORDS™ 2026 Edition
+-- MAIN APP DEPOTS
+addappid(3744101, 1, "fc403b73d44dd7ea4febea80168b71bb6a07e393f50369f37cb6d4cf9dad04db") -- Depot 3744101
+setManifestid(3744101, "2397400219902432209", 12173023124)
