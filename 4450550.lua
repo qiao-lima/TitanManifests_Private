50202886f71a410398d895a8a376bf4b7c979d5c
qiao-lima <qiao-lima@github.com>
2026-05-21 16:52:49 +0800

    Manual Update 4450550

---
+++ 4450550.lua
@@ -0,0 +1,17 @@
+-- 4450550's Lua and Manifest Created by Hubcap Manifest
+-- Femboy Next Door 🔞🚪
+-- Created: May 19, 2026 at 14:11:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4450550) -- Femboy Next Door 🔞🚪
+-- MAIN APP DEPOTS
+addappid(4450551, 1, "de60cdd9d70fb1ff1656de0967a08902acb5a0c905b494f4235d44fe1ff62124") -- Depot 4450551
+setManifestid(4450551, "2914560209923537797", 3721105583)
+-- DLCS WITH DEDICATED DEPOTS
+-- Femboy Next Door  - Wallpapers 18 (AppID: 4734660)
+addappid(4734660)
+addappid(4734660, 1, "ea92c72a16891b68884b51ad5fd7ef23063318c2fe8c8a0d9238902d363e3e1f") -- Femboy Next Door  - Wallpapers 18 - Depot 4734660
+setManifestid(4734660, "6042184828095310531", 230840595)
