a559daa741b82d1708a92f6b3fe91662e96c4435
qiao-lima <qiao-lima@github.com>
2026-05-21 16:52:49 +0800

    Manual Update 4330860

---
+++ 4330860.lua
@@ -0,0 +1,14 @@
+-- 4330860's Lua and Manifest Created by Hubcap Manifest
+-- Desktop Nard: Dice Idle
+-- Created: May 20, 2026 at 21:22:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4330860) -- Desktop Nard: Dice Idle
+-- MAIN APP DEPOTS
+addappid(4330861, 1, "023424af9af043f2d861b584616a17af03b6999e3313c6133d65b5d1b14231fd") -- Depot 4330861
+setManifestid(4330861, "5879124638497022974", 413010387)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4725160) -- Desktop Nard - Supporter Pack
