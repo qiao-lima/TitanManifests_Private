cb0299f4f3599fd06333685b6e982b3cbacf4967
qiao-lima <qiao-lima@github.com>
2026-05-21 16:50:37 +0800

    Manual Update 3934290

---
+++ 3934290.lua
@@ -0,0 +1,12 @@
+-- 3934290's Lua and Manifest Created by Hubcap Manifest
+-- Cafe of Roses
+-- Created: May 21, 2026 at 00:11:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3934290) -- Cafe of Roses
+-- MAIN APP DEPOTS
+addappid(3934291, 1, "ba5d3b29380fee1234056e9746379b605ba6ca7e9b09f0fb94f09b3c94e3d4e0") -- Depot 3934291
+setManifestid(3934291, "3369729479890931545", 812391782)
