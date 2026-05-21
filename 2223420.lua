299259ecb31cf741a4969af7f83598c228ad7f2b
qiao-lima <qiao-lima@github.com>
2026-05-21 16:50:34 +0800

    Manual Update 2223420

---
+++ 2223420.lua
@@ -0,0 +1,16 @@
+-- 2223420's Lua and Manifest Created by Hubcap Manifest
+-- 紡織者之詠Voice of Belldona
+-- Created: May 20, 2026 at 05:50:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(2223420) -- 紡織者之詠Voice of Belldona
+-- MAIN APP DEPOTS
+addappid(2223421, 1, "a5bb6f6d51212c9e49725443b65d908237b14371852b9080f4f576d7aa03f9b6") -- Depot 2223421
+setManifestid(2223421, "3149197725651175442", 8248789950)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3399800) -- clothes pass
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3399800) -- Depot 3399800 (empty depot)
