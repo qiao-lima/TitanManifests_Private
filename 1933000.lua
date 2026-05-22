2b6dc951437e701633eec4c6312141ad86c2dd7e
qiao-lima <qiao-lima@github.com>
2026-05-22 22:32:08 +0800

    Manual Update 1933000

---
+++ 1933000.lua
@@ -0,0 +1,18 @@
+-- 1933000's Lua and Manifest Created by Hubcap Manifest
+-- Luna Abyss
+-- Created: May 21, 2026 at 11:00:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1933000) -- Luna Abyss
+-- MAIN APP DEPOTS
+addappid(1933001, 1, "053e48598d2860c9d84005844ad2ba7158e805efa30803798f2d4d9c9b6bacc6") -- Depot 1933001
+setManifestid(1933001, "3660680832711043914", 9624518857)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
