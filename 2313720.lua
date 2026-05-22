554bfc31fa76fd6332147775280b3f44a37d0eb7
qiao-lima <qiao-lima@github.com>
2026-05-22 22:32:08 +0800

    Manual Update 2313720

---
+++ 2313720.lua
@@ -0,0 +1,12 @@
+-- 2313720's Lua and Manifest Created by Hubcap Manifest
+-- Noun Town Language Learning
+-- Created: September 30, 2025 at 04:07:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2313720) -- Noun Town Language Learning
+-- MAIN APP DEPOTS
+addappid(2313721, 1, "28deea2a3c397cb3cb4bab57efcd08e787e65a139b8c96bb5c725f39ee4088cd") -- Depot 2313721
+setManifestid(2313721, "5293538532083130922", 3132150221)
