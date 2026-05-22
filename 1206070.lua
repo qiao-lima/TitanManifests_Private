89c593b60c050a1504b557475234ed74da8ceed0
qiao-lima <qiao-lima@github.com>
2026-05-22 22:32:08 +0800

    Manual Update 1206070

---
+++ 1206070.lua
@@ -0,0 +1,14 @@
+-- 1206070's Lua and Manifest Created by Hubcap Manifest
+-- Phonopolis
+-- Created: May 22, 2026 at 05:14:19 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1206070) -- Phonopolis
+-- MAIN APP DEPOTS
+addappid(1206071, 1, "327734e8dd1eb7d1a30d703deabf8df35d25496893f6e462d73e10a843fc9a36") -- Depot 1206071
+setManifestid(1206071, "4877906844921946028", 11530761046)
+addappid(1206072, 1, "b4579c1fb08d5abc650941d951d35b3bc1ea1cb2188794bc718e8148e9d46f59") -- Depot 1206072
+setManifestid(1206072, "2299970344274948016", 11684547876)
