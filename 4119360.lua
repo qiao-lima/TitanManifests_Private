b8ac45a3994b162e1536785cad9570412ba5c76a
qiao-lima <qiao-lima@github.com>
2026-05-22 22:32:11 +0800

    Manual Update 4119360

---
+++ 4119360.lua
@@ -0,0 +1,25 @@
+-- 4119360's Lua and Manifest Created by Hubcap Manifest
+-- It Reaches
+-- Created: May 22, 2026 at 00:36:29 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 2
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4119360) -- It Reaches
+-- MAIN APP DEPOTS
+addappid(4119361, 1, "490a592856bd970d1535644b5690e69015f1a260a0f00696b4e4260e6f960f7e") -- Depot 4119361
+setManifestid(4119361, "3707258888869251898", 5104913904)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- It Reaches - HQ Wallpaper Pack (AppID: 4705840)
+addappid(4705840)
+addappid(4705840, 1, "d5387c83dab31b0c662d5cea8dde42ff36f44eab4226a67231c021d09658d1dd") -- It Reaches - HQ Wallpaper Pack - Depot 4705840
+setManifestid(4705840, "9201572106280009671", 64634417)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4705830) -- It Reaches - Supporter Pack
