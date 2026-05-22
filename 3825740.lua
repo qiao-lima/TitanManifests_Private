fbdcde6959302373b3174f6eb32e31cb4f8f0303
qiao-lima <qiao-lima@github.com>
2026-05-22 22:32:11 +0800

    Manual Update 3825740

---
+++ 3825740.lua
@@ -0,0 +1,12 @@
+-- 3825740's Lua and Manifest Created by Hubcap Manifest
+-- 2 Bows 1 Arrow
+-- Created: May 22, 2026 at 05:48:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3825740, 1, "aa1edc794d0c2d075debdd613ea75af8ca93a67a01d2f36e599e794c70e9ffd5") -- 2 Bows 1 Arrow
+-- MAIN APP DEPOTS
+addappid(3825741, 1, "3c124b50eb85c4de302964d6b46ac0635c658407fc8d64b58c49ea6f04de17e3") -- Depot 3825741
+setManifestid(3825741, "8775658140741796706", 449741752)
