7cb4cf151a08712f79a71400e2f288d1c7f0252f
qiao-lima <qiao-lima@github.com>
2026-05-22 22:32:09 +0800

    Manual Update 2506530

---
+++ 2506530.lua
@@ -0,0 +1,12 @@
+-- 2506530's Lua and Manifest Created by Hubcap Manifest
+-- It Has My Face
+-- Created: April 22, 2026 at 10:18:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2506530) -- It Has My Face
+-- MAIN APP DEPOTS
+addappid(2506531, 1, "80c4294f187f8851e0f632effc5b8621fd291ecc6d016f57bb6c749c2742862e") -- Depot 2506531
+setManifestid(2506531, "5982722511482207117", 687470204)
