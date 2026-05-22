86b455f5c3ba8fb048474622ccf21bc55258f9ae
qiao-lima <qiao-lima@github.com>
2026-05-22 22:32:10 +0800

    Manual Update 3412160

---
+++ 3412160.lua
@@ -0,0 +1,23 @@
+-- 3412160's Lua and Manifest Created by Hubcap Manifest
+-- The House Always Wins
+-- Created: May 22, 2026 at 09:53:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3412160) -- The House Always Wins
+-- MAIN APP DEPOTS
+addappid(3412161, 1, "e13415101c7f16966fbeb09c1666b9b664b1f8587de1b79e5ff93f37a85f1b00") -- Depot 3412161
+setManifestid(3412161, "3076886698716877777", 6184444791)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- The House Always Wins - Goose Supporter Pack (AppID: 4579370)
+addappid(4579370)
+addappid(4579370, 1, "85efbaa380949c2b7ab1edfde70b05ca834e47fe581784d03b33f6f2582326ac") -- The House Always Wins - Goose Supporter Pack - Depot 4579370
+setManifestid(4579370, "2135984907072273245", 32351876)
