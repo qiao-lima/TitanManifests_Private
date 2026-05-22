9db8971076c57e07aafcd1b3dfc7c029c8ec4d28
qiao-lima <qiao-lima@github.com>
2026-05-22 22:32:10 +0800

    Manual Update 3161220

---
+++ 3161220.lua
@@ -0,0 +1,18 @@
+-- 3161220's Lua and Manifest Created by Hubcap Manifest
+-- Coffee Talk Tokyo
+-- Created: May 22, 2026 at 05:07:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3161220) -- Coffee Talk Tokyo
+-- MAIN APP DEPOTS
+addappid(3161221, 1, "e46e303ff9ade44112958b4a29fd9bce1e30e2f17009de4d24aa48fa6f429cd0") -- Depot 3161221
+setManifestid(3161221, "6120189935873029320", 895751148)
+addappid(3161222, 1, "e8d9ea752f01a8bc4b40ce2d0777806c67a9072269965a8dbb2a49cb934d3ca9") -- Depot 3161222
+setManifestid(3161222, "8662843691166860776", 531923612)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4676450) -- Coffee Talk Tokyo Deluxe Edition Upgrade
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4676451) -- Depot 4676451 (empty depot)
