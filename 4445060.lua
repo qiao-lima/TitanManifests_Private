bc994733d685948c4826f4b28826f024a4e2a6d2
qiao-lima <qiao-lima@github.com>
2026-05-22 22:32:12 +0800

    Manual Update 4445060

---
+++ 4445060.lua
@@ -0,0 +1,12 @@
+-- 4445060's Lua and Manifest Created by Hubcap Manifest
+-- Shikhondo: Blue Pieta
+-- Created: May 22, 2026 at 10:24:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4445060) -- Shikhondo: Blue Pieta
+-- MAIN APP DEPOTS
+addappid(4445061, 1, "431caa799458eaad113a2dfcdb6deaf38f8bcd02a139f56a631183de2f0a7813") -- Depot 4445061
+setManifestid(4445061, "2988577134011320512", 1629898654)
