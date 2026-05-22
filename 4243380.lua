b22929ab1116ec8809293df98bdae38693b77910
qiao-lima <qiao-lima@github.com>
2026-05-22 22:32:11 +0800

    Manual Update 4243380

---
+++ 4243380.lua
@@ -0,0 +1,12 @@
+-- 4243380's Lua and Manifest Created by Hubcap Manifest
+-- Idle Quest Simulator
+-- Created: May 21, 2026 at 09:57:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4243380) -- Idle Quest Simulator
+-- MAIN APP DEPOTS
+addappid(4243381, 1, "880fa7564041ba0228922128b30e3846d6f2e92088de162a8eacc8afb4693405") -- Depot 4243381
+setManifestid(4243381, "3090977873061628879", 613181096)
