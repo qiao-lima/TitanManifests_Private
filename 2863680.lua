fa69b7f9d562aa24dc6d420bfb5cd82dc0aab54a
qiao-lima <qiao-lima@github.com>
2026-05-22 22:32:10 +0800

    Manual Update 2863680

---
+++ 2863680.lua
@@ -0,0 +1,17 @@
+-- 2863680's Lua and Manifest Created by Hubcap Manifest
+-- ZERO PARADES: For Dead Spies
+-- Created: May 21, 2026 at 06:03:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(2863680) -- ZERO PARADES: For Dead Spies
+-- MAIN APP DEPOTS
+addappid(2863681, 1, "6f8570b7626034b90d4244ec942a2e30b85f4a6c139c76622a364117e5e49be4") -- Depot 2863681
+setManifestid(2863681, "1416711104331163916", 10521977303)
+-- DLCS WITH DEDICATED DEPOTS
+-- ZERO PARADES For Dead Spies (Art Booklet) (AppID: 4719150)
+addappid(4719150)
+addappid(4719150, 1, "9ede90e3a696df35e824df54f8a882256941165d2b283859e3a422c7dccf75bb") -- ZERO PARADES For Dead Spies (Art Booklet) - Depot 4719150
+setManifestid(4719150, "6005577581934271192", 59547847)
