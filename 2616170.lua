be9a72a0e116463cd5c72a3279377c9bb3098dd7
qiao-lima <qiao-lima@github.com>
2026-05-22 22:32:10 +0800

    Manual Update 2616170

---
+++ 2616170.lua
@@ -0,0 +1,12 @@
+-- 2616170's Lua and Manifest Created by Hubcap Manifest
+-- Nebula Flow
+-- Created: May 22, 2026 at 05:48:19 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2616170, 1, "31f149fde9461a1622b35309b09bb6fcdae98bfbdfa1e72a6a577d6cfb90a5d0") -- Nebula Flow
+-- MAIN APP DEPOTS
+addappid(2616171, 1, "a1274538cd325a0ad155e4bae379cdfe7790d592d17ed838ae3f194221ffd217") -- Depot 2616171
+setManifestid(2616171, "8521810701062589793", 192910008)
