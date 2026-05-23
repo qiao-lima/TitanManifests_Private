88015f6769da7785364611c50a02ab002a306136
qiao-lima <qiao-lima@github.com>
2026-05-23 19:11:18 +0800

    Manual Update 3892270

---
+++ 3892270.lua
@@ -0,0 +1,12 @@
+-- 3892270's Lua and Manifest Created by Hubcap Manifest
+-- Gamble With Your Friends
+-- Created: May 10, 2026 at 23:33:08 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3892270, 1, "792fb0e3dedf02f8756774da38656b5b7212250cf8e33822e6b27199b2530385") -- Gamble With Your Friends
+-- MAIN APP DEPOTS
+addappid(3892271, 1, "90695be6010d5ec2e8890f1d742357316f4d5f74a8a9d22b2ae076b184797167") -- Depot 3892271
+setManifestid(3892271, "1917689724381081508", 2257963802)
