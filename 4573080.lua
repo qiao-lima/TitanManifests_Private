0e50239d098ad38c812d44073136663087cfce29
qiao-lima <qiao-lima@github.com>
2026-05-23 19:11:20 +0800

    Manual Update 4573080

---
+++ 4573080.lua
@@ -0,0 +1,12 @@
+-- 4573080's Lua and Manifest Created by Hubcap Manifest
+-- 执念刑警之消失的证人
+-- Created: May 23, 2026 at 00:52:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4573080) -- 执念刑警之消失的证人
+-- MAIN APP DEPOTS
+addappid(4573081, 1, "898847fdc3702fd751d7ed84f3e662f2854514838dca2d66f5975bbc36b1109d") -- Depot 4573081
+setManifestid(4573081, "7168019453951515602", 887168591)
