f37b10e6dd95c7d5d0103d054612c558aea54345
qiao-lima <qiao-lima@github.com>
2026-05-23 19:11:19 +0800

    Manual Update 4118140

---
+++ 4118140.lua
@@ -0,0 +1,22 @@
+-- 4118140's Lua and Manifest Created by Hubcap Manifest
+-- 馬場大亨Online國際版
+-- Created: May 23, 2026 at 00:53:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 5
+-- Total DLCs: 0
+-- Shared Depots: 4
+
+-- MAIN APPLICATION
+addappid(4118140) -- 馬場大亨Online國際版
+-- MAIN APP DEPOTS
+addappid(4118141, 1, "4e57b48730b0a72865022dece9cfa6145e850164332229a481f5807ba642b81e") -- Depot 4118141
+setManifestid(4118141, "524695398704528160", 1107418150)
+-- SHARED DEPOTS (from other apps)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (Shared from App 228980)
+setManifestid(228983, "8124929965194586177", 19265607)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- VC 2013 Redist (Shared from App 228980)
+setManifestid(228985, "3966345552745568756", 13699237)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
