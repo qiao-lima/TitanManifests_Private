6518c4b22eade4ef23e013ae09c2881dcb09b572
qiao-lima <qiao-lima@github.com>
2026-05-23 19:11:16 +0800

    Manual Update 2862210

---
+++ 2862210.lua
@@ -0,0 +1,21 @@
+-- 2862210's Lua and Manifest Created by Hubcap Manifest
+-- Trasmoz Legends
+-- Created: May 23, 2026 at 01:25:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(2862210) -- Trasmoz Legends
+-- MAIN APP DEPOTS
+addappid(2862211, 1, "ac13082ce49e7c46c0a8f33689646753daa294ed985f2fb60f31ebe6cab373a8") -- Depot 2862211
+setManifestid(2862211, "3446884431378648681", 33981124)
+-- DLCS WITH DEDICATED DEPOTS
+-- Trasmoz Legends - The Curse of Trasmoz (AppID: 3939530)
+addappid(3939530)
+addappid(3939530, 1, "b46144739b7f5bb43d664d116999b1d8fc1d9de8b3579e6c7f69748bd092971f") -- Trasmoz Legends - The Curse of Trasmoz - Depot 3939530
+setManifestid(3939530, "1289264096914713124", 73076356)
+-- Trasmoz Legends - Revenge of Trasmoz (AppID: 3939540)
+addappid(3939540)
+addappid(3939540, 1, "8fc627c9eae21ed91c863b4f0c175ff10a2b95e5077d69affc16d13e14017e77") -- Trasmoz Legends - Revenge of Trasmoz - Depot 3939540
+setManifestid(3939540, "8602449733016448117", 73232471)
