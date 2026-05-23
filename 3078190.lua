39ca7d019e2101124959dfe752e065a62bd610d8
qiao-lima <qiao-lima@github.com>
2026-05-23 19:11:17 +0800

    Manual Update 3078190

---
+++ 3078190.lua
@@ -0,0 +1,12 @@
+-- 3078190's Lua and Manifest Created by Hubcap Manifest
+-- Decipher The Deck
+-- Created: May 23, 2026 at 01:25:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3078190) -- Decipher The Deck
+-- MAIN APP DEPOTS
+addappid(3078191, 1, "0b9e7846a6bf8e94f842eacb137e1e402a610fccd534fe1f6ddd04e58f20c887") -- Depot 3078191
+setManifestid(3078191, "4102882918023323015", 139466075)
