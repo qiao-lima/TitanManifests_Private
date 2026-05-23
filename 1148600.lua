192b5653edeaf8a20db36f90839920ecedbcc4cd
qiao-lima <qiao-lima@github.com>
2026-05-23 19:11:14 +0800

    Manual Update 1148600

---
+++ 1148600.lua
@@ -0,0 +1,12 @@
+-- 1148600's Lua and Manifest Created by Hubcap Manifest
+-- Fluffy Arena
+-- Created: May 23, 2026 at 01:24:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1148600) -- Fluffy Arena
+-- MAIN APP DEPOTS
+addappid(1148601, 1, "3171b05705c8763abd3dac7e003df8a6c78c33de94848bb9a3652823f2a501dc") -- Depot 1148601
+setManifestid(1148601, "8892562068239235220", 156693036)
