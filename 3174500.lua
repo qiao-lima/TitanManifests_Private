cd97b8ace2148efd7d6aaa9bc4101692e5758e3c
qiao-lima <qiao-lima@github.com>
2026-05-23 19:11:17 +0800

    Manual Update 3174500

---
+++ 3174500.lua
@@ -0,0 +1,18 @@
+-- 3174500's Lua and Manifest Created by Hubcap Manifest
+-- Winter Survivor Protocol
+-- Created: May 23, 2026 at 02:27:58 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3174500, 1, "29d34200b9f9d8b30e001d9f9cc2583c76281b6e6ca1fbe928f0734e3bfb060c") -- Winter Survivor Protocol
+-- MAIN APP DEPOTS
+addappid(3174501, 1, "894e98e4349255d43750361c31f3066ad47f6cee4e1f8aa990f94aa869bea868") -- Depot 3174501
+setManifestid(3174501, "6273299751608887517", 16834105006)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
