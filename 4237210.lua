37a5e842238b7050d02efeee8db740a68e3cc1e9
qiao-lima <qiao-lima@github.com>
2026-05-23 19:11:20 +0800

    Manual Update 4237210

---
+++ 4237210.lua
@@ -0,0 +1,16 @@
+-- 4237210's Lua and Manifest Created by Hubcap Manifest
+-- Afternoon Affairs & Helpless Housewives
+-- Created: May 23, 2026 at 01:25:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4237210) -- Afternoon Affairs & Helpless Housewives
+-- MAIN APP DEPOTS
+addappid(4237211, 1, "c17f155d11339a4cb02b6e4cd9f792a2ee22c4454a257a2eb019a701855865ad") -- Depot 4237211
+setManifestid(4237211, "2240183389313593128", 344020142)
+addappid(4237212, 1, "7fa1690645bf9dcd09d7ba14787d6c22182ed9982ccdc3331bd6c893543dd339") -- Depot 4237212
+setManifestid(4237212, "5636328165212484385", 344370534)
+addappid(4237213, 1, "04d7296505e37b7cf8191582349321eeac7220d38bf5f355ec1ec51add4fd553") -- Depot 4237213
+setManifestid(4237213, "505351361789637418", 340839106)
