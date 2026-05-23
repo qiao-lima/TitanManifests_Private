18a458b68c3b2d178800a275e323a0984b94bd31
qiao-lima <qiao-lima@github.com>
2026-05-23 19:11:16 +0800

    Manual Update 2271030

---
+++ 2271030.lua
@@ -0,0 +1,15 @@
+-- 2271030's Lua and Manifest Created by Hubcap Manifest
+-- Tacape
+-- Created: May 23, 2026 at 02:19:25 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2271030) -- Tacape
+-- MAIN APP DEPOTS
+addappid(2271031, 1, "41523935d72151d4741dcb8cb8eecb8da9eec6a18595c40797e7ac1e0988a8b3") -- Depot 2271031
+setManifestid(2271031, "4069966661957275018", 1567620598)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2271032) -- Depot 2271032 (empty depot)
+-- addappid(2271033) -- Depot 2271033 (empty depot)
