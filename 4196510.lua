e7015d27098b8c9973e880e16a525abf95abb837
qiao-lima <qiao-lima@github.com>
2026-05-23 19:11:19 +0800

    Manual Update 4196510

---
+++ 4196510.lua
@@ -0,0 +1,12 @@
+-- 4196510's Lua and Manifest Created by Hubcap Manifest
+-- THROUGH TIME·SUMMER OF GIRL PUZZLE
+-- Created: May 23, 2026 at 00:51:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4196510) -- THROUGH TIME·SUMMER OF GIRL PUZZLE
+-- MAIN APP DEPOTS
+addappid(4196511, 1, "a62951f5ee64579f4340e344539e137a201f79e933a4cd7f8f87fed3aa5c914e") -- Depot 4196511
+setManifestid(4196511, "5299365375605621920", 440195168)
