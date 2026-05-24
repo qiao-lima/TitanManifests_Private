098c5fd5e2b42e9fe3e682d3bfc7547f8b0d74bc
qiao-lima <qiao-lima@github.com>
2026-05-25 05:10:55 +0800

    Manual Update 3605070

---
+++ 3605070.lua
@@ -0,0 +1,14 @@
+-- 3605070's Lua and Manifest Created by Hubcap Manifest
+-- ラミィの大冒険 1&2 リメイク
+-- Created: May 24, 2026 at 08:35:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3605070, 1, "3e6509a4a5a87ac18dd7bf0e40744d13c9c761bb7d7edbdf7cd4c8bdc5f480c9") -- ラミィの大冒険 1&2 リメイク
+-- MAIN APP DEPOTS
+addappid(3605071, 1, "d3fb836dde271ea5f54f1623c8bda6e117e10393c9a5d8af21f83cae189b854b") -- Depot 3605071
+setManifestid(3605071, "1705478968039558869", 390115699)
+addappid(3605073, 1, "74e7fabacd4c58d7e04bde468db16c52bb1736da7a2ccf4f2dcee2904ca85733") -- Depot 3605073
+setManifestid(3605073, "6038992846086258571", 385699382)
