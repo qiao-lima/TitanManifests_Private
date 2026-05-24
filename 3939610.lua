d20be0ac675a46f28397e268f925182fcc1b4a34
qiao-lima <qiao-lima@github.com>
2026-05-24 19:02:40 +0800

    Manual Update 3939610

---
+++ 3939610.lua
@@ -0,0 +1,12 @@
+-- 3939610's Lua and Manifest Created by Hubcap Manifest
+-- Horny Wives' Yoga Class
+-- Created: April 03, 2026 at 12:38:08 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3939610, 1, "40a709bf4496881baac1ee7b86ff3c26c30987f46c53ad4e54fb482048af6ca2") -- Horny Wives' Yoga Class
+-- MAIN APP DEPOTS
+addappid(3939611, 1, "c37f714fef9b0f8e9c306a8f05d123719a68054f4e048b3a7b5b107e5c69d930") -- Depot 3939611
+setManifestid(3939611, "1660989043812989564", 998510614)
