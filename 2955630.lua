2c8dc07b8511127954cb6788141f9f857fe77649
qiao-lima <qiao-lima@github.com>
2026-05-25 05:10:54 +0800

    Manual Update 2955630

---
+++ 2955630.lua
@@ -0,0 +1,12 @@
+-- 2955630's Lua and Manifest Created by Hubcap Manifest
+-- Catjinni
+-- Created: May 24, 2026 at 14:04:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2955630) -- Catjinni
+-- MAIN APP DEPOTS
+addappid(2955631, 1, "76a2e9d5a90aeb5a0b09d84524c56d85232c8be145471cea1b204fc8590e1393") -- Depot 2955631
+setManifestid(2955631, "6275406881466596370", 837279047)
