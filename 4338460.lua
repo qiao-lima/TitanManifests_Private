bd0b2d39394381111b4f2e03a4aafc3e7e0e4d87
qiao-lima <qiao-lima@github.com>
2026-05-25 05:11:20 +0800

    Manual Update 4338460

---
+++ 4338460.lua
@@ -0,0 +1,16 @@
+-- 4338460's Lua and Manifest Created by Hubcap Manifest
+-- Lost Lands: Stories about the Sorceress, the Prince and the Minotaur
+-- Created: May 24, 2026 at 09:32:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(4338460) -- Lost Lands: Stories about the Sorceress, the Prince and the Minotaur
+-- MAIN APP DEPOTS
+addappid(4338461, 1, "39336bcf82f47266dd1e2e63c75a5b8d54f94dad26cc4ca0c72bb76e4780d2e6") -- Depot 4338461
+setManifestid(4338461, "2440190113417629639", 2791529618)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
