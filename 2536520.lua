b01747ef5625d2468259e8f3e96573031d7c42f3
qiao-lima <qiao-lima@github.com>
2026-05-25 05:10:53 +0800

    Manual Update 2536520

---
+++ 2536520.lua
@@ -0,0 +1,14 @@
+-- 2536520's Lua and Manifest Created by Hubcap Manifest
+-- Diablo II: Resurrected – Infernal Edition
+-- Created: May 20, 2026 at 06:21:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2536520) -- Diablo II: Resurrected – Infernal Edition
+-- MAIN APP DEPOTS
+addappid(2536521, 1, "6ae0dd2e9f10f50302bd6fdaf0da8f6e724e970e56d35bbc5a2d3047969cf0ea") -- Depot 2536521
+setManifestid(2536521, "5854044149239771277", 42143759544)
+addappid(2536522, 1, "2ac8c20f9db336d670c6221c832758c6fc05a8cc16eea81805efed379bc9f60f") -- Depot 2536522
+setManifestid(2536522, "5462146561360755662", 86746444)
