9453c04e7fe4b12c87a92b02ba3f20440368786b
qiao-lima <qiao-lima@github.com>
2026-05-25 05:10:56 +0800

    Manual Update 3870580

---
+++ 3870580.lua
@@ -0,0 +1,25 @@
+-- 3870580's Lua and Manifest Created by Hubcap Manifest
+-- Nocan's Quest: Sex Gold Glory
+-- Created: February 05, 2026 at 20:19:56 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 3
+
+-- MAIN APPLICATION
+addappid(3870580) -- Nocan's Quest: Sex Gold Glory
+-- MAIN APP DEPOTS
+addappid(3870581, 1, "c3d471463ecbb3d56f72425d97b8a15cb18c311c46ea88d5854392f9b1521af4") -- Depot 3870581
+setManifestid(3870581, "8757836272753780789", 2451490506)
+-- DLCS WITH DEDICATED DEPOTS
+-- Nocans Quest Sex Gold Glory - Digital Artbook (AppID: 4203930)
+addappid(4203930)
+addappid(4203930, 1, "958c93468e03eef6f616017fa0f79545c5128da9c0681908aaeca945f130c840") -- Nocans Quest Sex Gold Glory - Digital Artbook - Depot 4203930
+setManifestid(4203930, "6426055927881689570", 1277774520)
+-- Nocans Quest Sex Gold Glory - Wallpaper Pack (AppID: 4203940)
+addappid(4203940)
+addappid(4203940, 1, "b8bee274035d6879db826571df8d8bc1eb2089da819a3fd673179180c4d223d5") -- Nocans Quest Sex Gold Glory - Wallpaper Pack - Depot 4203940
+setManifestid(4203940, "3151926734915614097", 580096532)
+-- Nocans Quest Sex Gold Glory - Animation pack (AppID: 4203960)
+addappid(4203960)
+addappid(4203960, 1, "e38a283f1fe722ab7420015e7a3536854bd7f3423438bf18699b3630574e8484") -- Nocans Quest Sex Gold Glory - Animation pack - Depot 4203960
+setManifestid(4203960, "23842501879476391", 1462181579)
