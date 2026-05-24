9ad7f2d03705f0e3c4402f69bd99619ee655d2fd
qiao-lima <qiao-lima@github.com>
2026-05-25 05:11:19 +0800

    Manual Update 4276660

---
+++ 4276660.lua
@@ -0,0 +1,16 @@
+-- 4276660's Lua and Manifest Created by Hubcap Manifest
+-- Yomifuda: Kanji Survivors
+-- Created: May 24, 2026 at 15:53:08 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(4276660) -- Yomifuda: Kanji Survivors
+-- MAIN APP DEPOTS
+addappid(4276661, 1, "ce2bbf298ab010b66c79f8e5d94b2f3e26d241351929cfb1fe21a772edfab1e6") -- Depot 4276661
+setManifestid(4276661, "2327831445311234959", 1721671371)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
