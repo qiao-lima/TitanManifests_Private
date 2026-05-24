0092606c6eb36714bdabe7740cbf2adffd6d654e
qiao-lima <qiao-lima@github.com>
2026-05-24 10:39:09 +0800

    Manual Update 3094270

---
+++ 3094270.lua
@@ -0,0 +1,12 @@
+-- 3094270's Lua and Manifest Created by Hubcap Manifest
+-- Crash Puzzle Hammer-San
+-- Created: May 23, 2026 at 14:23:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3094270, 1, "a804c5eeebb34c82a714838360ea0865f75019dd1cf162eccc2ae0ec5d41c6df") -- Crash Puzzle Hammer-San
+-- MAIN APP DEPOTS
+addappid(3094271, 1, "27bac5c05bf0677b449d0b182d018347ec77424b0a1ea31acf7ea35f4932af3b") -- Depot 3094271
+setManifestid(3094271, "617533554175354017", 340083264)
