26aea0eb1e7590c5ca7e3b0dcc104129a041ce94
qiao-lima <qiao-lima@github.com>
2026-05-25 05:10:56 +0800

    Manual Update 4242290

---
+++ 4242290.lua
@@ -0,0 +1,12 @@
+-- 4242290's Lua and Manifest Created by Hubcap Manifest
+-- SPITE: Taking the Boss's Wife, Sister and Mother
+-- Created: May 24, 2026 at 07:48:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4242290) -- SPITE: Taking the Boss's Wife, Sister and Mother
+-- MAIN APP DEPOTS
+addappid(4242291, 1, "193f712b8c3e06b1d21b9073523678087406578590945666d73079a628e08f6c") -- Depot 4242291
+setManifestid(4242291, "7990686667438958502", 831132723)
