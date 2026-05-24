87b4fd6eeacdffc4f8faca00647dcf633455f1a6
qiao-lima <qiao-lima@github.com>
2026-05-25 05:10:56 +0800

    Manual Update 3907210

---
+++ 3907210.lua
@@ -0,0 +1,17 @@
+-- 3907210's Lua and Manifest Created by Hubcap Manifest
+-- Married Into Hell
+-- Created: March 10, 2026 at 22:54:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3907210) -- Married Into Hell
+-- MAIN APP DEPOTS
+addappid(3907211, 1, "9115b152cbb8ab5b2550874aff099232d39184e43841cfeba8e346774d902229") -- Depot 3907211
+setManifestid(3907211, "2976642793536142521", 2283899751)
+-- DLCS WITH DEDICATED DEPOTS
+-- Married Into Hell - Digital Artbook (AppID: 4347060)
+addappid(4347060)
+addappid(4347061, 1, "356a8d143d63b7a4c1eb9363919f0db5a7cd49bd611794a8309900125ccb9571") -- Married Into Hell - Digital Artbook - Depot 4347061
+setManifestid(4347061, "103760923736615207", 154527761)
