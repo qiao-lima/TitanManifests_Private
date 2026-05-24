dd2fe929404c6eb79f8c736fda7dcd1a661df4c5
qiao-lima <qiao-lima@github.com>
2026-05-24 19:02:38 +0800

    Manual Update 3161400

---
+++ 3161400.lua
@@ -0,0 +1,14 @@
+-- 3161400's Lua and Manifest Created by Hubcap Manifest
+-- Infinity Sweeper
+-- Created: May 17, 2026 at 11:58:13 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3161400, 1, "913bb06365730d67f5543f7b62b4990edb7a327aa43afb49bfb941b1d5876ccc") -- Infinity Sweeper
+-- MAIN APP DEPOTS
+addappid(3161402, 1, "7069d2ffdd7957f267e862396d6792fb4eb7619a3a473e142ef0d5427cb67aa8") -- Depot 3161402
+setManifestid(3161402, "6738453963548504500", 408767617)
+addappid(3161403, 1, "2ae561e2200c05543de39012055b9492ffcbee0fd6e2cc54ea9de34478944dbf") -- Depot 3161403
+setManifestid(3161403, "2295982864686158781", 580482426)
