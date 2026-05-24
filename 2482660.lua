0e4cad13c335810fc9a9fa2c4657fdecb048820b
qiao-lima <qiao-lima@github.com>
2026-05-24 10:39:08 +0800

    Manual Update 2482660

---
+++ 2482660.lua
@@ -0,0 +1,12 @@
+-- 2482660's Lua and Manifest Created by Hubcap Manifest
+-- Horror Gallery
+-- Created: May 23, 2026 at 15:21:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2482660) -- Horror Gallery
+-- MAIN APP DEPOTS
+addappid(2482661, 1, "faca1684ce048e09ea2c3d6a3ead982a4c6576d4bca68d94954bce047f71d96a") -- Depot 2482661
+setManifestid(2482661, "6917333002653125238", 3227395072)
