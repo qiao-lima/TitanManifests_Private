9102497d14f65eaed7389028d5aa72d8a51c0cc0
qiao-lima <qiao-lima@github.com>
2026-05-25 05:11:21 +0800

    Manual Update 4654280

---
+++ 4654280.lua
@@ -0,0 +1,12 @@
+-- 4654280's Lua and Manifest Created by Hubcap Manifest
+-- 我的钓鱼世界
+-- Created: May 24, 2026 at 15:52:08 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4654280) -- 我的钓鱼世界
+-- MAIN APP DEPOTS
+addappid(4654281, 1, "6798bf79b09c53d17ac8332fa7c8d48614117ae973d74ab6defcc44dd8c1e22d") -- Depot 4654281
+setManifestid(4654281, "5254983712543483060", 2178350508)
