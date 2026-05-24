108ca1e9297291e5453871771819653930bc376e
qiao-lima <qiao-lima@github.com>
2026-05-24 10:39:11 +0800

    Manual Update 4558500

---
+++ 4558500.lua
@@ -0,0 +1,12 @@
+-- 4558500's Lua and Manifest Created by Hubcap Manifest
+-- Squarsenal
+-- Created: May 23, 2026 at 14:23:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4558500) -- Squarsenal
+-- MAIN APP DEPOTS
+addappid(4558501, 1, "5892ccc78cb32dc7be0daae63e66e2bbd36219c7a8eafba5adf33e00d625b96b") -- Depot 4558501
+setManifestid(4558501, "6554516770237586117", 232646053)
