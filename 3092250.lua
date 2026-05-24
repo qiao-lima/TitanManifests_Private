e5f852c169b9fec72c0c4fa720d0c10891426285
qiao-lima <qiao-lima@github.com>
2026-05-25 05:10:54 +0800

    Manual Update 3092250

---
+++ 3092250.lua
@@ -0,0 +1,12 @@
+-- 3092250's Lua and Manifest Created by Hubcap Manifest
+-- Mirror World
+-- Created: May 24, 2026 at 16:07:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3092250) -- Mirror World
+-- MAIN APP DEPOTS
+addappid(3092251, 1, "a327652b2e091649e85f7b97b6bfcbb31ce4c92d2373880222e9e3258234f1d8") -- Depot 3092251
+setManifestid(3092251, "499951537129252881", 2928958856)
