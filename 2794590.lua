7a0d41d214e2b9a357ac9764f4c59767732e8280
qiao-lima <qiao-lima@github.com>
2026-05-25 05:10:53 +0800

    Manual Update 2794590

---
+++ 2794590.lua
@@ -0,0 +1,12 @@
+-- 2794590's Lua and Manifest Created by Hubcap Manifest
+-- Scam Line
+-- Created: March 24, 2026 at 20:09:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2794590) -- Scam Line
+-- MAIN APP DEPOTS
+addappid(2794591, 1, "51f8d8cb073b66732142b3b3a3effe4a35611fd37f066ac010cce82e62edd0ef") -- Depot 2794591
+setManifestid(2794591, "2848370058653328420", 348347837)
