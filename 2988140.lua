eb6d8cc4fd1a1e28eda07901692d302f6711bf93
qiao-lima <qiao-lima@github.com>
2026-05-24 10:39:09 +0800

    Manual Update 2988140

---
+++ 2988140.lua
@@ -0,0 +1,12 @@
+-- 2988140's Lua and Manifest Created by Hubcap Manifest
+-- Lacrosse 26
+-- Created: May 23, 2026 at 14:23:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2988140, 1, "457c2f2f2a4728af1ac335fe27fb8a227573cf63fa98943fff1809bacf3a4f00") -- Lacrosse 26
+-- MAIN APP DEPOTS
+addappid(2988142, 1, "c596e12e3b5fe9762b00b765e20f1a34367181ba911a1b1250721e1b2516cfb8") -- Depot 2988142
+setManifestid(2988142, "2377149785074234595", 4619973494)
