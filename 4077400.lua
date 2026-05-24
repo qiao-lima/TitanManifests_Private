0bad9da929d19e031477ca919f30d29cd191441e
qiao-lima <qiao-lima@github.com>
2026-05-24 10:39:11 +0800

    Manual Update 4077400

---
+++ 4077400.lua
@@ -0,0 +1,18 @@
+-- 4077400's Lua and Manifest Created by Hubcap Manifest
+-- Blood High!
+-- Created: May 23, 2026 at 02:28:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4077400, 1, "8b572380adc784e059a0dd16a65af062016867d3ad864849d605b798e4210b27") -- Blood High!
+-- MAIN APP DEPOTS
+addappid(4077401, 1, "6eb5d82fdaa20c8e41bda33f77f0660c162c218dcbffbcf4c3f4d2b9a5c3e241") -- Depot 4077401
+setManifestid(4077401, "6419967096833249616", 1342296563)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
