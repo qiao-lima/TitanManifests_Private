0858a6d88bffb274c0f7710301a61566671acd3e
qiao-lima <qiao-lima@github.com>
2026-05-25 05:10:57 +0800

    Manual Update 4272540

---
+++ 4272540.lua
@@ -0,0 +1,14 @@
+-- 4272540's Lua and Manifest Created by Hubcap Manifest
+-- Lovers' Fun!
+-- Created: May 24, 2026 at 15:45:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4272540) -- Lovers' Fun!
+-- MAIN APP DEPOTS
+addappid(4272541, 1, "f9567ffbae07ae203edc1521a8b1202445fab5f626da378f6b3bba23794c45e4") -- Depot 4272541
+setManifestid(4272541, "1937533008306961244", 479131805)
+addappid(4272543, 1, "f155cc17a68a7d02cef4e7e2c80855faf2d64beeea7e3613dedbfcb786b3f8a6") -- Depot 4272543
+setManifestid(4272543, "930591219893056823", 510120507)
