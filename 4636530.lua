7b0616618bc8a93acc131e16e04f9f216c045752
qiao-lima <qiao-lima@github.com>
2026-05-25 05:11:20 +0800

    Manual Update 4636530

---
+++ 4636530.lua
@@ -0,0 +1,13 @@
+-- 4636530's Lua and Manifest Created by Hubcap Manifest
+-- Glint Translator
+-- Created: May 24, 2026 at 15:51:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+-- Shared Depots: 6
+
+-- MAIN APPLICATION
+addappid(4636530) -- Glint Translator
+-- MAIN APP DEPOTS
+addappid(4636531, 1, "c12382f3ec2933b8502e388efd34e08c788bf1ebd18d976c38d826aa94ff28da") -- Depot 4636531
+setManifestid(4636531, "4888155917337560644", 267888729)
