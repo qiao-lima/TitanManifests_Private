140350a75592e09b0ad3c76435d5d081eebb62fd
qiao-lima <qiao-lima@github.com>
2026-05-25 22:00:47 +0800

    Manual Update 4089690

---
+++ 4089690.lua
@@ -0,0 +1,12 @@
+-- 4089690's Lua and Manifest Created by Hubcap Manifest
+-- Lofi Mahjong
+-- Created: May 25, 2026 at 08:11:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4089690) -- Lofi Mahjong
+-- MAIN APP DEPOTS
+addappid(4089691, 1, "ae16edec502be909c0da04d41dc666c2283233d09c52ec40ac7f4fbd8b7db8db") -- Depot 4089691
+setManifestid(4089691, "4036217369775599086", 252974571)
