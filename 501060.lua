bb7f286280ad97e38e9300169847e57968775a1e
qiao-lima <qiao-lima@github.com>
2026-05-25 23:35:59 +0800

    Manual Update 501060

---
+++ 501060.lua
@@ -0,0 +1,8 @@
+-- ============================================
+--  Credits: K3rnelPan1c (KernelOS / !K3rnalyze)
+--  Discord: https://discord.gg/k3rnalyze
+--  Website: https://kernelos.org/games
+-- ============================================
+
+addappid(501060)
+addappid(501061,0,"4a09c63af9fbf79d3c640f32fdac411c49f11497c41da166b08ee0668dea0acc")
