6911bc4e1b3dc3faffcd52fbda806fd1c0d964a8
qiao-lima <qiao-lima@github.com>
2026-05-25 22:00:47 +0800

    Manual Update 3935260

---
+++ 3935260.lua
@@ -0,0 +1,14 @@
+-- 3935260's Lua and Manifest Created by Hubcap Manifest
+-- OneDay: Eastern Gardens and Wilderness(Lofi)
+-- Created: May 25, 2026 at 06:18:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3935260) -- OneDay: Eastern Gardens and Wilderness(Lofi)
+-- MAIN APP DEPOTS
+addappid(3935263, 1, "6d85ea48cbcce6e46389717708d1de424e4caa61df0cc2ed3a4fe979ca8c32a9") -- Depot 3935263
+setManifestid(3935263, "1936958291110387254", 5021002853)
+addappid(3935264, 1, "53c8b3cfd9afd92f53a2aa5d36afb99d244d953bf2119a5db778600e75885069") -- Depot 3935264
+setManifestid(3935264, "2015856989406346585", 5013128887)
