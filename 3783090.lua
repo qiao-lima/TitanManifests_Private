7d2dc2d2554788bb85bf1220fa9ff674157c5f03
qiao-lima <qiao-lima@github.com>
2026-05-25 22:00:47 +0800

    Manual Update 3783090

---
+++ 3783090.lua
@@ -0,0 +1,13 @@
+-- 3783090's Lua and Manifest Created by Hubcap Manifest
+-- Hot and Lovely : Purple Lust
+-- Created: May 25, 2026 at 08:11:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3783090, 1, "1e77cc14ed452d14267300da4efc43030f5f802ccfab7a3bd38522ac47f41ec2") -- Hot and Lovely : Purple Lust
+addtoken(3783090, "10093652871749631995")
+-- MAIN APP DEPOTS
+addappid(3783091, 1, "f730b31efb60144645ef62f7f6ee12801a995033efdd3a0944a123e13eb63684") -- Depot 3783091
+setManifestid(3783091, "9038115026233936236", 2026448356)
