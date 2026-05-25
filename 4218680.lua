7f7a9e1cbb8ff8ef88d764bfbb8f96a68d1e5e59
qiao-lima <qiao-lima@github.com>
2026-05-25 22:00:48 +0800

    Manual Update 4218680

---
+++ 4218680.lua
@@ -0,0 +1,16 @@
+-- 4218680's Lua and Manifest Created by Hubcap Manifest
+-- Bird Game 3
+-- Created: May 25, 2026 at 07:44:58 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4218680, 1, "74455bde1979e79a6f9fb1552a40e15937b0ecc9df0c702a51a6bb7ba4cecec2") -- Bird Game 3
+-- MAIN APP DEPOTS
+addappid(4218681, 1, "875178362dac697335bcc5a341f4b48b9d7819765571937a69785778a0f4f830") -- Depot 4218681
+setManifestid(4218681, "4874055753674466856", 3063665033)
+addappid(4218682, 1, "d1cbad7099b32fceae5eeb95e6de4d3888654e13f3401ffc08e958bed82f3b6c") -- Depot 4218682
+setManifestid(4218682, "1067695285807969125", 3109031275)
+addappid(4218683, 1, "4b9bbdbedcda92fe0601c05eb19234556bb4a29f40c25c63d3cbe66a1cb87311") -- Depot 4218683
+setManifestid(4218683, "3654124610578605522", 3086418312)
