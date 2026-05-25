293f40983d46e4268542ec526ff936373034c094
qiao-lima <qiao-lima@github.com>
2026-05-25 23:35:59 +0800

    Manual Update 816020

---
+++ 816020.lua
@@ -0,0 +1,41 @@
+-- 816020's Lua and Manifest Created by Hubcap Manifest
+-- JUMP FORCE
+-- Created: September 29, 2025 at 20:16:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 19
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(816020, 1, "32e83f63a7c0a26027fad1cca8b4a3d26bcc12b53996baf333b996abb1df0532") -- JUMP FORCE
+-- MAIN APP DEPOTS
+addappid(816021, 1, "06212bff2603c46ed13947689e04c3513c44d7a2e28898f4b66b848dbbbb3bb1") -- revo Content
+setManifestid(816021, "2700341200775595261", 23856777652)
+-- SHARED DEPOTS (from other apps)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- VC 2015 Redist (Shared from App 228980)
+setManifestid(228986, "8782296191957114623", 29759921)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(974700) -- JUMP FORCE Character Pack 1 Seto Kaiba
+addtoken(974700, "7936578275621938410")
+addappid(974701) -- JUMP FORCE Character Pack 2 Biscuit Krueger
+addtoken(974701, "7676490120808512297")
+addappid(974702) -- JUMP FORCE Character Pack 3 All Might
+addtoken(974702, "10171135764648568884")
+addappid(974720) -- JUMP FORCE Character Pack 4 Majin Buu (Good)
+addappid(974721) -- JUMP FORCE Character Pack 5 Katsuki Bakugo
+addappid(974722) -- JUMP FORCE Character Pack 6 Toshiro Hitsugaya
+addappid(974723) -- JUMP FORCE Character Pack 7 Madara Uchiha
+addappid(974724) -- JUMP FORCE Character Pack 8 Grimmjow Jaegerjaquez
+addappid(974725) -- JUMP FORCE Character Pack 9 Trafalgar Law
+addappid(974726) -- JUMP FORCE - Characters Pass
+addappid(974727) -- JUMP FORCE - Pre-Order DLC Bundle
+addappid(974728) -- JUMP FORCE - Ultimate Edition DLC Bundle
+addappid(974729) -- JUMP FORCE - Game Logo Avatar T-Shirt
+addappid(1248330) -- JUMP FORCE Character Pack 10 Shoto Todoroki
+addappid(1248331) -- JUMP FORCE Character Pack 11 Meruem
+addappid(1248332) -- JUMP FORCE Character Pack 12 Hiei
+addappid(1248333) -- JUMP FORCE Character Pack 13 Yoruichi Shihoin
+addappid(1248334) -- JUMP FORCE Character Pack 14 Giorno Giovanna
+addappid(1248335) -- JUMP FORCE - Characters Pass 2
