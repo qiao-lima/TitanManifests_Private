6296ee94a081160e38d51b7c88ee91c7c43aa39b
qiao-lima <qiao-lima@github.com>
2026-05-25 22:00:48 +0800

    Manual Update 4646160

---
+++ 4646160.lua
@@ -0,0 +1,12 @@
+-- 4646160's Lua and Manifest Created by Hubcap Manifest
+-- VR180 SyncPro
+-- Created: May 25, 2026 at 06:18:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4646160) -- VR180 SyncPro
+-- MAIN APP DEPOTS
+addappid(4646161, 1, "f2c69f08e5acb67d9f27fdf1afa453a85a7d27cefbb2cea7c9946e8217717904") -- Depot 4646161
+setManifestid(4646161, "71359436180038833", 4312905544)
