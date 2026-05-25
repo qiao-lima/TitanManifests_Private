a0abf82b8ff6117eff74ffbcd121358d03d68be8
qiao-lima <qiao-lima@github.com>
2026-05-25 22:00:45 +0800

    Manual Update 3143570

---
+++ 3143570.lua
@@ -0,0 +1,12 @@
+-- 3143570's Lua and Manifest Created by Hubcap Manifest
+-- Holy Valkyrie EXS-TiA: Concerto 1
+-- Created: May 25, 2026 at 07:46:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3143570, 1, "a2aa386a79525d7794d0a355077b6e5d8fa2bca2f18d8a87bbb84139c43fe97c") -- Holy Valkyrie EXS-TiA: Concerto 1
+-- MAIN APP DEPOTS
+addappid(3143571, 1, "4d0ee90bf430c1a0759eef06a4aff8a0081f9818e451d28337d93e15960e360a") -- Depot 3143571
+setManifestid(3143571, "8775241465840793751", 108889632)
