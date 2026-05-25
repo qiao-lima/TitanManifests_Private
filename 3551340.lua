50cb8cfd4636088cfb057a3fee0d509b035d8bf6
qiao-lima <qiao-lima@github.com>
2026-05-25 23:35:56 +0800

    Manual Update 3551340

---
+++ 3551340.lua
@@ -0,0 +1,10 @@
+-- ============================================
+--  Credits: K3rnelPan1c (KernelOS / !K3rnalyze)
+--  Discord: https://discord.gg/k3rnalyze
+--  Website: https://kernelos.org/games
+-- ============================================
+
+addappid(3551340)
+addappid(3551341,0,"a4f9c46a0797978fa0b278dac08a0f473872fc74c6b6d9bf45b30d56e33a42c7")
+addappid(3551342,0,"31edad03a3daebca77afad83adb899f2ff6611d949738ad3d15e708da8f2c7ca")
+addappid(3551410)
