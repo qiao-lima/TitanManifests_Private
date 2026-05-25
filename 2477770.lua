17b0dbec0745c539c4fe597a773aa65d64de6c3f
qiao-lima <qiao-lima@github.com>
2026-05-25 22:00:45 +0800

    Manual Update 2477770

---
+++ 2477770.lua
@@ -0,0 +1,16 @@
+-- 2477770's Lua and Manifest Created by Hubcap Manifest
+-- Summer Party Time
+-- Created: May 25, 2026 at 08:17:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(2477770) -- Summer Party Time
+-- MAIN APP DEPOTS
+addappid(2477771, 1, "821628ba2910bc4b9be183153318786bfacd49530e507b72828abd89638fe628") -- Depot 2477771
+setManifestid(2477771, "7602601429472378382", 352938309)
+addappid(2477772, 1, "5167426ee3aad8d53ccf77cc3b57f0e0b962d9d54308816db0b8e4488f4bf93c") -- Depot 2477772
+setManifestid(2477772, "1562022007488872928", 352938310)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2518030) -- Summer Party Time - Survival Pack
