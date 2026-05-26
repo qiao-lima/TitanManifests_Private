5605b648008555f45c1e78cf9a527463cd9a1129
qiao-lima <qiao-lima@github.com>
2026-05-26 11:59:37 +0800

    Manual Update 4621590

---
+++ 4621590.lua
@@ -0,0 +1,12 @@
+-- 4621590's Lua and Manifest Created by Hubcap Manifest
+-- Dereology Chime
+-- Created: May 16, 2026 at 01:39:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4621590) -- Dereology Chime
+-- MAIN APP DEPOTS
+addappid(4621591, 1, "4da968b3af37f5e8ddc31d884017986f82d881ec6960deb74ada8409b1cecb5d") -- Depot 4621591
+setManifestid(4621591, "3226931149793472441", 1318811824)
