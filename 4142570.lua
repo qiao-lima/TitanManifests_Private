62c114213e7b3d36089c05922365f1e7f4505d1c
qiao-lima <qiao-lima@github.com>
2026-05-26 12:26:24 +0800

    Manual Update 4142570

---
+++ 4142570.lua
@@ -0,0 +1,12 @@
+-- 4142570's Lua and Manifest Created by Hubcap Manifest
+-- Evil Girls Party!
+-- Created: May 26, 2026 at 00:24:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4142570) -- Evil Girls Party!
+-- MAIN APP DEPOTS
+addappid(4142571, 1, "32703d1248a75e6037cac9f0abf71c4dbc3f086b337f71d14eea5b01eeba7df3") -- Depot 4142571
+setManifestid(4142571, "6990651331425418637", 981060418)
