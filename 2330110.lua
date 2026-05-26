2ab415b19ae1f3027c28e9eccc66332f69dee082
qiao-lima <qiao-lima@github.com>
2026-05-27 06:06:23 +0800

    Manual Update 2330110

---
+++ 2330110.lua
@@ -0,0 +1,14 @@
+-- 2330110's Lua and Manifest Created by Hubcap Manifest
+-- Yerba Buena
+-- Created: May 26, 2026 at 14:16:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2330110, 1, "1c6c8bba52af19155d3455458f2faa1ff1861daae1b76accd3b29804a2de5bec") -- Yerba Buena
+-- MAIN APP DEPOTS
+addappid(2330111, 1, "c77eb4c925f1bb7e50b0cb6d97ade03a695ac8fa35f20b6d4957e35cd2ce4b2d") -- Depot 2330111
+setManifestid(2330111, "3079248203994920906", 7027947018)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2330119) -- Depot 2330119 (empty depot)
