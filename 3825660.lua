88b676148f0d58718d2d1be348feb539adccbbbe
qiao-lima <qiao-lima@github.com>
2026-05-27 06:06:24 +0800

    Manual Update 3825660

---
+++ 3825660.lua
@@ -0,0 +1,12 @@
+-- 3825660's Lua and Manifest Created by Hubcap Manifest
+-- Isekai Big Dick
+-- Created: May 26, 2026 at 14:16:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3825660, 1, "d19d293d08999e561460698073ff306a90881f852e2405264c8259410c2c5a38") -- Isekai Big Dick
+-- MAIN APP DEPOTS
+addappid(3825661, 1, "5106a11c11738158f0a8fa8e312fb8f3e5cb831c70bd2f329326b6b5f2014272") -- Depot 3825661
+setManifestid(3825661, "1049897559512836620", 2757089431)
