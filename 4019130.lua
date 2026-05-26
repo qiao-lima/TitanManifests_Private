c654a4247fc78f462d253306229de644b4b209c3
qiao-lima <qiao-lima@github.com>
2026-05-26 11:07:29 +0800

    Manual Update 4019130

---
+++ 4019130.lua
@@ -0,0 +1,12 @@
+-- 4019130's Lua and Manifest Created by Hubcap Manifest
+-- Psyvariar 3
+-- Created: May 25, 2026 at 20:36:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4019130) -- Psyvariar 3
+-- MAIN APP DEPOTS
+addappid(4019131, 1, "c469988058ed29381d8f3c0a3c0cec1f2c24a711f9d13184c3dc4bf86461a247") -- Depot 4019131
+setManifestid(4019131, "3896306347247031027", 4046859718)
