23976b1fb761ad76c9c63adc6cac74d813a0070b
qiao-lima <qiao-lima@github.com>
2026-05-26 22:59:26 +0800

    Manual Update 4033080

---
+++ 4033080.lua
@@ -0,0 +1,14 @@
+-- 4033080's Lua and Manifest Created by Hubcap Manifest
+-- Deep Corp
+-- Created: May 26, 2026 at 07:43:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4033080) -- Deep Corp
+-- MAIN APP DEPOTS
+addappid(4033081, 1, "1dea9b4f2e212d96bcdf50eb5bed511bfb493c6d4b49112b55824e7b62b44949") -- Depot 4033081
+setManifestid(4033081, "2393844833314590555", 1183929165)
+addappid(4033082, 1, "60d4e97948be13db6358b47f1a829a0496b98e2023d63a4a955e51c8e62f88c1") -- Depot 4033082
+setManifestid(4033082, "8560882388274912999", 1306061201)
