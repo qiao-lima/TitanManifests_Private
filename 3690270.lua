993cc7a07248bee6a8b754c43112740012999413
qiao-lima <qiao-lima@github.com>
2026-05-26 11:59:35 +0800

    Manual Update 3690270

---
+++ 3690270.lua
@@ -0,0 +1,12 @@
+-- 3690270's Lua and Manifest Created by Hubcap Manifest
+-- Girls' Dorm -Managing an all-girls' dormitory- After
+-- Created: May 19, 2026 at 05:30:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3690270) -- Girls' Dorm -Managing an all-girls' dormitory- After
+-- MAIN APP DEPOTS
+addappid(3690271, 1, "6d53c8e096cff122788ed03b7dd570c94ac432cab409956bbfc2fc85c32a1603") -- Depot 3690271
+setManifestid(3690271, "6066226132806900554", 573636468)
