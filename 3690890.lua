d83305dc5f750dc3d985900ef96df4ecba6b127c
qiao-lima <qiao-lima@github.com>
2026-05-26 22:59:26 +0800

    Manual Update 3690890

---
+++ 3690890.lua
@@ -0,0 +1,12 @@
+-- 3690890's Lua and Manifest Created by Hubcap Manifest
+-- Odd Dorable
+-- Created: May 26, 2026 at 09:21:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3690890) -- Odd Dorable
+-- MAIN APP DEPOTS
+addappid(3690891, 1, "23845f995f3c859115edbb249cb8268d81d98ed21a91c2b829815647fcfbdb53") -- Depot 3690891
+setManifestid(3690891, "4692442731143482738", 806171147)
