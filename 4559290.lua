167e8d7e6d812d6102166a1bbe2035097b3f40b6
qiao-lima <qiao-lima@github.com>
2026-05-26 22:59:27 +0800

    Manual Update 4559290

---
+++ 4559290.lua
@@ -0,0 +1,18 @@
+-- 4559290's Lua and Manifest Created by Hubcap Manifest
+-- UNDERKING KINGDOM
+-- Created: May 26, 2026 at 05:15:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4559290) -- UNDERKING KINGDOM
+-- MAIN APP DEPOTS
+addappid(4559291, 1, "571c1a463b6c3fb91c2582f925a627f22f6fb07fc2983826c81775330cb955ed") -- Depot 4559291
+setManifestid(4559291, "1861147583507215389", 6093657992)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
