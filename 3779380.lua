4419f759d29fb1c672344aa231d39106c78b7b6b
qiao-lima <qiao-lima@github.com>
2026-05-26 11:59:36 +0800

    Manual Update 3779380

---
+++ 3779380.lua
@@ -0,0 +1,12 @@
+-- 3779380's Lua and Manifest Created by Hubcap Manifest
+-- Abyss Runner
+-- Created: May 15, 2026 at 05:14:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3779380) -- Abyss Runner
+-- MAIN APP DEPOTS
+addappid(3779383, 1, "410f0bca2d28eebec80b8e97faddda2285fb39954ef089f90feb10ce3fb753c3") -- Depot 3779383
+setManifestid(3779383, "5566512982691165102", 186649595)
