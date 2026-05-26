fc69db707025cb24f2cbc97547a7c70011c861e6
qiao-lima <qiao-lima@github.com>
2026-05-26 22:59:23 +0800

    Manual Update 2692360

---
+++ 2692360.lua
@@ -0,0 +1,12 @@
+-- 2692360's Lua and Manifest Created by Hubcap Manifest
+-- うっかり婚～目覚めたら、ワケあり上司の妻でした～
+-- Created: May 26, 2026 at 07:43:30 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2692360, 1, "c2c1b6965fc23f5c12abfde3b6f12ce4e73386ebc0099574f935912454157964") -- うっかり婚～目覚めたら、ワケあり上司の妻でした～
+-- MAIN APP DEPOTS
+addappid(2692361, 1, "99fc954217a050a56bc618e9945af77849deb690dc18978237ede7a862e464ba") -- Depot 2692361
+setManifestid(2692361, "3733225585228164688", 1143059609)
