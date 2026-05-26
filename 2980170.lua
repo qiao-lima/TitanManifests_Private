ec3769a7e778d7aa63fbe1581ae4ff2d76821cfa
qiao-lima <qiao-lima@github.com>
2026-05-26 11:07:28 +0800

    Manual Update 2980170

---
+++ 2980170.lua
@@ -0,0 +1,20 @@
+-- 2980170's Lua and Manifest Created by Hubcap Manifest
+-- Vortica
+-- Created: May 25, 2026 at 20:36:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(2980170) -- Vortica
+-- MAIN APP DEPOTS
+addappid(2980171, 1, "62b4a4381b1d199d92e324a842d7089d9b5b11437defb03b45d1d434c51cc528") -- Depot 2980171
+setManifestid(2980171, "3532318061052151954", 5667580988)
+-- SHARED DEPOTS (from other apps)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 117381405)
+addappid(229012, 1, "421e499f39a617c1a3b6baf11fbc65ffebd541d2c39c38d3bf6ffbd87919ce12") -- XNA 4.0 Redist (Shared from App 228980)
+setManifestid(229012, "4353723233161159493", 7061608)
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") -- OpenAL 2.0.7.0 Redist (Shared from App 228980)
+setManifestid(229020, "5799761707845834510", 810085)
