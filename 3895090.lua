748f8863cafb08089538abff490fbb38e8370997
qiao-lima <qiao-lima@github.com>
2026-05-26 11:59:36 +0800

    Manual Update 3895090

---
+++ 3895090.lua
@@ -0,0 +1,12 @@
+-- 3895090's Lua and Manifest Created by Hubcap Manifest
+-- Evil Doll 2
+-- Created: May 25, 2026 at 23:55:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3895090) -- Evil Doll 2
+-- MAIN APP DEPOTS
+addappid(3895091, 1, "a8331cc63af4d133654f16a7ac5f534840833bce1ae513ec911a53aaf7d4d119") -- Depot 3895091
+setManifestid(3895091, "2836879117757344666", 320337233)
