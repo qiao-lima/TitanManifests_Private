5b7170289d6b1baffe7dc6bd2edaced1e4f80669
qiao-lima <qiao-lima@github.com>
2026-05-26 11:07:28 +0800

    Manual Update 3400350

---
+++ 3400350.lua
@@ -0,0 +1,14 @@
+-- 3400350's Lua and Manifest Created by Hubcap Manifest
+-- Third Crisis: Neon Nights
+-- Created: May 25, 2026 at 07:57:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3400350, 1, "b5b7a7ddb7bd6becf50f0f3e59b6fce4335796f33bfc57465433be563aa52872") -- Third Crisis: Neon Nights
+-- MAIN APP DEPOTS
+addappid(3400351, 1, "d7235ae13dc595910e55dbe00a78c7f1944a44268fa6fea54330f1451bed200f") -- Depot 3400351
+setManifestid(3400351, "455322201823925033", 3814624704)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4508330) -- Third Crisis Neon Nights - Bonus Outfit
