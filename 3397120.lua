92c3a9a3207b404deb4ebbdc7b145efc3073a517
qiao-lima <qiao-lima@github.com>
2026-05-26 11:59:33 +0800

    Manual Update 3397120

---
+++ 3397120.lua
@@ -0,0 +1,13 @@
+-- 3397120's Lua and Manifest Created by Hubcap Manifest
+-- Holy Valkyrie ExS-TIA P
+-- Created: May 20, 2026 at 23:20:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3397120) -- Holy Valkyrie ExS-TIA P
+addtoken(3397120, "6507985806190611907")
+-- MAIN APP DEPOTS
+addappid(3397121, 1, "b761646a2cc1fc887fb9318f4cd11a8e7464a590d82d5f4dc43c8b82e1432d12") -- Depot 3397121
+setManifestid(3397121, "1219968210652606310", 391806730)
