a0bea4fbe46f5d9550e3138718b4b8ec9c91feaa
qiao-lima <qiao-lima@github.com>
2026-05-26 22:59:24 +0800

    Manual Update 2752240

---
+++ 2752240.lua
@@ -0,0 +1,12 @@
+-- 2752240's Lua and Manifest Created by Hubcap Manifest
+-- Fine Work: Act I
+-- Created: May 26, 2026 at 07:43:36 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2752240, 1, "5d801a7754b41bcb24402d597050351c6a8f0026f7788522353f750c1ec0ef69") -- Fine Work: Act I
+-- MAIN APP DEPOTS
+addappid(2752241, 1, "53b5287ce90e9ca91e728a72b157be5ab561300fce7656c4ff09a3ac60c62451") -- Depot 2752241
+setManifestid(2752241, "2284430814336910040", 6188594546)
