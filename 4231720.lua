166775414ec67585405f7eecfc021d31bc4a5b72
qiao-lima <qiao-lima@github.com>
2026-05-26 11:59:37 +0800

    Manual Update 4231720

---
+++ 4231720.lua
@@ -0,0 +1,26 @@
+-- 4231720's Lua and Manifest Created by Hubcap Manifest
+-- Gardener's Luck
+-- Created: May 21, 2026 at 13:42:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 7
+-- Total DLCs: 0
+-- Shared Depots: 6
+
+-- MAIN APPLICATION
+addappid(4231720) -- Gardener's Luck
+-- MAIN APP DEPOTS
+addappid(4231721, 1, "280d9e9426286c57aa78bfee3841e3fba15dc12832fbdb1e7b263e42017f110c") -- Depot 4231721
+setManifestid(4231721, "8853273680374444604", 153913071)
+-- SHARED DEPOTS (from other apps)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (Shared from App 228980)
+setManifestid(228983, "8124929965194586177", 19265607)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- VC 2012 Redist (Shared from App 228980)
+setManifestid(228984, "2547553897526095397", 13742505)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- VC 2013 Redist (Shared from App 228980)
+setManifestid(228985, "3966345552745568756", 13699237)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") -- OpenAL 2.0.7.0 Redist (Shared from App 228980)
+setManifestid(229020, "5799761707845834510", 810085)
