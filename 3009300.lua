1443630229886bae74342d2d1d91e8c26c95597f
qiao-lima <qiao-lima@github.com>
2026-05-26 11:07:28 +0800

    Manual Update 3009300

---
+++ 3009300.lua
@@ -0,0 +1,12 @@
+-- 3009300's Lua and Manifest Created by Hubcap Manifest
+-- Teamfight Manager 2
+-- Created: May 25, 2026 at 20:10:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3009300) -- Teamfight Manager 2
+-- MAIN APP DEPOTS
+addappid(3009301, 1, "b23fe424cc9f269898756472a513ce0395b175590259861bb8a68429fdc53fd2") -- Depot 3009301
+setManifestid(3009301, "4564373287300291877", 1175569397)
