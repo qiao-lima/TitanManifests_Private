d168c315bdfeb2a2223b5254813666873f9bd61f
qiao-lima <qiao-lima@github.com>
2026-05-27 15:18:34 +0800

    Manual Update 3668560

---
+++ 3668560.lua
@@ -0,0 +1,23 @@
+-- 3668560's Lua and Manifest Created by Hubcap Manifest
+-- Happy Island Fantasy
+-- Created: May 25, 2026 at 08:06:36 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(3668560, 1, "b8ef045400b51d405e25654062870d7f44851d5fcbe1bc5c1e097c84215c4997") -- Happy Island Fantasy
+-- MAIN APP DEPOTS
+addappid(3668561, 1, "2fba95b2f05878acc5d5b57f1429cc236645a6aed513ae6d733ed589422235ec") -- Depot 3668561
+setManifestid(3668561, "662033717158621408", 8860491230)
+addappid(3668562, 1, "4e174fc55441d6c22cea85197ccc692e37ce48737a3cd8c00858a45439e02dbe") -- Depot 3668562
+setManifestid(3668562, "9199082076241038414", 9098210773)
+-- DLCS WITH DEDICATED DEPOTS
+-- Happy Island Fantasy - OliviaSophia (AppID: 4306470)
+addappid(4306470)
+addappid(4306470, 1, "91e409e2d7a38234508c772677400d2c3b4a9669b5e575ec2de3e00a04b836a7") -- Happy Island Fantasy - OliviaSophia - Depot 4306470
+setManifestid(4306470, "6867262603850589435", 1539)
+-- Happy Island Fantasy - Sexual Bliss (AppID: 4603500)
+addappid(4603500)
+addappid(4603500, 1, "3651a8c1f7e94be15db12a30553a774d8b526a7787544dce0e27cc2b651f94b2") -- Happy Island Fantasy - Sexual Bliss - Depot 4603500
+setManifestid(4603500, "7959890016306506922", 1546)
