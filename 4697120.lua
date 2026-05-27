ce24bd7745b12174acdd79d0538db500371f27c1
qiao-lima <qiao-lima@github.com>
2026-05-28 07:33:15 +0800

    Manual Update 4697120

---
+++ 4697120.lua
@@ -0,0 +1,14 @@
+-- 4697120's Lua and Manifest Created by Hubcap Manifest
+-- Hearts in Open Water
+-- Created: May 27, 2026 at 08:29:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4697120) -- Hearts in Open Water
+-- MAIN APP DEPOTS
+addappid(4697121, 1, "953c0f1d6ea2788a6134f3267c690b9d44a505351e44ecc1a51038196e6ec0e4") -- Depot 4697121
+setManifestid(4697121, "161547451713022932", 1253176360)
+addappid(4697122, 1, "ad2b6f5b14b5c1393d0606a07307f5d7bd7a0b900c07d220440559e36b66c18b") -- Depot 4697122
+setManifestid(4697122, "6641975992715070940", 1101333926)
