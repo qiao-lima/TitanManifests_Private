ec75979b4eeac4e9756bdbde56992f58ae1071dd
qiao-lima <qiao-lima@github.com>
2026-05-28 07:33:15 +0800

    Manual Update 4691650

---
+++ 4691650.lua
@@ -0,0 +1,18 @@
+-- 4691650's Lua and Manifest Created by Hubcap Manifest
+-- Hell Girl
+-- Created: May 27, 2026 at 05:53:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4691650) -- Hell Girl
+-- MAIN APP DEPOTS
+addappid(4691651, 1, "cabdaa94c5345362563a6e765331e24e7cb83142280d4238fa35a83ec5d115e4") -- Depot 4691651
+setManifestid(4691651, "4927548507261428761", 14527374200)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
