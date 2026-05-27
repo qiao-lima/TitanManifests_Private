fa8539bb730e5d2f23a80b24f12a07330c04f488
qiao-lima <qiao-lima@github.com>
2026-05-28 07:32:17 +0800

    Manual Update 2090390

---
+++ 2090390.lua
@@ -0,0 +1,12 @@
+-- 2090390's Lua and Manifest Created by Hubcap Manifest
+-- Schrödinger's Call
+-- Created: May 27, 2026 at 13:00:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2090390) -- Schrödinger's Call
+-- MAIN APP DEPOTS
+addappid(2090391, 1, "75acf6e2ba3f06b8950ce2148df1590982695f5bbb8a46ee83607eb1ad1c9a38") -- Depot 2090391
+setManifestid(2090391, "1146912156244640291", 4809860437)
