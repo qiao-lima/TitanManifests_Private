89a7e54daacffc39c289a7f8f8acfe6a15aa0b3b
qiao-lima <qiao-lima@github.com>
2026-05-27 15:18:34 +0800

    Manual Update 2857880

---
+++ 2857880.lua
@@ -0,0 +1,17 @@
+-- 2857880's Lua and Manifest Created by Hubcap Manifest
+-- Yakuza Rogue: Yokohama massage parlor chapter
+-- Created: May 25, 2026 at 07:32:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(2857880, 1, "8b7c11ab9584a550c048cb59a19e10a91201c3a4ed68c699d2103f2b1ece1046") -- Yakuza Rogue: Yokohama massage parlor chapter
+-- MAIN APP DEPOTS
+addappid(2857881, 1, "0e3aed59a84f0f573d8dde17640d405192be412ff8acd5121f9d435260394e04") -- Depot 2857881
+setManifestid(2857881, "4171387496846962596", 1161088201)
+-- DLCS WITH DEDICATED DEPOTS
+-- Yakuza Rogue-DLC1 (AppID: 3021070)
+addappid(3021070)
+addappid(3021070, 1, "c15898f5cb9817204c1ced116d37a58b054feca4371bd675c4cc81a832ab1ec2") -- Yakuza Rogue-DLC1 - Depot 3021070
+setManifestid(3021070, "9044974343654646778", 84260359)
