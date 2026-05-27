4ec04c9f58a0314b96514d30ad0084f0bffd7772
qiao-lima <qiao-lima@github.com>
2026-05-27 15:18:33 +0800

    Manual Update 2544720

---
+++ 2544720.lua
@@ -0,0 +1,39 @@
+-- 2544720's Lua and Manifest Created by Hubcap Manifest
+-- Workplace Fantasy
+-- Created: May 25, 2026 at 07:18:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 10
+-- Total DLCs: 4
+
+-- MAIN APPLICATION
+addappid(2544720, 1, "4cdf8c779f01c40a7a2ba4941b6e8e314b5aa3e477c258e46bf30cfeeb0b0b21") -- Workplace Fantasy
+-- MAIN APP DEPOTS
+addappid(2544721, 1, "8efd5e37dbb85b2b655f5332cb3c5060ffa02e17f594658dfb45a8fad670b98b") -- Depot 2544721
+setManifestid(2544721, "2221037705025236469", 5056396829)
+addappid(2544722, 1, "2cb439f5772acb927266fc571f420a59e5a7cdfd2dadb8e927588a990cfad5d7") -- Depot 2544722
+setManifestid(2544722, "8759634088052172378", 4299685641)
+-- DLCS WITH DEDICATED DEPOTS
+-- Workplace Fantasy - New Girl Chapter (AppID: 2626390)
+addappid(2626390)
+addappid(2626390, 1, "4f84453ae42b864391def14a64ea4d671247a7901028bfd862caa3696ef944e9") -- Workplace Fantasy - New Girl Chapter - Depot 2626390
+setManifestid(2626390, "2066296729126124092", 14822087)
+addappid(2626391, 1, "3046b08a55a7155ebd8a6c8cbf05532986541e8047e036e3edc96999e150fe2b") -- Workplace Fantasy - New Girl Chapter - Depot 2626391
+setManifestid(2626391, "516761692965800638", 14822072)
+-- Workplace Fantasy - Rainas ending (AppID: 2626400)
+addappid(2626400)
+addappid(2626400, 1, "9faaeab00864fc722e7ba01773578d2f988824fc3b82aad4aa798787b5bf5a0b") -- Workplace Fantasy - Rainas ending - Depot 2626400
+setManifestid(2626400, "464847531475260553", 1265)
+addappid(2626401, 1, "c03ef7081fd15b896e1ceb9fbde70da02699907326c7242c673919e64f95a0d7") -- Workplace Fantasy - Rainas ending - Depot 2626401
+setManifestid(2626401, "2761523799002420457", 1263)
+-- Workplace Fantasy - Anabellas ending (AppID: 3040550)
+addappid(3040550)
+addappid(3040550, 1, "dd8c2eb93a2af8a6460a19012bbe414f38f6fc8537de2f547b0cbdd43cbd974c") -- Workplace Fantasy - Anabellas ending - Depot 3040550
+setManifestid(3040550, "3317301064900056772", 1261)
+addappid(3040551, 1, "24bc6038a2653c9ea4687bb9eb89bdbf8c16f272959d1f1313a7ed92f4d2fdf9") -- Workplace Fantasy - Anabellas ending - Depot 3040551
+setManifestid(3040551, "7758864056058425119", 1259)
+-- Workplace Fantasy - New Girl Chapter 2 (AppID: 3606670)
+addappid(3606670)
+addappid(3606670, 1, "a2182366192e13278c19aede58968a3cc78d42adb068881e240ee1996fd40274") -- Workplace Fantasy - New Girl Chapter 2 - Depot 3606670
+setManifestid(3606670, "7414568202853787552", 1262)
+addappid(3606671, 1, "d25f7aa03ea9bf9b610ff1a809051bcb3bd3bb42c8ad0864ce3f1bf0cfb6eb1e") -- Workplace Fantasy - New Girl Chapter 2 - Depot 3606671
+setManifestid(3606671, "4541827185413840347", 1260)
