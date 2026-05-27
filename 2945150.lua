b16f4818fed7f0c3c9d082e0fe6b221dc961e916
qiao-lima <qiao-lima@github.com>
2026-05-27 14:09:28 +0800

    Manual Update 2945150

---
+++ 2945150.lua
@@ -0,0 +1,12 @@
+-- 2945150's Lua and Manifest Created by Hubcap Manifest
+-- Feline Forensics and the Meowseum Mystery
+-- Created: May 25, 2026 at 14:08:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2945150, 1, "296785f6538b069705d8c4910d744f0f70b5e4f8341d6f8f28839d7cac91d13e") -- Feline Forensics and the Meowseum Mystery
+-- MAIN APP DEPOTS
+addappid(2945151, 1, "b48630843d8394116f100ce7ab0fea9f06fa9c9107726b9ce4baeed02e555a2b") -- Depot 2945151
+setManifestid(2945151, "1024979383815921590", 3139094289)
