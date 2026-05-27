d361b9574ee02e8994013d16420c033b6b16f113
qiao-lima <qiao-lima@github.com>
2026-05-27 16:34:01 +0800

    Manual Update 2932150

---
+++ 2932150.lua
@@ -0,0 +1,24 @@
+-- 2932150's Lua and Manifest Created by Hubcap Manifest
+-- Life Below
+-- Created: May 26, 2026 at 13:23:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 2 (3 excluded)
+
+-- MAIN APPLICATION
+addappid(2932150) -- Life Below
+addtoken(2932150, "5910094996003344048")
+-- MAIN APP DEPOTS
+addappid(2932151, 1, "1bb778455d12189287bcc3ea536a24651f8ca4274cc1f74810d31a9e317cb6e7") -- Depot 2932151
+setManifestid(2932151, "5636633264292531817", 5301183555)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4684890) -- Life Below - Special Edition Wallpapers
+addappid(4684900) -- Life Below - Special Edition Sprites
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Life Below - Art Book (AppID: 4660330) - missing depot keys
+-- addappid(4660330)
+-- Life Below - WDC Supporter Pack (AppID: 4660340) - missing depot keys
+-- addappid(4660340)
+-- Life Below - Special Edition Upgrade (AppID: 4711690) - missing depot keys
+-- addappid(4711690)
