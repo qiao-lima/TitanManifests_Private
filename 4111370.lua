5c34e0ec27ff80cbe2d6fd004e87f3d00b9286c2
qiao-lima <qiao-lima@github.com>
2026-05-28 07:32:46 +0800

    Manual Update 4111370

---
+++ 4111370.lua
@@ -0,0 +1,16 @@
+-- 4111370's Lua and Manifest Created by Hubcap Manifest
+-- Birdwatching Notebook
+-- Created: May 27, 2026 at 17:57:19 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4111370) -- Birdwatching Notebook
+-- MAIN APP DEPOTS
+addappid(4111371, 1, "258de01e34d9e9d177ee225ff6f340dd6989df5232a3899e9746873557f2a0a3") -- Depot 4111371
+setManifestid(4111371, "4672253786680517708", 1229704138)
+addappid(4111372, 1, "fe103cf09e1761983bd52cddc7307e21ee26230a3eba9f2b9a92c67bd3e66630") -- Depot 4111372
+setManifestid(4111372, "391138192446310879", 1254217798)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4687630) -- Birdwatching Notebook - Support Pack
