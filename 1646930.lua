e3b78f0ea6ca83d31574987142435d382b2cdc1c
qiao-lima <qiao-lima@github.com>
2026-05-28 07:32:16 +0800

    Manual Update 1646930

---
+++ 1646930.lua
@@ -0,0 +1,12 @@
+-- 1646930's Lua and Manifest Created by Hubcap Manifest
+-- The Feeble Files
+-- Created: May 27, 2026 at 10:50:36 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1646930) -- The Feeble Files
+-- MAIN APP DEPOTS
+addappid(1646931, 1, "4552489b6ca1b7cf403ef9df33ab52693747b5275336dc64234ca8e2ae591125") -- The Feeble Files English
+setManifestid(1646931, "1706254200893536628", 2891393178)
