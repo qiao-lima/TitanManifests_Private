e671223bb01f74a50eb0b44ed69f285eb1904d47
qiao-lima <qiao-lima@github.com>
2026-05-28 07:32:46 +0800

    Manual Update 4178920

---
+++ 4178920.lua
@@ -0,0 +1,14 @@
+-- 4178920's Lua and Manifest Created by Hubcap Manifest
+-- My Dorm - Season 2
+-- Created: May 27, 2026 at 05:55:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4178920) -- My Dorm - Season 2
+-- MAIN APP DEPOTS
+addappid(4178921, 1, "cdeb07a0aaba01d542ad2c2fcfdf93b9685e66718492fd910c9594a5044f4480") -- Depot 4178921
+setManifestid(4178921, "107162711882579414", 5251304221)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4178922) -- Depot 4178922 (empty depot)
