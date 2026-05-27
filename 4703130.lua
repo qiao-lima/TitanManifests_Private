d21bba5555e8d5707559720f063497a77f7ea66b
qiao-lima <qiao-lima@github.com>
2026-05-28 07:33:16 +0800

    Manual Update 4703130

---
+++ 4703130.lua
@@ -0,0 +1,12 @@
+-- 4703130's Lua and Manifest Created by Hubcap Manifest
+-- 巴比伦:人与人之战
+-- Created: May 27, 2026 at 07:52:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4703130) -- 巴比伦:人与人之战
+-- MAIN APP DEPOTS
+addappid(4703131, 1, "7de7fafb93dadaf74eaa09a7acb299e690e1c02bc9e7143566425a0c27816e9d") -- Depot 4703131
+setManifestid(4703131, "7874089764558714059", 1824921384)
