333ad036fb20953b8b7a7c6654f997486848ae8d
qiao-lima <qiao-lima@github.com>
2026-05-28 07:32:44 +0800

    Manual Update 4004830

---
+++ 4004830.lua
@@ -0,0 +1,18 @@
+-- 4004830's Lua and Manifest Created by Hubcap Manifest
+-- They Left Us Here
+-- Created: May 27, 2026 at 09:45:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4004830, 1, "0bf973c94ff5ebd5456c6fbb3cde29488569ef5c2b3c18c406663271b3c161c2") -- They Left Us Here
+-- MAIN APP DEPOTS
+addappid(4004831, 1, "df66c088a450becf4e4d77f4552a2b659d12080a6461930aebed1cd1a68ce035") -- Depot 4004831
+setManifestid(4004831, "2523982213049837690", 5259256052)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
