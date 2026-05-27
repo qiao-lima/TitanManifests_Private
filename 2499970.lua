16e7f8c9b3b10c4553a7198a6c32d9c6a35e31bf
qiao-lima <qiao-lima@github.com>
2026-05-27 16:34:00 +0800

    Manual Update 2499970

---
+++ 2499970.lua
@@ -0,0 +1,12 @@
+-- 2499970's Lua and Manifest Created by Hubcap Manifest
+-- Detective Montgomery Fox 3: The Revenge of Victor Draven
+-- Created: February 08, 2026 at 13:17:21 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2499970) -- Detective Montgomery Fox 3: The Revenge of Victor Draven
+-- MAIN APP DEPOTS
+addappid(2499971, 1, "be820e1e40d6fef94f62eb07f21a7a574de99c8eec8489da41e37949d30283d3") -- Depot 2499971
+setManifestid(2499971, "5547310530859233685", 465865028)
