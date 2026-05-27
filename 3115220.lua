da88a43bc6ff5dc68d896bb1b82ed676f6ddc556
qiao-lima <qiao-lima@github.com>
2026-05-27 16:34:03 +0800

    Manual Update 3115220

---
+++ 3115220.lua
@@ -0,0 +1,23 @@
+-- 3115220's Lua and Manifest Created by Hubcap Manifest
+-- Town To City
+-- Created: May 26, 2026 at 11:44:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3115220, 1, "2dca5bdc5b7be5a765eaa8693ccc21391d40685099f898aa747fcc728d4f9279") -- Town To City
+-- MAIN APP DEPOTS
+addappid(3115221, 1, "2bee0d6f4424d4ab2e390fe00b111e2c779f83a34f8a9fd16984e6edba09f8be") -- Depot 3115221
+setManifestid(3115221, "851550512850797242", 2343948971)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Town to City Digital Artbook (AppID: 3873380)
+addappid(3873380)
+addappid(3873380, 1, "0d30c479951e8bf4f4f5f50e5c9412f92f4608184fb459a04871939c13067219") -- Town to City Digital Artbook - Depot 3873380
+setManifestid(3873380, "8326927651341020770", 61710052)
