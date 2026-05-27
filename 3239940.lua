3f6a4548a0a759f7fd598ec8de8a95780996f20e
qiao-lima <qiao-lima@github.com>
2026-05-28 07:32:21 +0800

    Manual Update 3239940

---
+++ 3239940.lua
@@ -0,0 +1,12 @@
+-- 3239940's Lua and Manifest Created by Hubcap Manifest
+-- Mesoké
+-- Created: May 27, 2026 at 05:53:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3239940) -- Mesoké
+-- MAIN APP DEPOTS
+addappid(3239941, 1, "22b6d046f69ad53e195745bd8a614bd428aa815901473132e94cde1cfb2082f6") -- Depot 3239941
+setManifestid(3239941, "2818413709331815704", 3334714554)
