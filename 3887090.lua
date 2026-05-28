789a4c35fe69f415305a05b9d05e440a1f1df8a9
qiao-lima <qiao-lima@github.com>
2026-05-29 00:46:12 +0800

    Manual Update 3887090

---
+++ 3887090.lua
@@ -0,0 +1,18 @@
+-- 3887090's Lua and Manifest Created by Hubcap Manifest
+-- Airport Baggage Simulator
+-- Created: May 28, 2026 at 09:20:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(3887090) -- Airport Baggage Simulator
+-- MAIN APP DEPOTS
+addappid(3887091, 1, "95576b6e77d5133dd1e65650c37f627fe6f0ef43d37133a872bea7ea9af2cce4") -- Depot 3887091
+setManifestid(3887091, "3085312548684146160", 6245176395)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4659740) -- Airport Baggage Simulator - Airport Code Quiz
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Airport Baggage Simulator - The Official Book (AppID: 4733940) - missing depot keys
+-- addappid(4733940)
