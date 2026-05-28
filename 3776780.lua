d2842bd83e4804da3d6b20a9790c5677d7ab9af0
qiao-lima <qiao-lima@github.com>
2026-05-28 14:26:38 +0800

    Manual Update 3776780

---
+++ 3776780.lua
@@ -0,0 +1,14 @@
+-- 3776780's Lua and Manifest Created by Hubcap Manifest
+-- EmyLiveShow: Beach Days of Sun and Moon
+-- Created: May 26, 2026 at 01:41:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3776780, 1, "479edfec401ecfef64f840f4d6f1db7c10ec281f5491584ed856ef2d3e42856e") -- EmyLiveShow: Beach Days of Sun and Moon
+-- MAIN APP DEPOTS
+addappid(3776781, 1, "d8644cdf798075acb69a3deb7853fd65692c2644d1a601347faa8316e380bbce") -- Depot 3776781
+setManifestid(3776781, "8697822962539987727", 2715236417)
+addappid(3776782, 1, "04a0372f808268fccba840299cfcfd140d0f52b0f502881e199ec73c6d6c61e4") -- Depot 3776782
+setManifestid(3776782, "8607713310375527017", 2784018465)
