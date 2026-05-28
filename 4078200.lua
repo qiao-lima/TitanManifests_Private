503ef11ac3b30ac45ad5ab369fd763503f539bbd
qiao-lima <qiao-lima@github.com>
2026-05-29 00:49:42 +0800

    Manual Update 4078200

---
+++ 4078200.lua
@@ -0,0 +1,16 @@
+-- 4078200's Lua and Manifest Created by Hubcap Manifest
+-- Dwarf Eats Mountain
+-- Created: May 24, 2026 at 03:19:37 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4078200) -- Dwarf Eats Mountain
+-- MAIN APP DEPOTS
+addappid(4078201, 1, "92bb914bf6864cd05a4afd5bd5a3c9e478625d1bd308f34d2bc585d1a08761b9") -- Depot 4078201
+setManifestid(4078201, "493534072550761756", 136647950)
+addappid(4078202, 1, "82e44f3409535248f2d90f41501628a2feea2ac1b2aa46698db12f5c58d59e2e") -- Depot 4078202
+setManifestid(4078202, "2463416770765380135", 140075228)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4732170) -- Dwarf Eats Mountain - Supporter Pack
