69eff72ed2c3a765306cf5d48c592fd2f627e233
qiao-lima <qiao-lima@github.com>
2026-05-29 00:52:34 +0800

    Manual Update 4719930

---
+++ 4719930.lua
@@ -0,0 +1,21 @@
+-- 4719930's Lua and Manifest Created by Hubcap Manifest
+-- Aethelgard: Galactic Guardian
+-- Created: May 28, 2026 at 12:21:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 3 (3 excluded)
+
+-- MAIN APPLICATION
+addappid(4719930) -- Aethelgard: Galactic Guardian
+-- MAIN APP DEPOTS
+addappid(4719931, 1, "e9e7407c05e4b493a2f1acc039d74fb69b26ea57baa08b99172ecbd3da1d8f82") -- Depot 4719931
+setManifestid(4719931, "6921503756335552889", 1645494536)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4724550) -- Aethelgard - Credit Boost (50)
+addappid(4724560) -- Aethelgard - XP Boost (2x)
+addappid(4724570) -- Aethelgard - Damage Boost (50)
+-- EXCLUDED DLCS:
+-- UNRELEASED DLCS (COMMENTED OUT)
+-- addappid(4724490) -- Aethelgard - 1,000 Astralite Pack (unreleased)
+-- addappid(4724530) -- Aethelgard - 5,500 Astralite Pack (unreleased)
+-- addappid(4724540) -- Aethelgard - 12,000 Astralite Pack (unreleased)
