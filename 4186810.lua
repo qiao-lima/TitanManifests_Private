f78bd4c0b66b5561ce785c233ddaa359aef4a1bc
qiao-lima <qiao-lima@github.com>
2026-05-29 00:49:44 +0800

    Manual Update 4186810

---
+++ 4186810.lua
@@ -0,0 +1,12 @@
+-- 4186810's Lua and Manifest Created by Hubcap Manifest
+-- Darkest Messia
+-- Created: May 28, 2026 at 05:57:36 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4186810) -- Darkest Messia
+-- MAIN APP DEPOTS
+addappid(4186811, 1, "8378125bb9a2ed71298fe91b30212f44f8d9ab26889e2e433127bf507bb1e2c2") -- Depot 4186811
+setManifestid(4186811, "588102133518322770", 12685733753)
