6373c15b1672ca4103c1e758a7470739aed2e539
qiao-lima <qiao-lima@github.com>
2026-05-29 00:41:25 +0800

    Manual Update 3176190

---
+++ 3176190.lua
@@ -0,0 +1,16 @@
+-- 3176190's Lua and Manifest Created by Hubcap Manifest
+-- Sips and Sonnets
+-- Created: May 28, 2026 at 10:58:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3176190, 1, "dea558f974e87fe658dd973b5cb7ff565f016d3ebe828e3cb52eb6b43b50de43") -- Sips and Sonnets
+-- MAIN APP DEPOTS
+addappid(3176191, 1, "38565a7729f850b2221b739cd32e4cb7abc1b6f98845444defaf505f138cd272") -- Depot 3176191
+setManifestid(3176191, "6079502640619704701", 474764655)
+addappid(3176192, 1, "f6c82c1bac073fa67f981b233539bc414d03eb8cde0e3b6dddb6b6d5e348cc21") -- Depot 3176192
+setManifestid(3176192, "2189886758729370405", 494717767)
+addappid(3176193, 1, "3ddc300d9c1892a5a46d5f0b51da27ec11e6e5c2ae3ff2870281cf6c55338d9f") -- Depot 3176193
+setManifestid(3176193, "5795367115394831510", 464184585)
