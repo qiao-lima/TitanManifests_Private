0dc9fb6249fa447249b4a1540b4e53617bf40e26
qiao-lima <qiao-lima@github.com>
2026-05-29 00:46:11 +0800

    Manual Update 3856040

---
+++ 3856040.lua
@@ -0,0 +1,12 @@
+-- 3856040's Lua and Manifest Created by Hubcap Manifest
+-- Just Move Fall Dungeon Endless Abyss
+-- Created: May 28, 2026 at 09:37:57 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3856040) -- Just Move Fall Dungeon Endless Abyss
+-- MAIN APP DEPOTS
+addappid(3856041, 1, "73eb39cd2ebccd9f31719847bef74f6318beebd885879b9007391f0ae48e9c31") -- Depot 3856041
+setManifestid(3856041, "6969619341821764520", 685700181)
