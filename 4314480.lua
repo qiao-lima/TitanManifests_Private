7841355759b2a0879252cb35a28c684815dde082
qiao-lima <qiao-lima@github.com>
2026-05-28 14:26:39 +0800

    Manual Update 4314480

---
+++ 4314480.lua
@@ -0,0 +1,12 @@
+-- 4314480's Lua and Manifest Created by Hubcap Manifest
+-- Havenfall is For Lovers
+-- Created: May 28, 2026 at 00:29:18 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4314480) -- Havenfall is For Lovers
+-- MAIN APP DEPOTS
+addappid(4314481, 1, "c029838a37e0180a395c3679f090eac2dfd4594b6420c3576ec282d73961f6e6") -- Depot 4314481
+setManifestid(4314481, "8653212103055606391", 1914702886)
