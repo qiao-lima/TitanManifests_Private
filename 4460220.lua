d6c63697ad0f452e28e3ad8eb2db41276813fde2
qiao-lima <qiao-lima@github.com>
2026-05-28 14:26:40 +0800

    Manual Update 4460220

---
+++ 4460220.lua
@@ -0,0 +1,12 @@
+-- 4460220's Lua and Manifest Created by Hubcap Manifest
+-- Secret Sexy Streamer
+-- Created: May 23, 2026 at 05:09:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4460220) -- Secret Sexy Streamer
+-- MAIN APP DEPOTS
+addappid(4460221, 1, "610010da1b4b8adaaf6135847c047a2e86ebf1e6a78bbee0b3ec92a690606c96") -- Depot 4460221
+setManifestid(4460221, "1820935121296918112", 2034916767)
