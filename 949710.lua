2986be53326379ee36868db4cee2a613e8b1cdb1
qiao-lima <qiao-lima@github.com>
2026-05-29 00:52:35 +0800

    Manual Update 949710

---
+++ 949710.lua
@@ -0,0 +1,14 @@
+-- 949710's Lua and Manifest Created by Hubcap Manifest
+-- Exiled to the Void
+-- Created: May 28, 2026 at 08:46:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(949710) -- Exiled to the Void
+-- MAIN APP DEPOTS
+addappid(949711, 1, "b95bc062a3bd8905d569f40be3d36a9eb0dc7f689a2b9b2e97035f82cce28a1d") -- Exiled to the Void Win
+setManifestid(949711, "6217709505824142325", 204368563)
+addappid(949712, 1, "3b0bec1f0ecfb354f6c93165bdfb48220ebf514f9b9b22fa85d54b334e97c7d6") -- Exiled to the Void Mac
+setManifestid(949712, "7302998178002311146", 223164689)
