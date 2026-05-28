c1481d3119799f06e0adc7fbca40b0bfe85261b5
qiao-lima <qiao-lima@github.com>
2026-05-29 00:41:23 +0800

    Manual Update 2904040

---
+++ 2904040.lua
@@ -0,0 +1,12 @@
+-- 2904040's Lua and Manifest Created by Hubcap Manifest
+-- Cheap Car Repair
+-- Created: May 28, 2026 at 10:13:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2904040) -- Cheap Car Repair
+-- MAIN APP DEPOTS
+addappid(2904041, 1, "5f2fc48115edd9e7bfc1546f9dc43c8d900013aaf3d4f5035fb69ff28899f050") -- Depot 2904041
+setManifestid(2904041, "875396870397667654", 5301082419)
