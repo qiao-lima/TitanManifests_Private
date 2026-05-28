176a53b67a2cba1532ef98041f2fee69e250c916
qiao-lima <qiao-lima@github.com>
2026-05-29 00:46:09 +0800

    Manual Update 3549690

---
+++ 3549690.lua
@@ -0,0 +1,12 @@
+-- 3549690's Lua and Manifest Created by Hubcap Manifest
+-- Moondusk Masquerade
+-- Created: May 28, 2026 at 05:09:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3549690) -- Moondusk Masquerade
+-- MAIN APP DEPOTS
+addappid(3549691, 1, "2f2c77ba90938551554eccd9223ddef33dceccda2c0febfe45f6ca1cb01f7fbc") -- Depot 3549691
+setManifestid(3549691, "2049775896913189779", 452252438)
