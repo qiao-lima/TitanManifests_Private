40312d74582ae68d43df4d3b8a815652cc01fd4c
qiao-lima <qiao-lima@github.com>
2026-05-28 14:26:39 +0800

    Manual Update 4038160

---
+++ 4038160.lua
@@ -0,0 +1,18 @@
+-- 4038160's Lua and Manifest Created by Hubcap Manifest
+-- Fpv Labs - Drone Simulator
+-- Created: May 27, 2026 at 18:29:06 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4038160) -- Fpv Labs - Drone Simulator
+-- MAIN APP DEPOTS
+addappid(4038161, 1, "ed5ec986503936b92b0e159e994881ac416300ca8e8fe4e63a4d30aa4154e1fa") -- Depot 4038161
+setManifestid(4038161, "8037002054808009046", 7161972482)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
