f437496e83339ef9cb27059bfaa8dac4908870fe
qiao-lima <qiao-lima@github.com>
2026-05-29 00:46:07 +0800

    Manual Update 3367520

---
+++ 3367520.lua
@@ -0,0 +1,14 @@
+-- 3367520's Lua and Manifest Created by Hubcap Manifest
+-- Wrath and Retribution
+-- Created: May 28, 2026 at 11:06:18 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3367520, 1, "c12f6c21d8511a6cdeaae391265c1b9c74b57d7cd2ddb218e4bcb0ad4a82d52b") -- Wrath and Retribution
+-- MAIN APP DEPOTS
+addappid(3367522, 1, "00cbd5580c90ba4bee812f5b4de9f697f3d96d9cda5a7b01504e8747b07aaae1") -- Depot 3367522
+setManifestid(3367522, "6226718056540850905", 5992756430)
+addappid(3367523, 1, "242d379d9c076dfe341783a238ad94342717ece92b1daf1eb111f5dc801eea33") -- Depot 3367523
+setManifestid(3367523, "9097911551403191967", 6091736635)
