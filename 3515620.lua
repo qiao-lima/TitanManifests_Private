4fab5e2969c9a3efa0d0f5fdac6e60edb5175e0a
qiao-lima <qiao-lima@github.com>
2026-05-29 00:46:09 +0800

    Manual Update 3515620

---
+++ 3515620.lua
@@ -0,0 +1,18 @@
+-- 3515620's Lua and Manifest Created by Hubcap Manifest
+-- Bunny Hurling
+-- Created: May 28, 2026 at 09:37:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3515620) -- Bunny Hurling
+-- MAIN APP DEPOTS
+addappid(3515621, 1, "3947a8245bc1833af7e27ab1ad09081ea8995962b5358ac968df3d8453e07740") -- Depot 3515621
+setManifestid(3515621, "895070919881286003", 1506442408)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
