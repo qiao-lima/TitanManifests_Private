0514c442b00152cddd974ed572ef8cde060a8e06
qiao-lima <qiao-lima@github.com>
2026-05-29 00:41:22 +0800

    Manual Update 265670

---
+++ 265670.lua
@@ -0,0 +1,22 @@
+-- 265670's Lua and Manifest Created by Hubcap Manifest
+-- Imagine Me
+-- Created: May 28, 2026 at 08:46:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(265670, 1, "625f94fbfdede0655d6fdb1b7a89b3bb1be685ad9b180b7fba787cc6ead5aa67") -- Imagine Me
+-- MAIN APP DEPOTS
+addappid(265672, 1, "8da77e8bdaaf2fd1ed6ab9625a50de7082fe7287fcec9c886c8724039df89bfa") -- Imagine Me Mac
+setManifestid(265672, "8858435713449479753", 1486114091)
+addappid(265674, 1, "e3d59fdaf63af3d56bc6b349135d76a633959c267ecf3a7f9350b2aaa33da3e8") -- Imagine Me Windows
+setManifestid(265674, "4662511354037729554", 1477925795)
+addappid(265671, 1, "51758780230eeeef35691d508112690c4ce8ba146760354e07856eb15d3eda23") -- Imagine Me Linux
+setManifestid(265671, "4575687613077037247", 1484333284)
+-- DLCS WITH DEDICATED DEPOTS
+-- Imagine Me Original Soundtrack (AppID: 327080)
+addappid(327080)
+addtoken(327080, "10196603649990743697")
+addappid(327080, 1, "6fc8b3c047d6d5b2d725c53618c86cb6c8eb1bf85e6187282a11648b0baa3dec") -- Imagine Me Original Soundtrack - Imagine Me Original Soundtrack (327080) Depot
+setManifestid(327080, "910278430803512064", 11722774)
