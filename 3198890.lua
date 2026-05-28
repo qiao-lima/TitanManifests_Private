a7ba38f473bcd2c4301ee6fd8bb717ba7072dd41
qiao-lima <qiao-lima@github.com>
2026-05-29 00:46:05 +0800

    Manual Update 3198890

---
+++ 3198890.lua
@@ -0,0 +1,12 @@
+-- 3198890's Lua and Manifest Created by Hubcap Manifest
+-- Scale the Depths
+-- Created: May 28, 2026 at 09:21:57 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3198890) -- Scale the Depths
+-- MAIN APP DEPOTS
+addappid(3198891, 1, "51f594d6d9bf65f6fc859dd65ab3da9bc1131f58eb6a910625b2e7d42f190271") -- Depot 3198891
+setManifestid(3198891, "4881793719175208032", 2682350349)
