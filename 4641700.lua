41bee8ea86e6a2ca4e381fafa5a68c1d495ba7d0
qiao-lima <qiao-lima@github.com>
2026-05-29 13:14:03 +0800

    Manual Update 4641700

---
+++ 4641700.lua
@@ -0,0 +1,15 @@
+-- 4641700's Lua and Manifest Created by Hubcap Manifest
+-- Sill Cats 
+-- Created: May 28, 2026 at 15:58:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(4641700) -- Sill Cats 
+-- MAIN APP DEPOTS
+addappid(4641701, 1, "d0d99ada2581d5e0b9becb11787c5df83503e044bb1d68cc36ac0c6df22bdfef") -- Depot 4641701
+setManifestid(4641701, "2737782576989401654", 399380704)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4641710) -- Sill Cats - Cat Pack 1
+addappid(4641720) -- Sill Cats - Cat Pack 2
