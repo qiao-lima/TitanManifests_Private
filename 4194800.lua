2641b582b03c8cc8ad9c3207ee713a9fd35041d7
qiao-lima <qiao-lima@github.com>
2026-05-30 02:14:16 +0800

    Manual Update 4194800

---
+++ 4194800.lua
@@ -0,0 +1,14 @@
+-- 4194800's Lua and Manifest Created by Hubcap Manifest
+-- Starvester
+-- Created: May 29, 2026 at 13:16:03 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4194800) -- Starvester
+-- MAIN APP DEPOTS
+addappid(4194801, 1, "82294b7c6aba9874f3e2f742a0b1ec18a1835eaab8ff8a6665f6bbc70e12b706") -- Depot 4194801
+setManifestid(4194801, "1898772981231351347", 64170903)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4759650) -- Starvester - Supporter Pack
