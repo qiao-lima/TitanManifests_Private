c55a397aea001b8fb9c90e1d5c61fc411a11069b
qiao-lima <qiao-lima@github.com>
2026-05-30 01:58:41 +0800

    Manual Update 4452480

---
+++ 4452480.lua
@@ -0,0 +1,12 @@
+-- 4452480's Lua and Manifest Created by Hubcap Manifest
+-- Caldra
+-- Created: May 29, 2026 at 03:03:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4452480) -- Caldra
+-- MAIN APP DEPOTS
+addappid(4452481, 1, "0fe2befbb629e79c32e4bb97a8a785bd836a746ee39e533fb0ff951def8d593c") -- Depot 4452481
+setManifestid(4452481, "4357200579512063469", 1773475900)
