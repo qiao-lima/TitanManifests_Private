ca1a739b2a77119796acbd82f9a377610d519f53
qiao-lima <qiao-lima@github.com>
2026-05-29 15:52:32 +0800

    Manual Update 2964960

---
+++ 2964960.lua
@@ -0,0 +1,23 @@
+-- 2964960's Lua and Manifest Created by Hubcap Manifest
+-- 52赫兹
+-- Created: February 10, 2026 at 21:28:58 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2964960) -- 52赫兹
+-- MAIN APP DEPOTS
+addappid(2964961, 1, "f7464ac19ed0b94f0b97980d137c8d215bd7119e54ffa1e8d86bac67299a31bb") -- Depot 2964961
+setManifestid(2964961, "4168943857140671462", 16231046766)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- 52Hertz Artbook (AppID: 4282550)
+addappid(4282550)
+addappid(4282551, 1, "78d0174d3395a16db55567835bb2df4df14551d97f28e533340b8a00b96a88f7") -- 52Hertz Artbook - Depot 4282551
+setManifestid(4282551, "377866843185882392", 657551100)
