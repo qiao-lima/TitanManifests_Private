5649a82bdb4f1e539ac42e711b1dc14b41c0cd65
qiao-lima <qiao-lima@github.com>
2026-05-29 15:52:31 +0800

    Manual Update 1875580

---
+++ 1875580.lua
@@ -0,0 +1,18 @@
+-- 1875580's Lua and Manifest Created by Hubcap Manifest
+-- Mina the Hollower
+-- Created: May 29, 2026 at 00:07:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1875580) -- Mina the Hollower
+-- MAIN APP DEPOTS
+addappid(1875581, 1, "4b564e5c98b9b3813becf9cce3759f649a5c9f1a912f49aa1b003e4378674428") -- Depot 1875581
+setManifestid(1875581, "1445772993091680244", 856882186)
+addappid(1875582, 1, "413c4c1b115d4862272593db196e15a8a03b03761b8d10ba98fa2b4ebb68a196") -- Depot 1875582
+setManifestid(1875582, "8011602359793183891", 26108320)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
