38f79ff9e18ca5586fdee03a1cdd8cf12577cd92
qiao-lima <qiao-lima@github.com>
2026-05-29 15:34:11 +0800

    Manual Update 2290180

---
+++ 2290180.lua
@@ -0,0 +1,50 @@
+-- 2290180's Lua and Manifest Created by Hubcap Manifest
+-- Riders Republic
+-- Created: May 21, 2026 at 10:23:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 29
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(2290180, 1, "f4dc4dfa72e4a27abc58f753127c9574426a2ea4c2daeede7a14338535f6df5d") -- Riders Republic
+-- MAIN APP DEPOTS
+addappid(2290181, 1, "87bb928cf12682614e38b33c1fdbcfe71c6473b5bad0c278583e2478ccf34f36") -- Depot 2290181
+setManifestid(2290181, "2820170942921901271", 31708116002)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") -- Ubisoft Connect PC Client Content (Shared from App 1716750)
+setManifestid(1716751, "1719625392615074191", 264771328)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2389390) -- Riders Republic - Deluxe Edition Ubisoft Activation
+addappid(2389391) -- Riders Republic - Gold Edition Ubisoft Activation
+addappid(2389392) -- Riders Republic - Ultimate Edition Ubisoft Activation
+addappid(2389393) -- Riders Republic Season 6 Welcome Pack
+addappid(2389394) -- Riders Republic Season 6 Welcome Pack  Ubisoft Activation
+addappid(2400940) -- Riders Republic Ubisoft Activation
+addappid(2466100) -- Riders Republic - Year One Pass
+addappid(2466110) -- Riders Republic - Year One Pass Ubisoft Activation
+addappid(2468280) -- Riders Republic Season 7 Welcome Pack
+addappid(2468281) -- Riders Republic Season 7 Welcome Pack Ubisoft Activation
+addappid(2550510) -- Riders Republic Season 8 Welcome Pack
+addappid(2550520) -- Riders Republic Season 8 Welcome Pack Ubisoft Activation
+addappid(2550540) -- Riders Republic Skate Edition Ubisoft Activation
+addappid(2550550) -- Riders Republic 360 Edition Ubisoft Activation
+addappid(2550560) -- Riders Republic Complete Edition Ubisoft Activation
+addappid(2605010) -- Riders Republic Skate Plus Pack
+addappid(2605020) -- Riders Republic Skate Plus Pack Ubisoft Activation
+addappid(2735800) -- Riders Republic Season 9 Welcome Pack
+addappid(2735810) -- Riders Republic Season 9 Welcome Pack Ubisoft Activation
+addappid(4508860) -- Riders Republic Deluxe Edition 2026 - Ubisoft Activation
+addappid(4508870) -- Riders Republic Premium Edition 2026 - Ubisoft Activation
+addappid(4508890) -- Riders Republic Careers Pack
+addappid(4508900) -- Riders Republic Frenzy Pack
+addappid(4508910) -- Riders Republic X-TREME Board Pack
+addappid(4508920) -- Riders Republic Motor Pack
+addappid(4508930) -- Riders Republic - Careers Pack - Ubisoft Activation
+addappid(4508940) -- Riders Republic - Frenzy Pack - Ubisoft Activation
+addappid(4508950) -- Riders Republic - Xtreme Pack - Ubisoft Activation
+addappid(4508960) -- Riders Republic - Motor Pack - Ubisoft Activation
