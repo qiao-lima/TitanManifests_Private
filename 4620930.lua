039bd57f5a700e7a791f681d1576959dd0b4e34f
qiao-lima <qiao-lima@github.com>
2026-05-29 09:54:33 +0800

    Manual Update 4620930

---
+++ 4620930.lua
@@ -0,0 +1,12 @@
+-- 4620930's Lua and Manifest Created by Hubcap Manifest
+-- Ghost of Tokyo
+-- Created: May 28, 2026 at 10:28:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4620930) -- Ghost of Tokyo
+-- MAIN APP DEPOTS
+addappid(4620931, 1, "06978ae0a33ba11984816885d91fc7a73ddc56ad542de5a17c4bd261d6c4c7b6") -- Depot 4620931
+setManifestid(4620931, "6046218456228477274", 1121604668)
