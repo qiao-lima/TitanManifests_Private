4fa75493b0f38a8b8041ca0150f9f100948bda55
qiao-lima <qiao-lima@github.com>
2026-05-29 13:14:03 +0800

    Manual Update 4687470

---
+++ 4687470.lua
@@ -0,0 +1,12 @@
+-- 4687470's Lua and Manifest Created by Hubcap Manifest
+-- 1.76复古沉默
+-- Created: May 28, 2026 at 23:01:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4687470) -- 1.76复古沉默
+-- MAIN APP DEPOTS
+addappid(4687471, 1, "64f6f811fe86a6003b8757fbb2d2c244da0bd4245fcfe87237652446ca3b56b1") -- Depot 4687471
+setManifestid(4687471, "5245347314727765060", 57863679)
