c811a0e3658fcbbd6d284a97353adad7ffef8332
qiao-lima <qiao-lima@github.com>
2026-05-30 01:58:16 +0800

    Manual Update 3550880

---
+++ 3550880.lua
@@ -0,0 +1,12 @@
+-- 3550880's Lua and Manifest Created by Hubcap Manifest
+-- 武道残编
+-- Created: May 29, 2026 at 02:23:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3550880) -- 武道残编
+-- MAIN APP DEPOTS
+addappid(3550882, 1, "ae35eb522cbd148e4de6256f2c2bbdb75d6b6e00fcf28ceb38c44382d75e22f1") -- Depot 3550882
+setManifestid(3550882, "4480091534908349680", 753419077)
