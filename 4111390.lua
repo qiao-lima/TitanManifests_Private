6accbc6a39fee376b745d3b474ed9a01309572c0
qiao-lima <qiao-lima@github.com>
2026-05-29 13:13:57 +0800

    Manual Update 4111390

---
+++ 4111390.lua
@@ -0,0 +1,12 @@
+-- 4111390's Lua and Manifest Created by Hubcap Manifest
+-- Nickelodeon Extreme Tennis : Next!
+-- Created: May 28, 2026 at 15:59:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4111390) -- Nickelodeon Extreme Tennis : Next!
+-- MAIN APP DEPOTS
+addappid(4111391, 1, "342e53e5854a01fa52ec234f80c619a76394095c51fbf08d88541c40f08ff1a1") -- Depot 4111391
+setManifestid(4111391, "942497860384000404", 3785086082)
