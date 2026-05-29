f43dbfcfd273d090ebf710870fa6cf940b2093b6
qiao-lima <qiao-lima@github.com>
2026-05-30 02:14:16 +0800

    Manual Update 4384080

---
+++ 4384080.lua
@@ -0,0 +1,14 @@
+-- 4384080's Lua and Manifest Created by Hubcap Manifest
+-- Life is Vanilla
+-- Created: May 29, 2026 at 08:36:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4384080) -- Life is Vanilla
+-- MAIN APP DEPOTS
+addappid(4384082, 1, "abf6a33f1d95218206af1857ed4940d74ad563723d1780b9fb7862e559004d81") -- Depot 4384082
+setManifestid(4384082, "9153998342168239430", 9928689538)
+addappid(4384084, 1, "7ac92e187da522cb56981911ddd74224325fff096dc10388caf6e80e26ff47e3") -- Depot 4384084
+setManifestid(4384084, "3186260461039742542", 9911699325)
