43a649d5ffb971e1c9d7127be813af27f304ecf9
qiao-lima <qiao-lima@github.com>
2026-05-29 13:10:06 +0800

    Manual Update 3561030

---
+++ 3561030.lua
@@ -0,0 +1,12 @@
+-- 3561030's Lua and Manifest Created by Hubcap Manifest
+-- Rock Island
+-- Created: May 28, 2026 at 22:41:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3561030) -- Rock Island
+-- MAIN APP DEPOTS
+addappid(3561031, 1, "585d80c13b1651760f01bf703fbd43bb141f0dde23a791703dcb619b5fc52027") -- Depot 3561031
+setManifestid(3561031, "9132784250689772657", 339307245)
