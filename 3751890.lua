a99413f68bd2ec1c1c3a86743d706e54299a2e94
qiao-lima <qiao-lima@github.com>
2026-05-29 15:34:12 +0800

    Manual Update 3751890

---
+++ 3751890.lua
@@ -0,0 +1,12 @@
+-- 3751890's Lua and Manifest Created by Hubcap Manifest
+-- 我的26岁女房客：在云端
+-- Created: December 07, 2025 at 04:34:14 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3751890) -- 我的26岁女房客：在云端
+-- MAIN APP DEPOTS
+addappid(3751891, 1, "a2238efc6413704b566956b6837306571a284f7dd67836026beeb4be6f011dca") -- Depot 3751891
+setManifestid(3751891, "5424808469521075192", 5081344536)
