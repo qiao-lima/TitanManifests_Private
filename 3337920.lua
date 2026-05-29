a73967d975c7085bfd2986d925d642c9c32fb1d8
qiao-lima <qiao-lima@github.com>
2026-05-30 02:14:15 +0800

    Manual Update 3337920

---
+++ 3337920.lua
@@ -0,0 +1,14 @@
+-- 3337920's Lua and Manifest Created by Hubcap Manifest
+-- Hidden Winter Things
+-- Created: May 29, 2026 at 10:07:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3337920) -- Hidden Winter Things
+-- MAIN APP DEPOTS
+addappid(3337921, 1, "a43e80f4dd282746e33f376dd92d9f72aaca1625f8227e355a747c761d750707") -- Depot 3337921
+setManifestid(3337921, "1974626021470729824", 605642128)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3337922) -- Depot 3337922 (empty depot)
