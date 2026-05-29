885256be105db3261c940f785a1c9cb77374b959
qiao-lima <qiao-lima@github.com>
2026-05-29 09:54:33 +0800

    Manual Update 3780290

---
+++ 3780290.lua
@@ -0,0 +1,12 @@
+-- 3780290's Lua and Manifest Created by Hubcap Manifest
+-- Magic Dodgeball: Homeless Edition
+-- Created: May 28, 2026 at 10:42:13 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3780290) -- Magic Dodgeball: Homeless Edition
+-- MAIN APP DEPOTS
+addappid(3780291, 1, "88cf167c1ad8e80dbde29b2da96d4c28fdf8c12433fc608b9621e20119c38776") -- Depot 3780291
+setManifestid(3780291, "523765511851769880", 600152068)
