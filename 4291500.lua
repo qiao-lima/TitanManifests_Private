5fcfdf2f2ac9a92393dc75e765a230053c507715
qiao-lima <qiao-lima@github.com>
2026-05-30 02:14:16 +0800

    Manual Update 4291500

---
+++ 4291500.lua
@@ -0,0 +1,12 @@
+-- 4291500's Lua and Manifest Created by Hubcap Manifest
+-- 春日狂想曲：情窦初开的秘密
+-- Created: May 29, 2026 at 12:29:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4291500) -- 春日狂想曲：情窦初开的秘密
+-- MAIN APP DEPOTS
+addappid(4291501, 1, "7ab2b0479ed7ba2f24dd4e5fecfa7f933ceb1da77d4d6ab5ea606a9697d77af4") -- Depot 4291501
+setManifestid(4291501, "4946870310872475585", 1189342252)
