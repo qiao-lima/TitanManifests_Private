1ba0705a473f18da2d6dcd66751ae3b6225a09ac
qiao-lima <qiao-lima@github.com>
2026-05-30 01:58:18 +0800

    Manual Update 3830690

---
+++ 3830690.lua
@@ -0,0 +1,19 @@
+-- 3830690's Lua and Manifest Created by Hubcap Manifest
+-- Flee, My Elven Ninja!  3D
+-- Created: May 25, 2026 at 08:12:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3830690, 1, "5cb2cdcc8f99109f033680141f166a8c355a65e1480ea0d36fe1d157346edf35") -- Flee, My Elven Ninja!  3D
+-- MAIN APP DEPOTS
+addappid(3830691, 1, "b7dc6defefd989232eaa64bc1b6251b5e1a5ea073241601dd1d7706eb75376f5") -- Depot 3830691
+setManifestid(3830691, "1736241096064091406", 2238255235)
+addappid(3830692, 1, "ec74050ea5920381bc0eab9189d805b5dd6cbd7cbe429433be92756d849edf57") -- Depot 3830692
+setManifestid(3830692, "4706614662425489149", 2238393240)
+-- DLCS WITH DEDICATED DEPOTS
+-- Flee, My Elven Ninja 3D - Artbook (AppID: 4209540)
+addappid(4209540)
+addappid(4209541, 1, "daba8942e158f08028623cd7393d181a3ae668a5c316fd657d0329d846ac2e91") -- Flee, My Elven Ninja 3D - Artbook - Depot 4209541
+setManifestid(4209541, "4414725775365648680", 79313844)
