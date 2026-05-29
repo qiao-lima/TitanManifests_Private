f134109b7230b2cfd4a12cbc09c631c93ea48fb6
qiao-lima <qiao-lima@github.com>
2026-05-29 09:54:32 +0800

    Manual Update 3264960

---
+++ 3264960.lua
@@ -0,0 +1,12 @@
+-- 3264960's Lua and Manifest Created by Hubcap Manifest
+-- CardBurners
+-- Created: May 28, 2026 at 10:56:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3264960) -- CardBurners
+-- MAIN APP DEPOTS
+addappid(3264961, 1, "2a1417733a454db2d7a7321f824414176fdaa8597b6d313545857c72e003298e") -- Depot 3264961
+setManifestid(3264961, "3142131040404223401", 3718015124)
