a2cf3977fa251a82974e6ab9c578de6e51f6ba05
qiao-lima <qiao-lima@github.com>
2026-05-30 02:14:15 +0800

    Manual Update 3711170

---
+++ 3711170.lua
@@ -0,0 +1,12 @@
+-- 3711170's Lua and Manifest Created by Hubcap Manifest
+-- 冰点之下
+-- Created: May 29, 2026 at 11:06:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3711170) -- 冰点之下
+-- MAIN APP DEPOTS
+addappid(3711171, 1, "6f71f3c96f8ac493e890b4d852ac300b8a806e5a4174c644ac560dddfae12432") -- Depot 3711171
+setManifestid(3711171, "7428050317738293696", 16963771756)
