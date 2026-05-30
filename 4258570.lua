a76423aab9d919b840edec9516d84e2d2a569f00
qiao-lima <qiao-lima@github.com>
2026-05-30 17:44:44 +0800

    Manual Update 4258570

---
+++ 4258570.lua
@@ -0,0 +1,12 @@
+-- 4258570's Lua and Manifest Created by Hubcap Manifest
+-- Wall Of Insanity 2
+-- Created: May 30, 2026 at 01:55:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4258570) -- Wall Of Insanity 2
+-- MAIN APP DEPOTS
+addappid(4258571, 1, "68fc06e03da3cbfdfc417b7ff7daf0c3482ed1302010b0f166f2b1ae04a3e6f8") -- Depot 4258571
+setManifestid(4258571, "6821309616170615790", 3454351605)
