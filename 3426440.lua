c39d866e6d4657a96c50240de0023a9998ba255f
qiao-lima <qiao-lima@github.com>
2026-05-30 17:41:34 +0800

    Manual Update 3426440

---
+++ 3426440.lua
@@ -0,0 +1,16 @@
+-- 3426440's Lua and Manifest Created by Hubcap Manifest
+-- LOVE SHOP Simulator 💕🛒
+-- Created: May 30, 2026 at 01:55:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3426440) -- LOVE SHOP Simulator 💕🛒
+-- MAIN APP DEPOTS
+addappid(3426441, 1, "e8823e3d9f23a2363633e9563c3866130a3189c7ab0d0c374666ed7e6f650983") -- Depot 3426441
+setManifestid(3426441, "5503399731075028154", 5552801070)
+addappid(3426442, 1, "be76f65dbce8e03f8c11a4ee83401c2b0a8d7619d381f21a508fa26758902d34") -- Depot 3426442
+setManifestid(3426442, "6741275670300830128", 5582427298)
+addappid(3426443, 1, "f575349fced8ed4a96138256ca63c35569120dbd09b9366a7c4c6cd61f624135") -- Depot 3426443
+setManifestid(3426443, "4079439400973383908", 5614699361)
