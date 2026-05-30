902327f86c0563e2b338d9df7631350bedd71c16
qiao-lima <qiao-lima@github.com>
2026-05-30 17:44:41 +0800

    Manual Update 3855140

---
+++ 3855140.lua
@@ -0,0 +1,16 @@
+-- 3855140's Lua and Manifest Created by Hubcap Manifest
+-- Moonlit Lanterns
+-- Created: May 30, 2026 at 01:57:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3855140) -- Moonlit Lanterns
+-- MAIN APP DEPOTS
+addappid(3855141, 1, "7af8ad39917e501fcf39d4a66224ed76a88ecb0b4d472b11da4f0f0e046e5876") -- Depot 3855141
+setManifestid(3855141, "225207286835624029", 465193738)
+addappid(3855142, 1, "2d510b04a5d04a57889905821d8143119cdce840074d5eb23e6371a986ae87bf") -- Depot 3855142
+setManifestid(3855142, "5177371707609385844", 428472091)
+addappid(3855143, 1, "b71fd4a549fdd35610902adf28395989bf7c6cf0710a68464b6b2f4ab23ad960") -- Depot 3855143
+setManifestid(3855143, "8331597139355103741", 448293368)
