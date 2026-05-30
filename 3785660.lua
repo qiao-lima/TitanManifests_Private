d71167ff08f193fc295be784a56528c40d098768
qiao-lima <qiao-lima@github.com>
2026-05-30 17:41:36 +0800

    Manual Update 3785660

---
+++ 3785660.lua
@@ -0,0 +1,12 @@
+-- 3785660's Lua and Manifest Created by Hubcap Manifest
+-- Breeding Visa
+-- Created: May 30, 2026 at 01:51:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3785660) -- Breeding Visa
+-- MAIN APP DEPOTS
+addappid(3785661, 1, "10a85c62e2754ad6094d1d1b0a2b1fc0b72e83a63c9157ea918656e183fed102") -- Depot 3785661
+setManifestid(3785661, "5707883106220812496", 1483376801)
