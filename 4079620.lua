ad05573e6eb2aa587a00fd5fdf306a2514e0d061
qiao-lima <qiao-lima@github.com>
2026-05-30 17:44:43 +0800

    Manual Update 4079620

---
+++ 4079620.lua
@@ -0,0 +1,12 @@
+-- 4079620's Lua and Manifest Created by Hubcap Manifest
+-- 潔姊不要停
+-- Created: May 30, 2026 at 01:55:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4079620) -- 潔姊不要停
+-- MAIN APP DEPOTS
+addappid(4079621, 1, "61ed6f5f4dbbedff5ea1e9a13dad56a6e0f7bab505cc4823697e70a6ba100b21") -- Depot 4079621
+setManifestid(4079621, "6819558591390409169", 537848828)
