11a0350b519c683773512e2bf1863e743b80299e
qiao-lima <qiao-lima@github.com>
2026-05-30 17:41:33 +0800

    Manual Update 3032150

---
+++ 3032150.lua
@@ -0,0 +1,18 @@
+-- 3032150's Lua and Manifest Created by Hubcap Manifest
+-- Frontier Legends
+-- Created: May 30, 2026 at 01:59:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3032150) -- Frontier Legends
+-- MAIN APP DEPOTS
+addappid(3032151, 1, "85cf9fff9efd2b60f37c2788e95923a29c58130b529c982da8294d2f4378f076") -- Depot 3032151
+setManifestid(3032151, "4489792547968634257", 9988400123)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
