becf40c5567b36547ea804630e3030ac40db2086
qiao-lima <qiao-lima@github.com>
2026-05-30 17:44:46 +0800

    Manual Update 4492650

---
+++ 4492650.lua
@@ -0,0 +1,12 @@
+-- 4492650's Lua and Manifest Created by Hubcap Manifest
+-- Cozy Petchi Cats
+-- Created: May 30, 2026 at 01:55:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4492650) -- Cozy Petchi Cats
+-- MAIN APP DEPOTS
+addappid(4492651, 1, "679fe7fa65b9ebc3fe893f17774f8f84f562a5e1899ec54ca19b58cbbeadc790") -- Depot 4492651
+setManifestid(4492651, "3076933713827637919", 330338824)
