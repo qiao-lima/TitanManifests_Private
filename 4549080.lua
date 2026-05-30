976b3fa87d97a6ad3778acd45d8cf9b0d71d1922
qiao-lima <qiao-lima@github.com>
2026-05-30 17:47:58 +0800

    Manual Update 4549080

---
+++ 4549080.lua
@@ -0,0 +1,12 @@
+-- 4549080's Lua and Manifest Created by Hubcap Manifest
+-- MarbleFlow
+-- Created: May 30, 2026 at 01:56:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4549080) -- MarbleFlow
+-- MAIN APP DEPOTS
+addappid(4549081, 1, "a3df54be69718b37ee874f5d9db52b371b87de022e198e6212b7d114b6ea170f") -- Depot 4549081
+setManifestid(4549081, "103132263250445780", 1399724431)
