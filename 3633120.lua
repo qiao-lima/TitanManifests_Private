b7b4da45c1322200a3088804f64c793af8c75ff6
qiao-lima <qiao-lima@github.com>
2026-05-30 17:41:35 +0800

    Manual Update 3633120

---
+++ 3633120.lua
@@ -0,0 +1,12 @@
+-- 3633120's Lua and Manifest Created by Hubcap Manifest
+-- God's Blessing On That Desire World!
+-- Created: May 30, 2026 at 01:55:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3633120) -- God's Blessing On That Desire World!
+-- MAIN APP DEPOTS
+addappid(3633121, 1, "9b68ce4812ac775ed804288c8130ff01d3c3790e90a2f61bcdba179a45359bd4") -- Depot 3633121
+setManifestid(3633121, "2589289950815095461", 830679574)
