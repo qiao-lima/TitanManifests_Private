090d5eee7607ce55a9513f48089733909c5e84ac
qiao-lima <qiao-lima@github.com>
2026-05-30 17:44:41 +0800

    Manual Update 3851470

---
+++ 3851470.lua
@@ -0,0 +1,12 @@
+-- 3851470's Lua and Manifest Created by Hubcap Manifest
+-- BarrelBots
+-- Created: May 30, 2026 at 01:56:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3851470) -- BarrelBots
+-- MAIN APP DEPOTS
+addappid(3851471, 1, "355fd4425bc6cd88241c3691bba7b434f3dcab083897b2760f2121529ba0cbfe") -- Depot 3851471
+setManifestid(3851471, "3223380016169479659", 1146580946)
