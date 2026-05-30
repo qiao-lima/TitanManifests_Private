b6d2e1d510ff1f6f74a1e3897ad9d3c40e2859cf
qiao-lima <qiao-lima@github.com>
2026-05-30 17:41:32 +0800

    Manual Update 1965980

---
+++ 1965980.lua
@@ -0,0 +1,12 @@
+-- 1965980's Lua and Manifest Created by Hubcap Manifest
+-- Gelli Fields
+-- Created: May 30, 2026 at 01:55:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1965980) -- Gelli Fields
+-- MAIN APP DEPOTS
+addappid(1965981, 1, "f72bb8b6f241c6395cd16c9e9618cb3df93082e56a42fab2a2b6e2977c0cf3ac") -- Depot 1965981
+setManifestid(1965981, "5045738791678296614", 179705927)
