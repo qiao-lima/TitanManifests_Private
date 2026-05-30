48dfcaa241d150731ce503d3e650f26fbe73e5bd
qiao-lima <qiao-lima@github.com>
2026-05-30 17:44:45 +0800

    Manual Update 4394020

---
+++ 4394020.lua
@@ -0,0 +1,18 @@
+-- 4394020's Lua and Manifest Created by Hubcap Manifest
+-- Morels: Out of this World
+-- Created: May 30, 2026 at 01:56:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4394020) -- Morels: Out of this World
+-- MAIN APP DEPOTS
+addappid(4394021, 1, "836fdd493b076a475de65e0fdc01d2fcb5afd2a358708db968042d01eedc393c") -- Depot 4394021
+setManifestid(4394021, "4049273932075501620", 18623947513)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
