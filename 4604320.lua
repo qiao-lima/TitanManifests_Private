c3ad123bc7373333fb330c2d70ae16edc8f27f52
qiao-lima <qiao-lima@github.com>
2026-05-30 17:47:59 +0800

    Manual Update 4604320

---
+++ 4604320.lua
@@ -0,0 +1,12 @@
+-- 4604320's Lua and Manifest Created by Hubcap Manifest
+-- 失焦 Out of Focus
+-- Created: May 30, 2026 at 01:56:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4604320) -- 失焦 Out of Focus
+-- MAIN APP DEPOTS
+addappid(4604321, 1, "f1ad405f44fe2225dbed4e45f5c13243d7e4426170373ca1f4588b4dcf84af28") -- Depot 4604321
+setManifestid(4604321, "6039778774746742445", 511868638)
