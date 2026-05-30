55cf40705f8ad182bf63271d4445b7cbdfcf5e24
qiao-lima <qiao-lima@github.com>
2026-05-30 17:44:42 +0800

    Manual Update 3999940

---
+++ 3999940.lua
@@ -0,0 +1,12 @@
+-- 3999940's Lua and Manifest Created by Hubcap Manifest
+-- The Suburbs
+-- Created: May 30, 2026 at 01:50:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3999940) -- The Suburbs
+-- MAIN APP DEPOTS
+addappid(3999942, 1, "11108575d64257045396a7b7fcddb2e3f31be67001a21919eb4cf4cef4afecd6") -- Depot 3999942
+setManifestid(3999942, "1416792971729517463", 1336144508)
