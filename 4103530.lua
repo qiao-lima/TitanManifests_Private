e60948a23d8753c96749df51638951b7d8745218
qiao-lima <qiao-lima@github.com>
2026-05-30 17:44:44 +0800

    Manual Update 4103530

---
+++ 4103530.lua
@@ -0,0 +1,12 @@
+-- 4103530's Lua and Manifest Created by Hubcap Manifest
+-- Heatwave: Sam's Stay
+-- Created: May 30, 2026 at 01:53:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4103530) -- Heatwave: Sam's Stay
+-- MAIN APP DEPOTS
+addappid(4103531, 1, "f56632043bcdfd12d80102562e2be9a624170f14f4426951e264dbede8224bbc") -- Depot 4103531
+setManifestid(4103531, "2111291643187161079", 2189995159)
