0d90da3b8ec676cb93047c80e86cd5e305c6936f
qiao-lima <qiao-lima@github.com>
2026-05-30 17:44:43 +0800

    Manual Update 4016880

---
+++ 4016880.lua
@@ -0,0 +1,12 @@
+-- 4016880's Lua and Manifest Created by Hubcap Manifest
+-- Secrets of Velendar Castle - Escape Room
+-- Created: May 30, 2026 at 01:55:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4016880) -- Secrets of Velendar Castle - Escape Room
+-- MAIN APP DEPOTS
+addappid(4016881, 1, "57c66dc86c8bd76165242a7f1899e9b3ded7dc0629a0e4b2d715e8436d7805da") -- Depot 4016881
+setManifestid(4016881, "7057390260067665923", 2313083474)
