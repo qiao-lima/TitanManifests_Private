34ad6a80392b56e41b557eaf71670efb2284eaf3
qiao-lima <qiao-lima@github.com>
2026-05-30 17:41:36 +0800

    Manual Update 3764940

---
+++ 3764940.lua
@@ -0,0 +1,14 @@
+-- 3764940's Lua and Manifest Created by Hubcap Manifest
+-- 暴杀残雨之夜
+-- Created: May 30, 2026 at 01:57:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3764940) -- 暴杀残雨之夜
+-- MAIN APP DEPOTS
+addappid(3764941, 1, "3ade93d412c2ae1759276a80a7b74e84094a6b9398f7b79fef56a2a22c100f4f") -- Depot 3764941
+setManifestid(3764941, "99956474027804911", 2238934381)
+addappid(3764942, 1, "e881be2c645ab39b62dea38b25c1f6bbf9642c35cf58c4aba5aaebbf53ec51ca") -- Depot 3764942
+setManifestid(3764942, "6600401455587376003", 2221944166)
