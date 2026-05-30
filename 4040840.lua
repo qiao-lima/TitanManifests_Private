a0a11e7484bab33e13230bba9fb5ad29e7b243bd
qiao-lima <qiao-lima@github.com>
2026-05-30 17:44:43 +0800

    Manual Update 4040840

---
+++ 4040840.lua
@@ -0,0 +1,14 @@
+-- 4040840's Lua and Manifest Created by Hubcap Manifest
+-- Taffy Tales Redux : The Morning After
+-- Created: May 30, 2026 at 01:57:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4040840) -- Taffy Tales Redux : The Morning After
+-- MAIN APP DEPOTS
+addappid(4040841, 1, "dfdb23ef8f14af688d2ab9343ce45f77874f094a9423c10712d6b61925c56865") -- Depot 4040841
+setManifestid(4040841, "1575404861774932457", 4642756049)
+addappid(4040842, 1, "cb5b92c51613f10d8bdff8be894875a7d7a0c714528cdbe322605a27fb12b93e") -- Depot 4040842
+setManifestid(4040842, "3977258495258649773", 4751587148)
