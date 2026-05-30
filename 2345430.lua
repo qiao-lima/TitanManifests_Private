54c50cff65b27a3f1b3bdcab9ea79ad865f989a4
qiao-lima <qiao-lima@github.com>
2026-05-30 17:41:32 +0800

    Manual Update 2345430

---
+++ 2345430.lua
@@ -0,0 +1,12 @@
+-- 2345430's Lua and Manifest Created by Hubcap Manifest
+-- Escape From Crimson Manor: Trapped Together
+-- Created: May 30, 2026 at 02:48:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2345430, 1, "b532d889baa8d4f4869d38f18e9f54f324e7bc6db78bedbf27533df9a0dec180") -- Escape From Crimson Manor: Trapped Together
+-- MAIN APP DEPOTS
+addappid(2345431, 1, "d5a4edf89057386ef0d422165c97627ec25b9edde0aefaa1a053b15836671b40") -- Depot 2345431
+setManifestid(2345431, "8698854969758702377", 2523159568)
