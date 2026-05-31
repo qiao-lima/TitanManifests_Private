833b188304aa774c35916157b6298e9b5e321901
qiao-lima <qiao-lima@github.com>
2026-05-31 12:34:14 +0800

    Manual Update 4110360

---
+++ 4110360.lua
@@ -0,0 +1,14 @@
+-- 4110360's Lua and Manifest Created by Hubcap Manifest
+-- Tavern, Inn, and the Halberd
+-- Created: May 30, 2026 at 09:17:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4110360) -- Tavern, Inn, and the Halberd
+-- MAIN APP DEPOTS
+addappid(4110362, 1, "3f03e4bf301b0a1a69b83e2a3d64de5ec5577a9527eafddc9180adbe0371b8c9") -- Depot 4110362
+setManifestid(4110362, "7123260710098291374", 546813330)
+addappid(4110363, 1, "327472ba55ea1cbf2c1ad3882b9f8d070de2342e6eeb2dd47c416912782bc351") -- Depot 4110363
+setManifestid(4110363, "4942873524153568929", 546813330)
