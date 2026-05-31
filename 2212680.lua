bf2fdfe51677e06cb122f4aab8bbae417a71ca50
qiao-lima <qiao-lima@github.com>
2026-05-31 12:34:13 +0800

    Manual Update 2212680

---
+++ 2212680.lua
@@ -0,0 +1,18 @@
+-- 2212680's Lua and Manifest Created by Hubcap Manifest
+-- Evil Of Fate
+-- Created: May 30, 2026 at 15:34:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2212680) -- Evil Of Fate
+-- MAIN APP DEPOTS
+addappid(2212681, 1, "a824469511e0a6711fa2db5351100f7c7bcc2294ddba811534f7c3a5edd4138d") -- Depot 2212681
+setManifestid(2212681, "1266045749616681419", 6602833992)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
