884469872a02015370bec786ea8e779de00d19c5
qiao-lima <qiao-lima@github.com>
2026-05-31 13:52:20 +0800

    Manual Update 2161440

---
+++ 2161440.lua
@@ -0,0 +1,16 @@
+-- 2161440's Lua and Manifest Created by Hubcap Manifest
+-- 风帆纪元 Sailing Era
+-- Created: May 16, 2026 at 01:37:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(2161440, 1, "f88f1525ce86ebbd9e6f496f70344581df22203c76e57f7263f76c5bf793befd") -- 风帆纪元 Sailing Era
+addtoken(2161440, "18177970239540104474")
+-- MAIN APP DEPOTS
+addappid(2161441, 1, "058d2d96ec93cd84305ae7b4f77ef2a8211aaae87cb74503b16c15b25dfb45d1") -- Depot 2161441
+setManifestid(2161441, "8719227337040431077", 8071914470)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2224340) -- Sailing Era Adventure of 2233
+addappid(2401890) -- Sailing Era Edge of the World
