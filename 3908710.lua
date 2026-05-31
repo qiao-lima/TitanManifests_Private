83996179a627b8ddccf9e00792a2140c1db373ca
qiao-lima <qiao-lima@github.com>
2026-05-31 12:34:14 +0800

    Manual Update 3908710

---
+++ 3908710.lua
@@ -0,0 +1,12 @@
+-- 3908710's Lua and Manifest Created by Hubcap Manifest
+-- Dramatour
+-- Created: May 30, 2026 at 01:57:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3908710) -- Dramatour
+-- MAIN APP DEPOTS
+addappid(3908711, 1, "e323d2a428e688f14423a4bbd22c5f548fc6020ddf16ecd45403fbe888eab6b9") -- Depot 3908711
+setManifestid(3908711, "3508594717019486159", 302440894)
