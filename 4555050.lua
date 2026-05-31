d4afa1598a60ce3a22d811aa0f8f89fbb6073495
qiao-lima <qiao-lima@github.com>
2026-05-31 14:16:05 +0800

    Manual Update 4555050

---
+++ 4555050.lua
@@ -0,0 +1,14 @@
+-- 4555050's Lua and Manifest Created by Hubcap Manifest
+-- Teaching Gender-Affirming Care
+-- Created: May 28, 2026 at 13:09:58 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4555050) -- Teaching Gender-Affirming Care
+-- MAIN APP DEPOTS
+addappid(4555052, 1, "d1118f320c92e23793d3ae62d189bb7d8b08828e9a1b4d6aba3f2abe277c57fc") -- Depot 4555052
+setManifestid(4555052, "6740928147876289246", 1089582813)
+addappid(4555053, 1, "87a8b80c5199e3614955d7c3dd79655f4658deecd9652a932c5502aad64ffc46") -- Depot 4555053
+setManifestid(4555053, "5786724055159136159", 1111303234)
