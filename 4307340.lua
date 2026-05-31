e98761c9a2daf3af107c35d81cd241d67f6e3414
qiao-lima <qiao-lima@github.com>
2026-05-31 12:34:15 +0800

    Manual Update 4307340

---
+++ 4307340.lua
@@ -0,0 +1,13 @@
+-- 4307340's Lua and Manifest Created by Hubcap Manifest
+-- Loli Ninja Village
+-- Created: May 30, 2026 at 19:54:19 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4307340) -- Loli Ninja Village
+addtoken(4307340, "11221136154274903324")
+-- MAIN APP DEPOTS
+addappid(4307344, 1, "984be720e76abf5e14626ed0bb02fc62ff49ab982764c0bc7179ea0dfaec09b9") -- Depot 4307344
+setManifestid(4307344, "4260786778507940106", 559572783)
