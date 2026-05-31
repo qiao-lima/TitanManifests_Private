d89b55dbde1730bcb50c803cbdca9618a313c1de
qiao-lima <qiao-lima@github.com>
2026-05-31 14:16:04 +0800

    Manual Update 4330260

---
+++ 4330260.lua
@@ -0,0 +1,12 @@
+-- 4330260's Lua and Manifest Created by Hubcap Manifest
+-- Choozle!
+-- Created: May 29, 2026 at 07:26:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4330260) -- Choozle!
+-- MAIN APP DEPOTS
+addappid(4330261, 1, "1caa9f18718b590e1db5fc37e4dfbebaab5e038f55355eabe6d2d93065ccaf0b") -- Depot 4330261
+setManifestid(4330261, "8225888318951886723", 1301041341)
