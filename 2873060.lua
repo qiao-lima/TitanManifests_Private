6631cfd3f6197efd4b1669a5e5814925da151f84
qiao-lima <qiao-lima@github.com>
2026-05-31 12:34:13 +0800

    Manual Update 2873060

---
+++ 2873060.lua
@@ -0,0 +1,12 @@
+-- 2873060's Lua and Manifest Created by Hubcap Manifest
+-- The Bell Echoes
+-- Created: May 30, 2026 at 13:55:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2873060, 1, "a9b0527922c277b9f8b14385d78df1cd17dc96f7e5518cfcf5ff30c6077d6267") -- The Bell Echoes
+-- MAIN APP DEPOTS
+addappid(2873061, 1, "629847494753a91bb0c243329050ca6e9133570aa5a149cd9805db0c313ab9f9") -- Depot 2873061
+setManifestid(2873061, "915002627842323625", 5739951000)
