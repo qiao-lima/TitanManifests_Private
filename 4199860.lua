33dafc954a59afb38c929b4e29b17d1e01eb0399
qiao-lima <qiao-lima@github.com>
2026-05-31 14:16:04 +0800

    Manual Update 4199860

---
+++ 4199860.lua
@@ -0,0 +1,16 @@
+-- 4199860's Lua and Manifest Created by Hubcap Manifest
+-- Paklyx
+-- Created: May 27, 2026 at 08:34:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4199860) -- Paklyx
+-- MAIN APP DEPOTS
+addappid(4199862, 1, "02946e9d52dfaae3bdb485b8432e080d4d02a1c3a16f1a914d84db10f180ff64") -- Depot 4199862
+setManifestid(4199862, "5528918907484514593", 312465287)
+addappid(4199863, 1, "b1fd7f2fa64625bdbf2031f8427f740e12f3d5b83f89bd71a12f4bad402bb037") -- Depot 4199863
+setManifestid(4199863, "5793256613183393738", 287133601)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4199861) -- Depot 4199861 (empty depot)
