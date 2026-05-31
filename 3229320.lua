ab47588d0c200319e8e6852edcc3807c69efbf81
qiao-lima <qiao-lima@github.com>
2026-05-31 13:52:21 +0800

    Manual Update 3229320

---
+++ 3229320.lua
@@ -0,0 +1,18 @@
+-- 3229320's Lua and Manifest Created by Hubcap Manifest
+-- LightRay | Wayfinder
+-- Created: May 30, 2026 at 20:17:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3229320) -- LightRay | Wayfinder
+-- MAIN APP DEPOTS
+addappid(3229321, 1, "44245e323502788006b9fc6d0c2fbb611f0e9e55d7b739a47f3faf727471d067") -- Depot 3229321
+setManifestid(3229321, "4019705372108455560", 4928681938)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
