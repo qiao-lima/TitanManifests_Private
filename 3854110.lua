129940750273138f36244b7a3b4c8a5cb5cdf996
qiao-lima <qiao-lima@github.com>
2026-05-31 14:16:03 +0800

    Manual Update 3854110

---
+++ 3854110.lua
@@ -0,0 +1,14 @@
+-- 3854110's Lua and Manifest Created by Hubcap Manifest
+-- Mnemonimov
+-- Created: May 28, 2026 at 15:23:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3854110, 1, "f23827373d6b375430abe24aa112cb06aea86f6b75f73b7933917a6556813ebc") -- Mnemonimov
+-- MAIN APP DEPOTS
+addappid(3854111, 1, "a6b35dfb36455d3e6b141b00924fb6737593fa938a0fb266df3a159de8e534ed") -- Depot 3854111
+setManifestid(3854111, "3497443911510494940", 113762484)
+addappid(3854112, 1, "4ee8135137d02b0e56212c61c6b95e7f60fda2c323859432f655aa5424972956") -- Depot 3854112
+setManifestid(3854112, "6454501450372376633", 133508193)
