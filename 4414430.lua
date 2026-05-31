16121d27b080dea6820bd893fc3883f359143b20
qiao-lima <qiao-lima@github.com>
2026-05-31 14:16:05 +0800

    Manual Update 4414430

---
+++ 4414430.lua
@@ -0,0 +1,12 @@
+-- 4414430's Lua and Manifest Created by Hubcap Manifest
+-- Spell Alchemist
+-- Created: May 28, 2026 at 12:52:06 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4414430) -- Spell Alchemist
+-- MAIN APP DEPOTS
+addappid(4414432, 1, "f28dcb97dc4b99dc3eac3c1cdd099dd9dd15e017161114db0ee74958a9499faa") -- Depot 4414432
+setManifestid(4414432, "3024238672406191731", 1406113653)
