8b2cc233f420b684feb9ed0ea46f857cee38c0aa
qiao-lima <qiao-lima@github.com>
2026-05-31 14:16:05 +0800

    Manual Update 4682430

---
+++ 4682430.lua
@@ -0,0 +1,14 @@
+-- 4682430's Lua and Manifest Created by Hubcap Manifest
+-- Robosynthesis
+-- Created: May 31, 2026 at 02:12:25 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4682430) -- Robosynthesis
+-- MAIN APP DEPOTS
+addappid(4682431, 1, "b3b66790e1b0eda856b688faf1390d477985ce0f86d3d650c82c1a7ab7ca3e55") -- Depot 4682431
+setManifestid(4682431, "1784720491214877835", 636021358)
+addappid(4682432, 1, "372f773cdc86bf5e65e46acb6230cd5eebd6b93f9bb4730fed55b005bcba6439") -- Depot 4682432
+setManifestid(4682432, "4497135260839859448", 603944663)
