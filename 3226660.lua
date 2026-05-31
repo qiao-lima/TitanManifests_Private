cb86b1eeeb9dc8cfd3809e553703f3436c5967a5
qiao-lima <qiao-lima@github.com>
2026-05-31 12:34:13 +0800

    Manual Update 3226660

---
+++ 3226660.lua
@@ -0,0 +1,12 @@
+-- 3226660's Lua and Manifest Created by Hubcap Manifest
+-- Heroes of Magic and Steel
+-- Created: May 30, 2026 at 19:47:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3226660) -- Heroes of Magic and Steel
+-- MAIN APP DEPOTS
+addappid(3226661, 1, "bb98f582bc14260d092af178d51bbef8d1cc218d3f03a39cd02894d7d5ffa0fa") -- Depot 3226661
+setManifestid(3226661, "4342352360541416958", 9900184726)
