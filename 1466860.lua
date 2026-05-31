f6805b32c0a4c8264a9d7d5fc72063283f0963e1
qiao-lima <qiao-lima@github.com>
2026-05-31 13:52:19 +0800

    Manual Update 1466860

---
+++ 1466860.lua
@@ -0,0 +1,132 @@
+-- 1466860's Lua and Manifest Created by Hubcap Manifest
+-- Age of Empires IV: Anniversary Edition
+-- Created: May 12, 2026 at 19:35:36 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 53
+-- Total DLCs: 8
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1466860, 1, "2dd3c665298a08edc3f9c406d25ba4a419dd9d3f3acecd9fdbe78ba38d057b5e") -- Age of Empires IV: Anniversary Edition
+-- MAIN APP DEPOTS
+addappid(1466861, 1, "3e9a11fa63f3e2e44b67e70f2333c54bb22330f4af7030669958a23b66e5569f") -- Cardinal Retail Content
+setManifestid(1466861, "5816376800768081265", 41550313526)
+addappid(1466862, 1, "be78108889970e40d71948bc186887b738fc0857c83e748602658f7c78a085a2") -- Cardinal Retail French Depot
+setManifestid(1466862, "8210395090850568304", 6890186473)
+addappid(1466863, 1, "105cf1c75e7cbb62a0f58b5a8a0cd59bc43d182ad8062db246917f641fb43034") -- Cardinal Retail Brazilian Portuguese Depot
+setManifestid(1466863, "2137235961037058220", 6836739157)
+addappid(1466864, 1, "0389461caff2cc22524621a0102d29d28922be6b9cfddc170481d8d78f44fe1c") -- Cardinal Retail German Depot
+setManifestid(1466864, "5150568144488435771", 6913056166)
+addappid(1466865, 1, "ce635b1b01602f57beeddc5722bb176778e1e369621368e2d946b9fa6d0aede0") -- Cardinal Retail Italian Depot
+setManifestid(1466865, "4155554997815603852", 6895920591)
+addappid(1466866, 1, "8c06b5f69b7fcedd02d23c6bac403f27f35adb5d7658f6a1c95f924b8b1e6924") -- Cardinal Retail Japanese Depot
+setManifestid(1466866, "2964218135138380359", 6437269908)
+addappid(1466867, 1, "d0750a64346f58303a3b45d7ea7b7ada26def4aea52cf1d4b9d471d4e2417907") -- Cardinal Retail Korean Depot
+setManifestid(1466867, "7562378097897535299", 6838451276)
+addappid(1466868, 1, "5c048641ca482fc8a1b43bea8abfb6827e42d1da5917ebdb4694c4c4d51bb2b9") -- Cardinal Retail Latin American Spanish Depot
+setManifestid(1466868, "6416039551445910783", 6811350057)
+addappid(1466869, 1, "591d2a66e3d42d006b2b6eab458be9847fed5344f2c4702d209996dd6988e934") -- Cardinal Retail Simplified Chinese Depot
+setManifestid(1466869, "9118276500387744409", 6865663027)
+addappid(1668576, 1, "94a7b00216874eae019f32023f03802341e1f337d1a1636f4ec246e860fcff54") -- Cardinal Retail English Depot
+setManifestid(1668576, "8237595818235435486", 7006985905)
+addappid(1668577, 1, "7bd75a2840cb0168f98a09837fe9642688737960f87716cb7fc258047093cad3") -- Cardinal Retail Spanish Depot
+setManifestid(1668577, "2206904226212846804", 6818910191)
+addappid(1668578, 1, "7fe6fbf544fa3e71d89df78bf736c659eca45684f62eb9c195c86ed8d9326b1d") -- Cardinal Retail Russian Depot
+setManifestid(1668578, "5017651509079678179", 7006985905)
+addappid(1668579, 1, "ae833f35348170217dd8491b0ad50d4c4bacde5a50056225759c5d13aa0e636e") -- Cardinal Retail Traditional Chinese Depot
+setManifestid(1668579, "8781103265254006256", 7006985905)
+addappid(1670951, 1, "a1487775f036a73c3447b7d09efc0fa8874eb42027071fee3856067ff0f836d8") -- Cardinal Retail Turkish Depot
+setManifestid(1670951, "4818130858131845336", 7006985905)
+addappid(1670952, 1, "ffc324f83fdc7bef75c21ae3a38efecc7d69ef4e32369e294b594f738f21fc97") -- Cardinal Retail Vietnamese Depot
+setManifestid(1670952, "319337191773200673", 7006985905)
+addappid(1670953, 1, "37b354352c19bd1217c8189572be4ebe06c373f07c7dec0e6d2bf7aa41f08859") -- Depot 1670953
+setManifestid(1670953, "4833052868282951804", 7006985905)
+addappid(1670954, 1, "ecb697535dfb8db0477d358fe969525840546f0f626b4531be9b3e12dfa7a42a") -- Depot 1670954
+setManifestid(1670954, "4793732855292674147", 7006985905)
+addappid(1670955, 1, "287913ddfab260cc29c04102d2d837f50713fe5917b718bdcb86c1bb1bdde894") -- Depot 1670955
+setManifestid(1670955, "4152536145461201347", 7006985905)
+addappid(1670956, 1, "509c839224605b2cdcce635c2812898c84b019287d7161136f4aad066ce440c1") -- Depot 1670956
+setManifestid(1670956, "4775215358318263749", 7006985905)
+addappid(1670957, 1, "1d1b250dc321e3f6be848fd85ce83f657ffb818dc1e3afe82eff2a676a1f366f") -- Depot 1670957
+setManifestid(1670957, "6404910367642923811", 7006985905)
+addappid(1670958, 1, "70637aa7ae9843b7308c5404d5d291715a41b3723bb300d95194b3ec8ab6997c") -- Depot 1670958
+setManifestid(1670958, "2898786839313867977", 7006985905)
+addappid(1670959, 1, "02fa42218460b398d1d58218e8a2c22445bda2358a6e5420fe1e5aa71124b0c3") -- Depot 1670959
+setManifestid(1670959, "4698263737274814902", 7006985905)
+addappid(1670960, 1, "bcbfa6af93f0a8a519b35e6f0dc9998d6840fd8bc2335fafca6d0b74d0fda79b") -- Depot 1670960
+setManifestid(1670960, "5129561200184982061", 7006985905)
+addappid(1670961, 1, "307a8752e1c575b45077cc0004003d2f911819566056c4d399bd682a17082b39") -- Depot 1670961
+setManifestid(1670961, "1042307936761136563", 7006985905)
+addappid(1670962, 1, "b68d70abce268eb72a8989eb5f734053ec89793add99cff43b3662f75b24698d") -- Depot 1670962
+setManifestid(1670962, "6886430158690367023", 7006985905)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITH DEDICATED DEPOTS
+-- Age of Empires IV - 4K HDR Video Pack (AppID: 1668560)
+addappid(1668560)
+addappid(1668560, 1, "d36f4c3985bb7dfc2e13a30c14a3673c76d5d014b731faa2edf4e115f777c934") -- Age of Empires IV - 4K HDR Video Pack - Age of Empires IV - 4K Graphics Pack (1668560) Depot
+setManifestid(1668560, "679585415305256255", 5932872252)
+addappid(1668561, 1, "ba6cd9247e1b9006ea3ae0886e2f0d229152150f4ad0d160089aec858494d925") -- Age of Empires IV - 4K HDR Video Pack - 4K Graphics Pack Brazilian Portuguese Depot
+setManifestid(1668561, "5477369601472219127", 13023934676)
+addappid(1668562, 1, "567fce2f8350811739f3d54e4a1ebd5ae23b6c3bb5c1a5ba7ddb093e0e3adea1") -- Age of Empires IV - 4K HDR Video Pack - 4K Graphics Pack English Depot
+setManifestid(1668562, "6549698333908356662", 13247800937)
+addappid(1668563, 1, "6afcffdf2c3dfdfbf8ab5ab2cc874a2b1555b5f6e3804f89d90d87ad9e3adf1a") -- Age of Empires IV - 4K HDR Video Pack - 4K Graphics Pack French Depot
+setManifestid(1668563, "492570845073703563", 13081261691)
+addappid(1668564, 1, "f19cc20bbfad1aefcd7d65e17f8a5c21dc4bc81d93982923e2ec19f196b7118a") -- Age of Empires IV - 4K HDR Video Pack - 4K Graphics Pack German Depot
+setManifestid(1668564, "1754271301489404944", 13063133828)
+addappid(1668565, 1, "2bc9e8a46074186b218a55813e34335848095fd16970423ff717f756885776fc") -- Age of Empires IV - 4K HDR Video Pack - 4K Graphics Pack Italian Depot
+setManifestid(1668565, "3034855857581089779", 13068014687)
+addappid(1668566, 1, "c50fee4814a770f56e6828abafcce1c7f54442cb054668173d0c3bd4ffae62ab") -- Age of Empires IV - 4K HDR Video Pack - 4K Graphics Pack Japanese Depot
+setManifestid(1668566, "1447188873334377391", 13011978015)
+addappid(1668567, 1, "6f5f876588a8176f7251dc3b0a113bb570d3590bb405e4036103806b662b9213") -- Age of Empires IV - 4K HDR Video Pack - 4K Graphics Pack Korean Depot
+setManifestid(1668567, "3704394592305333844", 13059481197)
+addappid(1668568, 1, "b54d14ce060e61b8a2428f1ec879b0d2c5c2f7bac78094c0ac45304e9d4783c6") -- Age of Empires IV - 4K HDR Video Pack - 4K Graphics Pack Latin American Spanish Depot
+setManifestid(1668568, "755431912528671787", 13077921035)
+addappid(1668569, 1, "990c8c23dfc1437056ab3344413c31a40a59723711e2a547cdd9138f79d2afe7") -- Age of Empires IV - 4K HDR Video Pack - 4K Graphics Pack Simplified Chinese Depot
+setManifestid(1668569, "7646206649279646764", 13075693260)
+addappid(1668571, 1, "d89bc074198d0c6094d116cc567c1e683a542d6680455e72217a03a3e396b680") -- Age of Empires IV - 4K HDR Video Pack - 4K Graphics Pack Spanish - Spain Depot
+setManifestid(1668571, "5805122192946586344", 13077921035)
+addappid(1668572, 1, "225d892ffebfdc435a51976f826ee46bb9c851f25d52fc07dfe3da540f2bb833") -- Age of Empires IV - 4K HDR Video Pack - 4K Graphics Pack Russian Depot
+setManifestid(1668572, "501093048546748912", 13247800937)
+addappid(1668573, 1, "c3d4e418aa6f3684eec8b49361dc9ceff8e78bb9feabe86fd902888ce2f4ef52") -- Age of Empires IV - 4K HDR Video Pack - 4K Graphics Pack Traditional Chinese Depot
+setManifestid(1668573, "7426142869141921231", 13247800937)
+addappid(1668574, 1, "0cd49ca4b57c6a34d05369b5a8bfceae406a561706a47ade50447ef9227fafb5") -- Age of Empires IV - 4K HDR Video Pack - 4K Graphics Pack Turkish Depot
+setManifestid(1668574, "5451306710457090161", 13247800937)
+addappid(1668575, 1, "39526fbd7371fd97d52646c9ef55b1e19666a6d5dbe57b6e1ce7e8f1d786d4d2") -- Age of Empires IV - 4K HDR Video Pack - 4K Graphics Pack Vietnamese Depot
+setManifestid(1668575, "3528078400764547325", 13247800937)
+addappid(1670964, 1, "717351d2dc29b301ef8d30db6a1f80b40687d580051f0c3cb9bf3d61929ea50a") -- Age of Empires IV - 4K HDR Video Pack - Depot 1670964
+setManifestid(1670964, "7812655020914186380", 13247800937)
+addappid(1670965, 1, "da6e6041fc0925d1074d6b11772e80c048e1ec088f03fd29cac9a4243ae99ae0") -- Age of Empires IV - 4K HDR Video Pack - Depot 1670965
+setManifestid(1670965, "1582218669874149641", 13247800937)
+addappid(1670966, 1, "f19c4030db757f7cf9172bd3417244adf319b17b3d1dd6d2234b2d18ce0ca3a0") -- Age of Empires IV - 4K HDR Video Pack - Depot 1670966
+setManifestid(1670966, "670444913470643007", 13247800937)
+addappid(1670967, 1, "c1d2a5fe05bd0f4bc53eb3d47bbc0015afb0114c2d8066990bdc352a219471ee") -- Age of Empires IV - 4K HDR Video Pack - Depot 1670967
+setManifestid(1670967, "8117301583025580887", 13247800937)
+addappid(1670968, 1, "614e967a43027cdc9e55a0676a6f17597b8c665a3474d1d6a6fff87e0ca82be4") -- Age of Empires IV - 4K HDR Video Pack - Depot 1670968
+setManifestid(1670968, "5137141778673678818", 13247800937)
+addappid(1670969, 1, "e6e8095d1fbca0b44e0e02536387c41ac5a37e69ab2aec14f331a19767eefe42") -- Age of Empires IV - 4K HDR Video Pack - Depot 1670969
+setManifestid(1670969, "4872448515233483244", 13247800937)
+addappid(1670970, 1, "3920bbba1d9820cd34c157353d88854e2f77269fdbfe23bcbb3f9f7441ffa374") -- Age of Empires IV - 4K HDR Video Pack - Depot 1670970
+setManifestid(1670970, "5439377956453704871", 13247800937)
+addappid(1670971, 1, "3bb5f7a7ed83ecc804c860ce27af3a7d5f1668a5d9bd65b577a3fe71865bd632") -- Age of Empires IV - 4K HDR Video Pack - Depot 1670971
+setManifestid(1670971, "3636830549104194720", 13247800937)
+addappid(1670972, 1, "d5a5af3680a56a7c4b29a7eba33aea6bc59f0c7510d2669373bf53ddc1262be2") -- Age of Empires IV - 4K HDR Video Pack - Depot 1670972
+setManifestid(1670972, "5587384387490018518", 13247800937)
+addappid(1670973, 1, "c74b67b4981a91300037b0bde395c20e934c5f9b0adf667663248459be3bfb12") -- Age of Empires IV - 4K HDR Video Pack - Depot 1670973
+setManifestid(1670973, "2619726927987078136", 13247800937)
+-- Age of Empires IV - Digital Deluxe Edition Content (AppID: 1668570)
+addappid(1668570)
+addappid(1668570, 1, "c40940d72ebd0b57476916e018a049913651c81005ec507fd01a5b1545f7ad74") -- Age of Empires IV - Digital Deluxe Edition Content - Age of Empires IV - Digital Deluxe Edition Content (1668570) Depot
+setManifestid(1668570, "5021418869429789881", 69860445)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1779350) -- CC Gift
+addappid(1959430) -- Age of Empires IV  The Sultans Ascend
+addappid(2131250) -- Age of Empires IV Digital Deluxe Upgrade Pack
+addappid(3067190) -- Age of Empires IV Dynasties of the East
+addappid(3144890) -- Age of Empires IV Knights of Cross and Rose
+addappid(3746540) -- Age of Empires IV Yue Feis Legacy
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(1959431) -- Depot 1959431 (empty depot)
