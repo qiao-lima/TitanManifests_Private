843587d9a7123c6db31ccfc9a95d7dbe0078a059
qiao-lima <qiao-lima@github.com>
2026-05-31 12:34:15 +0800

    Manual Update 4429950

---
+++ 4429950.lua
@@ -0,0 +1,12 @@
+-- 4429950's Lua and Manifest Created by Hubcap Manifest
+-- Kunugi's Life is a Mess!
+-- Created: May 30, 2026 at 09:27:37 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4429950) -- Kunugi's Life is a Mess!
+-- MAIN APP DEPOTS
+addappid(4429953, 1, "f3e77260bc0beee9c1a67fbd186607e20b05d0370c9df4356bcdabe97a5aa012") -- Depot 4429953
+setManifestid(4429953, "8990541859882953267", 970132805)
