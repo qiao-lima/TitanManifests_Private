4bf25b2455312257939370a4b2bf9a22f547aae6
qiao-lima <qiao-lima@github.com>
2026-05-31 14:16:04 +0800

    Manual Update 4147050

---
+++ 4147050.lua
@@ -0,0 +1,12 @@
+-- 4147050's Lua and Manifest Created by Hubcap Manifest
+-- Salmon Tower
+-- Created: May 31, 2026 at 02:13:08 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4147050) -- Salmon Tower
+-- MAIN APP DEPOTS
+addappid(4147051, 1, "789e253e177fe06423aba4497ad23c5a8fd4a770b84cce55bd6dc95925d825c4") -- Depot 4147051
+setManifestid(4147051, "1498820074549535172", 115597210)
