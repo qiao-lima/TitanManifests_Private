307d822eb849d159f786a364fa54f42bfc447df2
qiao-lima <qiao-lima@github.com>
2026-05-31 14:16:03 +0800

    Manual Update 4079420

---
+++ 4079420.lua
@@ -0,0 +1,16 @@
+-- 4079420's Lua and Manifest Created by Hubcap Manifest
+-- FROGBLOCK
+-- Created: May 28, 2026 at 15:30:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4079420) -- FROGBLOCK
+-- MAIN APP DEPOTS
+addappid(4079422, 1, "44c6a6d4d06bb78bcdb401b8dc51a86f3595969d8bfdb8f6733086bdf5068ef0") -- Depot 4079422
+setManifestid(4079422, "5157273874749114733", 901776770)
+addappid(4079423, 1, "e096a164d5cc3bb0ed441b9b0f4275b80df42393f170a68a7409e28f67e20490") -- Depot 4079423
+setManifestid(4079423, "2640287537577099724", 1002478029)
+addappid(4079424, 1, "6f51610ac5abb003cdfa098344a70ae04fdf887b83c7077545ece655d1c74dd3") -- Depot 4079424
+setManifestid(4079424, "7067821183228550438", 953408130)
