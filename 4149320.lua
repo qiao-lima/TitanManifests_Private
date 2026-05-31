ae1e1c0bb6155e9b1ceff6448b9158a549dfc972
qiao-lima <qiao-lima@github.com>
2026-05-31 16:16:46 +0800

    Manual Update 4149320

---
+++ 4149320.lua
@@ -0,0 +1,14 @@
+-- 4149320's Lua and Manifest Created by Hubcap Manifest
+-- Don't Touch the Snail
+-- Created: May 31, 2026 at 02:51:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4149320, 1, "65bf8a2711513caec9d7349c839d37cf0a65faa8edd400c37f7a164692b01720") -- Don't Touch the Snail
+-- MAIN APP DEPOTS
+addappid(4149321, 1, "f2f16919603444f8509af38b7e5ba3b39479bab6c130bc6eec958344cd0ce5d2") -- Depot 4149321
+setManifestid(4149321, "8250511955704717559", 133789725)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4730530) -- Dont Touch the Snail - Supporter Pack
