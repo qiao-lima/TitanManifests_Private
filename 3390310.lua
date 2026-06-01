1da814b174df5e1b3e110298578d1a655c03bf9c
qiao-lima <qiao-lima@github.com>
2026-06-02 06:46:06 +0800

    Manual Update 3390310

---
+++ 3390310.lua
@@ -0,0 +1,12 @@
+-- 3390310's Lua and Manifest Created by Hubcap Manifest
+-- Once Glorious Artahk
+-- Created: June 01, 2026 at 08:09:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3390310) -- Once Glorious Artahk
+-- MAIN APP DEPOTS
+addappid(3390311, 1, "43c1b7aeb543791d9888a8b75261ea377581f5b006532a53f1cbc1dca9901a35") -- Depot 3390311
+setManifestid(3390311, "5365725309727177241", 107645318)
