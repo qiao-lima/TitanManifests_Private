76ae7bab9f234bbff1752bd19009fcf6ab3dd11a
qiao-lima <qiao-lima@github.com>
2026-06-02 06:46:06 +0800

    Manual Update 3200290

---
+++ 3200290.lua
@@ -0,0 +1,12 @@
+-- 3200290's Lua and Manifest Created by Hubcap Manifest
+-- Sister Ray
+-- Created: June 01, 2026 at 18:43:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3200290) -- Sister Ray
+-- MAIN APP DEPOTS
+addappid(3200291, 1, "f7392f97b439d8454727bb864789c64644a56ff99cbce444e8758478fb616ba0") -- Depot 3200291
+setManifestid(3200291, "8376416483933011186", 5083166261)
