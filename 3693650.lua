4a3f005e378196463f6a6639ccc80fd48315a722
qiao-lima <qiao-lima@github.com>
2026-06-02 06:46:08 +0800

    Manual Update 3693650

---
+++ 3693650.lua
@@ -0,0 +1,16 @@
+-- 3693650's Lua and Manifest Created by Hubcap Manifest
+-- Thrifty Business
+-- Created: May 26, 2026 at 17:08:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3693650, 1, "d05e74b65fc9bcf809821a7681751fc39b94050f08e28f06d652c841e98793f4") -- Thrifty Business
+-- MAIN APP DEPOTS
+addappid(3693651, 1, "973abb52688d3779bd907b093851c353df1d37971711870f6e72bc0737e3d0cf") -- Depot 3693651
+setManifestid(3693651, "8736521276000140095", 299190147)
+addappid(3693652, 1, "9d4871076d2c999981a0f937cd07607de2140b010f26467662c9b8f03d342304") -- Depot 3693652
+setManifestid(3693652, "6155690878289563623", 305028452)
+addappid(3693653, 1, "f52221cc82559e0ccd60cf4c821e0e1cf0abd30c700c7f2217cf483574ad20cb") -- Depot 3693653
+setManifestid(3693653, "1667205598872989140", 301546747)
