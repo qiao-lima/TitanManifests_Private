d1e8c7c2f29c2a5c8d98f134ae11a13f4cde855a
qiao-lima <qiao-lima@github.com>
2026-06-01 12:55:39 +0800

    Manual Update 895820

---
+++ 895820.lua
@@ -0,0 +1,43 @@
+-- 895820's Lua and Manifest Created by Hubcap Manifest
+-- Kokorogawari
+-- Created: May 31, 2026 at 13:37:30 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 8
+-- Total DLCs: 7
+
+-- MAIN APPLICATION
+addappid(895820, 1, "ef230d8b23184260cdec9e9e4900ccbc5dff7ef1666327335c1e58e7279cdaaa") -- Kokorogawari
+-- MAIN APP DEPOTS
+addappid(895822, 1, "5dd78a2a216e4024ca0cfbce7b7030ca71f07f3a569b2046c1b13ad05a2f1b6e") -- Kokorogawari Main Depot
+setManifestid(895822, "654538528967821019", 562407994)
+-- DLCS WITH DEDICATED DEPOTS
+-- Kokorogawari Soundtrack (AppID: 934610)
+addappid(934610)
+addappid(934611, 1, "580a9f5bb7ddaab675087915f0003d634dc51f241c4ef2cf5fcaf5c706ad326e") -- Kokorogawari Soundtrack - Kokorogawari Soundtrack Depot
+setManifestid(934611, "2489258510466730800", 133794615)
+-- Kokorogawari Mini Quiz Game (AppID: 934960)
+addappid(934960)
+addappid(934962, 1, "a6aaeceb3c9db12d68049ca0a3e431363ce22f52f80ec31ae521c042d4678cbb") -- Kokorogawari Mini Quiz Game - Kokorogawari Mini Quiz Game Depot
+setManifestid(934962, "5681378551452994247", 140770024)
+-- Kokorogawari Mini RPG (AppID: 934961)
+addappid(934961)
+addappid(934961, 1, "8cc5bcc9a943c370959a58f3581a3aadcf09d4209c950d3a19bce6c4457c1f4c") -- Kokorogawari Mini RPG - "Kokorogawari Mini RPG"-Depot
+setManifestid(934961, "6971517072599023660", 548038783)
+-- Kokorogawari - Watercolour Paint Set (AppID: 946900)
+addappid(946900)
+addappid(946903, 1, "e290c95ec26c76c9f0d3a782a0d0f8c6ed30eeb676c613524b6349511289ef8f") -- Kokorogawari - Watercolour Paint Set - "Kokorogawari - Watercolour Paint Set"-Depot
+setManifestid(946903, "4650641684206557436", 199344046)
+-- Kokorogawari - Secret Artwork Collection (AppID: 946901)
+addappid(946901)
+addappid(946901, 1, "67a962fd319a52fe9b2240f5f4dd92154d5ed5d239e7dc58c42f96893d2fdf15") -- Kokorogawari - Secret Artwork Collection - "Kokorogawari - Secret Artwork Collection"-Depot
+setManifestid(946901, "8798025368402331123", 242914884)
+-- Kokorogawari - Best Value Pack (AppID: 946902)
+addappid(946902)
+addappid(946902, 1, "17e4825ea893f6422b25021c378eae294f5f90aa84f04f3fa6940e04c751fa2c") -- Kokorogawari - Best Value Pack - "Kokorogawari - Best Value Pack"-Depot
+setManifestid(946902, "7271526012075338100", 949360424)
+-- Kokorogawari - Charcoal Sketches (AppID: 947810)
+addappid(947810)
+addappid(947811, 1, "fdc4b7db9280982775cd53567acd60c4e5f0aa102de5562c7d40b75bbb5f26cb") -- Kokorogawari - Charcoal Sketches - Kokorogawari - Charcoal Sketches Depot
+setManifestid(947811, "5141896943114075518", 2584009)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(895821) -- Depot 895821 (empty depot)
