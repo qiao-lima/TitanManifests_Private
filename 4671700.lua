1bc1793b7f60b96c978125e745027c63fdd4fcb9
qiao-lima <qiao-lima@github.com>
2026-06-01 13:05:55 +0800

    Manual Update 4671700

---
+++ 4671700.lua
@@ -0,0 +1,12 @@
+-- 4671700's Lua and Manifest Created by Hubcap Manifest
+-- 城主与少女
+-- Created: June 01, 2026 at 00:07:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4671700) -- 城主与少女
+-- MAIN APP DEPOTS
+addappid(4671701, 1, "4d8886940d4374c69a3ec81157559803fca042696791f4c6649cee79281e5c41") -- Depot 4671701
+setManifestid(4671701, "5711217493952010903", 2817999777)
