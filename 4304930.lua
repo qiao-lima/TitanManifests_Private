340381e65d609cca6a6a99fdb9012f294e46f1fd
qiao-lima <qiao-lima@github.com>
2026-06-02 06:46:29 +0800

    Manual Update 4304930

---
+++ 4304930.lua
@@ -0,0 +1,12 @@
+-- 4304930's Lua and Manifest Created by Hubcap Manifest
+-- Chef Knight
+-- Created: June 01, 2026 at 16:11:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4304930) -- Chef Knight
+-- MAIN APP DEPOTS
+addappid(4304931, 1, "a4a17a17eaec59396387e89f1a1dcf8852d3ad8ea046988b327fa0462292b715") -- Depot 4304931
+setManifestid(4304931, "5992826273200024822", 342546048)
