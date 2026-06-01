9afe84764383d2a3cf433a3e00dc865aefa6e771
qiao-lima <qiao-lima@github.com>
2026-06-02 06:46:28 +0800

    Manual Update 4118220

---
+++ 4118220.lua
@@ -0,0 +1,12 @@
+-- 4118220's Lua and Manifest Created by Hubcap Manifest
+-- Hell Forge
+-- Created: June 01, 2026 at 11:07:25 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4118220) -- Hell Forge
+-- MAIN APP DEPOTS
+addappid(4118221, 1, "5014328ab21946237ad1ae6fd8c06f38064122a49bb716515e8d43254b179760") -- Depot 4118221
+setManifestid(4118221, "1712460747852680740", 228819111)
