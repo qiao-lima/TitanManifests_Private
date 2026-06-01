d84d1086c2ec2674652b0c85a2dc44df42b0f3d7
qiao-lima <qiao-lima@github.com>
2026-06-02 06:46:28 +0800

    Manual Update 4054480

---
+++ 4054480.lua
@@ -0,0 +1,16 @@
+-- 4054480's Lua and Manifest Created by Hubcap Manifest
+-- HeroTail: Survivor
+-- Created: June 01, 2026 at 18:44:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 3
+
+-- MAIN APPLICATION
+addappid(4054480) -- HeroTail: Survivor
+-- MAIN APP DEPOTS
+addappid(4054481, 1, "47cd42d826ee90ca077c86a6966470f4eccdc9d0de0be5d2613548096e66b051") -- Depot 4054481
+setManifestid(4054481, "7936742488581274148", 1224980054)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4668290) -- HeroTail Survivors - Warlock
+addappid(4668300) -- HeroTail Survivors - Samurai
+addappid(4668310) -- HeroTail Survivors - Cleric
