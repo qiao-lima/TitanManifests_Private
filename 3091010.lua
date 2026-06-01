d46734fd4b58f5ec545ddfe1ffcbffcd3515b564
qiao-lima <qiao-lima@github.com>
2026-06-02 06:46:06 +0800

    Manual Update 3091010

---
+++ 3091010.lua
@@ -0,0 +1,13 @@
+-- 3091010's Lua and Manifest Created by Hubcap Manifest
+-- イカスミポーション
+-- Branch: PUBLIC
+-- Created: January 07, 2026 at 08:54:11 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3091010, 1, "6073ec2a0155dfd669ec7443a5ce5e9e8e09aa2ac0a20699dbb385a2cb057175") -- イカスミポーション
+-- MAIN APP DEPOTS
+addappid(3091011, 1, "32ade5df7e52c67cedd9eef6fbfad42a7ee4e270b746a50fd357e091bf6e20dc") -- Depot 3091011
+setManifestid(3091011, "33467542687788852", 3217009629)
