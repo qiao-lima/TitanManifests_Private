dfcf5b1bfdf6823b5cceeb84c5237f08eed2763c
qiao-lima <qiao-lima@github.com>
2026-06-02 06:46:29 +0800

    Manual Update 4727810

---
+++ 4727810.lua
@@ -0,0 +1,12 @@
+-- 4727810's Lua and Manifest Created by Hubcap Manifest
+-- Blooming Winter
+-- Created: June 01, 2026 at 12:29:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4727810) -- Blooming Winter
+-- MAIN APP DEPOTS
+addappid(4727811, 1, "0eb285b1745ea6a99998f26161fbdbd476535e3dc130ad7b757e62ce99bc5f27") -- Depot 4727811
+setManifestid(4727811, "2086925305621917635", 117770633)
