54f7c1fe3a92cb3d8d0a65dd11ac83be45702961
qiao-lima <qiao-lima@github.com>
2026-06-01 12:50:38 +0800

    Manual Update 4310660

---
+++ 4310660.lua
@@ -0,0 +1,17 @@
+-- 4310660's Lua and Manifest Created by Hubcap Manifest
+-- 100 Music Cats
+-- Created: May 31, 2026 at 14:05:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4310660) -- 100 Music Cats
+-- MAIN APP DEPOTS
+addappid(4310661, 1, "e9e60955ad4c89ce57950ca75836684e9f74252a94df929d96c28eec523c5867") -- Depot 4310661
+setManifestid(4310661, "2999500703022073138", 330435478)
+-- DLCS WITH DEDICATED DEPOTS
+-- 100 Music Cats - Artbook (AppID: 4320690)
+addappid(4320690)
+addappid(4320690, 1, "5f3a12be741885a7e28d42dfbe77342b4cd653a05cb368b9d1b3dfc90a4fec6c") -- 100 Music Cats - Artbook - Depot 4320690
+setManifestid(4320690, "8440909181257419349", 43812495)
