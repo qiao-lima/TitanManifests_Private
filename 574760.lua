de62d974ee316517deda37cc605312a26c758dc2
qiao-lima <qiao-lima@github.com>
2026-06-02 06:46:29 +0800

    Manual Update 574760

---
+++ 574760.lua
@@ -0,0 +1,16 @@
+-- 574760's Lua and Manifest Created by Hubcap Manifest
+-- Starship Theory
+-- Created: September 30, 2025 at 19:48:37 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(574760) -- Starship Theory
+-- MAIN APP DEPOTS
+addappid(574761, 1, "e35a1333e9f7d27c1bd30bef78800dabb4dbd2a32ce640282dbbe272416b6920") -- Starship Theory Content
+setManifestid(574761, "5250982978172908259", 250979845)
+addappid(574762, 1, "0bfe796d9497942d633e0934b4f133f5d84019653f6a29d2201316620459fefb") -- Starship Theory Content Linux
+setManifestid(574762, "4646178055837445859", 396962931)
+addappid(574763, 1, "45f1d08b9fbeb95795438b085c2b0bb5d54112d33c12081e1c19618abdfe5c0c") -- Starship Theory Content Mac
+setManifestid(574763, "1092922352051592230", 361810870)
