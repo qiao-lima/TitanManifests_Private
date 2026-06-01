ba6e3240b997ecfe294109a4af3d15fd4bcd7072
qiao-lima <qiao-lima@github.com>
2026-06-01 12:47:31 +0800

    Manual Update 3178350

---
+++ 3178350.lua
@@ -0,0 +1,109 @@
+-- 3178350's Lua and Manifest Created by Hubcap Manifest
+-- WARRIORS: Abyss
+-- Created: March 19, 2026 at 08:12:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 24
+-- Total DLCs: 26
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3178350, 1, "877c7e708b69446f9bd31a893051cf5d6f9cdde5ae5c662a9d9b3075898ab47a") -- WARRIORS: Abyss
+-- MAIN APP DEPOTS
+addappid(3178351, 1, "50d5b19878cdd566dccd7b3f0042e13d42e13d32f5be8c88e71ea45a4d38323d") -- Depot 3178351
+setManifestid(3178351, "985815300853754457", 3675743659)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- WARRIORS Abyss - DYNASTY WARRIORS Costume Set (AppID: 3358070)
+addappid(3358070)
+addappid(3358070, 1, "7f29a98df78d333fa66611c3d323c6312f0c42aacd986b5459db60dee34ac7fa") -- WARRIORS Abyss - DYNASTY WARRIORS Costume Set - Depot 3358070
+setManifestid(3358070, "692117097771144508", 8190892)
+-- WARRIORS Abyss - HacknDash Legendary Costume Set (AppID: 3358080)
+addappid(3358080)
+addappid(3358080, 1, "6146f8b9ef4662268b7ebbe73c86f084c6b391aabecbfdc0c2586cbc59fa0594") -- WARRIORS Abyss - HacknDash Legendary Costume Set - Depot 3358080
+setManifestid(3358080, "544705766344584054", 101652924)
+-- WARRIORS Abyss - DYNASTY WARRIORS Classic Costume Set (AppID: 3358090)
+addappid(3358090)
+addappid(3358090, 1, "b435576191cc296a2c028424739cb9736645d34a1ec08a28d3099401d923172b") -- WARRIORS Abyss - DYNASTY WARRIORS Classic Costume Set - Depot 3358090
+setManifestid(3358090, "8944576031920283815", 130129252)
+-- WARRIORS Abyss - SAMURAI WARRIORS Classic Costume Set (AppID: 3358100)
+addappid(3358100)
+addappid(3358100, 1, "c0c507bc3f9d4633c5839394fa0af8e7467e111fbd99fc015f1f6caa887cc374") -- WARRIORS Abyss - SAMURAI WARRIORS Classic Costume Set - Depot 3358100
+setManifestid(3358100, "119839112108696445", 130619744)
+-- WARRIORS Abyss - 3,000 Karma Embers (AppID: 3358110)
+addappid(3358110)
+addappid(3358110, 1, "2f525b2ffbe129a613aa2a8d468d4aeed107e092c88d8ab33c53654d6794bb89") -- WARRIORS Abyss - 3,000 Karma Embers - Depot 3358110
+setManifestid(3358110, "6770187367082928544", 16)
+-- WARRIORS Abyss - 5,000 Karma Embers (AppID: 3358120)
+addappid(3358120)
+addappid(3358120, 1, "0abef5f189b4ff09f6ce15796aa9bbb502493c3f8cd9d38cf01319b93564b26b") -- WARRIORS Abyss - 5,000 Karma Embers - Depot 3358120
+setManifestid(3358120, "7983060936108053808", 16)
+-- WARRIORS Abyss - DYNASTY WARRIORS Jin Dynasty Classic Costume Set (AppID: 3358130)
+addappid(3358130)
+addappid(3358130, 1, "8c9d92b16d66e041a2e23f9c0f9bec94b6193bc05170e70e102dda1e4943dce7") -- WARRIORS Abyss - DYNASTY WARRIORS Jin Dynasty Classic Costume Set - Depot 3358130
+setManifestid(3358130, "1899387090726308255", 33049476)
+-- WARRIORS Abyss - SAMURAI WARRIORS Western Japan Classic Costume Set (AppID: 3358140)
+addappid(3358140)
+addappid(3358140, 1, "5d84acaacba8f481033a57f86abbc5d6d025f677f8a05544851fcc83c47c11cc") -- WARRIORS Abyss - SAMURAI WARRIORS Western Japan Classic Costume Set - Depot 3358140
+setManifestid(3358140, "7538682689799024329", 12768916)
+-- WARRIORS Abyss - NOBUNAGAS AMBITION Yukimura Sanada Costume Set (AppID: 3358160)
+addappid(3358160)
+addappid(3358160, 1, "92e45e04b2863544247f5e07ec54e39a35f2a27d0d234bfba0336d799a16f1f3") -- WARRIORS Abyss - NOBUNAGAS AMBITION Yukimura Sanada Costume Set - Depot 3358160
+setManifestid(3358160, "9189886800285596040", 4890896)
+-- WARRIORS Abyss - MASTER NINJA Legacy Costume Set (AppID: 3480150)
+addappid(3480150)
+addappid(3480150, 1, "e423e85e8c584a65695d96890aeddf854bc742a32a1a71fa6002d160b51e2350") -- WARRIORS Abyss - MASTER NINJA Legacy Costume Set - Depot 3480150
+setManifestid(3480150, "2438764950333787560", 5483896)
+-- WARRIORS Abyss - MASTER NINJA Formal Costume Set (AppID: 3480160)
+addappid(3480160)
+addappid(3480160, 1, "3a59daf7f9721792286915ce6ddcfbbd504b646b31cc1681b874e9cdc0acfe3c") -- WARRIORS Abyss - MASTER NINJA Formal Costume Set - Depot 3480160
+setManifestid(3480160, "7328736649700441702", 7184376)
+-- WARRIORS Abyss - DYNASTY WARRIORS ORIGINS Main Content Set (AppID: 4239840)
+addappid(4239840)
+addappid(4239840, 1, "e2545896ee483d6af242f0a85e45dddc172cb52a2a89e522a81cf7baa40dad81") -- WARRIORS Abyss - DYNASTY WARRIORS ORIGINS Main Content Set - Depot 4239840
+setManifestid(4239840, "930266028662551007", 40679296)
+-- WARRIORS Abyss - DYNASTY WARRIORS ORIGINS Playable Character Set (AppID: 4239850)
+addappid(4239850)
+addappid(4239850, 1, "417758e28d8195837308ea796b9cacf902255d8994770dfbec361761e1dcc658") -- WARRIORS Abyss - DYNASTY WARRIORS ORIGINS Playable Character Set - Depot 4239850
+setManifestid(4239850, "5774075741421807952", 16)
+-- WARRIORS Abyss - DYNASTY WARRIORS ORIGINS Other Character Version Set (Wei) (AppID: 4239860)
+addappid(4239860)
+addappid(4239860, 1, "88228596e91ebd3a15337f34574cbdb67b8ece21d671e1dd3e6cf6343480feca") -- WARRIORS Abyss - DYNASTY WARRIORS ORIGINS Other Character Version Set (Wei) - Depot 4239860
+setManifestid(4239860, "5329418268762243288", 10353656)
+-- WARRIORS Abyss - DYNASTY WARRIORS ORIGINS Other Character Version Set (Wu) (AppID: 4239870)
+addappid(4239870)
+addappid(4239870, 1, "d131896587f250f9b9fdc2d406f0a640bb805cda32d39dd1bb7ddf1851270242") -- WARRIORS Abyss - DYNASTY WARRIORS ORIGINS Other Character Version Set (Wu) - Depot 4239870
+setManifestid(4239870, "7457104204863146546", 10378744)
+-- WARRIORS Abyss - DYNASTY WARRIORS ORIGINS Other Character Version Set (Shu) (AppID: 4239880)
+addappid(4239880)
+addappid(4239880, 1, "d505bc12dbf606fa294949ae7cc8e78bc4eff3dfbe794c324d829a712d45133f") -- WARRIORS Abyss - DYNASTY WARRIORS ORIGINS Other Character Version Set (Shu) - Depot 4239880
+setManifestid(4239880, "7206052537663909099", 9688184)
+-- WARRIORS Abyss - DYNASTY WARRIORS ORIGINS Other Character Version Set (Other) (AppID: 4239900)
+addappid(4239900)
+addappid(4239900, 1, "b3441f8c3144fbe5cc1f87ee7eab709f464a77c2536b7fe7a1aecfcc42ca16f2") -- WARRIORS Abyss - DYNASTY WARRIORS ORIGINS Other Character Version Set (Other) - Depot 4239900
+setManifestid(4239900, "4760837785256106981", 10258832)
+-- WARRIORS Abyss - Zuo Ci Costume Set (AppID: 4239910)
+addappid(4239910)
+addappid(4239910, 1, "a2a41ec46c71a0ad5079ef4692f41845f279f1985cde69d968c9442f8abbca2c") -- WARRIORS Abyss - Zuo Ci Costume Set - Depot 4239910
+setManifestid(4239910, "1858876500138436220", 4303888)
+-- WARRIORS Abyss - Lianshi Costume Set (AppID: 4239920)
+addappid(4239920)
+addappid(4239920, 1, "089a15cd4a1028c3c5478d08bd3c545863f279991f04ff0dc5d53051aea8c8d7") -- WARRIORS Abyss - Lianshi Costume Set - Depot 4239920
+setManifestid(4239920, "8956320760319995487", 6413176)
+-- WARRIORS Abyss - Yu Jin Costume (AppID: 4239930)
+addappid(4239930)
+addappid(4239930, 1, "9605dacbdb7876d9e94a6510e4092bb5e1c90bc34873e76c7a7091a9ad6bb5aa") -- WARRIORS Abyss - Yu Jin Costume - Depot 4239930
+setManifestid(4239930, "4054271530167122153", 1789020)
+-- WARRIORS Abyss - Dong Bai Costume Set (AppID: 4239940)
+addappid(4239940)
+addappid(4239940, 1, "8c74f6b800972842ba568736fb23cf9ea72bb4be0c11198b75044377088a227d") -- WARRIORS Abyss - Dong Bai Costume Set - Depot 4239940
+setManifestid(4239940, "1616474336788560488", 6574840)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3480130) -- WARRIORS Abyss - Atelier Series Vacation Costume Set 
+addappid(3480140) -- WARRIORS Abyss - Atelier Series Academy Costume Set
+addappid(4337620) -- WARRIORS Abyss - DYNASTY WARRIORS ORIGINS Main Content Set  Playable Character Set
+addappid(4337630) -- WARRIORS Abyss - DYNASTY WARRIORS ORIGINS Other Character Version Set (Full Set)
+addappid(4347120) -- WARRIORS Abyss - DW 25th Anniversary Top Characters Costume Set Pack
