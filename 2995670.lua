5e01eacd205e9f9ba6b88465d979441d6dd555ca
qiao-lima <qiao-lima@github.com>
2026-06-02 06:46:05 +0800

    Manual Update 2995670

---
+++ 2995670.lua
@@ -0,0 +1,21 @@
+-- 2995670's Lua and Manifest Created by Hubcap Manifest
+-- Reaper Actual
+-- Created: June 01, 2026 at 18:30:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 2
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2995670) -- Reaper Actual
+-- MAIN APP DEPOTS
+addappid(2995671, 1, "d4e84a891054fe637a38fae289fb392f2692a12731eee78d5ee4a52b67e4a2cf") -- Depot 2995671
+setManifestid(2995671, "8621604521930703538", 32286536326)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4098550) -- Reaper Actual - Foundation Alpha - Charons Pact Edition
+addappid(4098560) -- Reaper Actual - Foundation Alpha - Stygian Oath Edition
