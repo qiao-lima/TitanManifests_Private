472e182db0e7843ab0790be06b27a8086edc8f24
qiao-lima <qiao-lima@github.com>
2026-06-02 06:46:07 +0800

    Manual Update 3574770

---
+++ 3574770.lua
@@ -0,0 +1,12 @@
+-- 3574770's Lua and Manifest Created by Hubcap Manifest
+-- Pre-Odyssey: Love at First Quack
+-- Created: June 01, 2026 at 16:11:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3574770, 1, "97199f930da7dfcf7f12e4f285cb4d469797e5a8e22e64611e0f66c94e19a73b") -- Pre-Odyssey: Love at First Quack
+-- MAIN APP DEPOTS
+addappid(3574771, 1, "bfdc25fa04ca802d7a1e11e5afc85f81d3efdc578f259cb2535f7847ca0a29e8") -- Depot 3574771
+setManifestid(3574771, "4825896579007430791", 592299797)
