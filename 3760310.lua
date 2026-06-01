a409c6588d12e8e2064125c0dea7880e30fb4c93
qiao-lima <qiao-lima@github.com>
2026-06-02 06:46:09 +0800

    Manual Update 3760310

---
+++ 3760310.lua
@@ -0,0 +1,12 @@
+-- 3760310's Lua and Manifest Created by Hubcap Manifest
+-- Heart of Gaia
+-- Created: June 01, 2026 at 13:50:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3760310) -- Heart of Gaia
+-- MAIN APP DEPOTS
+addappid(3760311, 1, "e588951aacce5fb9f3f429f707c5bf71a0194f882af08d4fc70a4f47ed7d5dc2") -- Depot 3760311
+setManifestid(3760311, "5404495240520566152", 3379463541)
