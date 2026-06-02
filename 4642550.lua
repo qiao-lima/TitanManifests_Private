f529e5e9bf65d9753ddc9c539f12d1b2f8014899
qiao-lima <qiao-lima@github.com>
2026-06-02 19:49:15 +0800

    Manual Update 4642550

---
+++ 4642550.lua
@@ -0,0 +1,14 @@
+-- 4642550's Lua and Manifest Created by Hubcap Manifest
+-- Ghost Town Seekers
+-- Created: June 02, 2026 at 06:53:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4642550) -- Ghost Town Seekers
+-- MAIN APP DEPOTS
+addappid(4642551, 1, "d97c9731d6e4873a28a60a8fdea9f4d91717bdf2ffb8d64101c79253d35b188f") -- Depot 4642551
+setManifestid(4642551, "7078028730545468756", 180367062)
+addappid(4642552, 1, "aab732beb9289d7f11e7116558c0da3960af6597a0b950f0bc57d3cd431ff19d") -- Depot 4642552
+setManifestid(4642552, "2336955675642776475", 197507171)
