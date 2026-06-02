0f05c7d1f1491de5d501426f215722b8e654876b
qiao-lima <qiao-lima@github.com>
2026-06-02 19:49:16 +0800

    Manual Update 4710730

---
+++ 4710730.lua
@@ -0,0 +1,12 @@
+-- 4710730's Lua and Manifest Created by Hubcap Manifest
+-- ClockOut
+-- Created: June 02, 2026 at 06:50:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4710730) -- ClockOut
+-- MAIN APP DEPOTS
+addappid(4710731, 1, "b7b984074865c59c4f506c387ed8c38cedf3140abc9fae5a89820036d43324ac") -- Depot 4710731
+setManifestid(4710731, "8463896725848507751", 421205176)
