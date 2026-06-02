d990dd9f37d355a821d31dcaaedb74567fc49ee3
qiao-lima <qiao-lima@github.com>
2026-06-02 13:30:29 +0800

    Manual Update 3416960

---
+++ 3416960.lua
@@ -0,0 +1,16 @@
+-- 3416960's Lua and Manifest Created by Hubcap Manifest
+-- Dorm Janitor's Revenge
+-- Created: June 01, 2026 at 01:03:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3416960, 1, "0ac347d960c9366f90f1a6a2d72760ad6a641e3e1551a522510ba941fcfa3498") -- Dorm Janitor's Revenge
+-- MAIN APP DEPOTS
+addappid(3416961, 1, "433bcaa862a7cdc61896ffe14ebd4e8320216d76491d07927265d8e9c50a5b4f") -- Depot 3416961
+setManifestid(3416961, "3095119715885958197", 467121608)
+addappid(3416962, 1, "41e2c7c0f147b3cc55ff97d5994d3e9d0baf442d74c72dead2d171c6895d5884") -- Depot 3416962
+setManifestid(3416962, "4279987186925016561", 647274946)
+addappid(3416963, 1, "82abe68dc0171790e3dca4932e668984db2809bc147612ca952907c3d2adf595") -- Depot 3416963
+setManifestid(3416963, "2705261269645989895", 509687107)
