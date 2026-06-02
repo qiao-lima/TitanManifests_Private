a1d7729c66a545131bc61524eb25989f33e2b95b
qiao-lima <qiao-lima@github.com>
2026-06-02 19:48:11 +0800

    Manual Update 3584870

---
+++ 3584870.lua
@@ -0,0 +1,12 @@
+-- 3584870's Lua and Manifest Created by Hubcap Manifest
+-- Lucha Chess 
+-- Created: June 02, 2026 at 06:51:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3584870) -- Lucha Chess 
+-- MAIN APP DEPOTS
+addappid(3584871, 1, "d3f7d0ff1a817a983c5033f1c03b96f91df61661cb9c83017d70f5831fa4c60c") -- Depot 3584871
+setManifestid(3584871, "60477342683498260", 358475489)
