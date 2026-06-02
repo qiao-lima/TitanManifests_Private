083cc1550e285418c2c667de5d4515a553cd8774
qiao-lima <qiao-lima@github.com>
2026-06-02 19:48:14 +0800

    Manual Update 4481440

---
+++ 4481440.lua
@@ -0,0 +1,12 @@
+-- 4481440's Lua and Manifest Created by Hubcap Manifest
+-- 命运之轮（序章）/Journey of Destiny(dependent origination)
+-- Created: June 02, 2026 at 06:52:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4481440) -- 命运之轮（序章）/Journey of Destiny(dependent origination)
+-- MAIN APP DEPOTS
+addappid(4481441, 1, "901f417f7bf956011d26008a84002ae2cd4af5ef377985f47211c9c6da199bab") -- Depot 4481441
+setManifestid(4481441, "2325817608129769521", 2381363128)
