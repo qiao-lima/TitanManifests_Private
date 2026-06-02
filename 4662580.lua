bad27ce57f1fc37a6d562495fd3506cb00ca6f1c
qiao-lima <qiao-lima@github.com>
2026-06-02 14:30:37 +0800

    Manual Update 4662580

---
+++ 4662580.lua
@@ -0,0 +1,14 @@
+-- 4662580's Lua and Manifest Created by Hubcap Manifest
+-- Duke vs The Gang
+-- Created: May 31, 2026 at 17:03:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4662580) -- Duke vs The Gang
+-- MAIN APP DEPOTS
+addappid(4662581, 1, "b243c2f47bf83f36c7c7d2a84386ef2acb33794eb440d831ecd15a62056267bb") -- Depot 4662581
+setManifestid(4662581, "4023019546500338399", 850456280)
+addappid(4662582, 1, "287a264dfe759a53c1827186dda26192342f317ed0f4eaeac20c82217eef54b2") -- Depot 4662582
+setManifestid(4662582, "7237308078042495143", 869008092)
