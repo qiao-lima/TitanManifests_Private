b5df80fe5ba9da56f0ac06808981384bda30fac2
qiao-lima <qiao-lima@github.com>
2026-06-02 13:30:26 +0800

    Manual Update 2270120

---
+++ 2270120.lua
@@ -0,0 +1,18 @@
+-- 2270120's Lua and Manifest Created by Hubcap Manifest
+-- HiveCorp
+-- Created: June 01, 2026 at 05:56:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2270120) -- HiveCorp
+-- MAIN APP DEPOTS
+addappid(2270121, 1, "2649234f8ddced11d911d5f9afcf7c65d153de00839a0c166719d582452bc874") -- Depot 2270121
+setManifestid(2270121, "6455051942850988401", 605049365)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
