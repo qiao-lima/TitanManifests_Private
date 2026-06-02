321de78a75b68c009f17fa9ce76b0bc70826a2ae
qiao-lima <qiao-lima@github.com>
2026-06-02 19:49:15 +0800

    Manual Update 4619290

---
+++ 4619290.lua
@@ -0,0 +1,14 @@
+-- 4619290's Lua and Manifest Created by Hubcap Manifest
+-- Fatimopolis
+-- Created: June 02, 2026 at 06:51:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4619290) -- Fatimopolis
+-- MAIN APP DEPOTS
+addappid(4619291, 1, "1a489e7296f7ff138343bf5fe22e91d7b0841b257c47b76ef399f161f3b18ef9") -- Depot 4619291
+setManifestid(4619291, "320269754933880524", 428890416)
+addappid(4619292, 1, "f3b387e41124cd62c5b3055ac80208dfefd4ffe8cac9bf09caa38a194764e756") -- Depot 4619292
+setManifestid(4619292, "200015132474424878", 395988280)
