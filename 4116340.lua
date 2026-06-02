67b6577cc491e203c7b27f88042711ff43b49522
qiao-lima <qiao-lima@github.com>
2026-06-02 19:48:13 +0800

    Manual Update 4116340

---
+++ 4116340.lua
@@ -0,0 +1,18 @@
+-- 4116340's Lua and Manifest Created by Hubcap Manifest
+-- Escape from Castle Porcrest
+-- Created: June 02, 2026 at 06:51:08 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4116340) -- Escape from Castle Porcrest
+-- MAIN APP DEPOTS
+addappid(4116341, 1, "bbe178e92ab47dba54487b84480ad940c451728f4ed776b6e8b84b408f9ed13c") -- Depot 4116341
+setManifestid(4116341, "1098692007483389116", 924392823)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
