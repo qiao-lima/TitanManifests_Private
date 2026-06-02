eb198978ce773906315d479369539785d6eb3260
qiao-lima <qiao-lima@github.com>
2026-06-02 19:48:11 +0800

    Manual Update 3910690

---
+++ 3910690.lua
@@ -0,0 +1,12 @@
+-- 3910690's Lua and Manifest Created by Hubcap Manifest
+-- Rescue the Puppies
+-- Created: June 02, 2026 at 06:51:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3910690) -- Rescue the Puppies
+-- MAIN APP DEPOTS
+addappid(3910691, 1, "df01b85ce10d08c6ea2438aa211968c796f0e8ebd6e74c793ba9ece04379f187") -- Depot 3910691
+setManifestid(3910691, "7621471387909199751", 484961928)
