4a701d66fa73e6f64d433592458f6262f11a2ae6
qiao-lima <qiao-lima@github.com>
2026-06-02 13:30:29 +0800

    Manual Update 3583670

---
+++ 3583670.lua
@@ -0,0 +1,18 @@
+-- 3583670's Lua and Manifest Created by Hubcap Manifest
+-- Where the forest ends
+-- Created: June 01, 2026 at 05:50:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3583670) -- Where the forest ends
+-- MAIN APP DEPOTS
+addappid(3583671, 1, "69ed7d0427ffb4a1b1c8cc49853cfd1ef042aebb82e13a9876cda46f9fa723be") -- Depot 3583671
+setManifestid(3583671, "3104350582923613628", 7108914173)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
