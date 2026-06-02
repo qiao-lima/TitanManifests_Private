279e7e54dec5d4dd97f3ca7dc347df0d6b2fd9ea
qiao-lima <qiao-lima@github.com>
2026-06-02 13:36:23 +0800

    Manual Update 4296980

---
+++ 4296980.lua
@@ -0,0 +1,12 @@
+-- 4296980's Lua and Manifest Created by Hubcap Manifest
+-- Enter The Moshpit
+-- Created: June 01, 2026 at 03:04:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4296980) -- Enter The Moshpit
+-- MAIN APP DEPOTS
+addappid(4296981, 1, "9b71efc3c8619b5e3f61c355eee28fdbe3924b7cffda66fd9b8dd7a239104541") -- Depot 4296981
+setManifestid(4296981, "7079412886482664827", 129801188)
