a37b8902596278cbb9929651e0d269562c8343c6
qiao-lima <qiao-lima@github.com>
2026-06-02 19:49:14 +0800

    Manual Update 4500270

---
+++ 4500270.lua
@@ -0,0 +1,12 @@
+-- 4500270's Lua and Manifest Created by Hubcap Manifest
+-- Global Protocol: New World Order
+-- Created: June 02, 2026 at 06:54:06 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4500270) -- Global Protocol: New World Order
+-- MAIN APP DEPOTS
+addappid(4500271, 1, "148968212a4e649e57378b52f756649c02ebdae667f9bfe24391c683a6c8098c") -- Depot 4500271
+setManifestid(4500271, "5388071138477580097", 2074531751)
