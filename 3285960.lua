bb8c7be867721cf01b3715c532e39deda78c96e2
qiao-lima <qiao-lima@github.com>
2026-06-02 19:48:10 +0800

    Manual Update 3285960

---
+++ 3285960.lua
@@ -0,0 +1,12 @@
+-- 3285960's Lua and Manifest Created by Hubcap Manifest
+-- The Victor Initiative
+-- Created: June 02, 2026 at 06:51:25 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3285960) -- The Victor Initiative
+-- MAIN APP DEPOTS
+addappid(3285961, 1, "11488440c9eaf96f68536e7d63723f6469c92d3837e3cadf37a38066ddd896b1") -- Depot 3285961
+setManifestid(3285961, "4383900283360624568", 8049632659)
