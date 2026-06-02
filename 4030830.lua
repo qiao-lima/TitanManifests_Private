56c078004b3ecdf1c9a6e09bf1a877d8fe51f12d
qiao-lima <qiao-lima@github.com>
2026-06-02 19:48:12 +0800

    Manual Update 4030830

---
+++ 4030830.lua
@@ -0,0 +1,14 @@
+-- 4030830's Lua and Manifest Created by Hubcap Manifest
+-- Focus Grove
+-- Created: June 02, 2026 at 06:50:57 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4030830) -- Focus Grove
+-- MAIN APP DEPOTS
+addappid(4030831, 1, "ec1eef3e3703cb35e72f02c6e48764c9889bd24dd9b355d0b0ce0262370c8431") -- Depot 4030831
+setManifestid(4030831, "2876667293916657958", 1387577601)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4751330) -- Focus Grove - Supporter Pack
