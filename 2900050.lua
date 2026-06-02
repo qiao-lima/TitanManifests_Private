0b2c3a275ad53264d9dea62788993ccc135470c3
qiao-lima <qiao-lima@github.com>
2026-06-02 19:48:10 +0800

    Manual Update 2900050

---
+++ 2900050.lua
@@ -0,0 +1,12 @@
+-- 2900050's Lua and Manifest Created by Hubcap Manifest
+-- Lost in Art: a Miniature Realm
+-- Created: June 02, 2026 at 06:53:57 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2900050) -- Lost in Art: a Miniature Realm
+-- MAIN APP DEPOTS
+addappid(2900051, 1, "3e5e1f4b99bfb388faea1dc88338729ec3d0ef032e261571bf53029d245e4e07") -- Depot 2900051
+setManifestid(2900051, "4975314569959591358", 1409526378)
