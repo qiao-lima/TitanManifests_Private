f46e201f9e3c578c24744843144bc36ac2fbd218
qiao-lima <qiao-lima@github.com>
2026-06-03 06:59:08 +0800

    Manual Update 2055050

---
+++ 2055050.lua
@@ -0,0 +1,18 @@
+-- 2055050's Lua and Manifest Created by Hubcap Manifest
+-- 铁骑少女（Cavalry Girls）
+-- Created: May 21, 2026 at 00:14:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 4
+
+-- MAIN APPLICATION
+addappid(2055050, 1, "3e4d55ceed5bc1e14e4048671cec6db2f2d3225b6006dd06ec5183453f87cd2b") -- 铁骑少女（Cavalry Girls）
+addtoken(2055050, "8873520807594454583")
+-- MAIN APP DEPOTS
+addappid(2055051, 1, "fe0a834b62767e6d6dd0742841fd2a2e1454e8a6f5768fba2031f3124516e5f8") -- Depot 2055051
+setManifestid(2055051, "864246460720513521", 7617629920)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2747070) -- DLC-CavalryGirls DLC - Clover
+addappid(3909630) -- DLC-CavalryGirls DLC - Red Lotus
+addappid(3912980) -- Cavalry Girls - Crowdfunding Supporter Skin Pack
+addappid(3912990) -- Cavalry Girls - Art Book
