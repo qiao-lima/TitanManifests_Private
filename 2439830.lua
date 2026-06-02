d59b8ea9996813dd1dee8ace8f96bf56c9a4dd9e
qiao-lima <qiao-lima@github.com>
2026-06-02 19:48:08 +0800

    Manual Update 2439830

---
+++ 2439830.lua
@@ -0,0 +1,16 @@
+-- 2439830's Lua and Manifest Created by Hubcap Manifest
+-- Demon Loop
+-- Created: June 02, 2026 at 06:53:08 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2439830) -- Demon Loop
+-- MAIN APP DEPOTS
+addappid(2439831, 1, "339a5c904f6d08b685a1e5a9058848b19d04a43e08f2ba38ed65c4dfb85f09e0") -- Depot 2439831
+setManifestid(2439831, "9036995785892593332", 1649223547)
+addappid(2439832, 1, "d0deebd90c6feb0453620141a364308274326838b74897478f1b3fdbfbe1ed9a") -- Depot 2439832
+setManifestid(2439832, "4697026586989733607", 1780164778)
+addappid(2439833, 1, "c30ba2603180ea5fa71d2a779d6f2268cfe69a6c7a769c364d21863d7a525fa3") -- Depot 2439833
+setManifestid(2439833, "7369595666065288621", 1719152870)
