4a3b889a84fefc3fd215504eacdd29c9f037371b
qiao-lima <qiao-lima@github.com>
2026-06-02 19:48:12 +0800

    Manual Update 3965240

---
+++ 3965240.lua
@@ -0,0 +1,14 @@
+-- 3965240's Lua and Manifest Created by Hubcap Manifest
+-- Protect Your Humans
+-- Created: June 02, 2026 at 06:52:29 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3965240) -- Protect Your Humans
+-- MAIN APP DEPOTS
+addappid(3965241, 1, "cfdfb4af4f87a06e9988e2fe76d7514960c99b836322bcd70de66c769d7e7bad") -- Depot 3965241
+setManifestid(3965241, "2318770526200392002", 330231712)
+addappid(3965242, 1, "cda5737bfd307d88083d0b5e22fef90f360aa18da045c7a8d23fafa29d6cbf6c") -- Depot 3965242
+setManifestid(3965242, "6906894778347407693", 303239704)
