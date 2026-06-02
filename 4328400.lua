65cb2cb6ef7439753e23fa1b61916e6d0693628b
qiao-lima <qiao-lima@github.com>
2026-06-02 19:48:14 +0800

    Manual Update 4328400

---
+++ 4328400.lua
@@ -0,0 +1,18 @@
+-- 4328400's Lua and Manifest Created by Hubcap Manifest
+-- Unclean Water
+-- Created: June 02, 2026 at 06:52:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4328400) -- Unclean Water
+-- MAIN APP DEPOTS
+addappid(4328401, 1, "829302585b55e491c2a06914cf255fd0d71c3dd3b1a0a88186fcf59ff688f71b") -- Depot 4328401
+setManifestid(4328401, "2574759635996956551", 3604232957)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
