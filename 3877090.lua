a542428a3133ab24355949b3d494445fd65d04a1
qiao-lima <qiao-lima@github.com>
2026-06-04 01:29:26 +0800

    Manual Update 3877090

---
+++ 3877090.lua
@@ -0,0 +1,12 @@
+-- 3877090's Lua and Manifest Created by Hubcap Manifest
+-- Home
+-- Created: June 03, 2026 at 07:07:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3877090, 1, "b718205bfb90492b7bb4ea8cfb19b417ea7e9b2c90ebef504751aaf18b68d1fc") -- Home
+-- MAIN APP DEPOTS
+addappid(3877091, 1, "8a41b4e0f0586297c4512dde935e689ec6efc04e8cb194ec075eb07ef4781b5a") -- Depot 3877091
+setManifestid(3877091, "7814804070648300849", 294630021)
