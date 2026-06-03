8e21219b0760fd27be9ae3c62ff4ba8c39df84e6
qiao-lima <qiao-lima@github.com>
2026-06-04 01:29:27 +0800

    Manual Update 4019180

---
+++ 4019180.lua
@@ -0,0 +1,12 @@
+-- 4019180's Lua and Manifest Created by Hubcap Manifest
+-- ULTRA0
+-- Created: June 03, 2026 at 07:10:30 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4019180) -- ULTRA0
+-- MAIN APP DEPOTS
+addappid(4019181, 1, "4edd407009e3c73ba1fe8a394cc5e52624d07f8f5c775249197cdabd847a3b89") -- Depot 4019181
+setManifestid(4019181, "5136620900834779981", 346719679)
