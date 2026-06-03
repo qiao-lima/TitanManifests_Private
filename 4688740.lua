d57a9e78671295764a380efb12dccd34b5975cca
qiao-lima <qiao-lima@github.com>
2026-06-04 01:33:15 +0800

    Manual Update 4688740

---
+++ 4688740.lua
@@ -0,0 +1,16 @@
+-- 4688740's Lua and Manifest Created by Hubcap Manifest
+-- Drink Water!
+-- Created: June 03, 2026 at 08:42:19 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4688740) -- Drink Water!
+-- MAIN APP DEPOTS
+addappid(4688741, 1, "8cd7f00f7ad7548f949ee2a4140066871c06f244ee2331a4cad2e2cda8d14fb0") -- Depot 4688741
+setManifestid(4688741, "3211162775331207137", 186003327)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4704730) -- Drink Water - Supporter Pack
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4704730) -- Depot 4704730 (empty depot)
