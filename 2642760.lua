1d7c06eab75a04f796405bdb211ecc7b85e22c7d
qiao-lima <qiao-lima@github.com>
2026-06-03 13:05:07 +0800

    Manual Update 2642760

---
+++ 2642760.lua
@@ -0,0 +1,14 @@
+-- 2642760's Lua and Manifest Created by Hubcap Manifest
+-- Back to Hearth
+-- Created: June 02, 2026 at 18:19:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2642760) -- Back to Hearth
+-- MAIN APP DEPOTS
+addappid(2642761, 1, "f39f5380aec82eee1c7d4955cbafcdd2a5dea7378de4ce40135db855edb69dec") -- Depot 2642761
+setManifestid(2642761, "4878318612670819257", 702588722)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2642763) -- Depot 2642763 (empty depot)
