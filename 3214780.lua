c14f47b71e0c23165da75bca90b1df519d7164e6
qiao-lima <qiao-lima@github.com>
2026-06-04 01:29:24 +0800

    Manual Update 3214780

---
+++ 3214780.lua
@@ -0,0 +1,12 @@
+-- 3214780's Lua and Manifest Created by Hubcap Manifest
+-- 3D PUZZLE - Colonial Graveyard
+-- Created: October 03, 2025 at 04:14:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3214780) -- 3D PUZZLE - Colonial Graveyard
+-- MAIN APP DEPOTS
+addappid(3214781, 1, "abda05cc6242604920f8fb9f6407f560b037e8705987074ba82ac1d904380163") -- Depot 3214781
+setManifestid(3214781, "3930888189955157639", 730406616)
