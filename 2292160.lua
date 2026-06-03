d407279a890ff89c0851715cdaf7f54c3f06df52
qiao-lima <qiao-lima@github.com>
2026-06-04 01:29:23 +0800

    Manual Update 2292160

---
+++ 2292160.lua
@@ -0,0 +1,18 @@
+-- 2292160's Lua and Manifest Created by Hubcap Manifest
+-- Remorses
+-- Created: June 03, 2026 at 06:22:29 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2292160) -- Remorses
+-- MAIN APP DEPOTS
+addappid(2292161, 1, "f8aeea91805f97af52a505d484cb04e61567a94d248cf6513201a387529cb2c7") -- Depot 2292161
+setManifestid(2292161, "6357171581456160202", 33266570797)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
