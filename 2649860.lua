7e8a5434ccdfd27dca60fbbd8d3eef292650d238
qiao-lima <qiao-lima@github.com>
2026-06-04 01:29:24 +0800

    Manual Update 2649860

---
+++ 2649860.lua
@@ -0,0 +1,16 @@
+-- 2649860's Lua and Manifest Created by Hubcap Manifest
+-- Gombo
+-- Created: June 03, 2026 at 11:42:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2649860) -- Gombo
+-- MAIN APP DEPOTS
+addappid(2649861, 1, "b732e2c8dd21d9f2e70f1a6fe512357f418c3c47179feb686625707f3ce31886") -- Depot 2649861
+setManifestid(2649861, "9212403532093440833", 274955946)
+addappid(2649862, 1, "c63ca4f3c2065b215f7aeb6cabab202849d9f45ff86b30060839fe4f6c417acf") -- Depot 2649862
+setManifestid(2649862, "1928452544395638498", 567439776)
+addappid(2649863, 1, "0b689a33320e9a38050d07810aa8271be580eaa0083538746b8dc28f7a7ed091") -- Depot 2649863
+setManifestid(2649863, "3496014056982899491", 275608913)
