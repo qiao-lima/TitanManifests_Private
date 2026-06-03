9223cc0bc3a68e92dc062ef28ef76ffd6c072f62
qiao-lima <qiao-lima@github.com>
2026-06-04 01:29:25 +0800

    Manual Update 3407700

---
+++ 3407700.lua
@@ -0,0 +1,12 @@
+-- 3407700's Lua and Manifest Created by Hubcap Manifest
+-- Witch’s Lewd Curse
+-- Created: June 03, 2026 at 09:19:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3407700, 1, "0d03d59fdbd82df269ab6a7a9ecaa9f8bbdc725e21c194ae4d976634e0f2fce6") -- Witch’s Lewd Curse
+-- MAIN APP DEPOTS
+addappid(3407701, 1, "f4fe1532faa281586b048b8e5c044bd09b9cc0dd8f024c84f6c9c9151bdc304e") -- Depot 3407701
+setManifestid(3407701, "234432830861442917", 584796156)
