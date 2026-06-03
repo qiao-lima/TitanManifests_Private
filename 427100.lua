362b95d4e965fb5e8738da73e388787b11bfa288
qiao-lima <qiao-lima@github.com>
2026-06-03 13:46:40 +0800

    Manual Update 427100

---
+++ 427100.lua
@@ -0,0 +1,141 @@
+-- 427100's Lua and Manifest Created by Hubcap Manifest
+-- Fernbus Simulator
+-- Created: September 29, 2025 at 12:05:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 36
+-- Total DLCs: 31
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(427100, 1, "6a8aab109ff6b1a3dfa8c0ee0b317ca59bf8ddaf98fcf14d1fd17784813c817b") -- Fernbus Simulator
+-- MAIN APP DEPOTS
+addappid(427101, 1, "565d1b14d850d4be0ee5a4304e7e23552107ff4279deb62d28a3b8d8bb279750") -- Windows Content
+setManifestid(427101, "6209185531376152440", 32650870412)
+addappid(427118, 1, "02eb02c4417a0cc7053dff6f8aab3e9dd02edeb775fbe698dda06bd7b8befc94") -- Fernbus Simulator - Steinhuder Meer
+setManifestid(427118, "8609301819124552363", 3417688)
+addappid(569708, 1, "710bbe21f5309cfb8b1fd454a425c163e2f8aca8eb9b747c327ed30ef3352224") -- Fernbus Simulator - Fix
+setManifestid(569708, "8063992118282527498", 0)
+addappid(602731, 1, "fd84f9c4485c037310152b9ba1f30d1bc7606f7cf264e9cf8e4dda7d2370f902") -- Fernbus Simulator - TC
+setManifestid(602731, "5949501301683070979", 0)
+addappid(602732, 1, "189064a9e581545f4501cd9c6925a373452a8c31bdc8fdfc4d87223622b836a7") -- Depot 602732
+setManifestid(602732, "5838210037125358651", 180814011)
+addappid(602733, 1, "c10105523bf33022d83ee2308934a95686e2e77bc050482b74cf0944ce8f5955") -- Depot 602733
+setManifestid(602733, "2696001778401728315", 175550890)
+addappid(602734, 1, "8ffb4606d92321fda89368fb588256e832c67c8c7d9e501d49953c38ebdbf41f") -- Depot 602734
+setManifestid(602734, "2485359697634522798", 175550786)
+addappid(602735, 1, "e4dfd5eea5b8ac67bc1744d18cb44ece3baf51505a3978610c6514eb39fd1614") -- Depot 602735
+setManifestid(602735, "3648565053815618997", 98273690)
+addappid(602736, 1, "a74901d1de5f62541d4107cbb6134e35a56ee1af88774af451cc6005fd9241a0") -- Depot 602736
+setManifestid(602736, "6643946220833519592", 102438889)
+addappid(602737, 1, "3315af8e64235a41862a31842c1f822ac680ca69db1f37b19aafb5c68a8e17f3") -- Depot 602737
+setManifestid(602737, "1595810466924213972", 105482176)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITH DEDICATED DEPOTS
+-- Fernbus Simulator - Neoplan Skyliner (AppID: 569700)
+addappid(569700)
+addappid(427102, 1, "b7e3efea77ad621fb4c4c81b0aebe935f81c085bbaab5772290e98349b626736") -- Fernbus Simulator - Neoplan Skyliner - Fernbus Simulator - Neoplan Skyliner
+setManifestid(427102, "3039167669064314363", 0)
+-- Fernbus Simulator - Multimedia Package (AppID: 602730)
+addappid(602730)
+addappid(427103, 1, "7097279c0c5cae6cabe195457e63660ebdc0385e5ffa2310e76d884de55b8e71") -- Fernbus Simulator - Multimedia Package - Fernbus Simulator - Multimedia Package
+setManifestid(427103, "3230596282498798904", 710765220)
+-- Fernbus Simulator - Anniversary Repaint Package  (AppID: 612260)
+addappid(612260)
+addappid(427104, 1, "9f5d13c02e8b037cb2f341f387c8e42d84bf91714632427072f21ae95a2b2a4d") -- Fernbus Simulator - Anniversary Repaint Package  - Fernbus Simulator - Anniversary Repaint Package
+setManifestid(427104, "299553215479527120", 196090664)
+-- Fernbus Simulator - Austria  Switzerland (AppID: 612261)
+addappid(612261)
+addappid(427105, 1, "ab1d52e3740785f9ad45a4499b95669b1c926d2f799e33cf7017f2a16485c749") -- Fernbus Simulator - Austria  Switzerland - Fernbus Simulator - Austria/Switzerland
+setManifestid(427105, "4068641150062983901", 1417803927)
+-- Fernbus Simulator - France (AppID: 654120)
+addappid(654120)
+addappid(427106, 1, "752c488131363f07898c253bf1144f453c458891be3d265d8d1daa2f3e30d054") -- Fernbus Simulator - France - Fernbus Simulator - France
+setManifestid(427106, "4030910872037196629", 7065406633)
+-- Fernbus Simulator - Usedom (AppID: 686280)
+addappid(686280)
+addappid(427107, 1, "2eb5875553aeb58b06eeb2be236551e9d13193ca40985f29b0d6ce34eaefe84f") -- Fernbus Simulator - Usedom - Fernbus Simulator - Usedom
+setManifestid(427107, "5356177378879402684", 30574407)
+-- Fernbus Simulator - Comfort Class HD (AppID: 724420)
+addappid(724420)
+addappid(427108, 1, "aacb4339b64782a0053b45bffbd3953814cf60f6fc19d35814d5c010fc555a4a") -- Fernbus Simulator - Comfort Class HD - Fernbus Simulator - Comfort Class
+setManifestid(427108, "6451832587382769975", 0)
+-- Fernbus Simulator - VDL Futura FHD2 (AppID: 724440)
+addappid(724440)
+addappid(427109, 1, "19604c5550435da3b7f8eaa97bb1e477f77cb2877fa2b22a89e896c57b78df3b") -- Fernbus Simulator - VDL Futura FHD2 - Fernbus Simulator - VDL Futura
+setManifestid(427109, "576903352774981856", 0)
+-- Fernbus Simulator - Fuball Mannschaftsbus (AppID: 729490)
+addappid(729490)
+addappid(427110, 1, "8ed03c8a144e76ff3248ead1d4bb29fa2cf4417d264482ef9365c0200f39916a") -- Fernbus Simulator - Fuball Mannschaftsbus - Fernbus Simulator - Football
+setManifestid(427110, "7766338427850198013", 138439433)
+-- Fernbus Simulator - Rennsteig (AppID: 937720)
+addappid(937720)
+addappid(427111, 1, "5936952c4a8fe4b0aa299d7cca335d0e856eefacbf10560f1f6f3abe62984d35") -- Fernbus Simulator - Rennsteig - Fernbus Simulator - Rennsteig
+setManifestid(427111, "1100020261178760872", 5703107)
+-- Fernbus Simulator - MAN Lions Intercity (AppID: 1026740)
+addappid(1026740)
+addappid(427112, 1, "e6a182f13fa091ec5db7647abc76097700eda1588b990ffa87019c3b05691b56") -- Fernbus Simulator - MAN Lions Intercity - Fernbus Simulator - MAN Lion's Intercity
+setManifestid(427112, "6745388767416594167", 0)
+-- Fernbus Simulator - BB40 (AppID: 1121030)
+addappid(1121030)
+addappid(427113, 1, "a4d6953bc9b84551963e5793464cbfd9414ce824d0b695d183744e406a66e13d") -- Fernbus Simulator - BB40 - Fernbus Simulator - BB40
+setManifestid(427113, "3246510030392480116", 0)
+-- Fernbus Simulator - Rhine Gorge (AppID: 1121090)
+addappid(1121090)
+addappid(427114, 1, "42ac453f629862a02eff7597f5d607417aa0e2ce5f24d7d3f0663e3c0537b4bc") -- Fernbus Simulator - Rhine Gorge - Fernbus Simulator - Rheintal
+setManifestid(427114, "2108548880410610523", 120901245)
+-- Fernbus Simulator - Belgium (AppID: 1140660)
+addappid(1140660)
+addappid(427115, 1, "5ad0196361913422bfd5a777f6ebc19f0ecb8635aff5ecb5629a29feb58b6040") -- Fernbus Simulator - Belgium - Fernbus Simulator - Belgium
+setManifestid(427115, "8129091546651496475", 987312726)
+-- Fernbus Simulator - Netherlands (AppID: 1140661)
+addappid(1140661)
+addappid(427116, 1, "4b6f0229290c208601c15ac5e67013ba4eb6e0ecf2976dec8528a939ee35fd6b") -- Fernbus Simulator - Netherlands - Fernbus Simulator - Netherlands
+setManifestid(427116, "1673878876618330691", 886811143)
+-- Fernbus Simulator - Luxembourg (AppID: 1140662)
+addappid(1140662)
+addappid(427117, 1, "35c397ce4c08698037401672c5754a3ef6b16536f4ea75e9ee93c525583323c8") -- Fernbus Simulator - Luxembourg - Fernbus Simulator - Luxembourg
+setManifestid(427117, "921255585315331232", 112937535)
+-- Fernbus Simulator - MAN Lions Coach 3rd Gen (AppID: 1154770)
+addappid(1154770)
+addappid(569702, 1, "e0de57ed2ef2e66e6fe3f59afcbaad4537bb32df5d447113aa0e676b32470462") -- Fernbus Simulator - MAN Lions Coach 3rd Gen - Fernbus Simulator - ML7
+setManifestid(569702, "2278123645898777675", 0)
+-- Fernbus Simulator - Scania Touring (AppID: 1158120)
+addappid(1158120)
+addappid(427119, 1, "b7d98a195c431fee4c2a5369322a9b542a65d08f5d6aeed85060109a792a4dc5") -- Fernbus Simulator - Scania Touring - Fernbus Simulator - Scania Touring
+setManifestid(427119, "1128993684715194911", 0)
+-- Fernbus Simulator - W906 (AppID: 1250730)
+addappid(1250730)
+addappid(569701, 1, "7aeaaf11c528b61e7409aee2fb979e63a4cdd56b0dea3ce0c8e84f0967afdae4") -- Fernbus Simulator - W906 - Fernbus Simulator - W906
+setManifestid(569701, "8497909674161473596", 0)
+-- Fernbus Simulator - VDL Futura FDD2 (AppID: 1370010)
+addappid(1370010)
+addappid(569703, 1, "962bb32ccc8db03a8fda8ac3d536fe09ba8db2e41c39ffe84af3fed8777aaf63") -- Fernbus Simulator - VDL Futura FDD2 - Fernbus Simulator - V2
+setManifestid(569703, "1588070790492004896", 0)
+-- Fernbus Simulator - Denmark (AppID: 1446990)
+addappid(1446990)
+addappid(569704, 1, "e393848b39a1a20f8f1ec171232f7a74d3cbc96ce94a5976b6252de12d42764a") -- Fernbus Simulator - Denmark - Fernbus Simulator - D
+setManifestid(569704, "5569204347930712098", 491203900)
+-- Fernbus Simulator - Czech (AppID: 1446991)
+addappid(1446991)
+addappid(569705, 1, "309174c6e512a5264217f9b3fb739302ed6831acd51d20c1597382cba9c04b48") -- Fernbus Simulator - Czech - Fernbus Simulator - Czech
+setManifestid(569705, "5188401913187414929", 653082551)
+-- Fernbus Simulator - Bavarian Castle (AppID: 1471310)
+addappid(1471310)
+addappid(569706, 1, "d67e720a9f88d76b5246aa6a6c9defa91b5377a6175b95b9f0802c6f1e710df6") -- Fernbus Simulator - Bavarian Castle - Fernbus Simulator - SW
+setManifestid(569706, "7097420164809987061", 4985840)
+-- Fernbus Simulator - Poland (AppID: 1635100)
+addappid(1635100)
+addappid(569707, 1, "b12b8fbdb9d5abb84106e9b52a71f45a8fd7877f58824722a4b31ed3f251f73f") -- Fernbus Simulator - Poland - Fernbus Simulator - P
+setManifestid(569707, "7899180655766344622", 2675538236)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1847550) -- Fernbus Simulator - Top Class DD
+addappid(2294410) -- Fernbus Simulator - Interlink HD
+addappid(2294411) -- Fernbus Simulator - Altano TX
+addappid(2294412) -- Fernbus Simulator - Altano TDX
+addappid(2294413) -- Fernbus Simulator - Astromega
+addappid(2407420) -- Fernbus Simulator - Top Class HDH
+addappid(2407421) -- Fernbus Simulator - Neoplan Cityliner
