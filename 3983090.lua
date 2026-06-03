105a8cc7f348abeeadcdad3edeb7201703ec22a2
qiao-lima <qiao-lima@github.com>
2026-06-04 01:29:27 +0800

    Manual Update 3983090

---
+++ 3983090.lua
@@ -0,0 +1,18 @@
+-- 3983090's Lua and Manifest Created by Hubcap Manifest
+-- Soap Slide
+-- Created: June 03, 2026 at 13:26:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3983090) -- Soap Slide
+-- MAIN APP DEPOTS
+addappid(3983091, 1, "b3d1f6df7668ddb110cd810787aa4198e2252ada8abc20ce5e892d800aff27ba") -- Depot 3983091
+setManifestid(3983091, "7749044983463303035", 1561626102)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
