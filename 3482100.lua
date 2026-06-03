59676333af85310aeed0325d12c55e26a367ea80
qiao-lima <qiao-lima@github.com>
2026-06-04 01:29:26 +0800

    Manual Update 3482100

---
+++ 3482100.lua
@@ -0,0 +1,12 @@
+-- 3482100's Lua and Manifest Created by Hubcap Manifest
+-- A Night With: SuperHero
+-- Created: June 03, 2026 at 09:20:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3482100) -- A Night With: SuperHero
+-- MAIN APP DEPOTS
+addappid(3482101, 1, "70dcdf811fc4f84b309bcb91c51d72acdefe000a8a845f1017b3bddef61b56b8") -- Depot 3482101
+setManifestid(3482101, "5435940392275849207", 312843597)
