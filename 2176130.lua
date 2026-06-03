abcfa28415c5397de6334ee9edca5276647d2534
qiao-lima <qiao-lima@github.com>
2026-06-04 01:29:23 +0800

    Manual Update 2176130

---
+++ 2176130.lua
@@ -0,0 +1,261 @@
+-- 2176130's Lua and Manifest Created by Hubcap Manifest
+-- Tower! Simulator 3
+-- Created: May 17, 2026 at 02:13:06 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 63
+-- Total DLCs: 62
+
+-- MAIN APPLICATION
+addappid(2176130, 1, "943611b083a252708b85a6832d3ea3fe6492ae9e20d7cb148bf86b7aca461e1a") -- Tower! Simulator 3
+-- MAIN APP DEPOTS
+addappid(2176131, 1, "646f01966806a6cff7c5929b91d7c280dff02b08fef9809a02de19a8ad295707") -- Depot 2176131
+setManifestid(2176131, "7286774141147351108", 20286686741)
+-- DLCS WITH DEDICATED DEPOTS
+-- Tower Simulator 3 - OMDB Airport (AppID: 2304800)
+addappid(2304800)
+addappid(2304800, 1, "65e14018cec79fdb03fe29e260537b66d3f3c9046dcedf722ea6ee7b26f57b61") -- Tower Simulator 3 - OMDB Airport - Depot 2304800
+setManifestid(2304800, "8394839094191716348", 1701450302)
+-- Tower Simulator 3 - KLAS Airport (AppID: 2351620)
+addappid(2351620)
+addappid(2351620, 1, "5b2d79234eaf757a76201ca6921c52936bd0670fbac6f84786c9d5972585ff85") -- Tower Simulator 3 - KLAS Airport - Depot 2351620
+setManifestid(2351620, "7788182394399649293", 2940462336)
+-- Tower Simulator 3 - LSZH Airport (AppID: 2384700)
+addappid(2384700)
+addappid(2384700, 1, "9b481b7081025323a45dbaccc8e5d56792d9b781a3b16d5c778367af29d500c5") -- Tower Simulator 3 - LSZH Airport - Depot 2384700
+setManifestid(2384700, "6493512744140868503", 794800487)
+-- Tower Simulator 3 - KBNA Airport (AppID: 2418950)
+addappid(2418950)
+addappid(2418950, 1, "cc2ff79a618ffa3fb8f5c37fe62c9b00aefdc634e03f5ecdc94ce58e85ca7765") -- Tower Simulator 3 - KBNA Airport - Depot 2418950
+setManifestid(2418950, "2432227668186754751", 973409115)
+-- Tower Simulator 3 - EGLL Airport (AppID: 2464110)
+addappid(2464110)
+addappid(2464110, 1, "3b667b2daec7461c5f85d83ce5a4060d14d527c4d0c7b9955dc0a30d87a2231c") -- Tower Simulator 3 - EGLL Airport - Depot 2464110
+setManifestid(2464110, "1036177215516626891", 1121340933)
+-- Tower Simulator 3 - KAUS Airport (AppID: 2540390)
+addappid(2540390)
+addappid(2540390, 1, "947c9a0c1495f5193b0824b8edf86d6b32eadd10ce1db60f72d450d4a071aa4f") -- Tower Simulator 3 - KAUS Airport - Depot 2540390
+setManifestid(2540390, "5953058764409545165", 793709640)
+-- Tower Simulator 3 - EDDF Airport (AppID: 2561590)
+addappid(2561590)
+addappid(2561590, 1, "d9aa69446a8aad7db3675357f73ea19cb94398e2d6fdebad2549ffe6ebf2901c") -- Tower Simulator 3 - EDDF Airport - Depot 2561590
+setManifestid(2561590, "7661884754852281576", 1810209141)
+-- Tower Simulator 3 - KSFO Airport (AppID: 2585540)
+addappid(2585540)
+addappid(2585540, 1, "dd7880d073d241365e41621c04e9d79f870ed37e73a43133ba325ea961d95156") -- Tower Simulator 3 - KSFO Airport - Depot 2585540
+setManifestid(2585540, "6241304351488913924", 1159301436)
+-- Tower Simulator 3 - KBOS Airport (AppID: 2638230)
+addappid(2638230)
+addappid(2638230, 1, "da06fc8eca669527a7c829d8392d06e9d94f1d886205b4dcbb05c769350927a6") -- Tower Simulator 3 - KBOS Airport - Depot 2638230
+setManifestid(2638230, "8700014554200196043", 701192993)
+-- Tower Simulator 3 - KFLL Airport (AppID: 2675380)
+addappid(2675380)
+addappid(2675380, 1, "9df4c808e4273b0eb25f4f735b4bc1a3df9da02b8b6056b3eb0302693085b74b") -- Tower Simulator 3 - KFLL Airport - Depot 2675380
+setManifestid(2675380, "54138679217250929", 3899704564)
+-- Tower Simulator 3 - KATL Airport (AppID: 2687330)
+addappid(2687330)
+addappid(2687330, 1, "ff3a8ace40e41da4b6652c434263fd748e1c68bdcc569de1e0861fb2052b452b") -- Tower Simulator 3 - KATL Airport - Depot 2687330
+setManifestid(2687330, "6740494549153619030", 2457824033)
+-- Tower Simulator 3 - LEBL Airport (AppID: 2709050)
+addappid(2709050)
+addappid(2709050, 1, "79a332f340215267a30bd36db7e64b6271495bbbc4099284eb12c30d7c123f67") -- Tower Simulator 3 - LEBL Airport - Depot 2709050
+setManifestid(2709050, "6490096567781310487", 2720610798)
+-- Tower Simulator 3 - VTBS Airport (AppID: 2729880)
+addappid(2729880)
+addappid(2729880, 1, "d27766fd9cfdae58c3b9e8a14ffd199a897077231915f946a038f6d80b9064bc") -- Tower Simulator 3 - VTBS Airport - Depot 2729880
+setManifestid(2729880, "4407282197415976251", 1625468053)
+-- Tower Simulator 3 - KDAL Airport (AppID: 2777340)
+addappid(2777340)
+addappid(2777340, 1, "b67b82ed17215576f99db7b9f6f65cb954c264060b483eaf58b5a3c72767a8c7") -- Tower Simulator 3 - KDAL Airport - Depot 2777340
+setManifestid(2777340, "6385786406115393277", 694044690)
+-- Tower Simulator 3 - EKCH Airport (AppID: 2800220)
+addappid(2800220)
+addappid(2800220, 1, "ef475ce40b413cdc6fee5f8d497020c183fe1373b5f682bc34f2085f111ba8a5") -- Tower Simulator 3 - EKCH Airport - Depot 2800220
+setManifestid(2800220, "3828541718274826047", 1556286944)
+-- Tower Simulator 3 - KSTL Airport (AppID: 2825490)
+addappid(2825490)
+addappid(2825490, 1, "792dcc1093259db79a390be76f95116b7793e1df811f9711be58476fe49d039c") -- Tower Simulator 3 - KSTL Airport - Depot 2825490
+setManifestid(2825490, "2207614968689064397", 1547952306)
+-- Tower Simulator 3 - KCVG Airport (AppID: 2860930)
+addappid(2860930)
+addappid(2860930, 1, "bb01e5d6de69d8aaecdd1d66eaabcceb726e3bc7f5f7645d70e1d5cc382a2974") -- Tower Simulator 3 - KCVG Airport - Depot 2860930
+setManifestid(2860930, "2878496487040397725", 912169743)
+-- Tower Simulator 3 - MMPR Airport (AppID: 2860940)
+addappid(2860940)
+addappid(2860940, 1, "05b987cfb4f15c555c4a4bbeab08492b7e035ccc4e838158af318ea75aeff38f") -- Tower Simulator 3 - MMPR Airport - Depot 2860940
+setManifestid(2860940, "9021961229381012714", 657596791)
+-- Tower Simulator 3 - KMCO Airport (AppID: 2909660)
+addappid(2909660)
+addappid(2909660, 1, "b02a26477c80c0b01db64621f76208f9272a654c931e58888a9ef5f472fe5b77") -- Tower Simulator 3 - KMCO Airport - Depot 2909660
+setManifestid(2909660, "8160475925771222646", 1789317457)
+-- Tower Simulator 3 - LPPT Airport (AppID: 2918110)
+addappid(2918110)
+addappid(2918110, 1, "e14377aea628d7b1c37221ea126c94bec4ac7a923523f91ba2f79498c6dbe514") -- Tower Simulator 3 - LPPT Airport - Depot 2918110
+setManifestid(2918110, "6045841485178518406", 1875329109)
+-- Tower Simulator 3 - KIAH Airport (AppID: 2944400)
+addappid(2944400)
+addappid(2944400, 1, "345e8981e4a6d69c9d09885ad82a2265e089e6472b8bcadd316115b0d491e292") -- Tower Simulator 3 - KIAH Airport - Depot 2944400
+setManifestid(2944400, "6463486157125593719", 1519286629)
+-- Tower Simulator 3 - RJTT Airport (AppID: 2971840)
+addappid(2971840)
+addappid(2971840, 1, "34206465cc93576fbee98b188f649d76c01cf439e9b84a285d8c3fcd6d63a81f") -- Tower Simulator 3 - RJTT Airport - Depot 2971840
+setManifestid(2971840, "525161681194227596", 1448390886)
+-- Tower Simulator 3 - KPIT Airport (AppID: 2994000)
+addappid(2994000)
+addappid(2994000, 1, "0b7129f14cd5483420ac4f793e550bb392a1bd64f69c90b8235dd19bb59f48dd") -- Tower Simulator 3 - KPIT Airport - Depot 2994000
+setManifestid(2994000, "9188627342646490592", 1443878582)
+-- Tower Simulator 3 - KONT Airport (AppID: 3015060)
+addappid(3015060)
+addappid(3015060, 1, "59e0c1a34bc70abc6fdfb4bfee1603878e8e29ca5f8fcded1ca518d3aed45df0") -- Tower Simulator 3 - KONT Airport - Depot 3015060
+setManifestid(3015060, "4491894681345861546", 1000175152)
+-- Tower Simulator 3 - KDTW Airport (AppID: 3052770)
+addappid(3052770)
+addappid(3052770, 1, "8050dafd65dafa5d484fe4d880e72fe9d5cb38ffb3097919284e83870fb177ae") -- Tower Simulator 3 - KDTW Airport - Depot 3052770
+setManifestid(3052770, "3433644910092300685", 1432553993)
+-- Tower Simulator 3 - EGKK Airport (AppID: 3073920)
+addappid(3073920)
+addappid(3073920, 1, "790f9c0bf3782b0b4245d90cd87075db46bdb4f5b8542c0385b526867151ed05") -- Tower Simulator 3 - EGKK Airport - Depot 3073920
+setManifestid(3073920, "7325071393041146232", 1265155040)
+-- Tower Simulator 3 - EHAM Airport (AppID: 3252030)
+addappid(3252030)
+addappid(3252030, 1, "987da4a6d5c7598676e47329dea0a8a33aeb8bcac8b6247849aab9ae09bedb3e") -- Tower Simulator 3 - EHAM Airport - Depot 3252030
+setManifestid(3252030, "5133975639353588036", 2211187916)
+-- Tower Simulator 3 - KSLC Airport (AppID: 3361800)
+addappid(3361800)
+addappid(3361800, 1, "54904fd1de27b414c06a9b310a2bf6c4a889dccc06595d5f66e1ebe7ba9a75d2") -- Tower Simulator 3 - KSLC Airport - Depot 3361800
+setManifestid(3361800, "8979608932781967705", 1636806887)
+-- MIYO for Tower Simulator 3 (AppID: 3412390)
+addappid(3412390)
+addappid(3412390, 1, "37e17674a05b51d07bbd80281c5d2d375fd403c39f888e847d32366b1bd099a6") -- MIYO for Tower Simulator 3 - Depot 3412390
+setManifestid(3412390, "5637267186622276311", 45047024)
+-- Tower Simulator 3 - KTUS Airport (AppID: 3457840)
+addappid(3457840)
+addappid(3457840, 1, "4348b0d910e4acdf3c55b9b1e7088c4ba9455e4c73ab3cfc4ba8fa7d2592ea4f") -- Tower Simulator 3 - KTUS Airport - Depot 3457840
+setManifestid(3457840, "7002951479532233594", 1455065293)
+-- Tower Simulator 3 - EDDM Airport (AppID: 3493210)
+addappid(3493210)
+addappid(3493210, 1, "bf3f7b11c5ddc58888fc77339adf0964ff3f98063a9890f4878e8547703e589a") -- Tower Simulator 3 - EDDM Airport - Depot 3493210
+setManifestid(3493210, "600652570698171015", 859869361)
+-- Tower Simulator 3 - CYYZ Airport (AppID: 3504000)
+addappid(3504000)
+addappid(3504000, 1, "0d931f2855f1c23609633cbf10a6439b3dedfbf01fb6e172b41cd8369a51f30c") -- Tower Simulator 3 - CYYZ Airport - Depot 3504000
+setManifestid(3504000, "1067639855291453619", 1226556897)
+-- Tower Simulator 3 - KRIC Airport (AppID: 3546350)
+addappid(3546350)
+addappid(3546350, 1, "34a538ea5b2ec4129102322b53660fd865701e97a12502c30a9a15dc37f49d11") -- Tower Simulator 3 - KRIC Airport - Depot 3546350
+setManifestid(3546350, "6495177242103157529", 1293653586)
+-- Tower Simulator 3 - KDEN Airport (AppID: 3579270)
+addappid(3579270)
+addappid(3579270, 1, "454723f48425f65546cb652d8be6e9bd78bf0f2476b3c38e451e06cc9540c5d6") -- Tower Simulator 3 - KDEN Airport - Depot 3579270
+setManifestid(3579270, "8320104620515259900", 4216606146)
+-- Tower Simulator 3 - KJFK Airport (AppID: 3613420)
+addappid(3613420)
+addappid(3613420, 1, "fbcd4dd3a445a22ee5440900d50971a8c166fcc8f3630d228292889a6cd5547f") -- Tower Simulator 3 - KJFK Airport - Depot 3613420
+setManifestid(3613420, "1965211302639787832", 3330827036)
+-- Tower Simulator 3 - KPHL Airport (AppID: 3632920)
+addappid(3632920)
+addappid(3632920, 1, "2425f5d8f1b396e2e8237a68bc7e15d6fdd0456d013c9032c600d0c6abaf7b8c") -- Tower Simulator 3 - KPHL Airport - Depot 3632920
+setManifestid(3632920, "8722664079738242", 2992396028)
+-- Tower Simulator 3 - KSEA Airport (AppID: 3632970)
+addappid(3632970)
+addappid(3632970, 1, "b5b275b59954e33dfe62a725ba9f15ea53f121d28bb83e7a4e2e14d87bb882f0") -- Tower Simulator 3 - KSEA Airport - Depot 3632970
+setManifestid(3632970, "2873326579955207077", 874849799)
+-- Tower Simulator 3 - KCLE Airport (AppID: 3734640)
+addappid(3734640)
+addappid(3734640, 1, "9fde90219519e8eca62429f2b13595596f59821d5e7853f61273d26c051b79ff") -- Tower Simulator 3 - KCLE Airport - Depot 3734640
+setManifestid(3734640, "5898286687982120817", 2030462108)
+-- Tower Simulator 3 - KPVD Airport (AppID: 3751440)
+addappid(3751440)
+addappid(3751440, 1, "def975c45bfffc1b64dad57b9c42c03203fe436576384cbd609e273db4384409") -- Tower Simulator 3 - KPVD Airport - Depot 3751440
+setManifestid(3751440, "4906901284920494287", 2573815988)
+-- Tower Simulator 3 - KSAN Airport (AppID: 3774470)
+addappid(3774470)
+addappid(3774470, 1, "04e032026bf749c8c125a33349ba853dbe99cfc45dd4e6bd7403035c7d559c4d") -- Tower Simulator 3 - KSAN Airport - Depot 3774470
+setManifestid(3774470, "1939390608078026767", 1873138967)
+-- Tower Simulator 3 - EGCC Airport (AppID: 3816780)
+addappid(3816780)
+addappid(3816780, 1, "177168ef03a3b20edbf74181a30ac744aec53019d395e4279d4f16478c0c8b4c") -- Tower Simulator 3 - EGCC Airport - Depot 3816780
+setManifestid(3816780, "7352544708504681076", 2527659134)
+-- Tower Simulator 3 - KIAD Airport (AppID: 3854530)
+addappid(3854530)
+addappid(3854530, 1, "bc5360cd97acb9e64d1e3e8b1ce78a889b851fcc27d49f5cc0bbf123e9219449") -- Tower Simulator 3 - KIAD Airport - Depot 3854530
+setManifestid(3854530, "3468366038142863458", 2495540309)
+-- Tower Simulator 3 - NZAA Airport (AppID: 3880160)
+addappid(3880160)
+addappid(3880160, 1, "a1615c592033bb4937bf927e59c3655baade701d9a4bad5f4a64127ae9f5667b") -- Tower Simulator 3 - NZAA Airport - Depot 3880160
+setManifestid(3880160, "6444035693534934144", 2421687729)
+-- Tower Simulator 3 - KDCA Airport (AppID: 3913330)
+addappid(3913330)
+addappid(3913330, 1, "a9566e36257cbc57ce03d955db9fc3cf38a71057be3d53604b5baa952c8f1e6e") -- Tower Simulator 3 - KDCA Airport - Depot 3913330
+setManifestid(3913330, "1485621837432894689", 1111926676)
+-- Tower Simulator 3 - KDFW Airport (AppID: 3985560)
+addappid(3985560)
+addappid(3985560, 1, "9c1d9a78a1cb5f52974886df51691b6f4cfc2a3f279d33ecdafd79175890bc8b") -- Tower Simulator 3 - KDFW Airport - Depot 3985560
+setManifestid(3985560, "4436711321132458544", 3191586233)
+-- Tower Simulator 3 - YSSY Airport (AppID: 4019120)
+addappid(4019120)
+addappid(4019120, 1, "c680a0dccd714d6337729f35ab6fb9388aad16178711caf55a0bb217594ea699") -- Tower Simulator 3 - YSSY Airport - Depot 4019120
+setManifestid(4019120, "126843289707712489", 3119418329)
+-- Tower Simulator 3 - EDDV Airport (AppID: 4043970)
+addappid(4043970)
+addappid(4043970, 1, "5b257e323e70fa9fbf1a7893e7df2025f0e3a9035890f7c1ff2b6d96bf6af9a0") -- Tower Simulator 3 - EDDV Airport - Depot 4043970
+setManifestid(4043970, "2030410725551502330", 2862732636)
+-- Tower Simulator 3 - KEWR Airport (AppID: 4094140)
+addappid(4094140)
+addappid(4094140, 1, "8af54ccb8216ce8a82ac8f4dee86e2217bd51fb5fb3fbbe5f06c14e3538e40c4") -- Tower Simulator 3 - KEWR Airport - Depot 4094140
+setManifestid(4094140, "3858279193218250037", 4854407332)
+-- Tower Simulator 3 - LFPG Airport (AppID: 4118890)
+addappid(4118890)
+addappid(4118890, 1, "e2566c2f3eeb2ba64333a05aaa21937916866596825b07be6004cc784807c568") -- Tower Simulator 3 - LFPG Airport - Depot 4118890
+setManifestid(4118890, "7465141419613601178", 2770180696)
+-- Tower Simulator 3 - EDDH Airport (AppID: 4140350)
+addappid(4140350)
+addappid(4140350, 1, "64bb7fbb7d9fbfd865270cf7b867bc6577bf10a8cd0d8720073c01c903151733") -- Tower Simulator 3 - EDDH Airport - Depot 4140350
+setManifestid(4140350, "825320910166678994", 3668158509)
+-- Tower Simulator 3 - KORD Airport (AppID: 4165790)
+addappid(4165790)
+addappid(4165790, 1, "65ac3998445d7e508aa46d49f3428c62af1278a6881406e246da9d9440ec4b1f") -- Tower Simulator 3 - KORD Airport - Depot 4165790
+setManifestid(4165790, "3567620203574987848", 5473497404)
+-- Tower Simulator 3 - CYVR Airport (AppID: 4197480)
+addappid(4197480)
+addappid(4197480, 1, "09d4c4b0e80257403ac33b6eada345a84511ae3ba6df2c32bcf0dc3c86a312a8") -- Tower Simulator 3 - CYVR Airport - Depot 4197480
+setManifestid(4197480, "2341153021877863626", 2416587624)
+-- Tower Simulator 3 - EDDB Airport (AppID: 4208750)
+addappid(4208750)
+addappid(4208750, 1, "ffd5593c99a903b6f767c9b0cb390fb6f1eba287e6514007450381a453d12186") -- Tower Simulator 3 - EDDB Airport - Depot 4208750
+setManifestid(4208750, "3535964075266476324", 1258233705)
+-- Tower Simulator 3 - PANC Airport (AppID: 4235050)
+addappid(4235050)
+addappid(4235050, 1, "eeef90ed69d37d714e0ddd1a572ed860d239f2fa23dd78e7aec7c54ae3269b4d") -- Tower Simulator 3 - PANC Airport - Depot 4235050
+setManifestid(4235050, "7577425922211696370", 4435610741)
+-- Tower Simulator 3 - LGAV Airport (AppID: 4241250)
+addappid(4241250)
+addappid(4241250, 1, "dc837bbbb7547083dd40c98ca1170d3a88558b223b92587ddc367f4209b16b1d") -- Tower Simulator 3 - LGAV Airport - Depot 4241250
+setManifestid(4241250, "6312959161844884288", 1914644154)
+-- Tower Simulator 3 - WSSS Airport (AppID: 4359950)
+addappid(4359950)
+addappid(4359950, 1, "8c425ffdb06cf88e57dd7f0e7cec8ddc6b23cf6ae0492f46deb8323f1bfb8e3b") -- Tower Simulator 3 - WSSS Airport - Depot 4359950
+setManifestid(4359950, "3745962685843200742", 3609787461)
+-- Tower Simulator 3 - EDDL Airport (AppID: 4406770)
+addappid(4406770)
+addappid(4406770, 1, "9180de52f6ae4d78d381de0d2092648ba32d24360a2da7c21482a3e03aa00003") -- Tower Simulator 3 - EDDL Airport - Depot 4406770
+setManifestid(4406770, "2178869262588377113", 3192517533)
+-- Tower Simulator 3 - ZSPD Airport (AppID: 4486110)
+addappid(4486110)
+addappid(4486110, 1, "8c122d685cf4281b9036d5a0eeb11da44070ccc250e86488548a40172c379f96") -- Tower Simulator 3 - ZSPD Airport - Depot 4486110
+setManifestid(4486110, "1331330967804765070", 2472211307)
+-- Tower Simulator 3 - EHEH Airport (AppID: 4553720)
+addappid(4553720)
+addappid(4553720, 1, "0766118a212c3af37ee7e9843408814413c24379c5cb92725d2eaee95cf6bce2") -- Tower Simulator 3 - EHEH Airport - Depot 4553720
+setManifestid(4553720, "6029167747071933570", 2175597060)
+-- Tower Simulator 3 - YBBN Airport (AppID: 4583990)
+addappid(4583990)
+addappid(4583990, 1, "9f58a0fc6fb7505e193b576f4a3ac3fb421954441fc82a259c9d1d42593b58ea") -- Tower Simulator 3 - YBBN Airport - Depot 4583990
+setManifestid(4583990, "2072508024828001335", 1974650982)
+-- Tower Simulator 3 - EBBR Airport (AppID: 4616440)
+addappid(4616440)
+addappid(4616440, 1, "5a79913f44f313ff7a377ba6f1cd4a39c341ea94d0dbb2f1d7629acc720c694d") -- Tower Simulator 3 - EBBR Airport - Depot 4616440
+setManifestid(4616440, "1504556251707552782", 5730989168)
+-- Tower Simulator 3 - CYYC Airport (AppID: 4684690)
+addappid(4684690)
+addappid(4684690, 1, "cc59cdb411913ec72f703c67d2fd65affe647662c7d54d9395e8d767dc9a20e6") -- Tower Simulator 3 - CYYC Airport - Depot 4684690
+setManifestid(4684690, "7354389865126866909", 4557243283)
