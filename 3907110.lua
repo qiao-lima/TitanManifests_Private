83f80ca0277e6d48f7befa751ed8b53548f10bfe
qiao-lima <qiao-lima@github.com>
2026-06-04 01:29:27 +0800

    Manual Update 3907110

---
+++ 3907110.lua
@@ -0,0 +1,12 @@
+-- 3907110's Lua and Manifest Created by Hubcap Manifest
+-- Up Cards
+-- Created: June 03, 2026 at 06:21:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3907110) -- Up Cards
+-- MAIN APP DEPOTS
+addappid(3907111, 1, "4d1fcb7239744575b4e4376cf3e85627fb8acf6e74d28f41ab0f5915deb792d9") -- Depot 3907111
+setManifestid(3907111, "5329774096725963431", 251458148)
