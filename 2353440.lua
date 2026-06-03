a3ac849351f22f0953619fc17fd077e9c1025eda
qiao-lima <qiao-lima@github.com>
2026-06-04 01:29:23 +0800

    Manual Update 2353440

---
+++ 2353440.lua
@@ -0,0 +1,12 @@
+-- 2353440's Lua and Manifest Created by Hubcap Manifest
+-- Carry Onward
+-- Created: June 03, 2026 at 05:34:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2353440) -- Carry Onward
+-- MAIN APP DEPOTS
+addappid(2353441, 1, "d01d5cbe7bb8a8d949dc53eabd34f2554316ec81b600c83ed7a6b4f431bea0c4") -- Depot 2353441
+setManifestid(2353441, "4618437864517658665", 2381571728)
