f5fa5ffb70e7f08b463581ace31f4e8ce1c1caf5
qiao-lima <qiao-lima@github.com>
2026-06-04 01:33:16 +0800

    Manual Update 4728990

---
+++ 4728990.lua
@@ -0,0 +1,14 @@
+-- 4728990's Lua and Manifest Created by Hubcap Manifest
+-- 白色归途
+-- Created: June 03, 2026 at 13:27:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4728990) -- 白色归途
+-- MAIN APP DEPOTS
+addappid(4728991, 1, "c7fc32e1938b2d996ceb335943aca1ba65c54f0ccfaf8623997783991c267d9a") -- Depot 4728991
+setManifestid(4728991, "1131688774796443531", 376141455)
+addappid(4728992, 1, "ff65a86e239531017bd4822a450f899b5369dbb1bb3c5671726d8a10ce6141cf") -- Depot 4728992
+setManifestid(4728992, "5267693787546788561", 460307673)
