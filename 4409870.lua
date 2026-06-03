6a10a5b438906b328442d89ad81c844d33e50446
qiao-lima <qiao-lima@github.com>
2026-06-04 01:33:15 +0800

    Manual Update 4409870

---
+++ 4409870.lua
@@ -0,0 +1,14 @@
+-- 4409870's Lua and Manifest Created by Hubcap Manifest
+-- Slots & Diapers
+-- Created: June 03, 2026 at 09:18:03 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4409870) -- Slots & Diapers
+-- MAIN APP DEPOTS
+addappid(4409871, 1, "6ea2d2ec53ed50488a427551bab306302893d3e7bc59666d70ff81abe5c58bdc") -- Depot 4409871
+setManifestid(4409871, "8330514833690967803", 693090570)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4761840) -- Slots  Diapers - Supporter Pack
