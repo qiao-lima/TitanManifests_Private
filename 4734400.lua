387d91b950b04bfaad6c1f4fac946614e98228d3
qiao-lima <qiao-lima@github.com>
2026-06-04 01:33:16 +0800

    Manual Update 4734400

---
+++ 4734400.lua
@@ -0,0 +1,12 @@
+-- 4734400's Lua and Manifest Created by Hubcap Manifest
+-- BAFF A
+-- Created: June 03, 2026 at 06:22:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4734400) -- BAFF A
+-- MAIN APP DEPOTS
+addappid(4734401, 1, "a200479280b67b6fc4b89d1720cf953a524b31e896708b31af0a990f5f666a6d") -- Depot 4734401
+setManifestid(4734401, "2993696951230985038", 43412806)
