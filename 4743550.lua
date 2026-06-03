d84603fe66677e7a32ff19fc4b89a8c95d125abd
qiao-lima <qiao-lima@github.com>
2026-06-04 01:33:16 +0800

    Manual Update 4743550

---
+++ 4743550.lua
@@ -0,0 +1,12 @@
+-- 4743550's Lua and Manifest Created by Hubcap Manifest
+-- Cakey & Tanaka : CandyLand Adventure
+-- Created: June 03, 2026 at 13:28:03 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4743550) -- Cakey & Tanaka : CandyLand Adventure
+-- MAIN APP DEPOTS
+addappid(4743551, 1, "ca0da0d6e004416191983d313786b63c86f90d25d0bda4b41bcdd54c2d8bae4a") -- Depot 4743551
+setManifestid(4743551, "6766335404311608720", 600861581)
