dcb4a6452e4217a947fbda30eb490537add41cba
qiao-lima <qiao-lima@github.com>
2026-06-04 20:52:06 +0800

    Manual Update 3850380

---
+++ 3850380.lua
@@ -0,0 +1,14 @@
+-- 3850380's Lua and Manifest Created by Hubcap Manifest
+-- VR Waifu - Ciel
+-- Created: March 14, 2026 at 17:42:08 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3850380) -- VR Waifu - Ciel
+-- MAIN APP DEPOTS
+addappid(3850381, 1, "6e4297e377a7ef6959417639895adf0a65d9444e4d43a0b6711fbc6310d7a627") -- Depot 3850381
+setManifestid(3850381, "4014986069662039291", 1409953471)
+addappid(3850382, 1, "e4a92bfe11ee811eb4c632d52f1d248197d393bc80ff76d784e0e642fc4002e2") -- Depot 3850382
+setManifestid(3850382, "4390935952808231093", 1485837813)
