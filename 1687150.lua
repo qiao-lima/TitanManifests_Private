323ee76fa54a60d15d85b8222798f61bf4c52280
qiao-lima <qiao-lima@github.com>
2026-06-04 20:52:00 +0800

    Manual Update 1687150

---
+++ 1687150.lua
@@ -0,0 +1,12 @@
+-- 1687150's Lua and Manifest Created by Hubcap Manifest
+-- Leaf Me Alone
+-- Created: June 03, 2026 at 18:38:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1687150) -- Leaf Me Alone
+-- MAIN APP DEPOTS
+addappid(1687151, 1, "a812726338bcbecf21e04f1601020f179b129518f3ee547a597a97f7b55eeaad") -- Depot 1687151
+setManifestid(1687151, "6090337001115915711", 3607746573)
