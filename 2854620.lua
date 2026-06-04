72a061422b2cbf176bcedbe5e125e3e023813b6b
qiao-lima <qiao-lima@github.com>
2026-06-04 20:52:02 +0800

    Manual Update 2854620

---
+++ 2854620.lua
@@ -0,0 +1,12 @@
+-- 2854620's Lua and Manifest Created by Hubcap Manifest
+-- SCAD
+-- Created: June 04, 2026 at 05:09:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2854620) -- SCAD
+-- MAIN APP DEPOTS
+addappid(2854621, 1, "80dfea33bb931e57256b8e366acd88644e53887538827de09d76c6f6b59e7a2b") -- Depot 2854621
+setManifestid(2854621, "2238692133668701807", 1322344094)
