8b20b6e37f3ed7870de285753ada5a9ab7f56b2a
qiao-lima <qiao-lima@github.com>
2026-06-04 20:52:00 +0800

    Manual Update 1864060

---
+++ 1864060.lua
@@ -0,0 +1,13 @@
+-- 1864060's Lua and Manifest Created by Hubcap Manifest
+-- Mirror Lover
+-- Created: June 04, 2026 at 07:11:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1864060) -- Mirror Lover
+addtoken(1864060, "514342717787602674")
+-- MAIN APP DEPOTS
+addappid(1864061, 1, "b708c47eaa933d9e3e8bbb6481d1e6d96281e4a1a4ac7b4357a299d43a34150b") -- Depot 1864061
+setManifestid(1864061, "9055000400269313238", 119909086)
