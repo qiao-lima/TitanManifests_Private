a862258f81cbfb6cc71215af17bf45c40b266c9d
qiao-lima <qiao-lima@github.com>
2026-06-04 20:52:05 +0800

    Manual Update 3797670

---
+++ 3797670.lua
@@ -0,0 +1,12 @@
+-- 3797670's Lua and Manifest Created by Hubcap Manifest
+-- Latin Shadows - The Visitor
+-- Created: June 03, 2026 at 18:21:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3797670) -- Latin Shadows - The Visitor
+-- MAIN APP DEPOTS
+addappid(3797671, 1, "6079fb257f86f38235db3f9b61f7e6bbb58fccaf620993c8a4c9799f5a960df8") -- Depot 3797671
+setManifestid(3797671, "1287283438777059859", 426022240)
