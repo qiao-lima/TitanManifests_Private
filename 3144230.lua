ee28e8a01865d017b54b91239f1d02d55d3d54ad
qiao-lima <qiao-lima@github.com>
2026-06-04 20:52:03 +0800

    Manual Update 3144230

---
+++ 3144230.lua
@@ -0,0 +1,19 @@
+-- 3144230's Lua and Manifest Created by Hubcap Manifest
+-- Modern Warships
+-- Created: June 04, 2026 at 06:57:37 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 6
+
+-- MAIN APPLICATION
+addappid(3144230) -- Modern Warships
+-- MAIN APP DEPOTS
+addappid(3144231, 1, "614f8f7a5ab52672063010fe405f2e3a11ec0309c6783fe2a6e3a4e4748a6f1c") -- Depot 3144231
+setManifestid(3144231, "7272886543050182326", 14384732831)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4584640) -- Modern Warships - Fast progress Pack
+addappid(4584650) -- Modern Warships - Battle Readiness Pack
+addappid(4584660) -- Modern Warships - Deep Strike Pack
+addappid(4584670) -- Modern Warships - Submarine Nemesis Bundle
+addappid(4584680) -- Modern Warships - Sky Dominion Pack
+addappid(4584690) -- Modern Warships - Missile Dominance Bundle
