6d5423549b24af8a3914eeca30348258b1970416
qiao-lima <qiao-lima@github.com>
2026-06-04 20:52:01 +0800

    Manual Update 2110690

---
+++ 2110690.lua
@@ -0,0 +1,12 @@
+-- 2110690's Lua and Manifest Created by Hubcap Manifest
+-- EXIT The Game – Trail of the Griffin
+-- Created: June 03, 2026 at 18:38:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2110690) -- EXIT The Game – Trail of the Griffin
+-- MAIN APP DEPOTS
+addappid(2110691, 1, "18e424e35e6025ba539341ad7321a6b40a21ccc49de7bd6ba9f76a69336b016d") -- Depot 2110691
+setManifestid(2110691, "3866590128620569484", 332296889)
