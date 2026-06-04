52ce52dc1ba18b9325eea76032a1c66ae406240b
qiao-lima <qiao-lima@github.com>
2026-06-04 20:56:20 +0800

    Manual Update 4650380

---
+++ 4650380.lua
@@ -0,0 +1,12 @@
+-- 4650380's Lua and Manifest Created by Hubcap Manifest
+-- にょきにょき スポッとチンアナゴ
+-- Created: June 04, 2026 at 07:08:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4650380) -- にょきにょき スポッとチンアナゴ
+-- MAIN APP DEPOTS
+addappid(4650381, 1, "f96ae296c792297b132a4ca36ddda5de6d82d3d3ffb16696282bd547b9576cd5") -- Depot 4650381
+setManifestid(4650381, "1587396473470431243", 151257525)
