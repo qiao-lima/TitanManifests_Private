bcc3ac38e894297e85fddf1ceb676cf2e9e80018
qiao-lima <qiao-lima@github.com>
2026-06-04 09:08:02 +0800

    Manual Update 4305480

---
+++ 4305480.lua
@@ -0,0 +1,12 @@
+-- 4305480's Lua and Manifest Created by Hubcap Manifest
+-- IncreKnight
+-- Created: June 03, 2026 at 14:10:37 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4305480) -- IncreKnight
+-- MAIN APP DEPOTS
+addappid(4305481, 1, "a03cf32d828a7326274da5e5ce4bd6fd2b26bfd0263cda3979bd8a9cded35137") -- Depot 4305481
+setManifestid(4305481, "511475739166710958", 309867972)
