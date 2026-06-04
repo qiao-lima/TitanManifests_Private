2a11ec390d6432562a015d31e289d035ed4c2e62
qiao-lima <qiao-lima@github.com>
2026-06-04 20:56:17 +0800

    Manual Update 4071230

---
+++ 4071230.lua
@@ -0,0 +1,12 @@
+-- 4071230's Lua and Manifest Created by Hubcap Manifest
+-- Memory Draw
+-- Created: June 04, 2026 at 01:31:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4071230) -- Memory Draw
+-- MAIN APP DEPOTS
+addappid(4071231, 1, "ab102a29506be28f4f3e502b2b88b9834eef71300e18aca846949f7bf7a699c6") -- Depot 4071231
+setManifestid(4071231, "3025714810456533938", 285320397)
