a38ba6c1c4db8bb31d49ab6143418fa4e7c01621
qiao-lima <qiao-lima@github.com>
2026-06-04 20:56:20 +0800

    Manual Update 4672340

---
+++ 4672340.lua
@@ -0,0 +1,14 @@
+-- 4672340's Lua and Manifest Created by Hubcap Manifest
+-- Ink & Void
+-- Created: June 04, 2026 at 07:11:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4672340) -- Ink & Void
+-- MAIN APP DEPOTS
+addappid(4672341, 1, "fadde089468827cdc7b7fabec11ebba01d5f033e23365da250aa4df6376df3e3") -- Depot 4672341
+setManifestid(4672341, "8453409029030810564", 1019030067)
+addappid(4672342, 1, "d23b4b467d1b2d40900831df7a5de0f6b150760249e7b9f1c97c825476522eee") -- Depot 4672342
+setManifestid(4672342, "5865372029509103158", 934519319)
