ff39a702234d64514e81f4140cdd0eb75f32fbb1
qiao-lima <qiao-lima@github.com>
2026-06-04 20:56:21 +0800

    Manual Update 4711890

---
+++ 4711890.lua
@@ -0,0 +1,12 @@
+-- 4711890's Lua and Manifest Created by Hubcap Manifest
+-- Aim/Flash Trainer
+-- Created: June 03, 2026 at 18:40:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4711890) -- Aim/Flash Trainer
+-- MAIN APP DEPOTS
+addappid(4711891, 1, "cefd99cfeb3df199fb6298ca7d75afa8516402d8c4d06616af98b040e7b3942d") -- Depot 4711891
+setManifestid(4711891, "6053506142509705076", 221291690)
