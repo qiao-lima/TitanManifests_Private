97a3ef1fcc0e699065432d0e9126c552721ed726
qiao-lima <qiao-lima@github.com>
2026-06-04 20:56:21 +0800

    Manual Update 4688580

---
+++ 4688580.lua
@@ -0,0 +1,12 @@
+-- 4688580's Lua and Manifest Created by Hubcap Manifest
+-- Fun Park Simulator: Prologue
+-- Created: June 03, 2026 at 18:22:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4688580) -- Fun Park Simulator: Prologue
+-- MAIN APP DEPOTS
+addappid(4688581, 1, "54d8653f22958f1eaba389d05abf4b0cc9e89d32730ae147a7336f24b213e0ce") -- Depot 4688581
+setManifestid(4688581, "8487482286846164789", 10057069736)
