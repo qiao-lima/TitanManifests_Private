c621f6e30418d30df71c7b5f2bf158f075ebf144
qiao-lima <qiao-lima@github.com>
2026-06-04 20:56:19 +0800

    Manual Update 4551520

---
+++ 4551520.lua
@@ -0,0 +1,14 @@
+-- 4551520's Lua and Manifest Created by Hubcap Manifest
+-- Your Daddy
+-- Created: June 03, 2026 at 18:41:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4551520) -- Your Daddy
+-- MAIN APP DEPOTS
+addappid(4551521, 1, "50908bcd31ff0ba8f230397139b677eb198b205e8e3bee8fb86e15018ccb2560") -- Depot 4551521
+setManifestid(4551521, "8704176310573960212", 204730082)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4551620) -- Your Daddy - Supporter Pack
