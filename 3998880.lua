e037e4c3661d60e3285ea38344c3d88c6191fdfe
qiao-lima <qiao-lima@github.com>
2026-06-04 20:52:06 +0800

    Manual Update 3998880

---
+++ 3998880.lua
@@ -0,0 +1,12 @@
+-- 3998880's Lua and Manifest Created by Hubcap Manifest
+-- SEXシミュレーター for Kikyo
+-- Created: June 03, 2026 at 18:27:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3998880) -- SEXシミュレーター for Kikyo
+-- MAIN APP DEPOTS
+addappid(3998881, 1, "101b85a63001472ff043ef7664e34674fd42f8b71e3cd04a4d36b2a298afc7ba") -- Depot 3998881
+setManifestid(3998881, "636136493832379946", 318729048)
