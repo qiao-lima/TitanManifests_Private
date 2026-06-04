d7ee85d9873f68755ad3d5e55cda5b2f79c3b11b
qiao-lima <qiao-lima@github.com>
2026-06-04 20:56:17 +0800

    Manual Update 4036720

---
+++ 4036720.lua
@@ -0,0 +1,12 @@
+-- 4036720's Lua and Manifest Created by Hubcap Manifest
+-- Scropia
+-- Created: June 04, 2026 at 07:08:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4036720) -- Scropia
+-- MAIN APP DEPOTS
+addappid(4036721, 1, "1cc68c6c5b20b73d7afae3a6145a7879003575c84cf225a156c745d7b19626c6") -- Depot 4036721
+setManifestid(4036721, "7182163733794003633", 324342414)
