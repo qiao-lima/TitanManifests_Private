e70596b850480963b1bbf8b9a1fc5fb099fff601
qiao-lima <qiao-lima@github.com>
2026-06-04 20:52:04 +0800

    Manual Update 3385710

---
+++ 3385710.lua
@@ -0,0 +1,22 @@
+-- 3385710's Lua and Manifest Created by Hubcap Manifest
+-- WASTED Bloodline
+-- Created: June 04, 2026 at 07:13:53 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 5
+-- Total DLCs: 0
+-- Shared Depots: 4
+
+-- MAIN APPLICATION
+addappid(3385710) -- WASTED Bloodline
+-- MAIN APP DEPOTS
+addappid(3385711, 1, "e4d237c31cfbe41eb2fab437ab5053f1c3e235ca97803b0f0db2d5f12035251c") -- Depot 3385711
+setManifestid(3385711, "6624971235921219953", 73816729713)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 117381405)
+addappid(229033, 1, "9a789440bbc16131547ac6c062e5f168f7684da533a2899a71fdfaaeec21efc3") -- PhysX System Software 9.14.0702 (Shared from App 228980)
+setManifestid(229033, "2059065101492814639", 60039803)
