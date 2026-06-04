fe8c101ad33f2d06e6655c375fef9bb84240567d
qiao-lima <qiao-lima@github.com>
2026-06-04 20:56:18 +0800

    Manual Update 4172560

---
+++ 4172560.lua
@@ -0,0 +1,12 @@
+-- 4172560's Lua and Manifest Created by Hubcap Manifest
+-- Battle Simulator: Counter Terrorist
+-- Created: June 03, 2026 at 18:40:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4172560) -- Battle Simulator: Counter Terrorist
+-- MAIN APP DEPOTS
+addappid(4172561, 1, "7038e1221ea56e7204160a320d46f45488bf18811d624bbb51f60ebc87362c76") -- Depot 4172561
+setManifestid(4172561, "4400652482786807547", 290950010)
