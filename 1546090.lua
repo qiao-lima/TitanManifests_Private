7064e5f33ff7d1cf5e3808ab9e5134a35528883f
qiao-lima <qiao-lima@github.com>
2026-06-04 09:06:31 +0800

    Manual Update 1546090

---
+++ 1546090.lua
@@ -0,0 +1,19 @@
+-- 1546090's Lua and Manifest Created by Hubcap Manifest
+-- RAIDBORN
+-- Created: June 03, 2026 at 14:04:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 2
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1546090) -- RAIDBORN
+-- MAIN APP DEPOTS
+addappid(1546091, 1, "8b8d5dd73c5bb456fc7355fc459e36c0c5add8237d7258d81bbe67591a40473d") -- Depot 1546091
+setManifestid(1546091, "9048926261854706566", 2360238234)
+-- SHARED DEPOTS (from other apps)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2895520) -- RAIDBORN - Founders Pack
+addappid(4566940) -- RAIDBORN - Spellcraft  Wizardry
