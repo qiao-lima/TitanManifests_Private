425c879a08550a6c952310507c287e7958c73562
qiao-lima <qiao-lima@github.com>
2026-06-04 20:56:18 +0800

    Manual Update 4396620

---
+++ 4396620.lua
@@ -0,0 +1,14 @@
+-- 4396620's Lua and Manifest Created by Hubcap Manifest
+-- Unlucky Mummy
+-- Created: June 04, 2026 at 07:11:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4396620) -- Unlucky Mummy
+-- MAIN APP DEPOTS
+addappid(4396622, 1, "ba05b5af4f75df1bd5060566a908ddaf3f901cc196234f114d0d68162b9f160d") -- Depot 4396622
+setManifestid(4396622, "5634836395649972619", 373519854)
+addappid(4396621, 1, "d5fbf9159afb66092630b00b2d9bb289ba85a9fc24c668e1a926140999a36c2d") -- Depot 4396621
+setManifestid(4396621, "8034913639923942139", 358590597)
