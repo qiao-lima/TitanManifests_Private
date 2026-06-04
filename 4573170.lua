8304e8df97ff3c9b160fe3872350fdb988d782c3
qiao-lima <qiao-lima@github.com>
2026-06-04 20:56:20 +0800

    Manual Update 4573170

---
+++ 4573170.lua
@@ -0,0 +1,12 @@
+-- 4573170's Lua and Manifest Created by Hubcap Manifest
+-- 我是卷王
+-- Created: June 04, 2026 at 07:14:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4573170) -- 我是卷王
+-- MAIN APP DEPOTS
+addappid(4573171, 1, "7a89dd4437b804f2f953e4e7c3280caa9fce416c6eee528c9fe441f31612e224") -- Depot 4573171
+setManifestid(4573171, "2416950138864325647", 355323814)
