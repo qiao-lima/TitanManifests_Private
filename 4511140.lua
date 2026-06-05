e107c952858a333bf51bf7a3c13c09e9b510af6c
qiao-lima <qiao-lima@github.com>
2026-06-05 18:49:39 +0800

    Manual Update 4511140

---
+++ 4511140.lua
@@ -0,0 +1,8 @@
+-- htz11
+addappid(4511140)
+addappid(4511141,1,"72524cd199db63267dabc5a122cc91c14831d48c744dfeef15e3d63355ad54aa")
+addappid(4511520)
+addappid(4529880)
+addappid(4529890)
+addappid(4529900)
+addappid(4529910)
