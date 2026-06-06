669e33a1766b0c36ae05f44c0fdee242ad435c74
qiao-lima <qiao-lima@github.com>
2026-06-06 10:37:29 +0800

    Manual Update 4710400

---
+++ 4710400.lua
@@ -0,0 +1,14 @@
+-- 4710400's Lua and Manifest Created by Hubcap Manifest
+-- Deep Sea Fish: Hidden World
+-- Created: June 05, 2026 at 08:48:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4710400) -- Deep Sea Fish: Hidden World
+-- MAIN APP DEPOTS
+addappid(4710401, 1, "8cd699c8594d5cd1ede7133f82837f4857733fba7a558e0043d8ceefda3571ff") -- Depot 4710401
+setManifestid(4710401, "7326332496110894075", 215199776)
+addappid(4710402, 1, "ce98caf0839a8c9efe8e2ec82fcf58c2916bad3f87cde641efbfb525f54891d9") -- Depot 4710402
+setManifestid(4710402, "4280190140983573874", 276022663)
