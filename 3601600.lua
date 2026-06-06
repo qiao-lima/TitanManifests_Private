c70e6ec2cb713bcc5673759a0bb3f363f7e44163
qiao-lima <qiao-lima@github.com>
2026-06-06 10:33:21 +0800

    Manual Update 3601600

---
+++ 3601600.lua
@@ -0,0 +1,14 @@
+-- 3601600's Lua and Manifest Created by Hubcap Manifest
+-- Sudoku Bactron
+-- Created: June 05, 2026 at 22:31:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3601600) -- Sudoku Bactron
+-- MAIN APP DEPOTS
+addappid(3601601, 1, "bc41e8012c3a897432aecfd18e69b6b1334fbf3a2a6fe1a7aadf1dbaabad0a42") -- Depot 3601601
+setManifestid(3601601, "8272559283237913580", 156451197)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3601602) -- Depot 3601602 (empty depot)
