3da1ab04c2e07b85e8a4c5b369ceb6b79cfaa881
qiao-lima <qiao-lima@github.com>
2026-06-06 10:35:58 +0800

    Manual Update 4145320

---
+++ 4145320.lua
@@ -0,0 +1,18 @@
+-- 4145320's Lua and Manifest Created by Hubcap Manifest
+-- 異世界樹の巫女〜魔法のチカラでおさわりHやりたい放題〜
+-- Created: June 05, 2026 at 04:54:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(4145320) -- 異世界樹の巫女〜魔法のチカラでおさわりHやりたい放題〜
+-- MAIN APP DEPOTS
+addappid(4145321, 1, "0a626b21b0f08726b37cb2a81d0d5364ddd793a2940a9c14d8b085f4fb896627") -- Depot 4145321
+setManifestid(4145321, "267813889750478355", 4646700868)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- HHDLC (AppID: 4376120) - missing depot keys
+-- addappid(4376120)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4376120) -- Depot 4376120 (empty depot)
