e83979ac1d292a921b3088cec2c3d8a2cbeaa1e7
qiao-lima <qiao-lima@github.com>
2026-06-06 10:33:23 +0800

    Manual Update 3888660

---
+++ 3888660.lua
@@ -0,0 +1,12 @@
+-- 3888660's Lua and Manifest Created by Hubcap Manifest
+-- 《溺光症》
+-- Created: June 05, 2026 at 08:47:36 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3888660) -- 《溺光症》
+-- MAIN APP DEPOTS
+addappid(3888661, 1, "20ca0b88275d486a7bde409510d6c88e7436ec4fc3fc5970381d77d788bebce3") -- Depot 3888661
+setManifestid(3888661, "2620655708680476229", 2567326273)
