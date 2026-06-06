0aa023b03c505bcb2328bd67d954b147906b0dc3
qiao-lima <qiao-lima@github.com>
2026-06-06 10:33:23 +0800

    Manual Update 3818900

---
+++ 3818900.lua
@@ -0,0 +1,12 @@
+-- 3818900's Lua and Manifest Created by Hubcap Manifest
+-- プトリカ 2nd.cut:For the Exquisite Attire
+-- Created: June 05, 2026 at 14:31:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3818900) -- プトリカ 2nd.cut:For the Exquisite Attire
+-- MAIN APP DEPOTS
+addappid(3818901, 1, "b4db425385528f8b88c01dbb5795fe3f88499dbb7f737787063b53e09619d620") -- Depot 3818901
+setManifestid(3818901, "932970225458579510", 2620840840)
