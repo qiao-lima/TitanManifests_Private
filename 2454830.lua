bf1c29876a521a33ba6e4b229cf5b257630fe0a6
qiao-lima <qiao-lima@github.com>
2026-06-06 10:33:17 +0800

    Manual Update 2454830

---
+++ 2454830.lua
@@ -0,0 +1,14 @@
+-- 2454830's Lua and Manifest Created by Hubcap Manifest
+-- Purgatory Survivors
+-- Created: June 05, 2026 at 12:42:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2454830, 1, "6ccb8bbcec35745f6de09c782317d0f527125cfd14be13f346b73970f31f986b") -- Purgatory Survivors
+-- MAIN APP DEPOTS
+addappid(2454831, 1, "c97bf73ac92fb772eabf38894e68b05d4ee6715a04d1f803272ca6fb366b1110") -- Depot 2454831
+setManifestid(2454831, "109632768803171367", 282538282)
+addappid(2454833, 1, "c8177cd39f71bd200c9db81ae8ee9013d3b6857c2432e19acfae3b80931fded1") -- Depot 2454833
+setManifestid(2454833, "6250602425216629437", 325181428)
