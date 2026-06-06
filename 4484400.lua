63e3b14f003eb4a26163e6f47755f86b9097b25c
qiao-lima <qiao-lima@github.com>
2026-06-06 12:59:11 +0800

    Manual Update 4484400

---
+++ 4484400.lua
@@ -0,0 +1,12 @@
+-- 4484400's Lua and Manifest Created by Hubcap Manifest
+-- Horny Massage Clinic
+-- Created: June 05, 2026 at 08:48:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4484400) -- Horny Massage Clinic
+-- MAIN APP DEPOTS
+addappid(4484401, 1, "3cee1aa8b1c1e8767fceb1b5907f518643b7d3fd1876272fcd25a5ec4189c5d0") -- Depot 4484401
+setManifestid(4484401, "6155732645423312380", 1742521551)
