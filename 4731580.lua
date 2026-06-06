1099ba759cb995657e45ce3667572eafaa39051e
qiao-lima <qiao-lima@github.com>
2026-06-06 10:37:29 +0800

    Manual Update 4731580

---
+++ 4731580.lua
@@ -0,0 +1,14 @@
+-- 4731580's Lua and Manifest Created by Hubcap Manifest
+-- Aligned Hearts
+-- Created: June 05, 2026 at 08:48:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4731580) -- Aligned Hearts
+-- MAIN APP DEPOTS
+addappid(4731581, 1, "1d907e30a14392e98129cc2fb3ccc2960147630e0f923e6633a93eddf7fa44a8") -- Depot 4731581
+setManifestid(4731581, "7352432439625714252", 1050285030)
+addappid(4731582, 1, "aa4df6a818175bbf2df4ac0e7f16a49ba75a4d46d12a9ea0596cc9633c8d6fa3") -- Depot 4731582
+setManifestid(4731582, "5029270324387355932", 898596450)
