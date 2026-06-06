cbb9f36f8c9b692f4f099139be3814888725f3ea
qiao-lima <qiao-lima@github.com>
2026-06-06 10:33:18 +0800

    Manual Update 2700790

---
+++ 2700790.lua
@@ -0,0 +1,18 @@
+-- 2700790's Lua and Manifest Created by Hubcap Manifest
+-- Shepherd Knight
+-- Created: June 05, 2026 at 05:26:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2700790) -- Shepherd Knight
+-- MAIN APP DEPOTS
+addappid(2700791, 1, "6d5c50dae53309e0d9ad7c96b37aa940cda61d9d5ff0859f1496a1f40470c66e") -- Depot 2700791
+setManifestid(2700791, "4205869061499937691", 1769524209)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
