ecbf45ba6bfad1ac65cc8f9080057d4055b3a104
qiao-lima <qiao-lima@github.com>
2026-06-06 12:59:07 +0800

    Manual Update 1808500

---
+++ 1808500.lua
@@ -0,0 +1,24 @@
+-- 1808500's Lua and Manifest Created by Hubcap Manifest
+-- ARC Raiders
+-- Created: May 21, 2026 at 07:57:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 5
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1808500, 1, "a59b9e251d25a204d737248ec72a807bd702fc1da64364cf6e7fe99fc8b45015") -- ARC Raiders
+-- MAIN APP DEPOTS
+addappid(1808501, 1, "9f7c91d4609594120c713e82bb64d0ebf15c63fdf6887bc2b3f8f20fcf58e005") -- Depot 1808501
+setManifestid(1808501, "2543040172726059778", 52776621299)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3948500) -- ARC Raiders - The Astro Bundle (Pre-Order Exclusive)
+addappid(3948510) -- ARC Raiders - Upgrade to Deluxe Edition
+addappid(4048380) -- ARC Raiders - Standard Edition Bonus (Pre-Order Exclusive)
+addappid(4210150) -- ARC Raiders - The Bonecrown Set
+addappid(4424920) -- ARC Raiders - The Wasp Hunter Set
