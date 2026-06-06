75e785c687bb93fe403481dc07851ee100f32349
qiao-lima <qiao-lima@github.com>
2026-06-06 12:59:10 +0800

    Manual Update 3446140

---
+++ 3446140.lua
@@ -0,0 +1,16 @@
+-- 3446140's Lua and Manifest Created by Hubcap Manifest
+-- Horny Haunts
+-- Created: May 25, 2026 at 08:00:13 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3446140, 1, "fc6012d7d394d82e3804dd4fe42dd5d90863a5c6c7009c0ac40efb80b701d8a8") -- Horny Haunts
+-- MAIN APP DEPOTS
+addappid(3446141, 1, "c366e219537572ac4e5038e9e92cadc81ae2e8472f855fd0deaf6186ce7dd3b7") -- Depot 3446141
+setManifestid(3446141, "9012514305529293184", 452705390)
+addappid(3446142, 1, "3288fe4ee0557d9a0375ef0ba0e66acb1d6f3c3f8fef49262bc58d4db63ae11d") -- Depot 3446142
+setManifestid(3446142, "3031663548053129016", 438201220)
+addappid(3446143, 1, "19aa1378c593dbfa70387da040718f7088de111c698eb5f5c2047c0a3274d6ba") -- Depot 3446143
+setManifestid(3446143, "6822799243060801465", 455376988)
