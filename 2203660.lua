c86ff9a43ce25aff96d56c5fd621556bc356b4ef
qiao-lima <qiao-lima@github.com>
2026-06-06 10:33:17 +0800

    Manual Update 2203660

---
+++ 2203660.lua
@@ -0,0 +1,13 @@
+-- 2203660's Lua and Manifest Created by Hubcap Manifest
+-- Crispy Puppy
+-- Created: June 05, 2026 at 05:25:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2203660) -- Crispy Puppy
+addtoken(2203660, "6981441114162048041")
+-- MAIN APP DEPOTS
+addappid(2203661, 1, "cf86d8ae2a1eaa1db0eed0a992c268b5fc451c202fbe1da0efb243a772a66ce6") -- Depot 2203661
+setManifestid(2203661, "3687449946590912601", 13223135)
