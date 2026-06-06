566de78f2c0b7673ff120a17e45e4cdeb018143e
qiao-lima <qiao-lima@github.com>
2026-06-06 10:33:20 +0800

    Manual Update 3515530

---
+++ 3515530.lua
@@ -0,0 +1,20 @@
+-- 3515530's Lua and Manifest Created by Hubcap Manifest
+-- Ice Cream Simulator
+-- Created: June 05, 2026 at 19:01:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3515530) -- Ice Cream Simulator
+-- MAIN APP DEPOTS
+addappid(3515531, 1, "8721ca94ac600fb7dd0d888249c92b19f18ab29a40d64e6268a0fc24597b1c49") -- Depot 3515531
+setManifestid(3515531, "5051756673867264172", 10054847348)
+addappid(3515532, 1, "106cde3444d7cf039b83411e0a7a3ad81ebca4f3b80d09a3ba54cac8794419fc") -- Depot 3515532
+setManifestid(3515532, "5810730349472199828", 9932094528)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
