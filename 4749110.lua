c4456894b0790de0e77f0394ab14932708a7dc42
qiao-lima <qiao-lima@github.com>
2026-06-06 10:37:30 +0800

    Manual Update 4749110

---
+++ 4749110.lua
@@ -0,0 +1,12 @@
+-- 4749110's Lua and Manifest Created by Hubcap Manifest
+-- 再见犹丽迪茜
+-- Created: June 05, 2026 at 08:48:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4749110) -- 再见犹丽迪茜
+-- MAIN APP DEPOTS
+addappid(4749111, 1, "01a68dfa8991fd5ff33dd0aeca020418780500dfad2b8841cfaef49d3bf912ea") -- Depot 4749111
+setManifestid(4749111, "1949858673879674146", 643736777)
