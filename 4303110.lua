7211d889c1e06b7e9b0e6158a1fdea832ec524aa
qiao-lima <qiao-lima@github.com>
2026-06-06 10:36:00 +0800

    Manual Update 4303110

---
+++ 4303110.lua
@@ -0,0 +1,12 @@
+-- 4303110's Lua and Manifest Created by Hubcap Manifest
+-- Just One More Link
+-- Created: June 05, 2026 at 12:42:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4303110) -- Just One More Link
+-- MAIN APP DEPOTS
+addappid(4303111, 1, "a1b52d0be6ceb0408eb1380279fb65eaa1aa0e69aee2eaac36a4669e946b5c96") -- Depot 4303111
+setManifestid(4303111, "62621838726563244", 294844714)
