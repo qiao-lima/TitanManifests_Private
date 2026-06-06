20d27ce97ecceb82ff01f92f7bd3db423166ab50
qiao-lima <qiao-lima@github.com>
2026-06-06 12:59:11 +0800

    Manual Update 4144580

---
+++ 4144580.lua
@@ -0,0 +1,12 @@
+-- 4144580's Lua and Manifest Created by Hubcap Manifest
+-- Christian Mother
+-- Created: May 25, 2026 at 08:22:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4144580, 1, "40e93e0f52365eb5ccba1493208d7c18c648be97a2ee343fc47036dccb4d64f9") -- Christian Mother
+-- MAIN APP DEPOTS
+addappid(4144581, 1, "95b3e0e70197fcebdaee8bdebcb8d4ecf5f186ebad0386cd9b48e5bce13e91ad") -- Depot 4144581
+setManifestid(4144581, "8934412880973476438", 717649212)
