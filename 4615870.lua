f5dc42f8eb55306acc6b16b07dc94fac3937c63e
qiao-lima <qiao-lima@github.com>
2026-06-06 10:36:04 +0800

    Manual Update 4615870

---
+++ 4615870.lua
@@ -0,0 +1,15 @@
+-- 4615870's Lua and Manifest Created by Hubcap Manifest
+-- Kromex
+-- Created: June 05, 2026 at 13:02:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(4615870) -- Kromex
+-- MAIN APP DEPOTS
+addappid(4615871, 1, "1f68397cc6e01151064264c305671680944bf379b7966261f45e50f0f3df6007") -- Depot 4615871
+setManifestid(4615871, "5281742454967581614", 1867945240)
+-- EXCLUDED DLCS:
+-- UNRELEASED DLCS (COMMENTED OUT)
+-- addappid(4616570) -- Founder’s Pack (unreleased)
