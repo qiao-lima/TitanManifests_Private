a4b8290339ad55bd5c3782982759face4778dc75
qiao-lima <qiao-lima@github.com>
2026-06-06 10:33:18 +0800

    Manual Update 2752350

---
+++ 2752350.lua
@@ -0,0 +1,12 @@
+-- 2752350's Lua and Manifest Created by Hubcap Manifest
+-- Deep Crafter
+-- Created: June 05, 2026 at 14:17:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2752350) -- Deep Crafter
+-- MAIN APP DEPOTS
+addappid(2752351, 1, "eac4bc941dca81e3f6edcc38380fe0a36d2aa76905d04fe7f4700cb41b32eae8") -- Depot 2752351
+setManifestid(2752351, "4538516465211611123", 3239276405)
