afdb8d5de22c4a6940404a1fbaff197c9c41fe92
qiao-lima <qiao-lima@github.com>
2026-06-06 10:33:19 +0800

    Manual Update 3114080

---
+++ 3114080.lua
@@ -0,0 +1,12 @@
+-- 3114080's Lua and Manifest Created by Hubcap Manifest
+-- Vtuber Hack
+-- Created: June 05, 2026 at 04:56:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3114080) -- Vtuber Hack
+-- MAIN APP DEPOTS
+addappid(3114081, 1, "2f3f90ce29a84f4f5948ca68b53382f706059a10cb76acb24e072e15d8465032") -- Depot 3114081
+setManifestid(3114081, "2336334129718200310", 9036826080)
