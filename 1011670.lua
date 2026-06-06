cbbafe2a2c72183aa2094c77f9e866dce01bfaa1
qiao-lima <qiao-lima@github.com>
2026-06-06 10:33:16 +0800

    Manual Update 1011670

---
+++ 1011670.lua
@@ -0,0 +1,28 @@
+-- 1011670's Lua and Manifest Created by Hubcap Manifest
+-- Totally Reliable Delivery Service
+-- Created: February 02, 2026 at 05:44:49 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 4 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(1011670, 1, "a24885b8df5bebc235f6119da8f076013f25d039960ed211b418e492b54602ab") -- Totally Reliable Delivery Service
+-- MAIN APP DEPOTS
+addappid(1011671, 1, "d23736dfc0877aa4088d86500401f1b65cc5d9467ecb33d9e5afba783b4eb5fc") -- Totally Reliable Delivery Service Content
+setManifestid(1011671, "8226427331524149437", 4600550310)
+addappid(1011672, 1, "1f5d0b09fc2169c96ae4e2fb61f7486b9bb1266f5c83b79a8af5bc6b46dc12a1") -- Totally Reliable Delivery Service Mac
+setManifestid(1011672, "6281665369501361211", 4638520401)
+addappid(1011673, 1, "7c2fd0355b548710df053fa78871deb3801d2e3a0179a1f39efbedf3f461b495") -- Totally Reliable Delivery Service Linux
+setManifestid(1011673, "4043707151849059025", 4355628243)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1528440) -- Totally Reliable Delivery Service - Stunt Sets
+addappid(1528441) -- Totally Reliable Delivery Service - Cyberfunk
+addappid(1528442) -- Totally Reliable Delivery Service - Dress Code
+addappid(3708960) -- Totally Reliable Delivery Service - Atari Attire
+-- EXCLUDED DLCS:
+-- UNRELEASED DLCS (COMMENTED OUT)
+-- addappid(3708970) -- Totally Reliable Delivery Service - Mascot Mayhem (unreleased)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(1528440) -- Depot 1528440 (empty depot)
+-- addappid(1528441) -- Depot 1528441 (empty depot)
+-- addappid(1528442) -- Depot 1528442 (empty depot)
