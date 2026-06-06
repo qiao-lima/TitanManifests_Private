5f641560c6ece657a2b75a993ae8f4b6b0438ba5
qiao-lima <qiao-lima@github.com>
2026-06-06 12:59:11 +0800

    Manual Update 4380310

---
+++ 4380310.lua
@@ -0,0 +1,25 @@
+-- 4380310's Lua and Manifest Created by Hubcap Manifest
+-- Cat Cursor by Travellin Cats
+-- Created: May 06, 2026 at 13:37:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 3
+
+-- MAIN APPLICATION
+addappid(4380310) -- Cat Cursor by Travellin Cats
+-- MAIN APP DEPOTS
+addappid(4380311, 1, "56be6386b63d84b0fe4f49e20f907fdcce8f04593d5153d2965c06127c6a334b") -- Depot 4380311
+setManifestid(4380311, "1184696410641880858", 172556968)
+-- DLCS WITH DEDICATED DEPOTS
+-- Cat Cursor by Travellin Cats - Fantasy Pack (AppID: 4399960)
+addappid(4399960)
+addappid(4399960, 1, "7e13ecc419113b8e9e787bfedad3a0e763bf8dd9629612554252f8e5fd26d9f5") -- Cat Cursor by Travellin Cats - Fantasy Pack - Depot 4399960
+setManifestid(4399960, "4513967375709697710", 206207)
+-- Cat Cursor by Travellin Cats - Dog Pack (AppID: 4409840)
+addappid(4409840)
+addappid(4409840, 1, "0dd13f0d148b60af2967d52f491924fd58998c12208ff670e0b91db7e2d4aa73") -- Cat Cursor by Travellin Cats - Dog Pack - Depot 4409840
+setManifestid(4409840, "3502886999020471340", 131653)
+-- Cat Cursor by Travellin Cats - Wild Animals Pack (AppID: 4409930)
+addappid(4409930)
+addappid(4409930, 1, "4960edea39859121fbe2f1d1f312a1c65ec2dc9a69b20a06c9cc045c6fb7bbe6") -- Cat Cursor by Travellin Cats - Wild Animals Pack - Depot 4409930
+setManifestid(4409930, "7425759639562526046", 139306)
