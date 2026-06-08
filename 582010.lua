a9c509cede0a43314488f70a2adb0cdc723cb299
qiao-lima <qiao-lima@github.com>
2026-06-08 17:28:42 +0800

    Manual Update 582010

---
+++ 582010.lua
@@ -0,0 +1,231 @@
+-- 582010's Lua and Manifest Created by Hubcap Manifest
+-- Monster Hunter: World
+-- Created: September 30, 2025 at 01:34:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 8
+-- Total DLCs: 200
+
+-- MAIN APPLICATION
+addappid(582010, 1, "0891b9c128dc04f20718b3ff6be13d601741e81b7a80bec80d3b9b9f70542ab9") -- Monster Hunter: World
+-- MAIN APP DEPOTS
+addappid(582011, 1, "da6b97dc25e43b8f7ceaf9bcdbfba960f2e3e855b855fc47975d58a68d3f7688") -- MONSTER HUNTER: WORLD Content
+setManifestid(582011, "2525674006025364114", 54986406857)
+-- DLCS WITH DEDICATED DEPOTS
+-- Monster Hunter World Original Soundtrack (AppID: 912540)
+addappid(912540)
+addappid(912541, 1, "a2cc9c4397c1aba71953063b342b2638b301f795798475a721e0a5a6a1364433") -- Monster Hunter World Original Soundtrack - Monster Hunter: World Original Soundtrack [MP3]
+setManifestid(912541, "5399336268074654699", 501430731)
+addappid(912542, 1, "76df1f76ea37dc51a168f5a67bd7c40065400547a8718e720e95ea62c31b74ad") -- Monster Hunter World Original Soundtrack - Monster Hunter: World Original Soundtrack [FLAC]
+setManifestid(912542, "1502354477762701985", 1317005580)
+addappid(912543, 1, "6df3b3c4def3325e47ce14e517a6c73f67fe4ffb21de65aff6f86436ed6fcf04") -- Monster Hunter World Original Soundtrack - Monster Hunter: World Original Soundtrack [AAC]
+setManifestid(912543, "6740370550279539406", 631945401)
+addappid(912544, 1, "e4bffdaf951a478e6d7dedb9debb6df4c15f5fc1eb192ea202cfd62429302784") -- Monster Hunter World Original Soundtrack - モンスターハンター:ワールド オリジナル・サウンドトラック [MP3]
+setManifestid(912544, "8174987582984342919", 501430731)
+addappid(912545, 1, "10ea07ab2bb68959b90c669ef9095ae987df86ec1a52c2c5288bbf53c65e8211") -- Monster Hunter World Original Soundtrack - モンスターハンター:ワールド オリジナル・サウンドトラック [FLAC]
+setManifestid(912545, "930427278381079634", 1317005580)
+addappid(912546, 1, "9ec2135cac5b243039cb39f5f870348f507caba7abc5f5f4fe4526cadefd0fc3") -- Monster Hunter World Original Soundtrack - モンスターハンター:ワールド オリジナル・サウンドトラック [AAC]
+setManifestid(912546, "1399605360143729538", 631945401)
+-- Monster Hunter World - High Resolution Texture Pack (AppID: 960781)
+addappid(960781)
+addappid(582012, 1, "92d06583c95939873c5c33658cd8b1111fa209a4c7576207565e942fa78eb888") -- Monster Hunter World - High Resolution Texture Pack - MONSTER HUNTER: WORLD Depot
+setManifestid(582012, "5526886592091189537", 50644138247)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(762174) -- Monster Hunter World - Gesture Devil May Cry Dual Guns
+addappid(762918) -- Monster Hunter World - Sticker Set Devil May Cry Set
+addappid(762919) -- Monster Hunter World - Sticker Set Mega Man Set
+addappid(763695) -- Monster Hunter World - Hairstyle Provisions Manager
+addappid(763696) -- Monster Hunter World - Hairstyle Field Team Leader
+addappid(763697) -- Monster Hunter World - Hairstyle The Handler
+addappid(763698) -- Monster Hunter World - Hairstyle The Admiral
+addappid(763699) -- Monster Hunter World - The Handlers Guildmarm Costume
+addappid(770970) -- Monster Hunter World - The Handlers Astera 3 Star Chef Coat
+addappid(770972) -- Monster Hunter World - The Handlers Busy Bee Dress
+addappid(770973) -- Monster Hunter World - The Handlers Sunshine Pareo
+addappid(770974) -- Monster Hunter World - The Handlers Mischievous Dress
+addappid(770975) -- Monster Hunter World - The Handlers Winter Spirit Coat
+addappid(770976) -- Monster Hunter World - The Handlers Friendly Felyne Costume
+addappid(770977) -- Monster Hunter World - Free Character Edit Voucher
+addappid(861870) -- Monster Hunter World - Character Edit Voucher Single Voucher
+addappid(861871) -- Monster Hunter World - Character Edit Voucher Two-Voucher Pack
+addappid(861872) -- Monster Hunter World - Character Edit Voucher Three-Voucher Pack
+addappid(891730) -- Monster Hunter World - Deluxe Kit
+addappid(896580) -- Monster Hunter World - Additional Gesture Bundle 1
+addappid(974302) -- Monster Hunter World - Free Sticker Set Mingle Hunter
+addappid(996740) -- Monster Hunter World - Free Gesture Happy Hunting
+addappid(1118010) -- Monster Hunter World Iceborne
+addappid(1118012) -- Monster Hunter World Iceborne Deluxe Kit
+addappid(1118013) -- Monster Hunter World - Palico Edit Voucher Single Voucher
+addappid(1118014) -- Monster Hunter World - Palico Edit Voucher Two-Voucher Pack
+addappid(1118015) -- Monster Hunter World - Palico Edit Voucher Three-Voucher Pack
+addappid(1118017) -- Monster Hunter World - Character  Palico Edit Voucher Single Voucher
+addappid(1118018) -- Monster Hunter World - Character  Palico Edit Voucher Two-Voucher Pack
+addappid(1118019) -- Monster Hunter World - Character  Palico Edit Voucher Three-Voucher Pack
+addappid(1118022) -- Monster Hunter World - Sticker Set Hunter Items Set
+addappid(1118023) -- Monster Hunter World - Sticker Set Monster Statuses Set
+addappid(1118024) -- Monster Hunter World - Pose Set Crouching
+addappid(1118025) -- Monster Hunter World - Pose Set Unique
+addappid(1118026) -- Monster Hunter World - The Handlers Kokoto Gals Costume
+addappid(1118029) -- Monster Hunter World Iceborne - MHWI Sticker Set Iceborne Monsters Set
+addappid(1211170) -- Monster Hunter World Iceborne - MHWI Room Decor Cute Decor Set
+addappid(1211171) -- Monster Hunter World Iceborne - MHWI Room Decor Giant Stuffed Doll Set
+addappid(1211173) -- Monster Hunter World Iceborne - MHWI Music Player Additional BGM Set Vol. 1
+addappid(1211174) -- Monster Hunter World - The Handlers Tyrant Costume
+addappid(1211175) -- Monster Hunter World - Sticker Set Raccoon City Set
+addappid(1211176) -- Monster Hunter World Iceborne - MHWI Monster Figure Nergigante
+addappid(1211177) -- Monster Hunter World Iceborne - MHWI Monster Figure Kushala Daora
+addappid(1211178) -- Monster Hunter World Iceborne - MHWI Monster Figure Dodogama
+addappid(1211179) -- Monster Hunter World Iceborne - MHWI Monster Figure Rathalos
+addappid(1211200) -- Monster Hunter World Iceborne - MHWI Monster Figure Anjanath
+addappid(1211201) -- Monster Hunter World Iceborne - MHWI Monster Figure Tobi-Kadachi
+addappid(1211202) -- Monster Hunter World Iceborne - MHWI Monster Figure Barioth
+addappid(1211203) -- Monster Hunter World Iceborne - MHWI Monster Figure Banbaro
+addappid(1211204) -- Monster Hunter World Iceborne - MHWI Monster Figure Boaboa
+addappid(1211205) -- Monster Hunter World Iceborne - MHWI Music Player Raccoon City Collaboration - Black Impact
+addappid(1211206) -- Monster Hunter World - The Handlers Graceful Short Dress
+addappid(1211207) -- Monster Hunter World - The Handlers Techno Handler Costume
+addappid(1211208) -- Monster Hunter World Iceborne - Hairstyle Wild Pompadour
+addappid(1211209) -- Monster Hunter World Iceborne - Hairstyle Mysterious Samurai
+addappid(1241260) -- Monster Hunter World Iceborne - Hairstyle Artful Buzz
+addappid(1241261) -- Monster Hunter World Iceborne - Hairstyle Hime Cut
+addappid(1241262) -- Monster Hunter World Iceborne - Hairstyle Light  Wavy
+addappid(1241263) -- Monster Hunter World Iceborne - Hairstyle Semi-long Up
+addappid(1241264) -- Monster Hunter World Iceborne - Hairstyle Great Mohawk
+addappid(1241265) -- Monster Hunter World Iceborne - Hairstyle Pleasant Ponytail
+addappid(1241266) -- Monster Hunter World Iceborne - Hairstyle Short Bob
+addappid(1241267) -- Monster Hunter World Iceborne - Hairstyle Long  Wavy
+addappid(1241268) -- Monster Hunter World - MHWI Gesture Pack Swag Dance Set
+addappid(1241269) -- Monster Hunter World - MHWI Gesture Pack Clean Dance Set
+addappid(1241270) -- Monster Hunter World - MHWI Gesture Pack Hype Up Set
+addappid(1241271) -- Monster Hunter World - Pose Set Weapon Pose (3)
+addappid(1241272) -- Monster Hunter World - Sticker Set Lynian Set
+addappid(1241273) -- Monster Hunter World Iceborne - Pendant Stuffed Felyne Teddy
+addappid(1241274) -- Monster Hunter World Iceborne - Pendant Stuffed Melynx Teddy
+addappid(1241275) -- Monster Hunter World Iceborne - Pendant White Felyne Teddy
+addappid(1241276) -- Monster Hunter World Iceborne - Pendant Pink Felyne Teddy
+addappid(1241277) -- Monster Hunter World Iceborne - Pendant Grape Felyne Teddy
+addappid(1241278) -- Monster Hunter World Iceborne - Pendant Mint Felyne Teddy
+addappid(1241279) -- Monster Hunter World Iceborne - Pendant Orange Felyne Teddy
+addappid(1241280) -- Monster Hunter World Iceborne - Pendant Moly Pendant - Luck
+addappid(1241281) -- Monster Hunter World Iceborne - Pendant Moly Pendant - Health
+addappid(1241282) -- Monster Hunter World Iceborne - Pendant Moly Pendant - Safety
+addappid(1241283) -- Monster Hunter World Iceborne - Pendant Moly Pendant - Love
+addappid(1241284) -- Monster Hunter World Iceborne - Pendant Moly Pendant - Protection
+addappid(1241285) -- Monster Hunter World Iceborne - Pendant Moly Pendant - Passion
+addappid(1241286) -- Monster Hunter World Iceborne - Pendant Moly Pendant - Peace
+addappid(1241287) -- Monster Hunter World Iceborne - MHWI Room Decor Intimate Decor Set
+addappid(1241288) -- Monster Hunter World Iceborne - MHW I Room Decor Lighting Set
+addappid(1241289) -- Monster Hunter World Iceborne - MHWI Special Monster Figure Grand Appreciation Fest
+addappid(1241290) -- Monster Hunter World Iceborne - MHWI Monster Figure Velkhana
+addappid(1241291) -- Monster Hunter World Iceborne - MHWI Monster Figure Legiana
+addappid(1241292) -- Monster Hunter World Iceborne - MHWI Monster Figure Beotodus
+addappid(1241293) -- Monster Hunter World Iceborne - MHWI Monster Figure Nargacuga
+addappid(1241294) -- Monster Hunter World Iceborne - MHWI Monster Figure Pukei-Pukei
+addappid(1241295) -- Monster Hunter World Iceborne - MHWI Monster Figure Grimalkyne
+addappid(1241296) -- Monster Hunter World Iceborne - MHWI Monster Figure Glavenus
+addappid(1241297) -- Monster Hunter World Iceborne - MHWI Monster Figure Rathian
+addappid(1241298) -- Monster Hunter World Iceborne - MHWI Monster Figure Kulu-Ya-Ku
+addappid(1241299) -- Monster Hunter World Iceborne - MHWI Music Player Additional BGM Set Vol. 2
+addappid(1241300) -- Monster Hunter World Iceborne - MHWI Music Player Additional BGM Set Vol. 3
+addappid(1242740) -- Monster Hunter World - The Handlers Rose Vestido
+addappid(1242741) -- Monster Hunter World Iceborne - Hairstyle Commander
+addappid(1242742) -- Monster Hunter World Iceborne - Hairstyle Excitable A-Lister
+addappid(1242743) -- Monster Hunter World Iceborne - Hairstyle Analytics Director
+addappid(1242744) -- Monster Hunter World Iceborne - Hairstyle The Seeker
+addappid(1242745) -- Monster Hunter World Iceborne - Hairstyle Serious Handler
+addappid(1242746) -- Monster Hunter World Iceborne - Hairstyle Third Fleet Master
+addappid(1242747) -- Monster Hunter World Iceborne - Hairstyle The Tracker
+addappid(1242748) -- Monster Hunter World - Sticker Set Friendly Greetings Set
+addappid(1242749) -- Monster Hunter World Iceborne - Pendant Pukei Strap
+addappid(1242750) -- Monster Hunter World Iceborne - Pendant Coral Pukei Strap
+addappid(1242751) -- Monster Hunter World Iceborne - Pendant Swinging Rajang
+addappid(1242752) -- Monster Hunter World Iceborne - Pendant Swinging Furious Rajang
+addappid(1242753) -- Monster Hunter World Iceborne - Pendant Strollin Paolumu
+addappid(1242754) -- Monster Hunter World Iceborne - Pendant Strollin Nightshade
+addappid(1242755) -- Monster Hunter World Iceborne - Pendant Flying Meduso
+addappid(1242756) -- Monster Hunter World Iceborne - Pendant Flying Meduso Colony
+addappid(1242757) -- Monster Hunter World Iceborne - Pendant Red Balloon
+addappid(1242758) -- Monster Hunter World Iceborne - Pendant Red  White Balloons
+addappid(1242759) -- Monster Hunter World Iceborne - Pendant Rainbow Balloons
+addappid(1242760) -- Monster Hunter World Iceborne - MHWI Room Decor Splendid Decor Set
+addappid(1242761) -- Monster Hunter World Iceborne - MHWI Room Decor Lil Bit of Glamour Decor Set
+addappid(1242762) -- Monster Hunter World Iceborne - MHWI Room Decor Mini Model Set
+addappid(1242763) -- Monster Hunter World Iceborne - MHWI Monster Figure Zinogre
+addappid(1242764) -- Monster Hunter World Iceborne - MHWI Monster Figure Yian Garuga
+addappid(1242765) -- Monster Hunter World Iceborne - MHWI Monster Figure Girros  Great Girros
+addappid(1242766) -- Monster Hunter World Iceborne - MHWI Monster Figure Namielle
+addappid(1242767) -- Monster Hunter World Iceborne - MHWI Monster Figure Paolumu
+addappid(1242768) -- Monster Hunter World Iceborne - MHWI Monster Figure Tzitzi-Ya-Ku
+addappid(1242769) -- Monster Hunter World Iceborne - MHWI Monster Figure Jyuratodus
+addappid(1242770) -- Monster Hunter World Iceborne - MHWI Monster Figure Barroth
+addappid(1242771) -- Monster Hunter World Iceborne - MHWI Monster Figure Diablos
+addappid(1242772) -- Monster Hunter World Iceborne - MHWI Monster Figure Safijiiva
+addappid(1242773) -- Monster Hunter World Iceborne - MHWI Monster Figure Xenojiiva
+addappid(1242774) -- Monster Hunter World Iceborne - MHWI Monster Figure Wulg
+addappid(1242775) -- Monster Hunter World Iceborne - MHWI Music Player Additional BGM Set Vol. 4
+addappid(1242776) -- Monster Hunter World Iceborne - MHWI Music Player Additional BGM Set Vol. 5
+addappid(1242777) -- Monster Hunter World Iceborne - Hairstyle Half Ponytail
+addappid(1242778) -- Monster Hunter World Iceborne - Hairstyle Fluffy Mop
+addappid(1242779) -- Monster Hunter World Iceborne - Hairstyle Rath-a-like
+addappid(1265470) -- Monster Hunter World Iceborne - Hairstyle Wandering Samurai
+addappid(1265471) -- Monster Hunter World Iceborne - Pendant Emerald Kulve Heart
+addappid(1265472) -- Monster Hunter World Iceborne - Pendant Ruby Kulve Heart
+addappid(1265473) -- Monster Hunter World Iceborne - Pendant Crystal Kulve Heart
+addappid(1265474) -- Monster Hunter World Iceborne - Pendant Topaz Kulve Heart
+addappid(1265475) -- Monster Hunter World Iceborne - Pendant Sapphire Kulve Heart
+addappid(1265476) -- Monster Hunter World Iceborne - Pendant Rosegem Kulve Heart
+addappid(1287050) -- Monster Hunter World - The Handlers Festive Samba Costume
+addappid(1287051) -- Monster Hunter World Iceborne - Pendant Gold Heavenly Dragon
+addappid(1287052) -- Monster Hunter World Iceborne - Pendant Silver Heavenly Dragon
+addappid(1287053) -- Monster Hunter World Iceborne - Pendant Ruby Crystal Knife
+addappid(1287054) -- Monster Hunter World Iceborne - Pendant Amber Crystal Knife
+addappid(1287055) -- Monster Hunter World Iceborne - Pendant Azure Crystal Knife
+addappid(1287056) -- Monster Hunter World Iceborne - Pendant Phantom Azure Butterflies
+addappid(1287057) -- Monster Hunter World Iceborne - Pendant Phantom Jade Butterflies
+addappid(1287058) -- Monster Hunter World Iceborne - Pendant Phantom Magenta Butterflies
+addappid(1287059) -- Monster Hunter World Iceborne - Pendant Fulgurbugs
+addappid(1287060) -- Monster Hunter World Iceborne - Pendant Dracophage Bugs
+addappid(1287061) -- Monster Hunter World Iceborne - Pendant Velkhana Snow Talisman
+addappid(1287062) -- Monster Hunter World Iceborne - MHWI Room Decor Poogie Set
+addappid(1287063) -- Monster Hunter World Iceborne - MHWI Room Decor Felyne Set
+addappid(1287064) -- Monster Hunter World Iceborne - MHWI Room Decor Pukei-Pukei Set
+addappid(1287065) -- Monster Hunter World Iceborne - MHWI Room Decor Boaboa Set
+addappid(1287066) -- Monster Hunter World Iceborne - MHWI Monster Figure Tigrex
+addappid(1287067) -- Monster Hunter World Iceborne - MHWI Monster Figure Odogaron
+addappid(1287068) -- Monster Hunter World Iceborne - MHWI Monster Figure Radobaan
+addappid(1287069) -- Monster Hunter World Iceborne - MHWI Monster Figure Rajang
+addappid(1287070) -- Monster Hunter World Iceborne - MHWI Monster Figure Kirin
+addappid(1287071) -- Monster Hunter World Iceborne - MHWI Monster Figure Vaal Hazak
+addappid(1287072) -- Monster Hunter World Iceborne - MHWI Monster Figure Teostra
+addappid(1287073) -- Monster Hunter World Iceborne - MHWI Monster Figure Gajalakas  King Gajalaka
+addappid(1287074) -- Monster Hunter World Iceborne - MHWI Monster Figure Brachydios
+addappid(1287075) -- Monster Hunter World Iceborne - MHWI Monster Figure Lavasioth
+addappid(1287076) -- Monster Hunter World Iceborne - MHWI Monster Figure Downy Crakes  Aptonoths
+addappid(1287081) -- Monster Hunter World Iceborne - MHWI Music Player Additional BGM Set Vol. 6
+addappid(1287082) -- Monster Hunter World Iceborne - MHWI Music Player Additional BGM Set Vol. 7
+addappid(1287083) -- Monster Hunter World - Gesture Hadoken
+addappid(1351860) -- Monster Hunter World - Gesture Shoryuken
+addappid(1351861) -- Monster Hunter World - Sticker Set Street Fighter V
+addappid(1371400) -- Monster Hunter World - The Handlers Cute Demoness Costume
+addappid(1371401) -- Monster Hunter World Iceborne - Pendant Mechanical Gold Watch
+addappid(1371402) -- Monster Hunter World Iceborne - Pendant Mechanical Silver Watch
+addappid(1371403) -- Monster Hunter World Iceborne - Pendant Blazing Glavenus Candle
+addappid(1371404) -- Monster Hunter World Iceborne - Pendant Shocked Kulu-Ya-Ku
+addappid(1371405) -- Monster Hunter World Iceborne - Pendant Super-8 Mini (Player 1)
+addappid(1371406) -- Monster Hunter World Iceborne - Pendant Super-8 Mini (Player 2)
+addappid(1371407) -- Monster Hunter World Iceborne - Pendant Heavenly Hog
+addappid(1371408) -- Monster Hunter World Iceborne - Pendant Beelzeboar
+addappid(1371409) -- Monster Hunter World Iceborne - Pendant Meowscular Gains Chain
+addappid(1371420) -- Monster Hunter World Iceborne - MHWI Room Decor Beotodus Skull
+addappid(1371421) -- Monster Hunter World Iceborne - MHWI Monster Figure Kulve Taroth
+addappid(1371422) -- Monster Hunter World Iceborne - MHWI Monster Figure Bazelgeuse
+addappid(1371423) -- Monster Hunter World Iceborne - MHWI Monster Figure Deviljho
+addappid(1371424) -- Monster Hunter World Iceborne - MHWI Monster Figure Shara Ishvalda 1
+addappid(1371425) -- Monster Hunter World Iceborne - MHWI Monster Figure Shara Ishvalda 2
+addappid(1371426) -- Monster Hunter World Iceborne - MHWI Monster Figure Alatreon
+addappid(1371427) -- Monster Hunter World Iceborne - MHWI Monster Figure Fatalis
+addappid(1371428) -- Monster Hunter World Iceborne - MHWI Monster Figure Lunastra
+addappid(1371429) -- Monster Hunter World Iceborne - MHWI Monster Figure Uragaan
+addappid(1377530) -- Monster Hunter World - The Handlers Chun-Li Costume
+addappid(1390430) -- Monster Hunter World Iceborne - Pendant MH Riders - Kirin
+addappid(1472030) -- MHWI -  Free Content Collection Pack
+addtoken(1472030, "2817285949295010895")
