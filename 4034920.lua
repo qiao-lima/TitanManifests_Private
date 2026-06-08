67e3ed4bb450541395ebcd3a01d78198473f6df0
qiao-lima <qiao-lima@github.com>
2026-06-08 17:28:39 +0800

    Manual Update 4034920

---
+++ 4034920.lua
@@ -0,0 +1,12 @@
+-- 4034920's Lua and Manifest Created by Hubcap Manifest
+-- Pop Pop Potato
+-- Created: June 08, 2026 at 04:11:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4034920) -- Pop Pop Potato
+-- MAIN APP DEPOTS
+addappid(4034921, 1, "698b65f7b48b74b09f4b1300774af6d2cd7c1c2e23b4d07a6a08734b627f192f") -- Depot 4034921
+setManifestid(4034921, "1296501830112510517", 340001465)
