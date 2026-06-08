bda03d72a49c1772193a0b736ff502b42d6cdd63
qiao-lima <qiao-lima@github.com>
2026-06-08 08:49:03 +0800

    Manual Update 4250260

---
+++ 4250260.lua
@@ -0,0 +1,12 @@
+-- 4250260's Lua and Manifest Created by Hubcap Manifest
+-- Slop Fest
+-- Created: June 07, 2026 at 06:16:58 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4250260) -- Slop Fest
+-- MAIN APP DEPOTS
+addappid(4250261, 1, "1547d05deaf3bee461acd947b469668c12396029166836bf40177f71c0570290") -- Depot 4250261
+setManifestid(4250261, "3992528669137407537", 181166822)
