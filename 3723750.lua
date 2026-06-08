8319ba53d501771a832201b522b6f2b99b1754e5
qiao-lima <qiao-lima@github.com>
2026-06-08 08:48:48 +0800

    Manual Update 3723750

---
+++ 3723750.lua
@@ -0,0 +1,14 @@
+-- 3723750's Lua and Manifest Created by Hubcap Manifest
+-- Trading Card Inspector
+-- Created: June 07, 2026 at 14:08:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3723750) -- Trading Card Inspector
+-- MAIN APP DEPOTS
+addappid(3723751, 1, "992e6d7da94a7f791ad2541101970268f977fcc603293cf9f8a879792b412995") -- Depot 3723751
+setManifestid(3723751, "5332990599467438974", 705476833)
+addappid(3723752, 1, "711dc2fdb1887a7fbb06ac146ae5ed80f4b3f7592e2489177751cdb622560200") -- Depot 3723752
+setManifestid(3723752, "5373302997465546552", 722984293)
