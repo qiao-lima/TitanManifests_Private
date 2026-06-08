4f865bedc15ceee838d9ec4ab72c7c647c4f186f
qiao-lima <qiao-lima@github.com>
2026-06-08 17:28:39 +0800

    Manual Update 3894790

---
+++ 3894790.lua
@@ -0,0 +1,16 @@
+-- 3894790's Lua and Manifest Created by Hubcap Manifest
+-- Professor Simulator
+-- Created: June 08, 2026 at 04:11:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3894790) -- Professor Simulator
+-- MAIN APP DEPOTS
+addappid(3894791, 1, "fbd0a0ce68491460ba7ce60f99ccf2e105c76a0141aa4a06086207150baac802") -- Depot 3894791
+setManifestid(3894791, "8742446386175628527", 293762550)
+addappid(3894792, 1, "8dfe9bceba50227ce6313bfcd4528bbe7ebd57d251d137b7703fc65a2f41d913") -- Depot 3894792
+setManifestid(3894792, "2238733316489272985", 310393344)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4779240) -- Professor Simulator - Lab Supporter Pack
