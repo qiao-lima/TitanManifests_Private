695f69947b6dfa11fcade49000aab09575ca6dc4
qiao-lima <qiao-lima@github.com>
2026-06-08 08:49:03 +0800

    Manual Update 4231020

---
+++ 4231020.lua
@@ -0,0 +1,12 @@
+-- 4231020's Lua and Manifest Created by Hubcap Manifest
+-- Gum Squad
+-- Created: June 06, 2026 at 13:10:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4231020) -- Gum Squad
+-- MAIN APP DEPOTS
+addappid(4231021, 1, "327f9d1b74905f2c4572830d0bda348fbaa58996a54dda54e863d343d07a5291") -- Depot 4231021
+setManifestid(4231021, "827301485657254279", 115612905)
