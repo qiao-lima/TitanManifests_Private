f70bb80a80084b9d6693f0a73bf71278a8354818
qiao-lima <qiao-lima@github.com>
2026-06-08 08:48:59 +0800

    Manual Update 3845640

---
+++ 3845640.lua
@@ -0,0 +1,12 @@
+-- 3845640's Lua and Manifest Created by Hubcap Manifest
+-- My Isekai Hotel
+-- Created: June 06, 2026 at 19:58:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3845640) -- My Isekai Hotel
+-- MAIN APP DEPOTS
+addappid(3845641, 1, "92ccc1b6aac924fde03898c5fb90d85998392e90805a9bc6ff6d60a32c527108") -- Depot 3845641
+setManifestid(3845641, "2566054937438983802", 823698834)
