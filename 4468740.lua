454670e5aa0335c32f4473f22cf6f6b0f758d619
qiao-lima <qiao-lima@github.com>
2026-06-08 08:49:04 +0800

    Manual Update 4468740

---
+++ 4468740.lua
@@ -0,0 +1,14 @@
+-- 4468740's Lua and Manifest Created by Hubcap Manifest
+-- Countryside Cumsluts
+-- Created: June 06, 2026 at 07:06:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4468740, 1, "dd09c035e2513a9fc986dc178ddbfc9d5ac749ab99105b83787ba34d460aad5c") -- Countryside Cumsluts
+-- MAIN APP DEPOTS
+addappid(4468741, 1, "03f644dd9cba35ed212c4e7f9b29fd2a4a62bd67eeec10fef6217aec54ce38d1") -- Depot 4468741
+setManifestid(4468741, "2126493959146513714", 1481368907)
+addappid(4468742, 1, "7c577020c3022165f5c57c56f9cf2a565cfa810526798698a1799d60ebfd6f25") -- Depot 4468742
+setManifestid(4468742, "4569011911886291249", 1480304854)
