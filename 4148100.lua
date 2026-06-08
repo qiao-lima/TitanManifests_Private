ad06d240f66fae07b51203634c2a25b8fdf4c129
qiao-lima <qiao-lima@github.com>
2026-06-08 08:49:02 +0800

    Manual Update 4148100

---
+++ 4148100.lua
@@ -0,0 +1,16 @@
+-- 4148100's Lua and Manifest Created by Hubcap Manifest
+-- Splash Shot Golf Legend
+-- Created: June 06, 2026 at 07:06:13 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4148100) -- Splash Shot Golf Legend
+-- MAIN APP DEPOTS
+addappid(4148101, 1, "63e56e9367d1cff32a260c70f7f592eb6ac808b83449738d17f7a04085801e82") -- Depot 4148101
+setManifestid(4148101, "6972140710090783504", 421590401)
+addappid(4148102, 1, "c2989a3be58922ffe7a7ca999966acd92ef2ead4493d8563248a709947d59c55") -- Depot 4148102
+setManifestid(4148102, "3304201750047521865", 428848594)
+addappid(4148103, 1, "faf0d6a04ad0ffda8947cc1f5ffca0b8a02c084fd96189c5b7a444b81becf58e") -- Depot 4148103
+setManifestid(4148103, "4527650828787456876", 421297785)
