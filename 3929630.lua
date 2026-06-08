cbd319bbf9e321b2a9bdd7e235d791898e19f2a1
qiao-lima <qiao-lima@github.com>
2026-06-08 14:37:23 +0800

    Manual Update 3929630

---
+++ 3929630.lua
@@ -0,0 +1,12 @@
+-- 3929630's Lua and Manifest Created by Hubcap Manifest
+-- Terrinoth®: Heroes of Descent
+-- Created: June 07, 2026 at 15:38:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3929630) -- Terrinoth®: Heroes of Descent
+-- MAIN APP DEPOTS
+addappid(3929631, 1, "cc5ae73b4401e1efe532ade9c7f067050c936a8293d7c3db6ddcee3908d667d4") -- Depot 3929631
+setManifestid(3929631, "1969532369337470067", 8742909594)
