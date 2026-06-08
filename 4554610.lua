e3f7570a2f21a9d9e92bb96ccd95587533af3a01
qiao-lima <qiao-lima@github.com>
2026-06-08 17:28:40 +0800

    Manual Update 4554610

---
+++ 4554610.lua
@@ -0,0 +1,16 @@
+-- 4554610's Lua and Manifest Created by Hubcap Manifest
+-- Kaishain
+-- Created: June 08, 2026 at 05:27:30 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(4554610) -- Kaishain
+-- MAIN APP DEPOTS
+addappid(4554611, 1, "6196e9ae8bcd47bec38361f0dbdcabc474d6089dfb9fbaa97d584682744fd4f9") -- Depot 4554611
+setManifestid(4554611, "3108629227952420733", 2390780511)
+-- SHARED DEPOTS (from other apps)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
