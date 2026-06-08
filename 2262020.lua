2161f6886e5ef17352a9ebfdfcdfbe75ca924a57
qiao-lima <qiao-lima@github.com>
2026-06-08 17:28:38 +0800

    Manual Update 2262020

---
+++ 2262020.lua
@@ -0,0 +1,12 @@
+-- 2262020's Lua and Manifest Created by Hubcap Manifest
+-- Exo Rally Championship
+-- Created: June 08, 2026 at 04:11:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2262020) -- Exo Rally Championship
+-- MAIN APP DEPOTS
+addappid(2262021, 1, "f1efccb2f7d5fcb0a6ec0f6e16cf59b895bdfd32020d6effe5ea2378d092dc75") -- Depot 2262021
+setManifestid(2262021, "7598534005152670286", 7490030644)
