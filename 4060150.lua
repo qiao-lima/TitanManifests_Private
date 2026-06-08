edc41f655c0bfe322299e2e3a2b5d1d2fa91c639
qiao-lima <qiao-lima@github.com>
2026-06-08 17:28:40 +0800

    Manual Update 4060150

---
+++ 4060150.lua
@@ -0,0 +1,12 @@
+-- 4060150's Lua and Manifest Created by Hubcap Manifest
+-- 阎罗索命
+-- Created: June 08, 2026 at 05:27:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4060150) -- 阎罗索命
+-- MAIN APP DEPOTS
+addappid(4060151, 1, "4f409512b70ddd1e0ad48233221ab7bca89aad9688f90ee225f4f2f5d82377a8") -- Depot 4060151
+setManifestid(4060151, "7965293900847872477", 1128641922)
