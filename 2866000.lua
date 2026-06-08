fff6f800416c712a17ad815f84b51bf73874b936
qiao-lima <qiao-lima@github.com>
2026-06-08 08:48:32 +0800

    Manual Update 2866000

---
+++ 2866000.lua
@@ -0,0 +1,12 @@
+-- 2866000's Lua and Manifest Created by Hubcap Manifest
+-- Sword vs Horde
+-- Created: June 06, 2026 at 10:18:59 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2866000) -- Sword vs Horde
+-- MAIN APP DEPOTS
+addappid(2866001, 1, "f8118dfaed41163d5f27c3a1172d0953d657572ab83ade2aec3c6ca9d4476274") -- Depot 2866001
+setManifestid(2866001, "4146680165332080815", 74107376)
