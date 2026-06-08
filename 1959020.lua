f0e3cff91ff6973eeb36a16e1d2f47a5b2d08f4d
qiao-lima <qiao-lima@github.com>
2026-06-08 08:48:13 +0800

    Manual Update 1959020

---
+++ 1959020.lua
@@ -0,0 +1,12 @@
+-- 1959020's Lua and Manifest Created by Hubcap Manifest
+-- ERODE
+-- Created: June 06, 2026 at 04:18:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1959020) -- ERODE
+-- MAIN APP DEPOTS
+addappid(1959021, 1, "2e3a59444e01dd6770ff4e885e15db505ab597625750e1678285220a95936a0a") -- Depot 1959021
+setManifestid(1959021, "5658354504279858195", 130088071)
