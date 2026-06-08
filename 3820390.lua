ba33fc68416a653bff47f717fc5e9927c7035672
qiao-lima <qiao-lima@github.com>
2026-06-08 14:37:22 +0800

    Manual Update 3820390

---
+++ 3820390.lua
@@ -0,0 +1,12 @@
+-- 3820390's Lua and Manifest Created by Hubcap Manifest
+-- 東方異遇証 ~ What the Bullet Hell!
+-- Created: June 07, 2026 at 23:24:18 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3820390) -- 東方異遇証 ~ What the Bullet Hell!
+-- MAIN APP DEPOTS
+addappid(3820391, 1, "3df230da2e9a997e22435f921754dcb32ca8f104b37298ccafc8de7701958a58") -- Depot 3820391
+setManifestid(3820391, "4412574993736103832", 296325690)
