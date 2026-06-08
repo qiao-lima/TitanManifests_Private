0c79159fb5984af274ed94985790324d75b3b476
qiao-lima <qiao-lima@github.com>
2026-06-08 08:49:01 +0800

    Manual Update 4071090

---
+++ 4071090.lua
@@ -0,0 +1,12 @@
+-- 4071090's Lua and Manifest Created by Hubcap Manifest
+-- Tiny Town Market Simulator
+-- Created: June 06, 2026 at 20:00:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4071090) -- Tiny Town Market Simulator
+-- MAIN APP DEPOTS
+addappid(4071091, 1, "1e1018ce40d3576ca183a43feba3c8bce194dc2009ef609ccb29bb85cdc13017") -- Depot 4071091
+setManifestid(4071091, "3032589423023376482", 1196324516)
