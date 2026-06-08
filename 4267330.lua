5ce1989af62c5d3dea73e1fdcdbe85f3b67ff737
qiao-lima <qiao-lima@github.com>
2026-06-08 08:49:03 +0800

    Manual Update 4267330

---
+++ 4267330.lua
@@ -0,0 +1,12 @@
+-- 4267330's Lua and Manifest Created by Hubcap Manifest
+-- nurny battles HD
+-- Created: June 07, 2026 at 20:36:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4267330) -- nurny battles HD
+-- MAIN APP DEPOTS
+addappid(4267331, 1, "5c24a76c149f8ec9ae302f7f3b821b1762945c78d317315e3961d916ecad740b") -- Depot 4267331
+setManifestid(4267331, "8593023154190394505", 102494549)
