3d340937a2671a88c883de24a9c4e9e60fe39b45
qiao-lima <qiao-lima@github.com>
2026-06-08 14:37:23 +0800

    Manual Update 4726950

---
+++ 4726950.lua
@@ -0,0 +1,12 @@
+-- 4726950's Lua and Manifest Created by Hubcap Manifest
+-- 天门问剑
+-- Created: June 07, 2026 at 15:37:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4726950) -- 天门问剑
+-- MAIN APP DEPOTS
+addappid(4726951, 1, "359fea4b1676aeaf18425bde54011ff5263e5e5d2b1c0047f846fe37166014df") -- Depot 4726951
+setManifestid(4726951, "5343928788858295730", 588758034)
