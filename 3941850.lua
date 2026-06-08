d2c5a493dfbbfbc1fa1a427346608157c53d4c24
qiao-lima <qiao-lima@github.com>
2026-06-08 08:49:00 +0800

    Manual Update 3941850

---
+++ 3941850.lua
@@ -0,0 +1,18 @@
+-- 3941850's Lua and Manifest Created by Hubcap Manifest
+-- 阴界诡录·尸语公寓
+-- Created: June 06, 2026 at 22:46:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3941850, 1, "f995d1e148d2296d017684496a8fab2883ce14881dcf8078b213ef51258dbd99") -- 阴界诡录·尸语公寓
+-- MAIN APP DEPOTS
+addappid(3941851, 1, "98f1646c2d30ac898451a1e489b3d45cdce064dad5deecbe545765c943a00e02") -- Depot 3941851
+setManifestid(3941851, "194010332781827193", 1828981266)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
