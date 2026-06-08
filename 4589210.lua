229091db51edd18e75ded59caeb2fdbc943297b6
qiao-lima <qiao-lima@github.com>
2026-06-08 08:49:15 +0800

    Manual Update 4589210

---
+++ 4589210.lua
@@ -0,0 +1,12 @@
+-- 4589210's Lua and Manifest Created by Hubcap Manifest
+-- IT HEARS YOU
+-- Created: June 06, 2026 at 20:01:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4589210) -- IT HEARS YOU
+-- MAIN APP DEPOTS
+addappid(4589211, 1, "37ddd6c1ddec41a081fbf7086007cf22c3e2ede4572cbd81fcdf169c17423ec9") -- Depot 4589211
+setManifestid(4589211, "1585847283160998810", 2768266737)
