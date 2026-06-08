35fd75f66280b0853ba87cc5bf607a61c7035e21
qiao-lima <qiao-lima@github.com>
2026-06-08 08:48:47 +0800

    Manual Update 3596330

---
+++ 3596330.lua
@@ -0,0 +1,12 @@
+-- 3596330's Lua and Manifest Created by Hubcap Manifest
+-- 盟军远征: 地下世界
+-- Created: June 06, 2026 at 23:57:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3596330) -- 盟军远征: 地下世界
+-- MAIN APP DEPOTS
+addappid(3596331, 1, "089d4e268efd9e8f4bd1063faff1b02c8e0a150b1800fd54883f88fbeeaa672f") -- Depot 3596331
+setManifestid(3596331, "8739498490436358756", 587387679)
