da8f66e8bb27d40a5e03cb9cb4501bd324fdffac
qiao-lima <qiao-lima@github.com>
2026-06-08 08:48:31 +0800

    Manual Update 2790190

---
+++ 2790190.lua
@@ -0,0 +1,12 @@
+-- 2790190's Lua and Manifest Created by Hubcap Manifest
+-- Polygon Bit Battle Royale
+-- Created: June 07, 2026 at 20:29:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2790190) -- Polygon Bit Battle Royale
+-- MAIN APP DEPOTS
+addappid(2790191, 1, "1ee585db4a6fd5f2dd5dc9a035450a8da1a56bea223e8a6ab32efe17d5494ed9") -- Depot 2790191
+setManifestid(2790191, "7752761750793557999", 1710125604)
