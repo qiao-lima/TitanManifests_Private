c7b1be7d2210eb039429a788327a54ed7fcf2de3
qiao-lima <qiao-lima@github.com>
2026-06-08 08:49:15 +0800

    Manual Update 4531090

---
+++ 4531090.lua
@@ -0,0 +1,30 @@
+-- 4531090's Lua and Manifest Created by Hubcap Manifest
+-- Hexa Vanguard
+-- Created: June 06, 2026 at 20:01:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 9
+-- Total DLCs: 0
+-- Shared Depots: 8
+
+-- MAIN APPLICATION
+addappid(4531090) -- Hexa Vanguard
+-- MAIN APP DEPOTS
+addappid(4531091, 1, "c90437e396d80a3edbe611a41662ac433c095086ffd1e5f3e0b400a59702b6c1") -- Depot 4531091
+setManifestid(4531091, "86651615154111960", 532895182)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") -- .NET 4.7 Redist (Shared from App 228980)
+setManifestid(229006, "1784011429307107530", 83944258)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 117381405)
+addappid(229012, 1, "421e499f39a617c1a3b6baf11fbc65ffebd541d2c39c38d3bf6ffbd87919ce12") -- XNA 4.0 Redist (Shared from App 228980)
+setManifestid(229012, "4353723233161159493", 7061608)
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") -- OpenAL 2.0.7.0 Redist (Shared from App 228980)
+setManifestid(229020, "5799761707845834510", 810085)
+addappid(229033, 1, "9a789440bbc16131547ac6c062e5f168f7684da533a2899a71fdfaaeec21efc3") -- PhysX System Software 9.14.0702 (Shared from App 228980)
+setManifestid(229033, "2059065101492814639", 60039803)
