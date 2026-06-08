99394f305dd1f193dd5b790c2ee8c3d035806326
qiao-lima <qiao-lima@github.com>
2026-06-08 08:48:15 +0800

    Manual Update 2474770

---
+++ 2474770.lua
@@ -0,0 +1,14 @@
+-- 2474770's Lua and Manifest Created by Hubcap Manifest
+-- Momento
+-- Created: June 07, 2026 at 00:55:37 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2474770) -- Momento
+-- MAIN APP DEPOTS
+addappid(2474771, 1, "0f02ca4247920ae8e6363c52e61053785ed520abf7e8e0c122df32b0eeb6a1f2") -- Depot 2474771
+setManifestid(2474771, "2633309641930827327", 1678950114)
+addappid(2474772, 1, "8adedace1b1018c1a9fca660a3fc1cdd76b224d1ac7bcfcb1f1af8b77752503e") -- Depot 2474772
+setManifestid(2474772, "5938910976835150374", 1470067841)
