739814294a6f2629b291fd5c1a905b47a7c1315b
qiao-lima <qiao-lima@github.com>
2026-06-08 17:28:41 +0800

    Manual Update 4660050

---
+++ 4660050.lua
@@ -0,0 +1,12 @@
+-- 4660050's Lua and Manifest Created by Hubcap Manifest
+-- Stop slacking
+-- Created: June 08, 2026 at 04:10:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4660050) -- Stop slacking
+-- MAIN APP DEPOTS
+addappid(4660051, 1, "67fa1bce9f2fb795fe27b64e2bf16c8e289aff77da2dfd98335de626c6c66912") -- Depot 4660051
+setManifestid(4660051, "2506057030512973845", 3082929204)
