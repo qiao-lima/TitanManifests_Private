84e547ab6912899d515511e736d2201b02d5e649
qiao-lima <qiao-lima@github.com>
2026-06-08 14:37:23 +0800

    Manual Update 4720650

---
+++ 4720650.lua
@@ -0,0 +1,14 @@
+-- 4720650's Lua and Manifest Created by Hubcap Manifest
+-- Hanta Hearts
+-- Created: June 07, 2026 at 15:36:58 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4720650) -- Hanta Hearts
+-- MAIN APP DEPOTS
+addappid(4720651, 1, "fe9e48da5796d5366dfac20c8fc9927bbf7fae4cdd5634bd3e77edb7646d515c") -- Depot 4720651
+setManifestid(4720651, "1257435655501104092", 1243684881)
+addappid(4720652, 1, "13365bf0a591d0e36689add8daf23255176976db83135fcfcad8867807b30e24") -- Depot 4720652
+setManifestid(4720652, "2896727742535186188", 1092597266)
