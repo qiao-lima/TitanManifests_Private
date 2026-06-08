544509052e959bc12002882dfacadb804a7c050c
qiao-lima <qiao-lima@github.com>
2026-06-08 08:49:14 +0800

    Manual Update 4475580

---
+++ 4475580.lua
@@ -0,0 +1,12 @@
+-- 4475580's Lua and Manifest Created by Hubcap Manifest
+-- Fallen Night
+-- Created: May 11, 2026 at 13:14:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4475580) -- Fallen Night
+-- MAIN APP DEPOTS
+addappid(4475581, 1, "001735bacbbd99c76a358c2cc33276f39278934bfe259c354f5406e13578d041") -- Depot 4475581
+setManifestid(4475581, "33444209884761133", 823308686)
