c008d60bbc96a4e91c2a582b37148c0f4472241c
qiao-lima <qiao-lima@github.com>
2026-06-08 08:49:04 +0800

    Manual Update 4398370

---
+++ 4398370.lua
@@ -0,0 +1,16 @@
+-- 4398370's Lua and Manifest Created by Hubcap Manifest
+-- Sumire's Secret
+-- Created: June 06, 2026 at 20:01:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4398370) -- Sumire's Secret
+-- MAIN APP DEPOTS
+addappid(4398371, 1, "abafbcccb326f424dc44a321d0b4930446bf45f79154b707ec264e9616a199db") -- Depot 4398371
+setManifestid(4398371, "7737509989777471859", 91440922)
+addappid(4398372, 1, "554cbbe3e71efb851d90fe099d4ca9b430919499589a535178e066eb9b2613aa") -- Depot 4398372
+setManifestid(4398372, "6972736041353724341", 77516333)
+addappid(4398373, 1, "57622efe4f8a91d9b5e6d4dc1ca92791b6c5ac38f756dc234d9856978e818d77") -- Depot 4398373
+setManifestid(4398373, "5970915747662077659", 79867200)
