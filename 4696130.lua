535012b3da639d7c345ad18543e51c6a61ab0dc6
qiao-lima <qiao-lima@github.com>
2026-06-08 08:49:16 +0800

    Manual Update 4696130

---
+++ 4696130.lua
@@ -0,0 +1,12 @@
+-- 4696130's Lua and Manifest Created by Hubcap Manifest
+-- Cog of Y
+-- Created: June 06, 2026 at 20:01:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4696130) -- Cog of Y
+-- MAIN APP DEPOTS
+addappid(4696131, 1, "c9c4979bf2aaf0f332fb72d98f18c16075b9684f70dde59ccaa8f1b97dbef7e3") -- Depot 4696131
+setManifestid(4696131, "5986570151268493966", 342641736)
