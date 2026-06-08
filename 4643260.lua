44261497f68a7577a2f1423b378bd997ff8df576
qiao-lima <qiao-lima@github.com>
2026-06-08 17:28:41 +0800

    Manual Update 4643260

---
+++ 4643260.lua
@@ -0,0 +1,12 @@
+-- 4643260's Lua and Manifest Created by Hubcap Manifest
+-- Trader Ascent
+-- Created: June 08, 2026 at 04:11:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4643260) -- Trader Ascent
+-- MAIN APP DEPOTS
+addappid(4643261, 1, "b774bf34728cd4642def985c8b86f089954210db92d52b2a96806e1da5717e8d") -- Depot 4643261
+setManifestid(4643261, "8559406251622576104", 333386232)
