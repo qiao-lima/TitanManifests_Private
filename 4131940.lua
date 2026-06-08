401eb32458c7e5f6791bdd02acf954484781ab5e
qiao-lima <qiao-lima@github.com>
2026-06-08 08:49:02 +0800

    Manual Update 4131940

---
+++ 4131940.lua
@@ -0,0 +1,12 @@
+-- 4131940's Lua and Manifest Created by Hubcap Manifest
+-- Shattered Chess
+-- Created: June 07, 2026 at 18:56:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4131940) -- Shattered Chess
+-- MAIN APP DEPOTS
+addappid(4131941, 1, "aa2bd559228d6994841a50bb8920ed9812e1d2bcd4de1eb1ebc3a66a414c19f7") -- Depot 4131941
+setManifestid(4131941, "159648254153980574", 5939245627)
