53b5b656ff41a5fc30a5a6f6d455a3e503578104
qiao-lima <qiao-lima@github.com>
2026-06-08 08:48:45 +0800

    Manual Update 3324250

---
+++ 3324250.lua
@@ -0,0 +1,12 @@
+-- 3324250's Lua and Manifest Created by Hubcap Manifest
+-- 杀手精神病兔子… 在太空！新星拓荒～
+-- Created: June 07, 2026 at 10:22:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3324250, 1, "457e847a3531ce82f69de45ed2ca8bb6dfd385de820d5f29d1b66764fe47bc26") -- 杀手精神病兔子… 在太空！新星拓荒～
+-- MAIN APP DEPOTS
+addappid(3324251, 1, "ef4963255307346a312d59bcdf0898ca435e4a9037ab34101da44f017a971470") -- Depot 3324251
+setManifestid(3324251, "5976949555627555538", 336379723)
