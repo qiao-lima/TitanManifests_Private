3d892b24c20346e840da4d2057b5731939f89975
qiao-lima <qiao-lima@github.com>
2026-06-08 14:37:20 +0800

    Manual Update 1297900

---
+++ 1297900.lua
@@ -0,0 +1,18 @@
+-- 1297900's Lua and Manifest Created by Hubcap Manifest
+-- Gothic 1 Remake
+-- Created: June 06, 2026 at 22:46:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1297900, 1, "f6f83cf86d7118ceede3e9d60492ccf6771d379ec14324b2bf990fc97855605a") -- Gothic 1 Remake
+-- MAIN APP DEPOTS
+addappid(1297901, 1, "63fe67b675132ec13ab1579e4a9b13f877167ef3b2a7fb4f101e85ca42c42814") -- Depot 1297901
+setManifestid(1297901, "8485080007761441690", 38282992399)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
