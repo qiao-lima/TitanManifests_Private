319bc9c9194a0efb36015542e5123396f7d4fb51
qiao-lima <qiao-lima@github.com>
2026-06-08 17:28:41 +0800

    Manual Update 4595540

---
+++ 4595540.lua
@@ -0,0 +1,12 @@
+-- 4595540's Lua and Manifest Created by Hubcap Manifest
+-- Touhou Erosion ~ The Fallen Shrine Maiden of Paradise
+-- Created: June 08, 2026 at 04:10:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4595540) -- Touhou Erosion ~ The Fallen Shrine Maiden of Paradise
+-- MAIN APP DEPOTS
+addappid(4595541, 1, "f0626a63635a7425a23270bada0ab8cc161dadb3ed56d30d128c9027759f7ea3") -- Depot 4595541
+setManifestid(4595541, "5080845076420319271", 1040545960)
