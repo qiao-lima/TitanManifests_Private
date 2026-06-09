0cdd67037e363c5f3cba14fd9b430b59a609d36c
qiao-lima <qiao-lima@github.com>
2026-06-09 18:45:52 +0800

    Manual Update 3396290

---
+++ 3396290.lua
@@ -0,0 +1,12 @@
+-- 3396290's Lua and Manifest Created by Hubcap Manifest
+-- Puni the Florist
+-- Created: June 08, 2026 at 11:21:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3396290) -- Puni the Florist
+-- MAIN APP DEPOTS
+addappid(3396291, 1, "5712dce17074d4d94fa405cd71a1ca75856be1482333c2b5df68819951605cc4") -- Depot 3396291
+setManifestid(3396291, "1184713316141087187", 1911517305)
