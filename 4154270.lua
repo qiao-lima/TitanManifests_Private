137d1a06563f26ac55f3bb57bfaff9995be46253
qiao-lima <qiao-lima@github.com>
2026-06-09 18:45:53 +0800

    Manual Update 4154270

---
+++ 4154270.lua
@@ -0,0 +1,12 @@
+-- 4154270's Lua and Manifest Created by Hubcap Manifest
+-- The Interdependent View-Dimension: Zena
+-- Created: June 08, 2026 at 10:49:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4154270) -- The Interdependent View-Dimension: Zena
+-- MAIN APP DEPOTS
+addappid(4154271, 1, "52d544191dcf626bc529168afcaaab878638e12d5191ad43ce3b620cd2ca9e06") -- Depot 4154271
+setManifestid(4154271, "2444059680243716829", 1180183685)
