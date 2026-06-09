a1972fde742a8234e58cd4a45ef48af116dd06d1
qiao-lima <qiao-lima@github.com>
2026-06-09 18:45:54 +0800

    Manual Update 4594500

---
+++ 4594500.lua
@@ -0,0 +1,12 @@
+-- 4594500's Lua and Manifest Created by Hubcap Manifest
+-- Skollorn
+-- Created: June 09, 2026 at 05:14:30 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4594500) -- Skollorn
+-- MAIN APP DEPOTS
+addappid(4594501, 1, "70c46aee096eac03f1d2b458c68d6be8414e3cb3e36f505350d8b95b33fb2750") -- Depot 4594501
+setManifestid(4594501, "2922489445490293365", 178612414)
