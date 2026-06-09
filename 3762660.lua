1c0d257c0cb52a9a60398ecfd355f3f560a3ce2c
qiao-lima <qiao-lima@github.com>
2026-06-09 18:45:52 +0800

    Manual Update 3762660

---
+++ 3762660.lua
@@ -0,0 +1,16 @@
+-- 3762660's Lua and Manifest Created by Hubcap Manifest
+-- Trials Survivors
+-- Created: June 09, 2026 at 05:14:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3762660) -- Trials Survivors
+-- MAIN APP DEPOTS
+addappid(3762661, 1, "128b125f17db3782de837110659a24e38a502ad6e9fa08ecafc1c7c5940e664a") -- Depot 3762661
+setManifestid(3762661, "429518112455714657", 958971705)
+addappid(3762662, 1, "a14d581c27cfb310c275499a6daa674ecd0c00ff4dafc0835af32ca3effb4cdd") -- Depot 3762662
+setManifestid(3762662, "8623119374357554831", 1033454720)
+addappid(3762663, 1, "258484e32b5261da4d0011083fb3387a95d735de7c23670a8dd59a672e8b47e5") -- Depot 3762663
+setManifestid(3762663, "1151613785751778541", 1110432049)
