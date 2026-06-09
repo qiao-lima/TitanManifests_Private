8b7de4ba6f407c327f113b12cb639de66380cc72
qiao-lima <qiao-lima@github.com>
2026-06-09 11:03:29 +0800

    Manual Update 3100310

---
+++ 3100310.lua
@@ -0,0 +1,17 @@
+-- 3100310's Lua and Manifest Created by Hubcap Manifest
+-- Arms of God
+-- Created: June 08, 2026 at 11:00:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3100310) -- Arms of God
+-- MAIN APP DEPOTS
+addappid(3100311, 1, "4f6806b594a4f6b601cbb79736c7004672bff04b8e0c273c0883d73839fe436c") -- Depot 3100311
+setManifestid(3100311, "8152333010718148400", 8375390256)
+-- DLCS WITH DEDICATED DEPOTS
+-- Arms of God - Supporter Pack (AppID: 4639880)
+addappid(4639880)
+addappid(4639880, 1, "cf806ec4dea7b88e82eb05b29e22309affec9c0f9eec14bdd8f705006aa9d04d") -- Arms of God - Supporter Pack - Depot 4639880
+setManifestid(4639880, "5470362654090727784", 1079329760)
