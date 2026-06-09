f18e24fc0c33805e237bbe32c6e600e81524988a
qiao-lima <qiao-lima@github.com>
2026-06-09 11:03:26 +0800

    Manual Update 1896780

---
+++ 1896780.lua
@@ -0,0 +1,16 @@
+-- 1896780's Lua and Manifest Created by Hubcap Manifest
+-- Mystic Courtesan Stories: Trainee Maria♥
+-- Created: June 08, 2026 at 05:59:59 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1896780) -- Mystic Courtesan Stories: Trainee Maria♥
+-- MAIN APP DEPOTS
+addappid(1896781, 1, "d4a6efb890c441960e089c9c1b3592a81fb18f9e26440749421db20738c57e83") -- Depot 1896781
+setManifestid(1896781, "2111135521378838628", 14674827328)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
