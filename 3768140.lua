1130179822cdd5a84c8a3c2ceb7a003cdf601ac6
qiao-lima <qiao-lima@github.com>
2026-06-09 18:45:52 +0800

    Manual Update 3768140

---
+++ 3768140.lua
@@ -0,0 +1,12 @@
+-- 3768140's Lua and Manifest Created by Hubcap Manifest
+-- SeekOut
+-- Created: June 09, 2026 at 05:13:53 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3768140) -- SeekOut
+-- MAIN APP DEPOTS
+addappid(3768141, 1, "7334ee2ff6e66f705d1ebe4b2b56394e08f632c74d3513d915cf44440cc74132") -- Depot 3768141
+setManifestid(3768141, "7428970157485915881", 7057507276)
