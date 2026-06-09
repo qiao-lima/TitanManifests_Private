66ecffdce9a82f431ee041fdb5c0b1943c0a5768
qiao-lima <qiao-lima@github.com>
2026-06-09 18:45:53 +0800

    Manual Update 4148240

---
+++ 4148240.lua
@@ -0,0 +1,16 @@
+-- 4148240's Lua and Manifest Created by Hubcap Manifest
+-- Road to Empress Ⅱ
+-- Created: June 09, 2026 at 03:44:58 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(4148240) -- Road to Empress Ⅱ
+-- MAIN APP DEPOTS
+addappid(4148241, 1, "a5eac25188569b44da6f828e35adf08f236dace1c4ed3ad45a4d6c747f5eaaf9") -- Depot 4148241
+setManifestid(4148241, "8216982804419174627", 9325677046)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
