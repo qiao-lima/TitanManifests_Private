2d6cab8d7d7b76bcc87d86639765fccc500612d2
qiao-lima <qiao-lima@github.com>
2026-06-09 18:45:52 +0800

    Manual Update 3031720

---
+++ 3031720.lua
@@ -0,0 +1,26 @@
+-- 3031720's Lua and Manifest Created by Hubcap Manifest
+-- Kamikaze Empire
+-- Created: June 09, 2026 at 05:16:13 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 7
+-- Total DLCs: 0
+-- Shared Depots: 6
+
+-- MAIN APPLICATION
+addappid(3031720) -- Kamikaze Empire
+-- MAIN APP DEPOTS
+addappid(3031721, 1, "a889e201babae9440e58122c3bc26c0771d2263016d7cf14d61e6ef162c78904") -- Depot 3031721
+setManifestid(3031721, "655149043398239235", 39215161)
+-- SHARED DEPOTS (from other apps)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (Shared from App 228980)
+setManifestid(228983, "8124929965194586177", 19265607)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- VC 2012 Redist (Shared from App 228980)
+setManifestid(228984, "2547553897526095397", 13742505)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- VC 2013 Redist (Shared from App 228980)
+setManifestid(228985, "3966345552745568756", 13699237)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
