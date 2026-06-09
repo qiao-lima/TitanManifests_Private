138f2b2c1ba35121f6c7775df0ad97e64e62f504
qiao-lima <qiao-lima@github.com>
2026-06-09 18:45:52 +0800

    Manual Update 3219180

---
+++ 3219180.lua
@@ -0,0 +1,12 @@
+-- 3219180's Lua and Manifest Created by Hubcap Manifest
+-- Cursemark
+-- Created: June 08, 2026 at 15:50:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3219180) -- Cursemark
+-- MAIN APP DEPOTS
+addappid(3219181, 1, "bf335808f54d637ed6e61f4c67962e4221a7ad4fdb72f32724b1f4d2e222d300") -- Depot 3219181
+setManifestid(3219181, "3910639037333872808", 123378673)
