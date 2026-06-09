02fb989883a351042c73b67d3e70eebabec813bf
qiao-lima <qiao-lima@github.com>
2026-06-09 18:45:51 +0800

    Manual Update 2805850

---
+++ 2805850.lua
@@ -0,0 +1,20 @@
+-- 2805850's Lua and Manifest Created by Hubcap Manifest
+-- Atre: Dominance Wars
+-- Created: June 08, 2026 at 19:29:57 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2805850) -- Atre: Dominance Wars
+-- MAIN APP DEPOTS
+addappid(2805851, 1, "1f5e332e888a50e5e5ed70f835b4a12f66b661eaf9d2cb40938c01e126fe5978") -- Depot 2805851
+setManifestid(2805851, "8925155679917911625", 5307960366)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2805854) -- Depot 2805854 (empty depot)
