a7591f0048938b8016a3d2ffa3d06023592d9713
qiao-lima <qiao-lima@github.com>
2026-06-10 20:03:44 +0800

    Manual Update 3312400

---
+++ 3312400.lua
@@ -0,0 +1,12 @@
+-- 3312400's Lua and Manifest Created by Hubcap Manifest
+-- Lust o’ Heaven
+-- Created: June 09, 2026 at 12:51:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3312400) -- Lust o’ Heaven
+-- MAIN APP DEPOTS
+addappid(3312401, 1, "109a0aee34e66885c856fe78d01b0ddcc672512af866b89e9a088fdbfcfd5ce1") -- Depot 3312401
+setManifestid(3312401, "2622291223229550810", 1193919115)
