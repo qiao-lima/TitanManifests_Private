7c700a130ab9267450dbec4647ea1a5b4cfb4f19
qiao-lima <qiao-lima@github.com>
2026-06-10 20:03:41 +0800

    Manual Update 233450

---
+++ 233450.lua
@@ -0,0 +1,44 @@
+-- 233450's Lua and Manifest Created by Hubcap Manifest
+-- Prison Architect
+-- Created: September 30, 2025 at 07:56:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 6
+-- Total DLCs: 16
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(233450) -- Prison Architect
+-- MAIN APP DEPOTS
+addappid(233451, 1, "27eb5fe0ce5772f90c109ac60918afc58a5870bbd1cdaef4b6c3919b0b63ef83") -- PC Content
+setManifestid(233451, "214199605922079846", 536985738)
+addappid(233452, 1, "0313b1a3064addbc9f94a29d64e52a2db1f9da529598fe6402af74482b81876d") -- Mac Content
+setManifestid(233452, "8796105605411077743", 565083957)
+addappid(233453, 1, "ba72480b04c6b8e3a27a7dab0ad8aae496aa36f4420a20e9b8aa8cb6c38b8b30") -- Linux Content
+setManifestid(233453, "6703655944775788476", 550031103)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+-- DLCS WITH DEDICATED DEPOTS
+-- Prison Architect Art Book (AppID: 234432)
+addappid(234432)
+addappid(234432, 1, "79c2572c2aea51e5da076c4656f66287663658804187cd5d78dba3a7eb801814") -- Prison Architect Art Book - Prison Architect Art Book (234432) Depot
+setManifestid(234432, "548000939191343126", 36878989)
+-- Prison Architect Soundtrack (AppID: 234433)
+addappid(234433)
+addappid(234433, 1, "60f2761aca08c5325e4b42c01f8b4196a3046e895062c3e60c7f916ea9541acb") -- Prison Architect Soundtrack - Prison Architect Soundtrack (234433) Depot
+setManifestid(234433, "836138921998834207", 762838238)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(234430) -- Prison Architect Standard Edition Key
+addappid(234431) -- Prison Architect Name in Game Edition Key
+addappid(1143650) -- Prison Architect - Psych Ward Wardens Edition
+addappid(1195240) -- Prison Architect - Aficionado
+addappid(1244780) -- Prison Architect - Cleared for Transfer
+addappid(1244781) -- Prison Architect - Island Bound
+addappid(1493470) -- Prison Architect - Going Green
+addappid(1601350) -- Prison Architect - Second Chances
+addappid(1760030) -- Prison Architect - Perfect Storm
+addappid(1948120) -- Prison Architect - Gangs
+addappid(2085570) -- Prison Architect - Free For Life
+addappid(2154010) -- Prison Architect - Undead
+addappid(2184800) -- Prison Architect - Future Tech Pack
+addappid(2237250) -- Prison Architect - Jungle Pack
