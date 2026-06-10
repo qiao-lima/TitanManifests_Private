b6ced03955612b8439dbff15b42aaad17ccd31bd
qiao-lima <qiao-lima@github.com>
2026-06-10 15:31:20 +0800

    Manual Update 4398530

---
+++ 4398530.lua
@@ -0,0 +1,12 @@
+-- 4398530's Lua and Manifest Created by Hubcap Manifest
+-- 万界·飞升成圣
+-- Created: June 10, 2026 at 00:34:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4398530) -- 万界·飞升成圣
+-- MAIN APP DEPOTS
+addappid(4398531, 1, "c0ca4d3c1f3e5d370b003d3c906a84b6fc5fd5e99662f7a74072939c2eec8735") -- Depot 4398531
+setManifestid(4398531, "2573711633639845823", 421402640)
