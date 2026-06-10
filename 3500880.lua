db18e05e4de213fd8085f435535ca4d686072ed2
qiao-lima <qiao-lima@github.com>
2026-06-10 15:31:19 +0800

    Manual Update 3500880

---
+++ 3500880.lua
@@ -0,0 +1,47 @@
+-- 3500880's Lua and Manifest Created by Hubcap Manifest
+-- Tavern Talk Stories: Dreamwalker
+-- Created: June 09, 2026 at 12:51:59 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 11
+-- Total DLCs: 7
+
+-- MAIN APPLICATION
+addappid(3500880, 1, "f759f47c4b4368213606988f02b3f16353dc5bd0ace7f5cff6546369ade40514") -- Tavern Talk Stories: Dreamwalker
+-- MAIN APP DEPOTS
+addappid(3500881, 1, "7394baabccb8dd4d3696b74b42816d187b20002e0246f37ccbf07423bd46ef69") -- Depot 3500881
+setManifestid(3500881, "1852701150839542929", 947264688)
+addappid(3500882, 1, "180111d38b80fda6b9ee7359b5802f15b07688a6de4c54020e3383f9ac70f542") -- Depot 3500882
+setManifestid(3500882, "3813436857009343270", 1083636759)
+-- DLCS WITH DEDICATED DEPOTS
+-- Tavern Talk Stories Dreamwalker - Drowsy Druid (Decoration Pack) (AppID: 4464060)
+addappid(4464060)
+addappid(4464060, 1, "09c1fae4a5c44ad56c8dcb26a38de8af61212e3cbdfa8ca4432299442fb18ce2") -- Tavern Talk Stories Dreamwalker - Drowsy Druid (Decoration Pack) - Depot 4464060
+setManifestid(4464060, "4685202038669820681", 2140181)
+addappid(4464061, 1, "118415b4c7b25639324a0c70f466335f44446710112248ffd2193f82ff598f65") -- Tavern Talk Stories Dreamwalker - Drowsy Druid (Decoration Pack) - Depot 4464061
+setManifestid(4464061, "4645106040715582910", 2140158)
+-- Tavern Talk Stories Dreamwalker - Wondrous Wizard (Decoration Pack) (AppID: 4464070)
+addappid(4464070)
+addappid(4464070, 1, "cdef29878c9bcd5645be408371a64496cac5b2d1bc3e20e6b9ceb87496357445") -- Tavern Talk Stories Dreamwalker - Wondrous Wizard (Decoration Pack) - Depot 4464070
+setManifestid(4464070, "4359594237950145058", 3595348)
+addappid(4464071, 1, "cfff3fb619df2d4f1c7420ad465029e2729ffa2ad4dfb737eb7c81c8334e4ce6") -- Tavern Talk Stories Dreamwalker - Wondrous Wizard (Decoration Pack) - Depot 4464071
+setManifestid(4464071, "1527651827378870195", 3595346)
+-- Tavern Talk Stories Dreamwalker - Artbook (AppID: 4464090)
+addappid(4464090)
+addappid(4464091, 1, "85325f9cc01c62f7b81512f4aba635d31fa1e11fadced5187b979fe851b20114") -- Tavern Talk Stories Dreamwalker - Artbook - Depot 4464091
+setManifestid(4464091, "280590587965702442", 179283177)
+-- Tavern Talk Stories Dreamwalker - TTRPG Character Sheets (AppID: 4464120)
+addappid(4464120)
+addappid(4464121, 1, "8cb08c9f3a629d07606ba4b44e62364097bfaf7a5104f98fef438de10ceaa551") -- Tavern Talk Stories Dreamwalker - TTRPG Character Sheets - Depot 4464121
+setManifestid(4464121, "1091984685711196928", 46176261)
+-- Tavern Talk Stories Dreamwalker - TTRPG Item Cards Potions (AppID: 4464130)
+addappid(4464130)
+addappid(4464131, 1, "77353b0554deee4f931581860b6bf60c3c650488223cd97734fe4a9352723b0b") -- Tavern Talk Stories Dreamwalker - TTRPG Item Cards Potions - Depot 4464131
+setManifestid(4464131, "2180348460108172171", 94268103)
+-- Tavern Talk Stories Dreamwalker - TTRPG Mini-Adventures (AppID: 4464140)
+addappid(4464140)
+addappid(4464141, 1, "428d4717111b41f1366fa0f6467d1aebd2c19aa18917601cd382c5ece6e05ab2") -- Tavern Talk Stories Dreamwalker - TTRPG Mini-Adventures - Depot 4464141
+setManifestid(4464141, "6268886983216740176", 9219877)
+-- Tavern Talk Stories Dreamwalker - Supporter Upgrade (AppID: 4498570)
+addappid(4498570)
+addappid(4498571, 1, "a5deb34322ffab0f7729b70de442c32a8f5a69272b173fe826377debf27a0aac") -- Tavern Talk Stories Dreamwalker - Supporter Upgrade - Depot 4498571
+setManifestid(4498571, "7989027858651744904", 641055468)
