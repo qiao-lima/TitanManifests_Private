3ee54951ea3c059503a76d504f94cf3e914089d9
qiao-lima <qiao-lima@github.com>
2026-06-10 15:31:19 +0800

    Manual Update 2866670

---
+++ 2866670.lua
@@ -0,0 +1,20 @@
+-- 2866670's Lua and Manifest Created by Hubcap Manifest
+-- NBA THE RUN
+-- Created: June 09, 2026 at 14:13:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2866670) -- NBA THE RUN
+-- MAIN APP DEPOTS
+addappid(2866671, 1, "7154869f1d566e3254a00f81174eab7c092954420a07e253597f960fe08494ff") -- Depot 2866671
+setManifestid(2866671, "4462019439051348979", 8955702415)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4799710) -- NBA The Run - Deluxe Edition Content
