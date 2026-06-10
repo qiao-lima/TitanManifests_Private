3744905bd7283f7f43302c88920738eb4ab195ae
qiao-lima <qiao-lima@github.com>
2026-06-10 20:03:39 +0800

    Manual Update 1372110

---
+++ 1372110.lua
@@ -0,0 +1,33 @@
+-- 1372110's Lua and Manifest Created by Hubcap Manifest
+-- JoJo's Bizarre Adventure: All-Star Battle R
+-- Created: September 29, 2025 at 20:06:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 14
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1372110) -- JoJo's Bizarre Adventure: All-Star Battle R
+-- MAIN APP DEPOTS
+addappid(1372111, 1, "219ba7ad894442207719fbcc8f5108ead1e6a7990e9570faff49adedcb1a0673") -- Depot 1372111
+setManifestid(1372111, "1920387574416877032", 5307141382)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1941060) -- JoJos Bizarre Adventure All-Star Battle R The Animation Special Event color set (5 types)
+addappid(1941061) -- JoJos Bizarre Adventure All-Star Battle R Jolyne Cujoh Costume Green Dolphin Street State Prison Jacket
+addappid(1941062) -- JoJos Bizarre Adventure All-Star Battle R Rohan Kishibe First Appearance Costume
+addappid(1941063) -- JoJos Bizarre Adventure All-Star Battle R Mohammed Avdols Father
+addappid(2001010) -- JoJos Bizarre Adventure All-Star Battle R - Risotto Nero DLC
+addappid(2001011) -- JoJos Bizarre Adventure All-Star Battle R - Rudol von Stroheim DLC
+addappid(2001012) -- JoJos Bizarre Adventure All-Star Battle R - Keicho Nijimura DLC
+addappid(2001013) -- JoJos Bizarre Adventure All-Star Battle R - Alternate World Diego DLC
+addappid(2001014) -- JoJos Bizarre Adventure All-Star Battle R Season Pass
+addappid(2382760) -- JoJos Bizarre Adventure All-Star Battle R - Leone Abbacchio DLC
+addappid(2382761) -- JoJos Bizarre Adventure All-Star Battle R - Yuya Fungami
+addappid(2382762) -- JoJos Bizarre Adventure All-Star Battle R - Wonder of U
+addappid(2382763) -- JoJos Bizarre Adventure All-Star Battle R - Giorno Giovanna Haruno Shiobana Costume
+addappid(2382764) -- JoJos Bizarre Adventure All-Star Battle R Season Pass 2
