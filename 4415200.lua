3f1ad3d105c65d4918f4825169dffef30c63df58
qiao-lima <qiao-lima@github.com>
2026-06-10 15:31:21 +0800

    Manual Update 4415200

---
+++ 4415200.lua
@@ -0,0 +1,16 @@
+-- 4415200's Lua and Manifest Created by Hubcap Manifest
+-- Draw Party
+-- Created: June 09, 2026 at 13:49:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4415200) -- Draw Party
+-- MAIN APP DEPOTS
+addappid(4415202, 1, "7116cfe28612e9e3b1933c4eff89f9e822a094363389a6174cc22305f416c76b") -- Depot 4415202
+setManifestid(4415202, "1439177305845231545", 36910905)
+addappid(4415203, 1, "4e21538be4f6f5110708b12a4030d8ff8334796853ba6f6658a497263e8ec3d8") -- Depot 4415203
+setManifestid(4415203, "5793882763232603652", 159164846)
+addappid(4415204, 1, "4975b23a958beeb85045638ac18ba9e44634338e8235a925916364725ff77416") -- Depot 4415204
+setManifestid(4415204, "2817858797027393257", 32521678)
