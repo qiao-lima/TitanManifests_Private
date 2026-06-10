f859496975ee5de53114fa059e9d9a885196171f
qiao-lima <qiao-lima@github.com>
2026-06-10 20:03:42 +0800

    Manual Update 2670630

---
+++ 2670630.lua
@@ -0,0 +1,22 @@
+-- 2670630's Lua and Manifest Created by Hubcap Manifest
+-- Supermarket Simulator
+-- Created: June 09, 2026 at 08:50:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 7
+
+-- MAIN APPLICATION
+addappid(2670630, 1, "506e8fb6ac803f12d68d4f15299eb63557c21ecada3e6e5a4081de6d1f05140e") -- Supermarket Simulator
+-- MAIN APP DEPOTS
+addappid(2670631, 1, "72fc26c211612ffaa7b30cdfea152be9628668260d7f8c375980e0f96e445701") -- Depot 2670631
+setManifestid(2670631, "985432630790485785", 2084387084)
+addappid(2670632, 1, "05a026749d66983d1a8b484cee9c7e2e320926d365ae5fe9f916ca8e96c00e4a") -- Depot 2670632
+setManifestid(2670632, "4181596598553624393", 2257019232)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4053820) -- Supermarket Simulator - Clothing DLC
+addappid(4078560) -- Supermarket Simulator - Electronics DLC
+addappid(4078570) -- Supermarket Simulator - Hardware DLC
+addappid(4196060) -- Supermarket Simulator - Bakery DLC
+addappid(4196070) -- Supermarket Simulator - Ice Cream DLC
+addappid(4196080) -- Supermarket Simulator - Essentials DLC
+addappid(4199610) -- Supermarket Simulator - Vending Machine DLC
