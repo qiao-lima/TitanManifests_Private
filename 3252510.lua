371cf6b725e4587e002cdf0abaefa31934af3a93
qiao-lima <qiao-lima@github.com>
2026-06-10 15:27:28 +0800

    Manual Update 3252510

---
+++ 3252510.lua
@@ -0,0 +1,18 @@
+-- 3252510's Lua and Manifest Created by Hubcap Manifest
+-- Crimson Lust
+-- Created: February 16, 2026 at 14:02:44 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3252510) -- Crimson Lust
+-- MAIN APP DEPOTS
+addappid(3252511, 1, "2523471c992e2117e7f33da66eb06d0cb0366fb0aea423b443e4127b1c77107d") -- Depot 3252511
+setManifestid(3252511, "6622247111906544396", 1631835171)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
