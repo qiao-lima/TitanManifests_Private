a2d7fe2c77e78c2e709c5c6ed9f22b442355afca
qiao-lima <qiao-lima@github.com>
2026-06-11 14:20:11 +0800

    Manual Update 2917850

---
+++ 2917850.lua
@@ -0,0 +1,14 @@
+-- 2917850's Lua and Manifest Created by Hubcap Manifest
+-- Photochemistry
+-- Created: June 10, 2026 at 14:47:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2917850) -- Photochemistry
+-- MAIN APP DEPOTS
+addappid(2917851, 1, "c1f8042b6404b2bab2bafc9ab1feea8fbfbee33e06637252522cfa96910a39ad") -- Depot 2917851
+setManifestid(2917851, "5326284310824564629", 883760272)
+addappid(2917852, 1, "94af193eb01b870b1519791d242c0929f9bd89d12156a94e55e0e26f82a73359") -- Depot 2917852
+setManifestid(2917852, "389826020153855226", 862207460)
