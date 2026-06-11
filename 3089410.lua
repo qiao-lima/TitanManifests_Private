fe5cbae37bffb296a5101c5964abfc582a0b4dee
qiao-lima <qiao-lima@github.com>
2026-06-11 21:19:11 +0800

    Manual Update 3089410

---
+++ 3089410.lua
@@ -0,0 +1,13 @@
+-- 3089410's Lua and Manifest Created by Hubcap Manifest
+-- Maid To End 迈向终焉的女仆
+-- Created: June 11, 2026 at 06:04:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3089410) -- Maid To End 迈向终焉的女仆
+addtoken(3089410, "5614101576066984256")
+-- MAIN APP DEPOTS
+addappid(3089411, 1, "8221dfeffe8887a82ebcb16c5e232fbca6c13a7e9b8ba8234973a0be68490b34") -- Depot 3089411
+setManifestid(3089411, "4401402017890740246", 525356275)
