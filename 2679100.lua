9313872570a5055a64e0e9ff1b65c7b5bc4607d5
qiao-lima <qiao-lima@github.com>
2026-06-11 14:20:10 +0800

    Manual Update 2679100

---
+++ 2679100.lua
@@ -0,0 +1,20 @@
+-- 2679100's Lua and Manifest Created by Hubcap Manifest
+-- Witchspire
+-- Created: June 10, 2026 at 16:36:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2679100) -- Witchspire
+-- MAIN APP DEPOTS
+addappid(2679101, 1, "fe034c141c4a7e40166c2a3c3e1113721f48dfdd6942e5767ad7532ade8d6982") -- Depot 2679101
+setManifestid(2679101, "6180149955513762066", 6836215750)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4695140) -- Witchspire - Founders Edition
