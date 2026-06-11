12c8d7477a20f2c02d3ab226be6e1f130f61eea8
qiao-lima <qiao-lima@github.com>
2026-06-11 14:20:12 +0800

    Manual Update 3590010

---
+++ 3590010.lua
@@ -0,0 +1,14 @@
+-- 3590010's Lua and Manifest Created by Hubcap Manifest
+-- Robot Hospice
+-- Created: June 10, 2026 at 09:01:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3590010) -- Robot Hospice
+-- MAIN APP DEPOTS
+addappid(3590011, 1, "f78f037a2395aafc0932d5e5ce1f7278a05612a0cb3c20090409be87e49525fa") -- Depot 3590011
+setManifestid(3590011, "3433932296637586653", 294833572)
+addappid(3590012, 1, "0f39f94fab467351089264035f9abb124c2bd54da72f0cfd351808378db7c108") -- Depot 3590012
+setManifestid(3590012, "4899059675766535189", 604209744)
