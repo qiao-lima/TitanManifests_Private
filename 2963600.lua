2d43275993350c6ee32a923f53f2b12b5d648925
qiao-lima <qiao-lima@github.com>
2026-06-11 14:20:11 +0800

    Manual Update 2963600

---
+++ 2963600.lua
@@ -0,0 +1,17 @@
+-- 2963600's Lua and Manifest Created by Hubcap Manifest
+-- Card Cultivation
+-- Created: June 10, 2026 at 13:59:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2963600) -- Card Cultivation
+addtoken(2963600, "17301741832411732605")
+-- MAIN APP DEPOTS
+addappid(2963601, 1, "b9bce55d661a1d2a0e7f71a006f265463f53bb49d6625bbdeb7f25359cbd2a0d") -- Depot 2963601
+setManifestid(2963601, "5165934957898427328", 1520838253)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
