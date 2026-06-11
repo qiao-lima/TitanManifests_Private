21df330dfc56ab39726b4dc169ec916d95dedd48
qiao-lima <qiao-lima@github.com>
2026-06-11 21:19:06 +0800

    Manual Update 1468810

---
+++ 1468810/1468810.lua
@@ -0,0 +1,26 @@
+-- 1468810's Lua and Manifest Created by Hubcap Manifest
+-- 鬼谷八荒 Tale of Immortal
+-- Created: February 10, 2026 at 00:19:29 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 3
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1468810, 1, "947a1353f497af3a44dd791bbc5add8463d0714e94f78e608ca135ec6ae698c5") -- 鬼谷八荒 Tale of Immortal
+addtoken(1468810, "2905683081384123386")
+-- MAIN APP DEPOTS
+addappid(1468811, 1, "57880f9288973b73552c08af505e7d3e21db84ff579c2b5d08a34f092f652349") -- Depot 1468811
+setManifestid(1468811, "5275750898890021375", 29270754987)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") -- .NET 4.7 Redist (Shared from App 228980)
+setManifestid(229006, "1784011429307107530", 83944258)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2408730) --  - 
+addappid(3294480) -- Tale of Immortal - BaHuang Chronicles  Digital Art Book
+addappid(3326620) -- Tale of Immortal - Five Blooms
+addappid(1468810,0,"947a1353f497af3a44dd791bbc5add8463d0714e94f78e608ca135ec6ae698c5")
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(1551860) -- Depot 1551860 (empty depot)
