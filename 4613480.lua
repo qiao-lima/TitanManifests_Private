c53d3035cb273689d796ed967e9e9ef54512aec5
qiao-lima <qiao-lima@github.com>
2026-06-11 14:20:15 +0800

    Manual Update 4613480

---
+++ 4613480.lua
@@ -0,0 +1,12 @@
+-- 4613480's Lua and Manifest Created by Hubcap Manifest
+-- 放开那个女孩让我来
+-- Created: June 10, 2026 at 10:18:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4613480) -- 放开那个女孩让我来
+-- MAIN APP DEPOTS
+addappid(4613481, 1, "76eb5cbe2f7b4a14a03d5e7fe1327380accf03f6da88f5adfcaed79ffad3953d") -- Depot 4613481
+setManifestid(4613481, "4623182490723567881", 32503266350)
