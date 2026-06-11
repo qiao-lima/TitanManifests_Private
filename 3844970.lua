5a41a2064a96fb970648cc8e4d3a47241d6a65bf
qiao-lima <qiao-lima@github.com>
2026-06-11 14:20:13 +0800

    Manual Update 3844970

---
+++ 3844970.lua
@@ -0,0 +1,16 @@
+-- 3844970's Lua and Manifest Created by Hubcap Manifest
+-- Burglin' Gnomes
+-- Created: June 10, 2026 at 12:55:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3844970) -- Burglin' Gnomes
+-- MAIN APP DEPOTS
+addappid(3844971, 1, "6d3197ad9feb781dfe4dfd67f09fb9e834c9bbb158108a27c31e31d3001ec326") -- Depot 3844971
+setManifestid(3844971, "1719422267292344556", 6660246734)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
