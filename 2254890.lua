4962aace95ef75712e79bbb6a6d9d85682228bb8
qiao-lima <qiao-lima@github.com>
2026-06-11 21:19:07 +0800

    Manual Update 2254890

---
+++ 2254890.lua
@@ -0,0 +1,34 @@
+-- 2254890's Lua and Manifest Created by Morrenus
+-- The Killing Antidote
+-- Created: March 15, 2026 at 06:57:52 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 6
+-- Total DLCs: 3
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2254890, 1, "2b61b633ed06c177f6c8ddff8eb747ace8180cbf013f81c0d854add7bcceb50b") -- The Killing Antidote
+-- MAIN APP DEPOTS
+addappid(2254891, 1, "729988b69fdd52990bafdf7bb4272df39dd5d2421cacb3e3ed9fe2d82354288f") -- Depot 2254891
+setManifestid(2254891, "4346761784022460582", 4948096987)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- The Killing Antidote - Cowgirl Costume Pack (AppID: 3761650)
+addappid(3761650)
+addappid(3761651, 1, "946f8e5f5707753e2a75d3e96a62d007bcf41fcd0ac167a1450b4622fb48f2f5") -- The Killing Antidote - Cowgirl Costume Pack - Depot 3761651
+setManifestid(3761651, "3796580347871242929", 255199466)
+-- The Killing Antidote - Maid Costume Pack (AppID: 4350520)
+addappid(4350520)
+addappid(4350521, 1, "5fb83713c1d4910bf68a5e5473b9eefc474f74945f778ab1cf2d832cdaa80cba") -- The Killing Antidote - Maid Costume Pack - Depot 4350521
+setManifestid(4350521, "6791221263400239466", 72957982)
+-- The Killing Antidote - Wuxia Allure Attire (AppID: 4350590)
+addappid(4350590)
+addappid(4350591, 1, "55151cd29438ae9fdd7edf5e70ac14c9f73a064c7f0ce347f8e0ac8f442f7c68") -- The Killing Antidote - Wuxia Allure Attire - Depot 4350591
+setManifestid(4350591, "23230248584909191", 69155981)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3761650) -- Depot 3761650 (empty depot)
+addappid(2254890,0,"2b61b633ed06c177f6c8ddff8eb747ace8180cbf013f81c0d854add7bcceb50b")
