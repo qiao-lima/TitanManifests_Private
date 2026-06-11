cdc2615b9062b474e6c7e6edce17a18886a9f587
qiao-lima <qiao-lima@github.com>
2026-06-11 14:20:09 +0800

    Manual Update 1681020

---
+++ 1681020.lua
@@ -0,0 +1,12 @@
+-- 1681020's Lua and Manifest Created by Hubcap Manifest
+-- SPACE STORESHIP
+-- Created: June 10, 2026 at 14:39:06 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1681020, 1, "51e00d93c15be9aa279d7c879637214b65a2529ccca7a56f00bce5c0f73da6b5") -- SPACE STORESHIP
+-- MAIN APP DEPOTS
+addappid(1681021, 1, "67cbb8027acf8c6d6a173cd602596dde1bdaf16776bfe03692c284d942a4fb25") -- Depot 1681021
+setManifestid(1681021, "3652561791985087251", 112130283)
