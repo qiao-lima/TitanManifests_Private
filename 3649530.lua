ed2f7be46aa1a6b1ae8860d1dfef7bdd4580f530
qiao-lima <qiao-lima@github.com>
2026-06-11 14:20:13 +0800

    Manual Update 3649530

---
+++ 3649530.lua
@@ -0,0 +1,12 @@
+-- 3649530's Lua and Manifest Created by Hubcap Manifest
+-- 4PGP
+-- Created: June 10, 2026 at 14:42:18 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3649530, 1, "b42f58641c2a5c9ad6fded644012c42088ecfebae386168bd2cae6e5d4d24d4c") -- 4PGP
+-- MAIN APP DEPOTS
+addappid(3649531, 1, "6ff0fb556567b1005014556a3edd7bb9cda16bb7bfc197ca323269213a88099d") -- Depot 3649531
+setManifestid(3649531, "8343288302509448710", 1379368128)
