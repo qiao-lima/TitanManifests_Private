cf3c4b437d7c903629170a870c3226b914ed55e4
qiao-lima <qiao-lima@github.com>
2026-06-13 00:58:24 +0800

    Manual Update 1144200

---
+++ 1144200.lua
@@ -0,0 +1,25 @@
+-- 1144200's Lua and Manifest Created by Hubcap Manifest
+-- Ready or Not
+-- Created: May 05, 2026 at 13:00:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 4
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(1144200, 1, "fb29b3865255ec0e9230c029502cd87b8626fd12f9bdb2fa3df6cddf34e17179") -- Ready or Not
+-- MAIN APP DEPOTS
+addappid(1144201, 1, "b9236894ba071d12dd117ce16220fefeb20574d7c6f7d2170e3113346867f3d6") -- Ready Or Not - Alpha Content
+setManifestid(1144201, "6744259790683578909", 51889648344)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 117381405)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1844910) -- Ready or Not Supporter Edition
+addappid(3015760) -- Ready or Not Home Invasion
+addappid(3174120) -- Ready or Not Dark Waters
+addappid(4224910) -- Ready or Not Boiling Point
