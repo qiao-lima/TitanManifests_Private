46a5134e1a4f4acc3c4187633a5d0ca12e2e82cb
qiao-lima <qiao-lima@github.com>
2026-06-12 22:18:23 +0800

    Manual Update 3146440

---
+++ 3146440.lua
@@ -0,0 +1,12 @@
+-- 3146440's Lua and Manifest Created by Hubcap Manifest
+-- SPORTAL
+-- Created: June 12, 2026 at 07:03:59 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3146440) -- SPORTAL
+-- MAIN APP DEPOTS
+addappid(3146441, 1, "503aac1a50ecc3536a61be529a9e9f9a89da416bfabcb2697184901807e052c5") -- Depot 3146441
+setManifestid(3146441, "5559684798864112379", 2755123827)
