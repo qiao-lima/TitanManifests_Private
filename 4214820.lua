0c4d7be4b9ee7eb09822a480c8fa26abd692278e
qiao-lima <qiao-lima@github.com>
2026-06-13 00:59:27 +0800

    Manual Update 4214820

---
+++ 4214820.lua
@@ -0,0 +1,20 @@
+-- 4214820's Lua and Manifest Created by Hubcap Manifest
+-- Game of Thrones Kingsroad
+-- Created: June 12, 2026 at 12:35:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4214820) -- Game of Thrones Kingsroad
+-- MAIN APP DEPOTS
+addappid(4214821, 1, "339d48020dcdfbcfecb33e1964ed1fd5cdcae4c987c73aac100c67f9e68badd5") -- Depot 4214821
+setManifestid(4214821, "4631283017919940678", 1492700296)
+addappid(4214822, 1, "1a20190cd79819bfdca356676eb64166d3068c239ffcd298c1afb664edfd56ea") -- Depot 4214822
+setManifestid(4214822, "8503527812210846161", 42678699663)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
