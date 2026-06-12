618bdcde49dc8cb6bcf6de0dfba77027150aac35
qiao-lima <qiao-lima@github.com>
2026-06-13 00:59:27 +0800

    Manual Update 4265120

---
+++ 4265120.lua
@@ -0,0 +1,17 @@
+-- 4265120's Lua and Manifest Created by Hubcap Manifest
+-- PlatyGuard: Swarm Slayer
+-- Created: May 30, 2026 at 02:36:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(4265120) -- PlatyGuard: Swarm Slayer
+-- MAIN APP DEPOTS
+addappid(4265121, 1, "b83851371092d05d7db5a97555332a603ba4d59280ce1ca3fa53c163f507c6c3") -- Depot 4265121
+setManifestid(4265121, "6267361015713381124", 1611305182)
+addappid(4265122, 1, "91b4c89f266839be0ccfba046d5b0e6996bdc710e3441cf9188c69253b3040e9") -- Depot 4265122
+setManifestid(4265122, "8510606898075166828", 1611305182)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4539790) -- PlatyGuard Swarm Slayer - Epic Outfit Pack
+addappid(4539800) -- PlatyGuard Swarm Slayer - Legendary Outfit Pack
