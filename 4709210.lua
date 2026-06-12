7a67208078039538b1f66425af3bae135ad962c1
qiao-lima <qiao-lima@github.com>
2026-06-12 10:21:27 +0800

    Manual Update 4709210

---
+++ 4709210.lua
@@ -0,0 +1,12 @@
+-- 4709210's Lua and Manifest Created by Hubcap Manifest
+-- Shardmirror
+-- Created: June 11, 2026 at 06:03:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4709210) -- Shardmirror
+-- MAIN APP DEPOTS
+addappid(4709211, 1, "b3c245c0b3e1543033cae549425204dc5675e4861dc66d945cd8b93888b132d2") -- Depot 4709211
+setManifestid(4709211, "2612805239703627014", 1308676624)
