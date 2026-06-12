d7c05a9a2d09690dd43ca560c5c171f36f6f0a09
qiao-lima <qiao-lima@github.com>
2026-06-12 22:18:22 +0800

    Manual Update 1295660

---
+++ 1295660.lua
@@ -0,0 +1,69 @@
+-- 1295660's Lua and Manifest Created by Hubcap Manifest
+-- Sid Meier's Civilization VII
+-- Created: May 19, 2026 at 12:22:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 6
+-- Total DLCs: 34
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1295660, 1, "f23c385f4f13e1385d24f0f6033c70e9dd9fbbbbc955227df88693a31db9411b") -- Sid Meier's Civilization VII
+addtoken(1295660, "16771268434109846988")
+-- MAIN APP DEPOTS
+addappid(1295661, 1, "59cf929fe0be7af1bba7498247138b359bcc8141d6921f65f06f5d3c4e604430") -- Depot 1295661
+setManifestid(1295661, "8883034523978679375", 22523196417)
+addappid(1295662, 1, "2d91bb0ac40f79f21d133921c8092cee1bbc5622c1e429d5d5e65a5543a440eb") -- Depot 1295662
+setManifestid(1295662, "3975609554075529981", 1846256790)
+addappid(1295663, 1, "129446ed0c86a3cecf211aca2aca7c0ef0b14248fe2e7229afb79e90f2b07531") -- Depot 1295663
+setManifestid(1295663, "7409660199452954544", 29982463943)
+addappid(1295665, 1, "ad00cf603a7538a190b6f7e523a9f9b1ff48759aacb39f2b35a70c5302fcef37") -- Depot 1295665
+setManifestid(1295665, "9003845158150669050", 263443680)
+addappid(1295666, 1, "68e1686bc8f5a23173a86f6a5c3fb61ae2714081bf956976f7f837a1f03a9779") -- Depot 1295666
+setManifestid(1295666, "4472383718678611911", 29309953098)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2778060) -- Sid Meiers Civilization VII Deluxe Cosmetics Pack
+addappid(3004650) -- Sid Meiers Civilization VII Founders Cosmetics Pack
+addappid(3004660) -- Sid Meiers Civilization VII Crossroads of the World Collection
+addtoken(3004660, "908074924523561409")
+addappid(3087220) -- Sid Meiers Civilization VII Xerxes (The Achaemenid) Persona
+addappid(3087240) -- Sid Meiers Civilization VII Friedrich (Baroque) Persona
+addappid(3087250) -- Sid Meiers Civilization VII Deluxe Content Pack
+addappid(3087260) -- Sid Meiers Civilization VII Himiko (High Shaman) Persona
+addappid(3087270) -- Sid Meiers Civilization VII Ashoka (World Conqueror) Persona
+addappid(3087280) -- Sid Meiers Civilization VII Carthage Pack
+addappid(3087290) -- Sid Meiers Civilization VII Ada Lovelace Pack
+addappid(3087300) -- Sid Meiers Civilization VII Great Britain Pack
+addappid(3087310) -- Sid Meiers Civilization VII Simn Bolvar Pack
+addappid(3087320) -- Sid Meiers Civilization VII Bulgaria Pack
+addappid(3087330) -- Sid Meiers Civilization VII Nepal Pack
+addappid(3087350) -- Sid Meiers Civilization VII Crossroads of the World Collection - Wonder Pack
+addappid(3087370) -- Sid Meiers Civilization VII Genghis Khan Pack
+addtoken(3087370, "17615123042722017763")
+addappid(3087380) -- Sid Meiers Civilization VII Dai Viet Pack
+addtoken(3087380, "7871402320395250466")
+addappid(3087390) -- Sid Meiers Civilization VII Lakshmibai Pack
+addappid(3087400) -- Sid Meiers Civilization VII Silla Pack
+addappid(3087410) -- Sid Meiers Civilization VII Assyria Pack
+addtoken(3087410, "16879705066946463605")
+addappid(3087420) -- Sid Meiers Civilization VII Qajar Pack
+addappid(3087430) -- Sid Meiers Civilization VII Right to Rule Collection
+addappid(3087440) -- Sid Meiers Civilization VII Right to Rule Collection - Wonder Pack
+addtoken(3087440, "14302846944858302509")
+addappid(3087460) -- Sid Meiers Civilization VII Tecumseh and Shawnee Pack
+addappid(3369860) -- Sid Meiers Civilization VII Tides of Power Collection
+addappid(3490290) -- Sid Meiers Civilization VII Upgrade to Founders Edition
+addtoken(3490290, "6861301468872530534")
+addappid(3502600) -- Sid Meiers Civilization VII Upgrade to Deluxe Edition
+addtoken(3502600, "2609124707744154156")
+addappid(3958320) -- Sid Meiers Civilization VII Edward Teach Pack
+addappid(3958330) -- Sid Meiers Civilization VII Republic of Pirates Pack
+addappid(3958340) -- Sid Meiers Civilization VII Tonga Pack
+addappid(3958350) -- Sid Meiers Civilization VII Sayyida al Hurra Pack
+addappid(3958360) -- Sid Meiers Civilization VII Ottomans Pack
+addappid(3958370) -- Sid Meiers Civilization VII Iceland Pack
+addappid(3958380) -- Civilization VII - CC3 Natural Wonders  Narrative Pack
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(1295664) -- Depot 1295664 (empty depot)
