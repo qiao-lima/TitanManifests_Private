eae40d5b300809407d21ae37cd047e6fe3db04f7
qiao-lima <qiao-lima@github.com>
2026-06-12 10:21:24 +0800

    Manual Update 3250010

---
+++ 3250010.lua
@@ -0,0 +1,26 @@
+-- 3250010's Lua and Manifest Created by Hubcap Manifest
+-- Cold Bed on Route
+-- Created: June 10, 2026 at 14:38:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3250010) -- Cold Bed on Route
+-- MAIN APP DEPOTS
+addappid(3250018, 1, "141736858d0453503ff6d910649e0cc92185e2031156e974a9da741a02feb1a4") -- Depot 3250018
+setManifestid(3250018, "7956615727023690472", 255882598)
+addappid(3250019, 1, "1261d7068b2ed27e1bd4105585680eaf0f8d9ac634a26bb570725774de425fad") -- Depot 3250019
+setManifestid(3250019, "4841884451988102517", 289272487)
+-- DLCS WITH DEDICATED DEPOTS
+-- Cold Bed on Route - Artbook (AppID: 3771910)
+addappid(3771910)
+addappid(3771910, 1, "06d3d83b1664898f0c05a2fb9098d0457fbcf0670f7a1361a171997ad704fa49") -- Cold Bed on Route - Artbook - Depot 3771910
+setManifestid(3771910, "3288083352966536202", 27933956)
+addappid(3771911, 1, "59e8edfd7d9d3a8ef5ce817f8db16923e2d27023d1d5e241755d607e2bc67650") -- Cold Bed on Route - Artbook - Depot 3771911
+setManifestid(3771911, "1631917500679602841", 24191759)
+-- addappid(3771914, 1, "MISSING_KEY") -- Cold Bed on Route - Artbook - Depot 3771914 (no key available)
+-- addappid(3771912, 1, "MISSING_KEY") -- Cold Bed on Route - Artbook - Depot 3771912 (no key available)
+-- addappid(3771913, 1, "MISSING_KEY") -- Cold Bed on Route - Artbook - Depot 3771913 (no key available)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3250011) -- Depot 3250011 (empty depot)
