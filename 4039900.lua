190a096a0ce8bb452f24348b516857edde381c67
qiao-lima <qiao-lima@github.com>
2026-06-12 22:18:26 +0800

    Manual Update 4039900

---
+++ 4039900.lua
@@ -0,0 +1,18 @@
+-- 4039900's Lua and Manifest Created by Hubcap Manifest
+-- ZOMBUTCHER
+-- Created: June 12, 2026 at 00:09:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4039900) -- ZOMBUTCHER
+-- MAIN APP DEPOTS
+addappid(4039901, 1, "8ecde09923727d9747f95e819de663a4d465ab2351429357b4b928dd82557f17") -- Depot 4039901
+setManifestid(4039901, "2362247888247778140", 6618961284)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
