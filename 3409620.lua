f14790933eada508bc01748e619e359fc7f74c02
qiao-lima <qiao-lima@github.com>
2026-06-12 10:21:25 +0800

    Manual Update 3409620

---
+++ 3409620.lua
@@ -0,0 +1,12 @@
+-- 3409620's Lua and Manifest Created by Hubcap Manifest
+-- The Path Home
+-- Created: June 10, 2026 at 14:24:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3409620) -- The Path Home
+-- MAIN APP DEPOTS
+addappid(3409621, 1, "e707ada961d4ac82822f2ad6f2e94f50ea11875457dd5d057a93a59de35172fe") -- Depot 3409621
+setManifestid(3409621, "4005760112529872609", 261684995)
