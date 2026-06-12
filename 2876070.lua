377daefb74e302c94438cbb2d62e6f7434bc33b7
qiao-lima <qiao-lima@github.com>
2026-06-12 22:18:23 +0800

    Manual Update 2876070

---
+++ 2876070.lua
@@ -0,0 +1,22 @@
+-- 2876070's Lua and Manifest Created by Hubcap Manifest
+-- Black Hole Void
+-- Created: June 12, 2026 at 06:13:08 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 1
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(2876070) -- Black Hole Void
+-- MAIN APP DEPOTS
+addappid(2876071, 1, "1ff08a080a8038b3844514af65d993d3facb3214b85f740f94a9d48ec41bc121") -- Depot 2876071
+setManifestid(2876071, "5427734653510034701", 1511936379)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3061830) -- Black Hole Void Survive The Hole DLC
