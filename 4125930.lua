8f3408d5eec8748c8907f4d5a8e73464dd1b60c7
qiao-lima <qiao-lima@github.com>
2026-06-13 00:59:26 +0800

    Manual Update 4125930

---
+++ 4125930.lua
@@ -0,0 +1,18 @@
+-- 4125930's Lua and Manifest Created by Hubcap Manifest
+-- Punishing: Gray Raven
+-- Created: June 03, 2026 at 14:54:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4125930, 1, "dd72e7255410b6f1188dd532059d08f4cd09a790848dc866368d3b11a3efb946") -- Punishing: Gray Raven
+-- MAIN APP DEPOTS
+addappid(4125931, 1, "5ddf6d61db4383bdceb71393cde88e6e83a6024c96627e416e2fef737425499b") -- Depot 4125931
+setManifestid(4125931, "9021156471045188148", 64049832661)
+addappid(4125933, 1, "83ec653dfae7ee05c0f41238dd6fc540adac478ffe5cae5e6d4bc74b59b9c2c1") -- Depot 4125933
+setManifestid(4125933, "4963583631160590201", 64656517925)
+addappid(4125932, 1, "5fb9b2e60206f41999a4909c322293b58d88a1acde33061df30af9fb82ca02d0") -- Depot 4125932
+setManifestid(4125932, "2365023522214693765", 64097446572)
+addappid(4125934, 1, "ca723c2ece25aafbb1ee23425d389a32e4085e366a573e98227e61b3861fe11f") -- Depot 4125934
+setManifestid(4125934, "5813152099102118689", 64641053608)
