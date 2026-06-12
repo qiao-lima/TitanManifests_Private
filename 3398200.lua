18229baf9f2ebad7b77dce842e5d409ca571d48d
qiao-lima <qiao-lima@github.com>
2026-06-12 22:18:24 +0800

    Manual Update 3398200

---
+++ 3398200.lua
@@ -0,0 +1,12 @@
+-- 3398200's Lua and Manifest Created by Hubcap Manifest
+-- Pacifish: Recast
+-- Created: June 12, 2026 at 08:42:30 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3398200) -- Pacifish: Recast
+-- MAIN APP DEPOTS
+addappid(3398201, 1, "4924d06cf7eaa2175a080083af6906709c37350bf0df72529144de888566ec52") -- Depot 3398201
+setManifestid(3398201, "5570752351722322337", 176985363)
