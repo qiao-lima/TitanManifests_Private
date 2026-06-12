8413cb3244351c10c528a9d8bf9938812bb013a4
qiao-lima <qiao-lima@github.com>
2026-06-12 10:21:25 +0800

    Manual Update 3422290

---
+++ 3422290.lua
@@ -0,0 +1,12 @@
+-- 3422290's Lua and Manifest Created by Hubcap Manifest
+-- The Way We All Go
+-- Created: June 10, 2026 at 14:56:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3422290) -- The Way We All Go
+-- MAIN APP DEPOTS
+addappid(3422291, 1, "13f1c765c96b50fa890a0920cd64a84bbe358583e5417b4c2d769ac04743e77f") -- Depot 3422291
+setManifestid(3422291, "514777756603194086", 921373306)
