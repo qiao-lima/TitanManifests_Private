5ceb0f5d4135b6a775ece4373fde8a8da96cb708
qiao-lima <qiao-lima@github.com>
2026-06-12 22:18:24 +0800

    Manual Update 3428570

---
+++ 3428570.lua
@@ -0,0 +1,12 @@
+-- 3428570's Lua and Manifest Created by Hubcap Manifest
+-- Teller's Duty
+-- Created: June 12, 2026 at 08:16:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3428570) -- Teller's Duty
+-- MAIN APP DEPOTS
+addappid(3428571, 1, "bb82ff7741f017db02f1aae7dce5f05d252e53801837ff9b604f7175120e379c") -- Depot 3428571
+setManifestid(3428571, "5686536629838002157", 1858756474)
