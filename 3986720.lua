05c940f61741dd62b90f12b392d6e2e26787b671
qiao-lima <qiao-lima@github.com>
2026-06-12 22:18:26 +0800

    Manual Update 3986720

---
+++ 3986720.lua
@@ -0,0 +1,12 @@
+-- 3986720's Lua and Manifest Created by Hubcap Manifest
+-- Hentai Magical Warriors
+-- Created: June 12, 2026 at 00:05:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3986720) -- Hentai Magical Warriors
+-- MAIN APP DEPOTS
+addappid(3986721, 1, "764d68570c63668a6ce0c4218afee57c37dced524ed5a0985227d9ce042573a3") -- Depot 3986721
+setManifestid(3986721, "822213192427416551", 1145211117)
