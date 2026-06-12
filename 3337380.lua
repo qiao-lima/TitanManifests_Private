8a3fcffbf3cab0417c1dc730d8a508f7325d9be0
qiao-lima <qiao-lima@github.com>
2026-06-12 22:18:24 +0800

    Manual Update 3337380

---
+++ 3337380.lua
@@ -0,0 +1,12 @@
+-- 3337380's Lua and Manifest Created by Hubcap Manifest
+-- Tabletop Tavern
+-- Created: June 12, 2026 at 06:58:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3337380, 1, "42c99ecf0da79c0ace1c7b96e1cc3c05c294f4212fbe49ccdd19075d26ed3aae") -- Tabletop Tavern
+-- MAIN APP DEPOTS
+addappid(3337382, 1, "31e627b11c7ebb087b53481732fc2b3c8fde3a48fe11744a988ab6739d56e9f6") -- Depot 3337382
+setManifestid(3337382, "8301091239554396402", 6167603118)
