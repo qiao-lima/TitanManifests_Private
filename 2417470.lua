5c6d5e47e04d7755761b21d70b73dec1ef2fe676
qiao-lima <qiao-lima@github.com>
2026-06-12 22:18:22 +0800

    Manual Update 2417470

---
+++ 2417470.lua
@@ -0,0 +1,18 @@
+-- 2417470's Lua and Manifest Created by Hubcap Manifest
+-- Beastro
+-- Created: June 12, 2026 at 00:05:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2417470) -- Beastro
+-- MAIN APP DEPOTS
+addappid(2417471, 1, "b467b8937ecd8c2fb14ff891d7625e4bbfe31ade3c85328770590b0d6542592d") -- Depot 2417471
+setManifestid(2417471, "697813817562315626", 2902602261)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
