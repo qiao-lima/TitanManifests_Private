e538e1f8e08d23422d5550ac11c4c29603f3149d
qiao-lima <qiao-lima@github.com>
2026-06-12 10:21:26 +0800

    Manual Update 3989960

---
+++ 3989960.lua
@@ -0,0 +1,18 @@
+-- 3989960's Lua and Manifest Created by Hubcap Manifest
+-- Frog Sqwad 🐸
+-- Created: June 11, 2026 at 12:17:53 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(3989960) -- Frog Sqwad 🐸
+-- MAIN APP DEPOTS
+addappid(3989961, 1, "fb391a4810744e0dfb08b88ba978b48c0c32ebb6f333c21a49fcfa79c63a4e77") -- Depot 3989961
+setManifestid(3989961, "6044712557753671239", 1633157819)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4581310) -- Frog Sqwad  Supporter Pack
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Frog Sqwad  Artbook (AppID: 4795130) - missing depot keys
+-- addappid(4795130)
