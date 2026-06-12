fc13fe94c43d2e2929ee12fe9eea083dce2426e5
qiao-lima <qiao-lima@github.com>
2026-06-12 10:21:24 +0800

    Manual Update 3276050

---
+++ 3276050.lua
@@ -0,0 +1,16 @@
+-- 3276050's Lua and Manifest Created by Hubcap Manifest
+-- SpaceCraft
+-- Created: June 11, 2026 at 10:35:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3276050, 1, "5cc9654e1e343d72f13d62953a46077d18c93b90f7ec9058b0d827e613df9099") -- SpaceCraft
+-- MAIN APP DEPOTS
+addappid(3276051, 1, "16f4c3c685babde916480904915bceab31c662c08053e6776b5a875e9fefd614") -- Depot 3276051
+setManifestid(3276051, "6592442129920134465", 17339480915)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
