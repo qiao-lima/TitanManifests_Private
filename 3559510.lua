0cb534cd8903b4bad0b3f44b18d710e15f0ec75c
qiao-lima <qiao-lima@github.com>
2026-06-12 22:18:25 +0800

    Manual Update 3559510

---
+++ 3559510.lua
@@ -0,0 +1,17 @@
+-- 3559510's Lua and Manifest Created by Hubcap Manifest
+-- Capsule Lover
+-- Created: June 12, 2026 at 08:11:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3559510) -- Capsule Lover
+-- MAIN APP DEPOTS
+addappid(3559511, 1, "aa040048a15803e8d874471802560bd2357f389d1e9b0ae5fadc6bf15f02e35b") -- Depot 3559511
+setManifestid(3559511, "6059160550283441163", 1387009916)
+-- DLCS WITH DEDICATED DEPOTS
+-- Capsule Lover - Official Artworks (AppID: 4801440)
+addappid(4801440)
+addappid(4801440, 1, "fba0cce05f069a4d481a91a7ca914fa5ecfcf9cb61b65fb7cff0f9f1711023a3") -- Capsule Lover - Official Artworks - Depot 4801440
+setManifestid(4801440, "5417424108922796129", 137247646)
