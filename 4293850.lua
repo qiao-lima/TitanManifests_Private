5f65c196cc81997c28469a45ecdc916f1468602e
qiao-lima <qiao-lima@github.com>
2026-06-12 22:18:27 +0800

    Manual Update 4293850

---
+++ 4293850.lua
@@ -0,0 +1,12 @@
+-- 4293850's Lua and Manifest Created by Hubcap Manifest
+-- Wet Wealth
+-- Created: June 12, 2026 at 08:01:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4293850) -- Wet Wealth
+-- MAIN APP DEPOTS
+addappid(4293851, 1, "cf1d398f91159528ba2b18e81d5a2a1610063a14f0e72d9e46e87196ff4fdd4a") -- Depot 4293851
+setManifestid(4293851, "6997386660530228041", 4120546236)
