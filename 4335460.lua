00f1ea4065a979906990056c9472ccd2eff23e46
qiao-lima <qiao-lima@github.com>
2026-06-12 10:21:27 +0800

    Manual Update 4335460

---
+++ 4335460.lua
@@ -0,0 +1,12 @@
+-- 4335460's Lua and Manifest Created by Hubcap Manifest
+-- BOXROOM
+-- Created: June 11, 2026 at 19:25:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4335460) -- BOXROOM
+-- MAIN APP DEPOTS
+addappid(4335461, 1, "27cd94e1749b73fab0eeea2ac7ffdbf3c4ae2b3e85545ed38900f2b26be5f025") -- Depot 4335461
+setManifestid(4335461, "6303980265640031849", 1602557010)
