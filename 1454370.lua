7d76894a48c322cf7dc31b3e6863b69aca8b2757
qiao-lima <qiao-lima@github.com>
2026-06-12 10:21:21 +0800

    Manual Update 1454370

---
+++ 1454370.lua
@@ -0,0 +1,20 @@
+-- 1454370's Lua and Manifest Created by Hubcap Manifest
+-- STARSEEKER: Astroneer Expeditions
+-- Created: June 11, 2026 at 13:03:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1454370) -- STARSEEKER: Astroneer Expeditions
+-- MAIN APP DEPOTS
+addappid(1454371, 1, "a7c53d97c1889701de98ceb4868d7ad32c2d874300bca09aecaa38f202ad0c91") -- Depot 1454371
+setManifestid(1454371, "8791728349264112222", 5027524931)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4440450) -- STARSEEKER - Engineering Pack
