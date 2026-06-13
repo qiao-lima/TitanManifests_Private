66d9665ee7beb0a2fe719cf8148880452bd6439b
qiao-lima <qiao-lima@github.com>
2026-06-14 07:59:13 +0800

    Manual Update 4225020

---
+++ 4225020.lua
@@ -0,0 +1,18 @@
+-- 4225020's Lua and Manifest Created by Hubcap Manifest
+-- SILVARN
+-- Created: June 13, 2026 at 00:49:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4225020) -- SILVARN
+-- MAIN APP DEPOTS
+addappid(4225021, 1, "d09864af9a5342d3faecad691b21fa766fa48f4a1f383709dd3da09f33e841fa") -- Depot 4225021
+setManifestid(4225021, "2243685525388312869", 12127241892)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
