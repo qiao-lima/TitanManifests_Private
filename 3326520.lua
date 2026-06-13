51c136a3b323e8ca00a8543d8bda42c0a7bafaa0
qiao-lima <qiao-lima@github.com>
2026-06-14 07:59:09 +0800

    Manual Update 3326520

---
+++ 3326520.lua
@@ -0,0 +1,18 @@
+-- 3326520's Lua and Manifest Created by Hubcap Manifest
+-- Banisher Of Lust
+-- Created: February 16, 2026 at 07:57:29 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3326520) -- Banisher Of Lust
+-- MAIN APP DEPOTS
+addappid(3326521, 1, "b5c9f422fd760032faa712bc4f6c89af8e77793c3260cb51ddf0cf881aa96fb4") -- Depot 3326521
+setManifestid(3326521, "783816355920405056", 2134988896)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
