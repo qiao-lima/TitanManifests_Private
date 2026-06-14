4fdd33b4fdfba206b99d2f9a8901c2d1eaa0743b
qiao-lima <qiao-lima@github.com>
2026-06-14 08:24:14 +0800

    Manual Update 4762630

---
+++ 4762630.lua
@@ -0,0 +1,14 @@
+-- 4762630's Lua and Manifest Created by Hubcap Manifest
+-- 尸皇贴贴
+-- Created: June 12, 2026 at 16:05:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4762630) -- 尸皇贴贴
+-- MAIN APP DEPOTS
+addappid(4762632, 1, "03396fdf0b309b23ba85b5d47faee37a0a55a3dd785bb1481623dfd32ee024a0") -- Depot 4762632
+setManifestid(4762632, "8359949299287475438", 11894914174)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4762631) -- Depot 4762631 (empty depot)
