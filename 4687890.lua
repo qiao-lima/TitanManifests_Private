f422c88dcd1e498a3e20b71e59e77b20998b7322
qiao-lima <qiao-lima@github.com>
2026-06-14 19:48:15 +0800

    Manual Update 4687890

---
+++ 4687890.lua
@@ -0,0 +1,12 @@
+-- 4687890's Lua and Manifest Created by Hubcap Manifest
+-- Bunny Girl & Healing Service - A Special Menu Just for You
+-- Created: June 14, 2026 at 01:28:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4687890) -- Bunny Girl & Healing Service - A Special Menu Just for You
+-- MAIN APP DEPOTS
+addappid(4687891, 1, "209f8bbe3f729c766d11479b66ba693268477fac75afc184a17761f989aebc73") -- Depot 4687891
+setManifestid(4687891, "2182355615914028870", 548229067)
