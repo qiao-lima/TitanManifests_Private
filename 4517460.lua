ab010695bf101578bd4d7026e6273155056570ab
qiao-lima <qiao-lima@github.com>
2026-06-14 22:06:21 +0800

    Manual Update 4517460

---
+++ 4517460.lua
@@ -0,0 +1,18 @@
+-- 4517460's Lua and Manifest Created by Hubcap Manifest
+-- Total Football Online
+-- Created: June 14, 2026 at 07:52:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(4517460) -- Total Football Online
+-- MAIN APP DEPOTS
+addappid(4517461, 1, "ab4f87f5dd02dd9a885579aed060c43ae91607de5019ee265bcee6b3a4961ab9") -- Depot 4517461
+setManifestid(4517461, "3218778291129750493", 29787358136)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4774220) -- Total Football Online  Licensed National Teams Collection
