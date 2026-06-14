876756018f420a752ec334ef427b36a8d68e3fad
qiao-lima <qiao-lima@github.com>
2026-06-14 08:23:56 +0800

    Manual Update 4381330

---
+++ 4381330.lua
@@ -0,0 +1,18 @@
+-- 4381330's Lua and Manifest Created by Hubcap Manifest
+-- Disharmony
+-- Created: June 13, 2026 at 00:52:30 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4381330) -- Disharmony
+-- MAIN APP DEPOTS
+addappid(4381331, 1, "b0e353f1d5e4e388389e92b47f5eddfbb45637635ec71c3eed4390d7783c1ade") -- Depot 4381331
+setManifestid(4381331, "2779502460324483636", 7898660914)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
