25fd264507e476a485a85be8bcb885f955c97cbf
qiao-lima <qiao-lima@github.com>
2026-06-14 22:06:21 +0800

    Manual Update 4432340

---
+++ 4432340.lua
@@ -0,0 +1,12 @@
+-- 4432340's Lua and Manifest Created by Hubcap Manifest
+-- Nyan Nyan Maid: Sex Life!!
+-- Created: June 14, 2026 at 08:51:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4432340) -- Nyan Nyan Maid: Sex Life!!
+-- MAIN APP DEPOTS
+addappid(4432341, 1, "79db2b9432e1021f3a81235df19be0becb41d4217dc8a639856aba1be60f3273") -- Depot 4432341
+setManifestid(4432341, "2314214914662498418", 488823890)
