f9142306463ea6f7e35f25b1914a57c7849a80d6
qiao-lima <qiao-lima@github.com>
2026-06-14 08:24:13 +0800

    Manual Update 4762620

---
+++ 4762620.lua
@@ -0,0 +1,14 @@
+-- 4762620's Lua and Manifest Created by Hubcap Manifest
+-- 物理超度
+-- Created: June 12, 2026 at 16:05:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4762620) -- 物理超度
+-- MAIN APP DEPOTS
+addappid(4762622, 1, "f92753217d866f405530a6a012df06f71db04fe0dc2f600c084675035ff39f36") -- Depot 4762622
+setManifestid(4762622, "1273443602981596238", 5564065884)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4762621) -- Depot 4762621 (empty depot)
