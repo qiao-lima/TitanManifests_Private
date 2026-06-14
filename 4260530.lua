efaa06f4fc4fb46731d2df8412a1ce0160ca0125
qiao-lima <qiao-lima@github.com>
2026-06-14 08:23:55 +0800

    Manual Update 4260530

---
+++ 4260530.lua
@@ -0,0 +1,12 @@
+-- 4260530's Lua and Manifest Created by Hubcap Manifest
+-- Rina's Undercover Train Operation
+-- Created: June 13, 2026 at 05:56:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4260530) -- Rina's Undercover Train Operation
+-- MAIN APP DEPOTS
+addappid(4260531, 1, "1d52269a782113dfe959673c692f453d605d1bd70aeaf5fc099ab3cd6f409323") -- Depot 4260531
+setManifestid(4260531, "7156380283524005478", 1518856081)
