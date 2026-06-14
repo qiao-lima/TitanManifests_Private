df27dc1cd4610dd65706cf8fcd860cc3dfab6dc9
qiao-lima <qiao-lima@github.com>
2026-06-14 08:23:56 +0800

    Manual Update 4398360

---
+++ 4398360.lua
@@ -0,0 +1,12 @@
+-- 4398360's Lua and Manifest Created by Hubcap Manifest
+-- Reverse Dream of the Menasphere
+-- Created: June 13, 2026 at 03:32:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4398360) -- Reverse Dream of the Menasphere
+-- MAIN APP DEPOTS
+addappid(4398361, 1, "2222d8b40b60763ae70921be6af7e9774bfe19eb3de7105b7bb08fc25216de41") -- Depot 4398361
+setManifestid(4398361, "596973300118030815", 1304499758)
