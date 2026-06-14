0870daa0032b3bf5046d7038daa797ee9ec57129
qiao-lima <qiao-lima@github.com>
2026-06-14 08:23:58 +0800

    Manual Update 4668230

---
+++ 4668230.lua
@@ -0,0 +1,12 @@
+-- 4668230's Lua and Manifest Created by Hubcap Manifest
+-- My Neighbors are Actresses
+-- Created: June 13, 2026 at 06:20:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4668230, 1, "0c4b6e206158f183727dbe6c55da45f4a8e28c787f2aa90b332811239fdc00cb") -- My Neighbors are Actresses
+-- MAIN APP DEPOTS
+addappid(4668231, 1, "b2e69b6152aa3a7fa66400f5c152d20f69f46f81aaee1f164354fbcf75c35cb9") -- Depot 4668231
+setManifestid(4668231, "6450923207606405670", 573871499)
