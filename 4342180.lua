4a7a9acede5f3cf0c3927e91e8ce1804b5b824e6
qiao-lima <qiao-lima@github.com>
2026-06-14 08:23:56 +0800

    Manual Update 4342180

---
+++ 4342180.lua
@@ -0,0 +1,12 @@
+-- 4342180's Lua and Manifest Created by Hubcap Manifest
+-- Ice Melts 2: Caribbean Cruise
+-- Created: June 12, 2026 at 14:00:36 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4342180) -- Ice Melts 2: Caribbean Cruise
+-- MAIN APP DEPOTS
+addappid(4342181, 1, "1762d7aae0ee4fa0666a49c22660f239469dc28c50546cc8d6770c1153e99520") -- Depot 4342181
+setManifestid(4342181, "5424085854729385703", 878956722)
