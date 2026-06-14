8ea389489728084da74b0858c7bd33703dc28509
qiao-lima <qiao-lima@github.com>
2026-06-14 08:23:59 +0800

    Manual Update 4761870

---
+++ 4761870.lua
@@ -0,0 +1,16 @@
+-- 4761870's Lua and Manifest Created by Hubcap Manifest
+-- Buerokratie Idler: Germany
+-- Created: June 13, 2026 at 00:52:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(4761870) -- Buerokratie Idler: Germany
+-- MAIN APP DEPOTS
+addappid(4761871, 1, "93b2eba6b48d01333b47fbb3560551f1c0411408f8dfa02aff945c26f41f1fe2") -- Depot 4761871
+setManifestid(4761871, "8025197185826855316", 314766286)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
