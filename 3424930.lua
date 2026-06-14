eebe7adb270d81c56a4cfc10a38828e8ea7b5b41
qiao-lima <qiao-lima@github.com>
2026-06-14 08:23:54 +0800

    Manual Update 3424930

---
+++ 3424930.lua
@@ -0,0 +1,12 @@
+-- 3424930's Lua and Manifest Created by Hubcap Manifest
+-- Tome of the Damned
+-- Created: June 13, 2026 at 00:53:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3424930) -- Tome of the Damned
+-- MAIN APP DEPOTS
+addappid(3424931, 1, "5c7afbcab8684e970ed45ece63c32a661f90d5db49a2547304c3b9f55fce8afa") -- Depot 3424931
+setManifestid(3424931, "4461113818259088434", 7413534596)
