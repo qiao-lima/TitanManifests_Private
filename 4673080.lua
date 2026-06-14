9459c94ff4246c3c450d3f52988af12812c77b6d
qiao-lima <qiao-lima@github.com>
2026-06-14 08:23:58 +0800

    Manual Update 4673080

---
+++ 4673080.lua
@@ -0,0 +1,12 @@
+-- 4673080's Lua and Manifest Created by Hubcap Manifest
+-- Sloperator Inc.
+-- Created: June 13, 2026 at 20:20:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4673080) -- Sloperator Inc.
+-- MAIN APP DEPOTS
+addappid(4673081, 1, "92a82e1b5256c9f81cf2e73de5aa9ae17a5f2143345dabfac3e33b352017f718") -- Depot 4673081
+setManifestid(4673081, "108573795873123420", 1902822345)
