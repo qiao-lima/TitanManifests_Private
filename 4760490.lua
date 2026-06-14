8c56b2469b6ba8b29c6b4b29d4f1db11f71ef5d0
qiao-lima <qiao-lima@github.com>
2026-06-14 19:54:09 +0800

    Manual Update 4760490

---
+++ 4760490.lua
@@ -0,0 +1,12 @@
+-- 4760490's Lua and Manifest Created by Hubcap Manifest
+-- Ten Days Nightmare
+-- Created: June 14, 2026 at 02:26:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4760490) -- Ten Days Nightmare
+-- MAIN APP DEPOTS
+addappid(4760491, 1, "3dfefa59e9ec245defd67abdc2c890f5c2e88350ab9c83c7933f647fe1e95b45") -- Depot 4760491
+setManifestid(4760491, "337405174120282091", 1187923474)
