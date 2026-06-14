6f434ffcb6ab23d6c3becf2728e60023317864c5
qiao-lima <qiao-lima@github.com>
2026-06-14 08:24:00 +0800

    Manual Update 4762210

---
+++ 4762210.lua
@@ -0,0 +1,12 @@
+-- 4762210's Lua and Manifest Created by Hubcap Manifest
+-- Yandere Goth Boss 2: First Session
+-- Created: June 13, 2026 at 06:21:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4762210) -- Yandere Goth Boss 2: First Session
+-- MAIN APP DEPOTS
+addappid(4762211, 1, "dee428918b9deb86ec2a22d37a85da8dd9158cba176ef6b88bcbccc072469aa5") -- Depot 4762211
+setManifestid(4762211, "4945937339668338631", 311829462)
