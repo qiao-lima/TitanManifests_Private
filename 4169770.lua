d122b58864689d8c945a6db3454880d699e2f540
qiao-lima <qiao-lima@github.com>
2026-06-14 08:23:55 +0800

    Manual Update 4169770

---
+++ 4169770.lua
@@ -0,0 +1,12 @@
+-- 4169770's Lua and Manifest Created by Hubcap Manifest
+-- Slice the Crops!
+-- Created: June 13, 2026 at 15:02:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4169770, 1, "2ae6c7280a84d327f52710c34f49ce6533c1740a2dbb3cd6aed586a1b8096bfc") -- Slice the Crops!
+-- MAIN APP DEPOTS
+addappid(4169771, 1, "be56bdb6700fa0b2c0f07a88d3119705e152e7beb59faad508d9cd3404d0db97") -- Depot 4169771
+setManifestid(4169771, "6104482708823730692", 712454780)
