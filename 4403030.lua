9461ba5dcd09c3bf5d63566b07825246aa5a3fda
qiao-lima <qiao-lima@github.com>
2026-06-14 08:23:57 +0800

    Manual Update 4403030

---
+++ 4403030.lua
@@ -0,0 +1,12 @@
+-- 4403030's Lua and Manifest Created by Hubcap Manifest
+-- Girl, Don’t Distract My Zombie Survival
+-- Created: June 12, 2026 at 16:04:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4403030) -- Girl, Don’t Distract My Zombie Survival
+-- MAIN APP DEPOTS
+addappid(4403031, 1, "4e0d8614708bc1ec47468c8082d0830684bb2f9a712b67ee9366352ddef00f39") -- Depot 4403031
+setManifestid(4403031, "3499664386708695534", 11231057320)
