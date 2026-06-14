4d89e4325a21a76348e877436d0451380b11a8e0
qiao-lima <qiao-lima@github.com>
2026-06-14 19:48:10 +0800

    Manual Update 3125090

---
+++ 3125090.lua
@@ -0,0 +1,12 @@
+-- 3125090's Lua and Manifest Created by Hubcap Manifest
+-- FIGHTING GIRL YURI
+-- Created: June 14, 2026 at 01:27:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3125090) -- FIGHTING GIRL YURI
+-- MAIN APP DEPOTS
+addappid(3125091, 1, "bfaef6a6cf756d4786f8b7c6fc6b509394790a739126efb2fd1b4d180ff74bd1") -- Depot 3125091
+setManifestid(3125091, "3417933108533307073", 840317986)
