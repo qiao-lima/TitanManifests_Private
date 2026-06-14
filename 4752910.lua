76a7d61c1e65334bcc05f23cb535d097905d40a6
qiao-lima <qiao-lima@github.com>
2026-06-14 08:23:59 +0800

    Manual Update 4752910

---
+++ 4752910.lua
@@ -0,0 +1,12 @@
+-- 4752910's Lua and Manifest Created by Hubcap Manifest
+-- Android Who Dreams of Stars
+-- Created: June 12, 2026 at 16:05:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4752910, 1, "9b069d5283384f723a4aa696969b910266773431ac84ee2582a730a3cc4de857") -- Android Who Dreams of Stars
+-- MAIN APP DEPOTS
+addappid(4752911, 1, "d1b3f993b398a5d170ac3e672acf50496704ead0171653eb3329a448ece85f51") -- Depot 4752911
+setManifestid(4752911, "7929556764625291394", 193527407)
