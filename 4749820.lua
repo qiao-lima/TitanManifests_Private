0da2f77a0f63b289727d5426de38a1a9a735192b
qiao-lima <qiao-lima@github.com>
2026-06-14 08:23:59 +0800

    Manual Update 4749820

---
+++ 4749820.lua
@@ -0,0 +1,14 @@
+-- 4749820's Lua and Manifest Created by Hubcap Manifest
+-- IsoDead TD
+-- Created: June 13, 2026 at 00:51:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4749820) -- IsoDead TD
+-- MAIN APP DEPOTS
+addappid(4749821, 1, "3de7f1c5af545efeeed69948be1083d4afab1206b3e2d07c0d7e949c9bd2e0d8") -- Depot 4749821
+setManifestid(4749821, "2609049046143187509", 286077282)
+addappid(4749822, 1, "fd137a08d8f6461405f8539537511f7f949996e1834ba1c0e1b2f1e5926249cc") -- Depot 4749822
+setManifestid(4749822, "3399087833266362685", 285630968)
