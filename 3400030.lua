f225d79d131b572e1c453bcde0b524e50df8f6b2
qiao-lima <qiao-lima@github.com>
2026-06-14 08:23:53 +0800

    Manual Update 3400030

---
+++ 3400030.lua
@@ -0,0 +1,12 @@
+-- 3400030's Lua and Manifest Created by Hubcap Manifest
+-- Match! The 12 CZ Wars
+-- Created: June 13, 2026 at 07:24:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3400030) -- Match! The 12 CZ Wars
+-- MAIN APP DEPOTS
+addappid(3400031, 1, "2efc30cb957851ff7840f62af5a0e124b6b8113555d8079225bef0e6c7b05157") -- Depot 3400031
+setManifestid(3400031, "4727998384548136385", 568468760)
