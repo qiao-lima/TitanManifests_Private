718390d6557bd518c324869dfa67a4f69b924e1f
qiao-lima <qiao-lima@github.com>
2026-06-14 08:23:54 +0800

    Manual Update 3930510

---
+++ 3930510.lua
@@ -0,0 +1,14 @@
+-- 3930510's Lua and Manifest Created by Hubcap Manifest
+-- Don't Let It Starve
+-- Created: June 13, 2026 at 20:21:08 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3930510) -- Don't Let It Starve
+-- MAIN APP DEPOTS
+addappid(3930511, 1, "655f9e1801c4d299369bc9dea89e0b1277b1f793103b4ee9a27950df8da79c72") -- Depot 3930511
+setManifestid(3930511, "8926554154620340328", 309832968)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4805540) -- Dont Let It Starve - Supporter Pack
