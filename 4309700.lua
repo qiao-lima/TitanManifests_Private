bc48e4a84d2c925114d95b077739f307269bd8d7
qiao-lima <qiao-lima@github.com>
2026-06-14 19:48:14 +0800

    Manual Update 4309700

---
+++ 4309700.lua
@@ -0,0 +1,12 @@
+-- 4309700's Lua and Manifest Created by Hubcap Manifest
+-- 别被她抓到
+-- Created: June 14, 2026 at 01:27:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4309700) -- 别被她抓到
+-- MAIN APP DEPOTS
+addappid(4309701, 1, "690a63d4192db667e97af8b1c28dafbbeafc6ac8cfb6bdd4840f329a363a286e") -- Depot 4309701
+setManifestid(4309701, "4350619771845529963", 53827876)
