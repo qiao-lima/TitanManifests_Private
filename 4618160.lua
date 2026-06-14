ddb83bf2b18240dc35c9d71aaee5faca7bdf9770
qiao-lima <qiao-lima@github.com>
2026-06-14 08:23:57 +0800

    Manual Update 4618160

---
+++ 4618160.lua
@@ -0,0 +1,18 @@
+-- 4618160's Lua and Manifest Created by Hubcap Manifest
+-- Lobotomy
+-- Created: June 13, 2026 at 00:49:30 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4618160) -- Lobotomy
+-- MAIN APP DEPOTS
+addappid(4618161, 1, "3200f2a7807836c10563104d8d65f4779cff791ff9a372bd07343eccb425f585") -- Depot 4618161
+setManifestid(4618161, "4589603088770798518", 3043434930)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
