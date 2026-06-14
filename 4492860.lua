28c4f43cf19248e4b05af923be548e74d86d84a1
qiao-lima <qiao-lima@github.com>
2026-06-14 08:23:57 +0800

    Manual Update 4492860

---
+++ 4492860.lua
@@ -0,0 +1,12 @@
+-- 4492860's Lua and Manifest Created by Hubcap Manifest
+-- Sisters' Aegis
+-- Created: June 13, 2026 at 00:52:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4492860) -- Sisters' Aegis
+-- MAIN APP DEPOTS
+addappid(4492861, 1, "cb0444769a8fd84cb6a97732aa07d1cd05a80e3c5d96ef5d6ba49a8bc6b1f5d1") -- Depot 4492861
+setManifestid(4492861, "7606843687505441670", 777033376)
