4ea9c48395bc71cbaf30d2b4616ceb6a2fee53fd
qiao-lima <qiao-lima@github.com>
2026-06-14 08:24:00 +0800

    Manual Update 4762610

---
+++ 4762610.lua
@@ -0,0 +1,14 @@
+-- 4762610's Lua and Manifest Created by Hubcap Manifest
+-- 魔主归来
+-- Created: June 12, 2026 at 16:05:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4762610) -- 魔主归来
+-- MAIN APP DEPOTS
+addappid(4762612, 1, "701b6e179f717b3dafc9d93bceb3bff10f6229629c56324a081298d6b7372653") -- Depot 4762612
+setManifestid(4762612, "3767601182665027210", 10536670666)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4762611) -- Depot 4762611 (empty depot)
