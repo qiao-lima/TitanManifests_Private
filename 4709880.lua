403d4dbc4c1176dcc0a4980af54b588a1a0ac7ca
qiao-lima <qiao-lima@github.com>
2026-06-14 08:23:58 +0800

    Manual Update 4709880

---
+++ 4709880.lua
@@ -0,0 +1,14 @@
+-- 4709880's Lua and Manifest Created by Hubcap Manifest
+-- JPMAC: Privileged
+-- Created: June 13, 2026 at 06:21:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4709880) -- JPMAC: Privileged
+-- MAIN APP DEPOTS
+addappid(4709881, 1, "f80638b4192b437779444e69b256cafb6432415646180ed6d411d2473b455414") -- Depot 4709881
+setManifestid(4709881, "4112908823669605039", 1084914558)
+addappid(4709882, 1, "00d7f61196f67a4b1515145e4e4cf824e81e944df6d232b81036361d8d578cbb") -- Depot 4709882
+setManifestid(4709882, "7572213429085546843", 930556822)
