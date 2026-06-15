6181f1ec3bec70b3a96c530d32a49e565076ab44
qiao-lima <qiao-lima@github.com>
2026-06-16 07:30:59 +0800

    Manual Update 4216930

---
+++ 4216930.lua
@@ -0,0 +1,12 @@
+-- 4216930's Lua and Manifest Created by Hubcap Manifest
+-- Wicked Ways
+-- Created: June 15, 2026 at 16:21:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4216930, 1, "4491b6db068bc2029601613820d6b34877578ecef23c71c539926fee795b216c") -- Wicked Ways
+-- MAIN APP DEPOTS
+addappid(4216931, 1, "937c70ed676b11eab67a587b7b308458f13149403e3f7d060d8b26ffa56b083d") -- Depot 4216931
+setManifestid(4216931, "8365759293408171870", 1486696080)
