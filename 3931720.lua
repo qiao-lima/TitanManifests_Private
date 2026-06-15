4f3ab15624c004aae130892937cd917e27d578cb
qiao-lima <qiao-lima@github.com>
2026-06-15 19:37:28 +0800

    Manual Update 3931720

---
+++ 3931720.lua
@@ -0,0 +1,18 @@
+-- 3931720's Lua and Manifest Created by Hubcap Manifest
+-- All That Time
+-- Created: June 14, 2026 at 19:11:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3931720) -- All That Time
+-- MAIN APP DEPOTS
+addappid(3931721, 1, "e3494e218b52c68d4934e8f02a4cd80aa90555b18074535a9fd85a4273bf5983") -- Depot 3931721
+setManifestid(3931721, "8592277166426743568", 3278766874)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
