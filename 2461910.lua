781b9e2e1e69728b886b04d2d7297ce0834341bc
qiao-lima <qiao-lima@github.com>
2026-06-16 07:30:57 +0800

    Manual Update 2461910

---
+++ 2461910.lua
@@ -0,0 +1,14 @@
+-- 2461910's Lua and Manifest Created by Hubcap Manifest
+-- Immortal Payroll
+-- Created: June 15, 2026 at 07:58:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2461910, 1, "203a823432773c7f3bbf8f691be649748f5ad85f6f1cfc581ade1185bddd242e") -- Immortal Payroll
+-- MAIN APP DEPOTS
+addappid(2461911, 1, "bf3ea0228091584f63efa3dc2e09fec989848e94aaa232bd9c398be9c1752b8f") -- Depot 2461911
+setManifestid(2461911, "1749078064146730601", 361106224)
+addappid(2461912, 1, "96f42d5487a5de32758fe81d0c31018e479599b435eea9027b1b5cd777dc9957") -- Depot 2461912
+setManifestid(2461912, "2601587491089697933", 335386544)
