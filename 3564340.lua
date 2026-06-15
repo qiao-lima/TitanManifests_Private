3cb89611ef7355ab3f91fa0b3976c7bb6b695373
qiao-lima <qiao-lima@github.com>
2026-06-15 19:37:27 +0800

    Manual Update 3564340

---
+++ 3564340.lua
@@ -0,0 +1,12 @@
+-- 3564340's Lua and Manifest Created by Hubcap Manifest
+-- Hold Your King
+-- Created: June 14, 2026 at 18:09:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3564340) -- Hold Your King
+-- MAIN APP DEPOTS
+addappid(3564342, 1, "e90cbf63b9585418d94497473cfcd7ade207a7d857a301aa5705422daf1c9f34") -- Depot 3564342
+setManifestid(3564342, "3872044419204583644", 2734802998)
