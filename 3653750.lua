9c99e2c3e846aed58dec9f0dc47c149892fcb77b
qiao-lima <qiao-lima@github.com>
2026-06-16 07:30:58 +0800

    Manual Update 3653750

---
+++ 3653750.lua
@@ -0,0 +1,16 @@
+-- 3653750's Lua and Manifest Created by Hubcap Manifest
+-- Food Truck Chef - Full Course Edition
+-- Created: May 25, 2026 at 08:05:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3653750, 1, "90a13355c77869db754a73aca09d1e14aa6368c3be0740cff52a274c0f0f5129") -- Food Truck Chef - Full Course Edition
+-- MAIN APP DEPOTS
+addappid(3653751, 1, "2ad8040ea8205af9326c6cd9fe66bd22dc04e185654d4e027420c452882ff0f4") -- Depot 3653751
+setManifestid(3653751, "7446103403587413530", 2604395427)
+addappid(3653752, 1, "a7238d792b2a4b4e598227a2b05be23edce408f9d3e79b0009097f26d22aa5bd") -- Depot 3653752
+setManifestid(3653752, "2504109582098506874", 2865963310)
+addappid(3653753, 1, "3886e0827520b18ceb9ab0b25b71ab2bcb92aae68e5919ed763eb9998e7de9eb") -- Depot 3653753
+setManifestid(3653753, "3247842149391892778", 2726622035)
