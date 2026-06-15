964258b1a51eba19683dd44a64f94bcabba41ecf
qiao-lima <qiao-lima@github.com>
2026-06-15 19:37:28 +0800

    Manual Update 4285070

---
+++ 4285070.lua
@@ -0,0 +1,12 @@
+-- 4285070's Lua and Manifest Created by Hubcap Manifest
+-- Magic Hentai Juice Battle
+-- Created: May 25, 2026 at 08:25:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4285070, 1, "acd77ef82c2f08056a802c3c89f3dab91513150f9e6552c86e02fc7cbbf11e59") -- Magic Hentai Juice Battle
+-- MAIN APP DEPOTS
+addappid(4285071, 1, "6cfde41b6a9e636cfc9a698b9a2a8bff3f6b5008d3b9c01a5041825d4502136c") -- Depot 4285071
+setManifestid(4285071, "6810515700453231219", 557598486)
