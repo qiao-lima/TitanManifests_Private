14eb5d1a3ab562c6e2bf3a0bf7c2c2c5b576dcf1
qiao-lima <qiao-lima@github.com>
2026-06-15 19:37:29 +0800

    Manual Update 4785480

---
+++ 4785480.lua
@@ -0,0 +1,20 @@
+-- 4785480's Lua and Manifest Created by Hubcap Manifest
+-- Assassin Of Spider 2: 2099
+-- Created: June 13, 2026 at 00:50:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(4785480) -- Assassin Of Spider 2: 2099
+-- MAIN APP DEPOTS
+addappid(4785481, 1, "a7e2977ac9b1c3437797eb7b12fa8fe95a5894c119ba0d1381ce5ca5b745f898") -- Depot 4785481
+setManifestid(4785481, "4934321558331501211", 7589788174)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") -- .NET 3.5 Redist (Shared from App 228980)
+setManifestid(229000, "4622705914179893434", 242743889)
