2c7516c512a8f39e0d980492188478df39a15a85
qiao-lima <qiao-lima@github.com>
2026-06-16 07:31:00 +0800

    Manual Update 4669420

---
+++ 4669420.lua
@@ -0,0 +1,14 @@
+-- 4669420's Lua and Manifest Created by Hubcap Manifest
+-- 战法牧放置构筑
+-- Created: June 15, 2026 at 07:59:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4669420) -- 战法牧放置构筑
+-- MAIN APP DEPOTS
+addappid(4669421, 1, "52a314f0a17cecd8a2056b20194dbc7d850d8a27f39844a24a535b977f7ce062") -- Depot 4669421
+setManifestid(4669421, "1464428748787344176", 259284949)
+addappid(4669422, 1, "75040b7cb80326cccdef2bc908a18ceebebe3600f8aaa9abc7ebb3b84d07ce99") -- Depot 4669422
+setManifestid(4669422, "7329814462284452004", 250662346)
