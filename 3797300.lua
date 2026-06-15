26c69f6e56accf8ec8fab76c454e0c556951df7b
qiao-lima <qiao-lima@github.com>
2026-06-16 07:30:58 +0800

    Manual Update 3797300

---
+++ 3797300.lua
@@ -0,0 +1,12 @@
+-- 3797300's Lua and Manifest Created by Hubcap Manifest
+-- Dicey Words
+-- Created: June 15, 2026 at 06:08:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3797300) -- Dicey Words
+-- MAIN APP DEPOTS
+addappid(3797301, 1, "6470c2ee128bd53fcee3a772e5d18f86e090b828803ac605eb909d7d17cbfdab") -- Depot 3797301
+setManifestid(3797301, "1967188392127711561", 143869675)
