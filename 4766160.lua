287f0420c7ea98b630088582b5f187d882628545
qiao-lima <qiao-lima@github.com>
2026-06-16 21:23:13 +0800

    Manual Update 4766160

---
+++ 4766160.lua
@@ -0,0 +1,14 @@
+-- 4766160's Lua and Manifest Created by Hubcap Manifest
+-- Rock,Paper,Scissors,SHOOT!
+-- Created: June 16, 2026 at 04:24:29 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4766160) -- Rock,Paper,Scissors,SHOOT!
+-- MAIN APP DEPOTS
+addappid(4766162, 1, "65a964b2e44cd6d5ffb1d85c1f2763243ee5e3359604cbb2af393552f7d056b1") -- Depot 4766162
+setManifestid(4766162, "2948532655033923041", 847542796)
+addappid(4766163, 1, "c1da34b75eeb25ffbcd7ab8241b0a6be58c0058add21162b3e9b690556cd755c") -- Depot 4766163
+setManifestid(4766163, "6554654363578196203", 943211214)
