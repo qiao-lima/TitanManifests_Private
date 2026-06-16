b53c696895040d92997f21f1f92115c08d5ba8de
qiao-lima <qiao-lima@github.com>
2026-06-16 21:16:32 +0800

    Manual Update 4502020

---
+++ 4502020.lua
@@ -0,0 +1,14 @@
+-- 4502020's Lua and Manifest Created by Hubcap Manifest
+-- Sneaky Bimbuls
+-- Created: June 16, 2026 at 04:26:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4502020) -- Sneaky Bimbuls
+-- MAIN APP DEPOTS
+addappid(4502021, 1, "cf4d338c0bfafa7f0ca99fe8087b014358b88e6f92535e526188c5961b28c071") -- Depot 4502021
+setManifestid(4502021, "5974218788609765266", 114312576)
+addappid(4502022, 1, "34fc6c3a0542caf205abba7ce54866f0375ebbba05d3617bd9b52a41bf76594e") -- Depot 4502022
+setManifestid(4502022, "2529568624174872552", 81494656)
