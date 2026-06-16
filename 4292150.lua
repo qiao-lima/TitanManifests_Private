3a6964c3a1e869acfa57e9b456137f6d8009b2fb
qiao-lima <qiao-lima@github.com>
2026-06-16 21:16:30 +0800

    Manual Update 4292150

---
+++ 4292150.lua
@@ -0,0 +1,12 @@
+-- 4292150's Lua and Manifest Created by Hubcap Manifest
+-- Songtail: Whiskers of Destiny
+-- Created: June 16, 2026 at 04:25:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4292150) -- Songtail: Whiskers of Destiny
+-- MAIN APP DEPOTS
+addappid(4292151, 1, "869c1223b0955514bb672bd75ccd735d53129ca7b28182b826366056c608903c") -- Depot 4292151
+setManifestid(4292151, "5969808871655247576", 188793904)
