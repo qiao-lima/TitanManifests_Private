417cd1afcc5322d9dc77dbc3a4428e8931dc4556
qiao-lima <qiao-lima@github.com>
2026-06-16 21:16:31 +0800

    Manual Update 4400130

---
+++ 4400130.lua
@@ -0,0 +1,12 @@
+-- 4400130's Lua and Manifest Created by Hubcap Manifest
+-- Immortal Joan: Cyber Revolution
+-- Created: June 16, 2026 at 04:27:03 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4400130) -- Immortal Joan: Cyber Revolution
+-- MAIN APP DEPOTS
+addappid(4400131, 1, "368c24fc63e4144afe8fb647e8caa9f69577738ad70d796bfcc126a7ae4a63b1") -- Depot 4400131
+setManifestid(4400131, "2350201583982851016", 1121475642)
