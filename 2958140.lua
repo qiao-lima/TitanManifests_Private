451a5affe9f0a1eecb844650519e4c6db1e360ff
qiao-lima <qiao-lima@github.com>
2026-06-16 21:16:26 +0800

    Manual Update 2958140

---
+++ 2958140.lua
@@ -0,0 +1,18 @@
+-- 2958140's Lua and Manifest Created by Hubcap Manifest
+-- NET.CRAWL
+-- Created: June 16, 2026 at 09:13:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2958140, 1, "824f371004ec17ebaa56a4b9d357ad9b437419a6ea120b1dbf0bc49f14f6a254") -- NET.CRAWL
+-- MAIN APP DEPOTS
+addappid(2958145, 1, "dec6eefdda2344186a1bc758ca343ba356cebc72ae6fbd27fc445d9e0814c4d6") -- Depot 2958145
+setManifestid(2958145, "2764733188056022529", 488259169)
+addappid(2958146, 1, "252532cff6678116786a2ef15987061a750d3a466ec439ef2875e30afdb6b357") -- Depot 2958146
+setManifestid(2958146, "3335929770977761093", 506500966)
+addappid(2958141, 1, "6982ef92ec123708a0545f65f3dd385e3e35d771ca90d3f4404ddcbbdb4ecb57") -- Depot 2958141
+setManifestid(2958141, "8264492896306144823", 480914086)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2958142) -- Depot 2958142 (empty depot)
