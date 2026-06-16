4b9f78064c9e3e4f18ae627076c87affa0b8b92e
qiao-lima <qiao-lima@github.com>
2026-06-16 21:16:32 +0800

    Manual Update 4674970

---
+++ 4674970.lua
@@ -0,0 +1,12 @@
+-- 4674970's Lua and Manifest Created by Hubcap Manifest
+-- Cheese Stack
+-- Created: June 16, 2026 at 04:27:36 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4674970) -- Cheese Stack
+-- MAIN APP DEPOTS
+addappid(4674971, 1, "6bcbcd9ded791f1e90f445dc618014e7d34f6719cf01babf5571b188c465e724") -- Depot 4674971
+setManifestid(4674971, "8912345516803306758", 138252137)
