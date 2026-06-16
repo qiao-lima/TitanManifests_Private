00a4cd47ca5552e701ab25957865769149407373
qiao-lima <qiao-lima@github.com>
2026-06-16 21:16:30 +0800

    Manual Update 4285950

---
+++ 4285950.lua
@@ -0,0 +1,14 @@
+-- 4285950's Lua and Manifest Created by Hubcap Manifest
+-- Key Clover
+-- Created: June 16, 2026 at 04:24:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4285950) -- Key Clover
+-- MAIN APP DEPOTS
+addappid(4285951, 1, "d00e5104a5ef77354173699d15b9e4338e487d681e323c5a8dc509ee207394dc") -- Depot 4285951
+setManifestid(4285951, "8642525715720919933", 307016660)
+addappid(4285952, 1, "4d68070f20d2ebc6db1cd84f524c73e8589939817036a344e2ef10eb7cfe9dd3") -- Depot 4285952
+setManifestid(4285952, "2293917465248912127", 275558922)
