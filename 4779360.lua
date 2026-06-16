f356125596df4e30d90fa1544bae3c88a9008979
qiao-lima <qiao-lima@github.com>
2026-06-16 08:06:05 +0800

    Manual Update 4779360

---
+++ 4779360.lua
@@ -0,0 +1,12 @@
+-- 4779360's Lua and Manifest Created by Hubcap Manifest
+-- Day Trade Coach
+-- Created: June 15, 2026 at 18:33:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4779360) -- Day Trade Coach
+-- MAIN APP DEPOTS
+addappid(4779361, 1, "41dafd15f0a9d3c16c61add537df3f5ef22454ae16c24043538fa2b8afdb6ea0") -- Depot 4779361
+setManifestid(4779361, "2183275383326787409", 112161616)
