3899a8e11fefc33c07849e96861f1811f38eddd7
qiao-lima <qiao-lima@github.com>
2026-06-16 21:16:31 +0800

    Manual Update 4379740

---
+++ 4379740.lua
@@ -0,0 +1,12 @@
+-- 4379740's Lua and Manifest Created by Hubcap Manifest
+-- 挂机升级打怪兽
+-- Created: June 16, 2026 at 09:14:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4379740) -- 挂机升级打怪兽
+-- MAIN APP DEPOTS
+addappid(4379741, 1, "15967dfe460f18b5bb4cf3cc408a8cce865a13cad438323af51e3c81c5a19353") -- Depot 4379741
+setManifestid(4379741, "8845705034413929548", 1126144504)
