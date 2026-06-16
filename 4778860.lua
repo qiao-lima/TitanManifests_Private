7ea33cd0d7793ad1eb9a89e21574ea95607f20c2
qiao-lima <qiao-lima@github.com>
2026-06-16 21:23:13 +0800

    Manual Update 4778860

---
+++ 4778860.lua
@@ -0,0 +1,12 @@
+-- 4778860's Lua and Manifest Created by Hubcap Manifest
+-- 真弓-Mayumi-
+-- Created: June 16, 2026 at 04:26:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4778860) -- 真弓-Mayumi-
+-- MAIN APP DEPOTS
+addappid(4778861, 1, "f038ea788a8fd2a671bd7a72a7e3b5616581ce6e860544233bf7ea619e77c8cd") -- Depot 4778861
+setManifestid(4778861, "6776824804811287347", 16522787534)
