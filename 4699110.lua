9e1eeb14551d41fc4204da9c41ec48939499d974
qiao-lima <qiao-lima@github.com>
2026-06-16 21:23:12 +0800

    Manual Update 4699110

---
+++ 4699110.lua
@@ -0,0 +1,18 @@
+-- 4699110's Lua and Manifest Created by Hubcap Manifest
+-- Crococo
+-- Created: June 16, 2026 at 04:24:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4699110) -- Crococo
+-- MAIN APP DEPOTS
+addappid(4699111, 1, "5c9a7dc42b2fd9c9b422c80e2e6b948a4ef5b886fd07f8d421ddb20f521b7842") -- Depot 4699111
+setManifestid(4699111, "8756991685305684324", 6315321894)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
