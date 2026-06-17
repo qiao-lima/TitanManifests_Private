125c738571e7b2fcf54c6155c5d30c352fe077bd
qiao-lima <qiao-lima@github.com>
2026-06-17 22:44:51 +0800

    Manual Update 4162040

---
+++ 4162040.lua
@@ -0,0 +1,12 @@
+-- 4162040's Lua and Manifest Created by Hubcap Manifest
+-- Zenless Zone Zero
+-- Created: June 17, 2026 at 06:27:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4162040) -- Zenless Zone Zero
+-- MAIN APP DEPOTS
+addappid(4162041, 1, "14da0e7d18cab422fb796faf205ae4a7dfc4b6987e34eeaf73e2cf90286a63ff") -- Depot 4162041
+setManifestid(4162041, "3659768240677739039", 75282976281)
