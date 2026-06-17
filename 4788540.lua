65976bbeceacab3f9496b23d46685455bc1cc79d
qiao-lima <qiao-lima@github.com>
2026-06-17 08:49:07 +0800

    Manual Update 4788540

---
+++ 4788540.lua
@@ -0,0 +1,12 @@
+-- 4788540's Lua and Manifest Created by Hubcap Manifest
+-- Help! My Giant Goth Girlfriend Is Insane
+-- Created: June 16, 2026 at 11:09:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4788540) -- Help! My Giant Goth Girlfriend Is Insane
+-- MAIN APP DEPOTS
+addappid(4788541, 1, "9dae05b87e6770b1cdc65f1c98e6fdada93a33d33d09e6d8466dd3cb91e99f68") -- Depot 4788541
+setManifestid(4788541, "4982062111291276255", 412352949)
