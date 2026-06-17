69bd5386f499106f0db07ccc5cfda8abfc702243
qiao-lima <qiao-lima@github.com>
2026-06-17 22:44:49 +0800

    Manual Update 3739720

---
+++ 3739720.lua
@@ -0,0 +1,18 @@
+-- 3739720's Lua and Manifest Created by Hubcap Manifest
+-- Obscuris
+-- Created: June 16, 2026 at 19:50:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3739720, 1, "6776be7de013af7db25ff126a339fe2c28d0dc344ccb3be55988e2f551e364b8") -- Obscuris
+-- MAIN APP DEPOTS
+addappid(3739722, 1, "2584d79f41686936f210822fb46d283e30d8ee256ae5bfb65417d2c042c068d8") -- Depot 3739722
+setManifestid(3739722, "6338220744873997660", 5850501283)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
