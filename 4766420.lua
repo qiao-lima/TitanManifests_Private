b8d049cdb5d27dc4633693bbd51b9dfbcb8fa3e7
qiao-lima <qiao-lima@github.com>
2026-06-17 12:32:20 +0800

    Manual Update 4766420

---
+++ 4766420.lua
@@ -0,0 +1,14 @@
+-- 4766420's Lua and Manifest Created by Hubcap Manifest
+-- Uncle Otto's Mansion
+-- Created: June 16, 2026 at 19:02:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4766420) -- Uncle Otto's Mansion
+-- MAIN APP DEPOTS
+addappid(4766421, 1, "d8503ea05c60f107970be87fb1372c52281caea1cd94d7978230e512acc60790") -- Depot 4766421
+setManifestid(4766421, "6147571298615505065", 229763115)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4766780) -- Uncle Ottos Mansion - Supporter Pack
