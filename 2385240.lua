e29a8caf5af632a7d62ed6ff3e1e63df9940ac08
qiao-lima <qiao-lima@github.com>
2026-06-17 22:44:45 +0800

    Manual Update 2385240

---
+++ 2385240.lua
@@ -0,0 +1,12 @@
+-- 2385240's Lua and Manifest Created by Hubcap Manifest
+-- Spellstrife
+-- Created: June 17, 2026 at 08:10:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2385240, 1, "d8ab42c69188e386b02c609f8f64284f00d54f17c3e350a6c647f8c91f46ed61") -- Spellstrife
+-- MAIN APP DEPOTS
+addappid(2385241, 1, "87581c5b980c334b135d48b20ff9fadcf4130e26f9407c9950968a63996032ed") -- Depot 2385241
+setManifestid(2385241, "344103083604652920", 665872667)
