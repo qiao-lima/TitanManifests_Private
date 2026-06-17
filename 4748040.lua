7f0383891c02b44ffa05edfc076eeaac6d2cdb31
qiao-lima <qiao-lima@github.com>
2026-06-17 08:49:07 +0800

    Manual Update 4748040

---
+++ 4748040.lua
@@ -0,0 +1,12 @@
+-- 4748040's Lua and Manifest Created by Hubcap Manifest
+-- 我的修仙日志
+-- Created: June 16, 2026 at 08:31:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4748040) -- 我的修仙日志
+-- MAIN APP DEPOTS
+addappid(4748041, 1, "294f94676c2cdccf0055ee9603a33499501234fcdf5f0bb553ea9055ea02a2ad") -- Depot 4748041
+setManifestid(4748041, "8768564249956091265", 257457914)
