03975f3389b5ab5a052f1bd7a3be2f5e1e54a83f
qiao-lima <qiao-lima@github.com>
2026-06-17 08:49:03 +0800

    Manual Update 2327720

---
+++ 2327720.lua
@@ -0,0 +1,18 @@
+-- 2327720's Lua and Manifest Created by Hubcap Manifest
+-- City Car Driving 2.0
+-- Created: June 16, 2026 at 13:38:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2327720, 1, "698557f3ecf2f050f00b7fcf1c0ce9f89425e8a62dd2d2f932b694f3b622ae7f") -- City Car Driving 2.0
+-- MAIN APP DEPOTS
+addappid(2327721, 1, "e7f5c17d4db7edb932a65bef69f5d9dcf9f6519ce55c9cc89520deaa8024bc65") -- Depot 2327721
+setManifestid(2327721, "1081582614154082734", 12629624557)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
