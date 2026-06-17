314754ce4c2e7d266737d5e674cb3c6a79188fe1
qiao-lima <qiao-lima@github.com>
2026-06-17 22:44:49 +0800

    Manual Update 2661490

---
+++ 2661490.lua
@@ -0,0 +1,12 @@
+-- 2661490's Lua and Manifest Created by Hubcap Manifest
+-- Magia y Demonios
+-- Created: June 16, 2026 at 19:45:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2661490, 1, "c7cf1434f80a806356f7d2872866a9950f56d3f272ec9b2b593177f2c5b0d4b0") -- Magia y Demonios
+-- MAIN APP DEPOTS
+addappid(2661491, 1, "f77c1a4dcaf5fc44e1060e8859814d367d1e78e6b5a7d7eba233b3b85daf17fd") -- Depot 2661491
+setManifestid(2661491, "2130536059824794868", 2310073864)
