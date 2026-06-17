b4510ede4c45a00de2814671a3d14f762c217ce1
qiao-lima <qiao-lima@github.com>
2026-06-17 12:32:19 +0800

    Manual Update 4036160

---
+++ 4036160.lua
@@ -0,0 +1,12 @@
+-- 4036160's Lua and Manifest Created by Hubcap Manifest
+-- Backrooms Lost Dimension
+-- Created: June 16, 2026 at 18:01:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4036160) -- Backrooms Lost Dimension
+-- MAIN APP DEPOTS
+addappid(4036161, 1, "ddb5ecab0a0aa8450551616961fd16a49179d0140ba8582cb629e9811ce07124") -- Depot 4036161
+setManifestid(4036161, "1896939579118421324", 5718698604)
