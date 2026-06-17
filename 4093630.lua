cb8172f648916f8e71b24edb2c092e66fd1b2ff2
qiao-lima <qiao-lima@github.com>
2026-06-17 08:49:06 +0800

    Manual Update 4093630

---
+++ 4093630.lua
@@ -0,0 +1,18 @@
+-- 4093630's Lua and Manifest Created by Hubcap Manifest
+-- Crime Shadows : Stolen Spotlight Collector's Edition
+-- Created: June 16, 2026 at 04:25:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(4093630) -- Crime Shadows : Stolen Spotlight Collector's Edition
+-- MAIN APP DEPOTS
+addappid(4093631, 1, "9fda35d8da01fc736af16f145c111434e655ac4fda027dec84221b5438dffa53") -- Depot 4093631
+setManifestid(4093631, "3099588785030474743", 666791307)
+addappid(4093632, 1, "2dab0136e33fca9c934b3b79ae2a1a8e74fe983539c1c463fea9981b4ef282d9") -- Depot 4093632
+setManifestid(4093632, "284001523018137197", 663971964)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Crime Shadows  Stolen Spotlight DLC (AppID: 4101380) - missing depot keys
+-- addappid(4101380)
