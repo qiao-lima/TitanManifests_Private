ad35f5e3fceaa962a653907e496ff6dbe9280f16
qiao-lima <qiao-lima@github.com>
2026-06-17 08:49:06 +0800

    Manual Update 4407460

---
+++ 4407460.lua
@@ -0,0 +1,12 @@
+-- 4407460's Lua and Manifest Created by Hubcap Manifest
+-- Poker_Matrix
+-- Created: June 16, 2026 at 06:14:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4407460) -- Poker_Matrix
+-- MAIN APP DEPOTS
+addappid(4407461, 1, "711a2dc7c6cc985597736071e1315ecbcb68142594e9e2ebcba805ba40960bf9") -- Depot 4407461
+setManifestid(4407461, "1471846719678230287", 107868616)
