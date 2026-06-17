f9d50da739fd2c464822d0795c6e16cfb4194869
qiao-lima <qiao-lima@github.com>
2026-06-17 08:49:02 +0800

    Manual Update 2210520

---
+++ 2210520.lua
@@ -0,0 +1,16 @@
+-- 2210520's Lua and Manifest Created by Hubcap Manifest
+-- Copa City
+-- Created: June 16, 2026 at 13:45:13 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 3
+
+-- MAIN APPLICATION
+addappid(2210520) -- Copa City
+-- MAIN APP DEPOTS
+addappid(2210521, 1, "6a4ca55f1ce3d55f9714e65ee4e00533890b05808474a7e461b32b87a7db3565") -- Depot 2210521
+setManifestid(2210521, "1246385373629953564", 13551913970)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4684080) -- Urban Aesthetics Skin Pack
+addappid(4684300) -- Copa City Deluxe Upgrade Pack
+addappid(4727830) -- TIFO Pack
