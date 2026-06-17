d9edfaf5cf4424eea0a19a12852a24845a47d1f2
qiao-lima <qiao-lima@github.com>
2026-06-17 22:44:45 +0800

    Manual Update 2331880

---
+++ 2331880.lua
@@ -0,0 +1,12 @@
+-- 2331880's Lua and Manifest Created by Hubcap Manifest
+-- Otok
+-- Created: June 17, 2026 at 02:17:25 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2331880) -- Otok
+-- MAIN APP DEPOTS
+addappid(2331881, 1, "0df6f66aa437afecff5463412e387bf09c4da3e3e3a08e7180552f89796940c1") -- Depot 2331881
+setManifestid(2331881, "8153098856716205347", 508078829)
