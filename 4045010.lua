7e50b0f463e4c29b9d6ba5e026055d0b4af8979c
qiao-lima <qiao-lima@github.com>
2026-06-17 22:44:50 +0800

    Manual Update 4045010

---
+++ 4045010.lua
@@ -0,0 +1,20 @@
+-- 4045010's Lua and Manifest Created by Hubcap Manifest
+-- Meowtabolism - Examining Feline Behavior within Unicellular Algae
+-- Created: June 16, 2026 at 19:53:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4045010, 1, "2c278e9950de9ad073a75ba5ad0f36c2fd754829c403bea3a221e8094fced320") -- Meowtabolism - Examining Feline Behavior within Unicellular Algae
+-- MAIN APP DEPOTS
+addappid(4045011, 1, "69cbeedc223f74a86a379bfaee2a629f4c49eb0ca8b55bae891b17d5fe41e847") -- Depot 4045011
+setManifestid(4045011, "3037401473651352212", 3864562468)
+addappid(4045012, 1, "0306c3110a9f4d908c59157ba275ffdff5473b0ec7c38eee828cd2547d9c3a0d") -- Depot 4045012
+setManifestid(4045012, "3137501165950300733", 3864264216)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
