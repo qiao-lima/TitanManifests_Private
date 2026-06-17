331f9bd9668b64911a8f386b41c03f1c68e04cc1
qiao-lima <qiao-lima@github.com>
2026-06-17 12:32:18 +0800

    Manual Update 3037160

---
+++ 3037160.lua
@@ -0,0 +1,12 @@
+-- 3037160's Lua and Manifest Created by Hubcap Manifest
+-- Resonance Solstice
+-- Created: June 16, 2026 at 19:25:53 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3037160) -- Resonance Solstice
+-- MAIN APP DEPOTS
+addappid(3037161, 1, "6d8536603ac3523fb84ae933e6baefd4d45c6bcfb0da57567fb48cd57884989d") -- Depot 3037161
+setManifestid(3037161, "6466462981243984799", 14374658737)
