c97cf975adddb2c8b281dd1ef0b66caafc2e8e6f
qiao-lima <qiao-lima@github.com>
2026-06-17 22:44:51 +0800

    Manual Update 4248870

---
+++ 4248870.lua
@@ -0,0 +1,16 @@
+-- 4248870's Lua and Manifest Created by Hubcap Manifest
+-- Incremental Ragebait
+-- Created: June 17, 2026 at 09:45:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4248870) -- Incremental Ragebait
+-- MAIN APP DEPOTS
+addappid(4248871, 1, "12cbf1978dbf1f2bade6e42b33184fd786609de4fa239d2f70efc30c2e05d423") -- Depot 4248871
+setManifestid(4248871, "3125130907436263032", 1741227688)
+addappid(4248872, 1, "6f18031a0a3b564d0945cd19358129c617d4cfed401291ce01160211eee9c491") -- Depot 4248872
+setManifestid(4248872, "5978919383979189865", 1740245060)
+addappid(4248873, 1, "0d4baf76785b25876e838819af8ad35168ace355eaa622b82ea5907d4f01f4bd") -- Depot 4248873
+setManifestid(4248873, "7869090581231861262", 1734250642)
