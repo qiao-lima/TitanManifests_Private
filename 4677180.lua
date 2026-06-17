121937822003261973ec96cd2bd72be96f560d5a
qiao-lima <qiao-lima@github.com>
2026-06-17 12:32:20 +0800

    Manual Update 4677180

---
+++ 4677180.lua
@@ -0,0 +1,12 @@
+-- 4677180's Lua and Manifest Created by Hubcap Manifest
+-- 我为你种田
+-- Created: June 16, 2026 at 19:26:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4677180) -- 我为你种田
+-- MAIN APP DEPOTS
+addappid(4677181, 1, "7c618a93b13ffa03cf1fb075b798b8ca5ba011ae4fa131ee1426b016a8867546") -- Depot 4677181
+setManifestid(4677181, "7914842175138921160", 118734536)
