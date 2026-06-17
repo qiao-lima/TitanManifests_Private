7515223de1d7ddaec20de2beb943f8c6f517388b
qiao-lima <qiao-lima@github.com>
2026-06-17 08:49:02 +0800

    Manual Update 1675860

---
+++ 1675860.lua
@@ -0,0 +1,18 @@
+-- 1675860's Lua and Manifest Created by Hubcap Manifest
+-- Junkster
+-- Created: June 16, 2026 at 11:09:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1675860, 1, "7df3d309e2ade8d65c622c643eeb8c9bae29a9b53f682d818d208e40c1e2a102") -- Junkster
+-- MAIN APP DEPOTS
+addappid(1675861, 1, "1630874bee919eb39d238ed01f42da6cd3f1c15fc5dca0fdc3be1a05ab48d167") -- Depot 1675861
+setManifestid(1675861, "7211353425494136880", 9857358662)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
