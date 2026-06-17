8c88e63fcf5fb423fc874b082b61d42af90ef619
qiao-lima <qiao-lima@github.com>
2026-06-17 22:44:52 +0800

    Manual Update 4797940

---
+++ 4797940.lua
@@ -0,0 +1,14 @@
+-- 4797940's Lua and Manifest Created by Hubcap Manifest
+-- 军火修仙
+-- Created: June 17, 2026 at 07:43:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4797940) -- 军火修仙
+-- MAIN APP DEPOTS
+addappid(4797942, 1, "7129b0fd4c8a64f2e02f41621e949b462c876e887a9fbbfc3d48d62b58c19baa") -- Depot 4797942
+setManifestid(4797942, "3029310141038987232", 9394145038)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4797941) -- Depot 4797941 (empty depot)
