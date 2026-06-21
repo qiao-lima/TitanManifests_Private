c715ec67afa1a5920ddf91cfffa343f44b4a80ec
qiao-lima <qiao-lima@github.com>
2026-06-21 12:02:18 +0800

    Manual Update 3698350

---
+++ 3698350.lua
@@ -0,0 +1,3 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: depotbox_dlc_3698350.lua
+addappid(3698350, 0, "5e55d8477100b1a0fa0126df8d91f6adbdd8b35bf1804b6f35dd64a6e724d64c")
