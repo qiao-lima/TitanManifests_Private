8bbbcf13789dee98f4fa6a70f9605f3923828a87
qiao-lima <qiao-lima@github.com>
2026-06-21 12:02:16 +0800

    Manual Update 2860900

---
+++ 2860900.lua
@@ -0,0 +1,3 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: depotbox_dlc_2860900.lua
+addappid(2860900, 0, "cff59c82777c81fac5f9930141c210826bd3d2776b09d70d66fc11ede5ee7dbf")
