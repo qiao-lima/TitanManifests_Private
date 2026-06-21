77e62c4d90b2245d7acb5db961f428590a3a18d9
qiao-lima <qiao-lima@github.com>
2026-06-21 12:02:18 +0800

    Manual Update 4007700

---
+++ 4007700.lua
@@ -0,0 +1,8 @@
+-- ============================================
+--  Credits: K3rnelPan1c (KernelOS / !K3rnalyze)
+--  Discord: https://discord.gg/k3rnalyze
+--  Website: https://kernelos.org/games
+-- ============================================
+
+addappid(4007700)
+addappid(4007701,0,"bd1b8a1369f304aff011709b6a4d65633716cd6091851c87178865543ac70081")
