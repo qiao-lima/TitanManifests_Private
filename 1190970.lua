5256c9380738d3fa202bba2b2a93a77375995c78
qiao-lima <qiao-lima@github.com>
2026-06-24 05:35:59 +0800

    Manual Update 1190970

---
+++ 1190970.lua
@@ -0,0 +1,26 @@
+-- 1190970's Lua and Manifest Created by Hubcap Manifest
+-- House Flipper 2
+-- Created: June 11, 2026 at 10:39:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 4 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(1190970, 1, "f2f1aaf6899f899ad07cb2da7594595d1acc24d732a23f7a4a16cc959e5c697b") -- House Flipper 2
+-- MAIN APP DEPOTS
+addappid(1190971, 1, "a6202777d416dcf4d2f36cc5f7a69f90cca6e1bfbde6e27eaa4e97e86c7d3196") -- Depot 1190971
+setManifestid(1190971, "1043126876731964797", 12438175709)
+-- DLCS WITH DEDICATED DEPOTS
+-- House Flipper 2 - Supporter Pack (AppID: 2649200)
+addappid(2649200)
+addappid(2649200, 1, "75c666e7e7f011eddb178036bbb4b16446378ef945ecf6811f32e2a4f96bfcc7") -- House Flipper 2 - Supporter Pack - Depot 2649200
+setManifestid(2649200, "2894455407820860002", 3767259672)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3363370) -- House Flipper 2 - Co-op DLC
+addappid(3672960) -- House Flipper 2 - Scooby-Doo DLC
+addappid(4351410) -- House Flipper 2 - Sakura DLC
+-- EXCLUDED DLCS:
+-- UNRELEASED DLCS (COMMENTED OUT)
+-- addappid(3680100) -- DLC 3680100 (unreleased)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(1190972) -- Depot 1190972 (empty depot)
