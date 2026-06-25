c83688483be42a4dabe8e4d939f58c2074641f73
qiao-lima <qiao-lima@github.com>
2026-06-25 08:22:25 +0800

    Manual Update 2651410

---
+++ 2651410.lua
@@ -0,0 +1,12 @@
+-- 2651410's Lua and Manifest Created by Hubcap Manifest
+-- The Endless Abyss: A Busty Cop’s Corrupted Soul
+-- Created: June 18, 2026 at 04:54:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2651410) -- The Endless Abyss: A Busty Cop’s Corrupted Soul
+-- MAIN APP DEPOTS
+addappid(2651411, 1, "bfed488dd748d6b586605ec0e9b8a5bafdbe2b4d4d193ee2e6c9d3e93e3d90a1") -- Depot 2651411
+setManifestid(2651411, "4179475202283467201", 9616901819)
