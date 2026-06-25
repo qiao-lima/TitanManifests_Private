007f90992a9686ab661e6505db5847991e09e783
qiao-lima <qiao-lima@github.com>
2026-06-25 08:48:57 +0800

    Manual Update 2095420

---
+++ 2095420.lua
@@ -0,0 +1,7 @@
+addappid(2095420)
+addappid(3796390)
+addappid(3796400)
+addappid(4028530)
+addappid(4539690)
+addappid(2095421,0,"8c8b97a2cf80ee584aecffb4d2b5ff76c013144d7abf34d7e3c0bbc1c3b8065f")
+setManifestid(2095421,"1318317429010337480")
