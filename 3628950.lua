c37b172d2a04038c8403a4deddf943402e7d7bd1
qiao-lima <qiao-lima@github.com>
2026-06-26 13:15:12 +0800

    Manual Update 3628950

---
+++ 3628950.lua
@@ -0,0 +1,15 @@
+
+addappid(3628950, 1, "c6cb617a81045106ceb205a9c87b664229dd0424c49ea19baea25f0d5855c0c9") -- Succubus Successor: Delilah's Juicy Journey
+addappid(3628951, 1, "e3609b671bf2f7a6bac8a1a1400c623a98744aae13e7439da56e6d0671149ec6") -- Depot 3628951
+setManifestid(3628951, "3709481988791077518", 4494026654)
+addappid(3628952, 1, "b87ab725b1e81ad396d09037ad31b1ea27ba5d61cbc7ffadb2f8bb7fb7f3a8f6") -- Depot 3628952
+setManifestid(3628952, "5153959053708924780", 4520754560)
+addappid(4644260)
+addappid(4644260, 1, "41a885b51961ef2218d73264db11ba87f3edce28e17d081837d0b78f4d22298f") -- Succubus Successor - Digital Artbook - Depot 4644260
+setManifestid(4644260, "4295630627713167132", 317673980)
+-- Succubus Successor - Deluxe Weapon Appearance Pack (AppID: 4718750)
+addappid(4718750)
+addappid(4718750, 1, "9186347a9f3be8ed1990497511ee6d3363f4a48a60c513a7261cf017523163fd") -- Succubus Successor - Deluxe Weapon Appearance Pack - Depot 4718750
+setManifestid(4718750, "8594653028465372818", 6166)
+addappid(4718751, 1, "7b316edcb17b5b61df21a8bf5fe913d30c407c8a13ad347fc9adc21b058c92f9") -- Succubus Successor - Deluxe Weapon Appearance Pack - Depot 4718751
+setManifestid(4718751, "9161438700502255147", 6166)
