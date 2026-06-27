ac2edb49592d93dd990262e5559ec7c47e59a7d9
qiao-lima <qiao-lima@github.com>
2026-06-28 07:54:56 +0800

    Manual Update 3570070

---
+++ 3570070.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3570070 - Paddle Paddle Paddle
+-- Generated 2026-06-27 23:31:55 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3570070)
+
+-- Main Depots
+addappid(3570071, 1, "e82bef97ee4c2dc02e213361a9a2035f8421d41efec592dbd73f46ef6ef4ee56")
+setManifestid(3570071, "5359948128910977157", 496156251)
+
+-- DLC's (no depot keys required)
+addappid(3880690) -- Paddle Royale Pack
+addappid(4369820) -- Paddle Paddle Paddle: Up Up Up
