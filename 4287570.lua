389039584bcb20fa1d68b9d1ac2bab738c82aca4
qiao-lima <qiao-lima@github.com>
2026-06-28 07:54:57 +0800

    Manual Update 4287570

---
+++ 4287570.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4287570 - Homgard
+-- Generated 2026-06-27 23:34:18 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4287570)
+
+-- Main Depots
+addappid(4287571, 1, "99302b017f1f129bbbfc1aaa7b691b6327e1aeb186ac12d7f02b49736f29b3ec") -- English (windows)
+setManifestid(4287571, "492943046578122721", 2424883144)
