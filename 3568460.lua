0bdfa70f0a0ff5cfd18aaf6637dff5db86dd3c18
qiao-lima <qiao-lima@github.com>
2026-06-28 07:54:56 +0800

    Manual Update 3568460

---
+++ 3568460.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3568460 - MustScream
+-- Generated 2026-06-27 23:26:03 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(3568460)
+
+-- Main Depots
+addappid(3568461, 1, "a8b7d5e3954c4ae3615a0c6547a17c1a3a6de9e02048b0306c1717928bea5896") -- (windows)
+setManifestid(3568461, "7676585715661079279", 2055353792)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
