6be191551b0fb022507e2e5d1ce921546e2759e4
qiao-lima <qiao-lima@github.com>
2026-06-28 07:55:09 +0800

    Manual Update 537340

---
+++ 537340.lua
@@ -0,0 +1,23 @@
+-- Generated with Luie @ https://lua.tools/
+-- 537340 - Guts and Glory
+-- Generated 2026-06-27 23:36:22 UTC
+-- # Depots (Total/DLC/Shared): 8/5/0
+
+-- Main AppID
+addappid(537340, 1, "1b1e75652d752c2eb42202dac0a7d4360bc19afa6ed0a11f7e353a3472aa4a82")
+
+-- Main Depots
+addappid(537341, 1, "9666002b803cda0f99f93e3397766e9c6c0cb883c255707baf99c801e84a0925") -- (windows)
+setManifestid(537341, "1945190940842095773", 5848571551)
+addappid(537342, 1, "ed469e4612ea75de1c81b19cf544912616276e2ede60b1cfc0cd516fc73725d2") -- (macos)
+setManifestid(537342, "5945138272911753843", 5855874034)
+addappid(537343, 1, "4af51fcd9f2fdb0102b86c4d9d96bb6a8d926bdfb5d4980d88585e169183b3ff") -- (linux)
+setManifestid(537343, "29999913221118649", 5984226121)
+
+-- DLC's (with depot keys)
+-- Guts and Glory - Original Soundtrack (AppID: 889440)
+addappid(889440)
+addappid(889441, 1, "32aca97653fd610457c17e920f3ec6fe646a91e05f606c4cc13fc64ca0812c05")
+setManifestid(889441, "8778374899867251824", 177574355)
+
+-- Missing Depots (We don't have the keys yet lol): 646860, 646920, 646921, 646922
