693a8ac0a92caed127318fa3b6aa3abab898ae5a
qiao-lima <qiao-lima@github.com>
2026-06-28 07:54:53 +0800

    Manual Update 236390

---
+++ 236390.lua
@@ -0,0 +1,101 @@
+-- Generated with Luie @ https://lua.tools/
+-- 236390 - War Thunder
+-- Generated 2026-06-27 22:10:45 UTC
+-- # Depots (Total/DLC/Shared): 19/13/2
+
+-- Main AppID
+addappid(236390, 1, "2ca3cfdd7ef1bdb3ac92eb955284a30aa981b1a1512ef448307f398c13ae83cf")
+
+-- Main Depots
+addappid(236391, 1, "a6b3cd630da5dc58205f88ebc974d7cfb462e98bf51c716796ef57fbe222a8cf") -- (windows,linux)
+setManifestid(236391, "3308740494353083720", 90439347991)
+addappid(236392, 1, "d28ecfa5efd92fa7a5648a3083ca753c508e85ef28c56e64a3cdd75692147afd") -- (macos)
+setManifestid(236392, "2381828210620354002", 90219827585)
+addappid(236393, 1, "96647011e1f9d79b47fee098f8c5dc9675ca132a118dec941a5a8682812da58d") -- (linux)
+setManifestid(236393, "6919174160945667261", 468643488)
+addappid(236394, 1, "000c645c4187c9a4be937ac13100738fd2b80bd63db3c54800fb897877960aa8") -- (windows)
+setManifestid(236394, "7708872052166554034", 1699267254)
+
+-- DLC's (no depot keys required)
+addappid(1108450) -- War Thunder - US Starter Pack
+addappid(1233370) -- War Thunder - USSR Beginner's Pack
+addappid(1368110) -- War Thunder - Italian Starter Pack
+addappid(1368800) -- War Thunder - Japanese Starter Pack
+addappid(1368801) -- War Thunder - British Starter Pack
+addappid(1423760) -- War Thunder - French Starter Pack
+addappid(1582830) -- War Thunder - Swedish Starter Pack
+addappid(1937490) -- War Thunder - Chinese Starter Pack
+addappid(1947130) -- War Thunder - A-10A Thunderbolt (Early) Pack
+addappid(2054413) -- War Thunder - German Starter Pack
+addappid(2690300) -- War Thunder - M1A1 HC "Click-Bait" Pack
+addappid(3010110) -- War Thunder - Leopard 2A4M CAN Pack
+addappid(3010120) -- War Thunder - T-80U-E1 Pack
+addappid(3386540) -- War Thunder - RDF/LT Pack
+addappid(3386550) -- War Thunder - Two Fronts Pack
+addappid(3543750) -- War Thunder - F/A-18С Early (US) Pack
+addappid(3752040) -- War Thunder - Hell on Wheels Pack
+addappid(3752050) -- War Thunder - Echoes of Overlord Pack
+addappid(3752060) -- War Thunder - In May 45 Pack
+addappid(3780140) -- War Thunder - F/A-18C Hornet Pack
+addappid(3780150) -- War Thunder - RAH-66 Comanche Pack
+addappid(3780160) -- War Thunder - KMS Gneisenau Pack
+addappid(4078870) -- War Thunder - Su-30MK2 AMV Pack
+addappid(4078890) -- War Thunder - Senrai Maidens: Kate Pack
+addappid(4078900) -- War Thunder - Senrai Maidens: Elsa Pack
+addappid(4078910) -- War Thunder - Senrai Maidens: Maria Pack
+addappid(4184650) -- War Thunder - BMPT-72 “Terminator-2” Pack
+addappid(4184660) -- War Thunder - Su-30MKK Pack
+addappid(4434690) -- War Thunder - ZTZ96B Pack
+addappid(4640460) -- War Thunder - Senrai Maidens: Emily Pack
+addappid(4640470) -- War Thunder - Senrai Maidens: Usagi Pack
+addappid(4640480) -- War Thunder - Senrai Maidens: Ling Pack
+addappid(4739380) -- War Thunder - Puma u14 Pack
+addappid(4740880) -- War Thunder - F-14D Pack
+
+-- DLC's (with depot keys)
+-- War Thunder: Air Forces, Vol.1 (Original Game Soundtrack) (AppID: 1247130)
+addappid(1247130)
+addappid(1247131, 1, "bf718790da81d46db1e6b8578abd4d0ff2d909350dd13e17af78f027ee7a758a")
+setManifestid(1247131, "8341457290992740484", 115509273)
+addappid(1247132, 1, "c25ee82907f20047b5b295ecffc9c6e8cdd4ad1251e6d76cc7d119525ab40696")
+setManifestid(1247132, "6501045770135902028", 484210363)
+-- War Thunder: Ground Forces, Vol.1 (Original Game Soundtrack) (AppID: 1247140)
+addappid(1247140)
+addappid(1247141, 1, "e99459835ac42a72c5bc407fab1d177a84043f76d4cb4cd94897ba25bf2be18f")
+setManifestid(1247141, "2972527815839877100", 115220783)
+addappid(1247142, 1, "6669573a70eec9f322a8d38a6deca195ed0fe61480bc3c29eb70631a59c0fb48")
+setManifestid(1247142, "8095797568915179578", 434436090)
+-- War Thunder: Naval Forces, Vol.1 (Original Game Soundtrack) (AppID: 1247150)
+addappid(1247150)
+addappid(1247151, 1, "4f2a2b02c2bd060c77f55cafa375ad2844a70a954de2b49fac4445d788dd1d18")
+setManifestid(1247151, "4143773798004515647", 128389931)
+addappid(1247152, 1, "eb08b43cf150d14d2cc5c3fc3dfb1a88081bf71882a12cc51d7959eaeea76879")
+setManifestid(1247152, "5642245123210092266", 476282414)
+-- War Thunder - Ground Forces High-res Texture Pack (AppID: 1945840)
+addappid(1945840)
+addappid(1945840, 1, "bbc5fd9511b67ba8ba67bb1fdb24879d9b7f279607f1dbb697ad3141a50e8e56") -- (windows,linux)
+setManifestid(1945840, "503638760514180092", 46244452521)
+-- War Thunder - Air Forces High-res Texture Pack (AppID: 1945841)
+addappid(1945841)
+addappid(1945841, 1, "1793c762898ff4a24f7e64f9e6f88c92faf345c30578355b9a51aa1ac45f7eca") -- (windows,linux)
+setManifestid(1945841, "6663449930209494772", 19927537603)
+-- War Thunder - Environment High-res Texture Pack (AppID: 1945930)
+addappid(1945930)
+addappid(1945930, 1, "5bd05c73fd81d2ad1ba5e2a8f052147d5d7717da8f6c7fb2fda2022d35b420d6") -- (windows,linux)
+setManifestid(1945930, "8335748520405176177", 13744475889)
+-- War Thunder: Air Forces, Vol.2 (Original Game Soundtrack) (AppID: 2500430)
+addappid(2500430)
+addappid(2500432, 1, "295a945282470ba96f46c433fd85532e887955b6631e0ce022f56a1ae86df0ae")
+setManifestid(2500432, "5063212236635855991", 123384990)
+addappid(2500433, 1, "62c65cf9cdc6fcf3dc96faa8c58fc817deb9964b2e3456bbef841384bb4da81e")
+setManifestid(2500433, "787423604661100788", 389093216)
+-- War Thunder: Ground Forces, Vol.2 (Original Game Soundtrack) (AppID: 2500440)
+addappid(2500440)
+addappid(2500442, 1, "1a4790b180b1f2bdb850d9963c0d356de074e43fdc4443a7f97c8bf8521eefc9")
+setManifestid(2500442, "4763232738926203757", 117921634)
+addappid(2500443, 1, "f90f024e6d09a579e0454fef032ddcda495bd40305c1a368d4ab6a11e712c06f")
+setManifestid(2500443, "5751783119779533621", 348672174)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
