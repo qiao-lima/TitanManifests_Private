7b58cbecbb29ad755af772377a87d9f1d00dac32
qiao-lima <qiao-lima@github.com>
2026-06-27 10:42:25 +0800

    Manual Update 2186680

---
+++ 2186680.lua
@@ -0,0 +1,52 @@
+-- 2186680's Lua and Manifest Created by Hubcap Manifest
+-- Warhammer 40,000: Rogue Trader
+-- Created: June 23, 2026 at 04:53:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 8
+-- Total DLCs: 11
+
+-- MAIN APPLICATION
+addappid(2186680, 1, "61c3f89a7dddaee81487d704d3932e95dde00919dee5e8499770aba982703b83") -- Warhammer 40,000: Rogue Trader
+-- MAIN APP DEPOTS
+addappid(2186681, 1, "28fb2e6880c18c5a8f226e85577726ff170ba4d056a169247856dc1ad15c45a9") -- Depot 2186681
+setManifestid(2186681, "4923995158422422906", 39141203295)
+addappid(2186682, 1, "9f0466c4c63e89d1a6e336a98079e9635acfd48c7c127c1fbfcb04ccec12e05c") -- Depot 2186682
+setManifestid(2186682, "6826713910713885287", 34557595709)
+-- DLCS WITH DEDICATED DEPOTS
+-- Warhammer 40,000 Rogue Trader - Deluxe Pack (AppID: 2682660)
+addappid(2682660)
+addappid(2682660, 1, "42d397205a25cc88be18385660cbf934a5b60dc736bf4e610293bb24b581e1a9") -- Warhammer 40,000 Rogue Trader - Deluxe Pack - Depot 2682660
+setManifestid(2682660, "6430754017564528800", 2214045581)
+-- Warhammer 40,000 Rogue Trader - Void Shadows (AppID: 2951990)
+addappid(2951990)
+addappid(2951991, 1, "47fb2a7f5c69f478464b2dc16e22cb386f8937b69065b47c49e8e9140ea3c3f7") -- Warhammer 40,000 Rogue Trader - Void Shadows - Depot 2951991
+setManifestid(2951991, "2965308602618153492", 1427164438)
+addappid(2951992, 1, "11d8b1c92b160025d10ae272d231b075bb4520b3a4f7b85be57be2cd0c2d2d7a") -- Warhammer 40,000 Rogue Trader - Void Shadows - Depot 2951992
+setManifestid(2951992, "5940954301978112008", 1427387906)
+-- Warhammer 40,000 Rogue Trader - Lex Imperialis (AppID: 3622820)
+addappid(3622820)
+addappid(3622821, 1, "734180a123f43796ae882d1d4aff76d40e67dbe95392726ea51c7651634014a1") -- Warhammer 40,000 Rogue Trader - Lex Imperialis - Depot 3622821
+setManifestid(3622821, "8866551068909375260", 1139057242)
+addappid(3622822, 1, "1880e824e9b4a1d6886ebf35dfbdae11db20c4e67923e72e52e02b9740a8dad9") -- Warhammer 40,000 Rogue Trader - Lex Imperialis - Depot 3622822
+setManifestid(3622822, "7214804791659393313", 1139113958)
+-- Warhammer 40,000 Rogue Trader - The Infinite Museion (AppID: 4226770)
+addappid(4226770)
+addappid(4226771, 1, "1c77fac93754bfc4d46472bbe88f1c773724e6fc1881d762e8f3f8617bbea087") -- Warhammer 40,000 Rogue Trader - The Infinite Museion - Depot 4226771
+setManifestid(4226771, "8583540167492686735", 1312202410)
+-- addappid(4226772, 1, "MISSING_KEY") -- Warhammer 40,000 Rogue Trader - The Infinite Museion - Depot 4226772 (no key available)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2682670) -- Warhammer 40,000 Rogue Trader - Voidfarer Pack
+addappid(2687230) -- Warhammer 40,000 Rogue Trader - Season Pass
+addappid(2696650) -- Warhammer 40,000 Rogue Trader - Limited Outfit
+addappid(2700400) -- Warhammer 40,000 Rogue Trader - Founders Pack
+addappid(3650060) -- Warhammer 40,000 Rogue Trader - Season Pass 2
+addappid(3650800) -- Warhammer 40,000 Rogue Trader - Appearance Pack
+addappid(3672730) -- Warhammer 40,000 Rogue Trader - The Shovel DLC
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2682670) -- Depot 2682670 (empty depot)
+-- addappid(2687230) -- Depot 2687230 (empty depot)
+-- addappid(2696650) -- Depot 2696650 (empty depot)
+-- addappid(2700400) -- Depot 2700400 (empty depot)
+-- addappid(3650061) -- Depot 3650061 (empty depot)
+-- addappid(3650801) -- Depot 3650801 (empty depot)
+-- addappid(3672731) -- Depot 3672731 (empty depot)
