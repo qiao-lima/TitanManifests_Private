95fefec49f3eb29760f842ab2ed8f66fb802e241
qiao-lima <qiao-lima@github.com>
2026-06-28 07:54:54 +0800

    Manual Update 2870920

---
+++ 2870920.lua
@@ -0,0 +1,25 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2870920 - Restaurats
+-- Generated 2026-06-27 23:36:46 UTC
+-- # Depots (Total/DLC/Shared): 4/2/1
+
+-- Main AppID
+addappid(2870920, 1, "d9866abb18a9c9f05e1fe46a3f93ab247a113ad988bd20fd4d8c6361728c24d2")
+
+-- Main Depots
+addappid(2870921, 1, "67fa9cef9ad6d80631ce4a7d2b41231957cc8bb369793419e87eddeb04f1c1c0") -- (windows)
+setManifestid(2870921, "8644074461921147777", 8112013270)
+
+-- DLC's (no depot keys required)
+addappid(4118840) -- Restaurats - Supporter Pack
+
+-- DLC's (with depot keys)
+-- Restaurats - Digital Wallpaper Collection (AppID: 4132370)
+addappid(4132370)
+addappid(4132370, 1, "a3e761b05811d1a269070ca6ad57b7f5b8567956b5cdccf5d69bf4e88cd272da")
+setManifestid(4132370, "3347550773077506088", 166110204)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 4118851
