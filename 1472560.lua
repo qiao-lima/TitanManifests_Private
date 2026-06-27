b4792a684a9aca187d5e70b79ee76352357740f6
qiao-lima <qiao-lima@github.com>
2026-06-28 07:54:51 +0800

    Manual Update 1472560

---
+++ 1472560.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1472560 - I Am Fish
+-- Generated 2026-06-27 23:03:24 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(1472560, 1, "2e11e18d232d0fa36a7b1068b1d8c8319b8b23971210f22ca99a5b67c341eb9a")
+
+-- Main Depots
+addappid(1472561, 1, "6bef5b77e8fda2a7ee82a253648668dbdeb304d2b57d6c9f5f790febfc8b0b11") -- (windows)
+setManifestid(1472561, "1189058342588675758", 11048524877)
+addappid(1472562, 1, "ab88c1a0f08d5f7e531be35e21571389a263b7427179af0c36a5034ff00563e6") -- (windows)
+setManifestid(1472562, "1365806185189140546", 92950022)
