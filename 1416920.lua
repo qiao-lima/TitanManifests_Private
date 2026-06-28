3566e907ef7336307858b9888237e7a1b675159c
qiao-lima <qiao-lima@github.com>
2026-06-28 13:56:43 +0800

    Manual Update 1416920

---
+++ 1416920.lua
@@ -0,0 +1,23 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1416920 - 中国式网游
+-- Generated 2026-06-28 05:55:05 UTC
+-- # Depots (Total/DLC/Shared): 3/2/0
+
+-- Main AppID
+addappid(1416920)
+
+-- Main Depots
+addappid(1416921, 1, "baa863b7295b6249af3fd91f6f55956ca6f8b5185529106ae13f60c51ca21988")
+setManifestid(1416921, "8957170505581065647", 6200443799)
+
+-- DLC's (no depot keys required)
+addappid(3334700) -- 中国式网游 - 彩票模式
+addappid(3356630) -- 中国式网游 - 氪佬专属礼包2
+
+-- DLC's (with depot keys)
+-- 中国式网游 - 爽文 (AppID: 3193590)
+addappid(3193590)
+addappid(3193590, 1, "d20a4d95eebfeb18d60aff959a5c584787ab9793bcfa5ba415dcc7b3dffc3acf")
+setManifestid(3193590, "5342148085172353443", 6333017710)
+
+-- Missing Depots (We don't have the keys yet lol): 3214460
