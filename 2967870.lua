ac7b70065525157113770a16c8cda285b4b888b5
qiao-lima <qiao-lima@github.com>
2026-06-29 18:55:05 +0800

    Manual Update 2967870

---
+++ 2967870.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2967870 - Xi-Portal
+-- Generated 2026-06-29 10:24:27 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2967870)
+
+-- Main Depots
+addappid(2967871, 1, "5973EF941DB5AA49EA9946BCF7D0EB7B68C97DE832925BCA1C6161BE20D82424")
+setManifestid(2967871, "4394067069294225441", 794153660)
