8926f064a0f06d1d4dc9579019a330a3c27ecb52
qiao-lima <qiao-lima@github.com>
2026-06-29 18:57:19 +0800

    Manual Update 4193270

---
+++ 4193270.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4193270 - 日本事故物件監視協会2 -Japan Stigmatized Property-
+-- Generated 2026-06-29 10:43:27 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(4193270)
+
+-- Main Depots
+addappid(4193271, 1, "e244dc403ccdd385bb6d7a3cd66dc333718dc1866a91ada1032294f52d485029") -- (windows)
+setManifestid(4193271, "4429596019300047933", 2331782300)
+
+-- Missing Depots (We don't have the keys yet lol): 4193273
