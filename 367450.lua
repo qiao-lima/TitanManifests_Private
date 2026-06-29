4e1aa6d16e5534931302d93fa42052f0f1220472
qiao-lima <qiao-lima@github.com>
2026-06-29 18:57:17 +0800

    Manual Update 367450

---
+++ 367450.lua
@@ -0,0 +1,25 @@
+-- Generated with Luie @ https://lua.tools/
+-- 367450 - Poly Bridge
+-- Generated 2026-06-29 10:26:26 UTC
+-- # Depots (Total/DLC/Shared): 6/1/2
+
+-- Main AppID
+addappid(367450, 1, "db284e990493c81a8718c80e988cb165e376d959f646cc004219cdb4d7244075")
+
+-- Main Depots
+addappid(367451, 1, "d665d606792ae4631a504bd59f59310fffb87490652e9b149616eda8ad5962ba") -- (windows)
+setManifestid(367451, "3168549286285070087", 223243078)
+addappid(367452, 1, "1d911373e5f75e7fe50cf3e2708aab9380d1565dc3e7fc9bacb9339cc042dec6") -- (macos)
+setManifestid(367452, "5300884783176432417", 232849566)
+addappid(367453, 1, "03f1099f465d40ba0296be94392f13b6a696331a12f878d8a4931efb7a88397d") -- (linux)
+setManifestid(367453, "1283089157963056478", 254062310)
+
+-- DLC's (with depot keys)
+-- Poly Bridge Soundtrack (AppID: 447410)
+addappid(447410)
+addappid(447410, 1, "354be33073a04b1cdfbfc3407dd04eb7bbfe8ba51d462ca1e8b867834ea6f4c4")
+setManifestid(447410, "9194676910727092461", 128586079)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") -- (windows)
