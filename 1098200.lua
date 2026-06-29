e54f66b33a04e9f529b05a6831bed30d75d1deaf
qiao-lima <qiao-lima@github.com>
2026-06-29 18:55:02 +0800

    Manual Update 1098200

---
+++ 1098200.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1098200 - Vaporwave Drift
+-- Generated 2026-06-29 10:24:37 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(1098200)
+
+-- Main Depots
+addappid(1098201, 1, "7b53dfd7f0588fa34ae119e8a6f01f3e9f081d8e5a2ad83e279dc532f9a4c63f") -- (windows)
+setManifestid(1098201, "2204754084242985974", 1340540315)
