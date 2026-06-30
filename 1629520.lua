e805ed86dd5aee27e3f12c3da724a612dde5c5ff
qiao-lima <qiao-lima@github.com>
2026-06-30 17:35:46 +0800

    Manual Update 1629520

---
+++ 1629520.lua
@@ -0,0 +1,27 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1629520 - A Little to the Left
+-- Generated 2026-06-30 09:17:09 UTC
+-- # Depots (Total/DLC/Shared): 6/4/0
+
+-- Main AppID
+addappid(1629520, 1, "7c231ca596317a31a83963146ff548b359196437dfb3e8847612cf95274a6ed0")
+
+-- Main Depots
+addappid(1629522, 1, "75a26ba53fafd90934403fcc6435f5fe6e11f468f51fb71e0b5bc857da2d7d27") -- (windows)
+setManifestid(1629522, "4899327096371435302", 1424455605)
+addappid(1629523, 1, "7f486534cadce796e4a8f6002a3c4439f28bf578140a469499f5727a65a78eed") -- (macos)
+setManifestid(1629523, "5892889231134526472", 1532681165)
+
+-- DLC's (with depot keys)
+-- A Little to the Left: Cupboards & Drawers (AppID: 2343790)
+addappid(2343790)
+addappid(2343791, 1, "bca36b3144693458d85d458757a3b8ca728a9424fe307fd360cd81fa98efa882") -- (macos)
+setManifestid(2343791, "1813266626795120628", 937256916)
+addappid(2343792, 1, "25252034054079c81f78613eb721fe19652a6a90e7dbccc71b6f155fdc717355") -- (windows)
+setManifestid(2343792, "5567435932420847168", 898124654)
+-- A Little to the Left: Seeing Stars (AppID: 2828160)
+addappid(2828160)
+addappid(2828161, 1, "98a180b200e6353215265ca375508fd66912dd23fe542d4abec865944c9d0248") -- (windows)
+setManifestid(2828161, "7787817774865342072", 1560797688)
+addappid(2828162, 1, "8f2820eb2b3faa4103010eeccef4965ed9d27a3daba0dea27e7224698a060cab") -- (macos)
+setManifestid(2828162, "1372241225995081445", 1628361881)
