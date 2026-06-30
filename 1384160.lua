7eac100833c0a1ff680b27cba788322d24b050f8
qiao-lima <qiao-lima@github.com>
2026-06-30 17:53:46 +0800

    Manual Update 1384160

---
+++ 1384160.lua
@@ -0,0 +1,74 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1384160 - GUILTY GEAR -STRIVE-
+-- Generated 2026-06-30 06:38:10 UTC
+-- # Depots (Total/DLC/Shared): 8/5/2
+
+-- Main AppID
+addappid(1384160, 1, "4021c3f3cfb8f9988d57a209da688cab4371c55ff00d68981a69363aec1f8afc")
+
+-- Main Depots
+addappid(1384161, 1, "6a00a68034e78cffd8a0e7bb055e336446bf6b732c3605b5c96b73cffa5b3ecd")
+setManifestid(1384161, "111556483438237266", 31833656888)
+
+-- DLC's (no depot keys required)
+addappid(1544400) -- DLC 1544400
+addappid(1544401) -- GGST Additional Character 1 - Goldlewis Dickinson
+addappid(1544402) -- GGST Additional Character 2 - Jack-O
+addappid(1544403) -- GGST Additional Character 3 - Happy Chaos
+addappid(1544404) -- GGST Additional Character 4 - Baiken
+addappid(1544405) -- GGST Additional Character 5 - Testament
+addappid(1544406) -- Guilty Gear -Strive- Additional Colors #1 DLC
+addappid(1544407) -- GGST Additional Battle Stage 1 - Lap of the Kami
+addappid(1544408) -- GGST Additional Battle Stage 2 - White House Reborn
+addappid(1649880) -- Guilty Gear -Strive- Season Pass 1
+addappid(1652890) -- Guilty Gear -Strive- Season Pass 2
+addappid(1652892) -- Guilty Gear -Strive- Season Pass 3
+addappid(2081050) -- GGST Additional Character 6 - Bridget
+addappid(2081051) -- GGST Additional Character 7 - Sin Kiske
+addappid(2081052) -- GGST Additional Character 8 - Bedman?
+addappid(2081053) -- GGST Additional Character 9 - Asuka
+addappid(2081054) -- Guilty Gear -Strive- Additional Colors #2 DLC
+addappid(2081055) -- GGST Additional Battle Stage 3 - Fairy's Forest Factory
+addappid(2081056) -- GGST Additional Battle Stage 4 - Tír na nÓg
+addappid(2410760) -- GGST Guilty Gear 25th Anniversary Appreciation Color
+addappid(2410761) -- GGST Guilty Gear 25th Anniversary Colors
+addappid(2524030) -- GGST Additional Character 10 - Johnny
+addappid(2524040) -- GGST Additional Character 11 - Elphelt Valentine
+addappid(2524050) -- GGST Additional Character 12 - A.B.A
+addappid(2524060) -- GGST Additional Character 13 - Slayer
+addappid(2524070) -- GGST Additional Character 14 - Queen Dizzy
+addappid(2524080) -- GGST Additional Character 15 - Venom
+addappid(2524090) -- GGST Additional Battle Stage 5 - Fallen Prayer, Engulfed Lives
+addappid(2524100) -- GGST Additional Battle Stage 6 - Amber Fest with Kind Neighbors
+addappid(2524110) -- GGST Season 3 Characters Additional Colors
+addappid(2524120) -- GGST Season 4 Characters Additional Colors
+addappid(3001230) -- GGST Additional Character 16 - Unika
+addappid(3001240) -- GGST Additional Character 17 - Lucy
+addappid(3001250) -- GGST Additional Character 18 - Jam Kuradoberi
+addappid(3001260) -- GGST Additional Battle Stage 7 - In the Name of Peace
+addappid(3001270) -- GGST Additional Battle Stage 8 - New Dawn, A Future Unfolding
+addappid(3060720) -- Guilty Gear -Strive- Season Pass 4
+addappid(4413190) -- Guilty Gear -Strive- Season Pass 5
+addappid(4580410) -- GGST Blazing Pass Plus Duel 1: Reignite
+
+-- DLC's (with depot keys)
+-- Guilty Gear -Strive- Ultimate Edition Content Kit DLC (AppID: 1652891)
+addappid(1652891)
+addappid(1652891, 1, "927ff307380ff6d4ccf5e19cb9434dae2ce3aaa43296e650580564a68245684d")
+setManifestid(1652891, "7995703727095321935", 2413782153)
+-- Guilty Gear -Strive- Original Soundtrack Necessary Discrepancy Disc 1 (AppID: 1998650)
+addappid(1998650)
+addappid(1998651, 1, "dcbf48645dba4d9eca0d76f45d2cc74c27e570e14b6d7b5cdac07cc5f4b3fbbc")
+setManifestid(1998651, "7659407962823933595", 667454716)
+addappid(1998652, 1, "5aa40b440760e690eed6f20f20238a97e6a3a6adbc953307072eda8b315513eb")
+setManifestid(1998652, "5773295757972079837", 94190646)
+-- Guilty Gear -Strive- Original Soundtrack Necessary Discrepancy Disc 2 (AppID: 1998750)
+addappid(1998750)
+addappid(1998751, 1, "29417fd6ea45f6bdb66fecf96f126dc20637c62bcc6b2e5b3e8b786c42067acd")
+setManifestid(1998751, "4702522582092228189", 630126193)
+addappid(1998752, 1, "5076468fb90c02c82c17c09be767d009319d147b2edb3938a81a8dbd2fb25d06")
+setManifestid(1998752, "1135728528634307548", 89113825)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
