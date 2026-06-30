737a5859e61707b11c8d061e6feddfaaf5cbce5d
qiao-lima <qiao-lima@github.com>
2026-06-30 17:35:48 +0800

    Manual Update 3716600

---
+++ 3716600.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3716600 - Mage Arena
+-- Generated 2026-06-30 09:22:40 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3716600, 1, "3ac0037556dbd901a5d189ff5d24eaa047c533a150b78e9fb70e156c466d59b1")
+
+-- Main Depots
+addappid(3716601, 1, "e310c1b9ef27e0d9ec00ac3593dc3851ee94ffa39699fed48341039f9ec50fc6")
+setManifestid(3716601, "7650696202510746684", 920929600)
