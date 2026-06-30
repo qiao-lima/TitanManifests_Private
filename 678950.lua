04807d05b9b5b706a27e57d5bd15e672e95e61ee
qiao-lima <qiao-lima@github.com>
2026-06-30 17:53:48 +0800

    Manual Update 678950

---
+++ 678950.lua
@@ -0,0 +1,55 @@
+-- Generated with Luie @ https://lua.tools/
+-- 678950 - DRAGON BALL FighterZ
+-- Generated 2026-06-30 08:42:33 UTC
+-- # Depots (Total/DLC/Shared): 4/0/2
+
+-- Main AppID
+addappid(678950, 1, "2b86e8c2ba07861abfdc8079b82fd79a287244f6d90e81e425c01dda2a640832")
+
+-- Main Depots
+addappid(678951, 1, "cd01842cde6ddf8bae5ce50cdaa221bf46f81a94091514fad68425f49bbb028d")
+setManifestid(678951, "852949778273295123", 6951922626)
+
+-- DLC's (no depot keys required)
+addappid(729450) -- DRAGON BALL FighterZ - FighterZ Pass
+addappid(758770) -- DRAGON BALL FighterZ - SSGSS Goku and SSGSS Vegeta Unlock
+addappid(758771) -- DRAGON BALL FighterZ - SSGSS Lobby Avatars
+addappid(758772) -- DLC 758772
+addappid(758774) -- DLC 758774
+addappid(758780) -- DLC 758780
+addappid(779930) -- DRAGON BALL FighterZ - Anime Music Pack
+addappid(779931) -- DRAGON BALL FighterZ - Commentator Voice Pack
+addappid(779932) -- DRAGON BALL FighterZ - Bardock
+addappid(779933) -- DRAGON BALL FighterZ - Broly
+addappid(779934) -- DRAGON BALL FighterZ - Zamasu (Fused)
+addappid(779935) -- DRAGON BALL FighterZ - Vegito (SSGSS)
+addappid(779936) -- DRAGON BALL FighterZ - Goku
+addappid(779937) -- DRAGON BALL FighterZ - Vegeta
+addappid(779938) -- DRAGON BALL FighterZ - Android 17
+addappid(779939) -- DRAGON BALL FighterZ - Cooler
+addappid(779940) -- DLC 779940
+addappid(952080) -- DRAGON BALL FIGHTERZ – Anime Music Pack 2
+addappid(992170) -- DRAGON BALL FIGHTERZ - Jiren
+addappid(992171) -- DRAGON BALL FIGHTERZ - Videl
+addappid(992172) -- DRAGON BALL FighterZ - Goku (GT)
+addappid(992173) -- DRAGON BALL FIGHTERZ - Janemba
+addappid(992174) -- DRAGON BALL FIGHTERZ - Gogeta (SSGSS)
+addappid(992175) -- DRAGON BALL FIGHTERZ - Broly (DBS)
+addappid(992176) -- DRAGON BALL FIGHTERZ - FighterZ Pass 2
+addappid(1020570) -- DRAGON BALL FighterZ - Commentator Voice Pack 2
+addappid(1020571) -- DRAGON BALL FighterZ - Commentator Voice Pack 3
+addappid(1020572) -- DRAGON BALL FighterZ - Commentator Voice Pack 4
+addappid(1209880) -- DLC 1209880
+addappid(1248430) -- DRAGON BALL FIGHTERZ - Goku (Ultra Instinct)
+addappid(1248431) -- DRAGON BALL FIGHTERZ - Master Roshi
+addappid(1248432) -- DRAGON BALL FIGHTERZ - Gogeta (SS4)
+addappid(1248433) -- DRAGON BALL FIGHTERZ - Super Baby 2
+addappid(1248434) -- DRAGON BALL FIGHTERZ - FighterZ Pass 3
+addappid(1744780) -- DRAGON BALL FIGHTERZ - Android 21 (Lab Coat)
+addappid(4179010) -- DRAGON BALL FighterZ - DAIMA Pack
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 678952
