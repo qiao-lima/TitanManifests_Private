6ee17710ec0e172d196023b14fa317a2a4c75b17
qiao-lima <qiao-lima@github.com>
2026-06-30 17:35:46 +0800

    Manual Update 1942280

---
+++ 1942280.lua
@@ -0,0 +1,29 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1942280 - Brotato
+-- Generated 2026-06-30 09:24:28 UTC
+-- # Depots (Total/DLC/Shared): 6/3/0
+
+-- Main AppID
+addappid(1942280, 1, "bf5b1b4f28c4d1839b85a86e3bfdabc5435f8cc848ca968aec505c86c6ecb52e")
+
+-- Main Depots
+addappid(1942281, 1, "414bbd1359869bcea73f5e7f3a911ba4a8f487dee2cf0290cd2409f9ae410582") -- (windows)
+setManifestid(1942281, "5289220193624029472", 245990176)
+addappid(1942282, 1, "3494683f2e70afb7ae55048ce6d4c4fd6139904d7d783355a35bbac4d1fd2a3b") -- (linux)
+setManifestid(1942282, "4872816150142449642", 228114885)
+addappid(1942283, 1, "15D4A4598C84D88D00A61EF4BAEDBD99BBF62E47402A887EAD1872B8AA5C89E7") -- (macos)
+setManifestid(1942283, "2748719760099278892", 291245046)
+
+-- DLC's (with depot keys)
+-- Brotato: Abyssal Terrors (AppID: 2868390)
+addappid(2868390)
+addappid(2868390, 1, "d7b9838655889932b120a4bd3426b37af101b334e6602a5898a1d54e4dc8f0f8")
+setManifestid(2868390, "5627320447442986624", 57980096)
+-- Brotato Soundtrack (AppID: 3281080)
+addappid(3281080)
+addappid(3281081, 1, "e6d0a71df9e37dbb1c69621d45ab42a35f0bc52e1c716e17bac88ffb29fa6cc6")
+setManifestid(3281081, "5219183446974776720", 84196826)
+addappid(3281082, 1, "0b62d5b0ce0ca5679ccbfafa9035fcbd744dccf03d97e6f2305ef8d28bcb8c58")
+setManifestid(3281082, "8742789198982800516", 467513096)
+
+-- Token-locked DLCs (couldn't read depots): 4498220
