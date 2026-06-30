5bb13261f911b5ac887058a674d745f9238aeea1
qiao-lima <qiao-lima@github.com>
2026-06-30 17:35:48 +0800

    Manual Update 3345460

---
+++ 3345460.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3345460 - CyberLink PowerDirector 2025 Ultra
+-- Generated 2026-06-30 09:22:59 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3345460)
+
+-- Main Depots
+addappid(3345461, 1, "940acbaba0909c3e68d0a7399b218b8cebcdd31668200dd47b25cb1cc4c349d7") -- (windows)
+setManifestid(3345461, "7989331457087753283", 1002075470)
