21d8f0ea6e70c949bec3fd1275e59c9fd16f6071
qiao-lima <qiao-lima@github.com>
2026-07-01 10:35:25 +0800

    Manual Update 4449460

---
+++ 4449460.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4449460 - Summer Vacation
+-- Generated 2026-07-01 01:36:11 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4449460)
+
+-- Main Depots
+addappid(4449461, 1, "6413e71697e4c727c715c37aac43645c5a3f3d36eab898d592bd824291dc5e85")
+setManifestid(4449461, "4069640551178229214", 2056298784)
