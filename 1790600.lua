2c7706e19ec5f403a02da836abe3d1085c812875
qiao-lima <qiao-lima@github.com>
2026-07-01 10:35:07 +0800

    Manual Update 1790600

---
+++ 1790600.lua
@@ -0,0 +1,38 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1790600 - DRAGON BALL: Sparking! ZERO
+-- Generated 2026-07-01 02:16:30 UTC
+-- # Depots (Total/DLC/Shared): 5/2/2
+
+-- Main AppID
+addappid(1790600, 1, "5c8b5e47bb9da92fe0a31a0d56554bb62851cb9f846cc5f89fac68501d9964ff")
+
+-- Main Depots
+addappid(1790601, 1, "0721a07eb97ba7b4e59e31606ae9f0d67c66309cc467c5da1aa79a924c34e7d8")
+setManifestid(1790601, "4296220909017956466", 30605034480)
+
+-- DLC's (no depot keys required)
+addappid(2957460) -- DRAGON BALL: Sparking! ZERO Pre-Order Pack
+addappid(2957470) -- DRAGON BALL: Sparking! ZERO Ultimate Upgrade Pack
+addappid(2957500) -- DRAGON BALL: Sparking! ZERO Victory Pack
+addappid(2957510) -- DRAGON BALL: Sparking! ZERO Martial Arts Pack
+addappid(2957520) -- DRAGON BALL: Sparking! ZERO Season Pass
+addappid(2963440) -- DRAGON BALL: Sparking! ZERO Ultimate Edition Bonus
+addappid(2963450) -- DRAGON BALL: Sparking! ZERO Season Pass Bonus
+addappid(3310650) -- DRAGON BALL: Sparking! ZERO HERO OF JUSTICE Pack
+addappid(3456600) -- DRAGON BALL: Sparking! ZERO - Dragon Ball DAIMA: Character Pack 1
+addappid(3456610) -- DRAGON BALL: Sparking! ZERO - Dragon Ball DAIMA: Character Pack 2
+addappid(3693250) -- DRAGON BALL: Sparking! ZERO - Shallot (DRAGON BALL LEGENDS)
+
+-- DLC's (with depot keys)
+-- DRAGON BALL: Sparking! ZERO Anime Music Pack 1 (AppID: 2957480)
+addappid(2957480)
+addappid(2957480, 1, "a8bbd2bdc3022cc2edc229e300b7583436b29d0e469662a8a277cf066798aa9f")
+setManifestid(2957480, "2926878599274088682", 25771532)
+-- DRAGON BALL: Sparking! ZERO Anime Music Pack 2 (AppID: 2957490)
+addappid(2957490)
+addappid(2957490, 1, "1b1f85ab25eb92147de8a0c1531d9ed57f3e01cfd46c9bb37be377251c2d24cd")
+setManifestid(2957490, "6336205044388274470", 41112630)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
