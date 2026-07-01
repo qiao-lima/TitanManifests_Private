2cca273154c5286c2455156c447bf61c77530838
qiao-lima <qiao-lima@github.com>
2026-07-01 10:37:10 +0800

    Manual Update 627270

---
+++ 627270.lua
@@ -0,0 +1,36 @@
+-- Generated with Luie @ https://lua.tools/
+-- 627270 - Injustice™ 2
+-- Generated 2026-07-01 01:35:16 UTC
+-- # Depots (Total/DLC/Shared): 9/6/2
+
+-- Main AppID
+addappid(627270, 1, "8e2b1d9d91496bb09b68bef61986d2c5806f602bd4d9bcfc330c5ec54760cea2")
+
+-- Main Depots
+addappid(627271, 1, "7fa0ea96f7de44df35ea69af2de65094bb167d849e116d01707e30bb8739fb5b")
+setManifestid(627271, "2051570751135048395", 47700830102)
+
+-- DLC's (no depot keys required)
+addappid(704810) -- Starfire
+addappid(731150) -- Demons Shader Pack
+addappid(731160) -- Gods Shader Pack
+addappid(731170) -- John Stewart
+addappid(731171) -- Power Girl
+addappid(731172) -- Reverse Flash
+addappid(734280) -- Ultimate Pack
+addappid(734281) -- Fighter Pack 1
+addappid(734282) -- Fighter Pack 2
+addappid(734283) -- Fighter Pack 3
+addappid(747800) -- Black Lightning
+addappid(759580) -- Atom
+addappid(759581) -- Hellboy
+addappid(779400) -- Enchantress
+addappid(779401) -- TMNT
+addappid(814250) -- DLC 814250
+addappid(814251) -- Infinite Transforms
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 721260, 721270, 721280, 721290, 727980, 727990
