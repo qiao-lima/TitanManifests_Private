e53da5d9fd97edce3cf424f2e02a45b7a24ca5d4
qiao-lima <qiao-lima@github.com>
2026-07-01 10:35:27 +0800

    Manual Update 4631200

---
+++ 4631200.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4631200 - Bring The Rift
+-- Generated 2026-07-01 01:59:35 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4631200)
+
+-- Main Depots
+addappid(4631201, 1, "d693425bfbc5e44ebf576127ff91c468e4dafef796d13f6de3ca0940c35a5093")
+setManifestid(4631201, "2859611520964556501", 62321574)
