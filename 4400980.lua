244261cfef37e2bfba155188de74f4669a443c2c
qiao-lima <qiao-lima@github.com>
2026-07-01 10:35:25 +0800

    Manual Update 4400980

---
+++ 4400980.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4400980 - Blood Gauntlet
+-- Generated 2026-07-01 01:48:47 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4400980)
+
+-- Main Depots
+addappid(4400981, 1, "e16844d3a60dbdd00f0e66dfff0bbb85d5ad05e23fbabfd8721f5622811ea9b9") -- (windows)
+setManifestid(4400981, "2409445528914770858", 129591362)
