350d54fe6428d2c229cf78291a9667c4019ce3e1
qiao-lima <qiao-lima@github.com>
2026-07-01 10:35:25 +0800

    Manual Update 4476000

---
+++ 4476000.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4476000 - Remagotchi
+-- Generated 2026-07-01 01:41:58 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(4476000)
+
+-- Main Depots
+addappid(4476001, 1, "6728e9aad7757766260712c2e3aeee288269e4751c1564dcde9180edb1192088") -- (windows)
+setManifestid(4476001, "3704013502861310508", 148262551)
+
+-- Missing Depots (We don't have the keys yet lol): 4476002
