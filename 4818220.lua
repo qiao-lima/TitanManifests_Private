bb7868d7db194969a7d8905f8f63891cff7386b5
qiao-lima <qiao-lima@github.com>
2026-07-01 10:35:28 +0800

    Manual Update 4818220

---
+++ 4818220.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4818220 - Lust Horizons
+-- Generated 2026-07-01 01:17:36 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(4818220)
+
+-- Main Depots
+addappid(4818221, 1, "aaf3aed2b5237cf533d28c4c0fbd8e30bf2a69e42bcfbf5bc0163832262e84a6")
+setManifestid(4818221, "4234082122265568583", 18503252183)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
