910a390c46a152c5d06e4d3a57f3a9452e6fef9a
qiao-lima <qiao-lima@github.com>
2026-07-02 10:34:50 +0800

    Manual Update 638230

---
+++ 638230.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 638230 - Journey
+-- Generated 2026-07-02 02:12:11 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(638230, 1, "82c9cf0893a03d37d26a8e7bca505241b1e6375b3603af91f490f4222543fd5b")
+
+-- Main Depots
+addappid(638231, 1, "b49b74a2b8b9170fde12db70ff5271bfbe8cbd48407f3eab8bbe23f4671a4ba7") -- (windows)
+setManifestid(638231, "8350646427077084846", 3636184182)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
