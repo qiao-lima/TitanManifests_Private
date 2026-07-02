c4dffb8b0975d5d09c6831f977c39968557131a2
qiao-lima <qiao-lima@github.com>
2026-07-03 07:41:02 +0800

    Manual Update 848610

---
+++ 848610.lua
@@ -0,0 +1,16 @@
+-- Generated with Luie @ https://lua.tools/
+-- 848610 - Adult Toy Store
+-- Generated 2026-07-02 23:36:15 UTC
+-- # Depots (Total/DLC/Shared): 4/0/3
+
+-- Main AppID
+addappid(848610)
+
+-- Main Depots
+addappid(848611, 1, "ab704d115d96526e4f2eb2649d625c2b8e5a16bb5c6fb0e5fe45a0c840bdcbab") -- (windows)
+setManifestid(848611, "6809338667838673862", 1016934064)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") -- (windows)
