bdb7e3a6ab756cc5e5c02f5ac90075027dcf054b
qiao-lima <qiao-lima@github.com>
2026-07-03 07:41:02 +0800

    Manual Update 857060

---
+++ 857060.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 857060 - CCCP CALLS!
+-- Generated 2026-07-02 23:38:01 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(857060)
+
+-- Main Depots
+addappid(857061, 1, "bcb8013780757d478df71ad8fe93711116a5c6f7ec07bbd7c44d2b358bcfdc08")
+setManifestid(857061, "6222391486281299948", 168392097)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
