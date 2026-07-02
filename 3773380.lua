aadaf5f4f50819fabd649771fecd1bfdbfdc59e2
qiao-lima <qiao-lima@github.com>
2026-07-03 07:41:01 +0800

    Manual Update 3773380

---
+++ 3773380.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3773380 - Brainwashing Academy - The Cursed Person's Pot
+-- Generated 2026-07-02 23:35:56 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(3773380)
+
+-- Main Depots
+addappid(3773381, 1, "e5fab4bdfdc513e3c8cb57d31f3cbf609591c1c0220333395beb7ce1d61d17fc") -- English
+setManifestid(3773381, "7032226484848654158", 1059261210)
+
+-- Missing Depots (We don't have the keys yet lol): 3773382
