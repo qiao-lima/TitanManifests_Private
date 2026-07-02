29a8d619cad04102223fe3873f3e1ada24a755de
qiao-lima <qiao-lima@github.com>
2026-07-03 07:41:01 +0800

    Manual Update 3657210

---
+++ 3657210.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3657210 - Amanda the Adventurer 3
+-- Generated 2026-07-02 23:39:01 UTC
+-- # Depots (Total/DLC/Shared): 3/2/0
+
+-- Main AppID
+addappid(3657210)
+
+-- Main Depots
+addappid(3657211, 1, "e0e61f4993c2929f4ad84eea55c473f50fd8a91e974c3b1073028caaf9b75591")
+setManifestid(3657211, "8386410028301103832", 11836408217)
+
+-- Missing Depots (We don't have the keys yet lol): 4082521, 4082522
