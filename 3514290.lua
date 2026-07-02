47b56e12ed4b7e048054e20b71a6e38601243e41
qiao-lima <qiao-lima@github.com>
2026-07-02 10:32:03 +0800

    Manual Update 3514290

---
+++ 3514290.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3514290 - Piano Collections NieR Gestalt & Replicant
+-- Generated 2026-07-02 02:09:38 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3514290)
+
+-- Main Depots
+addappid(3514291, 1, "d13260b0102cc44a8ee911a6198987931e97e6c223c7ef75b3900157a6bb7687")
+setManifestid(3514291, "7712689375725388743", 109711585)
