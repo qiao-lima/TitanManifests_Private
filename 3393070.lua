e4038d0000e0636693a6eba6e6142bb366567faa
qiao-lima <qiao-lima@github.com>
2026-07-03 07:41:00 +0800

    Manual Update 3393070

---
+++ 3393070.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3393070 - KAIJU NO. 8 THE GAME
+-- Generated 2026-07-02 23:32:40 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(3393070, 1, "ba719336aa5a31abbc2f3e6c9162b1d948d124963acf1dc878d031600127302c")
+
+-- Main Depots
+addappid(3393071, 1, "8ab583a45b10ac9f5fdb78cf7b55b7299f90e72e8f557696699ba93af657677c") -- Japanese (windows)
+setManifestid(3393071, "7797800888384767139", 226149656)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- (windows)
