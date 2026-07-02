0487a64ed66ca49d8e1166dd2f9e4f1f1d127528
qiao-lima <qiao-lima@github.com>
2026-07-02 10:32:02 +0800

    Manual Update 2325920

---
+++ 2325920.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2325920 - Forestrike
+-- Generated 2026-07-02 02:10:24 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2325920)
+
+-- Main Depots
+addappid(2325921, 1, "58a47ef0da4d1c9cbee3849cfc8d552d835433c211a890943212b44b7e2b2ab5")
+setManifestid(2325921, "7710495538706107840", 2041844046)
