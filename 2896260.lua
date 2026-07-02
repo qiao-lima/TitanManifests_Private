f5fbbd6072d94d474ef23b51ab2e8006eedb7d64
qiao-lima <qiao-lima@github.com>
2026-07-03 07:41:00 +0800

    Manual Update 2896260

---
+++ 2896260.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2896260 - ODDCORE
+-- Generated 2026-07-02 23:40:01 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2896260, 1, "4d2e59460b3b1d1c3905bd0a2e0bd14c59589d94c711b3afab640e94dc52b069")
+
+-- Main Depots
+addappid(2896261, 1, "c82ef318eb2b98fa3a40efed97d8f237b4e34de5005b21e17c5c661bbb4e831a")
+setManifestid(2896261, "1826872441585321368", 1459864055)
