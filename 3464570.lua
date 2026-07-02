9680cf7b81ba15100f22440be06156fc682a76e7
qiao-lima <qiao-lima@github.com>
2026-07-03 07:41:00 +0800

    Manual Update 3464570

---
+++ 3464570.lua
@@ -0,0 +1,22 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3464570 - Я беременна в 16
+-- Generated 2026-07-02 23:28:00 UTC
+-- # Depots (Total/DLC/Shared): 5/4/0
+
+-- Main AppID
+addappid(3464570, 1, "958cb15f055fa10d624cf98ceb0dae3edc4cdc0e9bc564be5eccd3b684a84d22")
+
+-- Main Depots
+addappid(3464571, 1, "1b02e37e6d8533825a8916829ccad9424597c459f141b9f1bd002dc74547ea43")
+setManifestid(3464571, "6235658421093198096", 6692077541)
+
+-- DLC's (no depot keys required)
+addappid(4050150) -- Я беременна в 16: Эпизод 3
+
+-- DLC's (with depot keys)
+-- Я беременна в 16: Эпизод 2 (AppID: 3909910)
+addappid(3909910)
+addappid(3909910, 1, "9b282b787805c668622603e39994b1b8af9b1b15979b46b18c39fd7e99c38888")
+setManifestid(3909910, "6562072915117007233", 1202326)
+
+-- Missing Depots (We don't have the keys yet lol): 3464572, 3628480, 4033390
