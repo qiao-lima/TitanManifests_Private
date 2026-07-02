3dc9e84d994509010d40170d0c91951e8b1d2830
qiao-lima <qiao-lima@github.com>
2026-07-03 07:40:59 +0800

    Manual Update 2487150

---
+++ 2487150.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2487150 - Deconstruction Simulator
+-- Generated 2026-07-02 23:34:28 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(2487150, 1, "74bd2cac3087267e9905b6e3fcc0db2d351adda777799fb7484ff3400aef82ec")
+
+-- Main Depots
+addappid(2487151, 1, "734bff9aed757224ce32fdd52d7ef669c41d5e8d0c6f972006e554f1b8e0bbc1")
+setManifestid(2487151, "7776285176061663733", 9107648238)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
