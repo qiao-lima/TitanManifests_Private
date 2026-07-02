a0c7cfe0059f1d9fe2160f2a455ae8d46fe74209
qiao-lima <qiao-lima@github.com>
2026-07-02 10:32:03 +0800

    Manual Update 281260

---
+++ 281260.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 281260 - Desert Gunner
+-- Generated 2026-07-02 02:18:47 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(281260)
+
+-- Main Depots
+addappid(281261, 1, "0b6490f64611827cf98a47289cead03a4ae2c4abce020dd63375332316ca9687")
+setManifestid(281261, "5938977475612077020", 88671057)
