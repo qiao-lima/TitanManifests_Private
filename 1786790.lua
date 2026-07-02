7882a399451cbe4ae9e1be9f00ced3d97803f550
qiao-lima <qiao-lima@github.com>
2026-07-03 07:40:59 +0800

    Manual Update 1786790

---
+++ 1786790.lua
@@ -0,0 +1,25 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1786790 - Magical Girl Celesphonia
+-- Generated 2026-07-02 23:33:49 UTC
+-- # Depots (Total/DLC/Shared): 5/2/0
+
+-- Main AppID
+addappid(1786790)
+
+-- Main Depots
+addappid(1786791, 1, "79653d61e87d3f733ec7273ea4efcd0e16a1cc36adf01917ead5a96ba3eb1a78")
+setManifestid(1786791, "5210893118450742983", 1032766013)
+addappid(1786792, 1, "645a031f716157475cabad4b564b2f14c7eca13542f1bdfab17bd87988f9ac4c") -- Schinese
+setManifestid(1786792, "1657075419692784897", 886300549)
+addappid(1786793, 1, "7546a25c6642a0ca33fb91a86690ed53ce8b9907dc127b5c921b5e7c5c5f6f77") -- Japanese
+setManifestid(1786793, "7406223482748114845", 875858162)
+
+-- DLC's (with depot keys)
+-- Magical Girl Celesphonia - Official Amane Cosplay by Elizabeth Rage (AppID: 2203950)
+addappid(2203950)
+addappid(2203950, 1, "f5e2d062f2911908147776f591f2fdcce6749241fff5f34c184c33ed7a4497fd")
+setManifestid(2203950, "3837311882041404848", 10449779)
+-- Magical Girl Celesphonia - Official Celesphonia Cosplay by Luna Amemiya (AppID: 2862000)
+addappid(2862000)
+addappid(2862000, 1, "e65511f4d3d6c3413ff11e3ef222e1103316d8bff635ecc0627227c656b1534a")
+setManifestid(2862000, "135751573565927368", 11710233)
