6be747cf3f7505e3b86e7b3fa03dfdac85d748d7
qiao-lima <qiao-lima@github.com>
2026-07-02 10:32:02 +0800

    Manual Update 2654860

---
+++ 2654860.lua
@@ -0,0 +1,18 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2654860 - Mizi NO!
+-- Generated 2026-07-02 02:21:03 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2654860)
+
+-- Main Depots
+addappid(2654861, 1, "bd8ac616ad24f463930e70e2b1516583e74641fc4d0f4e4ddfb8cc087813ef48")
+setManifestid(2654861, "1766140045541217788", 675369486)
+
+-- DLC's (no depot keys required)
+addappid(2900150) -- Mizi NO! - Everyday Objects
+addappid(2900160) -- Mizi NO! - Playful Patterns
+addappid(2900170) -- Mizi NO! - Blank Slate
+addappid(2900180) -- Mizi NO! - Kintsugi
+addappid(3592670) -- Mizi NO! - Paperwork
