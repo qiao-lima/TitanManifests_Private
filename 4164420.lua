68cd52c306da0dc0b60bde3963d28265c69a4c8f
qiao-lima <qiao-lima@github.com>
2026-07-03 07:41:02 +0800

    Manual Update 4164420

---
+++ 4164420.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4164420 - My Winter Car
+-- Generated 2026-07-02 23:39:42 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4164420, 1, "865d1f548094d6e15893000b7ce7784bc339961b10a76a311f390757c4b1af36")
+
+-- Main Depots
+addappid(4164421, 1, "25786818231b18d0c50cadb12be7d574140d7de03edebbea0fdeb17c8ac49fb7") -- English (windows, 64-bit)
+setManifestid(4164421, "313414321358329271", 1163625971)
