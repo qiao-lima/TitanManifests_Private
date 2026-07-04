9c7e5b4fa1cd29886cb45100a2b73dcd639e49fe
qiao-lima <qiao-lima@github.com>
2026-07-04 13:51:54 +0800

    Manual Update 4450160

---
+++ 4450160.lua
@@ -0,0 +1,14 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4450160 - Spire of Ash
+-- Generated 2026-07-04 04:26:31 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4450160)
+
+-- Main Depots
+addappid(4450161, 1, "0f85365fbf769556bfb8b50fab38872d135c41b4850a529f674395930d2a855f")
+setManifestid(4450161, "6930352418053197205", 2680229384)
+
+-- DLC's (no depot keys required)
+addappid(4526170) -- Spire of Ash - Full Game Unlock
