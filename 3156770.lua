fc9712fed84db6daf17ee84c3d45da4f2697a04a
qiao-lima <qiao-lima@github.com>
2026-07-04 13:47:58 +0800

    Manual Update 3156770

---
+++ 3156770.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3156770 - Witchfire
+-- Generated 2026-07-04 04:18:17 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(3156770, 1, "a3d625ea35ccb9f6b7f85adb4bc578f136e51437a97ea3277887ca9ed995e2e8")
+
+-- Main Depots
+addappid(3156771, 1, "e09d98ec7fb6604ed8c2f44292e3f8cb278dd0add46641f0434375eeb9255e87")
+setManifestid(3156771, "7429280512449418268", 52515393913)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
