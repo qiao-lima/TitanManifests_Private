655a7df2260a004d2792a7f30cf2cba6c987a750
qiao-lima <qiao-lima@github.com>
2026-07-04 13:47:56 +0800

    Manual Update 2958130

---
+++ 2958130.lua
@@ -0,0 +1,20 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2958130 - Jurassic World Evolution 3
+-- Generated 2026-07-04 03:43:07 UTC
+-- # Depots (Total/DLC/Shared): 2/0/1
+
+-- Main AppID
+addappid(2958130, 1, "28c45829d1af12623b4fead679e384291bfb040d0241fc6be9d05f7d4cdbbd62")
+
+-- Main Depots
+addappid(2958131, 1, "dc7cb4cb8d7d76fdcab84fed34fa1f799e7459cf1be5ed041116ed846211889d")
+setManifestid(2958131, "7742518641681988092", 24821143114)
+
+-- DLC's (no depot keys required)
+addappid(3651190) -- Jurassic World Evolution 3: Badlands Set
+addappid(3651200) -- Jurassic World Evolution 3: Deluxe Upgrade Pack
+addappid(4131660) -- Jurassic World Evolution 3: Wetlands
+addappid(4131670) -- Jurassic World Evolution 3: Rebirth Expansion
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
