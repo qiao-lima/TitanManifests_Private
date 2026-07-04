89ef1fec3010ff0e4dc55d07bfd35985e9df689c
qiao-lima <qiao-lima@github.com>
2026-07-04 13:51:55 +0800

    Manual Update 4521430

---
+++ 4521430.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4521430 - Oblivion Maiden
+-- Generated 2026-07-04 05:41:59 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(4521430)
+
+-- Main Depots
+addappid(4521431, 1, "c70cfd937a8865e728368760b429100772836c31fb5091117774f27a7db29a5e") -- (windows)
+setManifestid(4521431, "5140275659380555789", 1886497879)
+
+-- Missing Depots (We don't have the keys yet lol): 4521432
