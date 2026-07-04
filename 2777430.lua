0f5893f890618fe37d67c4fcac097f51f52ac69e
qiao-lima <qiao-lima@github.com>
2026-07-04 13:44:11 +0800

    Manual Update 2777430

---
+++ 2777430.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2777430 - The Wedding
+-- Generated 2026-07-04 05:42:20 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2777430)
+
+-- Main Depots
+addappid(2777431, 1, "7ccbdac26df728169b47052c83f8d61edbb306bb2b764e13a00d3c1adbc60f19")
+setManifestid(2777431, "8186092641875026592", 578292049)
