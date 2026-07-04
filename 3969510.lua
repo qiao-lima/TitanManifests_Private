2fb1e40cde451a1238d60abe8a0f1d689d9d1d54
qiao-lima <qiao-lima@github.com>
2026-07-04 13:48:01 +0800

    Manual Update 3969510

---
+++ 3969510.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3969510 - LINK Penguins
+-- Generated 2026-07-04 04:25:29 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(3969510)
+
+-- Main Depots
+addappid(3969511, 1, "5ec0d195c4179312ac2963feb5624ba74edf1348ca2f9b0be681c73e8965338e")
+setManifestid(3969511, "7398407353484879806", 1989382591)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
