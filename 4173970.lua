3292c32586c53dfbeb3a0687bfc94447f0d86d2a
qiao-lima <qiao-lima@github.com>
2026-07-04 13:51:53 +0800

    Manual Update 4173970

---
+++ 4173970.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4173970 - Procelio
+-- Generated 2026-07-04 05:07:31 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4173970)
+
+-- Main Depots
+addappid(4173971, 1, "fdcc8b2a1bdabe97001edd140adfef6ca3f2fdbac0690b492401f77027e88cd8")
+setManifestid(4173971, "7537194847360667829", 4311005640)
