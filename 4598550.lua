81639beb537f481e6a4e90f468444018b77f470f
qiao-lima <qiao-lima@github.com>
2026-07-04 13:51:55 +0800

    Manual Update 4598550

---
+++ 4598550.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4598550 - Lovecraft Locker 2: Tentacle Breach Demo
+-- Generated 2026-07-04 04:39:19 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4598550)
+
+-- Main Depots
+addappid(4598551, 1, "e7a64aa4461561bd67879857ed9c0ca7534dbd3b9fbab37dd6d84bd386942f6f")
+setManifestid(4598551, "820985865786746959", 298735240)
