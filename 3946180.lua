b766114016216ef44bd558dfc70fdf44fea1c3fb
qiao-lima <qiao-lima@github.com>
2026-07-04 13:48:01 +0800

    Manual Update 3946180

---
+++ 3946180.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3946180 - When Stars Collide
+-- Generated 2026-07-04 05:39:28 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3946180)
+
+-- Main Depots
+addappid(3946181, 1, "838a2c4bc527c6a106a20d43f39ed9b93337d6c552d0e27695eb1b8aa5dffab8")
+setManifestid(3946181, "8056907024356461031", 1144198136)
