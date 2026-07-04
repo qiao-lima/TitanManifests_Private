5a94ea4a1d4d33e794b663c118541d466324270f
qiao-lima <qiao-lima@github.com>
2026-07-04 13:44:12 +0800

    Manual Update 289070

---
+++ 289070.lua
@@ -0,0 +1,157 @@
+-- 289070's Lua and Manifest Created by Hubcap Manifest
+-- Sid Meier's Civilization VI
+-- Created: January 26, 2026 at 20:52:23 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 61
+-- Total DLCs: 28
+-- Shared Depots: 4
+-- MAIN APPLICATION
+addappid(289070, 1, "ed38a5e929f3d0752878910f8e8020dcb191d9c469bd0c9a81568115703715f9") -- Sid Meier's Civilization VI
+-- MAIN APP DEPOTS
+addappid(289071, 1, "32741972cb390fe0fb0253da0e76fb113c9f92280b55093fb4c901b0c84e1a8d") -- Madrid Content
+addappid(289072, 1, "3d2108e73f87d05885853ba5e72d9fc0d80c7f4a834e3fec557479960a6f12e8") -- Madrid EXE
+addappid(533500, 1, "ee43c4a838717e3a4bd9973e4038155c27a9ebd0f0d9b9de4de50e272491b313") -- Mac Madrid Content
+addappid(533501, 1, "c9865d5c2a00f794d06e91b982f882e0b2cf9ca6bea37b5eb37d35cd83955b67") -- Mac Madrid EXE
+addappid(533502, 1, "6a28697b6e29d111b9b2789d8740b1e0d36cbf949a0ed00b500d438cf6ca81b1") -- Linux Content
+addappid(533503, 1, "ba65f8252337d21a1861ece22ebd5d5a8b6e0afec40e5ba20cb30d363a59afee") -- Linux Exe
+addappid(289089, 1, "a1cab903cd10b15478bc455b931245ef42becf828cbefc19bb922dfaf06776b2") -- Launcher Test
+addappid(289085, 1, "993eb4eff10f1f76852177fcd3e668b202059359f3f854085a162a269eb12e51") -- Depot 289085
+addappid(289086, 1, "4bcd85f1a6e269ac65b7637fcc6b922bf2cab229cc8a5f55e34a1a890490997d") -- Depot 289086
+-- SHARED DEPOTS (from other apps)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- VC 2012 Redist (Shared from App 228980)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- VC 2015 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+-- DLCS WITH DEDICATED DEPOTS
+-- Sid Meiers Civilization VI Aztec Civilization Pack (AppID: 512030)
+addappid(512030)
+addappid(512030, 1, "8414f9daababcb658bab729cfe1713cae6cc02e8418e12139132c6ca49275dce") -- Sid Meiers Civilization VI Aztec Civilization Pack - Aztec Pre-Order (512030) Depot
+addappid(533513, 1, "25c9aa2d1ad239ea2f131216fb0917f5ab8005af6ea35a468a4dfabee16fc1ee") -- Sid Meiers Civilization VI Aztec Civilization Pack - Mac Pre-Order DLC Aztec Civ Pack
+addappid(533504, 1, "5cfb7c534d8154a04622b21e0fd86b6f43338f9bbf96e42ff7e32d23a21fb258") -- Sid Meiers Civilization VI Aztec Civilization Pack - Linux DLC Aztec Civ Pack
+-- Sid Meiers Civilization VI 25th Anniversary Soundtrack (AppID: 512031)
+addappid(512031)
+addtoken(512031, "7126022563837032702")
+addappid(512031, 1, "bf4e034745cadce7e2deadbe49f0295fabda3bb4d53cb81245dedd8b4f864932") -- Sid Meiers Civilization VI 25th Anniversary Soundtrack - Madrid Soundtrack (512031) Depot
+-- Sid Meiers Civilization VI Vikings Scenario Pack (AppID: 512032)
+addappid(512032)
+addappid(512032, 1, "96db6adf9acfd23efac126a1b390566ce3d60c9383c8a12277a3364a0c9825b3") -- Sid Meiers Civilization VI Vikings Scenario Pack - Madrid DLC01 (512032) Depot
+addappid(533514, 1, "96bc216f9252b61f705d823293ed0a8cc7243021a3e9c914292525f3f8f654d3") -- Sid Meiers Civilization VI Vikings Scenario Pack - Mac Madrid DLC01
+addappid(533505, 1, "83feda3eccf1abb7cba2ab01bd2c9fa4c121b508ead25162c7e2ca9b6db56488") -- Sid Meiers Civilization VI Vikings Scenario Pack - Linux DLC 01
+-- Sid Meiers Civilization VI Poland Civilization  Scenario Pack (AppID: 512033)
+addappid(512033)
+addappid(512033, 1, "119999ebe1538e1bceacbccbd0a33aaab47cd471ef2dba9bae58afdeaf298c7f") -- Sid Meiers Civilization VI Poland Civilization  Scenario Pack - Madrid DLC02 (512033) Depot
+addappid(533515, 1, "fa7c250de971db52d1cc11d0ab324e83060acab493717015a0cb18b6805f5f3b") -- Sid Meiers Civilization VI Poland Civilization  Scenario Pack - Mac Madrid DLC02
+addappid(533506, 1, "f7d95d7da09211df4822cd1bf907b5677bb04a133d60fdd0048cd3b2954ae855") -- Sid Meiers Civilization VI Poland Civilization  Scenario Pack - Linux DLC 02
+-- Sid Meiers Civilization VI Australia Civilization  Scenario Pack (AppID: 512034)
+addappid(512034)
+addappid(512034, 1, "f1b7b50ee592be7ea3f70a78001aee65d745768a549fe125274c9c68377fe19d") -- Sid Meiers Civilization VI Australia Civilization  Scenario Pack - Madrid DLC03 (512034) Depot
+addappid(533516, 1, "8072993ec1d1f3c20674e14aca28a5cbbc462beaf45c771111381e434a184779") -- Sid Meiers Civilization VI Australia Civilization  Scenario Pack - Mac Madrid DLC03
+addappid(533507, 1, "acec8d06a13af1db888b52f8d7fe8848e03d8d1ec03e139c2ed8af0a7aafd9b5") -- Sid Meiers Civilization VI Australia Civilization  Scenario Pack - Linux DLC 03
+-- Sid Meiers Civilization VI Persia and Macedon Civilization  Scenario Pack (AppID: 512035)
+addappid(512035)
+addappid(512035, 1, "13581afaf8e722ba3a7eeec875c1214d186c9c139b7baf2fde68c13e928fe7c4") -- Sid Meiers Civilization VI Persia and Macedon Civilization  Scenario Pack - Madrid DLC04 (512035) Depot
+addappid(533517, 1, "ef85623de67473f14590612adf8e79f14c404895c4a4b86897581cbddd63c749") -- Sid Meiers Civilization VI Persia and Macedon Civilization  Scenario Pack - Mac Madrid DLC04
+addappid(533508, 1, "b014e8ca6d7083f95aa5885a5797716e3691f43341c3a9636c9ca731489d76c5") -- Sid Meiers Civilization VI Persia and Macedon Civilization  Scenario Pack - Linux DLC 04
+-- Sid Meiers Civilization VI Nubia Civilization  Scenario Pack (AppID: 645400)
+addappid(645400)
+addappid(645400, 1, "8a46441e5fdfc733e511fc579168bcd03791875c78251afb120f6a6ca8f58333") -- Sid Meiers Civilization VI Nubia Civilization  Scenario Pack - Madrid DLC05 (645400) Depot
+addappid(533518, 1, "d8def9e6eedd435729f252b4b7baab2108e2ca8136a1c777c011405d293740d2") -- Sid Meiers Civilization VI Nubia Civilization  Scenario Pack - Mac Madrid DLC05
+addappid(533509, 1, "c33fffb83594745bb85ebabafe952225ce80fe20b05bb6cc2c81ebf44d34198c") -- Sid Meiers Civilization VI Nubia Civilization  Scenario Pack - Linux DLC 05
+-- Sid Meiers Civilization VI Khmer and Indonesia Civilization  Scenario Pack (AppID: 645401)
+addappid(645401)
+addappid(645401, 1, "6ea08c1c060dcd07c457e12d8149ca548da5b9a40ef543fcd15691a3d1ba7784") -- Sid Meiers Civilization VI Khmer and Indonesia Civilization  Scenario Pack - Madrid DLC06 (645401) Depot
+addappid(533519, 1, "e7563567a842cfd82717ee1543b45c93430d81d01008c882b7d3418616ca6530") -- Sid Meiers Civilization VI Khmer and Indonesia Civilization  Scenario Pack - Mac Madrid DLC06
+addappid(533510, 1, "64e4e71723d9d13db750f4df85bfc5a0cb331d48c3aa4cd81091f39bea3c09a9") -- Sid Meiers Civilization VI Khmer and Indonesia Civilization  Scenario Pack - Linux DLC 06
+-- Sid Meiers Civilization VI Rise and Fall (AppID: 645402)
+addappid(645402)
+addappid(645402, 1, "9e2251ad6d619d6ac2645cdcc17ac9493f2fc9777a68ab304e211f5bf8b018d4") -- Sid Meiers Civilization VI Rise and Fall - Civ GH (645402) Depot
+addappid(533512, 1, "33a4921125ef9ce860aa0bbec3a0dc994171a3058be7e6d7ba868ece88742db7") -- Sid Meiers Civilization VI Rise and Fall - Mac Madrid Expansion1
+addappid(533511, 1, "5e295ec44b83955f08f49cf8884f4cb7f138461da3aad081e4649ddcfc7d268d") -- Sid Meiers Civilization VI Rise and Fall - Linux Expansion1
+-- Sid Meiers Civilization VI Gathering Storm (AppID: 947510)
+addappid(947510)
+addappid(947510, 1, "12ed67821997d2ff92e4155d8735d254c5f20f27abba7ba4eb1dde6759ec8e4e") -- Sid Meiers Civilization VI Gathering Storm - DepotTest (947510) Depot
+addappid(947518, 1, "5cc97a451ab6835a9748d42855586c4f83fef8581f0be62d55ab82963822d2c7") -- Sid Meiers Civilization VI Gathering Storm - Mac Madrid Expansion2
+addappid(947519, 1, "654ac94cedb0a52d9fa38d7231b8f4b4dfd62b821ea78c3a59f7d74242d331ca") -- Sid Meiers Civilization VI Gathering Storm - Linux Expansion2
+-- Sid Meiers Civilization VI Maya  Gran Colombia Pack (AppID: 1253990)
+addappid(1253990)
+addappid(1253991, 1, "e902367397b6079f60002946cc17e4d8caea4114f74f60d225be0004e721017d") -- Sid Meiers Civilization VI Maya  Gran Colombia Pack - Mac Runner
+addappid(1253992, 1, "6673a545570835463a5750d1ce4a4cbc7c696b7ab4dae26255c86fdfe91705d9") -- Sid Meiers Civilization VI Maya  Gran Colombia Pack - Linux Runner
+-- Sid Meiers Civilization VI Ethiopia Pack (AppID: 1270540)
+addappid(1270540)
+addappid(1270541, 1, "d863b54dcb049ec6e1efe4567b6a9a96c3b0a25d1262d267b9d75e99bb30ffdd") -- Sid Meiers Civilization VI Ethiopia Pack - Mac Plaid
+addappid(1270542, 1, "33500db5f561604706815a311f6c5340d247317b07f3335895163a3d06b97467") -- Sid Meiers Civilization VI Ethiopia Pack - Linux Plaid
+-- Sid Meiers Civilization VI Teddy Roosevelt Persona Pack (AppID: 1281820)
+addappid(1281820)
+addtoken(1281820, "13286021469407624402")
+addappid(1281821, 1, "bf4cad76e34f172e797fc09967c5f110d26cf7636bf832f7bc2184a0072b8bc5") -- Sid Meiers Civilization VI Teddy Roosevelt Persona Pack - Mac Zoomzoom
+addappid(1281822, 1, "06a9093294fc2dbd7335093ed9510aa44b3bdb0ba53157b86ba0b38703cc8a2a") -- Sid Meiers Civilization VI Teddy Roosevelt Persona Pack - Linux Zoomzoom
+-- Sid Meiers Civilization VI Byzantium  Gaul Pack (AppID: 1284470)
+addappid(1284470)
+addtoken(1284470, "887974276820259093")
+addappid(1284471, 1, "547ec1312d32f232e79c9a5ace35e93b454d517c48128f6d7a4d80355400c67b") -- Sid Meiers Civilization VI Byzantium  Gaul Pack - Mac SoonTM
+addappid(1284472, 1, "2a429b6c8ea5f7ca2c504765c250ae93114a2adbd711861692a6b2d6fbee0c21") -- Sid Meiers Civilization VI Byzantium  Gaul Pack - Linux SoonTM
+-- Sid Meiers Civilization VI Catherine de Medici Persona Pack (AppID: 1342010)
+addappid(1342010)
+addtoken(1342010, "9041882688234722446")
+addappid(1342011, 1, "ea124a7e31d2d3282d894a902771c1c879f1c3f6fd3d9fe48a6a6bc50dfaea42") -- Sid Meiers Civilization VI Catherine de Medici Persona Pack - Mac TinyCar
+addappid(1342012, 1, "70a1293d4491b8f45130bab379017d6d3cccbdfaa892a6ef610ea3762c0ee39b") -- Sid Meiers Civilization VI Catherine de Medici Persona Pack - Linux TinyCar
+-- Sid Meiers Civilization VI Babylon Pack (AppID: 1388850)
+addappid(1388850)
+addappid(1388851, 1, "9f241cb21ed61c3647868484c0cc309662fa50d0165c6c6b70c6950b1cd45dcf") -- Sid Meiers Civilization VI Babylon Pack - Mac MoorTires
+addappid(1388852, 1, "cc7f9df4fc6dbc1a4f17bfa6ba387c9b7f33b1cdf1c1cb59e4f9a7ea235c4812") -- Sid Meiers Civilization VI Babylon Pack - Linux MoorTires
+-- Sid Meiers Civilization VI Vietnam  Kublai Khan Pack (AppID: 1436950)
+addappid(1436950)
+addappid(1436951, 1, "60039cec493cb3168ba98eeb94de7f17f164dd0b991590ed284c132b076b2356") -- Sid Meiers Civilization VI Vietnam  Kublai Khan Pack - Mac MouthBag
+addappid(1436952, 1, "7ca4ca99a22e220b5033af83fef6b2b576dfd6feeeb54ce32c4a3227828be8d2") -- Sid Meiers Civilization VI Vietnam  Kublai Khan Pack - Linux MouthBag
+-- Sid Meiers Civilization VI Portugal Pack (AppID: 1478300)
+addappid(1478300)
+addappid(1478301, 1, "e5938fc09f846d85f360988efd1d6d1f27ae4fdeafcb9c1afe7106dd72ab5f4b") -- Sid Meiers Civilization VI Portugal Pack - Mac PeachNPoppy
+addappid(1478302, 1, "dac6b0d084e2419c74ece517da48803b2ef57aaf7d855d9c39c91dd8106ef1e1") -- Sid Meiers Civilization VI Portugal Pack - Linux PeachNPoppy
+-- Great Negotiators (AppID: 2173131)
+addappid(2173131)
+addappid(2173133, 1, "66330ed52cf95ee2ade74db13281d0c1ace4d44fae46be89dee64a565c5c5a1e") -- Great Negotiators - Depot 2173133
+-- Great Commanders (AppID: 2222600)
+addappid(2222600)
+addappid(2222605, 1, "cc93d280f3c6baab550228224611098ca46ecd76d84f5c3fb5511b6f788ce460") -- Great Commanders - Depot 2222605
+-- Rulers of China (AppID: 2222601)
+addappid(2222601)
+addappid(2222606, 1, "bef2ad041da59d4788214f7a01a3dbf7907819b0f4b591b41ecd1192eb5ffba1") -- Rulers of China - Depot 2222606
+-- Rulers of the Sahara (AppID: 2222602)
+addappid(2222602)
+addappid(2222607, 1, "69facab69f9fcda1656500a05d43b6e513b61bf73ea14145fc7aaaf204b94e6e") -- Rulers of the Sahara - Depot 2222607
+-- Great Builders (AppID: 2222603)
+addappid(2222603)
+addappid(2222608, 1, "232bdf85fd33ab840192f727dcdadb3c5cfd84346446611325acaf6197e204ef") -- Great Builders - Depot 2222608
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(521770) -- Sid Meiers Civilization VI Digital Deluxe Extras
+addappid(1308090) -- Sid Meiers Civilization VI New Frontier Pass
+addappid(2158250) -- Sid Meiers Civilization VI Leader Pass
+addappid(2222604) -- Rulers of England
+addappid(2297840) -- Julius Caesar
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(289073) -- Depot 289073 (empty depot)
+-- addappid(289074) -- Depot 289074 (empty depot)
+-- addappid(289075) -- Depot 289075 (empty depot)
+-- addappid(289076) -- Depot 289076 (empty depot)
+-- addappid(289077) -- Depot 289077 (empty depot)
+-- addappid(289078) -- Depot 289078 (empty depot)
+-- addappid(289079) -- Depot 289079 (empty depot)
+-- addappid(289080) -- Depot 289080 (empty depot)
+-- addappid(289081) -- Depot 289081 (empty depot)
+-- addappid(289082) -- Depot 289082 (empty depot)
+-- addappid(289083) -- Depot 289083 (empty depot)
+-- addappid(289084) -- Depot 289084 (empty depot)
+-- addappid(1253990) -- Depot 1253990 (empty depot)
+-- addappid(1270540) -- Depot 1270540 (empty depot)
+-- addappid(1281820) -- Depot 1281820 (empty depot)
+-- addappid(1284470) -- Depot 1284470 (empty depot)
+-- addappid(1388850) -- Depot 1388850 (empty depot)
+-- addappid(1436950) -- Depot 1436950 (empty depot)
+-- addappid(1478300) -- Depot 1478300 (empty depot)
+-- addappid(2173130) -- Depot 2173130 (empty depot)
+-- addappid(2173131) -- Depot 2173131 (empty depot)
+-- addappid(2222600) -- Depot 2222600 (empty depot)
+-- addappid(2222601) -- Depot 2222601 (empty depot)
+-- addappid(2222602) -- Depot 2222602 (empty depot)
+-- addappid(2222603) -- Depot 2222603 (empty depot)
+-- addappid(2222604) -- Depot 2222604 (empty depot)
+-- addappid(2297840) -- Depot 2297840 (empty depot)
