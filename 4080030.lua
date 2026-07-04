504b0cfc63ffafb7f0603f3c4a9948b221b47d87
qiao-lima <qiao-lima@github.com>
2026-07-04 13:51:52 +0800

    Manual Update 4080030

---
+++ 4080030.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4080030 - The Message from Deep Space
+-- Generated 2026-07-04 05:04:36 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(4080030)
+
+-- Main Depots
+addappid(4080031, 1, "e093f5e30ad405bdecb552151abbc551ce7356c37010809cc7d108e8ec5dfe49") -- (windows)
+setManifestid(4080031, "8123689831487265423", 298838797)
+
+-- Missing Depots (We don't have the keys yet lol): 4080032
