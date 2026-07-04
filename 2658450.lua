9af547670f218f58e33ff6df28e2b3e0a11e4345
qiao-lima <qiao-lima@github.com>
2026-07-04 13:44:10 +0800

    Manual Update 2658450

---
+++ 2658450.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2658450 - LOLLIPOP CHAINSAW RePOP
+-- Generated 2026-07-04 04:40:41 UTC
+-- # Depots (Total/DLC/Shared): 5/2/2
+
+-- Main AppID
+addappid(2658450)
+
+-- Main Depots
+addappid(2658451, 1, "d3c09b507a343a7ba3c75abd0ce562ceb1c9f33c4a5c840a65bf5f245df9d891") -- (windows, 64-bit)
+setManifestid(2658451, "5950658876616737822", 15114058568)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 3021431, 3299481
