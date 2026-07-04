56721fc5462b688ee3bb9516d5e2558574f0b4ce
qiao-lima <qiao-lima@github.com>
2026-07-04 13:44:09 +0800

    Manual Update 1860510

---
+++ 1860510.lua
@@ -0,0 +1,28 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1860510 - Total Conflict: Resistance
+-- Generated 2026-07-04 04:44:32 UTC
+-- # Depots (Total/DLC/Shared): 6/2/3
+
+-- Main AppID
+addappid(1860510, 1, "a815a935bf6125fec3ff1d1609f551efc617a90f2176a90beac941e21f432469")
+
+-- Main Depots
+addappid(1860511, 1, "6da400d94115d37c6474b287e5f8e41e78ec2083d54ff7dffd6d1b3b2dc8dc0d")
+setManifestid(1860511, "4493073028205614615", 34604460755)
+
+-- DLC's (no depot keys required)
+addappid(2628270) -- Eastern Airborne Forces
+addappid(2808880) -- Western Marine Corps
+
+-- DLC's (with depot keys)
+-- Total Conflict: Resistance Soundtrack (AppID: 2336780)
+addappid(2336780)
+addappid(2336781, 1, "ec410f8cfd9d2abc6d1e8c14ef0bffd41ca47c28650b0b56993397b2df9b1ca5")
+setManifestid(2336781, "9006593244154325920", 143546200)
+addappid(2336782, 1, "0bfd522f55d87ce73e94c976716221e18ae23c9f3792e9d2eb861f0f7ac9e7e3")
+setManifestid(2336782, "4032484244244502571", 1175066658)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+addappid(229003, 1, "8e0ded6e1d9481eb286217322054ce7b0b8243daae770f31a30667752b1a3c63") -- (windows)
