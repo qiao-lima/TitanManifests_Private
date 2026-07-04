39ba8c009d4faf2d7ff75ccd28e6340d2d3955e0
qiao-lima <qiao-lima@github.com>
2026-07-04 13:48:00 +0800

    Manual Update 3612060

---
+++ 3612060.lua
@@ -0,0 +1,21 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3612060 - Idle Land of Exile
+-- Generated 2026-07-04 04:25:44 UTC
+-- # Depots (Total/DLC/Shared): 2/1/0
+
+-- Main AppID
+addappid(3612060)
+
+-- Main Depots
+addappid(3612061, 1, "f6c53195645c43fe3c4b3aedcf9e39df07ecd9e73a974191efeecae301ce1442")
+setManifestid(3612061, "305128672568030059", 823514094)
+
+-- DLC's (no depot keys required)
+addappid(4007860) -- Idle Land of Exile-DLC2
+addappid(4007870) -- Idle Land of Exile-DLC3
+
+-- DLC's (with depot keys)
+-- Idle Land of Exile-dlc1 (AppID: 3843680)
+addappid(3843680)
+addappid(3843680, 1, "9569a73189c0cbb5501a3a37b7f865a291e06901ee91a7aaee3cabd7332e8491") -- (windows)
+setManifestid(3843680, "6804860478949237913", 0)
