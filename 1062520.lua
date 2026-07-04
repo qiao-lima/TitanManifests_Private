0c60d61b784fc9f5c53619395fb2e6063cd90e3f
qiao-lima <qiao-lima@github.com>
2026-07-04 13:44:07 +0800

    Manual Update 1062520

---
+++ 1062520.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1062520 - Dinkum
+-- Generated 2026-07-04 04:52:44 UTC
+-- # Depots (Total/DLC/Shared): 2/1/0
+
+-- Main AppID
+addappid(1062520, 1, "6b056ec921aea101e5c6375223c6799153334365e46c20a8da077871c0e1fd07")
+
+-- Main Depots
+addappid(1062521, 1, "ef0a62caf543b86609cfc0a77c6879d1f131a1fdb5cfdda01204f0171b126a56")
+setManifestid(1062521, "860259890814647034", 3171995578)
+
+-- DLC's (with depot keys)
+-- Dinkum Soundtrack (AppID: 2468700)
+addappid(2468700)
+addappid(2468701, 1, "1cf9b49a1c0f002bc8adfa8f0c786ac1be5ac773c330dddf622d7e002a1a62f9")
+setManifestid(2468701, "6496351598538545887", 1810318780)
