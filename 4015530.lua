33bff4a2b93b5ea3bf6fad395f55ddff6e3d729c
qiao-lima <qiao-lima@github.com>
2026-07-04 13:51:52 +0800

    Manual Update 4015530

---
+++ 4015530.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4015530 - MyVoiceZoo
+-- Generated 2026-07-04 04:56:32 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4015530, 1, "644aab77c29d20cf5d39961df7ebd3b0d77fbb165b6332320f2412071703ec27")
+
+-- Main Depots
+addappid(4015531, 1, "b560c7cd9497b0a08b869cd05ee3314e848c16967d2389b02efb45d57a112ff4")
+setManifestid(4015531, "1584372515069674227", 322810822)
