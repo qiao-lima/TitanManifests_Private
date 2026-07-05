20cb0f5f7226b70c7c1c1d81de509a977c42be54
qiao-lima <qiao-lima@github.com>
2026-07-05 12:40:34 +0800

    Manual Update 3385970

---
+++ 3385970.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3385970 - Beyond Sandbox
+-- Generated 2026-07-05 04:03:41 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(3385970, 1, "d8753bd436c09e3649ce975995d50715623ad6caab4e4eb4c58c377579e927ff")
+
+-- Main Depots
+addappid(3385971, 1, "b991030ebb6bd7b90ed0b0cb47f1407e281a608a8e7cd4c38d8ccb89dfb4949b")
+setManifestid(3385971, "441238769239822807", 12922261889)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
