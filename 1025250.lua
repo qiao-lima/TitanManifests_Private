f5a38d8d56bd6ab949769f331f02a676b579dccd
qiao-lima <qiao-lima@github.com>
2026-07-05 21:38:54 +0800

    Manual Update 1025250

---
+++ 1025250.lua
@@ -0,0 +1,21 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1025250 - Shadow Corridor
+-- Generated 2026-07-05 13:31:15 UTC
+-- # Depots (Total/DLC/Shared): 4/1/2
+
+-- Main AppID
+addappid(1025250)
+
+-- Main Depots
+addappid(1025251, 1, "dab6700339bc4b40a97556b01afc93b4b021494cddd77234b53f23b543a1ae78") -- (windows)
+setManifestid(1025251, "8075260940789424164", 1455755981)
+
+-- DLC's (with depot keys)
+-- Shadow Corridor - DLC (AppID: 1363880)
+addappid(1363880)
+addappid(1363880, 1, "321a5e727c1e68255d2b601008f27fa224b20300afc2275cb2c0d1c373d2e1c8")
+setManifestid(1363880, "8284349631726717145", 2378887)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
