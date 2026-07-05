be1a3ae4932a00553718622facb4ff19e3c35a5e
qiao-lima <qiao-lima@github.com>
2026-07-05 12:40:33 +0800

    Manual Update 2266290

---
+++ 2266290.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2266290 - Portal: Revolution Soundtrack
+-- Generated 2026-07-05 04:05:25 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(2266290)
+
+-- Main Depots
+addappid(2266291, 1, "a5a4dc34b098a0454ceac30ef10cbccbead228ef86ecb8b91d8522ded290b0e4")
+setManifestid(2266291, "3008365178465588858", 207730755)
+addappid(2266292, 1, "807f3fc22065c25a5e920a1c43c99800b02699f08e3d62e28701a2bd4c98c58f")
+setManifestid(2266292, "277073121405821802", 1173390800)
