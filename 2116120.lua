1978fb459e515d15fd929493b1131b00c921c2ad
qiao-lima <qiao-lima@github.com>
2026-07-05 21:38:55 +0800

    Manual Update 2116120

---
+++ 2116120.lua
@@ -0,0 +1,20 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2116120 - Incursion Red River
+-- Generated 2026-07-05 13:35:58 UTC
+-- # Depots (Total/DLC/Shared): 4/1/2
+
+-- Main AppID
+addappid(2116120, 1, "e8d8de54516b28bf4d096a77c70aa05add05a82eea917680858a79e1f3c04f53")
+
+-- Main Depots
+addappid(2116121, 1, "8f80a1590d1be6bfdde9c800499b420fe786838c27c69a6572bbb75c85c48007")
+setManifestid(2116121, "6672703203196539514", 75225524846)
+
+-- DLC's (with depot keys)
+-- Incursion Red River - Supporter Edition (AppID: 3405150)
+addappid(3405150)
+addappid(3405150, 1, "66466595306f5647e00fca5435234afc25e2a2f757c1ab2d550917767e80b71e")
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
