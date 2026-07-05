db856b21f6425d36235c61ab2d314a1134d06071
qiao-lima <qiao-lima@github.com>
2026-07-05 12:40:35 +0800

    Manual Update 3949040

---
+++ 3949040.lua
@@ -0,0 +1,23 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3949040 - RV There Yet?
+-- Generated 2026-07-05 04:12:57 UTC
+-- # Depots (Total/DLC/Shared): 5/2/2
+
+-- Main AppID
+addappid(3949040, 1, "4af18553dbd752cd4d8084f32b7cdb37d59b1cc4d946360519d03546911b7e9c")
+
+-- Main Depots
+addappid(3949041, 1, "98435e5f712a11c88e455130e718254b2745a33f7443b2034d634f54af0da031") -- (windows, 64-bit)
+setManifestid(3949041, "6388475935469519170", 2222971680)
+
+-- DLC's (with depot keys)
+-- RV There Yet? Soundtrack (AppID: 4212020)
+addappid(4212020)
+addappid(4212021, 1, "aa5222c833e4ae6cbf7a9cab80926f95125f4c005337fd33dee2a2f91e9ceb69")
+setManifestid(4212021, "8261571854280755475", 83929806)
+addappid(4212022, 1, "60df2d1ec00236333c86265da09e26667eccbd14af21c64354bfbb9c1d8740b5")
+setManifestid(4212022, "8859804068456999265", 574578826)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
