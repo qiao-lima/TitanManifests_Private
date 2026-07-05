e6d6ff1831765dc2a536b232ec7153ddec7aa58e
qiao-lima <qiao-lima@github.com>
2026-07-05 12:40:32 +0800

    Manual Update 1436640

---
+++ 1436640.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1436640 - 6-7
+-- Generated 2026-07-05 04:36:27 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(1436640)
+
+-- Main Depots
+addappid(1436642, 1, "bbeda45638d039f59c03e04209e769f48e90a1b4fccd3feee641fe7d09f0af0f") -- (windows, 64-bit)
+setManifestid(1436642, "8959968169776803377", 214596838)
