52c6428ce5f924a1b7e8b14f807c42ecbf73d246
qiao-lima <qiao-lima@github.com>
2026-07-05 21:38:55 +0800

    Manual Update 2157830

---
+++ 2157830.lua
@@ -0,0 +1,22 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2157830 - John Carpenter's Toxic Commando
+-- Generated 2026-07-05 13:31:31 UTC
+-- # Depots (Total/DLC/Shared): 4/2/1
+
+-- Main AppID
+addappid(2157830, 1, "323fee23e77e9fc177fae952a0ebf63f1764b48b94e8a45270e21e47bb00f01f")
+
+-- Main Depots
+addappid(2157831, 1, "3198b1f3831fde3e4f41577b2f01b20595ff7961ac9a679f856809d3219d0ef3")
+setManifestid(2157831, "6123300899332003446", 63907034443)
+
+-- DLC's (no depot keys required)
+addappid(3933860) -- John Carpenter's Toxic Commando - Leon's Secret Stash
+addappid(3933870) -- John Carpenter's Toxic Commando - Cosmetic Pack: Bloody Pass  
+addappid(3933880) -- John Carpenter's Toxic Commando - Bloody Pass 
+addappid(4148650) -- John Carpenter's Toxic Commando - Cosmetic Pack 1
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 4399811, 4399812
