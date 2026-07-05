16b55e2858d155b13238cf142718877029f63fa5
qiao-lima <qiao-lima@github.com>
2026-07-05 12:40:34 +0800

    Manual Update 3179350

---
+++ 3179350.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3179350 - Long Drive North
+-- Generated 2026-07-05 04:28:48 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3179350)
+
+-- Main Depots
+addappid(3179351, 1, "2cce8c2c19a27072f890b94b34692bdf66c9791c38871cf65cf06d5d4dcdce34")
+setManifestid(3179351, "4887201373764846774", 6077300019)
