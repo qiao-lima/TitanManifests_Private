79d10038f1511855a2bdae946f7c85817fb2bc33
qiao-lima <qiao-lima@github.com>
2026-07-05 21:38:58 +0800

    Manual Update 4254300

---
+++ 4254300.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4254300 - Swords And Magic: Path Of Conquest
+-- Generated 2026-07-05 13:24:51 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4254300)
+
+-- Main Depots
+addappid(4254301, 1, "4239c1c011cccb33a94c4d1a8619ef664d6a06b851d1ba5a3eb4274668cb79a4") -- (windows)
+setManifestid(4254301, "2823248545495428915", 14485579111)
