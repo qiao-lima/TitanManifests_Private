5d182d65c44490319ea54e3acd39055f28f98b57
qiao-lima <qiao-lima@github.com>
2026-07-05 12:40:31 +0800

    Manual Update 1250410

---
+++ 1250410.lua
@@ -0,0 +1,16 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1250410 - Microsoft Flight Simulator (2020)
+-- Generated 2026-07-05 04:37:16 UTC
+-- # Depots (Total/DLC/Shared): 2/0/1
+
+-- Main AppID
+addappid(1250410, 1, "8a8884d351f799545f675d56310da7e1749995d652576641056ee35cba44fa4f")
+
+-- Main Depots
+addappid(1250411, 1, "dbf9d5a5cae7eab44d452bfe53856e4b08b448ac7c224124316ca8cf3f91b3a8")
+setManifestid(1250411, "4980418302200182124", 2131626194)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+
+-- Token-locked DLCs (couldn't read depots): 1265910, 1265911
