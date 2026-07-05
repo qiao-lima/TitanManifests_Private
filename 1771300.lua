23175dec3b7651baf0c8b8ce4b57218f7f69c339
qiao-lima <qiao-lima@github.com>
2026-07-05 12:40:32 +0800

    Manual Update 1771300

---
+++ 1771300.lua
@@ -0,0 +1,57 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1771300 - Kingdom Come: Deliverance II
+-- Generated 2026-07-05 03:37:27 UTC
+-- # Depots (Total/DLC/Shared): 16/7/1
+
+-- Main AppID
+addappid(1771300, 1, "3caed047b77bd7be8e68f818a3a6729b75371e0f67c871728c7a19de18aa6f53")
+
+-- Main Depots
+addappid(1771302, 1, "41bc41c39ce79db318e98b533bab1fe71bfd294c4e8c90f90e4f8efa5fcc2a91") -- (windows)
+setManifestid(1771302, "257127247679847518", 199419496)
+addappid(1771303, 1, "397ab2e5c59bbb75f716c30dba3374be8af7ff49a2fc45c621e0bc23bafbd5f3") -- (windows)
+setManifestid(1771303, "1999516201078544527", 82572274727)
+addappid(1771305, 1, "66d5a42e0171363eac9b79352c78401952d616880979d26967c385d2d7a88368") -- Czech (windows)
+setManifestid(1771305, "877277304945139034", 15252544604)
+addappid(1771306, 1, "120aed0e767afa1ac85539240704d613b82c13473fcd7136d3cea555a784783e") -- English (windows)
+setManifestid(1771306, "3329997247863059841", 13650395848)
+addappid(1771307, 1, "8d47328b0b82d9b9ba25bfb199c69ea2dd005954298bd2aff2b448321569b727") -- French (windows)
+setManifestid(1771307, "1779956914792969310", 11885811406)
+addappid(1771308, 1, "b457fdf0e78f3465b9ccbfed5f8ecbb85f22428ba321534ee0df4a0d08754d02") -- German (windows)
+setManifestid(1771308, "7843156561372221832", 12908911612)
+addappid(1771309, 1, "1f8b55f9f4ac27eac8ffa310ca46163ee5aaad0937438035274a30b78de4e81a") -- Japanese (windows)
+setManifestid(1771309, "8473341515705503999", 12308779384)
+addappid(3118101, 1, "69d3aecd16754fea50cf46182e56be5d29647e571e45c1dd1ef3294b966d0ee4") -- Spanish (windows)
+setManifestid(3118101, "4032169301022912555", 12543419636)
+
+-- DLC's (no depot keys required)
+addappid(3118100) -- Kingdom Come: Deliverance II The Lion's Crest
+addappid(3118110) -- Kingdom Come: Deliverance II Gallant Huntsman's Kit
+addappid(3118120) -- Kingdom Come: Deliverance II Shields of Seasons Passing
+addappid(3119920) -- Kingdom Come: Deliverance II Expansion Pass
+addappid(3368600) -- Kingdom Come: Deliverance II Brushes with Death
+addappid(3368610) -- Kingdom Come: Deliverance II Legacy of the Forge
+addappid(3368620) -- Kingdom Come: Deliverance II Mysteria Ecclesiae
+
+-- DLC's (with depot keys)
+-- Kingdom Come: Deliverance II Soundtrack (AppID: 3468550)
+addappid(3468550)
+addappid(3468551, 1, "25b35bae1bb9e7a4cce9c03f6de8e02f15249954fc825b32b3d36db9844e04f2")
+setManifestid(3468551, "6872057523820499398", 200323678)
+addappid(3468552, 1, "1af88e054ff8a58910611d28559791d4cf5f49bf9f51d60536284f2ce725d341")
+setManifestid(3468552, "5959204188281972405", 925786496)
+-- Kingdom Come: Deliverance II Artbook (AppID: 4075290)
+addappid(4075290)
+addappid(4075290, 1, "5719ab17163fb31ff02aedc90c95c0b06a0a0d66b687706cbef92a539a773b1b")
+setManifestid(4075290, "7365651604166421259", 355819526)
+-- Kingdom Come: Deliverance II Extended Official Soundtrack (AppID: 4184930)
+addappid(4184930)
+addappid(4184931, 1, "80fc59ddf0f2637edab1af14b4f50e33cbf8e215f3923185de8d987df8fcd8bc")
+setManifestid(4184931, "1485580813939646155", 871330963)
+addappid(4184932, 1, "edbca866c82878c859fafffc256d4804fac8e7e9fcca9b7e4f10b7e7c182bc79")
+setManifestid(4184932, "5938459508071642275", 6503655601)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 1771301, 1771304
