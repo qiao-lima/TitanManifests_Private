acbcb5b30e38df46403b641433d7019321f56f09
qiao-lima <qiao-lima@github.com>
2026-07-05 21:38:56 +0800

    Manual Update 2821110

---
+++ 2821110.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2821110 - Subversive Memories
+-- Generated 2026-07-05 13:27:23 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2821110)
+
+-- Main Depots
+addappid(2821111, 1, "21b99521590aace181b6f13094c0e947e3f187d654db815d264c7870ec8136e1")
+setManifestid(2821111, "4688343732376480484", 1637145256)
