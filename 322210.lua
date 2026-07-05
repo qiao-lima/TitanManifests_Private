f2fa84c6dbc56fc0162925ca0d1ba77ce0ef4a61
qiao-lima <qiao-lima@github.com>
2026-07-05 12:40:34 +0800

    Manual Update 322210

---
+++ 322210.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 322210 - Guns, Gore & Cannoli
+-- Generated 2026-07-05 03:10:49 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(322210, 1, "db3c9f7c797adbd936647cf2268b92d4d7189b57e54cacbee4d1866b2cf3efb9")
+
+-- Main Depots
+addappid(322212, 1, "6cc2551f8d640dcef22e3b8e42ef73ecc99eab7746cef840452c81402a803885") -- (windows)
+setManifestid(322212, "5203239241664335591", 2852808384)
+addappid(322213, 1, "6ae067adc26cb651c142b5d607c4133a8421286b4a97bb1924d0d6549087b34c") -- (macos)
+setManifestid(322213, "7183844994724458647", 2931304676)
