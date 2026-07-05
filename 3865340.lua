c3ff8b061fa6a4acb56eff24847a83ff73a9be8d
qiao-lima <qiao-lima@github.com>
2026-07-05 21:38:57 +0800

    Manual Update 3865340

---
+++ 3865340.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3865340 - Deadcam
+-- Generated 2026-07-05 13:30:44 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(3865340)
+
+-- Main Depots
+addappid(3865341, 1, "9065431c279d043f6a958e7c72e60dcd882d05694e55975a57ce333ef069d69f")
+setManifestid(3865341, "2377654223159683104", 3216988637)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
