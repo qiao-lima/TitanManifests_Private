e2b5f3216dbd8a81b882dd214ab5dbd8d207858f
qiao-lima <qiao-lima@github.com>
2026-07-05 21:38:56 +0800

    Manual Update 2667480

---
+++ 2667480.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2667480 - Wolf  Sheep  Cabbage
+-- Generated 2026-07-05 13:36:06 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2667480)
+
+-- Main Depots
+addappid(2667481, 1, "46026a913d3d5f5aa4f398dc5443f80e4412d682865c7c9cea16c9ce5e6163b3") -- (windows)
+setManifestid(2667481, "4213003498591740722", 313949138)
