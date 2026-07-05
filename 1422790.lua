e7bbc27a61d18ba21e63e7c06879284df4bba6fa
qiao-lima <qiao-lima@github.com>
2026-07-05 12:40:31 +0800

    Manual Update 1422790

---
+++ 1422790.lua
@@ -0,0 +1,16 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1422790 - 门 The Door
+-- Generated 2026-07-05 03:59:18 UTC
+-- # Depots (Total/DLC/Shared): 4/0/3
+
+-- Main AppID
+addappid(1422790)
+
+-- Main Depots
+addappid(1422791, 1, "352f1b19b84cfaf9664fa39c95e608d2be91c66bb686678e9deec0d6a5e09de9") -- (windows, 64-bit)
+setManifestid(1422791, "6437803173212736878", 8953346417)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- (windows)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
