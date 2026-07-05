cc6c12ef3403efb63fd50798438e8541a03cb9a7
qiao-lima <qiao-lima@github.com>
2026-07-05 21:38:55 +0800

    Manual Update 1441550

---
+++ 1441550.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1441550 - Regular Human Workshop
+-- Generated 2026-07-05 13:34:38 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(1441550, 1, "de5b68602ceb25dc9a5a12ff4d806db45fecb5151f248553b92713d0087ca83c")
+
+-- Main Depots
+addappid(1441551, 1, "5285ca2ce89f0405ff79c94e294b47f77700d1b1d1db3f62e463ee5836f0006a") -- (windows)
+setManifestid(1441551, "7824262771412212505", 661484575)
