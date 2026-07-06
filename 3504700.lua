1476f0f03f807cc6281b2e09617fdb113328f5a8
qiao-lima <qiao-lima@github.com>
2026-07-06 22:10:46 +0800

    Manual Update 3504700

---
+++ 3504700.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3504700 - 4Wheel Challenge (Wheelchair Simulator)
+-- Generated 2026-07-06 13:19:52 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3504700)
+
+-- Main Depots
+addappid(3504701, 1, "71bb8381920b2c0c1df20a19ba5ad8afd3921df2073b3feda3ae8da0f7b500bc") -- (windows, 64-bit)
+setManifestid(3504701, "2237636480791367461", 1077335282)
+
+-- DLC's (no depot keys required)
+addappid(4570550) -- 4Wheel Challenge - ZRogueLike: The Last Wheelchair User
+addappid(4867090) -- 4Wheel Challenge - Only Up
