7a731e853f0cc5fc4a29bb9437dd0dbc70a8885a
qiao-lima <qiao-lima@github.com>
2026-07-06 22:10:43 +0800

    Manual Update 1084160

---
+++ 1084160.lua
@@ -0,0 +1,25 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1084160 - Jagged Alliance 3
+-- Generated 2026-07-06 13:31:55 UTC
+-- # Depots (Total/DLC/Shared): 6/2/2
+
+-- Main AppID
+addappid(1084160, 1, "e612678221b99a3e1c7d3b8cfd2dcf4550d8c51d13703240f4fa26ea7877c562")
+
+-- Main Depots
+addappid(1084161, 1, "4f94d949152ea26f9a3c60c1a8dd75fc4ac15d586bc1fc97ce67745a3be5faee")
+setManifestid(1084161, "1864261675204967035", 16303005299)
+addappid(1084162, 1, "1c73c3e64d01f3e0dd21767f188b872224a190a6a9c295bd430b8fbcbe8771cc") -- (windows)
+setManifestid(1084162, "5237382165839002838", 323192016)
+
+-- DLC's (with depot keys)
+-- Jagged Alliance 3 Soundtrack (AppID: 2469350)
+addappid(2469350)
+addappid(2469351, 1, "1a9a97d3a89e8c99dffc84f57b9cd7ab0cd01c557c3a51477ffd19d1a199d9e6")
+setManifestid(2469351, "1142654528176957091", 1253884054)
+addappid(2469352, 1, "276785045e18947700b587a2bd3f99a031c3402101db4f9e7f208270049af7dc")
+setManifestid(2469352, "7453962181113467881", 3740373418)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
