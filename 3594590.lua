fec2c557978e9e8f4893d3d41f1abea46be61ca6
qiao-lima <qiao-lima@github.com>
2026-07-06 22:10:47 +0800

    Manual Update 3594590

---
+++ 3594590.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3594590 - 异能重组
+-- Generated 2026-07-06 13:20:53 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3594590)
+
+-- Main Depots
+addappid(3594591, 1, "4c6d881c995bc41a1c69b548ca12acb07f4037ef0ebe3c34b05fbcd75323bc56")
+setManifestid(3594591, "8978471075729407164", 785780389)
