07402f416213f67197a6b32431c60a724827a2e2
qiao-lima <qiao-lima@github.com>
2026-07-06 22:10:46 +0800

    Manual Update 3552790

---
+++ 3552790.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3552790 - Roxanne
+-- Generated 2026-07-06 13:20:06 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3552790)
+
+-- Main Depots
+addappid(3552791, 1, "c7fcbb1dfff408a04a93a5be1dc56170f6ca13d26d4d7bbc13e6bc665b741535")
+setManifestid(3552791, "7757793573355197687", 9975675219)
