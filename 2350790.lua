f218c7e6ba11b3fd7e338fcacfdb0a67ac86a924
qiao-lima <qiao-lima@github.com>
2026-07-06 22:10:44 +0800

    Manual Update 2350790

---
+++ 2350790.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2350790 - Moonlighter 2: The Endless Vault
+-- Generated 2026-07-06 13:46:03 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2350790)
+
+-- Main Depots
+addappid(2350792, 1, "794eca19bb31db7e79bbc69f303feb210c8e3efca02f89ff7d38d9e0fed8705f") -- (windows, 64-bit)
+setManifestid(2350792, "8827496452742536676", 6725228287)
