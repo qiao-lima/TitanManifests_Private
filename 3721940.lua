c9daa8577e9c0dd005572f58a167d0d2e29f3289
qiao-lima <qiao-lima@github.com>
2026-07-06 22:17:43 +0800

    Manual Update 3721940

---
+++ 3721940.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3721940 - Undercity of Sin
+-- Generated 2026-07-06 13:43:45 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(3721940)
+
+-- Main Depots
+addappid(3721941, 1, "d24ee73f49887dbe3eb8cbc0b1187086b55fd587df7f94e6d6af7009cc42cdff")
+setManifestid(3721941, "3472876156740552024", 1377222556)
+
+-- Missing Depots (We don't have the keys yet lol): 3721942, 3721943
