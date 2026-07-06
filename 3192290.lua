d925c7a9a80a280ecf85dfa3d3e87dc6ce656b5d
qiao-lima <qiao-lima@github.com>
2026-07-06 22:10:45 +0800

    Manual Update 3192290

---
+++ 3192290.lua
@@ -0,0 +1,19 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3192290 - Midnight Exhibitionist DX Edition
+-- Generated 2026-07-06 13:51:09 UTC
+-- # Depots (Total/DLC/Shared): 5/0/0
+
+-- Main AppID
+addappid(3192290)
+
+-- Main Depots
+addappid(3192291, 1, "4b508f1352ea3b71d3e474eac6cd5f9d2c72ba8a3c3195efc4af6efa276d2180") -- Japanese (windows)
+setManifestid(3192291, "1475815033700320543", 1791060827)
+addappid(3192292, 1, "4ee9728fbd15d082eb82125297726761f5dc38f1b7475af0ba97ff416bc876b4") -- (windows)
+setManifestid(3192292, "2526001520438139051", 1832903042)
+addappid(3192293, 1, "7b7d8bd84f83820cd1b29cfc9c8344a4f63cb69ad056463a7d25a5c728120e6b") -- Tchinese (windows)
+setManifestid(3192293, "5860802128929425260", 1833137718)
+addappid(3192294, 1, "3a04eccbe521998fab5e2be525a9ac10eb8df4d73ec4f6e8b841233b45f60971") -- Schinese (windows)
+setManifestid(3192294, "3511654312302072801", 1833117091)
+
+-- Missing Depots (We don't have the keys yet lol): 3192295
