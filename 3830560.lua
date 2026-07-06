6acbdc2f7e361d3db886c8a37578d9798ef390a8
qiao-lima <qiao-lima@github.com>
2026-07-06 22:17:44 +0800

    Manual Update 3830560

---
+++ 3830560.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3830560 - AMANATSU+
+-- Generated 2026-07-06 13:24:15 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3830560)
+
+-- Main Depots
+addappid(3830561, 1, "b7eacfe8f5d3899e4707cdabe74389a525b75fb4020d953f14386f62685aad00")
+setManifestid(3830561, "7208159532409975830", 1838717614)
