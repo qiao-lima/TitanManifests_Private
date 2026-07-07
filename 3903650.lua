2817408c44d3b2902be8db69897a9f223882ff85
qiao-lima <qiao-lima@github.com>
2026-07-07 17:30:18 +0800

    Manual Update 3903650

---
+++ 3903650.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3903650 - FROST x HIME
+-- Generated 2026-07-07 09:00:15 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3903650)
+
+-- Main Depots
+addappid(3903651, 1, "3523e5ebc77afd646fe13dbca236865631d338e7e0f1d103e5ae9d9c17ba09d5")
+setManifestid(3903651, "4459105085684479178", 11089131905)
