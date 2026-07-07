8c76cf8ac0c3e30f2749bacc633b468f7f471c9e
qiao-lima <qiao-lima@github.com>
2026-07-07 17:30:15 +0800

    Manual Update 2981220

---
+++ 2981220.lua
@@ -0,0 +1,14 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2981220 - Forefront
+-- Generated 2026-07-07 08:50:42 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2981220, 1, "87ff1cf561adf0628a34bf164f0eac0fb0250d730d29fe3954f0b2614947876a")
+
+-- Main Depots
+addappid(2981221, 1, "615720e40e46de4a13f9c958eefadd591c5e4cf30dfe3c9e7cf26c70b94d6280")
+setManifestid(2981221, "4320025188271081341", 17431229435)
+
+-- DLC's (no depot keys required)
+addappid(4224720) -- Supporter Pack
