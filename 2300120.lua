2935bf73df60465daeab5b8490f7cbfbac5d289a
qiao-lima <qiao-lima@github.com>
2026-07-07 17:30:13 +0800

    Manual Update 2300120

---
+++ 2300120.lua
@@ -0,0 +1,22 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2300120 - Painkiller
+-- Generated 2026-07-07 09:20:18 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(2300120)
+
+-- Main Depots
+addappid(2300122, 1, "564f6b9b6546324d5955cca4017995913ecaa47e114dc895936fa767f514a829")
+setManifestid(2300122, "513417452571467674", 28517039229)
+
+-- DLC's (no depot keys required)
+addappid(3751350) -- Painkiller: Iron Crusade Pack
+addappid(3751360) -- Painkiller: Night Watch Pack
+addappid(3751370) -- Painkiller: Metal As Hell Pack
+addappid(3751380) -- Painkiller: Demon Awakening Pack
+addappid(3751400) -- Painkiller: Season Pass
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
