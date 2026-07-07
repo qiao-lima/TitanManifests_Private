21ea845dbf943a2b7b483fc2199d86a5a82fd3fd
qiao-lima <qiao-lima@github.com>
2026-07-07 17:30:16 +0800

    Manual Update 3289580

---
+++ 3289580.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3289580 - Winter ~Life in the Countryside~
+-- Generated 2026-07-07 08:56:32 UTC
+-- # Depots (Total/DLC/Shared): 4/0/0
+
+-- Main AppID
+addappid(3289580)
+
+-- Main Depots
+addappid(3289581, 1, "a5435012d9b4011281fbd9e9208e14af1593ad7f2670fe552875f5ebba8ebb51")
+setManifestid(3289581, "7300301600031974268", 841942485)
+addappid(3289582, 1, "52d62701fc2c1f1aac3a997161d1edda60a964dab4dd50037be116eb2075535d") -- Japanese
+setManifestid(3289582, "5443067978687044579", 840780640)
+addappid(3289583, 1, "a9d6f948d48b1091bde6ac5a56bea3529a9acea7158a920598fdfaf0196851ef") -- Russian
+setManifestid(3289583, "4948486293443453738", 852859109)
+addappid(3289584, 1, "2133e55c9979f7b89c2e094208541126f43efee6797c25d7250f2b27c09df632") -- Thai
+setManifestid(3289584, "7619739256679091883", 855125495)
