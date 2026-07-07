fbdee48a158c2c723850d7856ff09bdca81ae17a
qiao-lima <qiao-lima@github.com>
2026-07-07 17:30:18 +0800

    Manual Update 3789950

---
+++ 3789950.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3789950 - 转生Galgame成为女主角！
+-- Generated 2026-07-07 08:51:31 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(3789950)
+
+-- Main Depots
+addappid(3789952, 1, "2fd4ce74eee25d1fb0c8f5814167254ec1d7353783dd55b36a13ea83289790b9") -- Schinese (windows)
+setManifestid(3789952, "4097184760086967932", 921845592)
+
+-- Missing Depots (We don't have the keys yet lol): 3789951
