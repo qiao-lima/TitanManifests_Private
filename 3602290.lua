04f7c216b2dbe25198c4e63df21d74093dcb1bcf
qiao-lima <qiao-lima@github.com>
2026-07-07 17:30:17 +0800

    Manual Update 3602290

---
+++ 3602290.lua
@@ -0,0 +1,40 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3602290 - FEMBOY FUTA HOUSE
+-- Generated 2026-07-07 09:17:13 UTC
+-- # Depots (Total/DLC/Shared): 6/5/0
+
+-- Main AppID
+addappid(3602290, 1, "d798833008ca2ae8e3f1850f3e98e2cd6b8b16196c1d4c389badbce9bc0fe50e")
+
+-- Main Depots
+addappid(3602291, 1, "902bbf5f342cf5125a99fb40b231479b9547a9f597d3c915f9c91f72d27b875d") -- (windows)
+setManifestid(3602291, "3180391892144815688", 5275846532)
+
+-- DLC's (no depot keys required)
+addappid(3963430) -- FEMBOY FUTA HOUSE - Hot Extra Costumes
+addappid(3963440) -- FEMBOY FUTA HOUSE - Dirty Election
+addappid(4131790) -- FEMBOY FUTA HOUSE - Kinky Field Trip
+
+-- DLC's (with depot keys)
+-- FEMBOY FUTA HOUSE - Digital Artbook (AppID: 3963390)
+addappid(3963390)
+addappid(3963390, 1, "1d4ff18e36cb5855151cb3e52a33f0717b51a4d7a26d5e72c97de74292263eec")
+setManifestid(3963390, "3275917072001749456", 20909889)
+-- FEMBOY FUTA HOUSE - Soundtrack (AppID: 3963400)
+addappid(3963400)
+addappid(3963401, 1, "3ffc7b607cd75d95d3f06937ae1c57fd053812a8fdf613065def5a336aa58e11")
+setManifestid(3963401, "1015105784504543654", 112574460)
+-- FEMBOY FUTA HOUSE - Wallpapers Pack (AppID: 3963410)
+addappid(3963410)
+addappid(3963411, 1, "81712a9b58ab7f48580fe1c40a380c9089f22250d34cd694a7e3241599a3a55f")
+setManifestid(3963411, "4091445764721133555", 239791891)
+-- FEMBOY FUTA HOUSE - Animations Pack (AppID: 3963420)
+addappid(3963420)
+addappid(3963420, 1, "70f8d950f1e77bd927f59391cd61634caf6bfb04632b34c6b0f7ad5341db11c5")
+setManifestid(3963420, "81016670117772740", 971794459)
+-- FEMBOY FUTA HOUSE - Comics Vol.1 "Pineapple Trigger" (AppID: 4669020)
+addappid(4669020)
+addappid(4669021, 1, "f0619469526ef0dcf330d1a2e74796ccdbb61cf99344cf4d2c6d9ad1e05457da")
+setManifestid(4669021, "835011936454345467", 200628583)
+
+-- Token-locked DLCs (couldn't read depots): 4669010, 4834770
