650f17dbf4e02b9f66785fb75e9bca50c10de147
qiao-lima <qiao-lima@github.com>
2026-07-07 17:30:16 +0800

    Manual Update 3274580

---
+++ 3274580.lua
@@ -0,0 +1,38 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3274580 - Anno 117: Pax Romana
+-- Generated 2026-07-07 08:59:09 UTC
+-- # Depots (Total/DLC/Shared): 6/0/1
+
+-- Main AppID
+addappid(3274580, 1, "353c6ea14a59554350687fbc679379bf5094473ae8792b1324bb55536ae4d88e")
+
+-- Main Depots
+addappid(3274581, 1, "29f1b2f2b84b5981fe5e3ebadc14b925e747eea87b6556a95001d576acb9beb9")
+setManifestid(3274581, "2432904706852903349", 129324457709)
+addappid(3274582, 1, "f04b6ff10311fb1d95b43528e4681da8a1b615a31d69275af308d77f93602a4f")
+setManifestid(3274582, "880113201529822801", 482671963)
+addappid(3274583, 1, "dc97a69d99aeaea85bf8ec545b63e9a140606870c2f17f9fec7967c463060618")
+setManifestid(3274583, "1933490330341913902", 520142192)
+addappid(3274584, 1, "e8679d4318f2f44fc44ff183048c09321b4b84000680a1675fe1dd5463d2f24e")
+setManifestid(3274584, "3024077815389047676", 535050425)
+addappid(3274585, 1, "b08a4e3e71782afe4166e047a91bed2a236df33c2b0d0fe02a7ca3dba1a15912")
+setManifestid(3274585, "5684220175990669359", 743491839)
+
+-- DLC's (no depot keys required)
+addappid(3749050) -- Anno 117: Pax Romana – Year 1 Pass
+addappid(3749060) -- Anno 117: Pax Romana – Year 1 Pass - Ubisoft activation
+addappid(3749080) -- Anno 117: Pax Romana Standard Edition - Ubisoft Activation
+addappid(3749090) -- Anno 117: Pax Romana Gold Edition - Ubisoft Activation
+addappid(3858750) -- Anno 117: Pax Romana – Prophecies of Ash
+addappid(3858760) -- Anno 117: Pax Romana – Prophecies of Ash Ubisoft Activation
+addappid(3874230) -- Anno 117 - Marvellous Mosaic Pack
+addappid(3874240) -- Anno 117 - CDLC 01 Ubisoft Activation
+addappid(3874260) -- Blooming Cities Pack
+addappid(3874270) -- Blooming Cities Pack Ubisoft Activation
+addappid(3875910) -- Anno 117 - Closed Beta Ubisoft Activation
+addappid(4168120) -- Anno 117: Pax Romana Standard Edition Launch - Ubisoft Activation
+addappid(4168130) -- Anno 117: Pax Romana Gold Edition Launch - Ubisoft Activation
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
+setManifestid(1716751, "8936186649444731600", 264933784)
