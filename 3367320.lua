fd81883f235f5ddbcbdeca18515a498ee24eb797
qiao-lima <qiao-lima@github.com>
2026-07-07 17:30:16 +0800

    Manual Update 3367320

---
+++ 3367320.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3367320 - Bessie's Milk Delivery
+-- Generated 2026-07-07 09:10:36 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(3367320)
+
+-- Main Depots
+addappid(3367322, 1, "bebd983adfdfb79f1e0ede40f0b90fe19d5b8442b02e443a668439c342bd0502") -- English (windows)
+setManifestid(3367322, "5969072291284881950", 731296323)
+
+-- Missing Depots (We don't have the keys yet lol): 3367321, 3367323
