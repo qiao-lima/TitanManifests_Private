fe5bb8510250e1249873a47d49f200410e4e7b41
qiao-lima <qiao-lima@github.com>
2026-07-08 09:36:25 +0800

    Manual Update 1313140

---
+++ 1313140.lua
@@ -0,0 +1,58 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1313140 - Cult of the Lamb
+-- Generated 2026-07-08 00:40:27 UTC
+-- # Depots (Total/DLC/Shared): 6/4/0
+
+-- Main AppID
+addappid(1313140, 1, "27732d161d8388b7c13dd209b6ae7fca2ea73343ba541da90800c6bf521a50c3")
+
+-- Main Depots
+addappid(1313141, 1, "e7826851aae21726cd847e809a97378d381ba16449186ce1cdb57c572d703460") -- (windows)
+setManifestid(1313141, "8370169344031311791", 3696358303)
+addappid(1313142, 1, "242e4da3afdadce6aa2329f9f1c8c71fe09c06f51a80c105a444734a12c9e3f2") -- (macos)
+setManifestid(1313142, "8076812420343388316", 3709242156)
+
+-- DLC's (no depot keys required)
+addappid(1944680) -- Cult of the Lamb Plush Bonus
+addappid(2013550) -- Cult of the Lamb Pre-Order Bonus
+addappid(2015880) -- Cult of the Lamb: Cultist Pack
+addappid(2071370) -- Cult of the Lamb - Frankerz Twitch Drop
+addappid(2090900) -- Cult of the Lamb - Ralpherz Twitch Drop
+addappid(2090901) -- Cult of the Lamb - Pogchamp Twitch Drop
+addappid(2202620) -- Cult of the Lamb - Penguin Twitch Drop
+addappid(2202621) -- Cult of the Lamb - Lion Twitch Drop
+addappid(2202622) -- Cult of the Lamb - Kiwi Twitch Drop
+addappid(2202623) -- Cult of the Lamb - Pelican Twitch Drop
+addappid(2202624) -- Cult of the Lamb - Glommer Statue Twitch Drop
+addappid(2202625) -- Cult of the Lamb - Beefalo Skeleton Twitch Drop
+addappid(2202626) -- Cult of the Lamb PAX East 2023 Bonus
+addappid(2331540) -- Cult of the Lamb: Heretic Pack
+addappid(2646130) -- Cult of the Lamb: Sinful Pack
+addappid(2647140) -- Cult of the Lamb - Pea Twitch Drop
+addappid(2647150) -- Cult of the Lamb - Owe Twitch Drop
+addappid(2647160) -- Cult of the Lamb - Ghee Twitch Drop
+addappid(2890190) -- Cult of the Lamb: Pilgrim Pack
+addappid(2945120) -- Cult of the Lamb - Goat Statue Twitch Drop
+addappid(2945130) -- Cult of the Lamb - Goat Lantern Twitch Drop
+addappid(2945140) -- Cult of the Lamb - Goat Plant Twitch Drop
+addappid(3017350) -- Cult of the Lamb - Support a Streamer Pack
+addappid(3840050) -- Cult of the Lamb: Woolhaven
+addappid(4153010) -- Cult of the Lamb - Woolhaven Twitch Drop AnglerFish
+addappid(4153020) -- Cult of the Lamb - Woolhaven Twitch Drop SeaButterfly
+addappid(4153030) -- Cult of the Lamb - Woolhaven Twitch Drop Jellyfish
+addappid(4153040) -- Cult of the Lamb - Woolhaven Twitch Drop Leech
+addappid(4153050) -- Cult of the Lamb - Woolhaven Twitch Drop LizardTongue
+
+-- DLC's (with depot keys)
+-- Cult of the Lamb Soundtrack (AppID: 2015890)
+addappid(2015890)
+addappid(2015891, 1, "d6f6c983191a2f11a58a33670799f2bf707666d834710af4018a2bc57c24d13f")
+setManifestid(2015891, "104381208507820952", 305385844)
+addappid(2015892, 1, "ad979829eb577feabacff9f56f449de355bc43cdc60d88e3319e48fc234bade9")
+setManifestid(2015892, "3447023799514037295", 1260245714)
+-- Cult of the Lamb: Hymns of the Unholy (AppID: 3258330)
+addappid(3258330)
+addappid(3258331, 1, "1097abea93eecc9c6cb0156904736974ea48e9a6b301eab513565f942b5431e5")
+setManifestid(3258331, "1087005785590447186", 70746734)
+addappid(3258332, 1, "749cf2d81fe1751ecf6e9dacb1c31b95751969d2debb62488e236f66f6143a96")
+setManifestid(3258332, "2213715038201156417", 509907946)
