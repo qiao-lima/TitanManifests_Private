9d498dd17cee41340558cba47f6c46dfbcf3fcd9
qiao-lima <qiao-lima@github.com>
2026-07-08 09:36:25 +0800

    Manual Update 2549720

---
+++ 2549720.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2549720 - Burger Restaurant Simulator
+-- Generated 2026-07-08 01:16:07 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(2549720)
+
+-- Main Depots
+addappid(2549721, 1, "4eda7a1a12d4e349f50632440711ed37983dc5e981d3a3e51169753a653519d3") -- (windows)
+setManifestid(2549721, "2591247235193064409", 90834258)
+
+-- Missing Depots (We don't have the keys yet lol): 2549722
