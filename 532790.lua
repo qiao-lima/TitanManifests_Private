274a313b1bc69075e6ade5f8c627b7af12ae7d58
qiao-lima <qiao-lima@github.com>
2026-07-08 09:36:27 +0800

    Manual Update 532790

---
+++ 532790.lua
@@ -0,0 +1,27 @@
+-- Generated with Luie @ https://lua.tools/
+-- 532790 - Vampire: The Masquerade - Bloodlines 2
+-- Generated 2026-07-08 01:24:12 UTC
+-- # Depots (Total/DLC/Shared): 6/0/5
+
+-- Main AppID
+addappid(532790, 1, "90523a607f979ac7e71f9fb19c614b4751a8b4f9a7e7efcd5ad6fd72e8380b89")
+
+-- Main Depots
+addappid(532791, 1, "5031e75920df33014e680cad671ff0895f9e439d33eedb5b5878aa407d5cb177") -- (windows, 64-bit)
+setManifestid(532791, "767529984916007555", 25445341553)
+
+-- DLC's (no depot keys required)
+addappid(1048510) -- Vampire: The Masquerade® - Bloodlines™ 2 - Bloodlines Nostalgia Jukebox
+addappid(1048511) -- Vampire: The Masquerade® - Bloodlines™ 2 - Santa Monica Memories
+addappid(3992600) -- Vampire: The Masquerade® - Bloodlines™ 2 - Expansion Pass
+addappid(3996800) -- Vampire: The Masquerade® - Bloodlines™ 2 -  Loose Cannon 
+addappid(3996820) -- Vampire: The Masquerade® - Bloodlines™ 2 - The Flower & the Flame
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") -- (windows)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") -- (windows)
+addappid(229033, 1, "9a789440bbc16131547ac6c062e5f168f7684da533a2899a71fdfaaeec21efc3") -- (windows)
+
+-- Token-locked DLCs (couldn't read depots): 1048512
