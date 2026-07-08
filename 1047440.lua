6a2640a3e6b70954855b88fcb035822384f36825
qiao-lima <qiao-lima@github.com>
2026-07-08 20:21:17 +0800

    Manual Update 1047440

---
+++ 1047440.lua
@@ -0,0 +1,21 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1047440 - DATE A LIVE: Rio Reincarnation
+-- Generated 2026-07-08 12:16:10 UTC
+-- # Depots (Total/DLC/Shared): 4/1/2
+
+-- Main AppID
+addappid(1047440, 1, "92dbea6f5e5067f2d45aa4b0fb6e0f8c2cfdc4a81606dade9a28da01ac6c3ae0")
+
+-- Main Depots
+addappid(1047441, 1, "42c300ed85d7618f3fa71ad5ba690b96a67971de91e986d9e920527d400ec700") -- (windows)
+setManifestid(1047441, "8193004425746969958", 17280050176)
+
+-- DLC's (with depot keys)
+-- DATE A LIVE Rio Reincarnation Deluxe Pack (AppID: 1055670)
+addappid(1055670)
+addappid(1055670, 1, "8fc5e53493b08c7ffb83539a743a7c012b87566802eb320fc37fc43c7dae751a")
+setManifestid(1055670, "4108107192910403638", 74250931)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
