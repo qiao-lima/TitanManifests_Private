e47f4538c611a4413f7951eb1e5788975156e308
qiao-lima <qiao-lima@github.com>
2026-07-08 20:21:20 +0800

    Manual Update 4067590

---
+++ 4067590.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4067590 - VirtuStick
+-- Generated 2026-07-08 12:13:08 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4067590)
+
+-- Main Depots
+addappid(4067591, 1, "ed34a8ae2c2bf347f460bb9fef6c4994a3e655672888ce1b437317ab7c118df2")
+setManifestid(4067591, "3921357749879024009", 326390155)
