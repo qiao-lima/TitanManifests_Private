78aacb2d543eb75db8b58243ab22a1966749da33
qiao-lima <qiao-lima@github.com>
2026-07-08 20:21:17 +0800

    Manual Update 1509960

---
+++ 1509960.lua
@@ -0,0 +1,21 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1509960 - PICO PARK
+-- Generated 2026-07-08 12:19:41 UTC
+-- # Depots (Total/DLC/Shared): 4/1/2
+
+-- Main AppID
+addappid(1509960, 1, "de55873a706ae199d05ee85c55fa887e912a2d866128bbead08e8f5004c3c7d3")
+
+-- Main Depots
+addappid(1509961, 1, "1ea9efc77f966369354189cff52ee9d118e406ff157718c1aa540beff716a879")
+setManifestid(1509961, "1693052742714430748", 80009044)
+
+-- DLC's (with depot keys)
+-- PICO PARK Soundtrack (AppID: 1646370)
+addappid(1646370)
+addappid(1646371, 1, "680666d7a4a03c065cffd770b43cf1b0ab3ab544bf2152f91edb1af4b4d49d3a")
+setManifestid(1646371, "8315362776204359440", 90705982)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
