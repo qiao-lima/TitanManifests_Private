e23b350042eca789c6bf34555326b4e5dcd67f94
qiao-lima <qiao-lima@github.com>
2026-07-08 20:21:20 +0800

    Manual Update 3686400

---
+++ 3686400.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3686400 - Titan Core
+-- Generated 2026-07-08 12:14:21 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3686400)
+
+-- Main Depots
+addappid(3686401, 1, "c48aa366959d88f6e9d04a9d6e15c68a47b99ae0bb77f0678003465d737f8a1e") -- (windows)
+setManifestid(3686401, "3183106203603346380", 977999283)
