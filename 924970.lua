7a75e299940b532a209f4de759d8c6263f3f5426
qiao-lima <qiao-lima@github.com>
2026-07-09 22:57:44 +0800

    Manual Update 924970

---
+++ 924970.lua
@@ -0,0 +1,38 @@
+-- Generated with Luie @ https://lua.tools/
+-- 924970 - Back 4 Blood
+-- Generated 2026-07-09 14:41:52 UTC
+-- # Depots (Total/DLC/Shared): 13/4/2
+
+-- Main AppID
+addappid(924970, 1, "198c2187534fc1990742e1d64a8c8022cb46b87fb502c6b30bab34bb034e124e")
+
+-- Main Depots
+addappid(924971, 1, "783767a483d935235bc2a46262a58b4acc3524bdf6d52116f090fcccc0a89024")
+setManifestid(924971, "2194878379786247325", 49599168453)
+addappid(924972, 1, "ec1e4f563086da2780fdcb3f167c056d76e0c9ca49b6418b26d34ce26660ada7") -- French
+setManifestid(924972, "7652397841777917928", 723341245)
+addappid(924973, 1, "a729a752e939faa9c74e578c69b357cddf7d34ccd6c8564129117a16193fb003") -- German
+setManifestid(924973, "6035913895232866500", 753523965)
+addappid(924974, 1, "28ab0aa9e98d603cd572c921b820931dedcf94882988a8d4aaaf5374394ec9d8") -- Italian
+setManifestid(924974, "8294287718880305251", 747646484)
+addappid(924975, 1, "59f5fe1e63f36b4a43383d0f3eeef896f1375ff3a1d18bf5cd27c5756ff710fc") -- Brazilian
+setManifestid(924975, "2770145798175310955", 764817463)
+addappid(924976, 1, "1acba27ad19b4dd77a3130a775f5adeeebd0aa40ed3353f5cb80bfae583e60fd") -- Spanish
+setManifestid(924976, "2838371652923164555", 767084126)
+addappid(924977, 1, "bea7e2008e44c61676a5e44b067e4c637be848c3719337acb2cd55a1f7976693") -- Latam
+setManifestid(924977, "3599819504499511506", 758020498)
+
+-- DLC's (no depot keys required)
+addappid(1598440) -- DLC 1598440
+addappid(1598442) -- DLC 1598442
+addappid(1840320) -- Back 4 Blood - Expansion 1: Tunnels of Terror
+addappid(2015180) -- Back 4 Blood - Expansion 2: Children of the Worm
+addappid(2101940) -- Back 4 Blood - Expansion 3: River of Blood
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 1142380, 1142420, 1406830, 1406831
+
+-- Token-locked DLCs (couldn't read depots): 1598441
