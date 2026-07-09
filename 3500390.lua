d6e085ab7a5cd90707dbe0998166f3965e62791b
qiao-lima <qiao-lima@github.com>
2026-07-09 22:57:41 +0800

    Manual Update 3500390

---
+++ 3500390.lua
@@ -0,0 +1,30 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3500390 - Mega Man Star Force Legacy Collection
+-- Generated 2026-07-09 14:56:45 UTC
+-- # Depots (Total/DLC/Shared): 10/8/1
+
+-- Main AppID
+addappid(3500390)
+
+-- Main Depots
+addappid(3500391, 1, "febf6d8948ae4c2e1a199fa36f58cbfd6c5b3d81c130a612604f803316f5326b")
+setManifestid(3500391, "3147866540271715562", 5788265079)
+
+-- DLC's (with depot keys)
+-- Mega Man Star Force Legacy Collection – Geo Stelar and Omega-Xis Character Model Pack (AppID: 4059840)
+addappid(4059840)
+addappid(4059840, 1, "e36991eb8125945b9f99b2c11323010a5a918ad3f07b8bb7dc199a5cb4c49b42")
+setManifestid(4059840, "2274394409022129768", 50345033)
+-- Mega Man Star Force Legacy Collection – Mega Man Battle Network Series BGM (AppID: 4059850)
+addappid(4059850)
+addappid(4059850, 1, "53e2bea43b75aa1da51ae5db09413e9c1cc98f99de9b4b787fea8c2eeefcf45c")
+setManifestid(4059850, "1340711804696207363", 225964166)
+-- Mega Man Star Force Legacy Collection – Sonia Strumm and Luna Platz Character Model Pack (AppID: 4059860)
+addappid(4059860)
+addappid(4059860, 1, "a4a1b191f1d28dd691f0e9197d0f156a0781b407a466d44a7e455aa97903b032")
+setManifestid(4059860, "1976583030948263159", 16410091)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 4390970, 4867391, 4867392, 4867393, 4867394
