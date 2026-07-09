a1302d4af283039abf42fdf8deb9a3b41c945001
qiao-lima <qiao-lima@github.com>
2026-07-09 22:57:42 +0800

    Manual Update 4249130

---
+++ 4249130.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4249130 - Dino Crisis
+-- Generated 2026-07-09 14:57:10 UTC
+-- # Depots (Total/DLC/Shared): 9/6/0
+
+-- Main AppID
+addappid(4249130)
+
+-- Main Depots
+addappid(4249131, 1, "8a848b7691d0a3d0bf697cfc04bb44221a778b027d21c7330459a0a8fbfbbb3a")
+setManifestid(4249131, "7160042762315048429", 1400317)
+addappid(4249132, 1, "e0da4eae252a1a7e9ad8f1dcd3c6bec1f5399f5ada44585dc8b193615d4c6543")
+setManifestid(4249132, "5434870342583988935", 21699008)
+addappid(4249133, 1, "6f92df7d6327d7ed27320219fdd39495d2e1319925a587517f99467fc7a221f6")
+setManifestid(4249133, "2089596546887604834", 3386921200)
+
+-- Missing Depots (We don't have the keys yet lol): 1568801, 1568802, 1568803, 1568804, 1568805, 1568806
