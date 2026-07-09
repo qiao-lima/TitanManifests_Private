768b65ec1c9faa4d0b1155f9d75aa1ee0c20d07f
qiao-lima <qiao-lima@github.com>
2026-07-09 22:57:39 +0800

    Manual Update 1880620

---
+++ 1880620.lua
@@ -0,0 +1,35 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1880620 - Once Upon A KATAMARI
+-- Generated 2026-07-09 14:51:44 UTC
+-- # Depots (Total/DLC/Shared): 8/5/2
+
+-- Main AppID
+addappid(1880620)
+
+-- Main Depots
+addappid(1880621, 1, "e351457ac3bc22e5b454e7133ef92c44559e17d035c95fc5ec7932096ddcf6fe")
+setManifestid(1880621, "9179781944507203517", 2901916816)
+
+-- DLC's (no depot keys required)
+addappid(3728480) -- Once Upon A KATAMARI - Cousin Customization: Face (3 items)
+addappid(3839670) -- Once Upon A KATAMARI - Katamari Damacy Series Songs: Sides A + B Bundle
+
+-- DLC's (with depot keys)
+-- Once Upon A KATAMARI - Katamari Damacy Series Songs: Side A (AppID: 3644570)
+addappid(3644570)
+addappid(3644570, 1, "7a35a460962fe6f322e7a9546cae9a65dfff5419a3e4be13cc611cf2d9fef1c8")
+setManifestid(3644570, "3818063009635453138", 640437450)
+-- Once Upon A KATAMARI - Katamari Damacy Series Songs: Side B (AppID: 3644580)
+addappid(3644580)
+addappid(3644580, 1, "94e7021bf30c2352766358ab14a4a74dcbbf0e5d18ae5aba217c939c0d3ae621")
+setManifestid(3644580, "2917185460247202248", 375990280)
+-- DLC 3644590 (AppID: 3644590)
+addappid(3644590)
+addappid(3644590, 1, "6c42934d8a6b6e28819c6fef13d4120721675005a06e4a1903f8f639b1a6973b")
+setManifestid(3644590, "200137077103662778", 39326996)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 3644600, 4463160
