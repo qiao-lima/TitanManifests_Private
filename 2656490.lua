b267e04581f87a25c4a6dfe1b221e8acbdefaee7
qiao-lima <qiao-lima@github.com>
2026-07-09 22:57:40 +0800

    Manual Update 2656490

---
+++ 2656490.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2656490 - ANTHEM#9
+-- Generated 2026-07-09 14:56:23 UTC
+-- # Depots (Total/DLC/Shared): 3/2/0
+
+-- Main AppID
+addappid(2656490)
+
+-- Main Depots
+addappid(2656491, 1, "7aef194b82e43e4b6bb3b5bf0d07eaebf356239eff7a4f3628cc5fc439158a56")
+setManifestid(2656491, "91807539867144442", 1979370027)
+
+-- Missing Depots (We don't have the keys yet lol): 4510801, 4510802
