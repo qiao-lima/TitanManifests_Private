309d7e83c662a90ab994354a5865fda4dbd2186e
qiao-lima <qiao-lima@github.com>
2026-07-10 23:19:33 +0800

    Manual Update 3733960

---
+++ 3733960.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3733960 - CODE VEIN II - Character Creator Demo
+-- Generated 2026-07-10 14:25:27 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(3733960)
+
+-- Main Depots
+addappid(3733961, 1, "b795f85d0b818e32e758c6de09edc4d8d5e0af4c72ca74a60f298c4c2199b758")
+setManifestid(3733961, "6872852908735695731", 27440622759)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
