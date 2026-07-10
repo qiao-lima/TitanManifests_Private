a04d735e8d2f2213c46400526a8a4f78c7024a6d
qiao-lima <qiao-lima@github.com>
2026-07-10 23:14:06 +0800

    Manual Update 2287520

---
+++ 2287520.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2287520 - Five Nights at Freddy's: Help Wanted 2
+-- Generated 2026-07-10 14:26:35 UTC
+-- # Depots (Total/DLC/Shared): 4/1/2
+
+-- Main AppID
+addappid(2287520, 1, "dcebd59ed2e0cf53367ae7f25a3845d526ec4048eed6f7359b089cd8b673919d")
+
+-- Main Depots
+addappid(2287521, 1, "a7d107435e6dce4ba92250a6ae5b0df38e5adcb5254289c5c7633489a01afaa8")
+setManifestid(2287521, "7937359920328293297", 11450170974)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 2609630
