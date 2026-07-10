485350ab8fa845076657ea496f003cb667d5030d
qiao-lima <qiao-lima@github.com>
2026-07-10 23:25:49 +0800

    Manual Update 4380490

---
+++ 4380490.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4380490 - Cat Mail Co.
+-- Generated 2026-07-10 14:12:08 UTC
+-- # Depots (Total/DLC/Shared): 2/1/0
+
+-- Main AppID
+addappid(4380490)
+
+-- Main Depots
+addappid(4380491, 1, "1639ac5473e577209fec86c1386c93ce89b54703cc51f71956a7577b27ad278b")
+setManifestid(4380491, "5418991445805897126", 864405402)
+
+-- Missing Depots (We don't have the keys yet lol): 4911900
