72c3219d6ad0c4db75b2f6ea0c501a648fe3b299
qiao-lima <qiao-lima@github.com>
2026-07-10 23:19:34 +0800

    Manual Update 4172690

---
+++ 4172690.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4172690 - 1根香腸走江湖
+-- Generated 2026-07-10 14:55:06 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4172690)
+
+-- Main Depots
+addappid(4172691, 1, "1225fd509a094ed4a2f226fe75888fa4567eb3b5d010dbcec33185a99683f4ea")
+setManifestid(4172691, "6791486715490555963", 504977631)
+
+-- DLC's (no depot keys required)
+addappid(4193030) -- 1根香腸-高級禮包
+addappid(4193040) -- 1根香腸-至尊禮包
