5482db6a2182642c6e96cdf664cdfb4903e21311
qiao-lima <qiao-lima@github.com>
2026-07-10 23:14:08 +0800

    Manual Update 2947280

---
+++ 2947280.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2947280 - Solateria
+-- Generated 2026-07-10 14:25:46 UTC
+-- # Depots (Total/DLC/Shared): 2/1/0
+
+-- Main AppID
+addappid(2947280)
+
+-- Main Depots
+addappid(2947281, 1, "10572b974c4ffc9e9179323e537a5b6b71485fd55d6f083681652a526dece4f1") -- (windows)
+setManifestid(2947281, "4038713527446644810", 2560160996)
+
+-- DLC's (with depot keys)
+-- Solateria Soundtrack (AppID: 4611670)
+addappid(4611670)
+addappid(4611671, 1, "3d8052aa1f3dbed97f4d0aab2b58c5a6167c456eb2b4e4b6be37056d65e0e665")
+setManifestid(4611671, "6252289489869081843", 216926222)
