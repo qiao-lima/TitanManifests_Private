da7eb5d7ca33fe6321ff9a3c7ac0cc53190bacc9
qiao-lima <qiao-lima@github.com>
2026-07-10 23:14:03 +0800

    Manual Update 1672810

---
+++ 1672810.lua
@@ -0,0 +1,16 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1672810 - MIO: Memories in Orbit
+-- Generated 2026-07-10 14:26:50 UTC
+-- # Depots (Total/DLC/Shared): 3/1/1
+
+-- Main AppID
+addappid(1672810)
+
+-- Main Depots
+addappid(1672811, 1, "cb73bead2ca7cc72a2ad3fb210203ba2e0732db40cf3700d95b3fc62b825e490")
+setManifestid(1672811, "4711570043897766739", 3775770142)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 1672812
