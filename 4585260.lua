1fd1be75a80aa82d8bc9aca4a676c6990530eb14
qiao-lima <qiao-lima@github.com>
2026-07-10 23:25:49 +0800

    Manual Update 4585260

---
+++ 4585260.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4585260 - The Rake Sees You
+-- Generated 2026-07-10 14:21:13 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(4585260)
+
+-- Main Depots
+addappid(4585261, 1, "3f10fc62934db57cdaa25affeb7068a900aeb7a79756362c29b39cd1389c8862")
+setManifestid(4585261, "1342767416249974165", 3615296934)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
