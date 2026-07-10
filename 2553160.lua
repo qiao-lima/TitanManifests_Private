5577a9765a95fb0c7bfa07ea0e6aebcc1ae5bda5
qiao-lima <qiao-lima@github.com>
2026-07-10 23:14:07 +0800

    Manual Update 2553160

---
+++ 2553160.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2553160 - 只管战斗 Unorthodox game
+-- Generated 2026-07-10 14:23:39 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2553160)
+
+-- Main Depots
+addappid(2553161, 1, "fd9f38e92d8f603d060ffeb60b3ff00cf11f4bb145d47aab88f9416e0520bf6e")
+setManifestid(2553161, "3904280664461900795", 665584340)
