0ff8abbd5701dd8977af659ea91fd51eb2bda613
qiao-lima <qiao-lima@github.com>
2026-07-10 23:19:30 +0800

    Manual Update 3137350

---
+++ 3137350.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3137350 - Motion Soccer
+-- Generated 2026-07-10 13:40:41 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3137350)
+
+-- Main Depots
+addappid(3137351, 1, "2bb669932b7cf25317e237bf24377236c0ec3d19f455e83e0b178d0d8ddd8e95")
+setManifestid(3137351, "3892644552376860122", 3353729439)
