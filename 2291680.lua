28a0621c9882dc3c6fe3b3bf11e96b7fa369852e
qiao-lima <qiao-lima@github.com>
2026-07-10 23:14:06 +0800

    Manual Update 2291680

---
+++ 2291680.lua
@@ -0,0 +1,21 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2291680 - The Arrogant Kaiju Princess and The Detective Servant
+-- Generated 2026-07-10 14:16:14 UTC
+-- # Depots (Total/DLC/Shared): 3/2/0
+
+-- Main AppID
+addappid(2291680)
+
+-- Main Depots
+addappid(2291681, 1, "d5f249db247fe7b9d64a53304732e9775a8ffc470308d03c4e96fc84f8775f71")
+setManifestid(2291681, "1585438778104121488", 4279156943)
+
+-- DLC's (with depot keys)
+-- The Arrogant Kaiju Princess and The Detective Servant Soundtrack (AppID: 2714810)
+addappid(2714810)
+addappid(2714811, 1, "effac0195a538f873d695a538946b4fc0f02ff6f54eeda3908a2cde70fb889c7")
+setManifestid(2714811, "4205806703297978943", 117433944)
+-- The Arrogant Kaiju Princess and The Detective Servant  ArtBook (AppID: 2714820)
+addappid(2714820)
+addappid(2714820, 1, "232e0645e594dbe740dbb0bf1557d765dd0444558df6d159de8734b3fb51e255")
+setManifestid(2714820, "4057397915279419981", 150382556)
