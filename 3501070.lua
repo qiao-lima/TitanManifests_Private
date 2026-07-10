0c28cf86d53d39c3893e67319b657004a643df93
qiao-lima <qiao-lima@github.com>
2026-07-10 23:19:31 +0800

    Manual Update 3501070

---
+++ 3501070.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3501070 - Car Service Together
+-- Generated 2026-07-10 13:42:33 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3501070, 1, "e2342efc9b3a360291be146a6be30d7b129bbdad5900b09806483c244ab607cb")
+
+-- Main Depots
+addappid(3501071, 1, "7bb00da72e348f52d382147b41332f518de67a4ff8f3ab8cd203888982e72de4")
+setManifestid(3501071, "1980243417666500400", 6350041779)
