98b422f2b68d91512911c1fc57efce9179e038e9
qiao-lima <qiao-lima@github.com>
2026-07-10 23:19:32 +0800

    Manual Update 365450

---
+++ 365450.lua
@@ -0,0 +1,45 @@
+-- Generated with Luie @ https://lua.tools/
+-- 365450 - Hacknet
+-- Generated 2026-07-10 14:14:59 UTC
+-- # Depots (Total/DLC/Shared): 15/7/2
+
+-- Main AppID
+addappid(365450, 1, "2d05152a1b87ac918aab52f39201a9d630db7062613a265cb84f4d4f3540f48e")
+
+-- Main Depots
+addappid(365451, 1, "bbbb5f70e666008307c6c11f917cf6f03781c29e49fe5762536e5aca4c119b97") -- (windows)
+setManifestid(365451, "3334967212005253180", 285884745)
+addappid(365453, 1, "f1afe8097c6dbeead2d6613c83c4f1fd05e257a5fb947a488c6a2fd2921e3d9c") -- (linux, 32-bit)
+setManifestid(365453, "9118404707413857261", 0)
+addappid(365454, 1, "03f224c738b9bbf65120893dcdb5a3e8aa361fb9b15a48ba72b16272422becde") -- (macos)
+setManifestid(365454, "5237573375958690650", 319009333)
+addappid(365455, 1, "e78fa655688c8f09ce374b855e4dbf0307a1bea8082843118049700b0433c46c") -- (linux, 64-bit)
+setManifestid(365455, "1911080312180287555", 96703427)
+addappid(365456, 1, "0c7ed01961fbe859446edb5c1164fa186e956ea5907ec3dadd2fe3fd77a394da") -- (linux)
+setManifestid(365456, "3529038054491803885", 229001386)
+
+-- DLC's (with depot keys)
+-- Hacknet Official Soundtrack (AppID: 408710)
+addappid(408710)
+addappid(408710, 1, "4c1bfe6134004102f557bc1697720b6eebed307631985d1c25daa0f58c06be8f")
+setManifestid(408710, "5308985427482721397", 545585125)
+-- Hacknet - Labyrinths (AppID: 521840)
+addappid(521840)
+addappid(365458, 1, "a4ef3ad79afb43c168e0c3f756a4f7d4b5dec49ab6c30a4798ed65a445ae2470") -- (windows)
+setManifestid(365458, "2184418459091798884", 156831940)
+addappid(365459, 1, "354287bd02690e680e610dd55d1797ac6862e80015d3deeb49151696de98cb40") -- (macos)
+setManifestid(365459, "2964520419333359855", 156831940)
+addappid(408711, 1, "ac86110f9a605651695235db27a8a3554f3484a2de635e606af18125d41cb481") -- (linux)
+setManifestid(408711, "4525842105236732908", 156831940)
+-- Hacknet - Labyrinths Official Soundtrack (AppID: 612210)
+addappid(612210)
+addappid(612210, 1, "fabe8dc598ce459d137d0dc8a0cadcda8b4a7aeb447d4f0d51eb41e6cccf859a")
+setManifestid(612210, "8918019172760266162", 84718944)
+addappid(612211, 1, "6e8da019b1df81345043f47fc4756fb823e9976492e098b6acfc2462a28d65ad")
+setManifestid(612211, "5797891863547302311", 253979312)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(229003, 1, "8e0ded6e1d9481eb286217322054ce7b0b8243daae770f31a30667752b1a3c63") -- (windows)
+addappid(229012, 1, "421e499f39a617c1a3b6baf11fbc65ffebd541d2c39c38d3bf6ffbd87919ce12") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 365452, 521840
