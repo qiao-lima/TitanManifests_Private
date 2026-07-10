0be5add4d400b46a4d0655718fc0c0b9f3f95f1d
qiao-lima <qiao-lima@github.com>
2026-07-10 23:14:04 +0800

    Manual Update 1864000

---
+++ 1864000.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1864000 - Artis Impact
+-- Generated 2026-07-10 14:23:59 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(1864000)
+
+-- Main Depots
+addappid(1864001, 1, "9eccab235dfb23018531ba20ea2bb82f2b197494f54abad326278360bf8c8fc1")
+setManifestid(1864001, "6083553785048068894", 2796235748)
