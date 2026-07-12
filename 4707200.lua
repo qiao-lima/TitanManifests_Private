e1337a2a99818e5121419c51b14a0ece28e071e2
qiao-lima <qiao-lima@github.com>
2026-07-12 23:45:35 +0800

    Manual Update 4707200

---
+++ 4707200.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4707200 - Queen's Gamble
+-- Generated 2026-07-12 13:19:16 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(4707200)
+
+-- Main Depots
+addappid(4707201, 1, "3f591eb8083d42efe3f7f35dc11dc4d15a746acc9eebc5714232d997e54cc01e") -- (windows,linux)
+setManifestid(4707201, "676656603070969312", 1118254805)
+
+-- Missing Depots (We don't have the keys yet lol): 4707202
