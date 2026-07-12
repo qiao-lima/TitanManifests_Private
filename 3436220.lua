eda602114e88d280d83df1e89b4255a150773979
qiao-lima <qiao-lima@github.com>
2026-07-12 23:42:50 +0800

    Manual Update 3436220

---
+++ 3436220.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3436220 - U.F.F.C - Ultra Fuckin’ Fuckable Championship
+-- Generated 2026-07-12 14:31:19 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(3436220)
+
+-- Main Depots
+addappid(3436221, 1, "4e94a97c2aeb2f0be9dbf2e75964423dab7b8d47f3a481155a533cc676bdc047") -- (windows)
+setManifestid(3436221, "1793921100807304874", 2031940842)
+
+-- Missing Depots (We don't have the keys yet lol): 3436222
