7fa7135c5cdbacb77ada923fb22284ec9015bb7d
qiao-lima <qiao-lima@github.com>
2026-07-12 23:45:33 +0800

    Manual Update 4429920

---
+++ 4429920.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4429920 - Sex Shop Simulator: Sinful Girls
+-- Generated 2026-07-12 14:02:35 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4429920)
+
+-- Main Depots
+addappid(4429922, 1, "9cdf4c07eb3edb208d52366937f72ac34e75fedda02344bf0348d01e5cac0f75")
+setManifestid(4429922, "6968495495637167155", 4027326992)
