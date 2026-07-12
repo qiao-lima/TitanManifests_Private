82d8d35a8f63cb7cf730303648f4d9c0e0bdbc39
qiao-lima <qiao-lima@github.com>
2026-07-12 23:45:29 +0800

    Manual Update 3915320

---
+++ 3915320.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3915320 - I Wish I Had Never Gone into the Dungeon
+-- Generated 2026-07-12 13:31:46 UTC
+-- # Depots (Total/DLC/Shared): 4/0/0
+
+-- Main AppID
+addappid(3915320)
+
+-- Main Depots
+addappid(3915321, 1, "a6d0fe74dced51d95f6025d14caf83106a2e033799ab9909861021904551099f") -- Japanese
+setManifestid(3915321, "3679499486475570951", 1662833606)
+addappid(3915322, 1, "ef7cc751971b6c9486998c808ee21bc28f18f03364cc30e4c262e1a6adb04250") -- Schinese
+setManifestid(3915322, "5824865930392521916", 1670221067)
+
+-- Missing Depots (We don't have the keys yet lol): 3915323, 3915324
