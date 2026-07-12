896da53b6af622ef08e01907d2d0ad62965a393b
qiao-lima <qiao-lima@github.com>
2026-07-12 23:36:46 +0800

    Manual Update 2343140

---
+++ 2343140.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2343140 - My Bath Tub Companion
+-- Generated 2026-07-12 14:00:03 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2343140)
+
+-- Main Depots
+addappid(2343142, 1, "ea3e909abf6bab50359ef22a9d0a00a7784b416e0581a16de84d6d1f120284f3")
+setManifestid(2343142, "5949545053999118950", 1136898906)
