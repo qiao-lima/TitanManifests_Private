1421e2b5404d787051230e6ece1f649ec16a3e4f
qiao-lima <qiao-lima@github.com>
2026-07-12 23:36:45 +0800

    Manual Update 2005010

---
+++ 2005010.lua
@@ -0,0 +1,20 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2005010 - Warhammer 40,000: Boltgun
+-- Generated 2026-07-12 14:43:24 UTC
+-- # Depots (Total/DLC/Shared): 4/1/2
+
+-- Main AppID
+addappid(2005010, 1, "3f9c42686c3c0525202133ec01f8a6a18b9252d881972a431f97a5962b282ce9")
+
+-- Main Depots
+addappid(2005011, 1, "a0d26058a3aee26facb0319102648ee8172819bfba6358f4f375b89dd4070d36")
+setManifestid(2005011, "7274678156380217851", 6767496938)
+
+-- DLC's (no depot keys required)
+addappid(2668290) -- Warhammer 40,000: Boltgun - Forges Of Corruption Expansion
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 2225900
