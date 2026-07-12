a74f4384232c4a92bb0cccf8ca872dc0cdc082c8
qiao-lima <qiao-lima@github.com>
2026-07-12 23:36:48 +0800

    Manual Update 3044910

---
+++ 3044910.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3044910 - VPN Simulator
+-- Generated 2026-07-12 13:32:28 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3044910)
+
+-- Main Depots
+addappid(3044911, 1, "fa5c5b63b20978e96fcf4520dfddfbf6445df2e7c7aa13333d4ea9915f59b643")
+setManifestid(3044911, "1123980586577307408", 166037781)
