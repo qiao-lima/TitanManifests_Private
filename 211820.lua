374c15c27ef769172aceb4575dc118ea02f9a571
qiao-lima <qiao-lima@github.com>
2026-07-12 23:36:46 +0800

    Manual Update 211820

---
+++ 211820.lua
@@ -0,0 +1,28 @@
+-- Generated with Luie @ https://lua.tools/
+-- 211820 - Starbound
+-- Generated 2026-07-12 14:46:27 UTC
+-- # Depots (Total/DLC/Shared): 7/1/1
+
+-- Main AppID
+addappid(211820, 1, "5a5195b84171ddf8a2b74190df2a34b16bceaa05e310be9c5b0f9b5ff4f5e8ad")
+
+-- Main Depots
+addappid(211821, 1, "d38d22b9989c3c2c74a3ff2b9164637cd6816f06bbac1cbd57a79098f56e7a49")
+setManifestid(211821, "612190494005252909", 907077290)
+addappid(211822, 1, "f34154fb8d8c1bad83ea48eca72eb672004cd9c334cf1042c380b1439b0ec9f2") -- (windows)
+setManifestid(211822, "3176698905874508149", 357374252)
+addappid(211823, 1, "f557c69f92ae74f4d8658c8633837fd6605f355606ee5381330b63ed15553dc0") -- (windows, 64-bit)
+setManifestid(211823, "2204433742415235791", 406288241)
+addappid(211824, 1, "18da06f95aae21ebd6d63bf0ac30854b0b908bef896d1d8ff19aeb21e989a186") -- (macos)
+setManifestid(211824, "7357167301525874818", 161981070)
+addappid(211825, 1, "6e7664f162e86314482286f2d561b5158d708f2412a6b7bd2be03320e433b7c5") -- (linux)
+setManifestid(211825, "8349984589792238818", 219902104)
+
+-- DLC's (with depot keys)
+-- Starbound - Soundtrack (AppID: 268090)
+addappid(268090)
+addappid(268090, 1, "5113a6432e32c1ef6474caf358c74e24c48bdfacf2a607a2c81257af514abf26")
+setManifestid(268090, "8447998257602501186", 803897788)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- (windows)
