b458887ea37d5da00a9adbb0f78ecffacd097c5d
qiao-lima <qiao-lima@github.com>
2026-07-12 23:45:32 +0800

    Manual Update 4205060

---
+++ 4205060.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4205060 - DopePie Survivors 3D
+-- Generated 2026-07-12 14:47:48 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4205060)
+
+-- Main Depots
+addappid(4205061, 1, "29262a67cfb316d8aab36c5446f68274ce8bbae345c728be42d303aae1740711")
+setManifestid(4205061, "1619754634972004341", 1650305563)
