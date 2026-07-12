39ff6665041c486358248706c0fc5070b977ccb5
qiao-lima <qiao-lima@github.com>
2026-07-12 23:36:46 +0800

    Manual Update 224260

---
+++ 224260.lua
@@ -0,0 +1,27 @@
+-- Generated with Luie @ https://lua.tools/
+-- 224260 - No More Room in Hell
+-- Generated 2026-07-12 13:54:51 UTC
+-- # Depots (Total/DLC/Shared): 7/2/0
+
+-- Main AppID
+addappid(224260, 1, "5d8c4ae23001dbb1c64b949b3b5b922a95e9887db5073bf864e231bd9669fef4")
+
+-- Main Depots
+addappid(224261, 1, "bfd0df36252eee487fc1f04740fe609457da8672f9d0672da0a96026f09109be")
+setManifestid(224261, "7240923526129576869", 5504007758)
+addappid(224262, 1, "6c375fbc3957e3f37c6faec07844205e5069a1cdd2ece11b8918e901599f1f7c") -- (windows)
+setManifestid(224262, "5899424840868488871", 15326728)
+addappid(224263, 1, "5dc0b6adc1c917ae04472eda3063a2f678b3ae662e02f71428eb98859e539c88") -- (linux)
+setManifestid(224263, "327259344855384380", 66103402)
+addappid(224265, 1, "405e32e56a563553f8c3b22f9b6655e1a871f43b6b4fd95220243cf46e91442b") -- (windows)
+setManifestid(224265, "7714926495873074452", 2983619022)
+addappid(224266, 1, "db5c882cdbfa354e9db8b98d5fdec7cb2f631225c5bed500ed5ffab7ef77cb0a") -- (linux)
+setManifestid(224266, "7606456667394968342", 2942887105)
+
+-- DLC's (with depot keys)
+-- No More Room in Hell - Original Soundtrack Volume 1 (AppID: 310660)
+addappid(310660)
+addappid(310660, 1, "44f3049ae1bb29b26b1c535721495f8370ee4afbe571513bd445820746bcdf0c")
+setManifestid(310660, "7170683187232556788", 136527329)
+addappid(310661, 1, "721f1ffb29533b2278f000d9154dee5f5d34ba53257a31fba77091abed03ccb8")
+setManifestid(310661, "758102933840971922", 533485767)
