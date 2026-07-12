1368ecc9c23afaf062c62fc4047ed77043752133
qiao-lima <qiao-lima@github.com>
2026-07-12 23:36:44 +0800

    Manual Update 1425000

---
+++ 1425000.lua
@@ -0,0 +1,40 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1425000 - Endless Alice
+-- Generated 2026-07-09 14:20:15 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(1425000, 1, "107e15727a379425aad1300e87e64e52b1378b88e470e612dc5a06f9f74bc90f")
+
+-- Main Depots
+addappid(1425001, 1, "538a019189be9b823a8c7ff25c61d33657551e295cfe7a4d4abde120d7eb0eb7")
+setManifestid(1425001, "8019922969128809507", 7864319157)
+
+-- DLC's (no depot keys required)
+addappid(2779970) -- Endless Alice: Shimiao Swimsuit (Twin ponytail version)
+addappid(3758720) -- EndlessAlice: Shibako Swimsuit
+addappid(3758730) -- EndlessAlice: Shi Miao Swimsuit
+addappid(3818470) -- EndlessAlice: Lea Swimsuit
+addappid(3818480) -- EndlessAlice: Koraku Swimsuit
+addappid(3818490) -- EndlessAlice: TakeHime Swimsuit
+addappid(3823330) -- EndlessAlice: Alice Swimsuit
+addappid(4074020) -- EndlessAlice: Takehime Bunny Girl
+addappid(4131950) -- EndlessAlice: Takehime Mummy
+addappid(4139190) -- EndlessAlice: Koraku Bunny Girl
+addappid(4166830) -- EndlessAlice: Lea Bunny Girl
+addappid(4166840) -- EndlessAlice: Alice Bunny Girl
+addappid(4178740) -- EndlessAlice: Shibako Bunny Girl
+addappid(4234690) -- EndlessAlice: Shimiao Bunny Girl
+addappid(4347220) -- EndlessAlice: Shibako Modern Cheongsam
+addappid(4347230) -- EndlessAlice: Takehime Modern Cheongsam
+addappid(4347240) -- EndlessAlice: Koraku Modern Cheongsam
+addappid(4347250) -- EndlessAlice: ShiMiao Modern Cheongsam
+addappid(4414180) -- EndlessAlice: Alice Modern Cheongsam
+addappid(4414190) -- EndlessAlice: Lea Modern Cheongsam
+addappid(4550540) -- EndlessAlice: Takehime Reverse Bunny Suit
+addappid(4885420) -- EndlessAlice: Mia Sukumizu
+addappid(4885430) -- EndlessAlice: Lea Sukumizu
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
