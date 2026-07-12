e64c581a1107cbf09435347b50be101092850459
qiao-lima <qiao-lima@github.com>
2026-07-12 23:36:43 +0800

    Manual Update 1296360

---
+++ 1296360.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1296360 - Archvale
+-- Generated 2026-07-12 14:29:20 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(1296360)
+
+-- Main Depots
+addappid(1296361, 1, "cd8bd502bab629ae4c436ea94afeb2370b50ec2cc8919f583bc3989b20b46edc")
+setManifestid(1296361, "4448900037900655732", 1122979979)
