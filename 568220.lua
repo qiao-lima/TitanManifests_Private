d26e6c2eeccdf0a69b7b9ba50580bcb5e42ffe05
qiao-lima <qiao-lima@github.com>
2026-07-12 23:51:54 +0800

    Manual Update 568220

---
+++ 568220.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 568220 - Lobotomy Corporation
+-- Generated 2026-07-12 14:29:56 UTC
+-- # Depots (Total/DLC/Shared): 2/1/0
+
+-- Main AppID
+addappid(568220, 1, "fa653f2dc25ab3fc344e141d5179f2fa7daae1455414ed988a0ca8d7c0d72e04")
+
+-- Main Depots
+addappid(568221, 1, "4b83656636360e659df7b8f983d8436fe8216360fe0c6edf14f819a8622d70c7") -- (windows)
+setManifestid(568221, "8058397718201736202", 5529460097)
+
+-- DLC's (with depot keys)
+-- LobotomyCorporation_ArtBook (AppID: 896000)
+addappid(896000)
+addappid(896000, 1, "76d7260e37893f7d0e8100d73d3685b1ebe2a0c8e71be8034d6f4c2d6596374b")
+setManifestid(896000, "4668171197035314231", 423777286)
