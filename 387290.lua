d9fffa43455f1a39c15a218910996a7a1c15168f
qiao-lima <qiao-lima@github.com>
2026-07-12 23:45:29 +0800

    Manual Update 387290

---
+++ 387290.lua
@@ -0,0 +1,24 @@
+-- Generated with Luie @ https://lua.tools/
+-- 387290 - Ori and the Blind Forest: Definitive Edition
+-- Generated 2026-07-12 15:15:38 UTC
+-- # Depots (Total/DLC/Shared): 4/2/1
+
+-- Main AppID
+addappid(387290, 1, "599380c191242721c4784f2d5313ab154bc6419575141da471220315f1260e5b")
+
+-- Main Depots
+addappid(387291, 1, "e3379e56401649f05bf54aa6ef2b7e61b9603a14484529d84daa9f07ba0b24c5")
+setManifestid(387291, "8629676443251475535", 11058189790)
+
+-- DLC's (with depot keys)
+-- Ori and the Blind Forest (Original Soundtrack) (AppID: 465980)
+addappid(465980)
+addappid(465980, 1, "3ebeb57fc575aba316d19e27a787813044dddfb95ee036dab06fdf4f454550c1")
+setManifestid(465980, "1673698342912223461", 942323956)
+-- Ori and the Blind Forest (Additional Soundtrack) (AppID: 466390)
+addappid(466390)
+addappid(466390, 1, "cbc8339aee28cc9461aa44f0509230f175fbccef2ae0fc1eac84409a060e55e5")
+setManifestid(466390, "571191038733822378", 681095304)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- (windows)
