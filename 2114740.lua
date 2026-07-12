c63de4c8ffd65df1b93927779e96dae99ec9086a
qiao-lima <qiao-lima@github.com>
2026-07-12 23:36:45 +0800

    Manual Update 2114740

---
+++ 2114740.lua
@@ -0,0 +1,31 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2114740 - Blasphemous 2
+-- Generated 2026-07-12 13:56:34 UTC
+-- # Depots (Total/DLC/Shared): 6/3/1
+
+-- Main AppID
+addappid(2114740, 1, "d0cee23f4516768ad92273125718aba5c92b538394b7f0736ca0fa8370b00b27")
+
+-- Main Depots
+addappid(2114741, 1, "bfa23044329db73bff7359d046baad4e5d546333e7c34042a0065de17eb5350e")
+setManifestid(2114741, "3607367436135451382", 4231849749)
+addappid(2114742, 1, "a0950a724a09a3bc1a126e4543d910aafa0e83727704db0fd11745547ea0bd66")
+setManifestid(2114742, "8954525284424133301", 13197618)
+
+-- DLC's (no depot keys required)
+addappid(2879370) -- Blasphemous 2 - Mea Culpa
+
+-- DLC's (with depot keys)
+-- Blasphemous 2 - Digital Artbook (AppID: 2500160)
+addappid(2500160)
+addappid(2500160, 1, "503b5732abefe07001d4c001350558da4747a61c52562da4882858a9e2be2235")
+setManifestid(2500160, "1591885379887391557", 130182835)
+-- Blasphemous 2 - OST (AppID: 2500170)
+addappid(2500170)
+addappid(2500171, 1, "b9d3fa57528bb80453d54ddd9ac898b8d48e37176b1eaad3e8474b7b738c533d")
+setManifestid(2500171, "4007254842697346922", 393211188)
+addappid(2500172, 1, "ef49e4da832ef9aace023b93a7e0bd9129f58b731032bc2422e64e41a2505370")
+setManifestid(2500172, "2569618554006666471", 2772045466)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
