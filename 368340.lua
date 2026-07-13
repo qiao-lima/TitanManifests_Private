c812890a25c6e4a419922aff61ce56e200dd4671
qiao-lima <qiao-lima@github.com>
2026-07-14 00:55:37 +0800

    Manual Update 368340

---
+++ 368340.lua
@@ -0,0 +1,54 @@
+-- Generated with Luie @ https://lua.tools/
+-- 368340 - CrossCode
+-- Generated 2026-07-13 16:51:33 UTC
+-- # Depots (Total/DLC/Shared): 17/8/2
+
+-- Main AppID
+addappid(368340, 1, "20f8c2bcae5ffe899e49afe1c1a5f3223c0e7c4d8e6d65fccb7e35c6cd905eed")
+
+-- Main Depots
+addappid(368341, 1, "f23ec1f50724b261be734d4179c7638e3bad44067a4ad070210f260dac081d2e") -- (windows)
+setManifestid(368341, "35841010773456973", 1000640296)
+addappid(368342, 1, "0b9e73770000cb7ae009e6b907f827288b175a78a8d2562ffe2b985d955f1874") -- (linux, 32-bit)
+setManifestid(368342, "600430034998069468", 1222911502)
+addappid(368343, 1, "445122f9b4ed2cb291a49ff246a8d03c6f89e927cd026f4fc4eb519dca1e4362") -- (linux, 64-bit)
+setManifestid(368343, "4826422111242041856", 1224208954)
+addappid(368344, 1, "42ad4456bd022dd7c6f41479aec445991912d7aba0ce9b8f1d918670f462c13f") -- (macos)
+setManifestid(368344, "4969633018319200679", 1151954660)
+addappid(368349, 1, "d41585e4667c7f44a4a6c3c159275b7e4e1c42884d038fc7437d65f9fec1ea9b") -- (windows, 64-bit)
+setManifestid(368349, "4290908809258086474", 1022228384)
+addappid(916190, 1, "a6b297bd0c53fce42612ef9a3d06d11a25802b3e24319123f5c4ab1511181839")
+setManifestid(916190, "9078418595481119991", 349193372)
+
+-- DLC's (no depot keys required)
+addappid(916190) -- CrossCode Soundtrack
+
+-- DLC's (with depot keys)
+-- CrossCode - Ninja Skin (AppID: 960310)
+addappid(960310)
+addappid(916191, 1, "48b6b1143c3b8acb339bbf9ced98dc5d3626484b6bfa1bfc2a327a196de173c9") -- (macos)
+setManifestid(916191, "9153021691655009916", 118902)
+addappid(960310, 1, "74b7751678a077549deb0888ef55ce3090a8d9aade2f69fb062ea8f8bbfc3539")
+setManifestid(960310, "1938747516704495851", 118902)
+-- A New Home (AppID: 1517030)
+addappid(1517030)
+addappid(1517030, 1, "a11276ae85025542d7566dbe859762a35a68dd32561f5a6ad802d4090fa1fad9")
+setManifestid(1517030, "3380250610448322243", 40939243)
+addappid(1517031, 1, "f41df4910f95ce7e1efcf62e2e4b88e733937983cb064c499713928d56c016d6") -- (macos)
+setManifestid(1517031, "3433527923833667925", 40939243)
+-- CrossCode Original Soundtrack EX (AppID: 1517040)
+addappid(1517040)
+addappid(1517040, 1, "0f4f085fad241a267ec1277c76f2e9c9930088e21a0e01bb66be3048a294935d")
+setManifestid(1517040, "8410561209917648276", 295822389)
+addappid(1517041, 1, "9d544c96926f7884a67a0a3f2f282915c82b4e500b3224b13033145a59de09f3")
+setManifestid(1517041, "2187128933972727071", 1098386690)
+-- CrossCode Manlea Skin (AppID: 1585790)
+addappid(1585790)
+addappid(1585790, 1, "31f0be66fa28b4de35fe2fd80a9075a265323de025dec5b703c8edec4a8c43b7")
+setManifestid(1585790, "2845237873865804524", 1119404)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 368345, 1585791
