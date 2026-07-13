70cc9c2547d6af1a55cf3abb55f7f2e107e4de65
qiao-lima <qiao-lima@github.com>
2026-07-14 00:55:36 +0800

    Manual Update 260230

---
+++ 260230.lua
@@ -0,0 +1,21 @@
+-- Generated with Luie @ https://lua.tools/
+-- 260230 - Valiant Hearts: The Great War™ / Soldats Inconnus : Mémoires de la Grande Guerre™
+-- Generated 2026-07-13 16:04:28 UTC
+-- # Depots (Total/DLC/Shared): 6/0/4
+
+-- Main AppID
+addappid(260230)
+
+-- Main Depots
+addappid(260231, 1, "6c41bd4691a0b3d82697e2b41689bb5b94b99f6d250f162227b1cd98bd902c4a") -- (windows)
+setManifestid(260231, "4720306033921821350", 1233865646)
+addappid(260232, 1, "97b31d3ec1f40ad40fb9da61feb01eed731737b8f46453173d6d1999f1c9e2d9") -- Japanese (windows)
+setManifestid(260232, "3781811066406191622", 1233865646)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+addappid(406181, 1, "419e422f533fe69e81eaec351533ea335f44eaa94535e1c50e9ef31ac08e081f")
+setManifestid(406181, "314289967746074236", 66502828)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
+setManifestid(1716751, "8936186649444731600", 264933784)
