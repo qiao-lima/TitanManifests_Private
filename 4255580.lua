b19cb1e576dbfbd6dfeb48600893b00c15f41da6
qiao-lima <qiao-lima@github.com>
2026-07-13 16:33:16 +0800

    Manual Update 4255580

---
+++ 4255580.lua
@@ -0,0 +1,14 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4255580 - Mouse X
+-- Generated 2026-07-13 08:28:15 UTC
+-- # Depots (Total/DLC/Shared): 2/0/1
+
+-- Main AppID
+addappid(4255580, 1, "105b378e77b9cad7b861c872aa5f4e7e85e678340727b614da2a84fe169c9290")
+
+-- Main Depots
+addappid(4255581, 1, "aa4c00e5e89a211d2168823cfc0ab5d8080fb921d46daf7a498ed6d9b1d6a6f9")
+setManifestid(4255581, "5934864511575795904", 182679270)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
