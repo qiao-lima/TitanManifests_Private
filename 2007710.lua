147b7d207f52b56164945bd2f25f7212e6ffd484
qiao-lima <qiao-lima@github.com>
2026-07-14 00:55:35 +0800

    Manual Update 2007710

---
+++ 2007710.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2007710 - Betrix
+-- Generated 2026-07-13 16:52:51 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(2007710, 1, "32ae5686f0d53c58bc3999e87f7bc8e05142d2aec5bba64968f2ffcb3df660e2")
+
+-- Main Depots
+addappid(2007713, 1, "7a0aa3af848776e9cab0fd8a00caace9566b9bb5ad43e1a4e2723e958e71a88c") -- (windows)
+setManifestid(2007713, "3943806785739698781", 319578152)
+addappid(2007714, 1, "fedcdfe1e93067cefc1cec92abbcbd3505285d7f2276fe3c3b63a91ec3a097de") -- (windows)
+setManifestid(2007714, "6923043240716021486", 316564876)
