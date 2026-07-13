3754dc37735d057ded585ee448ac9f01ff675030
qiao-lima <qiao-lima@github.com>
2026-07-14 00:55:34 +0800

    Manual Update 1188930

---
+++ 1188930.lua
@@ -0,0 +1,20 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1188930 - Chrono Ark
+-- Generated 2026-07-13 16:49:59 UTC
+-- # Depots (Total/DLC/Shared): 4/0/3
+
+-- Main AppID
+addappid(1188930, 1, "7f77f7fbabbe52e9a96a377b6232e8686c81ac910e9425074b76f8fb5f8bdfd7")
+
+-- Main Depots
+addappid(1188931, 1, "2569525f62e7ec50cd4c99a87ac4c92412b017bef46ec0ebdee283c482bf438e")
+setManifestid(1188931, "6076301055459309929", 10139479735)
+
+-- DLC's (no depot keys required)
+addappid(2780000) -- Chrono Ark - Summer Twilight
+addappid(3193720) -- Chrono Ark - High Roller
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") -- (windows)
