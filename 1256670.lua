12c540a3a3ff83b6d8a0f9a1cfc1f42e4a629d4f
qiao-lima <qiao-lima@github.com>
2026-07-14 00:55:34 +0800

    Manual Update 1256670

---
+++ 1256670.lua
@@ -0,0 +1,23 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1256670 - Library Of Ruina
+-- Generated 2026-07-13 16:00:13 UTC
+-- # Depots (Total/DLC/Shared): 4/3/0
+
+-- Main AppID
+addappid(1256670, 1, "355c4d26e35405d2a7824b7a237c77b03f2da7856671bcb62383ead42108f5f9")
+
+-- Main Depots
+addappid(1256671, 1, "00c735126632c6eadd65b68ea7eda8c5b53533b7667a651747c006bd102491fb") -- (64-bit)
+setManifestid(1256671, "96563032411238562", 8106423933)
+
+-- DLC's (with depot keys)
+-- Library Of Ruina Soundtrack (AppID: 1658720)
+addappid(1658720)
+addappid(1658721, 1, "28eb5e8b390f5173c20a6505ce0a6646cf9b1380308aea71554ca1bdf6b0d0a1")
+setManifestid(1658721, "26521293845563082", 484259597)
+addappid(1658722, 1, "c4aee7604d49e5bf26413b1bea7882971c6fb538e3224ce0cac2887f18a583a3")
+setManifestid(1658722, "3398338911117558383", 1486625871)
+-- ArtBook (AppID: 1735680)
+addappid(1735680)
+addappid(1735680, 1, "7eec92553b09b2fd957f4be63eae32b05f0a496d42c8b311aea081606362b81c")
+setManifestid(1735680, "8865581939697287877", 1722163977)
