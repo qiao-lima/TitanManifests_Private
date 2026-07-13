56e6c9304a5e7e52b38a73f679776e7eaca7fd0c
qiao-lima <qiao-lima@github.com>
2026-07-14 00:59:32 +0800

    Manual Update 895870

---
+++ 895870.lua
@@ -0,0 +1,29 @@
+-- Generated with Luie @ https://lua.tools/
+-- 895870 - Project Wingman
+-- Generated 2026-07-13 16:37:18 UTC
+-- # Depots (Total/DLC/Shared): 8/3/4
+
+-- Main AppID
+addappid(895870)
+
+-- Main Depots
+addappid(895871, 1, "baec9f1a727613a15b8f270c022c13daaf28f3558205a542970c8f8cb3ee7569") -- (windows, 64-bit)
+setManifestid(895871, "2208154344462263035", 17847353077)
+
+-- DLC's (with depot keys)
+-- Project Wingman Soundtrack (AppID: 1476780)
+addappid(1476780)
+addappid(1476781, 1, "fe1041432da6a78d353dd5d68954e7357ad736baa768d8050066c297c1dbec9f")
+setManifestid(1476781, "1415850630920007137", 181126536)
+addappid(1476782, 1, "d68f490bf3e33ba5e91713432a6b8d43a4bb29097f6a4676734acc5febfb222f")
+setManifestid(1476782, "2305208602942741459", 1362303287)
+-- Project Wingman - Frontline 59 Campaign (AppID: 2536180)
+addappid(2536180)
+addappid(2536180, 1, "5288aed61347456ecab313d510bea0e96f2cef2694172a9bc427278a45e518b6") -- (windows, 64-bit)
+setManifestid(2536180, "8519105561406181350", 13)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- (windows)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- (windows)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
