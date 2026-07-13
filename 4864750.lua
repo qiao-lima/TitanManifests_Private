8ddb3ff700377140ac8f5dfc31db70e7493e15ca
qiao-lima <qiao-lima@github.com>
2026-07-14 00:55:39 +0800

    Manual Update 4864750

---
+++ 4864750.lua
@@ -0,0 +1,21 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4864750 - Ultimate Guess Game
+-- Generated 2026-07-13 16:54:36 UTC
+-- # Depots (Total/DLC/Shared): 4/1/2
+
+-- Main AppID
+addappid(4864750)
+
+-- Main Depots
+addappid(4864751, 1, "9cb263f86adf2af6ff8d277855fb5698f74c5b7cd667c00988f7cb1de1614ce5")
+setManifestid(4864751, "1518893609081345071", 3933173915)
+
+-- DLC's (with depot keys)
+-- Ultimate Guess Game Demo (AppID: 4869330)
+addappid(4869330)
+addappid(4869331, 1, "075f2c0dfb636e04cfa5d5da810df6f13e9563bee76214246b3d2eb69b9448aa")
+setManifestid(4869331, "6097427700658032384", 929548984)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
