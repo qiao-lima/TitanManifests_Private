a38e01dd795775a615316879f4b6eb6442b7cf0e
qiao-lima <qiao-lima@github.com>
2026-07-15 04:17:05 +0800

    Manual Update 3834090

---
+++ 3834090.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3834090 - YAPYAP
+-- Generated 2026-07-14 19:46:55 UTC
+-- # Depots (Total/DLC/Shared): 4/2/0
+
+-- Main AppID
+addappid(3834090, 1, "3240b2f5335e98c02f19e4c3ea220bd9c0e246edc28e4032308967d9d7df2ab8")
+
+-- Main Depots
+addappid(3834092, 1, "437181cac3669d1157a669c0fb4505c722725953180acd1511151a82cbc61667") -- (windows)
+setManifestid(3834092, "5476067415113430058", 2696651290)
+addappid(3834093, 1, "9a496ac01103d88e35eb7c21d96470a166a67dc7fe43abc793c2daaf813be511") -- (macos)
+setManifestid(3834093, "3056009424220934000", 2755124541)
+
+-- Missing Depots (We don't have the keys yet lol): 4385901, 4385902
