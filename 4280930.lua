c292371fb8929c5da41a1bbb9be17f2f5009d3c5
qiao-lima <qiao-lima@github.com>
2026-07-15 04:17:06 +0800

    Manual Update 4280930

---
+++ 4280930.lua
@@ -0,0 +1,19 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4280930 - Warhammer 40,000: Fire Warrior (Classic)
+-- Generated 2026-07-14 19:53:59 UTC
+-- # Depots (Total/DLC/Shared): 6/0/0
+
+-- Main AppID
+addappid(4280930)
+
+-- Main Depots
+addappid(4280931, 1, "88a25c2af373fb8b6f40ec3319e259248e8fbf3e725345d3f92120f6f003e7ae")
+setManifestid(4280931, "3081356026109578856", 1556876647)
+addappid(4280932, 1, "8ab00c72cd2476a8568121b91d34c1c374a0b504e0137fb8ee2a89d29547f676") -- French (windows)
+setManifestid(4280932, "6066145995791864772", 1556874742)
+addappid(4280934, 1, "dd9b2ab1f01db42001a00860384585d9c0bf5b7c61631000437a9e9a4f2abe01") -- German (windows)
+setManifestid(4280934, "8008862670765038516", 1556874742)
+addappid(4280935, 1, "a9b4549eed0ced246bb8dcbc30e48399cab20519eae2c595b5046989edb23de5") -- Spanish (windows)
+setManifestid(4280935, "3208622324167880967", 1556874742)
+
+-- Missing Depots (We don't have the keys yet lol): 4280933, 4280936
