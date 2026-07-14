b9162cbca6855bea976fa088a3ec65bc4bbcf5bc
qiao-lima <qiao-lima@github.com>
2026-07-15 04:16:52 +0800

    Manual Update 2849080

---
+++ 2849080.lua
@@ -0,0 +1,23 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2849080 - Kingdom Rush 5: Alliance TD
+-- Generated 2026-07-14 20:05:00 UTC
+-- # Depots (Total/DLC/Shared): 4/0/1
+
+-- Main AppID
+addappid(2849080, 1, "801898d184b5ad1b3c57c07d4324e25c122aae717f42aa702b1b616396086299")
+
+-- Main Depots
+addappid(2849081, 1, "e00df48b38c967f303c37f758389ff8e15505ab4416c522ea62ef425884d3849")
+setManifestid(2849081, "2986433879215574901", 4090472)
+addappid(2849082, 1, "4982ecd61fd529c34e6519a1a5004121f93ebb0e60d14d161fa95e4851ea8a32") -- (windows)
+setManifestid(2849082, "4575449103611673528", 1629563767)
+addappid(2849083, 1, "3d8ca2d9a2a948ec35d8046112d2bf42f03f46ae30dae1b842c8ec4dd26d285f") -- (macos)
+setManifestid(2849083, "2237783188623953254", 1640364261)
+
+-- DLC's (no depot keys required)
+addappid(3368630) -- Kingdom Rush 5: Alliance TD - Colossal Dwarfare Campaign
+addappid(3732970) -- Kingdom Rush 5: Alliance TD - Wukong's Journey Campaign
+addappid(4260510) -- Kingdom Rush 5: Alliance TD - Dragon Wars Campaign
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
