b2770b6fbd1ee7b572fcc551b5a3c5ffa3243e58
qiao-lima <qiao-lima@github.com>
2026-07-15 04:17:20 +0800

    Manual Update 616720

---
+++ 616720.lua
@@ -0,0 +1,27 @@
+-- Generated with Luie @ https://lua.tools/
+-- 616720 - Live2DViewerEX
+-- Generated 2026-07-14 19:42:37 UTC
+-- # Depots (Total/DLC/Shared): 7/1/4
+
+-- Main AppID
+addappid(616720, 1, "99529225ccd19a436d52b19894a9a5d24f0559ec79e9384570a58f11d9871727")
+
+-- Main Depots
+addappid(616721, 1, "34f7b93e924c5d236bba8833fd04d9489e5a55fc76bda6992d5e65eb4cfce6c3") -- (windows)
+setManifestid(616721, "973820133284247421", 2855949085)
+addappid(616722, 1, "9e382fdb223d32adfcb889f34734eb3e9d6097b029872159427c233d04928ff3") -- (macos, 64-bit)
+setManifestid(616722, "86255310388151035", 1491161823)
+
+-- DLC's (no depot keys required)
+addappid(641880) -- Live2D Wallpaper - [Effect] Rain and Snow
+addappid(642270) -- Live2D Wallpaper - [Widget] Digital Clock
+addappid(652840) -- Live2D Wallpaper - [Widget] Analog Clock
+addappid(1732010) -- Live2DViewerEX - Spine Expansion
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- (windows)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 1350680
