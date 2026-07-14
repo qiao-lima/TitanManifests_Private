3d5f46fcd4c6613a9cb13a946eaa3bdac8d53f64
qiao-lima <qiao-lima@github.com>
2026-07-15 04:17:09 +0800

    Manual Update 4634560

---
+++ 4634560.lua
@@ -0,0 +1,19 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4634560 - 絨毯を洗うゲーム
+-- Generated 2026-07-14 20:13:12 UTC
+-- # Depots (Total/DLC/Shared): 4/2/0
+
+-- Main AppID
+addappid(4634560)
+
+-- Main Depots
+addappid(4634561, 1, "fee095e703be6ccf094d44bf923b89d451931ea21bbd1b1e2ef3dbe28021a995") -- (windows,linux)
+setManifestid(4634561, "4889809406804776470", 1370711527)
+
+-- DLC's (with depot keys)
+-- Carpet Cleaning Simulator Demo (AppID: 4798550)
+addappid(4798550)
+addappid(4798551, 1, "bbec5aad62b66682c47283b1422991a1de19c62a2eb3a2bbbb7fc8cf96561e99") -- (windows,linux)
+setManifestid(4798551, "6431649751086405427", 1370974194)
+
+-- Missing Depots (We don't have the keys yet lol): 4634562, 4798552
