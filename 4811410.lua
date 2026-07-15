bdbb66af11617ca3dca5708bc693b7958f4bbd6d
qiao-lima <qiao-lima@github.com>
2026-07-16 05:16:44 +0800

    Manual Update 4811410

---
+++ 4811410.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4811410 - Zombie Queen
+-- Generated 2026-07-15 20:23:47 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4811410)
+
+-- Main Depots
+addappid(4811411, 1, "97e7eca5e9c7586f9dd8d71127929bf4dec997d92895895ca20222740d3c0161") -- (windows)
+setManifestid(4811411, "7766959546947396114", 4999858894)
