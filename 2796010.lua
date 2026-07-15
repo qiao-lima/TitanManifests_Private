b8adcb2d125d88f3de20ac28e554755ed8067960
qiao-lima <qiao-lima@github.com>
2026-07-15 22:26:24 +0800

    Manual Update 2796010

---
+++ 2796010.lua
@@ -0,0 +1,18 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2796010 - Party Club
+-- Generated 2026-07-15 14:19:05 UTC
+-- # Depots (Total/DLC/Shared): 4/0/0
+
+-- Main AppID
+addappid(2796010, 1, "e5ba5a3ec46bb3b341c4c096e62b62f3de20b6cc59b7bd8af9342ea328e9afa4")
+
+-- Main Depots
+addappid(2796011, 1, "5ae77169c533cca90d65bf5be54cc67554c4e7e6b9f2dc23e0bf4c158e0150ee") -- (windows)
+setManifestid(2796011, "6351305368969441347", 5908711088)
+addappid(2796012, 1, "8d9d5ca34932047596b7e438cc6e1ab794896b64808ba7c131aecbfde70769ef") -- (macos)
+setManifestid(2796012, "8451904875102828417", 6140366423)
+
+-- DLC's (no depot keys required)
+addappid(3913830) -- Party Club - Mini Mayhem DLC
+
+-- Missing Depots (We don't have the keys yet lol): 2796013, 2796014
