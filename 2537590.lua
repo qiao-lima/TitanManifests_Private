a85ef5d1ccf707c8bd6abbafcef9e22861b4ac03
qiao-lima <qiao-lima@github.com>
2026-07-15 22:26:24 +0800

    Manual Update 2537590

---
+++ 2537590.lua
@@ -0,0 +1,21 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2537590 - Microsoft Flight Simulator 2024
+-- Generated 2026-07-15 14:15:02 UTC
+-- # Depots (Total/DLC/Shared): 2/0/1
+
+-- Main AppID
+addappid(2537590, 1, "f264fcd2580ab71934f29046dc415c3a83163882e768246533ed1f20f1730835")
+
+-- Main Depots
+addappid(2537591, 1, "a149759cc259406fd0f2255297e089d9deb63fb59c263b7f2d1b7fcd7aded1a4")
+setManifestid(2537591, "1785542634196301496", 9449436474)
+
+-- DLC's (no depot keys required)
+addappid(3227340) -- Limitless - Preorder Bonus
+addappid(3245690) -- Microsoft Flight Simulator 2024 - Aviator
+addappid(3245700) -- Microsoft Flight Simulator 2024 Deluxe
+addappid(3245710) -- Microsoft Flight Simulator 2024 - Premium Deluxe
+addappid(3341960) -- Microsoft Flight Simulator 2024 - Premium Deluxe Upgrade
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
