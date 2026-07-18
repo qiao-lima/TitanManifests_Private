afa61c7533e47d886ca5db099e8d41c65e84ec58
qiao-lima <qiao-lima@github.com>
2026-07-18 17:27:30 +0800

    Manual Update 3816420

---
+++ 3816420.lua
@@ -0,0 +1,15 @@
+-- 3816420's Lua and Manifest Created by Hubcap Manifest
+-- Cafe Crew Simulator
+-- Created: July 16, 2026 at 03:24:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3816420) -- Cafe Crew Simulator
+-- MAIN APP DEPOTS
+addappid(3816421, 1, "d9199023776409b108ba98ac01cb3454446e4c317109b20ab1a1e41c5fe9a9c3") -- Depot 3816421
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
