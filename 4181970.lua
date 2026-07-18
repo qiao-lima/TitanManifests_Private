4aa2ea3a5bd4c25c50b227ea9e2299231bbeeadc
qiao-lima <qiao-lima@github.com>
2026-07-18 17:27:31 +0800

    Manual Update 4181970

---
+++ 4181970.lua
@@ -0,0 +1,15 @@
+-- 4181970's Lua and Manifest Created by Hubcap Manifest
+-- Full Stride
+-- Created: July 17, 2026 at 23:38:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4181970, 1, "68c7c9750f85b49c6aa38f384801be22bf8534f1b22afef3f51fda327f98c2da") -- Full Stride
+-- MAIN APP DEPOTS
+addappid(4181971, 1, "ea47f5b16048ef89138b0826ea5e790b9db5422300c6526a8e6352383ee9961f") -- Depot 4181971
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
