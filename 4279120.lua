7f7190032b0408c465f76b3e44dbd2327f89ed3b
qiao-lima <qiao-lima@github.com>
2026-07-22 05:06:23 +0800

    Manual Update 4279120

---
+++ 4279120.lua
@@ -0,0 +1,16 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4279120 - Budgie's Bug Shop
+-- Generated 2026-07-21 19:50:26 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(4279120)
+
+-- Main Depots
+addappid(4279121, 1, "2940610b2d663199860a3f2f6827c7e54f966cb96b00ac986f5fb9e91ead5ba4")
+setManifestid(4279121, "4391358865542673072", 485126963)
+
+-- DLC's (no depot keys required)
+addappid(4760600) -- Budgie's Bug Shop - Supporter Pack
+
+-- Missing Depots (We don't have the keys yet lol): 4279122
