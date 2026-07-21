4e0a7353666b77fc49e3fbed80bbf5523cf5ba8f
qiao-lima <qiao-lima@github.com>
2026-07-22 05:06:53 +0800

    Manual Update 798840

---
+++ 798840.lua
@@ -0,0 +1,21 @@
+-- Generated with Luie @ https://lua.tools/
+-- 798840 - Panzer War: Definitive Edition
+-- Generated 2026-07-21 20:32:06 UTC
+-- # Depots (Total/DLC/Shared): 4/1/1
+
+-- Main AppID
+addappid(798840, 1, "cbeb0611bd2afc590f8e06791c7a52c570b71ea85d05306337eb28451615b3ff")
+
+-- Main Depots
+addappid(798841, 1, "b76b149da694cf2ce80690e30879f3eeb86839127d379a035a7bfeac8fc3bc87") -- (windows)
+setManifestid(798841, "1040273434566074812", 9343649791)
+addappid(798842, 1, "93b94c8e57323e45a9d1a4da0f1f948463b7c853336f0bb812455479ab031a7a") -- (macos)
+setManifestid(798842, "9098025065693783228", 1291359555)
+
+-- DLC's (with depot keys)
+-- Panzer War (AppID: 1019010)
+addappid(1019010)
+addappid(1019010, 1, "028184b23abeefba817fc00511303f0bf14688df05a8eb25ac850f2f798f7ff3")
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
