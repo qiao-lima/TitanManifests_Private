7e7c0da833bb514501b8163d7fbdaffc619ea7c0
qiao-lima <qiao-lima@github.com>
2026-07-22 05:05:38 +0800

    Manual Update 2162800

---
+++ 2162800.lua
@@ -0,0 +1,23 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2162800 - shapez 2 - Factory
+-- Generated 2026-07-21 19:26:21 UTC
+-- # Depots (Total/DLC/Shared): 5/0/2
+
+-- Main AppID
+addappid(2162800, 1, "615a243c485abe945a4ff4a2a3e6a1663325ef15432b30f2f772d747cb1a0d2f")
+
+-- Main Depots
+addappid(2162802, 1, "163ae6ae731b47cb94170aee0ed8a4d4ecc62c6082bc319cc270e20c5eb0f759") -- (windows, 64-bit)
+setManifestid(2162802, "1468933197849336051", 3392719909)
+addappid(2162803, 1, "9b7c2e9a0f8d2a28b522233286436b360fffb4830d45a69a5a0bf35573da8647") -- (linux)
+setManifestid(2162803, "3949648320862048176", 3434727114)
+addappid(2162804, 1, "aba21c81613972e41bf5e0131f382113d13fbdee073a02f7065cfc4256630232") -- (macos)
+setManifestid(2162804, "3146392886771591485", 3412247157)
+
+-- DLC's (no depot keys required)
+addappid(3075620) -- shapez 2 Supporter Edition Upgrade
+addappid(4746220) -- Shapez 2 - Hyper Skin DLC
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
