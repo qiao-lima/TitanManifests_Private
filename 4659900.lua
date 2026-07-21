55f6710059c50339f6b4461d6eb45973eef799c3
qiao-lima <qiao-lima@github.com>
2026-07-22 05:06:35 +0800

    Manual Update 4659900

---
+++ 4659900.lua
@@ -0,0 +1,12 @@
+-- 4659900's Lua and Manifest Created by Hubcap Manifest
+-- Monitor
+-- Created: July 17, 2026 at 05:24:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4659900) -- Monitor
+-- MAIN APP DEPOTS
+addappid(4659901, 1, "20fdabcc22c28f3c65eb8596aa1a24dea505cf020c1497fbe8a889c5ad616851") -- Depot 4659901
+addappid(4659902, 1, "4be600ac2d7ceae18f893c161ab1b7b8109fdef20c63baf8cf1ef858573c44b5") -- Depot 4659902
