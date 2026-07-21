a86bef22618db311cc2e9323c5a4dcd811d06d68
qiao-lima <qiao-lima@github.com>
2026-07-22 05:06:34 +0800

    Manual Update 4599890

---
+++ 4599890.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4599890 - Hong Kong Horror Incident: Yik Sin House
+-- Generated 2026-07-21 20:39:39 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4599890)
+
+-- Main Depots
+addappid(4599891, 1, "5d86ed099afc8004a35b25aa2fa56d4eb5b754dc4c1b4402aae223f86be564af")
+setManifestid(4599891, "938878033562028680", 2767066896)
