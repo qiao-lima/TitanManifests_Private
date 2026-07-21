6bcd9ac7ac7f5efc06307bce60fe0cc0b9ac89de
qiao-lima <qiao-lima@github.com>
2026-07-22 05:06:07 +0800

    Manual Update 3417490

---
+++ 3417490.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3417490 - With Eyes Closed - Season 2
+-- Generated 2026-07-21 19:50:22 UTC
+-- # Depots (Total/DLC/Shared): 2/1/0
+
+-- Main AppID
+addappid(3417490)
+
+-- Main Depots
+addappid(3417491, 1, "e128044fbc2a624255ddd234282d8910e7847722d30f9031581336dfea4fed96")
+setManifestid(3417491, "6484562650162669546", 5690368546)
+
+-- DLC's (with depot keys)
+-- With Eyes Closed - S. 2 - Deluxe Edition (AppID: 4753630)
+addappid(4753630)
+addappid(4753630, 1, "7c75b23b76f6970904e771e6cb59c3b2a44fdafa43c91a6524b10a327a24a73d")
+setManifestid(4753630, "7064233265325664290", 9708077135)
