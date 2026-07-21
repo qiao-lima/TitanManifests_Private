e193f2f06d6db44a166b6c872687d449242e85db
qiao-lima <qiao-lima@github.com>
2026-07-22 05:05:20 +0800

    Manual Update 1372280

---
+++ 1372280.lua
@@ -0,0 +1,50 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1372280 - MELTY BLOOD: TYPE LUMINA
+-- Generated 2026-07-21 16:40:06 UTC
+-- # Depots (Total/DLC/Shared): 4/1/2
+
+-- Main AppID
+addappid(1372280, 1, "82443e6c2cf2abe527a833cf50fe938db6da38347d6ae0e72f9c1aac1934ec1e")
+
+-- Main Depots
+addappid(1372281, 1, "b4c236fa366c4f51e04430146cbfa0fd857d6bcb046c27c2989c4663dbbcbccc")
+setManifestid(1372281, "3667666581409166708", 22165110481)
+
+-- DLC's (no depot keys required)
+addappid(1645450) -- MELTY BLOOD: TYPE LUMINA - Arcueid Round Announcements
+addappid(1645452) -- MELTY BLOOD: TYPE LUMINA - Akiha Tohno Round Announcements
+addappid(1645460) -- MELTY BLOOD: TYPE LUMINA - Hisui Round Announcements
+addappid(1645470) -- MELTY BLOOD: TYPE LUMINA - Shiki Tohno Round Announcements
+addappid(1645480) -- MELTY BLOOD: TYPE LUMINA - Kohaku Round Announcements
+addappid(1645490) -- MELTY BLOOD: TYPE LUMINA - Roa Round Announcements
+addappid(1645500) -- MELTY BLOOD: TYPE LUMINA - Kouma Kishima Round Announcements
+addappid(1645510) -- MELTY BLOOD: TYPE LUMINA - Noel Round Announcements
+addappid(1645520) -- MELTY BLOOD: TYPE LUMINA - Vlov Round Announcements
+addappid(1645530) -- MELTY BLOOD: TYPE LUMINA - Red Arcueid Round Announcements
+addappid(1645540) -- MELTY BLOOD: TYPE LUMINA - Ciel Round Announcements
+addappid(1645550) -- MELTY BLOOD: TYPE LUMINA - Saber Round Announcements
+addappid(1649930) -- MELTY BLOOD: TYPE LUMINA - Miyako Arima Round Announcements
+addappid(1656770) -- MELTY BLOOD: TYPE LUMINA - Round Announcements - 13 Character Set
+addappid(1810560) -- MELTY BLOOD: TYPE LUMINA - Dead Apostle Noel Round Announcements
+addappid(1810561) -- MELTY BLOOD: TYPE LUMINA - Dead Apostle Noel Playable Character
+addappid(1810562) -- MELTY BLOOD: TYPE LUMINA - Aoko Aozaki Round Announcements
+addappid(1810563) -- MELTY BLOOD: TYPE LUMINA - Aoko Aozaki Playable Character
+addappid(1918660) -- DLC 1918660
+addappid(1918661) -- MELTY BLOOD: TYPE LUMINA - Powered Ciel Round Announcements
+addappid(1918662) -- DLC 1918662
+addappid(1918663) -- MELTY BLOOD: TYPE LUMINA - Mario Round Announcements
+addappid(2073610) -- DLC 2073610
+addappid(2111960) -- MELTY BLOOD: TYPE LUMINA - Neco-Arc Round Announcements
+addappid(2111970) -- MELTY BLOOD: TYPE LUMINA - Mash Round Announcements
+addappid(2172450) -- MELTY BLOOD: TYPE LUMINA - Ushiwakamaru Round Announcements
+addappid(2172460) -- MELTY BLOOD: TYPE LUMINA - The Count of Monte Cristo Round Announcements
+
+-- DLC's (with depot keys)
+-- MELTY BLOOD ARCHIVES (AppID: 1590580)
+addappid(1590580)
+addappid(1590581, 1, "436fa82f9d7f9bffc15db902c75faa6562cbf24ba1bbc9b1125841e2e7f00826")
+setManifestid(1590581, "7271406536397019712", 1971023885)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
