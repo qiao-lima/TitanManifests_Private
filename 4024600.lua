5ea0e3e1886b966e2faa747b70c5329ed04b53d5
qiao-lima <qiao-lima@github.com>
2026-07-22 05:06:21 +0800

    Manual Update 4024600

---
+++ 4024600.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4024600 - Pyramidion
+-- Generated 2026-07-21 18:16:00 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(4024600)
+
+-- Main Depots
+addappid(4024601, 1, "d9ce5d99acdf4f818ebaa39cecabc0c43c367c1ad35dc498c2e47e4a8b05d5d0") -- (windows)
+setManifestid(4024601, "8445445900612339351", 1865048126)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
