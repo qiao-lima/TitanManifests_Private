23bc52b352af8207c72e77b20baabbe1534c7deb
qiao-lima <qiao-lima@github.com>
2026-07-22 05:05:20 +0800

    Manual Update 1364780

---
+++ 1364780.lua
@@ -0,0 +1,59 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1364780 - Street Fighter™ 6
+-- Generated 2026-07-21 20:15:08 UTC
+-- # Depots (Total/DLC/Shared): 13/10/2
+
+-- Main AppID
+addappid(1364780, 1, "ab1ae48f25d0e9ba9404def1dae8aac005918547d5d9007b5ed3a39b906e7ebd")
+
+-- Main Depots
+addappid(1364781, 1, "cfec39716b5a5cae49b52b5c0e01e6db93356ee3df5a0d87590fd811ab6d06e1") -- (windows)
+setManifestid(1364781, "4741141599989541719", 86803973402)
+
+-- DLC's (no depot keys required)
+addappid(2224460) -- Street Fighter™ 6 - Year 1 Character Pass
+addappid(2224461) -- Street Fighter™ 6 - Year 1 Ultimate Pass
+addappid(2224462) -- Street Fighter™ 6 - Outfit 1 Color 10 for Chun-Li, Jamie, Manon, Dee Jay, Juri, Ken
+addappid(2825190) -- Street Fighter™ 6 - Year 2 Character Pass
+addappid(2825200) -- Street Fighter™ 6 - Year 2 Ultimate Pass
+addappid(3401790) -- Street Fighter™ 6 - Years 1-2 Fighters Edition Stages & Colors
+addappid(3410460) -- Street Fighter™ 6 - Year 3 Character Pass
+addappid(3410470) -- Street Fighter™ 6 - Year 3 Ultimate Pass
+addappid(4412680) -- Street Fighter™ 6 - Year 4 Character Pass
+addappid(4412690) -- Street Fighter™ 6 - Year 4 Ultimate Pass
+
+-- DLC's (with depot keys)
+-- Street Fighter™ 6: Fighting Ground (AppID: 1792750)
+addappid(1792750)
+addappid(1792750, 1, "321dd0bd6e467195d8890c9073d183075de7f8a4faf082155b08ef85bea618fe") -- (windows, 64-bit)
+setManifestid(1792750, "6884397220835125615", 4643138331)
+-- Street Fighter™ 6: World Tour (AppID: 1792751)
+addappid(1792751)
+addappid(1792751, 1, "b47545f3d9f54a63bd3cfe4028c8510ad05aead03842627326793508414ba122")
+setManifestid(1792751, "6646887922669668163", 19285493264)
+-- Street Fighter 6 Original Soundtrack (AppID: 2494130)
+addappid(2494130)
+addappid(2494131, 1, "4cad42a4f5ab48f2c49a71395723f437914559578118219165f65d131fac5167")
+setManifestid(2494131, "6407060023653373289", 962085882)
+addappid(2494132, 1, "e58f379b7c3857b503401044ed721ce2d0a2ba0b75b68d0c87ce61076ffc4878")
+setManifestid(2494132, "7386490442528570958", 2987010001)
+addappid(2494133, 1, "82bd0a5aa321bdff13c7cce543767a50dd21580be39e692772c5fc34e3908eba")
+setManifestid(2494133, "5349065594207009189", 1207883337)
+addappid(2494134, 1, "9e00bb0df6110883572a24f842f9eb3a08735247dc4b1418a99b0c7472fbdb1b") -- Japanese
+setManifestid(2494134, "6544188785495151671", 962085882)
+addappid(2494135, 1, "3c726e4a890c3d635127788a197bd91f4ea762f2cb2f577bda01d5b3d49a21f9") -- Japanese
+setManifestid(2494135, "5621437127152251054", 2987010001)
+addappid(2494136, 1, "a595336a7b30c81540cc27b371413f79a9c7a8c72676e1264bfd3d2f6b5cca3e") -- Japanese
+setManifestid(2494136, "4424110270717929631", 1207883337)
+-- Street Fighter 6 Original Soundtrack [Year 1] (AppID: 3594250)
+addappid(3594250)
+addappid(3594251, 1, "e73ba7077c6d42f417db793fa6933742619b3f83eca3b5855f2fac7fcac46ad6")
+setManifestid(3594251, "4609326181719447985", 219426816)
+-- Street Fighter 6 Original Soundtrack [Year 2] (AppID: 3899360)
+addappid(3899360)
+addappid(3899361, 1, "5901F0EC7147BDF44836FEA4D38BE00F852FF8459D203331364B97B328959F94")
+setManifestid(3899361, "1464902885242760437", 114483840)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
