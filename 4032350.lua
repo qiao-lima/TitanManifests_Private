a6894fb40afd5a0ff6e0a34d53dd35836af1961c
qiao-lima <qiao-lima@github.com>
2026-07-22 05:06:22 +0800

    Manual Update 4032350

---
+++ 4032350.lua
@@ -0,0 +1,28 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4032350 - EA SPORTS™ College Football 27
+-- Generated 2026-07-21 20:52:00 UTC
+-- # Depots (Total/DLC/Shared): 4/0/2
+
+-- Main AppID
+addappid(4032350)
+
+-- Main Depots
+addappid(4032351, 1, "17be6970e71f1345441b6dfeecf4ff073dcd3d973a0eee6f57f3aeec7851c418")
+setManifestid(4032351, "1614867032873776702", 37034696585)
+addappid(4032352, 1, "44afc77d5880b620ca157f231d5531c9da16308d7fbaddd0423bad7f07de1ce0")
+setManifestid(4032352, "3568790069296037349", 14266300439)
+
+-- DLC's (no depot keys required)
+addappid(4041510) -- EA SPORTS™ College Football 27
+addappid(4113840) -- EA SPORTS™ College Football 27 Deluxe Edition Pre-order Content
+addappid(4113850) -- EA SPORTS™ College Football 27 Deluxe Edition Pre-order Content
+addappid(4113870) -- EA SPORTS™ College Football 27 - College Football Points
+addappid(4115150) -- College Football 27 - EA Play Trial Key
+addappid(4681330) -- EA SPORTS™ MVP Bundle Pre-order Content
+addappid(4681570) -- EA SPORTS™ College Football 27 Deluxe Edition
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(3340991, 1, "023daedb070e5af8704dc88ee0af829f5c11923d2e6a42cca11ba5713b9f4491")
+setManifestid(3340991, "2155225770093841476", 239536599)
+addappid(3893181, 1, "7f675c2fe8e758d16f3cf8d3b493956afaee78e96d78cdb668a5edb8ac1580f6")
+setManifestid(3893181, "5085763297239765731", 290610401)
