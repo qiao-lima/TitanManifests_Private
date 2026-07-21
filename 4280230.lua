11382e6562fa177d2c96fde9c59a700fbf846bbb
qiao-lima <qiao-lima@github.com>
2026-07-22 05:06:23 +0800

    Manual Update 4280230

---
+++ 4280230.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4280230 - Super Wash Simulator
+-- Generated 2026-07-21 20:34:09 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4280230)
+
+-- Main Depots
+addappid(4280231, 1, "a5f4837f3836f9bce1f412b455bebfe38730c6851fe8dc41393feb1606e9e45a")
+setManifestid(4280231, "7601804405116967444", 1253196016)
