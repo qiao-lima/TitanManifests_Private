73c576abffa859adc7cf1cfa42c9367d177abc7f
qiao-lima <qiao-lima@github.com>
2026-07-22 05:06:36 +0800

    Manual Update 4781990

---
+++ 4781990.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4781990 - Rain Station Z
+-- Generated 2026-07-19 14:13:59 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(4781990)
+
+-- Main Depots
+addappid(4781991, 1, "5149dc86ad06bf5eed6962f34425d06ceff420c5386e2b3cdefcef73ce9d2922")
+setManifestid(4781991, "3746798357381581076", 935284165)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
