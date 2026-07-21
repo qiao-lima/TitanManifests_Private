7a87e5942e861f3e104f8a2309bd4a33f2dd82f8
qiao-lima <qiao-lima@github.com>
2026-07-22 05:06:34 +0800

    Manual Update 442120

---
+++ 442120.lua
@@ -0,0 +1,54 @@
+-- Generated with Luie @ https://lua.tools/
+-- 442120 - Pinball FX Classic
+-- Generated 2026-07-21 16:11:55 UTC
+-- # Depots (Total/DLC/Shared): 5/0/4
+
+-- Main AppID
+addappid(442120, 1, "27c8c4874d7a9e6784b086e60d72e5139d610e5548002f1af776ea9a7175296a")
+
+-- Main Depots
+addappid(442121, 1, "08f0e8392d005c3071af9f81d139539c81112a47be754f2b16cf2fa317887b31")
+setManifestid(442121, "8440010022120168517", 7914105546)
+
+-- DLC's (no depot keys required)
+addappid(646660) -- Pinball FX Classic - Marvel Pinball Original Pack
+addappid(646661) -- Pinball FX Classic - Marvel Pinball Vengeance and Virtue Pack
+addappid(646662) -- Pinball FX Classic - Marvel Pinball Avengers Chronicles
+addappid(646663) -- Pinball FX Classic - Marvel's Women of Power
+addappid(646664) -- Pinball FX Classic - Star Wars™ Pinball
+addappid(646665) -- Pinball FX3 - Star Wars™ Pinball: Balance of the Force
+addappid(646666) -- Pinball FX Classic - Star Wars™ Pinball: Heroes Within
+addappid(646667) -- Pinball FX Classic - Star Wars™ Pinball: The Force Awakens Pack
+addappid(646668) -- Pinball FX Classic - Bethesda® Pinball
+addappid(646669) -- DLC 646669
+addappid(646670) -- Pinball FX Classic - Aliens vs Pinball
+addappid(646671) -- Pinball FX Classic - Iron & Steel Pack
+addappid(646672) -- Pinball FX Classic - Zen Classics
+addappid(646673) -- Pinball FX Classic - Core Collection
+addappid(657420) -- Pinball FX Classic - Marvel Pinball: Marvel Legends Pack
+addappid(657421) -- Pinball FX Classic - Marvel Pinball: Heavy Hitters
+addappid(657422) -- Pinball FX Classic - Marvel Pinball: Cinematic Pack
+addappid(657423) -- Pinball FX Classic - Medieval Pack
+addappid(657424) -- Pinball FX Classic - Sci-Fi Pack
+addappid(657430) -- Pinball FX Classic - Portal ® Pinball
+addappid(657431) -- Pinball FX Classic - The Walking Dead Pinball
+addappid(715110) -- Pinball FX Classic - Universal Classics™ Pinball
+addappid(718800) -- Pinball FX Classic - Star Wars™ Pinball:  Unsung Heroes
+addappid(750060) -- Pinball FX Classic - Carnivals and Legends
+addappid(782110) -- Pinball FX Classic - Jurassic World™ Pinball
+addappid(836820) -- Pinball FX Classic - Star Wars™ Pinball: The Last Jedi
+addappid(931640) -- Pinball FX Classic - Star Wars™ Pinball: Solo Pack
+addappid(947000) -- Pinball FX Classic - Williams™ Pinball: Volume 1
+addappid(984180) -- Pinball FX Classic - Williams™ Pinball: Volume 2
+addappid(1044440) -- Pinball FX Classic - Williams™ Pinball: Volume 3
+addappid(1086760) -- Pinball FX3 - Williams™ Pinball: Volume 4
+addappid(1167870) -- Pinball FX Classic - Williams™ Pinball: Universal Monsters Pack
+addappid(1203510) -- Pinball FX Classic - Williams™ Pinball: Volume 5
+addappid(1450220) -- Pinball FX Classic - Williams™ Pinball: Volume 6
+addappid(1849550) -- Pinball FX Classic - Indiana Jones™: The Pinball Adventure
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- (windows)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- (windows)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
