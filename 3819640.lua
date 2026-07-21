12ef3078800eef392ff6bce1c95787df4824c089
qiao-lima <qiao-lima@github.com>
2026-07-22 05:06:20 +0800

    Manual Update 3819640

---
+++ 3819640.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3819640 - Megastore Simulator
+-- Generated 2026-07-21 20:21:46 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3819640, 1, "c5ca5045f5114cca24f02d4de5da563ad6097c7bfb1dd698c0be703c24c47be9")
+
+-- Main Depots
+addappid(3819641, 1, "cd3a3b0b3ca76fc3fd11c8c5426cc433df4d406d30e4d4fce18621a4742cdd64")
+setManifestid(3819641, "6052603184629719394", 6624146127)
