6233b5f265f41ab2ddd75debfd9161f7e7a8fc47
qiao-lima <qiao-lima@github.com>
2026-07-22 05:06:52 +0800

    Manual Update 730

---
+++ 730.lua
@@ -0,0 +1,52 @@
+-- Generated with Luie @ https://lua.tools/
+-- 730 - Counter-Strike 2
+-- Generated 2026-07-21 19:38:40 UTC
+-- # Depots (Total/DLC/Shared): 17/1/2
+
+-- Main AppID
+addappid(730, 1, "2e61570111ad4d1e4abcb711576f96b84907a024f9f9bf4a4d692d7c132ee91b")
+
+-- Main Depots
+addappid(731, 1, "bca9a9cde94bb4dff61849c6a87230ee45867a590fdd28826366e35e7d62c08e")
+setManifestid(731, "6536786002765576612", 8)
+addappid(732, 1, "da1f76913633e9ce1b2bda5ec464dc507205388fac5c4c614b6a2706cdbd0912") -- (windows)
+setManifestid(732, "4577466991136481399", 8)
+addappid(733, 1, "23c8e9a91561a5fb08abf22f2ac98929f4aad68a12164268d3b2800c3db4fb03") -- (macos)
+setManifestid(733, "5657593506144251850", 8)
+addappid(734, 1, "54fd243c5463c16ec7d32b5b4c7b792b3f2d3f75009ac3e81135ff999b56187f") -- (linux)
+setManifestid(734, "1869835707985324168", 8)
+addappid(735, 1, "a0ef69f0cf8abd70aa9ed79755fbe4d98cf7c6723ba9e0c66e0f7102e49541e6")
+setManifestid(735, "991580581070944647", 8)
+addappid(736, 1, "e81988c241b4ec9fc7fe1ab34697689f131a48c78da95c985ed347fb43bdc6de")
+setManifestid(736, "7762389079851171652", 8)
+addappid(737, 1, "c80f96efa325374c18c6d5d63ccd2687b1680a908c6def5c5c808e695f1ddbf9")
+setManifestid(737, "7809142821051002113", 8)
+addappid(738, 1, "fbc1a292c9b75e33512ed82476267efbf34f1ed1b177651ed8b091cf7174d183")
+setManifestid(738, "3946504905135396911", 8)
+addappid(2347770, 1, "b23a737920b6a72f932a5bcdbdf51770d7d4d394a13f95b1cf29db28c1043d88") -- (64-bit)
+setManifestid(2347770, "7673916425787288234", 61167996728)
+addappid(2347771, 1, "b80e2b4bd2a244ba996a013850b5d2f46c897f382164181b382c5d672fcb5090") -- (windows)
+setManifestid(2347771, "1756429801267405217", 7662610199)
+addappid(2347772, 1, "7938abf64ef719e6414d5e385f90ccbef9c6a2c21ab42b471a09641d2b2ba32f") -- (macos)
+setManifestid(2347772, "4427303554736365309", 9276)
+addappid(2347773, 1, "d030f2b7078446257db4b4decd86a38b0ae3816b4847a04e609277e20f25d5d7") -- (linux, 64-bit)
+setManifestid(2347773, "4042805601783611185", 7172679239)
+addappid(2347774, 1, "71e79678830ea0234437b03526e2ede2decfed847ce20bae413718b47db44bea") -- (64-bit)
+setManifestid(2347774, "6880616264887178762", 1142056124)
+
+-- DLC's (no depot keys required)
+addappid(1766730) -- CS:GO - Operation Riptide
+
+-- DLC's (with depot keys)
+-- Counter-Strike 2 Workshop Tools (AppID: 2279721)
+addappid(2279721)
+addappid(2347779, 1, "a3d6504606875447dc6dedcba78bebb3e0a122fb30a504ef805536f216354271") -- (64-bit)
+setManifestid(2347779, "3221426604446990793", 2090443621)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 2347777
+
+-- Token-locked DLCs (couldn't read depots): 1490530, 2279720, 2678630
