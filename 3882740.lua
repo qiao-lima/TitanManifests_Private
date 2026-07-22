c5f0338f8c6348b1d735ffdfec670a4ff865d89d
qiao-lima <qiao-lima@github.com>
2026-07-23 02:39:11 +0800

    Manual Update 3882740

---
+++ 3882740.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3882740 - Day Trading Simulator
+-- Generated 2026-07-22 18:38:10 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3882740)
+
+-- Main Depots
+addappid(3882741, 1, "744b071f8c8067c3c2e598a561eba90b88e84158b5e81fa64f1ecede783dfa26")
+setManifestid(3882741, "5901372027235951285", 147811517)
