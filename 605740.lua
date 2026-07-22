cd4b628c011d8d7b1533bd1e7d7b0982170086c9
qiao-lima <qiao-lima@github.com>
2026-07-22 11:18:28 +0800

    Manual Update 605740

---
+++ 605740.lua
@@ -0,0 +1,24 @@
+-- Generated with Luie @ https://lua.tools/
+-- 605740 - Flashing Lights
+-- Generated 2026-07-22 03:17:53 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(605740, 1, "82dbeed64c3b908db1fb45fc5cda0c15f4e12e554e0a45dd393727c7ad978b35")
+
+-- Main Depots
+addappid(605741, 1, "95d3d0ace153500ff96a04353c87950fc4420523d33bd980470ba1bcc5513ec2") -- (windows)
+setManifestid(605741, "6428379635124435562", 5647521829)
+addappid(605742, 1, "a8211cb137d2735d7ff465f3f245198f857855a1e31a0ed5d1cbd54f4e795989") -- (macos)
+setManifestid(605742, "5417503032927657954", 5589801026)
+
+-- DLC's (no depot keys required)
+addappid(2449260) -- Flashing Lights: Pickup Truck Triple Pack (Police, Fire, EMS)
+addappid(2649400) -- Flashing Lights - Visor Lights
+addappid(2649410) -- Flashing Lights: Beast Swat Truck DLC
+addappid(2649420) -- Flashing Lights: Emergency Response Lightbar Collection
+addappid(2649430) -- Flashing Lights: Thunder Sport Sedan Pack (Police, Fire, EMS)
+addappid(3051230) -- Flashing Lights: Interceptor SUV Pack (Police, Fire, EMS)
+addappid(3640340) -- Flashing Lights: BUCK - Police, Fire, Rescue Pack
+addappid(3640350) -- Flashing Lights: Guerrero Pursuit SUV Pack
+addappid(3640360) -- Flashing Lights: Department of Transportation
