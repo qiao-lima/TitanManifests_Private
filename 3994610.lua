e71fbf8574a22c0bd37556ec3c98f68303b3f70f
qiao-lima <qiao-lima@github.com>
2026-07-23 02:39:11 +0800

    Manual Update 3994610

---
+++ 3994610.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3994610 - Monster Lab Simulator
+-- Generated 2026-07-22 18:38:01 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3994610)
+
+-- Main Depots
+addappid(3994611, 1, "864b90ebcb0f375aff110ef0be14f0972dbb5db0b57ce2e7916e80a7e6b1b543")
+setManifestid(3994611, "11795862404122352", 4721481586)
