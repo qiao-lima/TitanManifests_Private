77cf2e3196a086edf8533878eb469d9bd5dac76c
qiao-lima <qiao-lima@github.com>
2026-07-23 02:39:10 +0800

    Manual Update 3614980

---
+++ 3614980.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3614980 - Keytamine
+-- Generated 2026-07-22 18:38:18 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3614980)
+
+-- Main Depots
+addappid(3614981, 1, "013dfca214b71934b93d877868f720b5faaafd6dcc4852e524fc2d4345411e1a")
+setManifestid(3614981, "3902836126827819464", 2169593833)
