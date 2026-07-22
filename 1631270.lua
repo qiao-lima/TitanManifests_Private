96e4dfef3eabb5b726972c9ea369f90a463eab4b
qiao-lima <qiao-lima@github.com>
2026-07-23 02:39:10 +0800

    Manual Update 1631270

---
+++ 1631270.lua
@@ -0,0 +1,23 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1631270 - StarRupture
+-- Generated 2026-07-22 18:38:25 UTC
+-- # Depots (Total/DLC/Shared): 7/4/2
+
+-- Main AppID
+addappid(1631270, 1, "58df0d0c7d2375066687753516dd994c4171bd5e33a9d304dc72bceea96e1bf0")
+
+-- Main Depots
+addappid(1631272, 1, "cbb99a831607efa2d9529979e09ca1e3b1692bf978684330d6db62a558c30b52") -- (windows)
+setManifestid(1631272, "895524552114594264", 41971316386)
+
+-- DLC's (with depot keys)
+-- StarRupture - Supporter Pack (AppID: 4167250)
+addappid(4167250)
+addappid(4167250, 1, "5ea58a4526ce19f7edf2448015e940043fc2da5d61a837f34dfdf2e2966a2f8c") -- (windows, 64-bit)
+setManifestid(4167250, "9015653026638990134", 2042036313)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 3795342, 3795343, 3795344
