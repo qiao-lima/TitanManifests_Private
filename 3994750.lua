764cf55a7b08f5f7de735e261d877313e9af4bd0
qiao-lima <qiao-lima@github.com>
2026-07-23 02:39:11 +0800

    Manual Update 3994750

---
+++ 3994750.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3994750 - Apes Ticker
+-- Generated 2026-07-22 18:36:03 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3994750)
+
+-- Main Depots
+addappid(3994751, 1, "C9B3DD922FAE94DF0782CD0F1C3F0F1442642D40CF1CBFD9A6FED8823D233FB7") -- (windows)
+setManifestid(3994751, "1401198798838906914", 489072339)
