52768b52fc07f1bd3a6b9f8af1277228bc1b807e
qiao-lima <qiao-lima@github.com>
2026-07-23 02:39:11 +0800

    Manual Update 774351

---
+++ 774351.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 774351 - Citystate
+-- Generated 2026-07-22 18:35:53 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(774351)
+
+-- Main Depots
+addappid(774352, 1, "b98d29ff7152dbeca46a70a720b1691443985468c8dea9a7ed5ce5163ba485af") -- (windows)
+setManifestid(774352, "7999434192636248851", 349377258)
+
+-- Missing Depots (We don't have the keys yet lol): 774354
