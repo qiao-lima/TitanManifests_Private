2ee2d678f28d4106819e44d7c59bfbd8997c4e29
qiao-lima <qiao-lima@github.com>
2026-07-24 22:22:46 +0800

    Manual Update 3778020

---
+++ 3778020.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3778020 - Walk The Plank
+-- Generated 2026-07-24 12:38:50 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3778020)
+
+-- Main Depots
+addappid(3778021, 1, "d451261a3fb5c5e208fb0f7eb167d172781b5d6238ccb157ba653e6846d250a4") -- (windows)
+setManifestid(3778021, "4459596004952182993", 552644906)
