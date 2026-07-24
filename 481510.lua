c977b3e82a214ba17f3a2744dbc4c2f18959727a
qiao-lima <qiao-lima@github.com>
2026-07-24 22:22:49 +0800

    Manual Update 481510

---
+++ 481510.lua
@@ -0,0 +1,39 @@
+-- Generated with Luie @ https://lua.tools/
+-- 481510 - Night in the Woods
+-- Generated 2026-07-24 13:40:10 UTC
+-- # Depots (Total/DLC/Shared): 11/3/0
+
+-- Main AppID
+addappid(481510, 1, "985a0f4aac611f323024ed34929fa585ca6df1edd9a04229b0619192aa9e40fb")
+
+-- Main Depots
+addappid(481511, 1, "2ae7653f7b15600a5fcb638fd3a6cb99d06989f5b85b0679ebf96b355d918e4f") -- (windows, 64-bit)
+setManifestid(481511, "3984466337233088420", 6124160278)
+addappid(481512, 1, "8719378f2bef4c87a788be1b2af5b11c4494f1a40aafdef6ada0125cfb5d168e") -- (macos)
+setManifestid(481512, "7592990455421658439", 6179889995)
+addappid(481513, 1, "eefefda41556696b2a216771cf87850e2d28885c55b67f745371831e0c204c5b") -- (linux)
+setManifestid(481513, "7465508604074136165", 6175101570)
+addappid(481514, 1, "31ab6a9049112dfbba73c6c435a642e4ba8df44fc1bdb637de742ab21fa9f47d") -- (windows, 32-bit)
+setManifestid(481514, "4717445706699576220", 6116132694)
+addappid(481515, 1, "fc5981ffd652222cd37c3a57736982460f7ff9eca42fda16544be0e08344600e") -- Japanese (windows, 64-bit)
+setManifestid(481515, "5427837867631130529", 6313623379)
+addappid(481516, 1, "e105dfbb20c944c774d1b6066217994a9bb3f5018d65684f1de769860170a461") -- Japanese (macos)
+setManifestid(481516, "7198167298203492610", 6333888609)
+addappid(481517, 1, "82a3d124a72069ec5dab1e8e51fb1ed65f1ababe7331abee3b439432eb22eead") -- Japanese (linux)
+setManifestid(481517, "7855972193608849456", 7956931807)
+addappid(481518, 1, "31c1a9716ca7f353d76db07c5f648dc77a33845012ec8f17ade6439ff6a21211") -- Japanese (windows, 32-bit)
+setManifestid(481518, "277609414232149246", 6306098695)
+
+-- DLC's (with depot keys)
+-- Night in the Woods - Soundtrack Vol. I (AppID: 610220)
+addappid(610220)
+addappid(610220, 1, "0619f44290d8ad686b7d9c411ac1da0f097e3396ca4d8b01c68e80d5df16626f")
+setManifestid(610220, "5736816488851148619", 480579042)
+-- Night in the Woods - Soundtrack Vol. II (AppID: 610221)
+addappid(610221)
+addappid(610221, 1, "23f056deadaf38d8c6bb78d9a9d42d23d36a1842f880824533520bee4b28df35")
+setManifestid(610221, "6252873580691099532", 395770775)
+-- Night in the Woods - Soundtrack Vol. III (AppID: 610222)
+addappid(610222)
+addappid(610222, 1, "20ad06dac2de5a396f57305a05c9264c46964c0fb4c10e7e230140a7bab33c16")
+setManifestid(610222, "4425030495697334071", 101122139)
