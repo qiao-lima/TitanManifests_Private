07d50a684561270efb820b9b3dadd14e3c144673
qiao-lima <qiao-lima@github.com>
2026-07-24 22:14:19 +0800

    Manual Update 1624220

---
+++ 1624220.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1624220 - IT TAKES THE BALL
+-- Generated 2026-07-24 12:57:50 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(1624220)
+
+-- Main Depots
+addappid(1624221, 1, "064a82586695efe518e96b18d5101c1259a5d4f569ff914d989e7be41a9f4cf1") -- (windows, 64-bit)
+setManifestid(1624221, "2011510295888619904", 1553236702)
