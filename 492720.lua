14998535feaed9cd46ed33aa3a4777ca3cf53d7c
qiao-lima <qiao-lima@github.com>
2026-07-24 22:22:49 +0800

    Manual Update 492720

---
+++ 492720.lua
@@ -0,0 +1,62 @@
+-- Generated with Luie @ https://lua.tools/
+-- 492720 - Tropico 6
+-- Generated 2026-07-24 13:39:17 UTC
+-- # Depots (Total/DLC/Shared): 18/2/2
+
+-- Main AppID
+addappid(492720, 1, "3d5a3467d900e030ecfe1b205db124966a55bec80fe529a0610d96892ee59e50")
+
+-- Main Depots
+addappid(492721, 1, "dd1294f56dcac65ce85f51bb16a22e48be2ae8a6ad5d4ca90c6dab91f11d11e0") -- (windows)
+setManifestid(492721, "2482048476725152063", 21544779904)
+addappid(492722, 1, "3f6d0842f19aa9a1e925efa65af938a7c0787a40772209f04f85b642390a7e7d") -- English
+setManifestid(492722, "4964144065790125117", 4283497129)
+addappid(492723, 1, "a7acf29ed791bbbbeab7db9de495da7a7a8de56210c1ecbfc1a136fd5164bafc") -- German
+setManifestid(492723, "4671638683689531000", 4885142847)
+addappid(492724, 1, "ab2df80459c17b89abb8dc0b273fc0428a0032b6ebc2c5027c98588279068a37") -- French
+setManifestid(492724, "8446271955596560256", 4334276779)
+addappid(492725, 1, "8efc4cf21f7207cea9c32f259680080dac5f5f942aaec8868cfbee4e8de36029") -- Spanish
+setManifestid(492725, "3757073033091442221", 4372272340)
+addappid(492726, 1, "9020266ef836d81e3607a7ef8c032c5d154101593ca7819008fe12e390b2b738") -- Italian
+setManifestid(492726, "7807448400604552744", 4281209979)
+addappid(492727, 1, "553570b3d73881c17534d6e148cf70cae386b3329d85e5b102865c6abe9b5780") -- Russian
+setManifestid(492727, "6683266592988829118", 4706694999)
+addappid(492728, 1, "b45380cb07f37f378d256c7db47227896c9fdac77516b840e8290a25d8b59a2f") -- Japanese
+setManifestid(492728, "3444659887075232254", 8684639807)
+addappid(492729, 1, "4188d6855228eedcaf3acee2a1177b56d9cadde1f6564d1fcc8b2d5e8fd07dd4") -- Schinese
+setManifestid(492729, "8641782660136044329", 4274644172)
+addappid(881961, 1, "4acb95226bd1bae883783022f7021263752f0ad2c41657310320ef2252167fb8") -- Koreana
+setManifestid(881961, "7548479188371965043", 4283497129)
+addappid(881962, 1, "17f8cb3323a7e98acdaf4db2607701917ed439757bc8adb8e81f69272de34077") -- (macos)
+setManifestid(881962, "3177376426867696405", 29342752438)
+addappid(881963, 1, "fa30a245f1c36ba45e97fe5366ffcc2d17c2b22f510ddc1c788b4260099e5af6") -- (linux)
+setManifestid(881963, "6053701911850756550", 23895706081)
+addappid(881964, 1, "cdcb5067d4bd9abc27cdf0c5bc5e389b5c04cd3d9b864539e4e5ef12c45b6e25") -- Arabic
+setManifestid(881964, "5477568930720155518", 5819057906)
+addappid(881965, 1, "e8c37b294ead4c0599777edbd97dd724ceb7c1140fb8a6cf165a0ac83cd140a9") -- Brazilian
+setManifestid(881965, "3182330635426176422", 4283497129)
+
+-- DLC's (no depot keys required)
+addappid(1100250) -- Tropico 6 - The Llama of Wall Street
+addappid(1259750) -- Tropico 6 - Spitter
+addappid(1282350) -- Tropico 6 - Lobbyistico
+addappid(1449310) -- Tropico 6 - Caribbean Skies
+addappid(1631380) -- Tropico 6 - Festival
+addappid(2186250) -- Tropico 6 - New Frontiers
+addappid(2594010) -- Tropico 6 - Going Viral
+addappid(2938080) -- Tropico 6 - Tropican Shores
+addappid(3391380) -- Tropico 6 - Return to Nature
+
+-- DLC's (with depot keys)
+-- Tropico 6 El Prez Edition Pack (AppID: 881960)
+addappid(881960)
+addappid(881960, 1, "9a205eb5d636da4082eef0bd4c83938e1eaf7f313cc8f19e3daaae78989b2b24")
+setManifestid(881960, "7166363854253545793", 592968505)
+-- Tropico 6 - Original Soundtrack (AppID: 1224940)
+addappid(1224940)
+addappid(1224941, 1, "098faf903d473c1d990ae386b46126fa5b17f29619db28faa2df3a942f1decc6")
+setManifestid(1224941, "289319977696074653", 467124297)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
