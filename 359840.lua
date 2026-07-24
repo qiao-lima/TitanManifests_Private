6727ae27600fc1daf122bac8602eaaefca9c99d8
qiao-lima <qiao-lima@github.com>
2026-07-24 22:22:46 +0800

    Manual Update 359840

---
+++ 359840.lua
@@ -0,0 +1,18 @@
+-- Generated with Luie @ https://lua.tools/
+-- 359840 - Shift Happens
+-- Generated 2026-07-24 12:52:21 UTC
+-- # Depots (Total/DLC/Shared): 4/0/1
+
+-- Main AppID
+addappid(359840, 1, "2c27d4a8151b43f53885f30a232f5c01ba325e8675e72739003d656e2e741f91")
+
+-- Main Depots
+addappid(359841, 1, "cc32cf49b1522b5157f606604c2faa3495a55b160c670e3ab7ba501eb298eb49") -- (windows)
+setManifestid(359841, "5564349444363454925", 4697805171)
+addappid(359842, 1, "a10b419acce9fd5c94bc7bf92828f5b00989012884468fef152f9cf86c6e5ff1") -- (windows)
+setManifestid(359842, "4470234587005825810", 0)
+addappid(359843, 1, "0ae9c1251bfd818b0ae748725e9b13073b29499ed16176812d563305ae101891")
+setManifestid(359843, "6698408231092498505", 0)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
