46d15bf564547350c4d935afba2b8ac7b5db002a
qiao-lima <qiao-lima@github.com>
2026-07-24 22:22:50 +0800

    Manual Update 690640

---
+++ 690640.lua
@@ -0,0 +1,38 @@
+-- Generated with Luie @ https://lua.tools/
+-- 690640 - Trine 4: The Nightmare Prince
+-- Generated 2026-07-24 12:50:01 UTC
+-- # Depots (Total/DLC/Shared): 9/5/3
+
+-- Main AppID
+addappid(690640)
+
+-- Main Depots
+addappid(690641, 1, "42a0d2c4b4508e91a808d579233e2f5cac28bd41a8610b36809a2ed73090cbd4") -- (windows)
+setManifestid(690641, "1751452529680355581", 16882667866)
+
+-- DLC's (no depot keys required)
+addappid(1113990) -- DLC 1113990
+addappid(1444560) -- Trine 4: Melody of Mystery
+
+-- DLC's (with depot keys)
+-- Trine 4: The Nightmare Prince (Original Soundtrack) (AppID: 1224800)
+addappid(1224800)
+addappid(1224801, 1, "9709b449a3f85572abc5e327b197e80e0b9ddd11ec5da083d773365d45cfba75")
+setManifestid(1224801, "6151668597854479035", 196050628)
+addappid(1224802, 1, "aecd53a59585dbad33b6df8bc4412db1746b5a2319de516bd5c044098cc704b8")
+setManifestid(1224802, "2708478289790762521", 556719083)
+-- Trine 4: Melody of Mystery Soundtrack (AppID: 1503550)
+addappid(1503550)
+addappid(1503551, 1, "dda43115ecca85852da10d7c041dcdd7848d28126b7879f1f90e9ee1f20b1e54")
+setManifestid(1503551, "4294990941929641904", 66724836)
+addappid(1503552, 1, "3fe04ea99a602d3ba2fa6b0816b0ac4f63747a9bf06a1ea635f26b12a756f91c")
+setManifestid(1503552, "6956938612800263155", 231252590)
+-- Trine 4: The Nightmare Prince - Artbook (AppID: 1504370)
+addappid(1504370)
+addappid(1504371, 1, "25cd6a286dbc5bda444a8a9d3a68226ab661ade835f83e57cf04c6f3a751c783")
+setManifestid(1504371, "1312752376837469527", 253871466)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- (windows)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
