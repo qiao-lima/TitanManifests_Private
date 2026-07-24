4fd279ccbcb4ea9707940f8e60bbf66cd2dbb4b9
qiao-lima <qiao-lima@github.com>
2026-07-24 15:05:10 +0800

    Manual Update 291550

---
+++ 291550.lua
@@ -0,0 +1,77 @@
+-- Generated with Luie @ https://lua.tools/
+-- 291550 - Brawlhalla
+-- Generated 2026-07-23 03:03:14 UTC
+-- # Depots (Total/DLC/Shared): 3/0/1
+
+-- Main AppID
+addappid(291550, 1, "2ad10d73f7ddad2ef7a200a81623dc2de40d961ef25955aa7aafd6376552b499")
+
+-- Main Depots
+addappid(291551, 1, "7912e24d0afdea4e76e9c8b3dfb438d294bfa7552216541f60d6638c81892d0e") -- English (windows)
+setManifestid(291551, "4560833756450754138", 1410566134)
+addappid(291552, 1, "7bc0a2d1a917e8f3c0046aa1281e248a5e40491c7af4a03e28703b734a13cc3d") -- English (macos)
+setManifestid(291552, "8515617850455048112", 1431168692)
+
+-- DLC's (no depot keys required)
+addappid(297630) -- DLC 297630
+addappid(298640) -- DLC 298640
+addappid(298641) -- Brawlhalla - Collectors Pack
+addappid(387020) -- Brawlhalla - All Legends (Current and Future)
+addappid(389720) -- Brawlhalla - Valhalla Pack
+addappid(462190) -- DLC 462190
+addappid(554950) -- Brawlhalla - BCX 2016 Pack
+addappid(636900) -- Brawlhalla - Spring Championship 2017 Pack
+addappid(682870) -- Brawlhalla - Summer Championship 2017 Pack
+addappid(742720) -- Brawlhalla - BCX 2017 Pack
+addappid(802910) -- Brawlhalla - Winter Championship 2018 Pack
+addappid(847210) -- Brawlhalla - Spring Championship 2018 Pack
+addappid(881480) -- Brawlhalla - Summer Championship 2018 Pack
+addappid(941100) -- Brawlhalla - Fall Championship 2018 Pack
+addappid(953450) -- Brawlhalla - BCX 2018 Pack
+addappid(1180580) -- DLC 1180580
+addappid(1235280) -- DLC 1235280
+addappid(1268560) -- DLC 1268560
+addappid(1268561) -- DLC 1268561
+addappid(1353520) -- DLC 1353520
+addappid(1353521) -- Brawlhalla - Battle Pass Season 2
+addappid(1353522) -- DLC 1353522
+addappid(1382220) -- Brawlhalla - Fall Championship 2020 Pack
+addappid(1492700) -- DLC 1492700
+addappid(1492701) -- DLC 1492701
+addappid(1589030) -- Brawlhalla - Spring Championship 2021 Pack
+addappid(1649230) -- Brawlhalla - Summer Championship 2021 Pack
+addappid(1691670) -- DLC 1691670
+addappid(1712220) -- Brawlhalla - Autumn Championship 2021 Pack
+addappid(1774370) -- DLC 1774370
+addappid(1837830) -- DLC 1837830
+addappid(1844960) -- Brawlhalla - Battle Pass Season 5
+addappid(1920300) -- DLC 1920300
+addappid(2013950) -- DLC 2013950
+addappid(2073080) -- DLC 2073080
+addappid(2109890) -- Brawlhalla - Autumn Championship 2022 Pack
+addappid(2172170) -- DLC 2172170
+addappid(2261790) -- Brawlhalla - Battle Pass Season 7
+addappid(2261840) -- DLC 2261840
+addappid(2338560) -- Brawlhalla - Spring Championship 2023 Pack
+addappid(2493440) -- Brawlhalla - Summer Championship Pack 2023
+addappid(2517370) -- DLC 2517370
+addappid(2570010) -- DLC 2570010
+addappid(2632670) -- Brawlhalla - BCX 2023 Pack
+addappid(2781890) -- Brawlhalla - Winter Championship 2024 Pack
+addappid(2855410) -- Brawlhalla - Spring Esports 2024 Pack
+addappid(2855420) -- DLC 2855420
+addappid(2855430) -- Brawlhalla - Autumn Esports 2024 Pack
+addappid(2855440) -- Brawlhalla - BCX 2024 Pack
+addappid(2974360) -- Brawlhalla: Ezio Starter Pack
+addappid(3392620) -- Brawlhalla - Winter Esports 2025 Pack
+addappid(3392630) -- DLC 3392630
+addappid(3392640) -- Brawlhalla - Summer Esports 2025 Pack
+addappid(3392650) -- Brawlhalla - Autumn Esports 2025 Pack
+addappid(3392660) -- Brawlhalla - BCX 2025 Pack
+addappid(4192260) -- Brawlhalla x Lara Croft Bundle
+addappid(4263290) -- Brawlhalla - Winter Esports 2026 Pack
+addappid(4263300) -- Brawlhalla - Spring Esports 2026 Pack
+addappid(4263310) -- Brawlhalla - Summer Esports 2026 Pack
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
