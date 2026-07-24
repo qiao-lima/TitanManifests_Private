7b6d65474faf79944ccf64e1c64e979708ddae7a
qiao-lima <qiao-lima@github.com>
2026-07-24 22:14:21 +0800

    Manual Update 2153350

---
+++ 2153350.lua
@@ -0,0 +1,23 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2153350 - Brothers: A Tale of Two Sons Remake
+-- Generated 2026-07-24 12:51:06 UTC
+-- # Depots (Total/DLC/Shared): 5/2/2
+
+-- Main AppID
+addappid(2153350, 1, "9923e9dd536e976807062cd2d2e3ae2db0e114ad48d49e11cf3f035b7dea37d9")
+
+-- Main Depots
+addappid(2153351, 1, "997da40415d881772fa4957ed930334f03fd3e2c3c3b6d5d832caadaba4d0d88") -- (windows, 64-bit)
+setManifestid(2153351, "5661410614045627278", 29543247619)
+
+-- DLC's (with depot keys)
+-- Brothers: A Tale of Two Sons Remake - Soundtrack (AppID: 2692240)
+addappid(2692240)
+addappid(2692241, 1, "ed7cf7779d50008b0e8de00eddb4dd636265608c905232ac818d9dd2d9c1bde7")
+setManifestid(2692241, "8932953002926207062", 726116606)
+addappid(2692242, 1, "cc9782ef5365a6f10da2b68a3c099cd70032a36298baaf0125e75b9193af7e44")
+setManifestid(2692242, "6827915278776074757", 56312132)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
