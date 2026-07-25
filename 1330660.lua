bd2db4f926a41a3b00d4b50b7388e7fda4c28f05
qiao-lima <qiao-lima@github.com>
2026-07-25 17:06:46 +0800

    Manual Update 1330660

---
+++ 1330660.lua
@@ -0,0 +1,22 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1330660 - Train Life - A Railway Simulator
+-- Generated 2026-07-25 07:08:13 UTC
+-- # Depots (Total/DLC/Shared): 5/0/4
+
+-- Main AppID
+addappid(1330660)
+
+-- Main Depots
+addappid(1330661, 1, "fd30ed39664ca6cd3ffd28962acac5155d8486e254c47c221471b1c67660aeec")
+setManifestid(1330661, "3079090555906567684", 22386764174)
+
+-- DLC's (no depot keys required)
+addappid(2052580) -- Train Life - Early Access exclusive content
+addappid(2052581) -- Train Life - 1920's Orient-Express Train
+addappid(2057990) -- Train Life - Supporter Pack
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- (windows)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
