46ef5f2c55ae63b6612ae44b87c4046af44ca371
qiao-lima <qiao-lima@github.com>
2026-07-25 17:06:48 +0800

    Manual Update 3609750

---
+++ 3609750.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3609750 - Organized Inside
+-- Generated 2026-07-25 07:41:30 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(3609750, 1, "7d33e472189b8aa076af3a25143c706c2c9d09bb530c557fe438248a8ee710a5")
+
+-- Main Depots
+addappid(3609752, 1, "3d42072bc0f21dee543859501019a77cc5e016020eb3beafb57ce8b610307594") -- (windows, 64-bit)
+setManifestid(3609752, "864745232987769654", 481461696)
+addappid(3609753, 1, "a072dd2fc3a0c6e57da4c5898ceeebd4ea6c55307d19a64de9f68f1cc8cdcf38") -- (macos, 64-bit)
+setManifestid(3609753, "7650352421389803294", 598143102)
