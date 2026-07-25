aa47be6bd97afadbab9271a62f93a09bd1861b2b
qiao-lima <qiao-lima@github.com>
2026-07-25 21:32:25 +0800

    Manual Update 3282510

---
+++ 3282510.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3282510 - Body Pay "OMG! The girl with the big titties is too hitting on me!"
+-- Generated 2026-07-25 13:31:17 UTC
+-- # Depots (Total/DLC/Shared): 2/1/0
+
+-- Main AppID
+addappid(3282510)
+
+-- Main Depots
+addappid(3282511, 1, "432b0bcbbb1e83634d3d077b25540bcf99a3c9f4ec122aafc0f753378d29e37e") -- (windows)
+setManifestid(3282511, "205414075115816784", 1100391672)
+
+-- DLC's (with depot keys)
+-- Body Pay OMG! - Anime & Story DLC (AppID: 3351580)
+addappid(3351580)
+addappid(3351580, 1, "4b9d3e1df13dbff4ad8092cb06a89520b841e880e83748b3ab724c7ac1c34d16") -- (windows)
+setManifestid(3351580, "9066534230683392085", 57871028)
