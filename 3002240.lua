22d92852d3288410014b8b2e7397d0f2e96dff09
qiao-lima <qiao-lima@github.com>
2026-07-25 17:06:47 +0800

    Manual Update 3002240

---
+++ 3002240.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3002240 - Lunchbreak Tactics
+-- Generated 2026-07-25 07:22:24 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(3002240)
+
+-- Main Depots
+addappid(3002242, 1, "9c7c1626f20fac33e7d8fe2131edf25c424ee8dff287ffafa91fe3f77a87a22e") -- (windows)
+setManifestid(3002242, "9221526291672937997", 476274788)
+
+-- Missing Depots (We don't have the keys yet lol): 3002241
