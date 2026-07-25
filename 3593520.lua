f9263bf87e97b437b1c0afa557ea11d359b19c5b
qiao-lima <qiao-lima@github.com>
2026-07-25 17:06:48 +0800

    Manual Update 3593520

---
+++ 3593520.lua
@@ -0,0 +1,23 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3593520 - Lust Arcanum
+-- Generated 2026-07-25 07:25:50 UTC
+-- # Depots (Total/DLC/Shared): 8/6/0
+
+-- Main AppID
+addappid(3593520, 1, "9a8871503e4521c7cd5f8fe07ffde6014abc3d9c0e487b6359cfa52c1c289a8f")
+
+-- Main Depots
+addappid(3593521, 1, "bd6bb10ab70cb52f63ee37b59c309351e8e2533a9eff71ef0186bca783665200") -- (windows,linux)
+setManifestid(3593521, "2725724944349164755", 299822938)
+
+-- DLC's (with depot keys)
+-- Lust Arcanum - Supporter Pack (AppID: 4288870)
+addappid(4288870)
+addappid(4288870, 1, "1a5d8eaf19570ae85e5b33cc3c6e3d0626c6971a539074d54b429ed893850077")
+setManifestid(4288870, "9079209659804008560", 1159123)
+-- Lust Arcanum - Free Wallpaper (AppID: 4507250)
+addappid(4507250)
+addappid(4507250, 1, "706e0a861ad5a7dfaea32a97174a3ce5f8dacfd33b6df7716b0e6a7b7bd6c0a8")
+setManifestid(4507250, "786422480352914214", 6296889)
+
+-- Missing Depots (We don't have the keys yet lol): 3593522, 4231282, 4231283, 4231284, 4231285
