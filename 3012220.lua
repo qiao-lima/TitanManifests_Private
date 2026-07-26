c7b8783e7707f2d6627cd50deeec1825c7126afc
qiao-lima <qiao-lima@github.com>
2026-07-26 10:29:29 +0800

    Manual Update 3012220

---
+++ 3012220.lua
@@ -0,0 +1,23 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3012220 - Double Dragon Revive
+-- Generated 2026-07-26 02:02:14 UTC
+-- # Depots (Total/DLC/Shared): 5/1/2
+
+-- Main AppID
+addappid(3012220)
+
+-- Main Depots
+addappid(3012221, 1, "fbfa5df59dd28a25b63459f8d9071a821fe32bd1b1047f3d04e720fab6c1601f")
+setManifestid(3012221, "4519109501072660107", 12265538749)
+
+-- DLC's (with depot keys)
+-- Double Dragon Revive Deluxe Edition Upgrade Kit (AppID: 3386970)
+addappid(3386970)
+addappid(3386970, 1, "9ab6b7f58a2ef7e15cead63972f671a94818faa6c45f8ffb7a758ab7462a18c1")
+setManifestid(3386970, "687333521733178004", 301539092)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 3012223
