76887a608fde5f50d8a539ae8d706622667d4288
qiao-lima <qiao-lima@github.com>
2026-07-26 11:20:31 +0800

    Manual Update 701160

---
+++ 701160.lua
@@ -0,0 +1,34 @@
+-- Generated with Luie @ https://lua.tools/
+-- 701160 - Kingdom Two Crowns
+-- Generated 2026-07-25 16:27:48 UTC
+-- # Depots (Total/DLC/Shared): 6/2/1
+
+-- Main AppID
+addappid(701160, 1, "a870b8cb617fd6f06ec9d1b0a89f514f8611794aa043288467001423108182c7")
+
+-- Main Depots
+addappid(701161, 1, "5aae8ca5c454854bdf1b25248fe5f3c01a97c983489767c17930cc8a02156ac3") -- (windows)
+setManifestid(701161, "6639004889238773204", 2441905893)
+addappid(701162, 1, "17174ac183441ce4785eb189f7a80fc0fd9a202494cad02f1ddf4bc3d87aade4") -- (macos)
+setManifestid(701162, "8199715182987815586", 2583283512)
+addappid(701163, 1, "88068941a333be0784b8fd4fe2c414237610909671e189f3068177fc84e0352e") -- (linux)
+setManifestid(701163, "2442055016837937796", 2420311120)
+
+-- DLC's (no depot keys required)
+addappid(980450) -- Kingdom Two Crowns: Shogun
+addappid(1299270) -- Kingdom Two Crowns: Dead Lands
+addappid(2736340) -- Kingdom Two Crowns: Call of Olympus
+addappid(3062350) -- Kingdom Two Crowns: Regent's Royal Wardrobe
+addappid(3062360) -- Kingdom Two Crowns: Archon's Royal Wardrobe
+addappid(3939430) -- Kingdom Two Crowns: Decennial Royal Wardrobe
+
+-- DLC's (with depot keys)
+-- Kingdom Two Crowns: OST (AppID: 988810)
+addappid(988810)
+addappid(988810, 1, "09f7b9d014059b08ea21838c59e9edadb8d14238c0e97a6cbb6d597d98f49174")
+setManifestid(988810, "9153904362077354978", 2243072013)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 1708680
