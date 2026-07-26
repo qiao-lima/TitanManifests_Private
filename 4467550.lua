61f697b6a2d5ea15d00b249b4f0e18e3b0851cca
qiao-lima <qiao-lima@github.com>
2026-07-26 11:00:24 +0800

    Manual Update 4467550

---
+++ 4467550.lua
@@ -0,0 +1,16 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4467550 - Lust Solitaire: Horny Cards
+-- Generated 2026-07-26 02:45:36 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(4467550)
+
+-- Main Depots
+addappid(4467551, 1, "51f44503f766c819cd90f2cfa028f021978529564b17e9eb222f70e50b9efcdd") -- (windows)
+setManifestid(4467551, "5027965676703976472", 221095710)
+
+-- DLC's (no depot keys required)
+addappid(4500930) -- Lust Solitaire: 5-Deck Bundle
+
+-- Missing Depots (We don't have the keys yet lol): 4467552
