ed47021102a6c8a33e9b66850f704fb421916178
qiao-lima <qiao-lima@github.com>
2026-07-26 11:00:22 +0800

    Manual Update 2810210

---
+++ 2810210.lua
@@ -0,0 +1,18 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2810210 - Codename CURE II
+-- Generated 2026-07-26 02:58:34 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(2810210)
+
+-- Main Depots
+addappid(2810211, 1, "44913761ae63c85e2d8120a74224831cebeddd76da0b01b9db0219d1ddb79223") -- (windows, 64-bit)
+setManifestid(2810211, "8128831694422797351", 11992174726)
+
+-- DLC's (no depot keys required)
+addappid(4605390) -- Codename CURE II – Combat Specialist Pack
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
