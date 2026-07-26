a6bcec2453992a2332e713c713471d77f28dc38c
qiao-lima <qiao-lima@github.com>
2026-07-26 11:00:25 +0800

    Manual Update 4546270

---
+++ 4546270.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4546270 - Elven Friends
+-- Generated 2026-07-26 01:58:26 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(4546270)
+
+-- Main Depots
+addappid(4546271, 1, "e969620ba257d56fa2d9d5bc3de411683de0a1cfcfcc08656f70e59e09920713") -- (windows)
+setManifestid(4546271, "2135698040667164600", 989340397)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
