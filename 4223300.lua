d5c9e9d1419637e27626db238d48a20d02326695
qiao-lima <qiao-lima@github.com>
2026-07-26 11:00:24 +0800

    Manual Update 4223300

---
+++ 4223300.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4223300 - Birds of War
+-- Generated 2026-07-26 02:53:30 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(4223300)
+
+-- Main Depots
+addappid(4223301, 1, "f10565d093f0b37a04379ae5c7e35d3135dc8bfe6ed4380c03e06369dac5c981")
+setManifestid(4223301, "6059322336842511782", 6998286707)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
