1a0915968e21805e97ec30e9a061a9c997a8a28c
qiao-lima <qiao-lima@github.com>
2026-07-26 10:29:31 +0800

    Manual Update 4651940

---
+++ 4651940.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4651940 - Desire Empire
+-- Generated 2026-07-26 02:07:19 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4651940)
+
+-- Main Depots
+addappid(4651941, 1, "3a55b0a36a3f20ecde0a61a4f7778efa3e7a5562e3fca795f7869c0b27a7fb21")
+setManifestid(4651941, "4063152292382984105", 3728609177)
