9369c1abced3999cf2b9852a6bdc30a20340f278
qiao-lima <qiao-lima@github.com>
2026-07-26 10:29:32 +0800

    Manual Update 4758360

---
+++ 4758360.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4758360 - Escape The Sex Room
+-- Generated 2026-07-26 01:27:37 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4758360)
+
+-- Main Depots
+addappid(4758361, 1, "f9e58d5cd76059501ba680017781787e8ad6182bdac9e992b7cc5d86f6ad5960")
+setManifestid(4758361, "6971981340285600220", 1636756043)
