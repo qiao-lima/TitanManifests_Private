f9ca09d881269e70ce9dc9f13e6dd062076bc610
qiao-lima <qiao-lima@github.com>
2026-07-26 11:00:22 +0800

    Manual Update 2819470

---
+++ 2819470.lua
@@ -0,0 +1,22 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2819470 - Aquapark Tycoon
+-- Generated 2026-07-26 02:36:20 UTC
+-- # Depots (Total/DLC/Shared): 6/2/2
+
+-- Main AppID
+addappid(2819470)
+
+-- Main Depots
+addappid(2819471, 1, "a38c0eb936bc8daad59b9f70ca0203ed4b5c30ba0750466288574a541d8f67c5")
+setManifestid(2819471, "4220244544360190664", 1096702473)
+addappid(2819472, 1, "d367f19ab03b2e0f7e3f06064165e3019cd0def122126f73b40e8d6e1349bdc8") -- (windows, 64-bit)
+setManifestid(2819472, "8382473824329847950", 49402553)
+
+-- DLC's (no depot keys required)
+addappid(4948710) -- Aquapark Tycoon - Supporter Pack
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 4947861, 4947862
