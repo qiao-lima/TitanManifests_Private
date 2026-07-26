e8b8be81255ffd032154e35dd002c27b3c57c283
qiao-lima <qiao-lima@github.com>
2026-07-26 11:00:25 +0800

    Manual Update 931060

---
+++ 931060.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 931060 - Tormentum II
+-- Generated 2026-07-26 02:48:10 UTC
+-- # Depots (Total/DLC/Shared): 4/0/0
+
+-- Main AppID
+addappid(931060)
+
+-- Main Depots
+addappid(931061, 1, "84fc879027a2114ec39eb42ecfc8ac4faa2b5be9062dc88bdf96714931e8b114") -- (linux, 64-bit)
+setManifestid(931061, "8534304177193655619", 2053526921)
+addappid(931062, 1, "49742768f1091f1c81a9115b5f95013d13c4678689e15d0c914c753d7d6f236d") -- (windows, 32-bit)
+setManifestid(931062, "4442121640180609842", 2030332217)
+addappid(931063, 1, "ffcff5c9d57debcd2b9b97221ed1916af7ccaf54ad1522a5a77cfe59582a7c45") -- (macos, 64-bit)
+setManifestid(931063, "5811556181113112842", 2042238064)
+addappid(931064, 1, "bae82f887195849ccdc4ea3a82fb7582da6082b799f813d863366ca5f7d7e5f5") -- (windows, 64-bit)
+setManifestid(931064, "6605267934894667607", 2037671865)
