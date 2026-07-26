03257358be88801942250c7b8eeccb1c077b99ed
qiao-lima <qiao-lima@github.com>
2026-07-26 11:00:25 +0800

    Manual Update 4597380

---
+++ 4597380.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4597380 - Angel Essence
+-- Generated 2026-07-26 02:34:46 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(4597380)
+
+-- Main Depots
+addappid(4597381, 1, "2cffa9bce53d3e650bc062e11305a3fe3883693d1401475303d41a51971af40f") -- English (windows)
+setManifestid(4597381, "7673451233023822051", 601755503)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
