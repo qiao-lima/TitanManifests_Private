3fb2ddb5ddadd13194c16f17c16ffd69db257547
qiao-lima <qiao-lima@github.com>
2026-07-26 11:00:25 +0800

    Manual Update 4519130

---
+++ 4519130.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4519130 - MOLDRISE
+-- Generated 2026-07-26 01:53:22 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(4519130)
+
+-- Main Depots
+addappid(4519132, 1, "657a9bd87fa5635826abb5e3f63567020dbdc97c66f6eecf2fa78521c2a9adc7") -- (windows, 64-bit)
+setManifestid(4519132, "3303850529587493335", 91591660)
+
+-- Missing Depots (We don't have the keys yet lol): 4519131, 4519133
