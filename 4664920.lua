57d0b0a2615399766aa820db1c6b32261c2886db
qiao-lima <qiao-lima@github.com>
2026-07-26 11:00:25 +0800

    Manual Update 4664920

---
+++ 4664920.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4664920 - Wiseman: No More Breakups
+-- Generated 2026-07-26 02:42:45 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(4664920)
+
+-- Main Depots
+addappid(4664921, 1, "2ab4104ef15ee6ff447173b2da42eba4b6619a8d582c9567fd2bc9c6a48a60ea") -- (windows)
+setManifestid(4664921, "7784318039917858123", 1625758000)
+
+-- Missing Depots (We don't have the keys yet lol): 4664922
