5dadd9f5b3ec028ca7df63a5f8dc98bc146446eb
qiao-lima <qiao-lima@github.com>
2026-07-27 10:27:47 +0800

    Manual Update 1060220

---
+++ 1060220.lua
@@ -0,0 +1,28 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1060220 - Langrisser I & II
+-- Generated 2026-07-27 01:56:57 UTC
+-- # Depots (Total/DLC/Shared): 4/3/0
+
+-- Main AppID
+addappid(1060220)
+
+-- Main Depots
+addappid(1060221, 1, "b77999544d6ac2ca6c0a643a0edb7b6b6860a569b70c88486826bf9092be691c")
+setManifestid(1060221, "211510448520939098", 4863635943)
+
+-- DLC's (no depot keys required)
+addappid(1205510) -- Langrisser I & II - Legacy BGM Pack
+
+-- DLC's (with depot keys)
+-- Langrisser I & II - Visual Book (AppID: 1205511)
+addappid(1205511)
+addappid(1205511, 1, "84760c9609474694c694666849c904fa5767a2684b15762e67b1212bc49fe637")
+setManifestid(1205511, "8130984878561580684", 133865824)
+-- Langrisser I & II - Original 2-Disc Soundtrack (AppID: 1205512)
+addappid(1205512)
+addappid(1205512, 1, "5a584ea821de1ec0b842455af205716c8c5f223188547a4827578ba207d64a36")
+setManifestid(1205512, "4917765894600830609", 2729376048)
+-- Langrisser I & II - Songs of War 3-Disc Soundtrack (AppID: 1205513)
+addappid(1205513)
+addappid(1205513, 1, "ff8697fd3fe26d292fddf3b2b0a6962a6ae700b9664c7abebbdb7dbee55aca6e")
+setManifestid(1205513, "1028733006558096820", 8113854518)
