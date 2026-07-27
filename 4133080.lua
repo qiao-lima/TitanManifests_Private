d159129b78ed46cdf4cd674e9fe65fce8352f177
qiao-lima <qiao-lima@github.com>
2026-07-27 10:27:49 +0800

    Manual Update 4133080

---
+++ 4133080.lua
@@ -0,0 +1,16 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4133080 - MOMMY'S SQUIRT!
+-- Generated 2026-07-27 02:02:53 UTC
+-- # Depots (Total/DLC/Shared): 4/0/3
+
+-- Main AppID
+addappid(4133080)
+
+-- Main Depots
+addappid(4133081, 1, "bb3af3186ffa33a2862f13c1083fac73a2a24fe92077db94aa93f795454b956a")
+setManifestid(4133081, "1695529493840276710", 822466237)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
