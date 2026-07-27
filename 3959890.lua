c10f2e3f5bab3227fb1b5037ea20bc9e493c7258
qiao-lima <qiao-lima@github.com>
2026-07-27 10:27:48 +0800

    Manual Update 3959890

---
+++ 3959890.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3959890 - Tap Tap Loot
+-- Generated 2026-07-27 02:09:29 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3959890, 1, "7bc6a4e3146f0b45da5739e6c97bdd46c2098416433a1b2bacb38a566e18b6fc")
+
+-- Main Depots
+addappid(3959891, 1, "a0523a3673d8526c8c42a0daf53dbcb40ee7784ad18c12b3935cfa0b98de4aff")
+setManifestid(3959891, "1414250463899376686", 152521841)
