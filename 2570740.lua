8c923ac9a399769f673570e2854f2a0591d75927
qiao-lima <qiao-lima@github.com>
2026-07-27 23:31:18 +0800

    Manual Update 2570740

---
+++ 2570740.lua
@@ -0,0 +1,18 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2570740 - Deadly Contagion
+-- Generated 2026-07-27 15:25:25 UTC
+-- # Depots (Total/DLC/Shared): 6/0/5
+
+-- Main AppID
+addappid(2570740)
+
+-- Main Depots
+addappid(2570741, 1, "6cc7eae48cee12c75e30c102db8fabdbbe752b1745913b50b795ed751fda1b93") -- (windows, 64-bit)
+setManifestid(2570741, "2988063661473366592", 4178394109)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- (windows)
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") -- (windows)
+addappid(229030, 1, "92f0f88867c8d5f351090eb2d27aeb50e2b621ae8a34708265d2c20c36fbcf5e") -- (windows)
