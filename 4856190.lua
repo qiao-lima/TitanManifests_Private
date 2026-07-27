2feb7e3ed0fa02ede23b0344d38e779053e4ea56
qiao-lima <qiao-lima@github.com>
2026-07-27 10:27:49 +0800

    Manual Update 4856190

---
+++ 4856190.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4856190 - Tech Store & Repair Simulator
+-- Generated 2026-07-27 02:11:24 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4856190)
+
+-- Main Depots
+addappid(4856191, 1, "ac0883f94df1a87d804f20aaba82f3e782beda3725b49db183bc04fd1557de6e")
+setManifestid(4856191, "9090005025722659711", 657593146)
