dab7e4e3e1780000a2a5a5c983085e73061613d7
qiao-lima <qiao-lima@github.com>
2026-07-27 23:31:19 +0800

    Manual Update 4606430

---
+++ 4606430.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4606430 - Monster Girls: You Can't Say No 🔞 Demo
+-- Generated 2026-07-27 15:27:23 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4606430)
+
+-- Main Depots
+addappid(4606431, 1, "087237c214ca0ee8539d92828fcb3c42f3a1918c2b83a3a8353de643b81f14b0")
+setManifestid(4606431, "4192397162627978763", 2712205940)
