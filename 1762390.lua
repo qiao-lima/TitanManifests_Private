31e6fc7c265604c14b1b48ccd63e91a18b83c6cb
qiao-lima <qiao-lima@github.com>
2026-07-27 10:27:47 +0800

    Manual Update 1762390

---
+++ 1762390.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1762390 - HeroBattle
+-- Generated 2026-07-27 02:01:32 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(1762390)
+
+-- Main Depots
+addappid(1762391, 1, "32f14278d391e673f95dd554b1d5e05fdcd241817b35aadbb5f71eca00f66f9e")
+setManifestid(1762391, "9212812107738780358", 8232366532)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
