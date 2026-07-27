b4d125e53536092501eaf0341d5da5ede5467746
qiao-lima <qiao-lima@github.com>
2026-07-27 23:31:18 +0800

    Manual Update 3512910

---
+++ 3512910.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3512910 - IN HEAT: Lustful Nights Soundtrack
+-- Generated 2026-07-27 15:26:25 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(3512910)
+
+-- Main Depots
+addappid(3512912, 1, "b87b270cd721013f6449ff26a09d9ea04a9e772947153465bf021f356ab43c8f")
+setManifestid(3512912, "3128504594719195631", 284796622)
+addappid(3512913, 1, "a2e6a82f1cc7082b77ec88c30babc6035764cc59bf0a9b639fed3f46f57521d9")
+setManifestid(3512913, "3247005877764422277", 948791191)
