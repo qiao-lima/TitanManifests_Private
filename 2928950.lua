711adc2994cb39b9fdabdf7ca8e8c37e2233e5d1
qiao-lima <qiao-lima@github.com>
2026-07-28 12:41:02 +0800

    Manual Update 2928950

---
+++ 2928950.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2928950 - Cats in Heat - Sunshine Resort
+-- Generated 2026-07-28 04:38:41 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2928950)
+
+-- Main Depots
+addappid(2928951, 1, "6c8bc1ae9806b28701832afafb5797ab1e505536a32fab4e72e00f1bb29ee19b")
+setManifestid(2928951, "8648169283762540327", 366797328)
