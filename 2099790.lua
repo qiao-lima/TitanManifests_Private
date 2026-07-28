d966fa377427834701f96825b70de021b71bac17
qiao-lima <qiao-lima@github.com>
2026-07-28 12:16:41 +0800

    Manual Update 2099790

---
+++ 2099790.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2099790 - The Monstrous Horror Show
+-- Generated 2026-07-28 04:03:00 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(2099790)
+
+-- Main Depots
+addappid(2099791, 1, "21c59f6da3aac1789bb6de1858e280b3f43728e60338a4aa4bf362c7b412973b") -- Japanese
+setManifestid(2099791, "2954646803858174615", 569488744)
+addappid(2099792, 1, "9f88764c638e6cef92da071fe1c492fce6a0d209010fa6e4fe9a573d37c52422") -- Schinese
+setManifestid(2099792, "5088477549814871272", 583046980)
+addappid(2099793, 1, "58d19bad8c91a9d40ccfa655baf0d6ebf18875f7709b64cdc0f9be626c321fc5") -- English
+setManifestid(2099793, "5279959657920342871", 571093554)
