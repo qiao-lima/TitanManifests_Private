145479fa79ca3312eded45dc02fe2804ede9bf11
qiao-lima <qiao-lima@github.com>
2026-07-28 12:16:44 +0800

    Manual Update 4651220

---
+++ 4651220.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4651220 - 战意：三国
+-- Generated 2026-07-28 03:36:11 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4651220)
+
+-- Main Depots
+addappid(4651221, 1, "edfae0c4f4980d60ee0dcb2b7e1c23beda818694d3ac8ba2ff8814c4cf5c4319")
+setManifestid(4651221, "6251991514996170604", 44338038625)
