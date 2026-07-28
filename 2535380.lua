00b1ecf017fc56e8a7864ff847a7b3556dab44c3
qiao-lima <qiao-lima@github.com>
2026-07-29 04:44:33 +0800

    Manual Update 2535380

---
+++ 2535380.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2535380 - 星火
+-- Generated 2026-07-28 20:42:15 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2535380)
+
+-- Main Depots
+addappid(2535381, 1, "81b53ba532066b7626aeafb4826888b2d498a1ee70a6bb485a0a0fb678939dd4")
+setManifestid(2535381, "5206566207271605714", 142818862)
