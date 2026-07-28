be8b5d9fdbf7dd36ab0cfe6bd9614043571d3128
qiao-lima <qiao-lima@github.com>
2026-07-28 12:41:02 +0800

    Manual Update 1546990

---
+++ 1546990.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1546990 - Grand Theft Auto: Vice City - The Definitive Edition
+-- Generated 2026-07-28 04:22:36 UTC
+-- # Depots (Total/DLC/Shared): 4/0/3
+
+-- Main AppID
+addappid(1546990, 1, "4f32d3506c690f43ee97ce40f3ceb2a5105cf42554bea78d07fa68bfd419887d")
+
+-- Main Depots
+addappid(1546991, 1, "8ffae608bee3a6766651bc7b0b2122f7248955251de38eafb6161ea30434979d")
+setManifestid(1546991, "6147427494442580236", 9689200375)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+addappid(1899671, 1, "b7921da5e50d00b2238d0fe870a354cb572bc5d397955fef02a439103f62827b")
+setManifestid(1899671, "1378788310039702778", 239518253)
