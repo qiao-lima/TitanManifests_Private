fe357d85e29b32156950fe0e7d100ffeb68f2090
qiao-lima <qiao-lima@github.com>
2026-07-29 04:30:15 +0800

    Manual Update 3337180

---
+++ 3337180.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3337180.lua
+--Gamename Tiny Isle
+addappid(3337180) --Mainappid Tiny Isle
+addappid(3337181, 1, "c666412ce5eaa9ec5add214cc3253106876cf0867b5567f21fa1ac8689e64971") --Main Windows Depot Tiny Isle
+setManifestid(3337181, "7704241584514708831", 313317008)
+addappid(4306950) --Dlcname Tiny Isle - 绮梦花园DLC
