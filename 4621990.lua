ce9e95a4174bef04c4fa257e29268590494996ef
qiao-lima <qiao-lima@github.com>
2026-07-28 12:16:43 +0800

    Manual Update 4621990

---
+++ 4621990.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4621990.lua
+--Gamename Special Request: Chapter 2
+addappid(4621990, 1, "7a3bfcdceb43ca8518838bf965049bfd94ad4f5337a81fea5afa0af6fb4c6275") --Mainappid Special Request: Chapter 2
+addappid(4621991, 1, "1e1fe6d2f830da181f5d8b8e9ff2f3356315df91c5545613484319b615985f6b") --Main Windows Language Depot English Special Request: Chapter 2
+setManifestid(4621991, "8985378193546314866", 1986221232)
+addappid(4621992, 1, "0198776331ebb56414ac2bafa637cce786706baab4a25bdb45f506b0fe21acd6") --Main Macos Language Depot English Special Request: Chapter 2
+setManifestid(4621992, "3147422383835718781", 2012891632)
