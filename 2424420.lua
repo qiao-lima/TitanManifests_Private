df3b8304d5fdd04f6edeb0267707fb5b6571d40b
qiao-lima <qiao-lima@github.com>
2026-07-28 12:16:41 +0800

    Manual Update 2424420

---
+++ 2424420.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2424420 - Avatar Legends: The Fighting Game 
+-- Generated 2026-07-28 03:06:05 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(2424420)
+
+-- Main Depots
+addappid(2424421, 1, "aecbafdba75c145b913095a622b5665b86c3589fe2a77351c9ddbe48b185c4bd")
+setManifestid(2424421, "7266337798456641018", 8573076521)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
