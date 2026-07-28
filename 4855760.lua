8f1c831fdf5ea4c11a036f14aea426c2580a18d5
qiao-lima <qiao-lima@github.com>
2026-07-29 04:44:34 +0800

    Manual Update 4855760

---
+++ 4855760.lua
@@ -0,0 +1,16 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4855760 - Tung Tung Tung Survival
+-- Generated 2026-07-28 20:05:35 UTC
+-- # Depots (Total/DLC/Shared): 2/1/0
+
+-- Main AppID
+addappid(4855760)
+
+-- Main Depots
+addappid(4855761, 1, "befe2b3ce63f7cc66fd6b03e8b98433b0658d5ee7ee28c6dae7ccfceee6a9c91")
+setManifestid(4855761, "2242650454657146310", 837911529)
+
+-- DLC's (no depot keys required)
+addappid(4864670) -- Tung Tung Tung Survival - Supporter Pack
+
+-- Missing Depots (We don't have the keys yet lol): 4856631
