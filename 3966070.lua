2e6d4d9cf39874beaf4ce0c0402fafdc04c8f23e
qiao-lima <qiao-lima@github.com>
2026-07-29 04:44:33 +0800

    Manual Update 3966070

---
+++ 3966070.lua
@@ -0,0 +1,21 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3966070 - The Dead Thread
+-- Generated 2026-07-28 20:42:30 UTC
+-- # Depots (Total/DLC/Shared): 4/1/2
+
+-- Main AppID
+addappid(3966070)
+
+-- Main Depots
+addappid(3966071, 1, "aee82036f90ad479c8590bf6def126da1722782230f75646c6df544b9872917b") -- (windows, 64-bit)
+setManifestid(3966071, "6991858578129310725", 12408203728)
+
+-- DLC's (with depot keys)
+-- The Dead Thread - Survivor Pack (AppID: 4544300)
+addappid(4544300)
+addappid(4544300, 1, "483e3722b820ccb1a78c15c5f98eb70dcfa8249c28694cbf9208e113a0f498b2") -- (windows, 64-bit)
+setManifestid(4544300, "902379438429095595", 168024122)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
