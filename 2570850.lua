692b9acf9a8bb4f7ca850b10ee8a1033656ca31c
qiao-lima <qiao-lima@github.com>
2026-07-28 12:16:42 +0800

    Manual Update 2570850

---
+++ 2570850.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2570850 - Heart Cage
+-- Generated 2026-07-28 04:05:51 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(2570850)
+
+-- Main Depots
+addappid(2570851, 1, "e56a5310ce0b5c36c5dbf3dc588ebea445f21481cfc91dc1eb9cd0ad59b639f8") -- (windows,linux)
+setManifestid(2570851, "8032177260761147184", 2301496872)
+
+-- Missing Depots (We don't have the keys yet lol): 2570852
+
+-- Token-locked DLCs (couldn't read depots): 4270530
