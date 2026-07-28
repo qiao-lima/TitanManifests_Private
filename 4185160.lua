60349718897b41bf76ea7b122197d96efcd8b274
qiao-lima <qiao-lima@github.com>
2026-07-28 12:16:43 +0800

    Manual Update 4185160

---
+++ 4185160.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4185160 - Special Request: Chapter 1
+-- Generated 2026-07-28 03:25:26 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(4185160, 1, "32dd438a9b7233e18172db19348b6cae1ba193fa289faaf57f1ef30df324f0b8")
+
+-- Main Depots
+addappid(4185161, 1, "877ced47717427029668ccff7d1027a5e1ada085a088b8e475137aa3116bd0a3") -- English (windows, 64-bit)
+setManifestid(4185161, "3318394493220743264", 784479219)
+addappid(4185162, 1, "8FA0ED3E940123C8C0E07B371234FA59F69B4DE68A64A89DE99A41BDF1795978") -- English (macos)
+setManifestid(4185162, "3315378135335766025", 875252112)
