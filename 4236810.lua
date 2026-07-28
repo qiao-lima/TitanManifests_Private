a47c6ea9c5da5d1277692eac791efab02aaadff5
qiao-lima <qiao-lima@github.com>
2026-07-29 04:44:34 +0800

    Manual Update 4236810

---
+++ 4236810.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4236810 - Halu - The Fish
+-- Generated 2026-07-28 20:41:46 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(4236810)
+
+-- Main Depots
+addappid(4236811, 1, "0b298a191db916026f165e26e7146ce262846dbdd19b4b28fcbb848de7218cbe")
+setManifestid(4236811, "121414082925022425", 2353393136)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
