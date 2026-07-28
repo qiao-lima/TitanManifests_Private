8b19840d5cd978ac775d29f7aecc0cc091c5ea68
qiao-lima <qiao-lima@github.com>
2026-07-28 12:16:43 +0800

    Manual Update 4518040

---
+++ 4518040.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4518040 - Goblins Stole My Panties 👙
+-- Generated 2026-07-28 03:09:18 UTC
+-- # Depots (Total/DLC/Shared): 2/1/0
+
+-- Main AppID
+addappid(4518040, 1, "d20ad58c34bf6cf6a25172cd4de4ed3afd76ae9ea26eaff9099adf3fe2922023")
+
+-- Main Depots
+addappid(4518041, 1, "2589c0d1818c2175cf72a7fa30c099753132fdc6894176444834019e738c3850")
+setManifestid(4518041, "974535161009655907", 2269186591)
+
+-- DLC's (with depot keys)
+-- Goblins Stole My Panties 👙 - Wallpapers 18+ (AppID: 4918130)
+addappid(4918130)
+addappid(4918130, 1, "77325fa8956bbc1c22eee2e9d6369dcf6cdd679dc8bb222a1aaa0d446d7b8ab0")
+setManifestid(4918130, "2179908750259802412", 37403325)
