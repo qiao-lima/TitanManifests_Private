42e425c07b142046e507a8cd54797bac1cb2e107
qiao-lima <qiao-lima@github.com>
2026-07-29 04:44:33 +0800

    Manual Update 3985290

---
+++ 3985290.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3985290 - Samara
+-- Generated 2026-07-28 19:59:07 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(3985290)
+
+-- Main Depots
+addappid(3985291, 1, "924babeb1b0e2f047bdd09c8043d0426ca339cf8d17d95d66129a372b948140a")
+setManifestid(3985291, "5991798736990465342", 4463638612)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
