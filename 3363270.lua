02e6564b73be58d7f9915a87088996f77c9d1ccb
qiao-lima <qiao-lima@github.com>
2026-07-29 17:58:59 +0800

    Manual Update 3363270

---
+++ 3363270.lua
@@ -0,0 +1,20 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3363270 - Fischer's Fishing Journey
+-- Generated 2026-07-29 08:51:44 UTC
+-- # Depots (Total/DLC/Shared): 2/1/0
+
+-- Main AppID
+addappid(3363270)
+
+-- Main Depots
+addappid(3363271, 1, "6abedbca0382129ced58360f9b8f867a06eed267036e30933c88bdfbe9456bfe")
+setManifestid(3363271, "5449180784526264135", 354074539)
+
+-- DLC's (no depot keys required)
+addappid(4244400) -- Fischer's Fishing Journey - Snow-Crowned Mountain  DLC
+
+-- DLC's (with depot keys)
+-- Fischer's Fishing Journey - Supporter Pack (AppID: 3888270)
+addappid(3888270)
+addappid(3888270, 1, "db7af457afc6725956162c43638bfd876143b6ac86301d88285b88d4a35c7a33")
+setManifestid(3888270, "3241092621326483671", 28447)
