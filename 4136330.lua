a7c4444b5eea41ea622c73b1593e823e2ccfbe58
qiao-lima <qiao-lima@github.com>
2026-07-29 17:59:01 +0800

    Manual Update 4136330

---
+++ 4136330.lua
@@ -0,0 +1,12 @@
+-- 4136330's Lua and Manifest Created by Hubcap Manifest
+-- MILF WILD PARADISE
+-- Created: July 05, 2026 at 13:34:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4136330) -- MILF WILD PARADISE
+-- MAIN APP DEPOTS
+addappid(4136331, 1, "d94e3928396781a898dcec76bf6c509a27d52d6901236d3f9f6f266898723aa3") -- Depot 4136331
+setManifestid(4136331, "4768296801290351724", 430754281)
