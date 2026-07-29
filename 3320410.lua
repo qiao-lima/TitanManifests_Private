67e73998a0ab036ebe48f4f82e81064cc09201d8
qiao-lima <qiao-lima@github.com>
2026-07-29 17:58:59 +0800

    Manual Update 3320410

---
+++ 3320410.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3320410 - Thot on Trial
+-- Generated 2026-07-29 08:54:18 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3320410, 1, "82c53b0ab5c36433eac2735d8349f1e2e9a924fbb349c87ef75ee8e4a2a715c3")
+
+-- Main Depots
+addappid(3320411, 1, "31d38745003b9f2606f94181d704772d21bac8bdb5499d91149f7077e20cc252")
+setManifestid(3320411, "8926584728336724734", 3242793720)
