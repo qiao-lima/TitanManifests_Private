fadbe6c16eb4cfaa8e0be92867c2350e3c2d0b9b
qiao-lima <qiao-lima@github.com>
2026-07-29 17:59:01 +0800

    Manual Update 4249850

---
+++ 4249850.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4249850 - E-Shop Tycoon
+-- Generated 2026-07-29 08:08:29 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(4249850)
+
+-- Main Depots
+addappid(4249851, 1, "97d1672397100486c472e04fc3a201d94d04224ee65b12a396d63bb7c67547aa")
+setManifestid(4249851, "9167159513779867762", 431182962)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
