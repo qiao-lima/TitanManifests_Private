2737fdcb128495510b1ca60c6892793d41d133ca
qiao-lima <qiao-lima@github.com>
2026-07-29 17:58:58 +0800

    Manual Update 2840470

---
+++ 2840470.lua
@@ -0,0 +1,20 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2840470 - SimplePlanes 2
+-- Generated 2026-07-29 08:42:48 UTC
+-- # Depots (Total/DLC/Shared): 5/1/1
+
+-- Main AppID
+addappid(2840470, 1, "577dd1b87a004f12d083d2f2ac9bc0532fa0bf378151e9a18289620aece787c6")
+
+-- Main Depots
+addappid(2840471, 1, "1849882c014676488fe6b3e4ac5a33a61ee95e803353a176bd37243396d1d137")
+setManifestid(2840471, "1378061969111111748", 0)
+addappid(2840472, 1, "8684d95bf48a00217eec9db68b71fafb1f80c2871f9a882c5f10e3f8e4550b52") -- (windows)
+setManifestid(2840472, "5382416779146098730", 3965287066)
+addappid(2840473, 1, "9ba75c82180014bb943ec67d908f59d13814ce38bfc808f853fe7121e567bac5") -- (macos)
+setManifestid(2840473, "3568988425770330582", 4119772131)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 3635841
