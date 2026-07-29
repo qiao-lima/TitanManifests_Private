6f170e4dafdcff5df7c0ef0cb1e02711d257e715
qiao-lima <qiao-lima@github.com>
2026-07-29 17:58:59 +0800

    Manual Update 2875820

---
+++ 2875820.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2875820 - P4ST3L
+-- Generated 2026-07-29 08:52:19 UTC
+-- # Depots (Total/DLC/Shared): 6/3/2
+
+-- Main AppID
+addappid(2875820)
+
+-- Main Depots
+addappid(2875821, 1, "42e33b2e6786692a855f528406f9b77729bbe135132bcff3da6714f572c66c60") -- (windows, 64-bit)
+setManifestid(2875821, "2395147626848334897", 2542891622)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 4259391, 4265631, 4265632
