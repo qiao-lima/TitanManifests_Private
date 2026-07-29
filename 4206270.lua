75eac753456c580497078f40ba67e2dc4e06e283
qiao-lima <qiao-lima@github.com>
2026-07-29 17:59:01 +0800

    Manual Update 4206270

---
+++ 4206270.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4206270 - Wireworks
+-- Generated 2026-07-29 08:51:29 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(4206270)
+
+-- Main Depots
+addappid(4206271, 1, "f51d69140d4aa55e5000e3256085c77b895abc9c19e4127f7c7ff15d711f4694") -- (windows)
+setManifestid(4206271, "1436152341577267501", 299146298)
+addappid(4206272, 1, "bd3b303ad75e27572a861c0589f6b8a62676d6c4353deb8ec3eb71b112e08f80") -- (linux)
+setManifestid(4206272, "3230466131967432877", 301050282)
