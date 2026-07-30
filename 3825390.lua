567f9cef67d26e62d43fed515b43f2f28debafb6
qiao-lima <qiao-lima@github.com>
2026-07-30 11:27:18 +0800

    Manual Update 3825390

---
+++ 3825390.lua
@@ -0,0 +1,20 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3825390 - BLUE REFLECTION Quartet
+-- Generated 2026-07-30 02:24:32 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(3825390)
+
+-- Main Depots
+addappid(3825391, 1, "63c7a50c91918a8eabcccbc40a90996bd220ae272570423970f75e6044c29bf4")
+setManifestid(3825391, "1598436630448407770", 56396935872)
+
+-- DLC's (no depot keys required)
+addappid(4313510) -- BLUE REFLECTION Quartet: Set of 4 Special Photo Frames
+addappid(4313520) -- BLUE REFLECTION Quartet: Uniform (Partner World Version)
+addappid(4353430) -- BLUE REFLECTION Quartet: Special Photo Frame - Quartet
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
