5de599ef87e5447b6e07b5a672c6f3c1a569fa91
qiao-lima <qiao-lima@github.com>
2026-07-30 11:27:17 +0800

    Manual Update 3151270

---
+++ 3151270.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3151270 - One Turn Kill
+-- Generated 2026-07-29 11:11:58 UTC
+-- # Depots (Total/DLC/Shared): 3/2/0
+
+-- Main AppID
+addappid(3151270)
+
+-- Main Depots
+addappid(3151271, 1, "c8707841fa61d54c4dcc4bdb02044e76bd32b7f2abf8c61286a6795586222c10")
+setManifestid(3151271, "8961679401473762902", 523257054)
+
+-- Missing Depots (We don't have the keys yet lol): 4395231, 4395232
