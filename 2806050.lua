303f0c028c383d08ff9667641b43c591f49d1464
qiao-lima <qiao-lima@github.com>
2026-07-30 11:27:16 +0800

    Manual Update 2806050

---
+++ 2806050.lua
@@ -0,0 +1,29 @@
+-- 2806050's Lua and Manifest Created by Hubcap Manifest
+-- Halo: Campaign Evolved
+-- Created: July 29, 2026 at 13:15:03 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 3
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2806050, 1, "ed813e8e735c9a37bc3698804c2f49a73f4cd0ece4535e2b25d9f2f069abfe8e") -- Halo: Campaign Evolved
+-- MAIN APP DEPOTS
+addappid(2806051, 1, "400618d2330230a74b26331220f2aed43a92bf65eb23147779810f48f893c6e5") -- Depot 2806051
+--setManifestid(2806051, "457322918737678760", 76634062508)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+--setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+--setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Digital Story  Art Collection - Halo Campaign Evolved (AppID: 4192200)
+addappid(4192200)
+addtoken(4192200, "10428145664877263814")
+addappid(4192200, 1, "ba2cc915e2f0112e4af89f7147a980880639c08aed6b47703282f5175355a021") -- Digital Story  Art Collection - Halo Campaign Evolved - Depot 4192200
+--setManifestid(4192200, "7241924470348670959", 2889807339)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4073600) -- Foundry Armory Pack - Halo Campaign Evolved
+addtoken(4073600, "940170714388947598")
+addappid(4485640) -- Halo Campaign Evolved - Premium Edition Upgrade
+addtoken(4485640, "10481697796346159886")
