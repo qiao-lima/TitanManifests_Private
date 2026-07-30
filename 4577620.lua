ab40d6c108aa61695f18646aef3bc78b2c409a58
qiao-lima <qiao-lima@github.com>
2026-07-30 11:29:25 +0800

    Manual Update 4577620

---
+++ 4577620.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4577620 - Bills Must Be Paid Demo
+-- Generated 2026-07-30 02:36:01 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(4577620)
+
+-- Main Depots
+addappid(4577622, 1, "3836317b405ab1a54498d0d0b5fd384da2a36d11e1620b31764832c9e9311a7b") -- (windows)
+setManifestid(4577622, "8965938094380586626", 666592699)
+
+-- Missing Depots (We don't have the keys yet lol): 4577623
