82e90fcb46e6fb4b3dad4098cfbcb67c3c9f7bba
qiao-lima <qiao-lima@github.com>
2026-07-30 11:27:17 +0800

    Manual Update 3246690

---
+++ 3246690.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3246690 - MELONS MILKSHAKE: THE CREAMPIED MOMMY
+-- Generated 2026-07-30 03:17:10 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3246690)
+
+-- Main Depots
+addappid(3246691, 1, "0946db55b2618a3194b6bf4b7e1446e48f2aa8e70c149a1acd1ec412b25332f5")
+setManifestid(3246691, "7472426624730805287", 251266569)
