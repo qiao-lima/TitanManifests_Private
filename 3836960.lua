ae41fe46993ea45a3417db43965d679ef513fec7
qiao-lima <qiao-lima@github.com>
2026-07-30 11:27:19 +0800

    Manual Update 3836960

---
+++ 3836960.lua
@@ -0,0 +1,16 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3836960 - ENCOUNTER BAR
+-- Generated 2026-07-30 03:16:46 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3836960, 1, "bd8cae0611ffbef6d416c95491d398f89eb920007640978a50933bf3fdb40640")
+
+-- Main Depots
+addappid(3836961, 1, "b78fca717f1f37230078936cb64cd00e1987a06cb513ebad1ee03823a70a3d4b")
+setManifestid(3836961, "5134616501486978400", 397692008)
+
+-- DLC's (no depot keys required)
+addappid(3901860) -- ENCOUNTER BAR - Salsa的挑逗礼包
+addappid(4041710) -- ENCOUNTER BAR - Maid的高潮礼包
+addappid(4041720) -- ENCOUNTER BAR - Vivian的SM礼包
