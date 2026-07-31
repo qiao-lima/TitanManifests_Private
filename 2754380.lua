68aa720c7a2b6084bdedd1deeee6335fc0f5d85d
qiao-lima <qiao-lima@github.com>
2026-07-31 19:04:12 +0800

    Manual Update 2754380

---
+++ 2754380.lua
@@ -0,0 +1,21 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2754380 - The Roottrees are Dead
+-- Generated 2026-07-31 11:03:42 UTC
+-- # Depots (Total/DLC/Shared): 4/1/0
+
+-- Main AppID
+addappid(2754380, 1, "36f2a533bd44ae7ba2f5776fe764026bff6d177d28fff60cd38a3af11815dee3")
+
+-- Main Depots
+addappid(2754381, 1, "a144e003752b9be3b56e1699fd3513b52775d8de366af34cd9464bf289edc3b0") -- (windows, 64-bit)
+setManifestid(2754381, "6319422680076939883", 925001182)
+addappid(2754382, 1, "21607E0D78125818E58BB6096BA915A6243B3519103469661F91415049AD15D0") -- (macos, 64-bit)
+setManifestid(2754382, "8573609045154482072", 1006565389)
+addappid(2754383, 1, "0D744A745C9FA391B25B9A66312B594082B72580521C285A07639BD6F11D09B7") -- (linux, 64-bit)
+setManifestid(2754383, "7162062451633398737", 897492768)
+
+-- DLC's (with depot keys)
+-- The Roottrees are Dead Soundtrack (AppID: 3578780)
+addappid(3578780)
+addappid(3578781, 1, "5419685cf1fbff506f9e1866fbb3d500057abb081bdf239f099bfc2e1b042d5f")
+setManifestid(3578781, "7169054802440325139", 72126480)
