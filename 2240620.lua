4789319def0496d3cc7549dc8ffe144039c2141a
qiao-lima <qiao-lima@github.com>
2026-07-31 19:04:12 +0800

    Manual Update 2240620

---
+++ 2240620.lua
@@ -0,0 +1,23 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2240620 - UNBEATABLE
+-- Generated 2026-07-31 08:18:20 UTC
+-- # Depots (Total/DLC/Shared): 4/3/0
+
+-- Main AppID
+addappid(2240620, 1, "0b9f9476af9653b00b10e7ab09243919b4f73092c54831ca40fd8d3f22b89570")
+
+-- Main Depots
+addappid(2240621, 1, "80b19fbd6c9057548e637a2e743cb48d753af68fa08227be8200dc8e3cc33753")
+setManifestid(2240621, "1980536609350468931", 34707525538)
+
+-- DLC's (with depot keys)
+-- UNBEATABLE - Breakout Edition Upgrade (AppID: 3951650)
+addappid(3951650)
+addappid(3951650, 1, "706689526031d649f39c7ce4ed4c578bfffbc570afae867b5c7464e12b7259ed")
+setManifestid(3951650, "7217017791303010860", 45055150)
+-- UNBEATABLE - The Jamie Paige Content Companion (AppID: 4796640)
+addappid(4796640)
+addappid(2240622, 1, "edc969336542b43d505d010672a2062c10303456f95a9e556f157ebdad629e05")
+setManifestid(2240622, "7576859122192423823", 845508577)
+
+-- Missing Depots (We don't have the keys yet lol): 4118720
