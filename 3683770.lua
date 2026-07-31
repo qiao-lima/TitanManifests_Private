26d524cc0501240193831ebcd9242dba05536d4a
qiao-lima <qiao-lima@github.com>
2026-07-31 15:20:31 +0800

    Manual Update 3683770

---
+++ 3683770.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3683770 - Boba Cafe Simulator
+-- Generated 2026-07-31 01:39:59 UTC
+-- # Depots (Total/DLC/Shared): 4/0/2
+
+-- Main AppID
+addappid(3683770, 1, "806268cd176417e23f4b6cf0384a0398611ff81017d30b394ab10e9ad26b0d96")
+
+-- Main Depots
+addappid(3683771, 1, "33607e45578e4e6c16c8a04b37464bc01872718a1021df09fbc986410192064f")
+setManifestid(3683771, "699530402160116771", 3023314683)
+addappid(3683772, 1, "45f179e1f84c69b6a0056baddf27163c4497c273bf8f1bb55988134d8095fd38") -- (macos, 64-bit)
+setManifestid(3683772, "6440103399704852571", 3643007103)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
