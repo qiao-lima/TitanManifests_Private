e32acd6912416307a2a6865d0421eda5f313e2de
qiao-lima <qiao-lima@github.com>
2026-07-31 08:36:20 +0800

    Manual Update 4031490

---
+++ 4031490.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4031490.lua
+--Gamename キスリム
+addappid(4031490) --Mainappid キスリム
+addappid(4031491, 1, "754a6ecfab55020b6622fc93f829c49ff2ac990ed7601504913ac440039c91dd") --Main Windows Depot キスリム
+setManifestid(4031491, "3506450940204913809", 647133056)
+--Missing Dlcs
+--Dlcname Kissrim - Content Expansion Pack
