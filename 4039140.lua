ddcc26aaa98911bca2d889922710315b503dc024
qiao-lima <qiao-lima@github.com>
2026-07-31 08:36:20 +0800

    Manual Update 4039140

---
+++ 4039140.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4039140 - Subway Builder
+-- Generated 2026-07-30 23:13:41 UTC
+-- # Depots (Total/DLC/Shared): 5/0/0
+
+-- Main AppID
+addappid(4039140)
+
+-- Main Depots
+addappid(4039141, 1, "885d511987f450168feb6794c8541d4698bfb0d5e98a6428b24601d19b6ccc89") -- (windows)
+setManifestid(4039141, "3682741504337241005", 629925107)
+addappid(4039143, 1, "65dbd77266a470322b391ca032d9ed1a035dc8f1bfb62deb0ed9a5b57c70ebc4") -- (macos)
+setManifestid(4039143, "674921000879865520", 792488827)
+addappid(4039144, 1, "b22b5a3f2b4ed75d082bf754b02a192d82b09a87344c4ae9290372a1e7dc0cf9") -- (linux)
+setManifestid(4039144, "9066240000812092037", 611657466)
+
+-- Missing Depots (We don't have the keys yet lol): 4039142, 4039145
