44b3c03549edbd425c1054014fb7a51a9ab70e2a
qiao-lima <qiao-lima@github.com>
2026-07-31 10:06:14 +0800

    Manual Update 1124180

---
+++ 1124180.lua
@@ -0,0 +1,28 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1124180 - Rail Route
+-- Generated 2026-07-31 02:05:43 UTC
+-- # Depots (Total/DLC/Shared): 5/2/0
+
+-- Main AppID
+addappid(1124180, 1, "dc3cca428261e662f28d43a2c0fea65b846eb96d77a9ffbbac897c9f5ad902b2")
+
+-- Main Depots
+addappid(1124182, 1, "81bb06ebf8b5dd9a399a9d53dcc4757d72d52e79abf196618e56468ecd339e95") -- (windows)
+setManifestid(1124182, "6366749901654415127", 720618429)
+addappid(1124183, 1, "8f80b57f698bd67ec3f502fb703dfca2f502c9223f1e7f6cbd2c43fe809229bc") -- (macos)
+setManifestid(1124183, "4442490516743746387", 771688639)
+addappid(1124184, 1, "b034d6a660d0f077240a0059876177ac31e50ac363f6f328f44eb16ef9d950fd") -- (linux)
+setManifestid(1124184, "213600001412129827", 749669045)
+
+-- DLC's (no depot keys required)
+addappid(2943430) -- Rail Route - Happy Passengers
+addappid(3570380) -- Rail Route - Curated Map Pack
+addappid(4540370) -- Rail Route - Expect Delays
+
+-- DLC's (with depot keys)
+-- Rail Route - Soundtrack and Music Player (AppID: 2788780)
+addappid(2788780)
+addappid(2788781, 1, "4262165e1a12d8a626843a0f60ed12b292278d56e806364c41e36ffd678ea987")
+setManifestid(2788781, "2824733118001773387", 67741119)
+addappid(2788782, 1, "2262279722aa69bc36b435851eeb02d2c2ca074b5a2ae16987be5b3bdbb9ba9a")
+setManifestid(2788782, "5558993961383088647", 747634100)
