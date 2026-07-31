a59521acc02cd53d3bfb7c0a3b92582d110df497
qiao-lima <qiao-lima@github.com>
2026-07-31 09:06:02 +0800

    Manual Update 3093590

---
+++ 3093590.lua
@@ -0,0 +1,27 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3093590 - Kemopop!
+-- Generated 2026-07-31 01:04:57 UTC
+-- # Depots (Total/DLC/Shared): 6/3/0
+
+-- Main AppID
+addappid(3093590, 1, "d2ef3d71acee18fec388c39f07d9c1fbe04beec5eecd1782a43a250170ebaa43")
+
+-- Main Depots
+addappid(3093591, 1, "bb1643f6f7a3b4bd4f286d307082298064737c237f7cd8a8546dc399590f82d7") -- (windows)
+setManifestid(3093591, "8030094015888848853", 629870900)
+addappid(3093592, 1, "9856967b46d610ef9086b2f912d23d060b782860fc2df2ea559d070c0fea0ffc") -- (linux)
+setManifestid(3093592, "3580721502340906650", 603582884)
+
+-- DLC's (with depot keys)
+-- Kemopop! Soundtrack (AppID: 3136200)
+addappid(3136200)
+addappid(3136201, 1, "9d99038dc415c46a0b6cfba4f39da72e1fcf7c5ab7feee57c317eccfb1397271")
+setManifestid(3136201, "7085987456968709172", 82784986)
+addappid(3136202, 1, "014664a611fa7da95e2cc1d55d6520e167861500574e3d1b2b3c87e0f029984e")
+setManifestid(3136202, "3577267681198452407", 394353459)
+-- Kemopop! Boys in Hawaii! (AppID: 3257130)
+addappid(3257130)
+addappid(3257130, 1, "3175103c641185286c081e55cc0197ccd774ef29d17ebe7356d78e9a1e581089")
+setManifestid(3257130, "1759020458883877113", 739038106)
+
+-- Missing Depots (We don't have the keys yet lol): 3093593
