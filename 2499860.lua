5fb1039fcbfaabbd86ed79907a729afb151cd5b5
qiao-lima <qiao-lima@github.com>
2026-07-31 23:07:29 +0800

    Manual Update 2499860

---
+++ 2499860.lua
@@ -0,0 +1,27 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2499860 - DRAGON QUEST VII Reimagined
+-- Generated 2026-07-31 14:57:46 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(2499860, 1, "1e58a20f0aa0e0d214a207103d0d8042cd3d432a0ecf06e62a381366aa724dbe")
+
+-- Main Depots
+addappid(2499861, 1, "78d85bb7957e80d4fac0655a65ff9bee61dafc06c753de2713726e9860d6a860")
+setManifestid(2499861, "6283106044578104469", 8342849409)
+
+-- DLC's (no depot keys required)
+addappid(3257600) -- DRAGON QUEST VII Reimagined: The Road of Regal Wretches
+addappid(3378680) -- DRAGON QUEST VII Reimagined: Luminary's Livery
+addappid(3378690) -- DRAGON QUEST VII Reimagined: Jam-Packed Swag Bag
+addappid(3378700) -- DRAGON QUEST VII Reimagined: Early Bird Bonus: Trodain Togs, Seed of Proficiency x3
+addappid(3378720) -- DRAGON QUEST VII Reimagined: Silver Slime Shield
+addappid(3378760) -- DRAGON QUEST VII Reimagined: Item Set: Gold Bracer, Seed of Magic
+addappid(3543080) -- DRAGON QUEST VII Reimagined: White Wolf Costume
+addappid(3995580) -- DRAGON QUEST VII Reimagined: Additional DLC Sets
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Token-locked DLCs (couldn't read depots): 3378750
