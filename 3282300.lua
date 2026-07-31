cd05884c8e5071c6912fa52a1ecdd45e2c0899fb
qiao-lima <qiao-lima@github.com>
2026-07-31 08:35:52 +0800

    Manual Update 3282300

---
+++ 3282300.lua
@@ -0,0 +1,20 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3282300 - Mistfall Hunter
+-- Generated 2026-07-30 21:55:48 UTC
+-- # Depots (Total/DLC/Shared): 4/0/2
+
+-- Main AppID
+addappid(3282300)
+
+-- Main Depots
+addappid(3282301, 1, "9f7b869c77fc59d473b73126990a24d4f6f45d5d87fe6cc97e6c45e085b8b187")
+setManifestid(3282301, "2642696512229785043", 41871123617)
+addappid(3282302, 1, "bb3b0f6ef8d41954c574b4ca98950102ebe524e81ccee9f67430b750f39a492e")
+setManifestid(3282302, "7311184871978359361", 41644789101)
+
+-- DLC's (no depot keys required)
+addappid(4572310) -- Mistfall Hunter - Upgrade to Deluxe Edition
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
