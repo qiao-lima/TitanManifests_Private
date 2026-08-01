e4d74ab50195b7465a70c080afc8c2bf5078d25d
qiao-lima <qiao-lima@github.com>
2026-08-01 17:23:57 +0800

    Manual Update 3934270

---
+++ 3934270.lua
@@ -0,0 +1,19 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3934270 - How Many Dudes?
+-- Generated 2026-08-01 07:56:21 UTC
+-- # Depots (Total/DLC/Shared): 3/2/0
+
+-- Main AppID
+addappid(3934270)
+
+-- Main Depots
+addappid(3934271, 1, "36c4014d117bde08b3fe39c4f0974fe3fd9dab69393e815c7c7fd008c0680363")
+setManifestid(3934271, "7400163792309379566", 196509242)
+
+-- DLC's (with depot keys)
+-- How Many Dudes? - Supporter Pack (AppID: 4815310)
+addappid(4815310)
+addappid(4815311, 1, "a71a82affca1675f08f3e9e13b4c1f6f912196c28f2cc0ea63582216221db25f")
+setManifestid(4815311, "3677334875839138521", 77698472)
+
+-- Missing Depots (We don't have the keys yet lol): 4769791
