1844d0c163d82bd44583b5edbdb3684b4d45cd4b
qiao-lima <qiao-lima@github.com>
2026-08-01 17:28:24 +0800

    Manual Update 4929970

---
+++ 4929970.lua
@@ -0,0 +1,21 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4929970 - No Socks RPG
+-- Generated 2026-08-01 08:12:55 UTC
+-- # Depots (Total/DLC/Shared): 4/2/0
+
+-- Main AppID
+addappid(4929970, 1, "cf86a1cd618843c36a0cc194adfdd9922a0b8bca2ab0b2e918ec1ea7dc2cd747")
+
+-- Main Depots
+addappid(4929971, 1, "d52d3e344542db326e19499a99160d0bccb658fa6d1283d17d3d8a02753f552c") -- (windows)
+setManifestid(4929971, "5388726005361771858", 732918857)
+addappid(4929972, 1, "ec92a70d16f7ddcc0c990efc9c9a399fe8d3ab873ba0280c13c16c844667c124") -- (macos)
+setManifestid(4929972, "1675910577732776509", 1562915979)
+
+-- DLC's (with depot keys)
+-- No Socks RPG Demo (AppID: 4930240)
+addappid(4930240)
+addappid(4930241, 1, "19dec417e4dbc2812a1b8aec0451dcde923a5dae8d1233f6cacdc910520f0f06") -- (windows)
+setManifestid(4930241, "7150404980429655330", 696700546)
+
+-- Missing Depots (We don't have the keys yet lol): 4930242
