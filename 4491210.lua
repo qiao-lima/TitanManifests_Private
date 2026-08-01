855168db1554670016db0876c289797b41f62585
qiao-lima <qiao-lima@github.com>
2026-08-01 17:28:24 +0800

    Manual Update 4491210

---
+++ 4491210.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4491210 - FUTA FIGHT & FUCK 🥊
+-- Generated 2026-08-01 09:01:10 UTC
+-- # Depots (Total/DLC/Shared): 4/1/0
+
+-- Main AppID
+addappid(4491210)
+
+-- Main Depots
+addappid(4491211, 1, "701bd7ff1d125359ba89f8bab04d6834adb229be93d8bafc590b90e483fe98df")
+setManifestid(4491211, "2107159404591161319", 1270010878)
+
+-- Missing Depots (We don't have the keys yet lol): 4491212, 4491213, 4696650
