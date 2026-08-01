69fa32e30d57b0e0a0f37a0845e715e902590852
qiao-lima <qiao-lima@github.com>
2026-08-01 17:23:55 +0800

    Manual Update 3524640

---
+++ 3524640.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3524640 - Fallen Elf Freya Demo
+-- Generated 2026-08-01 08:23:56 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3524640)
+
+-- Main Depots
+addappid(3524641, 1, "07a17b8f5b025fa9c107fe51c4c4b3687a2ffd62d12d263e8a1217d544f9dcee")
+setManifestid(3524641, "1763084808071512721", 705216827)
