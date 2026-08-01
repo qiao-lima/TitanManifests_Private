f09a1bd12641daa28923bd1cf277142432e3a4db
qiao-lima <qiao-lima@github.com>
2026-08-01 17:19:55 +0800

    Manual Update 2303120

---
+++ 2303120.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2303120 - Atomic Heart - Original Soundtrack
+-- Generated 2026-08-01 08:57:44 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(2303120)
+
+-- Main Depots
+addappid(2303121, 1, "6fd3db1eba411d9ecc68741d7ebf12f38bfb738c8578dd71419af57f7b2f73c4")
+setManifestid(2303121, "3587340790320923187", 852516865)
+addappid(2303122, 1, "49808584cc51f185a98e76678db9045fcfccc7474b9bccb847a92aa223ed8aff")
+setManifestid(2303122, "41228753438295057", 5122844316)
