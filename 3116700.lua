7de5533dfb70674e89f68cf5129e538f284dfa4e
qiao-lima <qiao-lima@github.com>
2026-08-01 15:45:51 +0800

    Manual Update 3116700

---
+++ 3116700.lua
@@ -0,0 +1,24 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3116700 - Reap and Rush
+-- Generated 2026-08-01 07:36:38 UTC
+-- # Depots (Total/DLC/Shared): 4/1/2
+
+-- Main AppID
+addappid(3116700)
+
+-- Main Depots
+addappid(3116701, 1, "59e214b6551875afe40dc6b443eb79edd0708241d372252991ec7c145cd584b8")
+setManifestid(3116701, "3845860546358417947", 3285380166)
+
+-- DLC's (no depot keys required)
+addappid(4942620) -- 地府有点忙 - 开发者支持包
+
+-- DLC's (with depot keys)
+-- 地府有点忙 Soundtrack (AppID: 4942910)
+addappid(4942910)
+addappid(4942911, 1, "844ac7e3d3116875a656f15d7f8fcf40295dbf41637fd2918cf06e8d395fa21e")
+setManifestid(4942911, "8718121260582521960", 81645345)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
