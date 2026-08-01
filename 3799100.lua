7b69c30eecdb015ca5e6197f59d729bc181b1f6b
qiao-lima <qiao-lima@github.com>
2026-08-01 17:23:56 +0800

    Manual Update 3799100

---
+++ 3799100.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3799100 - Side Effects
+-- Generated 2026-08-01 09:08:13 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3799100, 1, "b4f394d0f71092bd40c699aee9de04fb795ac450d4d792f7c751d89f69129370")
+
+-- Main Depots
+addappid(3799101, 1, "cb02a7ff0425aed0f5a9ea9376e2c2f0fb2a59cc14d14b7f23792e63fc6d0aa7")
+setManifestid(3799101, "5112222304742848737", 1798924464)
