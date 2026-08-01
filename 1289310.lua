2c089d8f9a12a98af00d0d4cf7818f54983b32c6
qiao-lima <qiao-lima@github.com>
2026-08-01 17:19:53 +0800

    Manual Update 1289310

---
+++ 1289310.lua
@@ -0,0 +1,25 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1289310 - Helltaker
+-- Generated 2026-08-01 09:16:13 UTC
+-- # Depots (Total/DLC/Shared): 6/1/0
+
+-- Main AppID
+addappid(1289310, 1, "61b522f50b7263640eec49fdf744ab50f1794d1391b19dc6435782ba4a15a4a0")
+
+-- Main Depots
+addappid(1289311, 1, "8a76446486a985b6fb8019f9ff9cb51717155afebaf093f777936ca31df07bd8") -- (windows, 64-bit)
+setManifestid(1289311, "9164867365911753620", 348099925)
+addappid(1289312, 1, "0aef62a3de894c4ef02336c5798e38c21a2a4564caa327e5412fe7d240811288") -- (macos)
+setManifestid(1289312, "5439647822064687051", 349158366)
+addappid(1289313, 1, "36a1150a257c814238b63986e8bd03f1179691714589bff626e2bf0b7d6794a2") -- (windows, 32-bit)
+setManifestid(1289313, "6051994899057452556", 341250557)
+addappid(1289314, 1, "0ae3f8de8828c5aa3bafe13d91bb5c66830de99cae356751025b076dfbff3cb1") -- (linux)
+setManifestid(1289314, "8723838119065609357", 349387016)
+addappid(1289315, 1, "77e4cd127cd6a189e8409ad5b3bed50b03b00b130c9e35a17f02b8d1daefa9fd")
+setManifestid(1289315, "6394422377711576735", 50692)
+
+-- DLC's (with depot keys)
+-- Helltaker: Artbook + Pancake Recipe (AppID: 1298590)
+addappid(1298590)
+addappid(1298590, 1, "839e47fd3b43d3eba1d5cff9162c1b94ee0ac5053f347e46cf3dbd1d8de44acd")
+setManifestid(1298590, "5335439833889055261", 169293695)
