cf4b6aad1e0bdd44eb2c5ec0ac25262a23c0372f
qiao-lima <qiao-lima@github.com>
2026-08-01 17:23:55 +0800

    Manual Update 3555430

---
+++ 3555430.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3555430 - Money Simulator: Brokeville
+-- Generated 2026-08-01 08:23:37 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(3555430, 1, "7468fe5f5753d3ec24f545418ef1ca42539b6574fe540708aa2399d30f2f9de0")
+
+-- Main Depots
+addappid(3555432, 1, "1bc56a50226c299e2b7accca279af332221d0dcfe4abcadfcfaabf9ca5277588")
+setManifestid(3555432, "6043154663710056053", 4379276564)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
