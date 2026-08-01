bf406793774296c8cccc1c5f047e7c65b69b675a
qiao-lima <qiao-lima@github.com>
2026-08-01 17:19:53 +0800

    Manual Update 1285190

---
+++ 1285190.lua
@@ -0,0 +1,39 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1285190 - Borderlands® 4
+-- Generated 2026-08-01 08:36:39 UTC
+-- # Depots (Total/DLC/Shared): 7/1/2
+
+-- Main AppID
+addappid(1285190, 1, "8e3b6ca08795b1f1eedfaecae2826907cb959185d8c42ecd658c614bc0e3529c")
+
+-- Main Depots
+addappid(1285191, 1, "aabb315ef5573fc2633c461cd2ac9b553e2713ca90a67ad2df09781274cbb20f") -- (windows)
+setManifestid(1285191, "8463128240834064140", 149385098620)
+addappid(1285192, 1, "3e212ce90691e4e8f2f1418c35495c50a725bb0d012d9f415b0a8a9119dfb0f4") -- (windows)
+setManifestid(1285192, "5558974000206017761", 866558480)
+addappid(1285193, 1, "5c38ce39a639096877f707cb88dea4250f2a4b53ae5e1b67654e243d2b988ede")
+setManifestid(1285193, "1587472964084640951", 0)
+addappid(1285195, 1, "ccf45efe0bf7e329f6b3fa0b29af3c0c7a609c49f6e9b7018bf644fe2f158a82") -- (windows)
+setManifestid(1285195, "218636985744352584", 18977609)
+
+-- DLC's (no depot keys required)
+addappid(3802360) -- Borderlands®4: Firehawk's Fury Weapon Skin
+addappid(3802370) -- Borderlands®4: Ornate Order Pack
+addappid(3802420) -- Borderlands®4 - Bounty Pack 2: Legend of the Stone Demon
+addappid(3802450) -- Borderlands®4 - Story Pack 1: Mad Ellie and the Vault of the Damned
+addappid(3829670) -- Borderlands®4: Bounty Pack Bundle
+addappid(3829680) -- Borderlands®4: Vault Hunter Pack
+addappid(4598000) -- Borderlands®4 - Bounty Pack 3: A Zane to Kill For
+addappid(4598010) -- Borderlands®4 - Bounty Pack 3: Vault Card 3
+addappid(4614350) -- Borderlands®4 - Bounty Pack 4: Murders and Acquisitions
+addappid(4614360) -- Borderlands®4 - Bounty Pack 4: Vault Card 4
+
+-- DLC's (with depot keys)
+-- Borderlands®4: Gilded Glory Pack (AppID: 2013850)
+addappid(2013850)
+addappid(2013850, 1, "f0f6f03e213ec6effac974bc92309dc05e11e8d49013fabccdc253f8479d1c44")
+setManifestid(2013850, "1885194575676718061", 11)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
