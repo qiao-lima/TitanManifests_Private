a29077376564e6c9d4755d429c9d2736a0b2d94b
qiao-lima <qiao-lima@github.com>
2026-08-01 17:28:24 +0800

    Manual Update 4468150

---
+++ 4468150.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4468150 - Hot Girls Gym - Nudist Gym Manager
+-- Generated 2026-08-01 08:09:49 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4468150)
+
+-- Main Depots
+addappid(4468151, 1, "307a8acba4d31037efae6808ecf6a7762f99203a779402f140bf38411bd9abc9") -- (windows, 64-bit)
+setManifestid(4468151, "5701748672684876312", 4092724224)
