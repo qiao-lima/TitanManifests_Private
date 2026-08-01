b446b294ff49d381a6f2588aaeb6152203e3d0d9
qiao-lima <qiao-lima@github.com>
2026-08-01 17:28:25 +0800

    Manual Update 696170

---
+++ 696170.lua
@@ -0,0 +1,51 @@
+-- Generated with Luie @ https://lua.tools/
+-- 696170 - SENRAN KAGURA Peach Beach Splash
+-- Generated 2026-07-29 16:56:51 UTC
+-- # Depots (Total/DLC/Shared): 4/1/2
+
+-- Main AppID
+addappid(696170)
+
+-- Main Depots
+addappid(696171, 1, "f2a70bd8a233738a89e8dd85ff113275521c835401e52d8abdb3a67a88243112")
+setManifestid(696171, "1283774790167967241", 23060383029)
+
+-- DLC's (no depot keys required)
+addappid(793530) -- SENRAN KAGURA Peach Beach Splash - Awakened Character Pack
+addappid(793531) -- SENRAN KAGURA Peach Beach Splash - Mega Accessory Pack
+addappid(793532) -- SENRAN KAGURA Peach Beach Splash - Mega Weapon Pack
+addappid(793533) -- SENRAN KAGURA Peach Beach Splash - Hairstyle and Extension Pack
+addappid(793534) -- SENRAN KAGURA Peach Beach Splash - Mega Outfit Pack 1
+addappid(793535) -- SENRAN KAGURA Peach Beach Splash - Mega Outfit Pack 2
+addappid(793536) -- SENRAN KAGURA Peach Beach Splash - Taunt Pack
+addappid(793537) -- SENRAN KAGURA Peach Beach Splash - Sakura Swimsuit Pack
+addappid(793538) -- SENRAN KAGURA Peach Beach Splash - Hanzō Item Pack
+addappid(793539) -- SENRAN KAGURA Peach Beach Splash - Crimson Squad Item Pack
+addappid(793540) -- SENRAN KAGURA Peach Beach Splash - Gessen Item Pack
+addappid(793541) -- SENRAN KAGURA Peach Beach Splash - Hebijo Item Pack
+addappid(793542) -- SENRAN KAGURA Peach Beach Splash - Mikagura Item Pack
+addappid(793543) -- SENRAN KAGURA Peach Beach Splash - NW Item Pack
+addappid(793544) -- SENRAN KAGURA Peach Beach Splash - Basho Character Pack
+addappid(793545) -- SENRAN KAGURA Peach Beach Splash - DOAX3 Collaboration Pack
+addappid(793546) -- SENRAN KAGURA Peach Beach Splash - Super Sonico Character Pack
+addappid(793547) -- SENRAN KAGURA Peach Beach Splash - Ikki Tōsen EE Collaboration Pack
+addappid(793548) -- SENRAN KAGURA Peach Beach Splash - Ikki Tōsen EE Card Pack
+addappid(793549) -- SENRAN KAGURA Peach Beach Splash - VALKYRIE DRIVE -BHIKKHUNI- Collaboration Pack 
+addappid(793550) -- SENRAN KAGURA Peach Beach Splash - VALKYRIE DRIVE -BHIKKHUNI- Card Pack 
+addappid(793551) -- SENRAN KAGURA Peach Beach Splash - Neptune Character Pack
+addappid(793552) -- SENRAN KAGURA Peach Beach Splash - Special Outfit Pack
+addappid(793553) -- SENRAN KAGURA Peach Beach Splash - Dengeki Pack
+addappid(793554) -- SENRAN KAGURA Peach Beach Splash - Famitsu Pack
+addappid(793750) -- SENRAN KAGURA Peach Beach Splash - Clean-Up Maid Pack
+addappid(793751) -- SENRAN KAGURA Peach Beach Splash - Eternal Summer Camisole
+addappid(915340) -- SENRAN KAGURA Peach Beach Splash - Sunshine Swimsuit Pack
+
+-- DLC's (with depot keys)
+-- DLC 793555 (AppID: 793555)
+addappid(793555)
+addappid(793555, 1, "dc09ddd624e703d5bc28bdf82c687888ca820bb20bb4581a395b39e29081e166")
+setManifestid(793555, "8123557547448820850", 174665029)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
