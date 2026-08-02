df1a345c477b7499fd7f392e41e58cb0f6bf298f
qiao-lima <qiao-lima@github.com>
2026-08-02 13:38:35 +0800

    Manual Update 2394650

---
+++ 2394650.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2394650 - Crypt Custodian
+-- Generated 2026-08-02 04:39:13 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2394650)
+
+-- Main Depots
+addappid(2394651, 1, "025ab4303ab5f8d193676b20d64663eaf6418bfd1ab8be75d3c404cb67c597f1")
+setManifestid(2394651, "6880077896267403089", 559860949)
