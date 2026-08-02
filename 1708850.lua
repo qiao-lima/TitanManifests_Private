1a8611bed67319a3453d171dd71feaadaebe20dd
qiao-lima <qiao-lima@github.com>
2026-08-02 09:05:40 +0800

    Manual Update 1708850

---
+++ 1708850.lua
@@ -0,0 +1,25 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1708850 - Drug Dealer Simulator 2
+-- Generated 2026-08-01 09:12:13 UTC
+-- # Depots (Total/DLC/Shared): 4/1/2
+
+-- Main AppID
+addappid(1708850, 1, "2ed9510c7d5533c55a1929f4dbd468c1a6a8ceb3003dd0a3f927d5572417df04")
+
+-- Main Depots
+addappid(1708851, 1, "9da58ce930b5409698dfdd9945f19efdbe1e0b7acead98d61080b5b7058f9890")
+setManifestid(1708851, "6220526600698612644", 17467074921)
+
+-- DLC's (no depot keys required)
+addappid(3169480) -- Drug Dealer Simulator 2: Casino
+addappid(3385220) -- Drug Dealer Simulator 2: White Xmas Free DLC
+
+-- DLC's (with depot keys)
+-- Drug Dealer Simulator 2: New Life - A Comic Book Prequel (AppID: 3064210)
+addappid(3064210)
+addappid(3064210, 1, "a2662bf7c65a5827264dfb642bb6dec3ab58a1e2025302d82f67797be1ae4c79")
+setManifestid(3064210, "5029667159910191789", 23492197)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
