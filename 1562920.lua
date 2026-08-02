ce1508b514ae8e98314b26d145c78ab46aa8bf7f
qiao-lima <qiao-lima@github.com>
2026-08-02 17:28:42 +0800

    Manual Update 1562920

---
+++ 1562920.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1562920 - 7 Years From Now
+-- Generated 2026-08-02 09:25:10 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(1562920)
+
+-- Main Depots
+addappid(1562921, 1, "235a8632e8dc285dda762bbbf565d23fa30d2e2995d04da43c8861be2716ed2d") -- (windows)
+setManifestid(1562921, "3323765808959111741", 290412040)
