4dfb88c3f7d3a3997b8318d2776f4c5eecb1a95c
qiao-lima <qiao-lima@github.com>
2026-08-02 17:28:43 +0800

    Manual Update 2392500

---
+++ 2392500.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2392500 - Ever 17 - The Out of Infinity
+-- Generated 2026-08-02 09:25:26 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2392500)
+
+-- Main Depots
+addappid(2392501, 1, "2c5fc59275ad44981947a41574e33c7eae97db7a03d72e2bd056f3f8abe352d4") -- (windows, 64-bit)
+setManifestid(2392501, "6213077749480317156", 2869676449)
