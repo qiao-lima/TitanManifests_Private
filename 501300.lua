2cc474bcaa78b49a21dd64f368e749f5af4d6c45
qiao-lima <qiao-lima@github.com>
2026-08-02 17:28:44 +0800

    Manual Update 501300

---
+++ 501300.lua
@@ -0,0 +1,21 @@
+-- Generated with Luie @ https://lua.tools/
+-- 501300 - What Remains of Edith Finch
+-- Generated 2026-08-02 09:24:03 UTC
+-- # Depots (Total/DLC/Shared): 4/1/2
+
+-- Main AppID
+addappid(501300, 1, "3811cee5832fcb7005fdf48f5f8b920743c8e1e1d43454ef2389d39a998505f0")
+
+-- Main Depots
+addappid(501301, 1, "957d96d75b0602ec2946f46e298b1c9179a719d977dd1e80d037e13f9c4a7f7d")
+setManifestid(501301, "3230192986366154672", 2540778487)
+
+-- DLC's (with depot keys)
+-- What Remains of Edith Finch - Original Soundtrack (AppID: 629930)
+addappid(629930)
+addappid(629931, 1, "15aabf292cf8cf590cf21bd3b946c41d5bac32bb4c0319bf218d226d8e9523bf")
+setManifestid(629931, "1238865701426985327", 83109889)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
