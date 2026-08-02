3ba68b045bb790d8992efe836694f56903017e73
qiao-lima <qiao-lima@github.com>
2026-08-02 13:45:05 +0800

    Manual Update 4578490

---
+++ 4578490.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4578490 - Mei Music
+-- Generated 2026-08-02 05:24:20 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4578490)
+
+-- Main Depots
+addappid(4578491, 1, "3d8c1d76504d3f61655b857e3ee025ed078d94104c7eafea163b19d73fcb1992") -- (windows)
+setManifestid(4578491, "6484420744617396690", 29006247)
