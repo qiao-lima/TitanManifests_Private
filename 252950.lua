ce73114521b0ddddaa2a506b50381e969b0bfdc5
qiao-lima <qiao-lima@github.com>
2026-08-02 13:38:36 +0800

    Manual Update 252950

---
+++ 252950.lua
@@ -0,0 +1,69 @@
+-- Generated with Luie @ https://lua.tools/
+-- 252950 - Rocket League
+-- Generated 2026-08-02 05:30:06 UTC
+-- # Depots (Total/DLC/Shared): 17/0/3
+
+-- Main AppID
+addappid(252950, 1, "97978ac90e1684bd157296ba59e97d1abcd1df15d9337e4d6c18d1e1f7ef53c3")
+
+-- Main Depots
+addappid(252951, 1, "2c5162dd5164c160a18d4a0086a9a540bf27b1879d0a0e7c69e2d5e0b287e681") -- (windows)
+setManifestid(252951, "1164224895606048168", 42450737001)
+addappid(252952, 1, "e07657ba2a95aa1599fee2e0dc27f86d21c280c6e36295d61a10478251efbd80") -- Japanese
+setManifestid(252952, "4510403162949335447", 3)
+addappid(252953, 1, "82fe5333148dc6dadd932dd622d8594a3f2cfbc96bdc5dd70a2261ebb90f2c69") -- French
+setManifestid(252953, "2514327339850257685", 3)
+addappid(252954, 1, "706ffd8207c3bf1bf598421fd53ae108587bcf77f03cd5be2d1bd03e510aacf7") -- Italian
+setManifestid(252954, "6604331299753353584", 3)
+addappid(252955, 1, "9cdc308ddc198a1176112ee82dec8c39c35d412a638ea50ffddaec6bb9c112ae") -- German
+setManifestid(252955, "9065328472878819409", 3)
+addappid(252956, 1, "ed2bc8a848fd33009e08cf5ab297e2530dcf0605e33d2b072e4d8b68ef75da24") -- Spanish
+setManifestid(252956, "316973403359005795", 3)
+addappid(252957, 1, "0ac63ca60da191510247cf382fdbf94bae44000450557b658a7f93b49fb4457b") -- Dutch
+setManifestid(252957, "550143062542037081", 3)
+addappid(252958, 1, "fbd245fd77848586642e8c5c02b0478a4810b4e895edbddd11e8771f6a637ed0") -- Portuguese
+setManifestid(252958, "8936650610860102348", 3)
+addappid(252959, 1, "93c5d4454fc08a119685a61c5b02af961a183eefa3550a67b5c64a8b1170d205") -- Koreana
+setManifestid(252959, "4386213019025687925", 3)
+addappid(252960, 1, "0729c40cd6ccb20223e55f65ff65e386c7ddbe91b91ea3eb3d56d2634e3c6543") -- Turkish
+setManifestid(252960, "4118993315564346938", 3)
+addappid(252961, 1, "97fc37ccba41374f4d55ae03feeeb2144def3f55ece0856e41f6162a730a81f5") -- (linux)
+setManifestid(252961, "4730643967328491213", 10795936576)
+addappid(252962, 1, "112d4200f836026eaa24f3ac6ff4c405dada93daa3779ab92e3dd387693c1dbb") -- Russian
+setManifestid(252962, "3990443630786511251", 3)
+addappid(252963, 1, "176b94925a2f7f9452c7089ffb870f53bdb9564c993deb43d1cadbc3beedc6b8") -- (macos)
+setManifestid(252963, "4010319018365688560", 10899770095)
+addappid(252964, 1, "fa024050d977900bd48d5c3e9968694a8161d792487375b1c9ee8944b679c583") -- Polish
+setManifestid(252964, "6304967143002381104", 3)
+
+-- DLC's (no depot keys required)
+addappid(391680) -- Rocket League® - Supersonic Fury DLC Pack
+addappid(399730) -- Rocket League® - Revenge of the Battle-Cars DLC Pack
+addappid(405170) -- Rocket League® - Back to the Future™ Car Pack
+addappid(408610) -- Rocket League® - Chaos Run DLC Pack
+addappid(438690) -- Rocket League® - Batman v Superman: Dawn of Justice Car Pack
+addappid(457160) -- Rocket League® - Proteus
+addappid(457162) -- Rocket League® - Vulcan
+addappid(457190) -- Rocket League® - Triton
+addappid(457191) -- Rocket League® - Hot Wheels® Twin Mill™ III
+addappid(457192) -- Rocket League® - The Fate of the Furious™ Ice Charger
+addappid(457194) -- Rocket League® - Hot Wheels® Bone Shaker™
+addappid(457195) -- Rocket League® - Aftershock
+addappid(457196) -- Rocket League® - Esper
+addappid(457197) -- Rocket League® - Masamune
+addappid(457198) -- Rocket League® - Marauder
+addappid(457199) -- Rocket League® - NBA Flag Pack
+addappid(675280) -- Rocket League® – Fast & Furious™ '70 Dodge® Charger R/T
+addappid(675290) -- Rocket League® – Fast & Furious™ '99 Nissan Skyline GT-R R34
+addappid(772150) -- Rocket League® - DC Super Heroes DLC Pack
+addappid(838770) -- Rocket League® - Jurassic World™ Car Pack
+addappid(910940) -- Rocket League® - Hot Wheels® Triple Threat DLC Pack
+addappid(943740) -- Rocket League® - McLaren 570S Car Pack
+addappid(1002130) -- Rocket League® - MLB Fan Pack
+addappid(1018530) -- Rocket League® - Ghostbusters™ Ecto-1 Car Pack
+addappid(1077690) -- Rocket League® - Knight Rider Car Pack
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+addappid(229033, 1, "9a789440bbc16131547ac6c062e5f168f7684da533a2899a71fdfaaeec21efc3") -- (windows)
