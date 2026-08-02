ce3b5686f0c127350c28a2762ad791f68e60ca04
qiao-lima <qiao-lima@github.com>
2026-08-02 17:28:44 +0800

    Manual Update 460950

---
+++ 460950.lua
@@ -0,0 +1,21 @@
+-- Generated with Luie @ https://lua.tools/
+-- 460950 - Katana ZERO
+-- Generated 2026-08-02 08:42:34 UTC
+-- # Depots (Total/DLC/Shared): 4/2/0
+
+-- Main AppID
+addappid(460950, 1, "8032eec020e5541737ed51223b0f4c541bedb08035f0623e214fab5786a18fe6")
+
+-- Main Depots
+addappid(460951, 1, "76672e24adb8b313cdc23f79a51982c3fd243414f79426ed46e8bbae264cb202") -- (windows)
+setManifestid(460951, "7302141199497370222", 227260007)
+addappid(460952, 1, "d0715cdb92b3e7017a3e6378d03cee9db4e669dbe34639616706f0a336e1c540") -- (macos)
+setManifestid(460952, "4979081960644650413", 245925800)
+
+-- DLC's (with depot keys)
+-- Katana ZERO Soundtrack (AppID: 1075710)
+addappid(1075710)
+addappid(1075711, 1, "b6f4fe40f6e957091d029ec75df5f69dc3eb451f9546fd25fc600b3fecddb514")
+setManifestid(1075711, "2317345883246807243", 1138181440)
+addappid(1075712, 1, "c4a9f4b5b6320b5b5eaa4afdadae99249df23bff0fea23321f3fd3560ac745a3")
+setManifestid(1075712, "8342623529071936919", 257386960)
