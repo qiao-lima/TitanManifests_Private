fd60d8d2aafaba1dde30e2a5f2764b0a5ee9e683
qiao-lima <qiao-lima@github.com>
2026-08-02 16:04:54 +0800

    Manual Update 2780980

---
+++ 2780980.lua
@@ -0,0 +1,20 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2780980 - LOCKDOWN Protocol
+-- Generated 2026-08-02 08:04:04 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(2780980, 1, "497193992e42b80f08a177748514004c9839995f9d22d2b84e4ffddc8adcd604")
+
+-- Main Depots
+addappid(2780981, 1, "b810b1ec8feac6ecba5f581a91d6cc4f32d3e7e37668fa911a7890c98941b508") -- (windows)
+setManifestid(2780981, "4000945935168615803", 2672678597)
+
+-- DLC's (no depot keys required)
+addappid(3391950) -- LOCKDOWN Protocol - Contrast
+addappid(3391960) -- LOCKDOWN Protocol - Winter
+addappid(3391970) -- LOCKDOWN Protocol - Tiphanie
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
