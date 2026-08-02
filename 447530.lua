b993c040214d14b91cbfbbf1ac6e6e7c004c71d8
qiao-lima <qiao-lima@github.com>
2026-08-02 17:28:44 +0800

    Manual Update 447530

---
+++ 447530.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 447530 - VA-11 Hall-A: Cyberpunk Bartender Action
+-- Generated 2026-08-02 09:24:55 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(447530, 1, "3834301957782fcbc360ddce377a704347e5bd6245c3f611452aead68fc54367")
+
+-- Main Depots
+addappid(447531, 1, "47fcb0672e67462091fdc31eae398a3710cdde69f7ead7e9addf905b627df0d7") -- (windows)
+setManifestid(447531, "774045415162108276", 239182266)
+addappid(447532, 1, "14a19be13d568a45a54303696f185911c8f92dd7312735eaf06002330e58dd04") -- (linux)
+setManifestid(447532, "6035459783033527047", 237787560)
+addappid(447533, 1, "efb2ad0bb91cea272ff67141a74130f693893d512b2130d1987b57402a92a03e") -- (macos)
+setManifestid(447533, "2504264547604944456", 240817249)
