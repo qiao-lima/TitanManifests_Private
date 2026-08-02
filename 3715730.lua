43934b619ae583f7c4d5dbd1e1fe2b36df7582d9
qiao-lima <qiao-lima@github.com>
2026-08-02 13:38:38 +0800

    Manual Update 3715730

---
+++ 3715730.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3715730 - 男娘恋爱物语
+-- Generated 2026-08-02 04:46:46 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3715730)
+
+-- Main Depots
+addappid(3715731, 1, "b2dd4804c330115987ed47b4954bbbfd40b7e90f58eb1dc03f41a3694d04a94a")
+setManifestid(3715731, "2133328210776836174", 637269668)
