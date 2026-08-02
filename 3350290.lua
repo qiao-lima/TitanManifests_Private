214bd260560de05bf4e16b13604c36be73b32e65
qiao-lima <qiao-lima@github.com>
2026-08-02 13:38:37 +0800

    Manual Update 3350290

---
+++ 3350290.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3350290 - Ravager - Moon-Friendly Edition
+-- Generated 2026-08-02 05:15:38 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3350290)
+
+-- Main Depots
+addappid(3350291, 1, "2c90ad380c2e11442f77cf035483e2b72f03c293a2de3f00490d16f322e40591") -- (64-bit)
+setManifestid(3350291, "979814126072883735", 3919664404)
