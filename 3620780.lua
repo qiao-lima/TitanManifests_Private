cd29582af5226d52a705b18b5387f5e12fc86afe
qiao-lima <qiao-lima@github.com>
2026-08-02 13:38:38 +0800

    Manual Update 3620780

---
+++ 3620780.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3620780 - AI-deal-Rays
+-- Generated 2026-08-02 05:19:56 UTC
+-- # Depots (Total/DLC/Shared): 2/1/0
+
+-- Main AppID
+addappid(3620780)
+
+-- Main Depots
+addappid(3620781, 1, "f12ac6abfda9bcf2d85eb2f81ee1efbfb313afe57e458694b144d11a937a2ba2") -- (windows, 64-bit)
+setManifestid(3620781, "6054473792289445222", 15839714663)
+
+-- Missing Depots (We don't have the keys yet lol): 4006901
