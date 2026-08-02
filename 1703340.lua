2404b22be46ccd6ebe7ace72ffd2a76265cff1b6
qiao-lima <qiao-lima@github.com>
2026-08-02 17:28:42 +0800

    Manual Update 1703340

---
+++ 1703340.lua
@@ -0,0 +1,23 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1703340 - The Stanley Parable: Ultra Deluxe
+-- Generated 2026-08-02 09:23:51 UTC
+-- # Depots (Total/DLC/Shared): 5/2/0
+
+-- Main AppID
+addappid(1703340, 1, "a6b615dade75006e07887d351cea07c56a2e6d57d50da3d8eb2bc311a059319a")
+
+-- Main Depots
+addappid(1703341, 1, "4ad33a8eebc89ec9c5958991c29f4e6107081b775da63780ea7a65863e8720b1") -- (windows)
+setManifestid(1703341, "666286276060684573", 7744662731)
+addappid(1703342, 1, "966eb6d445b023674763f835aa0f2785afa11b860655b1a61602f12208dbf796") -- (linux)
+setManifestid(1703342, "4431616934395467188", 7982568474)
+addappid(1703343, 1, "fc29c1a04e7652e4b7f63ee3f942884a38bfb77e3ffef99a95e6c289cc7d2923") -- (macos)
+setManifestid(1703343, "6417175135100130100", 7799070784)
+
+-- DLC's (with depot keys)
+-- The Stanley Parable: Ultra Deluxe Soundtrack (AppID: 2021310)
+addappid(2021310)
+addappid(2021311, 1, "f75303151c041412f4d00b942fa1a62623222e92ea9e90027513e912ecf37e77")
+setManifestid(2021311, "1888746739022538380", 1175860712)
+addappid(2021312, 1, "b7d7bbb005ae6c0a0be8489bf6856af801ad5c1ea7a6e73aa35d2cc81a9e8966")
+setManifestid(2021312, "8620736652629058121", 266871744)
