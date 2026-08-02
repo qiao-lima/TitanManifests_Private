6944fafe5f33e0ff426971ee66b71285f63e9298
qiao-lima <qiao-lima@github.com>
2026-08-02 17:28:43 +0800

    Manual Update 406550

---
+++ 406550.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 406550 - Umineko When They Cry - Question Arcs
+-- Generated 2026-08-02 09:25:19 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(406550)
+
+-- Main Depots
+addappid(406551, 1, "e75ace7261010d5f7159124e7b433f0aa26dfbe0367c396cafe3152a3fc988a6")
+setManifestid(406551, "8049229731852969071", 2913806203)
+addappid(406552, 1, "E541D264977BAE99FCD3819F95CD27ED014F372111807B24AF2257F85DA04DB4") -- (linux, 64-bit)
+setManifestid(406552, "1458265691352860752", 4119774)
