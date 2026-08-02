e8a520463427874253ea5fecde2739716abf46af
qiao-lima <qiao-lima@github.com>
2026-08-02 13:38:36 +0800

    Manual Update 2806120

---
+++ 2806120.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2806120 - Bio Goddess: Doomsday Begins
+-- Generated 2026-08-02 05:31:55 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2806120)
+
+-- Main Depots
+addappid(2806121, 1, "b2fdbde8cee12f9bfa492ac7083e3b922b06eb750d4ed3133a2ec34d67381ccb")
+setManifestid(2806121, "5114206561906260815", 14595852687)
