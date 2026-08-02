2513a76cbe8cecbc1ec55407d0ed0ba73b7d11a1
qiao-lima <qiao-lima@github.com>
2026-08-02 13:38:38 +0800

    Manual Update 3553190

---
+++ 3553190.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3553190 - Anime FPS: Guns vs Magic
+-- Generated 2026-08-02 05:17:41 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3553190)
+
+-- Main Depots
+addappid(3553191, 1, "210e0d88401515defce99e8903f521d8028ef6d74033ed2e01f81c4727b5fd74")
+setManifestid(3553191, "788746206583317071", 4955381738)
