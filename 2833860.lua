6096790481eb0346c76c7882272c9ca76a63091d
qiao-lima <qiao-lima@github.com>
2026-08-02 13:38:36 +0800

    Manual Update 2833860

---
+++ 2833860.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2833860 - KUNKUN League
+-- Generated 2026-08-02 05:25:32 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(2833860)
+
+-- Main Depots
+addappid(2833861, 1, "b4bc59a52bc9c582de1f63d4c7e739d2a85897a4e349e2bc0d0c9e2bab9e4047")
+setManifestid(2833861, "7873573849262718051", 2046843231)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
