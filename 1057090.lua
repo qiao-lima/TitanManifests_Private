492be3100c14fd2e8c368de81d0e011205a1a8e3
qiao-lima <qiao-lima@github.com>
2026-08-02 17:28:42 +0800

    Manual Update 1057090

---
+++ 1057090.lua
@@ -0,0 +1,22 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1057090 - Ori and the Will of the Wisps
+-- Generated 2026-08-02 08:36:20 UTC
+-- # Depots (Total/DLC/Shared): 4/2/1
+
+-- Main AppID
+addappid(1057090, 1, "1441906c390a3caf52202420ce4720bfcf215ed097fc191694eea582c4aea1f4")
+
+-- Main Depots
+addappid(1057091, 1, "f2095fc72f59315e1315914a5a349e23488d3e48897b16bc733e6cad2ca059a1")
+setManifestid(1057091, "1069298212161557632", 12149438740)
+
+-- DLC's (with depot keys)
+-- Ori and the Will of the Wisps Soundtrack (AppID: 1258740)
+addappid(1258740)
+addappid(1258741, 1, "333cc36da18ee28253c5dcedee37858e449c97d3df6e9618f20f8540712fc39d")
+setManifestid(1258741, "3832800547358284023", 543670957)
+addappid(1258742, 1, "e6c0fe164d33686134aa4baa7a8ae8535a7a3fdcf2da90a1d3f1f4a3df825253")
+setManifestid(1258742, "1750814743816009241", 1995590565)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
