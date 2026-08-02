2ab807d02384554e967c1ee9c095d32325e308f6
qiao-lima <qiao-lima@github.com>
2026-08-02 17:28:43 +0800

    Manual Update 2115610

---
+++ 2115610.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2115610 - If only I could go home early
+-- Generated 2026-08-02 09:24:28 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2115610)
+
+-- Main Depots
+addappid(2115611, 1, "b03d80382c43fa77773f7b5219b3326d5801d4a07400192af43a67f8c06c55f0")
+setManifestid(2115611, "904971046058524159", 254344168)
