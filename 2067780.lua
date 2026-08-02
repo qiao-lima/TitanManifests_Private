ce3291f4749165bec365120669a1208c36ce57b5
qiao-lima <qiao-lima@github.com>
2026-08-02 17:28:43 +0800

    Manual Update 2067780

---
+++ 2067780.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2067780 - Your Turn To Die -Death Game By Majority- 
+-- Generated 2026-08-02 09:24:48 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2067780)
+
+-- Main Depots
+addappid(2067782, 1, "b035394eecff1845cf9348b51745e71b0bedbbe7be661c70b4acf85963ca1c2e")
+setManifestid(2067782, "6892872906416968419", 1886445497)
