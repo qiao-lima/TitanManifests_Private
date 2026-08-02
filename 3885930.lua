d57f0ccec6cd1b67793cb5b9a7dda3c95627cab8
qiao-lima <qiao-lima@github.com>
2026-08-02 13:38:39 +0800

    Manual Update 3885930

---
+++ 3885930.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3885930 - SandTable War: Three Kingdoms
+-- Generated 2026-08-02 05:16:18 UTC
+-- # Depots (Total/DLC/Shared): 2/1/0
+
+-- Main AppID
+addappid(3885930)
+
+-- Main Depots
+addappid(3885931, 1, "9b6481c802021cbeb5a31cbe0b9aae244cf26628564bc56fc6ced93dbab503ae")
+setManifestid(3885931, "5456810736228718494", 351714042)
+
+-- Missing Depots (We don't have the keys yet lol): 4821973
