bcfe00c2d79b15decc0c4c359e7205ef6947c94e
qiao-lima <qiao-lima@github.com>
2026-08-03 15:13:59 +0800

    Manual Update 554750

---
+++ 554750.lua
@@ -0,0 +1,16 @@
+-- Generated with Luie @ https://lua.tools/
+-- 554750 - Axis Football 2017
+-- Generated 2026-08-03 07:09:04 UTC
+-- # Depots (Total/DLC/Shared): 4/0/1
+
+-- Main AppID
+addappid(554750)
+
+-- Main Depots
+addappid(554751, 1, "e8fd59ad59f872fe05c1d40f2e1aff1185a4cfeca63a6ad11f2ff6aa3486bafd") -- (windows)
+setManifestid(554751, "6697982672093814283", 1677961135)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 554752, 554753
