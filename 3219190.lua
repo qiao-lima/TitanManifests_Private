ea3d226e6acdcb24c97e668d7234f6fab73682b8
qiao-lima <qiao-lima@github.com>
2026-08-03 15:13:58 +0800

    Manual Update 3219190

---
+++ 3219190.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3219190 - Axis Football 2026
+-- Generated 2026-08-03 07:07:11 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(3219190)
+
+-- Main Depots
+addappid(3219191, 1, "9aa33643bb186ce1aaba7354a8b8e65d875c7ff6669d01ed4b635e8a51f6f505")
+setManifestid(3219191, "2842698258530610330", 9157841769)
+
+-- Missing Depots (We don't have the keys yet lol): 3219192, 3219193
