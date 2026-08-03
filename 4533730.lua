462aebc0a13d2dc32b683c369064b35f842e4415
qiao-lima <qiao-lima@github.com>
2026-08-03 15:13:59 +0800

    Manual Update 4533730

---
+++ 4533730.lua
@@ -0,0 +1,21 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4533730 - Yokai: Lust Secret 🪭
+-- Generated 2026-08-03 03:27:26 UTC
+-- # Depots (Total/DLC/Shared): 4/1/0
+
+-- Main AppID
+addappid(4533730)
+
+-- Main Depots
+addappid(4533731, 1, "d4c284aa36843d609b14073ee909a76f26bbe15f895232b52bc204d5053acad8")
+setManifestid(4533731, "937743914662042191", 5415190798)
+addappid(4533732, 1, "0f153dc90ab768a54f8860b642f228f9532bd54d367df7e3ea28c57f87fa6255") -- (macos)
+setManifestid(4533732, "2053814354102174594", 5260862315)
+addappid(4533733, 1, "652c0301e9c3994e700f4ebaffb69c9ec44c9783caaa6cd050954028a309af4c") -- (linux)
+setManifestid(4533733, "2019401445008847865", 4128831585)
+
+-- DLC's (with depot keys)
+-- Yokai: Lust Secret 🪭 - Wallpapers (AppID: 4993100)
+addappid(4993100)
+addappid(4993100, 1, "94365b285fedb7350d8d37179c3841444ea899074181d282b156846c307a221a")
+setManifestid(4993100, "3706156483814972962", 192698616)
