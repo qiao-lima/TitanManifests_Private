04c5e9b93d6a4dbc82dd0b5b4914d96fd2df26b0
qiao-lima <qiao-lima@github.com>
2026-08-03 15:13:58 +0800

    Manual Update 3585800

---
+++ 3585800.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3585800 - Keep Digging
+-- Generated 2026-08-03 07:11:23 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(3585800, 1, "58fb64e8f3896776423864b6b129ad552db6f25e51078fd76e88f8df3bf18968")
+
+-- Main Depots
+addappid(3585801, 1, "f33a53bbd898df27fa1e75b7476d717e94e363d94a10212e1a2b99f7daa0eca7") -- (windows, 64-bit)
+setManifestid(3585801, "9174011449428569542", 8940888493)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
