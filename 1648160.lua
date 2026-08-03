682c63736ab3118757b5f266df30de68e18a9e48
qiao-lima <qiao-lima@github.com>
2026-08-03 15:13:57 +0800

    Manual Update 1648160

---
+++ 1648160.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1648160 - Kestrel
+-- Generated 2026-08-03 07:06:19 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(1648160)
+
+-- Main Depots
+addappid(1648161, 1, "36f68740d16c09cb883cf428962a451182a3ca1bca98400d3425454dd53f06e9")
+setManifestid(1648161, "4825091616612807060", 128372369)
