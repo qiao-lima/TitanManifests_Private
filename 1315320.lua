90c631ebb8fcefc4b1a13e3ca7b496d808885d56
qiao-lima <qiao-lima@github.com>
2026-08-03 15:13:56 +0800

    Manual Update 1315320

---
+++ 1315320.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1315320 - The Necromancer's Tale
+-- Generated 2026-08-03 07:11:36 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(1315320)
+
+-- Main Depots
+addappid(1315321, 1, "9c80e09df9d0580652b17104727c2ce98713a2b24953185565d314ccb3226ac9") -- (windows, 64-bit)
+setManifestid(1315321, "7382073623250931233", 4286203189)
+addappid(1315322, 1, "A675E42B650003C3A989018D0D18B994D82C901DABAF58519FE64EDFE01232D2") -- (macos, 64-bit)
+setManifestid(1315322, "8210119405837955781", 4321816112)
