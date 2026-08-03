dbef0cc5844afb13df78134850b342fa763287f2
qiao-lima <qiao-lima@github.com>
2026-08-03 15:13:58 +0800

    Manual Update 3664010

---
+++ 3664010.lua
@@ -0,0 +1,24 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3664010 - Drift Legends 2
+-- Generated 2026-08-03 07:10:46 UTC
+-- # Depots (Total/DLC/Shared): 10/8/0
+
+-- Main AppID
+addappid(3664010, 1, "43c9a93a8d1ef2e991bf372d908aef917013277ac81670246394291058adb928")
+
+-- Main Depots
+addappid(3664011, 1, "EF87640DA3A5D0168B16DF271D9546CE064F52DCE5C2FBC9AAA5F4E39073D76D") -- (linux)
+setManifestid(3664011, "7574683505717062779", 1959490893)
+addappid(3664012, 1, "bc2208b6760edd5a07856b17c58810c09c26a7dbb10b61c9cf6c5db523680194") -- (windows, 64-bit)
+setManifestid(3664012, "6206476696651068139", 1875656692)
+
+-- DLC's (no depot keys required)
+addappid(4915400) -- Drift Legends 2 EXCLUSIVE PACK
+
+-- DLC's (with depot keys)
+-- Drift legends 2 BEGINNER PACK (AppID: 3681930)
+addappid(3681930)
+addappid(3681930, 1, "79c5bcaf0ff69532b9797d92ed672609592a1f53e35a1740c35a510f1f5589ad")
+setManifestid(3681930, "4062524625060521871", 8)
+
+-- Missing Depots (We don't have the keys yet lol): 3707410, 3707420, 3707430, 3707440, 3707450, 3707460, 4254810
