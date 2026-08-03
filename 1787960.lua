37b80fe030fe3a234fbc2a273bb0d665ce16e37c
qiao-lima <qiao-lima@github.com>
2026-08-03 15:13:57 +0800

    Manual Update 1787960

---
+++ 1787960.lua
@@ -0,0 +1,18 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1787960 - Milling machine simulator
+-- Generated 2026-08-03 07:06:00 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(1787960)
+
+-- Main Depots
+addappid(1787961, 1, "3b607d998f0743eab417b67359783a77214886d5932bcc9cb0c8ba05b572b369") -- (windows)
+setManifestid(1787961, "2416315538241916432", 5865717089)
+
+-- DLC's (no depot keys required)
+addappid(2601070) -- Lathe Machine
+addappid(3505090) -- CNC Milling Machine
+addappid(4379500) -- CNC Lathe Machine
+
+-- Missing Depots (We don't have the keys yet lol): 1787962
