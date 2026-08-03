3b2bf92735f003f89975971a20d3af7643abb33f
qiao-lima <qiao-lima@github.com>
2026-08-03 15:13:58 +0800

    Manual Update 3902430

---
+++ 3902430.lua
@@ -0,0 +1,19 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3902430 - War of Dots
+-- Generated 2026-08-03 07:08:41 UTC
+-- # Depots (Total/DLC/Shared): 4/2/0
+
+-- Main AppID
+addappid(3902430, 1, "cac20488e3adff4bb334870507485e03d0b1f0b632ee42bbb5071f89d5e35003")
+
+-- Main Depots
+addappid(3902431, 1, "e2a061b424954fa719d819911e116efaf29fbd880314531bcd078f453c8b9887") -- (windows)
+setManifestid(3902431, "3415773471114708203", 516991125)
+
+-- DLC's (with depot keys)
+-- War of Dots Plus (AppID: 4626610)
+addappid(4626610)
+addappid(4626611, 1, "b922c8a1bd50cf2b3fa716e5c2dffccea13247850a33f7a90a4c40e692f3ad40") -- (windows, 64-bit)
+setManifestid(4626611, "4788951770033406282", 13517721)
+
+-- Missing Depots (We don't have the keys yet lol): 3902432, 4916531
