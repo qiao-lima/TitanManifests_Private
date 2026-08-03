881f7eb170f9bf2514719b329202be8e00bb9667
qiao-lima <qiao-lima@github.com>
2026-08-03 15:13:56 +0800

    Manual Update 1412780

---
+++ 1412780.lua
@@ -0,0 +1,16 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1412780 - Axis Football 2020
+-- Generated 2026-08-03 07:08:10 UTC
+-- # Depots (Total/DLC/Shared): 4/0/1
+
+-- Main AppID
+addappid(1412780)
+
+-- Main Depots
+addappid(1412781, 1, "ea0b524830205b5c7cb1842f25e56a56a7fcefea91d045a8376c9d2903467b5e") -- (windows)
+setManifestid(1412781, "6861036611780590561", 2971231845)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 1412782, 1412783
