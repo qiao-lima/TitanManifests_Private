2c86b7203936490bdad9a33fbd072f2b4a37f634
qiao-lima <qiao-lima@github.com>
2026-08-04 14:48:45 +0800

    Manual Update 4025700

---
+++ 4025700.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4025700 - Heartopia
+-- Generated 2026-08-04 06:29:22 UTC
+-- # Depots (Total/DLC/Shared): 5/0/4
+
+-- Main AppID
+addappid(4025700, 1, "74d52b5ca89424bd49a5e4d6e6c6f7f200aa41cd5cdc94b80792b4134c908182")
+
+-- Main Depots
+addappid(4025701, 1, "eedcc8eccb376e46a2677edf66f972ae1467d4f36322e1d2620a14641b29876b") -- (windows)
+setManifestid(4025701, "8818578371196768672", 13790595678)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- (windows)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- (windows)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
