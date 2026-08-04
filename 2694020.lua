093f052d78ff51199555f34d3a565b5ef15b3245
qiao-lima <qiao-lima@github.com>
2026-08-04 14:43:00 +0800

    Manual Update 2694020

---
+++ 2694020.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2694020 - Starbreed
+-- Generated 2026-08-04 06:31:47 UTC
+-- # Depots (Total/DLC/Shared): 3/1/0
+
+-- Main AppID
+addappid(2694020)
+
+-- Main Depots
+addappid(2694021, 1, "2b96fc752a81ae0beabc903d287d5211cc913e5334276a526ad32d30af530837") -- (windows)
+setManifestid(2694021, "5033343549712278310", 3515057346)
+
+-- Missing Depots (We don't have the keys yet lol): 2694022, 4932901
