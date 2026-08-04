a424861608a9d6e58de0a72a245d3c76d2886e9e
qiao-lima <qiao-lima@github.com>
2026-08-04 15:21:39 +0800

    Manual Update 3122920

---
+++ 3122920.lua
@@ -0,0 +1,6 @@
+--B-I-A-O
+--主游戏APPID: 3122920
+addappid(3122920,0,"9b0d5f458dd4b7deac213487c086d29f4820bc52ea6099e479a4ef7dfa3cf1ca")  -- 主游戏
+
+--depotsID
+addappid(3122921,0,"92afeb987eae274fe15c78cada5c7de9f84d2e623ee74631870e69f06e324dfd")
