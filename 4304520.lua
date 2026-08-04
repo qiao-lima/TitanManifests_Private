d5df247090e118b63577f8d4745c5150775e0745
qiao-lima <qiao-lima@github.com>
2026-08-04 14:48:46 +0800

    Manual Update 4304520

---
+++ 4304520.lua
@@ -0,0 +1,12 @@
+-- 4304520's Lua and Manifest Created by Hubcap Manifest
+-- 《比特尔特岛：与她们共度的日常》
+-- Created: August 03, 2026 at 07:24:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4304520) -- 《比特尔特岛：与她们共度的日常》
+-- MAIN APP DEPOTS
+addappid(4304521, 1, "3ba9c24fd1682c09b5fe561678c6cdf77931affece3aefc7278215a84827b075") -- Depot 4304521
+setManifestid(4304521, "7876617660820766615", 2827603227)
