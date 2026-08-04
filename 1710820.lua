3e8bb23a4f30399bc6db48fb40b496de766a5e0c
qiao-lima <qiao-lima@github.com>
2026-08-04 14:42:58 +0800

    Manual Update 1710820

---
+++ 1710820.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1710820 - Cleared Hot
+-- Generated 2026-08-04 05:55:28 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(1710820)
+
+-- Main Depots
+addappid(1710821, 1, "9197cd6bc000eebaa64e5d4729911bf00721c5d822077175022782a5aa0aff40") -- (windows)
+setManifestid(1710821, "293504003477783773", 4847399276)
+
+-- Missing Depots (We don't have the keys yet lol): 1710822
