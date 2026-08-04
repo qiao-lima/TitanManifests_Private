94759368a80b204cb66d16457015308ee30049be
qiao-lima <qiao-lima@github.com>
2026-08-04 14:48:45 +0800

    Manual Update 4108760

---
+++ 4108760.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4108760 - Hypnotic Eyes
+-- Generated 2026-08-04 05:52:21 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4108760)
+
+-- Main Depots
+addappid(4108761, 1, "d2cbc027a1d1d50097f0bf77709a6a16191ee5ff6b9ca8a81ed06af85dc14fc6")
+setManifestid(4108761, "7129906717157723132", 1313316829)
