5536e80f4f5d0ce1bd048eb218c402c0e8816f53
qiao-lima <qiao-lima@github.com>
2026-08-04 14:48:46 +0800

    Manual Update 4423700

---
+++ 4423700.lua
@@ -0,0 +1,27 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4423700 - LAST STAND Apocalypse
+-- Generated 2026-08-04 05:02:46 UTC
+-- # Depots (Total/DLC/Shared): 9/4/0
+
+-- Main AppID
+addappid(4423700)
+
+-- Main Depots
+addappid(4423701, 1, "2c0d4c4e338a5c29657313528c8d3a01a756901c6832712605383424403b3e2d")
+setManifestid(4423701, "1205647410446644962", 586219156)
+addappid(4423702, 1, "b8cd8634d71eac02946359a5f29385c4d0a2c3d80e7cbde841537f20ad0cce92") -- Schinese
+setManifestid(4423702, "4054553625442981290", 654933386)
+addappid(4423703, 1, "e1ef039e0512f5d18a123e15f38a2d4a136c3e2e3141572287c65dc864be2d44") -- Tchinese
+setManifestid(4423703, "5546402129440111181", 654933386)
+addappid(4423704, 1, "5c2d1f810b89ca9e45cdc71b051ba363d8e1bdfd0d7695ea4e6dd5601eae4ce1") -- English
+setManifestid(4423704, "7212955435174485840", 654933386)
+addappid(4423705, 1, "a5848fe6de264409f9f610cc61742a4cbead1824224613fdb10324ac943e7202") -- Koreana
+setManifestid(4423705, "7854443826553433384", 654933386)
+
+-- DLC's (with depot keys)
+-- LAST STAND Apocalypse Demo (AppID: 4745590)
+addappid(4745590)
+addappid(4745594, 1, "de12b9c30403cfa00a5a9c2f5fdd0bfeaccffe0431240313848b533094f7350d") -- English
+setManifestid(4745594, "9095704427369537105", 908069705)
+
+-- Missing Depots (We don't have the keys yet lol): 4745591, 4745592, 4745593
