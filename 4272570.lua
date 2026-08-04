7619aeb301dd164d6465421fe13550b00f038fa6
qiao-lima <qiao-lima@github.com>
2026-08-04 14:48:46 +0800

    Manual Update 4272570

---
+++ 4272570.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4272570 - Futa on Top
+-- Generated 2026-08-04 04:26:27 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4272570)
+
+-- Main Depots
+addappid(4272571, 1, "597a9879b39201c64037d0a11786045846c6db23f739c3eb00d970b4e3ae83ce")
+setManifestid(4272571, "3700732768389638978", 5211470496)
