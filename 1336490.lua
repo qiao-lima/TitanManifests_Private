1a9803ceee4bcc46d8f24c60e083f4e371f19587
qiao-lima <qiao-lima@github.com>
2026-08-05 20:04:45 +0800

    Manual Update 1336490

---
+++ 1336490.lua
@@ -0,0 +1,21 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1336490 - Against the Storm
+-- Generated 2026-08-05 11:37:11 UTC
+-- # Depots (Total/DLC/Shared): 2/1/0
+
+-- Main AppID
+addappid(1336490, 1, "66eeeb696a07fb6d6e9b309366dcfb7fac46f85aeb1f28bb161f58817b3cd9ae")
+
+-- Main Depots
+addappid(1336491, 1, "c62157594f44e2a3087998f70ca9dfe4736a0a98d9d1eca658ecaaf102f17dec") -- (windows)
+setManifestid(1336491, "7639203643718080630", 6974837406)
+
+-- DLC's (no depot keys required)
+addappid(3075500) -- Against the Storm - Keepers of the Stone
+addappid(3725110) -- Against the Storm - Nightwatchers
+
+-- DLC's (with depot keys)
+-- Against the Storm - Supporter Pack (AppID: 2712950)
+addappid(2712950)
+addappid(2712950, 1, "e87c2fd0103b135d75ea1e7ab9961328a7d9bdd04bc07c98c60539ac490017fb")
+setManifestid(2712950, "6876970076660568639", 479961443)
