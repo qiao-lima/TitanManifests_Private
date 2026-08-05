8d33457df045a1db7ea7edd12c70c00a433ab9ea
qiao-lima <qiao-lima@github.com>
2026-08-05 20:04:48 +0800

    Manual Update 3638420

---
+++ 3638420.lua
@@ -0,0 +1,18 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3638420 - Inspiral: Echoes of Gravity
+-- Generated 2026-08-05 11:35:48 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(3638420)
+
+-- Main Depots
+addappid(3638421, 1, "693ECAE61A6BD01B92C65D9EBE59EADAE388746EEA155B7105A5C18DE0B75C50") -- (windows)
+setManifestid(3638421, "322770620028701387", 1028628165)
+addappid(3638422, 1, "0129E20797183DE96C94789C213817CB6CF6CD26952718CC33839C71DD62D4CC") -- (macos)
+setManifestid(3638422, "7234672430877207573", 1177164536)
+
+-- DLC's (no depot keys required)
+addappid(3877450) -- Inspiral: Multiplayer
+addappid(3878010) -- Inspiral: Supporter Pack
+addappid(4362100) -- Inspiral: Evolution
