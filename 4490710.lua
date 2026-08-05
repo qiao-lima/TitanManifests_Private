f33abf2c7a675775c7e93d4199d8adb42864b76c
qiao-lima <qiao-lima@github.com>
2026-08-05 20:04:49 +0800

    Manual Update 4490710

---
+++ 4490710.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4490710 - Hardware Tycoon
+-- Generated 2026-08-05 11:20:04 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4490710)
+
+-- Main Depots
+addappid(4490711, 1, "762cfb0a44f5421bd7416b90f3eb63e1c2d615114a85d87d1523c1dccfe89729")
+setManifestid(4490711, "4914872352725962107", 531741330)
