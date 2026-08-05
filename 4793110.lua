515adc81a88bcc0e6ddeb51e510c804aaeb27234
qiao-lima <qiao-lima@github.com>
2026-08-05 20:04:49 +0800

    Manual Update 4793110

---
+++ 4793110.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4793110 - House of Temptations
+-- Generated 2026-08-05 11:36:10 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(4793110)
+
+-- Main Depots
+addappid(4793111, 1, "9f5d123cd2ca638d816e5cc45d83616d44a5d9e476ab766783154778e8ef2bde")
+setManifestid(4793111, "4970121454984310488", 2750446254)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
