1221902ca6e8344bfcfc67d664d4ee8cb14eef16
qiao-lima <qiao-lima@github.com>
2026-08-05 20:04:46 +0800

    Manual Update 2488540

---
+++ 2488540.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2488540 - Crimson Capes
+-- Generated 2026-08-05 11:16:46 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2488540)
+
+-- Main Depots
+addappid(2488541, 1, "1d403803cc46bcd3f69eb8d5a4ddc69a52e2972bc4340d2fac978d7e153222c5")
+setManifestid(2488541, "4361484298844444064", 975669691)
