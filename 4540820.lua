c410a60b8e23f3d17d6c93725f0e8b8f38e1f1ed
qiao-lima <qiao-lima@github.com>
2026-08-05 20:04:49 +0800

    Manual Update 4540820

---
+++ 4540820.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4540820 - Kin and Conquest
+-- Generated 2026-08-05 11:30:32 UTC
+-- # Depots (Total/DLC/Shared): 2/1/0
+
+-- Main AppID
+addappid(4540820)
+
+-- Main Depots
+addappid(4540821, 1, "eaaad65590522e8c055755f43c02db9041e786eefc36c040fc0f0c0248ca71d2")
+setManifestid(4540821, "2250254785618578193", 597696424)
+
+-- Missing Depots (We don't have the keys yet lol): 4962101
