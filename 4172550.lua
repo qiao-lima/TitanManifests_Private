99efb36c591b360619c06db77ae75324f8256ea7
qiao-lima <qiao-lima@github.com>
2026-08-05 20:04:49 +0800

    Manual Update 4172550

---
+++ 4172550.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4172550 - Stickman Zombie Shooting 3D
+-- Generated 2026-08-05 11:26:42 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(4172550)
+
+-- Main Depots
+addappid(4172551, 1, "9e02a725caf55006984501b0d9d149ea240246a2934beca276faa88df39d22b7") -- (windows)
+setManifestid(4172551, "910866934427899214", 224577710)
+
+-- Missing Depots (We don't have the keys yet lol): 4172552, 4172553
