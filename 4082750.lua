2ddc481ede6f3a3dc871ebf66f38905337d2ed0d
qiao-lima <qiao-lima@github.com>
2026-08-05 20:04:48 +0800

    Manual Update 4082750

---
+++ 4082750.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4082750 - Log Riders
+-- Generated 2026-08-05 11:55:57 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(4082750)
+
+-- Main Depots
+addappid(4082751, 1, "25e409fecd35d4e0d0c4f118068763d697c1e9c37454b404e64cec9ff356f482") -- (windows)
+setManifestid(4082751, "5274990186380014955", 2623239385)
+
+-- Missing Depots (We don't have the keys yet lol): 4082752, 4082753
