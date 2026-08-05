0d730d41291051e8bfed7c86e800b4b4bb2e00f9
qiao-lima <qiao-lima@github.com>
2026-08-06 04:03:38 +0800

    Manual Update 2692990

---
+++ 2692990.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2692990 - DON'T SCREAM TOGETHER
+-- Generated 2026-08-05 19:38:42 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(2692990, 1, "8f558551691a84ee7adfbbcc07a062a4666fec6eef992fdd2d2971b3b3253e9b")
+
+-- Main Depots
+addappid(2692991, 1, "9b971dfb6dc8c67c3cd15d90b0daf3432bca02a2816476c9d877630216b0ec9a") -- (windows,linux, 64-bit)
+setManifestid(2692991, "7852515881052496041", 15043040479)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
