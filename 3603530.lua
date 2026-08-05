f305d4321641ba07df34c4d1e34a47ce6a822df7
qiao-lima <qiao-lima@github.com>
2026-08-05 20:04:48 +0800

    Manual Update 3603530

---
+++ 3603530.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3603530 - Dungeon with Girl
+-- Generated 2026-08-05 11:50:23 UTC
+-- # Depots (Total/DLC/Shared): 4/0/0
+
+-- Main AppID
+addappid(3603530)
+
+-- Main Depots
+addappid(3603531, 1, "225c8309befd5e8daaed0039fd60ae53ee5d069d79b98b05914d519bc2d528a0")
+setManifestid(3603531, "4970166955667213392", 581065705)
+
+-- Missing Depots (We don't have the keys yet lol): 3603532, 3603533, 3603534
