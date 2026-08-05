7245acc97ee07f33425df81256f861de3519586b
qiao-lima <qiao-lima@github.com>
2026-08-05 20:04:47 +0800

    Manual Update 3323490

---
+++ 3323490.lua
@@ -0,0 +1,16 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3323490 - Truxton Extreme
+-- Generated 2026-08-05 11:46:32 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3323490)
+
+-- Main Depots
+addappid(3323491, 1, "5f19ca5931a9fc50e69027b18638dc0751283f0ee74c06249b69241bfcb40ac9") -- (windows)
+setManifestid(3323491, "5998309314940607796", 12248069874)
+
+-- DLC's (no depot keys required)
+addappid(4317760) -- TXE - Batsugun Music&Skin set
+addappid(4317780) -- TXE - Truxton 2 Music&Skin set
+addappid(4317800) -- TXE - Truxton Music&Skin set
