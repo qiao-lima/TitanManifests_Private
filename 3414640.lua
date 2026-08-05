359c97b597d4c19ae9fab8952985ba66e7a87872
qiao-lima <qiao-lima@github.com>
2026-08-05 19:05:34 +0800

    Manual Update 3414640

---
+++ 3414640.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3414640 - No One's Island
+-- Generated 2026-08-04 08:43:04 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(3414640)
+
+-- Main Depots
+addappid(3414642, 1, "03eecfd78ed7192cd5dce13caa629c7fd1a5323da7e80fd78749fa394e14c2e8") -- (windows, 64-bit)
+setManifestid(3414642, "205405050277425180", 773630154)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
