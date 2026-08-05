b36ec26b0f78b99e47c07fcc25768cd96e95453c
qiao-lima <qiao-lima@github.com>
2026-08-06 04:03:37 +0800

    Manual Update 1085040

---
+++ 1085040.lua
@@ -0,0 +1,16 @@
+-- 1085040's Lua and Manifest Created by Hubcap Manifest
+-- Raider Kid and the Ruby Chest
+-- Created: May 15, 2026 at 10:19:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1085040) -- Raider Kid and the Ruby Chest
+-- MAIN APP DEPOTS
+addappid(1085041, 1, "a8d2d6e02230a8fe24ef46400ece2ae1f90ce0a75fe7113148515a39eacc4556") -- Depot 1085041
+setManifestid(1085041, "1781765577751658091", 208087896)
+addappid(1085043, 1, "6033a1dea831d572df8e503121ed62de989bc6f0e9f67b74795b0e7738522476") -- Depot 1085043
+setManifestid(1085043, "5009705649400076174", 203159402)
+addappid(1085045, 1, "e44c3b0bd93ee5f9e2dff592eca466ed41e2c7e3cc22e3336d0efed1fe67cf9b") -- Depot 1085045
+setManifestid(1085045, "5566095335008951647", 223688748)
