a810d01f7e12fc55135aaec76d68f2c96ce35ad0
qiao-lima <qiao-lima@github.com>
2026-08-06 16:34:35 +0800

    Manual Update 4307260

---
+++ 4307260.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4307260 - Memories : Silenced.
+-- Generated 2026-08-06 07:32:18 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4307260)
+
+-- Main Depots
+addappid(4307261, 1, "5cf4e93337460456d72c43cffa32bee6cceddda78dd40fb1f28fe93bdcf399a0")
+setManifestid(4307261, "9174806052636775394", 2058621775)
