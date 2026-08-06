dbd17762fd36fde2ae068eced5fc29260c214215
qiao-lima <qiao-lima@github.com>
2026-08-06 16:34:34 +0800

    Manual Update 3902390

---
+++ 3902390.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3902390 - Tokyo Hotel
+-- Generated 2026-08-06 08:11:46 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(3902390, 1, "b7780948c745ba4260a39e43170d644aab65fdbe6994768a7024674bc378f38e")
+
+-- Main Depots
+addappid(3902391, 1, "7a0788032783668a19f153a698afb64c6e0f6c88ee156a6900b4656cf8a76db2") -- (windows,linux)
+setManifestid(3902391, "3559256153494527951", 750406076)
+
+-- Missing Depots (We don't have the keys yet lol): 3902392
