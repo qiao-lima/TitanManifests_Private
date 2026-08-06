04166a151f351832ef4c42a331e90854dfc30cdf
qiao-lima <qiao-lima@github.com>
2026-08-06 16:34:34 +0800

    Manual Update 3879760

---
+++ 3879760.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3879760 - Gamer Girl Adventure
+-- Generated 2026-08-06 07:54:43 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(3879760)
+
+-- Main Depots
+addappid(3879761, 1, "9359cd0537a87e7e6c8479048fa208498710d8c76f593f7b1dfc028261765be8") -- (windows,linux)
+setManifestid(3879761, "2666625236562376617", 1445148200)
+
+-- Missing Depots (We don't have the keys yet lol): 3879762
