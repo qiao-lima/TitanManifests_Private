0325e9f6fa18c5c524b43c127757e96c1e2f2ae8
qiao-lima <qiao-lima@github.com>
2026-08-06 16:34:34 +0800

    Manual Update 3990990

---
+++ 3990990.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3990990 - 偶像調教事件簿
+-- Generated 2026-08-06 07:29:18 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3990990)
+
+-- Main Depots
+addappid(3990991, 1, "19995c2dd7fa0f45ff6186d23ca1e421acb6a80b1694b9c3c67d6956a9074728")
+setManifestid(3990991, "5571315913783530022", 778433607)
