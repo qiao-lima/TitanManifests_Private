d3392c008e9e411a983844d8fa2578df84baf9a6
qiao-lima <qiao-lima@github.com>
2026-08-06 16:34:34 +0800

    Manual Update 3767740

---
+++ 3767740.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3767740 - Outhold
+-- Generated 2026-08-06 07:49:39 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(3767740, 1, "0a7fc916f3b34078c17cbdff6101fb9f59fbf33a9a984e8b07a469a0aa9e9e36")
+
+-- Main Depots
+addappid(3767741, 1, "97fbb25eacb8358b777d45b36c5a08a6cadb9260f44a15944a73764e84c01278") -- (windows)
+setManifestid(3767741, "1068501148644185741", 155028152)
+
+-- Missing Depots (We don't have the keys yet lol): 3767742
