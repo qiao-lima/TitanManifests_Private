b144def6424f44b631a5f81a595583b1e0d03cfd
qiao-lima <qiao-lima@github.com>
2026-08-08 03:41:12 +0800

    Manual Update 3849430

---
+++ 3849430.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3849430 - お姉さんと一緒
+-- Generated 2026-08-07 19:12:11 UTC
+-- # Depots (Total/DLC/Shared): 3/2/0
+
+-- Main AppID
+addappid(3849430)
+
+-- Main Depots
+addappid(3849431, 1, "edfb94d431fd362df012078e1b25daf267a2108b14cbaf2d116ec39c7a355a4f")
+setManifestid(3849431, "9050387016009077005", 267084226)
+
+-- Missing Depots (We don't have the keys yet lol): 4209621, 4209622
