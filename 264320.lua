2bb969f6f28241a4870a0d8a35534fea4f43d0b7
qiao-lima <qiao-lima@github.com>
2026-08-08 03:41:12 +0800

    Manual Update 264320

---
+++ 264320.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 264320 - Captain Morgane and the Golden Turtle
+-- Generated 2026-08-07 19:13:15 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(264320)
+
+-- Main Depots
+addappid(264322, 1, "feca330ccfa8f76159af7102843b57b9cf346054df8c0d16fb9902b761aa0b3c") -- English (windows)
+setManifestid(264322, "7987987995989467367", 2887530891)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228981, 1, "9afdbd47677424be993f6b516a15ebdb5281aa318114631d14b2b23999ae97ac") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
