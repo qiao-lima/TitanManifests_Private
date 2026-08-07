ebd4317396cd6d9d2bd60001fe6da63efadbf804
qiao-lima <qiao-lima@github.com>
2026-08-08 03:41:13 +0800

    Manual Update 4888720

---
+++ 4888720.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4888720 - Zombie City Together
+-- Generated 2026-08-07 19:17:09 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4888720)
+
+-- Main Depots
+addappid(4888721, 1, "6bdba66c0362b64ab34ed53d3643afa598d90d9a82091bae89e4b757218a853c")
+setManifestid(4888721, "2384106493175387477", 1588691688)
