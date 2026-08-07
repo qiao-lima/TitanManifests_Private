d1adf44635611ebbee52f3667f30c3f07601ad04
qiao-lima <qiao-lima@github.com>
2026-08-07 15:02:05 +0800

    Manual Update 3787240

---
+++ 3787240.lua
@@ -0,0 +1,29 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3787240 - MARVEL Tōkon: Fighting Souls
+-- Generated 2026-08-07 06:57:22 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(3787240)
+
+-- Main Depots
+addappid(3787241, 1, "bf2facc10027681f29ed4be380c37d63e38063a45ae478290aa3eae4da76db6a")
+setManifestid(3787241, "695883899870354976", 27474447073)
+
+-- DLC's (no depot keys required)
+addappid(4839960) -- MARVEL Tōkon: Fighting Souls - Pre-purchase Bonus
+addappid(4848500) -- MARVEL Tōkon: Fighting Souls - Year 1 Characters and Stage Pass
+addappid(4848520) -- MARVEL Tōkon: Fighting Souls - Set of result screen poses
+addappid(4848530) -- MARVEL Tōkon: Fighting Souls - Lobby Avatar and Lobby Pet
+addappid(4848540) -- MARVEL Tōkon: Fighting Souls - Animated Chromatic Color Unlock
+addappid(4848550) -- MARVEL Tōkon: Fighting Souls - Spider-Man “Advanced Suit 2.0” Costume
+addappid(4848570) -- MARVEL Tōkon: Fighting Souls - Storm Punk Rock Costume
+addappid(4848580) -- MARVEL Tōkon: Fighting Souls - Iron Man Classic Costume
+addappid(4848590) -- MARVEL Tōkon: Fighting Souls - Captain America Classic Costume
+addappid(4848600) -- MARVEL Tōkon: Fighting Souls - Doctor Doom Supreme Ruler Costume
+addappid(5030920) -- MARVEL Tōkon: Fighting Souls – Standard – Ultimate Edition Upgrade
+addappid(5030930) -- MARVEL Tōkon: Fighting Souls – Deluxe – Ultimate Edition Upgrade
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
