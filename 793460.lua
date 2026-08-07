0117b359e69d369fdfc6325642db6f2970b03474
qiao-lima <qiao-lima@github.com>
2026-08-08 03:41:13 +0800

    Manual Update 793460

---
+++ 793460.lua
@@ -0,0 +1,28 @@
+-- Generated with Luie @ https://lua.tools/
+-- 793460 - 112 Operator
+-- Generated 2026-08-07 19:11:32 UTC
+-- # Depots (Total/DLC/Shared): 4/2/0
+
+-- Main AppID
+addappid(793460, 1, "502854089d81c8c121c1277de8da3d540638ba2786fdd7f539b01e293cde8609")
+
+-- Main Depots
+addappid(793461, 1, "2923abff356f40a6626759eef98e74c0fab20968d61d1d0a75916c77ea6bc0b6") -- (windows)
+setManifestid(793461, "2537084539328178491", 3747178867)
+addappid(793462, 1, "6f789024b4260378373b82f48b9ebf951de6ee06ca48a46558fdbfd80b5efd04") -- (macos)
+setManifestid(793462, "6135015662444699188", 3775514480)
+
+-- DLC's (no depot keys required)
+addappid(1300850) -- 112 Operator - CHARITY MASKS
+addappid(1300851) -- 112 Operator - Facilities
+addappid(1412760) -- 112 Operator - Water Operations
+addappid(1412761) -- 112 Operator - Pandemic Outbreak
+addappid(1570180) -- 112 Operator - The Last Duty
+
+-- DLC's (with depot keys)
+-- 112 Operator Soundtrack (AppID: 1299630)
+addappid(1299630)
+addappid(1299631, 1, "bac1e63646f84137d1f98ad6b28d9c1a7c7366ffd8b68ae88409a699b4beef33")
+setManifestid(1299631, "7086017445620466175", 26156491)
+addappid(1299632, 1, "a36d590b31f734c3a37f910662797e2a4ee2283ddfe350d0fe1b758e46a10e2a")
+setManifestid(1299632, "3643553947060743093", 203620090)
