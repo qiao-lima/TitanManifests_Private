deb6c95d6116972d8a5d8f3d67ec71f048d54ac3
qiao-lima <qiao-lima@github.com>
2026-08-07 15:02:04 +0800

    Manual Update 3447510

---
+++ 3447510.lua
@@ -0,0 +1,14 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3447510 - Village Tale
+-- Generated 2026-08-07 06:22:22 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3447510)
+
+-- Main Depots
+addappid(3447511, 1, "1b25479bb3f84d9747e1b244ce6246e6b1a50330b267ff867f4c95e80d1e7071") -- (windows)
+setManifestid(3447511, "1847654351664741953", 277609591)
+
+-- DLC's (no depot keys required)
+addappid(3896900) -- Village Tale - Supporter Pack
