2b4072e7f7e50238c89e1709a0b43c450490a5d5
qiao-lima <qiao-lima@github.com>
2026-08-08 03:41:11 +0800

    Manual Update 2204850

---
+++ 2204850.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2204850 - Spooky Men
+-- Generated 2026-08-07 19:29:55 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(2204850)
+
+-- Main Depots
+addappid(2204852, 1, "259694eeca290669decc8f382e22b84f62d1f392bfb23daea2c89cfa4d3dcfdd")
+setManifestid(2204852, "561471619057050274", 799437122)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
