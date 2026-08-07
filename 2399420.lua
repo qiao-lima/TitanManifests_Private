5dc8d9aa5458d835580c9699707736e71332beeb
qiao-lima <qiao-lima@github.com>
2026-08-08 03:41:11 +0800

    Manual Update 2399420

---
+++ 2399420.lua
@@ -0,0 +1,31 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2399420 - Le Mans Ultimate
+-- Generated 2026-08-07 18:56:42 UTC
+-- # Depots (Total/DLC/Shared): 2/0/1
+
+-- Main AppID
+addappid(2399420, 1, "2784b943e43ff0ed7cc14dac688e4fde472c00c51fbce6f66dff8e06d30e44b8")
+
+-- Main Depots
+addappid(2399421, 1, "5ebe75565e2b00384fc9a712d85cb05cff556cce0e0a2566bbb63be290aaa378")
+setManifestid(2399421, "4404747626650753007", 51447996857)
+
+-- DLC's (no depot keys required)
+addappid(2973280) -- BMW M Hybrid V8
+addappid(2973290) -- Le Mans Ultimate - 2024 Pack 1
+addappid(2997280) -- DLC 2997280
+addappid(3151390) -- Le Mans Ultimate - 2024 Pack 2
+addappid(3260810) -- Le Mans Ultimate - 2024 Pack 3
+addappid(3260820) -- Le Mans Ultimate - 2024 Pack 4
+addappid(3307210) -- DLC 3307210
+addappid(3468240) -- DLC 3468240
+addappid(3511300) -- Le Mans Ultimate - 2024 Pack 5
+addappid(3948300) -- Le Mans Ultimate - ELMS Season Pass
+addappid(3954000) -- Le Mans Ultimate - ELMS Pack 1
+addappid(3954010) -- Le Mans Ultimate - ELMS Pack 2
+addappid(3954020) -- Le Mans Ultimate - ELMS Pack 3
+addappid(4694190) -- Le Mans Ultimate - US Track Pack 1
+addappid(4906890) -- Le Mans Ultimate - US Track Pass
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
