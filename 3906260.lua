99cdf2d53e93217e68c1706dbe6f1268e7ff1caa
qiao-lima <qiao-lima@github.com>
2026-08-07 15:02:06 +0800

    Manual Update 3906260

---
+++ 3906260.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3906260 - Red Masked Ronin: Desires and Shadows
+-- Generated 2026-08-07 05:38:44 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3906260)
+
+-- Main Depots
+addappid(3906261, 1, "b7dc304555784772915e632198a1dba31f81470e5f4549ebb763a98b97bc9f22")
+setManifestid(3906261, "2000301549347129141", 18078606296)
