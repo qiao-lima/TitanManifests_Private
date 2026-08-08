550b5f1653dd997662916e3ddf06531f4569a73b
qiao-lima <qiao-lima@github.com>
2026-08-08 15:07:48 +0800

    Manual Update 3856460

---
+++ 3856460.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3856460 - Cursed Words: The Word Game That Isn't
+-- Generated 2026-08-08 07:01:35 UTC
+-- # Depots (Total/DLC/Shared): 2/1/0
+
+-- Main AppID
+addappid(3856460)
+
+-- Main Depots
+addappid(3856461, 1, "23db969267704646917ace252094fce4d1e37e7e039bccae85a33a958038fc4b") -- (windows)
+setManifestid(3856461, "9170837311699582332", 6187735644)
+
+-- Missing Depots (We don't have the keys yet lol): 4557811
