c12de68501813ef28c18b4312f39dfbe73ac67be
qiao-lima <qiao-lima@github.com>
2026-08-08 15:07:49 +0800

    Manual Update 4491980

---
+++ 4491980.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4491980 - 我家恋天使太废柴鸟真～可怕。
+-- Generated 2026-08-08 06:57:54 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4491980)
+
+-- Main Depots
+addappid(4491981, 1, "0eb1296b5f2ad6037ebc415b376e1c3d142b67280b0d8fd7c05c4a8d680f5111")
+setManifestid(4491981, "754703703326421683", 559097291)
