91dc49db1d86a2296b385be5128f21bbc34469cd
qiao-lima <qiao-lima@github.com>
2026-08-08 15:07:47 +0800

    Manual Update 2109460

---
+++ 2109460.lua
@@ -0,0 +1,21 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2109460 - 乡村狂想曲
+-- Generated 2026-08-08 06:47:20 UTC
+-- # Depots (Total/DLC/Shared): 4/1/2
+
+-- Main AppID
+addappid(2109460, 1, "d0865eaf21ec8e0177b947101bd0f45e4f745c99f12c901111b6a5296a4745d6")
+
+-- Main Depots
+addappid(2109461, 1, "f77ae6d25c175811951ff2fb68f85fb653d802e950393094175b1f84598999a1") -- (windows)
+setManifestid(2109461, "2220123672812714340", 1338665298)
+
+-- DLC's (with depot keys)
+-- DLC 2179250 (AppID: 2179250)
+addappid(2179250)
+addappid(2179250, 1, "f5a7fa1b530cd3dc9345d00c4222701be82991ddaff54d552c7fa29915bfd428") -- (windows)
+setManifestid(2179250, "191588528380038495", 83499668)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") -- (windows)
