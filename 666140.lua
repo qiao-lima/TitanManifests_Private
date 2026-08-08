633615b366bad78de83e7015791406c658b06a2d
qiao-lima <qiao-lima@github.com>
2026-08-08 20:38:00 +0800

    Manual Update 666140

---
+++ 666140.lua
@@ -0,0 +1,33 @@
+-- Generated with Luie @ https://lua.tools/
+-- 666140 - My Time at Portia
+-- Generated 2026-08-08 08:58:02 UTC
+-- # Depots (Total/DLC/Shared): 4/1/0
+
+-- Main AppID
+addappid(666140)
+
+-- Main Depots
+addappid(666141, 1, "caba4d1fb75ba07e16c9cf25312648ff7d37f77df1b30238f5e069d54257b9d5") -- (windows, 64-bit)
+setManifestid(666141, "505910649525385526", 10327126958)
+addappid(666142, 1, "061eb1a76e81ff214a863af97636561615e4efa1d4f37bde4a4b155a2d19c9f9") -- (macos)
+setManifestid(666142, "2575947428129196803", 9973829675)
+addappid(666143, 1, "2619a8a0873ecdbe14188c37a5bfb0541460c5c54a5c13872b44e756fbb7ac1c")
+setManifestid(666143, "7491349219066139121", 10293028169)
+
+-- DLC's (no depot keys required)
+addappid(921820) -- DLC 921820
+addappid(995390) -- DLC 995390
+addappid(995400) -- DLC 995400
+addappid(1014410) -- DLC 1014410
+addappid(1025030) -- DLC 1025030
+addappid(1075040) -- My Time At Portia - Player Attire Package
+addappid(1075041) -- My Time At Portia - NPC Attire Package
+addappid(1134460) -- My Time At Portia - Bikini
+addappid(1968910) -- My Time At Portia - Player Costume Package
+addappid(1968911) -- My Time At Portia - NPC Eveningwear Package
+
+-- DLC's (with depot keys)
+-- My Time At Portia - OST (AppID: 974290)
+addappid(974290)
+addappid(974290, 1, "84be8752960a67692f7cd6bbf8a304251476906f7f6cba2c63b8c90f9b12cf76")
+setManifestid(974290, "3720290297552821141", 87039488)
