dc1d687e43f69b200804678542414176b2ddef1b
qiao-lima <qiao-lima@github.com>
2026-08-08 15:07:50 +0800

    Manual Update 965680

---
+++ 965680.lua
@@ -0,0 +1,15 @@
+-- 965680's Lua and Manifest Created by Hubcap Manifest
+-- Boomerang Fu
+-- Created: November 13, 2025 at 14:44:16 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(965680) -- Boomerang Fu
+-- MAIN APP DEPOTS
+addappid(965681, 1, "05685e2eda75d5459cd0ec3a1735e0eed68414c8fb3a56585c6c0938b3102f57") -- Boomerang Fu Content
+setManifestid(965681, "8723037355367748083", 697287050)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1685690) -- Boomerang Fu - Fresh Flavors Pack
+addappid(2851830) -- Boomerang Fu - Just Desserts DLC
