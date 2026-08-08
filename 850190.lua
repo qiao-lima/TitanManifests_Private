8ea60fc171069052fa20ac10119858794ae49314
qiao-lima <qiao-lima@github.com>
2026-08-08 15:07:50 +0800

    Manual Update 850190

---
+++ 850190.lua
@@ -0,0 +1,23 @@
+-- Generated with Luie @ https://lua.tools/
+-- 850190 - Goat Simulator 3
+-- Generated 2026-08-08 07:02:22 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(850190, 1, "243ce3059f2b83879ca2ad1fbcca40a41ea4f75349082d44beee4f36e5dacd57")
+
+-- Main Depots
+addappid(850191, 1, "05fc8de1333cce70dc8b8f7f2e79735da8ba77bc8d8a05a2ab711bdda428c90f")
+setManifestid(850191, "7315100551114631352", 14819779143)
+
+-- DLC's (no depot keys required)
+addappid(2666450) -- Goat Simulator 3 - Digital Downgrade
+addappid(2809180) -- Goat Simulator 3 - Multiverse of Nonsense
+addappid(3204290) -- Goat Simulator 3 - Hocus Pocus Pack
+addappid(3299590) -- Goat Simulator 3 - Super Duper Pack 
+addappid(3708030) -- Goat Simulator 3 – Baadlands: Furry Road
+addappid(4370220) -- Goat Simulator 3 – Buck to the Future Pack
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
