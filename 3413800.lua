3c866ad99565f9f39b54868fa085a0389dcebfad
qiao-lima <qiao-lima@github.com>
2026-08-08 20:37:58 +0800

    Manual Update 3413800

---
+++ 3413800.lua
@@ -0,0 +1,38 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3413800 - Hotel Tales
+-- Generated 2026-08-08 08:48:16 UTC
+-- # Depots (Total/DLC/Shared): 7/6/0
+
+-- Main AppID
+addappid(3413800)
+
+-- Main Depots
+addappid(3413801, 1, "40b64b20f4fe3a698d29fc24d855b03e15d28f15d7826e77207413b7332af0ad")
+setManifestid(3413801, "9018515394304452120", 6890165268)
+
+-- DLC's (no depot keys required)
+addappid(4082180) -- Hotel Tales - Skins Pack
+
+-- DLC's (with depot keys)
+-- Hotel Tales Comics Vol.1 - "The Curse of the Crown" (AppID: 3669340)
+addappid(3669340)
+addappid(3669341, 1, "c217be7c325787cab056de6ebad207adc6d4e4e1c00abb7b4c7080861d7bb273")
+setManifestid(3669341, "3171855033570074717", 389358333)
+-- Hotel Tales - Artbook (AppID: 4082150)
+addappid(4082150)
+addappid(4082151, 1, "b83b532d76606ad857393cd76851cec8fa1ee26fd8afd1c094a30ac9d490bb78")
+setManifestid(4082151, "8717376480136022240", 80981724)
+-- Hotel Tales - Animations Pack (AppID: 4082160)
+addappid(4082160)
+addappid(4082161, 1, "835944d43af1ca8b4f968ba8abf6d6ef2c837c4c70d78504ec93bb1476d52378")
+setManifestid(4082161, "3237705050483718198", 3191179664)
+-- Hotel Tales - Wallpapers Pack (AppID: 4082170)
+addappid(4082170)
+addappid(4082171, 1, "227064216432ce8a031b1dd27c25b6289633049b79188524c69199601d10da14")
+setManifestid(4082171, "3216364654135406257", 132746263)
+-- Hotel Tales Comics Vol.2 - "The Bite That Broke Love" (AppID: 4655580)
+addappid(4655580)
+addappid(4655581, 1, "c47b2adb33fe771946f8a173cfd844b61da19420e147c98ea4d48820cfdf0e02")
+setManifestid(4655581, "9102407573570467095", 66169265)
+
+-- Missing Depots (We don't have the keys yet lol): 4082191
