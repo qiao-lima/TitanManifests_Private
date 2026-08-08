0dbf604786e0030f66c27418148bd5f208cd8667
qiao-lima <qiao-lima@github.com>
2026-08-08 15:07:48 +0800

    Manual Update 3883970

---
+++ 3883970.lua
@@ -0,0 +1,19 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3883970 - Love at Work
+-- Generated 2026-08-08 06:03:59 UTC
+-- # Depots (Total/DLC/Shared): 3/2/0
+
+-- Main AppID
+addappid(3883970)
+
+-- Main Depots
+addappid(3883971, 1, "48da3481435110927ccc6bb427fd01207bd386f1331d8a66dd43db4abfdfd9b9")
+setManifestid(3883971, "2096802030317746943", 3374235825)
+
+-- DLC's (with depot keys)
+-- Love at Work - Digital Artbook (AppID: 4880070)
+addappid(4880070)
+addappid(4880070, 1, "29f201c4a0b23a55938ba5b2bd6e2604c09e3be52ca8f7383db8d846fd624d54")
+setManifestid(4880070, "5100365906457894057", 217396482)
+
+-- Missing Depots (We don't have the keys yet lol): 4879981
