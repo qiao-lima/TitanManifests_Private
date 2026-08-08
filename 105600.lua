c270580353a00b7791b2f0b58316a6b5399108ec
qiao-lima <qiao-lima@github.com>
2026-08-08 15:07:46 +0800

    Manual Update 105600

---
+++ 105600.lua
@@ -0,0 +1,27 @@
+-- Generated with Luie @ https://lua.tools/
+-- 105600 - Terraria
+-- Generated 2026-08-08 06:25:57 UTC
+-- # Depots (Total/DLC/Shared): 6/3/0
+
+-- Main AppID
+addappid(105600, 1, "b2e8550a65d11a0766af8031eff5de2f979f958a5343319070008a728cfe9150")
+
+-- Main Depots
+addappid(105601, 1, "612f916fe8230616a9c57a1da6e8ae2b642833ef5074b36be5fa7c8515d7155d") -- (windows)
+setManifestid(105601, "5751495116255795831", 801432759)
+addappid(105602, 1, "15da89575c5f9ae52e76cacec4f912f9fb3b4f1c152f88b03a14b9acf1c1a173") -- (linux)
+setManifestid(105602, "3908368967129848993", 828396557)
+addappid(105603, 1, "a735ff57e669edad75db38c477a9e732f87fb05c824da7cd842712e07e9e51d9") -- (macos, 64-bit)
+setManifestid(105603, "387313944418126565", 854269622)
+
+-- DLC's (with depot keys)
+-- Terraria: Official Soundtrack (AppID: 409210)
+addappid(409210)
+addappid(409210, 1, "43a0e5ba53c4ddb0351171f12d78d32519486b4da58ccfdda5d77397a1e2f663")
+setManifestid(409210, "1981267687838922181", 354146321)
+-- Terraria: Otherworld Official Soundtrack (AppID: 1323320)
+addappid(1323320)
+addappid(1323321, 1, "7f12593d69251ea1c746f77aef43b5717e1080e3edad0b43c96e17b5c552e920")
+setManifestid(1323321, "6244534947342681572", 192513394)
+addappid(1323322, 1, "ff38e4256915093266d39cd019b3f6b8e1a596fae5b066dc824de64ab42c42d3")
+setManifestid(1323322, "3764439085248285461", 809537740)
