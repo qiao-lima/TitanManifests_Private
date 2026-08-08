9253048dac35e65d3ac2d9bda06e762ed477f72f
qiao-lima <qiao-lima@github.com>
2026-08-08 15:07:49 +0800

    Manual Update 4450620

---
+++ 4450620.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4450620 - GRAIN ROT
+-- Generated 2026-08-08 07:00:08 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(4450620)
+
+-- Main Depots
+addappid(4450621, 1, "76eb98135a9676ffb42f3b3663a3785045bdb22af29f92034e509d5423217b04") -- (windows)
+setManifestid(4450621, "5982775281597055314", 4500489988)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
