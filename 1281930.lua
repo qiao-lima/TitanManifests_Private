bbd021a2a77458c224b1f467d04c39d6fcde93b8
qiao-lima <qiao-lima@github.com>
2026-08-08 15:07:46 +0800

    Manual Update 1281930

---
+++ 1281930.lua
@@ -0,0 +1,23 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1281930 - tModLoader
+-- Generated 2026-08-08 06:45:43 UTC
+-- # Depots (Total/DLC/Shared): 8/0/4
+
+-- Main AppID
+addappid(1281930, 1, "cea459cb1fc2b96a6c9f32861040255ae4a22255050586592cf7129496bc22c5")
+
+-- Main Depots
+addappid(1281931, 1, "2f3d1de6d30d21aef8361e268a82c2b030971620d9ae0611ca452d04d3db8481")
+setManifestid(1281931, "1779141483676093144", 0)
+addappid(1281932, 1, "5385c2c3b4d8b9e7365f5679ddb90c26af9660fb624768d2fba141b46cf58ec8") -- (windows)
+setManifestid(1281932, "1772273791636884302", 206596611)
+addappid(1281933, 1, "ace4929293e347c514440fb664ba011219611629c187e7d79d5656eaadc70e82") -- (linux)
+setManifestid(1281933, "3638398573375887047", 173418893)
+addappid(1281934, 1, "c1d827072fb6ba28cd0eb5de52b3f9ae84506edacbdcb8c0e006c28ee27338fe") -- (macos)
+setManifestid(1281934, "8446099637492854113", 173418893)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") -- (windows)
+addappid(229012, 1, "421e499f39a617c1a3b6baf11fbc65ffebd541d2c39c38d3bf6ffbd87919ce12") -- (windows)
