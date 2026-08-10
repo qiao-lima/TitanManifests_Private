dcdd6e8a0f0580e5fc8d6de4ebd5290b7562d82e
qiao-lima <qiao-lima@github.com>
2026-08-10 19:34:03 +0800

    Manual Update 246620

---
+++ 246620.lua
@@ -0,0 +1,38 @@
+-- Generated with Luie @ https://lua.tools/
+-- 246620 - Plague Inc: Evolved
+-- Generated 2026-08-10 10:30:44 UTC
+-- # Depots (Total/DLC/Shared): 11/2/3
+
+-- Main AppID
+addappid(246620, 1, "540f338bcb2eb1e9d469f9302ea6b60481008cef56be0e01e378a92f72041732")
+
+-- Main Depots
+addappid(246621, 1, "19ad23e511f566d12808b52a4cfabb6457d8acc91815acbaddc6fdfe76dacfbe") -- (windows)
+setManifestid(246621, "3606174870070499347", 1278078130)
+addappid(246622, 1, "dd2e9f4b104c4ce27c22dadf3dd13210e54a51c0fa09f14addd72d8c05121f89") -- (macos)
+setManifestid(246622, "850903150096442847", 1283014219)
+addappid(246623, 1, "516e04bc485cebe525f9d28a727a2fbc29aad932656bcb36238448797d52a337") -- (linux)
+setManifestid(246623, "3693726968909105767", 1293011386)
+addappid(246624, 1, "b7170469891310d1335cb12516164cff304a536657ad2c35fad0950719133123") -- (windows)
+setManifestid(246624, "712597846969897429", 193682344)
+addappid(246625, 1, "3c1666a26d525bf81d9b9c90bbd47b0fd637db2d2056950f5a9c8c72f76109bb") -- (macos)
+setManifestid(246625, "8060770308095908779", 195821014)
+addappid(246626, 1, "8d0fb3394cd06ae93b744373a1024f9a4c4b5c4a1e30ed95fadbb05718d02ff2") -- (linux)
+setManifestid(246626, "1898025581291238622", 231643815)
+
+-- DLC's (no depot keys required)
+addappid(1472780) -- Plague Inc: The Cure
+addappid(4508320) -- Plague Inc: Aliens & Anti-Vaxxers
+
+-- DLC's (with depot keys)
+-- Plague Inc: Evolved - Soundtrack (AppID: 315960)
+addappid(315960)
+addappid(315960, 1, "b33ff1bb7dee008b9d577c0cb20fa81f7df0c3cf54d23032e3b6710f2734d324")
+setManifestid(315960, "4019614499794935390", 86524452)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- (windows)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 317520
