5c3a90d71b5579c2cfb422da1367b0534d78b515
qiao-lima <qiao-lima@github.com>
2026-08-10 19:34:06 +0800

    Manual Update 3344740

---
+++ 3344740.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3344740 - Hunk Empire
+-- Generated 2026-08-10 11:00:08 UTC
+-- # Depots (Total/DLC/Shared): 2/1/0
+
+-- Main AppID
+addappid(3344740)
+
+-- Main Depots
+addappid(3344741, 1, "57ab081a3a5d843106a7272bc7fdd052cc3e7dbf7c8417f9cb8e35ee3ea94ec3")
+setManifestid(3344741, "2711781343326534219", 1768986142)
+
+-- Missing Depots (We don't have the keys yet lol): 4552060
