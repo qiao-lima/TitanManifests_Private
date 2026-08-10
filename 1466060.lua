27a3242679e8905476adc1cfc227dd088af7c1d1
qiao-lima <qiao-lima@github.com>
2026-08-11 04:46:50 +0800

    Manual Update 1466060

---
+++ 1466060.lua
@@ -0,0 +1,28 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1466060 - Tainted Grail: The Fall of Avalon
+-- Generated 2026-08-10 11:25:20 UTC
+-- # Depots (Total/DLC/Shared): 2/1/0
+
+-- Main AppID
+addappid(1466060, 1, "f53acb7f143a62f46c302975a2d5cdb7d58fb41a5a7d9a0a03f722a39493e160")
+
+-- Main Depots
+addappid(1466062, 1, "b164844c9d1dcef4202a397a747eff0fc24b75b5553e21bebbd7e86042d94bca")
+setManifestid(1466062, "7923749585628793804", 42260507263)
+
+-- DLC's (no depot keys required)
+addappid(4132150) -- Tainted Grail: The Fall of Avalon - Sanctuary of Sarras DLC
+addappid(4183140) -- Tainted Grail: The Fall of Avalon - Bonus Content #1
+addappid(4492250) -- Tainted Grail: The Fall of Avalon - Bonus Content #2
+addappid(4514980) -- Tainted Grail: The Fall of Avalon - Merlin's Tomb DLC
+addappid(4639800) -- Tainted Grail: The Fall of Avalon - Challenge Mode DLC
+addappid(4704460) -- Tainted Grail: The Fall of Avalon - Bonus Content #3: Anniversary
+addappid(4831420) -- Tainted Grail: The Fall of Avalon - Bonus Content #4
+
+-- DLC's (with depot keys)
+-- Tainted Grail: The Fall of Avalon - Supporters Pack (AppID: 3687050)
+addappid(3687050)
+addappid(3687050, 1, "8a5843ffcee408b2a16680531955ed57dd20d577be56ebb42d7c99dd653a3b2e")
+setManifestid(3687050, "3877787421677591921", 818012192)
+
+-- Token-locked DLCs (couldn't read depots): 3681950
