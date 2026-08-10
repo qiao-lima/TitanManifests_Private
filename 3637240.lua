c7aad73e3827e492ff2011441892aec28ec0f363
qiao-lima <qiao-lima@github.com>
2026-08-10 19:36:15 +0800

    Manual Update 3637240

---
+++ 3637240.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3637240 - QR Controller
+-- Generated 2026-08-10 11:29:35 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(3637240, 1, "f344f514d7b4bc64aa5ead3846bb242c785e4e6dc1c0c6a51bf2da1b62947131")
+
+-- Main Depots
+addappid(3637241, 1, "21e9be622fcab85e7f8b11ca2bb9afe75a4579516d620399c867424192437d9d") -- (windows)
+setManifestid(3637241, "9187705690795158746", 72370952)
+
+-- Missing Depots (We don't have the keys yet lol): 3637242, 3637244
