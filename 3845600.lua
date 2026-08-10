6ff7695b08cc9d06865c5010337296a46636f200
qiao-lima <qiao-lima@github.com>
2026-08-10 19:36:17 +0800

    Manual Update 3845600

---
+++ 3845600.lua
@@ -0,0 +1,19 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3845600 - Living with a Little Fox Girl
+-- Generated 2026-08-10 08:12:38 UTC
+-- # Depots (Total/DLC/Shared): 5/0/0
+
+-- Main AppID
+addappid(3845600)
+
+-- Main Depots
+addappid(3845601, 1, "2d8364f2c5ac538d21eae15a9d72aa05fb83c2441ae893803d6b9f071569d3fc") -- English (windows)
+setManifestid(3845601, "4832271126686775499", 312905386)
+addappid(3845602, 1, "2b1f7e7f91b8c7f7bbf64e51abed0b04b192a08a5865724dfaf265b9e4fb86e0") -- Japanese (windows)
+setManifestid(3845602, "6515101814037182785", 312889786)
+addappid(3845603, 1, "d86ad6f2608d9e73d3235bafa31f6f32940bab52b0445e4f43835890739f0c84") -- Schinese (windows)
+setManifestid(3845603, "619247420104831004", 324824698)
+addappid(3845604, 1, "a4a7fde02df89e85d3e99cf4bbb9a9ca65d438fb483fc1fbd15554d6272cac1a") -- Tchinese (windows)
+setManifestid(3845604, "6420451039250001022", 324824698)
+
+-- Missing Depots (We don't have the keys yet lol): 3845605
