b9988f8660318a7383c92029822b41fb700f523f
qiao-lima <qiao-lima@github.com>
2026-08-11 04:46:50 +0800

    Manual Update 1326470

---
+++ 1326470.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1326470 - Sons Of The Forest
+-- Generated 2026-08-10 11:49:06 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(1326470, 1, "c553d89fa378bd6b5f4a82e9f61b9b085561e8481150c7ab1f2f5744af1d018c")
+
+-- Main Depots
+addappid(1326471, 1, "dcc8b76a7563c836c1aed54a46abf85e7fec5fccabc0dd8e176d2151173e508e")
+setManifestid(1326471, "8162016777027728069", 15989503975)
