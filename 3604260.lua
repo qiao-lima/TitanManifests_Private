87f2f72e95e496531dd35f976cd617880bc5066b
qiao-lima <qiao-lima@github.com>
2026-08-10 19:36:15 +0800

    Manual Update 3604260

---
+++ 3604260.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3604260 - Liminal City
+-- Generated 2026-08-10 09:58:21 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(3604260)
+
+-- Main Depots
+addappid(3604261, 1, "f48e1f95053965a9d50e5f84fe189a8c7549d5ee46fbacec0b3b8e8697664288")
+setManifestid(3604261, "8443585464629924560", 4596670127)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
