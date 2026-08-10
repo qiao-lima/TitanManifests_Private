1cc2293d425f7b676bce5c860a8db8107e0c34db
qiao-lima <qiao-lima@github.com>
2026-08-10 19:37:55 +0800

    Manual Update 4657570

---
+++ 4657570.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4657570 - SEXOPHOBIA 🔞
+-- Generated 2026-08-10 09:53:52 UTC
+-- # Depots (Total/DLC/Shared): 4/1/0
+
+-- Main AppID
+addappid(4657570)
+
+-- Main Depots
+addappid(4657571, 1, "6e7bdc2c99cf31c2498389ce49ac13504429eacd16eb76f0795e208156cb0858")
+setManifestid(4657571, "1239687251085626628", 1572391600)
+
+-- Missing Depots (We don't have the keys yet lol): 4657572, 4657573, 5032240
