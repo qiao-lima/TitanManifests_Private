c2978d089ddaa5d07178cf987a45218072eec57c
qiao-lima <qiao-lima@github.com>
2026-08-10 19:34:04 +0800

    Manual Update 2743520

---
+++ 2743520.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2743520 - TRS22 Model Railroaders Edition
+-- Generated 2026-08-10 07:35:28 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(2743520)
+
+-- Main Depots
+addappid(2743521, 1, "0115be30fe2313292bbe024be60f0769504c8810d6ec7fbe42d65b8916a540cb") -- (windows, 64-bit)
+setManifestid(2743521, "1213308950977932569", 14963023913)
+
+-- Missing Depots (We don't have the keys yet lol): 2743522
