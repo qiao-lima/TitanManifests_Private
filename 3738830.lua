8b6648b05e580b22cf550d3b8e5648938004447d
qiao-lima <qiao-lima@github.com>
2026-08-10 19:36:16 +0800

    Manual Update 3738830

---
+++ 3738830.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3738830 - FIRE STATE
+-- Generated 2026-08-10 07:44:29 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(3738830)
+
+-- Main Depots
+addappid(3738831, 1, "f7cfa2e0e1a6b6940b155923613b25849fd38a098f80a6876b0f124b76e6be86")
+setManifestid(3738831, "1277391005005147554", 29117676208)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
