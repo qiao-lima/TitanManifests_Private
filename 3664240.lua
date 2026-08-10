03488d9a0da867e1740fc1e96b3a6ee7d0302879
qiao-lima <qiao-lima@github.com>
2026-08-10 19:36:16 +0800

    Manual Update 3664240

---
+++ 3664240.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3664240 - Norn's Dine
+-- Generated 2026-08-10 10:25:28 UTC
+-- # Depots (Total/DLC/Shared): 3/1/0
+
+-- Main AppID
+addappid(3664240)
+
+-- Main Depots
+addappid(3664242, 1, "5a7fb6c8968ed6a5f00ad3c347ce493d9c8770c467bdb4b748aa7acb7b4deb46") -- (macos, 64-bit)
+setManifestid(3664242, "3677364794502427209", 1925779669)
+
+-- Missing Depots (We don't have the keys yet lol): 3664241, 4335531
