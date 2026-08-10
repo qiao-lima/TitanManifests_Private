83c7655bfe83544d394fd616b71b16a42ecc98a9
qiao-lima <qiao-lima@github.com>
2026-08-10 19:34:01 +0800

    Manual Update 1058830

---
+++ 1058830.lua
@@ -0,0 +1,38 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1058830 - Spin Rhythm XD
+-- Generated 2026-08-10 10:29:45 UTC
+-- # Depots (Total/DLC/Shared): 10/6/1
+
+-- Main AppID
+addappid(1058830, 1, "ac62c7f6c5481376fdae93137554363b9711825561b05073deeac8e469a10aee")
+
+-- Main Depots
+addappid(1058831, 1, "3f26db997b2f0251aeb078e6ea68fba45a9b67c18863ff4c467f6f03aa0eeae4") -- (windows)
+setManifestid(1058831, "7270214690023187382", 1253620488)
+addappid(1058832, 1, "de813043e58eacf88f155d5ea29ae67c2db721b05892742680892b29872caddb") -- (macos)
+setManifestid(1058832, "3667892385095448355", 1249797376)
+addappid(1058833, 1, "0cfbdacb00c012f9bd209b7cfd60b9f97f9ccf8de14d8dfeac47198d63e8624f") -- (linux, 64-bit)
+setManifestid(1058833, "2344139020391076024", 1210350456)
+
+-- DLC's (with depot keys)
+-- Spin Rhythm XD - Supporter Pack (AppID: 1664540)
+addappid(1664540)
+addappid(1664540, 1, "1f2524ed33a3c11d30d6f2fff2588dd755f3580da01c0c112cdbb63faa8f6f05")
+setManifestid(1664540, "1475478277321023689", 31639510)
+-- Spin Rhythm XD - Monstercat DLC (AppID: 1668790)
+addappid(1668790)
+addappid(1668790, 1, "1f931f673b51c59f94252105c8c93fabe144d8240f5377da2f268333bb27be5b")
+setManifestid(1668790, "3414187976531821981", 129008457)
+-- Spin Rhythm XD - Chillhop DLC (AppID: 1668791)
+addappid(1668791)
+addappid(1668791, 1, "64a1081e7e6d408a166bf3cf058fd384486eef13abdcbbc477f3dec90267f5a2")
+setManifestid(1668791, "6456328265460959649", 95840674)
+-- Spin Rhythm XD - Indie Pack DLC (AppID: 1677790)
+addappid(1677790)
+addappid(1677790, 1, "5e19cee912dcb39cb56c05d06e8c35684e6f5d5ef3c9417cf165d77dcf7c20db")
+setManifestid(1677790, "7136735991350752330", 101779612)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 1250551, 1250552
