0e5a62d9623a1c90f34908b36f87a4ba362331af
qiao-lima <qiao-lima@github.com>
2026-08-10 19:36:18 +0800

    Manual Update 4145350

---
+++ 4145350.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4145350 - Alice and the Devil's Prison
+-- Generated 2026-08-10 09:50:10 UTC
+-- # Depots (Total/DLC/Shared): 4/0/0
+
+-- Main AppID
+addappid(4145350)
+
+-- Main Depots
+addappid(4145354, 1, "3db7b6901cd51c777930e1f4065c5da12dcdfbfb4fe69c21f4217bf69c72e692") -- English
+setManifestid(4145354, "7621716881475598517", 233160224)
+
+-- Missing Depots (We don't have the keys yet lol): 4145351, 4145352, 4145353
