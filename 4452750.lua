b03397bf0136d4f537042bcd9f36209df0ce6936
qiao-lima <qiao-lima@github.com>
2026-08-10 19:36:19 +0800

    Manual Update 4452750

---
+++ 4452750.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4452750 - カエルvs壁画娘3 クロスロード
+-- Generated 2026-08-09 08:58:49 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4452750)
+
+-- Main Depots
+addappid(4452751, 1, "4e5017cdd2fe5321b77f8d2f6f6c0cc67d34a15260c27afcf2c067f51af236f5")
+setManifestid(4452751, "2670686281354352639", 443976669)
