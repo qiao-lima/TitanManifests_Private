f53ac15ba03eff89318ce26620bb23b5b0bdf59e
qiao-lima <qiao-lima@github.com>
2026-08-10 19:36:18 +0800

    Manual Update 4266000

---
+++ 4266000.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4266000 - PortalVR Motion
+-- Generated 2026-08-10 11:20:06 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4266000)
+
+-- Main Depots
+addappid(4266001, 1, "fff68cb45f2f2a6466cf684de30f060ce9a13bcdc5ebada078e0131751ba313e") -- (windows, 64-bit)
+setManifestid(4266001, "7526997697285037544", 408600377)
