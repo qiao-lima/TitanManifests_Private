0fba3641b018187c70ac24a993e3eefcbd169a8b
qiao-lima <qiao-lima@github.com>
2026-08-11 04:46:50 +0800

    Manual Update 1962700

---
+++ 1962700.lua
@@ -0,0 +1,24 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1962700 - Subnautica 2
+-- Generated 2026-08-10 11:48:22 UTC
+-- # Depots (Total/DLC/Shared): 6/2/3
+
+-- Main AppID
+addappid(1962700, 1, "f1218dc455570758f1829f86bbfab5e00806e8a4063b13c786c37ff641e8053f")
+
+-- Main Depots
+addappid(1962701, 1, "96feb03fc707b975cf8271da4659fdb16c14c4b1a5c5bf8f94bd66e39edd4d96")
+setManifestid(1962701, "2045494873371530057", 15377792227)
+
+-- DLC's (with depot keys)
+-- Subnautica 2 Soundtrack (AppID: 3803320)
+addappid(3803320)
+addappid(3803321, 1, "838cddad95174d4eba833bbd3194cdb121a6c59a0b5fea1ce8b5c796e2d86449")
+setManifestid(3803321, "5719658270261268016", 200484444)
+addappid(3803322, 1, "7eaea8f9d28c829c78f341808f18830cbd44ce705c0a0f73de3d2bda53571058")
+setManifestid(3803322, "8650163981997321126", 704085910)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
