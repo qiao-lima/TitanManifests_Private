0f497a4e341a341bc106ddfc690603b0801b49f1
qiao-lima <qiao-lima@github.com>
2026-08-10 19:37:55 +0800

    Manual Update 4782560

---
+++ 4782560.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4782560 - BDSM Sex - Futanari 2
+-- Generated 2026-08-10 11:15:18 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(4782560)
+
+-- Main Depots
+addappid(4782561, 1, "ec57a7a814a0af300bb56e802eadc92a1324481215ccd8ab2bd099938390798e")
+setManifestid(4782561, "5216809909659541669", 907972764)
+
+-- Missing Depots (We don't have the keys yet lol): 4782562, 4782563
