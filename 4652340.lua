0dcce9c0b66d6e3326b52a8daa87fc96fc57aa45
qiao-lima <qiao-lima@github.com>
2026-08-10 19:36:19 +0800

    Manual Update 4652340

---
+++ 4652340.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4652340 - Cloudrooms
+-- Generated 2026-08-10 09:57:04 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(4652340)
+
+-- Main Depots
+addappid(4652341, 1, "7d3b8b54c6feb44f5336b328d2bb7c79975105abcbe30293bc94f72e79c0650e")
+setManifestid(4652341, "932188118340787625", 3364393311)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
