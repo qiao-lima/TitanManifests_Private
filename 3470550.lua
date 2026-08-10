261c949a5fe6b22f123ed0c56c084d5cbf59070e
qiao-lima <qiao-lima@github.com>
2026-08-11 04:46:51 +0800

    Manual Update 3470550

---
+++ 3470550.lua
@@ -0,0 +1,14 @@
+-- 3470550's Lua and Manifest Created by Hubcap Manifest
+-- We, Junk Artists
+-- Created: August 10, 2026 at 03:01:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3470550) -- We, Junk Artists
+-- MAIN APP DEPOTS
+addappid(3470551, 1, "a7a20e1beb0adf423182885a2b1d8a620a88513e1033831dc8b010d188499ce9") -- Depot 3470551
+setManifestid(3470551, "458121201406271358", 951960542)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4789910) -- We, Junk Artists - Supporter Pack
