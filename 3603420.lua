5b688fdf33c8d42d8e77eee009f14dd2817c7683
qiao-lima <qiao-lima@github.com>
2026-08-10 19:36:15 +0800

    Manual Update 3603420

---
+++ 3603420.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3603420 - Seven Nexus
+-- Generated 2026-08-10 11:00:18 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3603420)
+
+-- Main Depots
+addappid(3603421, 1, "008f6a88636886259709fad8a0a16195255bdde5698c09f6550f979af52b8f25")
+setManifestid(3603421, "4072235210441270519", 1330089277)
