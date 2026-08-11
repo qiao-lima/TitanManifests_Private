87464c04fe1dcc06114b8c8f25bf17d168f3b5bf
qiao-lima <qiao-lima@github.com>
2026-08-12 04:07:11 +0800

    Manual Update 4619420

---
+++ 4619420.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4619420 - Slot Grind
+-- Generated 2026-08-11 19:42:56 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(4619420)
+
+-- Main Depots
+addappid(4619422, 1, "71c7092180cdba66e32c059fe3d27eeb33933d4ebcbb50381a0702eed5719b07") -- (windows)
+setManifestid(4619422, "8817954294971066251", 135455268)
+addappid(4619423, 1, "f7188c34bb961b1aa4eeaff8da2d7cf2f0db1b45ab191c1e548c5a52fd59c193") -- (macos)
+setManifestid(4619423, "2405286743376626135", 217484921)
