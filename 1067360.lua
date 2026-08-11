4c6503b4118ecf64f92b358a1aec6f0371675a8e
qiao-lima <qiao-lima@github.com>
2026-08-12 04:06:53 +0800

    Manual Update 1067360

---
+++ 1067360.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1067360 - Pax Autocratica
+-- Generated 2026-08-11 17:47:32 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(1067360)
+
+-- Main Depots
+addappid(1067361, 1, "23f1500469c517a7c93decc37b90c10f898209b03d691861820093164f4a1db7") -- (windows)
+setManifestid(1067361, "3020511451103679008", 33790444870)
