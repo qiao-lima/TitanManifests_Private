3ea5e5b63fa43bf292528983485f42cd34a9d1f8
qiao-lima <qiao-lima@github.com>
2026-08-12 04:07:07 +0800

    Manual Update 3659810

---
+++ 3659810.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3659810 - Zombie Chef
+-- Generated 2026-08-11 18:24:43 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(3659810)
+
+-- Main Depots
+addappid(3659811, 1, "4dac5004786ec945fc66d335714ef8f6261ea92a453ab7780aa48590790693fb")
+setManifestid(3659811, "3977144651547544676", 3577562839)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
