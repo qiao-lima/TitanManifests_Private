d55719b9d88740f0be8a8ce5d26e09248fe79575
qiao-lima <qiao-lima@github.com>
2026-08-12 04:07:07 +0800

    Manual Update 3527690

---
+++ 3527690.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3527690.lua
+--Gamename Mind-Control Talisman ~Strong and Beautiful Adventurers Meet Their End~
+addappid(3527690, 1, "6f39677d6d2f43a6d87e3361a77b07c8ff4a46fad12d649ddb47af3463cbd884") --Mainappid Mind-Control Talisman ~Strong and Beautiful Adventurers Meet Their End~
+addappid(3527691, 1, "a789235727e2b2407d58292f30c54584272629c4248ae16f9a2ee161c60c94c6") --Main Windows Language Depot Japanese Mind-Control Talisman ~Strong and Beautiful Adventurers Meet Their End~
+setManifestid(3527691, "2677784466534742664", 937512720)
+addappid(3527692, 1, "e3c3b1985f1636f94854de46128d04e4e04c5d2f8a58d1e60aea8c73911f5987") --Main Windows Language Depot English Mind-Control Talisman ~Strong and Beautiful Adventurers Meet Their End~
+setManifestid(3527692, "6128221333145435433", 959706304)
+addappid(3527693, 1, "c9a790ca6e74736d622648788c0fec9741f70385d58c8797f970c7f25cfb93b3") --Main Windows Language Depot Tchinese Mind-Control Talisman ~Strong and Beautiful Adventurers Meet Their End~
+setManifestid(3527693, "3472682713390765504", 959782496)
+addappid(3527694, 1, "194451fb5b5e18b9b59cc75d2c944e701120f502943add502a8ab0858c90594a") --Main Windows Language Depot Schinese Mind-Control Talisman ~Strong and Beautiful Adventurers Meet Their End~
+setManifestid(3527694, "8626361960875584606", 959798256)
