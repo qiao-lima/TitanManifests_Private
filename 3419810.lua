5ec1e98a9571a61fab69a63def310a73433b85ae
qiao-lima <qiao-lima@github.com>
2026-08-12 04:07:07 +0800

    Manual Update 3419810

---
+++ 3419810.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3419810 - はじめるセカイの理想論 -goodbye world index
+-- Generated 2026-08-11 18:23:11 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3419810)
+
+-- Main Depots
+addappid(3419811, 1, "dd126ee7eb0296a1e2f400784adc2be4445f5731242565959cbc1773e32e23c7")
+setManifestid(3419811, "8180290276388167160", 2757247359)
