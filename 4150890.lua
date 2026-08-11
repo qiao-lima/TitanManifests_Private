0507c90dcf72c4b1d79f6851f328363155edd97d
qiao-lima <qiao-lima@github.com>
2026-08-12 04:07:10 +0800

    Manual Update 4150890

---
+++ 4150890.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4150890 - The Fiancée - Paths of Corruption
+-- Generated 2026-08-11 18:11:34 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4150890, 1, "6861f4edb9eb4c7de5bd4fcb4fb11714ff991d814e8e6feffc1a303c3fd93914")
+
+-- Main Depots
+addappid(4150891, 1, "69abe67d0a39eca027e0dab8fba5e09755b59d85a9de0e06349effbdaf56017e")
+setManifestid(4150891, "4564094711271547571", 943911951)
