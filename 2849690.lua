82e9ebf2cb51b789c09694c90e4f7326950adb04
qiao-lima <qiao-lima@github.com>
2026-08-12 04:06:57 +0800

    Manual Update 2849690

---
+++ 2849690.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2849690 - Springs of Ardor
+-- Generated 2026-08-11 19:31:15 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(2849690)
+
+-- Main Depots
+addappid(2849691, 1, "cd9248ee3181020b3a4a542b8db3414bc324605dd03af293a474250931d338b4") -- (windows)
+setManifestid(2849691, "8039813064027697357", 1247215936)
+addappid(2849692, 1, "36ebf2ecdbd2b45fa37a27367b3782dd7aaa937023d18d43cd328d3c26680f82") -- (macos)
+setManifestid(2849692, "4119194160796360167", 1232306457)
