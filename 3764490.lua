d67c26452537ca996bc2fcc2d0a0b70c06a45b5e
qiao-lima <qiao-lima@github.com>
2026-08-12 04:07:08 +0800

    Manual Update 3764490

---
+++ 3764490.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3764490 - Witch Play House
+-- Generated 2026-08-11 19:24:59 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3764490)
+
+-- Main Depots
+addappid(3764491, 1, "2fe347cfc7b9ce82f1c95198041cd7483daf447edb0d913e5ac478406e844a42")
+setManifestid(3764491, "7310215501323016054", 1246672446)
