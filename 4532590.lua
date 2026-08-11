200e1e395b71c8e3103b4d7b9a8076d7cf836ac4
qiao-lima <qiao-lima@github.com>
2026-08-12 04:07:10 +0800

    Manual Update 4532590

---
+++ 4532590.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4532590.lua
+--Gamename BSide: Olivia Lin
+addappid(4532590) --Mainappid BSide: Olivia Lin
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Main Windows Depot BSide: Olivia Lin
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Main Windows Depot BSide: Olivia Lin
+addappid(4532591, 1, "350d24b5e19c0b8bfc415478b722c7d78dc6e358d5f0bacfec8e2ef232e13985") --Main Windows Depot BSide: Olivia Lin
+setManifestid(4532591, "9025800509182076010", 3102351280)
