3b86f41e8b15974848caf06f19ac2d99a3a6f788
qiao-lima <qiao-lima@github.com>
2026-08-12 04:06:55 +0800

    Manual Update 1651600

---
+++ 1651600.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1651600 - Reigns: The Witcher
+-- Generated 2026-08-11 19:41:20 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(1651600)
+
+-- Main Depots
+addappid(1651601, 1, "786cf30c2c57ad345e63974626ad56563c40df6b0861ade02d0c65e05c04bf98") -- (windows)
+setManifestid(1651601, "7025432701939555642", 215742646)
+
+-- Missing Depots (We don't have the keys yet lol): 1651602
