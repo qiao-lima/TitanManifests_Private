d6e8fae624978c2e37b60fc26e292772537f09dc
qiao-lima <qiao-lima@github.com>
2026-08-12 04:07:09 +0800

    Manual Update 3830780

---
+++ 3830780.lua
@@ -0,0 +1,16 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3830780 - 下一段路途 The Next Journey Of Life
+-- Generated 2026-08-11 19:27:19 UTC
+-- # Depots (Total/DLC/Shared): 4/0/3
+
+-- Main AppID
+addappid(3830780)
+
+-- Main Depots
+addappid(3830781, 1, "aac1ae7948aaa6b0c415b03f68b41415f960c7f5122ed0056f08db922c482386") -- (windows)
+setManifestid(3830781, "229986350194055420", 35883298699)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
