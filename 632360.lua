ff6a0f575cb51583c0a87522cd3cd872644dfbf9
qiao-lima <qiao-lima@github.com>
2026-08-12 04:07:11 +0800

    Manual Update 632360

---
+++ 632360.lua
@@ -0,0 +1,24 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 632360.lua
+--Gamename Risk of Rain 2
+addappid(632360, 1, "92e4aee4d6eb4a7cf376684e71a8264784ad227de23e06f5b8f22ac7d9f759ad") --Mainappid Risk of Rain 2
+addtoken(632360, "6020475959078937019")
+addappid(632361, 1, "074d702b52f219d0d3440d09a1ded40e4ab4468c8a77b96dd97b1467a3e1a9f0") --Main Windows Depot Risk of Rain 2
+--Dlcname Risk of Rain 2 Soundtrack
+addappid(1236890) --Dlcname Risk of Rain 2 Soundtrack
+addappid(1236891, 1, "eea0afe4967df8f08eb94bba9f0563a39e97c260f345c73cf1f16ac87e87627a") --Dlc Windows Depot Risk of Rain 2 Soundtrack
+setManifestid(1236891, "5473053658314089432", 266204432)
+addappid(1236892, 1, "12eab3fd4e783ddc2fdd8a8d59379945449056e08ba6be0d9b8b9965e75044f5") --Dlc Windows Depot Risk of Rain 2 Soundtrack
+setManifestid(1236892, "3820583338390907523", 745989120)
+--Dlcname Survivors of the Void Soundtrack
+addappid(1905390) --Dlcname Survivors of the Void Soundtrack
+addappid(1905393, 1, "f8ae1eab05f478f114bc7a9285ce1fbabe29c9a8fa932469d4650079d30185b9") --Dlc Windows Depot Survivors of the Void Soundtrack
+setManifestid(1905393, "4277229262955714280", 83076896)
+addappid(1905394, 1, "99f1bb81bde821aff6c4d39dd440b7c43e9c8e4ff45d35193f99c83f07bba3a4") --Dlc Windows Depot Survivors of the Void Soundtrack
+setManifestid(1905394, "3335283220081415656", 232423280)
+addappid(1607890) --Dlcname Risk of Rain 2 - Expansion 1
+addappid(2306620) --Dlcname Risk of Rain 2: Seekers of the Storm
+addappid(2781620) --Dlcname Risk of Rain 2: Alloyed Collective
+addappid(3055670) --Dlcname Risk of Rain 2: Hallowed Concepts
+--Missing Dlcs
+--Dlcname Risk of Rain 2 SOTS Soundtrack
