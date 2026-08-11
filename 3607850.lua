509064867c1e971f72df6abf3a6c83798e582a1a
qiao-lima <qiao-lima@github.com>
2026-08-11 18:13:55 +0800

    Manual Update 3607850

---
+++ 3607850.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3607850 - Yandere Maid~Love-Crazed Maid~
+-- Generated 2026-08-11 10:04:58 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3607850)
+
+-- Main Depots
+addappid(3607852, 1, "140a97662275f5aa2efdfb56a692ee590015a33287350947ff3dd25d8538bcef") -- (windows)
+setManifestid(3607852, "405478724789969169", 3142845124)
