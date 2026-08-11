7d24a8f3989e57690b257c84114f86f1867bf315
qiao-lima <qiao-lima@github.com>
2026-08-12 04:07:11 +0800

    Manual Update 4550370

---
+++ 4550370.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4550370.lua
+--Gamename Tower Realm
+addappid(4550370) --Mainappid Tower Realm
+addappid(4550371, 1, "cc5d8bf21b776f362537855e98abd39eb94960dff5777b1ee40ca4172006fbbf") --Main Windows Depot Tower Realm
+setManifestid(4550371, "1947895533989129623", 620728016)
+addappid(4613820) --Dlcname Tower Realm ：The Saga Of Tower Chapter 2
+addappid(4613830) --Dlcname Tower Realm ：Rhythmic Cultivation Chapter 1
