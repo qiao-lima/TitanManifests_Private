09ec6ec368d369cd42d15a75076dc4b839d1bcf4
qiao-lima <qiao-lima@github.com>
2026-08-12 04:07:09 +0800

    Manual Update 3873980

---
+++ 3873980.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3873980 - Shadow Lotus
+-- Generated 2026-08-11 19:51:39 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(3873980)
+
+-- Main Depots
+addappid(3873981, 1, "3fcd7bee71b7f194287d434482c2f8a1db3400032740cdbbd9db5fe1390ba3b3")
+setManifestid(3873981, "5947762873022765897", 5441117052)
+
+-- Missing Depots (We don't have the keys yet lol): 3873982
