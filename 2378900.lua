ba117afc7fe7685e7966443ed33cfedc4aad84e3
qiao-lima <qiao-lima@github.com>
2026-08-12 04:06:56 +0800

    Manual Update 2378900

---
+++ 2378900.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2378900 - The Coffin of Andy and Leyley
+-- Generated 2026-08-11 18:06:24 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2378900, 1, "5d5af2080591bab55c149876e568463033216f6ac49bb9d57e3edeec66115024")
+
+-- Main Depots
+addappid(2378901, 1, "7e8df27c22fc52c5f740357a93710dcdb613817ef5c9e61763f20eb28b73d525") -- English
+setManifestid(2378901, "3682254389579820333", 744803416)
