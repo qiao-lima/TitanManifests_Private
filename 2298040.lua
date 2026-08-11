56f79a7e36e25d62138deb7a520a6de5cd2638c6
qiao-lima <qiao-lima@github.com>
2026-08-12 04:06:56 +0800

    Manual Update 2298040

---
+++ 2298040.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2298040.lua
+--Gamename Alien Garden Playtest
+addappid(2298040) --Mainappid Alien Garden Playtest
+addappid(2298042, 1, "2372fd7be1b82394dae137c570317d83e4c5e4767dc58d7cf88a484253cc5755") --Main Windows Depot Alien Garden Playtest
+setManifestid(2298042, "8733779267215766240", 84428736)
