93a3ea9ec39d2631feeb66212548ff774ab35c95
qiao-lima <qiao-lima@github.com>
2026-08-12 04:07:09 +0800

    Manual Update 4055380

---
+++ 4055380.lua
@@ -0,0 +1,16 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4055380 - SpaceSim
+-- Generated 2026-08-11 13:37:46 UTC
+-- # Depots (Total/DLC/Shared): 3/0/1
+
+-- Main AppID
+addappid(4055380, 1, "1413874e8afb00c37463990868f9cfb47ea876020a63d11fd02de3f8d941e939")
+
+-- Main Depots
+addappid(4055381, 1, "c9d66eb676ab6239bc5e1dd5c5f5d57ecd66c7f72930b3958dffde1e9a6abc4a") -- (windows, 64-bit)
+setManifestid(4055381, "6800861914574142305", 143321045)
+addappid(4055382, 1, "ce71523e1994ec2cc7471adab6bcf85fde819fa99c76736288bb72aa3a586e7d") -- (linux, 64-bit)
+setManifestid(4055382, "4552476307892501571", 146410922)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
