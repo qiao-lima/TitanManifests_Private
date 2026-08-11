1ec8ce50bf80157a5913601b4c77df0a22f59d43
qiao-lima <qiao-lima@github.com>
2026-08-12 04:06:56 +0800

    Manual Update 2736370

---
+++ 2736370.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2736370 - Helix: Descent N Ascent
+-- Generated 2026-08-11 20:01:49 UTC
+-- # Depots (Total/DLC/Shared): 3/2/0
+
+-- Main AppID
+addappid(2736370)
+
+-- Main Depots
+addappid(2736371, 1, "01862ad97c64caa841fc493256c297fdcb9a1be33f3b630f20cd2b9ea7314b09")
+setManifestid(2736371, "541697984874277210", 1726694287)
+
+-- Missing Depots (We don't have the keys yet lol): 4796111, 4796112
