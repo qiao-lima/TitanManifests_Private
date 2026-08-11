09fdaed67a9a9857587e54418c0b69ac184150e1
qiao-lima <qiao-lima@github.com>
2026-08-12 04:06:55 +0800

    Manual Update 2218290

---
+++ 2218290.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2218290 - 理想の恋人になりなさいっ！- My Dream Lover -
+-- Generated 2026-08-11 18:58:15 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2218290)
+
+-- Main Depots
+addappid(2218291, 1, "586e1c01ffe549e8040343a984014bc2cb7a6794c2278e886507dc352157884c")
+setManifestid(2218291, "2812131389477110254", 186989655)
