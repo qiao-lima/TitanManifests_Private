77d4d1e1e0cbd02412600b5092d2b2467a995e80
qiao-lima <qiao-lima@github.com>
2026-08-12 04:06:58 +0800

    Manual Update 3257000

---
+++ 3257000.lua
@@ -0,0 +1,21 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3257000 - SCP: Containment Breach 2
+-- Generated 2026-08-11 19:36:19 UTC
+-- # Depots (Total/DLC/Shared): 9/0/8
+
+-- Main AppID
+addappid(3257000, 1, "16574401e547e9ca405615281689bce55a22d6213f5fef4a10b5a8f0eff8dc31")
+
+-- Main Depots
+addappid(3257001, 1, "ca929f21408c5381ee0fffb170b4eba57902066e94ba58fa21ade7d565769b6e")
+setManifestid(3257001, "4811187260076109955", 2039822119)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228981, 1, "9afdbd47677424be993f6b516a15ebdb5281aa318114631d14b2b23999ae97ac") -- (windows)
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") -- (windows)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- (windows)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- (windows)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- (windows)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
