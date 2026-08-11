a176bad76651f53e538a09458ff214901349d45f
qiao-lima <qiao-lima@github.com>
2026-08-12 04:07:09 +0800

    Manual Update 3959590

---
+++ 3959590.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3959590 - Typing Break
+-- Generated 2026-08-11 18:58:09 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3959590)
+
+-- Main Depots
+addappid(3959591, 1, "4452a2da197a33842dbbc072d526307ea310ad4d87d1c24c273a1ddfb6a7057a")
+setManifestid(3959591, "191338343785971964", 583116090)
