3fc0faee969b86b37de64314edb2727ee4826572
qiao-lima <qiao-lima@github.com>
2026-08-11 18:13:54 +0800

    Manual Update 1162070

---
+++ 1162070.lua
@@ -0,0 +1,14 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1162070 - 喵可莉的兔玩偶
+-- Generated 2026-08-11 10:09:10 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(1162070)
+
+-- Main Depots
+addappid(1162071, 1, "e7f32bf7cfa527733b7e1f9e704417e0c73c9e87b5944720c77455a35d417727") -- (windows)
+setManifestid(1162071, "659389518112216239", 461426672)
+
+-- DLC's (no depot keys required)
+addappid(1246570) -- 喵可莉的兔玩偶 - 后日谈
