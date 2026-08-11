7d423f3089e4954377a3ddc652fd93a1d1f832f0
qiao-lima <qiao-lima@github.com>
2026-08-12 04:07:08 +0800

    Manual Update 3660220

---
+++ 3660220.lua
@@ -0,0 +1,20 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3660220 - Syberia - Remastered
+-- Generated 2026-08-11 18:46:05 UTC
+-- # Depots (Total/DLC/Shared): 3/1/1
+
+-- Main AppID
+addappid(3660220, 1, "400f2b05a526fd81c27af718021f7b3905804aad3589149b5dcfcfd6724ca170")
+
+-- Main Depots
+addappid(3660221, 1, "09e84824836ec46d9f4a3442489edf8dd34bb42b305837623aa25d9df72f0bd2")
+setManifestid(3660221, "7742523081199976792", 20527356185)
+
+-- DLC's (with depot keys)
+-- Syberia - Remastered - Artbook & Artworks (AppID: 4197380)
+addappid(4197380)
+addappid(4197380, 1, "9632487e45e126a377b826adad2d14ee69a8574ddd6ff68b875dc53f96e5c62f")
+setManifestid(4197380, "8409540226793518420", 181570797)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
