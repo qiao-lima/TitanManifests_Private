b0b3531301a016acbec05529defb617eafa6a275
qiao-lima <qiao-lima@github.com>
2026-08-11 18:13:56 +0800

    Manual Update 4158040

---
+++ 4158040.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4158040 - Chicken Fries
+-- Generated 2026-08-11 10:11:01 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4158040)
+
+-- Main Depots
+addappid(4158042, 1, "902113f241abfe741991f7ac3f67abc58195597bcfe816edc13de0520bd7dc25") -- (windows, 64-bit)
+setManifestid(4158042, "2140420371778525052", 1951870498)
