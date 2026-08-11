983344f55f47eb0dccbc566d0f48174656694fad
qiao-lima <qiao-lima@github.com>
2026-08-12 04:07:08 +0800

    Manual Update 3764460

---
+++ 3764460.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3764460 - DarkDecember
+-- Generated 2026-08-11 19:22:44 UTC
+-- # Depots (Total/DLC/Shared): 4/0/2
+
+-- Main AppID
+addappid(3764460)
+
+-- Main Depots
+addappid(3764461, 1, "895e55ef9762ed24fd22a0370b6f71950d3d6f3613a7186596a92c3b84eba69d") -- English (windows, 64-bit)
+setManifestid(3764461, "6846283819468552", 12344890073)
+addappid(3764462, 1, "60EAB63C374D0748C3494A3336F277EFD790BB405EA546F2B7E91994E4949108") -- Koreana (windows, 64-bit)
+setManifestid(3764462, "5345552969414803117", 12344890073)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
