2d481b7f68cfb1563df5a8d45c6454f8af58e25e
qiao-lima <qiao-lima@github.com>
2026-08-12 21:05:20 +0800

    Manual Update 2684630

---
+++ 2684630.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2684630.lua
+--Gamename Endacopia
+addappid(2684630) --Mainappid Endacopia
+addappid(2684631, 1, "1f7bf3167ca617389ebcd08272a36520465e1b68f1209370d34e4edfb6e4d78d") --Main Windows Depot Endacopia
+setManifestid(2684631, "6268098507437396757", 572613552)
+--Missing Dlcs
+--Dlcname Endacopia Soundtrack
