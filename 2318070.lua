4c787c8f77045b9b482e39ffe487fc1f66482772
qiao-lima <qiao-lima@github.com>
2026-08-12 21:05:20 +0800

    Manual Update 2318070

---
+++ 2318070.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2318070.lua
+--Gamename Little Big Adventure – Twinsen’s Quest
+addappid(2318070) --Mainappid Little Big Adventure – Twinsen’s Quest
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Main Windows Depot Little Big Adventure – Twinsen’s Quest
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Main Windows Depot Little Big Adventure – Twinsen’s Quest
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Main Windows Depot Little Big Adventure – Twinsen’s Quest
+addappid(2318071, 1, "fe0630ba22144c01246b8de2907391e20b0c6e3e1e7ecf69db3f2d773f41f174") --Main Windows Depot Little Big Adventure – Twinsen’s Quest
+setManifestid(2318071, "6492160351607717125", 2467715056)
+addappid(3270650) --Dlcname Little Big Adventure - Twinsen's Quest - Digital Deluxe Upgrade
