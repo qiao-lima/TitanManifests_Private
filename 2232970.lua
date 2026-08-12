13af309877a36adb681a497a697b94f20c10a4c2
qiao-lima <qiao-lima@github.com>
2026-08-12 21:05:19 +0800

    Manual Update 2232970

---
+++ 2232970.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2232970.lua
+--Gamename Sisters hypnosis sex
+addappid(2232970) --Mainappid Sisters hypnosis sex
+addappid(2232971, 1, "7d740c7619cf8de838af5a8cd6bbe2e4201e2a950eb8731b2e982b850084fc02") --Main Windows Depot Sisters hypnosis sex
+setManifestid(2232971, "8803088775179072180", 1269567216)
+--Missing Dlcs
+--Dlcname 姉妹X催眠 MOTION EDITION
