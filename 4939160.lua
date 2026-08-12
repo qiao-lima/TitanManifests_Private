bebb578eb18ef667d3268ac5b60f3f0523dd6984
qiao-lima <qiao-lima@github.com>
2026-08-12 21:12:43 +0800

    Manual Update 4939160

---
+++ 4939160.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4939160 - SEX Dreams at the Motel
+-- Generated 2026-08-12 12:20:07 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4939160)
+
+-- Main Depots
+addappid(4939161, 1, "4a10b77b2a006dd7d4efa9b3af1e4ce6b41ebfdf7b01512faac077486ec5100b") -- (windows)
+setManifestid(4939161, "6840975360334194711", 6501616881)
