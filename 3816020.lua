e19575f30d61202c8d712a7d96e9aecbc9ade6f1
qiao-lima <qiao-lima@github.com>
2026-08-14 03:47:31 +0800

    Manual Update 3816020

---
+++ 3816020.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3816020.lua
+--Gamename The Sojourning Wise Woman's Final Quest
+addappid(3816020) --Mainappid The Sojourning Wise Woman's Final Quest
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Main Windows Depot The Sojourning Wise Woman's Final Quest
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Main Windows Depot The Sojourning Wise Woman's Final Quest
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Main Windows Depot The Sojourning Wise Woman's Final Quest
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") --Main Windows Depot The Sojourning Wise Woman's Final Quest
+addappid(4604450) --Dlcname The Sojourning Wise Woman's final Quest - Digital album
+--Missing Dlcs
+--Dlcname The sojourning wise woman and her final quest - Soundtrack
