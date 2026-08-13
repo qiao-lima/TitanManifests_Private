8bc358010626fe588fcbb157105f33a145bcc11b
qiao-lima <qiao-lima@github.com>
2026-08-14 03:47:04 +0800

    Manual Update 264380

---
+++ 264380.lua
@@ -0,0 +1,16 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 264380.lua
+--Gamename Narcissu 1st & 2nd
+addappid(264380) --Mainappid Narcissu 1st & 2nd
+addappid(264381, 1, "f647d97996cb560fd85d225b4ce0c7191c9811168a61d237ba5e3ec1beb7d353") --Main Windows Depot Narcissu 1st & 2nd
+setManifestid(264381, "2419091365627035631", 289188720)
+addappid(264382, 1, "19f1fb0d9b1c872fc0bcfe1c95b6d0c7694b8bf3088ca56570679bdc9febc2a9") --Main Linux Depot Narcissu 1st & 2nd
+setManifestid(264382, "388753571294651044", 287308304)
+addappid(264383, 1, "46de6ba0c3c3da07bf85009dd6ab4fba1b7ae3eb867a62242063e5441986ab70") --Main Macos Depot Narcissu 1st & 2nd
+setManifestid(264383, "937999851592139424", 289846560)
+--Dlcname Narcissu Soundtrack
+addappid(295510) --Dlcname Narcissu Soundtrack
+addappid(1252440, 1, "0c17a000f8f1095b56a2ff5ee2e736d4f239bee2bb6c4307b63724b6c99af533") --Dlc Windows Depot Narcissu Soundtrack
+setManifestid(1252440, "1619859766133148360", 165055296)
+addappid(1252441, 1, "2fcf0f7ba559662199755fb49fd17b99d3d73d3d5efb8ab0d54d03ef39a9b72e") --Dlc Windows Depot Narcissu Soundtrack
+setManifestid(1252441, "2786315275343068288", 401560160)
