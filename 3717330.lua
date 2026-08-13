086594e0798a1740b08c76d7e988d594527cc791
qiao-lima <qiao-lima@github.com>
2026-08-14 03:47:20 +0800

    Manual Update 3717330

---
+++ 3717330.lua
@@ -0,0 +1,16 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3717330 - Yakuza Kiwami
+-- Generated 2026-08-13 19:46:14 UTC
+-- # Depots (Total/DLC/Shared): 3/1/1
+
+-- Main AppID
+addappid(3717330, 1, "1a3204b9e9acaff1be13f257805f5aece4c12c8c6c5d62b5ac9a514913370f2c")
+
+-- Main Depots
+addappid(3717331, 1, "0ae165b84122aca403f33db5776efe0b7470e19c5dd5f3049ad9b0a149bebc86")
+setManifestid(3717331, "8313693570548550289", 32414290613)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 4145090
