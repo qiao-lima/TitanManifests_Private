0537197010be3969fc124cdbf350a99e508a1e1a
qiao-lima <qiao-lima@github.com>
2026-08-14 03:47:04 +0800

    Manual Update 2950760

---
+++ 2950760.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2950760 - EXIT16: Byilhan Hotel
+-- Generated 2026-08-13 18:20:08 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(2950760)
+
+-- Main Depots
+addappid(2950761, 1, "05bb36a331486ca4a487d37e56c2793eae6a420e3efa32ea9ee7fb9f865a3ba8")
+setManifestid(2950761, "507260660192054558", 3483624308)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
