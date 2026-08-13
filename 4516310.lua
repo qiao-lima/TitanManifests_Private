ce02dda913b1672104b2feb590e4d2954b8d3041
qiao-lima <qiao-lima@github.com>
2026-08-14 03:47:35 +0800

    Manual Update 4516310

---
+++ 4516310.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4516310 - Clicker Complex: Vira’s Private Stream
+-- Generated 2026-08-13 16:10:18 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(4516310)
+
+-- Main Depots
+addappid(4516311, 1, "0000000000000000000000000000000000000000000000000000000000000000") -- (windows)
+setManifestid(4516311, "5499717506780507695", 390270500)
+
+-- Missing Depots (We don't have the keys yet lol): 4516312
