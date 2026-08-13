ce29f3bc2a61339c683efd0a8573b13a04bea330
qiao-lima <qiao-lima@github.com>
2026-08-14 03:47:05 +0800

    Manual Update 2960430

---
+++ 2960430.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2960430.lua
+--Gamename 电竞教父：传奇将至
+addappid(2960430) --Mainappid 电竞教父：传奇将至
+addappid(2960431, 1, "901496B9F4E03A05B66956A5C08AD8629ED1FE39FBAEE311A7CC924A13A9F87E") --Main Windows Depot 电竞教父：传奇将至
+setManifestid(2960431, "2185640822170573983", 2241856720)
