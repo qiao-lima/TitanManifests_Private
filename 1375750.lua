e8401f79703236eb3b6db04bb385b378218263c6
qiao-lima <qiao-lima@github.com>
2026-08-14 03:47:01 +0800

    Manual Update 1375750

---
+++ 1375750.lua
@@ -0,0 +1,18 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1375750 - Regions of Ruin: Runegate
+-- Generated 2026-08-13 15:10:15 UTC
+-- # Depots (Total/DLC/Shared): 4/2/0
+
+-- Main AppID
+addappid(1375750)
+
+-- Main Depots
+addappid(1375752, 1, "af95fcca9c379f1d71fd424952bb1692e14f1214d52a3c25e85d4a4b60b7b61a") -- (windows)
+setManifestid(1375752, "968121247695557892", 7004892916)
+addappid(1375753, 1, "b5b89890195a3fd0d315013dca681714ef671a4ca9643423179cb7a476cbccea") -- (macos)
+setManifestid(1375753, "4285063225518362613", 7067725078)
+
+-- DLC's (no depot keys required)
+addappid(4446470) --  Regions of Ruin: Runegate - The Merchant
+
+-- Missing Depots (We don't have the keys yet lol): 4338371, 4338372
