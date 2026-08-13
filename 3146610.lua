543b71d459eea443cf80d09c7571e6ee6261600a
qiao-lima <qiao-lima@github.com>
2026-08-14 03:47:16 +0800

    Manual Update 3146610

---
+++ 3146610.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3146610 - Spiritual Bond ～Breaking the curse, Intertwining fates～
+-- Generated 2026-08-13 18:23:37 UTC
+-- # Depots (Total/DLC/Shared): 2/1/0
+
+-- Main AppID
+addappid(3146610)
+
+-- Main Depots
+addappid(3146611, 1, "acf496acf8b86ed867b6c0d197cff77c1f256607bca1bbefa45035103b0a8762")
+setManifestid(3146611, "4813494680702210394", 5747578490)
+
+-- Missing Depots (We don't have the keys yet lol): 4447070
