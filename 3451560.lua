9977e44b4bff142398c1e93b66347dd2440945ba
qiao-lima <qiao-lima@github.com>
2026-08-14 03:47:18 +0800

    Manual Update 3451560

---
+++ 3451560.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3451560.lua
+--Gamename PC Store Simulator
+addappid(3451560, 1, "963b45f03fc91b1de6ea48b0030dc92bd609ea2aec61d9f5b68e7424580ed81f") --Mainappid PC Store Simulator
+addappid(3451561, 1, "8d3bb0f750d70fa49fc72ffbca5841a66eb1452889d431f662c47982223fb1c6") --Main Windows Depot PC Store Simulator
+setManifestid(3451561, "6189774312681722387", 169945264)
