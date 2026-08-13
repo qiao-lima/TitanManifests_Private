ee44f247332d92d8aaee4ed7088fc4832d5406b5
qiao-lima <qiao-lima@github.com>
2026-08-14 03:47:06 +0800

    Manual Update 3088400

---
+++ 3088400.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3088400.lua
+--Gamename Deadly Trick
+addappid(3088400) --Mainappid Deadly Trick
+addtoken(3088400, "11328311114004228908")
