920653b976d124091d48bd78d646f26c406840bd
qiao-lima <qiao-lima@github.com>
2026-08-14 03:47:02 +0800

    Manual Update 2413950

---
+++ 2413950.lua
@@ -0,0 +1,20 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2413950 - Final Sentence
+-- Generated 2026-08-13 17:12:42 UTC
+-- # Depots (Total/DLC/Shared): 5/2/2
+
+-- Main AppID
+addappid(2413950)
+
+-- Main Depots
+addappid(2413951, 1, "3ad7272d45fc2e119edbee37c6ba3717db9e05e3f86ef888f322187368c078e3")
+setManifestid(2413951, "4359476531449613829", 847825203)
+
+-- DLC's (no depot keys required)
+addappid(4556350) -- Final Sentence Supporter Pack
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 4675681, 4675682
