f4ba43f80d15fd6a9c8e397f7b36d75da7644e67
qiao-lima <qiao-lima@github.com>
2026-08-14 03:47:19 +0800

    Manual Update 3524180

---
+++ 3524180.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3524180 - 终曲。
+-- Generated 2026-08-13 17:20:08 UTC
+-- # Depots (Total/DLC/Shared): 3/2/0
+
+-- Main AppID
+addappid(3524180)
+
+-- Main Depots
+addappid(3524181, 1, "9fe35de165042a3c26a324286adeb6e8967be974ad08fe303249090e8f18ba43")
+setManifestid(3524181, "6987937770112442178", 3710572995)
+
+-- Missing Depots (We don't have the keys yet lol): 4324171, 4333990
