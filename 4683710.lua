42285274eb3382bbed9fb608abc463685c8689f1
qiao-lima <qiao-lima@github.com>
2026-08-14 03:47:45 +0800

    Manual Update 4683710

---
+++ 4683710.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4683710.lua
+--Gamename 空匣子 Empty Box
+addappid(4683710) --Mainappid 空匣子 Empty Box
+addappid(4683711, 1, "77a865ea22e745c7c2616ec55db005395344e5fa5bc3dba0da55b4e99951c06a") --Main Windows Depot 空匣子 Empty Box
+setManifestid(4683711, "9053855725765704964", 660882864)
