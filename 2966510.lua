54df3500b032ce5aff98b5c42209a247f03b8a3e
qiao-lima <qiao-lima@github.com>
2026-08-14 03:47:05 +0800

    Manual Update 2966510

---
+++ 2966510.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2966510 - Outlying Islands
+-- Generated 2026-08-13 17:12:56 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2966510)
+
+-- Main Depots
+addappid(2966511, 1, "c8b1b60721e8a4b46877aa63db6ba9f205669ba4a5598631e2cf64d8040a7ae5")
+setManifestid(2966511, "9123414792612577729", 1374200536)
