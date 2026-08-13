cc7e2e38e677c7444ebff49d4ca7e8e07ad08e24
qiao-lima <qiao-lima@github.com>
2026-08-14 03:47:03 +0800

    Manual Update 2591150

---
+++ 2591150.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2591150 - Philna Fantasy
+-- Generated 2026-08-13 17:47:52 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(2591150)
+
+-- Main Depots
+addappid(2591151, 1, "beb31bf18540bebc85eec1603619a95f13cd68f786674e61d1f5d77cdc6052e6") -- (windows)
+setManifestid(2591151, "3572671674041116112", 4377376151)
+
+-- Missing Depots (We don't have the keys yet lol): 2591152
