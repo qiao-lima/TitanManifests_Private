918d13213aeab4490b6c16f15b0c1342d8f89b00
qiao-lima <qiao-lima@github.com>
2026-08-14 03:47:47 +0800

    Manual Update 4927490

---
+++ 4927490.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4927490.lua
+--Gamename Cleaning Up The Puzzle Gallery
+addappid(4927490, 1, "7941b83e0494bdcf2eb9db4784fe3f308ce9ccda0771c980cbd8710eba969437") --Mainappid Cleaning Up The Puzzle Gallery
+addappid(4927491, 1, "0a9c18b5a6327e0308e30ea86b76789f462d4fa7c14774be318f2ab4c0165f51") --Main Windows Depot Cleaning Up The Puzzle Gallery
+setManifestid(4927491, "606741045659371887", 559819680)
