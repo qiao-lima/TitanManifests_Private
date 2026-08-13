3a9fa788ea96ead03ff7a08615f89735514da4ca
qiao-lima <qiao-lima@github.com>
2026-08-14 03:47:17 +0800

    Manual Update 3221170

---
+++ 3221170.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3221170 - 唯
+-- Generated 2026-08-13 16:16:53 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3221170)
+
+-- Main Depots
+addappid(3221171, 1, "2f6b8427b8c4da778d7176d6877505fcb1c62efe9081c78907ce0e715f370605") -- (windows)
+setManifestid(3221171, "5689500747712963632", 1777010288)
