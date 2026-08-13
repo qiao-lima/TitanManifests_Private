331d3d90fee6ffe74f48c60238624daa1194e7bd
qiao-lima <qiao-lima@github.com>
2026-08-14 03:47:04 +0800

    Manual Update 2818010

---
+++ 2818010.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2818010.lua
+--Gamename 沉舟
+addappid(2818010) --Mainappid 沉舟
+addappid(2818011, 1, "5ec8385c554373f20ad5ae27367b85f8acbdabb364810af90fbf6f721d30ee86") --Main Windows Depot 沉舟
+setManifestid(2818011, "4109364920500316256", 713638464)
