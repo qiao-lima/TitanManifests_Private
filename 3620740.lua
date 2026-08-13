082c79133b10ea815a898758bbb8de7f15fa116e
qiao-lima <qiao-lima@github.com>
2026-08-14 03:47:20 +0800

    Manual Update 3620740

---
+++ 3620740.lua
@@ -0,0 +1,19 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3620740 - 未完信䇳：纸鸢
+-- Generated 2026-08-13 17:47:26 UTC
+-- # Depots (Total/DLC/Shared): 5/4/0
+
+-- Main AppID
+addappid(3620740)
+
+-- Main Depots
+addappid(3620741, 1, "b7654562959944aeb39faeafe91cbfdc26bfc969659d5362400c31d9311cf149")
+setManifestid(3620741, "8226228936378200259", 3962837895)
+
+-- DLC's (with depot keys)
+-- 纸鸢数字海报与动态壁纸 (AppID: 3654080)
+addappid(3654080)
+addappid(3654080, 1, "b75075059b452f2221633e3c5607d79a72779f82dbab4740fc2af6f067cd0f9d")
+setManifestid(3654080, "1701904860488260544", 922864604)
+
+-- Missing Depots (We don't have the keys yet lol): 3654070, 4065111, 4065112
