75c4c4681213cd8b9b42f57a09b9bb59027a29ca
qiao-lima <qiao-lima@github.com>
2026-08-15 05:35:37 +0800

    Manual Update 4673900

---
+++ 4673900.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4673900 - 完蛋！我被男同学包围了
+-- Generated 2026-08-14 21:15:36 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(4673900)
+
+-- Main Depots
+addappid(4673901, 1, "eb9ba76769fcc3accdc927fcdec32524f37615a71813b179dcb5e810f3914e04") -- (windows)
+setManifestid(4673901, "5131200304595808294", 900311997)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
