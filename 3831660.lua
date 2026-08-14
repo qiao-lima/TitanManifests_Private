03ecfdde6af35394a60b492e04972b97d32e2fb7
qiao-lima <qiao-lima@github.com>
2026-08-15 05:35:06 +0800

    Manual Update 3831660

---
+++ 3831660.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3831660 - Obsessed : Night Shift
+-- Generated 2026-08-14 20:33:08 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3831660)
+
+-- Main Depots
+addappid(3831661, 1, "0f8cfa6fe6e6a0b869fcbcfb10734b15a73d4775be453c922a307ce3f9d42623") -- (windows, 64-bit)
+setManifestid(3831661, "7163076774412427178", 303939561)
