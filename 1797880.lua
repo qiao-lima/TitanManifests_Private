a198fc74144cabd7a97b8d04f46a7a2f2f3badb7
qiao-lima <qiao-lima@github.com>
2026-08-15 05:34:32 +0800

    Manual Update 1797880

---
+++ 1797880.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1797880.lua
+--Gamename Lost Light
+addappid(1797880, 1, "40c1c01b203e0fd809d2246fcc55bfcdf37757ef39a50be63e6b74456a8455cb") --Mainappid Lost Light
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Main Windows Depot Lost Light
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Main Windows Depot Lost Light
+addappid(1797881, 1, "f3d278fb8fcc98ab8d1d29ec4a8b5693e0e5dab346f4130f2b75f0b66a6a5cb9") --Main Windows Depot Lost Light
+setManifestid(1797881, "4547314078823372187", 55483210416)
