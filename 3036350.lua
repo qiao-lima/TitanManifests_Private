6027b3e0440a44330d252ca600b1306527549939
qiao-lima <qiao-lima@github.com>
2026-08-15 05:34:33 +0800

    Manual Update 3036350

---
+++ 3036350.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3036350 - A Case of Fraud
+-- Generated 2026-08-14 21:18:53 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(3036350)
+
+-- Main Depots
+addappid(3036351, 1, "14f0d3e7bd767b82a75d2afc7a55a40a58c041f944785f300cda6694a723fbef") -- (windows, 64-bit)
+setManifestid(3036351, "2667566495972891954", 1448392268)
+
+-- Missing Depots (We don't have the keys yet lol): 3036352
