c358220cc61fe8cdc9dc5adc1c3d2fb39be71133
qiao-lima <qiao-lima@github.com>
2026-08-15 05:34:35 +0800

    Manual Update 3557620

---
+++ 3557620.lua
@@ -0,0 +1,14 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3557620 - Blue Archive
+-- Generated 2026-08-14 20:36:20 UTC
+-- # Depots (Total/DLC/Shared): 2/0/1
+
+-- Main AppID
+addappid(3557620, 1, "0325c980bc73744580479fb1c2f91af69ee3f2bb73691fc3d199fee4f9ae87b5")
+
+-- Main Depots
+addappid(3557621, 1, "0b4e98809f1c718b10332fe0fe98bf591b2fd8fe88d47e04737caacde6c28c65") -- (windows, 64-bit)
+setManifestid(3557621, "189058156303746206", 26343139331)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
