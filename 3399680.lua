13dcf2bbae41ec37e06c4c802fab8da49e41a6cc
qiao-lima <qiao-lima@github.com>
2026-08-15 05:34:34 +0800

    Manual Update 3399680

---
+++ 3399680.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3399680.lua
+--Gamename Cooking Clash
+addappid(3399680) --Mainappid Cooking Clash
+addappid(3399681, 1, "1347554e57ffaff846110770857223f5a1358f24e9e80203dae7576fe504961c") --Main Windows Depot Cooking Clash
+setManifestid(3399681, "1850058944233688990", 627150400)
