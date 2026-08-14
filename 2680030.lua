95f1cb9caa091c14a3f957902894c0574aaffef4
qiao-lima <qiao-lima@github.com>
2026-08-15 05:34:32 +0800

    Manual Update 2680030

---
+++ 2680030.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2680030 - Desperate 1: After Sunset
+-- Generated 2026-08-14 21:13:19 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2680030)
+
+-- Main Depots
+addappid(2680031, 1, "a288bb69e8e57c6577000b03a4dfc18a77903755f0c0d570f93893d5ab07395f")
+setManifestid(2680031, "9171813845217820883", 9401267798)
