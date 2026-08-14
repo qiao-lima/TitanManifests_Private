80d5fe8a71c5b84040620193eed65e1fc9a24c0a
qiao-lima <qiao-lima@github.com>
2026-08-15 05:35:10 +0800

    Manual Update 4212210

---
+++ 4212210.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4212210.lua
+--Gamename Demon Castle Story
+addappid(4212210) --Mainappid Demon Castle Story
+addappid(4212211, 1, "b547e05c946ae420857fd0341b45110d7aa140a7a3e324ddb476cff4d306a202") --Main Windows Depot Demon Castle Story
+setManifestid(4212211, "9027225883762069692", 54558320)
