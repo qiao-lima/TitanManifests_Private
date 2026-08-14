6a07e89e4edb936bbfcc4465a4eee5202f2f49b0
qiao-lima <qiao-lima@github.com>
2026-08-15 05:35:07 +0800

    Manual Update 3846120

---
+++ 3846120.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3846120.lua
+--Gamename MineMogul
+addappid(3846120) --Mainappid MineMogul
+addappid(3846121, 1, "762f14d01797d4a5166b7f103193c6aadf093edc99b8a83004e2c18c1de4a281") --Main Windows Depot MineMogul
+setManifestid(3846121, "73274613683124036", 778775056)
