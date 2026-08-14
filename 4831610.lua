d655cbd0d9b72c88e6a3f9794c185098e5a40328
qiao-lima <qiao-lima@github.com>
2026-08-15 05:35:38 +0800

    Manual Update 4831610

---
+++ 4831610.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4831610 - Prison Of Sissy
+-- Generated 2026-08-14 18:24:55 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4831610, 1, "aeb9078b493e32d3791bca47433d89a27553c220e970c089e8cc665163bfb5cb")
+
+-- Main Depots
+addappid(4831611, 1, "e5d914b05edea80c2095fc5db6f169837a5428c048eaf0f864476f8bf8dc0514")
+setManifestid(4831611, "7010335211490075838", 2941776631)
