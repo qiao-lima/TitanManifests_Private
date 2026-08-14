b56c769ed7378e537014cbaa0e0225c3408481bf
qiao-lima <qiao-lima@github.com>
2026-08-15 05:34:35 +0800

    Manual Update 3418500

---
+++ 3418500.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3418500 - CS Manager
+-- Generated 2026-08-14 20:52:51 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3418500, 1, "069f14875b926ebf3d9ee09a74bcad534762c6789ab54705af609c991490a5b9")
+
+-- Main Depots
+addappid(3418501, 1, "df2fe40942f40cba6dc3b4903b569a308c15be35318f6bfb482fc19f86ddfca3")
+setManifestid(3418501, "5899164972984688346", 541878512)
