233f8ad3ee8c18963f45d072795312ec31d69508
qiao-lima <qiao-lima@github.com>
2026-08-15 05:35:08 +0800

    Manual Update 3965810

---
+++ 3965810.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3965810 - 推币机模拟器
+-- Generated 2026-08-14 20:20:08 UTC
+-- # Depots (Total/DLC/Shared): 3/2/0
+
+-- Main AppID
+addappid(3965810)
+
+-- Main Depots
+addappid(3965811, 1, "385d546aa2f7fc2b9d33f3a8971b8ede79a9bcc6970bd1cf25eca9c933dd08ed")
+setManifestid(3965811, "2767453975601014946", 596430930)
+
+-- Missing Depots (We don't have the keys yet lol): 3996510, 4226660
