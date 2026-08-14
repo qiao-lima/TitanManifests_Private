c8f10d4b0f296baf707568715a1f1d52f1ff2209
qiao-lima <qiao-lima@github.com>
2026-08-15 05:34:32 +0800

    Manual Update 2241940

---
+++ 2241940.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2241940 - MMDEngine
+-- Generated 2026-08-14 06:01:12 UTC
+-- # Depots (Total/DLC/Shared): 2/0/1
+
+-- Main AppID
+addappid(2241940, 1, "54ff8659443e8d5036503ce2db5119b5389d4cab57b2a34e95e09cf5c46346b9")
+
+-- Main Depots
+addappid(2241941, 1, "7c5602b2d11a2a1e97a39c2bf419955084e163fdca14ffc72140b0ad62008821")
+setManifestid(2241941, "4159024377092942790", 237525497)
+
+-- DLC's (no depot keys required)
+addappid(4478370) -- MMDEngine - Supporter Pack
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
