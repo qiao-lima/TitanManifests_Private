0f50a2cfbb52064e0169365f4108056ca32a9b25
qiao-lima <qiao-lima@github.com>
2026-08-16 00:31:55 +0800

    Manual Update 4093480

---
+++ 4093480.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4093480.lua
+--Gamename Kaiju Princess 2: Poochi Q ASMR ～A Magic Ticket That Grants Any Desire～
+addappid(4093480) --Mainappid Kaiju Princess 2: Poochi Q ASMR ～A Magic Ticket That Grants Any Desire～
+addappid(4093481, 1, "a56adbc19f153214a7984760814f62dfd0222739845c3bf6d429a007c5cd5fa5") --Main Windows Depot Kaiju Princess 2: Poochi Q ASMR ～A Magic Ticket That Grants Any Desire～
+setManifestid(4093481, "4522466918139710630", 2184040784)
+addappid(4093482, 1, "af0b18fcd75fe36f616d97e912c9d572d471de808df278a89f22347faa8c945d") --Main Windows Depot Kaiju Princess 2: Poochi Q ASMR ～A Magic Ticket That Grants Any Desire～
+setManifestid(4093482, "810786541302386208", 2184040784)
