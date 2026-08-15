cf210c3e52f1e949586c8cc3f4f40aea5a404c1a
qiao-lima <qiao-lima@github.com>
2026-08-16 00:31:54 +0800

    Manual Update 3135200

---
+++ 3135200.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3135200 - Legacy of Defense
+-- Generated 2026-08-15 15:18:20 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3135200)
+
+-- Main Depots
+addappid(3135201, 1, "5bdcd70ca53e92ad415e84d190f09456613f4428fb457985c6db88d6afbfe4f7")
+setManifestid(3135201, "1527451965423588290", 131629189)
+
+-- DLC's (no depot keys required)
+addappid(4032960) -- Legacy of Defense - Supporter Pack
+addappid(5081250) -- Legacy of Defence - Hero Pack
