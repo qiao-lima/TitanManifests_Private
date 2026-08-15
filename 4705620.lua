6a40381faac23d654c01f13d12e067c0d6dbacf5
qiao-lima <qiao-lima@github.com>
2026-08-16 02:26:40 +0800

    Manual Update 4705620

---
+++ 4705620.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4705620 - WHERE the F$CK is my BITCOIN ⁉️
+-- Generated 2026-08-14 01:59:38 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4705620)
+
+-- Main Depots
+addappid(4705621, 1, "4b6d18c5e167f28ed9a2f82c28ceff3aa5d5dcdcdb9acc315e9cb673b8e6b901")
+setManifestid(4705621, "3210731054279073697", 383407050)
