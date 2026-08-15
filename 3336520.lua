184843539c7271d7d7eb04d1d868feee0e388c68
qiao-lima <qiao-lima@github.com>
2026-08-16 00:31:54 +0800

    Manual Update 3336520

---
+++ 3336520.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3336520 - Dinotica
+-- Generated 2026-08-15 10:50:04 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3336520, 1, "5c74fd782b69b72a6c0fdd2aa246b0a261ce68d5a21c0106f9d54507b412c1c9")
+
+-- Main Depots
+addappid(3336521, 1, "68e205ddef53d8cbb83d901755b78843f881986204299c2f023e29b11d20a6cc")
+setManifestid(3336521, "6005241044190103833", 1334087168)
