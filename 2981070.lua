b478ba415d682bc8e1612af58423166e91ec5242
qiao-lima <qiao-lima@github.com>
2026-08-16 02:26:38 +0800

    Manual Update 2981070

---
+++ 2981070.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2981070.lua
+--Gamename Arcane Trigger
+addappid(2981070) --Mainappid Arcane Trigger
+addappid(2981071, 1, "18eea82a85dfbcb1fbe1497983d36bbe6f253ba389c141e3ae345d5eace12d0b") --Main Windows Depot Arcane Trigger
+setManifestid(2981071, "2250496564846259776", 138577568)
+addappid(2981072, 1, "07438fdf52dd50c3b1d420e1dcc202589e12a66c3f0a651a3c62e21d5db55545") --Main Macos Depot Arcane Trigger
+setManifestid(2981072, "6576066341529199019", 136997008)
+addappid(4853050) --Dlcname 奥术扳机-维多利亚外观包
