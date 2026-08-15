380490e0f46521a8aa110735076fbe5e6c891d80
qiao-lima <qiao-lima@github.com>
2026-08-16 02:26:39 +0800

    Manual Update 4035270

---
+++ 4035270.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4035270 - Space Rock Breaker
+-- Generated 2026-08-14 10:33:42 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4035270, 1, "cfae0dce8eb0b2be4c5aff25245064086328e31e1e515ac18f598f84034ee8f5")
+
+-- Main Depots
+addappid(4035271, 1, "084d71897edcf8f46539799b40dc381abe72ade818aa843b695937dd8ab2c6f0")
+setManifestid(4035271, "3233647901997886068", 340203389)
