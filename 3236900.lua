7e4f59de310275c24d924690b376a5a3af4d7697
qiao-lima <qiao-lima@github.com>
2026-08-16 00:31:54 +0800

    Manual Update 3236900

---
+++ 3236900.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3236900 - Rebirth Pub
+-- Generated 2026-08-15 15:35:27 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3236900)
+
+-- Main Depots
+addappid(3236901, 1, "72a760c7a60c21e98890498394a318403547b3ff2c25b5f7cb5382def3ac8199")
+setManifestid(3236901, "723790525164026491", 11453139053)
