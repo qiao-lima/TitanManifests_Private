debd337b7386e9165b05c4cc4c8b9249fcea0979
qiao-lima <qiao-lima@github.com>
2026-08-16 02:26:37 +0800

    Manual Update 1239980

---
+++ 1239980.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1239980.lua
+--Gamename Luciform
+addappid(1239980) --Mainappid Luciform
+addappid(1239981, 1, "e1ec55a03becb98e3b15a3d1c6802f3bd1bda2e05d3d405eaa986f071c49b63e") --Main Windows Depot Luciform
+setManifestid(1239981, "186700540190897852", 188679984)
+addappid(1239982, 1, "0ee94cb1509c4b2a24a71d299b54820230b344d9ba15aa08fde31894a243520b") --Main Windows Depot Luciform
+setManifestid(1239982, "5768795783396897061", 186253584)
