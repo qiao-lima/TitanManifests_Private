29a80cbd99428dbebad95f7946676122042be4e5
qiao-lima <qiao-lima@github.com>
2026-08-16 00:31:55 +0800

    Manual Update 4011180

---
+++ 4011180.lua
@@ -0,0 +1,19 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4011180 - NTR Idol: Déjà Vu
+-- Generated 2026-08-15 16:00:45 UTC
+-- # Depots (Total/DLC/Shared): 4/3/0
+
+-- Main AppID
+addappid(4011180)
+
+-- Main Depots
+addappid(4011181, 1, "984dbfea7fafeba01c1c2a44fd4b89f20d38c54bf67395ecc67bcb6bced1f7c4")
+setManifestid(4011181, "6069859317959917275", 3767486339)
+
+-- DLC's (with depot keys)
+-- NTR Idol: Déjà Vu Artbook (AppID: 4340680)
+addappid(4340680)
+addappid(4340681, 1, "3a30173dd61441e7b251f08fc0cecce9e086dfc78205c50ea22376003af38bcc")
+setManifestid(4340681, "6007369577231587917", 1175371830)
+
+-- Missing Depots (We don't have the keys yet lol): 4340671, 4340672
