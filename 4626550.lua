78e3b4b10df0cb2ea64ac262775b6068b0089bd0
qiao-lima <qiao-lima@github.com>
2026-08-16 02:26:39 +0800

    Manual Update 4626550

---
+++ 4626550.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4626550 - My Best Friends’ Girlfriends
+-- Generated 2026-08-15 18:14:47 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4626550)
+
+-- Main Depots
+addappid(4626551, 1, "372b0c020f8f40390c3057985509d2c3685dd5c1baf1c037ee74e9d6a3a3b1c7")
+setManifestid(4626551, "8742345771075842030", 1642290849)
