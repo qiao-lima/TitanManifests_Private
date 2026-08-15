01136fab3609cfb0753348e80588fd414b2796c7
qiao-lima <qiao-lima@github.com>
2026-08-16 00:31:53 +0800

    Manual Update 2878450

---
+++ 2878450.lua
@@ -0,0 +1,25 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2878450 - Master of Command
+-- Generated 2026-08-15 00:20:19 UTC
+-- # Depots (Total/DLC/Shared): 4/3/0
+
+-- Main AppID
+addappid(2878450, 1, "3b2ed429ba668eca9c4646e85a886d2a3813d06a3897941e07e9897a22bff1b7")
+
+-- Main Depots
+addappid(2878451, 1, "39088c896b7d3fbd00dc4d6ceffc568de759d24c8be29d76139e3349cce53555")
+setManifestid(2878451, "2861219135674619326", 4124897314)
+
+-- DLC's (with depot keys)
+-- Master of Command - Supporter Edition (AppID: 4130010)
+addappid(4130010)
+addappid(4130010, 1, "f7dc01918e8337c21a145bce691a39fc6a1b50bc7071c055250934cfc62d8538")
+setManifestid(4130010, "4361993302272211209", 3239673315)
+-- Master of Command - Reichsarmee (AppID: 4235830)
+addappid(4235830)
+addappid(4235830, 1, "126830d172ec4df771a54085781dfe608fec4b86a09e4c35bf7c0306ef191a28")
+setManifestid(4235830, "1390842223067097456", 10258)
+-- Master of Command - The Lion & Eagle (AppID: 4764930)
+addappid(4764930)
+addappid(4764930, 1, "f2908bd54ff3f287d44863a64e6cb10946ceb48b6a29c89874faae5f8b5fb47b")
+setManifestid(4764930, "8814480602159195802", 15454)
