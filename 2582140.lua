e53f0bff66d93af3ee89b5a4093328055199c411
qiao-lima <qiao-lima@github.com>
2026-08-16 02:26:38 +0800

    Manual Update 2582140

---
+++ 2582140.lua
@@ -0,0 +1,18 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2582140 - Seafarer: The Ship Sim
+-- Generated 2026-08-15 15:19:00 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(2582140)
+
+-- Main Depots
+addappid(2582141, 1, "f40d0c88571b28f840178b77be55334a2e94510b18e939af7c2da27f9d855b4e")
+setManifestid(2582141, "3640560191063347762", 19267821931)
+
+-- DLC's (no depot keys required)
+addappid(4213410) -- Seafarer: The Ship Sim - Supporter DLC
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
