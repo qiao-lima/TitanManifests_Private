dd0d8521bf223c9fd6c9670f8279c2e5fadbc1a6
qiao-lima <qiao-lima@github.com>
2026-08-16 00:31:52 +0800

    Manual Update 2767530

---
+++ 2767530.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2767530 - Wicked Seed
+-- Generated 2026-08-15 15:44:38 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2767530, 1, "ec2f97fbb0aeaed9123f621fd42c41c474d3c3085455dd207522aba25313cbc2")
+
+-- Main Depots
+addappid(2767531, 1, "5e98faea04897f670898e4eac039ecfe4eff5770d0fe517f44a3731de86f130c")
+setManifestid(2767531, "492721406154623323", 32994865945)
