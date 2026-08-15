657c95a1252524b518f92bcaf21d58ab55ec630d
qiao-lima <qiao-lima@github.com>
2026-08-16 02:26:39 +0800

    Manual Update 3466670

---
+++ 3466670.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3466670 - Ichigo's Study Sessions
+-- Generated 2026-08-15 01:16:40 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(3466670)
+
+-- Main Depots
+addappid(3466671, 1, "12d931f98b1a880e6a8e4aa78274e6fe40a2caeb47c254d45bd33873942fbb2d")
+setManifestid(3466671, "2509949678659140328", 99803529)
+addappid(3466672, 1, "c605ad35e615c48937422778a73d2fdb86a13464bf95b540cb90b7d8c8fb049a") -- Schinese
+setManifestid(3466672, "3054626486296509086", 100095941)
+addappid(3466673, 1, "1951b26498a7a994afbdf2a06d14f655f6bbdf9dad110caae39e9b340813b945") -- Japanese
+setManifestid(3466673, "8274566905987898004", 99510103)
