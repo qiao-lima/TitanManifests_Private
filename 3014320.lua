3c97e8dae0ebe983c691fe4a6c6142d88f136b1f
qiao-lima <qiao-lima@github.com>
2026-08-16 00:31:53 +0800

    Manual Update 3014320

---
+++ 3014320.lua
@@ -0,0 +1,20 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3014320 - OCTOPATH TRAVELER 0
+-- Generated 2026-08-14 19:33:08 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(3014320, 1, "f4fd915fb13264ea1005edb8f7beee8699ea3a94262534f617592669ee815ab8")
+
+-- Main Depots
+addappid(3014321, 1, "376135a423323c017c43031fbf0c4048cf6ea9a6ca94311cd04879ad9a043491")
+setManifestid(3014321, "6649250304437060125", 6930504004)
+
+-- DLC's (no depot keys required)
+addappid(3576180) -- オクトパストラベラー0 旅立ち応援パック
+addappid(3718180) -- オクトパストラベラー0 旅立ち祝福パック
+addappid(3825480) -- OCTOPATH TRAVELER 0 Digital Deluxe Upgrade
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
