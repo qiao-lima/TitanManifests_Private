4b35a61864c0a2f0e29798baf6f4be0b12f75d2b
qiao-lima <qiao-lima@github.com>
2026-08-17 06:49:24 +0800

    Manual Update 3774300

---
+++ 3774300.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3774300.lua
+--Gamename Jeff The Killer: Horror Game
+addappid(3774300) --Mainappid Jeff The Killer: Horror Game
+addappid(3774302, 1, "de76b8bea219282a9e0c098ab7eb28a12427636e1138bfde76902c9098099927") --Main Windows Depot Jeff The Killer: Horror Game
+setManifestid(3774302, "3139800460381384799", 1308831024)
