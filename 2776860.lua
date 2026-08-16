7ce67e9efcb9d3a3068427b62b6ec162790d3231
qiao-lima <qiao-lima@github.com>
2026-08-17 06:49:21 +0800

    Manual Update 2776860

---
+++ 2776860.lua
@@ -0,0 +1,18 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2776860 - Let's Patiti!
+-- Generated 2026-08-16 22:07:51 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(2776860)
+
+-- Main Depots
+addappid(2776861, 1, "9d0ba8ca5da63999b949cfc9d7cc4358638af01f5aa6f90c926a0d5fecaf9788")
+setManifestid(2776861, "7595414640192068657", 2126146202)
+
+-- DLC's (no depot keys required)
+addappid(3530330) -- Let's Patiti! 数字豪华版
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
