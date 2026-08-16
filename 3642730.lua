d07787c913a9c137904d130844b1a9abc7ba6d62
qiao-lima <qiao-lima@github.com>
2026-08-17 06:49:23 +0800

    Manual Update 3642730

---
+++ 3642730.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3642730 - Backrooms: Exit from Supermarket
+-- Generated 2026-08-16 21:55:14 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3642730)
+
+-- Main Depots
+addappid(3642731, 1, "d80c8777f6b5d5da7abc3e6040b9ebddd3e98ee57d0c4caaa4fdfdd98e9cb565")
+setManifestid(3642731, "467777583283044015", 1321204770)
