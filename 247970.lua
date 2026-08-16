b24cabc141c149c28d90a2020ba730ab1c1d5e70
qiao-lima <qiao-lima@github.com>
2026-08-17 06:49:21 +0800

    Manual Update 247970

---
+++ 247970.lua
@@ -0,0 +1,22 @@
+-- Generated with Luie @ https://lua.tools/
+-- 247970 - Korea. IL-2 Series
+-- Generated 2026-08-16 20:07:26 UTC
+-- # Depots (Total/DLC/Shared): 5/2/2
+
+-- Main AppID
+addappid(247970)
+
+-- Main Depots
+addappid(247971, 1, "38512b456adc44f3e9fa5db22ae548f2712e75ba1edab033d8d65c88f3a43909") -- (windows, 64-bit)
+setManifestid(247971, "1838517021591500937", 40224437434)
+
+-- DLC's (no depot keys required)
+addappid(5026020) -- Korea. IL-2 Series: F-84E Thunderjet Collector Plane
+addappid(5026030) -- Korea. IL-2 Series: Lavochkin La-11 Collector Plane
+addappid(5026460) -- Korea. IL-2 Series: Unique Aerobatic Team Liveries
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 5026801, 5026802
