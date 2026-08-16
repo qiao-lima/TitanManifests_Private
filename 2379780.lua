3aefc737c4e61583895dd68e1aa7b730edd76ec9
qiao-lima <qiao-lima@github.com>
2026-08-17 06:49:20 +0800

    Manual Update 2379780

---
+++ 2379780.lua
@@ -0,0 +1,22 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2379780 - Balatro
+-- Generated 2026-08-16 22:20:13 UTC
+-- # Depots (Total/DLC/Shared): 4/1/1
+
+-- Main AppID
+addappid(2379780, 1, "917725b9bf04cd3320dcb8406cfe267bd1b051351b00fa9a6dd47c133be93419")
+
+-- Main Depots
+addappid(2379781, 1, "16261e41d3e864018778d4a1d81658521a67d9ffb8543ea7e3e21f0685721af1") -- (windows)
+setManifestid(2379781, "3512319404653808464", 66662933)
+addappid(2379782, 1, "d0d563bdb51d625d0ed3a3234be0282f8d9a25d81dce599cdcace7b88c3b4719") -- (macos)
+setManifestid(2379782, "1898957422191678575", 85164047)
+
+-- DLC's (with depot keys)
+-- Balatro Soundtrack (AppID: 2834460)
+addappid(2834460)
+addappid(2834461, 1, "4f3bc0746cd896ec3e138561b98b1d1b0e8a1e0d771c8f013f5badb7cd39dc32")
+setManifestid(2834461, "7953749596502960011", 218737418)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
