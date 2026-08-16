a164f63fd82671502382d55b7d64def99bd6e82e
qiao-lima <qiao-lima@github.com>
2026-08-17 06:49:21 +0800

    Manual Update 2821400

---
+++ 2821400.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2821400 - Backward poiesis
+-- Generated 2026-08-16 21:55:37 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(2821400)
+
+-- Main Depots
+addappid(2821401, 1, "77244c1e8b59f232bacffed01f0424488b21f190925943224b334d05f4fc2742")
+setManifestid(2821401, "256126413197424385", 5376922758)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
