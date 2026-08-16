34f7e38377055efcb964e0d6915d1b80fd138bc8
qiao-lima <qiao-lima@github.com>
2026-08-17 06:49:57 +0800

    Manual Update 3809110

---
+++ 3809110.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3809110.lua
+--Gamename 日本事故物件監視協会 -Japan Stigmatized Property-
+addappid(3809110) --Mainappid 日本事故物件監視協会 -Japan Stigmatized Property-
+addappid(3809111, 1, "2e17755ea4a2836a0d38a614a7686c6c77f4c392cbd35ae322035ab1bee06d45") --Main Windows Depot 日本事故物件監視協会 -Japan Stigmatized Property-
+setManifestid(3809111, "1437354914791807753", 457010128)
