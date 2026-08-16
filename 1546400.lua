28985e9d870f6102a64c805368b4113c6cab298f
qiao-lima <qiao-lima@github.com>
2026-08-17 06:49:19 +0800

    Manual Update 1546400

---
+++ 1546400.lua
@@ -0,0 +1,28 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1546400 - GigaBash
+-- Generated 2026-08-16 21:22:29 UTC
+-- # Depots (Total/DLC/Shared): 5/2/2
+
+-- Main AppID
+addappid(1546400, 1, "22cc9243a3385adf19bce1940554f32b9274a472c8650edf0de0ef84a583a5e4")
+
+-- Main Depots
+addappid(1546401, 1, "204960045ed3c99ef55795d7304c0d2fb3b2c548b70fabf87559565b88696b18") -- (windows)
+setManifestid(1546401, "1056025093395279521", 14376677104)
+
+-- DLC's (no depot keys required)
+addappid(2161720) -- GigaBash - Godzilla 4 Kaiju Pack
+addappid(2574860) -- GigaBash - Ultraman 4 Characters Pack
+addappid(2920450) -- GigaBash - Godzilla: Nemesis DLC
+addappid(3063700) -- GigaBash - Mighty DLC: R.O.J.A.K & Balzarr
+addappid(3279990) -- GigaBash | Ultraman: Rising DLC
+addappid(3516760) -- GigaBash - MechaJURAS Free DLC
+addappid(4074560) -- GigaBash - GAMERA -Rebirth- DLC
+addappid(4372300) -- GigaBash - Final Ascension DLC
+addappid(4744900) -- GigaBash - Ultraman Zero DLC
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 2214291, 2214293
