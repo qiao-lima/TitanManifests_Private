afdfd846a6de81b52338f10fc1a2912069a2ec23
qiao-lima <qiao-lima@github.com>
2026-08-17 06:49:59 +0800

    Manual Update 4285690

---
+++ 4285690.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4285690 - Airport Security Sucks!
+-- Generated 2026-08-16 22:19:43 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4285690, 1, "27d5af42bda4afe5d34d5fabfccd0f5df60ec9e7850a77f5898f5e71cec305a5")
+
+-- Main Depots
+addappid(4285691, 1, "eb7f2322781794e5387ce47e5c1e52ef69cad6de4dc686775446066ef4ab3920")
+setManifestid(4285691, "7836582520369082099", 3127078607)
