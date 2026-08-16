a7ff9984a3e5bbe3a362ffe0b680075620ed6d0f
qiao-lima <qiao-lima@github.com>
2026-08-17 06:50:13 +0800

    Manual Update 859870

---
+++ 859870.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 859870 - Frqncy
+-- Generated 2026-08-16 22:21:21 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(859870)
+
+-- Main Depots
+addappid(859872, 1, "6044a9806d1965a1581c73aa35ede916945441eba226edf78ddeec00cb718e41") -- (windows, 64-bit)
+setManifestid(859872, "7126384386135350038", 2583791875)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
