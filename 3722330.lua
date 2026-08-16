0a2776bee3fe9a1a953911082e53201bb0681916
qiao-lima <qiao-lima@github.com>
2026-08-17 06:49:23 +0800

    Manual Update 3722330

---
+++ 3722330.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3722330 - Shift At Midnight
+-- Generated 2026-08-16 22:12:58 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3722330, 1, "837512e9ffbe1435f39a77a9010a70a3285befe9b540f084e1757623c7bd324e")
+
+-- Main Depots
+addappid(3722331, 1, "241b88aeb27a250cbfff336e4e5ec6f28a60d136f4431f8bdcbf84a0506e5ed4")
+setManifestid(3722331, "5526189819263516595", 887911876)
