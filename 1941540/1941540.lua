1963a5562b73f5e16c40403bc156075c37ab8e97
qiao-lima <qiao-lima@github.com>
2026-08-17 06:49:19 +0800

    Manual Update 1941540

---
+++ 1941540/1941540.lua
@@ -0,0 +1,11 @@
+addappid(1941540, 1, "7ef6aa6db9c09188f240b84e342b8a15bfa20d13ad0398983a6a305e9ae407cd") --Mainappid Mafia: The Old Country
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Main Windows Depot Mafia: The Old Country
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Main Windows Depot Mafia: The Old Country
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") --Main Windows Depot Mafia: The Old Country
+addappid(1941541, 1, "47c1ea957b9ecc8662a117fa6b19eea01ed30ef06478a364110fe40afa4e9f0e") --Main Windows Depot Mafia: The Old Country
+setManifestid(1941541, "3121778726057603637", 24657362)
+addappid(3262000) --Dlcname Mafia: The Old Country - Soldato Pack
+addappid(3269260) --Dlcname Mafia: The Old Country - Padrino Pack
+addappid(3269270) --Dlcname Mafia: The Old Country - Gatto Nero Pack
+addappid(3269280) --Dlcname Mafia: The Old Country - Soundtrack and Digital Artbook
+addappid(4492700) --Dlcname Mafia: The Old Country - Man of Honor
