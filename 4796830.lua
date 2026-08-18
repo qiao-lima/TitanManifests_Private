5391ad24e6953a9b934b2ca281645a84ae1eb917
qiao-lima <qiao-lima@github.com>
2026-08-19 05:17:22 +0800

    Manual Update 4796830

---
+++ 4796830.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4796830 - WE ARE SO DEAD
+-- Generated 2026-08-18 20:31:15 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(4796830)
+
+-- Main Depots
+addappid(4796831, 1, "6929528029baed36b462e27b746056daf70c4ffdb93040d3bb0c0a22c1338dd2")
+setManifestid(4796831, "3926972336528043281", 3858945881)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
