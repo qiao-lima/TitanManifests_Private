bb6de22580a479930a48433804595a2e2a5a38c2
qiao-lima <qiao-lima@github.com>
2026-08-18 08:05:39 +0800

    Manual Update 4119870

---
+++ 4119870.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4119870 - Major Milkers on Double Booty Duty
+-- Generated 2026-08-17 23:35:11 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(4119870)
+
+-- Main Depots
+addappid(4119871, 1, "5928843ea4c6aa828ba853eaf525af464fa039a3df33d3331c7c45e75aeeed9d") -- (windows,linux)
+setManifestid(4119871, "8308671713773189767", 1049199276)
+
+-- Missing Depots (We don't have the keys yet lol): 4119872
