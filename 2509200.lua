8dc56c163b8f01aef4737a227f63eb07b28078bd
qiao-lima <qiao-lima@github.com>
2026-08-18 08:05:16 +0800

    Manual Update 2509200

---
+++ 2509200.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2509200 - ERA ONE
+-- Generated 2026-08-17 23:59:41 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2509200)
+
+-- Main Depots
+addappid(2509202, 1, "034c3ac621a0396536dc367d7faaef543ddfd67f28576c55fabe139eed737daa") -- (windows, 64-bit)
+setManifestid(2509202, "7271276049144948275", 9075402610)
