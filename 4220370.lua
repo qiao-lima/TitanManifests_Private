a2159370b23b936f02759f8d10d3fb584f0ff72d
qiao-lima <qiao-lima@github.com>
2026-08-18 08:05:40 +0800

    Manual Update 4220370

---
+++ 4220370.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4220370 - KUMASAMA
+-- Generated 2026-08-17 23:46:14 UTC
+-- # Depots (Total/DLC/Shared): 4/0/0
+
+-- Main AppID
+addappid(4220370, 1, "0671eab70b72c7f4f1171b8e1f5c5ceea027171d579cc2de72eb66cf0a2cf59c")
+
+-- Main Depots
+addappid(4220371, 1, "f766c04bd2add2019a5dce7bbf496fec93ad1c8d6c0ca1419363bbdb6c86567d") -- (windows,linux)
+setManifestid(4220371, "7129253906309744284", 261953546)
+addappid(4220373, 1, "65e487b8803c74eaa6f18c444782deeb7b6a23c5df43c137e7ee32cf99e5d43e") -- Russian (windows,linux)
+setManifestid(4220373, "7691675711818987138", 261960252)
+
+-- Missing Depots (We don't have the keys yet lol): 4220372, 4220374
