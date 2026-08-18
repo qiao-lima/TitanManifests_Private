ccb60d761bd967423a5c2bdeda9062b24eb344fb
qiao-lima <qiao-lima@github.com>
2026-08-19 05:16:58 +0800

    Manual Update 3493400

---
+++ 3493400.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3493400 - BLOODLETTER
+-- Generated 2026-08-18 21:09:50 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3493400, 1, "66b152e6cc0e995b3e68e4efccdf650f5b2c792b63ec83d1eda8d6bc746afa51")
+
+-- Main Depots
+addappid(3493401, 1, "8a6abb5aad134d861c0a29f71a53939beb784d0c3cf5d5cb5ee9dc99695c0191") -- (windows)
+setManifestid(3493401, "7236762630544277558", 5494780342)
