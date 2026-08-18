11d552ff4af5d16a673a37760a830dc921109953
qiao-lima <qiao-lima@github.com>
2026-08-19 06:37:42 +0800

    Manual Update 3576520

---
+++ 3576520.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3576520.lua
+--Gamename Spinera
+addappid(3576520) --Mainappid Spinera
+addappid(3576521, 1, "d3d5112344c4739f075e9277aef455d27c775bf0dd2875960a030a3b54974620") --Main Windows Depot Spinera
+setManifestid(3576521, "4660567707181423111", 358102624)
