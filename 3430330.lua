333d9060213a376ccf955616015df62784257935
qiao-lima <qiao-lima@github.com>
2026-08-19 05:16:58 +0800

    Manual Update 3430330

---
+++ 3430330.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3430330 - Body Sushi
+-- Generated 2026-08-18 20:56:21 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3430330)
+
+-- Main Depots
+addappid(3430331, 1, "020500b2110fedf0ae46883f597b791a469584a9c359fb8d49f432e0fcf98637")
+setManifestid(3430331, "2822726478617150508", 3427894558)
