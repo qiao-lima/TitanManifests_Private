68dd65ec7e71982f8f1eacf86926e3e6028f827f
qiao-lima <qiao-lima@github.com>
2026-08-19 05:16:59 +0800

    Manual Update 3729550

---
+++ 3729550.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3729550 - Stackmon
+-- Generated 2026-08-18 19:53:52 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(3729550)
+
+-- Main Depots
+addappid(3729551, 1, "e5e9399d4f8900ee91c0dc8e562d8be46db19fb6d92153a57e899e7cf9a218fe") -- (windows)
+setManifestid(3729551, "3495676180886328074", 1004007439)
+
+-- Missing Depots (We don't have the keys yet lol): 3729552
