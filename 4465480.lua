0509d7a32173e2529887dea923901d76bba33ed3
qiao-lima <qiao-lima@github.com>
2026-08-18 08:05:41 +0800

    Manual Update 4465480

---
+++ 4465480.lua
@@ -0,0 +1,29 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4465480 - Counter-Strike:Global Offensive
+-- Generated 2026-08-17 23:28:27 UTC
+-- # Depots (Total/DLC/Shared): 10/0/2
+
+-- Main AppID
+addappid(4465480, 1, "3d663a830faf1b70625be48115681d57af4a6e532b5aea8fc307063255e0db2c")
+
+-- Main Depots
+addappid(731, 1, "bca9a9cde94bb4dff61849c6a87230ee45867a590fdd28826366e35e7d62c08e")
+setManifestid(731, "1224088799001669801", 32722411363)
+addappid(732, 1, "da1f76913633e9ce1b2bda5ec464dc507205388fac5c4c614b6a2706cdbd0912") -- (windows)
+setManifestid(732, "6314304446937576250", 363675822)
+addappid(733, 1, "23c8e9a91561a5fb08abf22f2ac98929f4aad68a12164268d3b2800c3db4fb03") -- (macos)
+setManifestid(733, "7818492753742006607", 276643099)
+addappid(734, 1, "54fd243c5463c16ec7d32b5b4c7b792b3f2d3f75009ac3e81135ff999b56187f") -- (linux)
+setManifestid(734, "3343055176458696864", 434785022)
+addappid(735, 1, "a0ef69f0cf8abd70aa9ed79755fbe4d98cf7c6723ba9e0c66e0f7102e49541e6")
+setManifestid(735, "3867231304834558645", 0)
+addappid(736, 1, "e81988c241b4ec9fc7fe1ab34697689f131a48c78da95c985ed347fb43bdc6de")
+setManifestid(736, "1949918900514112752", 0)
+addappid(737, 1, "c80f96efa325374c18c6d5d63ccd2687b1680a908c6def5c5c808e695f1ddbf9")
+setManifestid(737, "8959772282070246004", 417072383)
+addappid(738, 1, "fbc1a292c9b75e33512ed82476267efbf34f1ed1b177651ed8b091cf7174d183")
+setManifestid(738, "313951540668567317", 920601806)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
