576623bd4dc6ee24dbd891b5c81e57bd820632c1
qiao-lima <qiao-lima@github.com>
2026-08-19 05:17:00 +0800

    Manual Update 3832120

---
+++ 3832120.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3832120.lua
+--Gamename Femboy Survival
+addappid(3832120, 1, "021077d4f2f0762d0a69fa3302dec04f5653d09c1287b6c13f4830e7ad725949") --Mainappid Femboy Survival
+addappid(3832121, 1, "5fe0e6ae52258769eb74bc7a5f9438ffd660489495b7e15f347074e9f10bbc80") --Main Windows Depot Femboy Survival
+setManifestid(3832121, "6677307511273697633", 314709184)
