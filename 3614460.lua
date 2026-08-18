d677d1e9569a0df57331d110922aa6e0713622be
qiao-lima <qiao-lima@github.com>
2026-08-19 05:16:59 +0800

    Manual Update 3614460

---
+++ 3614460.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3614460.lua
+--Gamename Hypermarket Simulator
+addappid(3614460) --Mainappid Hypermarket Simulator
+addappid(3614461, 1, "61694a135077713cd302b58355b57bef28b20a5464f80f6a7fe941290dc74637") --Main Windows Depot Hypermarket Simulator
+setManifestid(3614461, "5681841297246258280", 7079004080)
