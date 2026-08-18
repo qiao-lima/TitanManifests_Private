83b2d3935e22e5b2f3d8a7a614660357e27d32e5
qiao-lima <qiao-lima@github.com>
2026-08-18 08:05:16 +0800

    Manual Update 2584270

---
+++ 2584270.lua
@@ -0,0 +1,14 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2584270 - Mortal Shell II
+-- Generated 2026-08-17 22:05:54 UTC
+-- # Depots (Total/DLC/Shared): 2/0/1
+
+-- Main AppID
+addappid(2584270, 1, "2b9f4a2124d5757f3d4f46c81c6d4082f3b1d1ac9b3233acf41ddc6b5c15810a")
+
+-- Main Depots
+addappid(2584271, 1, "bb59e6dc30df8728a4bfcc3129ca9b8284ed3808d42a9a272e65d4904b4bc445")
+setManifestid(2584271, "8675094712350771048", 73091282862)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
