9bab4b2ab805a4148eb02fa6696aaedc43acc434
qiao-lima <qiao-lima@github.com>
2026-08-18 08:05:19 +0800

    Manual Update 3722230

---
+++ 3722230.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3722230.lua
+--Gamename OutpostAssault
+addappid(3722230) --Mainappid OutpostAssault
+addtoken(3722230, "15011772234163725685")
