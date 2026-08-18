930093dd7722d85fc2c5777c7ddf19f8ecb23aa8
qiao-lima <qiao-lima@github.com>
2026-08-18 08:05:19 +0800

    Manual Update 3646510

---
+++ 3646510.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3646510 - Aisha’s Futa Diaries
+-- Generated 2026-08-17 23:42:58 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(3646510)
+
+-- Main Depots
+addappid(3646511, 1, "e971b64d80f13f4724977aa8635d27ed9fb0b06f11b79b2c1f63cfc63cb70ba0")
+setManifestid(3646511, "6323725127635893116", 1169278806)
+
+-- Missing Depots (We don't have the keys yet lol): 3646512, 3646513
