27c23fbb651d725ee8945c147024781361a0a62d
qiao-lima <qiao-lima@github.com>
2026-08-19 05:16:58 +0800

    Manual Update 3226450

---
+++ 3226450.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3226450 - iRacing Arcade
+-- Generated 2026-08-18 19:40:19 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3226450, 1, "5c6beaa1c6efa8261eccdf7fcdc0dfc9fbe962efd413be9d75a6690b0b5daae2")
+
+-- Main Depots
+addappid(3226451, 1, "32cb58f0c03b4a64f5f7488fcfb1e5671a279f1af40021cdc6c77ee6263fc8ba")
+setManifestid(3226451, "6787547287593120713", 2444603033)
