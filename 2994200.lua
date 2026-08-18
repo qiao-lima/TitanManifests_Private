4dabcb6df2cff5fe4509e0ee1e8a1375e4a8bc43
qiao-lima <qiao-lima@github.com>
2026-08-18 08:05:17 +0800

    Manual Update 2994200

---
+++ 2994200.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2994200 - Ritter & Rotwein
+-- Generated 2026-08-17 23:24:18 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2994200, 1, "97465c3cfa92f765074b78f24a1ab094eda9e2760dfead872ca5ad1130d2881a")
+
+-- Main Depots
+addappid(2994201, 1, "aed8af5f706d2a26435db418bdb272c5089e9a937eda79b8a55fca40993ecce0") -- (windows)
+setManifestid(2994201, "1114714965561115793", 5126190481)
