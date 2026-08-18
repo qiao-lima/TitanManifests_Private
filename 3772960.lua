7a67fc3991574f045fb1b044ac748bceff1abb5f
qiao-lima <qiao-lima@github.com>
2026-08-19 06:37:42 +0800

    Manual Update 3772960

---
+++ 3772960.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3772960 - UMIGARI | ウミガリ
+-- Generated 2026-08-18 18:27:39 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(3772960)
+
+-- Main Depots
+addappid(3772961, 1, "2cf04aa079347c28e4bd8e9fd4b600d7687adfd37a82df9a34b0e271b1716af0")
+setManifestid(3772961, "8370065284053763517", 5057579924)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
