073c96b4d2daaa28103bb9f55cb95c03632e114c
qiao-lima <qiao-lima@github.com>
2026-08-18 09:15:57 +0800

    Manual Update 4041240

---
+++ 4041240.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4041240 - LOVEPICAL-POPPY!
+-- Generated 2026-08-18 00:19:25 UTC
+-- # Depots (Total/DLC/Shared): 4/0/0
+
+-- Main AppID
+addappid(4041240)
+
+-- Main Depots
+addappid(4041241, 1, "25d89490edc066f4797e627dd240af94964168bbd85daa994d9c14717dd2d48e")
+setManifestid(4041241, "2111301743474730947", 1749205030)
+addappid(4041243, 1, "10d91b06f19cbb133875f3d4b9e6879d8dd35643a503e4c7194e9bcb001c80c4") -- Schinese
+setManifestid(4041243, "2768409763653434", 1517568)
+
+-- Missing Depots (We don't have the keys yet lol): 4041242, 4041244
