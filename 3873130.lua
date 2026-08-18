1e80031dacb32ac04a8b280a60229567f41e00e1
qiao-lima <qiao-lima@github.com>
2026-08-19 05:17:00 +0800

    Manual Update 3873130

---
+++ 3873130.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3873130 - NTR
+-- Generated 2026-08-18 20:14:30 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3873130)
+
+-- Main Depots
+addappid(3873131, 1, "17fed5c1228aeef353a0716137a5e04959f084068a4cdbb1be4efcf148a342d9")
+setManifestid(3873131, "2050592037596329497", 471034798)
