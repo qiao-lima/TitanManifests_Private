ad479c0a9f04c6e7e854a8466def89eb658d9b55
qiao-lima <qiao-lima@github.com>
2026-08-18 08:05:40 +0800

    Manual Update 4195570

---
+++ 4195570.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4195570 - NTR Fantasy
+-- Generated 2026-08-17 23:53:45 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4195570)
+
+-- Main Depots
+addappid(4195571, 1, "08b65c052d5b3ca20eedbb47977ac149ec72c365c86ef77c1f8790a1884d55ab") -- (windows)
+setManifestid(4195571, "2716866959545397665", 744877638)
