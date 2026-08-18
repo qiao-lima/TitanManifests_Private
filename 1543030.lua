ddb4ced9cbf58860deaeab67ae1478dc453ce360
qiao-lima <qiao-lima@github.com>
2026-08-18 08:05:14 +0800

    Manual Update 1543030

---
+++ 1543030.lua
@@ -0,0 +1,27 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1543030 - Sword and Fairy 7
+-- Generated 2026-08-17 22:40:22 UTC
+-- # Depots (Total/DLC/Shared): 6/3/2
+
+-- Main AppID
+addappid(1543030, 1, "cdbc7dfc9ee082feb4798333026064d76efe9ae2dd75ffd31d4dbb7bd4ae8185")
+
+-- Main Depots
+addappid(1543031, 1, "80c82f692c1dccb0e5de735f6b7513dc62421d6f9bfdf44af13992f5de4367cd") -- (windows)
+setManifestid(1543031, "1328293710815739891", 32459173251)
+
+-- DLC's (with depot keys)
+-- Sword and Fairy 7 Soundtrack (AppID: 1867890)
+addappid(1867890)
+addappid(1867891, 1, "3f301b765cbacde2590bec1ef5a1d1b51108d13dd5c1b29ba09b562fcd6b995e")
+setManifestid(1867891, "2059192692005900934", 490295450)
+addappid(1867892, 1, "6b32f373adbc55bf32d79707d497ac1c450c7c74737173b0c501a507fdab9060")
+setManifestid(1867892, "7849036732852795147", 2844140124)
+-- Sword and Fairy 7 - Dreamlike World Expansion (AppID: 2223240)
+addappid(2223240)
+addappid(2223241, 1, "c7d3ba2342020f382cb7e6c7f202bbb7a136283e87870d04190562e15966f398")
+setManifestid(2223241, "8900201496308069518", 12702625858)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
