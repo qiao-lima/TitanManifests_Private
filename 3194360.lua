b0f06b224d3507ffc7c48286c5423531996612e8
qiao-lima <qiao-lima@github.com>
2026-08-19 05:16:57 +0800

    Manual Update 3194360

---
+++ 3194360.lua
@@ -0,0 +1,26 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3194360 - Formula Legends
+-- Generated 2026-08-18 20:11:32 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(3194360, 1, "3781566b837b2f6018dc45d577f6ff11f140c795ca80a887b663da1c7028effd")
+
+-- Main Depots
+addappid(3194361, 1, "2eab901f3297c0706ad8606e564f7913bf97b561233741ca20f0de137d4142f3")
+setManifestid(3194361, "7846579156213736652", 13935044635)
+
+-- DLC's (no depot keys required)
+addappid(4060830) -- Formula Legends DLC 1
+addappid(4060840) -- Formula Legends DLC2
+addappid(4143900) -- Formula Legends : Pioneer Pack
+addappid(4143910) -- Formula Legends : Early 2010’s Season Pack
+addappid(4143920) -- Formula Legends : Turbo Power Pack
+addappid(4216430) -- Formula Legends : Late 2020’s Season Pack
+addappid(4222670) -- Formula Legends : Formula E - Electric Evolution
+addappid(4362310) -- Formula Legends : Iconic Tracks
+addappid(4362320) -- Formula Legends : Historic Tracks Pack
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
