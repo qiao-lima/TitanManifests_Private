dfd2898050da14ab6178d1701d060b070eee63f4
qiao-lima <qiao-lima@github.com>
2026-08-18 08:05:38 +0800

    Manual Update 3803780

---
+++ 3803780.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3803780 - Lost in the World of Succubi
+-- Generated 2026-08-17 23:41:24 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3803780, 1, "2086c1dd74d7e2c78ecae297ed1b00529bebfe1f4538db6601241b4cc9eac51a")
+
+-- Main Depots
+addappid(3803781, 1, "57610de94ac62dc7f3a80474a1254daa590c7801562f23c5004e57cad24c0d2b")
+setManifestid(3803781, "7183699668530150619", 162644372)
