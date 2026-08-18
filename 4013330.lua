adb78c5f837f57fa75c9559eaa89dd99f92cad0e
qiao-lima <qiao-lima@github.com>
2026-08-18 08:05:39 +0800

    Manual Update 4013330

---
+++ 4013330.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4013330 - Fapout 69 ☢️
+-- Generated 2026-08-17 23:35:02 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(4013330)
+
+-- Main Depots
+addappid(4013331, 1, "686a9f8e96f4223d104f5bad0f3ea4f24d6d937ab8797dbfadc55752d50dd60a")
+setManifestid(4013331, "1543867438186442356", 4369253059)
+
+-- Missing Depots (We don't have the keys yet lol): 4013332, 4013333
