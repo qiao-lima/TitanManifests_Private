fb3c149638e4d46d7e2e03549ce7edd8de822ccb
qiao-lima <qiao-lima@github.com>
2026-08-19 05:17:00 +0800

    Manual Update 3812250

---
+++ 3812250.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3812250 - Bow Down & Eyes up
+-- Generated 2026-08-18 20:28:49 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3812250)
+
+-- Main Depots
+addappid(3812251, 1, "705692a9049745cbe6b849b8dbcf64f4b30e215ca89523457cda39b731f768b2")
+setManifestid(3812251, "180788343382864899", 5134181368)
