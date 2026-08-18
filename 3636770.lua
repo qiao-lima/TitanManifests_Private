1b04eb43cb55956a0dd4b59400bb8a214b1d53cb
qiao-lima <qiao-lima@github.com>
2026-08-19 05:16:59 +0800

    Manual Update 3636770

---
+++ 3636770.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3636770 - Servant of the Lake
+-- Generated 2026-08-18 19:18:26 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(3636770, 1, "1dc4585b4e63a97e168470a4cfcf2812c60219abfe7d544c332fb08ace7d7141")
+
+-- Main Depots
+addappid(3636773, 1, "47efd8d9e4421b857f586ee20c067790f4360b03d9b21e7137ceb751d0c6d638") -- (windows)
+setManifestid(3636773, "1854209052457508367", 713945974)
+addappid(3636774, 1, "6dde3b2c075c250b5d135902338e5991ce3da4992f204d3e3140821d89390c5a") -- (macos)
+setManifestid(3636774, "6595385569529456018", 759190214)
