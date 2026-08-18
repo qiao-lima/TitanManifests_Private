e38a1e74bd31251913c623aa4789437554029dc8
qiao-lima <qiao-lima@github.com>
2026-08-19 05:17:00 +0800

    Manual Update 3869210

---
+++ 3869210.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3869210 - カエルvs壁画娘2　裏階層
+-- Generated 2026-08-18 20:21:23 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3869210)
+
+-- Main Depots
+addappid(3869211, 1, "b871a8d2e46a18481e498d0c3a90c49116e8b04045d07bc11b75ac5dcba91189")
+setManifestid(3869211, "4431405393541011746", 318735084)
