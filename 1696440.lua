afeabdc658d5f6e7f1446ed1ba3980bffe908966
qiao-lima <qiao-lima@github.com>
2026-08-18 08:05:14 +0800

    Manual Update 1696440

---
+++ 1696440.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1696440.lua
+--Gamename 绝世好武功 The Matchless KungFu
+addappid(1696440, 1, "deb687a9d31a413fe2b84142979a8f528cbf24ad6492b1b5defe1bb227ae13fe") --Mainappid 绝世好武功 The Matchless KungFu
+addtoken(1696440, "421702179611066545")
+addappid(1696441, 1, "d9d9203084ea5a76bb40325b0d450fda89e2f4b27f1c4a336f062110ed51c526") --Main Windows Depot 绝世好武功 The Matchless KungFu
