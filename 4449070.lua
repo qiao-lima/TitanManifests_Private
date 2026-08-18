fe88671a01abdd891bc7243ee8f4adff5449ae90
qiao-lima <qiao-lima@github.com>
2026-08-18 08:05:41 +0800

    Manual Update 4449070

---
+++ 4449070.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4449070 - Game Game Developer
+-- Generated 2026-08-17 23:45:34 UTC
+-- # Depots (Total/DLC/Shared): 4/0/0
+
+-- Main AppID
+addappid(4449070, 1, "987ced8f5f5367f461c6c7e20467b0103753151689bd0021f1cce8e3468c0a40")
+
+-- Main Depots
+addappid(4449071, 1, "8bdad54edbc4c5770e1e5d84cc3d63257d6b83a9bfef863299806b91f930fade") -- (windows,linux)
+setManifestid(4449071, "9109756678062674443", 268753498)
+addappid(4449072, 1, "0159082978ed42f01f0d139c233328ae8f5da03a24437413a6db19be91e608dd") -- Schinese (windows,linux)
+setManifestid(4449072, "6076679056324789540", 268753230)
+addappid(4449073, 1, "3162d0e4931e743952cc4146cf6a3c3f6e58d4eed781169f91ae32d5dab813c8") -- Russian (windows,linux)
+setManifestid(4449073, "7248149400965699594", 268761137)
+addappid(4449074, 1, "5fbbd886a7e7e1873b96cf145905e00fa9d414ff69a1be39f9cc7d14c577cfa7") -- Japanese (windows,linux)
+setManifestid(4449074, "8622226449446446446", 268757289)
