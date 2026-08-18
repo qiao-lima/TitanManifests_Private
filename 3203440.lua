a809f3893fa0c350e99bc0c7495ff49a454ef7cb
qiao-lima <qiao-lima@github.com>
2026-08-18 08:05:18 +0800

    Manual Update 3203440

---
+++ 3203440.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3203440 - Tales of the Moon
+-- Generated 2026-08-18 00:02:04 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3203440, 1, "0aa7b0fb5106039fc960f8d6554ff389b3649752d8b66edcedf5d51fe7756659")
+
+-- Main Depots
+addappid(3203441, 1, "256e14177ea492931e14bac1c96941e54e81be712efce2de7f98067c452ed405")
+setManifestid(3203441, "3809872956507342631", 415586391)
