4d841821ae5f9b7b98c161b27296d4d5668d12dc
qiao-lima <qiao-lima@github.com>
2026-08-18 08:05:18 +0800

    Manual Update 3311670

---
+++ 3311670.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3311670 - Bento Blocks
+-- Generated 2026-08-17 23:32:02 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3311670)
+
+-- Main Depots
+addappid(3311671, 1, "9cf9b1abe85d18535f146e5b45206ff653844049ef9479e6d62f74947eaf2a53")
+setManifestid(3311671, "8868623302903180445", 199797214)
