4c5c612e3b39540f48ea371122e5c448521dfcef
qiao-lima <qiao-lima@github.com>
2026-08-18 08:05:19 +0800

    Manual Update 3713000

---
+++ 3713000.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3713000 - Whimsy Bake＆Craft
+-- Generated 2026-08-18 00:00:47 UTC
+-- # Depots (Total/DLC/Shared): 2/0/1
+
+-- Main AppID
+addappid(3713000)
+
+-- Main Depots
+addappid(3713001, 1, "6462b361546855d29d374a01fc2216201cc282f6b1edf947f879f509524f4b3d")
+setManifestid(3713001, "6765399894904177038", 642630010)
+
+-- DLC's (no depot keys required)
+addappid(4212450) -- Whimsy Bake＆Craft - Supporter Pack
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
