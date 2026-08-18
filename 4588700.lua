0523a1b998baf3964e14a0c016671ec64a2788b8
qiao-lima <qiao-lima@github.com>
2026-08-18 08:05:42 +0800

    Manual Update 4588700

---
+++ 4588700.lua
@@ -0,0 +1,19 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4588700 - Monster Girls: You Can't Say No 🔞
+-- Generated 2026-08-17 22:52:36 UTC
+-- # Depots (Total/DLC/Shared): 4/1/0
+
+-- Main AppID
+addappid(4588700)
+
+-- Main Depots
+addappid(4588701, 1, "39d3ce7afbfbac82a7a7fb7a12f9cf32ca44c3c0aa2fc298f16aba4259e47f48") -- (windows)
+setManifestid(4588701, "8261394324198423215", 7313673520)
+
+-- DLC's (with depot keys)
+-- Monster Girls: You Can't Say No - Wallpapers🔞 (AppID: 5054000)
+addappid(5054000)
+addappid(5054000, 1, "11998b2b4b69d8901039b2d2053942713338a338ce1d736583747e3c174f7f41")
+setManifestid(5054000, "1920668810363759483", 219260698)
+
+-- Missing Depots (We don't have the keys yet lol): 4588702, 4588703
