881843e2ffe40865ed0e3f30ef6dc6c19360c738
qiao-lima <qiao-lima@github.com>
2026-08-19 05:16:58 +0800

    Manual Update 3371510

---
+++ 3371510.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3371510 - Roulette Hero
+-- Generated 2026-08-18 21:01:35 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3371510)
+
+-- Main Depots
+addappid(3371511, 1, "ae4ab53e71e6c9538308429a49eb3200c0cf62b550d49febfaa47379b7ac1763")
+setManifestid(3371511, "2698347342064183617", 171343033)
