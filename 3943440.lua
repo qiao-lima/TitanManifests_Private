bb57d55d0c301ecef1b252ac01a9c7567568c2f9
qiao-lima <qiao-lima@github.com>
2026-08-19 21:55:24 +0800

    Manual Update 3943440

---
+++ 3943440.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3943440 - WAGAMAMA HIGH SPEC OC
+-- Generated 2026-08-19 13:29:21 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(3943440, 1, "9727bb9714697ddb9702c1f83101d97b5711c9a35a176f3745ab78e4e9dfe4e7")
+
+-- Main Depots
+addappid(3943441, 1, "32d1ba1c33feeffe473c573ab9b4f474b351038538bf2458b847527756ba077e") -- Schinese
+setManifestid(3943441, "2272820141039912505", 2403122144)
+addappid(3943442, 1, "2c30145fb55a59fad7c5e1e5ab37b37827d9d2c267279da6ebf88b840fb87370") -- English
+setManifestid(3943442, "3099337504084810655", 2612079458)
