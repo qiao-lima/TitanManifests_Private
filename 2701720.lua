b85c31dfadd860d754b1f88308871f51aab5962d
qiao-lima <qiao-lima@github.com>
2026-08-19 21:47:57 +0800

    Manual Update 2701720

---
+++ 2701720.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2701720 - Wagotabi
+-- Generated 2026-08-19 12:22:13 UTC
+-- # Depots (Total/DLC/Shared): 4/1/0
+
+-- Main AppID
+addappid(2701720, 1, "1c96176c0d51cbfa78621d5c1f7b5687f300e7959353b84c5b70961f1449c3d6")
+
+-- Main Depots
+addappid(2701721, 1, "3f442267dbf86101cef73584e54483d3877b949124ad3af302989feb19fbc8a4") -- (windows)
+setManifestid(2701721, "1659064701226403810", 258984633)
+addappid(2701722, 1, "9d7c9584cd970afb7547b5886a3a9f5379ed4fea8080df886890b854b3ae03b2") -- (macos)
+setManifestid(2701722, "8267055033138503832", 264064675)
+addappid(2701723, 1, "0e03dc2e2319d6b17f4ec6b469d534d4a239fb9e0e48128fed8c49e30886cc86") -- (linux)
+setManifestid(2701723, "2899142033222816233", 250685230)
+
+-- Missing Depots (We don't have the keys yet lol): 5092311
