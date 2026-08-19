85d26a6f9599b46b5735e26081e149f952c03d25
qiao-lima <qiao-lima@github.com>
2026-08-19 21:55:26 +0800

    Manual Update 623360

---
+++ 623360.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 623360 - Grim Tales: The Legacy Collector's Edition
+-- Generated 2026-08-19 13:34:23 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(623360)
+
+-- Main Depots
+addappid(623361, 1, "622dad4a310ce9d51d42c2131f86b064331cfd1fd5ef47e6b00c1b227a5b92bb")
+setManifestid(623361, "1235023974993098149", 960979866)
