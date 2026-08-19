5e7575cc4412766072b0f2ebf32a4208d235f78a
qiao-lima <qiao-lima@github.com>
2026-08-19 22:12:18 +0800

    Manual Update 3065800

---
+++ 3065800.lua
@@ -0,0 +1,46 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3065800 - Marathon
+-- Generated 2026-08-19 14:11:36 UTC
+-- # Depots (Total/DLC/Shared): 14/0/0
+
+-- Main AppID
+addappid(3065800, 1, "7427ddedb10de59975456fe3d9b1f363d03109321fa2f2aa2187d82b85c4aab1")
+
+-- Main Depots
+addappid(3065801, 1, "0e21278144bf376c7d8a34cb2a28b38a275c0948bd8fc261dd86cba20a0b8ea3") -- (windows)
+setManifestid(3065801, "5145610235728519528", 30288142065)
+addappid(3065802, 1, "38a83f9c6804a02c2d936cf077be40121279da28d9e149a70a1e8ca56f42b259") -- (windows)
+setManifestid(3065802, "2290991522450037940", 728493528)
+addappid(3065803, 1, "2496fa9f81f8aacaacfac61694400da95175f86b031427303fd5b0cff59775cb") -- French (windows)
+setManifestid(3065803, "9214917815790999036", 657647064)
+addappid(3065804, 1, "a9fb32de62e3185f7da88c5cfeebfa8be3e95f12fe7947c202c6c72cffd9463a") -- Italian (windows)
+setManifestid(3065804, "1866622524050978812", 705332696)
+addappid(3065805, 1, "d9c1100fd72d15632afa42ca88ef15942940bf1d9fe988e7ccd4f89956d9f1bd") -- German (windows)
+setManifestid(3065805, "4185609586633401896", 672613848)
+addappid(3065806, 1, "cbbe4001937a5869feb979a0735838178e11f396145a8ba3b429605559571e11") -- Spanish (windows)
+setManifestid(3065806, "3426690065044786590", 711562712)
+addappid(3065807, 1, "64134cffcc214f0050db0303a38ff31eb6f9b0bd8bfc6fe6c54d7491cd2bfedf") -- Latam (windows)
+setManifestid(3065807, "5650301522526704065", 720166360)
+addappid(3065808, 1, "c0465c21465d02c42fefc99f192d3fc7706c0fb1d5137d43eda0a230d32bd81d") -- Brazilian
+setManifestid(3065808, "8331569755826249378", 696800728)
+addappid(3065809, 1, "c70d1f620b43534dba5c2f6fecba228b93987bbe9e13b73e5ce82dcd44e9dd83") -- Russian (windows)
+setManifestid(3065809, "14183529737912780", 704861656)
+addappid(3551860, 1, "38ae85dcafe40d80464799e89aba553a4a38b5056beecd7c1baa4e572a87d8b1") -- Polish (windows)
+setManifestid(3551860, "7270856531815691784", 684889560)
+addappid(3551861, 1, "95915f90c16210b1e78922f01e3256ab2d8cc91ce74f77a2a60b49eb8600dbfc") -- Japanese (windows)
+setManifestid(3551861, "3481866897051112580", 667532760)
+addappid(3551862, 1, "7be7f43cb3baac63b8b48b58634ea732cb0e4dda0f16bbe34d08e3c86d5817c4") -- Koreana (windows)
+setManifestid(3551862, "4940661245959432022", 667565528)
+addappid(3551863, 1, "3add136df05da956359e78385400e4f12ceb9ef369a49143158612d4a0414217") -- Schinese (windows)
+setManifestid(3551863, "6511565729840073499", 674608600)
+addappid(3551864, 1, "7ebd0fd7666847e4a3b10ee9ef53948296bfccdd0edb1f0167265d85b5f1c7e3") -- Tchinese (windows)
+setManifestid(3551864, "5750179708911729732", 26529240)
+
+-- DLC's (no depot keys required)
+addappid(3569730) -- Marathon Pre-Order Pack
+addappid(3569750) -- Marathon Deluxe Edition
+addappid(3569830) -- Marathon Deluxe Upgrade
+addappid(4055980) -- Marathon Tech Test Access
+addtoken(4055980, "10136859699694388933")
+addappid(4623170) -- Marathon Seasonal LUX Fun//Police Bundle
+addappid(4623200) -- Marathon Steam Bundle - Spectral Mist
