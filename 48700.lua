5bf2f85accb78319f0aafcb9ef871a46dd38a980
qiao-lima <qiao-lima@github.com>
2026-08-19 21:55:25 +0800

    Manual Update 48700

---
+++ 48700.lua
@@ -0,0 +1,29 @@
+-- Generated with Luie @ https://lua.tools/
+-- 48700 - Mount & Blade: Warband
+-- Generated 2026-08-19 10:53:39 UTC
+-- # Depots (Total/DLC/Shared): 7/2/0
+
+-- Main AppID
+addappid(48700, 1, "93270c0945874093a7f733e4f3239c0b033bafe98794b75cfd70314e4867721d")
+
+-- Main Depots
+addappid(48701, 1, "3b2550812603ff57114045fdf119440addee401b021057a937f78c5e7f906e54")
+setManifestid(48701, "6798402207518250693", 1290371872)
+addappid(48702, 1, "724d93c7d316c3d4882c4312660c6e34f0b79ac4463a115e693143be5b413083") -- (macos)
+setManifestid(48702, "955981833923587415", 55956219)
+addappid(48703, 1, "9c724d6901ab92f7243c75445865f0c2d8933d1af6a685a997c41db8b4a90ddb") -- (linux)
+setManifestid(48703, "3759292232020211898", 37527324)
+addappid(48704, 1, "8ddd597bd2616f4abd0c53d7345c2bde0a1b168be8748d4d1fe0a0978d044788") -- (windows)
+setManifestid(48704, "6672793413405812033", 35234204)
+
+-- DLC's (with depot keys)
+-- Mount & Blade: Warband - Napoleonic Wars (AppID: 48705)
+addappid(48705)
+addappid(48705, 1, "829e7e048e3b414bcfbb0054716965b72644f93190099f726256ab9827baff79")
+setManifestid(48705, "4716110400195818096", 1557301679)
+-- Mount & Blade: Warband - Viking Conquest Reforged Edition (AppID: 321300)
+addappid(321300)
+addappid(321300, 1, "fdbd2493ce84b35d21f96cdfb7abec5ead0b2f30c0974365e7834f6e69150eab")
+setManifestid(321300, "7390827904724489385", 2066255954)
+
+-- Missing Depots (We don't have the keys yet lol): 48706
