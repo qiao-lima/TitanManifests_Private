70e760bf5d55c0fc1676efaf4cd95aa88d682148
qiao-lima <qiao-lima@github.com>
2026-08-19 21:47:59 +0800

    Manual Update 3616960

---
+++ 3616960.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3616960 - His Bet, Her Loss
+-- Generated 2026-08-19 13:03:59 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3616960, 1, "5fcc22a9bd001606c61ea61f8cf52157e3d92371382a4bd526845539b10a8a9d")
+
+-- Main Depots
+addappid(3616961, 1, "2df6485d6776355a0cd9250925e826031af208b26eb439bb2ddd40d0f1481ec9")
+setManifestid(3616961, "4889756460017698690", 4166553896)
