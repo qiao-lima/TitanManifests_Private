aaa1229823bad6156c0d865aaa316b8a84383477
qiao-lima <qiao-lima@github.com>
2026-08-19 21:47:54 +0800

    Manual Update 1044720

---
+++ 1044720.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1044720.lua
+--Gamename Farthest Frontier
+addappid(1044720, 1, "80c177162f7bd99385d7da291b7b6c430b54b2e6e470d2a25a26b4344efb5fbe") --Mainappid Farthest Frontier
+addappid(1044721, 1, "2a605d9a2257c347b93162b67aa9844b9719d858501c8df4bcf4982565069ab7") --Main Windows Depot Farthest Frontier
+setManifestid(1044721, "1670354868070364289", 3018682240)
+addappid(4242820, 1, "099f69e5dc8f30a384d34fddd001e0f03518ec242ac346c3d19dd4b84381d694") --Main Windows Depot Farthest Frontier
+setManifestid(4242820, "5628376518574748445", 194985568)
+--Dlcname Farthest Frontier - Cats and Dogs
