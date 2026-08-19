0e70d1e974fe574a5cc0f2273c06aa012de1ac54
qiao-lima <qiao-lima@github.com>
2026-08-19 21:47:56 +0800

    Manual Update 2001120

---
+++ 2001120.lua
@@ -0,0 +1,26 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2001120 - Split Fiction
+-- Generated 2026-08-19 12:16:44 UTC
+-- # Depots (Total/DLC/Shared): 5/2/2
+
+-- Main AppID
+addappid(2001120, 1, "3ad571280aa49958160c0270aa4a31064e7e917b44b0a0b36d13e03bb7929c01")
+
+-- Main Depots
+addappid(2001121, 1, "27f6aac2ce9d80ad4ed8fe3710d306ae108f00ddcf005d47ecf7bfa2a258964c")
+setManifestid(2001121, "4899075162680269256", 94109488417)
+
+-- DLC's (no depot keys required)
+addappid(3050960) -- Split Fiction - Full Game Unlock
+
+-- DLC's (with depot keys)
+-- Split Fiction - Original Soundtrack (AppID: 4110950)
+addappid(4110950)
+addappid(4110951, 1, "0359f50b5814950537d5fb52d353c6262fad8e41c720103435de9208b14d7949")
+setManifestid(4110951, "1497142047146784507", 345960192)
+addappid(4110952, 1, "0db626922af091f02ab545eb507f259869dae6a4787c5a5c0681a20c79e16eec")
+setManifestid(4110952, "2424117993464905013", 1086760841)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
