d9430dc2ed74c1e2a4143e9e8be9ab90f739cac6
qiao-lima <qiao-lima@github.com>
2026-08-19 21:47:58 +0800

    Manual Update 3079210

---
+++ 3079210.lua
@@ -0,0 +1,16 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3079210 - Hell Let Loose: Vietnam
+-- Generated 2026-08-19 12:40:59 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3079210)
+
+-- Main Depots
+addappid(3079211, 1, "f7eb2462eb25f9a05bb91c0a20e77a6f943af632b06af7d4f2e19dd912176e6c")
+setManifestid(3079211, "6903283821410146071", 23311074317)
+
+-- DLC's (no depot keys required)
+addappid(4447970) -- Field Supply: US Army Boat Crew Uniform
+addappid(4447980) -- Field Supply: NVA Boat Crew Uniform
+addappid(4448010) -- Hell Let Loose: Vietnam - 2026 Field Supplies
