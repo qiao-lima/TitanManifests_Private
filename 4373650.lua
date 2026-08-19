78043e7c7208050bac7d4aabbab35dbfdcd9351d
qiao-lima <qiao-lima@github.com>
2026-08-19 21:55:24 +0800

    Manual Update 4373650

---
+++ 4373650.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4373650 - 俺の母ちゃんが考えたゲーム
+-- Generated 2026-08-19 12:48:03 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4373650)
+
+-- Main Depots
+addappid(4373651, 1, "bafa4721f0831b2613fcbe0b285c7060dd509973ef3316a476704085a4975aae")
+setManifestid(4373651, "7782463354079987491", 301664435)
