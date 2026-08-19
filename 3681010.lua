0fdb9de2f2520d4098bf1164e3a8d8c347f1f20d
qiao-lima <qiao-lima@github.com>
2026-08-19 22:26:24 +0800

    Manual Update 3681010

---
+++ 3681010.lua
@@ -0,0 +1,26 @@
+-- Main AppID
+addappid(3681010, 1, "accea0016c6c5df7d031a08df7a460e90673b8da5c7cfb57432bb7e962893897")
+
+-- Main Depots
+addappid(3681011, 1, "1c16eb875f0bd40521cc0ddb2ff4eb5772f0b1976252122abc5f9a09ddaaf995") -- (windows, 64-bit)
+setManifestid(3681011, "5580625622975190763", 135815893236)
+addappid(3681012, 1, "b7bbd78bac448c4d49e1373da25413b0d4cd06a418cd429a4d89dc9da6a9bfe8") -- (windows, 64-bit)
+setManifestid(3681012, "2784576299370497423", 319584)
+addappid(3681013, 1, "b97a4048032dc27c3cf1c6887e6069b0f0d51da2c352c671278b00eb4423de35") -- (windows, 64-bit)
+setManifestid(3681013, "3120992644566989993", 319584)
+
+-- DLC's (no depot keys required)
+addappid(4003880) -- Nioh 3 - Hellfire Equipment Set (Hellfire Warrior Armor set + Hellfire Ninja Attire set)
+addappid(4003890) -- Nioh 3 - Scampuss Furball Netsuke Charm
+addappid(4003900) -- Nioh 3 - Hellfrost Equipment Set (Hellfrost Warrior Armor set + Hellfrost Ninja Attire set)
+addappid(4003910) -- Nioh 3 - Infernal Weapons Set
+addappid(4003920) -- Nioh 3 - Kodama Netsuke Charm
+addappid(4003930) -- Nioh 3 Season Pass
+addappid(4003940) -- Nioh 3 - Chijiko Netsuke Charm
+addappid(4003950) -- Nioh 3 Hell Rising
+addappid(4232000) -- Nioh 3 - Kusanagi Netsuke Charm
+addappid(4232020) -- Nioh 3 - Youngblood Armor
+addappid(4232030) -- Nioh 3 - Lone Wolf's Armor
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
