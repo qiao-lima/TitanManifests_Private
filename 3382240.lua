42a348e0bbed3587578dc076a3c0fa74191ead08
qiao-lima <qiao-lima@github.com>
2026-08-19 21:47:58 +0800

    Manual Update 3382240

---
+++ 3382240.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3382240 - 恋爱猪脚饭
+-- Generated 2026-08-19 13:30:06 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3382240)
+
+-- Main Depots
+addappid(3382241, 1, "e144d7c6d38d7207328d050703d811444a4e4a93e26805e20894e15ea97a9193") -- (windows)
+setManifestid(3382241, "5139412600227438423", 2390076910)
