f6f5d7bdff7bcdf26ade35d4f50e00934ea92bf0
qiao-lima <qiao-lima@github.com>
2026-08-19 21:47:57 +0800

    Manual Update 2813720

---
+++ 2813720.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2813720 - Meow Playground
+-- Generated 2026-08-19 12:38:30 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(2813720)
+
+-- Main Depots
+addappid(2813721, 1, "f14540d41c4d4066d7a1117fe80739478ff0e43db0bc9db8fd3e1b421fbd1121") -- (windows)
+setManifestid(2813721, "8019304832757350292", 280753095)
+addappid(2813722, 1, "87b9b6c8c2b133f0d4582eba0476dfebf12d5cc9e76b6aaee243920ea069e565") -- (linux)
+setManifestid(2813722, "423005600994086079", 283698215)
+addappid(2813723, 1, "173ac40b3f003015d18ebe583520a64a54d4bf0ae75dbba59472b1d9e0c95f83") -- (macos)
+setManifestid(2813723, "8279002100873770928", 266500189)
