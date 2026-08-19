875b0fdf8392846b8f06bb6c65d31331c328f7a0
qiao-lima <qiao-lima@github.com>
2026-08-19 21:47:55 +0800

    Manual Update 1393410

---
+++ 1393410.lua
@@ -0,0 +1,21 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1393410 - Seek Girl V
+-- Generated 2026-08-19 12:23:36 UTC
+-- # Depots (Total/DLC/Shared): 3/2/0
+
+-- Main AppID
+addappid(1393410)
+
+-- Main Depots
+addappid(1393411, 1, "0d077d2e75e5d02614893862de4917255e66ce933eeb55b4ed2b8afbd946fb53") -- (windows)
+setManifestid(1393411, "6114609519500065396", 295678410)
+
+-- DLC's (with depot keys)
+-- Seek Girl V - Patch (AppID: 1407020)
+addappid(1407020)
+addappid(1407020, 1, "84c55c4b22fb43893e3170d737645b03abf17ff6de85d9044b1435f02f54235c") -- (windows)
+setManifestid(1407020, "5460809327046144891", 2239172205)
+-- Seek Girl V ：Character voice (AppID: 1424920)
+addappid(1424920)
+addappid(1424920, 1, "06aeb88bfcd05e85df747149507782d5293c9fdf035265a58a16a38628c0c0d5") -- (windows)
+setManifestid(1424920, "3243461963985876825", 5)
