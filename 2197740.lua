2035e5adf3bf98c661892d1531fa079b6f20d249
qiao-lima <qiao-lima@github.com>
2026-08-21 03:03:14 +0800

    Manual Update 2197740

---
+++ 2197740.lua
@@ -0,0 +1,35 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2197740 - Goblin Dungeons
+-- Generated 2026-08-20 12:51:48 UTC
+-- # Depots (Total/DLC/Shared): 6/4/0
+
+-- Main AppID
+addappid(2197740)
+
+-- Main Depots
+addappid(2197741, 1, "2a13ee198f6abb4a8cf41520c7d02205057bf399acdcd6630f2b00284c5d170b") -- (windows)
+setManifestid(2197741, "1305993888125818820", 13778730456)
+
+-- DLC's (no depot keys required)
+addappid(4075350) -- Goblin Dungeons: Flame Mage Set
+addappid(4075410) -- Goblin Dungeons: Frost Mage Set
+addappid(4075480) -- Goblin Dungeons: Storm Mage Set
+addappid(4075580) -- Goblin Dungeons: Crimson Mage Set
+addappid(4080560) -- Goblin Dungeons: Bunny Set
+addappid(4268020) -- Goblin Dungeons: New Year Set
+
+-- DLC's (with depot keys)
+-- Goblin Dungeons: Digital Artbook (AppID: 4064970)
+addappid(4064970)
+addappid(4064970, 1, "969bf496f66a84d39145bbf0d2ed9a43f3f7a236e93b9543d31a6e070b2e3ab4")
+setManifestid(4064970, "4026105636953998218", 45138720)
+-- Goblin Dungeons: Wallpapers Pack (AppID: 4065240)
+addappid(4065240)
+addappid(4065240, 1, "2fe9f125c574679afc0055e246422ecefb45346a6ed969a4260861ff9361d8b0")
+setManifestid(4065240, "120305218524074971", 59094473)
+-- Goblin Dungeons: Ellie Cosplay (AppID: 4065570)
+addappid(4065570)
+addappid(4065570, 1, "6d7e02c6e57bdfd2de7d48ba5511b579de04aeb18287afebc8ffca848ee22bed")
+setManifestid(4065570, "4716990142233372669", 610813346)
+
+-- Missing Depots (We don't have the keys yet lol): 2197742, 4259400
