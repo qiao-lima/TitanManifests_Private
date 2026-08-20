49ef0d74f347ef28bfc32ce4f634e2a73367b553
qiao-lima <qiao-lima@github.com>
2026-08-21 03:03:44 +0800

    Manual Update 4001890

---
+++ 4001890.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4001890 - How to Fish
+-- Generated 2026-08-20 18:43:38 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4001890)
+
+-- Main Depots
+addappid(4001891, 1, "b5c8ad37740a8db6d370a6934bf203cfaeb3343003e6277ffc96c63f4df2b7ca")
+setManifestid(4001891, "965517989535574899", 641281676)
