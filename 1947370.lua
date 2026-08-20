04d091454f85a5210a5a5069ba6529a027e6fc61
qiao-lima <qiao-lima@github.com>
2026-08-21 03:03:13 +0800

    Manual Update 1947370

---
+++ 1947370.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1947370.lua
+--Gamename Siege Engine Arena Playtest
+addappid(1947370) --Mainappid Siege Engine Arena Playtest
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Main Windows Depot Siege Engine Arena Playtest
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Main Windows Depot Siege Engine Arena Playtest
