5f12686815ab6981f857adb583a5e81761c26dcd
qiao-lima <qiao-lima@github.com>
2026-08-20 13:47:39 +0800

    Manual Update 3837390

---
+++ 3837390.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3837390 - Pikabuu: STOP!
+-- Generated 2026-08-20 05:02:07 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3837390)
+
+-- Main Depots
+addappid(3837391, 1, "2bc25a431f2cd1c74d2d4f6f7dc7c022088463dc8861adf562ced84b9980cbb9") -- (windows, 64-bit)
+setManifestid(3837391, "5512825149342628321", 155631674)
