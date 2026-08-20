8510df5f96a671526fd38227e334bba1e9a7de0b
qiao-lima <qiao-lima@github.com>
2026-08-21 03:03:14 +0800

    Manual Update 2250500

---
+++ 2250500.lua
@@ -0,0 +1,20 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2250500 - Rhythia
+-- Generated 2026-08-20 18:44:26 UTC
+-- # Depots (Total/DLC/Shared): 4/0/1
+
+-- Main AppID
+addappid(2250500, 1, "4d5dec3d68a5e565c694bac1e018660dcc211d42871d79ec6b4f252faefab282")
+
+-- Main Depots
+addappid(2250502, 1, "abcdacfdd298bd11058c6e08fa32e0ff0d398467ffb9d99520a9a2fd04ac0bb9") -- English (windows)
+setManifestid(2250502, "392948253838846846", 307275931)
+addappid(2250503, 1, "363dc89a44c1617dc26ee81bf6b3ed6b0e6789efb90ade4cb1cb387110da3f54") -- (linux, 64-bit)
+setManifestid(2250503, "3522328300749287046", 326317568)
+addappid(2250504, 1, "e93ac2cd5e40811b05f0e044fb3f84a31ce10e266ce9c90731ed9ea96872710d") -- (macos)
+setManifestid(2250504, "4690516888435425527", 577382416)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+
+-- Token-locked DLCs (couldn't read depots): 4018960
