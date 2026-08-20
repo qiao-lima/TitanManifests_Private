282350003d7e854792db6c8029df581dd5e1a933
qiao-lima <qiao-lima@github.com>
2026-08-21 03:03:15 +0800

    Manual Update 2702630

---
+++ 2702630.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2702630 - Chernobyl: Simulator
+-- Generated 2026-08-20 18:40:01 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(2702630)
+
+-- Main Depots
+addappid(2702631, 1, "3a2e1aada970449c4090040fd312879d9361430e24dd54a1c6d65af8538fc268") -- (windows)
+setManifestid(2702631, "4656024891625684836", 2153208548)
+addappid(2702632, 1, "8c0853ba9aa7aa350afd1ac42aba6a872924315a6d80ba3df983417181ae67f0") -- (macos)
+setManifestid(2702632, "8513625277039883857", 2415273347)
