ef9b71b57b8526dec8d95af1d152c8f0c6e5cb7a
qiao-lima <qiao-lima@github.com>
2026-08-21 03:03:17 +0800

    Manual Update 3180830

---
+++ 3180830.lua
@@ -0,0 +1,12 @@
+-- 3180830's Lua and Manifest Created by Hubcap Manifest
+-- Circuit Stance
+-- Created: August 20, 2026 at 02:51:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3180830) -- Circuit Stance
+-- MAIN APP DEPOTS
+addappid(3180831, 1, "3ff1ae286e2c110859274b313908223a0d0c6c78f0fb3678b9d22fe7aa9d3a94") -- Depot 3180831
+setManifestid(3180831, "6407684726432636863", 1229281842)
