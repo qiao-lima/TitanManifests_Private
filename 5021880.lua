1e83700772879e7604043b0656f01f3071a08294
qiao-lima <qiao-lima@github.com>
2026-08-21 03:03:47 +0800

    Manual Update 5021880

---
+++ 5021880.lua
@@ -0,0 +1,11 @@
+
+
+-- MAIN APPLICATION
+addappid(5021880) -- Alice in Wonderland: The Crown of Wishes
+-- MAIN APP DEPOTS
+addappid(5021881, 1, "da8e1ad456af5f615b5b5871a8344752521effd4401dececdb4fecce9b3d70e0") -- Depot 5021881
+
+-- DLCS WITH DEDICATED DEPOTS
+-- Alice in Wonderland The Crown of Wishes - Supporter Pack (AppID: 5111000)
+addappid(5111000)
+addappid(5111000, 1, "080b3d597a8fd5f288fa808bf39ad7ef20df5cca44f609cc7d399f25974f0d26") -- Alice in Wonderland The Crown of Wishes - Supporter Pack - Depot 5111000
