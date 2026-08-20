52dfc30932fe91f96c828a33f1b56b03cd72aee9
qiao-lima <qiao-lima@github.com>
2026-08-20 13:47:38 +0800

    Manual Update 2364580

---
+++ 2364580.lua
@@ -0,0 +1,21 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2364580 - Titanium Court
+-- Generated 2026-08-20 05:25:57 UTC
+-- # Depots (Total/DLC/Shared): 4/2/0
+
+-- Main AppID
+addappid(2364580, 1, "3db0d579067ac552cfe6a1a9d03f7a93deae0b4c0efe2322b10d89c521aefb4c")
+
+-- Main Depots
+addappid(2364581, 1, "32986d1befc5db2bf68171fd48a9b431e1bf8fdf72bffbfb3bb65a908616baa4") -- (windows)
+setManifestid(2364581, "348683312422094668", 543401680)
+addappid(2364582, 1, "a0b4bbde9428c87000b2c59fa9bc4ea7f1af9d47908aaa31b2f0be2c2618c84c") -- (macos)
+setManifestid(2364582, "1176928738981529846", 600085902)
+
+-- DLC's (with depot keys)
+-- Titanium Court Soundtrack (AppID: 4438810)
+addappid(4438810)
+addappid(4438811, 1, "9b9dbf03f844040e9c3fbc1748ef8f98e069a6ca905eee62833f47a9adfccd51")
+setManifestid(4438811, "7918236414889146597", 135512173)
+addappid(4438812, 1, "4967141eb0c36caf10873186d87a0b7fce439dcd731498c3fa17e25151356ab2")
+setManifestid(4438812, "8437916569966103053", 1276814252)
