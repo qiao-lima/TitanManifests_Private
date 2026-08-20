d1ba8abb91a0c6242223b5222c02343b084deee8
qiao-lima <qiao-lima@github.com>
2026-08-21 03:03:16 +0800

    Manual Update 2758360

---
+++ 2758360.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2758360.lua
+--Gamename Scorpion's Curse
+addappid(2758360) --Mainappid Scorpion's Curse
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Main Windows Depot Scorpion's Curse
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Main Windows Depot Scorpion's Curse
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Main Windows Depot Scorpion's Curse
