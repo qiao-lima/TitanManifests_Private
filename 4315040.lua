e60e4bb36a12a403f7393c875c612782e1008fb9
qiao-lima <qiao-lima@github.com>
2026-08-20 13:47:41 +0800

    Manual Update 4315040

---
+++ 4315040.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4315040 - Hearth and Hamlet
+-- Generated 2026-08-20 05:21:08 UTC
+-- # Depots (Total/DLC/Shared): 4/2/0
+
+-- Main AppID
+addappid(4315040)
+
+-- Main Depots
+addappid(4315041, 1, "9a026c36ec2e999340ac2f8abfb6b3618b8ab5222b430be7fad4f6ffd3111ab0")
+setManifestid(4315041, "5256410707204114419", 932152968)
+
+-- Missing Depots (We don't have the keys yet lol): 4315042, 4951752, 4951753
