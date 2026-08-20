13e55840850aff6a89aff43e0be0902d560dcbaa
qiao-lima <qiao-lima@github.com>
2026-08-21 03:03:45 +0800

    Manual Update 4761320

---
+++ 4761320.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4761320 - MEGABOOBS 🔞
+-- Generated 2026-08-20 18:46:50 UTC
+-- # Depots (Total/DLC/Shared): 2/1/0
+
+-- Main AppID
+addappid(4761320)
+
+-- Main Depots
+addappid(4761321, 1, "9455b0a87787b06ebf5a75e6bf8022728be742673dd52fe8c8cb672e89f2cd4f")
+setManifestid(4761321, "8489621080874994833", 3619318499)
+
+-- Missing Depots (We don't have the keys yet lol): 4861160
