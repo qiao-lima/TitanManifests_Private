5669ef214ae92a1e4131f38232b983a149141943
qiao-lima <qiao-lima@github.com>
2026-08-21 13:52:58 +0800

    Manual Update 3998560

---
+++ 3998560.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3998560 - Trainz Simulator: World Tour
+-- Generated 2026-08-20 20:44:45 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(3998560)
+
+-- Main Depots
+addappid(3998562, 1, "0e396611125224ec79529e65eeb336e383dd3256faddefc6b1bbac94cd3866dc") -- (windows, 64-bit)
+setManifestid(3998562, "3529106730671090253", 20537184585)
+addappid(3998563, 1, "3c869edfdfee9e3a9057461286e58e77db0dddda159f95eb1432df367f18226e") -- (macos, 64-bit)
+setManifestid(3998563, "460980826891830194", 20348729024)
