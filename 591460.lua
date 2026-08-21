d4c1d0b98329768859a5d2f0732c19b53c405ec8
qiao-lima <qiao-lima@github.com>
2026-08-21 13:52:59 +0800

    Manual Update 591460

---
+++ 591460.lua
@@ -0,0 +1,19 @@
+-- Generated with Luie @ https://lua.tools/
+-- 591460 - Parkasaurus
+-- Generated 2026-08-21 05:30:53 UTC
+-- # Depots (Total/DLC/Shared): 4/2/0
+
+-- Main AppID
+addappid(591460, 1, "8327035f2fa594f225888a418daed391a3c2a818dc9a532272b1d28efc6f7fa9")
+
+-- Main Depots
+addappid(591461, 1, "fecec5a1bb11beaed5f93f92404793b769fa5913d436ad4a3da8a7868a52a271") -- (windows)
+setManifestid(591461, "4407419122187998362", 2744168129)
+addappid(591462, 1, "cc70030d3dc4a120b24fbe251e9f825e0aa849652486a71cde751495677accd6") -- (windows)
+setManifestid(591462, "7922292456972073123", 2442210598)
+
+-- DLC's (no depot keys required)
+addappid(1931130) -- Parkasaurus - Sea Monsters
+addappid(2435170) -- Parkasaurus - Prehistoric Wonders
+
+-- Missing Depots (We don't have the keys yet lol): 2388591, 2388593
