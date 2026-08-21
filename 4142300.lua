64816ffd80a83ce6dbfaac4ea4cf5f0bf6901287
qiao-lima <qiao-lima@github.com>
2026-08-21 14:33:35 +0800

    Manual Update 4142300

---
+++ 4142300.lua
@@ -0,0 +1,20 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4142300 - MELFIAS －Azure and Crimson Verge－
+-- Generated 2026-08-21 06:29:34 UTC
+-- # Depots (Total/DLC/Shared): 5/0/0
+
+-- Main AppID
+addappid(4142300, 1, "f7f4ebc7ce06a02ab4fdd983c0731bdae887aff08ed4ccae1301615c45accc05")
+addtoken(4142300, "7007618772408407221")
+
+-- Main Depots
+addappid(4142301, 1, "dbf203d5c5db5260da136e3085b5acbfb5bedb9a52c8697c51e856c4ef3e8c52") -- Tchinese (windows)
+setManifestid(4142301, "1610038748907839082", 4948308734)
+addappid(4142302, 1, "4d2c85c0df450c9f4369a2855b4357426b4f35d5668d45216ef3d8e7be34e345") -- Schinese (windows)
+setManifestid(4142302, "7648876242197790705", 4972281454)
+addappid(4142303, 1, "1fcd355e9cb11befe13afa2e90f6d75aeb7bdf8f5f564e6fdd2e2bfad47abe58") -- Japanese (windows)
+setManifestid(4142303, "6835136805161757119", 5400182986)
+addappid(4142304, 1, "ae0cc3c0f493bc06ea564b91b8575acd88c589ee6c0130097a265647babfa528") -- Koreana (windows)
+setManifestid(4142304, "6478213062388270180", 4952247268)
+addappid(4142305, 1, "ddc688401449041692c6f876d430d4a5e765e80cbb1b00631cb076846150d5b0") -- English (windows)
+setManifestid(4142305, "9209805545501539405", 5006281429)
