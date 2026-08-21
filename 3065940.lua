b010c3aa00527a10d0da01eb162bc3836a8d9302
qiao-lima <qiao-lima@github.com>
2026-08-21 13:52:56 +0800

    Manual Update 3065940

---
+++ 3065940.lua
@@ -0,0 +1,26 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3065940 - Gallipoli
+-- Generated 2026-08-21 04:15:43 UTC
+-- # Depots (Total/DLC/Shared): 5/2/2
+
+-- Main AppID
+addappid(3065940)
+
+-- Main Depots
+addappid(3065941, 1, "6b014e239ca878274757cee72f94484f4b88eae13426fad855897d6b46e339b1") -- (windows, 64-bit)
+setManifestid(3065941, "2367264261538219421", 18998711147)
+
+-- DLC's (no depot keys required)
+addappid(4202450) -- Gallipoli - Sunburnt Pack
+addappid(4202460) -- Gallipoli - Light Horse Pack
+addappid(4202470) -- Gallipoli - Besieged Pack
+addappid(4202490) -- Gallipoli - Conscript Pack
+addappid(4202500) -- Gallipoli - Ordered to Die Pack
+addappid(4584010) -- Gallipoli - First Expedition
+addappid(4584020) -- Gallipoli - Second Expedition
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 4768091, 4768092
