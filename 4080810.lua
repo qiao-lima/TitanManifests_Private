da1d53f43b02ed744e56d9e399d970057f9fb889
qiao-lima <qiao-lima@github.com>
2026-08-21 13:52:58 +0800

    Manual Update 4080810

---
+++ 4080810.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4080810 - Construction Simulator 4 - Pocket Edition
+-- Generated 2026-08-21 05:32:03 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4080810, 1, "c5c0a16e3ab4e4e3e9e9c71f3e6ed4f8c7571230f65adcbbd2fb8a2cd736e786")
+
+-- Main Depots
+addappid(4080811, 1, "6938a42ea94ee3567f4e3c624e0222619cc13e589dc8fa5fe8b9f409954a4f07") -- (64-bit)
+setManifestid(4080811, "3639582197581505571", 3568533111)
