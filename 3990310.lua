a92efb6fe91141e933ba8dd70b7248ec96de8b72
qiao-lima <qiao-lima@github.com>
2026-08-22 11:09:44 +0800

    Manual Update 3990310

---
+++ 3990310.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3990310 - Shino's Insatiable Pursuit of Knowledge
+-- Generated 2026-08-22 01:23:47 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(3990310, 1, "b1f832c43c2bf3cb171f25616b1ebc23844e2125ad38fb0982e6505269c7d048")
+
+-- Main Depots
+addappid(3990311, 1, "b92aa118a7aa9276eb78c88e50362045bfa64832d248400d1c8f54acd12fb857")
+setManifestid(3990311, "3550967779182202092", 1496024150)
+addappid(3990312, 1, "7dd5380ab1b0b7e2a2ed56aea147bd3836ba2e83f4bb4e15a8d0fa1a393d2108") -- Schinese
+setManifestid(3990312, "5391665259545610803", 1329006229)
+addappid(3990313, 1, "d736f38dc1ba10c51d8486a620731b297ab42bc5147fd12b6d4259ab313224d3") -- Japanese
+setManifestid(3990313, "1691928845363549641", 1326105412)
