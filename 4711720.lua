5af278958e5a328b14ff56d9d1740d85604a98aa
qiao-lima <qiao-lima@github.com>
2026-08-22 11:14:38 +0800

    Manual Update 4711720

---
+++ 4711720.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4711720.lua
+--Gamename Steam Store
+addappid(4711720) --Mainappid Steam Store
+addtoken(4711720, "14235145898889595086")
