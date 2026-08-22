9ceac473310e9bb3161ef11c10a5c62bd9872c47
qiao-lima <qiao-lima@github.com>
2026-08-22 11:07:46 +0800

    Manual Update 3376690

---
+++ 3376690.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3376690 - THE HOUSE OF THE DEAD 2: Remake
+-- Generated 2026-08-22 01:06:23 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3376690)
+
+-- Main Depots
+addappid(3376691, 1, "bb300f0659435ce2241c40d01d149835d706a1d843d04fbe39dd1c91037433ea")
+setManifestid(3376691, "5584701060621771418", 8801458736)
