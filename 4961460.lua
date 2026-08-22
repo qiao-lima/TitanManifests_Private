ca41ba93f2683fa522441e31ba538aa007679e40
qiao-lima <qiao-lima@github.com>
2026-08-22 11:14:40 +0800

    Manual Update 4961460

---
+++ 4961460.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4961460 - MarginCall
+-- Generated 2026-08-22 02:53:39 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4961460)
+
+-- Main Depots
+addappid(4961461, 1, "438de88fd1189cf8417133265a0668b397316dd75beaa625ea524f4f8e9bd0c1") -- (windows, 64-bit)
+setManifestid(4961461, "2075046049047902178", 680903467)
