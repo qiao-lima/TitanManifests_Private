faed064cd2cd14b8e8496a67ef11f85110ca27d7
qiao-lima <qiao-lima@github.com>
2026-08-23 05:22:24 +0800

    Manual Update 2676780

---
+++ 2676780.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2676780.lua
+--Gamename Ground Zero Hero Demo
+addappid(2676780) --Mainappid Ground Zero Hero Demo
+addappid(2676781, 1, "bc4087c308b7d253e18a2df569c4ae2479b298b03ceac6ffb1ac568ef69d52d5") --Main Windows Depot Ground Zero Hero Demo
+setManifestid(2676781, "4063022060061937571", 174813248)
