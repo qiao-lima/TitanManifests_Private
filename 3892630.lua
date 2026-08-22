8dd54d6a921c48482efa5bd6a95a584235cfc5a1
qiao-lima <qiao-lima@github.com>
2026-08-23 05:22:35 +0800

    Manual Update 3892630

---
+++ 3892630.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3892630.lua
+--Gamename Brothel Master
+addappid(3892630) --Mainappid Brothel Master
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Main Windows Depot Brothel Master
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Main Windows Depot Brothel Master
+addappid(3892631, 1, "72d3255179fc2c3ab11c64f0d1fe242222e82649edc57cb4a7314038d127c1b6") --Main Windows Depot Brothel Master
+setManifestid(3892631, "1693085976868216130", 2835346032)
