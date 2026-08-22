880de7b36234071952ac00441a6da698e1b7d713
qiao-lima <qiao-lima@github.com>
2026-08-23 05:22:25 +0800

    Manual Update 3308200

---
+++ 3308200.lua
@@ -0,0 +1,19 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3308200 - Tombwater
+-- Generated 2026-08-22 05:54:05 UTC
+-- # Depots (Total/DLC/Shared): 3/2/0
+
+-- Main AppID
+addappid(3308200, 1, "c6f431e496ab0219740dd2561dfc270feebe7df66dc2204514c01b64fb32831f")
+
+-- Main Depots
+addappid(3308201, 1, "5d0eef7ab9ce5801114ebe4042e5537d86e34bc6c7ce2f869215a653e4d9a8ed")
+setManifestid(3308201, "6573352595559219921", 573651574)
+
+-- DLC's (with depot keys)
+-- Gunslinger's Guide to Tombwater (AppID: 4063620)
+addappid(4063620)
+addappid(4063620, 1, "88c443e2afd716a5e34aef7e7c10fb32e44ac254dbb680f011e27e3655371710")
+setManifestid(4063620, "4803679840509693811", 117474774)
+
+-- Missing Depots (We don't have the keys yet lol): 3859411
