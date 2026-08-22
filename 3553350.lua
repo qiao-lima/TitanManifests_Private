e9ccc7690e59c9dd5e02ddb0da00be8d8988ebf4
qiao-lima <qiao-lima@github.com>
2026-08-23 05:22:34 +0800

    Manual Update 3553350

---
+++ 3553350.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3553350.lua
+--Gamename The Hilltop Funeral
+addappid(3553350) --Mainappid The Hilltop Funeral
+addappid(3553351, 1, "ea46558ea579ff60b1fbdf3b05f7aabee3f46900d35f09301c404cb420708eb0") --Main Windows Depot The Hilltop Funeral
+setManifestid(3553351, "3033303161748979306", 654410432)
