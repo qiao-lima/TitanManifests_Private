1fc63416f0c130006dc14746d1d161cb48e61247
qiao-lima <qiao-lima@github.com>
2026-08-22 11:07:45 +0800

    Manual Update 297550

---
+++ 297550.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 297550.lua
+--Gamename No Time To Explain OST
+addappid(297550) --Mainappid No Time To Explain OST
+addappid(297551, 1, "{") --Main Windows Depot No Time To Explain OST
+setManifestid(297551, "8325152121921717422", 72487088)
