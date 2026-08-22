7af2fc66740d7921aebd76da2ca0c71a98aaee53
qiao-lima <qiao-lima@github.com>
2026-08-23 05:22:48 +0800

    Manual Update 4415740

---
+++ 4415740.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4415740 - The Pale Mimic
+-- Generated 2026-08-22 20:06:52 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4415740)
+
+-- Main Depots
+addappid(4415741, 1, "76c25e70e520284e31a9b9d74114e03c8950a8a63fed6759afd839472c753d22")
+setManifestid(4415741, "1198956021522064325", 3400131012)
