ac3773c136fc56ac72ec464cc62f133c1f574310
qiao-lima <qiao-lima@github.com>
2026-08-22 11:09:47 +0800

    Manual Update 4198660

---
+++ 4198660.lua
@@ -0,0 +1,10 @@
+-- MAIN APPLICATION
+addappid(4198660) -- Demons' Timeline
+-- MAIN APP DEPOTS
+addappid(4198662, 1, "953979cdd1c1e12d075c2f885c200c0c011bda867589a503e287e756b3271eab") -- Depot 4198662
+--setManifestid(4198662, "243468262344154300", 1507937741)
+-- DLCS WITH DEDICATED DEPOTS
+-- Digital Artbook -Demons Timeline- (AppID: 4511430)
+addappid(4511430)
+addappid(4511430, 1, "051cbadf9a03683b4240a2401423bab5d71c0ce8ff69b8c3f36524aba45ee0c7") -- Digital Artbook -Demons Timeline- - Depot 4511430
+--setManifestid(4511430, "1044057516195995481", 306247144)
