7c799c073eaa4ca52f18228510a9f4d156d219fb
qiao-lima <qiao-lima@github.com>
2026-08-22 11:07:42 +0800

    Manual Update 1456720

---
+++ 1456720.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1456720 - Into the Abyss
+-- Generated 2026-08-22 02:48:46 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(1456720)
+
+-- Main Depots
+addappid(1456721, 1, "b3aacdc9cc8195c5900455b0117fee8981c4e4c14831ca0a474e3c8dd86fb5d8")
+setManifestid(1456721, "4674423780440301751", 555018476)
