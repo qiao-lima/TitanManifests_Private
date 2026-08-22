74775b3b5753c8881c1fd5f007809199865f6df1
qiao-lima <qiao-lima@github.com>
2026-08-22 11:07:42 +0800

    Manual Update 1340810

---
+++ 1340810.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1340810 - The Night Fisherman
+-- Generated 2026-08-22 02:45:18 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(1340810)
+
+-- Main Depots
+addappid(1340811, 1, "db9be1b7d527ce3ec57cf6fe16ffabcd854f8466e38047edcb2bf65d031ea6eb") -- (windows)
+setManifestid(1340811, "9153101558697464467", 305578851)
+addappid(1340812, 1, "24d8fe8984d491d7f63efcce1c59667af17016e81640a18297aa0af82c142265") -- (macos)
+setManifestid(1340812, "6948644217780149837", 308453299)
