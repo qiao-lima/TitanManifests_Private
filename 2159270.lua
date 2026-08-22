9894e41eeb43db09e704d56e02d563f0ad1c6fb4
qiao-lima <qiao-lima@github.com>
2026-08-23 05:22:22 +0800

    Manual Update 2159270

---
+++ 2159270.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2159270.lua
+--Gamename Kingdoms of the Dump
+addappid(2159270) --Mainappid Kingdoms of the Dump
+addappid(2159271, 1, "564d1d0697f374127df3426d28e89253c7d923e5f7693064d4c28cae5f287399") --Main Windows Depot Kingdoms of the Dump
+setManifestid(2159271, "5822654115663792223", 717614720)
+addappid(2159272, 1, "4e7ea532bd4bc34f3e4f8ce6fde6c6e998f3a9db8d7def5453029175394bf174") --Main Macos Language Depot English Kingdoms of the Dump
+setManifestid(2159272, "3909535367180911728", 718332896)
+addappid(2159273, 1, "301116ff139edaf0d4de7c234726423e0056b57b24d4c87f703fc5bb90838d39") --Main Linux Language Depot English Kingdoms of the Dump
+setManifestid(2159273, "3978760835998342598", 714874240)
