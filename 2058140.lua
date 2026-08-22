82d45aeeea88ecdd6bdff80825c907103e85729f
qiao-lima <qiao-lima@github.com>
2026-08-23 05:22:22 +0800

    Manual Update 2058140

---
+++ 2058140.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2058140.lua
+--Gamename Henry Halfhead
+addappid(2058140) --Mainappid Henry Halfhead
+addappid(2058141, 1, "9927eff97e443d137311265471da5e38ce4e8d0d854a2a2946e1a6693ed67d3d") --Main Windows Depot Henry Halfhead
+setManifestid(2058141, "1941868667601020719", 381131984)
+--Dlcname Henry Halfhead Soundtrack
+addappid(3942680) --Dlcname Henry Halfhead Soundtrack
+addappid(3942681, 1, "667d13a6de0d03103c63efe77829855f7d9d7e232a054810640e15356a141a71") --Dlc Windows Depot Henry Halfhead Soundtrack
+setManifestid(3942681, "850009514868638841", 257051424)
+addappid(3942682, 1, "71ed24f3aa1425cef0b39de5458c78a48b2fded710f05a8e4ef5f14f59b50ed6") --Dlc Windows Depot Henry Halfhead Soundtrack
+setManifestid(3942682, "5368692219005839661", 3336271824)
