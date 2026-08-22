585c3d2d4037bda38e363911f7832b571b6f69b3
qiao-lima <qiao-lima@github.com>
2026-08-22 11:07:43 +0800

    Manual Update 1737460

---
+++ 1737460.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1737460 - Into The Abyss
+-- Generated 2026-08-22 02:48:15 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(1737460)
+
+-- Main Depots
+addappid(1737461, 1, "78b07aaa17fc7b09738e4d1baa26127bdcf4400f8734bd035b3764f7720190f7") -- English (windows)
+setManifestid(1737461, "5256597463081385196", 24571562762)
+
+-- Missing Depots (We don't have the keys yet lol): 1737462
