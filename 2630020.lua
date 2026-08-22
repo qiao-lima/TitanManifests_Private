7a8f18ad169704be4fa1b53e32c1d286de19399c
qiao-lima <qiao-lima@github.com>
2026-08-23 05:22:24 +0800

    Manual Update 2630020

---
+++ 2630020.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2630020.lua
+--Gamename Nightmare Knight ~Sacred Maiden & Fallen Magic~
+addappid(2630020, 1, "14239b8ce0d603e0206f5fc578e6f2d437e1d6ba720744659b08f08d3abdfb2d") --Mainappid Nightmare Knight ~Sacred Maiden & Fallen Magic~
+addappid(2630021, 1, "fce8a249fca8f7ca90a1c07b59461501e7d9e097968ddff0cbd9e633cb0a485d") --Main Windows Language Depot Japanese Nightmare Knight ~Sacred Maiden & Fallen Magic~
+setManifestid(2630021, "4646700021258551524", 402832672)
+addappid(2630022, 1, "5bb927d646015c0d347b2f7ff8e549c13979f4a10412e760645843d7b4c5f872") --Main Windows Language Depot Schinese Nightmare Knight ~Sacred Maiden & Fallen Magic~
+setManifestid(2630022, "8000762393846336918", 404030336)
+addappid(2630023, 1, "530868cd7a8978ee42af20513b9b61d82d1cca514f1219e3b864ef4a68d27545") --Main Windows Language Depot English Nightmare Knight ~Sacred Maiden & Fallen Magic~
+setManifestid(2630023, "2708424516372971753", 404067632)
