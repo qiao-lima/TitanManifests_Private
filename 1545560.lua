7d25e9e160cc2fd7cd568e0d705cfb9ba1290b24
qiao-lima <qiao-lima@github.com>
2026-08-23 05:39:29 +0800

    Manual Update 1545560

---
+++ 1545560.lua
@@ -0,0 +1,18 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1545560.lua
+--Gamename Shadow Gambit: The Cursed Crew
+addappid(1545560, 1, "24256ad951709f249d26693ed16f0a7363954d0aa55ce6d23a961f85debdc565") --Mainappid Shadow Gambit: The Cursed Crew
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Main Windows Depot Shadow Gambit: The Cursed Crew
+addappid(1545561, 1, "c9d50c94293025191fbb84ba06e8c755d529767197b2b642bc8a922a8a20b9a4") --Main Windows Depot Shadow Gambit: The Cursed Crew
+setManifestid(1545561, "6688794995992364269", 14848322640)
+addappid(2505850, 1, "968e0fc74bd31aa95a7542bfdd737ecebe2abc837f0e06a266eb4fff4036073b") --Main Windows Depot Shadow Gambit: The Cursed Crew
+setManifestid(2505850, "4895193766798963042", 242904176)
+--Dlcname Shadow Gambit: The Cursed Crew - Soundtrack
+addappid(2504700) --Dlcname Shadow Gambit: The Cursed Crew - Soundtrack
+addappid(2504701, 1, "2d00863f4eeb86e7e600cf5c1ab6f55b8a985a2871668776ff89799d8dff2f4c") --Dlc Windows Depot Shadow Gambit: The Cursed Crew - Soundtrack
+setManifestid(2504701, "7618320214578362538", 396351616)
+addappid(2504702, 1, "a7b1600ceb42bead1df5b8531f5344f2a515083a0d8b3ad680c7cfc112d07779") --Dlc Windows Depot Shadow Gambit: The Cursed Crew - Soundtrack
+setManifestid(2504702, "1591816052719440816", 1976274560)
+--Dlcname Shadow Gambit: The Cursed Crew - Artbook & Strategy Guide
+addappid(2304810) --Dlcname Shadow Gambit: Yuki's Wish
+addappid(2576620) --Dlcname Shadow Gambit: Zagan's Ritual
