ed9ca2c08841a7870b412a5d166a78ea50a86db0
qiao-lima <qiao-lima@github.com>
2026-08-22 11:09:47 +0800

    Manual Update 4133830

---
+++ 4133830.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4133830 - Tasty Planet: Final Bite
+-- Generated 2026-08-22 02:35:25 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(4133830)
+
+-- Main Depots
+addappid(4133831, 1, "0f85508f73deb6c226deedd590858715cdffdb11d95c9bf66cadf2ea3f003282") -- (windows)
+setManifestid(4133831, "7561006672817814143", 291326672)
+addappid(4133832, 1, "01daf039c9d53af0d46e08522589f1a8d1c654c70843dcb509d57ed071f61aae") -- (macos)
+setManifestid(4133832, "7734295894448730020", 306277322)
