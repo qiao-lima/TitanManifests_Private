b93f57bee0b5acc38f3221215bbd3f623de391ac
qiao-lima <qiao-lima@github.com>
2026-08-23 05:22:38 +0800

    Manual Update 4147190

---
+++ 4147190.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4147190.lua
+--Gamename Risky Business
+addappid(4147190, 1, "5245c704f048eda1bc359dadb3bd64957af426e0be1908c7b3dddad891de0693") --Mainappid Risky Business
+addappid(4147191, 1, "9f13124402a28564aec502fc1d5c5caeb185942dc490bb848d8986e20175fb7b") --Main Windows Depot Risky Business
+setManifestid(4147191, "284753255231238557", 857424528)
+--Missing Dlcs
+--Dlcname Risky Business - Soundtrack
