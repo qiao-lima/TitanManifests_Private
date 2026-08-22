4119635c61f5d78c7b72de2f17e96e3fcfcb0448
qiao-lima <qiao-lima@github.com>
2026-08-23 05:22:26 +0800

    Manual Update 3333670

---
+++ 3333670.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3333670.lua
+--Gamename Mr. President
+addappid(3333670, 1, "42abb352263c5416facaa335e0fd2cb0fa2134072c0d27f204df5dc8ca779584") --Mainappid Mr. President
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Main Windows Depot Mr. President
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Main Windows Depot Mr. President
+addappid(3333671, 1, "bc04a177b8c7462f0232b8314423877f88bece07769f168cea7d541d413b871f") --Main Windows Depot Mr. President
+setManifestid(3333671, "410674696022833899", 387955344)
+addappid(3333673, 1, "78515b46a28512f724ea4699f44f1f30b6280c7425375e75bb8dfe01c409b696") --Main Macos Language Depot English Mr. President
+setManifestid(3333673, "7995872247962622017", 411101728)
