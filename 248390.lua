8f01579401ba62f5ab260f65e345552a0216629b
qiao-lima <qiao-lima@github.com>
2026-08-23 05:22:24 +0800

    Manual Update 248390

---
+++ 248390.lua
@@ -0,0 +1,21 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 248390.lua
+--Gamename Craft The World
+addappid(248390, 1, "e38c422a6e0aef0a80a2319e980462cd3fab0a4a9cfeb279a430abcc37b5c55e") --Mainappid Craft The World
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Main Windows Depot Craft The World
+addappid(248391, 1, "274bd06c718fcd808c0da011f82ad6dd30d033820190fb44486a5d9620b13d4b") --Main Windows Depot Craft The World
+setManifestid(248391, "9075462849595865750", 699193216)
+addappid(248392, 1, "7be3e3d259ff5cd3081f3ff4835215fca8d605a5967ae88d08892a48ccc916a0") --Main Macos Depot Craft The World
+setManifestid(248392, "3238348043060555881", 649641328)
+addappid(492680) --Dlcname Craft The World - Sisters in Arms
+addappid(609800) --Dlcname Craft The World - Dig with Friends
+addappid(1105370) --Dlcname Craft The World - Bosses & Monsters
+addappid(1105371) --Dlcname Craft The World - Abandoned Mines
+addappid(1157170) --Dlcname Craft The World - Temples of 4 Elements
+addappid(1157310) --Dlcname Craft The World - Grottoes
+addappid(1209150) --Dlcname Craft The World - Lonely Mountain
+addappid(1338630) --Dlcname Craft The World - Heroes
+addappid(1509120) --Dlcname Craft The World - Heart of Evil
+addappid(2739550) --Dlcname Craft The World - Invasion
+addappid(3392970) --Dlcname Craft The World - Wonderwood
+addappid(4010190) --Dlcname Craft The World - Magic Mirror
