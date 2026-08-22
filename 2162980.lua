992746495484c1b733acf18ff6fa514d7e6e706c
qiao-lima <qiao-lima@github.com>
2026-08-22 11:19:03 +0800

    Manual Update 2162980

---
+++ 2162980.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2162980 - MiLu MiLan
+-- Generated 2026-08-22 03:02:18 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(2162980)
+
+-- Main Depots
+addappid(2162981, 1, "7e9cb7aa2ef240bee61acb0116db3b9377b7643968635dc72299dbf106616824") -- (windows)
+setManifestid(2162981, "1300665247224387158", 951259772)
+
+-- Missing Depots (We don't have the keys yet lol): 2162982
