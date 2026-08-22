e9554d9337ce374c53223041121619a66553700d
qiao-lima <qiao-lima@github.com>
2026-08-23 05:22:23 +0800

    Manual Update 2209140

---
+++ 2209140.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2209140.lua
+--Gamename Ascent of Ashes
+addappid(2209140) --Mainappid Ascent of Ashes
+addappid(2209141, 1, "913AEA2ED06E754DCDD99ED4BDE8B4115E2BFDC50AE41F0B6D384854CDC36229") --Main Windows Depot Ascent of Ashes
+setManifestid(2209141, "5484705082048315781", 239556560)
