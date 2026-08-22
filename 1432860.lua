b8128b9bc858dba3eb483ccf7720b8f52eb9f30d
qiao-lima <qiao-lima@github.com>
2026-08-22 11:07:42 +0800

    Manual Update 1432860

---
+++ 1432860.lua
@@ -0,0 +1,69 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1432860 - Sun Haven
+-- Generated 2026-08-22 00:57:44 UTC
+-- # Depots (Total/DLC/Shared): 9/6/1
+
+-- Main AppID
+addappid(1432860, 1, "1c4c95617cace6c87b862d88f8caf88757ce5976ede0edbb3bef6bda4fae0a20")
+
+-- Main Depots
+addappid(1432861, 1, "699136c056b2ebdf81900c0ca6bffabdf5b83899c1d451837b3fbc945c986d86") -- English (windows)
+setManifestid(1432861, "2994138885476124310", 1663591766)
+addappid(1432862, 1, "26c2186d937d6a9271998f3740326a40582ea51a565d46f1aa620cb228c104e2") -- English (macos)
+setManifestid(1432862, "4552976601978599813", 1659514239)
+
+-- DLC's (no depot keys required)
+addappid(2305660) -- Sun Haven Soundtrack Vol. 1
+addappid(2375130) -- Sun Haven: Dreamy Pet Pack
+addappid(2375140) -- Sun Haven: Spooky Pet Pack
+addappid(2401500) -- Sun Haven: Sweet Mounts
+addappid(2401501) -- Sun Haven: Wicked Mounts Pack
+addappid(2488150) -- Sun Haven: Ocean Shores Pack
+addappid(2488151) -- Sun Haven: Set Sail Pack
+addappid(2488152) -- Sun Haven: Mushy Pack
+addappid(2488153) -- Sun Haven: Free Snaccoon Costume
+addappid(2659990) -- Sun Haven: Dreamy Ram Pack
+addappid(2660000) -- Sun Haven: Funky Monkey Pack
+addappid(2660010) -- Sun Haven: Cyberpop Pack
+addappid(2660020) -- Sun Haven: Rock 'n' Roll Pack
+addappid(2660030) -- Sun Haven: Spirit Petal Pack
+addappid(2660040) -- Sun Haven: Trick or Treat Pack
+addappid(2743900) -- Sun Haven: Pop Sensation Pack
+addappid(2743910) -- Sun Haven: Starlight Pack
+addappid(2743920) -- Sun Haven: Toy Pack
+addappid(2743930) -- Sun Haven: Snow Day Pack
+addappid(2743940) -- Sun Haven: Tis the Season Pack
+addappid(2743950) -- Sun Haven: Sugar Rush Pack
+addappid(2952640) -- Sun Haven: Deep Sea Pack
+addappid(2952650) -- Sun Haven: Celestial Pack
+addappid(2952660) -- Sun Haven: Bloom and Doom Pack
+addappid(2952670) -- Sun Haven: Claws and Paws Pack
+addappid(3139200) -- Sun Haven: Tea Temple Pack
+addappid(3139210) -- Sun Haven: Emerald Elegance Pack
+addappid(3139220) -- Sun Haven: Flames and Ashes Pack
+addappid(3139230) -- Sun Haven: Squeaky Clean Pack
+addappid(3139240) -- Sun Haven: Snow Yeti Pack
+addappid(3819940) -- Sun Haven: Once Upon a Time Pack
+addappid(3819950) -- Sun Haven: Cyberknight Pack
+addappid(3819960) -- Sun Haven: Radiant Royalty Pack
+addappid(3819970) -- Sun Haven: Astral Pack
+addappid(3819980) -- Sun Haven: Myths and Muses Pack
+addappid(4209190) -- Sun Haven: Gearwork Glamour Pack
+addappid(4209200) -- Sun Haven: Día de Delights Pack
+addappid(4209210) -- Sun Haven: Cozy Cottage Pack
+addappid(4209220) -- Sun Haven: Garden Party Pack
+addappid(4534970) -- Sun Haven: Sakura Pet Pack
+addappid(4534980) -- Sun Haven: Snack Pet Pack
+addappid(4534990) -- Sun Haven: Sweet Pet Pack
+
+-- DLC's (with depot keys)
+-- Sun Haven Soundtrack Vol. 2 (AppID: 2375000)
+addappid(2375000)
+addappid(2375001, 1, "9e35d8896c45172937a1b2ffe2f95285b7be1a250c0e89ce0d9bdcf4cca754d5")
+setManifestid(2375001, "7475523022309329780", 178416003)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(2305662, 1, "fd3834626fe0fb254f567e19c07831ea4684b5b1dc4360fdd0fd76a99491acd5")
+setManifestid(2305662, "5013926395044398502", 243290568)
+
+-- Missing Depots (We don't have the keys yet lol): 1559320, 1667600, 1667610, 1667620, 1667660
