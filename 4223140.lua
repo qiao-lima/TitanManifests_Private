60ce902b4e31492e422e87af91e25ef291f9668e
qiao-lima <qiao-lima@github.com>
2026-08-22 11:09:47 +0800

    Manual Update 4223140

---
+++ 4223140.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4223140 - Petal by Petal
+-- Generated 2026-08-22 02:44:07 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4223140, 1, "f8b55364a125f36f908cbfc5f0aee7c08c7fc5b5fd93ab5561c6a0320a17cc21")
+
+-- Main Depots
+addappid(4223141, 1, "c18a439102a48da7b225193331ef48a552fa1fed1a36d63517f5cf90c4fb4a1c") -- (windows)
+setManifestid(4223141, "1022696143744713241", 194101136)
