489ddd6080ffc93ff061e6017d19c415fff82e35
qiao-lima <qiao-lima@github.com>
2026-08-23 05:22:39 +0800

    Manual Update 4333200

---
+++ 4333200.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4333200.lua
+--Gamename Lost & Found: A This Bed We Made Story
+addappid(4333200, 1, "62bfbbb7c598313c200e5087f4203b012841f1dc700428254d8e56e5f8e3e92c") --Mainappid Lost & Found: A This Bed We Made Story
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Main Windows Depot Lost & Found: A This Bed We Made Story
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Main Windows Depot Lost & Found: A This Bed We Made Story
+addappid(4333201, 1, "c6ef4bf622688db0394ee043858049682bd14d69e02a959f87e7f83b3917bd27") --Main Windows Depot Lost & Found: A This Bed We Made Story
+setManifestid(4333201, "7722886653119526981", 3110438752)
