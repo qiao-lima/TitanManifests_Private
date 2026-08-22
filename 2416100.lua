7059e01724a7a15553c5d790e6313baf265c7b78
qiao-lima <qiao-lima@github.com>
2026-08-23 05:22:23 +0800

    Manual Update 2416100

---
+++ 2416100.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2416100 - Goodnight Universe
+-- Generated 2026-08-22 19:44:47 UTC
+-- # Depots (Total/DLC/Shared): 3/2/0
+
+-- Main AppID
+addappid(2416100)
+
+-- Main Depots
+addappid(2416101, 1, "3b2ece3bb72a83680b1f96a267ed516bebbb10fcf38e465e0b723e6447d7380f") -- (windows)
+setManifestid(2416101, "2228434543157502835", 7946123423)
+
+-- Missing Depots (We don't have the keys yet lol): 4132381, 4132382
