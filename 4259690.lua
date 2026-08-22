9eb228f746384f8534b027b58a9ea6441f9281d2
qiao-lima <qiao-lima@github.com>
2026-08-23 05:22:38 +0800

    Manual Update 4259690

---
+++ 4259690.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4259690.lua
+--Gamename Ajnabee - The Unknown
+addappid(4259690) --Mainappid Ajnabee - The Unknown
+addappid(4259691, 1, "fd68b10ba1a1cccc18f2f569938632593483b0214dec813958a6ca0c2bd8f117") --Main Windows Depot Ajnabee - The Unknown
+setManifestid(4259691, "8757362662374150330", 4805629776)
