b1182c13116cd428f9b7cb3ad3c446a241bf6b7b
qiao-lima <qiao-lima@github.com>
2026-08-23 05:22:38 +0800

    Manual Update 4243700

---
+++ 4243700.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4243700.lua
+--Gamename 27-year-old Female Teacher – A Moment in Daily Life, Chapter 1
+addappid(4243700) --Mainappid 27-year-old Female Teacher – A Moment in Daily Life, Chapter 1
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Main Windows Depot 27-year-old Female Teacher – A Moment in Daily Life, Chapter 1
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Main Windows Depot 27-year-old Female Teacher – A Moment in Daily Life, Chapter 1
