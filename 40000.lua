1c9ebbba6567c92aa5598cfd3640161d8b97ee18
qiao-lima <qiao-lima@github.com>
2026-08-23 05:22:36 +0800

    Manual Update 40000

---
+++ 40000.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 40000.lua
+--Gamename Fallen Earth
+addappid(40000) --Mainappid Fallen Earth
+addappid(40001, 1, "{") --Main Windows Depot Fallen Earth
+setManifestid(40001, "4694440457349043105", 0)
+addappid(40002) --Dlcname Fallen Earth - Special Item Code
+addappid(40003) --Dlcname Fallen Earth - Faction T-Shirt
+addappid(40004) --Dlcname Fallen Earth - CleanerGun Promo
+addappid(40005) --Dlcname Fallen Earth - CleanerGun Ammo Promo
+addappid(40006) --Dlcname Fallen Earth F2P Trash the Hermit Crab Code
+addappid(226491) --Dlcname Fallen Earth - Survivalist Pack - Fall 2012
