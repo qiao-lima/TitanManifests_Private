3b174169bf4049866bbf0619e68e6ec8fe354083
qiao-lima <qiao-lima@github.com>
2026-08-23 05:22:51 +0800

    Manual Update 4945850

---
+++ 4945850.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4945850.lua
+--Gamename Galumb
+addappid(4945850) --Mainappid Galumb
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Main Windows Depot Galumb
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Main Windows Depot Galumb
+addappid(4945851, 1, "d6db5363b047e5d3dcc579a72704e5276d62b67f2f34095ad043ed638dcf49dd") --Main Windows Depot Galumb
+setManifestid(4945851, "5361767475493956399", 3784544304)
