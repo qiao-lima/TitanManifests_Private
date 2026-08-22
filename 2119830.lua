8b62d9198f31a112e53650586d8897b6cdb35b8d
qiao-lima <qiao-lima@github.com>
2026-08-23 05:22:22 +0800

    Manual Update 2119830

---
+++ 2119830.lua
@@ -0,0 +1,19 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2119830 - MISERY
+-- Generated 2026-08-22 20:47:23 UTC
+-- # Depots (Total/DLC/Shared): 4/0/3
+
+-- Main AppID
+addappid(2119830, 1, "bbe49d60bab6efe43cb94be96a638eecd9c31a13ee527b847405f39f3cf064d3")
+
+-- Main Depots
+addappid(2119831, 1, "2322e54a5ba6afee939e05b81bdc7b5477adee6b3ef54dcab5d2ccc32243c086") -- (windows,linux, 64-bit)
+setManifestid(2119831, "3002776385514127223", 5057001973)
+
+-- DLC's (no depot keys required)
+addappid(4119690) -- MISERY - Zaslavie Supporter Pack
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- (windows)
