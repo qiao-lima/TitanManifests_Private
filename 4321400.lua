85669fdf7663b286b85c0eebd6ce80b095e6d90b
qiao-lima <qiao-lima@github.com>
2026-08-23 05:22:39 +0800

    Manual Update 4321400

---
+++ 4321400.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4321400.lua
+--Gamename Gold Up
+addappid(4321400) --Mainappid Gold Up
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Main Windows Depot Gold Up
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Main Windows Depot Gold Up
+--Dlcname Gold Up Demo
+addappid(4773960) --Dlcname Gold Up Demo
+addappid(4773961, 1, "6e5534725e06f60c322f902f0872a1e906d82e93ce54930224ab6fdec529e2df") --Dlc Windows Depot Gold Up Demo
+setManifestid(4773961, "1431331079196254691", 1780470528)
