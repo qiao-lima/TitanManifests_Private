870066dffa928bcd5c44dbddaf7ab5bf08682d25
qiao-lima <qiao-lima@github.com>
2026-08-23 05:22:36 +0800

    Manual Update 3995190

---
+++ 3995190.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3995190 - Obsessed Lucy
+-- Generated 2026-08-22 19:46:00 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3995190, 1, "4ec6554966aed2b780d3e8fbbc1e860cbd36774bc3be0a96951dfa44d45a6aa4")
+
+-- Main Depots
+addappid(3995191, 1, "d51221beb57afe8c73d2f7550b4da163a690e5b288bbed0f8f826b94eceb2f0c")
+setManifestid(3995191, "3237506606280884808", 1036882830)
