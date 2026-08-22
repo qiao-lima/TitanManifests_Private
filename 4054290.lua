689dfb6ecda06c254cbf9b2fdf3524dff4253c22
qiao-lima <qiao-lima@github.com>
2026-08-23 05:22:37 +0800

    Manual Update 4054290

---
+++ 4054290.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4054290.lua
+--Gamename DONTCRUSH
+addappid(4054290) --Mainappid DONTCRUSH
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Main Windows Depot DONTCRUSH
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Main Windows Depot DONTCRUSH
+addappid(4054291, 1, "bf01cc86849058470c0482b40d3627da0e538787df15479cb45355adf6f7f3bf") --Main Windows Depot DONTCRUSH
+setManifestid(4054291, "31276849555847534", 290616400)
