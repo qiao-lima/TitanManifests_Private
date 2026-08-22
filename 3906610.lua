bc0343fb5dd489fe111c97613d53b0f1290d3a8d
qiao-lima <qiao-lima@github.com>
2026-08-22 11:54:27 +0800

    Manual Update 3906610

---
+++ 3906610.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3906610 - Yoko no Hanashi
+-- Generated 2026-08-22 03:53:39 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3906610)
+
+-- Main Depots
+addappid(3906611, 1, "0e0450364058c541dcf9951c168ff9478ec67ae5ac9d9f98ca2d1836e7294ec8") -- (windows, 64-bit)
+setManifestid(3906611, "3557413658089320563", 2601515706)
