22fcb93b5a4c81e643affb25d867f54376d20a47
qiao-lima <qiao-lima@github.com>
2026-08-22 11:14:38 +0800

    Manual Update 4412710

---
+++ 4412710.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4412710 - Gummy Invaders
+-- Generated 2026-08-22 01:22:18 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4412710)
+
+-- Main Depots
+addappid(4412711, 1, "04b241d843eeb53f998de6c80ea8adc70b20dc51f072c6dadefe7b51376074a3")
+setManifestid(4412711, "385939165853647096", 310458354)
