19ae2d5503652790c60caa733e8d8a678ef64cdf
qiao-lima <qiao-lima@github.com>
2026-08-23 05:22:26 +0800

    Manual Update 3505390

---
+++ 3505390.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3505390 - Car Crash X
+-- Generated 2026-08-22 20:46:49 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3505390)
+
+-- Main Depots
+addappid(3505391, 1, "5dd1d5dbc4967fa2d79f5f6b04fe7e9331cc78cab0a68b408e48f1b5e7ac4a69")
+setManifestid(3505391, "1251308740242086218", 3800799931)
