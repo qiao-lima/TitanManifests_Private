6c52765e43d4b06e00b5a619c93d21e8ce084b53
qiao-lima <qiao-lima@github.com>
2026-08-22 11:07:41 +0800

    Manual Update 1263200

---
+++ 1263200.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1263200 - Smashing Spirits: Brazil’s First Boxer
+-- Generated 2026-08-22 02:46:07 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(1263200)
+
+-- Main Depots
+addappid(1263201, 1, "e0896e66ea44cfb0da400d2e21670b864a38a51e3c5fd4e0f096d29627872d3b") -- (windows)
+setManifestid(1263201, "5026088763803234602", 338747281)
+
+-- Missing Depots (We don't have the keys yet lol): 1263203, 1263204
