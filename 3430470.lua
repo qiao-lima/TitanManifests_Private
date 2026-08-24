1d0701ec20c26a9b8e678372bd733842dff9b3af
qiao-lima <qiao-lima@github.com>
2026-08-24 16:24:30 +0800

    Manual Update 3430470

---
+++ 3430470.lua
@@ -0,0 +1,9 @@
+-- Main AppID
+addappid(3430470, 1, "b8a27d1c17e32ae2fb9fb63aed48176f5fbbac4361bdd8a0816a9d386c9803f6")
+-- Main Depots
+addappid(3430471, 1, "7f478c739cf9e5b4ccacc2527b6ad86e4bbfc13453783e9bf5db05fd5f371673")
+-- DLC's (no depot keys required)
+addappid(3900890) -- PLATiNA :: LAB - AQUA BLUE
+addappid(4143860) -- PLATiNA :: LAB - sasakure.UK x TJ.hangneil
+addappid(4340790) -- PLATiNA :: LAB - Zundamon「Songs」
+addappid(4340800) -- PLATiNA :: LAB - Zundamon「Character」
