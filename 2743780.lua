ff7ba65bbff14f85cbb7b612b01c1cacdf28b777
qiao-lima <qiao-lima@github.com>
2026-08-24 16:24:30 +0800

    Manual Update 2743780

---
+++ 2743780.lua
@@ -0,0 +1,14 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2743780 - Bunny’s Rent-a-Mom Agency
+-- Generated 2026-08-21 02:16:55 UTC
+-- # Depots (Total/DLC/Shared): 2/0/1
+
+-- Main AppID
+addappid(2743780, 1, "ffccd3e4206c71303748d8af0e147e47550fc69eb939caf40fc8fd6a123b35e1")
+
+-- Main Depots
+addappid(2743781, 1, "147d0784d7eb4b943060ef9588269e71128640676f8c998a562f70a0142844df")
+setManifestid(2743781, "4920258771172433609", 920550247)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
