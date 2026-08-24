5c6c724dd6a57ea7d369ce64af08018488e7603c
qiao-lima <qiao-lima@github.com>
2026-08-24 16:30:10 +0800

    Manual Update 4304230

---
+++ 4304230.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4304230.lua
+--Gamename 历史模拟器：崇祯
+addappid(4304230, 1, "e5b36307a9983f9e2cf221e3d0c4833f608627b7b61e9796eacbbe9384764081") --Mainappid 历史模拟器：崇祯
+addtoken(4304230, "6733678450081382879")
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Main Windows Depot 历史模拟器：崇祯
+addappid(4304231, 1, "1a2840a22f903875aa5b87679de41cfb36b0eaf1b56d22da1dd2f5795fa4e6f0") --Main Windows Depot 历史模拟器：崇祯
+setManifestid(4304231, "7393331690123012957", 571131152)
+addappid(4304232, 1, "275edfe8e4a0741473d54bc820774a03cae9efc2b8effae79d5fc8495af89330") --Main Windows Depot 历史模拟器：崇祯
+setManifestid(4304232, "1848854249900237529", 484864768)
+addappid(4629290) --Dlcname 历史模拟器：崇祯 - preview
