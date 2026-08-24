3c7e6a3d40721e6f9f80020bec9bf0d0fc9739f6
qiao-lima <qiao-lima@github.com>
2026-08-24 16:24:32 +0800

    Manual Update 3811560

---
+++ 3811560.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3811560 - Angry Dik
+-- Generated 2026-08-24 01:22:26 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3811560, 1, "718d7b3bbf63f71e63b2d80c9d660044299cb47821dad4370a05c06d28cc1f38")
+
+-- Main Depots
+addappid(3811561, 1, "c4ecbe6a4f0507c44a8e64c974e06a632993630eeebf417450a6636f17efd8c3")
+setManifestid(3811561, "7278449253792796909", 1234032981)
