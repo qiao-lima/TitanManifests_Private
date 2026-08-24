ccacd40a9bd95e30f6b153e46540817875f8bbd3
qiao-lima <qiao-lima@github.com>
2026-08-24 16:30:11 +0800

    Manual Update 4534000

---
+++ 4534000.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4534000 - PROFITS 2167: Planetary Resource Optimization & Freight Infrastructure Technician Simulator
+-- Generated 2026-08-24 00:23:03 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4534000)
+
+-- Main Depots
+addappid(4534001, 1, "a4b678a53ea0b7d845941bf138d7e018a7898227727ce2cbd6922a34abae9973") -- (windows, 64-bit)
+setManifestid(4534001, "4513561328347289221", 1724518046)
