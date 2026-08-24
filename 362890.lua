36d56d4b4f9a4053f6a506e1de278d4e0940d409
qiao-lima <qiao-lima@github.com>
2026-08-24 16:24:31 +0800

    Manual Update 362890

---
+++ 362890.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 362890.lua
+--Gamename Black Mesa
+addappid(362890, 1, "4b7e10266c887ed53367d047d2689b4361f00cd5d51cd85e4f6b01aeef7641bb") --Mainappid Black Mesa
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Main Windows Depot Black Mesa
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Main Windows Depot Black Mesa
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Main Windows Depot Black Mesa
+addappid(362891, 1, "46ba8e1d09bb724bb5274d7fa497e4acd1186881d41a91857922c979d4f37344") --Main Windows Depot Black Mesa
+setManifestid(362891, "3745641804950828055", 12923134976)
+addappid(362892, 1, "1141f7df13dcc852bac94bbe7a7d684903dbb97ef068228782c62ac4f8adae23") --Main Linux Depot Black Mesa
+setManifestid(362892, "4588672532934012726", 72618400)
+addappid(362894, 1, "d7ead84613b380590a839d924354b02e5943f64d6d291045d209b2ba4f72f1c4") --Main Windows Depot Black Mesa
+setManifestid(362894, "8196368488078724424", 71857536)
+addappid(362895, 1, "89498042a0fa6ab71fe0ed1cb35240a02daffe7e8f622856eefa3e46dd87e68d") --Main Windows Language Depot English Black Mesa
+setManifestid(362895, "5909980735403050706", 684455136)
