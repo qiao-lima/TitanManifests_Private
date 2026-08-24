41838f96c20e22401347a1b1ad7c5f171a203af4
qiao-lima <qiao-lima@github.com>
2026-08-25 04:54:00 +0800

    Manual Update 4941910

---
+++ 4941910.lua
@@ -0,0 +1,5 @@
+addappid(4941910) 
+addappid(4941911, 1, "89b056a35dcf7091aa873e5ff497c69785c1b61f1e3f1fce3cd90ee86dfe3c12") 
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") 
+addappid(4942160) -- Readerpet - Garden Dcor Pack
+addappid(4942170) -- Readerpet - Cottage Furnishing Pack
