d22ff25d08e541e46ff75f7649d10250397b906e
qiao-lima <qiao-lima@github.com>
2026-08-25 04:53:46 +0800

    Manual Update 3342800

---
+++ 3342800.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3342800.lua
+--Gamename 代筆屋ワールドワード
+addappid(3342800) --Mainappid 代筆屋ワールドワード
+addappid(3342801, 1, "7156dee0a00b1716c8ec1684f35c548de43069313ac1e68bb94a72038a01b091") --Main Windows Depot 代筆屋ワールドワード
+setManifestid(3342801, "6747859738432296308", 58841520)
