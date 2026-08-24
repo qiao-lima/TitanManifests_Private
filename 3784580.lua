623716851e8c341deb3ba4d26d4fa24d3eb5d134
qiao-lima <qiao-lima@github.com>
2026-08-25 04:53:47 +0800

    Manual Update 3784580

---
+++ 3784580.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3784580.lua
+--Gamename Goth Obsession: Touch Adventure
+addappid(3784580, 1, "b83a241b60374330eb0fb1deef8e8f10674c9a29f4920fcd27c85bef11e70ca4") --Mainappid Goth Obsession: Touch Adventure
+addappid(3784581, 1, "ca3769f9afa313e70f71afa567192ea71142916660a39b7b9fd085f42aa8abcc") --Main Windows Depot Goth Obsession: Touch Adventure
+setManifestid(3784581, "4121198897447445714", 388727376)
+addappid(4669990, 1, "11622f9d7807f87c7cb01b0c908904e3c5502d85dbccc74505dbd75cb8445b2f") --Main Windows Depot Goth Obsession: Touch Adventure
+setManifestid(4669990, "8262450828349926295", 403621360)
+--Dlcname Goth Obsession Touch Adventure Wallpaper pack
