da97e0460bf010f177416d9fe039cb5979bd0037
qiao-lima <qiao-lima@github.com>
2026-08-25 04:53:44 +0800

    Manual Update 1721110

---
+++ 1721110.lua
@@ -0,0 +1,29 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1721110 - Abyssus
+-- Generated 2026-08-24 19:04:29 UTC
+-- # Depots (Total/DLC/Shared): 5/2/2
+
+-- Main AppID
+addappid(1721110, 1, "84ff222c6c3b02088667934e3315d227bd86751a3ceee639b8683e0ffcc89feb")
+
+-- Main Depots
+addappid(1721111, 1, "fd31fe45269b1676240f9cabc945952eddc0eca492eed6d02938264d045af58b")
+setManifestid(1721111, "2812786191767295318", 5940063701)
+
+-- DLC's (no depot keys required)
+addappid(3853680) -- Abyssus - Brinehunter Pack
+addappid(3853790) -- Abyssus - Brineguard Pack
+addappid(4168330) -- Abyssus - Brinefrost Pack
+addappid(4508360) -- Abyssus - Brinerot Pack
+
+-- DLC's (with depot keys)
+-- Abyssus (Original Game Soundtrack) (AppID: 3853800)
+addappid(3853800)
+addappid(3853801, 1, "3d1ec2cd4b3ab78e6b74d7ad261a0d77e657bedebadbf7ab6e1079545601cac3")
+setManifestid(3853801, "7499188759332597283", 28426320)
+addappid(3853802, 1, "d5a12418aca4da9a563c544f5fe7cc1ec2d17471f5f86cc5800ff6caab2fdb4c")
+setManifestid(3853802, "3343777574236290517", 511437134)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
