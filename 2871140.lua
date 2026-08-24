eb7ad46e4f427d022f17ffbcd2bb8cb795902de2
qiao-lima <qiao-lima@github.com>
2026-08-24 16:24:30 +0800

    Manual Update 2871140

---
+++ 2871140.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2871140 - 誰是中之人
+-- Generated 2026-08-24 00:29:40 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2871140)
+
+-- Main Depots
+addappid(2871141, 1, "7a00b7fad72e0785e0ba6bb0b76a7901bda736ed3dac8dd9f9bbf45d703fcfe6")
+setManifestid(2871141, "1314844280807278911", 1527463789)
