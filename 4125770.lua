f9b79ccff6b29406cea5c83c6f9211875256c1da
qiao-lima <qiao-lima@github.com>
2026-08-24 16:30:09 +0800

    Manual Update 4125770

---
+++ 4125770.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4125770 - 大侠狂想曲
+-- Generated 2026-08-24 00:27:33 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4125770)
+
+-- Main Depots
+addappid(4125771, 1, "866f4b633f759e3892bbe667084fabb689c0c0d396339515df7d227109f949e0")
+setManifestid(4125771, "2085292750453469070", 5882576735)
