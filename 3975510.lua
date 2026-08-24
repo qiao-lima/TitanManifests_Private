cd002b0c410420bf999e08db398ab08e9036328d
qiao-lima <qiao-lima@github.com>
2026-08-25 04:53:48 +0800

    Manual Update 3975510

---
+++ 3975510.lua
@@ -0,0 +1,19 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3975510 - CYAN BRAIN
+-- Generated 2026-08-24 19:02:35 UTC
+-- # Depots (Total/DLC/Shared): 3/1/0
+
+-- Main AppID
+addappid(3975510, 1, "9869fa745fa9f5866ec8f1dd4b4720306c4d0e5c8e8f6044acaf19b746238534")
+
+-- Main Depots
+addappid(3975511, 1, "a1bbbcf2b8771f1c4c286a07e143de31eba09b74be882c33eb28c9b41a1fbc4f")
+setManifestid(3975511, "1877905708940531969", 1735347913)
+addappid(3975512, 1, "6e06cb855c8129b3fb32d81ee71434f2af43076d8afeaa4a232619a00e07c304") -- Japanese
+setManifestid(3975512, "8654483468199817105", 1753978510)
+
+-- DLC's (with depot keys)
+-- CYAN BRAIN - Artbook (AppID: 4218650)
+addappid(4218650)
+addappid(4218651, 1, "674ecc1152df4182a3583aafe8befc80adb589fae688e694c3d2286fe89d6cef")
+setManifestid(4218651, "472226173205832182", 49685626)
