ab05c45d99e642d423b56da78e8fc95c2a02a8bb
qiao-lima <qiao-lima@github.com>
2026-08-25 04:53:47 +0800

    Manual Update 3814570

---
+++ 3814570.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3814570.lua
+--Gamename 凡人修仙传奇
+addappid(3814570) --Mainappid 凡人修仙传奇
+addappid(3814571, 1, "06cdcd0f4bd9051908355d4305408b4c6e34bf2a3a79dde173529edc14262569") --Main Windows Depot 凡人修仙传奇
+setManifestid(3814571, "8146630607663577269", 5236702000)
