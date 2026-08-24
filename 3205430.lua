418767242619a190c5a1c86629c02886246c9e97
qiao-lima <qiao-lima@github.com>
2026-08-24 16:24:30 +0800

    Manual Update 3205430

---
+++ 3205430.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3205430 - Condo Desires
+-- Generated 2026-08-24 01:19:20 UTC
+-- # Depots (Total/DLC/Shared): 2/1/0
+
+-- Main AppID
+addappid(3205430, 1, "4130ccd17471114cb4a62c3607188e641120ace3384895d64edaff77f138cddd")
+
+-- Main Depots
+addappid(3205431, 1, "c15f48df109afd3f050d8c078ba53885182e2f5689ae168c9f9e20ebec1556f7")
+setManifestid(3205431, "5173198128499294611", 316527426)
+
+-- DLC's (with depot keys)
+-- Condo Desires - Olivia, The Maid (AppID: 3712010)
+addappid(3712010)
+addappid(3712010, 1, "9a591c39426e23a05550d942f70a3f6d2003048fef38dac4e88be50668420b8d")
+setManifestid(3712010, "3570910169785449600", 319867391)
