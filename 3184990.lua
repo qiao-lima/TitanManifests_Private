e65d7c181ab215df300bfc3d4c917b9b978a9ce3
qiao-lima <qiao-lima@github.com>
2026-08-25 04:53:46 +0800

    Manual Update 3184990

---
+++ 3184990.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3184990.lua
+--Gamename 诡秘推理
+addappid(3184990) --Mainappid 诡秘推理
+addappid(3184991, 1, "8c4a096cb4909d10338ba908f3d9a7bb173b0d9830798a4d8a9691bc36575dde") --Main Windows Depot 诡秘推理
+setManifestid(3184991, "4913307119381584261", 1344443456)
+addappid(4145340) --Dlcname 首发纪念礼
+--Missing Dlcs
+--Dlcname 诡秘推理 Soundtrack
