c1b1b0790572bad1fec43b34aeb41bd72e299413
qiao-lima <qiao-lima@github.com>
2026-08-24 16:24:29 +0800

    Manual Update 2742280

---
+++ 2742280.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2742280 - Fantasista Asuka
+-- Generated 2026-08-24 01:24:10 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2742280, 1, "a71981c931c78cb78b5b9d39a5a62378da9706b1fa740c6e865c7f61f000edb2")
+
+-- Main Depots
+addappid(2742281, 1, "119e74230884862ea503bfc0d472854424990a8e167cfde48536a8724ec1eee9")
+setManifestid(2742281, "5856249164538147916", 2624836164)
