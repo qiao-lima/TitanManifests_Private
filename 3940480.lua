e918c0020438860b2e945d6d2b39a88b3a9cab2c
qiao-lima <qiao-lima@github.com>
2026-08-24 16:24:33 +0800

    Manual Update 3940480

---
+++ 3940480.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3940480.lua
+--Gamename Alone With my StepSister
+addappid(3940480) --Mainappid Alone With my StepSister
+addappid(3940481, 1, "6f06a6b2f81229dfcf2d2a4b4685f4677c2baae2254cd9e15b9bd3978c6ec000") --Main Windows Depot Alone With my StepSister
+setManifestid(3940481, "3611548813059043872", 361783120)
