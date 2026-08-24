87cf0248e70790dbecaaa169c60db9b56c4abf99
qiao-lima <qiao-lima@github.com>
2026-08-25 04:53:46 +0800

    Manual Update 3587610

---
+++ 3587610.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3587610.lua
+--Gamename The Dark Queen of Mortholme
+addappid(3587610, 1, "5e3ac6dc266957ea1bf51f7f7c00b75dd53c1f15385783e3b5083de59dcb5970") --Mainappid The Dark Queen of Mortholme
+addappid(3587611, 1, "9f4086779aebd132bebd7d39a5b6d294e2b31219df6d5df5dab4d06e44c03389") --Main Windows Depot The Dark Queen of Mortholme
+setManifestid(3587611, "8300298083749268048", 108182128)
