d0808f62e35f42a231b8d17d88d7289069cc9b17
qiao-lima <qiao-lima@github.com>
2026-08-25 04:54:00 +0800

    Manual Update 4972200

---
+++ 4972200.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4972200 - Electrical Leak
+-- Generated 2026-08-24 19:12:33 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4972200)
+
+-- Main Depots
+addappid(4972201, 1, "929bb0a751bd307c18e6545e36192078a3b925552be4652f07e41865866d173a")
+setManifestid(4972201, "7347576546423125320", 384506355)
