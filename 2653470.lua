5d668c02226d7306a63bfef2f4be4c367ecb9d01
qiao-lima <qiao-lima@github.com>
2026-08-24 16:24:29 +0800

    Manual Update 2653470

---
+++ 2653470.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2653470 - Little Problems
+-- Generated 2026-08-24 00:29:29 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2653470, 1, "57e70653e968081080c302bd388c959d67d9881ecdcb4776632e6e0f30c5ab60")
+
+-- Main Depots
+addappid(2653471, 1, "a65ecc1aa6fe82087aa5493cff88ac235bc5594fc66f1ac809fbac147b89947d")
+setManifestid(2653471, "8387910848926260293", 2948623704)
