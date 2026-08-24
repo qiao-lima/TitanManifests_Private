e26d309c7dc07e7eef8996994b3c99b3c7ed1043
qiao-lima <qiao-lima@github.com>
2026-08-24 16:24:31 +0800

    Manual Update 3591400

---
+++ 3591400.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3591400 - Cosplayer's Quest
+-- Generated 2026-08-24 01:07:42 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3591400, 1, "f8f7c96dd2b060299ae1bdbe1c157e82b54dcba99bcf610cfb9869722f6296dd")
+
+-- Main Depots
+addappid(3591401, 1, "f69e7015e5373738e8a1ab5d9548b84270a5a90198bcc2796a2008451f15e58e") -- (windows)
+setManifestid(3591401, "7852419288936421966", 759770704)
