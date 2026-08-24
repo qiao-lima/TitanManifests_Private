6b79626e7f26e2d0a29e6517c18e63d008948a60
qiao-lima <qiao-lima@github.com>
2026-08-25 04:53:44 +0800

    Manual Update 2361770

---
+++ 2361770.lua
@@ -0,0 +1,20 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2361770.lua
+--Gamename SHINOBI: Art of Vengeance
+addappid(2361770, 1, "7802d8db8a3c67b9d5478e24cde9ba28aebeb37dae8d7aec63b5e43ec19cc451") --Mainappid SHINOBI: Art of Vengeance
+addappid(2361771, 1, "6077dc2b1a88a7ae7a34470113e609a77bfc721374f4b17e20abc2f975ad2d85") --Main Windows Depot SHINOBI: Art of Vengeance
+setManifestid(2361771, "8572567774510642096", 8697651296)
+addappid(3381241, 1, "a8471adc3c0b8d999f3576f04318b08df5a03609a11bf628534753443d469cdf") --Main Windows Depot SHINOBI: Art of Vengeance
+setManifestid(3381241, "1281949326523039310", 1937970384)
+--Dlcname SHINOBI: Art of Vengeance - Digital Art Book and Original Soundtrack
+addtoken(3381250, "5370029251971047888")
+addappid(3381250, 1, "155147f9c285079d0c8de3a293bb7e0ebc4c5e59763e5eb6f6ddb5a14c37df25") --Dlcname SHINOBI: Art of Vengeance - Digital Art Book and Original Soundtrack
+setManifestid(3381250, "2060539196030722798", 1131327552)
+addappid(3381210) --Dlcname SHINOBI: Art of Vengeance - SEGA Blue Outfit
+addappid(3381220) --Dlcname SHINOBI: Art of Vengeance - Original Arcade Outfit and Fortune Hunter Amulet
+addtoken(3381220, "6948985102103530865")
+addappid(3381230) --Dlcname SHINOBI: Art of Vengeance - Starter Pack
+addtoken(3381230, "6898260294414315152")
+addappid(3381240) --Dlcname SHINOBI: Art of Vengeance - SEGA Villains Stage
+addappid(3381260) --Dlcname SHINOBI: Art of Vengeance Digital Deluxe Upgrade
+addtoken(3381260, "11276603698809480240")
