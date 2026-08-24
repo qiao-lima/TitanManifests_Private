d0a790e75b5265fab33f5ee6704a2433de0b3b6b
qiao-lima <qiao-lima@github.com>
2026-08-25 04:53:48 +0800

    Manual Update 4190170

---
+++ 4190170.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4190170.lua
+--Gamename 让我支配你
+addappid(4190170) --Mainappid 让我支配你
+addappid(4190171, 1, "a994a2ffc8330b939fabaf86b257fb50aaf790f89036584a4d3d106e22615288") --Main Windows Depot 让我支配你
+setManifestid(4190171, "1512008568930489168", 136973712)
