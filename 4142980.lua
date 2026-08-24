0c65cb543636906ec85769be2048c3408953c993
qiao-lima <qiao-lima@github.com>
2026-08-25 04:53:48 +0800

    Manual Update 4142980

---
+++ 4142980.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4142980.lua
+--Gamename Toilet 8
+addappid(4142980) --Mainappid Toilet 8
+addappid(4142981, 1, "91656b74ec7f0bda9ac97e8f75317e274770c608e0f429afc981c537b075f220") --Main Windows Depot Toilet 8
+setManifestid(4142981, "560469039507051564", 269567744)
+addappid(4142984, 1, "7b919acbf77f54bf38057aedc87dcb1b97f0f7fea3b88f37b33ad0f49c84a298") --Main Windows Depot Toilet 8
+setManifestid(4142984, "8479463597996060114", 22212880)
+addappid(4586100) --Dlcname Toilet 8 - Supporter Pack
