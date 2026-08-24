93accc917dbaa8ec5542cafd5d8426953ca73434
qiao-lima <qiao-lima@github.com>
2026-08-24 16:30:12 +0800

    Manual Update 4951190

---
+++ 4951190.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4951190 - Konbini Cleanup
+-- Generated 2026-08-24 01:15:48 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(4951190)
+
+-- Main Depots
+addappid(4951191, 1, "87dd9b754c592a929b284876378d6510276a00defcc991edb3a6df314c1016c7")
+setManifestid(4951191, "1443305812236724435", 2379608100)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
