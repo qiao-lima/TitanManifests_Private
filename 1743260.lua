401dc98ce171d68cf936e63bf41c7aa63b01003c
qiao-lima <qiao-lima@github.com>
2026-08-24 16:24:28 +0800

    Manual Update 1743260

---
+++ 1743260.lua
@@ -0,0 +1,21 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1743260 - Welcome to Free Will
+-- Generated 2026-08-24 01:20:25 UTC
+-- # Depots (Total/DLC/Shared): 3/2/0
+
+-- Main AppID
+addappid(1743260, 1, "fd22ac0836febd175449be1c98f221560a16871864c94a2e570865f39c6040e8")
+
+-- Main Depots
+addappid(1743261, 1, "5a9cbf53408fd3e0a08c0363e38d49de21b37bd4d1745919aec4f1ac3c27b2c0")
+setManifestid(1743261, "8839827245898814431", 1625521493)
+
+-- DLC's (with depot keys)
+-- Welcome to Free Will - Episode 2 (AppID: 1824630)
+addappid(1824630)
+addappid(1824630, 1, "ef325656c2577b9ef4ef2e00c71a21a687953967af37567a0209b02b2bdf990e")
+setManifestid(1824630, "2360874250769021", 3583561724)
+-- Welcome to Free Will - Episode 3 (AppID: 2241140)
+addappid(2241140)
+addappid(2241140, 1, "9de3ce33b1ca902aa32f326de8c43095dffe2b93265ffe7d13ec0f7138b6047f")
+setManifestid(2241140, "7830558279113624357", 5348536324)
