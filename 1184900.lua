533dccfe32a9c2f8ab0b613770c0a096131f3660
qiao-lima <qiao-lima@github.com>
2026-08-25 04:53:44 +0800

    Manual Update 1184900

---
+++ 1184900.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1184900.lua
+--Gamename Unitied
+addappid(1184900) --Mainappid Unitied
+addappid(1184902, 1, "b6c92d0f06323c17a1cfc7fda24460c80efaabf6ce29fb578afb19b8545f252d") --Main Windows Depot Unitied
+setManifestid(1184902, "7507116914515461239", 21806976)
