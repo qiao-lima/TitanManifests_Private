64e811e875909355b026d09c951850cf60f9f929
qiao-lima <qiao-lima@github.com>
2026-08-24 16:30:11 +0800

    Manual Update 4738000

---
+++ 4738000.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4738000 - My Sexy Fairies
+-- Generated 2026-08-24 00:34:46 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(4738000)
+
+-- Main Depots
+addappid(4738001, 1, "aadf6861eee799003d62ed87e21343ed326f57d4e4fbf18de23d05f2620f4d5f")
+setManifestid(4738001, "2699890580736960215", 2257027183)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
