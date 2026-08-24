4e4649a871fc6ec77c0048fe30137edea345b918
qiao-lima <qiao-lima@github.com>
2026-08-25 04:53:59 +0800

    Manual Update 4889540

---
+++ 4889540.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4889540 - Citadel Zero
+-- Generated 2026-08-24 19:19:18 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(4889540)
+
+-- Main Depots
+addappid(4889541, 1, "4275a44a6f088abb5686d1c5cb6c736fe9afc3f4013973bed2303bc43f4db866") -- (windows)
+setManifestid(4889541, "2760077757990835778", 546368909)
+
+-- Missing Depots (We don't have the keys yet lol): 4889542, 4889543
