7474f456df7d85671d65867a922c3c681a02f445
qiao-lima <qiao-lima@github.com>
2026-08-25 04:53:47 +0800

    Manual Update 361420

---
+++ 361420.lua
@@ -0,0 +1,19 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 361420.lua
+--Gamename ASTRONEER
+addappid(361420, 1, "42b70c4530261e89bf4e49edb02f7f08fad27c78a425e15ea124dcf8543e0e1a") --Mainappid ASTRONEER
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Main Windows Depot ASTRONEER
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Main Windows Depot ASTRONEER
+addappid(361422, 1, "471756d013ea0594ce2fe60fa33915663d90cf64b086b5b992b85f5fc73d5283") --Main Windows Language Depot English ASTRONEER
+setManifestid(361422, "500421776047685533", 3025271152)
+--Dlcname ASTRONEER (Original Soundtrack)
+addappid(571010, 1, "1a421af94606284807e157154ee5a36f0ddf5a3b37d518a3390a98bf0de5d525") --Dlcname ASTRONEER (Original Soundtrack)
+setManifestid(571010, "3618608313430424138", 288216448)
+addappid(2566990) --Dlcname ASTRONEER Essential Bundle
+addappid(2567000) --Dlcname ASTRONEER Suit Bundle
+addappid(2567010) --Dlcname ASTRONEER Athleisure Bundle
+addappid(2876620) --Dlcname ASTRONEER Evolution Bundle
+addappid(3042220) --Dlcname Astroneer: Glitchwalkers
+addappid(3315070) --Dlcname ASTRONEER: Glitchwalkers - Deluxe Cosmetic Pack
+addappid(3884830) --Dlcname Astroneer: Megatech
+addappid(4144820) --Dlcname Astroneer: Megatech - Deluxe Cosmetic Pack
