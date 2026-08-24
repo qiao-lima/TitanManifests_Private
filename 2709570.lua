a3d34275ae04c6dc626811e0ec4b7b3e443a9f3f
qiao-lima <qiao-lima@github.com>
2026-08-24 16:24:29 +0800

    Manual Update 2709570

---
+++ 2709570.lua
@@ -0,0 +1,18 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2709570 - Supermarket Together
+-- Generated 2026-08-24 08:12:57 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(2709570, 1, "a65a2b8c30c13032c589a0b486771edbef0b42afd4932da912a01858523527e2")
+
+-- Main Depots
+addappid(2709571, 1, "7fa16318d187d201cd5762fec6ac1022fb99b54b7ecb11f228286d5ee2a9790e")
+setManifestid(2709571, "584155826548533509", 11007187118)
+
+-- DLC's (no depot keys required)
+addappid(3146530) -- Supermarket Together - The Cool Pack
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
