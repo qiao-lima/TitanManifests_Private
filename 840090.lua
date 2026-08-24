e295af6496eb8f1dad0ad4b6cb67c43dd5f02047
qiao-lima <qiao-lima@github.com>
2026-08-25 04:54:01 +0800

    Manual Update 840090

---
+++ 840090.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 840090.lua
+--Gamename Battleground Shooting Training 吃鸡枪法训练器
+addappid(840090) --Mainappid Battleground Shooting Training 吃鸡枪法训练器
+addappid(840091, 1, "26795bee23f5a60872f9b1f664626e92b3bcdf61f3202d9699881d4da4535be4") --Main Windows Depot Battleground Shooting Training 吃鸡枪法训练器
+setManifestid(840091, "7227870692259356376", 274979536)
