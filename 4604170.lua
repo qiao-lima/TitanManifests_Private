af8adc46b8a6291809824a950535f871c54446f0
qiao-lima <qiao-lima@github.com>
2026-08-25 04:53:58 +0800

    Manual Update 4604170

---
+++ 4604170.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4604170 - Tattoo Shop Simulator : Prologue
+-- Generated 2026-08-24 19:19:04 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4604170)
+
+-- Main Depots
+addappid(4604171, 1, "ad11b7393ec6b58361d822408936e1c43bf490e0b9785801b9b598db5aea07a9")
+setManifestid(4604171, "1299682501160418219", 1507159793)
