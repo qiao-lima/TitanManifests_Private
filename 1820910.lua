eda347d51434cb88ad6c4c73c571c8f1870e3170
qiao-lima <qiao-lima@github.com>
2026-08-24 16:24:28 +0800

    Manual Update 1820910

---
+++ 1820910.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1820910 - Labyrinth of death
+-- Generated 2026-08-24 00:17:31 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(1820910)
+
+-- Main Depots
+addappid(1820911, 1, "c27b244ffd1f07ada9e0ef356b3fdfc67eebdab0773e55de1686e3e193e3ad05")
+setManifestid(1820911, "3033669742653108503", 8030752)
