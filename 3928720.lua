c2fb470326c59f5160c36511bd19c7f915ec9586
qiao-lima <qiao-lima@github.com>
2026-08-24 16:24:33 +0800

    Manual Update 3928720

---
+++ 3928720.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3928720 - Hollow Knight: Silksong Soundtrack
+-- Generated 2026-08-24 01:12:10 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(3928720)
+
+-- Main Depots
+addappid(3928721, 1, "9e8285f4b529ce55e85dfefe797a57d8e186415988dbed1ee0a18b1901285d63")
+setManifestid(3928721, "7748525586300936826", 479778754)
+addappid(3928722, 1, "f4101b8f87779f16d1030a016a0c579b5f031202ef788788e32b2f0be4d77721")
+setManifestid(3928722, "5502287679669921147", 1828399818)
