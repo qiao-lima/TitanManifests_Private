50d9c2ec184e5d27f2e45256a96606f2859889f8
qiao-lima <qiao-lima@github.com>
2026-08-25 04:53:44 +0800

    Manual Update 1837200

---
+++ 1837200.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1837200.lua
+--Gamename 一坨便便
+addappid(1837200) --Mainappid 一坨便便
+addappid(1837201, 1, "f32dc38e4495f84ae7527f46c3960cdf5cafd2cb883a86d2fa2ef26bfbb806c5") --Main Windows Language Depot Schinese 一坨便便
+setManifestid(1837201, "1584251503953883297", 77421552)
