68f4e3cf92434b024da70d45fa44a63f532b60f5
qiao-lima <qiao-lima@github.com>
2026-08-25 04:53:45 +0800

    Manual Update 2468550

---
+++ 2468550.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2468550.lua
+--Gamename Vivid World
+addappid(2468550) --Mainappid Vivid World
+addappid(2468551, 1, "7631546e2ef0acd42faa89d2f4179d07312134329f58fb2ad3e7248585c12dd4") --Main Windows Depot Vivid World
+setManifestid(2468551, "6012946965540781193", 881077360)
+--Dlcname Vivid World / ビビッドワールド - Soundtrack
+addappid(4026890) --Dlcname Vivid World / ビビッドワールド - Soundtrack
+addappid(4026891, 1, "39d97cb43b5e7adfcf308beed3a655d640c34d2f2374197c5f903326e0cb0390") --Dlc Windows Depot Vivid World / ビビッドワールド - Soundtrack
+setManifestid(4026891, "6622688673041480677", 194714032)
