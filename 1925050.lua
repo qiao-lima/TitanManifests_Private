c6997e14b38f969e1ec3ca47bb4b40b701539912
qiao-lima <qiao-lima@github.com>
2026-08-24 16:24:29 +0800

    Manual Update 1925050

---
+++ 1925050.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1925050 - Sub for You
+-- Generated 2026-08-24 01:11:33 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(1925050, 1, "a377f4c6d8ade3488c9762751f3e0bf0b5dc7b04e049a4df72385a4184aac056")
+
+-- Main Depots
+addappid(1925051, 1, "fbbf250a18281a9e3d4cbf9349773320e93e3568d9e07450755e2dca09c3cdb3")
+setManifestid(1925051, "4778871635372153326", 369869419)
