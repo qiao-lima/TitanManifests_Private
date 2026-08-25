ad0f0531473529f2b3333fc6f95f2ebd2b5236a5
qiao-lima <qiao-lima@github.com>
2026-08-25 19:43:55 +0800

    Manual Update 3812600

---
+++ 3812600.lua
@@ -0,0 +1,21 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3812600 - ReStory: Chill Electronics Repairs
+-- Generated 2026-08-25 11:04:18 UTC
+-- # Depots (Total/DLC/Shared): 5/3/0
+
+-- Main AppID
+addappid(3812600, 1, "28dcae12b1fc1cc8730d44073f4378f9ae73e32de2639581a693dd0bfcd42163")
+
+-- Main Depots
+addappid(3812601, 1, "5f6abd59a61b1a803b5a34eef196a26cd035da18f255212a544d5158dbfc626e") -- (windows,linux)
+setManifestid(3812601, "654423896192937661", 958888251)
+addappid(3812602, 1, "6fcf1eb039f557a73d5ed9b5fc840b753c184899a3e48ef44d98bf4bc423c31f") -- (macos)
+setManifestid(3812602, "6341355155354872355", 1003265452)
+
+-- DLC's (with depot keys)
+-- Restory: Chill Electronics Repairs - Digital Artbook (AppID: 5034500)
+addappid(5034500)
+addappid(5034500, 1, "7b50eb279041c0949418aece25a74fc3c349e149ad3cfdfe093f5ea2c6954065")
+setManifestid(5034500, "4573138219617442396", 52964384)
+
+-- Missing Depots (We don't have the keys yet lol): 5020361, 5020362
