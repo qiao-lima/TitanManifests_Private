12d2b7df72a6fdceef0249747bbd795d23cd36b9
qiao-lima <qiao-lima@github.com>
2026-08-25 19:43:53 +0800

    Manual Update 2163330

---
+++ 2163330.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2163330 - Yet Another Zombie Survivors
+-- Generated 2026-08-25 11:21:37 UTC
+-- # Depots (Total/DLC/Shared): 3/2/0
+
+-- Main AppID
+addappid(2163330, 1, "685bc3c89a6debbe36217fa0abb80a93867699477fb8a5d5d2aee86599f14e79")
+
+-- Main Depots
+addappid(2163331, 1, "f83759c39e0a087b6ee076427f28b6fa19f8e6400ef9e3843bc4ea1d0ba5afff")
+setManifestid(2163331, "7839150826718234465", 6428115800)
+
+-- Missing Depots (We don't have the keys yet lol): 4377471, 4377472
