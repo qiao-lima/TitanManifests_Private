db536d6a63cad76a6f3484f4a903d5efccd1de48
qiao-lima <qiao-lima@github.com>
2026-08-25 19:43:55 +0800

    Manual Update 3854420

---
+++ 3854420.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3854420.lua
+--Gamename My Lovable Demon
+addappid(3854420) --Mainappid My Lovable Demon
+addappid(3854421, 1, "472790060214a6d4f97b512830d36ce54c0d3cb4e3bb7a347e5d19b586c9598b") --Main Windows Language Depot English My Lovable Demon
+setManifestid(3854421, "6938883754402650970", 381871776)
+addappid(5099350, 1, "b2ab20856fccd62bd14fdf759f37d8062ab911c7cb1585b587b9a725fc84b60f") --Main Windows Depot My Lovable Demon
+setManifestid(5099350, "7483010933310063257", 277323280)
+--Dlcname Art Pack
