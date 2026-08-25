b5d34a71488bd04b5d429283633086ebe60cfc96
qiao-lima <qiao-lima@github.com>
2026-08-25 19:43:55 +0800

    Manual Update 3105890

---
+++ 3105890.lua
@@ -0,0 +1,18 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3105890 - PIONER
+-- Generated 2026-08-25 11:32:12 UTC
+-- # Depots (Total/DLC/Shared): 5/1/3
+
+-- Main AppID
+addappid(3105890)
+
+-- Main Depots
+addappid(3105891, 1, "41388d8391e1158182588d11836e34b4e997721800c682092454aca81a282acd")
+setManifestid(3105891, "2049750391179156479", 102598858408)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 4241041
