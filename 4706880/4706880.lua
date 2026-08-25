437c5f0fc8814fbd26f271731c61cf7179c188f7
qiao-lima <qiao-lima@github.com>
2026-08-25 19:43:58 +0800

    Manual Update 4706880

---
+++ 4706880/4706880.lua
@@ -0,0 +1,12 @@
+-- 4706880's Lua and Manifest Created by Hubcap Manifest
+-- Grizzy and the Lemmings - Crazy Party
+-- Created: August 22, 2026 at 13:56:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4706880) -- Grizzy and the Lemmings - Crazy Party
+-- MAIN APP DEPOTS
+addappid(4706881, 1, "50345629962cb60e6e2fd48fd817f89d05beb3d4f28572f77733711c3ea29efc") -- Depot 4706881
+setManifestid(4706881, "8793257469547115676", 5244225063)
