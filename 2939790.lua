0273d132d24821e8907aa55955d6eb18406c0176
qiao-lima <qiao-lima@github.com>
2026-08-25 19:43:54 +0800

    Manual Update 2939790

---
+++ 2939790.lua
@@ -0,0 +1,12 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2939790 - Lunarium
+-- Generated 2026-08-25 11:20:33 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2939790, 1, "284de98dbdcbb88e6dc8cf756873870bf89ac0de18a60198723c1f8cc27d34f2")
+addtoken(2939790, "13885911450102068332")
+
+-- Main Depots
+addappid(2939791, 1, "b30295c5743552adf6bd73ffc9a1ccbdb81623e854ab761f45162d8af3f0e188")
+setManifestid(2939791, "1214131950500250737", 3530426642)
