78e4bb4e7ae78d3e95f44de4758e5876d85bfa60
qiao-lima <qiao-lima@github.com>
2026-08-26 07:29:56 +0800

    Manual Update 373460

---
+++ 373460.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 373460 - Fury Of The Gods
+-- Generated 2026-08-25 23:25:18 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(373460)
+
+-- Main Depots
+addappid(373461, 1, "dc30c0242772ac04b9a015b4cef707cceea1a8d1cd56cfcf6316287de70930bf")
+setManifestid(373461, "8413286940191790604", 203220623)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
