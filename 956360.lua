9191c1820c59e77e4637d73ce26b3e9cb354435a
qiao-lima <qiao-lima@github.com>
2026-08-25 19:47:45 +0800

    Manual Update 956360

---
+++ 956360.lua
@@ -0,0 +1,27 @@
+-- Generated with Luie @ https://lua.tools/
+-- 956360 - Badminton Warrior
+-- Generated 2026-08-25 11:22:57 UTC
+-- # Depots (Total/DLC/Shared): 9/0/0
+
+-- Main AppID
+addappid(956360)
+
+-- Main Depots
+addappid(956361, 1, "114dd874bab603d4cccc35a0b52c59f2085ac97cf6636d0e60e66c0476d32410") -- Schinese (windows, 32-bit)
+setManifestid(956361, "3548200528290808683", 1343635475)
+addappid(956362, 1, "c2e7e86ff68c0d2db8694a58c889d145555ee9100deeca15be2c45fa34249d60") -- Schinese (windows, 64-bit)
+setManifestid(956362, "591802048826512281", 1349441083)
+addappid(956363, 1, "62ea9fe5cbd69e7472d45324089eb0f2fbeb9067aff0f1583e55bb06b8abb4cd") -- Schinese (macos)
+setManifestid(956363, "6622633618252759269", 1358562645)
+addappid(956364, 1, "c5b9e19a5a0e2790fe8aa2ed539d0ab050b7e908b8d4a5c897180424418c69f9") -- (windows, 32-bit)
+setManifestid(956364, "4068435427195525313", 1343635587)
+addappid(956365, 1, "27a8df19f534cf314cec68d61a047b9a72ec0a24d51974632b9d51bb128328bf") -- (windows, 64-bit)
+setManifestid(956365, "5492332615537750286", 1349441195)
+addappid(956366, 1, "244f5549aef72bf0dbeb63551e6ac62da70645ce5a924710c8f0aeb1fcfde903") -- (macos)
+setManifestid(956366, "3545839417864792074", 1358562757)
+addappid(956367, 1, "a0b9d8219cfb5722ca26e261cd6a741d78836da5285e237494e5132725490386") -- (windows, 32-bit)
+setManifestid(956367, "4777246163925472455", 1336178168)
+addappid(956368, 1, "862fde05ea26ca93f4a3d5d43d001ca69a812d3e28aa8c3820131135ecf86f90") -- (windows, 64-bit)
+setManifestid(956368, "1261716939202154353", 1341983776)
+
+-- Missing Depots (We don't have the keys yet lol): 956369
