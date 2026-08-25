3cc6a1ae916117d6a047669e4752e492365c2cc1
qiao-lima <qiao-lima@github.com>
2026-08-25 19:43:55 +0800

    Manual Update 375480

---
+++ 375480.lua
@@ -0,0 +1,7 @@
+-- lua by Fluent-Steam-Lua (https://github.com/huanyuejue/Fluent-Steam-Lua)
+
+addappid(375480) -- Chronicon - 英雄旧忆
+addappid(375481, 1, "9b3ec65b4136ebdb9d7fcd81ac8f1df6bf938c65e42d704b0971d9ec93baf58a")
+addappid(375482, 1, "6836e7d940b17e4d85ad32b6cb7ca44feaa969843e7565c6f6fc409a7422b352")
+addappid(1599010) -- Chronicon - Ancient Beasts
+addappid(2206120) -- Chronicon - The Mechanist
