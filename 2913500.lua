a9352e75f8d9d66710e307f92d43107de5e32ce6
qiao-lima <qiao-lima@github.com>
2026-08-25 19:43:54 +0800

    Manual Update 2913500

---
+++ 2913500.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2913500 - Star Conqueror
+-- Generated 2026-08-25 11:34:00 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2913500)
+
+-- Main Depots
+addappid(2913501, 1, "fce3ea2cd32a56c2fc2234d9234a3d6b4b731363d3c7556a6e3788cddf6232d9")
+setManifestid(2913501, "463378072866271666", 1655295533)
+
+-- Token-locked DLCs (couldn't read depots): 2936640, 2949590
