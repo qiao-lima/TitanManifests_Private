24e0faf462afd44cc89c3ef94bfa7fc8143b3dfa
qiao-lima <qiao-lima@github.com>
2026-08-26 07:29:59 +0800

    Manual Update 914720

---
+++ 914720.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 914720 - シキヨク -夢魅テルは夢見てる-
+-- Generated 2026-08-25 23:26:49 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(914720)
+
+-- Main Depots
+addappid(914722, 1, "2e9ba0f0acfe18a4405d356ef8b1e71be20fc981b0fe97775dd04cd586d5558a") -- English (windows)
+setManifestid(914722, "1176797258442839814", 2934753627)
+addappid(914723, 1, "17780277b535e518bfce0af8547491583185e45241c031dfd4e07082bd1b2a06") -- Japanese (windows)
+setManifestid(914723, "5692880135103648964", 2936869325)
+addappid(914724, 1, "5d7046178982d33bf358ca9d3635426f53ec89b2c637c43edfdecb0e65c3136d") -- Schinese (windows)
+setManifestid(914724, "1488880589972171417", 3110858988)
