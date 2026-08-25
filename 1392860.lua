7b621085cafeb0dbb77ac1587026c4a9acb5b262
qiao-lima <qiao-lima@github.com>
2026-08-26 07:29:55 +0800

    Manual Update 1392860

---
+++ 1392860.lua
@@ -0,0 +1,28 @@
+-- 1392860's Lua and Manifest Created by Hubcap Manifest
+-- Little Nightmares III
+-- Created: July 15, 2026 at 10:33:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 7
+-- Total DLCs: 8
+-- Shared Depots: 6
+
+-- MAIN APPLICATION
+addappid(1392860, 1, "8629cb2029941f570bf6fa875b80ed70fb66f92674c8f43d25937825732ae4a2") -- Little Nightmares III
+-- MAIN APP DEPOTS
+addappid(1392863, 1, "2a40e8e4f16a349cec1ad4cdd732e6a88afb1d0676c8de3061c87c7d815343d0") -- Depot 1392863
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") -- .NET 4.7 Redist (Shared from App 228980)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+addappid(229033, 1, "9a789440bbc16131547ac6c062e5f168f7684da533a2899a71fdfaaeec21efc3") -- PhysX System Software 9.14.0702 (Shared from App 228980)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2088780) -- Little Nightmares III - Full Game
+addappid(2088781) -- Little Nightmares III - The Backstage
+addappid(2088783) -- Little Nightmares III - Ferryman Costumes Set
+addappid(2088785) -- Little Nightmares III - Residents Costumes Pack
+addappid(2088786) -- Little Nightmares III - Dark Six Costumes Set
+addappid(2088787) -- Little Nightmares III - Nomes Costumes Set
+addappid(2088788) -- Little Nightmares III - Klonoa Costumes Set
+addappid(2170190) -- Little Nightmares III - Secrets of The Spiral - Expansion Pass
