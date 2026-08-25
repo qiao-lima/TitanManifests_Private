3f11e5a40a945abd89b936aa1e2df5c389fbced0
qiao-lima <qiao-lima@github.com>
2026-08-25 19:43:53 +0800

    Manual Update 1571040

---
+++ 1571040.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1571040 - 恶搞赛车
+-- Generated 2026-08-25 11:34:09 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(1571040)
+
+-- Main Depots
+addappid(1571041, 1, "12e93e8d1b490d8bb3123605d5478e9206114b23baca28f0be352edeb2604a4c")
+setManifestid(1571041, "2650814495829816529", 654532685)
