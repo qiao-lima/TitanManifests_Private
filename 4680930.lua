9ca211d5febcc86ff947b768308650149e93ef8b
qiao-lima <qiao-lima@github.com>
2026-08-26 18:11:56 +0800

    Manual Update 4680930

---
+++ 4680930.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4680930 - Garbage Growth
+-- Generated 2026-08-26 09:55:34 UTC
+-- # Depots (Total/DLC/Shared): 2/1/0
+
+-- Main AppID
+addappid(4680930)
+
+-- Main Depots
+addappid(4680931, 1, "7669341c3a27613028361d9d1e8a756b40ab27ddad0f2e92ac6a48e11ef8a115")
+setManifestid(4680931, "5917384289178634365", 178105885)
+
+-- Missing Depots (We don't have the keys yet lol): 4796601
