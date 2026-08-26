49806c9ad327087acc8710572858f8f629ac6beb
qiao-lima <qiao-lima@github.com>
2026-08-26 18:08:05 +0800

    Manual Update 3493540

---
+++ 3493540.lua
@@ -0,0 +1,22 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3493540 - Transport Fever 3
+-- Generated 2026-08-26 09:55:46 UTC
+-- # Depots (Total/DLC/Shared): 3/2/0
+
+-- Main AppID
+addappid(3493540)
+addtoken(3493540, "12729446192953376118")
+
+-- Main Depots
+addappid(3493542, 1, "2ccd7e92c47a0da5141377e6adb2a243206b24e3de8272b887854b93ddb77128") -- (windows)
+setManifestid(3493542, "7440966280815666702", 67635473)
+
+-- DLC's (with depot keys)
+-- Transport Fever 3: Deluxe Upgrade Pack (AppID: 3979620)
+addappid(3979620)
+addtoken(3979620, "4551533807665092458")
+addappid(3979620, 1, "75fe214544650fbe102852bf55d217db1b7e21de232aea73d1b595a4d90fa3b2")
+-- Transport Fever 3: Pre-Order Pack (AppID: 3979630)
+addappid(3979630)
+addtoken(3979630, "14318610725734177629")
+addappid(3979630, 1, "11ae0744206b99f1c4056c7e921c65d0f6d1cb8dd41cf077dc1603fb128b21ce")
