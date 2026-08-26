edd92989cf8e55c4de6e8fca04ce317e1fb10c7d
qiao-lima <qiao-lima@github.com>
2026-08-26 18:11:56 +0800

    Manual Update 4687230

---
+++ 4687230.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4687230 - Sex, Secrets & Used Tech -Original Soundtrack-
+-- Generated 2026-08-26 09:58:28 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(4687230)
+
+-- Main Depots
+addappid(4687231, 1, "d0237f57cadc97279966c9715ea0529c62c26c50b0648ea80fa890ab5351d96e")
+setManifestid(4687231, "8696165317913555382", 446335332)
+addappid(4687232, 1, "019ff02a491005904a32d6d7f6652aaa28ccb2dd82175c6be7fdc25585550e46")
+setManifestid(4687232, "2240238159036993937", 57402263)
