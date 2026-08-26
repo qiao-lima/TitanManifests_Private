218fbdc54846305a500e3451bc8e47b6a6b507a5
qiao-lima <qiao-lima@github.com>
2026-08-26 18:08:04 +0800

    Manual Update 2978500

---
+++ 2978500.lua
@@ -0,0 +1,28 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2978500.lua
+--Gamename 火纹幸存者Fire Survivors
+addappid(2978500) --Mainappid 火纹幸存者Fire Survivors
+addappid(228981, 1, "9afdbd47677424be993f6b516a15ebdb5281aa318114631d14b2b23999ae97ac") --Main Windows Depot 火纹幸存者Fire Survivors
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") --Main Windows Depot 火纹幸存者Fire Survivors
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Main Windows Depot 火纹幸存者Fire Survivors
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Main Windows Depot 火纹幸存者Fire Survivors
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Main Windows Depot 火纹幸存者Fire Survivors
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Main Windows Depot 火纹幸存者Fire Survivors
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Main Windows Depot 火纹幸存者Fire Survivors
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Main Windows Depot 火纹幸存者Fire Survivors
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") --Main Windows Depot 火纹幸存者Fire Survivors
+addappid(229001, 1, "ec23e1a1e1da6205722291f91bf72d1989cb347245e5a14d8d514c4ad5ba4e97") --Main Windows Depot 火纹幸存者Fire Survivors
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") --Main Windows Depot 火纹幸存者Fire Survivors
+addappid(229003, 1, "8e0ded6e1d9481eb286217322054ce7b0b8243daae770f31a30667752b1a3c63") --Main Windows Depot 火纹幸存者Fire Survivors
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Main Windows Depot 火纹幸存者Fire Survivors
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Main Windows Depot 火纹幸存者Fire Survivors
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Main Windows Depot 火纹幸存者Fire Survivors
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Main Windows Depot 火纹幸存者Fire Survivors
+addappid(229010, 1, "9ea8b334311d1b69b962aac11d4cce01f3dfb21722d4198c12314f2d67a0196f") --Main Windows Depot 火纹幸存者Fire Survivors
+addappid(229011, 1, "1752f232024a07db347d180533cf95ce5a1233ae3bad0b0093949174079898df") --Main Windows Depot 火纹幸存者Fire Survivors
+addappid(229012, 1, "421e499f39a617c1a3b6baf11fbc65ffebd541d2c39c38d3bf6ffbd87919ce12") --Main Windows Depot 火纹幸存者Fire Survivors
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") --Main Windows Depot 火纹幸存者Fire Survivors
+addappid(229030, 1, "92f0f88867c8d5f351090eb2d27aeb50e2b621ae8a34708265d2c20c36fbcf5e") --Main Windows Depot 火纹幸存者Fire Survivors
+addappid(229031, 1, "98d01f1eaa90ae08f978994e043bcbbf1f23fc72a325528ca7d722c827c0eb54") --Main Windows Depot 火纹幸存者Fire Survivors
+addappid(229032, 1, "15b3308d4e95d1156182211bab1c823b6df0a06fd74a5c0833abed89bc307cfb") --Main Windows Depot 火纹幸存者Fire Survivors
+addappid(229033, 1, "9a789440bbc16131547ac6c062e5f168f7684da533a2899a71fdfaaeec21efc3") --Main Windows Depot 火纹幸存者Fire Survivors
