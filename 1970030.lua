94757eb06410455b83f40d032caa55ebd23a9795
qiao-lima <qiao-lima@github.com>
2026-08-26 18:08:02 +0800

    Manual Update 1970030

---
+++ 1970030.lua
@@ -0,0 +1,21 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1970030 - 东方华心传 - Touhou Blooming Soul
+-- Generated 2026-08-26 09:51:24 UTC
+-- # Depots (Total/DLC/Shared): 4/2/0
+
+-- Main AppID
+addappid(1970030)
+
+-- Main Depots
+addappid(1970031, 1, "9a290d6e0f330eb905b5d539cc93de2c5f8eba6017776e65a4824e89c8907072") -- (windows, 64-bit)
+setManifestid(1970031, "6739476264412432219", 1567018219)
+addappid(1970032, 1, "49caa29f6ab69a4ee0ebfc9a19ce2ba8074c42c23cc0fff4968ff0df748f5d8e") -- (macos, 64-bit)
+setManifestid(1970032, "833849719068484604", 1719404487)
+
+-- DLC's (with depot keys)
+-- 东方华心传 - OST原声音乐集 (AppID: 4261310)
+addappid(4261310)
+addappid(4261311, 1, "3993e165953abca08ff8a9efbcb25f98c038c741d9101a257a9753a912d29900")
+setManifestid(4261311, "160705413138777814", 142128822)
+addappid(4261312, 1, "0be21574be426483d129852861d484c97adbf842731df5c7e32fbd0d4f1ca4f9")
+setManifestid(4261312, "2932072093062652225", 747417788)
