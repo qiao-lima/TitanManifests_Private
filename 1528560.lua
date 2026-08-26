1be4936a61b9f6584777b7970393e83fdcebe50b
qiao-lima <qiao-lima@github.com>
2026-08-26 18:08:02 +0800

    Manual Update 1528560

---
+++ 1528560.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1528560 - Living together with Fox Demon
+-- Generated 2026-08-26 10:04:38 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(1528560, 1, "67302ff5168d1980e77a5d79564d893ba8821284b9d2cb6ed17e5baefbfb97f9")
+
+-- Main Depots
+addappid(1528561, 1, "8cefcac931f4343c5e9cf54bbfba7b1d25679404d89fd7011664471498f3ec92")
+setManifestid(1528561, "1098159671275521963", 474911629)
