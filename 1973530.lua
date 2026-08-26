b56bada1253c388188d50cc2247b89135f87cac7
qiao-lima <qiao-lima@github.com>
2026-08-26 18:08:03 +0800

    Manual Update 1973530

---
+++ 1973530.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1973530 - Limbus Company
+-- Generated 2026-08-26 10:06:07 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(1973530, 1, "20b19e078463ba53cc525b8fdaf37d7058a260e54d27a09fa6f267614bc85c7e")
+
+-- Main Depots
+addappid(1973531, 1, "b5b48790e4a9b5e1addac9fafe875d35ac0834bfe74548ec481407975ad1a0e6") -- (windows)
+setManifestid(1973531, "2380098781495801520", 10422859570)
