200bfd0931b4fe546d02542689ab5504b3d543cd
qiao-lima <qiao-lima@github.com>
2026-08-26 18:08:04 +0800

    Manual Update 3371770

---
+++ 3371770.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3371770 - Funguys Swarm
+-- Generated 2026-08-26 09:11:11 UTC
+-- # Depots (Total/DLC/Shared): 5/2/2
+
+-- Main AppID
+addappid(3371770, 1, "61ce08e65ec542b762ba148b1cfd2d1cafd3eceb629f7217a46b6c813728e22f")
+
+-- Main Depots
+addappid(3371771, 1, "7bc8b2933822c7530cba86ed05c2e04f175c299c9f90653f00ea439709431c31")
+setManifestid(3371771, "4905498103927212400", 4599786804)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 4005051, 4005052
