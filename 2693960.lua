2b0aaa968eb1e9b77709efe76ee82454bcebc216
qiao-lima <qiao-lima@github.com>
2026-08-26 18:08:03 +0800

    Manual Update 2693960

---
+++ 2693960.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2693960 - Void Gore
+-- Generated 2026-08-26 09:59:20 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2693960)
+
+-- Main Depots
+addappid(2693961, 1, "d423b95c6a6585378de483aa1b512ba8c8cf316961f4c1c3b687afa42f03120f")
+setManifestid(2693961, "1873069172791977059", 69391759)
