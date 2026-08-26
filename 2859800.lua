a4d71d31e85ec74556964741e18f994e4a52ecde
qiao-lima <qiao-lima@github.com>
2026-08-26 18:08:04 +0800

    Manual Update 2859800

---
+++ 2859800.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2859800 - Harbinger
+-- Generated 2026-08-26 09:51:37 UTC
+-- # Depots (Total/DLC/Shared): 4/1/2
+
+-- Main AppID
+addappid(2859800)
+
+-- Main Depots
+addappid(2859801, 1, "d3e41f8b207808e62aaa112a10a82db9d658915653b32a3ab2d11b3dda05f289")
+setManifestid(2859801, "8978604977477050742", 2340229067)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 3235280
