33574b0b603e64e3bab93d4af541f8fc925ea59c
qiao-lima <qiao-lima@github.com>
2026-08-26 18:08:05 +0800

    Manual Update 381210

---
+++ 381210.lua
@@ -0,0 +1,97 @@
+addappid(381210, 1, "f17be424bd1dc965706d5527f803a0c4ae2c7aae87449be6f5bab17b7ac3a20c") 
+addappid(381211, 1, "66477a849ed619510d127ff3f05d4d37b38a04fd7074654337053dfe817798ca") 
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") 
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") 
+
+addappid(492360)
+addappid(381212, 1, "d514d170477029b1fc73e157f9550e3591facfc4508a7568deaf4599fc3a54b4") 
+addappid(489980) -- Dead by Daylight Deluxe Edition
+addappid(509060) -- Dead by Daylight The Last Breath Chapter
+addappid(509061) -- Dead by Daylight Dweard  Sharp Mustache Jake
+addappid(516780) -- Dead by Daylight The Trappers Mask - Chuckles
+addappid(528320) -- Dead by Daylight Angrypug Exclusive
+addappid(528321) -- Dead by Daylight Ohmwrecker Exclusive
+addappid(528322) -- Dead by Daylight Cartoonz Exclusive
+addappid(528323) -- Dead by Daylight Bryce McQuaid Exclusive
+addappid(528324) -- Dead by Daylight H2O Delirious Exclusive
+addappid(530710) -- Dead by Daylight - The 80s Suitcase
+addappid(530711) -- Dead by Daylight - The Halloween Chapter
+addappid(534130) -- Dead by Daylight - S. F. Claudette  S. Meg
+addtoken(534130, "10361710686141983297")
+addappid(534131) -- Dead by Daylight - D. Jake
+addtoken(534131, "11636565156979948298")
+addappid(554380) -- Dead by Daylight - The Bloodstained Sack
+addappid(554381) -- Dead by Daylight - Of Flesh and Mud
+addappid(556360) -- Dead by Daylight 72hrs Exclusive
+addappid(556370) -- Dead by Daylight Adm. Bahroo Exclusive
+addappid(556371) -- Dead by Daylight SXYHXY Exclusive
+addappid(562130) -- Dead by Daylight - BIG Exclusive
+addtoken(562130, "11319676841542177788")
+addappid(566500) -- Dead By Daylight Diabetech Exclusive
+addappid(573240) -- Dead by Daylight PandaTV Exclusive
+addappid(573940) -- Dead by Daylight - Left Behind
+addtoken(573940, "18105380312885266784")
+addappid(577900) -- Dead by Daylight - Taipei Game Show Exclusive
+addtoken(577900, "6140623372922911603")
+addappid(577901) -- Dead by Daylight - Headcase
+addappid(582600) -- Dead by Daylight - Spark of Madness
+addappid(620230) -- Dead by Daylight - Brazil Exclusive
+addappid(627510) -- Dead by Daylight - Charity Case
+addappid(639710) -- Dead by Daylight - Bloodletting
+addtoken(639710, "13687018254219025029")
+addappid(661770) -- Dead by Daylight - A Lullaby for the Dark
+addappid(700280) -- Dead by Daylight LEATHERFACE
+addappid(700282) -- Dead by Daylight - A Nightmare on Elm Street
+addappid(724650) -- Dead by Daylight - Feng Golden Exclusive
+addappid(750380) -- Dead by Daylight - Davids Donkey Jacket
+addappid(750381) -- Dead by Daylight - The Saw Chapter
+addappid(799200) -- Dead by Daylight - Curtain Call
+addappid(925750) -- Dead by Daylight - Shattered Bloodline
+addappid(971790) -- Dead by Daylight - Darkness Among Us
+addappid(1009820) -- Dead by Daylight - Demise of the Faithful chapter
+addappid(1009821) -- Dead by Daylight - Ash vs Evil Dead
+addappid(1089270) -- Dead by Daylight Ghost Face
+addappid(1135280) -- Dead by Daylight - Stranger Things Chapter
+addappid(1199880) -- Dead by Daylight - Cursed Legacy
+addappid(1251000) -- Dead by Daylight - Chains of Hate
+addappid(1324970) -- Dead By Daylight - Silent Hill Chapter
+addappid(1324971) -- Dead by Daylight - Silent Hill Cosmetic Pack
+addtoken(1324971, "18081697420483979648")
+addappid(1408020) -- Dead by Daylight - Descend Beyond chapter
+addappid(1474030) -- Dead by Daylight - A Binding of Kin Chapter
+addappid(1557310) -- Dead by Daylight - All-Kill Chapter
+addappid(1622940) -- Dead by Daylight - Terror Expansion Pack
+addappid(1622941) -- Dead by Daylight - Escape Expansion Pack
+addappid(1634040) -- Dead by Daylight - Resident Evil Chapter
+addappid(1734080) -- Dead by Daylight - Hellraiser chapter
+addappid(1763310) -- Dead by Daylight - Hour of the Witch Chapter
+addappid(1804690) -- Dead by Daylight - Portrait of a Murder
+addappid(1899750) -- Dead by Daylight - Sadako Rising Chapter
+addappid(1985790) -- Dead by Daylight - Roots of Dread Chapter
+addappid(2102730) -- Dead by Daylight - Resident Evil PROJECT W Chapter
+addappid(2198470) -- Dead by Daylight - Forged in Fog Chapter
+addappid(2294610) -- Dead by Daylight - Tools of Torment Chapter
+addappid(2399750) -- Dead by Daylight - End Transmission Chapter
+addappid(2405130) -- Dead by Daylight x Attack on Titan ArmoredPack
+addappid(2405140) -- Dead by Daylight x Attack on Titan War HammerPack
+addappid(2469400) -- Dead by Daylight - Nicolas Cage Chapter Pack
+addappid(2515990) -- Dead by Daylight - Alien Chapter Pack
+addappid(2526530) -- Dead by Daylight - Maddening Darkness Pack
+addappid(2526540) -- Dead by Daylight - Old Wounds Pack
+addappid(2526550) -- Dead by Daylight - Macabre Tales Pack
+addappid(2656010) -- Dead by Daylight - Chucky Chapter
+addappid(2661250) -- Dead by Daylight - Alan Wake Chapter
+addappid(2661270) -- Dead by Daylight - All Things Wicked Chapter
+addappid(2922730) -- Dead by Daylight - Endless Hunt Pack
+addappid(2958440) -- Dead by Daylight - Dungeons  Dragons
+addappid(3024580) -- Dead by Daylight - Tomb Raider Chapter
+addappid(3103150) -- Dead by Daylight - Castlevania Chapter
+addappid(3261720) -- Dead by Daylight - Doomed Course Chapter
+addappid(3448670) -- Dead by Daylight - Tokyo Ghoul
+addappid(3698790) -- Dead by Daylight - Steady Pulse
+addappid(3796620) -- Dead by Daylight Five Nights at Freddys
+addappid(3884020) -- Dead by Daylight The Walking Dead
+addappid(4019590) -- Dead by Daylight - Sinister Grace
+addappid(4318580) -- Dead by Daylight Stranger Things Chapter 2
+addappid(4791830) -- Dead by Daylight Jason
+addappid(5060920) -- Dead by Daylight Chorus of Sin
