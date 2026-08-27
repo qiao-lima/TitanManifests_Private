721ce7ec2fdaeb3227ac86a54747a66eda289419
qiao-lima <qiao-lima@github.com>
2026-08-27 08:55:14 +0800

    Manual Update 1583720

---
+++ 1583720.lua
@@ -0,0 +1,23 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1583720 - Entropy : Zero 2
+-- Generated 2026-08-27 00:53:26 UTC
+-- # Depots (Total/DLC/Shared): 7/4/0
+
+-- Main AppID
+addappid(1583720, 1, "6dca9d331195526081033b474dc9d7f8e1f92dd12e84e6a1da5278c7d7958e82")
+
+-- Main Depots
+addappid(1583721, 1, "5557bc8565d9e8785c7b3905b86faf8b5614b9dca33feb5002d16c9671947761")
+setManifestid(1583721, "348948426661780131", 13446517411)
+addappid(1583722, 1, "d42338c0e8e8d4d2539f838a82c9574e5a8cdb0a9d148e0dc01a21a92e59ea65") -- (linux)
+setManifestid(1583722, "4062498511763705580", 130706526)
+addappid(1583723, 1, "683ae974b6ca3e060888ff6f56206a42a01d4f4a158e397bdf39301432c40c15") -- (windows)
+setManifestid(1583723, "7387651231230992010", 142924553)
+
+-- DLC's (with depot keys)
+-- Entropy : Zero 2 Soundtrack (AppID: 2143080)
+addappid(2143080)
+addappid(2143081, 1, "5e8230c877b5c9c4bb0e1e8bd37da05cd402a7dce73d85ebaadf4b14f58b5691")
+setManifestid(2143081, "444103255941205270", 219469880)
+
+-- Missing Depots (We don't have the keys yet lol): 3170960, 3208310, 3208320
