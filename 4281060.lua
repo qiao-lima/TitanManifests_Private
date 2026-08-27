ffc5a160eee35eca7b72895cadd3d8bfff7f6b1c
qiao-lima <qiao-lima@github.com>
2026-08-27 08:55:17 +0800

    Manual Update 4281060

---
+++ 4281060.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4281060.lua
+--Gamename Police Officer
+addappid(4281060) --Mainappid Police Officer
+addappid(4281061, 1, "e3641aac8c628b8c06445994bc366962018ac491ae4e72578a63ee45f0e8550b") --Main Windows Depot Police Officer
+setManifestid(4281061, "2205971080125955834", 1389354688)
+--Missing Dlcs
+--Dlcname Police Officer Playtest
