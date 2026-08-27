3bc6eb5a7bb9052b8d9cf45b73d6ad83c19865de
qiao-lima <qiao-lima@github.com>
2026-08-27 08:55:29 +0800

    Manual Update 4798110

---
+++ 4798110.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4798110.lua
+--Gamename Order Picker Simulator
+addappid(4798110) --Mainappid Order Picker Simulator
+addappid(4798111, 1, "e5fb88020a6deb355a2498af9a3841e68481918db66a48899e4321d0f5b97bcb") --Main Windows Depot Order Picker Simulator
+setManifestid(4798111, "8380455168632235311", 922419824)
+--Missing Dlcs
+--Dlcname Order Picker Simulator Demo
