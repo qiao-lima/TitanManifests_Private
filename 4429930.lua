61684c42b709fc740a9816084374d82565b44b68
qiao-lima <qiao-lima@github.com>
2026-08-27 08:55:18 +0800

    Manual Update 4429930

---
+++ 4429930.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4429930 - Water You Doing?
+-- Generated 2026-08-27 00:05:42 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(4429930)
+
+-- Main Depots
+addappid(4429931, 1, "5fb5d51f8f8bd6def247ca6a2ac00c8ff4001c739567bb2d6bfcfe0142192c55")
+setManifestid(4429931, "5196963219976475783", 4160012957)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
