f2859cece38ee227a6a05461938561a60dbb8b78
qiao-lima <qiao-lima@github.com>
2026-08-27 08:55:16 +0800

    Manual Update 4159380

---
+++ 4159380.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4159380 - Hentai Isekai: The Sex Curse
+-- Generated 2026-08-27 00:50:30 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4159380)
+
+-- Main Depots
+addappid(4159381, 1, "4f02d8ad8f5446240822ca3e959a5cdac27b17bcf79e52d008542217eb10b73b")
+setManifestid(4159381, "1380073821535321550", 1115983217)
