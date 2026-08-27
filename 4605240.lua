35839ac1789a831775bb714e9258639d81571bd4
qiao-lima <qiao-lima@github.com>
2026-08-27 08:55:28 +0800

    Manual Update 4605240

---
+++ 4605240.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4605240.lua
+--Gamename Make my Sandwich
+addappid(4605240) --Mainappid Make my Sandwich
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Main Windows Depot Make my Sandwich
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Main Windows Depot Make my Sandwich
