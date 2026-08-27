d850b83b5e12f2cbeb3b98e24f3197401e44d465
qiao-lima <qiao-lima@github.com>
2026-08-27 08:55:29 +0800

    Manual Update 4706940

---
+++ 4706940.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4706940 - Furry Adventure!
+-- Generated 2026-08-27 00:51:36 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4706940)
+
+-- Main Depots
+addappid(4706941, 1, "02a919e80f852adb0c291ed80eff5f9660142e7c716e177e02a3473abcf64003")
+setManifestid(4706941, "3879135538740222037", 3242538117)
