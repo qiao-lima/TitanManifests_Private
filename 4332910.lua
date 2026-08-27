f0cbe385f992ce6125d939a29ce384153a816c73
qiao-lima <qiao-lima@github.com>
2026-08-27 08:55:17 +0800

    Manual Update 4332910

---
+++ 4332910.lua
@@ -0,0 +1,19 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4332910 - Twisted Party
+-- Generated 2026-08-27 00:30:19 UTC
+-- # Depots (Total/DLC/Shared): 3/0/1
+
+-- Main AppID
+addappid(4332910)
+
+-- Main Depots
+addappid(4332911, 1, "ae96627c80fd387fe7fa25b97acf62a2729ab16191a71bf61a221611c229847b") -- (windows)
+setManifestid(4332911, "9018700253703794495", 726514064)
+
+-- DLC's (no depot keys required)
+addappid(5020450) -- Twisted Party - Supporter Pack
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 4332912
