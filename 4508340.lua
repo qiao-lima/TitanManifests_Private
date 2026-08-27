04cda5e8833d786617808a5a5f4da267a4f9b7dd
qiao-lima <qiao-lima@github.com>
2026-08-27 08:55:28 +0800

    Manual Update 4508340

---
+++ 4508340.lua
@@ -0,0 +1,14 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4508340 - NTE: Neverness to Everness
+-- Generated 2026-08-27 00:53:38 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4508340, 1, "af624b139b152ff4f49c19f7ab34ab4f3aae321fca58884387871b8f6ae3ff99")
+
+-- Main Depots
+addappid(4508341, 1, "08a13e474db2e35584a634cdc868d2047fdddfa51346361442f6e904538912d8")
+setManifestid(4508341, "1158643254741858404", 95925092991)
+
+-- DLC's (no depot keys required)
+addappid(4869730) -- NTE: Neverness to Everness Starter Pack
