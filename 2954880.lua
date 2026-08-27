9316b924506b0fa278e6ad390528700b8f782bfa
qiao-lima <qiao-lima@github.com>
2026-08-27 08:55:15 +0800

    Manual Update 2954880

---
+++ 2954880.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2954880 - VRKanojo
+-- Generated 2026-08-27 00:52:12 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2954880, 1, "0ab5faf034b4df02b690f7cc6477f786b419b7ef08d2132d8b28fa22741f6e17")
+
+-- Main Depots
+addappid(2954881, 1, "0e0fd245c24e73b77f90071292cc1a0dff3a9352a4e1b565da232a1ed2931733")
+setManifestid(2954881, "2998826506356523523", 5900837124)
