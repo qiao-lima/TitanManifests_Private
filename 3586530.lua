7002f794053d56c745a579246abdb45cb807ab96
qiao-lima <qiao-lima@github.com>
2026-08-28 19:44:36 +0800

    Manual Update 3586530

---
+++ 3586530.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3586530 - 【REDACTED】：SexCP-069
+-- Generated 2026-08-27 13:55:51 UTC
+-- # Depots (Total/DLC/Shared): 3/2/0
+
+-- Main AppID
+addappid(3586530, 1, "cd6b6104d7d382f2481ca07cb1c1a1e867ccc49ddf23fc726c9d8183776b100e")
+
+-- Main Depots
+addappid(3586531, 1, "123b280cb963466ebe7e40e6c874c83a197df7738a3d61c0fbcb9f9d4ae3e6a4")
+setManifestid(3586531, "3840836390728974651", 1412721732)
+
+-- Missing Depots (We don't have the keys yet lol): 4123651, 4123652
