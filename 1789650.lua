9f5a04dfbf3cf9691c0cbc0ba4729dd027fc751a
qiao-lima <qiao-lima@github.com>
2026-08-28 19:44:33 +0800

    Manual Update 1789650

---
+++ 1789650.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1789650 - NEKO-MIMI SWEET HOUSEMATES Vol. 1
+-- Generated 2026-08-27 13:55:10 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(1789650)
+
+-- Main Depots
+addappid(1789653, 1, "9e29aae300a328155384435ec75eba7275bd804a895b60314ed2058c756a62c1")
+setManifestid(1789653, "8134899487426396739", 3377366055)
