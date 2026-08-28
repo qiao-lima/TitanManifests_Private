8084de86769b5627866b6c18f2006753d504068c
qiao-lima <qiao-lima@github.com>
2026-08-28 19:44:33 +0800

    Manual Update 1478500

---
+++ 1478500.lua
@@ -0,0 +1,16 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1478500 - Big Walk
+-- Generated 2026-08-27 13:38:04 UTC
+-- # Depots (Total/DLC/Shared): 3/0/1
+
+-- Main AppID
+addappid(1478500, 1, "1174edcf23ff4903776c928407704c67bf13b9f75a546c3587fe5413055b2ca8")
+
+-- Main Depots
+addappid(1478501, 1, "f1f21164a742a9a2ce6614fc29dbe3fda4a2365c1b73e88a2738bec5cca99d53") -- (windows, 64-bit)
+setManifestid(1478501, "6842760435702572030", 3797180235)
+addappid(1478502, 1, "595e8e546355fab56cf985a75b0c428014f60327b70dc584cba7a3bfbebe3c71") -- (macos, 64-bit)
+setManifestid(1478502, "2421718886436891408", 3988715954)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
