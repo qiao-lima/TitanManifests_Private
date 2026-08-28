355ec6e87e2df4040a569865a8bf218cf93eec71
qiao-lima <qiao-lima@github.com>
2026-08-28 19:44:35 +0800

    Manual Update 2739590

---
+++ 2739590.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2739590 - Mad Island
+-- Generated 2026-08-27 13:56:09 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2739590, 1, "cbed7a4927e60f35fa4fc303117841cc0b0480acaedd95fa870f3dc92d474cb5")
+
+-- Main Depots
+addappid(2739591, 1, "b4965a72b078a66e012d79a8af2548c3201e72b2fc234cc1e562863063dc42b8") -- Japanese (windows, 64-bit)
+setManifestid(2739591, "3631136809996453116", 1119946120)
