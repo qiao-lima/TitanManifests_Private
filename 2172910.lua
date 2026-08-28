7b311c8c60fb91e1b00d488a2e5120a3750f81fd
qiao-lima <qiao-lima@github.com>
2026-08-28 19:44:34 +0800

    Manual Update 2172910

---
+++ 2172910.lua
@@ -0,0 +1,9 @@
+-- htz11
+addappid(2172910)
+addappid(2172911,1,"135e5b2b665c27b9c8e25731dde82adac1fe04efca78531c66e8152e461084cf")
+addappid(4196700)
+addappid(4196710)
+addappid(4196720)
+addappid(4199240)
+addappid(4280300)
+addappid(4280310)
