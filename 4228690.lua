64ff085f138b411a671fe1d9c03fdf69b0df2938
qiao-lima <qiao-lima@github.com>
2026-08-30 15:34:56 +0800

    Manual Update 4228690

---
+++ 4228690.lua
@@ -0,0 +1,16 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4228690 - Black Gold - Shining Town
+-- Generated 2026-08-29 11:57:36 UTC
+-- # Depots (Total/DLC/Shared): 4/0/3
+
+-- Main AppID
+addappid(4228690, 1, "e2869a3be9814254c88a8d36e8068def7789c0b29050ccd12f0686eb451bf068")
+
+-- Main Depots
+addappid(4228691, 1, "7cd87fd9c8a5ef1a15f81f75bf5e8cc7c914b7a767d5da0d86d1706bf12eeb15") -- (windows, 64-bit)
+setManifestid(4228691, "466554627787853633", 1594903903)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- (windows)
