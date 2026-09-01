189f4fa922c5bfe98821780936cda120679bbcac
qiao-lima <qiao-lima@github.com>
2026-09-01 19:49:00 +0800

    Manual Update 2858500

---
+++ 2858500.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2858500 - Rhythm & Beats
+-- Generated 2026-09-01 11:34:45 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2858500)
+
+-- Main Depots
+addappid(2858501, 1, "10072e193dfe97d0545da92e7f1faeefea9dc9786051c50685788a998244a46c") -- (windows, 64-bit)
+setManifestid(2858501, "2730863741397385900", 172833853)
