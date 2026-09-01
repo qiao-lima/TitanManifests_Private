9a7cadeda97647543c9e549d5b878d106186ba15
qiao-lima <qiao-lima@github.com>
2026-09-01 19:53:25 +0800

    Manual Update 990380

---
+++ 990380.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 990380 - METAL EDEN
+-- Generated 2026-09-01 11:33:25 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(990380, 1, "9eca570111312f147f6fc5dd5ef025f443074700d31fbbc3e68aade11db415a9")
+
+-- Main Depots
+addappid(990381, 1, "8609143fc4bd7b68bc154ba5e0ca2a7f6a148b3ff593cbaf70b9938821696267") -- (windows, 64-bit)
+setManifestid(990381, "4578285982077398722", 22217967017)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
