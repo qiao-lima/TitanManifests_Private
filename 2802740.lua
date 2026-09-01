4c1fab744ff5200b122e486ab2671cfc6981553a
qiao-lima <qiao-lima@github.com>
2026-09-01 19:49:00 +0800

    Manual Update 2802740

---
+++ 2802740.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2802740 - Heave Ho 2
+-- Generated 2026-09-01 11:27:44 UTC
+-- # Depots (Total/DLC/Shared): 3/2/0
+
+-- Main AppID
+addappid(2802740, 1, "882aeadbe5eb3b06647eaa8f9be92841df62a7a4cb815f9ff2f572d569b74ccc")
+
+-- Main Depots
+addappid(2802741, 1, "1d791ebf9bb239893588e966e88b93d3f08eac26f5dbb2a315804fc66c9775fd") -- (windows, 64-bit)
+setManifestid(2802741, "851173964418844587", 3896700772)
+
+-- Missing Depots (We don't have the keys yet lol): 4926281, 4926282
