bc181b5a84025af4f62fa69fa255737bead5127c
qiao-lima <qiao-lima@github.com>
2026-09-01 19:50:42 +0800

    Manual Update 3946950

---
+++ 3946950.lua
@@ -0,0 +1,22 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3946950 - Click the Button
+-- Generated 2026-09-01 11:31:16 UTC
+-- # Depots (Total/DLC/Shared): 3/1/0
+
+-- Main AppID
+addappid(3946950, 1, "03728256e00d61346ad7e35116133db568b43d2378a8d18d3166a68df747cac1")
+
+-- Main Depots
+addappid(3946951, 1, "dbc3ed41113fd3842d880a61da7fb1ceb860fbf0d4fa251ff0d05185d1273e46")
+setManifestid(3946951, "3980636186183911639", 165704044)
+addappid(3946952, 1, "dc25330e5290585ead10914744ca8dc82e95d323a0ae7a922f42ed3ea13149e5") -- (linux)
+setManifestid(3946952, "7862337102796203886", 130178236)
+
+-- DLC's (no depot keys required)
+addappid(5110650) -- Click the Button - Supporter Pack
+
+-- DLC's (with depot keys)
+-- Click the Button Demo (AppID: 4805850)
+addappid(4805850)
+addappid(4805851, 1, "9a8d8ef9feb27a2b9ae3a9ef3c4d3241c9ea71e55cb76a9007de5226b50a44cb")
+setManifestid(4805851, "606837524329959361", 152512228)
