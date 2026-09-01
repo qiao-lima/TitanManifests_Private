5893ee837708fe2b091defc04c098cdfb64b3939
qiao-lima <qiao-lima@github.com>
2026-09-01 19:48:58 +0800

    Manual Update 1918480

---
+++ 1918480.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1918480 - Dragon Sleuth Brittany
+-- Generated 2026-09-01 11:46:24 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(1918480, 1, "b4058b69739749e667933774e600c86f967ec0b65c3b4e4984f8bdd6fa2c5047")
+
+-- Main Depots
+addappid(1918481, 1, "13654ded73aa769208dd59982159daecf9ed03aebb9454e1a237b3c852758e06") -- (windows)
+setManifestid(1918481, "1214085844944080805", 6941460180)
+addappid(1918482, 1, "248f4132aef64670fa4ee220fa629c50c7ac8a4186038873f6ffc865b9de3c52") -- (linux)
+setManifestid(1918482, "7337779979000373752", 6987754061)
+addappid(1918483, 1, "6d6e8d28f299f609aa9815a6f5ba84ef106baac847feef75af2a30fc15dd233d") -- (macos)
+setManifestid(1918483, "1800252412397128837", 6923991526)
