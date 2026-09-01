ac1c4ef9fc5448f8d5a060f321bc12440ae443dd
qiao-lima <qiao-lima@github.com>
2026-09-01 19:49:03 +0800

    Manual Update 3718190

---
+++ 3718190.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3718190 - スーパーリアル麻雀 Venus Returns
+-- Generated 2026-09-01 11:37:51 UTC
+-- # Depots (Total/DLC/Shared): 2/0/1
+
+-- Main AppID
+addappid(3718190)
+
+-- Main Depots
+addappid(3718191, 1, "4d7b23dc80a9a38ff9cf30b72a4a83dc4c564b43420112e20031c2c23cf8bb43") -- (windows, 64-bit)
+setManifestid(3718191, "2787620689882012763", 2395519085)
+
+-- DLC's (no depot keys required)
+addappid(3991320) -- スーパーリアル麻雀 Venus Returns - VRモード追加DLC
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
