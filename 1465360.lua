ce0ed62dc477f5b1251459c954b201a012f715fd
qiao-lima <qiao-lima@github.com>
2026-09-01 19:48:58 +0800

    Manual Update 1465360

---
+++ 1465360.lua
@@ -0,0 +1,58 @@
+-- Main AppID
+addappid(1465360, 1, "73f8f030255f23904e82de2afed639196197db79e1dc6e22d0d7e8eeb220fbd3")
+
+-- Main Depots
+addappid(1465361, 1, "0dd93272d1d47d5943b93081730b61e39d20a49e42873ca743dcd30322161fb0") -- (windows, 64-bit)
+setManifestid(1465361, "3353417886763760565", 72761299834)
+addappid(1465362, 1, "924ebaf5547a976d7080e139e3ce420ce10a76bdcd89d74fa74a98fb7312b7fc") -- (macos, 64-bit)
+setManifestid(1465362, "8920584982304117109", 52401032402)
+
+-- DLC's (no depot keys required)
+addappid(1533000) -- SnowRunner - Sabertooth Livery
+addappid(1533001) -- SnowRunner - Navistar 5000-MV Tractor
+addappid(1533002) -- SnowRunner - High Roller Pack
+addappid(1533003) -- SnowRunner - Loaded Dice Vinyl Wrap
+addappid(1533004) -- SnowRunner - Scorched Vinyl Wrap
+addappid(1533005) -- SnowRunner - True Colors Vinyl Wrap
+addappid(1533006) -- SnowRunner - Season 1: Search & Recover
+addappid(1533007) -- SnowRunner - Season 2: Explore & Expand
+addappid(1533008) -- SnowRunner - Season 3: Locate & Deliver
+addappid(1533009) -- SnowRunner - Season 4: New Frontiers
+addappid(1533010) -- SnowRunner - Western Star 49X
+addappid(1533011) -- SnowRunner - Clasico Pack
+addappid(1585810) -- SnowRunner - Anniversary DLC
+addappid(1585811) -- SnowRunner - TATRA Dual Pack
+addappid(1585812) -- SnowRunner - Season 5: Build & Dispatch
+addappid(1585813) -- SnowRunner - Season 6: Haul & Hustle
+addappid(1585814) -- SnowRunner - Season 7: Compete & Conquer
+addappid(1585815) -- SnowRunner - Season 8: Grand Harvest
+addappid(1585816) -- SnowRunner - GMC Brigadier
+addappid(1613110) -- SnowRunner - Jeep Dual Pack
+addappid(1613111) -- SnowRunner - Burning Bright Vinyl Wrap Pack
+addappid(1785850) -- SnowRunner - Land Rover Dual Pack
+addappid(1838500) -- SnowRunner - Crocodile Pack
+addappid(2058880) -- SnowRunner - Rezvani Hercules
+addappid(2058881) -- SnowRunner - Western Star Wolf Pack
+addappid(2154170) -- SnowRunner - Save the Day Vinyl Wrap Pack
+addappid(2154171) -- SnowRunner - Season 9: Renew & Rebuild
+addappid(2154172) -- SnowRunner - Season 10: Fix & Connect
+addappid(2154173) -- SnowRunner - Season 11: Lights & Cameras
+addappid(2154174) -- SnowRunner - Season 12: Public Energy
+addappid(2353720) -- SnowRunner - The Mastodon
+addappid(2353721) -- SnowRunner - Kenworth W990
+addappid(2353722) -- SnowRunner - Jack of All Treads Tire Pack
+addappid(2353723) -- SnowRunner - Kenworth Dual Pack
+addappid(2709430) -- SnowRunner - Salt of the Earth Vinyl Wrap Pack
+addappid(2709450) -- SnowRunner - Season 13: Dig & Drill
+addappid(2709460) -- SnowRunner - Season 14: Reap & Sow
+addappid(2709470) -- SnowRunner - Season 15: Oil & Dirt
+addappid(2709480) -- SnowRunner - Season 16: High Voltage
+addappid(2856380) -- SnowRunner - The Atom
+addappid(3023640) -- SnowRunner - Rezvani Tank
+addappid(3398280) -- SnowRunner - Mack Dual Pack
+addappid(3623240) -- SnowRunner - Freightliner & Western Star Dual Pack
+addappid(3623250) -- SnowRunner - Dragon Wagons Dual Pack
+addappid(3777600) -- SnowRunner – Pathfinders Vinyl Wrap Pack
+addappid(3777610) -- SnowRunner - Season 17: Repair & Rescue
+addappid(3777620) -- SnowRunner - Season 18: Patch & Power
+addappid(3777650) -- SnowRunner - Mercedes-Benz Trucks Dual Pack 1
