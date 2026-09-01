c3aa9ba857a0d4e235500b28acbe44be7c9e4886
qiao-lima <qiao-lima@github.com>
2026-09-01 19:50:43 +0800

    Manual Update 427410

---
+++ 427410.lua
@@ -0,0 +1,24 @@
+-- Generated with Luie @ https://lua.tools/
+-- 427410 - Abiotic Factor
+-- Generated 2026-09-01 11:36:00 UTC
+-- # Depots (Total/DLC/Shared): 5/0/4
+
+-- Main AppID
+addappid(427410, 1, "55e502d9f49fcda3d0661ea4eb0d785b4474c2f15ed585becfadf5fb80c59844")
+
+-- Main Depots
+addappid(427413, 1, "ff587f4073cb5fc9fb07cc1f4a0932a48fdbcab584bf8f74339e959cedc42e2b")
+setManifestid(427413, "8177575349509936967", 6916314532)
+
+-- DLC's (no depot keys required)
+addappid(3018120) -- Abiotic Factor - Early Access Pack
+addappid(3641780) -- Abiotic Factor - Supporter's Edition
+addappid(4026970) -- Abiotic Factor - Temple of Stone
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- (windows)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- (windows)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Token-locked DLCs (couldn't read depots): 4687390
