9a1738dd4468500ce93c8744ca922a00079aa146
qiao-lima <qiao-lima@github.com>
2026-09-01 19:48:58 +0800

    Manual Update 1245620

---
+++ 1245620.lua
@@ -0,0 +1,44 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1245620 - ELDEN RING
+-- Generated 2026-09-01 10:46:59 UTC
+-- # Depots (Total/DLC/Shared): 12/6/2
+
+-- Main AppID
+addappid(1245620, 1, "10f99dee41d0f96e312cf8c574420de0a018b6a26caf4c3e3aefa13cf90c1246")
+
+-- Main Depots
+addappid(1245621, 1, "5f560fca048a138487e1e634a1e60693a7ad3ae7318c31731d147f16e093f10b")
+setManifestid(1245621, "8950790353897081369", 55036294732)
+addappid(1245623, 1, "b27090a72caa5f3cf2e1f41a81c4bc1c422a6ac277e1cd3a6f1ea68200288d4b")
+setManifestid(1245623, "8755867293550015179", 87232496)
+addappid(1245624, 1, "e194ce5e5f83c427743781c1708c4bce6683d5d65521059f6e4ca64a8f36f3cd")
+setManifestid(1245624, "6170913735491975669", 87232507)
+
+-- DLC's (no depot keys required)
+addappid(1922350) -- ELDEN RING - EU Pre-Purchase DLC Bundle
+addappid(2778590) -- ELDEN RING Shadow of the Erdtree Premium Bundle
+addappid(2855530) -- ELDEN RING Shadow of the Erdtree Bonus Gesture
+
+-- DLC's (with depot keys)
+-- ELDEN RING Digital Artbook & Original Soundtrack (AppID: 1896300)
+addappid(1896300)
+addappid(1896300, 1, "946e5c4e2c91a3ed363efc688218994f7fa6e8fdd56aa8b5752456d3b28e0543")
+setManifestid(1896300, "7130086783926325209", 1004172702)
+-- ELDEN RING Adventure Guide (AppID: 1896320)
+addappid(1896320)
+addappid(1896320, 1, "de7086b709fdfc7d151bfce5d07509fd80a6c318a21e8fb1e820db5b1cda5860")
+setManifestid(1896320, "5664821938092422533", 869629166)
+-- ELDEN RING Shadow of the Erdtree (AppID: 2778580)
+addappid(2778580)
+addappid(2778580, 1, "9f1556645ea8ef43529f920cf02a2682a6da5756b29e630ba376a0cde24e3908")
+setManifestid(2778580, "1674424364022381183", 16125337789)
+-- ELDEN RING Shadow of the Erdtree - Artbook & Soundtrack (AppID: 2855520)
+addappid(2855520)
+addappid(2855520, 1, "476eca9191866d6743aa7ad82d4d7ce1fcd5e0f0613f2f4b6559635d68f8c0e3")
+setManifestid(2855520, "2785904640065824767", 1089487349)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 1245622, 1245625, 3655690
