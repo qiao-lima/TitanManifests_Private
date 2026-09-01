a764cb600631fdc6480ca13dac5513f3d0d06496
qiao-lima <qiao-lima@github.com>
2026-09-01 19:50:44 +0800

    Manual Update 4341260

---
+++ 4341260.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4341260 - WebCam: Behind The Screen
+-- Generated 2026-09-01 10:55:02 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(4341260)
+
+-- Main Depots
+addappid(4341261, 1, "fe9cbb5dc0323d133589a0ee47c89a00709c5d224e1a498934327e5608bba505")
+setManifestid(4341261, "2364999512312755132", 3596788986)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
