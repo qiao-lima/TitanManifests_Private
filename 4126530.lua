d790e2896d5f8a822f36534b99458b65987d4285
qiao-lima <qiao-lima@github.com>
2026-09-02 23:24:46 +0800

    Manual Update 4126530

---
+++ 4126530.lua
@@ -0,0 +1,3 @@
+-- htz11
+addappid(4126530,1,"2b5b591c2a8fc29ade1baaaca264a55fd9b0007553314e53873af7b5df284b58")
+addappid(4126531,1,"fbf9f0d9fbabcf8e9c4fbe168adeb6fec9b70e3a6cebc10cf6a572412c789007")
