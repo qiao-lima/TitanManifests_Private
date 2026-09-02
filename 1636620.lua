ab3856b87d89361623503b4c8a2bb29d6062e939
qiao-lima <qiao-lima@github.com>
2026-09-02 23:24:44 +0800

    Manual Update 1636620

---
+++ 1636620.lua
@@ -0,0 +1,12 @@
+-- 1636620's Lua and Manifest Created by Hubcap Manifest
+-- Cognition Method
+-- Created: September 01, 2026 at 20:00:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1636620, 1, "28b698ec2795b6bae5077572053b0bd6ef7596a93fc5764558894b6ef86765e7") -- Cognition Method
+-- MAIN APP DEPOTS
+addappid(1636621, 1, "5e83b630f4600c54f4fe85bf51e37a10e6d7be95a997317d1807484690d7b788") -- Depot 1636621
+setManifestid(1636621, "3167901717390316191", 17466410112)
