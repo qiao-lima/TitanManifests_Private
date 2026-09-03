743b9baabcc940e9aa76d4ccf06503cab4a5cc63
qiao-lima <qiao-lima@github.com>
2026-09-03 11:50:22 +0800

    Manual Update 2864560

---
+++ 2864560.lua
@@ -0,0 +1,39 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2864560 - Rune Factory: Guardians of Azuma
+-- Generated 2026-09-02 14:28:43 UTC
+-- # Depots (Total/DLC/Shared): 3/1/1
+
+-- Main AppID
+addappid(2864560, 1, "99528a47ca0f1e059cf25297426d2925593da6f516f2d5900fab0b84bb9082ce")
+
+-- Main Depots
+addappid(2864561, 1, "12f559d70c2bf66c2fe46e7363637bf089c3d3ca08c883741f9f7e3f20fdddbe") -- (windows)
+setManifestid(2864561, "5015965320437452414", 13339164296)
+
+-- DLC's (no depot keys required)
+addappid(3010040) -- Rune Factory: Guardians of Azuma - Seasons of Love: Pilika's Bonding Stories
+addappid(3139940) -- Rune Factory: Guardians of Azuma - Seasons of Love: Cuilang's Bonding Stories
+addappid(3139950) -- Rune Factory: Guardians of Azuma - Seasons of Love: Marvelous Swimsuit Bundle
+addappid(3139970) -- Rune Factory: Guardians of Azuma - Sakuna DLC 
+addappid(3139980) -- Rune Factory: Guardians of Azuma - Festive Attire & Dark Woolby Bundle
+addappid(3139990) -- Rune Factory: Guardians of Azuma - Rune Factory 4 Hero Outfit Bundle
+addappid(3140000) -- Rune Factory: Guardians of Azuma - Ulalaka's Rune Factory 4 Outfit (Margaret)
+addappid(3140010) -- Rune Factory: Guardians of Azuma - Iroha's Rune Factory 4 Outfit (Xiao Pai)
+addappid(3140020) -- Rune Factory: Guardians of Azuma - Matsuri's Rune Factory 4 Outfit (Forte)
+addappid(3140030) -- Rune Factory: Guardians of Azuma - Kanata's Rune Factory 4 Outfit (Amber)
+addappid(3140040) -- Rune Factory: Guardians of Azuma - Hina's Rune Factory 4 Outfit (Clorica)
+addappid(3140240) -- Rune Factory: Guardians of Azuma - Clarice's Rune Factory 4 Outfit (Dolce)
+addappid(3402710) -- Rune Factory: Guardians of Azuma - Iroha's Teahouse Treats
+addappid(3717920) -- Rune Factory: Guardians of Azuma - Digital Deluxe DLC Set
+addappid(3717930) -- Rune Factory: Guardians of Azuma - Super Digital Deluxe DLC Set
+addappid(3717940) -- Rune Factory: Guardians of Azuma - Rune Factory 4 Bachelorette Outfit Bundle
+addappid(3966040) -- Rune Factory: Guardians of Azuma - STORY OF SEASONS Bundle
+
+-- DLC's (with depot keys)
+-- Rune Factory: Guardians of Azuma - Art Book (AppID: 3717950)
+addappid(3717950)
+addappid(3717950, 1, "b12e4d792b17735ff82a052e418ef9f13f6d10a415e43c14c5f636d5a6494a7e")
+setManifestid(3717950, "524864029495544115", 661846986)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
