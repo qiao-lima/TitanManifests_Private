ec08b9519f59389fae9720a58fa81f40e654a7a6
qiao-lima <qiao-lima@github.com>
2026-09-04 07:13:28 +0800

    Manual Update 1778820

---
+++ 1778820.lua
@@ -0,0 +1,66 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1778820 - TEKKEN 8
+-- Generated 2026-09-03 10:05:35 UTC
+-- # Depots (Total/DLC/Shared): 10/6/2
+
+-- Main AppID
+addappid(1778820, 1, "d9720dee1d279ee1f25cb6bb6268d15a1c8a0295b4cab9bfd879db48ba5e48fe")
+
+-- Main Depots
+addappid(1778821, 1, "c7c87be587ce9a828a290d82a8650ff9edd77f53919f2f8d2f686f1e586417b7")
+setManifestid(1778821, "363231968272087719", 139069241910)
+
+-- DLC's (no depot keys required)
+addappid(2512940) -- TEKKEN 8 - Avatar Costume: Paul Phoenix Set
+addappid(2512950) -- TEKKEN 8 - Character Costume: Gold Suit Pack
+addappid(2512960) -- TEKKEN 8 - Avatar Costume: Classic TEKKEN T-Shirt Set
+addappid(2512970) -- TEKKEN 8 - Avatar Skin: Kazuya Mishima
+addappid(2512980) -- TEKKEN 8 - Avatar Skin: Jun Kazama
+addappid(2512990) -- TEKKEN 8 - Avatar Skin: Jin Kazama
+addappid(2513000) -- TEKKEN 8 - Avatar Skin: Kinjin
+addappid(2518100) -- TEKKEN 8 - Avatar Skin: Tetsujin
+addappid(2518110) -- TEKKEN 8 - Avatar Skin: Mokujin
+addappid(2732280) -- TEKKEN 8 - Eddy Gordo
+addappid(2741980) -- TEKKEN 8 - Deluxe Edition Upgrade Pack
+addappid(2741990) -- TEKKEN 8 - Ultimate Pack
+addappid(2891170) -- TEKKEN 8 - Lidia Sobieska
+addappid(3024980) -- TEKKEN 8 - Heihachi Mishima
+addappid(3145880) -- TEKKEN 8 - Clive Rosfield (FINAL FANTASY series)
+addappid(3303020) -- TEKKEN 8 - Collaboration Set Clive Rosfield & PHOENIX GATE (FINAL FANTASY series)
+addappid(3417000) -- TEKKEN 8 - Anna Williams
+addappid(3417230) -- TEKKEN 8 - Season 1 Character Pass
+addappid(3417240) -- TEKKEN 8 - Season 1 Character & Stage Pass
+addappid(3417250) -- TEKKEN 8 - Season 2 Character Pass
+addappid(3417270) -- TEKKEN 8 - Season 2 Character & Stage Pass
+addappid(3417280) -- TEKKEN 8 - Customization Item Pack
+addappid(3564540) -- TEKKEN 8 - Fahkumram
+addappid(3775990) -- TEKKEN 8 - Armor King
+addappid(4001560) -- TEKKEN 8 - Miary Zo
+addappid(4129090) -- TEKKEN 8 - Character Costume: Aurora Outfit Pack
+addappid(4129100) -- TEKKEN 8 - Kunimitsu
+addappid(4129110) -- TEKKEN 8 - Season 3 Pass
+addappid(4536140) -- TEKKEN 8 - Bob
+
+-- DLC's (with depot keys)
+-- TEKKEN 8 - GENMAJI TEMPLE (AppID: 3025000)
+addappid(3025000)
+addappid(3025000, 1, "afe85b92428daa47e2254a44004a7f97b0defce56555bcca59a44a94e357f373")
+setManifestid(3025000, "7282372065457185729", 34884128)
+-- TEKKEN 8 - PHOENIX GATE (FINAL FANTASY series) (AppID: 3145890)
+addappid(3145890)
+addappid(3145890, 1, "8587b0c4c63e914df99aedf5179181d680a74c88324d4e86a80cb08baff28efb")
+setManifestid(3145890, "8461657819190922589", 39846240)
+-- TEKKEN 8 - PAC-PIXELS (AppID: 3564550)
+addappid(3564550)
+addappid(3564550, 1, "4839f0f96b42c57b6e15373696ba97c30db5aff2511924e376a443f594af0ab4")
+setManifestid(3564550, "3006045251628593482", 35651936)
+-- TEKKEN 8 - BAOBAB HORIZON (AppID: 4001570)
+addappid(4001570)
+addappid(4001570, 1, "6a3eff47f7bf06fa34cc270d5a064396dc015de719efd55458d8bfedbb7951b4")
+setManifestid(4001570, "2738299123618148442", 67109216)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 1778822, 2497260, 5178880
