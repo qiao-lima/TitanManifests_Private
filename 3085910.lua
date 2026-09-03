beb4e4c8009d3c50615ff52122dbbfb1c0a08d40
qiao-lima <qiao-lima@github.com>
2026-09-04 07:13:29 +0800

    Manual Update 3085910

---
+++ 3085910.lua
@@ -0,0 +1,13 @@
+-- 3085910's Lua and Manifest Created by Hubcap Manifest
+-- Feastopia
+-- Created: June 18, 2026 at 03:51:13 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3085910, 1, "c77fb2d4c25e8654e3f702c6e8634a3982e55c27c0de7aa27fd1aaf7d3d90cc4") -- Feastopia
+addtoken(3085910, "5387819681192250210")
+-- MAIN APP DEPOTS
+addappid(3085911, 1, "abcabc8a18b9a4e7718872d5d72ddc6984132ab33add00e086bca32cd91fa6b2") -- Depot 3085911
+--setManifestid(3085911, "1979918722759308991", 1524283561)
