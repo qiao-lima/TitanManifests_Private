9b1cec3f5e7f1ba7dbe653e44a5c6e4d808c245a
qiao-lima <qiao-lima@github.com>
2026-09-04 16:54:57 +0800

    Manual Update 1721470

---
+++ 1721470.lua
@@ -0,0 +1,37 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1721470 - Poppy Playtime
+-- Generated 2026-09-04 08:12:19 UTC
+-- # Depots (Total/DLC/Shared): 10/4/4
+
+-- Main AppID
+addappid(1721470, 1, "93b2ee0b53ef3ced9770d2c9ffd33aa464f18c60307a1267655d297e56759f8e")
+
+-- Main Depots
+addappid(1721471, 1, "d8ffbaae7dda98a1608af24916982826d6ec61a8a1338fda6f793da5d3239152") -- (windows, 64-bit)
+setManifestid(1721471, "6958304966447160997", 5612538888)
+addappid(1721472, 1, "9ff882ee821997013c8a26b12ee0e04fe781434ec5e6d41881ec53c759bacdd0")
+setManifestid(1721472, "8897478021450885176", 286984144)
+
+-- DLC's (with depot keys)
+-- Poppy Playtime - Chapter 2 (AppID: 1817490)
+addappid(1817490)
+addappid(1817491, 1, "e8a0b69343b9b3297a7db08e084898fd14130435cf9b95a9e77b794a430e66a0")
+setManifestid(1817491, "2957411760343078601", 8913357573)
+-- Poppy Playtime - Chapter 3 (AppID: 2555190)
+addappid(2555190)
+addappid(2555198, 1, "9b728f6de496b74d3e277f9e8dd3c852dd0b70b4ab461fc5c2d5db73a5ffeec2")
+setManifestid(2555198, "7976244025258826224", 38840530233)
+-- Poppy Playtime - Chapter 4 (AppID: 3008670)
+addappid(3008670)
+addappid(3008670, 1, "05af48230917e5f7df38d99935306907947f276d13191bc0853a0338fc295fa7") -- (windows)
+setManifestid(3008670, "4811752737085405084", 7499725982)
+-- Poppy Playtime - Chapter 5 (AppID: 4100940)
+addappid(4100940)
+addappid(4100940, 1, "527fa72d80412026553480eaf2d04230eee3d58c07d133d290b72be6d664fe75") -- (windows, 64-bit)
+setManifestid(4100940, "3088401404182158647", 11526638487)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- (windows)
