50d7c9385b43f6f02126a224bfea355ef1558b7b
qiao-lima <qiao-lima@github.com>
2026-09-06 00:29:10 +0800

    Manual Update 4451400

---
+++ 4451400.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4451400 - Massage With Benefits
+-- Generated 2026-09-05 13:42:53 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4451400)
+
+-- Main Depots
+addappid(4451401, 1, "4b745825d1658296523d6885265d8aa3e17b87e951a270209924b47762bb50dc")
+setManifestid(4451401, "2581897061139730358", 3952258451)
