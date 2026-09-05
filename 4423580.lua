6aa21222841c7cff26a037205e2df444c45c778d
qiao-lima <qiao-lima@github.com>
2026-09-06 00:24:22 +0800

    Manual Update 4423580

---
+++ 4423580.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4423580 - Shadow Dungeon
+-- Generated 2026-09-05 15:27:56 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4423580, 1, "f84d451a2a5f233c0d0d77e6ff18affa489180ee56726e4cd00036fc5633e16d")
+
+-- Main Depots
+addappid(4423581, 1, "fa6d5e8b4f2ea3d8dacd02c20086cc8b10505fd45f5e0577517254dcbe97bd02") -- (windows)
+setManifestid(4423581, "5480450805618239330", 7423814500)
