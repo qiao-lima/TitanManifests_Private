04b2fae101d62854828e32277da9adac3fa01f21
qiao-lima <qiao-lima@github.com>
2026-09-06 00:29:10 +0800

    Manual Update 4428630

---
+++ 4428630.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4428630 - My Fire Is Bigger Than Yours
+-- Generated 2026-09-05 15:21:29 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4428630)
+
+-- Main Depots
+addappid(4428631, 1, "b4c7caa1b35149b6286859b63e591c4726f91dac17a40d62fa1a7d32c70033bc") -- (windows)
+setManifestid(4428631, "4495694839094516442", 594803142)
