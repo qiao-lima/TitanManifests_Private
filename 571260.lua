7c356793b9b8652f6733b82befff7d3af8fa0e1f
qiao-lima <qiao-lima@github.com>
2026-09-06 00:29:12 +0800

    Manual Update 571260

---
+++ 571260.lua
@@ -0,0 +1,25 @@
+-- Generated with Luie @ https://lua.tools/
+-- 571260 - THE KING OF FIGHTERS XIV STEAM EDITION
+-- Generated 2026-09-05 14:34:02 UTC
+-- # Depots (Total/DLC/Shared): 6/0/2
+
+-- Main AppID
+addappid(571260, 1, "636205d9b2acd57173d90785b713ca980a8e183c7d3bb6acf86ca78643258869")
+
+-- Main Depots
+addappid(571261, 1, "457e4521ec8293fcea5ce3c49d3793c9871ebffaea2088be5114fefe807be81f")
+setManifestid(571261, "3650600735644124658", 17892945687)
+addappid(571262, 1, "38948bed681ddb27502825e1f6676a6504308f59ebad2b398c5723bcce7d7ef6")
+setManifestid(571262, "5389439164999852313", 108327936)
+addappid(571263, 1, "1f6447e3daf3057072b1ee1dc7d3c3a3a5596d3dd89bf4f29d8d863bfd996f1c")
+setManifestid(571263, "8976719421875379464", 278163130)
+addappid(571264, 1, "1b6a174f702f7fdfb5ddd6ff37f9d6388fb7c12405a76bc6d904c031601b1130")
+setManifestid(571264, "3165318192289377592", 494816768)
+
+-- DLC's (no depot keys required)
+addappid(590390) -- THE KING OF FIGHTERS XIV STEAM EDITION UPGRADE PACK #1
+addappid(590391) -- THE KING OF FIGHTERS XIV STEAM EDITION UPGRADE PACK #2
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
