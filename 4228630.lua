07707fa27ee29e5bbeed1752c4683be0ea1355ff
qiao-lima <qiao-lima@github.com>
2026-09-06 00:24:21 +0800

    Manual Update 4228630

---
+++ 4228630.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4228630.lua
+--Gamename QUIET
+addappid(4228630) --Mainappid QUIET
+addappid(4890521, 1, "7f8591acb91b125859d9e9d91f9baecb901286595c72728bb1b98597342df964") --Main Windows Depot QUIET
+setManifestid(4890521, "1140158966397696747", 858653152)
+--Dlcname QUIET Playtest
+addappid(4890520) --Dlcname QUIET Playtest
