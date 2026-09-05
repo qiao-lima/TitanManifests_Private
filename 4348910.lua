3c6e459ce845aa0197d18b31fcf56bb3af706504
qiao-lima <qiao-lima@github.com>
2026-09-06 00:24:22 +0800

    Manual Update 4348910

---
+++ 4348910.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4348910.lua
+--Gamename Probably Stolen - Cyberpunk Shopkeeper Sim
+addappid(4348910) --Mainappid Probably Stolen - Cyberpunk Shopkeeper Sim
+addappid(4603731, 1, "24bc5004993d0c757d4904b19d92e5e7a8839be36b08441964dc48483284d113") --Main Windows Depot Probably Stolen - Cyberpunk Shopkeeper Sim
+addappid(5113870) --Dlcname Probably Stolen Soundtrack
