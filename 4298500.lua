94cb251cc5fb00da6f6e50005869acfe1dab5ab9
qiao-lima <qiao-lima@github.com>
2026-09-06 00:24:21 +0800

    Manual Update 4298500

---
+++ 4298500.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4298500 - Last Breath
+-- Generated 2026-09-05 07:07:19 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4298500)
+
+-- Main Depots
+addappid(4298501, 1, "8f6a36d642f08f18e19f5bdb7525cb614acf9c9fb31298a9346cd2f09384b786")
+setManifestid(4298501, "3742155741040403967", 864967004)
