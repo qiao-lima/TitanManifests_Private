747a459ead3cfe699b480fb58aa28022be6ddeaf
qiao-lima <qiao-lima@github.com>
2026-09-06 00:24:16 +0800

    Manual Update 2100020

---
+++ 2100020.lua
@@ -0,0 +1,22 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2100020 - Lou's Lagoon
+-- Generated 2026-09-05 13:37:00 UTC
+-- # Depots (Total/DLC/Shared): 5/4/0
+
+-- Main AppID
+addappid(2100020)
+
+-- Main Depots
+addappid(2100021, 1, "01a65b174aaf04d6a5bac593c360161f3f0454d8b3ac85e08525880723894918")
+setManifestid(2100021, "6975292777065288334", 9718023173)
+
+-- DLC's (no depot keys required)
+addappid(4831360) -- Lou's Lagoon Deluxe Pack 
+
+-- DLC's (with depot keys)
+-- Lou's Lagoon Digital Artbook (AppID: 4890570)
+addappid(4890570)
+addappid(4890570, 1, "d02a5846fa9d4187fd32526af7d6658a94be334dfa0bbc0b1f4af5edf0486079")
+setManifestid(4890570, "4629110426845429241", 35713302)
+
+-- Missing Depots (We don't have the keys yet lol): 4831380, 5026611, 5026612
