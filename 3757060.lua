97760b2a6e9259e31505bd63262af401fde97d0b
qiao-lima <qiao-lima@github.com>
2026-09-06 00:24:19 +0800

    Manual Update 3757060

---
+++ 3757060.lua
@@ -0,0 +1,29 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3757060 - Shana - priestess of Tona
+-- Generated 2026-09-05 16:20:44 UTC
+-- # Depots (Total/DLC/Shared): 5/4/0
+
+-- Main AppID
+addappid(3757060, 1, "2322e7792c415f481bdffad00fcfa75f60b75925902f4c18a822489b9942f726")
+
+-- Main Depots
+addappid(3757061, 1, "c3d860ccd6339fd9521a2eee0d34d94d3c0f3ef48a8b3a6100f2844f46314423")
+setManifestid(3757061, "6641359389375950863", 7308152004)
+
+-- DLC's (with depot keys)
+-- Shana - Priestess of Tona - Animations Pack (AppID: 4094040)
+addappid(4094040)
+addappid(4094040, 1, "015fcea15616fe66f9d7aa63a5e0c899a645cbb80ea1ac0d592c461388eaad71")
+setManifestid(4094040, "1412450716868346157", 12835643462)
+-- Shana - Priestess of Tona - Wallpapers Pack (AppID: 4094050)
+addappid(4094050)
+addappid(4094050, 1, "abbda48a7ef812cb02d4be0ad1ac2e04cdf77a9fbc4adb55ee1c9c5d32052ba1")
+setManifestid(4094050, "1957145719024085334", 1296167219)
+-- Shana - Priestess of Tona - Digital Artbook (AppID: 4094060)
+addappid(4094060)
+addappid(4094060, 1, "0ad914726ad3e84dd04aa185370702d8aa13fa9ce49dac3d52a7c0465bebf491")
+setManifestid(4094060, "6872835593229848770", 107112352)
+-- Shana - Priestess of Tona: Endless Desire (AppID: 4338850)
+addappid(4338850)
+addappid(4338850, 1, "341b1f59ebfb5a1aa8ef76a5de57f48190d146bec2d660efc87ba1fafe77c23f") -- (windows, 64-bit)
+setManifestid(4338850, "3837060063421097427", 1701171)
