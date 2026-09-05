c466d4c0039f2ee5d8f2974c8af2861f743cd571
qiao-lima <qiao-lima@github.com>
2026-09-06 00:29:10 +0800

    Manual Update 4483210

---
+++ 4483210.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4483210.lua
+--Gamename The Too-Cute Girl Who Moved in Next Door
+addappid(4483210) --Mainappid The Too-Cute Girl Who Moved in Next Door
+addappid(4483211, 1, "a20a682e9140edbfe57f07483bc603143c944cc62903a5c4c0275b9584b29e30") --Main Windows Depot The Too-Cute Girl Who Moved in Next Door
+setManifestid(4483211, "5113103607287693529", 286457600)
+--Missing Dlcs
+--Dlcname The Lewd Policewoman Drains Me Dry
+--Dlcname Baby-Making with the No.1 Hostess
