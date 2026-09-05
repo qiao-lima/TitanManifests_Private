abedd68961fee420bc046389fa2507fe9939b467
qiao-lima <qiao-lima@github.com>
2026-09-06 00:24:16 +0800

    Manual Update 2412960

---
+++ 2412960.lua
@@ -0,0 +1,16 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2412960 - Breathedge 2
+-- Generated 2026-09-05 16:14:31 UTC
+-- # Depots (Total/DLC/Shared): 4/0/3
+
+-- Main AppID
+addappid(2412960)
+
+-- Main Depots
+addappid(2412961, 1, "ca8c8335dc855cfed6d748d4613bdd3178b59d46c8f8213b5543251697ed1090")
+setManifestid(2412961, "8744808818032714486", 10800722555)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
