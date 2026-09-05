cc034ab5db541b48458d78e84086c5e5b4a7e169
qiao-lima <qiao-lima@github.com>
2026-09-06 00:29:11 +0800

    Manual Update 4758370

---
+++ 4758370.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4758370 - Erotic Fantasy Camp
+-- Generated 2026-09-05 15:41:16 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4758370)
+
+-- Main Depots
+addappid(4758371, 1, "bcde94182618cc554e841da8cecd6b288da3c44d99c8bd8a7efbf9a60759dd9c")
+setManifestid(4758371, "6709548965918548535", 2437558350)
