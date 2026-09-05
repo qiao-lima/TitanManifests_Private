04de76d6936b9ffc11549babc7150057cdb8ed51
qiao-lima <qiao-lima@github.com>
2026-09-06 00:29:11 +0800

    Manual Update 4657580

---
+++ 4657580.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4657580 - Lewd Dungeon Manager 🔞
+-- Generated 2026-09-05 14:51:02 UTC
+-- # Depots (Total/DLC/Shared): 4/1/0
+
+-- Main AppID
+addappid(4657580)
+
+-- Main Depots
+addappid(4657581, 1, "6dc18903910b33172e62e72917b7350ce5cf150415c3d65af43b6d3d020c8c53")
+setManifestid(4657581, "3578928120339334181", 1761141293)
+
+-- Missing Depots (We don't have the keys yet lol): 4657582, 4657583, 5152470
