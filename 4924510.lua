056bb86010ebac31cf88453746c82230368b1403
qiao-lima <qiao-lima@github.com>
2026-09-06 00:29:12 +0800

    Manual Update 4924510

---
+++ 4924510.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4924510 - Lust Share House
+-- Generated 2026-09-05 12:47:27 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4924510)
+
+-- Main Depots
+addappid(4924511, 1, "7b884af23cf7a3620670806cda01bd2df078be20a2597ce57093a102a3a84dc2")
+setManifestid(4924511, "2554751645030589188", 2828422917)
