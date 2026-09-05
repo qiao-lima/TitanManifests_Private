dc828d53ea4257d26e6afd0041d02cb305ba9402
qiao-lima <qiao-lima@github.com>
2026-09-06 00:24:16 +0800

    Manual Update 2870340

---
+++ 2870340.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2870340 - Decktamer
+-- Generated 2026-09-05 11:48:28 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(2870340)
+
+-- Main Depots
+addappid(2870341, 1, "1765a3ac027212b639b5bcf9f60a0762692150e41e9ed9c84da30116dc1a24f2")
+setManifestid(2870341, "2295445960615723710", 4536838245)
