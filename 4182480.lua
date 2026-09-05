0bbc1bcca7cf29e3b395c247155398a780689929
qiao-lima <qiao-lima@github.com>
2026-09-06 00:24:20 +0800

    Manual Update 4182480

---
+++ 4182480.lua
@@ -0,0 +1,14 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4182480 - Smash Hit Museum
+-- Generated 2026-09-05 14:24:27 UTC
+-- # Depots (Total/DLC/Shared): 2/0/1
+
+-- Main AppID
+addappid(4182480)
+
+-- Main Depots
+addappid(4182481, 1, "be37777c03f013823c95d376f0dbeb94096aa2713bfc0a4be91affb6eedda9f9")
+setManifestid(4182481, "8473449100781670936", 574179612)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
