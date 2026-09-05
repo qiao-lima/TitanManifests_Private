611e467f97db4c2967d2b474a5d99aa3335c6afa
qiao-lima <qiao-lima@github.com>
2026-09-06 00:24:18 +0800

    Manual Update 3491690

---
+++ 3491690.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3491690 - lovebyte.exe
+-- Generated 2026-09-05 15:47:03 UTC
+-- # Depots (Total/DLC/Shared): 4/2/0
+
+-- Main AppID
+addappid(3491690)
+
+-- Main Depots
+addappid(3491691, 1, "e9627fd04d019ee3b68140bb30a084e5ba88af14f333cce53efa1fc5e84c51cd") -- (windows)
+setManifestid(3491691, "1006713079967139047", 2265694100)
+
+-- Missing Depots (We don't have the keys yet lol): 3491692, 4796931, 4796932
