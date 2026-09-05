15136c73a977059d6b76ae168e31c7ff3c786d5d
qiao-lima <qiao-lima@github.com>
2026-09-06 00:29:11 +0800

    Manual Update 4656000

---
+++ 4656000.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4656000 - BOMBANANA!
+-- Generated 2026-09-05 14:58:43 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(4656000)
+
+-- Main Depots
+addappid(4656001, 1, "d4ca035dd638ad12e68cdacffbe742c7c3200ca84ddaf9ef21dfdbcbb1ab83fc") -- (macos)
+setManifestid(4656001, "633645399584328835", 1998435464)
+addappid(4656002, 1, "4253533257321c05573a2a4681e7bc10885e1338e0dc46bfe93b9a08863c9346") -- (windows)
+setManifestid(4656002, "6430056045087850389", 1353219938)
