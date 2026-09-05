cc57dec56d34f59bd6e112d61197136266be3271
qiao-lima <qiao-lima@github.com>
2026-09-06 00:24:20 +0800

    Manual Update 3841970

---
+++ 3841970.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3841970 - Battle On! Hero Academy
+-- Generated 2026-09-05 16:17:28 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3841970, 1, "0d19ebd381c1256516d53d374c3d233fbbcaaa664da3199658b61316fcb26722")
+
+-- Main Depots
+addappid(3841971, 1, "028a48cb720bdf7ea8a31180d23ac053a0d9eaccfdf00766157a4ca8531c235a")
+setManifestid(3841971, "4323404575648191363", 132268080)
