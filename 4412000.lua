19009039e2cd1752dc369324e2cc4b3c5ffcda70
qiao-lima <qiao-lima@github.com>
2026-09-06 00:24:22 +0800

    Manual Update 4412000

---
+++ 4412000.lua
@@ -0,0 +1,18 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4412000 - Zad Archery
+-- Generated 2026-09-05 15:21:29 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(4412000)
+
+-- Main Depots
+addappid(4412001, 1, "8431359d1e9c173ced3d18c8313bc5d41b3d1d7969780cc7422a2231b43c9e22") -- (windows)
+setManifestid(4412001, "922317437705817310", 379942151)
+addappid(4412002, 1, "aac1ac9373d25fedcb3e7538dd4aaaf2efb14e11c4a6b38047556a3a5ea79bc3") -- (macos)
+setManifestid(4412002, "8891562886989181016", 397093132)
+addappid(4412003, 1, "643ce514b64835ece6e8b2cbf74e85a376dcb4071ab700696809689d324cd4f3") -- (linux)
+setManifestid(4412003, "132532542696255363", 376874612)
+
+-- DLC's (no depot keys required)
+addappid(5053390) -- Zad Archery - Arrowfall Supporter Pack
