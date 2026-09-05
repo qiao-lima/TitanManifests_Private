3cdc108fad9b63b640ce541621f0297ca633fb2a
qiao-lima <qiao-lima@github.com>
2026-09-05 10:01:18 +0800

    Manual Update 4911470

---
+++ 4911470.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4911470 - FRESHIE SISTERS
+-- Generated 2026-09-04 10:10:33 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(4911470)
+
+-- Main Depots
+addappid(4911471, 1, "335043a525447f62f6b85458d00c80d359efb1c8885e6b21b97eee8ec028d9bb") -- (windows,linux)
+setManifestid(4911471, "5041628558896065881", 1402246131)
+addappid(4911472, 1, "7194d204504f939d30efe4c1d6434251ac92c31db832bb3a8d876990d748be21") -- (macos)
+setManifestid(4911472, "1737343145343754789", 1390714789)
