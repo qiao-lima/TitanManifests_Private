c902784795cdd841f4b39e8de2a8adfa0e0c5892
qiao-lima <qiao-lima@github.com>
2026-09-06 00:24:18 +0800

    Manual Update 3436330

---
+++ 3436330.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3436330 - Boxed Girl
+-- Generated 2026-09-05 06:34:21 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3436330)
+
+-- Main Depots
+addappid(3436331, 1, "8126405afab0c582674233f56af0c17beeea822b6886f6873461c52f359d75d9")
+setManifestid(3436331, "142076520192962287", 712653419)
