e7c50a9770c62faa489a1527da7d375fdf2d99df
qiao-lima <qiao-lima@github.com>
2026-09-06 00:29:12 +0800

    Manual Update 819030

---
+++ 819030.lua
@@ -0,0 +1,23 @@
+-- Generated with Luie @ https://lua.tools/
+-- 819030 - STEINS;GATE ELITE
+-- Generated 2026-09-05 15:32:09 UTC
+-- # Depots (Total/DLC/Shared): 7/0/2
+
+-- Main AppID
+addappid(819030, 1, "36ca6e17296fa2655655f9b0b2d72f638c92e7c2865d48558cf60ec04f6ce2b7")
+
+-- Main Depots
+addappid(819031, 1, "b3f296925d61e740e1c4e71abd4dde0c6d8689704dcc823b98a876935f4691b6") -- (windows)
+setManifestid(819031, "3212280727514959670", 14942392447)
+addappid(819032, 1, "ee9e4709ed5f70b9535f4e59d234d6fe801a3d01d6937ef2925a4c5b12c352a1") -- English (windows)
+setManifestid(819032, "5436224210371133763", 14942392447)
+addappid(819033, 1, "9092ca0aa541cb86cb57487455036fd4d39bc92fe710efe43513a91ed7be28c5") -- Japanese (windows)
+setManifestid(819033, "7615172363956099694", 14937137279)
+addappid(819034, 1, "C62A353ED1467297EB9AC7C8CF1EFFBE4253C829AFA8F88892F4F9F3CB454B99") -- Tchinese (windows)
+setManifestid(819034, "1415012490503964090", 14933815424)
+addappid(819035, 1, "335dca1b195e563ebceb96f6d068fe73a2fb73bafc5556ccf40261bef741c875") -- Schinese (windows)
+setManifestid(819035, "1270054482604651419", 14932492416)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
