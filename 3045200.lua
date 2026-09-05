e4cbc41ac1fcdbe71298441d1d0e5e85bdf734e3
qiao-lima <qiao-lima@github.com>
2026-09-06 00:24:17 +0800

    Manual Update 3045200

---
+++ 3045200.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3045200 - Beatblock
+-- Generated 2026-09-05 16:14:02 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(3045200, 1, "253cf3605a775735867e5ea04c855ed24a20124c9ee25bbc8783a3c3deaa2948")
+
+-- Main Depots
+addappid(3045201, 1, "8caf3bd25c4a41f3bdf2d009bc23c176c8cf9f0bd2634a90d72d749df985f85f") -- (windows)
+setManifestid(3045201, "6161020636600859375", 545374914)
+addappid(3045202, 1, "e04dcbac8edef943af8000b157d20fbda7c497456bafea65b0f540759a1cbc55") -- (macos, 64-bit)
+setManifestid(3045202, "2149261608586558686", 572114400)
+addappid(3045203, 1, "e11b4075b3b1356c0cd74a052fb842b5b4fe78cf50b0d8d0b695187f7338373a") -- (linux)
+setManifestid(3045203, "8366186891806716155", 549575935)
