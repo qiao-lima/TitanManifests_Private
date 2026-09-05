2f963612a7417481c40e0a0cfc7ae0856246e086
qiao-lima <qiao-lima@github.com>
2026-09-06 00:24:17 +0800

    Manual Update 3077570

---
+++ 3077570.lua
@@ -0,0 +1,21 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3077570 - MOMO Crash
+-- Generated 2026-09-05 16:20:28 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(3077570, 1, "782b4a52d7da8260fa983610151ef5b1caf340f93a61cd54a8a7e6f604d5f93f")
+
+-- Main Depots
+addappid(3077571, 1, "9cb171cc918395143a84b8aa2c59202144fb044f0db3848b24f27cd4438a209d") -- (windows)
+setManifestid(3077571, "4168148389033685634", 1201653755)
+
+-- DLC's (no depot keys required)
+addappid(4128700) -- MOMO Crash - Good Thigh Day DLC
+addappid(4226420) -- MOMO Crash - Nekomimi Pack DLC
+addappid(4226430) -- MOMO Crash - Xmas Pack DLC
+addappid(4406060) -- MOMO Crash - Tennis Pack DLC
+addappid(4406070) -- MOMO Crash - Easter Bunny Pack DLC
+addappid(4406080) -- MOMO Crash: Spring Song Pack 2026
+
+-- Missing Depots (We don't have the keys yet lol): 3077573
