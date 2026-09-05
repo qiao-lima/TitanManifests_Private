3429e3871e13747806c389ec1a5b63549ed8aa63
qiao-lima <qiao-lima@github.com>
2026-09-06 00:24:20 +0800

    Manual Update 3772010

---
+++ 3772010.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3772010 - 初遇时
+-- Generated 2026-08-21 12:48:37 UTC
+-- # Depots (Total/DLC/Shared): 5/2/0
+
+-- Main AppID
+addappid(3772010)
+
+-- Main Depots
+addappid(3772011, 1, "8193318b81b1ee41ecb6e2c2b58b3267556a17b51457286facd4659ffa142c89") -- (windows)
+setManifestid(3772011, "5467255626648663714", 797091655)
+
+-- Missing Depots (We don't have the keys yet lol): 3772012, 3772013, 4009302, 4009303
