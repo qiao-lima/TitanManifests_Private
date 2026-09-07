881b93bb01558ef5ccc0ae0f69d9b160e5bc592c
qiao-lima <qiao-lima@github.com>
2026-09-07 11:50:21 +0800

    Manual Update 1614840

---
+++ 1614840.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1614840 - VR Hentai Lesbians SEX
+-- Generated 2026-09-07 03:29:59 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(1614840)
+
+-- Main Depots
+addappid(1614841, 1, "772849924b03fc9c4d73d2e6a1eb7687c61f82446a471aaf847fc787f6e75cad")
+setManifestid(1614841, "8392054807978457462", 3678158255)
