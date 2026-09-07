9b6e8d0e993e9b6ba619b1d4cc2406526d0571af
qiao-lima <qiao-lima@github.com>
2026-09-07 11:53:04 +0800

    Manual Update 486320

---
+++ 486320.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 486320 - Orbit: Satellite Defense
+-- Generated 2026-09-07 03:36:32 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(486320)
+
+-- Main Depots
+addappid(486321, 1, "6f7ca76e59516aa2fb48629ee2a5916f45d560e0a423cac8506880a84269106e")
+setManifestid(486321, "6531550080372866707", 33585583)
