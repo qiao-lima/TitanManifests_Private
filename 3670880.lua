b24e07962af0da489b05b357d046b823195d7667
qiao-lima <qiao-lima@github.com>
2026-09-07 11:50:24 +0800

    Manual Update 3670880

---
+++ 3670880.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3670880 - Fallen Kunoichi ~Netorare Hidden Village~
+-- Generated 2026-09-07 03:36:25 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3670880, 1, "47ee063ad532cba203e87c936cf9f9cacd9dd1801de9a8ffa33c1c7306b8563c")
+
+-- Main Depots
+addappid(3670881, 1, "7d42e32fc92274cc4ddcf1c43efe15fc9abb1019b402a6a62423db4ac37324ad") -- (windows)
+setManifestid(3670881, "8184758250177075666", 3050428908)
