7218def010e663c16da242aa4f3206dafbbfb376
qiao-lima <qiao-lima@github.com>
2026-09-07 11:50:24 +0800

    Manual Update 3613960

---
+++ 3613960.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3613960.lua
+--Gamename Resort Island Simulator
+addappid(3613960, 1, "2055f61ab8f0241b1b0e1fbd52d8c2848adae581dddc7352d4f36a9b5a783bd7") --Mainappid Resort Island Simulator
+addappid(3613961, 1, "afe3ff4cf38e7fd02f9a1953bc3853ce2ccd227794be90b74448a7abe327c905") --Main Windows Depot Resort Island Simulator
+setManifestid(3613961, "7798008439125014127", 5501382704)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
