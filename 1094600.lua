3d7746ba781477e0519173d86137b4f7e18f0d42
qiao-lima <qiao-lima@github.com>
2026-09-07 11:50:20 +0800

    Manual Update 1094600

---
+++ 1094600.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1094600 - Kingdom of Night
+-- Generated 2026-09-07 03:23:48 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(1094600, 1, "778cebf593f48afabee40f4207f52dfd8eb611a3812852fbf7802ad7cc897b58")
+
+-- Main Depots
+addappid(1094601, 1, "18da74b260b5418fae1ef0bda9efa4f94e46ac6676e440789c724f4e73b66b23") -- (windows, 64-bit)
+setManifestid(1094601, "7765738024064342546", 582273128)
