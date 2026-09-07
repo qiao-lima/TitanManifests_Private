95fd33c9bf2b7167a96807cabfd1863b580c26b9
qiao-lima <qiao-lima@github.com>
2026-09-08 02:26:21 +0800

    Manual Update 4915450

---
+++ 4915450.lua
@@ -0,0 +1,12 @@
+-- 4915450's Lua and Manifest Created by Hubcap Manifest
+-- Grim Reach Idle
+-- Created: September 01, 2026 at 20:21:29 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4915450) -- Grim Reach Idle
+-- MAIN APP DEPOTS
+addappid(4915451, 1, "75688358b18b37b54b5e1c2796faf5f34c7510614fed8ab72929a6d461e8cfbc") -- Depot 4915451
+setManifestid(4915451, "3496398595039813780", 575542676)
