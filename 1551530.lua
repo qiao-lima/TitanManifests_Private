4e8ac653c6217a479f99449eac8f44abfa0d4c68
qiao-lima <qiao-lima@github.com>
2026-09-07 11:50:21 +0800

    Manual Update 1551530

---
+++ 1551530.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1551530 - Pray Game
+-- Generated 2026-09-07 03:37:22 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(1551530, 1, "198cf10107cf078b7df1fab8c32b9699fd51d66bd7a983946beba0cf6d8fb5a2")
+
+-- Main Depots
+addappid(1551531, 1, "8bf4bb9de5995d70de93ed82c12e20ab74698b9448371dda333a4e7f31e4ad35")
+setManifestid(1551531, "9092028098238687428", 1581280156)
+addappid(1551532, 1, "81d1809a02aacdb3d10af04d9637fcea028a71ef0eaf2827af9c07fe12f00998") -- Schinese
+setManifestid(1551532, "2758901282177187292", 1449799816)
+addappid(1551533, 1, "2b7505fb0b733627298e72ac45e1b03e2ccf63bb8f17130bc693465aa573b29b") -- Japanese
+setManifestid(1551533, "4794655782138282567", 1425331936)
