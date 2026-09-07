ec3aec472fd14e1175ae8ab0b859b410f9d0f945
qiao-lima <qiao-lima@github.com>
2026-09-08 02:26:19 +0800

    Manual Update 3915220

---
+++ 3915220.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3915220.lua
+--Gamename Love Too Easily 2: Summer Pocha
+addappid(3915220) --Mainappid Love Too Easily 2: Summer Pocha
+addappid(3915221, 1, "c01518af806cc5c8efa66a42c8be013ef54591e10bad99ce144dab87981a2bec") --Main Windows Depot Love Too Easily 2: Summer Pocha
+setManifestid(3915221, "4302440596758197194", 7309569968)
