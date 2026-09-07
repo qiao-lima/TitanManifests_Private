a441aa5c7560960d77c930e62da2c4cddf961012
qiao-lima <qiao-lima@github.com>
2026-09-07 11:50:22 +0800

    Manual Update 3389200

---
+++ 3389200.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3389200 - 我的恋爱逃生攻略 Love to Live
+-- Generated 2026-09-07 03:32:41 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(3389200)
+
+-- Main Depots
+addappid(3389201, 1, "5ea1d6ad789975f314500e7a4a7132d8fba4a865411eb9c6dcd05ad4f0697e3b") -- (windows)
+setManifestid(3389201, "1722336175147461949", 1508118586)
+
+-- Missing Depots (We don't have the keys yet lol): 3389202, 3389203
