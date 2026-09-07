8347ca9369e66a4d321a146fed06515606c5549b
qiao-lima <qiao-lima@github.com>
2026-09-07 11:50:24 +0800

    Manual Update 3764390

---
+++ 3764390.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3764390 - Sex Knight~Prey of the Lustful Nobles~
+-- Generated 2026-09-07 03:31:29 UTC
+-- # Depots (Total/DLC/Shared): 4/0/0
+
+-- Main AppID
+addappid(3764390, 1, "fedbd49e0fea81c078c6b952f829128a493019d534a62d1eb0bebfcbb765f98c")
+
+-- Main Depots
+addappid(3764391, 1, "c6730e81ca141c1deabcb15fb2bfeb55877f335125db6a86677e32a4ac630e46") -- Japanese (windows)
+setManifestid(3764391, "4944099693460793096", 950844848)
+addappid(3764392, 1, "a3fe5bc8bcc0629c9bff0c54d02f1bc62c5d429f19d412d1bc4c051d05b65e10") -- English (windows)
+setManifestid(3764392, "6037059316000596539", 951346602)
+addappid(3764393, 1, "a4e3de623cbbc2c928ca1dc1701cd1a39e11722e218039f8bfc09295f77fb687") -- Tchinese (windows)
+setManifestid(3764393, "5344236739417262463", 951295469)
+addappid(3764394, 1, "cf592a5c0815b8f8820e1e54b3a712d8853ee6b1ca8b70fd4579800dcfe2c9bc") -- Schinese (windows)
+setManifestid(3764394, "1991882859854313123", 951308566)
