4ac0aeb4fabbf446acff9df215b6f38221c3e2d1
qiao-lima <qiao-lima@github.com>
2026-09-07 11:50:22 +0800

    Manual Update 2103580

---
+++ 2103580.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2103580 - Rogue Blight Playtest
+-- Generated 2026-09-07 03:37:32 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(2103580)
+
+-- Main Depots
+addappid(2103581, 1, "7b04c2c69f9af09bbeb169500f06c853cffc2a2d886b8c01b9e0ead3b893b1ff") -- (windows)
+setManifestid(2103581, "5914967494550976798", 3666052092)
+
+-- Missing Depots (We don't have the keys yet lol): 2103582, 2103583
