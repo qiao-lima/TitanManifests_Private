87479db7d53edafe87fba5f4c6c480b440fab934
qiao-lima <qiao-lima@github.com>
2026-09-07 11:53:02 +0800

    Manual Update 3921360

---
+++ 3921360.lua
@@ -0,0 +1,25 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3921360 - DEFLATE
+-- Generated 2026-09-04 17:08:46 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(3921360)
+
+-- Main Depots
+addappid(3921361, 1, "e9751008dfb83d78df59fca81ea1360a07058e88f4a8313534765dab9860aab7") -- (windows)
+setManifestid(3921361, "5206971155802607375", 9217479750)
+addappid(3921362, 1, "b79f666ea5d8866404463b180998bea6a5a6ccedecf75bf1efd3491437574bbb") -- (macos, 64-bit)
+setManifestid(3921362, "3100245419969343028", 9281849906)
+
+-- DLC's (no depot keys required)
+addappid(3948090) -- BOOST
+addappid(4424590) -- Various Artists 1
+addappid(4539770) -- Various Artists 2
+addappid(4539830) -- DEFLATE - Lynn
+addappid(4651190) -- DEFLATE - Qili&Qifu Character Crossover Pack
+addappid(4651730) -- DEFLATE - Qili&Qifu Collaboration Song Pack
+addappid(4668540) -- DEFLATE - Electrode Core Brand Collaboration Pack 1
+addappid(4763230) -- DEFLATE - Phono Record Collaboration Pack
+addappid(4904880) -- DEFLATE - Electrode Core Brand Collaboration Pack 2
+addappid(4919530) -- DEFLATE - Various Artists 3
