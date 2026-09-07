c6963ffd934a4b64b4e0a13384a3bd7f8fc30969
qiao-lima <qiao-lima@github.com>
2026-09-07 11:53:05 +0800

    Manual Update 4920100

---
+++ 4920100.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4920100.lua
+--Gamename The Wraith Train: Illusory Domain
+addappid(4920100) --Mainappid The Wraith Train: Illusory Domain
+addappid(4920101, 1, "f7f4f2b276656840026dddd2aae2bb50ba3e26865d6f7f2935c4acf1ed9d9d51") --Main Windows Depot The Wraith Train: Illusory Domain
+setManifestid(4920101, "6296446581153104597", 15373804256)
