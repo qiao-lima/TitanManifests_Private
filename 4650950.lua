73c27aef35b36ed54497d4970f3a48caa03cd004
qiao-lima <qiao-lima@github.com>
2026-09-07 11:53:04 +0800

    Manual Update 4650950

---
+++ 4650950.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4650950.lua
+--Gamename COCORO
+addappid(4650950) --Mainappid COCORO
+addappid(4650951, 1, "1e3f539418ea19b479fb4e1fe8844beff7a07cb04353063e13b365d74f716e42") --Main Windows Depot COCORO
+setManifestid(4650951, "5347323168808967774", 4677413680)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
