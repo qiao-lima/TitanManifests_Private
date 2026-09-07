34b09458a33c32c03029a284d48e227601ba20e7
qiao-lima <qiao-lima@github.com>
2026-09-07 11:50:23 +0800

    Manual Update 3436280

---
+++ 3436280.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3436280 - Glamour Kaleidoscope
+-- Generated 2026-09-07 03:32:08 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3436280)
+
+-- Main Depots
+addappid(3436281, 1, "37b98b0feb7c17c8dfec87b981d89b0684bcb529c7884bb96577603d218889d7")
+setManifestid(3436281, "8549383308992896193", 1804595499)
