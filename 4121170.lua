a26810a3cb222be0158f1fcea16d589294007981
qiao-lima <qiao-lima@github.com>
2026-09-08 02:26:19 +0800

    Manual Update 4121170

---
+++ 4121170.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4121170.lua
+--Gamename Fears to Fathom® - Scratch Creek
+addappid(4121170, 1, "c3d258a70358329034d886d79fa4a102c3c9d9bec1038f6eb2a81266a0d99769") --Mainappid Fears to Fathom® - Scratch Creek
+addappid(4121171, 1, "039dd5543221c7529bdaf1452fbb32fae552d67e608265c9cf39dc1b787ae6da") --Main Windows Depot Fears to Fathom® - Scratch Creek
+setManifestid(4121171, "5089717844646392459", 7100209840)
