69ac09cd32352e92c74d5a8f834691efa2173896
qiao-lima <qiao-lima@github.com>
2026-09-07 11:50:23 +0800

    Manual Update 3486280

---
+++ 3486280.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3486280 - Long Knives
+-- Generated 2026-09-07 03:25:45 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(3486280)
+
+-- Main Depots
+addappid(3486281, 1, "ce8226fd017a463b515764cd5e6bbbaf7e4b500f8924ee10ff59830f29a2f6be") -- (windows)
+setManifestid(3486281, "9079666985932532227", 215964816)
+
+-- Missing Depots (We don't have the keys yet lol): 3486282
