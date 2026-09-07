d4fd3bcdfb7d08aa4031461e68d11c69fdd3edea
qiao-lima <qiao-lima@github.com>
2026-09-07 11:50:21 +0800

    Manual Update 1776200

---
+++ 1776200.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1776200.lua
+--Gamename The Inn Between
+addappid(1776200) --Mainappid The Inn Between
+addappid(1776201, 1, "cbafb285b9f5fedfeeb247a074c0550936808e322005e3310edd35c232e5bbad") --Main Windows Depot The Inn Between
+setManifestid(1776201, "753256103364750938", 2223610848)
