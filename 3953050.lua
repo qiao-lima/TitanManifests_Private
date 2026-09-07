d72b571123a7a6d004401690f54d38e4d1dbb083
qiao-lima <qiao-lima@github.com>
2026-09-07 11:53:03 +0800

    Manual Update 3953050

---
+++ 3953050.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3953050 - 广式双马尾的报恩
+-- Generated 2026-09-07 03:23:42 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3953050)
+
+-- Main Depots
+addappid(3953051, 1, "31e03c3576a8e5fbddbe2017680b7a1542e9b25d51117a8c8e1ac66179bd5fde")
+setManifestid(3953051, "3080549947417047118", 981490334)
