4e8b6ea5679ef8afc098dca1d61052da7a8f93c8
qiao-lima <qiao-lima@github.com>
2026-09-07 11:50:23 +0800

    Manual Update 3562310

---
+++ 3562310.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3562310 - Trials of interspecies Sisters~Erotic exchange with Humans~
+-- Generated 2026-09-07 03:38:13 UTC
+-- # Depots (Total/DLC/Shared): 4/0/0
+
+-- Main AppID
+addappid(3562310, 1, "f8b5af85244b86fb4493783284ad3cd925aa7ea7d5883629be1aeb42602a94c8")
+
+-- Main Depots
+addappid(3562311, 1, "34df029dc309b665454a9589d6f09cc5539c5fd7b93dd2d707c7cf5a86b0f888") -- Japanese (windows)
+setManifestid(3562311, "8028081765085187708", 1790287200)
+addappid(3562312, 1, "e118c7e559cd4092d8c0e7da7ff01e0950df15e4f9c2ab5fcde4780bdfe74ace") -- English (windows)
+setManifestid(3562312, "5993840799017128305", 1788105326)
+addappid(3562313, 1, "5d8dc30970925bf9db657cd7816b6efe4e539cce44658f89a064fbd97454b4b4") -- Tchinese (windows)
+setManifestid(3562313, "839309266845475183", 1804258755)
+addappid(3562314, 1, "eb4998add99536e229fb1c26ba90e0a92e0d26a4c2b85fc0a73f667cef68c5d4") -- Schinese (windows)
+setManifestid(3562314, "6375658317632603584", 1803165673)
