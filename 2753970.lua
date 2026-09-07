413bc0dd7b5f28462927d835982168e3efc127ee
qiao-lima <qiao-lima@github.com>
2026-09-07 11:50:22 +0800

    Manual Update 2753970

---
+++ 2753970.lua
@@ -0,0 +1,19 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2753970 - MARVEL Cosmic Invasion
+-- Generated 2026-09-07 03:29:59 UTC
+-- # Depots (Total/DLC/Shared): 5/1/2
+
+-- Main AppID
+addappid(2753970, 1, "aa109b006ad52895b9178cc16e9063c849e75acd7568ce58309fe475d24079e1")
+
+-- Main Depots
+addappid(2753971, 1, "ff109204aa554d768b70d4a127f48f336e2817a37eb4f43ece799db7987c596d")
+setManifestid(2753971, "8095149430037139487", 511375388)
+addappid(2753972, 1, "c37ee07f99ae25fae9bb80371746aa3ccb5b2d8eb9fc2f9bd590d0908c5e400f") -- (linux, 64-bit)
+setManifestid(2753972, "1354064700461420827", 64977124)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 4436470
