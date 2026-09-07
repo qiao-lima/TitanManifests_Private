59169dd9e5b40a013bb9ec5856e805c1d97aa6e2
qiao-lima <qiao-lima@github.com>
2026-09-07 11:53:03 +0800

    Manual Update 3969080

---
+++ 3969080.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3969080 - MapleStory M
+-- Generated 2026-09-07 03:31:58 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3969080)
+
+-- Main Depots
+addappid(3969081, 1, "c816cfd577bbad9173d800715fc0b8f66d524f85293f960fb6625e4722b5f6e3")
+setManifestid(3969081, "8568208869388419457", 18858631600)
