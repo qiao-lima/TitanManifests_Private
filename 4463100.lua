b60cbf0902c9d7e54cb24dfb83f913f78e78baeb
qiao-lima <qiao-lima@github.com>
2026-09-08 19:51:39 +0800

    Manual Update 4463100

---
+++ 4463100.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4463100.lua
+--Gamename Do As I Say - Obedience to the Village Leader
+addappid(4463100, 1, "6badd74dc9b43ab91d8da6408111b8b04b6c4f81bf51a47824eb9a25882c5561") --Mainappid Do As I Say - Obedience to the Village Leader
+addappid(4463101, 1, "fe4b67c30d84d5b6336f4b1540fc53ca9f6fcd08a803d6baecdf9ead7320d6de") --Main Windows Depot Do As I Say - Obedience to the Village Leader
+setManifestid(4463101, "8403304060353579379", 373777232)
+--Dlcname Do As I Say - Obedience to the Village Leader – Uncensor DLC
+addappid(4740910, 1, "6a9ab10cb08bfb7afcde1b7983e8c357f6016faf4656300b8eab9c078b853371") --Dlcname Do As I Say - Obedience to the Village Leader – Uncensor DLC
+setManifestid(4740910, "7733145382846569005", 222751280)
