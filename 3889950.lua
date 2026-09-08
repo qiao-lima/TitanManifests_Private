b92a15567200ee62652f61ddcbfc82cb57ee2ae4
qiao-lima <qiao-lima@github.com>
2026-09-08 19:48:23 +0800

    Manual Update 3889950

---
+++ 3889950.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3889950.lua
+--Gamename Neuter Master
+addappid(3889950) --Mainappid Neuter Master
+addappid(3889951, 1, "668aeb66328adf7271e757ac30f86d0c739a75a2371ea0345609492f539e0ce5") --Main Windows Depot Neuter Master
+setManifestid(3889951, "8329305788252461294", 395035296)
+addappid(3932820) --Dlcname Neuter Master: Supporter Pack
