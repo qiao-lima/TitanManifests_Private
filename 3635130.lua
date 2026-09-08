bfaf36eb63943556fdbf834a920c8920adc0e31c
qiao-lima <qiao-lima@github.com>
2026-09-08 19:48:16 +0800

    Manual Update 3635130

---
+++ 3635130.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3635130.lua
+--Gamename 4D Intuition
+addappid(3635130) --Mainappid 4D Intuition
+addappid(3635132, 1, "03e3d1cd03759396ef3e20ed2034bc60aeddf84e4d769664fea14d79ec0f8181") --Main Windows Depot 4D Intuition
+setManifestid(3635132, "4981523864809343164", 355642144)
