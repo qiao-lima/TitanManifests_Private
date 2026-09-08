b5966e5c2411aa30551ef9f9052f92335e10013b
qiao-lima <qiao-lima@github.com>
2026-09-08 19:36:23 +0800

    Manual Update 2692280

---
+++ 2692280.lua
@@ -0,0 +1,34 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2692280.lua
+--Gamename My Ex-Future Family Season 2
+addappid(2692280) --Mainappid My Ex-Future Family Season 2
+addappid(2692281, 1, "69e14e7a62c75d4b177707c06dfa45c7c1cfc3012e2dffc2a77fea3b4202c6fc") --Main Windows/Linux Depot My Ex-Future Family Season 2
+setManifestid(2692281, "6137050867485729193", 5088012784)
+--Dlcname My Ex-Future Family Season 2 Premium Edition
+addappid(3697760, 1, "582b1d88ea71f8852bfd7463bbbd13d03e475b48d3f65ae1e5ebbc7c6b5bbc82") --Dlcname My Ex-Future Family Season 2 Premium Edition
+addappid(3697761, 1, "5f784811d042a3b696f9acb754ca144b0231e107e5e09e61aac68a93fa0cece1") --Dlc Windows/Linux Depot My Ex-Future Family Season 2 Premium Edition
+setManifestid(3697761, "5101844377468838568", 5086847440)
+addappid(3697763, 1, "f9abdfac74834b81d423405e30a01982268c90101de9fb742dfffe8b25122237") --Dlc Windows Depot My Ex-Future Family Season 2 Premium Edition
+setManifestid(3697763, "1190794094449362355", 9452416)
+--Dlcname My Ex-Future Family Season 2: Artbook
+addappid(3698020) --Dlcname My Ex-Future Family Season 2: Artbook
+addappid(3698021, 1, "7decaa5530b10142dd7b49c1b5f6a96a48382ecec1937345dfecc49bdc60ff56") --Dlc Windows/Linux Depot My Ex-Future Family Season 2: Artbook
+setManifestid(3698021, "4666528850823435836", 69908128)
+addappid(3698023, 1, "8e90bc174eea5323b60f3f0605ca850417c9e7ba5daad36a5899cc10c30e93e1") --Dlc Windows Depot My Ex-Future Family Season 2: Artbook
+setManifestid(3698023, "7393191286697293684", 134563424)
+--Dlcname My Ex-Future Family Season 2: Animations Pack
+addappid(3698030, 1, "234cdf5534fa9ce3d7af69d59f1b809be7098431c3da2b251f661851ef84c3c4") --Dlcname My Ex-Future Family Season 2: Animations Pack
+addappid(3698031, 1, "0b6fa1dfed14f3e4d5a61a75e09c738f66dabf08edd1b4029313025cf9a957fb") --Dlc Windows Depot My Ex-Future Family Season 2: Animations Pack
+setManifestid(3698031, "82905583419135504", 2556492672)
+--Dlcname My Ex-Future Family Season 2: Calendar 2026
+addappid(3698040) --Dlcname My Ex-Future Family Season 2: Calendar 2026
+addappid(3698041, 1, "b6810230de76c55b5239f51b67335103bec1754bed90df0fbab7115e34595708") --Dlc Windows Depot My Ex-Future Family Season 2: Calendar 2026
+setManifestid(3698041, "4961342429216183196", 78766944)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+--Missing Dlcs
+--Dlcname My Ex-future Family: Twists of Fate
+--Dlcname My Ex-Future Family Season 2: Walkthrough
