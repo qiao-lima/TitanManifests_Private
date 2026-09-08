6a846363122624f12f70e2e1691515994e5c5e70
qiao-lima <qiao-lima@github.com>
2026-09-08 19:37:57 +0800

    Manual Update 2963720

---
+++ 2963720.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2963720.lua
+--Gamename The Memory Thieves
+addappid(2963720) --Mainappid The Memory Thieves
+addappid(2963721, 1, "8d0b335aabc9893a42bc96061736d71a4fb113eec463a2e2a5ea3f904668e6d5") --Main Windows Depot The Memory Thieves
+setManifestid(2963721, "2240167091342178917", 5756443792)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
