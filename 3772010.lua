a36cfe881b8a5dd31f50d472aef200686786969b
qiao-lima <qiao-lima@github.com>
2026-09-08 19:48:19 +0800

    Manual Update 3772010

---
+++ 3772010.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3772010.lua
+--Gamename 初遇时
+addappid(3772010) --Mainappid 初遇时
+addappid(3772011, 1, "8193318b81b1ee41ecb6e2c2b58b3267556a17b51457286facd4659ffa142c89") --Main Windows Depot 初遇时
+setManifestid(3772011, "5467255626648663714", 628749008)
+--Missing Dlcs
+--Dlcname 初遇时 Soundtrack
