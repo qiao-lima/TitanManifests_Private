eda94d0878a2af3432b59bf841a1bc9e25e6381a
qiao-lima <qiao-lima@github.com>
2026-09-08 19:53:58 +0800

    Manual Update 4694960

---
+++ 4694960.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4694960.lua
+--Gamename Kingdom Cumquest
+addappid(4694960) --Mainappid Kingdom Cumquest
+addappid(4694961, 1, "8ad68cdac1f5564850bab32bd81c6c12ae8bd8c11a0af8bb2af2879ee2ad943b") --Main Windows/Linux Depot Kingdom Cumquest
+setManifestid(4694961, "3405114832102340030", 1177524096)
+addappid(4694962, 1, "41c597d65f6531394bc216086b9004e566eed1767dde15bcb921825074c6e203") --Main Macos Depot Kingdom Cumquest
+setManifestid(4694962, "7913701881357860683", 1172723984)
+addappid(4993431, 1, "79da107a48b8cb05386fcdb95ab01d3a613fbf210e1a0371e6e4f950830218b4") --Main Windows/Linux Depot Kingdom Cumquest
+setManifestid(4993431, "7900655899502961667", 592)
+addappid(4993432, 1, "0d30e329cb0bf034bc83034847323cefa5225d5ffb54fe9f975503f82f31c56d") --Main Macos Depot Kingdom Cumquest
+setManifestid(4993432, "1987020898857882099", 1264)
+--Dlcname Kingdom Cumquest - Japanese Version
+addappid(4993430) --Dlcname Kingdom Cumquest - Japanese Version
