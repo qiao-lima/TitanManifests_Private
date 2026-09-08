85c41798a6f6e049ef391180e3f2f383a1f46c01
qiao-lima <qiao-lima@github.com>
2026-09-08 19:42:51 +0800

    Manual Update 3436330

---
+++ 3436330.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3436330.lua
+--Gamename Boxed Girl
+addappid(3436330) --Mainappid Boxed Girl
+addappid(3436331, 1, "8126405afab0c582674233f56af0c17beeea822b6886f6873461c52f359d75d9") --Main Windows Depot Boxed Girl
+setManifestid(3436331, "142076520192962287", 483545616)
