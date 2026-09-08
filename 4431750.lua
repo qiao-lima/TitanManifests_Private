bbb8d6ebe85284d078dd79dc194e9d901d5bcfad
qiao-lima <qiao-lima@github.com>
2026-09-09 04:54:46 +0800

    Manual Update 4431750

---
+++ 4431750.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4431750 - Restitutor
+-- Generated 2026-09-08 17:03:11 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(4431750)
+
+-- Main Depots
+addappid(4431751, 1, "a416a6caec94dd78f4017cb1291d9ee76a4ae81129e138943aa197947a33fc27") -- (windows, 64-bit)
+setManifestid(4431751, "2216452766593304157", 328715953)
+
+-- Missing Depots (We don't have the keys yet lol): 4431752, 4431753
