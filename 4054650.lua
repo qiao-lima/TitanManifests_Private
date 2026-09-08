1da79573d3e15901287e205bc0d0e584f2ff3a45
qiao-lima <qiao-lima@github.com>
2026-09-09 04:54:45 +0800

    Manual Update 4054650

---
+++ 4054650.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4054650.lua
+--Gamename Claws of Soria
+addappid(4054650) --Mainappid Claws of Soria
+addappid(4054651, 1, "25e873528a9abfc9eea415d4a9d9ae0f3f43c3d84d9c136880c1976d506c4f44") --Main Windows Depot Claws of Soria
+setManifestid(4054651, "5817290859226278461", 899472784)
+addappid(4054652, 1, "f683b529a5f89c693863409c752b100198757e7f6a90800d7c09533f7fc84b0d") --Main Macos Depot Claws of Soria
+setManifestid(4054652, "7939214088649982351", 933598576)
