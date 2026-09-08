73c36393eaa8ee94968ee2fd3d5608b6c7ad57c9
qiao-lima <qiao-lima@github.com>
2026-09-08 19:37:56 +0800

    Manual Update 2923970

---
+++ 2923970.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2923970.lua
+--Gamename Rayadillo
+addappid(2923970) --Mainappid Rayadillo
+addappid(2923971, 1, "297c7b459bbe922e3d65ce7155a380bfcd08094b48bd29d295aa997223d785e8") --Main Windows Depot Rayadillo
+setManifestid(2923971, "3693839859858902164", 100288736)
