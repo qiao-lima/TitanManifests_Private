9623f805d6806a14fb5fe25ee70b26df6a07b863
qiao-lima <qiao-lima@github.com>
2026-09-08 19:36:23 +0800

    Manual Update 2651350

---
+++ 2651350.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2651350.lua
+--Gamename Slip & Skid
+addappid(2651350, 1, "5173ed0ef196b156f43ebaef593ea5963a95119038a7dc63a146e5f221f25d56") --Mainappid Slip & Skid
+addappid(2651351, 1, "c97a4decce4b17d9fe9bd8a3bb20d9374afbe87821d7609a5e0b39db5d56e24e") --Main Windows Depot Slip & Skid
+setManifestid(2651351, "7800824032125430919", 182813280)
+addappid(2651352, 1, "a38d233d05607f14ad3a9becc87383a13b298dd566d10921fc9a105b120c6ac4") --Main Linux Depot Slip & Skid
+setManifestid(2651352, "1670274736721011239", 190978560)
