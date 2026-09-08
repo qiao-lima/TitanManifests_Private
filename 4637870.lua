4d43aee03637c6b01295df75011a1f069164262d
qiao-lima <qiao-lima@github.com>
2026-09-08 19:51:43 +0800

    Manual Update 4637870

---
+++ 4637870.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4637870.lua
+--Gamename Smashing Bottles
+addappid(4637870, 1, "d3685a5996c73b0b81bffbaaf48866c13cf4011fb9d0d3c91f50b1acddaf3c96") --Mainappid Smashing Bottles
+addappid(4637871, 1, "74863986fe72af3be1700ffb29fa63f1c4e59b348f06b39b8f048501a467bdf9") --Main Windows Depot Smashing Bottles
+setManifestid(4637871, "815887360263266750", 101535472)
+addappid(4637872, 1, "3eaf178b29fca3564a3efd3504bb912bad0de75094b6470ab2991ef09f601ccf") --Main Macos Depot Smashing Bottles
+setManifestid(4637872, "4361497225410860353", 124950064)
+addappid(4637873, 1, "6b2efb171d9a922a4cea9c376773c6eadbb9bb6470e5069b7661710a8911f02d") --Main Linux Depot Smashing Bottles
+setManifestid(4637873, "6643035265501886164", 93614880)
+addappid(4995210) --Dlcname Smashing Bottles - Supporter Pack
+--Missing Dlcs
+--Dlcname Smashing Bottles Soundtrack
