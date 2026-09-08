38da86266a07a9e6770f5843da1372fa66a9f703
qiao-lima <qiao-lima@github.com>
2026-09-08 19:32:55 +0800

    Manual Update 1681350

---
+++ 1681350.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1681350.lua
+--Gamename 时空梦魇Ⅰ代号608
+addappid(1681350) --Mainappid 时空梦魇Ⅰ代号608
+addappid(1681351, 1, "494435032705250cdb498c4a7e86386aee26198849f4ffe4810c953a5e3f5b54") --Main Windows Depot 时空梦魇Ⅰ代号608
+setManifestid(1681351, "1839518318604803159", 342381408)
