66d5f918af6f71fed943c69b293c59b2c29c05f3
qiao-lima <qiao-lima@github.com>
2026-09-08 19:36:18 +0800

    Manual Update 2072560

---
+++ 2072560.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2072560.lua
+--Gamename DRAGON BALL GEKISHIN SQUADRA
+addappid(2072560, 1, "c2c1ae353b6de2e41a49e86a28a45ce9149ed219c3777d55b6db2b4830eb472e") --Mainappid DRAGON BALL GEKISHIN SQUADRA
+addappid(2072561, 1, "08d9bec08f2cd9753d164478dadaf58e95a81b7d4e48197fd3dca8f2397f43f7") --Main Windows Depot DRAGON BALL GEKISHIN SQUADRA
+setManifestid(2072561, "9177710253888328975", 15564099936)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
