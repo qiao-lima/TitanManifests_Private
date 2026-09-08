bffd4c22d718b124a22f574b62ff8aa67642ef02
qiao-lima <qiao-lima@github.com>
2026-09-08 19:51:44 +0800

    Manual Update 464020

---
+++ 464020.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 464020.lua
+--Gamename Super Puzzle Galaxy
+addappid(464020) --Mainappid Super Puzzle Galaxy
+addappid(464021, 1, "28f098b14a9cba914cda852ad7c6eaaef8336bba44d13c71be65d5bd9c549d49") --Main Windows Depot Super Puzzle Galaxy
+setManifestid(464021, "720660548549897463", 1168237120)
+--Missing Dlcs
+--Dlcname Super Puzzle Galaxy: Boost Ball Pack
+--Dlcname Super Puzzle Galaxy: Warp Ball Pack
