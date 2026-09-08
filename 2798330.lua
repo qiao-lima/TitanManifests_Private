7be939e875e4739672c2bfe1c0f64e986eb4ed57
qiao-lima <qiao-lima@github.com>
2026-09-08 18:33:08 +0800

    Manual Update 2798330

---
+++ 2798330.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2798330 - Dungeon Settlers
+-- Generated 2026-09-08 08:01:06 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(2798330)
+
+-- Main Depots
+addappid(2798331, 1, "e9e6cfc71635a146bb6da13c0f181d3466b7ddcd63665ebf1138d568ca5b5622") -- (windows, 64-bit)
+setManifestid(2798331, "1347387287156377341", 1458992397)
+
+-- Missing Depots (We don't have the keys yet lol): 2798332
