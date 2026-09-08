ad81e1ae5c74e218c4f1f865ac974f13c9901652
qiao-lima <qiao-lima@github.com>
2026-09-08 19:51:43 +0800

    Manual Update 4603730

---
+++ 4603730.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4603730.lua
+--Gamename AppID_4603730
+addappid(4603730) --Mainappid AppID_4603730
+addappid(4603731, 1, "24bc5004993d0c757d4904b19d92e5e7a8839be36b08441964dc48483284d113") --Main Windows Depot AppID_4603730
+setManifestid(4603731, "3429659877545626352", 267398560)
