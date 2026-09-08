061a66a4ddf42d0e7e00800736a228b3f304b12e
qiao-lima <qiao-lima@github.com>
2026-09-08 19:36:16 +0800

    Manual Update 1919590

---
+++ 1919590.lua
@@ -0,0 +1,16 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1919590.lua
+--Gamename NBA 2K23
+addappid(1919590) --Mainappid NBA 2K23
+addappid(1919591, 1, "9a413d95948077bb749d1ac87416be4b6b0da6b5f64ad460fd8afe1dd4897f1e") --Main Windows Depot NBA 2K23
+setManifestid(1919591, "5390652279031895565", 127256978224)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2074590) --Dlcname AppID 2074590
+addappid(2074591) --Dlcname AppID 2074591
+addappid(2074592) --Dlcname AppID 2074592
+addappid(2074593) --Dlcname AppID 2074593
+addappid(2074594) --Dlcname AppID 2074594
