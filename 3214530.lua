495a89b3d83e5ef838ae211c2deeddec659b5736
qiao-lima <qiao-lima@github.com>
2026-09-08 19:42:47 +0800

    Manual Update 3214530

---
+++ 3214530.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3214530.lua
+--Gamename Treasure 'n Trio
+addappid(3214530) --Mainappid Treasure 'n Trio
+addappid(3214531, 1, "31A18F524B1FDBB178986F0E087F5A5054866AC54335129EAFFE3B58AC358A98") --Main Windows Depot Treasure 'n Trio
+setManifestid(3214531, "7957034622768762399", 236796144)
