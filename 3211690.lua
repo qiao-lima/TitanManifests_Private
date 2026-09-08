ffd9b295b821131905def0a1ef30d9c73933328e
qiao-lima <qiao-lima@github.com>
2026-09-08 19:42:46 +0800

    Manual Update 3211690

---
+++ 3211690.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3211690.lua
+--Gamename Tobacco Shop Simulator: Prologue
+addappid(3211690) --Mainappid Tobacco Shop Simulator: Prologue
+addappid(3211691, 1, "459ACCFCE972FD453D7B2D50B912A5239140119FFB9EAE40747AF5E3756B85D6") --Main Windows Depot Tobacco Shop Simulator: Prologue
+setManifestid(3211691, "2766200344252405697", 1200387088)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
