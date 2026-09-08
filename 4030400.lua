b0a1e716392ead2b8122c6708e5a12f32bb2c19b
qiao-lima <qiao-lima@github.com>
2026-09-08 19:50:37 +0800

    Manual Update 4030400

---
+++ 4030400.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4030400.lua
+--Gamename Loot Line - Season 2
+addappid(4030400) --Mainappid Loot Line - Season 2
+addappid(4030401, 1, "8bbdfbf249c97c7237b215d9ee4c8717b0a3b3a2e8e11e6ed6728015ec417b9a") --Main Windows Depot Loot Line - Season 2
+setManifestid(4030401, "332871641076623429", 4758742080)
