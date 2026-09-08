7c40dbc928dcd8b50e301a49cd44f484f6bf4697
qiao-lima <qiao-lima@github.com>
2026-09-08 19:37:55 +0800

    Manual Update 2858500

---
+++ 2858500.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2858500.lua
+--Gamename Rhythm & Beats
+addappid(2858500) --Mainappid Rhythm & Beats
+addappid(2858501, 1, "10072e193dfe97d0545da92e7f1faeefea9dc9786051c50685788a998244a46c") --Main Windows Depot Rhythm & Beats
+setManifestid(2858501, "2730863741397385900", 81845504)
