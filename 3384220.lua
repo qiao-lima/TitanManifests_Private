3a5afb635daa30a39e0e91f6b4833f1cda1e3002
qiao-lima <qiao-lima@github.com>
2026-09-08 19:42:49 +0800

    Manual Update 3384220

---
+++ 3384220.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3384220.lua
+--Gamename VR Secretary: Ailey Edition
+addappid(3384220) --Mainappid VR Secretary: Ailey Edition
+addappid(3384221, 1, "7FFAFB5ABAADACE20DF3DB8CF78D1FE18FAAB7EA270814BDFA9FABAE772EA8DD") --Main Windows Depot VR Secretary: Ailey Edition
+setManifestid(3384221, "9195986719044356743", 4279460048)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4552990) --Dlcname VR Secretary-1st DLC
