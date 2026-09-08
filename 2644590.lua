ac032f4cbcbdf9862dcaf971b6de005425b29e82
qiao-lima <qiao-lima@github.com>
2026-09-08 18:33:08 +0800

    Manual Update 2644590

---
+++ 2644590.lua
@@ -0,0 +1,14 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2644590 - Global Magnates: Shipping Tycoon
+-- Generated 2026-09-08 08:28:58 UTC
+-- # Depots (Total/DLC/Shared): 3/2/0
+
+-- Main AppID
+addappid(2644590, 1, "2b3b9c24862df683b11a012c2fff2a7163c994aa1a5ee0296aad3527e072e304")
+addtoken(2644590, "4578124485407577617")
+
+-- Main Depots
+addappid(2644591, 1, "f73171ad2295fd1043efbcba62ecb3f6626829607334bc412a46e3fac5ae0f53")
+setManifestid(2644591, "6658123649040950208", 3121256154)
+
+-- Missing Depots (We don't have the keys yet lol): 4256641, 4256642
