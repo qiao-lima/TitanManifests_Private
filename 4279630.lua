b9e5be5211f988e315aa1bba250d08b6e7c15afd
qiao-lima <qiao-lima@github.com>
2026-09-08 18:33:11 +0800

    Manual Update 4279630

---
+++ 4279630.lua
@@ -0,0 +1,16 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4279630 - Friendly Steps
+-- Generated 2026-09-08 08:28:28 UTC
+-- # Depots (Total/DLC/Shared): 3/0/0
+
+-- Main AppID
+addappid(4279630)
+
+-- Main Depots
+addappid(4279631, 1, "98085d698486e71620b4fcfb2ba3bd01b131dacf60c441cc457e6d7c519e2a46") -- (windows)
+setManifestid(4279631, "3682539113991891421", 1852981765)
+
+-- DLC's (no depot keys required)
+addappid(5159890) -- Friendly Steps: Mandatory Optional Gratuity Fee
+
+-- Missing Depots (We don't have the keys yet lol): 4279632, 4279633
