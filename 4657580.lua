cc0eaf50efd47f6cca91d98591c5b230b1a70dd2
qiao-lima <qiao-lima@github.com>
2026-09-08 19:53:57 +0800

    Manual Update 4657580

---
+++ 4657580.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4657580.lua
+--Gamename Lewd Dungeon Manager 🔞
+addappid(4657580) --Mainappid Lewd Dungeon Manager 🔞
+addappid(4657581, 1, "6dc18903910b33172e62e72917b7350ce5cf150415c3d65af43b6d3d020c8c53") --Main Windows Depot Lewd Dungeon Manager 🔞
+setManifestid(4657581, "3578928120339334181", 433459632)
+addappid(4657582, 1, "6faa3ab62303dbed6046d0dee07d5c44584b08baf43d781675ebc027452540af") --Main Macos Depot Lewd Dungeon Manager 🔞
+setManifestid(4657582, "6751627279631771118", 441665504)
+addappid(4657583, 1, "5deb2d1928550e8034340ff183fc15f139075fabf02efcedb14f897aca87762a") --Main Linux Depot Lewd Dungeon Manager 🔞
+setManifestid(4657583, "1641632377511766684", 433054512)
