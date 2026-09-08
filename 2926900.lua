9fefa3ab0b43deff7716bb87bcf9828c492f2f7f
qiao-lima <qiao-lima@github.com>
2026-09-08 19:37:57 +0800

    Manual Update 2926900

---
+++ 2926900.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2926900.lua
+--Gamename MAVRIX by Matt Jones
+addappid(2926900) --Mainappid MAVRIX by Matt Jones
+addappid(2926901, 1, "e055ca6a2eb7a6dc7b2684060158050e32c9d5364895e935691442d576fffa8f") --Main Windows Depot MAVRIX by Matt Jones
+setManifestid(2926901, "6611394028041479572", 11002873472)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
+addappid(229033, 1, "9a789440bbc16131547ac6c062e5f168f7684da533a2899a71fdfaaeec21efc3") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229033, "2059065101492814639", 59325760)
