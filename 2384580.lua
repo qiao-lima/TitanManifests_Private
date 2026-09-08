fccdfcd39d77a4bfd413df139843c48cda7ba92b
qiao-lima <qiao-lima@github.com>
2026-09-08 19:36:21 +0800

    Manual Update 2384580

---
+++ 2384580.lua
@@ -0,0 +1,44 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2384580.lua
+--Gamename DYNASTY WARRIORS: ORIGINS
+addappid(2384580, 1, "a48f892e064b01728580d88a2c13d6a003d87d467d7b5a16703684926b74965f") --Mainappid DYNASTY WARRIORS: ORIGINS
+addappid(2384581, 1, "c9bfe8a78dc43f75438b3a8e602c7a322b0b53418f80be108058121a71371130") --Main Windows Depot DYNASTY WARRIORS: ORIGINS
+setManifestid(2384581, "3792757300936452338", 49575006864)
+--Dlcname DYNASTY WARRIORS: ORIGINS - Protagonist's Costume "Nameless Warrior Garb"
+addtoken(3319840, "8824614407371289400")
+addappid(3319840, 1, "e3daad5e8a9c79487f8ea299232fe177f346a535339aced3dbb44b4123959142") --Dlcname DYNASTY WARRIORS: ORIGINS - Protagonist's Costume "Nameless Warrior Garb"
+setManifestid(3319840, "4766733277274996871", 52249280)
+--Dlcname DYNASTY WARRIORS: ORIGINS - Early Works Soundtrack Collection (Digital Edition)
+addtoken(3319850, "10944105112804011624")
+addappid(3319850, 1, "95133ff39c5da8ae0536933d236bb7eab7c1df24a6fedc606bcf5d0867c1a016") --Dlcname DYNASTY WARRIORS: ORIGINS - Early Works Soundtrack Collection (Digital Edition)
+setManifestid(3319850, "575849332506894225", 1135050880)
+--Dlcname DYNASTY WARRIORS: ORIGINS - Official Book (Digital Edition, Japanese version)
+addtoken(3319860, "525880456765100502")
+addappid(3319860, 1, "eb5c039bb499de6dea3bb580083c831393d697ff12404bae8b7ffcc0d8fe9f28") --Dlcname DYNASTY WARRIORS: ORIGINS - Official Book (Digital Edition, Japanese version)
+setManifestid(3319860, "7820196852362389506", 30489888)
+--Dlcname DYNASTY WARRIORS: ORIGINS - Original Soundtrack (Digital Edition)
+addtoken(3319870, "1595971489372741222")
+addappid(3319870, 1, "69380b72ac8cb5b3f7914bbb0e828d1529ef6b2a20076ceb73945c656515e5c7") --Dlcname DYNASTY WARRIORS: ORIGINS - Original Soundtrack (Digital Edition)
+setManifestid(3319870, "2027069962015143615", 149878768)
+--Dlcname DYNASTY WARRIORS: ORIGINS - Letters
+addtoken(3319890, "4925446025434514146")
+addappid(3319890, 1, "ee47966cbf4e985ece4730ea8e94a7766202486517e1dba9de172b6fe849760d") --Dlcname DYNASTY WARRIORS: ORIGINS - Letters
+setManifestid(3319890, "1226171390310445224", 192)
+--Dlcname DYNASTY WARRIORS: ORIGINS - ICHIRAN Noodles
+addappid(3319940, 1, "3a41ae63aea1d4d9ba069358a1159daa92ead59d2790df626c089512e6018c1f") --Dlcname DYNASTY WARRIORS: ORIGINS - ICHIRAN Noodles
+setManifestid(3319940, "4542174949091178289", 96)
+--Dlcname DYNASTY WARRIORS: ORIGINS Visions of Four Heroes
+addappid(4038930, 1, "f9f14de8b63c682e209ed7e0b885d5abd784ee5f1f3b08a333eb5902acf60692") --Dlcname DYNASTY WARRIORS: ORIGINS Visions of Four Heroes
+setManifestid(4038930, "4884216548703729445", 5305461904)
+--Dlcname DYNASTY WARRIORS: ORIGINS - Protagonist's Costume "Yellow Turban Attire"
+addappid(4101630, 1, "9e59d656348d3145eb23e69ece591c42ff3bc4b295de26c1d672729625f07fae") --Dlcname DYNASTY WARRIORS: ORIGINS - Protagonist's Costume "Yellow Turban Attire"
+setManifestid(4101630, "6713619555578120475", 46681488)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(3319880) --Dlcname DYNASTY WARRIORS: ORIGINS - Official Book & Original Soundtrack (Digital Edition)
+--Missing Dlcs
+--Dlcname DYNASTY WARRIORS: ORIGINS - Protagonist's Costume "Garb of the Azure Bird"
+--Dlcname DYNASTY WARRIORS: ORIGINS - Protagonist's Costume "Garb of the Crimson Bird"
+--Dlcname DYNASTY WARRIORS: ORIGINS - Protagonist's Costume "Garb of the Emerald Bird"
+--Dlcname DYNASTY WARRIORS: ORIGINS - Protagonist's Costume "Garb of the Violet Bird"
