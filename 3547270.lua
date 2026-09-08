bb72e7d052c867e2340762a9986fe1c05e3cf96e
qiao-lima <qiao-lima@github.com>
2026-09-08 19:42:52 +0800

    Manual Update 3547270

---
+++ 3547270.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3547270.lua
+--Gamename Worming from Home
+addappid(3547270, 1, "39d0060db71855b1c278aa3ebfcfdb3c150696489414952e259a06c03b955eba") --Mainappid Worming from Home
+addappid(3547271, 1, "8ea53d85025d9d9fbc69c2ce03357976d3e5d92eb6aadcd6a982c81ef1a51e95") --Main Windows Depot Worming from Home
+setManifestid(3547271, "5982634621677709809", 763604784)
+addappid(3547272, 1, "80fcafbc87e4e46399c5888d518d65234230aca64e5e5fb7b10b3b956c87ed29") --Main Macos Depot Worming from Home
+setManifestid(3547272, "2173108919808740660", 769357664)
+--Missing Dlcs
+--Dlcname Worming from Home - Supporter Pack
