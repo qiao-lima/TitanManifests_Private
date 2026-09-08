71d7ee8ebb60f6d4a5e350f4415928724220a7fb
qiao-lima <qiao-lima@github.com>
2026-09-08 18:33:08 +0800

    Manual Update 270470

---
+++ 270470.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 270470.lua
+--Gamename Portal: Alive & Kicking
+addappid(270470) --Mainappid Portal: Alive & Kicking
+addappid(270471, 1, "c686a69ba3e20efc840176fd9b65e0dbf683653b623cd781442196ebea1ac851") --Main Windows Depot Portal: Alive & Kicking
+setManifestid(270471, "5479661674552980541", 6463555728)
+--Missing Dlcs
+--Dlcname Portal: Alive & Kicking Soundtrack
