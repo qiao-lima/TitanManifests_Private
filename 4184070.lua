0f11aacba35b107939238bb31748e1321637b332
qiao-lima <qiao-lima@github.com>
2026-09-08 19:50:39 +0800

    Manual Update 4184070

---
+++ 4184070.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4184070.lua
+--Gamename Sani Yang's Laboratory - Soundtrack
+addappid(4184070) --Mainappid Sani Yang's Laboratory - Soundtrack
+addappid(4184071, 1, "b84e519098e5754d636981fb37475e5120833d87fd00df624cf8a238a6520802") --Main Windows Depot Sani Yang's Laboratory - Soundtrack
+setManifestid(4184071, "3093501774822447318", 216557648)
+addappid(4184072, 1, "a521d602b14a52b156190ec86c2899656e51159b074675a02dfa32cb8b98b870") --Main Windows Depot Sani Yang's Laboratory - Soundtrack
+setManifestid(4184072, "5049936781477319921", 1046351264)
