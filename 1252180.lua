ee02a1046f8fe48b9489c4e713114ad387339810
qiao-lima <qiao-lima@github.com>
2026-09-08 19:32:51 +0800

    Manual Update 1252180

---
+++ 1252180.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1252180.lua
+--Gamename Leon's crusade (La cruzada de León) Soundtrack
+addappid(1252180) --Mainappid Leon's crusade (La cruzada de León) Soundtrack
+addappid(1252181, 1, "45ed316a5051225aea7cc6a890d2819a94b6199e15c90932352864cf0ad8df9a") --Main Windows Depot Leon's crusade (La cruzada de León) Soundtrack
+setManifestid(1252181, "7435142725950582601", 187676848)
