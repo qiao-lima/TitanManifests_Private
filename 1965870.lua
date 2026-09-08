d655907cd27b6020b4666be572a9403897602d6f
qiao-lima <qiao-lima@github.com>
2026-09-08 19:36:17 +0800

    Manual Update 1965870

---
+++ 1965870.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1965870.lua
+--Gamename Anna bunny
+addappid(1965870) --Mainappid Anna bunny
+addappid(1965871, 1, "805e106142881165635f2810b1164b63af92d6ff5de8f7456b067963b9b540b8") --Main Windows Depot Anna bunny
+setManifestid(1965871, "1144653405230646175", 205524352)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
