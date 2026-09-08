ee937d3fd729fcea8fb975742c34d347c45f932c
qiao-lima <qiao-lima@github.com>
2026-09-08 19:38:00 +0800

    Manual Update 3194070

---
+++ 3194070.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3194070.lua
+--Gamename 全网公敌：诡村
+addappid(3194070) --Mainappid 全网公敌：诡村
+addappid(3194071, 1, "5659554cbb7bb5db743b347d5be953a766d7e82b764a4616f032bea6eb91047f") --Main Windows Depot 全网公敌：诡村
+setManifestid(3194071, "7327407597513693139", 841164336)
