da815959db42ebda654a7f1b20284703639e8a16
qiao-lima <qiao-lima@github.com>
2026-09-08 18:33:09 +0800

    Manual Update 3647400

---
+++ 3647400.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3647400.lua
+--Gamename Femtazio
+addappid(3647400, 1, "1ec67d26bc3420f3bc2922a5e69070c697b8f0df4f6d8f50f020dcb412b53e93") --Mainappid Femtazio
+addappid(3647401, 1, "43159e26fb02ce3d28bac9b7c67d5bc6da3f79d01754f4bf09816a20696283ae") --Main Windows Depot Femtazio
+setManifestid(3647401, "292089662834170401", 1293810464)
