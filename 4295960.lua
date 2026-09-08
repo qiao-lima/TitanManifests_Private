142063d90bbb8de8399cd48a7eae2a2b94052bf9
qiao-lima <qiao-lima@github.com>
2026-09-08 19:50:41 +0800

    Manual Update 4295960

---
+++ 4295960.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4295960.lua
+--Gamename Manga Club Advisor wants to support Saeki Sasara
+addappid(4295960) --Mainappid Manga Club Advisor wants to support Saeki Sasara
+addappid(4295961, 1, "33a361568df676818d557b5a091451edec28d9cc0fc251f385fc3e6eca7d3036") --Main Windows Language Depot Japanese Manga Club Advisor wants to support Saeki Sasara
+setManifestid(4295961, "1780608474726260303", 235405920)
+addappid(4295962, 1, "10e45c026f08de07b13dc95436d03b31d684beb3ef433c6d58c5ca7edf9cdda8") --Main Windows Language Depot English Manga Club Advisor wants to support Saeki Sasara
+setManifestid(4295962, "3822102559637625775", 238004192)
+addappid(4295963, 1, "b913d6fab5bb43d440e606562df89fa83e4f341caebc2a8dd18757288afb8276") --Main Windows Language Depot Tchinese Manga Club Advisor wants to support Saeki Sasara
+setManifestid(4295963, "4815682738215751684", 247177664)
+addappid(4295964, 1, "bd9961038f2e240fa76beb5638bcf9e5bd3e28e5ca92ff81c20f3070a579d6d1") --Main Windows Language Depot Schinese Manga Club Advisor wants to support Saeki Sasara
+setManifestid(4295964, "2433191114592919742", 247222064)
