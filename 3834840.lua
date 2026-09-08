d681c8e3bd47e35d3c5251bdd9865926fc120807
qiao-lima <qiao-lima@github.com>
2026-09-08 19:48:20 +0800

    Manual Update 3834840

---
+++ 3834840.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3834840.lua
+--Gamename Again!? The Sheriff’s Kinky Justice
+addappid(3834840) --Mainappid Again!? The Sheriff’s Kinky Justice
+addappid(3834841, 1, "223df7724a4c2ea21577b29d322b752cd5ec8821730df5c85fafdc1fdcf3089c") --Main Windows Depot Again!? The Sheriff’s Kinky Justice
+setManifestid(3834841, "4269932620014919462", 961361328)
