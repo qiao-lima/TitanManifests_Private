fcba42bf679cbf2a9f7c1d0c6135fe3fb1bd12e7
qiao-lima <qiao-lima@github.com>
2026-09-08 19:48:22 +0800

    Manual Update 3856980

---
+++ 3856980.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3856980.lua
+--Gamename Milky Rush-Two Paths
+addappid(3856980) --Mainappid Milky Rush-Two Paths
+addappid(3856981, 1, "fe1de9c89bfc0f169ec9797aace12842f72869fb22ebd32fc1d869bebcdf28f1") --Main Windows Depot Milky Rush-Two Paths
+setManifestid(3856981, "8355012837889319800", 884813248)
