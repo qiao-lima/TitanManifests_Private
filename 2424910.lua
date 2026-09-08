8e953d6f072ac6dfbf7261900392444473b1c6e9
qiao-lima <qiao-lima@github.com>
2026-09-08 19:36:22 +0800

    Manual Update 2424910

---
+++ 2424910.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2424910.lua
+--Gamename Red Ops: The First Infection
+addappid(2424910) --Mainappid Red Ops: The First Infection
+addappid(2424911, 1, "8c39ac678abc0f6b9cd27e638b5aa7d9a7a8adbbe9206f1e55f9f7f256b69f72") --Main Windows/Linux Depot Red Ops: The First Infection
+setManifestid(2424911, "6521475027110118156", 738429600)
+--Dlcname RED OPS - CG Pack
+addappid(2697500, 1, "f13faede04bdb9c106cc4530418a9983baff33acd4e108ef63ec8ef96b31d069") --Dlcname RED OPS - CG Pack
+setManifestid(2697500, "1996898052126986679", 17447280)
+--Dlcname RED OPS - Artbook
+addappid(3918000, 1, "b39e9dc6b2ed8725566f101eef7bb6b37c4a6b5d4a8f1950ac67b6b7421ea8ca") --Dlcname RED OPS - Artbook
+setManifestid(3918000, "3836112077975091089", 32235760)
