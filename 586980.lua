94cb595f03830c1cff20619304f0546d3511d433
qiao-lima <qiao-lima@github.com>
2026-09-08 19:54:04 +0800

    Manual Update 586980

---
+++ 586980.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 586980.lua
+--Gamename MadOut2 BigCityOnline
+addappid(586980) --Mainappid MadOut2 BigCityOnline
+addappid(586981, 1, "eb318c4209792b896a6d0b117febef4401128b9287194eb6d46db39f62bf25b5") --Main Windows Depot MadOut2 BigCityOnline
+setManifestid(586981, "2716105114277471171", 302330960)
+addappid(586982, 1, "6e0be3906477f32e1feea336b65ab8b5fb50aea3c45265eff6ea5a09bc45ad4b") --Main Macos Depot MadOut2 BigCityOnline
+setManifestid(586982, "8936664892887278146", 318390784)
+addappid(586983, 1, "13917716b4845da9cbb47991a2658a03d43d2b060ca07d47b56f12ef3bf4ba75") --Main Linux Depot MadOut2 BigCityOnline
+setManifestid(586983, "4461827431975952955", 312777456)
