408ee499450441e51001384ca8d7c1126f60a7f0
qiao-lima <qiao-lima@github.com>
2026-09-08 19:37:55 +0800

    Manual Update 2827750

---
+++ 2827750.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2827750.lua
+--Gamename Fatal Claw
+addappid(2827750, 1, "458fdcb460b7883d98116e0e82e17e333e13560262247cbfe1bc2eff9ad6c04d") --Mainappid Fatal Claw
+addappid(2827751, 1, "6fa752151773dec1703f2ef467723a5fe3f5ec5cfc917b6164e93406eb542bf2") --Main Windows Depot Fatal Claw
+setManifestid(2827751, "911113097274397440", 1475479152)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
