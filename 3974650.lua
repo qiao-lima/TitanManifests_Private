25505c82d17ce343a9203d9917480e190e1cfc32
qiao-lima <qiao-lima@github.com>
2026-09-08 19:50:35 +0800

    Manual Update 3974650

---
+++ 3974650.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3974650.lua
+--Gamename Onimusha: Way of the Sword DEMO
+addappid(3974650) --Mainappid Onimusha: Way of the Sword DEMO
+addappid(3974651, 1, "ab1400a8a355121c467f705baf6a4d551cedf4fdfc2801bced183c0ea42480a8") --Main Windows Depot Onimusha: Way of the Sword DEMO
+setManifestid(3974651, "6271680303048718615", 21056961504)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
