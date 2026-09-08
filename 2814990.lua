03ad16100f80a6ea1603a9de7f6fba9f4014aabd
qiao-lima <qiao-lima@github.com>
2026-09-08 19:37:54 +0800

    Manual Update 2814990

---
+++ 2814990.lua
@@ -0,0 +1,19 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2814990.lua
+--Gamename Screamer
+addappid(2814990, 1, "81996cb61dba4d7c437699a35d4a956b1aecd56ac80abff0d7643b12fd645b39") --Mainappid Screamer
+addappid(2814991, 1, "951656b29643ce6814f73241af3c3c14488f2c0f82567cd3bee3c56766fdb971") --Main Windows Depot Screamer
+setManifestid(2814991, "2394666536360783544", 29819427392)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4078390) --Dlcname Screamer - Chromed Style Pack
+addappid(4078400) --Dlcname Screamer - Iridescent Style Pack
+addappid(4191550) --Dlcname Screamer - Green Reapers Customization Pack
+addappid(4191560) --Dlcname Screamer - Strike Force Romanda Customization Pack
+addappid(4191570) --Dlcname Screamer - Jupiter Stormers Customization Pack
+addappid(4191580) --Dlcname Screamer - Anaconda Corp Customization Pack
+addappid(4191590) --Dlcname Screamer - Kagawa-Kai Customization Pack
+addappid(4193240) --Dlcname Screamer - Digital Deluxe Upgrade
