97b0e2d8a924e1adec9468294fd738e6ba2eb096
qiao-lima <qiao-lima@github.com>
2026-09-08 19:42:50 +0800

    Manual Update 3432110

---
+++ 3432110.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3432110.lua
+--Gamename Soulbound Roguelike
+addappid(3432110) --Mainappid Soulbound Roguelike
+addappid(3432111, 1, "fa7543c832cde84b864d0efcaeb7e809df65241fc8b853a397af09df60cd4dba") --Main Windows Depot Soulbound Roguelike
+setManifestid(3432111, "9089200574739876506", 143374720)
