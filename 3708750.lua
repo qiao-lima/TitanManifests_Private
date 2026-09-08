28a4937201fe4accfc948e7af15d727d8a5cb39c
qiao-lima <qiao-lima@github.com>
2026-09-08 19:48:18 +0800

    Manual Update 3708750

---
+++ 3708750.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3708750.lua
+--Gamename Virtual Skate Demo
+addappid(3708750) --Mainappid Virtual Skate Demo
+addappid(3708751, 1, "68f4881297b03618740ad71c4d2231c09af5df8087f7d61a375a8b83d7c27414") --Main Windows Depot Virtual Skate Demo
+setManifestid(3708751, "5737528667230620800", 575923984)
