2ca886b78a7662b8afc8e3ced09b28d38246b20d
qiao-lima <qiao-lima@github.com>
2026-09-08 19:53:56 +0800

    Manual Update 4651800

---
+++ 4651800.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4651800.lua
+--Gamename Forbidden Scrolls : Midnight Whispers
+addappid(4651800) --Mainappid Forbidden Scrolls : Midnight Whispers
+addappid(4651801, 1, "3947a6a2d6da5a59a2187b78294caae0557f956e58f7d60685c90e9d10828301") --Main Windows Depot Forbidden Scrolls : Midnight Whispers
+setManifestid(4651801, "5288805830209184695", 2611834128)
