cda40fc2bd3029828cf96e7bfebf7a1d29cff1eb
qiao-lima <qiao-lima@github.com>
2026-09-08 19:48:19 +0800

    Manual Update 377060

---
+++ 377060.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 377060.lua
+--Gamename On a Roll
+addappid(377060) --Mainappid On a Roll
+addappid(377061, 1, "1e41b7e9f778554ac3d570ecd08ed024bc2063542c6f43bdf4db9084c1e976c2") --Main Windows Depot On a Roll
+setManifestid(377061, "4815012745091088386", 2870727008)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
