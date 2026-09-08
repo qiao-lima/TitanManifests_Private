b99b85208a14a5e2ac4f16fa2f54a4589f8fa62a
qiao-lima <qiao-lima@github.com>
2026-09-08 19:32:53 +0800

    Manual Update 1465040

---
+++ 1465040.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1465040.lua
+--Gamename TCSTRIKERS1
+addappid(1465040) --Mainappid TCSTRIKERS1
+addappid(1465041, 1, "103c1586d4ecdec21f0753a071a67c5b395201691bf5d12bac14c36ff76791b2") --Main Windows Depot TCSTRIKERS1
+setManifestid(1465041, "7028928953777305766", 120823312)
+--Missing Dlcs
+--Dlcname TCSTRIKERS1 Soundtrack
