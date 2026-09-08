7e072a41b0932bc23496df145853e87c8f9fce52
qiao-lima <qiao-lima@github.com>
2026-09-08 19:48:20 +0800

    Manual Update 3779890

---
+++ 3779890.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3779890.lua
+--Gamename The Spiriting Away of Saooni Village
+addappid(3779890, 1, "95c06b33178eb1a1cf6cde6b10e2618d0999cd8909a6664e69d2690f075b3373") --Mainappid The Spiriting Away of Saooni Village
+addtoken(3779890, "9546359356541828077")
+addappid(3779891, 1, "0e19efdce3cbe564f80d6e1eaf3a6c25e7f934528e5a2211e589b2189c2fb95d") --Main Windows Language Depot Japanese The Spiriting Away of Saooni Village
+setManifestid(3779891, "1303525146547188813", 447818928)
+addappid(3779892, 1, "228f7651786b992a3d335521f0310eb276dc158e84e0043f96e235e6d7d8a7d4") --Main Windows Language Depot English The Spiriting Away of Saooni Village
+setManifestid(3779892, "8097029059002061723", 447450480)
+addappid(3779893, 1, "b94768f673bbecbefdb09f2a36558128027e8d7b596e48a8f85ac4b64de8ff0b") --Main Windows Language Depot Tchinese The Spiriting Away of Saooni Village
+setManifestid(3779893, "3646363136600035505", 447278208)
+addappid(3779894, 1, "ace6c7ee492a61521b8440bf5615e8756bd8dcf9604193ebe1a0f2aa5bfd065a") --Main Windows Language Depot Schinese The Spiriting Away of Saooni Village
+setManifestid(3779894, "6396134031194471750", 447278288)
