f9f9089ba26729d8db1b16bb3a1f93487be2984d
qiao-lima <qiao-lima@github.com>
2026-09-08 19:51:36 +0800

    Manual Update 4298840

---
+++ 4298840.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4298840.lua
+--Gamename CyberFuck 2067
+addappid(4298840) --Mainappid CyberFuck 2067
+addappid(4298844, 1, "04223876e434ef2368318bf260907460e50cb9553d16f89c093e91de4d245c12") --Main Windows Depot CyberFuck 2067
+setManifestid(4298844, "7223770540571796292", 15959043184)
+addappid(5033780) --Dlcname CyberFuck 2067 Demo
