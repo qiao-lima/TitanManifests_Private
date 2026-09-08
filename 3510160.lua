9a656c919c68c57c42d0d20cdf4c63ce21c027a8
qiao-lima <qiao-lima@github.com>
2026-09-08 19:42:51 +0800

    Manual Update 3510160

---
+++ 3510160.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3510160.lua
+--Gamename My Dorm - Season 1
+addappid(3510160) --Mainappid My Dorm - Season 1
+addappid(3510161, 1, "05f3c5952fc6c0db13fa2a73d783e6f0b6f6a5b4871d028b870f1718464c8ae2") --Main Windows Depot My Dorm - Season 1
+setManifestid(3510161, "4147032636132041913", 4594850976)
