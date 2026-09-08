5500977aed47483b907e808a63efc432549570c6
qiao-lima <qiao-lima@github.com>
2026-09-08 19:51:39 +0800

    Manual Update 4428630

---
+++ 4428630.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4428630.lua
+--Gamename My Fire Is Bigger Than Yours
+addappid(4428630, 1, "9c7269f7a44475cd98f4b0a7b7e23baf46405648ca3a79e546627fb3d58c05a5") --Mainappid My Fire Is Bigger Than Yours
+addappid(4428631, 1, "b4c7caa1b35149b6286859b63e591c4726f91dac17a40d62fa1a7d32c70033bc") --Main Windows Depot My Fire Is Bigger Than Yours
+setManifestid(4428631, "4495694839094516442", 163478560)
