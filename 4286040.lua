439068321610bdc2dae71071d0ce2de67c6dd63c
qiao-lima <qiao-lima@github.com>
2026-09-08 19:50:40 +0800

    Manual Update 4286040

---
+++ 4286040.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4286040.lua
+--Gamename Unicycle Together
+addappid(4286040) --Mainappid Unicycle Together
+addappid(4286041, 1, "b4a3961140cd6c8ec8fb7adca349c30ffd7c2bb970e2f8a556668bc786b3be59") --Main Windows Depot Unicycle Together
+setManifestid(4286041, "5511211142688193833", 2093441328)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4946210) --Dlcname Unicycle Together - All Cosmetics Pack
