91e2997f2ce7c822123fd43a911c390bd9fb4ba1
qiao-lima <qiao-lima@github.com>
2026-09-08 19:48:21 +0800

    Manual Update 3841010

---
+++ 3841010.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3841010.lua
+--Gamename Niramare Quest: Saving a World That Loves Hating Me
+addappid(3841010, 1, "ec5b5a1efd4d6daffc690734953d7f2f2265713a4b9abaa8e6d373d373619dd5") --Mainappid Niramare Quest: Saving a World That Loves Hating Me
+addappid(3841011, 1, "bb2d716ae4af9f55d98721d6511cf0e70a48d9d1fcb90beb547a3e062c8c9d12") --Main Windows Depot Niramare Quest: Saving a World That Loves Hating Me
+setManifestid(3841011, "6756503783175548719", 1436401968)
+addappid(3841012, 1, "aaa2a635c962b3ef7e9b0bf6a474a6c2fdf2dee48f4315452f7427eecb9c1687") --Main Windows Language Depot Schinese Niramare Quest: Saving a World That Loves Hating Me
+setManifestid(3841012, "6343924770785352570", 10965136)
+addappid(3841013, 1, "d57e86712c8fe2a165e79271eb161fa5fa9080eb674b9491e2b641f7dba76aa0") --Main Windows Language Depot Japanese Niramare Quest: Saving a World That Loves Hating Me
+setManifestid(3841013, "6285473311691225529", 1707328)
