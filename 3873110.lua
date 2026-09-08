cb34f10f7af7d0acd31414e14e5e40bb06da3c89
qiao-lima <qiao-lima@github.com>
2026-09-08 19:48:23 +0800

    Manual Update 3873110

---
+++ 3873110.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3873110.lua
+--Gamename 驾考家园
+addappid(3873110) --Mainappid 驾考家园
+addappid(3873111, 1, "1ec5a5800483af09c47704c9aef63e789490c839d81e512caa0044fac130ff74") --Main Windows Depot 驾考家园
+setManifestid(3873111, "9171676947416526139", 1715980208)
