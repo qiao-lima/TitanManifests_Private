a5978772bc9d925579a22941448ec6146e2d3b7e
qiao-lima <qiao-lima@github.com>
2026-09-08 19:37:58 +0800

    Manual Update 2970080

---
+++ 2970080.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2970080.lua
+--Gamename The Littlest Fry
+addappid(2970080) --Mainappid The Littlest Fry
+addappid(2970081, 1, "9BF4A49E5853E32935C9CC45DCD78912621DC7AB21FB8BA175EC06B46FDB2FF8") --Main Windows Depot The Littlest Fry
+setManifestid(2970081, "3514579350348039138", 63890736)
