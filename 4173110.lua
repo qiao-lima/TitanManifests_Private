6f0dc97670f1e7208acf134431485bd07fc912ea
qiao-lima <qiao-lima@github.com>
2026-09-08 18:33:11 +0800

    Manual Update 4173110

---
+++ 4173110.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4173110 - Cirno! Lifts a Boulder
+-- Generated 2026-09-08 08:27:01 UTC
+-- # Depots (Total/DLC/Shared): 4/2/0
+
+-- Main AppID
+addappid(4173110, 1, "135323d5892aadca5b0667b49ef1686b910d442112133d07ef46681e2769bfdb")
+
+-- Main Depots
+addappid(4173111, 1, "9c7af5bf3058cbc22a2aa1ab9727f2c9fd7c44b06dc84a3b6433b2b8cc7ce137") -- (windows, 64-bit)
+setManifestid(4173111, "8745254404729625841", 250867996)
+
+-- Missing Depots (We don't have the keys yet lol): 4173112, 4467281, 4467282
