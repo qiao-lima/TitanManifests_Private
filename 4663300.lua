aefad974a6b620ab8ea240e6601255e68822c28c
qiao-lima <qiao-lima@github.com>
2026-09-08 18:34:24 +0800

    Manual Update 4663300

---
+++ 4663300.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4663300 - 末世：我有一辆房车
+-- Generated 2026-09-08 08:28:18 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4663300)
+
+-- Main Depots
+addappid(4663301, 1, "d3ee2893a97cf04821de1eeb41050e022f209615c495da8040459b674f79e603") -- (windows)
+setManifestid(4663301, "1776432439894679905", 1831063594)
