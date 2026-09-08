18d2e8e2ee985b1c8afa7c97441cea7526e7ca4f
qiao-lima <qiao-lima@github.com>
2026-09-08 19:32:56 +0800

    Manual Update 1900250

---
+++ 1900250.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1900250.lua
+--Gamename Demonschool
+addappid(1900250) --Mainappid Demonschool
+addappid(1900251, 1, "e0df0de371e8f3d11412557184196c9414acf864f6dd0ebfe9383e1dc46ce298") --Main Windows Depot Demonschool
+setManifestid(1900251, "5275638757591335907", 819084448)
+--Dlcname The Art of Demonschool
+addappid(3964710, 1, "85cc66a51eeb87064d41a14e273b59bf0011f257740ebb96b26b401d2ee684ab") --Dlcname The Art of Demonschool
+setManifestid(3964710, "700990617285433373", 59961040)
+addappid(4820500) --Dlcname Demonschool - Puzzle Killers
+--Missing Dlcs
+--Dlcname Demonschool Soundtrack
