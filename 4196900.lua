8d6002c2ab2d642e9e3c2a2f0302127d16e55235
qiao-lima <qiao-lima@github.com>
2026-09-08 19:50:40 +0800

    Manual Update 4196900

---
+++ 4196900.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4196900.lua
+--Gamename AppID_4196900
+addappid(4196900) --Mainappid AppID_4196900
+addappid(4196901, 1, "32c7b9af03229520e72de30a7b2a0abc1e2f8f402fce1be30f56df0c9a956167") --Main Windows Depot AppID_4196900
