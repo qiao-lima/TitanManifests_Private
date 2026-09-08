9d1ed40ea853e73d39c7a547e22d2f3d54a0c3c5
qiao-lima <qiao-lima@github.com>
2026-09-08 18:33:06 +0800

    Manual Update 2305640

---
+++ 2305640.lua
@@ -0,0 +1,17 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2305640 - Bladesong
+-- Generated 2026-09-08 08:49:35 UTC
+-- # Depots (Total/DLC/Shared): 5/2/2
+
+-- Main AppID
+addappid(2305640, 1, "aa88bfa1b22d7de05ab1503a7b76e3721e4a151969a57c1ac4b1081c712b0668")
+
+-- Main Depots
+addappid(2305641, 1, "f8e650bf12a25f930bb720559f7f34466e1211868356e1bed58877f12701dcff")
+setManifestid(2305641, "9138161691523566022", 3804177184)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 4261741, 4261742
