754922555e46f95919902af4da30b40d83f88562
qiao-lima <qiao-lima@github.com>
2026-09-08 18:33:07 +0800

    Manual Update 2419670

---
+++ 2419670.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2419670.lua
+--Gamename Eclipsium
+addappid(2419670) --Mainappid Eclipsium
+addtoken(2419670, "5493430348669686175")
+addappid(2419671, 1, "c41e1728cc55c498046c15e73e08dca0970c9dbb67d95bb5231e4f1e53166437") --Main Windows Depot Eclipsium
+setManifestid(2419671, "2984893862230518839", 1213704064)
+--Missing Dlcs
+--Dlcname Eclipsium Soundtrack
