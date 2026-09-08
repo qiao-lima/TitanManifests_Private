7bf299d34818f2677fc25fb78b7ba0e6a81651ae
qiao-lima <qiao-lima@github.com>
2026-09-08 19:48:15 +0800

    Manual Update 3607470

---
+++ 3607470.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3607470.lua
+--Gamename 虫林好汉：乌合之众
+addappid(3607470) --Mainappid 虫林好汉：乌合之众
+addappid(3607471, 1, "f9a85630798c10065ee5a995ae6c39b6701397bdb6611bd97d2bb71c0d1e2c6d") --Main Windows Depot 虫林好汉：乌合之众
+setManifestid(3607471, "1153407298144498133", 607343648)
