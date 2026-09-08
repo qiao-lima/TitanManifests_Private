e2ba631004abc4aaea2c64e964ab26a92fd04e18
qiao-lima <qiao-lima@github.com>
2026-09-08 19:42:49 +0800

    Manual Update 3415240

---
+++ 3415240.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3415240.lua
+--Gamename Cozy Organizer
+addappid(3415240) --Mainappid Cozy Organizer
+addappid(3415241, 1, "d715ea838045658a58daf4a389fd23c71d84df14ec038271d4f21c5a493e9c5a") --Main Windows Depot Cozy Organizer
+setManifestid(3415241, "6995823076940800618", 238839216)
