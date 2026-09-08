7440ec666be26a29e26638e688ab720994353e13
qiao-lima <qiao-lima@github.com>
2026-09-08 19:50:39 +0800

    Manual Update 4187050

---
+++ 4187050.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4187050.lua
+--Gamename NTR CORE
+addappid(4187050) --Mainappid NTR CORE
+addappid(4187051, 1, "67665c5fcee5e171a3d5ab2dd829a6e06638a731acfc26e834aef04897b59d77") --Main Windows Depot NTR CORE
+setManifestid(4187051, "3490354927824032198", 238760000)
