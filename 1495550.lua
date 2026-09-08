6fe96137f35bb794d93bfb161b96301b4beaecf5
qiao-lima <qiao-lima@github.com>
2026-09-08 19:32:53 +0800

    Manual Update 1495550

---
+++ 1495550.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1495550.lua
+--Gamename AppID_1495550
+addappid(1495550) --Mainappid AppID_1495550
+addappid(1495551, 1, "b71424701c7da9f81c3cc1173e937b4bc05150b0a44d0937803249a8e6600ef1") --Main Windows Depot AppID_1495550
+setManifestid(1495551, "3230940899610312371", 10958736)
