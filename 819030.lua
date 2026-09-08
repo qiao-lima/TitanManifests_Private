464588141986c8544c85d980779f1ed86b43d8cf
qiao-lima <qiao-lima@github.com>
2026-09-08 19:59:44 +0800

    Manual Update 819030

---
+++ 819030.lua
@@ -0,0 +1,19 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 819030.lua
+--Gamename STEINS;GATE ELITE
+addappid(819030, 1, "36ca6e17296fa2655655f9b0b2d72f638c92e7c2865d48558cf60ec04f6ce2b7") --Mainappid STEINS;GATE ELITE
+addappid(819031, 1, "b3f296925d61e740e1c4e71abd4dde0c6d8689704dcc823b98a876935f4691b6") --Main Windows Depot STEINS;GATE ELITE
+setManifestid(819031, "3212280727514959670", 14370838304)
+addappid(819032, 1, "ee9e4709ed5f70b9535f4e59d234d6fe801a3d01d6937ef2925a4c5b12c352a1") --Main Windows Language Depot English STEINS;GATE ELITE
+setManifestid(819032, "5436224210371133763", 14370838304)
+addappid(819033, 1, "9092ca0aa541cb86cb57487455036fd4d39bc92fe710efe43513a91ed7be28c5") --Main Windows Language Depot Japanese STEINS;GATE ELITE
+setManifestid(819033, "7615172363956099694", 14366307680)
+addappid(819034, 1, "c62a353ed1467297eb9ac7c8cf1effbe4253c829afa8f88892f4f9f3cb454b99") --Main Windows Language Depot Tchinese STEINS;GATE ELITE
+setManifestid(819034, "1415012490503964090", 14359707888)
+addappid(819035, 1, "335dca1b195e563ebceb96f6d068fe73a2fb73bafc5556ccf40261bef741c875") --Main Windows Language Depot Schinese STEINS;GATE ELITE
+setManifestid(819035, "1270054482604651419", 14358782608)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
