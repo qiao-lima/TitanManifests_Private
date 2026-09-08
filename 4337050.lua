027a87fb4fd8bc813c4ac9bdc321e7f206e555df
qiao-lima <qiao-lima@github.com>
2026-09-08 19:51:37 +0800

    Manual Update 4337050

---
+++ 4337050.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4337050.lua
+--Gamename Colosseum Clothes-Ripper: The Erotic Arena RPG
+addappid(4337050) --Mainappid Colosseum Clothes-Ripper: The Erotic Arena RPG
+addappid(4337051, 1, "889f496c710cea0815328aae4f2249cd21588c8b1deeb8b76fe67a11c33e50f3") --Main Windows Language Depot Japanese Colosseum Clothes-Ripper: The Erotic Arena RPG
+setManifestid(4337051, "8591541705549828467", 199539216)
+addappid(4337052, 1, "ffbee3e4810b67d988c354d4bc559055d94455ff2dca572ebddab51c20dda43b") --Main Windows Language Depot English Colosseum Clothes-Ripper: The Erotic Arena RPG
+setManifestid(4337052, "9212054547142259613", 208686256)
+addappid(4337053, 1, "5cb6302676f7699dc5be1442b7fd97bfd48021a6aa432bfc8a861de4631f6356") --Main Windows Language Depot Tchinese Colosseum Clothes-Ripper: The Erotic Arena RPG
+setManifestid(4337053, "5856079572070521622", 208728608)
+addappid(4337054, 1, "de4aceca8b179dca49fbefcd87535bc0b8f953c321d2be1547972c8ecd02ebe0") --Main Windows Language Depot Schinese Colosseum Clothes-Ripper: The Erotic Arena RPG
+setManifestid(4337054, "4607172354185116935", 208727360)
