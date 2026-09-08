7cf337ee7d25e8c33ad65950f6bdffef3062f3d6
qiao-lima <qiao-lima@github.com>
2026-09-08 19:32:56 +0800

    Manual Update 1788780

---
+++ 1788780.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1788780.lua
+--Gamename VenusBlood HOLLOW International
+addappid(1788780) --Mainappid VenusBlood HOLLOW International
+addappid(1788781, 1, "a4fef81ebca7ef2750259701ea2af6a4e8d4f8c90685f0307bc607192321d228") --Main Windows Language Depot English VenusBlood HOLLOW International
+setManifestid(1788781, "3742837599900003813", 2779795088)
+addappid(1788782, 1, "ac244bffa9f83fc801d992f0fdbb2199b48291453b8d0be861b68bdc60c942fd") --Main Windows Language Depot Schinese VenusBlood HOLLOW International
+setManifestid(1788782, "6972841795611193793", 2806781392)
+--Dlcname VenusBlood HOLLOW International Soundtrack
+addappid(2808430) --Dlcname VenusBlood HOLLOW International Soundtrack
+addappid(2808431, 1, "4589068334c41bee2fb673979dc9cded2e4ab61585bb2a009b268d921289fd01") --Dlc Windows Depot VenusBlood HOLLOW International Soundtrack
+setManifestid(2808431, "7372399900866006108", 530026128)
+--Share Depots
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228982, "6413394087650432851", 9650016)
