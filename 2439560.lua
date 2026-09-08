1415e9a6c28b6cae6c0351314421df98c486188e
qiao-lima <qiao-lima@github.com>
2026-09-08 19:36:22 +0800

    Manual Update 2439560

---
+++ 2439560.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2439560.lua
+--Gamename Alice's World
+addappid(2439560) --Mainappid Alice's World
+addappid(2439561, 1, "a83154b16e91f66022956079e5ec81a27c71d8ade0a7cbbf6171d915fe7965b2") --Main Windows Depot Alice's World
+setManifestid(2439561, "7546829526484154465", 2042302192)
