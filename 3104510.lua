16c19198ff98bd8e1e47bcec2968998f268930b3
qiao-lima <qiao-lima@github.com>
2026-09-08 19:37:59 +0800

    Manual Update 3104510

---
+++ 3104510.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3104510.lua
+--Gamename Town Keeper
+addappid(3104510) --Mainappid Town Keeper
+addappid(3104511, 1, "dbfd1edd08c9a1c6661ed9ec6e85040ccad2e89692a34eef2c199b1a9a7c71f8") --Main Windows Depot Town Keeper
+setManifestid(3104511, "7474731859378892410", 67175856)
