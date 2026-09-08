b5be15a148a5cb9bdcde8af0e4bce1147e678e85
qiao-lima <qiao-lima@github.com>
2026-09-08 19:50:36 +0800

    Manual Update 3996680

---
+++ 3996680.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3996680.lua
+--Gamename 都市女孩:心跳海风
+addappid(3996680) --Mainappid 都市女孩:心跳海风
+addappid(3996681, 1, "625b6596bf65622d38c2400ef6e2bc5df6be794eb7f49ff83f92e99d109c3768") --Main Windows Depot 都市女孩:心跳海风
+setManifestid(3996681, "1095877686948621929", 662519808)
