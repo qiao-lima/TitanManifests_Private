73c865bdba3e4618f5fc50e320b213cc268f6db5
qiao-lima <qiao-lima@github.com>
2026-09-08 19:36:20 +0800

    Manual Update 2367120

---
+++ 2367120.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2367120.lua
+--Gamename Blood Lily Ghoststories
+addappid(2367120) --Mainappid Blood Lily Ghoststories
+addappid(2367121, 1, "25b770462e7a23f79881046630d2d66f15b03398b7e9fe40991e43e3a72dd4c6") --Main Windows Depot Blood Lily Ghoststories
+setManifestid(2367121, "6578583164852987911", 284713680)
