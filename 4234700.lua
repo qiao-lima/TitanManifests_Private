d05952cd551b99e2cc599a261262283b3a5173a6
qiao-lima <qiao-lima@github.com>
2026-09-08 19:50:40 +0800

    Manual Update 4234700

---
+++ 4234700.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4234700.lua
+--Gamename AppID_4234700
+addappid(4234700) --Mainappid AppID_4234700
+addappid(4234701, 1, "f3b5d2b54115437e1f68697b3c634633fea406968ea9105190d2ea93bff82c84") --Main Windows Depot AppID_4234700
