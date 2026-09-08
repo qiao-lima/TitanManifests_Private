4ca02cd83ea142cfaa59f8e52ca7b05d5e7e8e1c
qiao-lima <qiao-lima@github.com>
2026-09-08 19:37:56 +0800

    Manual Update 2861270

---
+++ 2861270.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2861270.lua
+--Gamename 停车老司机
+addappid(2861270) --Mainappid 停车老司机
+addappid(2861271, 1, "781a46c269ccf546fa914bdb974d33e36e8ba8db57517e3d2e052892646e4600") --Main Windows Depot 停车老司机
+setManifestid(2861271, "8246283579612599484", 34408448)
