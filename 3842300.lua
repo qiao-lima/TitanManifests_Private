2e38b3d5bd3985ee88bb631871951e7033fdf7d3
qiao-lima <qiao-lima@github.com>
2026-09-08 19:48:21 +0800

    Manual Update 3842300

---
+++ 3842300.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3842300.lua
+--Gamename Hell Builder
+addappid(3842300) --Mainappid Hell Builder
+addappid(3842301, 1, "15fe453a72ca08aea7b5342adc11b0d9048847ee135f043a3e97dba0c0870ae4") --Main Windows Depot Hell Builder
+setManifestid(3842301, "1608210282112882761", 38255840)
