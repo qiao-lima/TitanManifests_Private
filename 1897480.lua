b99666059b70502acfd3fe5de795fd8aad1a03e8
qiao-lima <qiao-lima@github.com>
2026-09-08 19:32:56 +0800

    Manual Update 1897480

---
+++ 1897480.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1897480.lua
+--Gamename Devil's Island
+addappid(1897480) --Mainappid Devil's Island
+addappid(1897481, 1, "10bb2a1c185ad3f191fd9463a0ae06db35a15560f2845049c68ba153eb822dbf") --Main Windows Language Depot English Devil's Island
+setManifestid(1897481, "2783947883556186680", 6120180928)
+addappid(1897482, 1, "8f46ec7e8eb2cae1a8ced9b4ef396e85174e0e82cb8c576e790a892d1b061a10") --Main Windows Language Depot German Devil's Island
+setManifestid(1897482, "5693011410772764876", 5877793984)
