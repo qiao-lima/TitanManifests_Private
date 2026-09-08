e168daf5be2e1c22506d8320c1999579d11efebf
qiao-lima <qiao-lima@github.com>
2026-09-08 19:42:48 +0800

    Manual Update 3325500

---
+++ 3325500.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3325500.lua
+--Gamename Worldwide Rush
+addappid(3325500, 1, "6b6b7fdd779375666a16a623daa77c450141e476eca067226a0add641486a069") --Mainappid Worldwide Rush
+addappid(3325501, 1, "eafb487c5a2d32c110755b81fa24973ffef0147da42a00936f5d73ebcc9a014b") --Main Windows Depot Worldwide Rush
+setManifestid(3325501, "695557801971051457", 5139311232)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
