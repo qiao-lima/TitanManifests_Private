456659bb734240773c8eee520b3c2d6fe9fda67b
qiao-lima <qiao-lima@github.com>
2026-09-08 19:42:54 +0800

    Manual Update 3591390

---
+++ 3591390.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3591390.lua
+--Gamename Vivi and the Magical Island
+addappid(3591390) --Mainappid Vivi and the Magical Island
+addtoken(3591390, "18132962444802646328")
