098eaa9815ad27f72e66625c74a10626c0cb215b
qiao-lima <qiao-lima@github.com>
2026-09-08 19:42:47 +0800

    Manual Update 3216340

---
+++ 3216340.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3216340.lua
+--Gamename Tearscape
+addappid(3216340, 1, "9374a82d11ad32c4dab3187edd95fb2388e6241efbf5ab46e00ecf4d892c5207") --Mainappid Tearscape
+addappid(3216341, 1, "dc2515005012b2d7531cdf2b283d58340c4bcb125ebc850bc8f351691b8bf587") --Main Windows Depot Tearscape
+setManifestid(3216341, "5101652306970246510", 113453136)
+addappid(3216342, 1, "817e9db5b83084630b9e8709d0712ee45c4345dc057665a35aa25d79692d4db2") --Main Macos Depot Tearscape
+setManifestid(3216342, "2617922496328447505", 145678128)
+addappid(3216343, 1, "cf84ad478310bc0be443e5ea69e03997b750abed2cfca7bf36034dd5a07fe5e8") --Main Linux Depot Tearscape
+setManifestid(3216343, "7605690808256054082", 106326768)
