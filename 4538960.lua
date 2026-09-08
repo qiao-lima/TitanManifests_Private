677fb0631aef3f253e64f8eb995f663de1e1648e
qiao-lima <qiao-lima@github.com>
2026-09-08 19:51:42 +0800

    Manual Update 4538960

---
+++ 4538960.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4538960.lua
+--Gamename ZENONIA 1
+addappid(4538960, 1, "327fa782cd82e67a2e509f67e367768bc5c994a59fbbb9287660bdb23a58bb20") --Mainappid ZENONIA 1
+addtoken(4538960, "12579548034132309972")
+addappid(4538961, 1, "5034f6a8570c1c532c135e9edff470a05ebc20899b9c214b2e00c1be000c6396") --Main Windows Depot ZENONIA 1
+setManifestid(4538961, "1001253214688917891", 152529472)
