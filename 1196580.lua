b698f972365d1bc0fe6f35a17e6cb668e6cf4c45
qiao-lima <qiao-lima@github.com>
2026-09-08 19:32:50 +0800

    Manual Update 1196580

---
+++ 1196580.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1196580.lua
+--Gamename The Pale City
+addappid(1196580) --Mainappid The Pale City
+addappid(1196581, 1, "a4ab0917e04727730a09efe544f1855d3722b441ecf5757dc7d60de5575f3f36") --Main Windows Depot The Pale City
+setManifestid(1196581, "6405450318507564267", 642259312)
