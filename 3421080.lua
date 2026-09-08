2deff0a721c03f934b91f57909ecc0d720b4ea51
qiao-lima <qiao-lima@github.com>
2026-09-08 19:42:50 +0800

    Manual Update 3421080

---
+++ 3421080.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3421080.lua
+--Gamename Island Life With Mayalala
+addappid(3421080, 1, "3bf04546352080c2fc6e637181b54147b2c25ed30e1b73776dc35c24ccd3cadb") --Mainappid Island Life With Mayalala
+addappid(3421081, 1, "dcef03a872c059c5936def3cea33a023a7cfafde0660918afadcbb5e92e8e93e") --Main Windows Depot Island Life With Mayalala
+setManifestid(3421081, "2108179601783126464", 618243536)
+addappid(3421082, 1, "9e300acdf87a9fc5a382634a4175413a0ac9865c049345c7e8178c4a358019d5") --Main Windows Language Depot Schinese Island Life With Mayalala
+setManifestid(3421082, "3056877058203046772", 495440512)
+addappid(3421083, 1, "e0c9c8088781b27c50167025b0802da9d1dd24656e8880b8598ad5cb516983a5") --Main Windows Language Depot Japanese Island Life With Mayalala
+setManifestid(3421083, "2762339430588793261", 495443312)
