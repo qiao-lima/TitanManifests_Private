bd4e0d4072c9cb262ade458a99446dd7afe736de
qiao-lima <qiao-lima@github.com>
2026-09-08 19:36:21 +0800

    Manual Update 2398320

---
+++ 2398320.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2398320.lua
+--Gamename AppID_2398320
+addappid(2398320) --Mainappid AppID_2398320
+addappid(2398321, 1, "5e884822e81d0359d995afdf0eaed0cee24a9e57b72ec3eb8fb1aaa955b5bc1d") --Main Windows Depot AppID_2398320
+setManifestid(2398321, "1454892932979748594", 18068384)
