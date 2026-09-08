d07a673082c2fc3042646704ddc0c4885b13c61e
qiao-lima <qiao-lima@github.com>
2026-09-08 19:51:37 +0800

    Manual Update 4412000

---
+++ 4412000.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4412000.lua
+--Gamename Zad Archery
+addappid(4412000) --Mainappid Zad Archery
+addappid(4412001, 1, "8431359d1e9c173ced3d18c8313bc5d41b3d1d7969780cc7422a2231b43c9e22") --Main Windows Depot Zad Archery
+setManifestid(4412001, "8715208282891332139", 105883680)
+addappid(4412002, 1, "aac1ac9373d25fedcb3e7538dd4aaaf2efb14e11c4a6b38047556a3a5ea79bc3") --Main Macos Depot Zad Archery
+setManifestid(4412002, "6321591623752653738", 114311088)
+addappid(4412003, 1, "643ce514b64835ece6e8b2cbf74e85a376dcb4071ab700696809689d324cd4f3") --Main Linux Depot Zad Archery
+setManifestid(4412003, "230652084880353535", 103781760)
+addappid(5053390) --Dlcname Zad Archery - Arrowfall Supporter Pack
