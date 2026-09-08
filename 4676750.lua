352e0826f877ff546f457d512e68c4acb0ef6cd2
qiao-lima <qiao-lima@github.com>
2026-09-08 19:53:57 +0800

    Manual Update 4676750

---
+++ 4676750.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4676750.lua
+--Gamename 点击地牢 Tap Dungeon
+addappid(4676750) --Mainappid 点击地牢 Tap Dungeon
+addappid(4676751, 1, "22e105025451a02291eda686ca17f91eecbe35dbff35a9019a73a64d2a1091bd") --Main Windows Depot 点击地牢 Tap Dungeon
+setManifestid(4676751, "3128190866805564080", 328180944)
