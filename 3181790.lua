bb7af395d3677ba343b4c8db93f4e2ecff6f2ed2
qiao-lima <qiao-lima@github.com>
2026-09-08 19:38:00 +0800

    Manual Update 3181790

---
+++ 3181790.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3181790.lua
+--Gamename Souper Game
+addappid(3181790, 1, "0e331b8c7693c922de90fec8ddc7311c899c30a19c4dbe7d18e263d367796076") --Mainappid Souper Game
+addappid(3181791, 1, "0bea736e5c7869a39e125bd41f1172b2a858332d4e06dc1c8149f848e479f7f2") --Main Windows Depot Souper Game
+setManifestid(3181791, "7426606591950195037", 570910864)
+addappid(3181792, 1, "fdca71c6efd475525fe93666ce41d41fefaad7b79870db7777ca5e99a9f7271e") --Main Macos Depot Souper Game
+setManifestid(3181792, "8313385894858994004", 570409216)
+--Missing Dlcs
+--Dlcname Souper Game Artbook
+--Dlcname Souper Game Wallpaper pack
+--Dlcname Souper Game Art Expansion
