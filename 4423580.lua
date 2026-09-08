a651bf407445c037489b89d21fd81a841f876ae0
qiao-lima <qiao-lima@github.com>
2026-09-08 19:51:38 +0800

    Manual Update 4423580

---
+++ 4423580.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4423580.lua
+--Gamename Shadow Dungeon
+addappid(4423580, 1, "f84d451a2a5f233c0d0d77e6ff18affa489180ee56726e4cd00036fc5633e16d") --Mainappid Shadow Dungeon
+addappid(4423581, 1, "fa6d5e8b4f2ea3d8dacd02c20086cc8b10505fd45f5e0577517254dcbe97bd02") --Main Windows Depot Shadow Dungeon
+setManifestid(4423581, "4723155940203358529", 2028658848)
