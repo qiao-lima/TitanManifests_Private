5d32a764d83008f9b9f58ef7591f71bb621425c0
qiao-lima <qiao-lima@github.com>
2026-09-08 19:42:47 +0800

    Manual Update 3218710

---
+++ 3218710.lua
@@ -0,0 +1,61 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3218710.lua
+--Gamename Everlusting Life
+addappid(3218710) --Mainappid Everlusting Life
+addappid(3218711, 1, "653272E5127E6E41A59CC9BB717ADFE41B08B7EA11B1421F56C133C12A1459D8") --Main Windows Depot Everlusting Life
+setManifestid(3218711, "1560638719569139371", 237979488)
+addappid(3218712, 1, "052D6748BD3F7D9372EF90971912FDE57EB8FB5E6BCBE7D87F013D81C23BC285") --Main Macos Depot Everlusting Life
+setManifestid(3218712, "2502569085904839989", 347899648)
+addappid(3780300) --Dlcname Everlusting Life - Mascot Alvira
+addappid(3780310) --Dlcname Everlusting Life - Mascot Butch
+addappid(3780320) --Dlcname Everlusting Life - Mascot Bonnie
+addappid(3780330) --Dlcname Everlusting Life - Mascot Ms. Storm
+addappid(3780340) --Dlcname Everlusting Life - Mascot Marylin
+addappid(3780350) --Dlcname Everlusting Life - Mascot A.V.A.
+addappid(3799420) --Dlcname Everlusting Life - Mascots Laura and Dora
+addappid(3815200) --Dlcname Everlusting Life - Mascot Messalina
+addappid(3815210) --Dlcname Everlusting Life - Mascot Marla
+addappid(3815220) --Dlcname Everlusting Life - Mascot Viola
+addappid(3815230) --Dlcname Everlusting Life - Mascot Delilah
+addappid(3815240) --Dlcname Everlusting Life - Mascot Octavianne
+addappid(3838590) --Dlcname Everlusting Life - Mascot Snezhana
+addappid(3838650) --Dlcname Everlusting Life - Mascot Carmilla
+addappid(3865520) --Dlcname Everlusting Life - Mascot Polly
+addappid(3873670) --Dlcname Everlusting Life - Mascot Trixie
+addappid(3873680) --Dlcname Everlusting Life - Mascot Trish
+addappid(3873690) --Dlcname Everlusting Life - Mascot Ingrid
+addappid(3916490) --Dlcname Everlusting Life - Mascot Roxanne
+addappid(3916500) --Dlcname Everlusting Life - Mascot Ruadriel
+addappid(3916510) --Dlcname Everlusting Life - Mascot Lamyris
+addappid(3916520) --Dlcname Everlusting Life - Mascot Fifi
+addappid(3956370) --Dlcname Everlusting Life - Mascot Ula
+addappid(3989440) --Dlcname Everlusting Life - Mascot Cedriel
+addappid(4010000) --Dlcname Everlusting Life - Mascot Vex
+addappid(4039700) --Dlcname Everlusting Life - Mascot Colombina
+addappid(4093780) --Dlcname Everlusting Life - Mascot Luna
+addappid(4151570) --Dlcname Everlusting Life - Mascot Chainkiss
+addappid(4151580) --Dlcname Everlusting Life - Mascot Lucifer
+addappid(4151590) --Dlcname Everlusting Life - Mascot Rory
+addappid(4151600) --Dlcname Everlusting Life - Mascot Corvina
+addappid(4151610) --Dlcname Everlusting Life - Mascot Katherina
+addappid(4229220) --Dlcname Everlusting Life - Mascot Mary
+addappid(4271270) --Dlcname Everlusting Life - Mascot Thanata
+addappid(4271280) --Dlcname Everlusting Life - Mascot Arabella
+addappid(4271290) --Dlcname Everlusting Life - Mascot Blair
+addappid(4379470) --Dlcname Everlusting Life - Mascot Sarissa
+addappid(4379480) --Dlcname Everlusting Life - Mascot Peonny
+addappid(4379490) --Dlcname Everlusting Life - Mascot Serleena
+addappid(4407710) --Dlcname Everlusting Life - Mascot Fifi
+addappid(4502460) --Dlcname Everlusting Life - Mascot Aelita
+addappid(4502470) --Dlcname Everlusting Life - Mascot Narcissa
+addappid(4502480) --Dlcname Everlusting Life - Mascot Castiel
+addappid(4511990) --Dlcname Everlusting Life - Mascot Constance
+addappid(4512000) --Dlcname Everlusting Life - Mascot Mycelia
+addappid(4619770) --Dlcname Everlusting Life - Mascot Shiloh
+addappid(4795480) --Dlcname Everlusting Life - Mascot Iris
+addappid(4795490) --Dlcname Everlusting Life - Mascot Colette
+addappid(4860380) --Dlcname Everlusting Life - Summer Mascot Butch
+addappid(4912450) --Dlcname Everlusting Life - Mascot Keres
+addappid(5006700) --Dlcname Everlusting Life - Mascot Arantis
+addappid(5006710) --Dlcname Everlusting Life - Mascot Katitzi
+addappid(5006720) --Dlcname Everlusting Life - Mascot Coraline
