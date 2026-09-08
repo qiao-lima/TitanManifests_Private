3c44106f4af1759e8999ad376c8a902d75b81527
qiao-lima <qiao-lima@github.com>
2026-09-08 19:48:19 +0800

    Manual Update 3736550

---
+++ 3736550.lua
@@ -0,0 +1,20 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3736550.lua
+--Gamename Work Life Balance
+addappid(3736550) --Mainappid Work Life Balance
+addappid(3736551, 1, "67cc460688483226a4970a7091b700398d247032fc7544c6ba280846f21b5412") --Main Windows Depot Work Life Balance
+setManifestid(3736551, "43797938631369709", 287097056)
+addappid(3736552, 1, "0868fa033693c164a8aaf75c89bbee28ebc4029ef59b6d6d471173083f9b9cdb") --Main Windows Language Depot Schinese Work Life Balance
+setManifestid(3736552, "4684069125874254438", 1499968)
+addappid(3736553, 1, "78af21288721ff0a6729cb462db9cc363f7ec9678bf906f049542edfa99abfaf") --Main Windows Language Depot Japanese Work Life Balance
+setManifestid(3736553, "4705878566087877556", 1519712)
+addappid(3736554, 1, "d416902ba6d504413502a587040c7edf526f8f90e05942b33e1d9722f0ad6433") --Main Macos Depot Work Life Balance
+setManifestid(3736554, "5307944708344265128", 283072976)
+addappid(3736555, 1, "ef41f75bce6d9525c71971e304cafd391b51d106b5290585777d10e6523044fc") --Main Macos Language Depot Schinese Work Life Balance
+setManifestid(3736555, "1355760809135089778", 1500016)
+addappid(3736556, 1, "fd5ca0ef48f9a0054f2d62036539360069d88acfeaff5ae064d35689492b4b95") --Main Macos Language Depot Japanese Work Life Balance
+setManifestid(3736556, "3840947286457262194", 1519712)
+addappid(3736557, 1, "9911b969d118efaf0646a53083ee8ce3485f88db5b0f4ec920e3c4963406fd1c") --Main Windows Language Depot Tchinese Work Life Balance
+setManifestid(3736557, "991715924206143407", 1526928)
+addappid(3736558, 1, "e1ca4d7d3fcc831393ef287a1d5a0ce2d64ca85f8a31d6926cb27ee9b8175381") --Main Macos Language Depot Tchinese Work Life Balance
+setManifestid(3736558, "2084856122712352468", 1526928)
