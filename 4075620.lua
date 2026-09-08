459ed203620a435666031e03cca198f73aec418d
qiao-lima <qiao-lima@github.com>
2026-09-08 19:50:37 +0800

    Manual Update 4075620

---
+++ 4075620.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4075620.lua
+--Gamename Combolands: Roguelike Citybuilder
+addappid(4075620, 1, "504f6eb7e6f4f87aef685dbc6b06fc0d241cf119f9d81f3d565559d8dd67c4b3") --Mainappid Combolands: Roguelike Citybuilder
+addappid(4075621, 1, "b6ca94ca10eaf4aea16292ac6c291acbbb0feba390f26f3abcf4d61504a66c68") --Main Windows Depot Combolands: Roguelike Citybuilder
+setManifestid(4075621, "7260698970157077224", 207770224)
