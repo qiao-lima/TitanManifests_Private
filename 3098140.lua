7e49eda7e290a33b878d45345db1ec20b3ae35d6
qiao-lima <qiao-lima@github.com>
2026-09-08 19:37:59 +0800

    Manual Update 3098140

---
+++ 3098140.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3098140.lua
+--Gamename Crown and Adventure
+addappid(3098140) --Mainappid Crown and Adventure
+addappid(3098141, 1, "fd797fa8dd21e697a60ebe34ae9376d72af28d3efd07a78be6a7c8873ce4a2b0") --Main Windows Depot Crown and Adventure
+setManifestid(3098141, "3191973783664121762", 5655177344)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
