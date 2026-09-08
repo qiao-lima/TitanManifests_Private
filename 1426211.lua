e84e29746826b9a0a25c9d885bea6f55b85f8f51
qiao-lima <qiao-lima@github.com>
2026-09-08 19:32:52 +0800

    Manual Update 1426211

---
+++ 1426211.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1426211.lua
+--Gamename AppID_1426211
+addappid(1426211, 1, "c450c44e032e27d0308cf864259fb2af91bcfc58401a0caf33bb8efba85bb323") --Mainappid AppID_1426211
+addappid(1426212, 1, "99090c0728bfb9251f59c85c9f06484721d78edc05c8ae341ec5c9467a1f5a1d") --Main Windows Depot AppID_1426211
