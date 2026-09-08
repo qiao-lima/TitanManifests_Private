cd128c8706c0db9b62cccb723b372658a29d86fc
qiao-lima <qiao-lima@github.com>
2026-09-08 19:54:03 +0800

    Manual Update 5084260

---
+++ 5084260.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5084260.lua
+--Gamename Fireboy & Watergirl 1: The Forest Temple
+addappid(5084260) --Mainappid Fireboy & Watergirl 1: The Forest Temple
+addappid(5084261, 1, "62a626ded6d4bf357d49c90ee02045b154b09dfbba1b7dcc93199e864d84c8e0") --Main Windows Depot Fireboy & Watergirl 1: The Forest Temple
+setManifestid(5084261, "7724257014901417296", 75766816)
