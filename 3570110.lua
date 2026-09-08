a57541f9ce2db562ba2cd9946906b989f431ea49
qiao-lima <qiao-lima@github.com>
2026-09-08 19:42:52 +0800

    Manual Update 3570110

---
+++ 3570110.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3570110.lua
+--Gamename 神之一手
+addappid(3570110) --Mainappid 神之一手
+addappid(3570111, 1, "ff9de3a74610c416d8fd320dfcb9d1f8fa9874ed3bee931eee92d6cb6d2ec3ec") --Main Windows Depot 神之一手
+setManifestid(3570111, "6465078544811061411", 496784736)
