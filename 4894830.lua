1bd6307918dce44be9cc58035d4dd98e34519606
qiao-lima <qiao-lima@github.com>
2026-09-08 19:54:01 +0800

    Manual Update 4894830

---
+++ 4894830.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4894830.lua
+--Gamename Sandcastle Demo
+addappid(4894830) --Mainappid Sandcastle Demo
+addappid(4894831, 1, "73246addf14f0144e5c26a216ef8aabbda7dc431cffb128bd7c7043e4777be96") --Main Windows Depot Sandcastle Demo
+setManifestid(4894831, "2041654968494746010", 145783040)
