e4d2721219730008c5a9cc78a8936b2ed86f6bca
qiao-lima <qiao-lima@github.com>
2026-09-08 19:53:58 +0800

    Manual Update 4738790

---
+++ 4738790.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4738790.lua
+--Gamename Happy Endings Demo
+addappid(4738790) --Mainappid Happy Endings Demo
+addappid(4738791, 1, "fdf556ee24e7ce5c3b78513849a28de60545af4712ca12376321441354fc2e49") --Main Windows Depot Happy Endings Demo
+setManifestid(4738791, "68328887794037513", 139027856)
+addappid(4738794, 1, "2348b7214edb7f7c8f35637c1e31c1f95b8d9cf789fb98a7079725797fcffdb4") --Main Linux Depot Happy Endings Demo
+setManifestid(4738794, "5955984151226006539", 129419088)
