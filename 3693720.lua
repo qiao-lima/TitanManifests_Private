0cdff15d3af2849725d07d8d52de5e238e44cd1f
qiao-lima <qiao-lima@github.com>
2026-09-08 19:48:17 +0800

    Manual Update 3693720

---
+++ 3693720.lua
@@ -0,0 +1,21 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3693720.lua
+--Gamename Concerto on White: Cajon Story
+addappid(3693720) --Mainappid Concerto on White: Cajon Story
+addappid(3693721, 1, "6adda05d1aa955936a8f0b5cd67219d6dd98ea602fb8816a7fee9def6a5147f0") --Main Windows Depot Concerto on White: Cajon Story
+setManifestid(3693721, "3729704873118209676", 975565344)
+--Dlcname Concerto on White: Cajon Story - Touhou Project Music Pack
+addappid(4001970, 1, "994dba09df3be53d4c6b5bf2edafc1ddc2d759a5a5c150176932614ab3018632") --Dlcname Concerto on White: Cajon Story - Touhou Project Music Pack
+setManifestid(4001970, "645914258465888403", 39287632)
+--Dlcname Concerto on White: Cajon Story - PYKAMIA Music Pack Vol.1
+addappid(4270720, 1, "ed2ac056c21d23250cb5ba5813727e693af4762ba69dc0decbc761f7bc7310ef") --Dlcname Concerto on White: Cajon Story - PYKAMIA Music Pack Vol.1
+setManifestid(4270720, "9078872360366699438", 46115808)
+--Dlcname Concerto on White: Cajon Story - PYKAMIA Music Pack Vol.2
+addappid(4270730, 1, "f37e31d952f777a46b78ad916cfac74fd729b1ac4a94483cf24921472b9a95c1") --Dlcname Concerto on White: Cajon Story - PYKAMIA Music Pack Vol.2
+setManifestid(4270730, "7096191563402027813", 42218992)
+addappid(4499080) --Dlcname Concerto on White: Cajon Story - Marimbeira Music Pack
+addappid(5101680) --Dlcname Concerto on White: Cajon Story - Digital Artbook
+--Missing Dlcs
+--Dlcname Concerto on White: Cajon Story - Livemania Music Pack
+--Dlcname Concerto on White: Cajon Story - Luna Music Pack
+--Dlcname Concerto on White: Cajon Story - Berry Melody Music Pack
