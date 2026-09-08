b7831f796c8b61f54dda2268235e679d405a6fb4
qiao-lima <qiao-lima@github.com>
2026-09-08 19:32:54 +0800

    Manual Update 1643650

---
+++ 1643650.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1643650.lua
+--Gamename Velis
+addappid(1643650) --Mainappid Velis
+addappid(1643651, 1, "d123ba4514ba8c0055bb83be3ac8cc87433d41da4d2e71da114087fc7b10fa7e") --Main Windows Language Depot Schinese Velis
+setManifestid(1643651, "5375156388413187226", 156650576)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
