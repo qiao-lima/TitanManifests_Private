ff270a567f0dc7dbbbdc5e5f1a493c5696538c08
qiao-lima <qiao-lima@github.com>
2026-09-08 19:51:37 +0800

    Manual Update 4355490

---
+++ 4355490.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4355490.lua
+--Gamename Sex-Pop Demon Hunters
+addappid(4355490) --Mainappid Sex-Pop Demon Hunters
+addappid(4355491, 1, "d1d255f2f54ba269a16eb39d8a72dddbb8e055dba5cbfa452efe8dc41cbcf75f") --Main Windows Depot Sex-Pop Demon Hunters
+setManifestid(4355491, "7819620673269099316", 1469258352)
+addappid(5054990) --Dlcname Sex-Pop Demon Hunters - Kinky Crimes
+--Missing Dlcs
+--Dlcname Sex-Pop Demon Hunters - Soundtrack
+--Dlcname Sex-Pop Demon Hunters - Animations Pack
+--Dlcname Sex-Pop Demon Hunters - Wallpapers Pack
+--Dlcname Sex-Pop Demon Hunters - Digital Artbook
