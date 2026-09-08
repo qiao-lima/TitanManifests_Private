3b84b6fc4199475dc86deb7d4e71d7b11df317f4
qiao-lima <qiao-lima@github.com>
2026-09-08 19:42:53 +0800

    Manual Update 3570370

---
+++ 3570370.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3570370.lua
+--Gamename CARIMARA: Beneath the forlorn limbs
+addappid(3570370) --Mainappid CARIMARA: Beneath the forlorn limbs
+addappid(3570372, 1, "09523c1d6511cd6591b36ed5b1d224329294afacd7ec947d803c269eeae1da68") --Main Macos Depot CARIMARA: Beneath the forlorn limbs
+setManifestid(3570372, "1423791342513120192", 269171312)
+addappid(3570373, 1, "4b19ddee3140df89a7334f21dc87be2ecebf8aa3acde63ecca13292eb2e694b5") --Main Windows Depot CARIMARA: Beneath the forlorn limbs
+setManifestid(3570373, "29460777853865779", 271536608)
