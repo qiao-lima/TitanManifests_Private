548bde280076fc0aed745406776957173b76dba0
qiao-lima <qiao-lima@github.com>
2026-09-08 18:34:24 +0800

    Manual Update 4809400

---
+++ 4809400.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4809400 - 长生仙途
+-- Generated 2026-09-08 08:14:46 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4809400)
+
+-- Main Depots
+addappid(4809401, 1, "1d49ea8d83399f48a82c3365c5c5ba6bf1729f0b89f2841a558a669c2805702f")
+setManifestid(4809401, "4701320726618214786", 160920824)
