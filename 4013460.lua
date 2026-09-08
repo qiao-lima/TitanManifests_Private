2ec9216e366805c21039fd389cbb0e6eebcf3c39
qiao-lima <qiao-lima@github.com>
2026-09-08 19:50:36 +0800

    Manual Update 4013460

---
+++ 4013460.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4013460.lua
+--Gamename Mushroom Island Work Diary
+addappid(4013460, 1, "947f6f9a96dab7797c1b93c905f4b5cd7c9d2e51820d2b6116bcfce9e717811e") --Mainappid Mushroom Island Work Diary
+addappid(4013461, 1, "b6d2cd121138a1064eac0083dca2a08a0e015e5a01d000a1e88f864880771054") --Main Windows Depot Mushroom Island Work Diary
+setManifestid(4013461, "2551449548713814404", 1041250592)
