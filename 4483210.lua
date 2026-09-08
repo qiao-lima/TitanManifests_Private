a17628b1732ceed59caa53b21ea9fa0e56d08bbc
qiao-lima <qiao-lima@github.com>
2026-09-08 19:51:41 +0800

    Manual Update 4483210

---
+++ 4483210.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4483210.lua
+--Gamename The Too-Cute Girl Who Moved in Next Door
+addappid(4483210) --Mainappid The Too-Cute Girl Who Moved in Next Door
+addappid(4483211, 1, "a20a682e9140edbfe57f07483bc603143c944cc62903a5c4c0275b9584b29e30") --Main Windows Depot The Too-Cute Girl Who Moved in Next Door
+setManifestid(4483211, "5113103607287693529", 286457600)
