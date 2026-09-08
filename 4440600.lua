096194ee4dd73cb68b829b41c4e0e0180dc361f1
qiao-lima <qiao-lima@github.com>
2026-09-08 19:51:39 +0800

    Manual Update 4440600

---
+++ 4440600.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4440600.lua
+--Gamename Hamsteria Demo
+addappid(4440600) --Mainappid Hamsteria Demo
+addappid(4440601, 1, "aa9023a1b8ad066e15763828fd19fea90d2f2b645c6b5009f6d89a9f43015d6b") --Main Windows Depot Hamsteria Demo
+setManifestid(4440601, "4810214490612839853", 2390606064)
