a732abad71991aaf9660dc8bb7f56ec90bec4a2f
qiao-lima <qiao-lima@github.com>
2026-09-08 19:32:53 +0800

    Manual Update 1513790

---
+++ 1513790.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1513790.lua
+--Gamename The Unbreakable Chain
+addappid(1513790) --Mainappid The Unbreakable Chain
+addappid(1513791, 1, "bfa03b51f90c3a610bb68029ca332460dbe0a317ad2c3d07c4136502f372abc7") --Main Windows Depot The Unbreakable Chain
+setManifestid(1513791, "6076726841345772930", 96336656)
