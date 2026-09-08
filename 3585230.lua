460589760585a7d847fd989f2b3ec14af6101b46
qiao-lima <qiao-lima@github.com>
2026-09-08 19:42:53 +0800

    Manual Update 3585230

---
+++ 3585230.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3585230.lua
+--Gamename Pro Basketball Manager 2026
+addappid(3585230, 1, "fcd38c63a9f93993c0d7d301e049f377ca4f0453f4507fa8722e97c491fb13ff") --Mainappid Pro Basketball Manager 2026
+addappid(3585231, 1, "bab5253869995441780e31e4f1e533cbe6126fa3bffc921da87c5e061f880878") --Main Windows Depot Pro Basketball Manager 2026
+setManifestid(3585231, "6979693433737596616", 2825835328)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
