f86ad0c4c69e90abeb7d745a4052dce9413e9a5d
qiao-lima <qiao-lima@github.com>
2026-09-08 19:32:50 +0800

    Manual Update 1148130

---
+++ 1148130.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1148130.lua
+--Gamename Divinum
+addappid(1148130, 1, "d667b5371575f1f1aa943bcdcd27e9d967c797f91f1a28d7517cbb8150b8e238") --Mainappid Divinum
+addappid(1148131, 1, "d51f35c9961ee97bfc99d4119281c8e0584cdb1616848d9702b73fe486bc975b") --Main Windows Depot Divinum
+setManifestid(1148131, "8082354149438763240", 472112928)
+addappid(1148132, 1, "9ce728f8e09394670d73111ec2ae5ae1d3f2569695d0eb31f03d83f73d538296") --Main Macos Depot Divinum
+setManifestid(1148132, "6691350325884332683", 481996304)
