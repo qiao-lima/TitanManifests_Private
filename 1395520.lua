d750e476353cd767293d27e01f0b30b5fc49c8cd
qiao-lima <qiao-lima@github.com>
2026-09-08 19:32:52 +0800

    Manual Update 1395520

---
+++ 1395520.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1395520.lua
+--Gamename The Séance of Blake Manor
+addappid(1395520) --Mainappid The Séance of Blake Manor
+addappid(1395525, 1, "74a594f4c8f56408880e8b86392a18804bdb951190f94ae55a751fac59e85b4f") --Main Windows Depot The Séance of Blake Manor
+setManifestid(1395525, "3435447801037222351", 1255530928)
+addappid(4159440, 1, "be673d32d476e178db3d5c7787a26c27e616afb3f6fae755fc14bfacd7e71fcd") --Main Windows Depot The Séance of Blake Manor
+setManifestid(4159440, "1854653772660097955", 76109776)
+--Dlcname The Séance of Blake Manor – Digital Artbook
+--Missing Dlcs
+--Dlcname The Séance of Blake Manor – Original Soundtrack
