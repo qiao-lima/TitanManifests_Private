dd1d842d6b191115e88c7ce6df0767826c9dfdda
qiao-lima <qiao-lima@github.com>
2026-09-10 07:00:05 +0800

    Manual Update 3219630

---
+++ 3219630.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3219630.lua
+--Gamename Halloween: The Game
+addappid(3219630) --Mainappid Halloween: The Game
+addappid(3219631, 1, "b189d305a354192815d0b3c4ab226bee93b92224596cb287589653b6a9de92cd") --Main Windows Depot Halloween: The Game
+setManifestid(3219631, "751841436966774928", 30189624240)
+addappid(4686720) --Dlcname Halloween: Digital Deluxe Upgrade
+addtoken(4686720, "11055506196269817430")
