404b61a463e13b15bcc50b2875ae160c4265f2f0
qiao-lima <qiao-lima@github.com>
2026-09-10 06:59:54 +0800

    Manual Update 2706020

---
+++ 2706020.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2706020.lua
+--Gamename ALL WILL FALL
+addappid(2706020, 1, "1e77dbc1e9853e8b3745dc5f83ea2f91a1a6235c8d318ce3b2d16c5396d90ba5") --Mainappid ALL WILL FALL
+addappid(2706021, 1, "8ca6191f4f09dcb309688901e511599e604be2d09f060774e028dce04bd063e3") --Main Windows Depot ALL WILL FALL
+setManifestid(2706021, "3519247360334837771", 1920137696)
+--Missing Dlcs
+--Dlcname All Will Fall Soundtrack
