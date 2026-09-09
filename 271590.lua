08b3a0bdc1dc27d45bb27e1864a6b5e3526b185e
qiao-lima <qiao-lima@github.com>
2026-09-10 06:59:54 +0800

    Manual Update 271590

---
+++ 271590.lua
@@ -0,0 +1,27 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 271590.lua
+--Gamename Grand Theft Auto V Legacy
+addappid(271590, 1, "8f165460b5772d55a815775064a9735dee57b9bb222d7fa1eeb1c261fe8dd6ad") --Mainappid Grand Theft Auto V Legacy
+addappid(271591, 1, "baebeedec48b33c68a322fea0cbaa8d023d1dab4d2870aa909b82c33caa53941") --Main Windows Depot Grand Theft Auto V Legacy
+setManifestid(271591, "5509090605608271459", 48009184)
+addappid(271592, 1, "e34313f0ec133a608e636cf04fd879d3ac1e8e10af38542436940be1b6834bee") --Main Windows Depot Grand Theft Auto V Legacy
+setManifestid(271592, "8179666891023172621", 28716656)
+addappid(271593, 1, "71b57be5c50e5fbc819068a5bf31db42b653f1c75d88307da682ff0d5cae9457") --Main Windows Depot Grand Theft Auto V Legacy
+setManifestid(271593, "2967789647252082634", 16813844576)
+addappid(271594, 1, "ae7ed6931a136d41d3aae146563368eb3b3e4e60963344f3f426c0abc74bbdab") --Main Windows Depot Grand Theft Auto V Legacy
+setManifestid(271594, "7977512212816798234", 68411437424)
+addappid(271595, 1, "6dc0d2ce6b9dc96469ff792f6a607f759f0abd851d496cac73d71cd145c70b4d") --Main Windows Depot Grand Theft Auto V Legacy
+setManifestid(271595, "1376135673068470825", 39063550000)
+--Share Depots
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1899671, 1, "b7921da5e50d00b2238d0fe870a354cb572bc5d397955fef02a439103f62827b") --Share Windows Depot RGL/SC
+setManifestid(1899671, "3914969219305676290", 209058416)
+addappid(362000) --Dlcname Grand Theft Auto V – Bonus $1,500,000 + GTA: San Andreas
+addappid(362001) --Dlcname Grand Theft Auto V – Bonus $1,500,000
+addappid(362002) --Dlcname Grand Theft Auto V – Bonus $1,350,000
+addappid(362003) --Dlcname Grand Theft Auto V
+addappid(413180) --Dlcname Grand Theft Auto V – Bonus $500,000
+addappid(771300) --Dlcname Grand Theft Auto V Legacy - Criminal Enterprise Starter Pack
