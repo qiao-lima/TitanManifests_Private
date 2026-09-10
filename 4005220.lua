62969a8a01d205cc97c997e0c1a7cdfe465a7d21
qiao-lima <qiao-lima@github.com>
2026-09-10 21:41:09 +0800

    Manual Update 4005220

---
+++ 4005220.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4005220.lua
+--Gamename Deep Blue Sushi
+addappid(4005220) --Mainappid Deep Blue Sushi
+addappid(4005221, 1, "a6d69f88f05a5cdd8e5a895a9bb7c771ddf8cb52bdd683b7042e95adbb945a1b") --Main Windows Depot Deep Blue Sushi
+setManifestid(4005221, "1858215042344541383", 2333987296)
