1f7cdc1826707f257d102835aaf182f526452b6c
qiao-lima <qiao-lima@github.com>
2026-09-10 21:41:08 +0800

    Manual Update 3170490

---
+++ 3170490.lua
@@ -0,0 +1,18 @@
+-- 3170490's Lua and Manifest Created by Hubcap Manifest
+-- Cat Me If You Can
+-- Created: September 08, 2026 at 15:24:30 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3170490) -- Cat Me If You Can
+-- MAIN APP DEPOTS
+addappid(3170491, 1, "83985420d0ce60b3b3d6c4637286b816c07628d9135d449ab2e997530eb69cc5") -- Depot 3170491
+setManifestid(3170491, "6226680451039899141", 5658816275)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
