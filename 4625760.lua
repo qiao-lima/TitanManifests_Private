4fcb2701925f3602f07d4aa8d6caf24589ce4b77
qiao-lima <qiao-lima@github.com>
2026-09-10 21:41:09 +0800

    Manual Update 4625760

---
+++ 4625760.lua
@@ -0,0 +1,12 @@
+-- 4625760's Lua and Manifest Created by Hubcap Manifest
+-- 山上那个猴儿
+-- Created: August 26, 2026 at 05:02:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4625760) -- 山上那个猴儿
+-- MAIN APP DEPOTS
+addappid(4625761, 1, "def855560c72b275f39ea2f79c37259491ae354cc5a2d76192301a18b6d9fedf") -- Depot 4625761
+setManifestid(4625761, "567884159229260517", 126816825)
