fe16e5edf5d61488e557866c421352f68afa0d8c
titan-cangku <titan-cangku@gamefun365.com>
2026-09-10 19:32:51 -0400

    restock 4622110

---
+++ 4622110.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4622110.lua
+--Gamename 我的傲娇妹妹 MY TSUNDERE SISTER
+addappid(4622110) --Mainappid 我的傲娇妹妹 MY TSUNDERE SISTER
+addappid(4622111, 1, "5654edef32b9dba8164fa8a69476d414dab331f3333a8ebc3569d841bf93ac1c") --Main Windows Depot 我的傲娇妹妹 MY TSUNDERE SISTER
+setManifestid(4622111, "2837075777114143615", 894286000)
