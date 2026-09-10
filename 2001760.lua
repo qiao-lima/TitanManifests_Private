9016607f638a0cee57488ab3979d25a5bf252b63
qiao-lima <qiao-lima@github.com>
2026-09-11 01:23:19 +0800

    Manual Update 2001760

---
+++ 2001760.lua
@@ -0,0 +1,32 @@
+-- ============================================================
+-- CRACK WORLD AUTOMATED STEAM CONFIG
+-- Generated and compiled by Crack World
+-- ============================================================
+-- Title:   Beast of Reincarnation
+-- AppID:   2001760
+-- Created: September 10, 2026 at 02:09:10 PM
+-- Discord: https://discord.gg/crackworld
+-- ============================================================
+
+-- [Core Application]
+addappid(2001760, 1, "5a7d287208fe812255055e80d0e5f02b15836607d90c100f2760a85cbf70cb33") -- Beast of Reincarnation
+
+-- [Content Depots]
+addappid(2001761, 1, "0d843c3fd4bba4400b829693c665e6394ad8311f3f7f6c0acf31c71eab887c0c")
+setManifestid(2001761, "5820605326682716309", 31561982976)
+
+-- [Shared Dependencies]
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
+setManifestid(228990, "1829726630299308803", 100658080)
+
+-- [DLC Entitlements]
+addappid(4146810, 1)
+addappid(4146830, 1)
+
+-- ============================================================
+-- Total Depots: 3
+-- Shared Depots: 2
+-- DLCs:         2
+-- ============================================================
