f58c8659a1010689a08c4b3826739190fa20b672
qiao-lima <qiao-lima@github.com>
2026-09-10 10:39:29 +0800

    Manual Update 2783870

---
+++ 2783870.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2783870.lua
+--Gamename 嘶哈
+addappid(2783870) --Mainappid 嘶哈
+addappid(2783871, 1, "497a7d0cd6b65d6094883d8c1d212d0655970ecd5406dac88fb819e47a1b97a1") --Main Windows Depot 嘶哈
+setManifestid(2783871, "5256435804040767905", 15544313488)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
