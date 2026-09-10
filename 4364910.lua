267cda912c069db4addf341c0473a7baad897b13
titan-cangku <titan-cangku@gamefun365.com>
2026-09-10 19:38:19 -0400

    restock 4364910

---
+++ 4364910.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4364910.lua
+--Gamename Ruins of Dawn
+addappid(4364910) --Mainappid Ruins of Dawn
+addappid(4364911, 1, "1ca0d151f24d942dd4e67ca79073604c05339db9cacb13362be1e7343aef6f59") --Main Windows Depot Ruins of Dawn
+setManifestid(4364911, "1954631530023888339", 2101326432)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
