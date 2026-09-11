0ced262eb24feb5ca53c3f202bde6b885e1a8cb6
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 10:30:47 -0400

    restock 1175080

---
+++ 1175080.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1175080.lua
+--Gamename Easy Bug Manager
+addappid(1175080) --Mainappid Easy Bug Manager
+addappid(1175081, 1, "82c17b9d6405e3fe95d79ddc6c3db7dd3611c93a5f8593cc870db0895a9198e9") --Main Windows Depot Easy Bug Manager
+setManifestid(1175081, "5544986577865374320", 24181776)
+--Share Depots
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
+--Missing Dlcs
+--Dlcname Easy Bug Manager - Cat Theme
