423631b3035bd4a23f74ea3b4485ace0041c3638
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 10:51:29 -0400

    restock 2399910

---
+++ 2399910.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2399910.lua
+--Gamename Byakuya Museum
+addappid(2399910) --Mainappid Byakuya Museum
+addappid(2399911, 1, "4d4a0639998c5613ef27a316980a362bd1f2141af4ef27ada817b05c89b200b0") --Main Windows Depot Byakuya Museum
+setManifestid(2399911, "1822267552696204580", 594433488)
+--Dlcname 白夜博物馆-周年纪念刊
+addappid(3763530, 1, "8641c30863bd53e8064eec2cc7f7f194943629d281d7b99e37700934373fe743") --Dlcname 白夜博物馆-周年纪念刊
+setManifestid(3763530, "3497203957262470758", 484196976)
