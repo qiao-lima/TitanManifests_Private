376e376d0d59015228ab4471224be217d899513a
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 05:59:17 -0400

    restock 2407990

---
+++ 2407990.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2407990.lua
+--Gamename Atomic Heart Demo
+addappid(2407990) --Mainappid Atomic Heart Demo
+addappid(2407991, 1, "8dd7d33327bc6c1cd6a0d57a2d8a3b4daae24a7da9b8ed05d604812a8e8b847e") --Main Windows Depot Atomic Heart Demo
+setManifestid(2407991, "6966247888851551753", 27888178848)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
