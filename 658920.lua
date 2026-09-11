ab93e35601c84f2d931e13405f1e23b1f3851b86
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 08:40:13 -0400

    restock 658920

---
+++ 658920.lua
@@ -0,0 +1,25 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 658920.lua
+--Gamename Half-Life 2: VR Mod
+addappid(658920, 1, "31cadb91f30d7944530d9ce94b1ff7c9a857a94e2f4847304ae769b1f0f0c091") --Mainappid Half-Life 2: VR Mod
+addappid(658921, 1, "bf05172a5920c5e1a6ba9874c83a1818cdba9fa1c2a454acbd9f96437d985c2b") --Main Windows Depot Half-Life 2: VR Mod
+setManifestid(658921, "2069911326857246809", 2065523200)
+addappid(658922, 1, "d1042677b012a687c3092aa3a9482943d4a7514e0a072371a9bc9f8410f43e89") --Main Windows Language Depot Schinese Half-Life 2: VR Mod
+setManifestid(658922, "6928326666383052808", 597118832)
+addappid(658923, 1, "0583cad37d3743eb5c9c91a9cd24b48462b45729ced4e4ea210bc0401bc3ecb7") --Main Windows Language Depot Italian Half-Life 2: VR Mod
+setManifestid(658923, "8102361801836811717", 607746768)
+addappid(658924, 1, "140ba2760fef31b67c0559f3a6d6fa4b33a94d6a140bbccd6dc3cf5dece12419") --Main Windows Language Depot Russian Half-Life 2: VR Mod
+setManifestid(658924, "5882571423559759093", 652067600)
+addappid(658925, 1, "8def9e7d0cc6bbb5284870ff26601645cfe9cf28e585aa729954f8d56776b72a") --Main Windows Language Depot Spanish Half-Life 2: VR Mod
+setManifestid(658925, "3804936728775688894", 606072672)
+addappid(658926, 1, "5e66881dad038b28b36b3f02ebbfa34f7a180e41230daf057bbd4a13136b54e2") --Main Windows Language Depot French Half-Life 2: VR Mod
+setManifestid(658926, "4570926226610948742", 593479056)
+addappid(658927, 1, "e7d83e4b158d82e262516627ec9ecba4574ddd80701291ee80badc62b7d219d4") --Main Windows Language Depot German Half-Life 2: VR Mod
+setManifestid(658927, "6342728565492612622", 599364208)
+addappid(658928, 1, "d1e2312f7b12e07ded09a1427fb188612d5d0f0850bbe5870b140a2a1b9dbf5d") --Main Windows Language Depot Koreana Half-Life 2: VR Mod
+setManifestid(658928, "5574630230695672735", 556896560)
+addappid(658929, 1, "3bd410f89d59c57645e5ed8dc231df53429c5dcee376d0fc289f385f502bce6f") --Main Windows Language Depot Tchinese Half-Life 2: VR Mod
+setManifestid(658929, "3508182197954080878", 562323184)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
