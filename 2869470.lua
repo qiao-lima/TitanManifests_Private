9a7346c7a89bf5b8da1bff1add204513cef334b8
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 06:04:31 -0400

    restock 2869470

---
+++ 2869470.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2869470.lua
+--Gamename Survivors
+addappid(2869470) --Mainappid Survivors
+addappid(2869471, 1, "e7a0c5c5773c733b56266c5829031b8b60b642d407c58040ca1ccbf4bfd3a5ee") --Main Windows Depot Survivors
+setManifestid(2869471, "3481652913248115333", 627391520)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
