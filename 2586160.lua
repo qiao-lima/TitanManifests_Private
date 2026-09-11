dd9e3c6ddec043afad2ea4b585f650f4777a9d24
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 13:10:06 -0400

    restock 2586160

---
+++ 2586160.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2586160.lua
+--Gamename Paper Planes
+addappid(2586160) --Mainappid Paper Planes
+addappid(2586161, 1, "c5710c3cb0ff2c4791c1e84feae6e02591107fca0d6a45847581cafd85aacdf9") --Main Windows Depot Paper Planes
+setManifestid(2586161, "3463416652459135710", 352744368)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
