683b756573f9a87b8b4505a3e99d96b3cd503763
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 04:15:11 -0400

    restock 680440

---
+++ 680440.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 680440.lua
+--Gamename Wallpaper Transformer
+addappid(680440) --Mainappid Wallpaper Transformer
+addappid(680441, 1, "d86be392c50a070fab4d30b60f1ad3e5cd426e3bf0e8c7a0766aed94a4d2f616") --Main Windows Depot Wallpaper Transformer
+setManifestid(680441, "2255485143128664440", 119576784)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229000, "4622705914179893434", 242681440)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
