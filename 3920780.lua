3d1dcc93cc14479a0747b35c187d7ccc7dc52ace
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 09:00:09 -0400

    restock 3920780

---
+++ 3920780.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3920780.lua
+--Gamename 最后的飞船
+addappid(3920780) --Mainappid 最后的飞船
+addappid(3920781, 1, "14d8876500f860dc74a39c5210744308588754aa4676ec0a7a7b8704d461919f") --Main Windows Depot 最后的飞船
+setManifestid(3920781, "4390375640216348026", 854769280)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
