92a2acc3696c5e034a546e9e025ee317957f06f6
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 07:43:15 -0400

    restock 1081160

---
+++ 1081160.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1081160.lua
+--Gamename Into The Core
+addappid(1081160) --Mainappid Into The Core
+addappid(1081161, 1, "49b2369340ff1b38247eb8e90523d89ed919ac88c6b96938b6054d41d344dba4") --Main Windows Depot Into The Core
+setManifestid(1081161, "4199569785232703089", 714560224)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
