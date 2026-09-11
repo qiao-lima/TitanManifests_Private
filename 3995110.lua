e6206dd2103ca069d9ca1517dd567b4778d7b584
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 07:20:17 -0400

    restock 3995110

---
+++ 3995110.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3995110.lua
+--Gamename 女儿国的诅咒 The Curse of Womanland
+addappid(3995110) --Mainappid 女儿国的诅咒 The Curse of Womanland
+addappid(3995111, 1, "622021b638c5b6f45c5e7d5cbaa8ab9ff928204817b8424f09012d79167da386") --Main Windows Depot 女儿国的诅咒 The Curse of Womanland
+setManifestid(3995111, "3427227932286671130", 1460439072)
