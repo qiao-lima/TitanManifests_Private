090e96002e7e9d179e7a1dd930b5dd4c39c1b2ee
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 06:04:58 -0400

    restock 837400

---
+++ 837400.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 837400.lua
+--Gamename Fated Kingdom
+addappid(837400) --Mainappid Fated Kingdom
+addappid(837401, 1, "951f87c2e607da74dfb90e79279a3a1cc0048246b7eb745e8525a6c7b74e0e0a") --Main Windows Depot Fated Kingdom
+setManifestid(837401, "82088322087743273", 485243120)
+--Share Depots
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229000, "4622705914179893434", 242681440)
+addappid(229001, 1, "ec23e1a1e1da6205722291f91bf72d1989cb347245e5a14d8d514c4ad5ba4e97") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229001, "4049573910112143457", 268006912)
