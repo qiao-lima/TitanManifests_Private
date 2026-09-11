f39776fe9ade0cfb096ba9190ef5b959fc400b30
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 10:27:29 -0400

    restock 4243700

---
+++ 4243700.lua
@@ -0,0 +1,17 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4243700.lua
+--Gamename 27-year-old Female Teacher – A Moment in Daily Life, Chapter 1
+addappid(4243700) --Mainappid 27-year-old Female Teacher – A Moment in Daily Life, Chapter 1
+addappid(4243701, 1, "1f49122e6ec4353520cd46beb840b3c94217d38b2bea55bf35d32a1600edb65e") --Main Windows Language Depot Japanese 27-year-old Female Teacher – A Moment in Daily Life, Chapter 1
+setManifestid(4243701, "8019971928115642247", 915326960)
+addappid(4243702, 1, "6296e2d8c9c9211cf11ec7e0ae23f2f152a713e5ff6ef6f7c52671e7ab9372ea") --Main Windows Language Depot Schinese 27-year-old Female Teacher – A Moment in Daily Life, Chapter 1
+setManifestid(4243702, "8527116022520930274", 914743152)
+addappid(4243703, 1, "4a207963715e9ca91e41fe39cf30fafc8656a8dcb1cd22ce662c027449c5fa2f") --Main Windows Language Depot Tchinese 27-year-old Female Teacher – A Moment in Daily Life, Chapter 1
+setManifestid(4243703, "1033387213710585619", 914743152)
+addappid(4243704, 1, "3aebefdb2bf977f22d0b03e140723af9ac5200041cdcf6a0d7146cd9926cb4d7") --Main Windows Language Depot English 27-year-old Female Teacher – A Moment in Daily Life, Chapter 1
+setManifestid(4243704, "2064331435679259079", 914743152)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
