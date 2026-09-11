160d36b32599b775e61c98ae4c08526910bb8ed0
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 08:58:48 -0400

    restock 2755480

---
+++ 2755480.lua
@@ -0,0 +1,24 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2755480.lua
+--Gamename The Censor DX Edition
+addappid(2755480, 1, "c286569c5a52094adae88ba151cd7148c60246821a2ffa060e81c6b056c56e57") --Mainappid The Censor DX Edition
+addappid(2755481, 1, "b5c225997ce7a240b0666f448a99189ebe6ac7fd0d3fc0be8bd148bc6aa3cf7f") --Main Windows Depot The Censor DX Edition
+setManifestid(2755481, "4152286130615104709", 841978480)
+--Dlcname The Censor DX Edition - Soundtrack
+addappid(3985140) --Dlcname The Censor DX Edition - Soundtrack
+addappid(3985141, 1, "5d04b10d562fbe06bdfc4110ef63ba157563559f6d32359993fb9c6a106ee8c1") --Dlc Windows Depot The Censor DX Edition - Soundtrack
+setManifestid(3985141, "733762560899494275", 8042144)
+--Dlcname The Censor DX Edition - ArtBook
+addappid(3985240) --Dlcname The Censor DX Edition - ArtBook
+addappid(3985241, 1, "6d0c26da70e7fdc64b17ef27250dc75d3aa964997eb9e5ba6f3c53830277fb60") --Dlc Windows Language Depot English The Censor DX Edition - ArtBook
+setManifestid(3985241, "2222090472775562595", 42524320)
+addappid(3985242, 1, "4478929d8b9317350ec21beca470e04461f3091f6aae1388d4c02f1cf66c3a99") --Dlc Windows Language Depot Tchinese The Censor DX Edition - ArtBook
+setManifestid(3985242, "7636927748298626872", 36190832)
+addappid(3985243, 1, "09d94d035d4e9c06b1b12c54c47c166eeb400d06bc9457ed640c575897b0d3c6") --Dlc Windows Language Depot Schinese The Censor DX Edition - ArtBook
+setManifestid(3985243, "7605395861022432205", 36185504)
+addappid(3985244, 1, "6031ff47bc1a45fc972d4076ede32b4aad3523d814d3afa903bdbe32645651d4") --Dlc Windows Language Depot Japanese The Censor DX Edition - ArtBook
+setManifestid(3985244, "162207239317546225", 36179872)
+addappid(3985245, 1, "0f1089070f283a0c5d82b05dd7b050ca1e889504738b8a6c06a5762822271b3e") --Dlc Windows Language Depot Koreana The Censor DX Edition - ArtBook
+setManifestid(3985245, "3025378093579297064", 36173808)
+addappid(3985246, 1, "9c737cc04106c47f54ddfceb44eae15386c50ca056d55e035a24e61be93e7975") --Dlc Windows Language Depot Russian The Censor DX Edition - ArtBook
+setManifestid(3985246, "1213673217536021792", 36159104)
