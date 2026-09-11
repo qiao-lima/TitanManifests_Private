cecfa05442734e373f4f0329bff54ef2e164d1ce
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 11:20:12 -0400

    restock 1604690

---
+++ 1604690.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1604690.lua
+--Gamename Last Blossom: Roleplaying tabletop based scene
+addappid(1604690) --Mainappid Last Blossom: Roleplaying tabletop based scene
+addappid(1604691, 1, "d4b0ab7c1e7436812cac9a4302e805d1971dbcb2e152ad6f8c2d32720d8837a8") --Main Windows Depot Last Blossom: Roleplaying tabletop based scene
+setManifestid(1604691, "3015016784126532684", 130574816)
+addappid(1604692, 1, "5ff8be443ab8f801e3638512fdf64e78e6cbff4b6e0a945113fba6fd3945a11d") --Main Windows Depot Last Blossom: Roleplaying tabletop based scene
+setManifestid(1604692, "7415318734210659973", 108654960)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
