69a3c7799622f0d1ead340722d6968262b779a78
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 12:47:27 -0400

    restock 3755920

---
+++ 3755920.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3755920.lua
+--Gamename Walking In
+addappid(3755920) --Mainappid Walking In
+addappid(3755921, 1, "0bd337ae602fac70fb9b5d851d541d56932316ab245befad7a4aaf5c25779fb7") --Main Windows Depot Walking In
+setManifestid(3755921, "8651069767872320018", 1166047040)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
