fe85bb8f0dd05912d9c79be74358952aa8582f88
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 13:57:55 -0400

    restock 3839250

---
+++ 3839250.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3839250.lua
+--Gamename Soulforge Lost Path
+addappid(3839250) --Mainappid Soulforge Lost Path
+addappid(3839252, 1, "a59f7b2c4a59610f978e4c6ebe79dc570cc2684b4be11e71ed5320c6f31806d0") --Main Windows Depot Soulforge Lost Path
+setManifestid(3839252, "3811437568365296507", 191805552)
