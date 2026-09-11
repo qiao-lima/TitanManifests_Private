1caf4edee613b1f176bb09278e6c8a7951e221b4
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 11:03:16 -0400

    restock 3761570

---
+++ 3761570.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3761570.lua
+--Gamename 逃出30天循环
+addappid(3761570) --Mainappid 逃出30天循环
+addappid(3761571, 1, "c61dde95c902ccd55d94011cad885b54cf6bbadb323665ee15fe979a9b3a4650") --Main Windows Depot 逃出30天循环
+setManifestid(3761571, "3015651152918358910", 2347926240)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
