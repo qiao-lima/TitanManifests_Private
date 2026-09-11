369f292796798fc0cf4dc8d2860624c817ab02c9
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 07:06:16 -0400

    restock 791930

---
+++ 791930.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 791930.lua
+--Gamename BombTag
+addappid(791930) --Mainappid BombTag
+addappid(791931, 1, "2b19031940641b1f61e6d338f4fc8d73c944da5b3e38ae72bb57c60bd9f40db9") --Main Windows Depot BombTag
+setManifestid(791931, "2841875482582270522", 176507184)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+--Missing Dlcs
+--Dlcname BombTag - Character Customization Pack
