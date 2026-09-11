50f343d082a06e121cd7fd4ab78596411e6290d4
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 08:22:08 -0400

    restock 2308770

---
+++ 2308770.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2308770.lua
+--Gamename Escape From The Depth
+addappid(2308770) --Mainappid Escape From The Depth
+addappid(2308771, 1, "41b73f9059c586b1a657616808d47ce2a5b3e74b2c22a3543167b4fd1c3c95a4") --Main Windows Depot Escape From The Depth
+setManifestid(2308771, "4275652168883204955", 336916160)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
