79c91d1d4e1deac1618fcb513de4e6deb3331d8e
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 08:40:23 -0400

    restock 1908720

---
+++ 1908720.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1908720.lua
+--Gamename Half-Life: VR Mod
+addappid(1908720, 1, "703b4103ff79f86faf1a210c355d23680ca8f746d3817247b7d2349620e53f30") --Mainappid Half-Life: VR Mod
+addappid(1908721, 1, "aa4fa33a4780625c1de2d18cfb0935b9a62a8934819e1beb1eee3505f6f5fa24") --Main Windows Depot Half-Life: VR Mod
+setManifestid(1908721, "752728885751567354", 1455328576)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
