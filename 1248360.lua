3262d2c5b549708e3b3a822a6423aa4cb5f86db0
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 12:47:23 -0400

    restock 1248360

---
+++ 1248360.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1248360.lua
+--Gamename WalkinVR
+addappid(1248360) --Mainappid WalkinVR
+addappid(1248361, 1, "43c02cb1f3feebb7d3225de97913e34d0aba6e8b67488b635a3984adbf51b279") --Main Windows Depot WalkinVR
+setManifestid(1248361, "7992191006063283676", 51092320)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(1854240) --Dlcname WalkinVR - Professional
+--Missing Dlcs
+--Dlcname WalkinVR - Premium
