18b9663e7c217fbf7851172740bfaad7f7471ccf
qiao-lima <qiao-lima@github.com>
2026-09-11 17:00:40 +0800

    Manual Update 1848440

---
+++ 1848440.lua
@@ -0,0 +1,16 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1848440.lua
+--Gamename Foolish Mortals
+addappid(1848440, 1, "3ce2d52a5260df0e41ab42bca9715ebf02653cae29fd2bfb78be5bad944b2851") --Mainappid Foolish Mortals
+addappid(1848441, 1, "6233ebbe8f68daab9000b194562b4336842da3a92fe0597e87c639a6f1128128") --Main Windows Depot Foolish Mortals
+setManifestid(1848441, "4143904680431244772", 5430638048)
+addappid(1848442, 1, "e3bd1bbb022837a40864a48eb69617b9e586f34015fe84a46c692cf4233e6edc") --Main Macos Depot Foolish Mortals
+setManifestid(1848442, "8393701307364356019", 5459646560)
+--Dlcname Foolish Mortals Artbook
+addappid(4095650, 1, "c604d0c2238d61c18655f43de0d3adec25ebba3e757aa178c835b9c06834083f") --Dlcname Foolish Mortals Artbook
+setManifestid(4095650, "8341723281835251951", 77262640)
+--Dlcname Foolish Mortals Murphy's Diary
+addappid(4095660, 1, "2d5dc596263b1141cd0c1962fbc5214cc43dd663d8e9ae118db95f1764f0a96f") --Dlcname Foolish Mortals Murphy's Diary
+setManifestid(4095660, "4738105389740079846", 5951872)
+--Missing Dlcs
+--Dlcname Foolish Mortals Soundtrack
