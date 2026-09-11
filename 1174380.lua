fc0df721e4a4d89490f9c46d0545409acc14ce05
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 06:49:30 -0400

    restock 1174380

---
+++ 1174380.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1174380.lua
+--Gamename ESPIONAGE: Mafia Evolved
+addappid(1174380) --Mainappid ESPIONAGE: Mafia Evolved
+addappid(1174381, 1, "a48669d982f20af808acfde0734d4303bd5a5e169991ed3f8fc21642ef407f40") --Main Windows Language Depot English ESPIONAGE: Mafia Evolved
+setManifestid(1174381, "8934189623680594622", 2776435056)
+addappid(1174382, 1, "55ad846c9e1792002d018fe5518236740eec9e0dfebe3bc0ff03300003d634af") --Main Macos Language Depot English ESPIONAGE: Mafia Evolved
+setManifestid(1174382, "5793289133693451862", 1298333568)
+addappid(1174383, 1, "be5c133479ef4bc4f5f1206a9c71b6839f889d8ec8b7d7438b010d07c7beb6e8") --Main Linux Depot ESPIONAGE: Mafia Evolved
+setManifestid(1174383, "6998261465179822731", 851930512)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+--Missing Dlcs
+--Dlcname ESPIONAGE: Mafia Evolved - Special Edition & OST
