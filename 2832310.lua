91be6e066fce161298be04907def30a9f9fac6e6
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 09:15:16 -0400

    restock 2832310

---
+++ 2832310.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2832310.lua
+--Gamename ITER-8
+addappid(2832310) --Mainappid ITER-8
+addappid(2832311, 1, "37177201a7ff08fab2aca16c7bd748e8decea54ab88b57ae592c23cfb037be1c") --Main Windows Depot ITER-8
+setManifestid(2832311, "5201227035837233350", 232903232)
