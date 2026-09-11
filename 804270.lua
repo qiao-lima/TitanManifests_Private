eccbd395c6f1eb41b0f3bcc102542237218a4599
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 10:17:31 -0400

    restock 804270

---
+++ 804270.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 804270.lua
+--Gamename Chex Quest HD
+addappid(804270) --Mainappid Chex Quest HD
+addappid(804271, 1, "795bc4057e2a1c0fa3346612d0a818d1a12855f7a5b95e46427b3d48f28650a2") --Main Windows Depot Chex Quest HD
+setManifestid(804271, "7550928319185394871", 2495872912)
+addappid(804272, 1, "ea378b0da9a3f178d6ea58dfd4045193838cc7fde969378008a4b4596044e7cb") --Main Macos Depot Chex Quest HD
+setManifestid(804272, "2049396206261335134", 2470151120)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
