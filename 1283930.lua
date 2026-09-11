8957a8a6a62d44b90af8e181f429e55aedd2fb72
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 10:15:05 -0400

    restock 1283930

---
+++ 1283930.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1283930.lua
+--Gamename Half-Life: Restored
+addappid(1283930, 1, "a24c9b1434a2e3af3dd7af964c6c30e1de96d306d47b94073bfbca2ea93dc357") --Mainappid Half-Life: Restored
+addappid(1283939, 1, "fc16a2eb8a0b2a7838948810600de387c81f2dece683b9cfde0e88f9432ccd92") --Main Windows Depot Half-Life: Restored
+setManifestid(1283939, "6064677690334270545", 422916400)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229020, "5799761707845834510", 591984)
