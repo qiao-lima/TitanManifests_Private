a778a26ed7b0a11828f928249be234c344ec74ad
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 08:22:04 -0400

    restock 1113640

---
+++ 1113640.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1113640.lua
+--Gamename Escape from Labyrinth
+addappid(1113640) --Mainappid Escape from Labyrinth
+addappid(1113641, 1, "a64c51236bd932f8ee4cae952c5524d1c375bdf69dd7f3e7facf0575e731edb2") --Main Windows Depot Escape from Labyrinth
+setManifestid(1113641, "776587995427160122", 5109697872)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
