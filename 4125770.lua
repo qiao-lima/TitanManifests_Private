1683a15af190d0b111b14dd8da09d081bd583134
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 12:08:11 -0400

    restock 4125770

---
+++ 4125770.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4125770.lua
+--Gamename 大侠狂想曲
+addappid(4125770, 1, "48330767b5b150bd5d8c94ef6dfddf6c7190f47ffe38e7ba9f3422d234b2c60b") --Mainappid 大侠狂想曲
+addappid(4125771, 1, "866f4b633f759e3892bbe667084fabb689c0c0d396339515df7d227109f949e0") --Main Windows Depot 大侠狂想曲
+setManifestid(4125771, "2131648989496446472", 1042994176)
