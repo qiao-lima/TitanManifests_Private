19e338a791e8450e53bce19867a5764fa9987158
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 06:16:10 -0400

    restock 2638990

---
+++ 2638990.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2638990.lua
+--Gamename AppID_2638990
+addappid(2638990) --Mainappid AppID_2638990
+addappid(2638991, 1, "0e38caf5c40fdfb79e5eef9a92f27e4d91f701ddad78ea1db88ac04b90577818") --Main Windows Depot AppID_2638990
+setManifestid(2638991, "1900815309817905870", 114397440)
