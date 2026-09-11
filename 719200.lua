657c754dfd8d2af299b27451a756e5850797f616
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 05:10:20 -0400

    restock 719200

---
+++ 719200.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 719200.lua
+--Gamename The Wall
+addappid(719200) --Mainappid The Wall
+addappid(719201, 1, "3f4078214f36bdafa7ad5c9709e55606be8ca7de1e5a72731da1551d8a13f9ee") --Main Windows Depot The Wall
+setManifestid(719201, "9054096065479329599", 4740970128)
+addappid(719202, 1, "57df4c46af84a9297a729d2545f94621786f50a13d782248d5de363377d77048") --Main Macos Depot The Wall
+setManifestid(719202, "6445382239256632927", 910078288)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
