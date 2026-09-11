52f744d77ac6d9f9fd46daa60768bac2103bb3d5
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 07:06:21 -0400

    restock 2147080

---
+++ 2147080.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2147080.lua
+--Gamename BomberGuys
+addappid(2147080) --Mainappid BomberGuys
+addappid(2147081, 1, "1410e6850a206490ef26fac958f672d2f58731134201bbdf64bdb186668cef71") --Main Windows Depot BomberGuys
+setManifestid(2147081, "3186451927292942659", 227372944)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
