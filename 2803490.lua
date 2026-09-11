4d24cd1ad2924bc3866d01146d5768806cfd90c8
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 06:01:37 -0400

    restock 2803490

---
+++ 2803490.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2803490.lua
+--Gamename Atomcraft
+addappid(2803490, 1, "a84e1bee501bb39c2573e70452a01cdb5bcd432afb51d3d21768caf42fa7eb3b") --Mainappid Atomcraft
+addappid(2803491, 1, "fd16ebfb5ff34a373679eba0a2c761e21f54f79ff3f0ffcc57f56f3f4e0b4458") --Main Windows Depot Atomcraft
+setManifestid(2803491, "8256755965275835337", 212564512)
