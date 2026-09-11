94fb5eb3254dd6f0b9c19005976747c26bd1ffb7
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 10:51:17 -0400

    restock 1373100

---
+++ 1373100.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1373100.lua
+--Gamename VR Museum
+addappid(1373100) --Mainappid VR Museum
+addappid(1373101, 1, "2de5aa0b534c1c630639c7562bf378f48d0e4f3dbdfd7359bda8b963c855b8c0") --Main Windows Depot VR Museum
+setManifestid(1373101, "191897605185037558", 841468256)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
