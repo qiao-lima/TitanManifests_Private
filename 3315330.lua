b198f11e9826da8bf3a3039e8538f4f4dff4b667
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 11:17:14 -0400

    restock 3315330

---
+++ 3315330.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3315330.lua
+--Gamename Big Winner
+addappid(3315330, 1, "3a537426603ce1c1a96586385944e902f16391534a72089208c194a5bd9f0892") --Mainappid Big Winner
+addappid(3315331, 1, "344564527350bf3b50dd5c3b53fad031f3bb5ada52f603d1bdb73a357ae67290") --Main Windows Depot Big Winner
+setManifestid(3315331, "2321490466982944097", 890014272)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(3798040) --Dlcname Big Winner Lucky Pack
