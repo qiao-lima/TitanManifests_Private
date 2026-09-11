b3dd886fe3d2fa56d68efe9ca44ebb9fa8822546
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 11:20:02 -0400

    restock 1022160

---
+++ 1022160.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1022160.lua
+--Gamename Rollossus
+addappid(1022160) --Mainappid Rollossus
+addappid(1022161, 1, "f4c1fb5ec34b7139581f9cc21234c83efc5bdfc6466c580b44ba212b7fcfe21a") --Main Windows Depot Rollossus
+setManifestid(1022161, "8601530191359165915", 531552784)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
