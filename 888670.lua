6360d75f42e4b5e8f21af0c882a0f74b54f0e6cf
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 16:47:16 -0400

    restock 888670

---
+++ 888670.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 888670.lua
+--Gamename Notified
+addappid(888670) --Mainappid Notified
+addappid(888671, 1, "b3356b5e3db3e3c88354a118fa89076206ffca7df0ada96536deec98aad1dee2") --Main Windows Depot Notified
+setManifestid(888671, "4077406674255840576", 82099584)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(891450) --Dlcname AppID 891450
+addappid(891451) --Dlcname AppID 891451
