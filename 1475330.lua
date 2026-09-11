36cd815835ead3d04887a60c9513136b822c075b
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 10:54:55 -0400

    restock 1475330

---
+++ 1475330.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1475330.lua
+--Gamename Peak Darkness
+addappid(1475330) --Mainappid Peak Darkness
+addappid(1475331, 1, "69c2a58e9d40b61059c8d793939bee3a33b755e3c0a7d15502c178846c506382") --Main Windows Depot Peak Darkness
+setManifestid(1475331, "6474287558406503691", 6167100480)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
