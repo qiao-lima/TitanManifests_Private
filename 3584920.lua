794ad1b1d59f80e7eb4d2bcc982316a5d7fb86e6
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 08:45:36 -0400

    restock 3584920

---
+++ 3584920.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3584920.lua
+--Gamename Chinese Street Food Legend
+addappid(3584920) --Mainappid Chinese Street Food Legend
+addappid(3584921, 1, "946ea666a855377effa39e81886fd373d6c9f0d98cc13e57b5592bc34fe773d1") --Main Windows Depot Chinese Street Food Legend
+setManifestid(3584921, "8192669522745192646", 243720192)
