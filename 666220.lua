495af23ba1d3354afe62c74b5e07e034e91005c3
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 10:15:55 -0400

    restock 666220

---
+++ 666220.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 666220.lua
+--Gamename CS2D
+addappid(666220, 1, "5728fb1aafecc229689c7d9ba74d30411daa888af0e89631549306c5ba81aa91") --Mainappid CS2D
+addappid(666221, 1, "9716cba73f793a930ca27a56f27e9600a08222877fd8fa932fcb7e8157b2189b") --Main Windows Depot CS2D
+setManifestid(666221, "2147264596015466221", 20003888)
+addappid(666222, 1, "78a5e07fb059499c46b3564933b4ec621e82ef602cc68c8fc4e1e6aecf888cac") --Main Windows Depot CS2D
+setManifestid(666222, "9140062110536608357", 5976400)
+addappid(666224, 1, "be67ee9b56bbf1e921bb05c42f423559ea4667838340a85fa2034ad1fd5590e4") --Main Linux Depot CS2D
+setManifestid(666224, "6295364722786887082", 5977664)
+--Share Depots
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229020, "5799761707845834510", 591984)
