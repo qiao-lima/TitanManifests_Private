36a9a542cde17e0c542f702713608166aa58972b
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 09:34:58 -0400

    restock 3718190

---
+++ 3718190.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3718190.lua
+--Gamename スーパーリアル麻雀 Venus Returns
+addappid(3718190, 1, "9f5e6889d8f015b8cd85a683ad40e2cbc774563092cbbe5450cc13e8001ffd07") --Mainappid スーパーリアル麻雀 Venus Returns
+addappid(3718191, 1, "4d7b23dc80a9a38ff9cf30b72a4a83dc4c564b43420112e20031c2c23cf8bb43") --Main Windows Depot スーパーリアル麻雀 Venus Returns
+setManifestid(3718191, "8635389702046162939", 1725832080)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(3991320) --Dlcname スーパーリアル麻雀 Venus Returns - VRモード追加DLC
