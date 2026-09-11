932d99bb25ede5ae0c81cbb645a65cb8c738e33f
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 11:54:24 -0400

    restock 922450

---
+++ 922450.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 922450.lua
+--Gamename 4X-Galaxy 无主之地：银河
+addappid(922450) --Mainappid 4X-Galaxy 无主之地：银河
+addappid(922451, 1, "a443146c275a32f06a493138eb415706ba75461349e232401e48908e3644c2a9") --Main Windows Language Depot Schinese 4X-Galaxy 无主之地：银河
+setManifestid(922451, "7646969725421082565", 729621248)
+addappid(922452, 1, "46d52c7afb65fb380f07005473587a30dff4564da35e4cd2a625ba3cc16a237a") --Main Windows Depot 4X-Galaxy 无主之地：银河
+setManifestid(922452, "4097725356304061265", 727895536)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
