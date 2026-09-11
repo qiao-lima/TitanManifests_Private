e660d88e45eab09157bf919e81434446b786d863
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 07:24:09 -0400

    restock 4522520

---
+++ 4522520.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4522520.lua
+--Gamename 若诅咒以爱之名
+addappid(4522520) --Mainappid 若诅咒以爱之名
+addappid(4522521, 1, "e9cdca5ed2607e84518110f6918ed897c3b9062cdadb8003e5b88ab4853c55d7") --Main Windows Depot 若诅咒以爱之名
+setManifestid(4522521, "1343999385221938493", 931491104)
