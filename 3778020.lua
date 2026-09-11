caae0b2868351129835ac52a7158d7d6408b8e43
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 04:56:16 -0400

    restock 3778020

---
+++ 3778020.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3778020.lua
+--Gamename Walk The Plank
+addappid(3778020) --Mainappid Walk The Plank
+addappid(3778021, 1, "d451261a3fb5c5e208fb0f7eb167d172781b5d6238ccb157ba653e6846d250a4") --Main Windows Depot Walk The Plank
+setManifestid(3778021, "4459596004952182993", 185120112)
