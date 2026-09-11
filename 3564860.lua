9705c970a17a3ae7992480ebd3856f3024881e0a
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 05:58:22 -0400

    restock 3564860

---
+++ 3564860.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3564860.lua
+--Gamename Stellar Blade™ Demo
+addappid(3564860) --Mainappid Stellar Blade™ Demo
+addappid(3564861, 1, "262176baa1fa0d44b7b0cad48968679d5c5a731eaceb4a7cd0eda5e2dae9818d") --Main Windows Depot Stellar Blade™ Demo
+setManifestid(3564861, "8699326867785914264", 17195401856)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
