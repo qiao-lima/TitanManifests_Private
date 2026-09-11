90891fde194d43fce97e7df6a0c81cbb771c081f
qiao-lima <qiao-lima@github.com>
2026-09-11 17:00:41 +0800

    Manual Update 3236900

---
+++ 3236900.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3236900.lua
+--Gamename Rebirth Pub
+addappid(3236900) --Mainappid Rebirth Pub
+addappid(3236901, 1, "72a760c7a60c21e98890498394a318403547b3ff2c25b5f7cb5382def3ac8199") --Main Windows Depot Rebirth Pub
+setManifestid(3236901, "4245850674700758240", 3039056576)
+--Dlcname Rebirth Pub Demo
+addappid(4848020) --Dlcname Rebirth Pub Demo
+addappid(4848021, 1, "fec401829a7baaa22d457525e54c849795ebbc5b531a0382c4499c1623ec7ca2") --Dlc Windows Depot Rebirth Pub Demo
+setManifestid(4848021, "6717510002904120483", 1142766512)
