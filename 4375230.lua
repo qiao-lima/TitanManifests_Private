63dffc7f1ae9ce25477daaef984ce7c39d348bf9
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 05:15:17 -0400

    restock 4375230

---
+++ 4375230.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4375230.lua
+--Gamename 三国志BOND
+addappid(4375230) --Mainappid 三国志BOND
+addappid(4375231, 1, "1e90440c36d4804a3de5683792cc114e271dfbb0280f8dd2d791a47115fcdb94") --Main Windows Depot 三国志BOND
+setManifestid(4375231, "5135366782264004957", 2224174224)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(4881400) --Dlcname 三国志BOND‐魏時短パック‐
+addappid(4881580) --Dlcname 三国志BOND‐蜀時短パック‐
+addappid(4881590) --Dlcname 三国志BOND‐呉時短パック‐
+addappid(4881600) --Dlcname 三国志BOND‐群雄時短パック‐
