752179baf8390d31d100eb0039d84d8548007ef0
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 11:51:25 -0400

    restock 4842670

---
+++ 4842670.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4842670.lua
+--Gamename PC FISH: Taskbar Aquarium
+addappid(4842670) --Mainappid PC FISH: Taskbar Aquarium
+addappid(4842671, 1, "111d4d0a6f05eb261c91e4fc14ebfbe84986e1de8e71a577b4d0cc2912d5fdf1") --Main Windows Depot PC FISH: Taskbar Aquarium
+setManifestid(4842671, "9152956216170006525", 128488400)
+addappid(4842672, 1, "0c0a844dc52b73160dacc41f1b8a49f0bf9f3b955c157223ebbde58c1b24a48f") --Main Macos Depot PC FISH: Taskbar Aquarium
+setManifestid(4842672, "5274528177231453104", 158550000)
+addappid(5005980) --Dlcname PC FISH - Extra Decors Pack
+addappid(5005990) --Dlcname PC FISH - Asian Garden Pack
+addappid(5111630) --Dlcname PC FISH - Tropical Paradise Pack
