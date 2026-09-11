c411c01bb601fdee89fa904690541e71c0c09a7b
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 08:20:57 -0400

    restock 2571140

---
+++ 2571140.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2571140.lua
+--Gamename Escape Z
+addappid(2571140) --Mainappid Escape Z
+addappid(2571141, 1, "3ccd2c31bf0903f00a1c351a55ad95e1dabf3afb8d6388d4d8461d02088f4ca8") --Main Windows Depot Escape Z
+setManifestid(2571141, "3289608801117965210", 8289990144)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
