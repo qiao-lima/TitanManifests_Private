cedeb0e3f3c378608846227e35695ec677008e54
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 05:31:17 -0400

    restock 3692100

---
+++ 3692100.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3692100.lua
+--Gamename Ghost VCD
+addappid(3692100) --Mainappid Ghost VCD
+addappid(3692101, 1, "5582657c8ed8b433011544859cfd3a3af760c638766ac94f7ad507dbf8262cc8") --Main Windows Depot Ghost VCD
+setManifestid(3692101, "3414649530306615934", 702258240)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
