97d31fd7e87517a6aecc2ecc74f4203ac7a541f7
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 11:09:35 -0400

    restock 4181410

---
+++ 4181410.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4181410.lua
+--Gamename The Alley
+addappid(4181410, 1, "1f1535030588a139cc034e8234c260fc0d87dff5d8dbd69fc21a7e3427786d94") --Mainappid The Alley
+addappid(4181411, 1, "a5f40763fbaba977a7db86b6111486094f613084a07e94b226db0a6ec09be368") --Main Windows Depot The Alley
+setManifestid(4181411, "4750799391214875708", 6902875856)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
