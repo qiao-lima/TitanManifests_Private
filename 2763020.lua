e344f9e53460925e9f829cdd7d273720c3939f02
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 14:05:01 -0400

    restock 2763020

---
+++ 2763020.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2763020.lua
+--Gamename ROAD
+addappid(2763020) --Mainappid ROAD
+addappid(2763021, 1, "18ade402352dcddc6403b2ba307e827fccf374d781aeb0343c62a803274eeddb") --Main Windows Depot ROAD
+setManifestid(2763021, "8360587227163816802", 991108976)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
