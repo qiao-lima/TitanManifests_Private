8ff1ab1a6af8a61dcdbf511ab989cff236fb63f6
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 05:15:22 -0400

    restock 1029500

---
+++ 1029500.lua
@@ -0,0 +1,18 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1029500.lua
+--Gamename 中华三国志 the Three Kingdoms of China
+addappid(1029500) --Mainappid 中华三国志 the Three Kingdoms of China
+addappid(1029501, 1, "1bcf5cc35a653ddd8bc273549f36d8f1ad9e9410cb0ec49fb0c0eb9b96458c7b") --Main Windows Depot 中华三国志 the Three Kingdoms of China
+setManifestid(1029501, "634297089222344663", 1185613248)
+--Dlcname 中华三国志-丝路山水版
+addappid(1062570) --Dlcname 中华三国志-丝路山水版
+addappid(1062571, 1, "6895423af57040b7d15f6d4b9b654602942d7a6b32442209c2177229307a6618") --Dlc Windows Language Depot Schinese 中华三国志-丝路山水版
+setManifestid(1062571, "6597952286246408164", 243440192)
+--Dlcname 中华三国志-DP版
+addappid(1206750) --Dlcname 中华三国志-DP版
+addappid(1206751, 1, "4f982c54da4a17cc811b02283613f03a3697940ecbcaf0037cde47c91c869daa") --Dlc Windows Language Depot Schinese 中华三国志-DP版
+setManifestid(1206751, "7828181220073597441", 170100240)
+--Share Depots
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
+addappid(1060270) --Dlcname 中华三国志-情怀版
