574beb441681b6b8fc02e8f8b632bdce8589ac52
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 16:15:02 -0400

    restock 2195620

---
+++ 2195620.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2195620.lua
+--Gamename 她毕业了 Demo
+addappid(2195620) --Mainappid 她毕业了 Demo
+addappid(2195621, 1, "7912c91d15dccd6baec087a7134a46baeb11dbd8798854ee4f117cfa23b1dcd4") --Main Windows Depot 她毕业了 Demo
+setManifestid(2195621, "7719887667349106584", 1326006624)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229020, "5799761707845834510", 591984)
