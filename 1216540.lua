20bf9931950785b761bc7e61efa5ada2f3e481bc
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 10:43:36 -0400

    restock 1216540

---
+++ 1216540.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1216540.lua
+--Gamename Fishy2
+addappid(1216540) --Mainappid Fishy2
+addappid(1216541, 1, "829e2b3490ffec9c8d94fc8aa3d9e3e1700a0b345aa57d10945dfd6608883093") --Main Windows Depot Fishy2
+setManifestid(1216541, "4910277450478724726", 564408288)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
