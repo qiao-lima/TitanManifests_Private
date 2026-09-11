ecc49c83b878f55d47bed1342247fc150f716abd
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 05:23:25 -0400

    restock 3397100

---
+++ 3397100.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3397100.lua
+--Gamename Eye of the Incubus
+addappid(3397100) --Mainappid Eye of the Incubus
+addtoken(3397100, "9638877320527686269")
+addappid(3397101, 1, "06bb5f6152fb9b698177672be584f46b06ab0f473276fbb4f53dab83facf2829") --Main Windows Depot Eye of the Incubus
+setManifestid(3397101, "4947601607254722173", 375528176)
