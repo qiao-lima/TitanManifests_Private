18650bd26a825dc240666c34533af272822d99b9
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 07:06:42 -0400

    restock 1814920

---
+++ 1814920.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1814920.lua
+--Gamename Bombyx
+addappid(1814920) --Mainappid Bombyx
+addappid(1814923, 1, "b6b06ef910b0536d8035be3ccf6aa690207ef5f137efadffae110a41c45fefbb") --Main Windows Language Depot English Bombyx
+setManifestid(1814923, "1892631733873607625", 1918660336)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+--Missing Dlcs
+--Dlcname Bombyx - Fuzion
