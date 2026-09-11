813f64d5cfc55aff56029428da1d28521e95fe41
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 06:58:17 -0400

    restock 1882750

---
+++ 1882750.lua
@@ -0,0 +1,21 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1882750.lua
+--Gamename Cluck
+addappid(1882750) --Mainappid Cluck
+addappid(1882751, 1, "663f02d2700ae549df660f924b5b986ca5da2bb3c00671d4a8f7f8804d069b39") --Main Windows Depot Cluck
+setManifestid(1882751, "4752482122482121720", 7293915344)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+--Missing Dlcs
+--Dlcname Cluck - Hat Pack 1
+--Dlcname Cluck - Color Pack 1
+--Dlcname Cluck - Color Pack 2
+--Dlcname Cluck - Face Pack 1
+--Dlcname Cluck - Hat Pack 2
+--Dlcname Cluck - Hat Pack 3
+--Dlcname Cluck - Face Pack 2
