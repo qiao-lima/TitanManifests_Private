37dc1bd65e1f4a67f0b710dfac958d102138fe54
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 07:35:55 -0400

    restock 4673900

---
+++ 4673900.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4673900.lua
+--Gamename 完蛋！我被男同学包围了
+addappid(4673900) --Mainappid 完蛋！我被男同学包围了
+addappid(4673901, 1, "eb9ba76769fcc3accdc927fcdec32524f37615a71813b179dcb5e810f3914e04") --Main Windows Depot 完蛋！我被男同学包围了
+setManifestid(4673901, "5131200304595808294", 775195136)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
