f072e9d91ea31183e88cdf160dcfe12a8267f23c
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 06:39:51 -0400

    restock 3395300

---
+++ 3395300.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3395300.lua
+--Gamename 哎呦!这么多美女呀!
+addappid(3395300) --Mainappid 哎呦!这么多美女呀!
+addappid(3395301, 1, "0ff5201bc86ae74d99b8558ade1d0ddf4cef9a9d322100c67616792f72f870b4") --Main Windows Depot 哎呦!这么多美女呀!
+setManifestid(3395301, "5682485315682832973", 175653078048)
