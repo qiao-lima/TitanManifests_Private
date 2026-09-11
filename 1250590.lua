639a2a371d37d8a01a871c3b6295591c222a3946
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 08:23:54 -0400

    restock 1250590

---
+++ 1250590.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1250590.lua
+--Gamename ESCAPE POINT
+addappid(1250590) --Mainappid ESCAPE POINT
+addappid(1250591, 1, "a11d836a7a2c80cd1b57153f20f7ea1b21e8abca39087d63ee24cbe9a3ac7a89") --Main Windows Depot ESCAPE POINT
+setManifestid(1250591, "6660321142964984681", 2036908544)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
