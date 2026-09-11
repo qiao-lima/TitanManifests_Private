9880561ebb1482847f44e42f275c376c7be1ce92
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 09:40:11 -0400

    restock 2322020

---
+++ 2322020.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2322020.lua
+--Gamename SeaFeud
+addappid(2322020) --Mainappid SeaFeud
+addappid(2322021, 1, "f1cf0a633ab7444b12cecc23749cb32dc1313068f97bddf5f800357544477c5a") --Main Windows Language Depot English SeaFeud
+setManifestid(2322021, "6943733495269646366", 1499056400)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
