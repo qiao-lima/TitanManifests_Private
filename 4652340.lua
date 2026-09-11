b1bbde64d3c6ceaee02683390981452a67921ecf
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 06:56:32 -0400

    restock 4652340

---
+++ 4652340.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4652340.lua
+--Gamename Cloudrooms
+addappid(4652340) --Mainappid Cloudrooms
+addappid(4652341, 1, "7d3b8b54c6feb44f5336b328d2bb7c79975105abcbe30293bc94f72e79c0650e") --Main Windows Depot Cloudrooms
+setManifestid(4652341, "3537522753019481472", 3198922240)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
