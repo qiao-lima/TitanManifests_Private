4006cf9ac7aa38d80da4528ac730d6fc403ea857
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 09:39:37 -0400

    restock 1628650

---
+++ 1628650.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1628650.lua
+--Gamename A Little Time
+addappid(1628650) --Mainappid A Little Time
+addappid(1628651, 1, "42092263675628f40dd2b150c37b69b3d17af0ae5942202f33005bfb41adc839") --Main Windows Depot A Little Time
+setManifestid(1628651, "6603137737505074778", 9560716944)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
