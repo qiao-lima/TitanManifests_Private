bd34b63cbe929e2182bde9b9b1255edd9f197eeb
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 10:16:10 -0400

    restock 1360500

---
+++ 1360500.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1360500.lua
+--Gamename Warrecs 2
+addappid(1360500) --Mainappid Warrecs 2
+addappid(1360501, 1, "160c69c2f5953575534f781573ac4806c6af51287a27b91ca6dc66c646c23264") --Main Windows Depot Warrecs 2
+setManifestid(1360501, "8700295324118082386", 9485312)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229020, "5799761707845834510", 591984)
