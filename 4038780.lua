376556b4e533457ff47f519294f30dd075a54925
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 12:30:01 -0400

    restock 4038780

---
+++ 4038780.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4038780.lua
+--Gamename ONE IN THE BACK
+addappid(4038780) --Mainappid ONE IN THE BACK
+addappid(4038781, 1, "02b8ebd9aca705b299a8156af92fbe7df55883ebea909430323fa6c07231b915") --Main Windows Depot ONE IN THE BACK
+setManifestid(4038781, "7099121872642741478", 82498176)
