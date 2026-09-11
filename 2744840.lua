d6457338cbc3803b68aba103f733a1ae30e05bde
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 14:05:05 -0400

    restock 2744840

---
+++ 2744840.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2744840.lua
+--Gamename Road Toad
+addappid(2744840) --Mainappid Road Toad
+addappid(2744841, 1, "42eb8a54c430ddac897e4c86daea7700c72d84b452b45e2b8541dcf3b46316a8") --Main Windows Depot Road Toad
+setManifestid(2744841, "6980909669449583651", 893222384)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
