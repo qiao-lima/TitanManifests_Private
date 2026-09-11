5249bb879f07de1e74957846ff68e26176a3b6e1
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 04:37:38 -0400

    restock 3301070

---
+++ 3301070.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3301070.lua
+--Gamename UWAR
+addappid(3301070) --Mainappid UWAR
+addappid(3301071, 1, "65e146c21ec3ce8a6acf22a37071f6983a630a8c2c5cd0231cfb7c5bd016760d") --Main Windows Depot UWAR
+setManifestid(3301071, "6764668857146450788", 7532099600)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
