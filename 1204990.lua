0ab119585c42c3e0e129d44e047dbf5cef7c2ad4
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 06:02:23 -0400

    restock 1204990

---
+++ 1204990.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1204990.lua
+--Gamename Milkcraft
+addappid(1204990) --Mainappid Milkcraft
+addappid(1204991, 1, "51f94df154b3c9a6d23cf0244be0ff564f197c61a63548097879ec2190f27b85") --Main Windows Language Depot English Milkcraft
+setManifestid(1204991, "1760072255919190065", 367648880)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
