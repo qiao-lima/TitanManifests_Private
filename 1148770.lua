a9faa8073b155a026ecf3c0ad7c3e93633197117
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 16:47:32 -0400

    restock 1148770

---
+++ 1148770.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1148770.lua
+--Gamename Not An Angels: Erotic Puzzle
+addappid(1148770) --Mainappid Not An Angels: Erotic Puzzle
+addappid(1148771, 1, "6d9c988edca4ce37031cd60f4d459740a2f2c134c4add94ca6582880f539baad") --Main Windows Depot Not An Angels: Erotic Puzzle
+setManifestid(1148771, "218138628390999347", 1487430128)
+addappid(1148772, 1, "9da7f1de0f57e6e98c4defc8517b52acaa8fa785c996ef07bcfd102d15ae8fab") --Main Macos Depot Not An Angels: Erotic Puzzle
+setManifestid(1148772, "6470299285187782172", 753979552)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
