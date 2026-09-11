69aceea5c0738fb08f8ecd1388f7ef56c2eef596
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 08:58:22 -0400

    restock 3839590

---
+++ 3839590.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3839590.lua
+--Gamename 逃离狮门外语学校 / Escape the School
+addappid(3839590) --Mainappid 逃离狮门外语学校 / Escape the School
+addappid(3839591, 1, "385177d41941dada4dd5653c1d6ff677bc6106bc5c0d53fe8d30084daf737f79") --Main Windows Depot 逃离狮门外语学校 / Escape the School
+setManifestid(3839591, "213657009558443327", 3343342608)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
