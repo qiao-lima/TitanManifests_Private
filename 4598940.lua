b7ed62677f8b3255d7b882914acd94e6f7d5062d
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 06:25:10 -0400

    restock 4598940

---
+++ 4598940.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4598940.lua
+--Gamename Last Dive: Echo Trench
+addappid(4598940) --Mainappid Last Dive: Echo Trench
+addappid(4598941, 1, "133eba62f94627fd37140c5c308d0e51963b4308ff4f4e9b6eb06f1dc80da33f") --Main Windows Depot Last Dive: Echo Trench
+setManifestid(4598941, "3546113591309708005", 329529648)
