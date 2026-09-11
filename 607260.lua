4b6754a6350a16e20560df2b1b847b8b5df27a47
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 10:42:05 -0400

    restock 607260

---
+++ 607260.lua
@@ -0,0 +1,18 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 607260.lua
+--Gamename McOsu
+addappid(607260, 1, "1c28c84d5d8f39f6c89e0c663fdf419e72b3b084bbdaee6301c99fb6c7b15540") --Mainappid McOsu
+addappid(607261, 1, "1bf9489c348bb3f4aa271ca23d5257a5babfc37fe9d3d47faa58ff9425d8528c") --Main Windows Depot McOsu
+setManifestid(607261, "3372609387182835768", 21382480)
+addappid(607262, 1, "07563fcd28142de0d0dba39da21ab344955caf6f1d434154f90ee94417a88b84") --Main Linux Depot McOsu
+setManifestid(607262, "2739198355470802067", 10865984)
+addappid(607263, 1, "2f831258a1e7a643f5b0b0d8ac30799b73b9cdea9c9198698dd927c7e5225c29") --Main Linux Depot McOsu
+setManifestid(607263, "3232947155251974112", 10865984)
+addappid(607264, 1, "88acbb30534c90651f6ccf5ffbe660b237fac458db5ce6cb00f05c6a092aee3b") --Main Macos Depot McOsu
+setManifestid(607264, "5960928338905586369", 6964240)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(608160) --Dlcname McOsu - Donation - 1
+addappid(608170) --Dlcname McOsu - Donation - 3
+addappid(608180) --Dlcname McOsu - Donation - 5
