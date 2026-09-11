604b2e9ff86b8e126cd9bacebbd45ae58edcd58a
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 09:20:51 -0400

    restock 3686930

---
+++ 3686930.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3686930.lua
+--Gamename 头七 TOUQI
+addappid(3686930, 1, "7423d8e007cfd23da8361f3d57d60c43cdd37e6cd8ccc2117ff1af01f948d07e") --Mainappid 头七 TOUQI
+addappid(3686931, 1, "c98412b675097a1beb19c035c8e852abe6b111fba17cb13470e740639da143e1") --Main Windows Depot 头七 TOUQI
+setManifestid(3686931, "294472891588785870", 441446448)
+--Share Depots
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229020, "5799761707845834510", 591984)
+--Missing Dlcs
+--Dlcname 頭七-樱桃小姐
