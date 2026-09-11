ed75bb7ec2a9114f407544698f006317ed2d8187
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 10:47:21 -0400

    restock 1927780

---
+++ 1927780.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1927780.lua
+--Gamename Galaxy Life
+addappid(1927780) --Mainappid Galaxy Life
+addappid(1927781, 1, "539cc9b882ac9f3a1f9ff2be8ff0d582d279d3314f47af2e6ab8813d303b1a39") --Main Windows Depot Galaxy Life
+setManifestid(1927781, "2950644338773953672", 74620768)
+addappid(1927782, 1, "8202bb0507c930bf917e073440fa3738bd204cb03993cc4f5803d4229086d44f") --Main Macos Depot Galaxy Life
+addappid(1927783, 1, "f8379a373d50347d251b2093dd0dae9a3417e18942957b9ddd35b303f8e73248") --Main Linux Depot Galaxy Life
+setManifestid(1927783, "2656809431908550263", 66780080)
+addappid(1927784, 1, "a084c0978f08a3f0ddce832e2723c44f5394d54db28c17653ad1a2cde1dc9cc0") --Main Windows Depot Galaxy Life
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
