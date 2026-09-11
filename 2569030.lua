00b693cffab60cca1ec3d5d5e9e43bf9aefad183
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 10:17:26 -0400

    restock 2569030

---
+++ 2569030.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2569030.lua
+--Gamename Depthdrops
+addappid(2569030) --Mainappid Depthdrops
+addappid(2569031, 1, "9eeed65f0d268a924b2b717bec6e194b0d940ce2ad661189017e23e7c29638c3") --Main Windows Depot Depthdrops
+setManifestid(2569031, "1006232368212199138", 361045520)
+--Share Depots
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229020, "5799761707845834510", 591984)
