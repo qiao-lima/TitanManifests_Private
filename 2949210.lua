980fcc484c7ff59781d8fd97732905e92b102fb6
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 04:56:42 -0400

    restock 2949210

---
+++ 2949210.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2949210.lua
+--Gamename The Hungry Lamb: Traveling in the Late Ming Dynasty (Original Soundtrack)
+addappid(2949210) --Mainappid The Hungry Lamb: Traveling in the Late Ming Dynasty (Original Soundtrack)
+addtoken(2949210, "9316827502926997238")
+addappid(2949211, 1, "2c677de37ebdff5eb3e320373750d234b35c7543d69d71528699e7c796086dfa") --Main Windows Depot The Hungry Lamb: Traveling in the Late Ming Dynasty (Original Soundtrack)
+setManifestid(2949211, "2822986787335951938", 50739744)
+addappid(2949212, 1, "1ac2f95e1efe601967d8a42205d2f73dfad60c5ea2d2bb53e44ff7ebb6efbed0") --Main Windows Depot The Hungry Lamb: Traveling in the Late Ming Dynasty (Original Soundtrack)
+setManifestid(2949212, "6953376174430784090", 267856864)
