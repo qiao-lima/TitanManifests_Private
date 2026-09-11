5c0134ceb7a65625c453d5412ffe2b1e5774825b
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 04:49:58 -0400

    restock 4169820

---
+++ 4169820.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4169820.lua
+--Gamename 永恒与星辰与日常
+addappid(4169820, 1, "773a83fad6b396e50f4bd43ff39d687a5407ccd7137dac0da2699b1ad2b93529") --Mainappid 永恒与星辰与日常
+addappid(4169821, 1, "6de6353b3d4c113d55fef119944d566d49b56b9d6fd5cf86cfcf6e4d819b11a7") --Main Windows Depot 永恒与星辰与日常
+setManifestid(4169821, "1840984659195566560", 1485305408)
