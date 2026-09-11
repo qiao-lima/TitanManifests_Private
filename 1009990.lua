b914bc903322ac5ed0bcb6ff3c304ba711a18900
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 09:20:19 -0400

    restock 1009990

---
+++ 1009990.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1009990.lua
+--Gamename SCP022
+addappid(1009990) --Mainappid SCP022
+addappid(1009991, 1, "ea0884860b9bb87171a911137286870755eabab590960ce80ce3257bb3d69031") --Main Windows Depot SCP022
+setManifestid(1009991, "2008769124752386705", 678871888)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
