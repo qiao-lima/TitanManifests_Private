29c9a2d1dddf95c20fbf3961d70a97d99a70da0c
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 04:40:11 -0400

    restock 3155730

---
+++ 3155730.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3155730.lua
+--Gamename Venus Vacation PRISM - DEAD OR ALIVE Xtreme -
+addappid(3155730) --Mainappid Venus Vacation PRISM - DEAD OR ALIVE Xtreme -
+addappid(3155731, 1, "f5f3f46814cdec1b4afab690622f6dffb042619e839eb1b08310252f9099e0fc") --Main Windows Depot Venus Vacation PRISM - DEAD OR ALIVE Xtreme -
+setManifestid(3155731, "5453751672566898454", 11982079776)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(3199230) --Dlcname Venus Vacation PRISM - DEAD OR ALIVE Xtreme - Digital Deluxe Upgrade Pack
+addappid(3204050) --Dlcname Venus Vacation PRISM - DEAD OR ALIVE Xtreme - "Plumeria Hair Ornament" Hairstyles (Set of 6)
+addappid(3204060) --Dlcname Venus Vacation PRISM - DEAD OR ALIVE Xtreme - "Cheongsam" Outfits (Set of 6)
