75d4374f7e4185bc4281eac6a461e2b2a981a571
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 05:22:39 -0400

    restock 3557620

---
+++ 3557620.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3557620.lua
+--Gamename Blue Archive
+addappid(3557620, 1, "0325c980bc73744580479fb1c2f91af69ee3f2bb73691fc3d199fee4f9ae87b5") --Mainappid Blue Archive
+addappid(3557621, 1, "0B4E98809F1C718B10332FE0FE98BF591B2FD8FE88D47E04737CAACDE6C28C65") --Main Windows Depot Blue Archive
+setManifestid(3557621, "3634031492119296676", 24156414592)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
