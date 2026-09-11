81db678f0c4ef0922ad9f3553387252f62628153
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 10:35:11 -0400

    restock 4876770

---
+++ 4876770.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4876770.lua
+--Gamename The Fishercat
+addappid(4876770) --Mainappid The Fishercat
+addappid(4876772, 1, "d6c509b425230d07a6aa8f7e21c17730ae7566c1c57d30c9cec88b92162acf67") --Main Windows Depot The Fishercat
+setManifestid(4876772, "6228434644653858483", 77912480)
+addappid(4876773, 1, "792755228bb58a0a965717630cbb071ac5cf65e98cd138a4728142f405326cc5") --Main Macos Depot The Fishercat
+setManifestid(4876773, "4041829882304650611", 69608688)
+addappid(4893820) --Dlcname The Fishercat - Supporter Pack
