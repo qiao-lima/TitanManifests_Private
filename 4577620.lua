3fc53b2196c5475cab5b10e8364d57f9c85fd709
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 08:30:02 -0400

    restock 4577620

---
+++ 4577620.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4577620.lua
+--Gamename Bills Must Be Paid Demo
+addappid(4577620) --Mainappid Bills Must Be Paid Demo
+addappid(4577622, 1, "3836317b405ab1a54498d0d0b5fd384da2a36d11e1620b31764832c9e9311a7b") --Main Windows Depot Bills Must Be Paid Demo
+setManifestid(4577622, "8965938094380586626", 143657392)
