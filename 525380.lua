93ab83c7e3c78ff1486c1bdb49ffdfb2353d0958
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 10:55:02 -0400

    restock 525380

---
+++ 525380.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 525380.lua
+--Gamename Demon Peak
+addappid(525380) --Mainappid Demon Peak
+addappid(525388, 1, "a0f9b8246f329f86a664436f3f9cfcce81fbf27993c3b14db312350ddb388d06") --Main Linux Depot Demon Peak
+setManifestid(525388, "5029201822885887831", 99773712)
+addappid(525389, 1, "27c149e5c916bc5412a7c577b73dc593432a6e30bd781ead506e574e5cff2c2d") --Main Windows Depot Demon Peak
+setManifestid(525389, "6187227606017951149", 77730240)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229020, "5799761707845834510", 591984)
