dd4baa16e745793608772e70414dc9f2b46f55b9
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 07:20:22 -0400

    restock 3773380

---
+++ 3773380.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3773380.lua
+--Gamename Brainwashing Academy - The Cursed Person's Pot
+addappid(3773380, 1, "b4b623573945587e09882838b6b2e54d5a33151b6fd856df16869126302a3ca6") --Mainappid Brainwashing Academy - The Cursed Person's Pot
+addappid(3773381, 1, "e5fab4bdfdc513e3c8cb57d31f3cbf609591c1c0220333395beb7ce1d61d17fc") --Main Windows Language Depot English Brainwashing Academy - The Cursed Person's Pot
+setManifestid(3773381, "7032226484848654158", 731732064)
+addappid(3773382, 1, "fe1c981b0e1afa2d6bac28c5e5b1534ef6a232cb769317192e68f95a7bb75c73") --Main Windows Language Depot Schinese Brainwashing Academy - The Cursed Person's Pot
+setManifestid(3773382, "8032185528361880604", 732121424)
