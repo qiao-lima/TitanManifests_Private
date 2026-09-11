6994378ae8ff09339fbf84f10a1c4041a2b0ba29
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 05:10:11 -0400

    restock 1145520

---
+++ 1145520.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1145520.lua
+--Gamename Wall Force
+addappid(1145520) --Mainappid Wall Force
+addappid(1145521, 1, "9196ebaaeb7184e6586591ed7c35af98ad59d517ea67dc0b2e3c7ad6c25a7e66") --Main Windows Depot Wall Force
+setManifestid(1145521, "1079360921161890904", 2378705184)
+--Share Depots
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
