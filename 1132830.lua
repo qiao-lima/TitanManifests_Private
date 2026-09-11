cf1fdb0e214f49b75c7d1b92e00e41f56d67188c
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 07:29:54 -0400

    restock 1132830

---
+++ 1132830.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1132830.lua
+--Gamename Poppy's Nightmare
+addappid(1132830) --Mainappid Poppy's Nightmare
+addappid(1132831, 1, "75e3ca9fecb48290d575e749f50499bcbf38557511387035f192fb7462702436") --Main Windows Depot Poppy's Nightmare
+setManifestid(1132831, "3985082293722458326", 205994576)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
