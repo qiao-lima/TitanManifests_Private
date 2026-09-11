2b25c4793864ae347774c41befc88375f26ebb98
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 09:09:53 -0400

    restock 2505590

---
+++ 2505590.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2505590.lua
+--Gamename 三国朋克: 爱与破坏之神
+addappid(2505590) --Mainappid 三国朋克: 爱与破坏之神
+addtoken(2505590, "16906287408241698490")
+addappid(2505591, 1, "fb14f1b6d07a571471a6f3b3f4f60bb69aa96ed93b6aca3b1c79640bcf3b405b") --Main Windows Depot 三国朋克: 爱与破坏之神
+setManifestid(2505591, "7263487878809671127", 1212793536)
+addappid(4147410) --Dlcname 三国朋克 - 泳装皮肤礼包
