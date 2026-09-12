215804262d0905f6ce8437e794bcc20468309b85
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 10:25:07 -0400

    restock 669630

---
+++ 669630.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 669630.lua
+--Gamename Hollowed
+addappid(669630) --Mainappid Hollowed
+addappid(669631, 1, "22f3657986438f386096ce7cb98056ff7ef37bb00989be1e2df99ad373f98ff1") --Main Windows Depot Hollowed
+setManifestid(669631, "465559345770370125", 3717592576)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
