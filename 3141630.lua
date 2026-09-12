7f1daa625d3a535c314103a9db8cc26b9be1a849
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 10:37:03 -0400

    restock 3141630

---
+++ 3141630.lua
@@ -0,0 +1,17 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3141630.lua
+--Gamename Montabi
+addappid(3141630, 1, "4e262aa2cb3f80d0939723f0e83bea3993c04f30182204e428157cf1c06cf648") --Mainappid Montabi
+addappid(3141631, 1, "b911a48b7e70338d2b3a6aa031bb231c5fd380697a82ffe75853775b2979e922") --Main Windows Depot Montabi
+setManifestid(3141631, "615101458080769564", 613176272)
+addappid(3141632, 1, "7593d9db3318d514ce3ae500b7e3a840dbf87dec99a8ba66ef72fa5c9f333c7a") --Main Macos Depot Montabi
+setManifestid(3141632, "4771792653520261129", 632817424)
+addappid(4946140, 1, "6c75534276479b1a4a4ddb63a28f2f532815abe2928fc0853c9c379ba6b7988c") --Main Windows Depot Montabi
+setManifestid(4946140, "2817375048785278672", 391397760)
+--Dlcname Montabi - Soundtrack
+addappid(4946090) --Dlcname Montabi - Soundtrack
+addappid(4946091, 1, "b04aba5bcee8d71e036c7ad658582eb58232b463e5606ea9dc0e2a68639caaf2") --Dlc Windows Depot Montabi - Soundtrack
+setManifestid(4946091, "1325870387236283103", 34155488)
+addappid(4946092, 1, "1546fb436bd641c29893efc1e3b5ebd6d9d354b534ccd6e968fb85f62b038bb4") --Dlc Windows Depot Montabi - Soundtrack
+setManifestid(4946092, "7646458208207222220", 363503008)
+--Dlcname Montabi - Art Book
