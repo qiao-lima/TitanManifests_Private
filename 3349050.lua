fefb58da1109c3525d94f26fc185fc6a8aaf6cdb
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 20:05:06 -0400

    restock 3349050

---
+++ 3349050.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3349050.lua
+--Gamename BackRooms X Playtest
+addappid(3349050) --Mainappid BackRooms X Playtest
+addappid(3349051, 1, "72ed7e6bb887890e08a34fcd42afa85377aa3fb74a78abf24fd51a01d49927b6") --Main Windows Depot BackRooms X Playtest
+setManifestid(3349051, "6776511304993535823", 5339300560)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
