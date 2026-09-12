0595fafe17a05d47f0f89eefc16f4257cd58a4f5
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 20:20:24 -0400

    restock 2837280

---
+++ 2837280.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2837280.lua
+--Gamename Counterpick Labs
+addappid(2837280) --Mainappid Counterpick Labs
+addappid(2837282, 1, "933c67158739a42d8bba63825169cb570c8def7102f74281cc2e8df32cced7ee") --Main Windows Depot Counterpick Labs
+setManifestid(2837282, "727762275672324590", 101100736)
+addappid(2837283, 1, "e4e48a5e2ecda312fc2a00961b5c7ace459bf2c89a4750718d230d7dbf721044") --Main Macos Depot Counterpick Labs
+setManifestid(2837283, "4356277497102343098", 148510992)
+addappid(2837284, 1, "d9bd3837dbb65ea4d53334d66dfe139c518c68eb9b87e87ec5aa8b18a4b7bdc6") --Main Linux Depot Counterpick Labs
+setManifestid(2837284, "4597627905569905621", 96699984)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
