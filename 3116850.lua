7d2882208064fdb4e07046140a91305b53b984e5
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 01:30:01 -0400

    restock 3116850

---
+++ 3116850.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3116850.lua
+--Gamename DRIFT ISLAND Playtest
+addappid(3116850) --Mainappid DRIFT ISLAND Playtest
+addappid(3116851, 1, "40b04f3e5fa7a13766061718bd55f1489d2143ab96b0f0ec29d2fd7ebd7063f1") --Main Windows Depot DRIFT ISLAND Playtest
+setManifestid(3116851, "9101889811479081649", 1963692864)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
