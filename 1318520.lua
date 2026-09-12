a35c535e34a6979404d7dd73aa8071679af46302
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 10:49:45 -0400

    restock 1318520

---
+++ 1318520.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1318520.lua
+--Gamename Dört Model Müzesi
+addappid(1318520) --Mainappid Dört Model Müzesi
+addappid(1318521, 1, "b203e8c16d440e51229b35bdca8ce26ebb329e3f65bd76a0f11b36da5cffa7e8") --Main Windows Depot Dört Model Müzesi
+setManifestid(1318521, "3562973761550886501", 605018992)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
