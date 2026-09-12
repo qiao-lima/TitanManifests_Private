58f50b4ade9dd375417e4442376e0693e4c2dd67
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 02:58:29 -0400

    restock 5035220

---
+++ 5035220.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5035220.lua
+--Gamename For the Fatherland: Chronicle of Park Chung-hee
+addappid(5035220, 1, "1415b8b25d292788fee22c66e238bcfa380589541c4494793e9e0d07008f2609") --Mainappid For the Fatherland: Chronicle of Park Chung-hee
+addappid(5035221, 1, "9d420cb361c36fb8081505958dd7091d6cac3c27fd33166ca7c42c82c34a9918") --Main Windows Depot For the Fatherland: Chronicle of Park Chung-hee
+setManifestid(5035221, "8459191725755217841", 1435339120)
