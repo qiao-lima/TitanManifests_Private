88b76368312c9328824cc38499e9125647e6a5f0
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 08:26:29 -0400

    restock 1982430

---
+++ 1982430.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1982430.lua
+--Gamename Kunai Strike
+addappid(1982430) --Mainappid Kunai Strike
+addappid(1982431, 1, "765f3fe16737d6b5f9caedbdc02e240ef71727aeedbbe718ddfa6d92a7585e23") --Main Windows Language Depot English Kunai Strike
+setManifestid(1982431, "706600059793226752", 4853578032)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
