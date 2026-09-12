1df44b104cd2eac43010a0675f9e354faa813a95
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 03:20:36 -0400

    restock 1745200

---
+++ 1745200.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1745200.lua
+--Gamename Sexy Airlines EVO
+addappid(1745200) --Mainappid Sexy Airlines EVO
+addappid(1745201, 1, "5e60ced700d81b335ee31ae7691836e9efc80bdd9236be3fd0211950504fbcc5") --Main Windows Language Depot English Sexy Airlines EVO
+setManifestid(1745201, "4640010151574218191", 159583712)
+--Share Depots
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
+addappid(3728840) --Dlcname Sexy Airlines EVO - Verónica Character Pack
+addappid(4612450) --Dlcname Sexy Airlines EVO - Talia Character Pack
+--Missing Dlcs
+--Dlcname Sexy-Airlines_DLC-tests
