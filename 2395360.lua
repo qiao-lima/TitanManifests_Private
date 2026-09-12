92cb5244e4ac62e92eb6a89832d759f174ee0497
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 05:00:22 -0400

    restock 2395360

---
+++ 2395360.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2395360.lua
+--Gamename Slime Knight
+addappid(2395360) --Mainappid Slime Knight
+addappid(2395361, 1, "c4e13c33de0499d63d9fb23b88b6deaf50d49e70b4dc1952b5e44661ae6d5eb8") --Main Windows Language Depot English Slime Knight
+setManifestid(2395361, "4012885811883554115", 474672288)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
