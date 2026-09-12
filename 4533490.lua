e2d9ebc8ac62c2c23ef88d2546913b3b02a95786
qiao-lima <qiao-lima@github.com>
2026-09-12 18:35:02 +0800

    Manual Update 4533490

---
+++ 4533490.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4533490.lua
+--Gamename 黯渊崛起：魔灵深渊
+addappid(4533490) --Mainappid 黯渊崛起：魔灵深渊
+addappid(4533491, 1, "8791cc91518821f171de061f79753459ac0ec9f3c61d4cb85fe42cfa3f7fadda") --Main Windows Depot 黯渊崛起：魔灵深渊
+setManifestid(4533491, "2701753397040248649", 728288864)
+--Dlcname 黯渊崛起 Demo
+addappid(4813890) --Dlcname 黯渊崛起 Demo
+addappid(4813892, 1, "587ae928def2290b5254b02bb7d8e23d6754cc06a2a139bc3106011130385e62") --Dlc Windows Depot 黯渊崛起 Demo
+setManifestid(4813892, "7546710553003816779", 402236624)
