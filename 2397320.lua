aad9f8c5120d14fc40a970baf0741b8ebd229a01
qiao-lima <qiao-lima@github.com>
2026-09-12 20:29:22 +0800

    Manual Update 2397320

---
+++ 2397320.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2397320.lua
+--Gamename Bus Simulator 27
+addappid(2397320, 1, "fd63a031b6494251093208b9d22023564ca45795708eebf17e2914e18457d9c7") --Mainappid Bus Simulator 27
+addappid(2397321, 1, "68f5e2da7f45dbde75df56661f7c514a0a65bf37f7d916aa17f690c8ae68a6e0") --Main Windows Depot Bus Simulator 27
+setManifestid(2397321, "2197209025329293764", 17942287520)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4217010) --Dlcname Bus Simulator 27: Deluxe Pack
+addappid(4315960) --Dlcname Bus Simulator 27: Mercedes-Benz Intouro
+addappid(4488260) --Dlcname Bus Simulator 27 - Season Pass
