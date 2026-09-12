7b0afdea545a562ef7d028b3bd7c3ee14fd8cd90
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 22:20:10 -0400

    restock 4108460

---
+++ 4108460.lua
@@ -0,0 +1,20 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4108460.lua
+--Gamename 灭寇1939：上川往事
+addappid(4108460) --Mainappid 灭寇1939：上川往事
+addappid(4108461, 1, "2ef50a5229464fc7ec2688dbddb4bbfef4a836ad7ddb83a9ca39cdc56296a82a") --Main Windows Depot 灭寇1939：上川往事
+setManifestid(4108461, "1957792741985974783", 42927449360)
+addappid(4108462, 1, "96c6dd084defd4a56b9790239b42eff46cd141aebb913005332e01d5e36f7c61") --Main Macos Depot 灭寇1939：上川往事
+setManifestid(4108462, "4210568830280904546", 42946393472)
+addappid(4306701, 1, "42f624502cdbd645089b999736e5c044c03e777323c3df34a6c79ae7336aaf75") --Main Windows Depot 灭寇1939：上川往事
+setManifestid(4306701, "4622450668007763443", 341620876320)
+addappid(4306702, 1, "e92c4b1d8170d5d91552fd21773c858f9a38468f1f240bbcfe8bf3636d785a03") --Main Macos Depot 灭寇1939：上川往事
+setManifestid(4306702, "6684147478024689800", 341619948192)
+addappid(4306711, 1, "81f9315b6008a4a8cc181c4f1fb296e0045a4dab19734ae8f21483fe4958a717") --Main Windows Depot 灭寇1939：上川往事
+setManifestid(4306711, "5218479606125971105", 1692482016)
+addappid(4306712, 1, "b249a6d9b780c76c6c2812d68754b4a8718e954eb473907b1fdcda6434a7a8fd") --Main Macos Depot 灭寇1939：上川往事
+setManifestid(4306712, "2213076753097465514", 1692810480)
+--Dlcname 灭寇1939:上川往事 4K版
+addappid(4306700) --Dlcname 灭寇1939:上川往事 4K版
+--Dlcname 灭寇1939:上川往事-额外壁纸相框资源包
+addappid(4306710) --Dlcname 灭寇1939:上川往事-额外壁纸相框资源包
