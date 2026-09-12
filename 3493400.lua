4c1ffaab82fbd3c6b1ff2f99fc2e57fbb9dae4d2
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 22:46:11 -0400

    restock 3493400

---
+++ 3493400.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3493400.lua
+--Gamename BLOODLETTER
+addappid(3493400, 1, "66b152e6cc0e995b3e68e4efccdf650f5b2c792b63ec83d1eda8d6bc746afa51") --Mainappid BLOODLETTER
+addtoken(3493400, "3659762617034554995")
+addappid(3493401, 1, "8a6abb5aad134d861c0a29f71a53939beb784d0c3cf5d5cb5ee9dc99695c0191") --Main Windows Depot BLOODLETTER
+setManifestid(3493401, "7344755028619259036", 1205524320)
