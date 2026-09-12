718d47fa8bd14dce800b0282fb2f8afe7f2b92f4
qiao-lima <qiao-lima@github.com>
2026-09-12 20:29:23 +0800

    Manual Update 491540

---
+++ 491540.lua
@@ -0,0 +1,44 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 491540.lua
+--Gamename The Bus
+addappid(491540, 1, "50ede99c2f96404b8a78ea512566035693baad8bf04610df397b9587b56ae155") --Mainappid The Bus
+addappid(491541, 1, "8d2383b07a93cb99ce9f861e608ebecb5f050f9b03c687a4c34d65810c7a6de2") --Main Windows Depot The Bus
+setManifestid(491541, "5160774850993936733", 46884663792)
+addappid(491542, 1, "17a413c346787a07c3435c510d605b955d08d830625bce61660fea8014c99ee3") --Main Windows Depot The Bus
+setManifestid(491542, "3599393296524611291", 153236656)
+addappid(897493, 1, "31e71ddf1e4074dd299969b0b2bf3d6752f7d420860a443729aa2b25ac6a15d6") --Main Windows Depot The Bus
+setManifestid(897493, "1099903074590598360", 133428768)
+addappid(897494, 1, "b3f0af7017d1c49fcf5e67fda2f8f0cf27a8497278ea73223937a1ed9b15df6a") --Main Windows Depot The Bus
+setManifestid(897494, "3725264354591688735", 0)
+addappid(897495, 1, "ee3dfb504287b75635b8f4f65e461b942c19ee2aa79f41ff33bd918c21968341") --Main Windows Depot The Bus
+setManifestid(897495, "5501507913094284178", 0)
+addappid(897496, 1, "775dc74ca6a581d55a7fbb9d866ed88bbb58af2b9db1830bba59605dfb4e4911") --Main Windows Depot The Bus
+setManifestid(897496, "7856527596074613875", 0)
+addappid(897498, 1, "4579ccfebc7d2b7503855a9ca06e5814dd25974c87ba487f9bcb5c57f4126928") --Main Windows Depot The Bus
+setManifestid(897498, "7575597901754286870", 236744368)
+addappid(897499, 1, "b590c139a5495cb19349ad13183148beebba64cba77fd97fd2203f5c3af3eb0e") --Main Windows Depot The Bus
+setManifestid(897499, "3424333586108645378", 0)
+addappid(898611, 1, "abbdea3d230c0dfec1c0e957b9e7e48d11cee42a4231596bcaf7f7782b0769f2") --Main Windows Depot The Bus
+setManifestid(898611, "2874276900602961734", 72840928)
+addappid(898612, 1, "7507d616d2d5d33fb200774c4721b24c7b12c9a6e244a7d955292b76d6bf0386") --Main Windows Depot The Bus
+setManifestid(898612, "6856078903255251146", 0)
+addappid(898621, 1, "d6ac0bafa7c3862e611df6645da819c058edc7bff67a77957644795a37d32403") --Main Windows Depot The Bus
+setManifestid(898621, "5079404577296001792", 116054736)
+addappid(898622, 1, "928e91bdd6aff07abd00679a7b57b6cf2afb5e03b0e9552fbfe21559381a2148") --Main Windows Depot The Bus
+setManifestid(898622, "1231484820983016185", 0)
+addappid(898623, 1, "c3946683cfdbc82316bce160e3d183a7910d1dcf95261729d2e831c28f705e0e") --Main Windows Depot The Bus
+setManifestid(898623, "2005502871026558840", 0)
+addappid(898624, 1, "3da00b447931500314e2ec3ef7824e0828a836c20b74edd3f9e46fc04f3a4580") --Main Windows Depot The Bus
+setManifestid(898624, "2265013552689358100", 0)
+addappid(898626, 1, "a0e87b2536cf223daa4572d5c494db6c1da23303a5bf1f00196af604a71915db") --Main Windows Depot The Bus
+setManifestid(898626, "8119239068154372879", 0)
+--Dlcname The Bus - Hamburg City
+addappid(3544250, 1, "25a1c05581659f93bcbf9d25a2f6c033ea73eb9fac6eda9727e03454fde92247") --Dlcname The Bus - Hamburg City
+setManifestid(3544250, "612415942197373708", 5518115040)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4656580) --Dlcname The Bus - Ebus 2.2
+addappid(5168640) --Dlcname The Bus - London
