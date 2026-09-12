c0450b0d89406bc5f29ed3eb4cea8a7d8ede547f
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 08:42:30 -0400

    restock 3365820

---
+++ 3365820.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3365820.lua
+--Gamename Palette Cleanser
+addappid(3365820) --Mainappid Palette Cleanser
+addappid(3365821, 1, "46703b5592dba31e636862e3f368c9a79a9fe6835979ca31d4347254d083a1ec") --Main Windows Depot Palette Cleanser
+setManifestid(3365821, "7831066772222691066", 1205531088)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
