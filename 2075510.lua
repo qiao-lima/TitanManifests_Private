fe7f44937b7038a26d43533215d63419609b3eeb
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 11:59:07 -0400

    restock 2075510

---
+++ 2075510.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2075510.lua
+--Gamename AIXIN: Goddess' Love
+addappid(2075510) --Mainappid AIXIN: Goddess' Love
+addappid(2075514, 1, "c5254516406ea5430f2e81c9c48971c1e4d2baf071df3fe4770c61e71fc67f2c") --Main Windows Depot AIXIN: Goddess' Love
+setManifestid(2075514, "8236827289398890525", 291578896)
