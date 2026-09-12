5c064559c6787a5b849faedb6e126a9f974401fb
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 20:20:15 -0400

    restock 2061750

---
+++ 2061750.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2061750.lua
+--Gamename Counterpact
+addappid(2061750) --Mainappid Counterpact
+addappid(2061751, 1, "bcda959476628afca89ef788a20699914040c626e960cb8343242204148de5ab") --Main Windows/Macos Depot Counterpact
+setManifestid(2061751, "2764766181147889675", 194563680)
+addappid(2061752, 1, "f19ec1efe8022d7d46a8dd5572d847559ff76bff69865467bab4312d7184a8eb") --Main Linux Depot Counterpact
+setManifestid(2061752, "3735749707561600720", 214963456)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
