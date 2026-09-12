b3d04e17af90ac5741d4551bac567ec3d26d9ef7
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 19:46:30 -0400

    restock 4254300

---
+++ 4254300.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4254300.lua
+--Gamename Swords And Magic: Path Of Conquest
+addappid(4254300) --Mainappid Swords And Magic: Path Of Conquest
+addappid(4254301, 1, "4239c1c011cccb33a94c4d1a8619ef664d6a06b851d1ba5a3eb4274668cb79a4") --Main Windows Depot Swords And Magic: Path Of Conquest
+setManifestid(4254301, "2216938428418985562", 10420637600)
