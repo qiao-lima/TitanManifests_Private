2f0f792ce9162068135296927b4ad24cb429f39f
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 07:22:48 -0400

    restock 4390720

---
+++ 4390720.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4390720.lua
+--Gamename Sweet Hamster Days
+addappid(4390720) --Mainappid Sweet Hamster Days
+addappid(4390721, 1, "d0ceb8c71e524b67aa66a7ebd767e14b5b02959755467696d1a6cf4abe9923dc") --Main Windows Depot Sweet Hamster Days
+setManifestid(4390721, "370911291891357884", 23646763456)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
