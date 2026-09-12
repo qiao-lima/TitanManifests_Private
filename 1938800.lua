03b3dccee8649b382db19220ac8e7b55b27073f6
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 22:53:13 -0400

    restock 1938800

---
+++ 1938800.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1938800.lua
+--Gamename Alone in the Dark Prologue
+addappid(1938800) --Mainappid Alone in the Dark Prologue
+addappid(1938801, 1, "86adf23b59fbe53326af3e95805f4ec2bebbb33f27cd674b50b06bfb05b54ceb") --Main Windows Depot Alone in the Dark Prologue
+setManifestid(1938801, "4572800341531867067", 7861568608)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
