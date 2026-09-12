820c879798750f3644d57bac356cb9296ac07d0b
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 15:10:06 -0400

    restock 1072870

---
+++ 1072870.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1072870.lua
+--Gamename Random War
+addappid(1072870) --Mainappid Random War
+addappid(1072871, 1, "ede19687495bea3d42d99eaf5665d2f03f7eb43a3eee83cb46a6988204a5c7ad") --Main Windows Depot Random War
+setManifestid(1072871, "4520118104258733919", 57444560)
+addappid(1072872, 1, "fd90deea468f6d5114921a2ee847adf77018a172c7b6ba9a36c120481a3c9260") --Main Linux Depot Random War
+setManifestid(1072872, "3222643106945035701", 59652432)
+addappid(1072873, 1, "3bd183182f5179759518e99eccc7ced811d085016ab94627c678823f2cb3db82") --Main Windows Depot Random War
+setManifestid(1072873, "925098772277439840", 55089472)
+--Share Depots
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229002, "7260605429366465749", 50365280)
