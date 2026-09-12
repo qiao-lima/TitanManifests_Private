e7a85291b8436b2dc4f93f442d34be7c313b39a1
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 09:46:45 -0400

    restock 1112400

---
+++ 1112400.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1112400.lua
+--Gamename Project Torque - Free 2 Play MMO Racing Game
+addappid(1112400) --Mainappid Project Torque - Free 2 Play MMO Racing Game
+addappid(1112402, 1, "7c73e5081cb5e20497ce7f17556b670a96885e758507f5066ae188de93339a93") --Main Windows Depot Project Torque - Free 2 Play MMO Racing Game
+setManifestid(1112402, "7956593234926471299", 2842219104)
+--Share Depots
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228982, "6413394087650432851", 9650016)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229000, "4622705914179893434", 242681440)
