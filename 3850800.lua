2169abdcee1ab59a068cd4bf1106d1b8419dc146
qiao-lima <qiao-lima@github.com>
2026-09-12 18:35:02 +0800

    Manual Update 3850800

---
+++ 3850800.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3850800.lua
+--Gamename WakuWaku Park
+addappid(3850800, 1, "9a95556df648b6ef76c6727f148a3eac128f29d8f0ef3f714305002626f6ec43") --Mainappid WakuWaku Park
+addappid(3850801, 1, "3c3b23c168d6607b8a1fd7d01f639332d14e22a979ef54ddd5ec61c97f85a3ad") --Main Windows Depot WakuWaku Park
+setManifestid(3850801, "4791813705613062298", 589461504)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
