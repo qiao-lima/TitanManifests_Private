2060151d6aed1f3d5fd1231f27e80d26f35de2f3
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 03:29:02 -0400

    restock 2401370

---
+++ 2401370.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2401370.lua
+--Gamename Dogs Club
+addappid(2401370) --Mainappid Dogs Club
+addappid(2401371, 1, "23bf9cb5bff143d1892be7594cd8a28d342b6d51966512c0af74d1f2aa1f4203") --Main Windows Depot Dogs Club
+setManifestid(2401371, "4140522126343952541", 4553767344)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
