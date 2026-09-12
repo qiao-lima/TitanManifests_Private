6fddc950abc400684d9e741e64fcd391c119666b
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 12:00:05 -0400

    restock 2527350

---
+++ 2527350.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2527350.lua
+--Gamename 水母之夜
+addappid(2527350) --Mainappid 水母之夜
+addappid(2527351, 1, "0dc2c991ef4a6f25f6af83e0bfe757fce33a3432f2b74807e40784ecfddb4c42") --Main Windows Language Depot Schinese 水母之夜
+setManifestid(2527351, "3329381546069710515", 224354416)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
