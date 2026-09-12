b26692588e47f65bae8bdc464f35aa6bcdbc1bbb
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 08:02:26 -0400

    restock 3512910

---
+++ 3512910.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3512910.lua
+--Gamename IN HEAT: Lustful Nights Soundtrack
+addappid(3512910) --Mainappid IN HEAT: Lustful Nights Soundtrack
+addappid(3512912, 1, "b87b270cd721013f6449ff26a09d9ea04a9e772947153465bf021f356ab43c8f") --Main Windows Depot IN HEAT: Lustful Nights Soundtrack
+setManifestid(3512912, "3128504594719195631", 283319328)
+addappid(3512913, 1, "a2e6a82f1cc7082b77ec88c30babc6035764cc59bf0a9b639fed3f46f57521d9") --Main Windows Depot IN HEAT: Lustful Nights Soundtrack
+setManifestid(3512913, "3247005877764422277", 948243744)
