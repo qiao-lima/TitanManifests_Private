abcc6dffbe27fd6d6cdd81cf80a8ca714dcf290a
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 11:59:26 -0400

    restock 268850

---
+++ 268850.lua
@@ -0,0 +1,19 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 268850.lua
+--Gamename EVGA Precision X1
+addappid(268850) --Mainappid EVGA Precision X1
+addappid(268851, 1, "5cc9bc2d471dcb7c404e6b53cc1ba2c7338208615adc76cff7b6a4eacc15ff91") --Main Windows Depot EVGA Precision X1
+setManifestid(268851, "5365060846135867990", 46270144)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
