37a8978227b54a96973186511fea2388600ec02a
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 05:00:01 -0400

    restock 863560

---
+++ 863560.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 863560.lua
+--Gamename Slime CCG
+addappid(863560) --Mainappid Slime CCG
+addappid(863561, 1, "b14a931d47e7243adf6c83b505f0b6ba2739971c92e1d3348dc71c54f841c485") --Main Windows Depot Slime CCG
+setManifestid(863561, "232431846007203971", 233476688)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+--Missing Dlcs
+--Dlcname Slime CCG - Expansion #1
