2441f3ae4efaf63c64b92a697a57da715e0b9552
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 20:34:53 -0400

    restock 1727930

---
+++ 1727930.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1727930.lua
+--Gamename Gorge
+addappid(1727930) --Mainappid Gorge
+addappid(1727931, 1, "2959e58e6dcaadc423544c95ff83d84f2104def0052590220ea9042f05cebb7a") --Main Windows Depot Gorge
+setManifestid(1727931, "1411113531088925894", 1355659408)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+--Missing Dlcs
+--Dlcname Gorge - Special Features Pack
