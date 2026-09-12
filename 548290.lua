7b8f948654f3266de76d5463dbd77ce42b39d7e7
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 12:37:56 -0400

    restock 548290

---
+++ 548290.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 548290.lua
+--Gamename Dark Eden
+addappid(548290) --Mainappid Dark Eden
+addappid(548291, 1, "bb89cf9848259d168f5f0a4114462c4b63f4ee9811a5e547d48e8fd217988e7c") --Main Windows Depot Dark Eden
+setManifestid(548291, "5890236402315114863", 1914534704)
+--Share Depots
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228982, "6413394087650432851", 9650016)
