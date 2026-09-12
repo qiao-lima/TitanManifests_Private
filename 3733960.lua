f15f5e96196ab93cc565d8a5f38b52f723d37b2b
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 04:08:54 -0400

    restock 3733960

---
+++ 3733960.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3733960.lua
+--Gamename CODE VEIN II - Character Creator Demo
+addappid(3733960) --Mainappid CODE VEIN II - Character Creator Demo
+addappid(3733961, 1, "b795f85d0b818e32e758c6de09edc4d8d5e0af4c72ca74a60f298c4c2199b758") --Main Windows Depot CODE VEIN II - Character Creator Demo
+setManifestid(3733961, "3577022248613533096", 26037124112)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
