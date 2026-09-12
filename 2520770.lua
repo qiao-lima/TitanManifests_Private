497cb1fc877bd63d74e57c7b9d914450c1c13699
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 11:30:18 -0400

    restock 2520770

---
+++ 2520770.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2520770.lua
+--Gamename 骑士派对
+addappid(2520770) --Mainappid 骑士派对
+addappid(2520771, 1, "568315870a01bf5e812386dd7c12b2410b847d38b2f60483f27c33d6db85eba0") --Main Windows Language Depot Schinese 骑士派对
+setManifestid(2520771, "312709546570271120", 3012770944)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
