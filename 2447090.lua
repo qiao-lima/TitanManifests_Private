a2ed3e13af7197aeec18c1b3004a6f8853e71e68
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 23:48:56 -0400

    restock 2447090

---
+++ 2447090.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2447090.lua
+--Gamename StarShip-DF
+addappid(2447090) --Mainappid StarShip-DF
+addtoken(2447090, "17828004687996343924")
+addappid(2447091, 1, "5d199825a8c589eb0d11c9245ca655806932e0b9d619f4aceab0f16cd6f01579") --Main Windows Depot StarShip-DF
+setManifestid(2447091, "4569780604864053671", 893393152)
