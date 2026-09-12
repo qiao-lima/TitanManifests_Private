a4c0068483a3ad8fbf4c53d4ad420ba7527cacf8
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 12:01:58 -0400

    restock 2572870

---
+++ 2572870.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2572870.lua
+--Gamename 堕落女警
+addappid(2572870) --Mainappid 堕落女警
+addappid(2572871, 1, "df2e431df99ac7bb7e8ea6c47bfbb293287709cae2c5fb1846a49478d8c059ab") --Main Windows Depot 堕落女警
+setManifestid(2572871, "2055682865849786178", 1904499472)
