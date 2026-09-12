3d18ba28b9ca19022792752fe7efdd30098d1121
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 07:48:35 -0400

    restock 246280

---
+++ 246280.lua
@@ -0,0 +1,28 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 246280.lua
+--Gamename Happy Wars
+addappid(246280) --Mainappid Happy Wars
+addappid(246281, 1, "25e0c796a892c43d00d493fc702789f6cfc9b9e9de736d843bedaadfb38c8e8d") --Main Windows Depot Happy Wars
+setManifestid(246281, "7227693480907289874", 793441104)
+addappid(246282, 1, "fc2d4321d3f19bc799f23bd47eba080352f6137d4fd874250ad95c1f5a600b80") --Main Windows Depot Happy Wars
+setManifestid(246282, "8566634511251701096", 0)
+addappid(246284, 1, "4fb1d99226090cd74e9ef45874aa157fce70163311a9705d3d6b90f75ee662bc") --Main Windows Depot Happy Wars
+setManifestid(246284, "9218594636393496296", 0)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229002, "7260605429366465749", 50365280)
+addappid(332190) --Dlcname Happy Wars - Customizable Parts A
+addappid(332191) --Dlcname Happy Wars - Customizable Parts B
+--Missing Dlcs
+--Dlcname Happy Wars - Happy Tickets - Starter Pack
+--Dlcname Happy Wars - Happy Tickets - Value Pack A
+--Dlcname Happy Wars - Happy Tickets - Value Pack B
+--Dlcname Happy Wars - Evil Warrior Set
+--Dlcname Happy Wars - Fast Food Cleric Set
+--Dlcname Happy Wars - Happy Tickets - Extreme Pack A
+--Dlcname Happy Wars - Happy Tickets - Extreme Pack B
+--Dlcname Happy Wars - Forest Mage Set
