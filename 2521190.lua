2f44acece1b902514fedabedeeb10131e08deecf
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 09:34:31 -0400

    restock 2521190

---
+++ 2521190.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2521190.lua
+--Gamename Werelderfgoed: Romeins Aquaduct van Nijmegen en Berg en Dal
+addappid(2521190) --Mainappid Werelderfgoed: Romeins Aquaduct van Nijmegen en Berg en Dal
+addappid(2521191, 1, "4a50bef67ef212fc1d413c755141e6073683c99745550ee118a6165eb2f7a6b4") --Main Windows Language Depot Dutch Werelderfgoed: Romeins Aquaduct van Nijmegen en Berg en Dal
+setManifestid(2521191, "4819549361475523836", 1462207344)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
