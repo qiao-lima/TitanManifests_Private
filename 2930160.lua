f48364a24a6883a1f5e622808dc38f5b82d6916c
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 11:56:38 -0400

    restock 2930160

---
+++ 2930160.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2930160.lua
+--Gamename Nightmare Kart
+addappid(2930160) --Mainappid Nightmare Kart
+addappid(2930161, 1, "cd1d4064c69736c29a4dab440c24942c5a8c9dc7c765f0c5739d400a45ea06e2") --Main Windows Depot Nightmare Kart
+setManifestid(2930161, "3603550845427015160", 336708016)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
