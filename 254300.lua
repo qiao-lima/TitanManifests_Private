a9a5543316f6c30cdbadf9b9e23748b800777ca6
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 12:35:11 -0400

    restock 254300

---
+++ 254300.lua
@@ -0,0 +1,21 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 254300.lua
+--Gamename Dofus
+addappid(254300) --Mainappid Dofus
+addappid(254302, 1, "c5e3d8cca1c66c9b79f22e91b2e39efd8b2cd0321378958f1a9a0ffe3148783d") --Main Windows Depot Dofus
+setManifestid(254302, "7495482935871237626", 268272)
+addappid(254303, 1, "f248a454276224f542e4dd9e20c032b7bee59a1758435169013d9249e51be11b") --Main Windows Depot Dofus
+setManifestid(254303, "6373202397015793521", 21695472)
+addappid(254304, 1, "f5a3eccf18b5ba17cc5dddda5723ff07e57008e4c51261b25e4cd3213fef3096") --Main Macos Depot Dofus
+setManifestid(254304, "5171684827027344933", 22183232)
+addappid(254305, 1, "b86bffa98577318de14d241013bbce67c434ef06b595e34242518a87937d1684") --Main Linux Depot Dofus
+setManifestid(254305, "916083715536845673", 37943840)
+addappid(254306, 1, "383044ee518deedc9cc372c30cd15085b0e79977567263d2364bb9745a555e03") --Main Linux Depot Dofus
+setManifestid(254306, "9190419277337196731", 38160352)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(552010) --Dlcname KERUBIM PACK
+addappid(552011) --Dlcname ATACHAM PACK
+addappid(553120) --Dlcname JULITH PACK
+addappid(849230) --Dlcname DOFUS - ADVENTURE PACK
