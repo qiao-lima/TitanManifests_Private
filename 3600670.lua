664625853ab41890471c2cc27afb4eae94d73f88
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 03:01:59 -0400

    restock 3600670

---
+++ 3600670.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3600670.lua
+--Gamename Zombie Town Breakout
+addappid(3600670) --Mainappid Zombie Town Breakout
+addappid(3600671, 1, "5e601e6283a47b487e5969b0cc908223eb6a4ead2735eac08756499d8b6e1881") --Main Windows Depot Zombie Town Breakout
+setManifestid(3600671, "4817625073954690983", 9939570336)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
