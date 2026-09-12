81fd838854d395983d7fede0324017e30b6953c5
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 11:56:44 -0400

    restock 1184140

---
+++ 1184140.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1184140.lua
+--Gamename KartRider: Drift
+addappid(1184140) --Mainappid KartRider: Drift
+addappid(1184141, 1, "9faa6dda489dcc2ebbeef1b3d802e441d51143cbf35679becde70609cb71601e") --Main Windows Depot KartRider: Drift
+setManifestid(1184141, "1081893653454082295", 28457197152)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
