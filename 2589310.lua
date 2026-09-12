82c60554fa90357cf024b083419e7cdf22ca2265
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 10:25:22 -0400

    restock 2589310

---
+++ 2589310.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2589310.lua
+--Gamename Hollow Survivors Demo
+addappid(2589310) --Mainappid Hollow Survivors Demo
+addappid(2589312, 1, "cc9e9e77de4a5cf31714a258cc5d8cb82c6acbfe21adbe8a213473834faae409") --Main Windows Depot Hollow Survivors Demo
+setManifestid(2589312, "1964928543460647636", 138903856)
+addappid(2589313, 1, "4966b091749f7893d1a9683cad83673b644c0c2e21f8e7177e1a8757b162b0af") --Main Macos Depot Hollow Survivors Demo
+setManifestid(2589313, "7273170285696333923", 147265392)
+addappid(2589314, 1, "918fda3122d45a79facd389c8120fdd2ff700040fe5e1bdd840546f542c7d8df") --Main Linux Depot Hollow Survivors Demo
+setManifestid(2589314, "4668553496681714078", 138111120)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
