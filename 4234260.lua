27c92c4d601ab3ccca9166afa0d08ca4ef20c22c
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 04:24:56 -0400

    restock 4234260

---
+++ 4234260.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4234260.lua
+--Gamename 主角
+addappid(4234260) --Mainappid 主角
+addappid(4234261, 1, "8f5f020ae76af275a55b73eaea5b781da5e7c19b8d0b487dcf1efc4ccabbdfe9") --Main Windows Language Depot Schinese 主角
+setManifestid(4234261, "6356033589061173682", 56953164672)
