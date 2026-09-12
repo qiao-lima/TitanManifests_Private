01efcec40bb56ef749f255f00aafdbf42265e1ad
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 21:19:52 -0400

    restock 3480200

---
+++ 3480200.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3480200.lua
+--Gamename 战国之影
+addappid(3480200) --Mainappid 战国之影
+addappid(3480201, 1, "9df0046528b7ed2ef9a9ea06af3c749b6aeef4760b369ec6f5d8ec7afb911603") --Main Windows Depot 战国之影
+setManifestid(3480201, "4730853828316239572", 13576152512)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
