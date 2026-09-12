eb805a49d778e1af6b71f81fd9d56993806aad33
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 08:56:48 -0400

    restock 2052970

---
+++ 2052970.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2052970.lua
+--Gamename STRIDEN
+addappid(2052970) --Mainappid STRIDEN
+addappid(2052971, 1, "a35e88cb228abc5ec0d678a3422974b99a6c006e420f70f70a1ca1ce49e952ce") --Main Windows Depot STRIDEN
+setManifestid(2052971, "8832363896866829452", 21490127312)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
