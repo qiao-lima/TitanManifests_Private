04c20c3766688243a6e11520b75360094559aee6
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 14:24:37 -0400

    restock 2355000

---
+++ 2355000.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2355000.lua
+--Gamename 少女幸存者
+addappid(2355000) --Mainappid 少女幸存者
+addappid(2355001, 1, "4fcc137e491938dec879cfee51be33cfbb99e5de18c1ffe7133db0dbdbde3da3") --Main Windows Depot 少女幸存者
+setManifestid(2355001, "5444070043738662510", 5167646400)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229000, "4622705914179893434", 242681440)
