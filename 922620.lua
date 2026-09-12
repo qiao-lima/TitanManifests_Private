23bee1fc92d2be7b9a7b64452320c7f2936e5021
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 09:55:16 -0400

    restock 922620

---
+++ 922620.lua
@@ -0,0 +1,21 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 922620.lua
+--Gamename Kingshunt
+addappid(922620) --Mainappid Kingshunt
+addappid(922621, 1, "0eecb44f9d8c5964d08dc78b0ed935869fe809d7382a6a35d9f3673eec1c4ceb") --Main Windows Depot Kingshunt
+setManifestid(922621, "952975977217428202", 4264513024)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
+addappid(229032, 1, "15b3308d4e95d1156182211bab1c823b6df0a06fd74a5c0833abed89bc307cfb") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229032, "3616495131483866412", 40038080)
+addappid(229033, 1, "9a789440bbc16131547ac6c062e5f168f7684da533a2899a71fdfaaeec21efc3") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229033, "2059065101492814639", 59325760)
+--Missing Dlcs
+--Dlcname Kingshunt Soundtrack
