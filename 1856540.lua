813cab87220ac06494f253c035a01cafee0bcf43
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 07:22:27 -0400

    restock 1856540

---
+++ 1856540.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1856540.lua
+--Gamename Mirror News Center
+addappid(1856540) --Mainappid Mirror News Center
+addappid(1856541, 1, "d6e65f39c752056082ab498d5b276d3e3f63dd45120f0f2bd5a1c53cc3275ca6") --Main Windows Depot Mirror News Center
+setManifestid(1856541, "5982484528180610673", 136880528)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
