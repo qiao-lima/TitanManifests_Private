6a837532e26ad73e51877219ffe6b1f81e6541c2
qiao-lima <qiao-lima@github.com>
2026-09-12 18:46:06 +0800

    Manual Update 1584090

---
+++ 1584090.lua
@@ -0,0 +1,75 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1584090.lua
+--Gamename 东方夜雀食堂 - Touhou Mystia's Izakaya
+addappid(1584090, 1, "b523c29692b17f3cbe6ce13844d4bb956941f74b26000fdc6e53f06eec2d7c20") --Mainappid 东方夜雀食堂 - Touhou Mystia's Izakaya
+addappid(1584091, 1, "7626ba6bac1a68d11a5685cac46137800840a0c24bbda35037b7fa1dffeaaf96") --Main Windows Depot 东方夜雀食堂 - Touhou Mystia's Izakaya
+setManifestid(1584091, "2219930341254286024", 1494534944)
+addappid(1584092, 1, "914bb29fcfe720613f9b7634be5361e77eaab81ce101b0a04d9b06c01d5f1b3b") --Main Macos Depot 东方夜雀食堂 - Touhou Mystia's Izakaya
+setManifestid(1584092, "5477612847196300111", 1285030480)
+--Dlcname 东方夜雀食堂 - OST原声音乐集1
+addappid(1663680) --Dlcname 东方夜雀食堂 - OST原声音乐集1
+addappid(1663681, 1, "0ea5c1607075ba7a3ccc9f44d3b1fd64067e6a3a6c9337402b7a311ba4e13d1d") --Dlc Windows Depot 东方夜雀食堂 - OST原声音乐集1
+setManifestid(1663681, "6780862591517193903", 158994320)
+addappid(1663682, 1, "aceee204bb814767bb868244f330ac10fe144553c7359cfcf2a8fafc3fd1a8be") --Dlc Windows Depot 东方夜雀食堂 - OST原声音乐集1
+setManifestid(1663682, "1421433781930220961", 501987584)
+--Dlcname 东方夜雀食堂 - OST原声音乐集2
+addappid(1663690) --Dlcname 东方夜雀食堂 - OST原声音乐集2
+addappid(1663691, 1, "9ed4afa1f6012270b0f598e3a02c0e9b53052c8301b2550a843607ff514302c5") --Dlc Windows Depot 东方夜雀食堂 - OST原声音乐集2
+setManifestid(1663691, "3668703231335811507", 137949104)
+addappid(1663692, 1, "2c33fd3939a0484d5430f344970a9723fd838dcf6e9d2205eb5c04b3de256c96") --Dlc Windows Depot 东方夜雀食堂 - OST原声音乐集2
+setManifestid(1663692, "6640746502535105010", 463017664)
+--Dlcname 东方夜雀食堂DLC1扩展包 - 魔法森林&妖怪山
+addappid(1838750) --Dlcname 东方夜雀食堂DLC1扩展包 - 魔法森林&妖怪山
+addappid(1838751, 1, "802c21518c05f219fe8cce3eff43d09ad71dc681f1e6941ae81ef4124682db07") --Dlc Windows Depot 东方夜雀食堂DLC1扩展包 - 魔法森林&妖怪山
+setManifestid(1838751, "9139550867638845281", 170221696)
+addappid(1838752, 1, "723a2afe9f3d1c328df82a5ecc6d93a197a149f9063a425f718b9b0d5c9f8007") --Dlc Macos Depot 东方夜雀食堂DLC1扩展包 - 魔法森林&妖怪山
+setManifestid(1838752, "3224049697019599645", 170179520)
+--Dlcname 东方夜雀食堂DLC2扩展包 - 旧地狱街道&地灵殿
+addappid(1927110) --Dlcname 东方夜雀食堂DLC2扩展包 - 旧地狱街道&地灵殿
+addappid(1927111, 1, "a2c87de0aa72853670544233e715acc81b75ccb1d6d9d6622d2376136e9630c3") --Dlc Windows Depot 东方夜雀食堂DLC2扩展包 - 旧地狱街道&地灵殿
+setManifestid(1927111, "4723380786634962487", 153046576)
+addappid(1927112, 1, "81db4096966234b8690ecd73a689f2043bf973a8878c3af75f6a7ef4049c4008") --Dlc Macos Depot 东方夜雀食堂DLC2扩展包 - 旧地狱街道&地灵殿
+setManifestid(1927112, "4923096582823720523", 152988768)
+--Dlcname 东方夜雀食堂 - OST原声音乐集3
+addappid(1982490) --Dlcname 东方夜雀食堂 - OST原声音乐集3
+addappid(1982491, 1, "b7fc6728ca1033cdb7043c88cefc305587851e3e01246620eaa049a8e075f7c2") --Dlc Windows Depot 东方夜雀食堂 - OST原声音乐集3
+setManifestid(1982491, "6427052909822856002", 221033376)
+addappid(1982492, 1, "8a00b55210725fcfb468f196a5a7044d2938054875e9d583cb8891e7da7ae7d0") --Dlc Windows Depot 东方夜雀食堂 - OST原声音乐集3
+setManifestid(1982492, "8281166644828049447", 976377424)
+--Dlcname 东方夜雀食堂DLC2.5扩展包 - 爱乐者的挑战赛！
+addappid(2122050) --Dlcname 东方夜雀食堂DLC2.5扩展包 - 爱乐者的挑战赛！
+addappid(2122051, 1, "7feb14a9d8b871b77147f0bb18733c0f858070cb0d33dd0f215b59f4674dcf8a") --Dlc Windows Depot 东方夜雀食堂DLC2.5扩展包 - 爱乐者的挑战赛！
+setManifestid(2122051, "4486833365435243238", 138166000)
+addappid(2122052, 1, "2aa2b3238105af8023e856070cf91316c0ab7c02db593f0fa806d3134f57e86d") --Dlc Macos Depot 东方夜雀食堂DLC2.5扩展包 - 爱乐者的挑战赛！
+setManifestid(2122052, "4205881150974300694", 138146128)
+--Dlcname 东方夜雀食堂DLC3扩展包 - 命莲寺&神灵庙
+addappid(2191950) --Dlcname 东方夜雀食堂DLC3扩展包 - 命莲寺&神灵庙
+addappid(2191951, 1, "dff24a4f956e290718903c4aa6e647e9239af39a9aad2c87be61b174a4b60385") --Dlc Windows Depot 东方夜雀食堂DLC3扩展包 - 命莲寺&神灵庙
+setManifestid(2191951, "1246642730370578296", 158871104)
+addappid(2191952, 1, "443e20c81147f4d1fad7e92b8bcec1da18953f4d985e7605fd3dd92233d86623") --Dlc Macos Depot 东方夜雀食堂DLC3扩展包 - 命莲寺&神灵庙
+setManifestid(2191952, "4049434692345131393", 158878656)
+--Dlcname 东方夜雀食堂 - OST原声音乐集4
+addappid(2194750) --Dlcname 东方夜雀食堂 - OST原声音乐集4
+addappid(2194751, 1, "4169ec238719eb5b19f5c856e289aa76d8492feb63ee1b0ad6593c6967bf877b") --Dlc Windows Depot 东方夜雀食堂 - OST原声音乐集4
+setManifestid(2194751, "8126122331354601186", 122148656)
+addappid(2194752, 1, "f3c4f503d6048b9ccc284bdbab0b3902d9b5bfeac97becd4d23fb3e5deebba2c") --Dlc Windows Depot 东方夜雀食堂 - OST原声音乐集4
+setManifestid(2194752, "6657211620998362287", 343502448)
+--Dlcname 东方夜雀食堂DLC4扩展包 - 太阳花田&辉针城
+addappid(2399330) --Dlcname 东方夜雀食堂DLC4扩展包 - 太阳花田&辉针城
+addappid(2399333, 1, "a922cd7cd42f5a333ad760b2029b8894ea28a3077dd6e41df15ca6cc3f93b8ae") --Dlc Windows Depot 东方夜雀食堂DLC4扩展包 - 太阳花田&辉针城
+setManifestid(2399333, "2515397388781918040", 199288352)
+addappid(2399334, 1, "57aabca3caf1c34a00d7ecde1d68b27ea157662ba10990fd14d6b095269977c5") --Dlc Macos Depot 东方夜雀食堂DLC4扩展包 - 太阳花田&辉针城
+setManifestid(2399334, "7985398766387701718", 199279616)
+--Dlcname 东方夜雀食堂DLC5扩展包 - 月都&魔界
+addappid(2399331) --Dlcname 东方夜雀食堂DLC5扩展包 - 月都&魔界
+addappid(2831161, 1, "852b19716db3348a46ee2e3b930ecc2d86834c533fd0450ed0ad04e59bb6c28f") --Dlc Windows Depot 东方夜雀食堂DLC5扩展包 - 月都&魔界
+setManifestid(2831161, "5211561367849300959", 169547360)
+addappid(2831162, 1, "1650cf4024fd1345a8781f6d1c7a39e8aea788517d4ffc26855978a93dff2776") --Dlc Macos Depot 东方夜雀食堂DLC5扩展包 - 月都&魔界
+setManifestid(2831162, "6020412267855899521", 169417456)
+--Dlcname 东方夜雀食堂 - OST原声音乐集5
+addappid(2797450) --Dlcname 东方夜雀食堂 - OST原声音乐集5
+addappid(2797451, 1, "dc88bc41dc2bae344bdacd8e38598a116c8042c58d18258dfffa6d8a82d67c50") --Dlc Windows Depot 东方夜雀食堂 - OST原声音乐集5
+setManifestid(2797451, "2160976927287614938", 187397280)
+addappid(2797452, 1, "0d7dfd633086ad7acfc483e788ac1a2a4299656a64bc44ceb95bd9d32351d86d") --Dlc Windows Depot 东方夜雀食堂 - OST原声音乐集5
+setManifestid(2797452, "7510118917088406825", 652126480)
+addappid(2829050) --Dlcname 东方夜雀食堂动画版 第一话 - 最强的是谁！？
