529ba981233c6f0ca8bf38dac94752f329df9f46
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 01:42:35 -0400

    restock 2011600

---
+++ 2011600.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2011600.lua
+--Gamename JoJo's Bizarre Adventure: All-Star Battle R Demo
+addappid(2011600) --Mainappid JoJo's Bizarre Adventure: All-Star Battle R Demo
+addappid(2011601, 1, "2c261f079316f04a6ccc23db2b0fced43215cd6d08930f6de80462dd6f64b748") --Main Windows Depot JoJo's Bizarre Adventure: All-Star Battle R Demo
+setManifestid(2011601, "5603348705024564410", 546801008)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
