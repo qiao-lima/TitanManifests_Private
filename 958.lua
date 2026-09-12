db3f43339ef2318fc74f11568a47ed84c1f31e84
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 05:16:09 -0400

    restock 958

---
+++ 958.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 958.lua
+--Gamename Infernal Trailer
+addappid(958) --Mainappid Infernal Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Infernal Trailer
+setManifestid(910, "1332084816209327708", 0)
