756c61eda6a67312670291e661e137628f83e484
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 01:58:30 -0400

    restock 1526540

---
+++ 1526540.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1526540.lua
+--Gamename Dungeons And Cat
+addappid(1526540) --Mainappid Dungeons And Cat
+addappid(1526541, 1, "5077549283abbbd1ced0cbe735ed8a17e35e8761c8f40698f13f38719182937f") --Main Windows Depot Dungeons And Cat
+setManifestid(1526541, "6144804597141926760", 811753360)
+--Missing Dlcs
+--Dlcname Dungeons And Cat - Artbook
+--Dlcname Dungeons And Cat Soundtrack
