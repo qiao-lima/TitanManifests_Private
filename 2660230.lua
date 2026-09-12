4b0cfbd52b612a13fe08617a86e2304bd40e7e5d
qiao-lima <qiao-lima@github.com>
2026-09-12 19:03:23 +0800

    Manual Update 2660230

---
+++ 2660230.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2660230.lua
+--Gamename Kill The Shadow
+addappid(2660230) --Mainappid Kill The Shadow
+addappid(2660233, 1, "2d7a0ded92c5e4d6341702c90709bdfd4f3a8837a09d2ae094d07fe132eca0b7") --Main Windows Depot Kill The Shadow
+setManifestid(2660233, "5267710196962238262", 1828642736)
