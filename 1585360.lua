6d5c03d98ae3796217e3b37054814a39d19c4063
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 16:10:15 -0400

    restock 1585360

---
+++ 1585360.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1585360.lua
+--Gamename Live/Wire
+addappid(1585360) --Mainappid Live/Wire
+addappid(1585361, 1, "4757c2f2ad5cb2b6098ac4e3ca5b72f2c19ba5822d7d579af52e77ef17e6cc23") --Main Windows Depot Live/Wire
+setManifestid(1585361, "7265201982176658678", 154978848)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
