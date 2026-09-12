8cba57f38826ac9048add085436b501412884822
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 10:43:58 -0400

    restock 1014350

---
+++ 1014350.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1014350.lua
+--Gamename Once A Stray
+addappid(1014350) --Mainappid Once A Stray
+addappid(1014351, 1, "c876f360afb957d54da3a1e580131203ff311d394a9f822b7ee5c694ba4447fb") --Main Windows Depot Once A Stray
+setManifestid(1014351, "6086939721678172302", 406615264)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
