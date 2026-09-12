eaf4a98ddac9e1338bfa8d7e3d77f738e2c16c04
qiao-lima <qiao-lima@github.com>
2026-09-12 20:46:48 +0800

    Manual Update 2483190

---
+++ 2483190.lua
@@ -0,0 +1,54 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2483190.lua
+--Gamename Forza Horizon 6
+addappid(2483190, 1, "bca2f0f9973d929c2c6bae884058b8bf7934e9ecc8ff34d584450dfbb3215040") --Mainappid Forza Horizon 6
+addtoken(2483190, "2264898939686399139")
+addappid(2483191, 1, "f2a09f0a31b4df681ec0d2d572f368dfa4cd6c37ae67ebdfd591cfbe1334fcd9") --Main Windows Depot Forza Horizon 6
+setManifestid(2483191, "1681298474714072536", 152295982800)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4439240) --Dlcname Forza Horizon 6 Expansion 1
+addtoken(4439240, "10618922691696453695")
+addappid(4439250) --Dlcname Forza Horizon 6 Expansion 2
+addtoken(4439250, "6382430189070780048")
+addappid(4439260) --Dlcname Forza Horizon 6 Welcome Pack
+addtoken(4439260, "5416694940851378001")
+addappid(4439270) --Dlcname Forza Horizon 6 Premium VIP
+addtoken(4439270, "10281334471861719873")
+addappid(4439280) --Dlcname Forza Horizon 6 Time Attack Car Pack
+addtoken(4439280, "4638210807385755524")
+addappid(4439290) --Dlcname Forza Horizon 6 Italian Passion Car Pack
+addtoken(4439290, "7027096395897994901")
+addappid(4439300) --Dlcname AppID 4439300
+addappid(4439750) --Dlcname Forza Horizon 6 Treasure Map
+addappid(4439790) --Dlcname Forza Horizon 6 Car Pass
+addtoken(4439790, "17274680636771417474")
+addappid(4439800) --Dlcname Forza Horizon 6 1990 Nissan #12 Skyline GT-R (BNR32 Gr.A) JTC
+addtoken(4439800, "17911414783810698062")
+addappid(4439810) --Dlcname Forza Horizon 6 2024 Koenigsegg Gemera
+addappid(4439820) --Dlcname Forza Horizon 6 1972 Datsun #269 Attacking the Clock Racing 240Z "All Carbon Hill Climb Beast"
+addappid(4439830) --Dlcname Forza Horizon 6 2008 Honda Civic Type R (FD2)
+addappid(4439840) --Dlcname Forza Horizon 6 2023 Audi R8 Coupé V10 GT RWD
+addappid(4439890) --Dlcname Forza Horizon 6 1974 Mazda #123 Mad Mike 808 Wagon 'FURSTY'
+addappid(4439900) --Dlcname Forza Horizon 6 2023 Toyota GR Corolla
+addappid(4440380) --Dlcname Forza Horizon 6 VIP Membership
+addtoken(4440380, "13331842867689162222")
+addappid(4444140) --Dlcname Forza Horizon 6 TBDS2
+addappid(4444150) --Dlcname Forza Horizon 6 1962 Peel P50 Trolli Edition
+addappid(4520350) --Dlcname Forza Horizon 6 Expansions Bundle
+addtoken(4520350, "7048682700970800609")
+addappid(4520360) --Dlcname Forza Horizon 6 Premium Upgrade
+addappid(4562050) --Dlcname AppID 4562050
+addappid(4610350) --Dlcname Forza Horizon 6 1998 Nissan Skyline GT-R 40th Anniversary
+addappid(4797330) --Dlcname Forza Horizon 6 1972 Honda Z GT
+addappid(4797340) --Dlcname Forza Horizon 6 1972 Nissan Patrol
+addappid(4797350) --Dlcname Forza Horizon 6 2024 Toyota Prius Prime XSE Premium
+addappid(4797360) --Dlcname Forza Horizon 6 2003 Aston Martin DB7 GT
+addappid(4913450) --Dlcname Forza Horizon 6 1968 Alfa Romeo Tipo 33/2
+addappid(4913460) --Dlcname Forza Horizon 6 1987 Porsche #203 Porsche AG 1
+addappid(4913470) --Dlcname Forza Horizon 6 1983 Nissan Skyline 2000 Turbo RS
+addappid(4913480) --Dlcname Forza Horizon 6 1957 Ford Thunderbird
+addappid(4949520) --Dlcname Forza Horizon 6 2025 McLaren W1
