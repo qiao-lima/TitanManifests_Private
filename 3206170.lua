2fc0d47bd97174bf1b96d1f2669489642be03fee
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 04:34:26 -0400

    restock 3206170

---
+++ 3206170.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3206170.lua
+--Gamename Food Park Manager
+addappid(3206170) --Mainappid Food Park Manager
+addappid(3206171, 1, "7758a329395bd443d3eeb962b4ea429dc8a8f6c68267efc8a873f635861a0995") --Main Windows Depot Food Park Manager
+setManifestid(3206171, "3104709071731862078", 511797632)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
