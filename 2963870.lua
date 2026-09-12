76da1831964f2e7a7afb24438c09c11dae7a0ea5
qiao-lima <qiao-lima@github.com>
2026-09-12 18:35:01 +0800

    Manual Update 2963870

---
+++ 2963870.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2963870.lua
+--Gamename Metal Slug: Awakening
+addappid(2963870) --Mainappid Metal Slug: Awakening
+addappid(2963871, 1, "d9d37ba599e18f1a9f535214896754232ecaebbb0438ff8516aa3d8eae599d8e") --Main Windows Language Depot English Metal Slug: Awakening
+setManifestid(2963871, "3363944276722443938", 13068533328)
