4c0222898c2ad2f0a7ae9d823008fcd5f93e9277
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 20:21:59 -0400

    restock 2100020

---
+++ 2100020.lua
@@ -0,0 +1,16 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2100020.lua
+--Gamename Lou's Lagoon
+addappid(2100020, 1, "bff0c5304bbbde13e0d6c7a555f1b71ed7762079e94d98da2b0992c974c25f00") --Mainappid Lou's Lagoon
+addappid(2100021, 1, "01a65b174aaf04d6a5bac593c360161f3f0454d8b3ac85e08525880723894918") --Main Windows Depot Lou's Lagoon
+setManifestid(2100021, "8333860058127222802", 4271011712)
+addappid(5026611, 1, "77d1a5a9ee816149f02305a9bfb4a424d21d66a81562e3d9899a406e87cebd5f") --Main Windows Depot Lou's Lagoon
+setManifestid(5026611, "7558924219766305243", 93944000)
+addappid(5026612, 1, "ac4af0b215b5f8d422cefeac40ee06339a9c07930f32b70d898d124fd2dcb20a") --Main Windows Depot Lou's Lagoon
+setManifestid(5026612, "1116594125282355883", 664051568)
+--Dlcname Lou's Lagoon Digital Artbook
+addappid(4890570, 1, "d02a5846fa9d4187fd32526af7d6658a94be334dfa0bbc0b1f4af5edf0486079") --Dlcname Lou's Lagoon Digital Artbook
+setManifestid(4890570, "4629110426845429241", 30509296)
+--Dlcname Lou's Lagoon Original Soundtrack
+addappid(5026610) --Dlcname Lou's Lagoon Original Soundtrack
+addappid(4831360) --Dlcname Lou's Lagoon Deluxe Pack
