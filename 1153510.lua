0b0d905b930c997f5ad9b940f789d46051111b72
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 02:10:15 -0400

    restock 1153510

---
+++ 1153510.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1153510.lua
+--Gamename Daily OVR Free
+addappid(1153510) --Mainappid Daily OVR Free
+addappid(1153511, 1, "d43e183172b70e89de60de9176bc74b0f0191493c9ee392d27e514f6715516c4") --Main Windows Depot Daily OVR Free
+setManifestid(1153511, "419836497267261013", 478733360)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
