373d640b0eb9253e994d2ce0033ceea150122296
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 00:15:07 -0400

    restock 4264950

---
+++ 4264950.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4264950.lua
+--Gamename Farewells clicker
+addappid(4264950) --Mainappid Farewells clicker
+addappid(4264951, 1, "d4b68ad6e26c68a2a1a1728014be106e57ed8e6d2f25c90c003f353c6d4de8ab") --Main Windows Depot Farewells clicker
+setManifestid(4264951, "6337359645964883726", 76337744)
+addappid(4264952, 1, "1baaee488cfab9beb84ec01da52cab58a116d7852ac270c2ef7fe7384054f14f") --Main Macos Depot Farewells clicker
+setManifestid(4264952, "3270885760306924265", 100479296)
