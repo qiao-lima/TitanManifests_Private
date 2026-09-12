79817390ec0a307acf04263764632c6ac1126944
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 04:02:44 -0400

    restock 4152550

---
+++ 4152550.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4152550.lua
+--Gamename Eggstreme Farming
+addappid(4152550) --Mainappid Eggstreme Farming
+addappid(4152551, 1, "a55a87ce27d14960c39df9dbb1a6b5d86bc948329ed13401da78d154d238bf6f") --Main Windows Depot Eggstreme Farming
+setManifestid(4152551, "8466975929613087685", 811916496)
