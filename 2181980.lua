36297ee9adbc9e2228be79ba0e2b0ea765a84915
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 15:10:16 -0400

    restock 2181980

---
+++ 2181980.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2181980.lua
+--Gamename Ranger Danger
+addappid(2181980) --Mainappid Ranger Danger
+addappid(2181981, 1, "604658b8c0278ae333fce05cf25a2d50b91d53d5b73dab417d88908bbea1d80e") --Main Windows Depot Ranger Danger
+setManifestid(2181981, "5060622378493925610", 736654624)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
