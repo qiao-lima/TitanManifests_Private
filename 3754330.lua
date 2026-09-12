186dfe11f5ae158120f1a7b135299bfefc2a663b
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 11:50:17 -0400

    restock 3754330

---
+++ 3754330.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3754330.lua
+--Gamename Apocalypse Tale(末日物语)
+addappid(3754330) --Mainappid Apocalypse Tale(末日物语)
+addappid(3754331, 1, "5de4bd57c39999a37cee51bb44c731016f86d84a062a48c674c0b482ea006a49") --Main Windows Depot Apocalypse Tale(末日物语)
+setManifestid(3754331, "2600484454516294474", 41176112)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
