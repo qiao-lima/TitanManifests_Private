a656645edca7413327c8bef8a10808a54f92225c
qiao-lima <qiao-lima@github.com>
2026-09-13 01:37:41 +0800

    Manual Update 3938640

---
+++ 3938640.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3938640.lua
+--Gamename Ugoku Sushi Bar
+addappid(3938640) --Mainappid Ugoku Sushi Bar
+addappid(3938641, 1, "0273ab2dc8703a3a60a94a8a1145cfb3c0e9418e66088b1754e618af892711cd") --Main Windows Depot Ugoku Sushi Bar
+setManifestid(3938641, "4883097677465801416", 399256448)
+addappid(3938642, 1, "f448fd2a949274159b33900908dc0ae874d4f31b453b49cef00aacd918357b87") --Main Macos Depot Ugoku Sushi Bar
+setManifestid(3938642, "228498117397396147", 407187968)
+addappid(3938643, 1, "8551351d89fd3b86c4132de374facbba19d0b2084eadb0254c67df6e61d6e11e") --Main Linux Depot Ugoku Sushi Bar
+setManifestid(3938643, "309873775643321362", 397048400)
