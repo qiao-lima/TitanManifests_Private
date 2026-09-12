ecca27fb3eae357cf4dd7f7c79b429cc49b3d866
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 14:08:34 -0400

    restock 4411600

---
+++ 4411600.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4411600.lua
+--Gamename 挂机破坏神 IdleWrecker
+addappid(4411600) --Mainappid 挂机破坏神 IdleWrecker
+addappid(4411601, 1, "f500117c30bf45cd570394269afb5e1bb179cf6e1f027e49c8072cf313a71ac0") --Main Windows Depot 挂机破坏神 IdleWrecker
+setManifestid(4411601, "7620053451485067784", 672409040)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
+addappid(4845720) --Dlcname 挂机破坏神 IdleWrecker 支持者包
+addappid(4972260) --Dlcname 挂机破坏神 IdleWrecker 宠物养成
