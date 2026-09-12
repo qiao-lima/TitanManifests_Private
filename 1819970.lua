b4b486a9442e0905fa6b19a61c9523095f0aa737
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 22:55:16 -0400

    restock 1819970

---
+++ 1819970.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1819970.lua
+--Gamename Jumpy Haha
+addappid(1819970) --Mainappid Jumpy Haha
+addappid(1819971, 1, "0ace174bc99c04485b08adae370e705fafe6ac26f92d1a0759649abd38ed23b6") --Main Windows Depot Jumpy Haha
+setManifestid(1819971, "799786981411790868", 433003648)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
