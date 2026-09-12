12d1802a88110b923cda2157b364107ce0f06ed6
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 03:05:10 -0400

    restock 4790620

---
+++ 4790620.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4790620.lua
+--Gamename Daily Brew Idle: Milk Tea Tycoon
+addappid(4790620) --Mainappid Daily Brew Idle: Milk Tea Tycoon
+addappid(4790621, 1, "89dedea74aaf42b6a221f01f4521de3d1192304684741e7307ee53dce66e3edc") --Main Windows Depot Daily Brew Idle: Milk Tea Tycoon
+setManifestid(4790621, "8558173855477438693", 593356944)
