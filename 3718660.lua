1c1161dfa555e809943062cf339fab2cc227c013
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 01:00:01 -0400

    restock 3718660

---
+++ 3718660.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3718660.lua
+--Gamename Coin Hunter
+addappid(3718660) --Mainappid Coin Hunter
+addappid(3718661, 1, "b7c44d51610401882bfbd12f1c951c79d9124c1de3ad6fd8d3a33c3fee2773d3") --Main Windows Depot Coin Hunter
+setManifestid(3718661, "6394240932934551744", 408242832)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
