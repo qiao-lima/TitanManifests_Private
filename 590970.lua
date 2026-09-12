489b96c5c7e0e96acaa624b89106206ab13e94db
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 10:25:02 -0400

    restock 590970

---
+++ 590970.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 590970.lua
+--Gamename Hollow
+addappid(590970) --Mainappid Hollow
+addappid(590971, 1, "e0fe08592a0e02eeada61bf6f16727afaec4250d3f71ef6905cee69ca4075cd5") --Main Windows Depot Hollow
+setManifestid(590971, "3747743140082041450", 1968809440)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229002, "7260605429366465749", 50365280)
