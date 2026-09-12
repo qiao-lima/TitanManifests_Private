eefb2e24d3c4eff10008276ef75b2d08983bd69b
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 20:54:50 -0400

    restock 1473480

---
+++ 1473480.lua
@@ -0,0 +1,24 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1473480.lua
+--Gamename A.V.A Global
+addappid(1473480) --Mainappid A.V.A Global
+addappid(1473481, 1, "489412c62cb5a465d06b78ebaaae2698e037823fa6f37abac88621bc5633a568") --Main Windows Depot A.V.A Global
+setManifestid(1473481, "6012511149527345211", 13251747552)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229000, "4622705914179893434", 242681440)
+addappid(229031, 1, "98d01f1eaa90ae08f978994e043bcbbf1f23fc72a325528ca7d722c827c0eb54") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229031, "7746630274301172884", 25905472)
+addappid(2884860) --Dlcname A.V.A Global - Masters Pack
+addappid(2961160) --Dlcname A.V.A Global - Busters Content
+addappid(2996300) --Dlcname A.V.A Global - Busters Content: Lili Edition
+--Missing Dlcs
+--Dlcname A.V.A Global - Starters Pack
