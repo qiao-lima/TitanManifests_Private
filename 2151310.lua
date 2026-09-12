8c8a2af1b354da950e6fd93526ca67aa2d4e0b5e
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 10:25:28 -0400

    restock 2151310

---
+++ 2151310.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2151310.lua
+--Gamename Dusk Hollow
+addappid(2151310) --Mainappid Dusk Hollow
+addappid(2151311, 1, "73a9ffc3854f876dac1fc385e97370a6961db818a7277b08a1c24a13386f0d90") --Main Windows Depot Dusk Hollow
+setManifestid(2151311, "5893929281013226853", 431778240)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
