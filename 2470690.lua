73270a79316ea81c3ea19ac5775f4ffde9f93880
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 22:55:11 -0400

    restock 2470690

---
+++ 2470690.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2470690.lua
+--Gamename Jumpah
+addappid(2470690) --Mainappid Jumpah
+addappid(2470691, 1, "ff7bd40e6c298d69110767a8530d1db5d8e350aeac26818e6471e57a68082e36") --Main Windows Language Depot English Jumpah
+setManifestid(2470691, "1129321386235942453", 13758882944)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
