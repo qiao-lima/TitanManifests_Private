822612ddc2248eacce5096730d665cf04a6d8d44
qiao-lima <qiao-lima@github.com>
2026-09-12 17:44:06 +0800

    Manual Update 1733690

---
+++ 1733690.lua
@@ -0,0 +1,16 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1733690.lua
+--Gamename Mandate Order
+addappid(1733690) --Mainappid Mandate Order
+addtoken(1733690, "12179485395462989180")
+addappid(1733691, 1, "63fd390e7dee4070c4502e1d6108ecf41ec357c93839d7e9399b9bd0c6e67470") --Main Windows Depot Mandate Order
+setManifestid(1733691, "3515671379978647843", 11910350528)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
