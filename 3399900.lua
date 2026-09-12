2b1000fe33a502375a1625fc816be8bdd338e96b
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 23:35:40 -0400

    restock 3399900

---
+++ 3399900.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3399900.lua
+--Gamename HellSlave II: Judgment of the Archon
+addappid(3399900) --Mainappid HellSlave II: Judgment of the Archon
+addappid(3399901, 1, "88e72ec90e6674a5ee2936a5caa8bf7b135728e41aff3c67f29c844a4ce2eb29") --Main Windows Depot HellSlave II: Judgment of the Archon
+setManifestid(3399901, "4257048812547046292", 1609016320)
