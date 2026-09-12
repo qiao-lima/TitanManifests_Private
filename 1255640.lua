44c3b883a6e0f9cf6db5a7bf0603fd6fc7972996
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 11:06:35 -0400

    restock 1255640

---
+++ 1255640.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1255640.lua
+--Gamename 吞噬追踪
+addappid(1255640) --Mainappid 吞噬追踪
+addappid(1255641, 1, "eee5c00f0cd33d063e1f15ac528ed1f4a8e7c061bab7049451a15255308318fc") --Main Windows Depot 吞噬追踪
+setManifestid(1255641, "3312689963748462266", 194544880)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
