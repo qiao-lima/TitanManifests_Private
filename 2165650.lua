dcad146534279668783cbf7f0dc2a277be64662f
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 22:22:39 -0400

    restock 2165650

---
+++ 2165650.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2165650.lua
+--Gamename Hoiwa Hub
+addappid(2165650) --Mainappid Hoiwa Hub
+addappid(2165651, 1, "dc39c27e5fd6284c5c0aa499d802ac89f1d6b39f28f771ee4b19acda8a7615f7") --Main Windows Depot Hoiwa Hub
+setManifestid(2165651, "2312707962669546784", 3353872320)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
