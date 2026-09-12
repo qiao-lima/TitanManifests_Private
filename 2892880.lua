0e60f00d08c608348b263402411c84c0239ec9a4
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 00:04:48 -0400

    restock 2892880

---
+++ 2892880.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2892880.lua
+--Gamename 修仙小屋
+addappid(2892880) --Mainappid 修仙小屋
+addappid(2892881, 1, "97371ce20a95049fb3b6aa47ada1b9a21429e2dbc8ca5c1488a31f46fb364900") --Main Windows Language Depot Schinese 修仙小屋
+setManifestid(2892881, "439983642279213414", 57132016)
+--Share Depots
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
