d83cbceed537fc7e7f5e117e47fb60ccfa219c4b
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 03:07:22 -0400

    restock 457670

---
+++ 457670.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 457670.lua
+--Gamename Vrideo
+addappid(457670) --Mainappid Vrideo
+addappid(457671, 1, "850321f4aba6eb668baa8fa8cf904f689326923f395171b2f2006f9c44a21e46") --Main Windows Language Depot English Vrideo
+setManifestid(457671, "7382455913739040394", 28838592)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229002, "7260605429366465749", 50365280)
