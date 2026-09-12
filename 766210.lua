c7d05c12819c5067c61df7f7d6f2b8d49c321fcb
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 02:27:04 -0400

    restock 766210

---
+++ 766210.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 766210.lua
+--Gamename Wargs
+addappid(766210) --Mainappid Wargs
+addappid(766211, 1, "62788e16f766c87de4e31a53df2a063286b21f93c18e0468dffff941e32ee89f") --Main Windows Depot Wargs
+setManifestid(766211, "2793879247387800357", 5662915232)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
