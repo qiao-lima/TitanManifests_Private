db6c96c73d56cf771ac0c6182742a0f4fbd512bb
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 10:05:25 -0400

    restock 1563490

---
+++ 1563490.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1563490.lua
+--Gamename Doggone Hungry
+addappid(1563490) --Mainappid Doggone Hungry
+addappid(1563491, 1, "d4b5d94eba662b8129b8df3299f0c4bdf6e2697df7226024a9cceae6ce12a67b") --Main Windows Language Depot English Doggone Hungry
+setManifestid(1563491, "6191993547628965701", 825390320)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
