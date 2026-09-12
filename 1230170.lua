8b315fc3570b14851d31f818aad2f4b2f0dde345
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 00:34:11 -0400

    restock 1230170

---
+++ 1230170.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1230170.lua
+--Gamename Gunsmith Simulator: Prologue
+addappid(1230170) --Mainappid Gunsmith Simulator: Prologue
+addappid(1230171, 1, "157da1d72d7f391c2a6b5cb72842229b872eb822cfc9bdd3d271882ccae57381") --Main Windows Depot Gunsmith Simulator: Prologue
+setManifestid(1230171, "7400528470921526924", 3966645936)
