628c672daaabcb6bff92f5ed8a92b30e5acd2943
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 15:10:21 -0400

    restock 2881270

---
+++ 2881270.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2881270.lua
+--Gamename Randy & Manilla
+addappid(2881270) --Mainappid Randy & Manilla
+addappid(2881271, 1, "219022978c5fb0ae9cd76ea1f9b4cc7825f26b6b7bcd550ff720a78b758c1f80") --Main Windows Depot Randy & Manilla
+setManifestid(2881271, "7760031893756627433", 3436991024)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
