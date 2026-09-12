d1db3ffe24b52772ab8e0083aa2066ff2c6b20b0
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 12:01:07 -0400

    restock 4598080

---
+++ 4598080.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4598080.lua
+--Gamename NEKO-MIMI SWEET HOUSEMATES Vol. 2
+addappid(4598080, 1, "5f94b197476c50018241ea938ddc23ad8fc01baf406a9a89dabbda6eb1e4bf1c") --Mainappid NEKO-MIMI SWEET HOUSEMATES Vol. 2
+addappid(4598081, 1, "8f32ea4b686b5dfac89047339792f70057414f17cbe44fa4b9254d53e52842c3") --Main Windows Depot NEKO-MIMI SWEET HOUSEMATES Vol. 2
+setManifestid(4598081, "3834244266659474487", 1385644832)
+--Missing Dlcs
+--Dlcname NEKO-MIMI SWEET HOUSEMATES Vol. 2 Demo
