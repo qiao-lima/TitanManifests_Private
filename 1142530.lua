77ed57d2514cbfe1c33b7ea520b2549c88c70f0e
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 03:28:58 -0400

    restock 1142530

---
+++ 1142530.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1142530.lua
+--Gamename Dog Clicker
+addappid(1142530) --Mainappid Dog Clicker
+addappid(1142531, 1, "201bb97933100cc25972ef589cf38088e27ffc367cf31ad2bc3603786a7265f3") --Main Windows Depot Dog Clicker
+setManifestid(1142531, "8941651215023032273", 19531888)
+addappid(1142534, 1, "6176faa4415378c63561e5ddaf248fcd6bd16502dd296768ad92e77bb784f06c") --Main Windows Depot Dog Clicker
+setManifestid(1142534, "7613487539463003345", 22329632)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229002, "7260605429366465749", 50365280)
