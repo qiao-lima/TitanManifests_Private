68572c8c4c5eec909bb580342e0d7f3ec11a6e5e
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 22:47:32 -0400

    restock 64

---
+++ 64.lua
@@ -0,0 +1,43 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 64.lua
+--Gamename AppID 64
+addappid(64, 1, "fb10ec6c3f6ea0b154d79061a5158b9e7eb426ec106dcd26083221b58fd1001a") --Mainappid AppID 64
+setManifestid(64, "7335488883881387432", 1888)
+addappid(65, 1, "55a73bdefe0a1e809caf75f3e32a4b27e96131a4e57a5ea8958199ee014f7101") --Main Windows Language Depot Spanish AppID 64
+setManifestid(65, "6913148842511053476", 1840)
+--Dlcname Ricochet
+addappid(60, 1, "a27d003909cb330f8a5aece1609834b4d613a69d911d937ec1543289b04b25b2") --Dlcname Ricochet
+addappid(1, 1, "b465d45ab2a7c396f7d1c08a6644e68529ec86b14da77e18588abbbcd2412060") --Dlc Windows Depot Ricochet
+setManifestid(1, "2667117727132184734", 264641152)
+addappid(2, 1, "fd681c065529f5ee95e920321a780a9290f8f31ef51c232ccb3e98d27e132ebc") --Dlc Windows Depot Ricochet
+setManifestid(2, "724247850320422500", 19297632)
+addappid(3, 1, "d249f7d93387a793ed021f0e2314ad0cea0d6aef0b0881c3b725a33deec37d59") --Dlc Windows Depot Ricochet
+setManifestid(3, "6081070194444336449", 148112)
+addappid(6, 1, "cd3d8b6f23fb863aea4d6789a53dd58dcf284141860e893e8e580d1988d93d3c") --Dlc Windows Depot Ricochet
+setManifestid(6, "8518478957342391261", 27950128)
+addappid(8, 1, "20af1043db56d6408ddd965580e1269005b6e04f81c3676d155174f73099da95") --Dlc Linux Depot Ricochet
+setManifestid(8, "1360709870205537514", 42477296)
+addappid(9, 1, "68c92cf2532cdc6e0b7a9d2783b473b6d16bac72218badf68d7be9b033076096") --Dlc Macos Depot Ricochet
+setManifestid(9, "1083345737969856561", 19253680)
+addappid(61, 1, "32786a74b6590545be36d6d2cdcd75008c950d0baba60407c54c8148743953f2") --Dlc Windows Depot Ricochet
+setManifestid(61, "4560730344969588055", 5540800)
+addappid(62, 1, "49eba36d29e68a3311048939f73db2d1accb48994d14958792c2be7599b7e928") --Dlc Windows Language Depot French Ricochet
+setManifestid(62, "7530803099210525545", 1808)
+addappid(63, 1, "4c7d5239740ea85831b7543da71032f0726b95080e4eda7c6efc69731c7af130") --Dlc Windows Language Depot Italian Ricochet
+setManifestid(63, "1685163940946214666", 1824)
+addappid(72, 1, "bbc1a14b0c63025aece7d9c3b5a98089205580f321bb5edf6b5f00d368c6face") --Dlc Windows Language Depot French Ricochet
+setManifestid(72, "491735997354925434", 29617440)
+addappid(73, 1, "9604a5ba9ad4b580269d5362e201d217cdd9d268bf1098e2c6beb0c5ade020d3") --Dlc Windows Language Depot Italian Ricochet
+setManifestid(73, "3587203030189636184", 36527248)
+addappid(74, 1, "49d573f303205df2316749a6ede6725b74651fae3e845494b88ac3d0022a2857") --Dlc Windows Language Depot German Ricochet
+setManifestid(74, "6929797608257779911", 35264800)
+addappid(75, 1, "1339202607b6485322859fc1f045c6790bbc470a2d3bb8158aa94b06da31f9bb") --Dlc Windows Language Depot Spanish Ricochet
+setManifestid(75, "6599379689843151742", 37926368)
+addappid(77, 1, "793780217977cc3a005dc4410e963c980137e4faca4442f92dc41ab56af96983") --Dlc Windows Language Depot Schinese Ricochet
+setManifestid(77, "8307934900153578333", 32815776)
+addappid(78, 1, "39f04a0cc8cffae8023eff1e3e933680681997409823942a5f0f2899d2d2c4b2") --Dlc Windows Language Depot Koreana Ricochet
+setManifestid(78, "2582370374354695075", 33761760)
+addappid(79, 1, "019e1cb0950e060fb992001657a444fe03cbc4984081e0d1d9a6505259a7a7e7") --Dlc Windows Language Depot Tchinese Ricochet
+setManifestid(79, "6488358189498321283", 32815632)
+addappid(141, 1, "0d85b74e163cceabf3651fdd6084585587461f6f7735c37eef875634fc6886cb") --Dlc Windows Language Depot Russian Ricochet
+setManifestid(141, "3921686991280371259", 10352)
