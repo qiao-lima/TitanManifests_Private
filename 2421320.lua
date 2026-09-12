122d93f2428eca286fb5037556de32bad4ce840c
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 23:37:01 -0400

    restock 2421320

---
+++ 2421320.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2421320.lua
+--Gamename 在賽博世界和100個道侶雙修
+addappid(2421320) --Mainappid 在賽博世界和100個道侶雙修
+addappid(2421321, 1, "7fdac9248d89183f444333467936c207c24fc2f3cd53094e9c35f9f09bab5ef0") --Main Windows Depot 在賽博世界和100個道侶雙修
+setManifestid(2421321, "7670133625360546789", 8172788992)
