c4aa584ab69eead8e5879722592afa597fa7922b
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 08:02:30 -0400

    restock 2175210

---
+++ 2175210.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2175210.lua
+--Gamename Winter Survival: Prologue
+addappid(2175210) --Mainappid Winter Survival: Prologue
+addappid(2175211, 1, "bc440a4b90e317373b6e839a4d3e9b1e70288038cbd19f6f8a79863b6d9e4b8d") --Main Windows Depot Winter Survival: Prologue
+setManifestid(2175211, "7821460691128584164", 3780205296)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229033, 1, "9a789440bbc16131547ac6c062e5f168f7684da533a2899a71fdfaaeec21efc3") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229033, "2059065101492814639", 59325760)
