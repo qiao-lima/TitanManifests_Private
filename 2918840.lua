ea09271ca0f33f725d971edc193b0dbfe75645bc
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 05:05:16 -0400

    restock 2918840

---
+++ 2918840.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2918840.lua
+--Gamename 史莱姆的抉择：The Slime's Choice：TSC
+addappid(2918840) --Mainappid 史莱姆的抉择：The Slime's Choice：TSC
+addappid(2918841, 1, "fff72a08b02f0b87e7ab729b82fd5c0c92fa62d12e133a4c341b0de12396dcb1") --Main Windows Depot 史莱姆的抉择：The Slime's Choice：TSC
+setManifestid(2918841, "600749139009254499", 4773608784)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
