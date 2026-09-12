40682855983b64c326132aae94650cd0ad125d17
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 00:29:07 -0400

    restock 4271680

---
+++ 4271680.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4271680.lua
+--Gamename outpost4
+addappid(4271680) --Mainappid outpost4
+addtoken(4271680, "18270652608302902804")
+addappid(4271681, 1, "6bb2fc83d77d7c3821e8a74787475b9a1c3b69ce30b842d035dfbfebf36c36a4") --Main Windows Depot outpost4
+setManifestid(4271681, "5386418521383645782", 158462048)
