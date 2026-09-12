9825994b7b9db802f75575e8da5b4ea24c929d2a
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 07:49:35 -0400

    restock 2088250

---
+++ 2088250.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2088250.lua
+--Gamename 瘟疫清零计划 Project Zero
+addappid(2088250) --Mainappid 瘟疫清零计划 Project Zero
+addappid(2088251, 1, "4df832f485ff8757b293b3a77abe85b5048625b9c9268db5f3a05295d292e221") --Main Windows Depot 瘟疫清零计划 Project Zero
+setManifestid(2088251, "3372087974299844283", 887949696)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
