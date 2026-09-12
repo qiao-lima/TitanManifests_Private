27dcaac0b8a3c6b9c09d5e87a95b28c66478c246
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 03:02:04 -0400

    restock 3536480

---
+++ 3536480.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3536480.lua
+--Gamename AC-130丧尸炮艇
+addappid(3536480) --Mainappid AC-130丧尸炮艇
+addappid(3536481, 1, "ba4f41b90ecfb239f53286abb138ea4dbe25f515f2cae98542e9d7a7b4bb1e93") --Main Windows Depot AC-130丧尸炮艇
+setManifestid(3536481, "8125870447540467764", 4564017184)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
