dc15593b1d767c98129ee916e56d6fbe12ce54ed
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 07:30:15 -0400

    restock 1890860

---
+++ 1890860.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1890860.lua
+--Gamename Arma Reforger Experimental
+addappid(1890860) --Mainappid Arma Reforger Experimental
+addappid(1890861, 1, "959ca5bfaceadcf9f4a31c1c9a0f21878dde64894cb0868d1aeb55a59c892bfe") --Main Windows Depot Arma Reforger Experimental
+setManifestid(1890861, "813352885187361319", 22295009760)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
