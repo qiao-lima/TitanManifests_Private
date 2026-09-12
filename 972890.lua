fcfe934c6b5a6f2fa3ce0211ced6d95612301968
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 08:55:29 -0400

    restock 972890

---
+++ 972890.lua
@@ -0,0 +1,19 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 972890.lua
+--Gamename RideOp - VR Thrill Ride Experience
+addappid(972890) --Mainappid RideOp - VR Thrill Ride Experience
+addappid(972891, 1, "3b168fa19b6a9aae2c69572752727c27c39263675255f6d443426080c4f5f9c5") --Main Windows Depot RideOp - VR Thrill Ride Experience
+setManifestid(972891, "2968860214114947134", 2444206912)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
+--Missing Dlcs
+--Dlcname RideOp - VR Pro Edition
