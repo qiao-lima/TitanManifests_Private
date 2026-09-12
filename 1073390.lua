661eb4dcf7394ac4e45699c234bc01569e06078e
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 14:33:00 -0400

    restock 1073390

---
+++ 1073390.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1073390.lua
+--Gamename Aircar
+addappid(1073390) --Mainappid Aircar
+addappid(1073391, 1, "3bf31f757ac00c0aaa0dac1f9aa0710b7cb4e316d7c31ef16b511df1eb429a9f") --Main Windows Depot Aircar
+setManifestid(1073391, "1310052055377678008", 843647920)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
