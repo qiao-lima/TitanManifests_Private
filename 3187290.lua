c92838a2b3e302fc2e9ddca4aaf7aa4b9397538e
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 11:55:12 -0400

    restock 3187290

---
+++ 3187290.lua
@@ -0,0 +1,108 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3187290.lua
+--Gamename Winning Post 10 2025
+addappid(3187290) --Mainappid Winning Post 10 2025
+addappid(3187291, 1, "b70f2db9caaaeec0af28a07621c4b42daa75fa5e69c6c60066ad110915cead63") --Main Windows Depot Winning Post 10 2025
+setManifestid(3187291, "7883421542628519044", 13474035952)
+--Dlcname WP10 2025 名手に初タイトルをもたらした名馬たち 購入権セット 全５頭
+addappid(3455480, 1, "03bb3eae9ad061fecc7200edc93cd5c191e99f2bb5a1cd6bceecdd6412ce5d5e") --Dlcname WP10 2025 名手に初タイトルをもたらした名馬たち 購入権セット 全５頭
+setManifestid(3455480, "445173418291314834", 0)
+--Dlcname WP10 2025 競走馬 所有可能数＋５
+addappid(3455510, 1, "af26b7d1b05b65df66455de3806ce003c4401f80a2d87a25f8837cae630d7c61") --Dlcname WP10 2025 競走馬 所有可能数＋５
+setManifestid(3455510, "1536508292543751762", 0)
+--Dlcname WP10 2025 繁殖牝馬 所有可能数＋５
+addappid(3455520, 1, "1f1f47b3e5bc8205f39ad956249af8696e814f85a7defe37e5c61db95db1f7f2") --Dlcname WP10 2025 繁殖牝馬 所有可能数＋５
+setManifestid(3455520, "5246403314681392395", 0)
+--Dlcname WP10 2025 種牡馬 所有可能数＋５
+addappid(3455530, 1, "29d44babe9b8f5de2010f67b4aa159ae9e93985db0e217fbb52c5115750af8c8") --Dlcname WP10 2025 種牡馬 所有可能数＋５
+setManifestid(3455530, "2733183481460021936", 0)
+--Dlcname WP10 2025 放牧場面積＋５
+addappid(3455540, 1, "e87829eaba140f3513e25c8b1561141ff54d0abbdfde0167e40c487d81f22e54") --Dlcname WP10 2025 放牧場面積＋５
+setManifestid(3455540, "2182254781328022291", 0)
+--Dlcname WP10 2025 放牧場・育成場面積＋２
+addappid(3455550, 1, "281eacdc6a4781e2cd4726666eb7001ce7ea700f94e840046a3e1a3bf9beddde") --Dlcname WP10 2025 放牧場・育成場面積＋２
+setManifestid(3455550, "478375465625690450", 0)
+--Dlcname WP10 2025 牧場 工事期間５０％短縮
+addappid(3455560, 1, "5151b35514dccbf77253dfab88d23171ca20a486bbb9c7a5b7cc23cd4c4bcdd9") --Dlcname WP10 2025 牧場 工事期間５０％短縮
+setManifestid(3455560, "4853457105217630660", 0)
+--Dlcname WP10 2025 クラシック３冠の激闘 購入権セット 全６頭
+addappid(3455630, 1, "893bf9f4e8ff6bdc776c27febe7818de514aa4e6cc55c8ac55a0564203c79063") --Dlcname WP10 2025 クラシック３冠の激闘 購入権セット 全６頭
+setManifestid(3455630, "7731018214503701312", 0)
+--Dlcname WP10 2025 芦毛の怪物の激闘 購入権セット 全４頭
+addappid(3455640, 1, "acd88ebd23d93f6b1737dccf2011f7fc6fb9dfd69d9f84a6feb4bd158ffb9363") --Dlcname WP10 2025 芦毛の怪物の激闘 購入権セット 全４頭
+setManifestid(3455640, "6916552669922552955", 0)
+--Dlcname WP10 2025 帝王とお祭り男の激闘 購入権セット 全６頭
+addappid(3455650, 1, "61a71dcacf170176ba6847c40cb0702270c8b25f54fcab31439e1f45dc67ae6b") --Dlcname WP10 2025 帝王とお祭り男の激闘 購入権セット 全６頭
+setManifestid(3455650, "2505795137297270554", 0)
+--Dlcname WP10 2025 シャドーロールの激闘 購入権セット 全４頭
+addappid(3455660, 1, "7760a59210b32e2bfb80786efdd8e105f77501a866f2ce3f66da84d974bf53c3") --Dlcname WP10 2025 シャドーロールの激闘 購入権セット 全４頭
+setManifestid(3455660, "4170582160719245550", 0)
+--Dlcname WP10 2025 98世代の激闘 購入権セット 全４頭
+addappid(3455670, 1, "e313782f6038a5b2464c41da6308a610bb4c386023672e89f24e0b3c6ea52ecc") --Dlcname WP10 2025 98世代の激闘 購入権セット 全４頭
+setManifestid(3455670, "6518323434373129013", 0)
+--Dlcname WP10 2025 牧場施設「低酸素トレッドミル」＆放牧場面積＋１
+addappid(3455960, 1, "8ece37a8db727af9234e6ecfc9696c9e10ba92aa6a21ee2a34f1deacc0b0ba2f") --Dlcname WP10 2025 牧場施設「低酸素トレッドミル」＆放牧場面積＋１
+setManifestid(3455960, "7415393212474092923", 0)
+--Dlcname WP10 2025 秘書・知人・牧場長・騎手・調教師 最速結婚可能セット
+addappid(3455980, 1, "dc01251c00d4c1f9f23b4e25ad5bf9323d07a3215c39ef5cf804102805022e77") --Dlcname WP10 2025 秘書・知人・牧場長・騎手・調教師 最速結婚可能セット
+setManifestid(3455980, "8718101732640801298", 0)
+--Dlcname WP10 2025 スタートダッシュセット
+addappid(3455990, 1, "da22ea47a70510320cb08120df7f19f77465e63940c6cfa5bd62341d83d78cba") --Dlcname WP10 2025 スタートダッシュセット
+setManifestid(3455990, "5710352681362715805", 0)
+--Dlcname WP10 2025 放牧場面積＋２＆最強馬ロード・オンライン-名馬カードコレクターセット
+addappid(3855750, 1, "363caec7da73df3a587990cb98e1266c5ec94b9d4839b9a8af9cccda71bb7ca7") --Dlcname WP10 2025 放牧場面積＋２＆最強馬ロード・オンライン-名馬カードコレクターセット
+setManifestid(3855750, "8324567486727667357", 0)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3456000) --Dlcname WP10 2025 馬所有可能数＆牧場面積拡張＆工事期間短縮&新施設DLCフルセット
+addappid(3456010) --Dlcname WP10 2025 1971年スタート 日本馬スターホース購入権フルセット 全１２頭
+addappid(3456020) --Dlcname WP10 2025 1971年スタート 海外馬スターホース購入権フルセット 全１２頭
+addappid(3456030) --Dlcname WP10 2025 第一次競馬ブームを巻き起こした名馬たち 購入権フルセット 全１０頭
+addappid(3456040) --Dlcname WP10 2025 激闘繰り広げし優駿たち 購入権フルセット 全２４頭
+addappid(3456050) --Dlcname WP10 2025 記憶に残る優駿たち 購入権フルセット 全２２頭
+addappid(3456060) --Dlcname WP10 2025 血脈継ぎし優駿たち 購入権フルセット 全１９頭
+addappid(3456070) --Dlcname WP10 2025 誇り高き優駿たち 購入権フルセット 全２０頭
+addappid(3456090) --Dlcname WP10 2025 世界のレジェンドホース 購入権フルセット 全１２頭
+addappid(3456100) --Dlcname WP10 2025 世界のミラクルホース 購入権フルセット 全１２頭
+addappid(3456110) --Dlcname WP10 2025 世界のグロリアスホース 購入権フルセット 全１２頭
+--Missing Dlcs
+--Dlcname WP10 2025 日本で輝いた外国産馬たち 購入権セット 全４頭
+--Dlcname WP10 2025 既存秘書・やよい＆夕映＆芽愛 コスプレ衣装セット 全１２点
+--Dlcname WP10 2025 新秘書・リオンローザ コスプレ衣装セット 全１２点
+--Dlcname WP10 2025 毛色名鑑セット（５種）
+--Dlcname WP10 2025 米国・欧州牧場 最速開設セット
+--Dlcname WP10 2025 常識を覆した名馬たち 購入権セット 全５頭
+--Dlcname WP10 2025 花の47年組 購入権セット 全７頭
+--Dlcname WP10 2025 初代アイドルホース・ハイセイコーの時代 購入権セット 全４頭
+--Dlcname WP10 2025 華麗なる一族の祖と八大競走の覇者たち 購入権セット 全６頭
+--Dlcname WP10 2025 記憶に残る最強逃げ馬 購入権セット 全５頭
+--Dlcname WP10 2025 記憶に残る最強追い込み馬 購入権セット 全４頭
+--Dlcname WP10 2025 記憶に残る最強スプリンター 購入権セット 全４頭
+--Dlcname WP10 2025 記憶に残る最強マイラー 購入権セット 全４頭
+--Dlcname WP10 2025 記憶に残る最強ダート馬 購入権セット 全５頭
+--Dlcname WP10 2025 名家の血脈 購入権セット 全５頭
+--Dlcname WP10 2025 大王の血脈 購入権セット 全５頭
+--Dlcname WP10 2025 衝撃の血脈 購入権セット 全４頭
+--Dlcname WP10 2025 黄金の血脈 購入権セット 全５頭
+--Dlcname WP10 2025 他を圧倒する王者の誇り 購入権セット 全５頭
+--Dlcname WP10 2025 牝馬３冠の誇り 購入権セット 全５頭
+--Dlcname WP10 2025 女帝たる誇り 購入権セット 全５頭
+--Dlcname WP10 2025 銀冠から掴んだ頂点の誇り 購入権セット 全５頭
+--Dlcname WP10 2025 オリジナルスーパーホース 購入権セット 全６頭
+--Dlcname WP10 2025 世界に挑んだ名馬たち 購入権セット 全９頭
+--Dlcname WP10 2025 世界 名誉ある王者たち 購入権セット 全６頭
+--Dlcname WP10 2025 世界 驚嘆の王者たち 購入権セット 全６頭
+--Dlcname WP10 2025 世界 伝説の王者たち 購入権セット 全６頭
+--Dlcname WP10 2025 世界 偉大なる王者たち 購入権セット 全６頭
+--Dlcname WP10 2025 世界 永遠の王者たち 購入権セット 全５頭
+--Dlcname WP10 2025 世界 奇跡の王者たち 購入権セット 全４頭
+--Dlcname WP10 2025 世界 はかなき王者たち 購入権セット 全３頭
+--Dlcname WP10 2025 世界 栄えある王者たち 購入権セット 全４頭
+--Dlcname WP10 2025 世界 絶対王者たち 購入権セット 全４頭
+--Dlcname WP10 2025 世界 新時代の王者たち 購入権セット 全４頭
+--Dlcname WP10 2025 世界で火花を散らしたライバルたち 購入権セット 全８頭
+--Dlcname WP10 2025 全世界 史実種牡馬＆繁殖牝馬 無料選択購入権セット
+--Dlcname WP10 2025 史実調教使用可能数＋１（永続型）
