d7167eddfeed5cc87677f4421e6bbd2c3cb071b5
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 20:05:11 -0400

    restock 1116520

---
+++ 1116520.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1116520.lua
+--Gamename The Backrooms
+addappid(1116520) --Mainappid The Backrooms
+addappid(1116521, 1, "8a896065a838227eb32e5cc6db9515227630682b4eb365c6f8920a771e19a0e4") --Main Windows Depot The Backrooms
+setManifestid(1116521, "2830234859435926364", 1411712448)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
