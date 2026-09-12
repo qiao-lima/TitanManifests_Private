e9332e759757e6de682fcae27fd5090851da0773
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 23:49:55 -0400

    restock 4298500

---
+++ 4298500.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4298500.lua
+--Gamename Last Breath 最后的气息
+addappid(4298500, 1, "cac04836629f91ec705caaaa8d1bfd6dae3a6dc036ca142a8d4b98776c1fc424") --Mainappid Last Breath 最后的气息
+addappid(4298501, 1, "8f6a36d642f08f18e19f5bdb7525cb614acf9c9fb31298a9346cd2f09384b786") --Main Windows Depot Last Breath 最后的气息
+setManifestid(4298501, "9175555991801751862", 720732544)
