65c6477e3953da1d1ed0c1181f0b4dd2510ad229
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 21:22:24 -0400

    restock 1600950

---
+++ 1600950.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1600950.lua
+--Gamename PROPHUNT
+addappid(1600950) --Mainappid PROPHUNT
+addappid(1600951, 1, "0f3ccec2df340443a7334c45793f27ee0ca17f2cc7e822cb698a684d33e9f0bb") --Main Windows Depot PROPHUNT
+setManifestid(1600951, "527824694768443403", 2509120816)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
