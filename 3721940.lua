c5b08a7d61dd289bb63d091c6aece7edc84ddc19
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 09:43:07 -0400

    restock 3721940

---
+++ 3721940.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3721940.lua
+--Gamename Undercity of Sin
+addappid(3721940, 1, "d48c22135ebc4be569e731cdce9569cd0d0bc4f22778d1251a70cc420a721df2") --Mainappid Undercity of Sin
+addappid(3721941, 1, "d24ee73f49887dbe3eb8cbc0b1187086b55fd587df7f94e6d6af7009cc42cdff") --Main Windows Depot Undercity of Sin
+setManifestid(3721941, "3472876156740552024", 1201627584)
+addappid(3721942, 1, "195e9731ba449cbd120b05484d5f95fcf9af2da292dac1c3b5561b4ba6628db7") --Main Windows Language Depot Schinese Undercity of Sin
+setManifestid(3721942, "3208832470010965704", 10795584)
+addappid(3721943, 1, "7a42961f428a68d5bb3868c63c8b237d7a41fda46a1beff6e9ed6be16fadc187") --Main Windows Language Depot Japanese Undercity of Sin
+setManifestid(3721943, "2070063400027175285", 4765728)
