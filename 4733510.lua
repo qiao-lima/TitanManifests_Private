025be6af3eff1fd97ba0694a695e0bf94faf72f7
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 14:25:13 -0400

    restock 4733510

---
+++ 4733510.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4733510.lua
+--Gamename WILDS MONSTER
+addappid(4733510) --Mainappid WILDS MONSTER
+addappid(4733511, 1, "b8128b2a5c87351b68fa8e130ad7b0642a1cf80025fdd416d3fdd15a2a13f45f") --Main Windows Depot WILDS MONSTER
+setManifestid(4733511, "5695781333386303922", 324863952)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
