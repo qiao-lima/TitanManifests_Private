e5ac589f0eaf2dbe39e6114d2bf56ad8546b9cb5
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 11:45:32 -0400

    restock 2788440

---
+++ 2788440.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2788440.lua
+--Gamename 20年前のツクール製ゲームをツクール最新作RPG MAKER UNITEでリメイクして無料配布中?!～ツクール歴代勇者VS魔王～
+addappid(2788440) --Mainappid 20年前のツクール製ゲームをツクール最新作RPG MAKER UNITEでリメイクして無料配布中?!～ツクール歴代勇者VS魔王～
+addappid(2788443, 1, "c63deeb6ca677e123d8bd73765f5f58b41daefa94f77b8fe80a3b738bd845143") --Main Windows Language Depot Schinese 20年前のツクール製ゲームをツクール最新作RPG MAKER UNITEでリメイクして無料配布中?!～ツクール歴代勇者VS魔王～
+setManifestid(2788443, "2156668716535318443", 1017880672)
