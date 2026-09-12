16a3ede2756b98cfb7b1bb70ffd88cf210f620df
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 01:48:11 -0400

    restock 3478590

---
+++ 3478590.lua
@@ -0,0 +1,17 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3478590.lua
+--Gamename 十二影Twelves:Shadow
+addappid(3478590) --Mainappid 十二影Twelves:Shadow
+addappid(3478591, 1, "7ded711ce22dd617ac754f83c09f4338bee15d234ce52afcb287048cf66ed20e") --Main Windows Depot 十二影Twelves:Shadow
+setManifestid(3478591, "8706959091073050113", 15344015200)
+--Dlcname 十二影 SoundTrack
+addappid(3871490) --Dlcname 十二影 SoundTrack
+addappid(3871491, 1, "4ecd562ed4a5c81f8a57c3cec210fa509634de3e83d66d39b4cb3cd54069e61d") --Dlc Windows Depot 十二影 SoundTrack
+setManifestid(3871491, "105501199471802099", 298686640)
+addappid(3871492, 1, "4f9a441d28422243364017c81a2e517d004452dc47256d7b851d6539394949df") --Dlc Windows Depot 十二影 SoundTrack
+setManifestid(3871492, "788021763084505410", 328768496)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
