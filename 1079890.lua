4957063baea478235f4bffc661ec6865c6cdfbfe
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 07:48:39 -0400

    restock 1079890

---
+++ 1079890.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1079890.lua
+--Gamename Happy World
+addappid(1079890) --Mainappid Happy World
+addappid(1079891, 1, "17751f28fd2b1933b5223dd08d888f741cf68cc617c15060273e947c4257297f") --Main Windows Depot Happy World
+setManifestid(1079891, "7003677047965430507", 2261090400)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
