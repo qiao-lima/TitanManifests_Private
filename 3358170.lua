7baa2c45273edb53b223487c73172598b139e557
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 08:48:19 -0400

    restock 3358170

---
+++ 3358170.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3358170.lua
+--Gamename Dodo Duckie
+addappid(3358170) --Mainappid Dodo Duckie
+addappid(3358172, 1, "1fd2b10cd546ab9e5215ac903985f339c1d7782ae336b11fd8c3e827406167d7") --Main Windows Depot Dodo Duckie
+setManifestid(3358172, "2360481034109145587", 2095627632)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
