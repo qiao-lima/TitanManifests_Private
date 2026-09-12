5bb3fce440ffa5b99eee62b69af0fa1e5dd1bd7f
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 11:59:34 -0400

    restock 2399190

---
+++ 2399190.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2399190.lua
+--Gamename 星际探索
+addappid(2399190) --Mainappid 星际探索
+addappid(2399191, 1, "430d889847c6c4eaada74aef2b3ad55cf26402e4f725f1ce403a609f634f1854") --Main Windows Depot 星际探索
+setManifestid(2399191, "1060483086852964667", 12445462624)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
