3332fbf1304f0e27814b59fa0709196aeae0d09d
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 04:07:59 -0400

    restock 2055600

---
+++ 2055600.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2055600.lua
+--Gamename XGamer - AI revolution
+addappid(2055600) --Mainappid XGamer - AI revolution
+addappid(2055601, 1, "988586e55707736982686495361afe39fe09b1b40c32c8f1781070476085ce53") --Main Windows Depot XGamer - AI revolution
+setManifestid(2055601, "1757786176522377747", 17831895264)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2087990) --Dlcname XGAMER - AI Revolution Soundtrack
+addappid(2089470) --Dlcname XGAMER - AI Revolution | World Simulation Lab
+addappid(2090720) --Dlcname XGAMER - AI Revolution | Arsenal Access Module
+addappid(2091430) --Dlcname XGAMER - AI Revolution | Titan Module
