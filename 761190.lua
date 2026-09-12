81c5e7a29bab6073b461edec8f4c2dd39306a1cc
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 20:15:05 -0400

    restock 761190

---
+++ 761190.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 761190.lua
+--Gamename 1010
+addappid(761190) --Mainappid 1010
+addappid(761191, 1, "d34de1d9115f350bd9e74917d3ec88592afaf04cdbb54463021f0ff66d54479c") --Main Windows Depot 1010
+setManifestid(761191, "6125760406447580977", 56886032)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
