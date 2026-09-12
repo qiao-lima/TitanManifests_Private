7f4f5413e33443ef02eaf80de05caa1904fc769e
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 03:29:06 -0400

    restock 905130

---
+++ 905130.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 905130.lua
+--Gamename Doggo Dig Down
+addappid(905130) --Mainappid Doggo Dig Down
+addappid(905131, 1, "427b6cc48e2caa63787982959685efc66a830c5dc2a250b2977ad5cb6e4ae427") --Main Windows Depot Doggo Dig Down
+setManifestid(905131, "3073809955880968097", 68588464)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229002, "7260605429366465749", 50365280)
