0b79e7930e167c3905ca71f87929219462e6d6ac
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 00:04:52 -0400

    restock 3135410

---
+++ 3135410.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3135410.lua
+--Gamename 修仙记——消消乐乱斗
+addappid(3135410) --Mainappid 修仙记——消消乐乱斗
+addappid(3135411, 1, "2e546362a072b4ea5a25c8f2b3dad5d60825d68d70af5589fe17f38efc7f56c2") --Main Windows Language Depot Schinese 修仙记——消消乐乱斗
+setManifestid(3135411, "6243639323400588698", 308837552)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
