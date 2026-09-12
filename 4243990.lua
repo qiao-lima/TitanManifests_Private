55744d306dcaa6f371225a432bd1f5434c684775
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 23:56:27 -0400

    restock 4243990

---
+++ 4243990.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4243990.lua
+--Gamename Path of Idle: Old Gods Rising
+addappid(4243990) --Mainappid Path of Idle: Old Gods Rising
+addappid(4243991, 1, "623ed85953c63973a1304b6251c1f4ae8ec579582fafa15adf440db37d696852") --Main Windows Depot Path of Idle: Old Gods Rising
+setManifestid(4243991, "2797698054593602156", 170469440)
+addappid(4243992, 1, "f98fc8450d889cf7c2b73a25f834a6f0f3c5839fe383771816440bf41d0fdcc6") --Main Macos Depot Path of Idle: Old Gods Rising
+setManifestid(4243992, "7202827951848713654", 196314880)
+addappid(4985510) --Dlcname Path of Idle: Old Gods Rising - Supporter Pack
