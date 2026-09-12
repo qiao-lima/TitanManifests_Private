7050df526e70f1dc9d540d209b63ea6eb0052b74
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 03:35:21 -0400

    restock 708190

---
+++ 708190.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 708190.lua
+--Gamename Code World
+addappid(708190) --Mainappid Code World
+addappid(708191, 1, "e0834e0d4604bb1a2a128f008ac72263d5df30f5c3433c0f464e4e74acd7a698") --Main Windows Depot Code World
+setManifestid(708191, "7852989607929599160", 129545456)
+--Share Depots
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
