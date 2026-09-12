bcf03043e6d2a1c38fa491cfa2de55b6678c551c
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 08:15:01 -0400

    restock 3545430

---
+++ 3545430.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3545430.lua
+--Gamename 妖精之地 Playtest
+addappid(3545430) --Mainappid 妖精之地 Playtest
+addappid(3545431, 1, "476806cd603b59bba50be6f9bf7ef7c09093cf4fa228231d0d34038230816367") --Main Windows Depot 妖精之地 Playtest
+setManifestid(3545431, "6697730729000354358", 8932410240)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
