4dd4c5fbcc497d9cafa0484f992b8f52f2f5fbfa
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 09:43:02 -0400

    restock 3076910

---
+++ 3076910.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3076910.lua
+--Gamename UNDERGROUNDED
+addappid(3076910, 1, "8c68f48f2ef8e8c5f66c0c41ee2171d8b9621d1b3d3f03bfe6df0b1682c7f505") --Mainappid UNDERGROUNDED
+addappid(3076911, 1, "3167c3bf96ea12a51adedd16bc676b29d8078410f1516d2f1758d3d421ca87f0") --Main Windows Depot UNDERGROUNDED
+setManifestid(3076911, "5765736556769552762", 620376400)
+--Missing Dlcs
+--Dlcname UNDERGROUNDED Soundtrack
