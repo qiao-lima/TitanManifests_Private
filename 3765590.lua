127f3ec150f6c068d44bb60f7895bde6d186c81a
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 14:35:02 -0400

    restock 3765590

---
+++ 3765590.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3765590.lua
+--Gamename 我的高冷女友 MY COLD GIRLFRIEND
+addappid(3765590, 1, "060320df5dae2fe3bca50527fc2ebe6cad99923c2dd47f92d0b5f58262d9f6fc") --Mainappid 我的高冷女友 MY COLD GIRLFRIEND
+addappid(3765591, 1, "aca3235b761cfbb8aa89164e633e14f5cd2fd02627bda0fc76e3b1a3028dce74") --Main Windows Depot 我的高冷女友 MY COLD GIRLFRIEND
+setManifestid(3765591, "8011931747447227978", 644516272)
