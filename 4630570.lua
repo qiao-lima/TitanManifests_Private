94f89fdb36a0e64b6f25975359160d876a04234b
qiao-lima <qiao-lima@github.com>
2026-09-12 19:51:46 +0800

    Manual Update 4630570

---
+++ 4630570.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4630570.lua
+--Gamename RUNNING TRAIN | 走ル列車！
+addappid(4630570) --Mainappid RUNNING TRAIN | 走ル列車！
+addappid(4630571, 1, "9ccfa133db29092f1015f0e57b04305349fac2913ae279e2278fea0183079dec") --Main Windows Depot RUNNING TRAIN | 走ル列車！
+setManifestid(4630571, "152611941783907699", 2457379408)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
