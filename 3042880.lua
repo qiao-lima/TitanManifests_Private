8452483f4a73921c3848f9fdc2969deb56d77c8b
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 01:20:01 -0400

    restock 3042880

---
+++ 3042880.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3042880.lua
+--Gamename 魔法钢琴
+addappid(3042880) --Mainappid 魔法钢琴
+addappid(3042881, 1, "0d5caf4bf8629576d0a962a2900579bd97f997ba3f5121e596a0352e2abeddb1") --Main Windows Depot 魔法钢琴
+setManifestid(3042881, "6706446654772562926", 1777841888)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
