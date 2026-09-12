b85acbcfb87f402a3f9ecf9ebcf793cb8132d6b5
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 15:10:27 -0400

    restock 1591960

---
+++ 1591960.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1591960.lua
+--Gamename Randy Blaster 3D
+addappid(1591960) --Mainappid Randy Blaster 3D
+addappid(1591961, 1, "bf0f37f4979e42462af23c28fc9d86d4e655ce3343dc09502e05487f421de1d6") --Main Windows Depot Randy Blaster 3D
+setManifestid(1591961, "6011930988408326484", 641379280)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
