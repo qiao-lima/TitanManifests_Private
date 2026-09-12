8ad82dd16dd4d8a56961e9579e1bff460bb211fa
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 02:27:09 -0400

    restock 1026420

---
+++ 1026420.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1026420.lua
+--Gamename WARSAW
+addappid(1026420) --Mainappid WARSAW
+addappid(1026421, 1, "fba1bfd66f2fa15b4c5f573353fee50e8649040c0112c0dd67984c39e264ea5b") --Main Windows Depot WARSAW
+setManifestid(1026421, "7178146382852588481", 1559570272)
+addappid(1026422, 1, "ead453012fde5ea3e2bf033f97c721aff954d90bf815f1f7a408b85c85402761") --Main Windows Depot WARSAW
+setManifestid(1026422, "2098185919695936181", 1561956800)
+--Dlcname WARSAW Legacy
+addappid(2940140, 1, "c12a689c1179c57453f4a5f33d7626e56817fe67867585b19321010af067749a") --Dlcname WARSAW Legacy
+setManifestid(2940140, "6629811495584544571", 722068368)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
