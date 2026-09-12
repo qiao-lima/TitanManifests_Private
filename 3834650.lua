fa936b99e29c021d10c7ffd51c5cf7cf614ebc04
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 11:50:08 -0400

    restock 3834650

---
+++ 3834650.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3834650.lua
+--Gamename 魔堡乱斗
+addappid(3834650) --Mainappid 魔堡乱斗
+addappid(3834651, 1, "7b5f0f9d1f7ba49f74cb5535b6a2426040adb647a9481b51dbb97b555b3f0df0") --Main Windows Depot 魔堡乱斗
+setManifestid(3834651, "5209130205177056489", 199253488)
