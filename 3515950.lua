a96f872922419c0fcff7f423fd595d786bf3e824
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 13:00:01 -0400

    restock 3515950

---
+++ 3515950.lua
@@ -0,0 +1,23 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3515950.lua
+--Gamename Star Wars Outlaws Demo
+addappid(3515950) --Mainappid Star Wars Outlaws Demo
+addappid(3515951, 1, "615b6fcb94405543d7713a51bc2ef113eb5c423b5b3e675a81216fc5e6a24f62") --Main Windows Depot Star Wars Outlaws Demo
+setManifestid(3515951, "5145536508212151577", 32478043248)
+addappid(3515952, 1, "F72333E1DDF8ED4BA42559E2108ECBC12FC975262E8BF2B7FA41BE959454DCC8") --Main Windows Language Depot French Star Wars Outlaws Demo
+setManifestid(3515952, "7630551382633001003", 1444277136)
+addappid(3515953, 1, "9671E811FF8C5A49934F5818EDAF8707584ED83324A0D1464EF3E6C61169810F") --Main Windows Language Depot German Star Wars Outlaws Demo
+setManifestid(3515953, "2508815244890683240", 1539655488)
+addappid(3515954, 1, "EC1E4544DCC9F9637370889F8411F764CEDBDF977E24FBF4B78625DE62E9358D") --Main Windows Language Depot Spanish Star Wars Outlaws Demo
+setManifestid(3515954, "2487393482149328275", 1577490048)
+addappid(3515955, 1, "7336f549ef47cc7e09e92f3f219b54422f01f387ddfee1978bb2386c96b146f7") --Main Windows Language Depot Brazilian Star Wars Outlaws Demo
+setManifestid(3515955, "5483967761384708955", 1504312656)
+addappid(3515956, 1, "A985DB2BD75E9DD82864672C7CF146930C580C92282DBD945B2AFAA1CDB649F0") --Main Windows Language Depot Japanese Star Wars Outlaws Demo
+setManifestid(3515956, "1254246906739466455", 1505485312)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") --Share Windows Depot Ubisoft Connect PC Client
+setManifestid(1716751, "6659642105086821873", 264636800)
