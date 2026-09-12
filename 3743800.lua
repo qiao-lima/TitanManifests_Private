5707d4b61190d214ef397964414b684cffc14dd4
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 07:35:33 -0400

    restock 3743800

---
+++ 3743800.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3743800.lua
+--Gamename Hozy Playtest
+addappid(3743800) --Mainappid Hozy Playtest
+addappid(3743801, 1, "b03bde1a555521807cec9c66e12e3f8a49c9f72bb643c55459095035d87dd9c5") --Main Windows Depot Hozy Playtest
+setManifestid(3743801, "4196876083658061078", 1372309296)
+addappid(3743802, 1, "79443380b30b4c771eb9cd4381a9b74af2e675b125cfd71fca8814d0185debfe") --Main Macos Depot Hozy Playtest
+setManifestid(3743802, "1358706321563118840", 1320740688)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
