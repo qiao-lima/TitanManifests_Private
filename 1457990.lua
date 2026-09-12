0bd01eeeb9498631c1fc12b335b2c105fe1e4ff8
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 21:45:20 -0400

    restock 1457990

---
+++ 1457990.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1457990.lua
+--Gamename Yuligans: Christmas is Coming!
+addappid(1457990) --Mainappid Yuligans: Christmas is Coming!
+addappid(1457991, 1, "0236d16f0be66cc9cdf4a1c8769fe3af2ee4e9954e6558ae5ee7cf2badccbcf8") --Main Windows Depot Yuligans: Christmas is Coming!
+setManifestid(1457991, "5575295279767412241", 191140704)
+addappid(1457992, 1, "e7079f3a57bb18495dda5672d4b5e427233b6ec4e6ef5f8b35da597c1f6b2839") --Main Linux Depot Yuligans: Christmas is Coming!
+setManifestid(1457992, "7535851305300616287", 147260768)
+addappid(1457993, 1, "1bf555de62c19ec2bbe0d31cca15b30cf5e7faa8275ea143e7ba6a897f365b0c") --Main Macos Depot Yuligans: Christmas is Coming!
+setManifestid(1457993, "6527509282216172787", 168349184)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
