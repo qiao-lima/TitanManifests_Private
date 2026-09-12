07e061a6d4ab10a02a082e8bbdb7348dfa1dcb2e
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 09:25:03 -0400

    restock 965190

---
+++ 965190.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 965190.lua
+--Gamename Night Rolls Cozy Store
+addappid(965190) --Mainappid Night Rolls Cozy Store
+addappid(965191, 1, "5960740cf4a7ea3cefdb785c5ef55aab51a3ca744663745a13ec6b65b839b630") --Main Windows Depot Night Rolls Cozy Store
+setManifestid(965191, "4079529827339568826", 754188288)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
