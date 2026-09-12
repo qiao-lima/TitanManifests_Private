682af636608d898ef6fca67a16c8ac10748433d5
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 21:24:27 -0400

    restock 985020

---
+++ 985020.lua
@@ -0,0 +1,21 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 985020.lua
+--Gamename Three Kingdoms：Ancient battlefield | 三国古战略
+addappid(985020) --Mainappid Three Kingdoms：Ancient battlefield | 三国古战略
+addappid(985021, 1, "d7f29ceed1aabebed7bc4539df0066c8c6796bea0c1a0ea212d299ca58824cea") --Main Windows Depot Three Kingdoms：Ancient battlefield | 三国古战略
+setManifestid(985021, "1280566638669363055", 106344416)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
