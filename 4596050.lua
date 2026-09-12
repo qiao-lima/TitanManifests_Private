9b3c5a9f8b0646f964e72ecf82f3f33d93249ce6
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 01:10:20 -0400

    restock 4596050

---
+++ 4596050.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4596050.lua
+--Gamename Eros Memories: The Lewd Way to Save the World
+addappid(4596050) --Mainappid Eros Memories: The Lewd Way to Save the World
+addappid(4596051, 1, "4308912aacd484570c19373f5aaf0770723724c115eb13fbd3c3490fec850ce7") --Main Windows Language Depot Japanese Eros Memories: The Lewd Way to Save the World
+setManifestid(4596051, "1270241829958165317", 1024027920)
+addappid(4596052, 1, "38251e446ab420eca94212e8218f55c64f3be1f6e9a1f0d7245093b6c58b4bec") --Main Windows Language Depot Schinese Eros Memories: The Lewd Way to Save the World
+setManifestid(4596052, "6328411917234209476", 1024045232)
+addappid(4596053, 1, "47c7cf52b13d1f7d9ddd3f1f6f9ee412c04faefb70de44fdb3179e252cb532fd") --Main Windows Language Depot Tchinese Eros Memories: The Lewd Way to Save the World
+setManifestid(4596053, "4776442428449690840", 1024045232)
+addappid(4596054, 1, "bcfd50c651fe4d3d9fe9c361cd7eabe42b2dab8c54afa312e15ca55088d91f2e") --Main Windows Language Depot English Eros Memories: The Lewd Way to Save the World
+setManifestid(4596054, "1663997726504849645", 1024045232)
+--Missing Dlcs
+--Dlcname Eros Memories: The Lewd Way to Save the World Demo
