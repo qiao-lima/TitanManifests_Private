50ffce1e335889f36bf7fc4b7818cba8adcc150e
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 04:33:49 -0400

    restock 2767030

---
+++ 2767030.lua
@@ -0,0 +1,17 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2767030.lua
+--Gamename Marvel Rivals
+addappid(2767030, 1, "5d52aa8e007a67fa5854c1c1135765906b92bbadaeaf4f77567c7a1d4477c7dd") --Mainappid Marvel Rivals
+addappid(2767031, 1, "6623611cb93523c4cc50d8400d48919b64f55d49dc81a8d6e0695fe740474e42") --Main Windows Depot Marvel Rivals
+setManifestid(2767031, "170653385048470424", 65310058928)
+addappid(2767033, 1, "ed2bff1e2d77e17b58d50b4f29d200ba0623dae1492580c11e494cb1136b8bb5") --Main Windows Depot Marvel Rivals
+setManifestid(2767033, "4276939759467466297", 17515705920)
+--Dlcname Marvel Rivals - High-Resolution Pack
+addappid(4406010) --Dlcname Marvel Rivals - High-Resolution Pack
+addappid(4406011, 1, "8ed536ae3655b8beb7d66ee5ebf2a6eb02ea6c215f046cd5a6d5eb22dfd43203") --Dlc Windows Depot Marvel Rivals - High-Resolution Pack
+setManifestid(4406011, "3528527589985225199", 21127923920)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
