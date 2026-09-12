e4b117d5179828f00d5f3ce4aa19aeb136d42297
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 22:46:00 -0400

    restock 2881360

---
+++ 2881360.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2881360.lua
+--Gamename Bloody Heaven 2 ：Prologue
+addappid(2881360) --Mainappid Bloody Heaven 2 ：Prologue
+addappid(2881361, 1, "67207ee9d724a04ee6b9ce469bdd126d8340986fc9eb51a6bf0bc0dc3a82dbca") --Main Windows Depot Bloody Heaven 2 ：Prologue
+setManifestid(2881361, "5704413546149531429", 5184568896)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
