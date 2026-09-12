3b0b8c87a503a9e2885776b5bc30c3172212d86b
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 07:48:01 -0400

    restock 649960

---
+++ 649960.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 649960.lua
+--Gamename Happy Lab
+addappid(649960) --Mainappid Happy Lab
+addappid(649961, 1, "2e8b8d139af63a0c5c67cd512da0dc5c6faaf44552f02383a04fd90b27e5545a") --Main Windows Depot Happy Lab
+setManifestid(649961, "1841645462180459817", 1635669024)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+--Missing Dlcs
+--Dlcname Happy Lab: Cybernetic Carnage
