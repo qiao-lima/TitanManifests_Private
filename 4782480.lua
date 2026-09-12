1f4fd80fe78316dce60181fe4217001b50d106b3
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 05:50:01 -0400

    restock 4782480

---
+++ 4782480.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4782480.lua
+--Gamename 我靠挂机撩女神
+addappid(4782480) --Mainappid 我靠挂机撩女神
+addappid(4782481, 1, "14302ab52f19fef6fb3ae1c5a0f029fe405cc80c2c7dbe65e31f87ec44f0d6d2") --Main Windows Depot 我靠挂机撩女神
+setManifestid(4782481, "5139260246948345362", 2300038848)
