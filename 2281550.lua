dfba0cab96f8e20513ced3d4e42d889feefb1774
titan-cangku <titan-cangku@gamefun365.com>
2026-09-11 22:55:22 -0400

    restock 2281550

---
+++ 2281550.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2281550.lua
+--Gamename JUMP de PON
+addappid(2281550) --Mainappid JUMP de PON
+addappid(2281551, 1, "068ffd13bb345b364dfa6a36c5404eefaf94c317726010b6353ad7e4a38353ea") --Main Windows Depot JUMP de PON
+setManifestid(2281551, "6980496159549138190", 1058001504)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
