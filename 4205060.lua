94c089cd29be3926c35ff688c9698e7064cf569f
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 12:35:07 -0400

    restock 4205060

---
+++ 4205060.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4205060.lua
+--Gamename DopePie Survivors 3D
+addappid(4205060) --Mainappid DopePie Survivors 3D
+addappid(4205061, 1, "29262a67cfb316d8aab36c5446f68274ce8bbae345c728be42d303aae1740711") --Main Windows Depot DopePie Survivors 3D
+setManifestid(4205061, "1619754634972004341", 463287696)
