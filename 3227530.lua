2834ccc9d01b18311399891cb89b761ed2cb25d8
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 01:15:36 -0400

    restock 3227530

---
+++ 3227530.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3227530.lua
+--Gamename 进击之钢
+addappid(3227530) --Mainappid 进击之钢
+addappid(3227531, 1, "c63e341852dd603c848ff91f7ad3b0c8d3d059b31e43816462c4b8c47ac5d0f0") --Main Windows Depot 进击之钢
+setManifestid(3227531, "8376229027583238139", 7080149744)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
