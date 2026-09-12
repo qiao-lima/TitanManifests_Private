5e937500141277aa2fdbdcc3532ef327e1a30475
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 08:50:00 -0400

    restock 4181370

---
+++ 4181370.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4181370.lua
+--Gamename Quack Quack Up
+addappid(4181370) --Mainappid Quack Quack Up
+addappid(4181371, 1, "7199e68f5c1fe5006e81eafd486a1e309e02d86907396448dd9974c6427236bd") --Main Windows Depot Quack Quack Up
+setManifestid(4181371, "982907462467626210", 4077034816)
