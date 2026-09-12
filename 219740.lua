3696542a5aeb8159bca94026f66ac1578759ee5a
qiao-lima <qiao-lima@github.com>
2026-09-13 01:37:41 +0800

    Manual Update 219740

---
+++ 219740.lua
@@ -0,0 +1,45 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 219740.lua
+--Gamename Don't Starve
+addappid(219740, 1, "45a77bdb3f9cee02f970d04ff5e781474ce7b40f9dd34c184b3f206f1033fedc") --Mainappid Don't Starve
+addappid(219741, 1, "e14a49c5eca3b608506ed6251c857f75028f14f1bb229c1718784241f5ed3b56") --Main Windows Depot Don't Starve
+setManifestid(219741, "5671901464708035024", 649517120)
+addappid(219742, 1, "ca18c6f72725b4e463654a3ff73206344cf40198f9bd38644cb769aae4bfca65") --Main Linux Depot Don't Starve
+setManifestid(219742, "3655952163592183953", 534218208)
+addappid(219743, 1, "05f7af6860b79d939e6e6ef7103cfe6d576745ccd84b18013943463f7630be45") --Main Macos Depot Don't Starve
+setManifestid(219743, "38669974034156206", 499815536)
+addappid(219750, 1, "bd5e06ef82387cb13e96da32f246f3b8224950d3b64a2640c475f8ae7627ac7c") --Main Windows Depot Don't Starve
+setManifestid(219750, "6558818855031758829", 579166448)
+addappid(282471, 1, "880e281d471b0ea67fee2a7eef4c91706e88abbcffaf0044f20cb91c3ace1fc6") --Main Windows Depot Don't Starve
+setManifestid(282471, "4580706141887861536", 434138608)
+addappid(282472, 1, "20128d2e67ff35a8bb5651a0714dee695976cf6af01c3e514c8eb082e1f14824") --Main Macos Depot Don't Starve
+setManifestid(282472, "5577712610765689412", 434134720)
+addappid(282473, 1, "504f352b73202a1bde6549d9ad9efd3544ce4ed367f5b63cae0e476d903e6d6a") --Main Linux Depot Don't Starve
+setManifestid(282473, "3751400669884311226", 434134720)
+--Dlcname Don't Starve Soundtrack
+addappid(1248440, 1, "26168de66cddde0536aff5aeee9e8b44c871c60a452fecc4342e5aaa65ab464d") --Dlc Windows Depot Don't Starve Soundtrack
+setManifestid(1248440, "4431599991257542089", 189373824)
+addappid(1248441, 1, "a8ed3012b69c98def96c710b2ae1e2efdd10b3c901099ab7fb30871aec7a460c") --Dlc Windows Depot Don't Starve Soundtrack
+setManifestid(1248441, "7484777413808796146", 967572560)
+--Dlcname Don't Starve: Shipwrecked
+addappid(393010) --Dlcname Don't Starve: Shipwrecked
+addappid(393011, 1, "381fe5734599b91b19765198b18aef0a9b781a70096c46f282ee7bb11e0c8d94") --Dlc Windows Depot Don't Starve: Shipwrecked
+setManifestid(393011, "6965739847178984679", 509601568)
+addappid(393012, 1, "0f67ee25c56a9a67c15eedc895d02f1dd247eef27398e1aca3c27700a43d59de") --Dlc Macos Depot Don't Starve: Shipwrecked
+setManifestid(393012, "806521661209386161", 509600752)
+addappid(393013, 1, "90552e25e88efb289b5f66006fec1b71e0620397d0af81b19045203b8a233db9") --Dlc Linux Depot Don't Starve: Shipwrecked
+setManifestid(393013, "8626671448895671703", 509600752)
+--Dlcname Don't Starve: Hamlet
+addappid(712640) --Dlcname Don't Starve: Hamlet
+addappid(219744, 1, "f007b7d29f8d91c288fc0055baad2b5325ec8edd226abc749c58b017b1d016c0") --Dlc Windows Depot Don't Starve: Hamlet
+setManifestid(219744, "5169458096710886865", 832541264)
+addappid(219745, 1, "32f8ccc409554b5be221400b3a8e910b60aff86ce399f9ac8115b2e6a928b1e5") --Dlc Macos Depot Don't Starve: Hamlet
+setManifestid(219745, "7238648121815552577", 832540720)
+addappid(219746, 1, "ddfbf8d8d402a5474e5498e0bec8639dea48684f9e88c939dddd7245e78e024e") --Dlc Linux Depot Don't Starve: Hamlet
+setManifestid(219746, "5286211488836978777", 832540720)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(239590) --Dlcname AppID 239590
+--Missing Dlcs
+--Dlcname Don't Starve Reign of Giants
