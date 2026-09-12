b31c23b156a41f235a9aa4c9101b391bb41f5053
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 02:44:29 -0400

    restock 2077150

---
+++ 2077150.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2077150.lua
+--Gamename TrainFormation
+addappid(2077150) --Mainappid TrainFormation
+addappid(2077151, 1, "44f47df0eef156841457929ecc89c92385a584048ddbf2f5ca206a651a6254cb") --Main Windows Depot TrainFormation
+setManifestid(2077151, "5862504063145006143", 38231232)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
