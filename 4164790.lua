b4b8a1ab59e6a98928320f819ca454553ebeab46
qiao-lima <qiao-lima@github.com>
2026-09-13 01:37:42 +0800

    Manual Update 4164790

---
+++ 4164790.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4164790.lua
+--Gamename Survival Log
+addappid(4164790) --Mainappid Survival Log
+addappid(4164791, 1, "ea042cfee2f842c31ecd3da8061c7dcd086ba70e586991035ef463d0b71ce284") --Main Windows Depot Survival Log
+setManifestid(4164791, "3458602074289408797", 3907806688)
