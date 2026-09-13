40b8b8ac7244b2464488c8e687c621df6c0b622c
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 04:48:17 -0400

    restock 3625600

---
+++ 3625600.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3625600.lua
+--Gamename 情圣 The Magnificent Scoundrels
+addappid(3625600) --Mainappid 情圣 The Magnificent Scoundrels
+addappid(3625601, 1, "a80e9cc58abd1c5a3b159a1882642265216aed0480f1067b011e1a5b3c94a194") --Main Windows Depot 情圣 The Magnificent Scoundrels
+setManifestid(3625601, "3122385229162364191", 728618512)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
