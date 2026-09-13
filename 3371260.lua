f7e7c09986230f37a314a28e394917bbf0b31a45
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 13:40:11 -0400

    restock 3371260

---
+++ 3371260.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3371260.lua
+--Gamename 列乌尼斯的挽歌 Requiem of Reuinis
+addappid(3371260) --Mainappid 列乌尼斯的挽歌 Requiem of Reuinis
+addappid(3371261, 1, "da7e7fe5ea749c63fa506bc3dfb78157189b8022b062deda23a7c4872d14aa05") --Main Windows Depot 列乌尼斯的挽歌 Requiem of Reuinis
+setManifestid(3371261, "3534378407037440413", 1282068816)
+addappid(3371262, 1, "2872c8932cb74fb8174530fd68210064b7d12fb7ac8d5f99d1f95af720ed0b91") --Main Linux Depot 列乌尼斯的挽歌 Requiem of Reuinis
+setManifestid(3371262, "3391860951873844815", 1268304192)
