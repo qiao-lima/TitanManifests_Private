d0b58e97d1858521883a532b1f61e4597af1a208
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 22:04:47 -0400

    restock 3418500

---
+++ 3418500.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3418500.lua
+--Gamename CS Manager
+addappid(3418500, 1, "069f14875b926ebf3d9ee09a74bcad534762c6789ab54705af609c991490a5b9") --Mainappid CS Manager
+addappid(3418501, 1, "df2fe40942f40cba6dc3b4903b569a308c15be35318f6bfb482fc19f86ddfca3") --Main Windows Depot CS Manager
+setManifestid(3418501, "3729126865431965939", 215310272)
