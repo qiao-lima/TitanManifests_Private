ea23becfa35a6f03f4afe03fd6173719b3bb8db9
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 23:42:28 -0400

    restock 3987560

---
+++ 3987560.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3987560.lua
+--Gamename 盛夏离与合
+addappid(3987560) --Mainappid 盛夏离与合
+addappid(3987561, 1, "089d4263a9fb9154ac369ed37875412140ced19993ee78aee2c660583c821609") --Main Windows Depot 盛夏离与合
+setManifestid(3987561, "8386344170323046790", 15408400368)
+addappid(3987562, 1, "55f28179b88436caaf486a2d60cf63ed10e999135b65cf85dd90c07bb631692e") --Main Macos Depot 盛夏离与合
+setManifestid(3987562, "5100641184940600330", 15332815248)
+--Dlcname 盛夏离与合 - 2K视频
+addappid(4511660) --Dlcname 盛夏离与合 - 2K视频
+addappid(4511661, 1, "7c4b882c5ddff794000c8f3f76b294ddcee3964dca9a8c14eba85963715abf6e") --Dlc Windows Depot 盛夏离与合 - 2K视频
+setManifestid(4511661, "5381341211527061839", 32990770064)
