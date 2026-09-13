0cc073656603427e59a4c826e5da78ff7c9ada94
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 10:35:08 -0400

    restock 2876050

---
+++ 2876050.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2876050.lua
+--Gamename 宠物街金鱼姬 Pet Street Story
+addappid(2876050) --Mainappid 宠物街金鱼姬 Pet Street Story
+addappid(2876051, 1, "b6a4ddbdad0aa82644890d97d25fe694bf9ea33cd07d13dd6de8aa971d94b506") --Main Windows Depot 宠物街金鱼姬 Pet Street Story
+setManifestid(2876051, "5835360565627342475", 499116432)
