e0338cd2e220d57e2bc9202e1816e128d5714a41
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 01:55:07 -0400

    restock 2945430

---
+++ 2945430.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2945430.lua
+--Gamename AppID_2945430
+addappid(2945430) --Mainappid AppID_2945430
+addappid(2945431, 1, "d81c6060cfaaa95f83657d67cd1540b1a0f44dd80062e5b9c1679ce40bc1ca03") --Main Windows Depot AppID_2945430
+setManifestid(2945431, "5188782399614448385", 3060399920)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
