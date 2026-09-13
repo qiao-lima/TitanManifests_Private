0d707ec170133dd8bd33a4ab58664d5130be47c9
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 23:29:29 -0400

    restock 2802550

---
+++ 2802550.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2802550.lua
+--Gamename Godrop:Prologue
+addappid(2802550) --Mainappid Godrop:Prologue
+addappid(2802551, 1, "9eca680d6aa1a30697ae883924d9971dcb0c01887daf2e96c7f767ea65749535") --Main Windows Depot Godrop:Prologue
+setManifestid(2802551, "4098001192863652170", 1035493584)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
