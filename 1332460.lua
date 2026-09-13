c7657c741fc51e96a4aac7e55ecaa68675fbadac
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 10:40:01 -0400

    restock 1332460

---
+++ 1332460.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1332460.lua
+--Gamename 足球梦之队
+addappid(1332460) --Mainappid 足球梦之队
+addappid(1332463, 1, "ac5fbfd85dacb61b52ff6b2549e16819d1a4e935576c19745507f6dd92e8a068") --Main Windows Depot 足球梦之队
+setManifestid(1332463, "1375233455750309225", 698605248)
