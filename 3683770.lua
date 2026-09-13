7e959809a94a1ba060cb79f36ecbc44d042f56d2
qiao-lima <qiao-lima@github.com>
2026-09-14 05:08:56 +0800

    Manual Update 3683770

---
+++ 3683770.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3683770.lua
+--Gamename Boba Cafe Simulator
+addappid(3683770, 1, "806268cd176417e23f4b6cf0384a0398611ff81017d30b394ab10e9ad26b0d96") --Mainappid Boba Cafe Simulator
+addappid(3683771, 1, "33607e45578e4e6c16c8a04b37464bc01872718a1021df09fbc986410192064f") --Main Windows Depot Boba Cafe Simulator
+setManifestid(3683771, "6630289536407132135", 2786260304)
+addappid(3683772, 1, "45f179e1f84c69b6a0056baddf27163c4497c273bf8f1bb55988134d8095fd38") --Main Macos Depot Boba Cafe Simulator
+setManifestid(3683772, "5217593632253144846", 2767957328)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
