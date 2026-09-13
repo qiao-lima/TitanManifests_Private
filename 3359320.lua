6ae4eeac14627a941e8c7438631bbf34d1ee8428
qiao-lima <qiao-lima@github.com>
2026-09-13 17:17:38 +0800

    Manual Update 3359320

---
+++ 3359320.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3359320.lua
+--Gamename SchoolBoy Runaway
+addappid(3359320) --Mainappid SchoolBoy Runaway
+addappid(3359321, 1, "6501fca770ca2ec4e4804a7c5674717d9e5fd6e864f3860cd9624379fafe214c") --Main Windows Depot SchoolBoy Runaway
+setManifestid(3359321, "8618726351839309504", 270146288)
