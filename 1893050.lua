733a1f070d824db6e7940d96a5dd9fbad80627d2
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 04:20:57 -0400

    restock 1893050

---
+++ 1893050.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1893050.lua
+--Gamename DOOM TOMB
+addappid(1893050) --Mainappid DOOM TOMB
+addappid(1893051, 1, "7d0a00417db3f92d8fb4230a188440ebb906aa2ee8c40a3986ca4346c7a2500a") --Main Windows Depot DOOM TOMB
+setManifestid(1893051, "5723676346424582484", 1746242400)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
