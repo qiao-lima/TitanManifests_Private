0ec33e766e30343814bbec62154a5bd1f560cbad
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 02:39:44 -0400

    restock 269450

---
+++ 269450.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 269450.lua
+--Gamename MindTex
+addappid(269450) --Mainappid MindTex
+addappid(269451, 1, "67c90a5db10777b6811f20d085a85efe62d65739369cb98a121e4178645963c1") --Main Windows Depot MindTex
+setManifestid(269451, "7387945685665025158", 0)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229002, "7260605429366465749", 50365280)
