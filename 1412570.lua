b0fde674c19ac69397f73a2af7259b34df9ac860
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 11:00:11 -0400

    restock 1412570

---
+++ 1412570.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1412570.lua
+--Gamename Monster Hunter Stories 2: Wings of Ruin Trial Version
+addappid(1412570) --Mainappid Monster Hunter Stories 2: Wings of Ruin Trial Version
+addappid(1412571, 1, "799764b28555acc75b6ec29d4a9b1c07777be1c374588fd5b8f6c28dd6b1e472") --Main Windows Depot Monster Hunter Stories 2: Wings of Ruin Trial Version
+setManifestid(1412571, "1473680064430459790", 3419116752)
+addappid(1412572, 1, "a3d9622bcd55e6fccbeeddd75163a3de84fab9c5915064f9e3eaf571f98b21c1") --Main Windows Depot Monster Hunter Stories 2: Wings of Ruin Trial Version
+setManifestid(1412572, "1537707373876134806", 0)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
