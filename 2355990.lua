ac45d1353a0a3b3706431e391f50584bec0e3f1f
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 07:32:37 -0400

    restock 2355990

---
+++ 2355990.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2355990.lua
+--Gamename Dippy & Chippy
+addappid(2355990) --Mainappid Dippy & Chippy
+addappid(2355991, 1, "77894f98a389476c5d7b3673dea3446be533e5ba223a994fbf43e9e2c1520790") --Main Windows Depot Dippy & Chippy
+setManifestid(2355991, "4709448940259303008", 2304906128)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
