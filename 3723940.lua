7804b3785568d5a5ddd47634033f42e01979a955
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 03:50:01 -0400

    restock 3723940

---
+++ 3723940.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3723940.lua
+--Gamename Stellar: Chroma Codex
+addappid(3723940) --Mainappid Stellar: Chroma Codex
+addappid(3723941, 1, "5ed15aaa4945e8220544699b73e29e4adbb9a2451c8a10c174094f143d2f70f0") --Main Windows Depot Stellar: Chroma Codex
+setManifestid(3723941, "1122432931945934528", 454038960)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
