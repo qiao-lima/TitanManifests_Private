58acc80d634c6607740c3a4b2b798ef82c281aa3
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 22:20:11 -0400

    restock 2944760

---
+++ 2944760.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2944760.lua
+--Gamename 我的时光：大学日记
+addappid(2944760) --Mainappid 我的时光：大学日记
+addappid(2944761, 1, "a5118af907526fed4d61309dae0336ac29ef41b29149a20392dc76dac759e46c") --Main Windows Depot 我的时光：大学日记
+setManifestid(2944761, "6674725746030210390", 3685364160)
