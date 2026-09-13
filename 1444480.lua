47167db9c3e8fdd975421cf6784c88d0abd3d61a
qiao-lima <qiao-lima@github.com>
2026-09-14 05:08:55 +0800

    Manual Update 1444480

---
+++ 1444480.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1444480.lua
+--Gamename Turing Complete
+addappid(1444480, 1, "8cd3e45bc7e232089e72b4df23013379c7581e9a3881d63181af999571fe7edb") --Mainappid Turing Complete
+addappid(1444482, 1, "2798d6c44cff7735a42b3ff85e5959d94dad076aee2feb1225d89f2430abc641") --Main Macos Depot Turing Complete
+setManifestid(1444482, "6377048052075704628", 242452368)
+addappid(1444483, 1, "e6d81997bd89c62ece287dcead19fb09666b7eb38fba61dea68017574de79fdd") --Main Windows Depot Turing Complete
+setManifestid(1444483, "975552553162858231", 243972224)
+addappid(1444484, 1, "7b51d8c87bac99437d6575f8b0056332bb667d27c390e94c9c029098f931b316") --Main Linux Depot Turing Complete
+setManifestid(1444484, "8866814711490779576", 246279520)
+--Missing Dlcs
+--Dlcname Superscalar Symphony - Turing Complete Soundtrack
