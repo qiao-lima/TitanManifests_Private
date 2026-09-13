b5ea0c39561ed1bb379fb237e5bf5b2fa2b95d4a
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 09:35:01 -0400

    restock 1850070

---
+++ 1850070.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1850070.lua
+--Gamename Fireteam
+addappid(1850070) --Mainappid Fireteam
+addappid(1850071, 1, "dbcd95c34ed21e2e1c6d112797fc634dd0d123c7eca91dfe433ca10b093577d8") --Main Windows Depot Fireteam
+setManifestid(1850071, "3351295239693970713", 3628192176)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
