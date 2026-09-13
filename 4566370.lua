c1b9b57c34446f4c29ba09730deb5add731e53b0
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 02:23:20 -0400

    restock 4566370

---
+++ 4566370.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4566370.lua
+--Gamename 空跃之旅：像素天穹（SKY LEAP PIXEL SKY)
+addappid(4566370) --Mainappid 空跃之旅：像素天穹（SKY LEAP PIXEL SKY)
+addtoken(4566370, "2949331257172662196")
+addappid(4566371, 1, "358bbc553db99183c4959b7931374ad83b75731a7faeb1d7ed40e08b4cfd3f6c") --Main Windows Depot 空跃之旅：像素天穹（SKY LEAP PIXEL SKY)
+setManifestid(4566371, "7234879337468573460", 663600496)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
