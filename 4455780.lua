a5555c9c7cc9fd8487329dcb678f5f25f8e7e4f3
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 02:43:56 -0400

    restock 4455780

---
+++ 4455780.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4455780.lua
+--Gamename Moonlight Dialogue
+addappid(4455780) --Mainappid Moonlight Dialogue
+addappid(4455781, 1, "003beac1df5d848e4330693cdce8a124b3abe8dbf5b2823b011ca6fbba46fdea") --Main Windows Depot Moonlight Dialogue
+setManifestid(4455781, "746093024230847606", 6667493168)
