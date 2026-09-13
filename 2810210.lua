c8f723fd395a7c6a277cc7d5284864fe9a777448
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 02:39:58 -0400

    restock 2810210

---
+++ 2810210.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2810210.lua
+--Gamename Codename CURE II
+addappid(2810210) --Mainappid Codename CURE II
+addappid(2810211, 1, "44913761ae63c85e2d8120a74224831cebeddd76da0b01b9db0219d1ddb79223") --Main Windows Depot Codename CURE II
+setManifestid(2810211, "6520824919548350352", 5288406176)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4605390) --Dlcname Codename CURE II – Combat Specialist Pack
+--Missing Dlcs
+--Dlcname Codename CURE II Soundtrack
