b5027828f5e5526bb481fa3f8656507d8427f17c
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 00:09:09 -0400

    restock 298000

---
+++ 298000.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 298000.lua
+--Gamename MotoGP™14 Demo
+addappid(298000) --Mainappid MotoGP™14 Demo
+addappid(298001, 1, "63cf9d65ebb00630f32f23d611c8b8c2aa175586eed36fd0d640b8b3641e66b2") --Main Windows Depot MotoGP™14 Demo
+setManifestid(298001, "5088232106891683372", 0)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
