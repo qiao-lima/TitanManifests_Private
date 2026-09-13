6bf0e6a9d2b2004d09b80cd7e7d69684d57d9bb7
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 09:50:01 -0400

    restock 1691510

---
+++ 1691510.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1691510.lua
+--Gamename Monster Wilds
+addappid(1691510) --Mainappid Monster Wilds
+addappid(1691511, 1, "d5d7391bc1200ee409187fe041d12b069f278f6de697801f4a9a1e08ec0312be") --Main Windows Depot Monster Wilds
+setManifestid(1691511, "6507401520288533501", 1118823264)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
