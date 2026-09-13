e5ee5b6c78fafc6b88674aa94d8b6cd59e09654d
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 02:43:51 -0400

    restock 4405880

---
+++ 4405880.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4405880.lua
+--Gamename 月之冕（Moon Corona）
+addappid(4405880, 1, "ee114406143909123cda717c2a19b4d3ae98591825f1450f2feafbbce1b07ae5") --Mainappid 月之冕（Moon Corona）
+addappid(4405881, 1, "44a1ba7045147107bb5a025bb221e5014e87fa37634d75a03008d3f6afcfced3") --Main Windows Depot 月之冕（Moon Corona）
+setManifestid(4405881, "2204714437977582510", 7939495424)
