6841f7e813fba3b4e0ae38c71c79e28bf5a43c3e
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 10:02:11 -0400

    restock 1971370

---
+++ 1971370.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1971370.lua
+--Gamename Assault Wing
+addappid(1971370) --Mainappid Assault Wing
+addappid(1971371, 1, "8689d6e653ada59cbadb5a79481db897adbab80d0cab6b95ed91494f2d9bc5c4") --Main Windows Depot Assault Wing
+setManifestid(1971371, "1227476288834375053", 42266000)
+addappid(1971372, 1, "7920567e2df6f21a602db2f0a39be5eb14c925ffe61d168eb80b0cf9456d09b8") --Main Windows Depot Assault Wing
+setManifestid(1971372, "674090611275795568", 28446512)
+addappid(1971373, 1, "fdb85db9182230da39c07c37854617229cc96db4be3915902174c6be9eb3365c") --Main Linux Depot Assault Wing
+setManifestid(1971373, "2674502833365950631", 28515504)
+addappid(1971374, 1, "f89094b0a31c6233562c940c2e73e497ec14e29c3206a4c70376ca836e51e910") --Main Macos Depot Assault Wing
+setManifestid(1971374, "3474630115714908315", 28890752)
