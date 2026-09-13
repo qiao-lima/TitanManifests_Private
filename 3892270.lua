e2787a59015648fa5d73166ed1a50ffb5255afa6
qiao-lima <qiao-lima@github.com>
2026-09-14 05:08:57 +0800

    Manual Update 3892270

---
+++ 3892270.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3892270.lua
+--Gamename Gamble With Your Friends
+addappid(3892270, 1, "792fb0e3dedf02f8756774da38656b5b7212250cf8e33822e6b27199b2530385") --Mainappid Gamble With Your Friends
+addappid(3892271, 1, "90695be6010d5ec2e8890f1d742357316f4d5f74a8a9d22b2ae076b184797167") --Main Windows Depot Gamble With Your Friends
+setManifestid(3892271, "4322338465787944513", 857538992)
+--Dlcname Gamble With Your Friends Soundtrack
+addappid(4635620) --Dlcname Gamble With Your Friends Soundtrack
+addappid(4635621, 1, "4c0749898c675ec914ba65ee29f75477aaeeb2b2d4a05443bc85e7f7f56baf67") --Dlc Windows Depot Gamble With Your Friends Soundtrack
+setManifestid(4635621, "5530338252013096819", 79932864)
+addappid(4635622, 1, "810eb0a9536df12226435abfc5968e158ca4a2ad011a5b9242727ae0f015bd53") --Dlc Windows Depot Gamble With Your Friends Soundtrack
+setManifestid(4635622, "6162677515757785595", 850818080)
