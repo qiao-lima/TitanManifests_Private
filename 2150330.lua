8cec2b6efa6f25476eb42eda8c7442128b05ad36
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 05:10:02 -0400

    restock 2150330

---
+++ 2150330.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2150330.lua
+--Gamename Rush Rally 3 Demo
+addappid(2150330) --Mainappid Rush Rally 3 Demo
+addappid(2150331, 1, "6b3f3289d50974c378b50b85762790f478fe9443d92ca9023683a3149f26181b") --Main Windows Depot Rush Rally 3 Demo
+setManifestid(2150331, "4872237429766676831", 124058384)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
