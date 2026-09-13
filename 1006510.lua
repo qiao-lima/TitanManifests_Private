8902b43fadd66051e57232705e4c9a358d1fce72
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 11:17:16 -0400

    restock 1006510

---
+++ 1006510.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1006510.lua
+--Gamename Devotion
+addappid(1006510) --Mainappid Devotion
+addappid(1006511, 1, "b0515ee5a374e6f06d1e93799d0bbec8efef4f9a2d74b40ffe98a8a07d710489") --Main Windows Depot Devotion
+setManifestid(1006511, "1295878910028272212", 3580537168)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
