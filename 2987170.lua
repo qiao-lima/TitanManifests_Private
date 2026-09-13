5722cd78feb5fed405e0c580b01fce8d02285792
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 23:25:01 -0400

    restock 2987170

---
+++ 2987170.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2987170.lua
+--Gamename WolfFang SkullFang Saturn Tribute Boosted
+addappid(2987170) --Mainappid WolfFang SkullFang Saturn Tribute Boosted
+addappid(2987171, 1, "b5ab2bf632336e459931fdc32e1ee2cb56cfb66b40d65c954ad881eb5cb582b9") --Main Windows Depot WolfFang SkullFang Saturn Tribute Boosted
+setManifestid(2987171, "4312135124189699031", 716347520)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
