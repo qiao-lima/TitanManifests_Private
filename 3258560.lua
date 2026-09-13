46e398e9c615ec79ebf7f848856d3c3bfe6061c0
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 22:20:16 -0400

    restock 3258560

---
+++ 3258560.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3258560.lua
+--Gamename 我不是魔王 I'm No Demonlord
+addappid(3258560, 1, "188b857d27c42321ac1ceb516e1ca3c1b7cd45dbc4ba95875a133576d8a14705") --Mainappid 我不是魔王 I'm No Demonlord
+addappid(3258561, 1, "28db195283cf08c37cb680cde5aa6881366edef903967a1b3515c88d9e7599f8") --Main Windows Depot 我不是魔王 I'm No Demonlord
+setManifestid(3258561, "2775273535577891555", 252727712)
+addappid(3258562, 1, "9ff76b0b370ecfaab48d56fb4a5f5b99254894ac796744cc1dac4789f020c6e2") --Main Linux Depot 我不是魔王 I'm No Demonlord
+setManifestid(3258562, "7790153277488094367", 246946112)
+addappid(3258563, 1, "cadad8473195b0de7c0e84b8e8103783f614bbbddf0cdc6480ac82d77502e056") --Main Macos Depot 我不是魔王 I'm No Demonlord
+setManifestid(3258563, "7977977059395351450", 235866640)
