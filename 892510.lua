8998146247c0d425fc2c286b9af3379c6be13923
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 01:34:36 -0400

    restock 892510

---
+++ 892510.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 892510.lua
+--Gamename Slay Together
+addappid(892510) --Mainappid Slay Together
+addappid(892512, 1, "c3a4573e7cc93fa1c91946534ad9496c1b293a52f3fd79e14ebcdd62a88a6329") --Main Windows Depot Slay Together
+setManifestid(892512, "5670464388685177254", 2098647344)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1033920) --Dlcname AppID 1033920
