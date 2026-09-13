978163da4944f8ff6a3b4d9de88a658b43bbb3b3
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 14:35:28 -0400

    restock 1847210

---
+++ 1847210.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1847210.lua
+--Gamename 中国传统武术 八卦掌 六十四手 Chinese martial arts (kungfu) The 64 Hands of Bagua Zhang
+addappid(1847210) --Mainappid 中国传统武术 八卦掌 六十四手 Chinese martial arts (kungfu) The 64 Hands of Bagua Zhang
+addappid(1847211, 1, "d0bc05c9a39a3f820a9535d0400c721feb642bd18450340c5d1fad27e5a545c2") --Main Windows Depot 中国传统武术 八卦掌 六十四手 Chinese martial arts (kungfu) The 64 Hands of Bagua Zhang
+setManifestid(1847211, "516909153351782256", 999971552)
+addappid(1847212, 1, "1ddcf4da0d0ebd89e30a30dd38e0fb636fb20ae7ac5abe20fb62f7649ff986b1") --Main Windows Depot 中国传统武术 八卦掌 六十四手 Chinese martial arts (kungfu) The 64 Hands of Bagua Zhang
+setManifestid(1847212, "207382213114003104", 838047344)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
