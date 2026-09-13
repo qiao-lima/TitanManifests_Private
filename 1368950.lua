7ccc7fd1cd1aecf608910850b49166b690804bbf
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 02:10:12 -0400

    restock 1368950

---
+++ 1368950.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1368950.lua
+--Gamename VTuber Maker
+addappid(1368950) --Mainappid VTuber Maker
+addappid(1368951, 1, "0676677d0c97e4daf341e9bf63d334597e819d1c956825feb7feb5a5699e620e") --Main Windows Depot VTuber Maker
+setManifestid(1368951, "1481874297801104589", 224381552)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229002, "7260605429366465749", 50365280)
