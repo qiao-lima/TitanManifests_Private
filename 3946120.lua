769ae5b4ae9a4a1afdf99f2fd989319ea0a5a8f9
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 20:45:01 -0400

    restock 3946120

---
+++ 3946120.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3946120.lua
+--Gamename 黎明探险队 / CTD
+addappid(3946120) --Mainappid 黎明探险队 / CTD
+addappid(3946121, 1, "641141823620508d6f3a913bcde25af6e65a842a3801372e7a8003cf3929c2ab") --Main Windows Depot 黎明探险队 / CTD
+setManifestid(3946121, "5664230385343951154", 188577552)
