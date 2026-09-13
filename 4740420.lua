0e7e7ce4a7c94015485b58184f694a988b9778c1
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 23:45:37 -0400

    restock 4740420

---
+++ 4740420.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4740420.lua
+--Gamename 雪落情书时
+addappid(4740420) --Mainappid 雪落情书时
+addappid(4740421, 1, "9c3769392113b7bbc10b996008a20c40b7593f6e32c1fcc93c70a31c9a0fa3f7") --Main Windows Depot 雪落情书时
+setManifestid(4740421, "7476518090904883189", 83959376)
+addappid(4740422, 1, "b8f9acb99d15abcbdd8d2c340f2c92aab17e7dcb4d84224fc83727d7ba88414e") --Main Windows Depot 雪落情书时
+setManifestid(4740422, "2354052098186628072", 1298387280)
+addappid(4740423, 1, "c506242cfa2a41e5a88fa837071203e787da6883ae31d8b89be7f2f6b3964524") --Main Windows Depot 雪落情书时
+setManifestid(4740423, "5963240667630131531", 1960432)
+addappid(4740424, 1, "772a1f25e52f8ac8ea009b020e56af8d21d853c4ff93944fe9c4bfff3f3ac36f") --Main Windows Depot 雪落情书时
+setManifestid(4740424, "6906705178646478155", 23334992464)
