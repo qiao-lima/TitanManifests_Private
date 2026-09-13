5b40ddd0bd979faff4eec9442be3497b9764ca82
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 04:48:12 -0400

    restock 4314010

---
+++ 4314010.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4314010.lua
+--Gamename Command by Voice: World Conquest
+addappid(4314010, 1, "1a53566d2a52866b61086e468622c4ed821ae5251290dd9969ea9855fa8004f6") --Mainappid Command by Voice: World Conquest
+addappid(4314011, 1, "658690def043382c12787ccae8acc3841d1801e746f996e2aa81f1125ec34fd5") --Main Windows Depot Command by Voice: World Conquest
+setManifestid(4314011, "7860935818768681094", 690184144)
