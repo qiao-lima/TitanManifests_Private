959fde04d65910d429a3f59a82f3695545ef6055
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 08:30:25 -0400

    restock 1701370

---
+++ 1701370.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1701370.lua
+--Gamename Practisim Designer
+addappid(1701370) --Mainappid Practisim Designer
+addappid(1701371, 1, "c534a1112736677ea840f7a94f59bc00bdba3f9c4690436ddbf0133a59e95ad2") --Main Windows Depot Practisim Designer
+setManifestid(1701371, "7627789730228348893", 3640689520)
+addappid(1701373, 1, "7beab312a641e3c5a41d01f6c452922f8982f1ea19b0905c13bf25aed7f65ca7") --Main Macos Language Depot English Practisim Designer
+setManifestid(1701373, "1655645312671384683", 3591021760)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
