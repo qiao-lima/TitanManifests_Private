5e330cf360761e830508533f110f1cffc1388217
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 02:36:50 -0400

    restock 4843190

---
+++ 4843190.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4843190.lua
+--Gamename Know Lychees / 来高州识荔枝
+addappid(4843190) --Mainappid Know Lychees / 来高州识荔枝
+addappid(4843191, 1, "2d6f0dbe0b08443b9b433fc7dfb2990000eb52d7a934dcd19f556be0f4c712de") --Main Windows Language Depot Schinese Know Lychees / 来高州识荔枝
+setManifestid(4843191, "193788920999031829", 353584624)
