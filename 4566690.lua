334d4ca75785668abdac8f04cf57ec94a5961c0c
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 03:15:12 -0400

    restock 4566690

---
+++ 4566690.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4566690.lua
+--Gamename 180°
+addappid(4566690) --Mainappid 180°
+addappid(4566691, 1, "19488e758398d3328156a3ff0420898f0b664357b1806699a0afbec462a8b7cf") --Main Windows Depot 180°
+setManifestid(4566691, "1431271398508086262", 120483136)
