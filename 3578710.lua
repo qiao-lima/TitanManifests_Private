bb98cf57a7571ed5ba17ed0835d72ca0bbd49d01
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 14:40:01 -0400

    restock 3578710

---
+++ 3578710.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3578710.lua
+--Gamename 第二中学-中国式校园-Chinese style school
+addappid(3578710) --Mainappid 第二中学-中国式校园-Chinese style school
+addappid(3578711, 1, "1e97c84479256425638d86f622caadf18ae6eb7304fcd60fcb788b9bdd761cd3") --Main Windows Depot 第二中学-中国式校园-Chinese style school
+setManifestid(3578711, "181276657397613044", 9417859248)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
