ac4f9c2ed4876238d3622ff883c8393d3fa9324e
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 10:37:02 -0400

    restock 4789880

---
+++ 4789880.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4789880.lua
+--Gamename SlackOffDiary
+addappid(4789880) --Mainappid SlackOffDiary
+addappid(4789881, 1, "e07598f089850e36f7ab181e7aa402595656d5d447c2d6f7e044ec48567242db") --Main Windows Depot SlackOffDiary
+setManifestid(4789881, "465150303563785870", 176660144)
