2d0680f4b733c2b7653e690aca5ef42b0f272b02
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 00:26:27 -0400

    restock 1536790

---
+++ 1536790.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1536790.lua
+--Gamename GIANTESS PLAYGROUND
+addappid(1536790) --Mainappid GIANTESS PLAYGROUND
+addappid(1536791, 1, "d4dab14b7446b3eb1c13333501a77d290f43d6fdce4f7ba44aff5944c495c802") --Main Windows Depot GIANTESS PLAYGROUND
+setManifestid(1536791, "1794652534229769903", 17764743568)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
