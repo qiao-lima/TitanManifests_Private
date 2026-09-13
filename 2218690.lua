f8e930f628f5cd9a499480d80746083eb51407a3
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 09:49:54 -0400

    restock 2218690

---
+++ 2218690.lua
@@ -0,0 +1,19 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2218690.lua
+--Gamename Monster Hide
+addappid(2218690) --Mainappid Monster Hide
+addappid(2218691, 1, "2d167273098375cc53fc16efa1ee1048d91799117cb6b9dfb1e73585fa33a5a6") --Main Windows Depot Monster Hide
+setManifestid(2218691, "6852296697813592986", 5070701632)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2312590) --Dlcname AppID 2312590
+addappid(2533200) --Dlcname AppID 2533200
+addappid(2533450) --Dlcname AppID 2533450
+addappid(2538660) --Dlcname AppID 2538660
+addappid(2541790) --Dlcname AppID 2541790
+addappid(2680710) --Dlcname AppID 2680710
+addappid(2684350) --Dlcname AppID 2684350
+addappid(2693520) --Dlcname AppID 2693520
