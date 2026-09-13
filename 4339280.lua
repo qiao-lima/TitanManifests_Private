29b484be113e8df20ba07e68a37642cca7f43ccf
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 23:30:33 -0400

    restock 4339280

---
+++ 4339280.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4339280.lua
+--Gamename ShipShaper: Falconeer Chronicles
+addappid(4339280) --Mainappid ShipShaper: Falconeer Chronicles
+addappid(4339281, 1, "11da066eec2561b1c9f265b85822d9978a169517cfdfb2d79779d778318d20bc") --Main Windows Depot ShipShaper: Falconeer Chronicles
+setManifestid(4339281, "8429164117961962814", 1520872176)
