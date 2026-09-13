c15c2bfc06139f214c8342425a38c1b056b69754
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 02:54:26 -0400

    restock 3830780

---
+++ 3830780.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3830780.lua
+--Gamename 下一段路途The Next Journey Of Life
+addappid(3830780) --Mainappid 下一段路途The Next Journey Of Life
+addappid(3830781, 1, "aac1ae7948aaa6b0c415b03f68b41415f960c7f5122ed0056f08db922c482386") --Main Windows Depot 下一段路途The Next Journey Of Life
+setManifestid(3830781, "229986350194055420", 13565150160)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
