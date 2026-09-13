275340a69600f69de5dbbe1c7f4dd1673e32748b
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 04:59:23 -0400

    restock 4877980

---
+++ 4877980.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4877980.lua
+--Gamename Saikey Assets Manager
+addappid(4877980) --Mainappid Saikey Assets Manager
+addappid(4877981, 1, "d13abb937ac06aeb5c58577a91188cae344ccdf2e7fd4dfa473f729884bbcb86") --Main Windows Depot Saikey Assets Manager
+setManifestid(4877981, "2826395019824107404", 90620192)
+--Missing Dlcs
+--Dlcname Saikey Assets - [R18] Fantasy Girls - Character Sprite Asset Pack
