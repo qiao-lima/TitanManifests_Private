8c51a2fc4d67aed9ec02e50616826312544ee1f5
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 20:40:09 -0400

    restock 820210

---
+++ 820210.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 820210.lua
+--Gamename Ashi Wash
+addappid(820210) --Mainappid Ashi Wash
+addappid(820211, 1, "f4ce5e3bc35d4ee61d771aafe2296740f49332650c5c13b22fd210106957f0ac") --Main Windows Depot Ashi Wash
+setManifestid(820211, "4851721598590959133", 360234784)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
