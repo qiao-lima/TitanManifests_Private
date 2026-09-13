4bb95bb993dd9c829d8a3a69072434f303da5b0a
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 19:00:05 -0400

    restock 3739300

---
+++ 3739300.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3739300.lua
+--Gamename Cat Squeeze
+addappid(3739300) --Mainappid Cat Squeeze
+addappid(3739301, 1, "84cb22a184589661b422df31519419c41cdc38e540782595e5412b369052fc16") --Main Windows Depot Cat Squeeze
+setManifestid(3739301, "1628606783438777767", 240899200)
+addappid(3739302, 1, "730e912215156d2d3b0c3a0bf62aa75975d8099df01781f90dca82df388804a7") --Main Macos Depot Cat Squeeze
+setManifestid(3739302, "7251571053600537921", 244909232)
+addappid(3739303, 1, "8cf795a1fad922ae18653be9f0058737cc66b2ac43020334b7254ebce19094e4") --Main Linux Depot Cat Squeeze
+setManifestid(3739303, "4571385720196345510", 231355856)
