3bb1a8f350984361eff093a99d9771ff4a95fa06
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 23:09:16 -0400

    restock 4331100

---
+++ 4331100.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4331100.lua
+--Gamename Warena
+addappid(4331100) --Mainappid Warena
+addappid(4331101, 1, "8cf572877d8ed03b92ef858b8223e423bf594be335985d8b6e07a397668f7d11") --Main Windows Depot Warena
+setManifestid(4331101, "2147565890690849567", 174951024)
