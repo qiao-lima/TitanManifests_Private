936f0e2aeacca7873f069982b7145fe2090342c9
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 02:39:53 -0400

    restock 2745930

---
+++ 2745930.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2745930.lua
+--Gamename MindKatt:First Strike
+addappid(2745930) --Mainappid MindKatt:First Strike
+addappid(2745931, 1, "c981946192598ad0652ead9f0e64b8ffdd87fbc61bd55d6d96f18d5e02bfa9e7") --Main Windows Depot MindKatt:First Strike
+setManifestid(2745931, "3768754647069598898", 2911581840)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
