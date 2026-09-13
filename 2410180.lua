51d6110bc165e9c3ee5f5be8229c74165cd77d34
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 12:38:28 -0400

    restock 2410180

---
+++ 2410180.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2410180.lua
+--Gamename Portal: Prelude RTX
+addappid(2410180, 1, "e9e03a23877321ada79865b5197deb01479c9ccaa4f94ac67b9f027814464c05") --Mainappid Portal: Prelude RTX
+addappid(2410181, 1, "5f0784a26fa1f9d8ceeda65d3883465bfb0092ca66033826ff694aca0afc7595") --Main Windows Depot Portal: Prelude RTX
+setManifestid(2410181, "4949530312671311881", 20975312624)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
