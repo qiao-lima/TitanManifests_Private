ada5726f6ab64a135b9fe940fb79c6fb29a666a2
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 09:32:23 -0400

    restock 1237840

---
+++ 1237840.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1237840.lua
+--Gamename Blood Relation Part1.
+addappid(1237840) --Mainappid Blood Relation Part1.
+addappid(1237841, 1, "aef63fa73514dcc6e352e58ae81126affbe8276436b4e54bf82bee38df57d08e") --Main Windows Depot Blood Relation Part1.
+setManifestid(1237841, "5380840205965883554", 2665364160)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
