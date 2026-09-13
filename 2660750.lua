6adad8c690c447424904933539d14bb043b64bfc
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 02:35:45 -0400

    restock 2660750

---
+++ 2660750.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2660750.lua
+--Gamename Teku
+addappid(2660750) --Mainappid Teku
+addappid(2660751, 1, "12916283fd1eb75e3cd6f0851c24e90543786653a848986f1691a7ff6c6dc99d") --Main Windows Depot Teku
+setManifestid(2660751, "9165876658874293746", 88760272)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
