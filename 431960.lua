2b88690156192295091ebf1a170b973274d2795c
qiao-lima <qiao-lima@github.com>
2026-09-14 05:08:58 +0800

    Manual Update 431960

---
+++ 431960.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 431960.lua
+--Gamename Wallpaper Engine
+addappid(431960, 1, "db983bc58a8cb46d89d1d70cc962cc733956e6124c4025a0c932f82b3c37144a") --Mainappid Wallpaper Engine
+addappid(431961, 1, "62ac71786b9fbeecc473fccb8a14da04ea676371addd8820638ee6cddac8c344") --Main Windows Depot Wallpaper Engine
+setManifestid(431961, "3687007859740550172", 377252704)
+addappid(431966, 1, "3217352ccc45b8943d6af58c14d359a38f2897bcf4d2f7f7eb117f79f1887fc4") --Main Windows Depot Wallpaper Engine
+setManifestid(431966, "5369633619945201395", 1737088)
+--Dlcname Wallpaper Engine - Editor Extensions
+addappid(1790230, 1, "28aacfb757c396f07f861f189a2c2ec78653123de2177c05f94a792d1a3c8bfd") --Dlcname Wallpaper Engine - Editor Extensions
+setManifestid(1790230, "4193888996927260780", 2460532448)
