0d636644ff02e2ded3978b6627e34bca37c41e08
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 07:19:27 -0400

    restock 2839050

---
+++ 2839050.lua
@@ -0,0 +1,22 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2839050.lua
+--Gamename Real Life
+addappid(2839050) --Mainappid Real Life
+addappid(2839051, 1, "74b41e5ce521da7cd5075e891981c53df8c606dc9d1411df56a2e97cba2d6c4b") --Main Windows Depot Real Life
+setManifestid(2839051, "14689564137806455", 46313520736)
+--Dlcname Real Life: Soundtrack "Quran" (Full Free)
+addappid(2867990) --Dlcname Real Life: Soundtrack "Quran" (Full Free)
+addappid(2867991, 1, "218a489a4e0e1162f69543f8465104b8277ca6d8f49e0b87a1c4c409ed0312a2") --Dlc Windows Depot Real Life: Soundtrack "Quran" (Full Free)
+setManifestid(2867991, "3286649929498642898", 3126031552)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+--Missing Dlcs
+--Dlcname Real Life: Soundtrack "Quran"
+--Dlcname Real Life Soundtrack "Quran" (1,2 Surahs)
+--Dlcname Real Life Soundtrack "Quran" (3,4,5 Surahs)
+--Dlcname Real Life Soundtrack "Quran" (6,7,8,9 Surahs)
+--Dlcname Real Life Soundtrack "Quran" (10-14 Surahs)
+--Dlcname Real Life Soundtrack "Quran" (15-77 Surahs)
