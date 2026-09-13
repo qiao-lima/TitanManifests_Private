cf86b63a1308c719050e5873e9b2120a3065872e
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 09:53:59 -0400

    restock 1097430

---
+++ 1097430.lua
@@ -0,0 +1,21 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1097430.lua
+--Gamename Book Of Yog Idle RPG 依盖之书
+addappid(1097430) --Mainappid Book Of Yog Idle RPG 依盖之书
+addappid(1097431, 1, "54261c3e89fb233b498eb6600dccae1f874a9996407f4cf30ad3a9754e8ade90") --Main Windows Depot Book Of Yog Idle RPG 依盖之书
+setManifestid(1097431, "3733391343079240505", 1349978640)
+addappid(1097432, 1, "2e4a3efb0a138c1b4f9b3027bd8f14651a1e451710784d013073c2533244430e") --Main Windows Depot Book Of Yog Idle RPG 依盖之书
+setManifestid(1097432, "903370832220483260", 1349826272)
+addappid(1097436, 1, "25e623c862ebb86f27aeb58b3f9de77f426e4754d3b49746aaa2385d7aaa7500") --Main Windows Depot Book Of Yog Idle RPG 依盖之书
+setManifestid(1097436, "776795775574073880", 21130611136)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
+addappid(2694630) --Dlcname 依盖之书-初雪之诞拓展包
+addappid(2872960) --Dlcname 依盖之书-启程血晶拓展包
+addappid(3440690) --Dlcname 依盖之书-悠闲时光拓展包
+addappid(3642190) --Dlcname 依盖之书-冰柠夏季拓展包
+--Missing Dlcs
+--Dlcname 依盖之书-诸圣鬼节扩展包
