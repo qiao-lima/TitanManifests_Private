6da6120d5619d5a21a62ef695e9f5d49820c85cf
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 07:50:07 -0400

    restock 3907350

---
+++ 3907350.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3907350.lua
+--Gamename The Prince of Tennis Sweet School Festival ~♡-40 and more...~
+addappid(3907350, 1, "674526e9deadc64124c653fd99fa8e28bc8c77eae1de02610540e9661bb04b74") --Mainappid The Prince of Tennis Sweet School Festival ~♡-40 and more...~
+addappid(3907351, 1, "5ef2d0adb60eeafddfa771b9f2a3b3b793d875d0ee8cb87ce5d79f136878627c") --Main Windows Depot The Prince of Tennis Sweet School Festival ~♡-40 and more...~
+setManifestid(3907351, "4747469278594307751", 3935821504)
+addappid(4190400) --Dlcname 学園祭の王子様 ミニドラマ用追加エフェクト「赤いバラ」
+addappid(4214740) --Dlcname 学園祭の王子様 ミニドラマ用追加ボイスセット
