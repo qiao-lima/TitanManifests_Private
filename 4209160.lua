2afa2eac4b20f4a7179412e779cde8b25879e573
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 16:16:31 -0400

    restock 4209160

---
+++ 4209160.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4209160.lua
+--Gamename Idyll Isle
+addappid(4209160) --Mainappid Idyll Isle
+addappid(4209161, 1, "778ec8859a11105b4c2843a87114b339c2f293eff88168988b4089b49ca8593e") --Main Windows Depot Idyll Isle
+setManifestid(4209161, "8394095028026703688", 64240656)
