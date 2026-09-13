95d7a5c3c503d82b7fb59f6bc0bbf423a3ea47b3
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 09:33:08 -0400

    restock 250820

---
+++ 250820.lua
@@ -0,0 +1,42 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 250820.lua
+--Gamename SteamVR
+addappid(250820, 1, "2b7e3dbcf0c186d8798830c79cab4c53ab54f1d3525d8e006df3f41b5825a2c9") --Mainappid SteamVR
+addappid(250821, 1, "00ec8a4b573555900cc68693dab8b90beaaf342afe9b21dfd266704b6f83f02f") --Main Windows Depot SteamVR
+setManifestid(250821, "8096773226691355335", 453392592)
+addappid(250822, 1, "ae4275e6bf4f3a8ae4588cfbab25516761c51bdeff913aa82614bb63ab8cf5e6") --Main Macos Depot SteamVR
+setManifestid(250822, "5914501107808599266", 0)
+addappid(250823, 1, "13c4c0c9bb0c0f90f532ea739dc8ef2d6de76244abe6c2ce563838e21015ba56") --Main Linux Depot SteamVR
+setManifestid(250823, "8327791659946528001", 386180240)
+addappid(250824, 1, "235ea3a0904c219552e3064384b27519259ddf6437861df84d5717580bf27166") --Main Windows/Linux Depot SteamVR
+setManifestid(250824, "6419669239042374220", 366537760)
+addappid(250825, 1, "e6d9ce434240a273d90a948ae91e3e3dc377bacede1984b597fad6789c47b604") --Main Windows Depot SteamVR
+setManifestid(250825, "6878307259591782433", 0)
+addappid(250827, 1, "b93b1524f9b31d6b9ca97fb10e05d4bcca9c4006ade419d336ffb065158e9354") --Main Windows/Linux Depot SteamVR
+setManifestid(250827, "4144201462816264829", 3070688)
+addappid(250828, 1, "f1da18663727a54b08c5c6365b5164c84d166cd7f9949500665998fed3415e73") --Main Windows/Linux Depot SteamVR
+setManifestid(250828, "7972087267127211047", 0)
+addappid(250829, 1, "f95cd38c1466f8e16eb9e9bfa44a1373e0b1b2dcd798b035d4ed85a70db9fbf9") --Main Windows/Linux Depot SteamVR
+setManifestid(250829, "8685571343659696296", 0)
+addappid(250830, 1, "5a7a10ea8d2063bdc97b64c976722ad23939d06aa47b5be021fa361d99f289b0") --Main Windows Depot SteamVR
+setManifestid(250830, "1874458903680892192", 1109881680)
+addappid(250831, 1, "4a702760a140bdc4c34652d735e28ab856ef63c40435ed6190c59b8dc63c45f3") --Main Windows Depot SteamVR
+setManifestid(250831, "3374051727602264819", 172356112)
+addappid(250832, 1, "e015685521784c8d38bd6174d542978588b998336d9f81eaed77cce78fe37305") --Main Linux Depot SteamVR
+setManifestid(250832, "3112697343985428860", 221733136)
+addappid(250833, 1, "78916d4412dfcc5376e8b9b05c35d51216f0b8add20680c56b18dee6f763096a") --Main Windows Depot SteamVR
+setManifestid(250833, "1878657507309650488", 91147664)
+--Dlcname AppID 3375720
+addappid(3375720) --Dlcname AppID 3375720
+addappid(250834, 1, "1fb03082dde3708a8972995d90a58f60dfe0e8f8738d011e9605937ec1ba9798") --Dlc Windows Depot AppID 3375720
+setManifestid(250834, "3078284652892312975", 0)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1245120) --Dlcname SteamVR - Half-Life: Alyx Environment
