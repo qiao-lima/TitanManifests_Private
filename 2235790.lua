885db5bdd7b52e121b17352e4d889b750704d844
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 01:35:06 -0400

    restock 2235790

---
+++ 2235790.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2235790.lua
+--Gamename Howl of Iron
+addappid(2235790) --Mainappid Howl of Iron
+addappid(2235791, 1, "f1268ef211d35ce25856c2bc00017323b69348588ce66f442e7e89f6387c0122") --Main Windows Depot Howl of Iron
+setManifestid(2235791, "3302836983533702420", 2896880320)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
