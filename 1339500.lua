c11578bbd90a6aa02a46b69bd0a0fce25f70d452
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 02:00:16 -0400

    restock 1339500

---
+++ 1339500.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1339500.lua
+--Gamename Gym Tycoon
+addappid(1339500) --Mainappid Gym Tycoon
+addappid(1339501, 1, "2cb18fe132495083a8ae13d2d86dd8d18809c98f70bb0422ecce843e6243d54e") --Main Windows Depot Gym Tycoon
+setManifestid(1339501, "251197295180127086", 228045392)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
