03c261d1a8b1e303147553b3ee1692bff5eb3494
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 23:41:34 -0400

    restock 3749320

---
+++ 3749320.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3749320.lua
+--Gamename 花满楼
+addappid(3749320) --Mainappid 花满楼
+addappid(3749321, 1, "e1e2d65e749b73e193dcda27ecf9ea2df6688b3960d74e53c2efedc6401201f2") --Main Windows Depot 花满楼
+setManifestid(3749321, "8412854823941817389", 54047305104)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
