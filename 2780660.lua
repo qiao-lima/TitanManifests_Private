f45e6bad90d4f26b4ea138dbabe72a878861638a
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 09:10:11 -0400

    restock 2780660

---
+++ 2780660.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2780660.lua
+--Gamename MR 333 II: Planetbirth
+addappid(2780660) --Mainappid MR 333 II: Planetbirth
+addappid(2780661, 1, "3d47b506fb1aacdf4abaa8367bb0e4d24961602104a2e71d341a5b015d2e01cc") --Main Windows Depot MR 333 II: Planetbirth
+setManifestid(2780661, "3799527238093999160", 357557360)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
