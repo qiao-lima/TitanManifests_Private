921cbf4a7d7dddd2c2d26c4e31e00751de7820a1
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 20:40:01 -0400

    restock 1114940

---
+++ 1114940.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1114940.lua
+--Gamename I Wanna Maker
+addappid(1114940) --Mainappid I Wanna Maker
+addappid(1114941, 1, "2d564317bf8974cf32f0503055f53da131390812658f4ab5066d2d05e2bb487b") --Main Windows Depot I Wanna Maker
+setManifestid(1114941, "5295932831646496316", 744895008)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
