d8ec786500030c371bc6ccdd9853a2101f417635
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 04:44:21 -0400

    restock 4341360

---
+++ 4341360.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4341360.lua
+--Gamename 嫂子
+addappid(4341360, 1, "2a138ac7c4b95a1640ac9d9b7bf8124b2501e1693bf3be9f96bce43c6568a0d2") --Mainappid 嫂子
+addappid(4341361, 1, "71fa1278a43bbee0c5b42f5fcb90d0041b63f2df790f1682c22c4d620aa42492") --Main Windows Depot 嫂子
+setManifestid(4341361, "607581414928943918", 26024848)
