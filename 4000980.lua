21563e14359b3c42e38dd2fcf292f2500fa002f3
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 02:52:07 -0400

    restock 4000980

---
+++ 4000980.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4000980.lua
+--Gamename 第零封情书
+addappid(4000980, 1, "fe7f20f0f93f0707df30c88a625232fa33802b7364209c34a81cc7a939d261a5") --Mainappid 第零封情书
+addappid(4000981, 1, "73099236aaeb8d8e2eb7b42fed1a369d49838ee6c3d9d2f68209800b38eae941") --Main Windows Depot 第零封情书
+setManifestid(4000981, "1731129886814971453", 1056043392)
+--Missing Dlcs
+--Dlcname 第零封情书 Soundtrack
