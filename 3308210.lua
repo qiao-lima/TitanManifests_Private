861adba2252f40384d5a736153297a6a358f2a99
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 11:17:06 -0400

    restock 3308210

---
+++ 3308210.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3308210.lua
+--Gamename Store Wars: Prologue - Multiplayer Shop Simulator
+addappid(3308210) --Mainappid Store Wars: Prologue - Multiplayer Shop Simulator
+addappid(3308211, 1, "9d75449a536e814829e0f91b9b8b6aa381d50a46585589e81acafdd8eb32a0e9") --Main Windows Depot Store Wars: Prologue - Multiplayer Shop Simulator
+setManifestid(3308211, "6698594265997687436", 5660510496)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
