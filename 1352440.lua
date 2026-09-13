8d466b9f980bc537acc4a469c18727d7c1c365d9
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 06:05:06 -0400

    restock 1352440

---
+++ 1352440.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1352440.lua
+--Gamename Brick Building
+addappid(1352440) --Mainappid Brick Building
+addappid(1352441, 1, "c455c05e071c509698a1b624d3c3b8d639b98f05242efd736e7ac4e41504a13b") --Main Windows Depot Brick Building
+setManifestid(1352441, "5217252769268939511", 645493072)
+addappid(1352442, 1, "9d6f7543e04e0df0b486398cf9e3da95ff84d3c920e3ff902ebb3f7ef0ec2cb2") --Main Macos Depot Brick Building
+setManifestid(1352442, "8158928920366012566", 709115600)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
