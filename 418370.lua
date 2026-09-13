e552ffeef1b22c5dc56fd656829f62e107655c88
qiao-lima <qiao-lima@github.com>
2026-09-14 05:08:57 +0800

    Manual Update 418370

---
+++ 418370.lua
@@ -0,0 +1,92 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 418370.lua
+--Gamename Resident Evil 7 Biohazard
+addappid(418370, 1, "9218e61652ae85ea8525d97fd62535f61209b1dd13e3551a5114592281f01313") --Mainappid Resident Evil 7 Biohazard
+addappid(418371, 1, "2df4ede71de8b8163daa12dbd679f25b89c131c806485d93fba323e47a2d8eb3") --Main Windows Depot Resident Evil 7 Biohazard
+setManifestid(418371, "5688882184765722061", 21933650976)
+addappid(418372, 1, "5ffba65b1aa063765d2dec7582c23b09661a6b361fa9657a2508ab2f5fb15be5") --Main Windows Depot Resident Evil 7 Biohazard
+setManifestid(418372, "4857073177631911945", 21923801440)
+--Dlcname Banned Footage Vol.1
+addappid(529930, 1, "f05d0430d570b49933e26bdf148b52ff2b68676636afc882087ed81b826de455") --Dlcname Banned Footage Vol.1
+addappid(418373, 1, "50c8b866482e9bd8c01be358b98eb68b599e62a0acd6e6e5ddb248a85440fddc") --Dlc Windows Depot Banned Footage Vol.1
+setManifestid(418373, "1850487337258647106", 8847769696)
+setManifestid(529930, "1885029655974643736", 8860282560)
+--Dlcname Survival Pack A
+addappid(530590, 1, "06f0d9d9de44f84a3f255e0a3f500152f680761eb95e3cc2187947df324c0f80") --Dlcname Survival Pack A
+setManifestid(530590, "2821452161378484140", 14896544)
+addtoken(530591, "14219514274219578439")
+addappid(530591, 1, "07761afe199d8438ba8b139f36a92a934b8bf18de5c222eede9a0877a1d2f8bc") --Dlcname AppID 530591
+setManifestid(530591, "8099799231745351555", 14750864)
+addtoken(530592, "137849144737045535")
+addappid(530592, 1, "b877d348297442a4a20326c8eb10445ac7061410396c429e453ef38a1666fb68") --Dlcname AppID 530592
+setManifestid(530592, "5193083539346488075", 14893200)
+addtoken(530593, "9657228074130537484")
+addappid(530593, 1, "6bb179b124231858379b4cb30cf3c12e446e00b432632152e3f17a0352912f2b") --Dlcname AppID 530593
+setManifestid(530593, "9195877253828425260", 15725984)
+addtoken(530594, "3083774502530847806")
+addappid(530594, 1, "f40098295d34d0d2f2522b04dbd43268de116800d0a6b43d5b8a26e9db50c5d5") --Dlcname AppID 530594
+setManifestid(530594, "6472280222538517211", 14356512)
+--Dlcname A Coin & Hard Mode
+addappid(530595, 1, "f532e1f9272faaca708b0d54b4aa86b9b5bfccfb61c1376c6e984e2be6cdee4f") --Dlcname A Coin & Hard Mode
+setManifestid(530595, "4096306864002722519", 12447120)
+--Dlcname B Coin & Hard Mode
+addappid(530596, 1, "05b334a227cbe67d789998bbdd2ab7726b891a1d7deff5349cb4938bc6304487") --Dlcname B Coin & Hard Mode
+setManifestid(530596, "2437844440160457168", 12303968)
+--Dlcname C Coin & Hard Mode
+addappid(530597, 1, "5f791b96cace35bd231db330435ad45b3f3a608805f020a0add4f5c116193f4b") --Dlcname C Coin & Hard Mode
+setManifestid(530597, "6364590930435507576", 12441888)
+--Dlcname D Coin & Hard Mode
+addappid(530598, 1, "01cd6f3745501d469f6193aa99becb7244a4b293031b09365d07e4f419c606d3") --Dlcname D Coin & Hard Mode
+setManifestid(530598, "3874000828551471378", 13283952)
+--Dlcname E Coin & Hard Mode
+addappid(530599, 1, "acc89294725ca72de44fa2e7d060e1479794516963c1aa999c03df609dbd372f") --Dlcname E Coin & Hard Mode
+setManifestid(530599, "2633628331817827169", 11905024)
+--Dlcname Banned Footage Vol.2
+addappid(530610, 1, "cc5848d208a6436651fa7875afd740c71bf799b0cc61e1677f7156ba27178e41") --Dlcname Banned Footage Vol.2
+addappid(418374, 1, "8b767d7724397841d78757c440f109fd2eda309263dfab9fe8d2b44042c93428") --Dlc Windows Depot Banned Footage Vol.2
+setManifestid(418374, "5417000559535840320", 11517102512)
+addappid(418375, 1, "75ef17978e6091765555cc1c2fe83e7886cfe49121baa935e8e0b80b6fe98e3e") --Dlc Windows Depot Banned Footage Vol.2
+setManifestid(418375, "5278038220232861179", 11515681312)
+setManifestid(530610, "8797633140491138315", 11511613856)
+--Dlcname End Of Zoe
+addappid(530611, 1, "32a01ec16f26a30cfbb8a309e864b0f6e22a2af0324100f0561857dcd8964e5d") --Dlcname End Of Zoe
+addappid(418378, 1, "e781d67979d2e4c82368105a246b50a2a60d58975dbb028d391e843f1529ab02") --Dlc Windows Depot End Of Zoe
+setManifestid(418378, "1210212300188934975", 10635535824)
+setManifestid(530611, "6056753254666249273", 10631324928)
+--Dlcname Not A Hero
+addappid(564190, 1, "3e28043753fe610017111a39a7a80aac705b7cfcddb1c420d50f388e6ea0b0b1") --Dlcname Not A Hero
+addappid(418376, 1, "ad6230311dea128611a7f6616e116755175138b3a505a4e371a8a7e8f45ad437") --Dlc Windows Depot Not A Hero
+setManifestid(418376, "1861368551167317490", 5556312016)
+addappid(418377, 1, "2376c48c0daa86e1edabafd112cf1be3c96aad06d1b7df745ec4b8580f4c0775") --Dlc Windows Depot Not A Hero
+setManifestid(418377, "1544223621847730578", 5563237840)
+setManifestid(564190, "30059815315701992", 5564200896)
+--Dlcname Resident Evil 7 biohazard Original Soundtrack
+addappid(583810) --Dlcname Resident Evil 7 biohazard Original Soundtrack
+addappid(583811, 1, "36e7b1016079bfb946e7c59b7677949091a517cd03587caa603dcfc20f17b28e") --Dlc Windows Depot Resident Evil 7 biohazard Original Soundtrack
+setManifestid(583811, "8253221820533898039", 313942896)
+addappid(583812, 1, "3cdde1e07a57728e52fdf3a16629160e3aa1a91141a1c068863a9cbb0d726ed0") --Dlc Windows Depot Resident Evil 7 biohazard Original Soundtrack
+setManifestid(583812, "966416827336040689", 396310656)
+addappid(583813, 1, "06419bb48cfc02165a531e9b917670688fa0f96ec65913c5b435822b7d7e9bb3") --Dlc Windows Depot Resident Evil 7 biohazard Original Soundtrack
+setManifestid(583813, "8960716292059363130", 691776528)
+addappid(583814, 1, "3347d5c4d50c459dd2a6c76a4c9681cf06dcbf4476558efaba51c37490587d06") --Dlc Windows Depot Resident Evil 7 biohazard Original Soundtrack
+setManifestid(583814, "7287749018283110606", 1481879456)
+addappid(583815, 1, "aaa2750460ed1409918a573753cf810acc13c670297d8ea6bd8b63a32c3b0f15") --Dlc Windows Depot Resident Evil 7 biohazard Original Soundtrack
+setManifestid(583815, "1679476974014982504", 399548608)
+addappid(583816, 1, "78ed9f0075ada547c58d79794bd965b4b19627d652196c4e9d1105aa150cff65") --Dlc Windows Language Depot Japanese Resident Evil 7 biohazard Original Soundtrack
+setManifestid(583816, "1715021259447610288", 311903312)
+addappid(583817, 1, "e775b88948beb9658f34d018a3fd48dac5776f4cbc8b2b0a07a19734fa818c09") --Dlc Windows Language Depot Japanese Resident Evil 7 biohazard Original Soundtrack
+setManifestid(583817, "7987493839995406587", 394274784)
+addappid(583818, 1, "f4a1c744ce7a4d5ba7144025a7da2f9151a85884548b737d0f2a397440b6fbe1") --Dlc Windows Language Depot Japanese Resident Evil 7 biohazard Original Soundtrack
+setManifestid(583818, "6399794036426392597", 689735664)
+addappid(583819, 1, "1d8620f6837e7b32f5eece01135b457123c6285f34e1eaa914586e2212a629dd") --Dlc Windows Language Depot Japanese Resident Evil 7 biohazard Original Soundtrack
+setManifestid(583819, "7943629174887838133", 1479839920)
+addappid(1233910, 1, "0d12b58407b173a09f242a91ff4027d998dbc52c93c143fd3f37e83f35163cd6") --Dlc Windows Language Depot Japanese Resident Evil 7 biohazard Original Soundtrack
+setManifestid(1233910, "4494750808554982374", 397509952)
+addappid(584960, 1, "5c48e5a099a9722def363cc8b8166fad713504c956f09cfed1a3aa87830e7d6d") --Dlcname AppID 584960
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(540340) --Dlcname Resident Evil 7 - Season Pass
+addappid(543550) --Dlcname AppID 543550
