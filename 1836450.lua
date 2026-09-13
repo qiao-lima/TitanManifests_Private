1cde117f740a333bcccdf75f6eccfa3b20199680
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 11:00:06 -0400

    restock 1836450

---
+++ 1836450.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1836450.lua
+--Gamename Monster Hunter Rise: Sunbreak Demo
+addappid(1836450) --Mainappid Monster Hunter Rise: Sunbreak Demo
+addappid(1836451, 1, "c19ddc833f834536776ad2aee3c707d6a35f1c3e389bb09efc8689f7ed131c09") --Main Windows Depot Monster Hunter Rise: Sunbreak Demo
+setManifestid(1836451, "8275233417070218834", 7696455184)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
