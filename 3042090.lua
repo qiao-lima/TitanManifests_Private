3d87da6167bb0343e5fe2c9df475236cbf61ec3a
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 00:36:18 -0400

    restock 3042090

---
+++ 3042090.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3042090.lua
+--Gamename Prison of Nightmare
+addappid(3042090) --Mainappid Prison of Nightmare
+addappid(3042091, 1, "5745caa06cc6e35068ec395d702f7d86d1f3eccf5613a8d8135ee2e5dee3dc19") --Main Windows Depot Prison of Nightmare
+setManifestid(3042091, "1362052692840473584", 23281556032)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
