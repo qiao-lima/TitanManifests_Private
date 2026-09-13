f7a42f4ba557f50316cb47dd090d034d86ac1b0f
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 11:07:03 -0400

    restock 3504700

---
+++ 3504700.lua
@@ -0,0 +1,5 @@
+addappid(3504701,0,"71bb8381920b2c0c1df20a19ba5ad8afd3921df2073b3feda3ae8da0f7b500bc")
+setManifestid(3504701,"4916378427700203122")
+addappid(3504700)
+addappid(4570550)
+addappid(4867090)
