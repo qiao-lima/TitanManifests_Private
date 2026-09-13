c98a909783bb3ac458d5e72e7e7bb037b6b4650d
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 11:00:01 -0400

    restock 2193990

---
+++ 2193990.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2193990.lua
+--Gamename Monster Hunters: Frost Giant
+addappid(2193990) --Mainappid Monster Hunters: Frost Giant
+addappid(2193991, 1, "47948647f831f6be9f684ecfef979eefc2ea98bc5a58546a4180a2cb0415f81e") --Main Windows Language Depot English Monster Hunters: Frost Giant
+setManifestid(2193991, "5713006676757601966", 349333216)
