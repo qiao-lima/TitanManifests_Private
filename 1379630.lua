72ea64e9368222aa64aa0706788de9fcbf037425
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 07:19:17 -0400

    restock 1379630

---
+++ 1379630.lua
@@ -0,0 +1,47 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1379630.lua
+--Gamename RealRTCW
+addappid(1379630, 1, "2c9e1204b2136a0ed4c7ed2700d09fd704d0d49539f39ef77440e6a9a75eeaba") --Mainappid RealRTCW
+addappid(1379631, 1, "9f4f2e92b359375599df34a6b6b476a0b35b85a1aa28017a2c0f6e329b69e067") --Main Windows Depot RealRTCW
+setManifestid(1379631, "3180195485247698961", 2944340592)
+addappid(1379632, 1, "9E1FFA53E9D664150163175C2364D97FBF2F69DAD939D1959F4AFD3D1C6839E1") --Main Windows Language Depot Czech RealRTCW
+setManifestid(1379632, "7636736862373507441", 335074416)
+addappid(1379633, 1, "a3cf098e90f877587c0a715f55c08aa0093399084762ada84d814ff360cf65d3") --Main Windows Language Depot Russian RealRTCW
+setManifestid(1379633, "4743386355300184980", 201433776)
+addappid(1379634, 1, "96e34c27674839adf11c4cdeae218dcedcf23e5a3933fabe7aaa69a0439059c2") --Main Windows Language Depot Ukrainian RealRTCW
+setManifestid(1379634, "1164696792548577488", 23969008)
+addappid(1379635, 1, "dbd9d382cfae061210972e95f2cc6964c6247be4413997d88b39f827dd906adc") --Main Windows Language Depot French RealRTCW
+setManifestid(1379635, "67797657904629234", 222417392)
+addappid(1379636, 1, "a4e427c3bdc93d3410b2097401f660152c2a22c56311a5af2a7e6314f63ae87e") --Main Windows Language Depot Italian RealRTCW
+setManifestid(1379636, "8568216601230874541", 217986528)
+addappid(1379637, 1, "a4e81fe33c9148cb7ab4c4b12e975af8cd66d68bd31ca2639a801fe68a40e9f0") --Main Windows Language Depot Polish RealRTCW
+setManifestid(1379637, "8174878296181099976", 58845856)
+--Dlcname RealRTCW Supporter Pack
+addappid(1508910, 1, "ca10cfbb1635c8469a82febd3692d3a1e3b2a6517bef0feb5cbb91dbbc7e228c") --Dlcname RealRTCW Supporter Pack
+setManifestid(1508910, "4443278120660085984", 7133664)
+--Dlcname RealRTCW German Voices Pack
+addappid(2180820, 1, "6b3dc0dcc54ae87b20e07ddbac01b268507e7c12253aa970f9d06318d27675dd") --Dlcname RealRTCW German Voices Pack
+setManifestid(2180820, "8854136907887566565", 83793664)
+--Dlcname AppID 2189060
+addappid(2189060, 1, "10e138b4703ea87b89d7c91fb9f6d72f329c51088954e194a347caf04350111f") --Dlcname AppID 2189060
+--Dlcname RealRTCW Agency Weapon Pack
+addappid(2993200, 1, "1debb75a348e8f95155b8a9da1f1c1127289968e83abc65f77942f395ffc1a8b") --Dlcname RealRTCW Agency Weapon Pack
+setManifestid(2993200, "2128518568790464627", 9400752)
+--Dlcname RealRTCW Cursed Sands
+addappid(4509610, 1, "8bb9d9bc214d9dc9d76ca2a6db969162bfc645571010a1ae06193a7c17da6bc3") --Dlcname RealRTCW Cursed Sands
+addappid(1379638, 1, "09abc9c5c66b2ba39fca9a47424d5d3d961f2f926230f13e9e7e3d40e8dea088") --Dlc Windows Language Depot French RealRTCW Cursed Sands
+setManifestid(1379638, "8823223263839109877", 168985104)
+addappid(1379639, 1, "08166a1992eb8fadcde2196211753d1ee4af4d57e807f1441963ff2167496d4d") --Dlc Windows Language Depot Russian RealRTCW Cursed Sands
+setManifestid(1379639, "9215284381307678138", 127175008)
+addappid(1508911, 1, "52cfb4ac9e8126484ab704779525551e0b8e46896b2669349f1eab32089cf4c9") --Dlc Windows Language Depot Polish RealRTCW Cursed Sands
+setManifestid(1508911, "4094984071842749559", 73063840)
+addappid(1508912, 1, "8de5bd212b7a3f2f8a5a493cb4449df229ef22dc06a14c2a55ce48e43d46b625") --Dlc Windows Language Depot Czech RealRTCW Cursed Sands
+setManifestid(1508912, "7486088155517247731", 206554512)
+addappid(1508913, 1, "720fdc179a6fa88243170ed3679a7e9a0bcdbbdc9fac2f96bffe5edbec07c778") --Dlc Windows Language Depot Ukrainian RealRTCW Cursed Sands
+setManifestid(1508913, "4406307831726674956", 65993664)
+addappid(1508914, 1, "08272faac4a971c5bb59da754008b52600ca0bec3f44295c0a29cb5c0981b005") --Dlc Windows Language Depot Italian RealRTCW Cursed Sands
+setManifestid(1508914, "2536002512541819201", 73059584)
+setManifestid(4509610, "8877153875410446042", 902487360)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
