de5c207f58dab95c19ef6668b10dfcaeff8931f9
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 10:35:01 -0400

    restock 2899660

---
+++ 2899660.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2899660.lua
+--Gamename 热血街头
+addappid(2899660) --Mainappid 热血街头
+addappid(2899661, 1, "529d9b8642d046ffebbbf619183e8cff0e5ad079c175276e73b6b27743cc1d03") --Main Windows Depot 热血街头
+setManifestid(2899661, "585343150889553342", 131975040)
+--Dlcname 热血→魔幻 - 拓展包
+addappid(2900290, 1, "a67650caba8908e933a352e9b0fd5053abc1f6cfafa36848af1aa0d8128abe38") --Dlcname 热血→魔幻 - 拓展包
+setManifestid(2900290, "3617385653826819641", 151221088)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
