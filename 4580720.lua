f988392dde2c9c82b1e3cd683a588ffb7215d53c
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 03:50:06 -0400

    restock 4580720

---
+++ 4580720.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4580720.lua
+--Gamename Rising Moonshade
+addappid(4580720) --Mainappid Rising Moonshade
+addappid(4580721, 1, "faf8d7da82f3103fb94f5d3d9b6016f72a59ad319a20bc9af2ba862046c0378d") --Main Windows Depot Rising Moonshade
+setManifestid(4580721, "5778434650600545442", 1370824640)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
