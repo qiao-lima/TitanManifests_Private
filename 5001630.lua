c0e9bc17c88b2f178e3d5476b55b6daa33ba9ec1
qiao-lima <qiao-lima@github.com>
2026-09-14 05:08:58 +0800

    Manual Update 5001630

---
+++ 5001630.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5001630.lua
+--Gamename 嘉豪
+addappid(5001630, 1, "353368dc1456518e19159de23a035bce0b2baad3553ebce094f0adaaf9f94909") --Mainappid 嘉豪
+addappid(5001631, 1, "890504cde54d58e73daf2d24fe25d8e806b75590246f768f3dcd167bd8c6aee5") --Main Windows Depot 嘉豪
+setManifestid(5001631, "5473713227066409394", 1478074336)
+--Dlcname JAJAの名画冒险
+addappid(5074810, 1, "fac745ace0dafb2a428ac67bb49f97e3a86ff65607192629be945da09f79dfd7") --Dlcname JAJAの名画冒险
+addappid(5063660) --Dlcname 嘉豪 Demo
+addappid(5066260) --Dlcname Jiahao - Boundary Heroes Expansion Pack (Supporter Pack)
+--Missing Dlcs
+--Dlcname 嘉豪 Soundtrack
