02140c50d7cec43b8f76e21a09aa86d35a10e47f
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 11:07:26 -0400

    restock 1486660

---
+++ 1486660.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1486660.lua
+--Gamename Dragon VR
+addappid(1486660) --Mainappid Dragon VR
+addappid(1486661, 1, "e5cd62215cacdda0bf8ca6f439140b97af70b1b31a547a4996b714e9357f6b9c") --Main Windows Depot Dragon VR
+setManifestid(1486661, "957935089597632816", 1507137440)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
