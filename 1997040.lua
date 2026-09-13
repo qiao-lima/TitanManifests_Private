ad87acae0fd1ee0749a9105de9e0e3e9e9d88b4d
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 09:38:19 -0400

    restock 1997040

---
+++ 1997040.lua
@@ -0,0 +1,16 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1997040.lua
+--Gamename MARVEL SNAP
+addappid(1997040, 1, "9b6f53432d968214455ff2269747c5c2b52e83dd00de09b889d8bc47a5b65497") --Mainappid MARVEL SNAP
+addappid(1997041, 1, "47f68d7b70bc1a054ba3ab802ab17b93d8e60c5aa51e5c4b123204acb0b97116") --Main Windows Depot MARVEL SNAP
+setManifestid(1997041, "7681937843223694573", 8764662176)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(4465130) --Dlcname Steam Starter
+addappid(4465140) --Dlcname Steam Level Up
+addappid(4465150) --Dlcname Steam Prestige
+addappid(4465160) --Dlcname Steam Resource Cache
+addappid(4465170) --Dlcname Power Pack: Destroy
+addappid(4465180) --Dlcname Power Pack: Discard
+addappid(4465190) --Dlcname Power Pack: On Reveal
