c795d670eb548685a79b8f3e248f180188383256
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 02:10:01 -0400

    restock 1957330

---
+++ 1957330.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1957330.lua
+--Gamename VTuber Plus
+addappid(1957330, 1, "cd5af39b44ca58c74a4a49a3638e9fc0f451ac475e850e9176486623cdbe05ee") --Mainappid VTuber Plus
+addappid(1957331, 1, "4c072bd778d71bb1cf3b9424a53cede86d2114e187f1299bcf0fa820697890ea") --Main Windows Depot VTuber Plus
+setManifestid(1957331, "2104699002465692531", 203916784)
+--Share Depots
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229002, "7260605429366465749", 50365280)
