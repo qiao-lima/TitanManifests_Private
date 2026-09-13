289e17ae6cb205a36ccb294adee185afc77d00ef
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 02:10:19 -0400

    restock 2828790

---
+++ 2828790.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2828790.lua
+--Gamename Hyper Online: Avatar VTuber Studio
+addappid(2828790) --Mainappid Hyper Online: Avatar VTuber Studio
+addappid(2828791, 1, "aebd61c48ce4554964f6a899e4b1e8b4b29dcdcfe5d28cba6e80a1c4186fc550") --Main Windows Depot Hyper Online: Avatar VTuber Studio
+setManifestid(2828791, "3133595595195924423", 1422307776)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(3718280) --Dlcname Hyper Online: VTuber Avatar Studio - Hyper Pro DLC
