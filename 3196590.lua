28772985b10abef8c11fdbc7cc921e19c916a65c
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 06:33:42 -0400

    restock 3196590

---
+++ 3196590.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3196590.lua
+--Gamename 仙之气
+addappid(3196590) --Mainappid 仙之气
+addappid(3196591, 1, "4b721e49a4a33f4c0d8801bb4b87aa48b5358681351471279b1278d44c437b16") --Main Windows Language Depot Schinese 仙之气
+setManifestid(3196591, "728090953100057935", 571102256)
+addappid(3773660) --Dlcname 仙之气-灰冥专属服装：神女初成
