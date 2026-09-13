002880923aa436cdd0095c11fecf05f90c7f5375
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 10:02:16 -0400

    restock 1302860

---
+++ 1302860.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1302860.lua
+--Gamename Assembly Planter
+addappid(1302860) --Mainappid Assembly Planter
+addappid(1302861, 1, "0e8356362899936e975b38cdcafdf7501f7b15d7079349532010688a912f7fec") --Main Windows Depot Assembly Planter
+setManifestid(1302861, "7175979115991306298", 227822944)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
