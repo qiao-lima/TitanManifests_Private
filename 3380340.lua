2621d5d5dd3dbc0f84aad56383e8c7781a47c148
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 11:10:01 -0400

    restock 3380340

---
+++ 3380340.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3380340.lua
+--Gamename DragonRoad龙道
+addappid(3380340) --Mainappid DragonRoad龙道
+addappid(3380341, 1, "814e2b2b09bf8ded67139879fabda1ccb65d2fd6b7bfa576fa62ddba00839873") --Main Windows Language Depot Schinese DragonRoad龙道
+setManifestid(3380341, "6249522952686353042", 58485969376)
+addappid(3380343, 1, "e5059638517907e880e7d25d8d30aee217de09a16cfd415f7777308a4f440c2f") --Main Windows Language Depot English DragonRoad龙道
+setManifestid(3380343, "7861768544591723583", 58450594928)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
