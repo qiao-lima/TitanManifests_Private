c0fdcae4df613a30056e4698e9358b6bd8fe922d
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 02:39:48 -0400

    restock 308520

---
+++ 308520.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 308520.lua
+--Gamename Minimon
+addappid(308520) --Mainappid Minimon
+addappid(308521, 1, "609d91195748560f0cac100702af911ee47a2032c5039b094d1c140ebbac536b") --Main Windows Depot Minimon
+setManifestid(308521, "3819576350332897280", 153378096)
+addappid(308522, 1, "5dc4adb88ebf3aebdc5bd3c0156c7826faf983d8b84f3eadec521da89d08e371") --Main Macos Depot Minimon
+setManifestid(308522, "2159450688523344371", 112390848)
+addappid(308523, 1, "da990ed7854be2ec97f2ffdf06945b7856023c33a4ccd163c76ad1f0fe7e8161") --Main Linux Depot Minimon
+setManifestid(308523, "1823775952727646827", 155518192)
+--Share Depots
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229020, "5799761707845834510", 591984)
