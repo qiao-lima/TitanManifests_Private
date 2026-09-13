9d3acee99f911bbe90621e3a26d1ee9c4886e4c5
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 05:13:45 -0400

    restock 3297360

---
+++ 3297360.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3297360.lua
+--Gamename We're Here, Papa
+addappid(3297360) --Mainappid We're Here, Papa
+addappid(3297361, 1, "56442f62d5822cca521039634c8284cf33d7851350018a63c95a40b96c7364de") --Main Windows Depot We're Here, Papa
+setManifestid(3297361, "5504313201822237393", 1780953120)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
