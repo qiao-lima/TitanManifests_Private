d32c96320ab41b0ef5d5fe44b0a4567875156ba4
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 01:44:04 -0400

    restock 4025700

---
+++ 4025700.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4025700.lua
+--Gamename Heartopia
+addappid(4025700) --Mainappid Heartopia
+addappid(4025701, 1, "eedcc8eccb376e46a2677edf66f972ae1467d4f36322e1d2620a14641b29876b") --Main Windows Depot Heartopia
+setManifestid(4025701, "2599526245774281809", 11173140752)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
