abf5277482785dde56afcfd97e2ec99f3063734d
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 07:52:06 -0400

    restock 4659040

---
+++ 4659040.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4659040.lua
+--Gamename 五行幻境：荒渡
+addappid(4659040) --Mainappid 五行幻境：荒渡
+addappid(4659041, 1, "ce5003d422c71ca3c82c934c42d9b73618facd5a03f5e763229dd8dddf5d6fb8") --Main Windows Depot 五行幻境：荒渡
+setManifestid(4659041, "825397439917680968", 1580149824)
