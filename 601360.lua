27c10d5278af78602627ecfad23efd5b46d49f1f
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 12:38:35 -0400

    restock 601360

---
+++ 601360.lua
@@ -0,0 +1,21 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 601360.lua
+--Gamename Portal: Revolution
+addappid(601360, 1, "1078ab5764b514a9f8b06a3a9eee7e4b8ac23cd7ceabaac411296619662a90a3") --Mainappid Portal: Revolution
+addappid(601361, 1, "c585b091c6692f9d8a5cc733c9ea372be00edbe7429019f9316e56ed4b935268") --Main Windows Depot Portal: Revolution
+setManifestid(601361, "7988610923866673849", 6444210976)
+addappid(601363, 1, "5608ad50c107578d774e8083d4129f7ef1156222ad2859256a8cb5043e428979") --Main Windows Depot Portal: Revolution
+setManifestid(601363, "4625918216674383223", 75499984)
+--Dlcname Portal: Revolution Plush Pack
+addappid(854730) --Dlcname Portal: Revolution Plush Pack
+addappid(854731, 1, "65a5bc95d8f34ca1e7bb447c58d7c67216ef82b52bf84ef8289c370a5fccc56b") --Dlc Windows Depot Portal: Revolution Plush Pack
+setManifestid(854731, "4299373800031489304", 27831664)
+--Dlcname Portal: Revolution Soundtrack
+addappid(2266290) --Dlcname Portal: Revolution Soundtrack
+addappid(2266291, 1, "a5a4dc34b098a0454ceac30ef10cbccbead228ef86ecb8b91d8522ded290b0e4") --Dlc Windows Depot Portal: Revolution Soundtrack
+setManifestid(2266291, "3008365178465588858", 198280144)
+addappid(2266292, 1, "807f3fc22065c25a5e920a1c43c99800b02699f08e3d62e28701a2bd4c98c58f") --Dlc Windows Depot Portal: Revolution Soundtrack
+setManifestid(2266292, "277073121405821802", 1118359520)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
