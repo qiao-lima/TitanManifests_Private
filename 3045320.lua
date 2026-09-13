4cd25b4c1bd36242c2cb8ec980c363604e690224
titan-cangku <titan-cangku@gamefun365.com>
2026-09-12 22:31:50 -0400

    restock 3045320

---
+++ 3045320.lua
@@ -0,0 +1,21 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3045320.lua
+--Gamename Cyborg: Rise
+addappid(3045320) --Mainappid Cyborg: Rise
+addappid(3045321, 1, "e252fd4b5386fb1202f62babb400630d5116b966d40bfe01a2e489e1e6370c7e") --Main Windows Depot Cyborg: Rise
+setManifestid(3045321, "7109613723075826658", 182791568)
+addappid(3088290) --Dlcname Cyborg-Π
+addappid(3088300) --Dlcname Cyborg-Ω
+addappid(3088310) --Dlcname Cyborg-Δ
+addappid(3088320) --Dlcname Cyborg-Θ
+addappid(3088330) --Dlcname Cyborg-Ψ
+addappid(3088340) --Dlcname Cyborg-Σ
+addappid(3135700) --Dlcname Cyborg-Zeus
+addappid(3135710) --Dlcname Cyborg-Hera
+addappid(3135720) --Dlcname Cyborg-Poseidon
+addappid(3135730) --Dlcname Cyborg-Hades
+addappid(3135740) --Dlcname Cyborg-Athena
+addappid(3135750) --Dlcname Cyborg-Apollo
+addappid(3135760) --Dlcname Cyborg-Ares
+addappid(3135770) --Dlcname Cyborg-Hestia
+addappid(3135780) --Dlcname Cyborg-Pandora
