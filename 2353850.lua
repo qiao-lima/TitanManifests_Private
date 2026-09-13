3f9757388d6bcd3aa134d835407a7162a709680e
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 02:51:58 -0400

    restock 2353850

---
+++ 2353850.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2353850.lua
+--Gamename 零号协议AgreementZero
+addappid(2353850) --Mainappid 零号协议AgreementZero
+addappid(2353851, 1, "267a39d3871010951147dd17a3307ff6596f9c892db20e0d12ed507758e1af44") --Main Windows Depot 零号协议AgreementZero
+setManifestid(2353851, "4715735507046370959", 413772000)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2370580) --Dlcname 零号协议AgreementZero-捐助
+addappid(2947960) --Dlcname 零号协议AgreementZero-体力PhysicalPower
+addappid(2953220) --Dlcname 零号协议AgreementZero-能量数据EnergyData
