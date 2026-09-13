73769bdab85292e876ff76587e6c2f80ba65fffb
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 08:27:45 -0400

    restock 3676460

---
+++ 3676460.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3676460.lua
+--Gamename 早洩外甥與性冷感阿姨—肉體交換物語～
+addappid(3676460, 1, "08ee93a6691b661b6679ae792a9895bcba4f8ef85cf2a0c83be18407d16c3d0b") --Mainappid 早洩外甥與性冷感阿姨—肉體交換物語～
+addappid(3676461, 1, "f2310c1f3a44a4040ea03e95daf4715451c0599f969d1171af79cb3335df80c4") --Main Windows Language Depot English 早洩外甥與性冷感阿姨—肉體交換物語～
+setManifestid(3676461, "862701183455120367", 547308512)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
