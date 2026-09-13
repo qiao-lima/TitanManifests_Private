ff7a9150be835749b426d1002aff424c4e7d8863
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 13:39:24 -0400

    restock 1436210

---
+++ 1436210.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1436210.lua
+--Gamename Requiem
+addappid(1436210) --Mainappid Requiem
+addappid(1436211, 1, "e91dbeca7fa7f43858e1f475b2eb7586d4226834c5129deb90c785cc492e7fe5") --Main Windows Language Depot English Requiem
+setManifestid(1436211, "1904554584923076089", 1419490768)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
