15f7bcce47120077ffefd1d9fc36c7d1830b4979
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 12:38:23 -0400

    restock 1468800

---
+++ 1468800.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1468800.lua
+--Gamename Portal Defect
+addappid(1468800) --Mainappid Portal Defect
+addappid(1468806, 1, "5038612d1821a01e44a932d2b120cd2efa9b4c68ed09b08c83bfe18399799932") --Main Windows Language Depot English Portal Defect
+setManifestid(1468806, "630813790561335629", 8175979248)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229032, 1, "15b3308d4e95d1156182211bab1c823b6df0a06fd74a5c0833abed89bc307cfb") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229032, "3616495131483866412", 40038080)
+addappid(229033, 1, "9a789440bbc16131547ac6c062e5f168f7684da533a2899a71fdfaaeec21efc3") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229033, "2059065101492814639", 59325760)
