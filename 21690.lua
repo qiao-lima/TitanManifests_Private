a7dd1f4a75132ba3053a97a4f6aa91fdc7573efa
qiao-lima <qiao-lima@github.com>
2026-09-14 05:08:56 +0800

    Manual Update 21690

---
+++ 21690.lua
@@ -0,0 +1,18 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 21690.lua
+--Gamename Resident Evil 5
+addappid(21690, 1, "674b774981f445f56cae8810d60a59d000a23656d366944d9203d13d356f4330") --Mainappid Resident Evil 5
+addappid(21691, 1, "b3ad2af3d25d179accc032ae7853c6f8a916bba21b5f0be9f11f22f0938110a4") --Main Windows Depot Resident Evil 5
+setManifestid(21691, "2911490666295175655", 1616)
+addappid(21692, 1, "556f26dec9fcd54f1ecb0f2af6b1259a325a3eed959c0b242c83fa176c74971d") --Main Windows Depot Resident Evil 5
+setManifestid(21692, "8019335905957731486", 7703584752)
+addappid(21693, 1, "a07f44cc54133c4921ca14f7014ea4a5fd0aa0dbc36d13f3d7abe5f3be00c564") --Main Windows Depot Resident Evil 5
+setManifestid(21693, "3603548504038570692", 88546480)
+--Dlcname Resident Evil 5 - UNTOLD STORIES BUNDLE
+addappid(352000, 1, "dba46409b51f3da1c7669a74a091ce7725eb5969705192edbb9ba3f9abd096d3") --Dlcname Resident Evil 5 - UNTOLD STORIES BUNDLE
+setManifestid(352000, "455789278129163691", 928)
+--Share Depots
+addappid(228981, 1, "9afdbd47677424be993f6b516a15ebdb5281aa318114631d14b2b23999ae97ac") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228981, "7613356809904826842", 5804736)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
