378ba77a56e976491c5bce26a636c18d002afab0
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 01:55:01 -0400

    restock 3117040

---
+++ 3117040.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3117040.lua
+--Gamename 问山海:悟空
+addappid(3117040) --Mainappid 问山海:悟空
+addappid(3117041, 1, "be31c4a087a20ce92432da5b6e188d593ca70127f1fd89196a54ff6abd8c2b30") --Main Windows Depot 问山海:悟空
+setManifestid(3117041, "6238890890514054895", 2445807344)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
