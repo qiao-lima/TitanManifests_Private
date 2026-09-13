bdd7c8646572b6c394532577c085b64893479f6f
qiao-lima <qiao-lima@github.com>
2026-09-14 05:08:56 +0800

    Manual Update 3091140

---
+++ 3091140.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3091140.lua
+--Gamename Lootbound
+addappid(3091140, 1, "1f05d6f3ddade4323eb34249f78ed3edae22a7b8d73bbf70c1ca4165965e54ca") --Mainappid Lootbound
+addappid(3091141, 1, "bfd0833e4e531ac8033b18ae9b79eea5b2ee46b29b62f7959f97caec7547be1c") --Main Windows Depot Lootbound
+setManifestid(3091141, "387461822166878621", 459428096)
+addappid(3091142, 1, "487ae9dc13c277989189e32614056f9fc545ad6860e154673c32dfd568b470de") --Main Macos Depot Lootbound
+setManifestid(3091142, "9057950061919802458", 501875952)
+addappid(3091143, 1, "9f32af5e3cc8ccf9bea79d4f5a5f09f8de74c727d5f64b023cdc40116dbbb9dd") --Main Linux Depot Lootbound
+setManifestid(3091143, "3681785425501401276", 466490896)
