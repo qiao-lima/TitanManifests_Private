98e7174a6c717b224ca788cc4e99a3c871ab9fc1
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 01:55:12 -0400

    restock 4832440

---
+++ 4832440.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4832440.lua
+--Gamename Black Saint: Goku
+addappid(4832440) --Mainappid Black Saint: Goku
+addappid(4832441, 1, "79b7e9b71750290b5e1fa40b5b624b97a0f3697d8916f71aa40563fd7992e3e5") --Main Windows Depot Black Saint: Goku
+setManifestid(4832441, "1442963444374526781", 108417728)
