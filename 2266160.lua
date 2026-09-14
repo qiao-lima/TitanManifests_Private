5b54b9c823a0e65f7c5c704fdf40dae9e5c06e2b
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 22:56:18 -0400

    restock 2266160

---
+++ 2266160.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2266160.lua
+--Gamename Modules
+addappid(2266160) --Mainappid Modules
+addappid(2266161, 1, "98ff098c0e628d54e67841219f0e24e70bcc05533ac4468f3b7b227810b7fb33") --Main Windows Depot Modules
+setManifestid(2266161, "7798512774234193381", 949315552)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
