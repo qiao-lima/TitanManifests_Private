ab9f80c40b65873d10cd0fa4eb678bf4824227f5
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 04:40:16 -0400

    restock 3694330

---
+++ 3694330.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3694330.lua
+--Gamename 怪谈调查局
+addappid(3694330) --Mainappid 怪谈调查局
+addappid(3694332, 1, "9f282568f1fecfe8d626a1ed585ab27e6ccf8ae63677356764c290f80ee95b2e") --Main Windows Depot 怪谈调查局
+setManifestid(3694332, "3945736686242116740", 169512384)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229020, "5799761707845834510", 591984)
