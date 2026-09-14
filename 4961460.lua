4d6ecfdfedf76bdc6e8b86dff0323f60d45c863a
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 14:19:20 -0400

    restock 4961460

---
+++ 4961460.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4961460.lua
+--Gamename MarginCall
+addappid(4961460) --Mainappid MarginCall
+addtoken(4961460, "3842927537449807955")
+addappid(4961461, 1, "438de88fd1189cf8417133265a0668b397316dd75beaa625ea524f4f8e9bd0c1") --Main Windows Depot MarginCall
+setManifestid(4961461, "2576586564396626300", 559871744)
