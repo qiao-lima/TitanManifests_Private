315b3f3a807f5a29c25450a4178d3ea118882b8f
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 11:48:43 -0400

    restock 1748740

---
+++ 1748740.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1748740.lua
+--Gamename Evolution: Moon Warfare
+addappid(1748740) --Mainappid Evolution: Moon Warfare
+addappid(1748741, 1, "4b933d333ee285ed95a5bc7d53a9cb8a639d66f670adcc770d2df7ab1fd8aee3") --Main Windows Depot Evolution: Moon Warfare
+setManifestid(1748741, "3177520651147863813", 1457874192)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
