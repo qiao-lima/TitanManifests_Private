308aa087a695d0af52e1e36a5a147a43b46a4c40
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 21:56:10 -0400

    restock 4587610

---
+++ 4587610.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4587610.lua
+--Gamename Dice Tavern: Farkle Roguelike
+addappid(4587610) --Mainappid Dice Tavern: Farkle Roguelike
+addappid(4587611, 1, "bea79a080b03baa52996fed3f14215470160f191d40d584fe613c97b3d8cb601") --Main Windows Depot Dice Tavern: Farkle Roguelike
+setManifestid(4587611, "7823921543326691798", 339618288)
