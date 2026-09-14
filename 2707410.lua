6d0d3e2adefd364fc4635cf2a1c05c572ac47c9d
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 11:50:21 -0400

    restock 2707410

---
+++ 2707410.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2707410.lua
+--Gamename Iron Line
+addappid(2707410) --Mainappid Iron Line
+addappid(2707411, 1, "00dafe7a2529b5258f7fd356d828044d3ee469c107d085aa05f68989cc7b07f6") --Main Windows Depot Iron Line
+setManifestid(2707411, "301799339722918336", 583850208)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
