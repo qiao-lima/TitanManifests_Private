f4bfada6a5ed488ff7e71948b43e503affba2fc7
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 03:30:07 -0400

    restock 1307240

---
+++ 1307240.lua
@@ -0,0 +1,17 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1307240.lua
+--Gamename Call of Coronga
+addappid(1307240) --Mainappid Call of Coronga
+addappid(1307241, 1, "e13ca4f5a679989a6d5d47837b6d13bf6e02fd377ff51b92725c8532db2fff6a") --Main Windows Depot Call of Coronga
+setManifestid(1307241, "3407716217523129679", 903987584)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229002, "7260605429366465749", 50365280)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
