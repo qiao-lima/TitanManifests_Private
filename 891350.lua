70afb4b9e99cd53c3b9920338d0cab69ea2dda5a
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 21:47:10 -0400

    restock 891350

---
+++ 891350.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 891350.lua
+--Gamename JUST DASH
+addappid(891350) --Mainappid JUST DASH
+addappid(891351, 1, "a974202d7320564a994980a3a108a308dcda3aa8ca93503db5399e3f5dfd0aad") --Main Windows Depot JUST DASH
+setManifestid(891351, "8799299909019959754", 1196294416)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
