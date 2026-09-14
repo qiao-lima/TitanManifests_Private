5413ffb35ca87946b668eefd74ca64dc24e6227c
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 04:21:58 -0400

    restock 1438710

---
+++ 1438710.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1438710.lua
+--Gamename Modern Arena
+addappid(1438710) --Mainappid Modern Arena
+addappid(1438711, 1, "919e96703b65c743f9480692798c4bb04b639cf76c6019185ecbfa137ce429a3") --Main Windows Depot Modern Arena
+setManifestid(1438711, "7241805228448236034", 22180166128)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2148530) --Dlcname Modern Arena - Donation
+--Missing Dlcs
+--Dlcname DLC - Character Selection
+--Dlcname DLC - 2nd Characters
