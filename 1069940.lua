b6f4242fbdfc9ca9613d1b231749276872a76410
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 12:20:12 -0400

    restock 1069940

---
+++ 1069940.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1069940.lua
+--Gamename Blood Brothers
+addappid(1069940) --Mainappid Blood Brothers
+addappid(1069941, 1, "381f5d7ec1df4f92ea83aa9874a734e3b9d5f8331a062f612962afbd57df3770") --Main Windows Depot Blood Brothers
+setManifestid(1069941, "7798231143421623000", 414150688)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
