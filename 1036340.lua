45868103dec5798a5722bf1c7eee2d5dff8ffd98
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 21:50:06 -0400

    restock 1036340

---
+++ 1036340.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1036340.lua
+--Gamename Hit n' Rush
+addappid(1036340) --Mainappid Hit n' Rush
+addappid(1036341, 1, "e4e98d103c4d3cee164ef60a85a91998368c20b7301e2f6abc500b389339e194") --Main Windows Depot Hit n' Rush
+setManifestid(1036341, "7581223051012623909", 3744688000)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
