9149892af4bffbda2f9c4a7d3522c828377cb983
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 04:15:16 -0400

    restock 965080

---
+++ 965080.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 965080.lua
+--Gamename 江湖求生 Ganghood Survival
+addappid(965080) --Mainappid 江湖求生 Ganghood Survival
+addappid(965081, 1, "6237e6828761dcb617fa6959276e78d9b416768657700a1e9dfb4a6ad3f7372a") --Main Windows Depot 江湖求生 Ganghood Survival
+setManifestid(965081, "2738780995189170872", 2371642656)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
+addappid(229031, 1, "98d01f1eaa90ae08f978994e043bcbbf1f23fc72a325528ca7d722c827c0eb54") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229031, "7746630274301172884", 25905472)
