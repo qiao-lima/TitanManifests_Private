c27be5134cc236c827ac6594df42792a8e2665f6
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 22:56:00 -0400

    restock 768970

---
+++ 768970.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 768970.lua
+--Gamename ModLab
+addappid(768970) --Mainappid ModLab
+addappid(768971, 1, "d4e76d27dff5522beb8d09a74d52b87289afaf51c46c5e0a4272e3dab69c0c71") --Main Windows Depot ModLab
+setManifestid(768971, "4798114938513422051", 5747776)
+--Share Depots
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
