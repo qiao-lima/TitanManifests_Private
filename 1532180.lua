58f2c31b60639573301680f56502f2a3217a68a5
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 11:48:38 -0400

    restock 1532180

---
+++ 1532180.lua
@@ -0,0 +1,17 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1532180.lua
+--Gamename Evo\Wave
+addappid(1532180) --Mainappid Evo\Wave
+addappid(1532182, 1, "61bd1727172de62a45084b1b6a2f80ca6c1d2874458916aac3b78b2d0829f7e3") --Main Windows Depot Evo\Wave
+setManifestid(1532182, "3511105849154260819", 2452560432)
+--Dlcname Evo\Wave Soundtrack
+addappid(1557970) --Dlcname Evo\Wave Soundtrack
+addappid(1557971, 1, "76c97696039444fbe987bcd3a6401cda9abe8e19947944d2a4ed3e6d2bfa8eee") --Dlc Windows Depot Evo\Wave Soundtrack
+setManifestid(1557971, "4849556670742759108", 109689264)
+addappid(1557972, 1, "5c3341ff7f5c37c2b62ea4d7cbff25832a847e04ec562c7dd6d5ce59d0a8efe5") --Dlc Windows Depot Evo\Wave Soundtrack
+setManifestid(1557972, "1999651633033971260", 718754192)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
