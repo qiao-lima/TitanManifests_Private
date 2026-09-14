2681bef905f21c4368a951accd03a80e67307919
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 10:57:06 -0400

    restock 1548430

---
+++ 1548430.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1548430.lua
+--Gamename Sacrifice of The Spirit
+addappid(1548430) --Mainappid Sacrifice of The Spirit
+addappid(1548431, 1, "5715c4d2421b1abab5d7629747d46aaf37399d8da03cb982eb7905cf82cad501") --Main Windows Depot Sacrifice of The Spirit
+setManifestid(1548431, "4482878679132909222", 4222238160)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
