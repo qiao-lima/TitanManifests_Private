a0b6f8062e1134f4dae0331ec70ad9b2abda4841
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 03:30:17 -0400

    restock 3044470

---
+++ 3044470.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3044470.lua
+--Gamename Call me Hero Demo
+addappid(3044470) --Mainappid Call me Hero Demo
+addappid(3044471, 1, "a7b7014eff3f577281086aef3e5a989f65a57fa73dc6fb7a8378ded172c77b2a") --Main Windows Depot Call me Hero Demo
+setManifestid(3044471, "6905329481892010061", 1731557376)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
