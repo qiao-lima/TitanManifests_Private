b71f7942b49a3da7376392727a0f0326380d483e
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 08:33:20 -0400

    restock 2647100

---
+++ 2647100.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2647100.lua
+--Gamename STEAMRUSH
+addappid(2647100) --Mainappid STEAMRUSH
+addappid(2647101, 1, "48c148de57bae833bb6aacb46c39fbefbbe24acf6960d5f4176ae16daa27c9d9") --Main Windows Depot STEAMRUSH
+setManifestid(2647101, "7697144883617460006", 4742942000)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
