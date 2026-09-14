381d485251e3d8c7a8dfd63083a9fe6b1d28045f
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 21:20:11 -0400

    restock 624460

---
+++ 624460.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 624460.lua
+--Gamename Fantasynth One
+addappid(624460) --Mainappid Fantasynth One
+addappid(624461, 1, "06153ae2d1a47246679adf1b9f4fe48fc942f90adf4a1aa9dc09e32bf25dcf9c") --Main Windows Depot Fantasynth One
+setManifestid(624461, "5812395518331257491", 118200928)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
