a0d5485a360b05e5439a605017a7046a40a8081a
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 07:32:47 -0400

    restock 1546900

---
+++ 1546900.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1546900.lua
+--Gamename Escape From My Turrets
+addappid(1546900) --Mainappid Escape From My Turrets
+addappid(1546901, 1, "a023ecfca73d15fb3bd4bc60a37405efc80e09b6eb886f9cbad1afcb6c490453") --Main Windows Language Depot English Escape From My Turrets
+setManifestid(1546901, "7800156572582743138", 60154640)
