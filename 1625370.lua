ea1a5318b698aeca3a77b4ad5835bc5fef46e6d2
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 00:20:28 -0400

    restock 1625370

---
+++ 1625370.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1625370.lua
+--Gamename CONNECTED!
+addappid(1625370, 1, "0b41cf303d1bb55c28dc2ad3073172fb4a35132cd3ebd4cfcd861522e09af45c") --Mainappid CONNECTED!
+addappid(1625371, 1, "36a44bb40bd40f002e78c6c750aae495ba61f5150ba23f2002f1dca2c5acb7d6") --Main Windows Depot CONNECTED!
+setManifestid(1625371, "6254794325562664184", 1205696)
+--Share Depots
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229002, "7260605429366465749", 50365280)
