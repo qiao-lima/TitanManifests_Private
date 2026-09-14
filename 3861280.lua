bc6f0c7c55040ffc5f2c438e0a6443cc986d60e4
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 10:50:17 -0400

    restock 3861280

---
+++ 3861280.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3861280.lua
+--Gamename 挂机手游模拟器
+addappid(3861280) --Mainappid 挂机手游模拟器
+addappid(3861281, 1, "c69cbc7a2dbc57b71b6c0c86903b10cd9fa6619403a79873fa4e9863fd41c34d") --Main Windows Depot 挂机手游模拟器
+setManifestid(3861281, "4759896474142888843", 192311360)
