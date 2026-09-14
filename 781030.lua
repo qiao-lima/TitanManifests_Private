94e308de7e72d6eca7839725a8ecf8e502c0fd18
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 22:57:45 -0400

    restock 781030

---
+++ 781030.lua
@@ -0,0 +1,35 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 781030.lua
+--Gamename DK Online
+addappid(781030) --Mainappid DK Online
+addappid(781031, 1, "74ce13b1a812b04f539853e106dde560649c458c94592d4f8bf2cf810a776823") --Main Windows Depot DK Online
+setManifestid(781031, "7449933702308435806", 12804109312)
+--Share Depots
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228982, "6413394087650432851", 9650016)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2216020) --Dlcname AppID 2216020
+addappid(2216021) --Dlcname AppID 2216021
+addappid(2216022) --Dlcname AppID 2216022
+addappid(2216023) --Dlcname AppID 2216023
+addappid(2216024) --Dlcname AppID 2216024
+addappid(2691770) --Dlcname AppID 2691770
+addappid(2732120) --Dlcname AppID 2732120
+addappid(2732130) --Dlcname AppID 2732130
+addappid(2732140) --Dlcname AppID 2732140
+addappid(2752540) --Dlcname AppID 2752540
+addappid(2752550) --Dlcname AppID 2752550
+addappid(3332290) --Dlcname AppID 3332290
+addappid(3332300) --Dlcname AppID 3332300
+addappid(4115630) --Dlcname DK ONLINE - SEASON4 FIRST STEPS PACK
+addappid(4115640) --Dlcname DK ONLINE - SEASON4 CONVENIENCE PACK
+addappid(4115650) --Dlcname DK ONLINE - SEASON4 GROWTH PACK
+addappid(4115660) --Dlcname DK ONLINE - SEASON4 LIMITLESS BOOST PACK 1
+addappid(4115670) --Dlcname DK ONLINE - SEASON4 ALL 4 ONE CARD PACK
+addappid(4310060) --Dlcname DK ONLINE - SEASON4 LIMITLESS BOOST PACK 2
+addappid(4310070) --Dlcname DK ONLINE - SEASON4 LIMITLESS BOOST PACK 3
