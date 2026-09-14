9c09bde073f3ec194d0799064ddec604906a793f
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 02:50:13 -0400

    restock 1729260

---
+++ 1729260.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1729260.lua
+--Gamename EOS 27
+addappid(1729260) --Mainappid EOS 27
+addappid(1729261, 1, "7d30306ecf56becf5dcd69de277066a242a35bb64f9df32f4f131e9eedbd37d3") --Main Windows Language Depot English EOS 27
+setManifestid(1729261, "9083870806496084802", 432661472)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
