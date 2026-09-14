08d660903f387cf21208661ac82bed63c7549b4b
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 08:50:01 -0400

    restock 1747590

---
+++ 1747590.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1747590.lua
+--Gamename Kenopsia
+addappid(1747590) --Mainappid Kenopsia
+addappid(1747591, 1, "73452b28b673c255295b7ae1264ff6fb40ac4506593a5a4f056b8e5765c7f4c5") --Main Windows Depot Kenopsia
+setManifestid(1747591, "3683127287512006531", 1807131600)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
