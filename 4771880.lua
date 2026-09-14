e19c3a87b675c208ba7cd91f2e26fd1d776fccea
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 12:07:33 -0400

    restock 4771880

---
+++ 4771880.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4771880.lua
+--Gamename 三国义
+addappid(4771880) --Mainappid 三国义
+addappid(4771881, 1, "de00da097310ad21b69c93530c8f51de6bebe5d37182170644e3e518ce8f5ae1") --Main Windows Depot 三国义
+setManifestid(4771881, "37862388283686320", 1443314160)
