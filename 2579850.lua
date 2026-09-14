622de832b8c3bf69feefb5f7a2c806c2e2396281
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 10:35:01 -0400

    restock 2579850

---
+++ 2579850.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2579850.lua
+--Gamename Noble's Life: Kingdom Reborn Demo
+addappid(2579850) --Mainappid Noble's Life: Kingdom Reborn Demo
+addappid(2579851, 1, "d1c14d0c4778b2b09e6c90660128662d2dd6c30e52ce0dfa137688d87a551f95") --Main Windows Depot Noble's Life: Kingdom Reborn Demo
+setManifestid(2579851, "4838352168841731801", 20787070368)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
