67915f0b2f9adb22c4fb56013ed289d2e4fea903
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 04:36:16 -0400

    restock 754350

---
+++ 754350.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 754350.lua
+--Gamename 忍者村大战2
+addappid(754350) --Mainappid 忍者村大战2
+addappid(754351, 1, "a3987690dec03bacbb3131fd81ff4d4087728bbda115cfbfeac23fa9eb824ed6") --Main Windows Depot 忍者村大战2
+setManifestid(754351, "5613586444801112152", 4286717184)
+--Share Depots
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228982, "6413394087650432851", 9650016)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
