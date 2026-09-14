7817a71836ac96eafb8a9dd71eeb5c040eb28b58
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 22:30:12 -0400

    restock 2702700

---
+++ 2702700.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2702700.lua
+--Gamename HEARTSHOT
+addappid(2702700) --Mainappid HEARTSHOT
+addappid(2702701, 1, "75ce8062241c9d9087218e9c995292ba85eb03c815396a08cd51433da82d6904") --Main Windows Depot HEARTSHOT
+setManifestid(2702701, "6295440780233904609", 3473796224)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
