53c620ea68cbc7a1701f173d0607a5e12846800f
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 13:48:59 -0400

    restock 3373550

---
+++ 3373550.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3373550.lua
+--Gamename なつ♡カノ SUMMER GIRLFRIEND
+addappid(3373550) --Mainappid なつ♡カノ SUMMER GIRLFRIEND
+addappid(3373551, 1, "63464712c37b1de9a2d098adcb25a9344cc69c12ddc8848204664e4ba5bd5b06") --Main Windows Depot なつ♡カノ SUMMER GIRLFRIEND
+setManifestid(3373551, "586622110065523942", 207294384)
