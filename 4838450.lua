84009980d9b67600ce344f9b8fa9e23572e3db94
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 07:39:50 -0400

    restock 4838450

---
+++ 4838450.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4838450.lua
+--Gamename 极乐妹土！2
+addappid(4838450) --Mainappid 极乐妹土！2
+addappid(4838451, 1, "e085bdf0d696ae1b71673c0cf0ce7ce50836e28b7b7fbab8f98b4ddbefc4f721") --Main Windows Depot 极乐妹土！2
+setManifestid(4838451, "6624305450045506797", 401529232)
