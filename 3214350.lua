eed82cf17b9ce2d9dc9dc3d8ccbc52878c13ea89
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 06:25:26 -0400

    restock 3214350

---
+++ 3214350.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3214350.lua
+--Gamename JR EAST Train Simulator Demo
+addappid(3214350) --Mainappid JR EAST Train Simulator Demo
+addappid(3214351, 1, "e6b2355a5ebede375a4b4dcd7d01b9839598ec67159cd4ee9813638fa02669eb") --Main Windows Depot JR EAST Train Simulator Demo
+setManifestid(3214351, "55994934656788717", 3977770352)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
