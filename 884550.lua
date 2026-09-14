c5479cc8b80c24ac021d9197f0b6dd7a12bbc631
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 20:46:59 -0400

    restock 884550

---
+++ 884550.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 884550.lua
+--Gamename Mist of the Dark
+addappid(884550) --Mainappid Mist of the Dark
+addappid(884551, 1, "07aeba55461c0cde133040e602182b88b7bb4ead6033e041af93f1dc8abffd9b") --Main Windows Depot Mist of the Dark
+setManifestid(884551, "2228634533454855552", 639472320)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
