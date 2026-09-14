bbcf9945a069a79684b79a348b31ee12d22cd73e
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 04:36:22 -0400

    restock 1986250

---
+++ 1986250.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1986250.lua
+--Gamename ヒロイン忍法帖 姫影・ 壱／Pretty Ninja HIMEKAGE_01
+addappid(1986250) --Mainappid ヒロイン忍法帖 姫影・ 壱／Pretty Ninja HIMEKAGE_01
+addappid(1986253, 1, "dc938fef414cab94676d44bb92f19456eb7f0e770c451258984f782ea06fb136") --Main Windows Language Depot Japanese ヒロイン忍法帖 姫影・ 壱／Pretty Ninja HIMEKAGE_01
+setManifestid(1986253, "2450935263902182787", 328010288)
+addappid(1986254, 1, "4a560802615ba99cca85cfe2403007671d3badf6d4226d1bc6ceb437b4346bdd") --Main Windows Depot ヒロイン忍法帖 姫影・ 壱／Pretty Ninja HIMEKAGE_01
+setManifestid(1986254, "4732558228446771217", 329002912)
