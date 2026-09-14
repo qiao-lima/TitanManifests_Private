e14ff66bb005bf0fe8d4e4e451d25d287a0f1efe
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 01:29:15 -0400

    restock 3092450

---
+++ 3092450.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3092450.lua
+--Gamename inZOI: Creative Studio
+addappid(3092450) --Mainappid inZOI: Creative Studio
+addappid(3092451, 1, "40cc0f7748b0623c087162a85440efcc41e602b66f3542801c0911d21bca8e3b") --Main Windows Depot inZOI: Creative Studio
+setManifestid(3092451, "5944823880248170936", 17376437456)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
