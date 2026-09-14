449b2202e6f2345c54802512acfc96db1b24c338
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 04:07:17 -0400

    restock 2847250

---
+++ 2847250.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2847250.lua
+--Gamename River Town Factory: Prologue
+addappid(2847250, 1, "023b249b97e648c98d4ac6b447ac09793bb9e095ae3715230b1334ea91685158") --Mainappid River Town Factory: Prologue
+addappid(2847251, 1, "c3a01eeb4f3ec03e7c5c9e1163da614b393df968eaeaf659837c3ba75a23fd62") --Main Windows Depot River Town Factory: Prologue
+setManifestid(2847251, "5554736357909409310", 904703664)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
