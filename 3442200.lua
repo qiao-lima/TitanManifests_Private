200377d38ca006d9ee19d410ba1ae6eadb29d7f2
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 00:17:10 -0400

    restock 3442200

---
+++ 3442200.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3442200.lua
+--Gamename 小明明的世界 Playtest
+addappid(3442200) --Mainappid 小明明的世界 Playtest
+addappid(3442201, 1, "f96197f5754033405c29d89a67a46ff6266177f284044c29dd1b6d9a29623559") --Main Windows Language Depot Schinese 小明明的世界 Playtest
+setManifestid(3442201, "8080283971293161440", 889315744)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
