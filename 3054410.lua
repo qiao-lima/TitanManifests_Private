346cbb574a1a1345e08f9e74b053fcd401fd2c86
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 11:45:54 -0400

    restock 3054410

---
+++ 3054410.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3054410.lua
+--Gamename Bushside Rangers
+addappid(3054410) --Mainappid Bushside Rangers
+addappid(3054411, 1, "f3d4245e90daabdf8d1139fd3fc806309720faaf24a60902e2331d579126f314") --Main Windows Depot Bushside Rangers
+setManifestid(3054411, "2344687459059902047", 3175839520)
+--Dlcname Bushside Rangers - Original Soundtrack
+addappid(3629890) --Dlcname Bushside Rangers - Original Soundtrack
+addappid(3641871, 1, "7aced8f4f4de82c7f569c5aabebdf24108d7070ecbfbeb2b8b1eca6a217e7595") --Dlc Windows Depot Bushside Rangers - Original Soundtrack
+setManifestid(3641871, "2276599980818881896", 70851072)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
