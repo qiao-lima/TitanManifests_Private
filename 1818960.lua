92f6a8c4f317047a307b6b73f6a31c06ec02294b
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 22:56:13 -0400

    restock 1818960

---
+++ 1818960.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1818960.lua
+--Gamename ModBots
+addappid(1818960) --Mainappid ModBots
+addappid(1818961, 1, "9630475e1251418d56f3abc6abdcc33157b9aa0813c1420fe03a5f9ab387027e") --Main Windows Language Depot English ModBots
+setManifestid(1818961, "9005721336561933575", 407060128)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
