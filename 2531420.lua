1c845d2591c46077975d7693daf22c5412cefa88
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 21:40:01 -0400

    restock 2531420

---
+++ 2531420.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2531420.lua
+--Gamename Bird Watching Simulator
+addappid(2531420) --Mainappid Bird Watching Simulator
+addappid(2531421, 1, "c71c47564c0e4cf412f99269334ab4f32e275c8c4c0cb3bea763537dea890232") --Main Windows Language Depot English Bird Watching Simulator
+setManifestid(2531421, "6864568797451041567", 753012544)
