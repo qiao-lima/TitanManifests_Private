453ee03a1241534de0577f8fe082d3543745c11c
titan-cangku <titan-cangku@gamefun365.com>
2026-09-13 20:46:45 -0400

    restock 3487240

---
+++ 3487240.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3487240.lua
+--Gamename Mistaken
+addappid(3487240) --Mainappid Mistaken
+addappid(3487241, 1, "cf269848d0ade748ec4303049b93cf466aaddd52dcc03a23e1405d13501adfef") --Main Windows Depot Mistaken
+setManifestid(3487241, "4225227264284135337", 156180752)
+addappid(3487242, 1, "a5b56cc2d2742a7ca83841bcf9e1d1f6cd3597a0786af95bee65faddd107f04a") --Main Macos Depot Mistaken
+setManifestid(3487242, "9134649910174762781", 150904384)
