e2159b59ebcb39c05732ca48cd471c40c4e4ba71
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 12:07:39 -0400

    restock 4818580

---
+++ 4818580.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4818580.lua
+--Gamename 삼국지 클래식 (Three Kingdoms Classic)
+addappid(4818580) --Mainappid 삼국지 클래식 (Three Kingdoms Classic)
+addappid(4818581, 1, "798fa573e9d25d0d70e6960eafda993bb521cbd64e703d304e9159416bf88f74") --Main Windows Depot 삼국지 클래식 (Three Kingdoms Classic)
+setManifestid(4818581, "8696855810143955669", 1044449632)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+--Missing Dlcs
+--Dlcname 삼국지 클래식 (Three Kingdoms Classic) Playtest
