68155343ac1c62b457376d187d42fc0e0271131b
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 00:17:54 -0400

    restock 2930120

---
+++ 2930120.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2930120.lua
+--Gamename 愤怒的孙子们
+addappid(2930120) --Mainappid 愤怒的孙子们
+addappid(2930121, 1, "700728292236a24b02ee1beb9a3d6e1d9688f7a7941471248bc46ca1bf1d9b99") --Main Windows Depot 愤怒的孙子们
+setManifestid(2930121, "1728723133610138917", 3533144848)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
