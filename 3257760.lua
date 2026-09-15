30a34bfebf2e645f41026bf2a8335a3c3f303945
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 07:50:01 -0400

    restock 3257760

---
+++ 3257760.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3257760.lua
+--Gamename 龙沉异世录
+addappid(3257760) --Mainappid 龙沉异世录
+addappid(3257762, 1, "661e76e994783b069d29a3592ac57d47bbbafe908c227f59bde9d1e720381b73") --Main Windows Depot 龙沉异世录
+setManifestid(3257762, "5770680988737020161", 61530288)
