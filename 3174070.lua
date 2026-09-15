436eb558330852ddebda5eebc071f28291b1ca9e
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 14:30:26 -0400

    restock 3174070

---
+++ 3174070.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3174070.lua
+--Gamename Texas Hold'em Poker: Pokerist
+addappid(3174070) --Mainappid Texas Hold'em Poker: Pokerist
+addappid(3174071, 1, "3D1BC416A49EC71A4551ECFDDBA45DDFED5E68E4FCA0E718A7E566CC76684B7C") --Main Windows Depot Texas Hold'em Poker: Pokerist
+setManifestid(3174071, "2101174671770085261", 334720944)
+addappid(3174072, 1, "46044EABF5012BC3F98C9D9841EC1ED6CFE90A17D15554B0738D07EAA19BFC85") --Main Macos Depot Texas Hold'em Poker: Pokerist
+setManifestid(3174072, "5433764217808069863", 381543280)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
