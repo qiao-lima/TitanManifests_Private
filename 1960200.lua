b57642314fa65cdc1d955b895c6b8b1db6ccf8d5
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 02:15:22 -0400

    restock 1960200

---
+++ 1960200.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1960200.lua
+--Gamename AChat Next
+addappid(1960200) --Mainappid AChat Next
+addappid(1960201, 1, "014b72bf7f9951d39b6dd8dd730b9c19145025ab3e0ebd58f0fb5c58c8cd2ced") --Main Windows Depot AChat Next
+setManifestid(1960201, "3084222487997535486", 2250608000)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
