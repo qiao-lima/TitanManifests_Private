8cf4a014921251424d53139471fb5c57085fe94c
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 13:30:29 -0400

    restock 3882270

---
+++ 3882270.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3882270.lua
+--Gamename idel world
+addappid(3882270) --Mainappid idel world
+addappid(3882271, 1, "6cd4577a43045af2fc95dba079fa736818994ee4f8185c5b0d6ea5578eb08425") --Main Windows Language Depot Schinese idel world
+setManifestid(3882271, "2186176919813148187", 98731296)
