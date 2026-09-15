399890e84e1f6ee31a14260c283fa6d74ae75609
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 09:20:01 -0400

    restock 3759220

---
+++ 3759220.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3759220.lua
+--Gamename Pawsome Resort
+addappid(3759220) --Mainappid Pawsome Resort
+addappid(3759221, 1, "9d23c1f5486c9942f97f267ab872f94e48b1fcfe20d9e57948c9e09ec1d48f63") --Main Windows Depot Pawsome Resort
+setManifestid(3759221, "626678192631390332", 197672432)
