905629727a9ebdd44be725aeecd3ca9815282dd0
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 03:16:00 -0400

    restock 4209180

---
+++ 4209180.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4209180.lua
+--Gamename Escape
+addappid(4209180) --Mainappid Escape
+addappid(4209181, 1, "559166ea98f6486729b2d84307ceb19c41d97de53d6e26c259c9bea7459ca1ef") --Main Windows Depot Escape
+setManifestid(4209181, "5834384835536484166", 433220960)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
