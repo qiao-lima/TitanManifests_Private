2028fe539f73aae7364161f60d71daa7b3cae481
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 09:18:18 -0400

    restock 2556100

---
+++ 2556100.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2556100.lua
+--Gamename 骰子模拟器
+addappid(2556100) --Mainappid 骰子模拟器
+addappid(2556101, 1, "7312758917c59beff8ce9e96575ba2f88ee86f643170426688f4e0259348c232") --Main Windows Depot 骰子模拟器
+setManifestid(2556101, "8753511876093316605", 339838464)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
