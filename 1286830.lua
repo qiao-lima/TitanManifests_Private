c31599e07547eccfffe8c8ff4766343f314982c1
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 10:21:07 -0400

    restock 1286830

---
+++ 1286830.lua
@@ -0,0 +1,35 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1286830.lua
+--Gamename STAR WARS™: The Old Republic™
+addappid(1286830, 1, "5e6331ba7d4ee839d71368e6e27b9af7c39b40cc36d050fc052399bfc0c9c483") --Mainappid STAR WARS™: The Old Republic™
+addappid(1286831, 1, "5f441ab499a56229f41d6a145429972e63e773be8f3028defce3e15cd0d2c397") --Main Windows Depot STAR WARS™: The Old Republic™
+setManifestid(1286831, "4663189834934870923", 25474608)
+addappid(1286833, 1, "0dc81a7e097e5bcaa2befe6feca6001c8b4e7ec674d5e5faeae16a1d720c6e05") --Main Windows Language Depot German STAR WARS™: The Old Republic™
+setManifestid(1286833, "3507465619429431466", 17713438080)
+addappid(1286834, 1, "14b0eb51cb078a993b393e640b71ca12dc5a1b8bb53b9423259e67a2963f374a") --Main Windows Language Depot French STAR WARS™: The Old Republic™
+setManifestid(1286834, "145376802164576208", 16738302080)
+addappid(1286835, 1, "3dff041725d43f20ec9fc52b005a9c94193a7076ad086dd58d989fceb4f8c717") --Main Windows Language Depot English STAR WARS™: The Old Republic™
+setManifestid(1286835, "2637383318600361030", 16058986832)
+addappid(1286836, 1, "b21b5ef05d644660d0c7ad11d5390622abd8a6513a7efa65da7f6ef07421f1d8") --Main Windows Depot STAR WARS™: The Old Republic™
+setManifestid(1286836, "7528648999821246844", 48023448800)
+--Share Depots
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228982, "6413394087650432851", 9650016)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1303600) --Dlcname STAR WARS™: The Old Republic™: Legacy of the Sith: Digital Collector's Edition
+addappid(1303605) --Dlcname STAR WARS™: The Old Republic™ - Cartel Coins
+addappid(1303891) --Dlcname STAR WARS™: The Old Republic™ - Galactic Leisure Bundle
+addappid(1333220) --Dlcname STAR WARS™: The Old Republic™ - Subscriptions
+addappid(1346540) --Dlcname AppID 1346540
+addappid(1346541) --Dlcname STAR WARS™: The Old Republic™ - Sith Bundles
+addappid(2550190) --Dlcname STAR WARS™: The Old Republic™ - Join the Fight Bundle
+addappid(3076010) --Dlcname AppID 3076010
+addappid(3076020) --Dlcname AppID 3076020
+addappid(3076030) --Dlcname AppID 3076030
+addappid(4005010) --Dlcname STAR WARS™: The Old Republic™ - Enter the Fight
+addappid(4023370) --Dlcname STAR WARS™: The Old Republic™ - Master the Fight: Mobile Market Edition
+addappid(4023380) --Dlcname STAR WARS™: The Old Republic™ - Continue the Fight
+addappid(4029110) --Dlcname STAR WARS™: The Old Republic™ - Master the Fight: Cargo Transport Edition
