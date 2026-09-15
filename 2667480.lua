f67aae9f942996dc74ee51c407bf0136cc25fc77
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 00:56:05 -0400

    restock 2667480

---
+++ 2667480.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2667480.lua
+--Gamename Wolf Sheep Cabbage
+addappid(2667480) --Mainappid Wolf Sheep Cabbage
+addappid(2667481, 1, "46026a913d3d5f5aa4f398dc5443f80e4412d682865c7c9cea16c9ce5e6163b3") --Main Windows Depot Wolf Sheep Cabbage
+setManifestid(2667481, "4213003498591740722", 31408336)
