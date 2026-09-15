e67a24867ec54057dcfacac14d15bcbf303b824b
titan-cangku <titan-cangku@gamefun365.com>
2026-09-14 21:22:41 -0400

    restock 4838470

---
+++ 4838470.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4838470.lua
+--Gamename My (♂) Life as a Vampire's Maid (♀)
+addappid(4838470) --Mainappid My (♂) Life as a Vampire's Maid (♀)
+addappid(4838471, 1, "0c8cf85a43b705ad241aa3483f14631f3b62f68fba1973622b85a5898b36a5fb") --Main Windows Depot My (♂) Life as a Vampire's Maid (♀)
+setManifestid(4838471, "292284628374256066", 1544674528)
+addappid(4838472, 1, "ad8190649b205110e02c061f7cb48a0d47916bd0e749a05221800231c7940f33") --Main Windows Language Depot English My (♂) Life as a Vampire's Maid (♀)
+setManifestid(4838472, "4695198193599260912", 144)
+addappid(4838473, 1, "d697cfbe2524ebee775151fb518585f4c8d3d48b589856624d1a714755e4ab24") --Main Windows Language Depot Schinese My (♂) Life as a Vampire's Maid (♀)
+setManifestid(4838473, "4933890284279683447", 144)
+addappid(4838474, 1, "8c5a32b867777f44bde53887f89a5abde9bd1f86cb2967515a967e077f31afdc") --Main Windows Language Depot Tchinese My (♂) Life as a Vampire's Maid (♀)
+setManifestid(4838474, "7031296528439466294", 144)
