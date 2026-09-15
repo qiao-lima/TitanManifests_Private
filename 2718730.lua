a8ede50d764fe842bfb7962fbf1dd2c0ce3251bf
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 13:20:04 -0400

    restock 2718730

---
+++ 2718730.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2718730.lua
+--Gamename 热血大唐
+addappid(2718730) --Mainappid 热血大唐
+addappid(2718731, 1, "d5c7583bf768f839222684a9e730132130d8d454d6c8c4502970f5ba482857d8") --Main Windows Depot 热血大唐
+setManifestid(2718731, "5665613307304580822", 505939248)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
