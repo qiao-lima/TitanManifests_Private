e431d828bec2fdb705b6bf6c2477af1bc02c5ae7
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 03:24:04 -0400

    restock 3963030

---
+++ 3963030.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3963030.lua
+--Gamename Pickle Pete
+addappid(3963030) --Mainappid Pickle Pete
+addappid(3963031, 1, "56713d06c37d88950d6b48d7afa383180e36ce2d8aa6151262dbfb125d0703f6") --Main Windows/Linux Depot Pickle Pete
+setManifestid(3963031, "1278709204699313100", 481858384)
