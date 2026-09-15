ed32a37e3daa73d595280e1fff3aee23e0f80c5e
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 14:17:38 -0400

    restock 4811410

---
+++ 4811410.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4811410.lua
+--Gamename Zombie Queen
+addappid(4811410) --Mainappid Zombie Queen
+addappid(4811411, 1, "97e7eca5e9c7586f9dd8d71127929bf4dec997d92895895ca20222740d3c0161") --Main Windows Depot Zombie Queen
+setManifestid(4811411, "2169726926169189423", 4693960800)
