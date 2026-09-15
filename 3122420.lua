59fc5c6139025b5193d82f3b904ca884ab9a3803
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 07:26:42 -0400

    restock 3122420

---
+++ 3122420.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3122420.lua
+--Gamename Five Nights And VTBs Demo
+addappid(3122420) --Mainappid Five Nights And VTBs Demo
+addappid(3122421, 1, "c4715566e1aede13dac870d54771ea60d42a4181fbdc9aada3826fdea2542cc4") --Main Windows Depot Five Nights And VTBs Demo
+setManifestid(3122421, "2702481569923207624", 544845296)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
