9b0427692233acef18f999af463ec1ab60f8a637
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 07:20:06 -0400

    restock 1815780

---
+++ 1815780.lua
@@ -0,0 +1,17 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1815780.lua
+--Gamename Asphalt Legends
+addappid(1815780, 1, "f1c3e8bb4e041dac475812b84aed835b9f7bdb135f8522a1dc947db417ebd74f") --Mainappid Asphalt Legends
+addappid(1815781, 1, "5bcde2a5b270cc5f3a1a571b16fba5c90347280e05940c8ac301a5a38df2a451") --Main Windows Depot Asphalt Legends
+setManifestid(1815781, "2611822832028185694", 9822485568)
+addappid(1815783, 1, "6dfefc3ab40f2cd060163b974417bee18cbee8cb1ad5c7cb9e8a10e4a4a71878") --Main Macos Depot Asphalt Legends
+setManifestid(1815783, "8877127493147430699", 8499164816)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+--Missing Dlcs
+--Dlcname ALU_DLC_01
+--Dlcname ALU_DLC_02
+--Dlcname Asphalt Legends - Single-Player Boost Pack
