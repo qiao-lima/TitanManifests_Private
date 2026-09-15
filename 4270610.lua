9db790e9cf3c24a0c2aedb12d928df14e3597acc
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 13:35:01 -0400

    restock 4270610

---
+++ 4270610.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4270610.lua
+--Gamename 放置:纸色新境
+addappid(4270610) --Mainappid 放置:纸色新境
+addappid(4270611, 1, "857d50525b251d140ce8ae0d20c24778d930b1370aeae007d661bcdc0d769992") --Main Windows Depot 放置:纸色新境
+setManifestid(4270611, "7734555636921427708", 320929200)
