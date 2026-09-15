1810008ca76c2674c894ebafa5683c0b59931ec4
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 13:22:31 -0400

    restock 2332220

---
+++ 2332220.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2332220.lua
+--Gamename Marionette Mates
+addappid(2332220) --Mainappid Marionette Mates
+addappid(2332221, 1, "9e0fa7f79c50d0f1146978d7ed4d9143e6a20070fd467e6bcb3fb72d2f307c44") --Main Windows Depot Marionette Mates
+setManifestid(2332221, "472647526749692199", 114099120)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
