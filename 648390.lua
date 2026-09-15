d5c0b3332ce5dd63227fe42d0938a0fde74983a7
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 13:22:20 -0400

    restock 648390

---
+++ 648390.lua
@@ -0,0 +1,17 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 648390.lua
+--Gamename Marie's Room
+addappid(648390, 1, "50cb15ec26304f59be2da32b476fe48e86350a570b4718297be9020623ab99a6") --Mainappid Marie's Room
+addappid(648391, 1, "3b9267f4ba84e272bd511f2fe7ca298a2b636e40c4b0fdb7fd05afc51c0e556a") --Main Windows Depot Marie's Room
+setManifestid(648391, "3665055097181273456", 686648064)
+--Dlcname Marie's Room - Soundtrack
+addappid(855020) --Dlcname Marie's Room - Soundtrack
+addappid(648392, 1, "ff13a97c6279174f1f56a4a48c6d813fae6e5549816f6c2d17773019c9698e00") --Dlc Windows Depot Marie's Room - Soundtrack
+setManifestid(648392, "7076304148289637656", 32159808)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+--Missing Dlcs
+--Dlcname Marie's Room - Artbook
