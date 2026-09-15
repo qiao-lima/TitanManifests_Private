ab4c8d162d6561f5d46932929496106ad6a40633
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 13:35:06 -0400

    restock 2434370

---
+++ 2434370.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2434370.lua
+--Gamename 放置地牢-AutoDungeon
+addappid(2434370) --Mainappid 放置地牢-AutoDungeon
+addappid(2434371, 1, "8b8e45d19d3fa99b66d12a1275c434f33e83f2c328b6b8b3173689af13cf03eb") --Main Windows Depot 放置地牢-AutoDungeon
+setManifestid(2434371, "6702848706573741129", 495157376)
