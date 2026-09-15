35c069e744cc05ad437eb90ffa699df669a56b37
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 10:00:31 -0400

    restock 3201830

---
+++ 3201830.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3201830.lua
+--Gamename The Hidden Scrolls
+addappid(3201830) --Mainappid The Hidden Scrolls
+addappid(3201831, 1, "ea8380cfdf5611aa6df9648daddcb0bef250d270a50af88d9a3f1efce20c943e") --Main Windows Depot The Hidden Scrolls
+setManifestid(3201831, "2307691679686657646", 1675951760)
