8d0cb07481f8bdfaa78367a5af39145b9e34de3e
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 08:30:01 -0400

    restock 4888520

---
+++ 4888520.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4888520.lua
+--Gamename 像素三国-僵尸地牢
+addappid(4888520) --Mainappid 像素三国-僵尸地牢
+addappid(4888521, 1, "50392ea17102b73ef971581702b371c2bc1b62a7710bed05de839097aaf7159f") --Main Windows Depot 像素三国-僵尸地牢
+setManifestid(4888521, "2637443558049913620", 130165872)
+addappid(4888523, 1, "87d197a79c4340f656d9901e00513ca02738f3e911655ff7c5413edc37d6a551") --Main Windows Depot 像素三国-僵尸地牢
+setManifestid(4888523, "5330059802430952458", 118479168)
+--Missing Dlcs
+--Dlcname 像素三国-僵尸地牢 Demo
