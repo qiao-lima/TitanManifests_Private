ce785e62b93b3e296cf74735f06b427d51087c21
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 07:45:02 -0400

    restock 2074920

---
+++ 2074920.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2074920.lua
+--Gamename The First Descendant
+addappid(2074920, 1, "c1bf7fcf7b2544fa5c6dc2e0f63344ff3d6e8deed3e588fe6f67aaf8e4e0af42") --Mainappid The First Descendant
+addappid(2074921, 1, "7b71a400e55a9f6781b1a47daa3ea3754b321c2fee535fbe882be8233fe557b3") --Main Windows Depot The First Descendant
+setManifestid(2074921, "8430703906037322649", 79036072400)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
