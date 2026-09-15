fc9224d1ebb1ecefe7800c9732068d1daa4f8216
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 07:10:01 -0400

    restock 4459820

---
+++ 4459820.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4459820.lua
+--Gamename Touhou Yukkuri Mountain
+addappid(4459820) --Mainappid Touhou Yukkuri Mountain
+addappid(4459821, 1, "ab95bfe3c5842b7c2ae9e1fb8afa1b86ab2da0902d82ac44b1e7180f040806c8") --Main Windows Depot Touhou Yukkuri Mountain
+setManifestid(4459821, "8040448686993701481", 152614704)
+--Missing Dlcs
+--Dlcname Touhou Yukkuri Mountain ORIGINAL SOUNDTRACK
