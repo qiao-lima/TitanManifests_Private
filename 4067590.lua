668f2fab3502b6da938cf761622a1d9c5da5796c
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 07:18:34 -0400

    restock 4067590

---
+++ 4067590.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4067590.lua
+--Gamename VirtuStick
+addappid(4067590) --Mainappid VirtuStick
+addappid(4067591, 1, "ed34a8ae2c2bf347f460bb9fef6c4994a3e655672888ce1b437317ab7c118df2") --Main Windows Depot VirtuStick
+setManifestid(4067591, "3921357749879024009", 116643728)
