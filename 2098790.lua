0750cea59512fcf50a9cd7c0ba2d8ac8b79bc378
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 13:24:02 -0400

    restock 2098790

---
+++ 2098790.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2098790.lua
+--Gamename 群侠传，启动！
+addappid(2098790, 1, "4f2934bc191ac93df7c21d0a6234b7c8aed9f87d5bda7d34b8138e81c1cb2166") --Mainappid 群侠传，启动！
+addappid(2098791, 1, "825cf47c37ca7e83ec4af73345f95cd74fdb642125da0e4815523fce51addae6") --Main Windows Depot 群侠传，启动！
+setManifestid(2098791, "6417719649220377782", 1119539936)
+--Missing Dlcs
+--Dlcname 群侠传，启动 - 捐款（源代码+完整工程）
