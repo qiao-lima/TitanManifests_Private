12de735623c7a55a69bbaa13089225a5c6b58ef2
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 13:20:11 -0400

    restock 753090

---
+++ 753090.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 753090.lua
+--Gamename Marius
+addappid(753090) --Mainappid Marius
+addappid(753091, 1, "214506cf0a714d1cebeb3d14eaab2f314a2556eb52f6bc66cae2b51b8910a53f") --Main Windows Depot Marius
+setManifestid(753091, "4205725592849551016", 596619408)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
