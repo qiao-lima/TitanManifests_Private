101e9d62cd22d4ab43d6f2a8f16cd2e507ae5f08
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 13:25:01 -0400

    restock 1881610

---
+++ 1881610.lua
@@ -0,0 +1,31 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1881610.lua
+--Gamename 江湖客栈-The Jianghu
+addappid(1881610) --Mainappid 江湖客栈-The Jianghu
+addappid(1881611, 1, "71c6d10530299d341b172ab6c29122935953738ae418806ab848181d50c8d59a") --Main Windows Depot 江湖客栈-The Jianghu
+setManifestid(1881611, "224767358044655497", 1392781456)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
+addappid(229010, 1, "9ea8b334311d1b69b962aac11d4cce01f3dfb21722d4198c12314f2d67a0196f") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229010, "3476809898398882586", 7193488)
+addappid(229011, 1, "1752f232024a07db347d180533cf95ce5a1233ae3bad0b0093949174079898df") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229011, "392351049714934122", 6911680)
+addappid(229012, 1, "421e499f39a617c1a3b6baf11fbc65ffebd541d2c39c38d3bf6ffbd87919ce12") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229012, "4353723233161159493", 6387264)
+addappid(229030, 1, "92f0f88867c8d5f351090eb2d27aeb50e2b621ae8a34708265d2c20c36fbcf5e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229030, "1043465440436835055", 50071472)
+addappid(229031, 1, "98d01f1eaa90ae08f978994e043bcbbf1f23fc72a325528ca7d722c827c0eb54") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229031, "7746630274301172884", 25905472)
+addappid(229032, 1, "15b3308d4e95d1156182211bab1c823b6df0a06fd74a5c0833abed89bc307cfb") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229032, "3616495131483866412", 40038080)
+addappid(229033, 1, "9a789440bbc16131547ac6c062e5f168f7684da533a2899a71fdfaaeec21efc3") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229033, "2059065101492814639", 59325760)
