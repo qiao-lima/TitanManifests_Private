0d739d43af6e4053efff7d95979549af5fa0032b
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 01:54:21 -0400

    restock 1420320

---
+++ 1420320.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1420320.lua
+--Gamename Let's Learn Japanese! Kanji Sudoku
+addappid(1420320) --Mainappid Let's Learn Japanese! Kanji Sudoku
+addappid(1420321, 1, "5203a7ad73aa4d82428606e04c53a82148f505963a1de1226a3e38728735b223") --Main Windows Depot Let's Learn Japanese! Kanji Sudoku
+setManifestid(1420321, "8313573774014005633", 262391216)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
