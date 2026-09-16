e13bc8ebfdaed618488c39e4c69c3cb44285497e
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 21:14:52 -0400

    restock 1114260

---
+++ 1114260.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1114260.lua
+--Gamename Under What?
+addappid(1114260) --Mainappid Under What?
+addappid(1114261, 1, "066ec84014f77073a1314d320e630ece8dc79bb0aa8fc27fcbcde5635af1175d") --Main Windows Depot Under What?
+setManifestid(1114261, "7820666449413945615", 108802112)
