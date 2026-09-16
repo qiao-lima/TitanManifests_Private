3618d6cff587b2f6aac57c7a444879e2cf8f029d
titan-cangku <titan-cangku@gamefun365.com>
2026-09-16 05:40:01 -0400

    restock 1098840

---
+++ 1098840.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1098840.lua
+--Gamename ZACH-LIKE
+addappid(1098840) --Mainappid ZACH-LIKE
+addappid(1098841, 1, "12214f0979c7e04813af92293b722d9f5efe2a422b4e3aa36f969d0f36774fc6") --Main Windows Depot ZACH-LIKE
+setManifestid(1098841, "5050166463256387022", 907891376)
+--Share Depots
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229000, "4622705914179893434", 242681440)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
+addappid(229010, 1, "9ea8b334311d1b69b962aac11d4cce01f3dfb21722d4198c12314f2d67a0196f") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229010, "3476809898398882586", 7193488)
