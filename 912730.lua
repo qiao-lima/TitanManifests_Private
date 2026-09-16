6a97a5f9172643770306a6918c7307e0e0dcaab7
titan-cangku <titan-cangku@gamefun365.com>
2026-09-16 08:25:06 -0400

    restock 912730

---
+++ 912730.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 912730.lua
+--Gamename Gamble of Gods
+addappid(912730) --Mainappid Gamble of Gods
+addappid(912731, 1, "9eab5659096a2ef1c9c9d1f7489625b1b49a5173c4ac7dca7b42803c89036ca5") --Main Windows Depot Gamble of Gods
+setManifestid(912731, "1384227338617178310", 523143840)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
