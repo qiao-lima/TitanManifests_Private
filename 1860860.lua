7a848505af8d505fb31dfe32fedfa335dc9a132f
titan-cangku <titan-cangku@gamefun365.com>
2026-09-16 11:35:54 -0400

    restock 1860860

---
+++ 1860860.lua
@@ -0,0 +1,17 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1860860.lua
+--Gamename Flower girl 2
+addappid(1860860) --Mainappid Flower girl 2
+addappid(1860861, 1, "fa22774a9d0baeb0835562cc5e81a83b31895b24390bb91990cb91e68cf001d2") --Main Windows Depot Flower girl 2
+setManifestid(1860861, "4968971053556605220", 323595936)
+--Dlcname Flower girl 2 - 5 new characters bonus
+addappid(2026550, 1, "f6ae664ff9f40645bf8b0e4c7cb690da8e384ff320b1570b60dd3daf4fe23caa") --Dlcname Flower girl 2 - 5 new characters bonus
+setManifestid(2026550, "2397745884713861947", 3509088)
+--Dlcname Flower girl 2 - Complete edition
+addappid(2026560, 1, "496c293603cf2024ec63f37569a3ef7370532af029584be370c5228e67ce2216") --Dlcname Flower girl 2 - Complete edition
+setManifestid(2026560, "986089852532481901", 874502624)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
