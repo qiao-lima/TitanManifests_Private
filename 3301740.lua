4413abf76fc290f654d4cc7c1eb0ec0da0d2ba68
titan-cangku <titan-cangku@gamefun365.com>
2026-09-16 07:10:08 -0400

    restock 3301740

---
+++ 3301740.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3301740.lua
+--Gamename TCG Master Trader
+addappid(3301740) --Mainappid TCG Master Trader
+addappid(3301741, 1, "a4d4d0d3c4a84b69eb59a26136d2616d4ec82d4f383561056a8b46cf2030c143") --Main Windows Depot TCG Master Trader
+setManifestid(3301741, "3289958100230915512", 483563312)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
