19dc0317c6f11a1a05457c54a9653e8f81b43a41
titan-cangku <titan-cangku@gamefun365.com>
2026-09-16 04:45:00 -0400

    restock 4328290

---
+++ 4328290.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4328290.lua
+--Gamename EagleRoad:WorldRise
+addappid(4328290) --Mainappid EagleRoad:WorldRise
+addappid(4328291, 1, "d35957bcf4535149bb18f43a65dce9da2353f134eb981207d03ccd6d4e6a763b") --Main Windows Depot EagleRoad:WorldRise
+setManifestid(4328291, "1242487395980781304", 16997370624)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
