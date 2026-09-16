19933c9db1366b5bff2a92a5971fec63678f1571
titan-cangku <titan-cangku@gamefun365.com>
2026-09-16 13:47:30 -0400

    restock 4825080

---
+++ 4825080.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4825080.lua
+--Gamename Night of Seven Dreams
+addappid(4825080) --Mainappid Night of Seven Dreams
+addappid(4825081, 1, "d06bce82289e9f5ba04fde39bf353ab29c6fe4ae287252bd53b870b615eb54b4") --Main Windows Depot Night of Seven Dreams
+setManifestid(4825081, "4731922782046780439", 6173654304)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
