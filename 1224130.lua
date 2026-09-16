ba9eb798e3467e4f9ca8745fbb45dc5f116bff84
titan-cangku <titan-cangku@gamefun365.com>
2026-09-16 07:38:59 -0400

    restock 1224130

---
+++ 1224130.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1224130.lua
+--Gamename DREAMLIVE
+addappid(1224130) --Mainappid DREAMLIVE
+addappid(1224131, 1, "af76545256cf33299628b2f4242e3e1ccac76e17f94b5421f081dbd6677729de") --Main Windows Depot DREAMLIVE
+setManifestid(1224131, "4126577760875025655", 23883576512)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
