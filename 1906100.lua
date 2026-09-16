54d1f405fdc1f31447e97e0ff5176040c8a02cb5
titan-cangku <titan-cangku@gamefun365.com>
2026-09-16 09:29:31 -0400

    restock 1906100

---
+++ 1906100.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1906100.lua
+--Gamename Mirror Shoot
+addappid(1906100) --Mainappid Mirror Shoot
+addappid(1906101, 1, "d3ec4ca9bf1874642155e50e38a6c6855ceb37f78d45256fcfad71a0eed99c32") --Main Windows Depot Mirror Shoot
+setManifestid(1906101, "6229126136174461007", 371210672)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
