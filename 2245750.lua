5c6fb7072c2deb3efcae097ca6047a5ad9b263a4
titan-cangku <titan-cangku@gamefun365.com>
2026-09-16 12:05:06 -0400

    restock 2245750

---
+++ 2245750.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2245750.lua
+--Gamename City Z
+addappid(2245750) --Mainappid City Z
+addappid(2245751, 1, "aefe6ee0797aed461360b8886f49fffedce9cc37c373f8d81ed0627805b5a702") --Main Windows Depot City Z
+setManifestid(2245751, "2157577261248283382", 3046157120)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
