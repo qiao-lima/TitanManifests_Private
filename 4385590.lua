50992c6f8d54251d10921d7d64534ed41eb29c1d
titan-cangku <titan-cangku@gamefun365.com>
2026-09-16 10:20:48 -0400

    restock 4385590

---
+++ 4385590.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4385590.lua
+--Gamename 大荒消劫录
+addappid(4385590) --Mainappid 大荒消劫录
+addappid(4385591, 1, "279c5a880bcf0693ad044e0141cd03ce06eed114c3dd3416756da2d07d971442") --Main Windows Depot 大荒消劫录
+setManifestid(4385591, "6624556358869103932", 963729776)
