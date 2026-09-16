19662b5c6fa6389841d9086a85f4e4502d0f696c
titan-cangku <titan-cangku@gamefun365.com>
2026-09-16 05:40:13 -0400

    restock 2737160

---
+++ 2737160.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2737160.lua
+--Gamename Za češtiny ve hrách!
+addappid(2737160) --Mainappid Za češtiny ve hrách!
+addappid(2737161, 1, "a1b035dec4c6b56e3ac32c1fb967e71afe6331ab38a3472376275b8efc400995") --Main Windows Depot Za češtiny ve hrách!
+setManifestid(2737161, "4049308706439668470", 214835504)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
