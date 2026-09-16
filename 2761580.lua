ea601a19dbccc4efb4a38b4dcf1667a6e127b683
titan-cangku <titan-cangku@gamefun365.com>
2026-09-16 07:38:50 -0400

    restock 2761580

---
+++ 2761580.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2761580.lua
+--Gamename Drealegy
+addappid(2761580) --Mainappid Drealegy
+addappid(2761581, 1, "66625bbac3d5b8d307f5d503eb8514a836ebabbd774598fadd47c2f0a0897e78") --Main Windows Depot Drealegy
+setManifestid(2761581, "4210356289528984853", 355406816)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
