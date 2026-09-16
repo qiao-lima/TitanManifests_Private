e776bf802fa95b08b7a8ffb8c4bcb413e572e2ce
titan-cangku <titan-cangku@gamefun365.com>
2026-09-16 02:08:06 -0400

    restock 3655220

---
+++ 3655220.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3655220.lua
+--Gamename Heroes of Mount Dragon Playtest
+addappid(3655220) --Mainappid Heroes of Mount Dragon Playtest
+addappid(3655221, 1, "d6bede9f044beee33a3502430ef3ebeef51854eda36f1ea0f456d84238b21606") --Main Windows Depot Heroes of Mount Dragon Playtest
+setManifestid(3655221, "524448755448739658", 2165905456)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
