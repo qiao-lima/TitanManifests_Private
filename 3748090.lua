399999d1b62c6c8c83d7c6329bc74418a821e050
titan-cangku <titan-cangku@gamefun365.com>
2026-09-16 13:47:23 -0400

    restock 3748090

---
+++ 3748090.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3748090.lua
+--Gamename Seven Hearts
+addappid(3748090) --Mainappid Seven Hearts
+addappid(3748091, 1, "748a884b2f571138a0268e6565d758bfa95fbb9e7a7b5d5c007f1e718b96755d") --Main Windows Depot Seven Hearts
+setManifestid(3748091, "8104715711611496335", 613443648)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
