4e68edbff99a05fed3c0e8022de2872680c5d81a
titan-cangku <titan-cangku@gamefun365.com>
2026-09-16 10:20:13 -0400

    restock 555440

---
+++ 555440.lua
@@ -0,0 +1,21 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 555440.lua
+--Gamename Deathgarden: BLOODHARVEST
+addappid(555440) --Mainappid Deathgarden: BLOODHARVEST
+addappid(555441, 1, "f61436a9e031da3683880eb427897fbedf798806e8b42b0f0f3a42d2806c9c1e") --Main Windows Depot Deathgarden: BLOODHARVEST
+setManifestid(555441, "847239704319176246", 9812024672)
+--Dlcname DEATHGARDEN - Origin Sets & Artbook
+addappid(872770, 1, "f0570f6119fb71f256eab5bc281530239446eae81bf14c50a961adea6732b92f") --Dlcname DEATHGARDEN - Origin Sets & Artbook
+setManifestid(872770, "6616785764602299797", 263694704)
+--Dlcname DEATHGARDEN - Original Soundtrack
+addappid(915350, 1, "495dd69102910d72dcec4db184a711edcc2d056e75de801f4f44b0ca784a395d") --Dlcname DEATHGARDEN - Original Soundtrack
+setManifestid(915350, "1803056620477543068", 203215296)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(920440) --Dlcname AppID 920440
+addappid(1024660) --Dlcname DEATHGARDEN - Terminator Set
+addappid(1083130) --Dlcname AppID 1083130
+addtoken(1083130, "15884459045659772886")
