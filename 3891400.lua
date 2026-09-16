45b800e9d0c7976252272cd2f84d629d9bcb0021
titan-cangku <titan-cangku@gamefun365.com>
2026-09-16 06:10:09 -0400

    restock 3891400

---
+++ 3891400.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3891400.lua
+--Gamename Mixed Spirits: Bartender
+addappid(3891400) --Mainappid Mixed Spirits: Bartender
+addappid(3891401, 1, "078ede55794a7752650b08bd3f404fb5f18028b0a0e50b585617c692b04c3331") --Main Windows Depot Mixed Spirits: Bartender
+setManifestid(3891401, "3266107397778334396", 846525120)
+addappid(3891402, 1, "3f82c9162042223bf36bba89fbd97c8aa832ad8c81eea48e37df5990627487cf") --Main Macos Depot Mixed Spirits: Bartender
+setManifestid(3891402, "5622359647144612565", 854834512)
+addappid(3891403, 1, "e842d67d7ab1210af461c69b24e977e0f457f4385fb2713fb4c99630ed56924b") --Main Linux Depot Mixed Spirits: Bartender
+setManifestid(3891403, "623917331743559214", 844122096)
