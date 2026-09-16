82ddbcf045019501b9237a3ff9090a6445c96922
titan-cangku <titan-cangku@gamefun365.com>
2026-09-16 10:55:18 -0400

    restock 1682330

---
+++ 1682330.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1682330.lua
+--Gamename The Dead Await: Prologue
+addappid(1682330) --Mainappid The Dead Await: Prologue
+addappid(1682331, 1, "8b07ee2857dbc9b3010eb51d2393e2989a84fbcb4409bbdb01159ff524c01385") --Main Windows Depot The Dead Await: Prologue
+setManifestid(1682331, "1187171268040811802", 885925024)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
