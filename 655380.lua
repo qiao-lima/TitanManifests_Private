67b714f81dce89f1e373abca0e0ed42e80a68976
titan-cangku <titan-cangku@gamefun365.com>
2026-09-16 04:00:21 -0400

    restock 655380

---
+++ 655380.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 655380.lua
+--Gamename Dig Dog
+addappid(655380) --Mainappid Dig Dog
+addappid(655381, 1, "4c2a4bb4d15328881db0ed55492393e5b364b965560c6eacf7912bf75e1a53c0") --Main Windows Depot Dig Dog
+setManifestid(655381, "3750529607189698949", 95457760)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
