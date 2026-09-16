7b9ff15f661eaf080116553f0f9799099719a3ee
titan-cangku <titan-cangku@gamefun365.com>
2026-09-16 10:20:08 -0400

    restock 3085780

---
+++ 3085780.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3085780.lua
+--Gamename Private Garden
+addappid(3085780) --Mainappid Private Garden
+addappid(3085781, 1, "E298E0CF220397C6C2EA698378719D6201F36FE578CBED9B44BFD6381577170A") --Main Windows Depot Private Garden
+setManifestid(3085781, "5381488224317790328", 810834048)
