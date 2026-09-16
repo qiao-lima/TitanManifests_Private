66eaec79086405b4978284d70dd80ff92f9b37a8
titan-cangku <titan-cangku@gamefun365.com>
2026-09-15 21:38:24 -0400

    restock 540250

---
+++ 540250.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 540250.lua
+--Gamename Tank Battle: 1945
+addappid(540250) --Mainappid Tank Battle: 1945
+addappid(540251, 1, "df3ceaf496a3e1ff0c5c0b10591f9df8ae6278309c0748bd4d77571c66393758") --Main Windows Depot Tank Battle: 1945
+setManifestid(540251, "6182985132521520784", 297028864)
+addappid(540252, 1, "10197e2586254a08356192cc2e5c5a2d2077b74ec9c17431a6613abd49ccc49b") --Main Macos Depot Tank Battle: 1945
+setManifestid(540252, "2273227813273757028", 304631680)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
