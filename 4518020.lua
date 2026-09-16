d8996281a03648906b1d11b8da30da3a7a5c5946
titan-cangku <titan-cangku@gamefun365.com>
2026-09-16 12:30:27 -0400

    restock 4518020

---
+++ 4518020.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4518020.lua
+--Gamename Ward X
+addappid(4518020) --Mainappid Ward X
+addappid(4518021, 1, "1cbeb3a20dc8fb4780aaa65b234deeabaf7e054b2b0121d9198b25b762f06d59") --Main Windows Depot Ward X
+setManifestid(4518021, "2841755423650283974", 1725618624)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
