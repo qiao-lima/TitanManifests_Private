f7160c741e258dc32506e9d76c68d61be8121727
titan-cangku <titan-cangku@gamefun365.com>
2026-09-16 23:05:01 -0400

    restock 1351170

---
+++ 1351170.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1351170.lua
+--Gamename 東方催狐譚 ～ Servants of Harvest Wish
+addappid(1351170) --Mainappid 東方催狐譚 ～ Servants of Harvest Wish
+addappid(1351171, 1, "8c5d458c320ed5fd9f3ebdf50b4f31325c15c518665d514d015c1819107045c9") --Main Windows Depot 東方催狐譚 ～ Servants of Harvest Wish
+setManifestid(1351171, "7147990529425738092", 178856320)
+--Dlcname 東方催狐譚 ～ Servants of Harvest Wish Soundtrack
+addappid(1353960) --Dlcname 東方催狐譚 ～ Servants of Harvest Wish Soundtrack
+addappid(1353961, 1, "5b1a6bd2c9e5e5ed011aff8b5d718d5b88ea1021838abcc8ebbec08e98ab5184") --Dlc Windows Depot 東方催狐譚 ～ Servants of Harvest Wish Soundtrack
+setManifestid(1353961, "8094370934317141924", 321013024)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
