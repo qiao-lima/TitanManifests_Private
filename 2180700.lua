8466e1c9b843661a4e741b9872753fdf6f958be7
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 16:23:37 -0400

    restock 2180700

---
+++ 2180700.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2180700.lua
+--Gamename ABI-DOS
+addappid(2180700) --Mainappid ABI-DOS
+addappid(2180701, 1, "1a3afd2b5bca70b964b2d7ac5c0bccbf8053789ba9850a7296df35e61999b7ac") --Main Windows Depot ABI-DOS
+setManifestid(2180701, "3269240194076402282", 427039856)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
