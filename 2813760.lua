e8c1a4f9d5eda038a9167955bad9a462cb68a3d5
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 12:25:18 -0400

    restock 2813760

---
+++ 2813760.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2813760.lua
+--Gamename Anomaly Loop
+addappid(2813760) --Mainappid Anomaly Loop
+addappid(2813761, 1, "32b6052f5117fc62d142b413ba5f8233a7c1389504f07db8c924f0300bb29466") --Main Windows Depot Anomaly Loop
+setManifestid(2813761, "8997525346713065233", 4145800544)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
