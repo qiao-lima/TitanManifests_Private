7cc753aff2d0f52a40790f98f4f68a7a16aa257d
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 17:21:03 -0400

    restock 3816890

---
+++ 3816890.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3816890.lua
+--Gamename Still It Runs
+addappid(3816890) --Mainappid Still It Runs
+addappid(3816891, 1, "5999cfb8b0f77ff9c728d921bf4d30c63b73b09e9099a53dde03b9a3f4eb3807") --Main Windows Depot Still It Runs
+setManifestid(3816891, "3960976791740191535", 2050939072)
