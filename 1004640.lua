c049240ce4b511fb8e3d91644f5ca9d0a29c8bc9
qiao-lima <qiao-lima@github.com>
2026-09-18 03:23:07 +0800

    Manual Update 1004640

---
+++ 1004640.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1004640.lua
+--Gamename FINAL FANTASY TACTICS - The Ivalice Chronicles
+addappid(1004640, 1, "bd74df1e233ffd3920f8bbf8fe4dc604f0793b026dea9d94c6ea296b7b8df896") --Mainappid FINAL FANTASY TACTICS - The Ivalice Chronicles
+addappid(1004641, 1, "b9ffdc9182829343bcdf50e8c3fe66b2fc58fbbe11980078cfc04e6f8d91701d") --Main Windows Depot FINAL FANTASY TACTICS - The Ivalice Chronicles
+setManifestid(1004641, "6104220840168111357", 9351358400)
+addappid(3681881, 1, "09425b64f789f5f1235017d941cee4891e4b633d840dfc6aeff8a13696582582") --Main Windows Depot FINAL FANTASY TACTICS - The Ivalice Chronicles
+setManifestid(3681881, "1473685029653626545", 436074880)
+--Dlcname FINAL FANTASY TACTICS Complete Soundtrack
+addappid(3681880) --Dlcname FINAL FANTASY TACTICS Complete Soundtrack
+addappid(3589750) --Dlcname FINAL FANTASY TACTICS - The Ivalice Chronicles: Pre-order Bonuses
+addappid(3589760) --Dlcname FINAL FANTASY TACTICS - The Ivalice Chronicles: Deluxe Edition Bonuses
