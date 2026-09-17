d721ea65eac2980a3c0c7a5d923558bb820c7ef3
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 10:27:35 -0400

    restock 2431510

---
+++ 2431510.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2431510.lua
+--Gamename BlueSuburbia
+addappid(2431510) --Mainappid BlueSuburbia
+addappid(2431511, 1, "5c9bf5937a1c3f30c86f879d772d0705ba4b37e386e1ab80a1853670b88c8106") --Main Windows Language Depot English BlueSuburbia
+setManifestid(2431511, "2795397262848308539", 4330749520)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
