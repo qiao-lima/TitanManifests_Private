487c404ccc85b95d94aa3f198df68812f280003f
qiao-lima <qiao-lima@github.com>
2026-09-18 03:23:07 +0800

    Manual Update 2273420

---
+++ 2273420.lua
@@ -0,0 +1,17 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2273420.lua
+--Gamename Peeping Dorm Manager
+addappid(2273420, 1, "f043070f4379c7a23f30aa11152267f5877d7c2d3ec095c28decd561a9b152e7") --Mainappid Peeping Dorm Manager
+addappid(2273421, 1, "2d351254c8aba18f5f0d54e49c256013359c2b32d66799579b6e65df85c4b1d4") --Main Windows Depot Peeping Dorm Manager
+setManifestid(2273421, "7345122348083809308", 588273536)
+--Dlcname Peeping Dorm Manager Soundtrack
+addappid(2714790) --Dlcname Peeping Dorm Manager Soundtrack
+addappid(2714791, 1, "2ab96d79039223c9c1b741a5ce0d39307a486c162f69902ba3a3f8823105e96f") --Dlc Windows Depot Peeping Dorm Manager Soundtrack
+setManifestid(2714791, "4632921184692652067", 49091856)
+--Dlcname 管理員的窺視 ArtBook
+addappid(2714800) --Dlcname 管理員的窺視 ArtBook
+addappid(2714801, 1, "5e83179df14d7e1a1ac7b82df9a72198fe26a5b6edfb1faa9bb8778059f86eda") --Dlc Windows Depot 管理員的窺視 ArtBook
+setManifestid(2714801, "4182717634502670780", 321558208)
+--Share Depots
+addappid(2590231, 1, "52bbf25c457e7d57eba3f948745922516ed32aa17b5e33c1987d1ca181ae0666") --Share Windows Depot 管理員的窺視 Demo
+setManifestid(2590231, "6907264164709637894", 224536672)
