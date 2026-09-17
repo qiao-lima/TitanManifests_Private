2d9738af2b9abf14686c2e333cedbaa3dadec8de
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 12:22:46 -0400

    restock 850310

---
+++ 850310.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 850310.lua
+--Gamename Deadly Station
+addappid(850310) --Mainappid Deadly Station
+addappid(850311, 1, "3a66ebdf241ec1bb9c365bb0a2ed6a0d2bc366929af9da9795f28eb08ebdda41") --Main Windows Depot Deadly Station
+setManifestid(850311, "3698060409958670353", 171521504)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+--Missing Dlcs
+--Dlcname Supporter Pack
