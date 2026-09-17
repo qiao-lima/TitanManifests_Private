99ef33f5e2f7babd8c42edc89c129f663bdbbd7d
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 06:25:01 -0400

    restock 4645760

---
+++ 4645760.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4645760.lua
+--Gamename How Much for the Body in the Freezer
+addappid(4645760) --Mainappid How Much for the Body in the Freezer
+addappid(4645761, 1, "943a270e7ce916ecba2e9751f089fabab2e0fd608832595f3c0a1ed664c0a05d") --Main Windows Depot How Much for the Body in the Freezer
+setManifestid(4645761, "7628977689349963790", 384244240)
