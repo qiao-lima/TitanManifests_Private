d3f152aa32cc76519e7313fdfc756c658b9a4fef
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 01:49:53 -0400

    restock 815030

---
+++ 815030.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 815030.lua
+--Gamename SKULL FEAST
+addappid(815030) --Mainappid SKULL FEAST
+addappid(815031, 1, "40793214cc8fdd4287efe6931d6e4a4255a2c39f7ecf2b9f2ba20759308ee26e") --Main Windows Depot SKULL FEAST
+setManifestid(815031, "7474807164411679260", 902232144)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
