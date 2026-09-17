a5678b4c39740805dd4f4adce3150ea8dab5f1b1
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 10:23:26 -0400

    restock 1705410

---
+++ 1705410.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1705410.lua
+--Gamename Bloody Efforts
+addappid(1705410) --Mainappid Bloody Efforts
+addappid(1705411, 1, "f782154f0073d8b41480bc75bde6f45405d2b8c6634decef750beb4fbd733003") --Main Windows Depot Bloody Efforts
+setManifestid(1705411, "8350466741917887286", 1245166256)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
