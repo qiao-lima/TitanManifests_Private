965329222f9d5caa30b8a85c46cfca0efa7f6d01
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 05:58:42 -0400

    restock 550650

---
+++ 550650.lua
@@ -0,0 +1,33 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 550650.lua
+--Gamename Black Squad
+addappid(550650, 1, "3c15e8a291ceb30459c0aa01e945504d18ab14ce9740daca4d74aaee94856ee6") --Mainappid Black Squad
+addappid(550651, 1, "ec537a88c8f574789b198a88f8dda212dee70f249d7ed3162e5143a4e354a827") --Main Windows Depot Black Squad
+setManifestid(550651, "4830696691282736379", 10506499104)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(654173) --Dlcname AppID 654173
+addappid(654174) --Dlcname Black Squad - EA FREE TIMED WEAPON PACKAGE 3
+addappid(654175) --Dlcname AppID 654175
+addappid(654176) --Dlcname AppID 654176
+addappid(654180) --Dlcname AppID 654180
+addappid(654181) --Dlcname Blacksquad - AXMC FIRST RELEASE PACKAGE
+addappid(654183) --Dlcname Blacksquad - SIZ556XI RUS FIRST RELEASE PACKAGE
+addappid(654185) --Dlcname Blacksquad - HNK416A5 FIRST RELEASE PACKAGE
+addappid(654186) --Dlcname Blacksquad - AEK973 FIRST RELEASE PACKAGE
+addappid(654187) --Dlcname Black Squad - EA FREE TIMED WEAPON PACKAGE 4
+addappid(654188) --Dlcname Black Squad - M40A5 FIRST RELEASE PACKAGE
+addappid(654189) --Dlcname AppID 654189
+addappid(1034450) --Dlcname AppID 1034450
+addappid(1053260) --Dlcname AppID 1053260
+addappid(1106070) --Dlcname AppID 1106070
+addappid(1710430) --Dlcname Black Squad - 4TH YEAR ANNIVERSARY PACKAGE
+--Missing Dlcs
+--Dlcname Black Squad - EA FREE TIMED WEAPON PACKAGE 1
+--Dlcname Black Squad - EA FREE TIMED WEAPON PACKAGE 2
+--Dlcname Black Squad - FOUNDER'S PACKAGE
