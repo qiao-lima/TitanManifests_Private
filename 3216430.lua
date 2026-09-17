947548d738c1d966afdea2baa016f562b1bdcca3
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 10:27:41 -0400

    restock 3216430

---
+++ 3216430.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3216430.lua
+--Gamename Blue Light
+addappid(3216430) --Mainappid Blue Light
+addappid(3216431, 1, "1a19450ef09cab66e0a6f46a7e4dffdf58e93f1bc94b47e9a8a2d983936894a2") --Main Windows Depot Blue Light
+setManifestid(3216431, "5399334526507276955", 8093344368)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+--Missing Dlcs
+--Dlcname Blue Light Soundtrack
