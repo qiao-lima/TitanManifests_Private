8e942e91c040cfd163c2082a3a5e2510ca7f6531
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 03:40:34 -0400

    restock 4348910

---
+++ 4348910.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4348910.lua
+--Gamename Probably Stolen - Cyberpunk Shopkeeper Sim
+addappid(4348910) --Mainappid Probably Stolen - Cyberpunk Shopkeeper Sim
+addappid(4603731, 1, "24bc5004993d0c757d4904b19d92e5e7a8839be36b08441964dc48483284d113") --Main Windows Depot Probably Stolen - Cyberpunk Shopkeeper Sim
+setManifestid(4603731, "3455648572528031759", 102853712)
+--Dlcname Probably Stolen Playtest
+addappid(4603730) --Dlcname Probably Stolen Playtest
+addappid(5113870) --Dlcname Probably Stolen Soundtrack
