a1ef4f4dfb4dcd6fe6829819c0434b85e4e97856
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 06:36:06 -0400

    restock 5075680

---
+++ 5075680.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5075680.lua
+--Gamename Graveyard Keeper 2 Demo
+addappid(5075680) --Mainappid Graveyard Keeper 2 Demo
+addappid(5075681, 1, "64bb4f28aa4cd3728d353ded300847ce773a676f86759c73d5a7fdb46514693c") --Main Windows Depot Graveyard Keeper 2 Demo
+setManifestid(5075681, "739448200032943577", 965182432)
