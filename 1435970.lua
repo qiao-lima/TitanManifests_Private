5194d164d9cd4027848428ef024c90a1ed43e764
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 12:25:11 -0400

    restock 1435970

---
+++ 1435970.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1435970.lua
+--Gamename Anomalous
+addappid(1435970) --Mainappid Anomalous
+addappid(1435971, 1, "4ad20283ec8ad9127ec0472e4078ef6aa667195d8c138718f2bb28b3e5339af5") --Main Windows Depot Anomalous
+setManifestid(1435971, "8538093646871757482", 6879944080)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
