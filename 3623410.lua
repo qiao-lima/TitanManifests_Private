ee419e9ba522c40fc40488921d0f3cc9b87d069f
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 10:15:16 -0400

    restock 3623410

---
+++ 3623410.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3623410.lua
+--Gamename REGION55
+addappid(3623410) --Mainappid REGION55
+addappid(3623411, 1, "F541345E3189B1012E153BCCDE8314B7A42806B91F7348EDE171ABD47710E291") --Main Windows Depot REGION55
+setManifestid(3623411, "3520982484985579605", 9776618624)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
