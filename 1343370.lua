968d285947329234ba526ef066739eacdeba7eb6
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 02:50:01 -0400

    restock 1343370

---
+++ 1343370.lua
@@ -0,0 +1,23 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1343370.lua
+--Gamename Old School RuneScape
+addappid(1343370, 1, "e2f41538a89ec70919d7db57e19470bd8a717835f219a756a4acbfb8e580b1a8") --Mainappid Old School RuneScape
+addappid(1343371, 1, "cacd3ac83c57cc56f952845b5044580599d8a268f8bbb3d3c72378bddb6e6f17") --Main Windows Depot Old School RuneScape
+setManifestid(1343371, "5356282975213836096", 5662256)
+addappid(1343374, 1, "0feff1a3db1ad149ffb7b2514437b4680dab11da7ba2898c1ed65afd5532365e") --Main Macos Depot Old School RuneScape
+setManifestid(1343374, "3406616487695917657", 7583856)
+--Dlcname RuneScape: Original Soundtrack Classics
+addappid(1397370) --Dlcname RuneScape: Original Soundtrack Classics
+addappid(1397371, 1, "4af7d021ce4fe67b3bf7b07eb10fca60ca6f3e0d2c5e85106104ab557ec6f7ed") --Dlc Windows Depot RuneScape: Original Soundtrack Classics
+setManifestid(1397371, "3384663580954518156", 255937072)
+addappid(1397372, 1, "6d00757008dd94a8be1f0e30557f3c5ab866b0a65a1c4cfb7dc49492969df692") --Dlc Windows Depot RuneScape: Original Soundtrack Classics
+setManifestid(1397372, "3958849428854089070", 1262307584)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1401460) --Dlcname AppID 1401460
+addappid(1401461) --Dlcname Old School RuneScape 6-Month Membership + OST
+addappid(1401462) --Dlcname Old School RuneScape 12-Month Membership + OST
+addappid(1474630) --Dlcname Old School RuneScape 1-Month Membership
