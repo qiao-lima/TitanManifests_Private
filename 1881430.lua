40f20d9c74a322ef4ca2d0c6554fb45d6ba8edf8
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 04:30:01 -0400

    restock 1881430

---
+++ 1881430.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1881430.lua
+--Gamename DeltaBlade 2700 Re:Create
+addappid(1881430) --Mainappid DeltaBlade 2700 Re:Create
+addappid(1881431, 1, "d3dd892eeea96bf51015e4577128a4409914724b219e9ce991ff669748a6b99b") --Main Windows Depot DeltaBlade 2700 Re:Create
+setManifestid(1881431, "2789973206441705015", 215554400)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
