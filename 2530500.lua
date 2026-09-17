d5eaa9fda4b0110feb53cdf832dc2e23d36497e0
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 09:40:26 -0400

    restock 2530500

---
+++ 2530500.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2530500.lua
+--Gamename Dropkick Navvy: First Step
+addappid(2530500) --Mainappid Dropkick Navvy: First Step
+addappid(2530501, 1, "d7e23d461b9faf29c2e3ec9d975f114cff06316e356fe47fe01c972038b6bd30") --Main Windows Depot Dropkick Navvy: First Step
+setManifestid(2530501, "4064681701766147495", 610640416)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
