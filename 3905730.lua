12f1bb034bfa76a9f31b7398f9fb5bae58c650a2
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 01:14:34 -0400

    restock 3905730

---
+++ 3905730.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3905730.lua
+--Gamename Nekopedia: Find Her Tail
+addappid(3905730) --Mainappid Nekopedia: Find Her Tail
+addtoken(3905730, "11774751098870751619")
+addappid(3905731, 1, "da18637fa5a348c8328556f264a2a5363398aaafb5f522e55b0961adf376c86e") --Main Windows Depot Nekopedia: Find Her Tail
+setManifestid(3905731, "7765237035088165938", 1547764768)
+addappid(4005180, 1, "39309379bdef4801c34588c3016ee91b25e19fb8dda7b49b1d1f3c31a3832a23") --Main Windows Depot Nekopedia: Find Her Tail
+setManifestid(4005180, "7958115481437788284", 2926700672)
+--Dlcname NSFW Content - Nekopedia: Find Her Tail
+--Missing Dlcs
+--Dlcname Nekopedia: Find Her Tail. The Official Artbook
