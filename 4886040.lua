9917c63fc42c26bdd8e92d7f4c648e251f1204f3
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 10:18:10 -0400

    restock 4886040

---
+++ 4886040.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4886040.lua
+--Gamename 魔法少女甜心 Magical Girl Sweetheart
+addappid(4886040) --Mainappid 魔法少女甜心 Magical Girl Sweetheart
+addappid(4886041, 1, "43190efb09639b4527cdf4ae33254054185b984d0a2ab4f812c4a9688af73606") --Main Windows Depot 魔法少女甜心 Magical Girl Sweetheart
+setManifestid(4886041, "7999579826208446938", 494459072)
