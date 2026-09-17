b9b1b03b7593a56f7cf90bfc3d7dcb59ace06847
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 01:50:13 -0400

    restock 1379200

---
+++ 1379200.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1379200.lua
+--Gamename Escape from Skull Dungeon
+addappid(1379200) --Mainappid Escape from Skull Dungeon
+addappid(1379201, 1, "fcfc06b0514378fdff6d7bdf3df34b4ae01f53230cebcb4a53c91101ef675087") --Main Windows Depot Escape from Skull Dungeon
+setManifestid(1379201, "2103056167539589277", 332240512)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
