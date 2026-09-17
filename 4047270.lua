4946b29fff481de96bebb863b26ab79757c18eda
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 05:37:44 -0400

    restock 4047270

---
+++ 4047270.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4047270.lua
+--Gamename Orc Incremental
+addappid(4047270, 1, "2469025d90da12231f737ddcf8830daf9b9b23eadd835e5ecc2948135d062421") --Mainappid Orc Incremental
+addappid(4047271, 1, "7d6e0717804828f83c3eb19f781a44d2327752ca3dbd8329bde81b527d1d47f9") --Main Windows Depot Orc Incremental
+setManifestid(4047271, "7589269425857793971", 84840960)
