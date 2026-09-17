a8b869d62b6b665a3f0b452f4e3765a15cdc31e9
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 02:45:06 -0400

    restock 1767750

---
+++ 1767750.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1767750.lua
+--Gamename Wild Life Demo
+addappid(1767750) --Mainappid Wild Life Demo
+addappid(1767751, 1, "248ad7baed6a4c0e069770318b696404c7462f13b1b4b8188c3e4b891450e0c6") --Main Windows Depot Wild Life Demo
+setManifestid(1767751, "6968912070797028370", 10993332624)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
