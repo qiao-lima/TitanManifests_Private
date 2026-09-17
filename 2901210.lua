0a7aaf803a6f1d99b0075e6dfbb8c7f7aa6c6624
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 12:15:06 -0400

    restock 2901210

---
+++ 2901210.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2901210.lua
+--Gamename Five Nights at Silver Pine
+addappid(2901210) --Mainappid Five Nights at Silver Pine
+addappid(2901211, 1, "82e385e09e31ad169a7f8bc2acaef97470d2271c8996a04d7ea52f8e98657c68") --Main Windows Depot Five Nights at Silver Pine
+setManifestid(2901211, "7071431069912297010", 359286832)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
