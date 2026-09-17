af7adb40af2b90e9ebd8b52400ff52d0a416bfc1
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 05:20:17 -0400

    restock 1186160

---
+++ 1186160.lua
@@ -0,0 +1,17 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1186160.lua
+--Gamename All Day Dying: Redux Edition
+addappid(1186160) --Mainappid All Day Dying: Redux Edition
+addappid(1186161, 1, "8fd595ffcd6ff1ab2f265d16902577abd9ea17eb0fd686d4c9034bff7391f4eb") --Main Windows Depot All Day Dying: Redux Edition
+setManifestid(1186161, "7934748902202238879", 3191205152)
+addappid(1186162, 1, "1fd738729298938db11e1a21be12c28421f80c7ecb6cea4d9199c96a76166406") --Main Windows Depot All Day Dying: Redux Edition
+setManifestid(1186162, "2424097705808448561", 3171712384)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229020, "5799761707845834510", 591984)
