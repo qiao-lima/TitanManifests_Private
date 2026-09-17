bc2b125edc1ab1f96896c8a6d2c35f01d6bb8d3f
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 01:08:19 -0400

    restock 4843710

---
+++ 4843710.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4843710.lua
+--Gamename Dealer's Dice
+addappid(4843710) --Mainappid Dealer's Dice
+addappid(4843712, 1, "d3e0a1f4661b1c707f5e14cafe393bd954fb54509a037a80a540281f7d974c62") --Main Windows/Linux Depot Dealer's Dice
+setManifestid(4843712, "4230726098649094197", 154946096)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
