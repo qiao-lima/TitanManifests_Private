59ec39a069f2bbf283b88705eee670fa7dc67309
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 12:21:10 -0400

    restock 3567350

---
+++ 3567350.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3567350.lua
+--Gamename Deadly Joy
+addappid(3567350) --Mainappid Deadly Joy
+addappid(3567351, 1, "3b868ee7ecefd35dede6486ffd9a0d92f8fdc9066d2e0b1db95414ea161ed08c") --Main Windows Depot Deadly Joy
+setManifestid(3567351, "5536838119256244260", 4277523056)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
+addappid(3703400) --Dlcname Deadly Joy - SilkenCore Skin Pack
