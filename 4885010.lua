7d10b2809bb352b3c8961b2e9380a36045cfe077
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 03:52:35 -0400

    restock 4885010

---
+++ 4885010.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4885010.lua
+--Gamename Daymark
+addappid(4885010) --Mainappid Daymark
+addappid(4885011, 1, "ee6c1a398ec8c079fcbf0121d18e364df4e609d91309bb0d40b70481244a5a87") --Main Windows Depot Daymark
+setManifestid(4885011, "6145942461637206400", 97231536)
