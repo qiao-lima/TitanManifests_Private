27fb06286c58f8b7c122cf3d5ac2ff1686cedc12
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 02:45:24 -0400

    restock 1343400

---
+++ 1343400.lua
@@ -0,0 +1,27 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1343400.lua
+--Gamename RuneScape ®
+addappid(1343400, 1, "fb6211d3a0da75c2058c2f424eae723c54c3ea4d8df464a3c5418f09fc7a3bd3") --Mainappid RuneScape ®
+addappid(1343401, 1, "74b386e9e10d6a92bc4b84e7dac36e20f5ad9c03e6202fc04146d7eacb0d46df") --Main Windows Depot RuneScape ®
+setManifestid(1343401, "5398945783619391473", 6106864)
+addappid(1343402, 1, "34d5ab598e253e3e1744ad62bfcd8ca83538b653aa8c018150913a161966b05f") --Main Windows Depot RuneScape ®
+setManifestid(1343402, "6409540644540252883", 3840693776)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1401450) --Dlcname RuneScape Starter Pack
+addappid(1401451) --Dlcname RuneScape Standard Pack
+addappid(1401452) --Dlcname AppID 1401452
+addappid(1664830) --Dlcname RuneScape Teatime Starter Pack
+addappid(1664831) --Dlcname RuneScape Teatime Standard Pack
+addappid(1664832) --Dlcname RuneScape Teatime Max Pack
+addappid(1998770) --Dlcname RuneScape: 200 RuneCoins
+addappid(1998771) --Dlcname RuneScape: 420 RuneCoins
+addappid(1998772) --Dlcname RuneScape: 900 RuneCoins
+addappid(1998773) --Dlcname RuneScape: 15 Treasure Hunter Keys
+addappid(1998774) --Dlcname RuneScape: 35 Treasure Hunter Keys
+addappid(1998775) --Dlcname RuneScape: 75 Treasure Hunter Keys
+addappid(2148810) --Dlcname RuneScape Anniversary Celebration Pack
+addappid(2148811) --Dlcname RuneScape Forgemaster Wyrm Bundle
+addappid(2148812) --Dlcname RuneScape Forgemaster Outfit Bundle
+addappid(4534280) --Dlcname RuneScape 1-Month Membership + The Orchestral Collection
