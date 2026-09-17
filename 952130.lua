82b6c7272905ad9f80d4b1c218d2a9f5181dfd25
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 12:25:55 -0400

    restock 952130

---
+++ 952130.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 952130.lua
+--Gamename Origin of Decay
+addappid(952130) --Mainappid Origin of Decay
+addappid(952131, 1, "277c8e9ab3557582c9f0160c2509941d27e2dd488d593892aeb3738ef8dceddf") --Main Windows Depot Origin of Decay
+setManifestid(952131, "8516733916964209946", 6921340384)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
