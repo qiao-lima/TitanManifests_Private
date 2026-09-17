0324f64f88fbdf784d4c09f0caf290fbf76b0f96
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 12:25:33 -0400

    restock 1471250

---
+++ 1471250.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1471250.lua
+--Gamename Orion: The Eternal Punishment
+addappid(1471250) --Mainappid Orion: The Eternal Punishment
+addappid(1471251, 1, "ac699abcd9f5df040eaf71f353276fd34a85bcaccbc2ce9975bd4d95cec2693f") --Main Windows Depot Orion: The Eternal Punishment
+setManifestid(1471251, "3856840264925236496", 826795408)
+addappid(1471252, 1, "6d58850195fec3e164dd5a194f3e42ee63cc8c0ff74ecc58fe41639bad54ccd7") --Main Windows Depot Orion: The Eternal Punishment
+setManifestid(1471252, "5606210380957477479", 805438656)
+addappid(1471253, 1, "00955f5d150050bfac22dccb803e46256b3ca4cff566307d6ddadf7a3a236ecc") --Main Macos Depot Orion: The Eternal Punishment
+setManifestid(1471253, "5459817579519490336", 888588112)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
