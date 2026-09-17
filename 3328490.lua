cfd25441707a18877d1df1d9438d11017403d10f
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 04:30:06 -0400

    restock 3328490

---
+++ 3328490.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3328490.lua
+--Gamename Airport Control 27
+addappid(3328490) --Mainappid Airport Control 27
+addappid(4004140, 1, "818bfc9d7a3e02ca1aa3e576db4f3dc4a1cb8519dd4a6986d1e0bae1e7e6c727") --Main Windows Depot Airport Control 27
+addappid(4004141, 1, "8c0df506e6af49ac9217269aeb5d8d938a86a615174d8dc8fadf315823231eab") --Main Windows Depot Airport Control 27
+setManifestid(4004141, "254498131905757005", 8407460752)
+--Dlcname Airport Control 27 Playtest
