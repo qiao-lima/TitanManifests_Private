b8eab30cacbe2d3006b5a5f87317db7ebf99d1ec
qiao-lima <qiao-lima@github.com>
2026-09-17 08:37:12 +0800

    Manual Update 2868840

---
+++ 2868840.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2868840.lua
+--Gamename Slay the Spire 2
+addappid(2868840, 1, "efe405b7cd4ee75b6961f28ad8ba9aa57a9e57057857dbd921e56ad3b347cd3b") --Mainappid Slay the Spire 2
+addappid(2868841, 1, "ed043fd4ac0899cd0b5fc59d5407115ff4f87793b162ba16fe49c7e2893938ab") --Main Windows Depot Slay the Spire 2
+setManifestid(2868841, "4733892780027453168", 1294497216)
+addappid(2868842, 1, "7036cfd5ff85a70ca002aeddf573311c3906b0f7d8d83d099592e759ddca5b31") --Main Macos Depot Slay the Spire 2
+setManifestid(2868842, "8653035385353091849", 1204382592)
+addappid(2868843, 1, "6e4518ed758f09a3fb1c07ed1ef14a8ccfb4bac6e1fc452adc1e1d8a1c024e42") --Main Linux Depot Slay the Spire 2
+setManifestid(2868843, "7881097836989620034", 1134795728)
