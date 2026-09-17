7d4d20e5511950d3ff537bbf7191afff2ede4cec
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 04:57:05 -0400

    restock 3668840

---
+++ 3668840.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3668840.lua
+--Gamename 笼中鸟
+addappid(3668840) --Mainappid 笼中鸟
+addappid(3668841, 1, "6bd39d1f4c9e95d5b77d98e9b8f4f9c5a45c5738d1475e944ca222b3a5051641") --Main Windows Depot 笼中鸟
+setManifestid(3668841, "3936462169414628724", 4187497568)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
