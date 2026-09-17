5a3cb9db95bb08149d304168af25f3490172f8b3
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 17:45:15 -0400

    restock 2020750

---
+++ 2020750.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2020750.lua
+--Gamename Where are my potatoes?
+addappid(2020750) --Mainappid Where are my potatoes?
+addappid(2020751, 1, "8df056ddd3f977144f33cf40e42b8f8ba8f93659f7cbc57256042c8975f1ac3a") --Main Windows Depot Where are my potatoes?
+setManifestid(2020751, "676344441280506640", 1975868928)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229000, "4622705914179893434", 242681440)
