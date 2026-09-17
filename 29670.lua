1aed03a60ca93f3b3f03d5b046fa1ffa6728363d
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 04:10:06 -0400

    restock 29670

---
+++ 29670.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 29670.lua
+--Gamename Aion Collectors Edition
+addappid(29670) --Mainappid Aion Collectors Edition
+addappid(29641, 1, "50b9fdffd7e9826b473b42aeeb373dfb1cee08304da47f6ca958253b4f7e1dc4") --Main Windows Depot Aion Collectors Edition
+setManifestid(29641, "2133658898499910452", 0)
