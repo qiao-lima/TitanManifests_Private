a46cbef9651b349efa9c25cb42692b56fd5b05f4
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 00:07:26 -0400

    restock 3559840

---
+++ 3559840.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3559840.lua
+--Gamename Sakura Hime 6
+addappid(3559840, 1, "7e841be408d7b5267fab333449c67d96b31723d05a27fa321f015a3b7ef04696") --Mainappid Sakura Hime 6
+addappid(3559841, 1, "2ff0ffad4ba92410ea12630a93a0d4cd3659f7aeaa455429d4ed33ef300a5e34") --Main Windows Depot Sakura Hime 6
+setManifestid(3559841, "8372525443772028213", 89922208)
+--Dlcname Sakura Hime 6 - 18+ Adult Only Content
+addappid(3601070, 1, "702c671a1a5590e22230205f6ab94028ecc798307e3dbd3f0300f7d523699773") --Dlcname Sakura Hime 6 - 18+ Adult Only Content
+setManifestid(3601070, "7969481574251881946", 129252256)
