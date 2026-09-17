c54fe584728f7d84859ad28f7e75f1669166c178
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 05:30:02 -0400

    restock 1168280

---
+++ 1168280.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1168280.lua
+--Gamename Resident Evil 2 "R.P.D. Demo"
+addappid(1168280, 1, "3d48631aee28c552dd8b3425bf9df5922f01d86b985006e50d990844bab4b955") --Mainappid Resident Evil 2 "R.P.D. Demo"
+addappid(1168281, 1, "e44fa5e8bbd41f5c809931f0e5a1919d41f3a5072f265d6c36599d5c38663d3c") --Main Windows Depot Resident Evil 2 "R.P.D. Demo"
+setManifestid(1168281, "5121525009824171114", 7588222592)
+--Share Depots
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
