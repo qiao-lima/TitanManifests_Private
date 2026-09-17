5faa2e4194c24a50034e706f11f950eeff723fee
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 00:02:05 -0400

    restock 3679440

---
+++ 3679440.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3679440.lua
+--Gamename Dichotomy Illusion: The Coser Disassembly Incident
+addappid(3679440) --Mainappid Dichotomy Illusion: The Coser Disassembly Incident
+addappid(3679441, 1, "27215e1644adb2d23ff5c486157ecfd2748992ea85c813a70b88a8dd77806cb8") --Main Windows Depot Dichotomy Illusion: The Coser Disassembly Incident
+setManifestid(3679441, "6668030690445536302", 947880864)
