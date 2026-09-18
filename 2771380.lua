639e8309db1698ef03b3013982f4533b7d8200bd
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 04:44:40 -0400

    restock 2771380

---
+++ 2771380.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2771380.lua
+--Gamename 奇妙大富翁(Wonderful Richman)
+addappid(2771380) --Mainappid 奇妙大富翁(Wonderful Richman)
+addappid(2771381, 1, "1a0297591717dcf26a544af48286ad8f21bfda7bab21e0ed34b5f5769364fa02") --Main Windows Depot 奇妙大富翁(Wonderful Richman)
+setManifestid(2771381, "90272154644408178", 437704992)
+--Share Depots
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
