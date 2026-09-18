21804dc4255b12e360c27ddc5deb4a32a68f3b52
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 08:55:06 -0400

    restock 2767280

---
+++ 2767280.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2767280.lua
+--Gamename Up Hike
+addappid(2767280) --Mainappid Up Hike
+addappid(2767281, 1, "bd1ef5df58f398866e5a755fed0bc333344048e4a0abfa250331ff837d2d305d") --Main Windows Depot Up Hike
+setManifestid(2767281, "3128791587376236696", 187156384)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
