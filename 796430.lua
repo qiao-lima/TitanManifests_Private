0f7ca5ad8411c0ba26036d43880e1d06f37ebf81
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 09:20:16 -0400

    restock 796430

---
+++ 796430.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 796430.lua
+--Gamename SHiRO 011
+addappid(796430) --Mainappid SHiRO 011
+addappid(796432, 1, "bf6fda882334abf4455ecefa65a85f9ad4cb10c64d0856f492e9d640e24f6883") --Main Windows Language Depot English SHiRO 011
+setManifestid(796432, "6954373365185927499", 265114560)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229002, "7260605429366465749", 50365280)
