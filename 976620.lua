a3fd619e40fb229f244153fdba235ea07beb1b83
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 00:45:01 -0400

    restock 976620

---
+++ 976620.lua
@@ -0,0 +1,56 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 976620.lua
+--Gamename Enderal: Forgotten Stories (Special Edition)
+addappid(976620) --Mainappid Enderal: Forgotten Stories (Special Edition)
+addappid(976621, 1, "b3afb8486fe97a7492a6df8a14752eaa5458f610b055315b02402b276075df5c") --Main Windows Depot Enderal: Forgotten Stories (Special Edition)
+setManifestid(976621, "983124346035775263", 9262861008)
+addappid(976622, 1, "6226de3ff0c142a1b377cf76259e202f8037fdcd96a7fcfd94827d075174a158") --Main Windows Language Depot Spanish Enderal: Forgotten Stories (Special Edition)
+setManifestid(976622, "3571225253294452255", 2353087488)
+addappid(976623, 1, "364965c7b27efe7031a28d03e674e8d6bb2f957e3ca912a15f1e116594728253") --Main Windows Depot Enderal: Forgotten Stories (Special Edition)
+setManifestid(976623, "4737953337756678336", 7693685296)
+addappid(976624, 1, "a38cd4451ff648fa65d1d20313ca65d69dbe2ac69d80d817dfdb12974ec500c2") --Main Windows Language Depot English Enderal: Forgotten Stories (Special Edition)
+setManifestid(976624, "5877117207518776126", 2068054112)
+addappid(976625, 1, "4edfebf5734cb39be40eb28ffc4027c7c867a0e502ca43679ca562e43537bf35") --Main Windows Language Depot German Enderal: Forgotten Stories (Special Edition)
+setManifestid(976625, "3810569725633000181", 2368520272)
+addappid(976626, 1, "95adce84de92f71185aecefb3dc78daf2ccc8f04c3a27692cd12cd952a00a1c5") --Main Windows Language Depot Italian Enderal: Forgotten Stories (Special Edition)
+setManifestid(976626, "6678449903511624132", 2293753792)
+addappid(976627, 1, "b104a549797cf35cc7801490e8fd203e2c9449dc8b2d51b67112d458776384d6") --Main Windows Language Depot Russian Enderal: Forgotten Stories (Special Edition)
+setManifestid(976627, "3789921182536976239", 2372973296)
+addappid(976628, 1, "9bc32ff8f1412ad8105cc844a961c53f3d1cd51ba1d22d74a99842f904a3519a") --Main Windows Language Depot French Enderal: Forgotten Stories (Special Edition)
+setManifestid(976628, "3164078863630040097", 2335938880)
+addappid(976629, 1, "bb27896bbe1a232aa986afff4fa036b95775b30aa6c06cd2ef23a0a82aa4de7d") --Main Windows Language Depot Japanese Enderal: Forgotten Stories (Special Edition)
+setManifestid(976629, "5500700008429994983", 2389214736)
+addappid(1574100, 1, "1862fa5c50429f356c716a6fffdb13fe535e9b16d6085b8a436c3b7554a546e9") --Main Windows Language Depot Schinese Enderal: Forgotten Stories (Special Edition)
+setManifestid(1574100, "5402019798809705967", 2335822560)
+addappid(1574101, 1, "5f33fac5ed6504696c65dbe34d8308997469893682eb146c8f82707743935b46") --Main Windows Language Depot Koreana Enderal: Forgotten Stories (Special Edition)
+setManifestid(1574101, "2633064824186014807", 2392481984)
+addappid(1574102, 1, "28eb49d040caad576c6abc93780b014ad37a6302c1018a8cc36f0a92488e6228") --Main Windows Depot Enderal: Forgotten Stories (Special Edition)
+setManifestid(1574102, "6436646842199889361", 6592)
+addappid(1574103, 1, "3828a918b38be69c71c623dd72dc2f2844f9c19f2a304d653eb51c202b09d0f9") --Main Windows Depot Enderal: Forgotten Stories (Special Edition)
+setManifestid(1574103, "6090138831677211187", 47509664)
+addappid(1574104, 1, "81de5ff382a802ace27d8d3a95c6dea87b39b0c31bc7b941914d8c0bd5106ccd") --Main Windows Language Depot Polish Enderal: Forgotten Stories (Special Edition)
+setManifestid(1574104, "3380003380266562213", 2451578048)
+addappid(1574105, 1, "b92818cd339cc131aaf4f9f13e5e7b50a1f855c1f6de3cc973ef27255cfcd807") --Main Windows Language Depot Hungarian Enderal: Forgotten Stories (Special Edition)
+setManifestid(1574105, "2199581265650988325", 2305104752)
+--Dlcname Enderal - Original Soundtrack
+addappid(944240, 1, "60a2da691ef2e69931fc0acb24e9ef178e8970fcfef6ffb60986dbdf06b31a3a") --Dlcname Enderal - Original Soundtrack
+setManifestid(944240, "3605138584526227890", 1712279424)
+--Dlcname Enderal - Novel: Dreams of the Dying
+addappid(955650, 1, "6e6a189ba55941d500c48e65a98c7781d377222b3c73bd3e02baceb111814d06") --Dlcname Enderal - Novel: Dreams of the Dying
+setManifestid(955650, "4338555528186244371", 414062176)
+--Dlcname Enderal - Original Soundtrack: The Bards
+addappid(955780, 1, "913fe6992946a9bd4f32098ec71a30f01d4f6c5074b5c98cfc6d1ef6227d10ea") --Dlcname Enderal - Original Soundtrack: The Bards
+setManifestid(955780, "134736829007057204", 733238384)
+--Dlcname Enderal: Forgotten Stories Soundtrack
+addappid(1225960) --Dlcname Enderal: Forgotten Stories Soundtrack
+addappid(1225961, 1, "1be6dd7b00567de8bba56f32dca8842bcdd07376c6a74bb666804bdaa65fa5f3") --Dlc Windows Depot Enderal: Forgotten Stories Soundtrack
+setManifestid(1225961, "2626255820642176099", 136509808)
+addappid(1225962, 1, "2fb00845b96a911f3b5948d251e42d5d629b52c0c45212ce6de162c337be6186") --Dlc Windows Depot Enderal: Forgotten Stories Soundtrack
+setManifestid(1225962, "4761330260753880707", 87700448)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
