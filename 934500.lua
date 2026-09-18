66a7dd3b0596d020fc932d232efadf1e0d8afa48
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 04:33:06 -0400

    restock 934500

---
+++ 934500.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 934500.lua
+--Gamename StoneDefence
+addappid(934500) --Mainappid StoneDefence
+addappid(934501, 1, "0627e8793c8273b57edec8bf88729fc33a09470c2ab700b7bfeac8645abdc834") --Main Windows Depot StoneDefence
+setManifestid(934501, "5895160706248446336", 625266160)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
