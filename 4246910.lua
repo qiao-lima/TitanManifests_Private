188ea9c22fcb3ef4e661472cece2eb5f9897d043
qiao-lima <qiao-lima@github.com>
2026-09-18 20:11:31 +0800

    Manual Update 4246910

---
+++ 4246910.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4246910.lua
+--Gamename Femboy Futa Cozy Cafe
+addappid(4246910) --Mainappid Femboy Futa Cozy Cafe
+addappid(4246911, 1, "349265065c0b7e2e008590125731d8a00e8d64bb0b34ea3ef18fdc73678b2d6b") --Main Windows Depot Femboy Futa Cozy Cafe
+setManifestid(4246911, "6488927793555268487", 887362496)
+--Dlcname Femboy Futa Cozy Cafe - Wallpapers Pack
+addappid(5059820, 1, "09be09d8436dc9006a8bfd119e7b7ffe77d0996ed60e9414b044bc659283fdba") --Dlcname Femboy Futa Cozy Cafe - Wallpapers Pack
+setManifestid(5059820, "4824277425495690982", 396640144)
+addappid(5123600) --Dlcname Femboy Futa Cozy Cafe - Team Building: Campfire Trip
+--Missing Dlcs
+--Dlcname Femboy Futa Cozy Cafe Soundtrack
