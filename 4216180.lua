839dc060291fa3f4107fbb0179eff4d085791f3f
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 07:09:52 -0400

    restock 4216180

---
+++ 4216180.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4216180.lua
+--Gamename IGNOBLE
+addappid(4216180) --Mainappid IGNOBLE
+addappid(4216181, 1, "620cc46202752feafcbcd2056e914ef87c10f9672b66fcfd9b6f2d9839aa730e") --Main Windows Depot IGNOBLE
+setManifestid(4216181, "8043242756180693009", 149290976)
