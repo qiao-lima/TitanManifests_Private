49610c2e7ee274afd1a7f23bea5bb94afd5d5cc6
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 01:38:11 -0400

    restock 2620940

---
+++ 2620940.lua
@@ -0,0 +1,20 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2620940.lua
+--Gamename Fallen Seeds
+addappid(2620940) --Mainappid Fallen Seeds
+addappid(2620941, 1, "4c74f0fff3808a7daa1013db8d6188ad0402939b8685f028a1f2f5fed7a665ea") --Main Windows Depot Fallen Seeds
+setManifestid(2620941, "9049999317250958143", 1003517264)
+addappid(3012391, 1, "731e864f4a986d0424501383fe3cdd03a1aeab122890e6775e24ad1c16973132") --Main Windows Depot Fallen Seeds
+setManifestid(3012391, "4417385430951242449", 88629696)
+addappid(3012392, 1, "9e23e48cd9a8301e52f459762b98acfcde65eeef1f936a0a19b534645eac61d3") --Main Windows Depot Fallen Seeds
+setManifestid(3012392, "4265869229903316065", 353150112)
+--Dlcname Fallen Seeds Soundtrack
+addappid(3012390) --Dlcname Fallen Seeds Soundtrack
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+--Missing Dlcs
+--Dlcname Fallen Seeds Artbook
+--Dlcname Fallen Seeds Artbook Extra
