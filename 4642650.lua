108974a1efb82cbd816e6bfeec5521d4bbd96ded
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 09:29:35 -0400

    restock 4642650

---
+++ 4642650.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4642650.lua
+--Gamename Souls Inferno
+addappid(4642650) --Mainappid Souls Inferno
+addappid(4642651, 1, "5df90d82e0694ef1df43b35382f8cbbd761381ea7eeb963b207a8164bfaedc10") --Main Windows Depot Souls Inferno
+setManifestid(4642651, "5251291449190863068", 47848752)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
