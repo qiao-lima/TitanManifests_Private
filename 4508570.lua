0d59e455865e6e5adefbc88a2e59b5c2adb827e1
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 05:55:39 -0400

    restock 4508570

---
+++ 4508570.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4508570.lua
+--Gamename Veritas Tales: Witch of the Dark Castle
+addappid(4508570) --Mainappid Veritas Tales: Witch of the Dark Castle
+addappid(4508571, 1, "550f24a225addd73b56bc470ffded912bd16ff6a82ab919072a6781c239963b5") --Main Windows Depot Veritas Tales: Witch of the Dark Castle
+setManifestid(4508571, "2799884192551779900", 856989104)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
