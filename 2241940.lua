a804ee8000d2c83e4e47104fe22073e7a2e541a6
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 06:30:02 -0400

    restock 2241940

---
+++ 2241940.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2241940.lua
+--Gamename MMDEngine
+addappid(2241940, 1, "54ff8659443e8d5036503ce2db5119b5389d4cab57b2a34e95e09cf5c46346b9") --Mainappid MMDEngine
+addappid(2241941, 1, "7c5602b2d11a2a1e97a39c2bf419955084e163fdca14ffc72140b0ad62008821") --Main Windows Depot MMDEngine
+setManifestid(2241941, "4159024377092942790", 125247120)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(4478370) --Dlcname MMDEngine - Supporter Pack
