03d517ab2ac795eb4e52d60d7104fa4cef38a772
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 21:55:19 -0400

    restock 4221990

---
+++ 4221990.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4221990.lua
+--Gamename Fish Lab
+addappid(4221990) --Mainappid Fish Lab
+addappid(4221991, 1, "bfd0777fd4fed0e834227630f10e9b28a167250c2298bc6df4f7673ec256b963") --Main Windows Depot Fish Lab
+setManifestid(4221991, "6432074660723705728", 293765824)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+--Missing Dlcs
+--Dlcname Fish Lab Playtest
