58853b14c07ea30f0503c4710ef0b2e89afa430a
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 01:57:48 -0400

    restock 2082490

---
+++ 2082490.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2082490.lua
+--Gamename Roomvas
+addappid(2082490) --Mainappid Roomvas
+addappid(2082491, 1, "f830bfda8d0217e0b12d0175114816034fb26f9c47cbeeea7884107a74017211") --Main Windows Depot Roomvas
+setManifestid(2082491, "6601632207379096589", 2191832640)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2597270) --Dlcname AppID 2597270
