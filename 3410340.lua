4c8d38c8a8a4fc1faa7fae85f7d20f5ab0c889c8
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 04:35:01 -0400

    restock 3410340

---
+++ 3410340.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3410340.lua
+--Gamename 绝境守护 Dao's Cover
+addappid(3410340) --Mainappid 绝境守护 Dao's Cover
+addappid(3410341, 1, "468c34d7b6e5e631588c1b84cb7a5796d5b0e9c5295f468af990c1e1bc01f8f1") --Main Windows Depot 绝境守护 Dao's Cover
+setManifestid(3410341, "4796376860339775597", 10280134592)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
