68636e457e8e1654cf2845313c2cb27175d4e670
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 10:25:42 -0400

    restock 3432270

---
+++ 3432270.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3432270.lua
+--Gamename Monmusu Girls: Autobattler
+addappid(3432270) --Mainappid Monmusu Girls: Autobattler
+addappid(3432271, 1, "D49287B4A40FDE6A98BC0EC214ABB3A9C600E4FA2E1E0B4DD28C77C382EAAFAD") --Main Windows Depot Monmusu Girls: Autobattler
+setManifestid(3432271, "7266273307869546301", 696093040)
