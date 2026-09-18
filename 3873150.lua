248d5d5c21672dd3f2bf77e9ee77b50705cd50e6
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 09:29:40 -0400

    restock 3873150

---
+++ 3873150.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3873150.lua
+--Gamename [Mulmiyac] for 3D motion sickness
+addappid(3873150) --Mainappid [Mulmiyac] for 3D motion sickness
+addappid(3873151, 1, "956154ba8f5a985691b66c9e59cf7d0f9173f24b69275de055b7a57a48f2e84e") --Main Windows Depot [Mulmiyac] for 3D motion sickness
+setManifestid(3873151, "2938525746220061241", 39435984)
