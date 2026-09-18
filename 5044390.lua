cedb0d725ec9d95d27c517b54db02c8a69ca65a7
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 08:07:52 -0400

    restock 5044390

---
+++ 5044390.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5044390.lua
+--Gamename BLOODMONEY: Requiem
+addappid(5044390) --Mainappid BLOODMONEY: Requiem
+addappid(5044391, 1, "0318dbbaa8973a4d722e4539fc474be684ed852af080dc47a6ce0e44a59bd511") --Main Windows Depot BLOODMONEY: Requiem
+setManifestid(5044391, "5929901565516140414", 286796032)
