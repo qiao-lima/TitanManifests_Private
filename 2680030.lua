f015db07c28fff94a9653d2342412721bc848ec2
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 06:05:37 -0400

    restock 2680030

---
+++ 2680030.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2680030.lua
+--Gamename 生命火热 / Desperate 1
+addappid(2680030) --Mainappid 生命火热 / Desperate 1
+addappid(2680031, 1, "a288bb69e8e57c6577000b03a4dfc18a77903755f0c0d570f93893d5ab07395f") --Main Windows Depot 生命火热 / Desperate 1
+setManifestid(2680031, "9171813845217820883", 3573899712)
