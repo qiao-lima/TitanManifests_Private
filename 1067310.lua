7bce75a0121cfa3ff5deea4a02c401093f336ea6
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 04:32:05 -0400

    restock 1067310

---
+++ 1067310.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1067310.lua
+--Gamename Gravitas
+addappid(1067310) --Mainappid Gravitas
+addappid(1067311, 1, "831dcedfbc9aa233d52a1311746432cd0c85a0772fc1e178d1ee8a7f4255be4b") --Main Windows Depot Gravitas
+setManifestid(1067311, "3644709482770328412", 1213719648)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
