44c30832795a3d25278b204356d13edca826c1e9
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 02:23:34 -0400

    restock 734350

---
+++ 734350.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 734350.lua
+--Gamename Rocket Armor
+addappid(734350) --Mainappid Rocket Armor
+addappid(734351, 1, "83df962ae46db793fa5ef8dc1a20af6e5e2cad3423f166e5549c24ac8ca3e067") --Main Windows Depot Rocket Armor
+setManifestid(734351, "6682758977223870962", 1606032992)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
