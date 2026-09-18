2a5c9c3164362f86533bd3611fc01bfdd0555f09
qiao-lima <qiao-lima@github.com>
2026-09-18 20:11:30 +0800

    Manual Update 3376290

---
+++ 3376290.lua
@@ -0,0 +1,18 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3376290.lua
+--Gamename 海蓝星之梦
+addappid(3376290, 1, "ccd00d7a58647c866ec75087a7a7293a3df9b5065a183d8fb4995ca06c3b1f97") --Mainappid 海蓝星之梦
+addappid(3376291, 1, "6a6a00130228e1bcca55b3c846a290c21c16e76b36432b0534bdf22e44355789") --Main Windows Depot 海蓝星之梦
+setManifestid(3376291, "8823920284277557143", 1629786752)
+--Dlcname 海蓝星之梦 特典音声
+addappid(4106350) --Dlcname 海蓝星之梦 特典音声
+addappid(4406110, 1, "d420892ffceba9413e6b55a51a7ab281297c680d5010c97fe50b8dc54be43329") --Dlc Windows Depot 海蓝星之梦 特典音声
+setManifestid(4406110, "8606316524636089600", 41478928)
+addappid(4406111, 1, "171c812d315948c2c5f15a8d1757813141e564e67f517f30c118d1e357b3363c") --Dlc Windows Depot 海蓝星之梦 特典音声
+setManifestid(4406111, "1281364318272056684", 173221184)
+--Dlcname 海蓝星之梦DLC·远行
+addappid(4800850) --Dlcname 海蓝星之梦DLC·远行
+addappid(4800851, 1, "05597ee50fb26d4d71d1896734fc4592aa67100d59b662a5bee439bb3be39c4b") --Dlc Windows Depot 海蓝星之梦DLC·远行
+setManifestid(4800851, "5235386765053566999", 1726908320)
+--Missing Dlcs
+--Dlcname 海蓝星之梦 壁纸合集
