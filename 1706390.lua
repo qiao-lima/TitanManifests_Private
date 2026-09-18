2211d4da9100e5255261422e722f98036a851b5e
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 00:45:07 -0400

    restock 1706390

---
+++ 1706390.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1706390.lua
+--Gamename renderB
+addappid(1706390) --Mainappid renderB
+addappid(1706391, 1, "6d7c5157297848b04de60d7bb3097dabe4a0d5f91d9e6039c0dcee733aaa5860") --Main Windows Language Depot English renderB
+setManifestid(1706391, "7048773052350237023", 381501504)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
