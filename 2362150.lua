9715fa0ca5022ba8b93f3b0aa191cc0a54ddef0e
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 07:25:06 -0400

    restock 2362150

---
+++ 2362150.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2362150.lua
+--Gamename 13iew
+addappid(2362150) --Mainappid 13iew
+addappid(2362151, 1, "9cf48bd9986f0cee18c8807ecb4d04b0efa5181cd6f1eadbd295bbbf776968a2") --Main Windows Depot 13iew
+setManifestid(2362151, "1761978129721081695", 4379980992)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
