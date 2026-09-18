ee6cb6b27b5fece7e3c1dfeaec619596dfe94a37
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 00:45:11 -0400

    restock 2386590

---
+++ 2386590.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2386590.lua
+--Gamename Dwarfender
+addappid(2386590) --Mainappid Dwarfender
+addappid(2386591, 1, "a0532500245737806132b04d7530f3d4c0e6387703cba4365b48fa986473c531") --Main Windows Depot Dwarfender
+setManifestid(2386591, "3065026782405883286", 642952512)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
