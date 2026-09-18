201c66a6afea174e15b668cd42f2524709b8d46a
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 11:18:37 -0400

    restock 3787060

---
+++ 3787060.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3787060.lua
+--Gamename It Works: Electronics Repair Simulator Demo
+addappid(3787060) --Mainappid It Works: Electronics Repair Simulator Demo
+addappid(3787061, 1, "f21d933368caa40ad4252bfe305646fc5575aea1d411a8498e62eef1ab1fb7d9") --Main Windows Depot It Works: Electronics Repair Simulator Demo
+setManifestid(3787061, "2817719032017005169", 1419982016)
