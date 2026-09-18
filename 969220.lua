ac4cb420465319db9932bd7103c4f0f976a5b2f1
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 08:55:16 -0400

    restock 969220

---
+++ 969220.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 969220.lua
+--Gamename Up in the Air
+addappid(969220) --Mainappid Up in the Air
+addappid(969221, 1, "484b54e85a5a93a753baa714588a7a6d53636b163f48c58fdfd96d03f347d174") --Main Windows Depot Up in the Air
+setManifestid(969221, "8047629196591471844", 469923328)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
