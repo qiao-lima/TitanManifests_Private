17ed83b6305e64f74c49861906ec7b5eb128f03c
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 11:30:06 -0400

    restock 3846300

---
+++ 3846300.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3846300.lua
+--Gamename MandarinMastermind
+addappid(3846300) --Mainappid MandarinMastermind
+addappid(3846301, 1, "d41bda521def217807a71fad338b4f04806de13b31b0c474c6632afaec75c018") --Main Windows Depot MandarinMastermind
+setManifestid(3846301, "698082036701324407", 5305322976)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
