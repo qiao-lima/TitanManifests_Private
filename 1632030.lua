99ceecfd3792ab1a3aa6b46b4d447ac989083c91
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 01:25:05 -0400

    restock 1632030

---
+++ 1632030.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1632030.lua
+--Gamename Soran
+addappid(1632030) --Mainappid Soran
+addappid(1632031, 1, "171b034407430d37375ca1a3b35ebd4069230b5e64f70a0a1cb042c3ec98e045") --Main Windows Depot Soran
+setManifestid(1632031, "3040207517788902961", 425594064)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1890090) --Dlcname AppID 1890090
