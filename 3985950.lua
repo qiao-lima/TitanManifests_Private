3c7576f16bbc5dc815096304c3df2019d0bb7853
qiao-lima <qiao-lima@github.com>
2026-09-18 20:11:30 +0800

    Manual Update 3985950

---
+++ 3985950.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3985950.lua
+--Gamename R.I.P. - Reincarnation Insurance Program
+addappid(3985950, 1, "3cae8a31a2f5ca92726cc7fd7a4ed4de214a8e4804584a0664edb26e1642e388") --Mainappid R.I.P. - Reincarnation Insurance Program
+addtoken(3985950, "13680587760972106125")
+addappid(3985951, 1, "7bfe7abe6093a6a1a9ba424c4b1139c0df5a57eda478a74ac79c775455fcaad2") --Main Windows Depot R.I.P. - Reincarnation Insurance Program
