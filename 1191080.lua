b526d0021cfb0fcc6e743d6ddf335cea132c60e2
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 11:42:06 -0400

    restock 1191080

---
+++ 1191080.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1191080.lua
+--Gamename SANCTION
+addappid(1191080) --Mainappid SANCTION
+addappid(1191081, 1, "8a8d04b9e18ea83016acebd8dd8cdd0b60d75c8f6e2ff1616f8ea41c13f2107b") --Main Windows Depot SANCTION
+setManifestid(1191081, "4359074401728827784", 4759789744)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
