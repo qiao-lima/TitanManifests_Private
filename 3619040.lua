97bf4388e2faf0768a9652b665fc577b88909fcf
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 07:18:19 -0400

    restock 3619040

---
+++ 3619040.lua
@@ -0,0 +1,33 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3619040.lua
+--Gamename Half-Life Legacy
+addappid(3619040, 1, "8e3dbc8336266b3fa7764941b5da40ac29108b53e06a3175549d613d2ab74086") --Mainappid Half-Life Legacy
+addappid(3619041, 1, "5d6a7e6f6a65dce831da8e4c3b35c4d3997d56cbe2d38c5602eba643932bd71a") --Main Windows Depot Half-Life Legacy
+setManifestid(3619041, "5288578877008505797", 159258976)
+addappid(3619042, 1, "77a51bca982a681f6823d87fff94aeee61f9c50b4a56c23ebe17cd6d0f386a66") --Main Windows Depot Half-Life Legacy
+setManifestid(3619042, "5713172075124306309", 62235776)
+addappid(3619043, 1, "99f011a514d181214b9055e3f9a5a2b695c37c9b08e25914f9514ce7b0a4bc99") --Main Windows Language Depot Spanish Half-Life Legacy
+setManifestid(3619043, "530715061144748097", 37874192)
+addappid(3619044, 1, "e956391f3144577ec4475361745f8e50798afe2585a85d1c35c82d8d4d149d56") --Main Windows Language Depot Italian Half-Life Legacy
+setManifestid(3619044, "8080935331307877285", 36166016)
+addappid(3619045, 1, "bee3e50adf96c093dd00b7fd5cd648578d7161a81be6e0e941883eaf276109f4") --Main Windows Language Depot German Half-Life Legacy
+setManifestid(3619045, "5166509989874442739", 34618000)
+addappid(3619046, 1, "099f859c04e1a38d6cf4b56a55334767c97bb6897d320da30acc5b5f5c0b178f") --Main Windows Language Depot French Half-Life Legacy
+setManifestid(3619046, "8688484421834020232", 29336064)
+addappid(3619047, 1, "0db018b7d0a7718e698cffefdc3d6604ed137b6ebd36a084d22e2c6e2d1fe041") --Main Windows Language Depot Schinese Half-Life Legacy
+setManifestid(3619047, "5112856699431859026", 32803312)
+addappid(3619048, 1, "8cd9209fa1004b283be63b039d4650610063257a992eb99f515e7b93d82fc9fe") --Main Windows Language Depot Tchinese Half-Life Legacy
+setManifestid(3619048, "8600549139074925221", 32802944)
+addappid(3619049, 1, "4a1090962eaf0e9a95d6fcb3a875942c8d863c5e80e195037315c13e9ea35d3c") --Main Windows Language Depot Koreana Half-Life Legacy
+setManifestid(3619049, "5389350141449911029", 33824800)
+addappid(3620901, 1, "0d32e0f6ea52dae9393a3fa8552150e39c764617c75eedd941c001b2ce0b7200") --Main Windows Language Depot Russian Half-Life Legacy
+setManifestid(3620901, "3247638361003011644", 33568)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(3416641, 1, "6AAA3F7ACBF3F34B5F41CE9FE40F8F81F9617598DE1A8D10A4B71F27A61F419E") --Share Windows Depot HALF-LIFE: CROSS PRODUCT
+setManifestid(3416641, "7130706212543535888", 4304)
+addappid(3416643, 1, "BDDDB186828B345AED442D40F580DB3AEFE5AA4299FFA64B7E652709D8D9018A") --Share Windows Depot HALF-LIFE: CROSS PRODUCT
+setManifestid(3416643, "6812917444531732819", 112730912)
+addappid(3416644, 1, "2A6A412C544DEE0640D2B05BC313087AC39443C92F2781411C6EC11DB3F091EC") --Share Windows Depot HALF-LIFE: CROSS PRODUCT
+setManifestid(3416644, "2955963720068931753", 17413136)
