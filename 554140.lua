6d67bebaf05d4e17423d75aba0984b46016a1f19
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 06:45:12 -0400

    restock 554140

---
+++ 554140.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 554140.lua
+--Gamename Desert Ride Coaster
+addappid(554140) --Mainappid Desert Ride Coaster
+addappid(554141, 1, "cf2e2620c015e09be80f4b72753b0a83c45ef5b95e3495df13e9e30576ac0ba8") --Main Windows Depot Desert Ride Coaster
+setManifestid(554141, "5280505391582300636", 462245088)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
