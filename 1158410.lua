a61906926c018189b5008f89b9d04d891527d520
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 12:10:06 -0400

    restock 1158410

---
+++ 1158410.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1158410.lua
+--Gamename Mole Game
+addappid(1158410) --Mainappid Mole Game
+addappid(1158411, 1, "a4c0ffee8984472f152f317e0cdeb5450bd7b9587a62cd9f3a733e1b9b25693c") --Main Windows Depot Mole Game
+setManifestid(1158411, "3225139301910599928", 675778880)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
