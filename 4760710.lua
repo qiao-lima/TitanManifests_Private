12f55d3a53a2114fa1f4b253dafcf75eba3f0873
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 08:01:04 -0400

    restock 4760710

---
+++ 4760710.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4760710.lua
+--Gamename EDGE CITY 边缘城市
+addappid(4760710) --Mainappid EDGE CITY 边缘城市
+addappid(4760711, 1, "cfa1981288874817195ba092de3e82f34888b1991c7eaf3371544d5a01c292e3") --Main Windows Language Depot Schinese EDGE CITY 边缘城市
+setManifestid(4760711, "6307677989516919373", 335864064)
