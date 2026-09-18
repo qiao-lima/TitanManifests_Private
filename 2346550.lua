e0121826b63397f03dc679965e8c80d7e7235996
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 10:30:02 -0400

    restock 2346550

---
+++ 2346550.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2346550.lua
+--Gamename Rise of Eros
+addappid(2346550) --Mainappid Rise of Eros
+addappid(2346551, 1, "8d61c9043983abae106ea8241bb683c5604945f0bf49e2638ace99e883c11c74") --Main Windows Depot Rise of Eros
+setManifestid(2346551, "3700450256907411842", 32637106416)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(4981860) --Dlcname Rise of Eros - Divine Summoning Pack
