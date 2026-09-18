792b4e0306db006be529e9860d24e87a988fa66d
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 08:53:40 -0400

    restock 1575870

---
+++ 1575870.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1575870.lua
+--Gamename UpGun
+addappid(1575870) --Mainappid UpGun
+addappid(1575871, 1, "ae76f883bf9da485b62cb310b68a3962b1d6e3d4962c3562a52df301deaffa89") --Main Windows Depot UpGun
+setManifestid(1575871, "6953756017251625667", 315657168)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1977800) --Dlcname UpGun - FullGame
