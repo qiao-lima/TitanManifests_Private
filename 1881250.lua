02bc34c9c2f8b94382f0a30ef5ba432dc470d17a
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 07:51:40 -0400

    restock 1881250

---
+++ 1881250.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1881250.lua
+--Gamename ProBee
+addappid(1881250) --Mainappid ProBee
+addappid(1881251, 1, "1dca5de6123155bb89868f7011d9a52c5be36342d0338838844ed7031f7a2cf2") --Main Windows Depot ProBee
+setManifestid(1881251, "2446515971856854938", 240192032)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
