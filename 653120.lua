119f662d5f5b08e92b00ed8bd31b0990927bf9ec
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 02:23:21 -0400

    restock 653120

---
+++ 653120.lua
@@ -0,0 +1,17 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 653120.lua
+--Gamename RockShot
+addappid(653120) --Mainappid RockShot
+addappid(653121, 1, "6a5816966b34887bf6ea53aad7ecd0599d6504e04920d14cce32f689084c2323") --Main Windows Depot RockShot
+setManifestid(653121, "2435595151501815516", 8766620848)
+addappid(653123, 1, "5fee795c96de2fb8fc5d1a41559d03b12170199b764a74c2eacd547f9b97bc3d") --Main Windows Depot RockShot
+setManifestid(653123, "1085803682714976338", 0)
+addappid(653124, 1, "b47da34616a1f443feba6e1b961f7706cd11d2e5a91f3c72bc1ddee0d17fa429") --Main Windows Depot RockShot
+setManifestid(653124, "2676474854407790928", 19179184)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
