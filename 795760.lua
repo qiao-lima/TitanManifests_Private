b701426c595d1682db3bd0882aea562f5f3030eb
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 08:55:02 -0400

    restock 795760

---
+++ 795760.lua
@@ -0,0 +1,18 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 795760.lua
+--Gamename UPPERS
+addappid(795760) --Mainappid UPPERS
+addappid(795761, 1, "14bf47834557d54fa30c0534b2fcf5171cd81877085541bfcab5ab06741bcf67") --Main Windows Depot UPPERS
+setManifestid(795761, "5424319370738680444", 2155202064)
+--Dlcname UPPERS Artbook
+addappid(1449930, 1, "46cb564224d92ab2bd490b3ec506f485f5a7cff2aebdd691c30f230c83526208") --Dlcname UPPERS Artbook
+setManifestid(1449930, "1427959162863192000", 103225808)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229020, "5799761707845834510", 591984)
