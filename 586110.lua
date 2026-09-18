f7a8fc9d2f3f59e60812c71e207ce6184ce23a01
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 11:41:51 -0400

    restock 586110

---
+++ 586110.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 586110.lua
+--Gamename Sansar
+addappid(586110) --Mainappid Sansar
+addappid(586111, 1, "7b42788ec9757a4101680cb36315382f1b601da5da44080ea98cc3e51e2769c2") --Main Windows Depot Sansar
+setManifestid(586111, "6954404407085749204", 619591920)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
