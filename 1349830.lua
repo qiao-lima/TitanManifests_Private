a6155b1515b837955d6e70e5728d13f6989164b9
titan-cangku <titan-cangku@gamefun365.com>
2026-09-17 20:36:39 -0400

    restock 1349830

---
+++ 1349830.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1349830.lua
+--Gamename RONIN: Two Souls CHAPTER 1
+addappid(1349830) --Mainappid RONIN: Two Souls CHAPTER 1
+addappid(1349831, 1, "012be62c593569b20a806c4fe652fcb49d5b0d9bcd6747d57c9865ff5124d757") --Main Windows Depot RONIN: Two Souls CHAPTER 1
+setManifestid(1349831, "5434767340004985352", 3401736272)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
