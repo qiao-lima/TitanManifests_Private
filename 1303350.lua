2acd6e2ea60e87793e38f38f6a8317690c97319b
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 02:23:30 -0400

    restock 1303350

---
+++ 1303350.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1303350.lua
+--Gamename Rocky Mayhem
+addappid(1303350) --Mainappid Rocky Mayhem
+addappid(1303351, 1, "36aa84a239bdc5d2d1090ae7db234e68773b3cccff26b1d70eca4d000cd90dcf") --Main Windows Depot Rocky Mayhem
+setManifestid(1303351, "909270629281168919", 54710448)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
