a803a3744c59f2b7b293b63ae09308ea376b4aa2
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 12:10:14 -0400

    restock 2060450

---
+++ 2060450.lua
@@ -0,0 +1,19 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2060450.lua
+--Gamename Wanted! Monty Mole
+addappid(2060450) --Mainappid Wanted! Monty Mole
+addappid(2060451, 1, "81a0570edd8bf3493081241a3fcc4e3d963574be6c908414b31865c69a679163") --Main Windows Depot Wanted! Monty Mole
+setManifestid(2060451, "9181277921955637411", 52224)
+addappid(2060452, 1, "a9d5fdf988bce76db56e7553a94b0454b0f0f3cd0ca73b640950c51964d07d7f") --Main Windows Depot Wanted! Monty Mole
+setManifestid(2060452, "2495596460883249826", 750592)
+addappid(2060453, 1, "e15fd9ccd559d48a341b097ab012c181f4e11aacbc7f825b1aafe241eb891114") --Main Windows Depot Wanted! Monty Mole
+setManifestid(2060453, "7211847290805040498", 769536)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(1942051, 1, "fe1ff730b91012f303847522af7b0ec3cd8b46944de3cd3a5967c18e483403ec") --Share Windows Depot Alchemist
+setManifestid(1942051, "8772130629607178472", 437936)
+addappid(1942052, 1, "e17bc02e0eaeec284ac41fc3fad55b920dfe2cd9418911100e0b745dec995547") --Share Windows Depot Alchemist
+setManifestid(1942052, "4371739647487958503", 1554048)
+addappid(1942053, 1, "84db2633f90e0c317bb92b12524bc2b4bc7ad1d757b50122d2b209a6b11b4c5f") --Share Windows Depot Alchemist
+setManifestid(1942053, "3323729397932562790", 1673680)
