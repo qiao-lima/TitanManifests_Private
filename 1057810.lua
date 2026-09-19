d7274442e54a321b931829b5770b4dd7f89edfc9
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 02:55:32 -0400

    restock 1057810

---
+++ 1057810.lua
@@ -0,0 +1,20 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1057810.lua
+--Gamename Wolfenstein: Youngblood Deutsche Version
+addappid(1057810) --Mainappid Wolfenstein: Youngblood Deutsche Version
+addappid(1057811, 1, "7d8b46b2bd4970ae9dabc3a3a990f49330f8f34811f12fb3ee98a3a64c116adc") --Main Windows Depot Wolfenstein: Youngblood Deutsche Version
+setManifestid(1057811, "6820061605114712160", 27128072832)
+addappid(1057812, 1, "ed874035763a08c1f3cd88ce8e7247305040414ac0631662cfc5da3efa761631") --Main Windows Depot Wolfenstein: Youngblood Deutsche Version
+setManifestid(1057812, "465173436051315716", 11216471376)
+addappid(1057813, 1, "b731eaba4a5b116ba2e551baa2fc24671559a87809864a1e7f74db734e0eb0ba") --Main Windows Depot Wolfenstein: Youngblood Deutsche Version
+setManifestid(1057813, "1631123797454483891", 96)
+addappid(1057814, 1, "b47f4a69cdc1800b6a8302bf9b7b2c21b3270e96e820e9d7f56b5f4f9a72ee98") --Main Windows Depot Wolfenstein: Youngblood Deutsche Version
+setManifestid(1057814, "6454762572812340534", 25209328)
+--Share Depots
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1057840) --Dlcname AppID 1057840
+addappid(1057841) --Dlcname AppID 1057841
+addappid(1068000) --Dlcname Wolfenstein: Youngblood Deutsche Version - Gold Bars
