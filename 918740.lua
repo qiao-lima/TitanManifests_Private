53be2c389ab38f394765454483836d4e66f9f6c3
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 10:28:13 -0400

    restock 918740

---
+++ 918740.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 918740.lua
+--Gamename Flat Trip
+addappid(918740) --Mainappid Flat Trip
+addappid(918741, 1, "57b1246a15af14fea2c770b2ca3370af1a9baf62be9b7d018dad9ccc23c1c1ca") --Main Windows Depot Flat Trip
+setManifestid(918741, "7767348374922600949", 105549408)
+addappid(938430, 1, "5f63fc5f84572c96419ea815adbc86d2172c99254262b1d6c5d3d6c276cc2493") --Main Windows Depot Flat Trip
+setManifestid(938430, "1922628935555218815", 71093008)
+--Dlcname Flat Trip OST and Art
+--Share Depots
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
