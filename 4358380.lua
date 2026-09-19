9a5174fe4891c812f96db079007bab91ffe4963d
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 08:30:59 -0400

    restock 4358380

---
+++ 4358380.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4358380.lua
+--Gamename Qianli: The Vastscape Scroll
+addappid(4358380) --Mainappid Qianli: The Vastscape Scroll
+addappid(4358381, 1, "e57c33e0e918d7a2a92a5ce17e9b85c885b2f9cc6d549f1b764edd9352419ecb") --Main Windows Depot Qianli: The Vastscape Scroll
+setManifestid(4358381, "8991003886422163642", 3200631456)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
