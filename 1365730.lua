fe15bc48a7b3d49da462fdea3af5f37e7d32f13b
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 03:44:15 -0400

    restock 1365730

---
+++ 1365730.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1365730.lua
+--Gamename BB Chicken
+addappid(1365730) --Mainappid BB Chicken
+addappid(1365731, 1, "9f0dcbe4a96af1b4a89678331f59e30536837cd6ba26650cbe6e14758dfc0911") --Main Windows Depot BB Chicken
+setManifestid(1365731, "5371128839017767517", 52106656)
+addappid(1365732, 1, "837c5593f0a0ee482b6a9f485a80b28c7ab6fc2537b73347b6fa1f906940cbe4") --Main Windows Depot BB Chicken
+setManifestid(1365732, "5353852903607527566", 52401824)
