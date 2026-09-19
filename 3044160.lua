716d0239b490fa3a8c2c3e89529f610d0b5b89ed
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 03:40:11 -0400

    restock 3044160

---
+++ 3044160.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3044160.lua
+--Gamename 我的刀盾
+addappid(3044160) --Mainappid 我的刀盾
+addappid(3044161, 1, "684ce4723abb25652af97964df875a934b4886bc4ab5fa6c15bc68ec619f15b5") --Main Windows Depot 我的刀盾
+setManifestid(3044161, "4482006545013692047", 1117864048)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229032, 1, "15b3308d4e95d1156182211bab1c823b6df0a06fd74a5c0833abed89bc307cfb") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229032, "3616495131483866412", 40038080)
