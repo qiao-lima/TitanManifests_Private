677ff7efbf1e0317612837b06d7e3aa72f5153fd
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 02:49:00 -0400

    restock 4080220

---
+++ 4080220.lua
@@ -0,0 +1,23 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4080220.lua
+--Gamename EA SPORTS FC™ 27
+addappid(4080220) --Mainappid EA SPORTS FC™ 27
+addappid(4080221, 1, "7049f0ef11b8ebe50c06c4543fe973208ba01b2d8da3069e108627fbf352cce4") --Main Windows Depot EA SPORTS FC™ 27
+setManifestid(4080221, "8878500642775157634", 62117658608)
+addappid(4080261, 1, "82236ba461d7223a5639914e8e3218f4bed8dffa99eaa6e1126eac8302c7d6d8") --Main Windows Language Depot Schinese EA SPORTS FC™ 27
+setManifestid(4080261, "770041676407763391", 1288844224)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(3340991, 1, "023daedb070e5af8704dc88ee0af829f5c11923d2e6a42cca11ba5713b9f4491") --Share Windows Depot EA APP Installer
+setManifestid(3340991, "3755387105551220105", 246722480)
+addappid(3893181, 1, "7f675c2fe8e758d16f3cf8d3b493956afaee78e96d78cdb668a5edb8ac1580f6") --Share Windows Depot EA Javelin Anticheat Installer
+setManifestid(3893181, "4878189275181659469", 317406032)
+addappid(4314910) --Dlcname EA SPORTS FC™ 27 - FC Points
+addappid(4458650) --Dlcname EA SPORTS FC™ 27
+addappid(4458660) --Dlcname EA SPORTS FC™ 27 Standard Edition Pre-Purchase content
+addappid(4458680) --Dlcname EA SPORTS FC™ 27 Ultimate Edition Pre-Purchase content
+addappid(4458690) --Dlcname EA SPORTS FC™ 27 Ultimate Edition Pre-Purchase content
+addappid(4458700) --Dlcname EA SPORTS FC 27 - Press Offer Key
+addappid(4574040) --Dlcname EA SPORTS FC™ 27 Ultimate Plus Edition content
+addappid(4917970) --Dlcname EA SPORTS FC™ 27
