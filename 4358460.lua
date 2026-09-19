9af19c4a11ebfa430f11963a370146e864eeafc8
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 07:02:51 -0400

    restock 4358460

---
+++ 4358460.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4358460.lua
+--Gamename Card Homestead
+addappid(4358460) --Mainappid Card Homestead
+addappid(4358461, 1, "a418ce1cddb623fe5ab91a5efd008777e67127b3059ea063f65b335d15e52b23") --Main Windows Depot Card Homestead
+setManifestid(4358461, "2001630862847315034", 267491376)
