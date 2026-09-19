4427000ef28229dadecc8b375baa4a7bccd54aa6
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 21:42:08 -0400

    restock 4801740

---
+++ 4801740.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4801740.lua
+--Gamename Dude Simulator: My Apologies
+addappid(4801740) --Mainappid Dude Simulator: My Apologies
+addappid(4801741, 1, "deabbdab9a301bece0a2f47e12fe8bb5b31d4b48b103b9403c8adb2f39dfd408") --Main Windows Depot Dude Simulator: My Apologies
+setManifestid(4801741, "5758801702953670351", 412584112)
