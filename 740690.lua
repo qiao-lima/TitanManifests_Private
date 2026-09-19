26ade92aebf2f48bbeff9589b9adba0955e0c97b
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 00:45:26 -0400

    restock 740690

---
+++ 740690.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 740690.lua
+--Gamename Plangman
+addappid(740690) --Mainappid Plangman
+addappid(740691, 1, "8d1f6155abb1937b7c55016d9005b37d6d3c9811732630c0f68f3c54caa27c5a") --Main Windows Depot Plangman
+setManifestid(740691, "7233547484874246501", 20674016)
+addappid(740692, 1, "a79cda672059750a5291837bd25ca779bbd6d92c86d50e2d359c68acaaa2e68a") --Main Macos Depot Plangman
+setManifestid(740692, "9219892773935944829", 34052464)
+addappid(740693, 1, "6913a82cc9cc1974c6a0264877342df0490aad4ba28dcf71a7c5514c7888dc0d") --Main Linux Depot Plangman
+setManifestid(740693, "609689086539512798", 36148704)
+--Missing Dlcs
+--Dlcname Plangman Soundtrack
