f16e591dd00f84fbe6738bed004716cc8f330ba1
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 07:46:22 -0400

    restock 790250

---
+++ 790250.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 790250.lua
+--Gamename Bloodgeon
+addappid(790250) --Mainappid Bloodgeon
+addappid(790251, 1, "f6a3513c763c96602890cbf68ada28b6aa9e670162b5eafd6fd90d7df9e4db52") --Main Windows Depot Bloodgeon
+setManifestid(790251, "1763543685934000151", 137791024)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
