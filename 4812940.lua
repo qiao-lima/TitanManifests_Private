2fc28d10c0c7a12d0d38063f1d556e6f629551a8
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 03:44:09 -0400

    restock 4812940

---
+++ 4812940.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4812940.lua
+--Gamename 你画你猜
+addappid(4812940) --Mainappid 你画你猜
+addappid(4812941, 1, "cd74095a2b0ffad3990f91f77469edda794cd4a9d014153425794fd689d5d8e6") --Main Windows Depot 你画你猜
+setManifestid(4812941, "7107373199702306079", 67130800)
