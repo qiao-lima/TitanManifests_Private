8279c245ebefced215f0b6357d84bc96dc075129
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 10:29:35 -0400

    restock 3659150

---
+++ 3659150.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3659150.lua
+--Gamename 出張2 Next Business Trip
+addappid(3659150) --Mainappid 出張2 Next Business Trip
+addappid(3659151, 1, "af78fc62ad9331ea8b13192b9e5dce3f78e1c037341ae014115507be5d75e64b") --Main Windows Depot 出張2 Next Business Trip
+setManifestid(3659151, "8264575729359079134", 657791600)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
