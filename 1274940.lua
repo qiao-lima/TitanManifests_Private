2923b32da734bce6ef6120224747af280521c57f
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 10:28:18 -0400

    restock 1274940

---
+++ 1274940.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1274940.lua
+--Gamename Strip 'Em
+addappid(1274940) --Mainappid Strip 'Em
+addappid(1274941, 1, "7dff4c8aa52773e0c953c153ee4446e84349224b47337208a1ac6b9795408eba") --Main Windows Depot Strip 'Em
+setManifestid(1274941, "3538721800359845403", 690107376)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
