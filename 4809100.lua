3955cac05e483e378dc0da1169ee3fbcd0d3575f
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 01:40:52 -0400

    restock 4809100

---
+++ 4809100.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4809100.lua
+--Gamename Heartbeat Impact
+addappid(4809100) --Mainappid Heartbeat Impact
+addappid(4809101, 1, "33431ef429fade2e2fe1b07bb05f3389de0c88f6e0d1ab079b748e8e990aad9c") --Main Windows Depot Heartbeat Impact
+setManifestid(4809101, "6920449890072995203", 245141440)
