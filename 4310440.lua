78199eee18c0f21b15a2cac0aa06643b89e7d3d0
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 00:50:46 -0400

    restock 4310440

---
+++ 4310440.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4310440.lua
+--Gamename Crown Siege
+addappid(4310440, 1, "654c8c32e8738da37b388e54edcd0f6674b5a2acd3aa38232ada89fe22242ac5") --Mainappid Crown Siege
+addappid(4310441, 1, "3980b3eb5f96ce793b31f551c1d4d8c8b9885d04f1c44a62b684e578d9b263fe") --Main Windows Depot Crown Siege
+setManifestid(4310441, "7907242799842714864", 158179408)
+addappid(4310442, 1, "3be3890ed757f730847e35526ad6a36a394950a3cd73e74497d8da1012b0b496") --Main Macos Depot Crown Siege
+setManifestid(4310442, "1671100049169537619", 166682816)
+addappid(4310443, 1, "ac99b8fd87edf721f95080172179fa4eac5fec99602c0ed3b3afcb8f951b16c1") --Main Linux Depot Crown Siege
+setManifestid(4310443, "2918589083419820674", 155975424)
