499285254f8b996f31885ba904e45d6a0d7249bf
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 12:38:52 -0400

    restock 4175870

---
+++ 4175870.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4175870.lua
+--Gamename Project: Doors Demo
+addappid(4175870) --Mainappid Project: Doors Demo
+addappid(4175871, 1, "caa4fdcaf10b7bee4778f26f716c24004f17e761e88c51b7261da1e0519a6e78") --Main Windows Depot Project: Doors Demo
+setManifestid(4175871, "4127798768597696258", 1767422208)
