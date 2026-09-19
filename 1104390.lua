8c3fc1d17c0ebe8c7d50f59ebcd1bd49f0b48073
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 22:10:26 -0400

    restock 1104390

---
+++ 1104390.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1104390.lua
+--Gamename Ravenfield: Multiplayer Mod
+addappid(1104390) --Mainappid Ravenfield: Multiplayer Mod
+addappid(1104391, 1, "f25fe3b050eb54fd7b9e20831047b9d4fbe5da9297b461916762b10a8424e45f") --Main Windows Language Depot English Ravenfield: Multiplayer Mod
+setManifestid(1104391, "7482028812178957966", 162272320)
+addappid(1104392, 1, "9e1f33531a3c68ee9728499434eb86e187bec95fbdbf7dea900ebd671cd5b8aa") --Main Macos Language Depot English Ravenfield: Multiplayer Mod
+setManifestid(1104392, "295401077184191848", 173857872)
+addappid(1104393, 1, "8e850ef53bc6d63d174e89963348f6f69c41e0eb9359c7869b639ee7c42dc131") --Main Linux Language Depot English Ravenfield: Multiplayer Mod
+setManifestid(1104393, "3498652988938284279", 187198608)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
