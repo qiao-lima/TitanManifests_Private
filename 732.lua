8e3570e5dda3bd8b1950e8c3619df818b5128bf9
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 07:03:15 -0400

    restock 732

---
+++ 732.lua
@@ -0,0 +1,37 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 732.lua
+--Gamename AppID 732
+addappid(732, 1, "da1f76913633e9ce1b2bda5ec464dc507205388fac5c4c614b6a2706cdbd0912") --Mainappid AppID 732
+setManifestid(732, "2849850780169022159", 64)
+addappid(733, 1, "23c8e9a91561a5fb08abf22f2ac98929f4aad68a12164268d3b2800c3db4fb03") --Main Macos Depot AppID 732
+setManifestid(733, "7479871044339759718", 64)
+--Dlcname Counter-Strike 2
+addappid(730, 1, "2e61570111ad4d1e4abcb711576f96b84907a024f9f9bf4a4d692d7c132ee91b") --Dlcname Counter-Strike 2
+addappid(731, 1, "bca9a9cde94bb4dff61849c6a87230ee45867a590fdd28826366e35e7d62c08e") --Dlc Windows Depot Counter-Strike 2
+setManifestid(731, "6436761910375028561", 64)
+addappid(734, 1, "54fd243c5463c16ec7d32b5b4c7b792b3f2d3f75009ac3e81135ff999b56187f") --Dlc Linux Depot Counter-Strike 2
+setManifestid(734, "6690307387168637445", 64)
+addappid(735, 1, "a0ef69f0cf8abd70aa9ed79755fbe4d98cf7c6723ba9e0c66e0f7102e49541e6") --Dlc Windows Depot Counter-Strike 2
+setManifestid(735, "2430139882439017832", 64)
+addappid(736, 1, "e81988c241b4ec9fc7fe1ab34697689f131a48c78da95c985ed347fb43bdc6de") --Dlc Windows Depot Counter-Strike 2
+setManifestid(736, "5751260873739093822", 64)
+addappid(737, 1, "c80f96efa325374c18c6d5d63ccd2687b1680a908c6def5c5c808e695f1ddbf9") --Dlc Windows Depot Counter-Strike 2
+setManifestid(737, "172338446784131635", 64)
+addappid(738, 1, "fbc1a292c9b75e33512ed82476267efbf34f1ed1b177651ed8b091cf7174d183") --Dlc Windows Depot Counter-Strike 2
+setManifestid(738, "2019725392635854178", 64)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Dlc Windows Depot Counter-Strike 2
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Dlc Windows Depot Counter-Strike 2
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2347770, 1, "b23a737920b6a72f932a5bcdbdf51770d7d4d394a13f95b1cf29db28c1043d88") --Dlc Windows Depot Counter-Strike 2
+setManifestid(2347770, "2053759441494650084", 53900780944)
+addappid(2347771, 1, "b80e2b4bd2a244ba996a013850b5d2f46c897f382164181b382c5d672fcb5090") --Dlc Windows Depot Counter-Strike 2
+setManifestid(2347771, "5806169188224907599", 4995013808)
+addappid(2347772, 1, "7938abf64ef719e6414d5e385f90ccbef9c6a2c21ab42b471a09641d2b2ba32f") --Dlc Macos Depot Counter-Strike 2
+setManifestid(2347772, "4427303554736365309", 1696)
+addappid(2347773, 1, "d030f2b7078446257db4b4decd86a38b0ae3816b4847a04e609277e20f25d5d7") --Dlc Linux Depot Counter-Strike 2
+setManifestid(2347773, "8639120305802825922", 4604239936)
+addappid(2347774, 1, "71e79678830ea0234437b03526e2ede2decfed847ce20bae413718b47db44bea") --Dlc Windows Depot Counter-Strike 2
+setManifestid(2347774, "6880616264887178762", 949604816)
+addappid(2347779, 1, "a3d6504606875447dc6dedcba78bebb3e0a122fb30a504ef805536f216354271") --Dlc Windows Depot Counter-Strike 2
+setManifestid(2347779, "2145418671218218617", 582148112)
