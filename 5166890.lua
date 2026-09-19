495458f06c702a4346ad38125a10de6208d3b792
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 08:38:59 -0400

    restock 5166890

---
+++ 5166890.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5166890.lua
+--Gamename Post Office: Tidy Up!
+addappid(5166890, 1, "2605e7864985207704ba20ae8ad1558cb945ee995f9b4d8bc400a30a1057fbf6") --Mainappid Post Office: Tidy Up!
+addappid(5166891, 1, "2605e7864985207704ba20ae8ad1558cb945ee995f9b4d8bc400a30a1057fbf6") --Main Windows Depot Post Office: Tidy Up!
+addappid(5166892, 1, "24ed62bf973d83323e1569cd8e753d9e968d5a4261e469dd04f55e67028156e5") --Main Windows Depot Post Office: Tidy Up!
+setManifestid(5166892, "7643861563062999650", 93843472)
