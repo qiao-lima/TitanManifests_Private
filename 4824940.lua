6d2914c05abb879a0f1fa675c52f43d6a378eee5
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 09:04:52 -0400

    restock 4824940

---
+++ 4824940.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4824940.lua
+--Gamename XINGTU
+addappid(4824940) --Mainappid XINGTU
+addappid(4824941, 1, "a7d0d66b9e48d13f6ec660c257cd9f718b3a82816efbdc874ecd918097443be9") --Main Windows Language Depot Schinese XINGTU
+setManifestid(4824941, "3665387142379315757", 4336069648)
