9de83231149162a0c228688413c4aede2578e3cf
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 05:30:01 -0400

    restock 1826980

---
+++ 1826980.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1826980.lua
+--Gamename FreestyleFootball R
+addappid(1826980) --Mainappid FreestyleFootball R
+addappid(1826981, 1, "a671259d25610c370b20709bafa39defa2f0d63a7d8d4f2e7d3b7e44e6cb63ea") --Main Windows Depot FreestyleFootball R
+setManifestid(1826981, "3723887692815257359", 2493734416)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
