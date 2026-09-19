4a35094e661f410675fe9726455f6408fa0254f1
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 23:39:50 -0400

    restock 1674340

---
+++ 1674340.lua
@@ -0,0 +1,20 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1674340.lua
+--Gamename 仙侠世界2
+addappid(1674340) --Mainappid 仙侠世界2
+addappid(1674341, 1, "d9685ae72013ab52c65a29b7f72c5be61cba3256ad06145dcb966431bd9f3eca") --Main Windows Language Depot Schinese 仙侠世界2
+setManifestid(1674341, "4284777602971487012", 20891115760)
+--Share Depots
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+--Missing Dlcs
+--Dlcname 仙侠世界2-神武神龙版
+--Dlcname 仙侠世界2-神武麒麟版
+--Dlcname 仙侠世界2-神武暴熊版
+--Dlcname 仙侠世界2-神武仙鹤版
+addappid(1873680) --Dlcname AppID 1873680
+addappid(1873690) --Dlcname AppID 1873690
+addappid(1873700) --Dlcname AppID 1873700
+addappid(1873710) --Dlcname AppID 1873710
