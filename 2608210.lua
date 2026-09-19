7c7a2e390a668026bd17b5c6119777674fdc3d5d
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 07:50:07 -0400

    restock 2608210

---
+++ 2608210.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2608210.lua
+--Gamename BLOODKILL
+addappid(2608210) --Mainappid BLOODKILL
+addappid(2608211, 1, "03191d4178fb0483dfc03cb473e0f6910fe8ca6d3e5d1ebd4f9b76a913b48082") --Main Windows Depot BLOODKILL
+setManifestid(2608211, "4779273948003087613", 5782838496)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
