01f0f80b72a943a2c599beeb7cbbc3646126db06
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 07:01:47 -0400

    restock 49

---
+++ 49.lua
@@ -0,0 +1,46 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 49.lua
+--Gamename AppID 49
+addappid(49) --Mainappid AppID 49
+addappid(50, 1, "6e0381daf27e6b6a25feb5d16230d35236aaa6ec3fa65a2739baa8a222ce256e") --Main Windows Depot AppID 49
+--Dlcname Half-Life: Opposing Force
+addappid(1, 1, "b465d45ab2a7c396f7d1c08a6644e68529ec86b14da77e18588abbbcd2412060") --Dlc Windows Depot Half-Life: Opposing Force
+setManifestid(1, "2667117727132184734", 264641152)
+addappid(2, 1, "fd681c065529f5ee95e920321a780a9290f8f31ef51c232ccb3e98d27e132ebc") --Dlc Windows Depot Half-Life: Opposing Force
+setManifestid(2, "724247850320422500", 19297632)
+addappid(3, 1, "d249f7d93387a793ed021f0e2314ad0cea0d6aef0b0881c3b725a33deec37d59") --Dlc Windows Depot Half-Life: Opposing Force
+setManifestid(3, "6081070194444336449", 148112)
+addappid(6, 1, "cd3d8b6f23fb863aea4d6789a53dd58dcf284141860e893e8e580d1988d93d3c") --Dlc Windows Depot Half-Life: Opposing Force
+setManifestid(6, "8518478957342391261", 27950128)
+addappid(8, 1, "20af1043db56d6408ddd965580e1269005b6e04f81c3676d155174f73099da95") --Dlc Linux Depot Half-Life: Opposing Force
+setManifestid(8, "1360709870205537514", 42477296)
+addappid(9, 1, "68c92cf2532cdc6e0b7a9d2783b473b6d16bac72218badf68d7be9b033076096") --Dlc Macos Depot Half-Life: Opposing Force
+setManifestid(9, "1083345737969856561", 19253680)
+addappid(51, 1, "cfb8898867eae1901bce2e85444f2cf5c689d4806c137692f0f370868d0b4f03") --Dlc Windows Depot Half-Life: Opposing Force
+setManifestid(51, "789184054796507140", 147668032)
+addappid(52, 1, "9f4f1e296fdb38bfbc27c771600e598198839f9503135a4b2b813536c48c516c") --Dlc Windows Language Depot German Half-Life: Opposing Force
+setManifestid(52, "5528482501962900096", 18320000)
+addappid(53, 1, "3ff910cb008ad4571814eb9367e42406cc39e0e6096ed428cdc5b182bb7874b4") --Dlc Windows Language Depot French Half-Life: Opposing Force
+setManifestid(53, "5763450929861348423", 19131840)
+addappid(56, 1, "4f56f187e00ad0aafbc1a33ce5e8f39c68ffd641e19596fb63395ec0fb71c758") --Dlc Windows Language Depot Koreana Half-Life: Opposing Force
+setManifestid(56, "6741694038486968355", 348112)
+addappid(72, 1, "bbc1a14b0c63025aece7d9c3b5a98089205580f321bb5edf6b5f00d368c6face") --Dlc Windows Language Depot French Half-Life: Opposing Force
+setManifestid(72, "491735997354925434", 29617440)
+addappid(73, 1, "9604a5ba9ad4b580269d5362e201d217cdd9d268bf1098e2c6beb0c5ade020d3") --Dlc Windows Language Depot Italian Half-Life: Opposing Force
+setManifestid(73, "3587203030189636184", 36527248)
+addappid(74, 1, "49d573f303205df2316749a6ede6725b74651fae3e845494b88ac3d0022a2857") --Dlc Windows Language Depot German Half-Life: Opposing Force
+setManifestid(74, "6929797608257779911", 35264800)
+addappid(75, 1, "1339202607b6485322859fc1f045c6790bbc470a2d3bb8158aa94b06da31f9bb") --Dlc Windows Language Depot Spanish Half-Life: Opposing Force
+setManifestid(75, "6599379689843151742", 37926368)
+addappid(77, 1, "793780217977cc3a005dc4410e963c980137e4faca4442f92dc41ab56af96983") --Dlc Windows Language Depot Schinese Half-Life: Opposing Force
+setManifestid(77, "8307934900153578333", 32815776)
+addappid(78, 1, "39f04a0cc8cffae8023eff1e3e933680681997409823942a5f0f2899d2d2c4b2") --Dlc Windows Language Depot Koreana Half-Life: Opposing Force
+setManifestid(78, "2582370374354695075", 33761760)
+addappid(79, 1, "019e1cb0950e060fb992001657a444fe03cbc4984081e0d1d9a6505259a7a7e7") --Dlc Windows Language Depot Tchinese Half-Life: Opposing Force
+setManifestid(79, "6488358189498321283", 32815632)
+addappid(96, 1, "aa927e7fc0b2d0ff4a2b681657e816ef56995ec76aa99069ebdc1b01d8d226af") --Dlc Windows Depot Half-Life: Opposing Force
+setManifestid(96, "6298465564582633871", 5963504)
+addappid(141, 1, "0d85b74e163cceabf3651fdd6084585587461f6f7735c37eef875634fc6886cb") --Dlc Windows Language Depot Russian Half-Life: Opposing Force
+setManifestid(141, "3921686991280371259", 10352)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Dlc Windows Depot Half-Life: Opposing Force
+setManifestid(228988, "6645201662696499616", 22411856)
