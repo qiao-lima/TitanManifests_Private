18a65f8d16712816f6056478262573640296f0b6
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 09:14:07 -0400

    restock 4038570

---
+++ 4038570.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4038570.lua
+--Gamename 购物车管理员
+addappid(4038570) --Mainappid 购物车管理员
+addappid(4038571, 1, "f52398bb4d991a0a6586d5721fdbf8f3b1857a90e7f1b0ad0ef1559d11cf14e1") --Main Windows Depot 购物车管理员
+setManifestid(4038571, "1216231476406724723", 292764640)
