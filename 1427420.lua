c43c1f319d0b89856270e24785edbafb2d778154
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 07:50:02 -0400

    restock 1427420

---
+++ 1427420.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1427420.lua
+--Gamename Bloodland
+addappid(1427420) --Mainappid Bloodland
+addappid(1427421, 1, "da86c00d628d2c6f2e2ba5f29eb27912b09a48e379cd19db42bbb4718b79b1ff") --Main Windows Depot Bloodland
+setManifestid(1427421, "383742565822949877", 1580986224)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
