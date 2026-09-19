1f9524c4b8c8b2475f59431913cc9e7b80253b64
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 20:20:01 -0400

    restock 1630280

---
+++ 1630280.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1630280.lua
+--Gamename R2Beat: 音速觉醒
+addappid(1630280) --Mainappid R2Beat: 音速觉醒
+addappid(1630281, 1, "9d3db1527a848c41d838da2046ec9a7fe3cf5f40c511ef4b9e522d71d6118d22") --Main Windows Depot R2Beat: 音速觉醒
+setManifestid(1630281, "6445163827929490405", 13521849392)
+--Share Depots
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228982, "6413394087650432851", 9650016)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
