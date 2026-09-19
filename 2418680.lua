7f15af51d62514e71a915b7a9877664ec0a6bc55
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 22:06:42 -0400

    restock 2418680

---
+++ 2418680.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2418680.lua
+--Gamename Revenguard
+addappid(2418680) --Mainappid Revenguard
+addappid(2418681, 1, "06ab765c957e705110d8434cfb36c03268fff884f9a997882407a0b6d5c12511") --Main Windows Depot Revenguard
+setManifestid(2418681, "5791567517894566460", 147730720)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
