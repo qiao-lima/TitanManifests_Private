05048e688608d537f829d675923455e39e00bb20
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 23:26:59 -0400

    restock 3585360

---
+++ 3585360.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3585360.lua
+--Gamename 九渊
+addappid(3585360) --Mainappid 九渊
+addappid(3585471, 1, "4ea01bfe8e975b17862307b4bf81cae49b801768beee1bd57147ea39085f3cf9") --Main Windows Depot 九渊
+setManifestid(3585471, "6724065261922990", 1302074544)
+--Dlcname 九渊 Playtest
+addappid(3585470) --Dlcname 九渊 Playtest
