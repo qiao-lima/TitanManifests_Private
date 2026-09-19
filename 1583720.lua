cdf2fc6e976974e83b9d6322449d90d45cf4b013
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 22:21:16 -0400

    restock 1583720

---
+++ 1583720.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1583720.lua
+--Gamename Entropy : Zero 2
+addappid(1583720, 1, "6dca9d331195526081033b474dc9d7f8e1f92dd12e84e6a1da5278c7d7958e82") --Mainappid Entropy : Zero 2
+addappid(1583721, 1, "5557bc8565d9e8785c7b3905b86faf8b5614b9dca33feb5002d16c9671947761") --Main Windows Depot Entropy : Zero 2
+setManifestid(1583721, "348948426661780131", 6596186832)
+addappid(1583722, 1, "d42338c0e8e8d4d2539f838a82c9574e5a8cdb0a9d148e0dc01a21a92e59ea65") --Main Linux Depot Entropy : Zero 2
+setManifestid(1583722, "4062498511763705580", 36455952)
+addappid(1583723, 1, "683ae974b6ca3e060888ff6f56206a42a01d4f4a158e397bdf39301432c40c15") --Main Windows Depot Entropy : Zero 2
+setManifestid(1583723, "7387651231230992010", 47852208)
+--Dlcname Entropy : Zero 2 Soundtrack
+addappid(2143080) --Dlcname Entropy : Zero 2 Soundtrack
+addappid(2143081, 1, "5e8230c877b5c9c4bb0e1e8bd37da05cd402a7dce73d85ebaadf4b14f58b5691") --Dlc Windows Depot Entropy : Zero 2 Soundtrack
+setManifestid(2143081, "444103255941205270", 188702224)
