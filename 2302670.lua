2b559e55ff2b09aea507b2238386af8dceb7bc44
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 22:16:45 -0400

    restock 2302670

---
+++ 2302670.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2302670.lua
+--Gamename 重返越南
+addappid(2302670) --Mainappid 重返越南
+addappid(2302671, 1, "43cd5065e5cd157ce5400ea4c54f98eb92631ff3a9146515f810232cbaa8952a") --Main Windows Depot 重返越南
+setManifestid(2302671, "7684269951654372265", 5509928784)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229000, "4622705914179893434", 242681440)
