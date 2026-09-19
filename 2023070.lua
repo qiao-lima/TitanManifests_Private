23328c03c969f5e34e5d7223cb30af091002650e
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 09:31:16 -0400

    restock 2023070

---
+++ 2023070.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2023070.lua
+--Gamename Mine Seeker
+addappid(2023070) --Mainappid Mine Seeker
+addappid(2023071, 1, "579b07b5c73f952a9822dac82fc376cc0a157244d6e53f3a81a550f789d58219") --Main Windows Language Depot English Mine Seeker
+setManifestid(2023071, "719658983064515537", 1248667904)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
