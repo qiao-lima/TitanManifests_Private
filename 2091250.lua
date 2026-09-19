a3055ef39393aa46c68a86e02f3a94f69c45ff3f
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 10:24:21 -0400

    restock 2091250

---
+++ 2091250.lua
@@ -0,0 +1,17 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2091250.lua
+--Gamename 旅行こいし: デスクトップ少女 ~ Koishi Navigation Desktop Youkai
+addappid(2091250) --Mainappid 旅行こいし: デスクトップ少女 ~ Koishi Navigation Desktop Youkai
+addappid(2091251, 1, "070bc5bd81c704e395c3725b9c0f5f10f29e61e7d532bfc15d27db5af226046c") --Main Windows Depot 旅行こいし: デスクトップ少女 ~ Koishi Navigation Desktop Youkai
+setManifestid(2091251, "9072258115993516596", 166577040)
+--Dlcname 旅行恋恋Art Book: 恋恋下午茶喂养指南
+addappid(2180320) --Dlcname 旅行恋恋Art Book: 恋恋下午茶喂养指南
+addappid(2180321, 1, "5fb0c3e8d2752c1da28fd7baf1dc33d84e2d24bd2548af8f7db0a3cdb7d38b2c") --Dlc Windows Depot 旅行恋恋Art Book: 恋恋下午茶喂养指南
+setManifestid(2180321, "5162097556259818758", 283084528)
+--Dlcname 旅行恋恋Art Book: 恋恋新天地喂养指南
+addappid(3617260) --Dlcname 旅行恋恋Art Book: 恋恋新天地喂养指南
+addappid(3617261, 1, "b134c0a62bf43bf0c3fcb98accd36064e0f6388e7fbb82b60889ccb365d53923") --Dlc Windows Depot 旅行恋恋Art Book: 恋恋新天地喂养指南
+setManifestid(3617261, "8853754793931855572", 38870672)
+--Share Depots
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
