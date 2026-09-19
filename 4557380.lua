b60780f32c8f724ae7b2b7396611bb879e2fc982
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 04:32:21 -0400

    restock 4557380

---
+++ 4557380.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4557380.lua
+--Gamename Batomon Showdown
+addappid(4557380, 1, "8a26e9c8803617d23e9b122df8a3ce079b69239c3eb73a7723f4f2f7cd07a6a3") --Mainappid Batomon Showdown
+addappid(4557381, 1, "f91cfea9850309980846d66c044e3be2aa9157556b52ec32e3d456f8152aee13") --Main Windows Depot Batomon Showdown
+setManifestid(4557381, "549568348540740525", 212678208)
+addappid(5255370) --Dlcname Batomon Showdown - Supporter Pack
