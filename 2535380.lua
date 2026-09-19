56606f2cdc5eb773eee3a7c56fa85d56d629aec9
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 09:04:48 -0400

    restock 2535380

---
+++ 2535380.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2535380.lua
+--Gamename 星火
+addappid(2535380) --Mainappid 星火
+addappid(2535381, 1, "81b53ba532066b7626aeafb4826888b2d498a1ee70a6bb485a0a0fb678939dd4") --Main Windows Depot 星火
+setManifestid(2535381, "5206566207271605714", 76359312)
