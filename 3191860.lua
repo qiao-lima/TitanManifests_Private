646b7089e2ba7f8136f3e3e1dfb587aee0213f22
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 00:00:01 -0400

    restock 3191860

---
+++ 3191860.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3191860.lua
+--Gamename Cult -Vein-
+addappid(3191860) --Mainappid Cult -Vein-
+addappid(3191861, 1, "72eaa995a9af09ffafe7d8c2e8537e1d1cbe56d90ca94ae759475f60666fcd4d") --Main Windows Depot Cult -Vein-
+setManifestid(3191861, "4518227337848583681", 3866062240)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
