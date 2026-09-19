445401e1552af5eb0de3d48438a5abdfa00d0ccb
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 00:52:28 -0400

    restock 1341060

---
+++ 1341060.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1341060.lua
+--Gamename Half-Life 2: Genry's Great Escape From City 13
+addappid(1341060) --Mainappid Half-Life 2: Genry's Great Escape From City 13
+addappid(1341061, 1, "5319e96f52bb9c254e72a71af2119e57c0193d4283fcef6f0c0d6dd313574458") --Main Windows Depot Half-Life 2: Genry's Great Escape From City 13
+setManifestid(1341061, "2823321939324568716", 3473483808)
