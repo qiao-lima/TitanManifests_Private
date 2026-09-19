f334036b8f159cb4907fe7e59b8dadfe8e9625c1
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 07:30:42 -0400

    restock 1330290

---
+++ 1330290.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1330290.lua
+--Gamename READY AIM FIRE
+addappid(1330290) --Mainappid READY AIM FIRE
+addappid(1330291, 1, "44173e587667bad322cd5e9f798791f6989914d11821806e33ee647210ec4534") --Main Windows Depot READY AIM FIRE
+setManifestid(1330291, "4224466193092200577", 286432176)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229033, 1, "9a789440bbc16131547ac6c062e5f168f7684da533a2899a71fdfaaeec21efc3") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229033, "2059065101492814639", 59325760)
