ef2f0c2f4926ec7a91c1b48914b86847f5459b4f
titan-cangku <titan-cangku@gamefun365.com>
2026-09-18 22:19:17 -0400

    restock 2765320

---
+++ 2765320.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2765320.lua
+--Gamename 天临混元
+addappid(2765320) --Mainappid 天临混元
+addappid(2765321, 1, "5eec5890b7ec60429f0efbbc22d5434d42f1c13ad31fd9ad5388521cee770f85") --Main Windows Depot 天临混元
+setManifestid(2765321, "8958680281006725866", 11207421248)
