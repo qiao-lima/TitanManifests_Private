98599d8c36c52003bea24c85d7ea6923b1fabc2d
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 07:35:07 -0400

    restock 717090

---
+++ 717090.lua
@@ -0,0 +1,17 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 717090.lua
+--Gamename Game-Ready Assets
+addappid(717090) --Mainappid Game-Ready Assets
+addappid(717091, 1, "faa7fec07af4a0ef9fbf78fa7659e725991d4c548858418973aa32a6218d5d89") --Main Windows Depot Game-Ready Assets
+setManifestid(717091, "3354178444889959860", 162390448)
+addappid(718290, 1, "4cb97966f367fa7c3eebd2ebfafcc5b7c4c83fa32d31e757725706be9d615d19") --Dlcname AppID 718290
+setManifestid(718290, "1525237977366471705", 1024314560)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+--Missing Dlcs
+--Dlcname Game-Ready - Misc Collection
+addappid(717130) --Dlcname AppID 717130
+addappid(719920) --Dlcname AppID 719920
