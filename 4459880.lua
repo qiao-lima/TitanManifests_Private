631a2dcfc40efb3eaa93e80263c40da2b91a10bd
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 06:40:45 -0400

    restock 4459880

---
+++ 4459880.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4459880.lua
+--Gamename Homura Hime Official Soundtrack
+addappid(4459880) --Mainappid Homura Hime Official Soundtrack
+addappid(4459881, 1, "c465d3d76857a5852e9fb4001c0eba4a27af519f8d32ef81febcb31b5c055e7e") --Main Windows Depot Homura Hime Official Soundtrack
+setManifestid(4459881, "7561405978316044721", 405716896)
+addappid(4459882, 1, "daa8082047121ead4d30e8980badb514a55d6215583e4d6aba636de6df6135dc") --Main Windows Depot Homura Hime Official Soundtrack
+setManifestid(4459882, "4321428366436860380", 1682863472)
