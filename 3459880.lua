d705867e7949aef4db426f620975187c64219b3f
titan-cangku <titan-cangku@gamefun365.com>
2026-09-19 01:05:06 -0400

    restock 3459880

---
+++ 3459880.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3459880.lua
+--Gamename FantasyJourney
+addappid(3459880) --Mainappid FantasyJourney
+addappid(3459881, 1, "37033ec64da43c9acb4b67804daf8785717abdb9469335da88a7a14154b218d6") --Main Windows Depot FantasyJourney
+setManifestid(3459881, "8526460976563624623", 654839232)
